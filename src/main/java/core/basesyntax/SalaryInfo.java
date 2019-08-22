f486ed9bc2d49e4ee4360272bad8a3a471c4cc13
Саша Бунин <sasha192.bunin@gmail.com>
2019-08-22 23:54:47 +0300

    Update SalaryInfo.java

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,5 +1,15 @@
 package core.basesyntax;
 
+
+import java.lang.ArrayIndexOutOfBoundsException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
+
+
 public class SalaryInfo {
     /**
      * Реализуйте метод getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo)
@@ -10,14 +20,11 @@ public class SalaryInfo {
      * ставка за 1 час. Метод должен вернуть отчёт за период, который передали в метод
      * (обе даты включительно) составленный по следующей форме: Отчёт за период
      * #дата_1# - #дата_2# Имя сотрудника - сумма заработанных средств за этот период
-     *
      * Пример ввода: date from = 01.04.2019 date to = 30.04.2019
-     *
      * names:
      * Сергей
      * Андрей
      * София
-     *
      * data:
      * 26.04.2019 Сергей 60 50
      * 26.04.2019 Андрей 3 200
@@ -28,7 +35,6 @@ public class SalaryInfo {
      * 26.04.2019 Сергей 7 100
      * 26.04.2019 София 9 100
      * 26.04.2019 Сергей 11 50
-     *
      * Пример вывода:
      * Отчёт за период 01.04.2019  - 30.04.2019
      * Сергей - 1550
@@ -36,6 +42,39 @@ public class SalaryInfo {
      * София - 900
      */
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        HashMap<String, Integer> salary = new HashMap<String, Integer>(names.length);
+        DateFormat format = new SimpleDateFormat("dd.MM.yyyy", Locale.ENGLISH);
+        for (String name : names) {
+            salary.put(name, 0);
+        }
+        try {
+            Date end = format.parse(dateTo);
+            Date start = format.parse(dateFrom);
+            if (start.after(end)) {
+                return null;
+            }
+            for (int i = 0; i < data.length; i++) {
+                String[] splitString;
+                Date date;
+                splitString = data[i].split(" ");
+                date = format.parse(splitString[0]);
+                if (!date.after(end) && !date.before(start)) {
+                    salary.put(splitString[1], salary.get(splitString[1])
+                            + Integer.parseInt(splitString[2]) * Integer.parseInt(splitString[3]));
+                }
+            }
+        } catch (ParseException exc1) {
+            exc1.printStackTrace();
+        } catch (ArrayIndexOutOfBoundsException exc2) {
+            exc2.printStackTrace();
+        }
+        StringBuilder returnString = new StringBuilder("Отчёт за период "
+                .concat(dateFrom)
+                .concat(" - ")
+                .concat(dateTo + '\n'));
+        for (int i = 0; i < names.length; i++) {
+            returnString.append(names[i] + " - " + salary.get(names[i]) + '\n');
+        }
+        return returnString.toString();
     }
 }
