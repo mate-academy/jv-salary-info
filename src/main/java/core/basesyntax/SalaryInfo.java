a33b860d682c6f3c777c7cda19aae6c1fe79da65
Саша Бунин <sasha192.bunin@gmail.com>
2019-08-23 00:21:49 +0300

    Update SalaryInfo.java

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,14 +1,14 @@
 package core.basesyntax;
 
-
 import java.lang.ArrayIndexOutOfBoundsException;
+
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Locale;
-
 
 public class SalaryInfo {
     /**
