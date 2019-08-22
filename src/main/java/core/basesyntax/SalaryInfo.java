d9da9479a56ae4550af1b2c166443440de8011fe
AndreyDMA <andreydmitriev@ukr.net>
2019-08-22 16:58:10 +0300

    first try solution

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -10,25 +10,21 @@ public class SalaryInfo {
      * ставка за 1 час. Метод должен вернуть отчёт за период, который передали в метод
      * (обе даты включительно) составленный по следующей форме: Отчёт за период
      * #дата_1# - #дата_2# Имя сотрудника - сумма заработанных средств за этот период
-     *
      * Пример ввода: date from = 01.04.2019 date to = 30.04.2019
-     *
      * names:
      * Сергей
      * Андрей
      * София
-     *
      * data:
      * 26.04.2019 Сергей 60 50
      * 26.04.2019 Андрей 3 200
-     * 26.04.2019 Сергей 7 100
-     * 26.04.2019 София 9 100
-     * 26.04.2019 Сергей 11 50
-     * 26.04.2019 Андрей 3 200
-     * 26.04.2019 Сергей 7 100
-     * 26.04.2019 София 9 100
-     * 26.04.2019 Сергей 11 50
-     *
+     * 27.04.2019 Сергей 7 100
+     * 28.04.2019 София 9 100
+     * 28.04.2019 Сергей 11 50
+     * 28.04.2019 Андрей 3 200
+     * 29.04.2019 Сергей 7 100
+     * 29.04.2019 София 9 100
+     * 30.04.2019 Сергей 11 50
      * Пример вывода:
      * Отчёт за период 01.04.2019  - 30.04.2019
      * Сергей - 1550
@@ -36,6 +32,27 @@ public class SalaryInfo {
      * София - 900
      */
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        if (Integer.valueOf(dateFrom.replaceAll("[^0-9]",""))
+                > Integer.valueOf(dateTo.replaceAll("[^0-9]",""))) {
+            return null;
+        }
+        String header = "Отчёт за период " + dateFrom + " - " + dateTo + "\n";
+        String list = "";
+        String[] stringData;
+        for (String n : names) {
+            int salary = 0;
+            for (int i = 0; i < data.length; i++) {
+                stringData = data[i].split(" ");
+                if (Integer.valueOf(stringData[0].replaceAll("[^0-9]",""))
+                        > Integer.valueOf(dateTo.replaceAll("[^0-9]",""))) {
+                    break;
+                }
+                if (n.equals(stringData[1])) {
+                    salary += Integer.valueOf(stringData[2]) * Integer.valueOf(stringData[3]);
+                }
+            }
+            list += n + " - " + salary + "\n";
+        }
+        return header + list;
     }
 }
