cc2ca47cf4fc184b116dc42dcfe703816bc97d2b
NikolayKatsai <alaska.rok123@gmail.com>
2019-08-22 23:36:35 +0300

    getSalaryInfo method finished in SalaryInfo class

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,5 +1,7 @@
 package core.basesyntax;
 
+import java.util.Date;
+
 public class SalaryInfo {
     /**
      * Реализуйте метод getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo)
@@ -10,14 +12,14 @@ public class SalaryInfo {
      * ставка за 1 час. Метод должен вернуть отчёт за период, который передали в метод
      * (обе даты включительно) составленный по следующей форме: Отчёт за период
      * #дата_1# - #дата_2# Имя сотрудника - сумма заработанных средств за этот период
-     *
+     * <p>
      * Пример ввода: date from = 01.04.2019 date to = 30.04.2019
-     *
+     * <p>
      * names:
      * Сергей
      * Андрей
      * София
-     *
+     * <p>
      * data:
      * 26.04.2019 Сергей 60 50
      * 26.04.2019 Андрей 3 200
@@ -28,7 +30,7 @@ public class SalaryInfo {
      * 26.04.2019 Сергей 7 100
      * 26.04.2019 София 9 100
      * 26.04.2019 Сергей 11 50
-     *
+     * <p>
      * Пример вывода:
      * Отчёт за период 01.04.2019  - 30.04.2019
      * Сергей - 1550
@@ -36,6 +38,31 @@ public class SalaryInfo {
      * София - 900
      */
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        if (Integer.parseInt(dateTo.substring(0, 2))
+                < Integer.parseInt(dateFrom.substring(0, 2))) {
+            return null;
+        }
+        String infoString = "";
+        infoString += "Отчёт за период " + dateFrom + " - " + dateTo + "\n";
+        for (int i = 0; i < names.length; i++) {
+            int currentWorkerSalary = 0;
+            for (String nextData : data) {
+                String[] parsedData = nextData.split(" ");
+                if (Integer.parseInt(dateTo.substring(0, 2))
+                        < Integer.parseInt(parsedData[0].substring(0, 2))) {
+                    break;
+                }
+                if (names[i].equals(parsedData[1])) {
+                    currentWorkerSalary += Integer.parseInt(parsedData[2])
+                            * Integer.parseInt(parsedData[3]);
+                }
+
+            }
+            infoString += names[i] + " - " + currentWorkerSalary;
+            if (i < names.length) {
+                infoString += "\n";
+            }
+        }
+        return infoString;
     }
 }
