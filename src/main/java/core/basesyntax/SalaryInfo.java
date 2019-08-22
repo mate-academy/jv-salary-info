56ae18f92a4376760b50a0b3524faf9089670c91
joej0e <36197513+joej0e@users.noreply.github.com>
2019-08-22 19:20:56 +0300

    Update SalaryInfo.java

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,39 +1,32 @@
 package core.basesyntax;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class SalaryInfo {
 
-    static String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        List<String[]> nameAndMoney = new ArrayList<>();
+    protected String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         int dateF = Integer.parseInt(dateFrom.replace(".", ""));
         int dateT = Integer.parseInt(dateTo.replace(".", ""));
+        StringBuilder stringBuilder =
+                new StringBuilder("Отчёт за период " + dateFrom + " - " + dateTo + '\n');
         if (dateT < dateF) {
             return null;
         }
-        for (int i = 0; i < data.length; i++) {
-            String[] splitData = data[i].split(" ");
-            if (dateF <= Integer.parseInt(splitData[0].replace(".", ""))
-                    && dateT >= Integer.parseInt(splitData[0].replace(".", ""))) {
-                splitData[0] = splitData[1];
-                splitData[1] = String.valueOf(Integer.parseInt(splitData[2]) * Integer.parseInt(splitData[3]));
-                splitData = Arrays.copyOf(splitData, splitData.length - 2);
-                nameAndMoney.add(splitData);
-            }
-        }
-        int[] salaries = new int[names.length];
-        for (int i = 0; i < names.length; i++) {
-            for (String[] s : nameAndMoney) {
-                if (names[i].equals(s[0])) {
-                    salaries[i] += Integer.parseInt(s[1]);
+        for (String s : names) {
+            String name;
+            int salary;
+            int salaryCounter = 0;
+            for (String string : data) {
+                String day = string.split(" ")[0];
+                if (dateF <= Integer.parseInt(day.replace(".", ""))
+                        && dateT >= Integer.parseInt(day.replace(".", ""))) {
+                    name = string.split(" ")[1];
+                    salary = Integer.parseInt(string.split(" ")[2]) *
+                            Integer.parseInt(string.split(" ")[3]);
+                    if (s.equals(name)) {
+                        salaryCounter += salary;
+                    }
                 }
             }
-        }
-        StringBuilder stringBuilder = new StringBuilder("Отчёт за период " + dateFrom + " - " + dateTo + '\n');
-        for (int i = 0; i < names.length; i++) {
-            stringBuilder.append(names[i] + " - " + salaries[i] + '\n');
+            stringBuilder.append(s + " - " + salaryCounter + '\n');
         }
         return new String(stringBuilder);
     }
