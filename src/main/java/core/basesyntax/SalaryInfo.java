7f3c2a14896c6a9b6004712fa46deb635da4fa20
Rosinskyi <sergey22061993@gmail.com>
2019-08-22 22:04:32 +0300

    add solution for jv-salary-info

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,5 +1,6 @@
 package core.basesyntax;
 
+
 public class SalaryInfo {
     /**
      * Реализуйте метод getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo)
@@ -10,14 +11,11 @@ public class SalaryInfo {
      * ставка за 1 час. Метод должен вернуть отчёт за период, который передали в метод
      * (обе даты включительно) составленный по следующей форме: Отчёт за период
      * #дата_1# - #дата_2# Имя сотрудника - сумма заработанных средств за этот период
-     *
      * Пример ввода: date from = 01.04.2019 date to = 30.04.2019
-     *
      * names:
      * Сергей
      * Андрей
      * София
-     *
      * data:
      * 26.04.2019 Сергей 60 50
      * 26.04.2019 Андрей 3 200
@@ -28,7 +26,6 @@ public class SalaryInfo {
      * 26.04.2019 Сергей 7 100
      * 26.04.2019 София 9 100
      * 26.04.2019 Сергей 11 50
-     *
      * Пример вывода:
      * Отчёт за период 01.04.2019  - 30.04.2019
      * Сергей - 1550
@@ -36,6 +33,28 @@ public class SalaryInfo {
      * София - 900
      */
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        StringBuilder stringBuilder = new StringBuilder("Отчёт за период " + dateFrom
+                + " - " + dateTo + "\n");
+        String[] parsedDateFrom = dateFrom.split("\\D");
+        String[] parsedDateTo = dateTo.split("\\D");
+
+        for (String name : names) {
+            int salary = 0;
+            for (int i = 0; i < data.length; i++) {
+                String[] parsedData = data[i].split("\\s");
+                String[] parsedDate = parsedData[0].split("\\D");
+                if (Integer.parseInt(parsedDateFrom[0]) >= Integer.parseInt(parsedDate[0])) {
+                    return null;
+                }
+                if (Integer.parseInt(parsedDateFrom[0]) <= Integer.parseInt(parsedDate[0])
+                        && Integer.parseInt(parsedDate[0]) <= Integer.parseInt(parsedDateTo[0])
+                        && name.equals(parsedData[1])) {
+                    salary += (Integer.parseInt(parsedData[2]) * Integer.parseInt(parsedData[3]));
+                }
+            }
+            stringBuilder.append(name + " - " + salary + "\n");
+        }
+
+        return stringBuilder.toString();
     }
 }
