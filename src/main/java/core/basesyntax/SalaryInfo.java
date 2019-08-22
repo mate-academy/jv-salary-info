b7cd2c886eba155d2719edf0f7555add53325e9d
Korfat <38156885+Korfat@users.noreply.github.com>
2019-08-23 02:10:03 +0300

    Added solution for salary info

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -36,6 +36,45 @@ public class SalaryInfo {
      * София - 900
      */
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        String[] strDateFrom = dateFrom.replaceAll("[^0-9]+", " ").split(" ");
+        String[] strDateTo = dateTo.replaceAll("[^0-9]+", " ").split(" ");
+        String[] strDateCheck;
+        int[] numDateFrom = new int[strDateFrom.length];
+        int[] numDateTo = new int[strDateTo.length];
+        int[] numDateCheck = new int[strDateTo.length];
+        for (int i = 0; i < strDateFrom.length; i++) {
+            numDateFrom[i] = Integer.parseInt(strDateFrom[i]);
+            numDateTo[i] = Integer.parseInt(strDateTo[i]);
+        }
+        if (numDateFrom[0] > numDateTo[0] || numDateFrom[1] > numDateTo[1]
+                || numDateFrom[2] > numDateTo[2]) {
+            return null;
+        }
+        String[][] dataName = new String[data.length][];
+        int[] salary = new int[names.length];
+        for (int i = 0; i < data.length; i++) {
+            dataName[i] = data[i].split(" ");
+        }
+        for (int i = 0; i < dataName.length; i++) {
+            strDateCheck = dataName[i][0].replaceAll("[^0-9]+", " ").split(" ");
+            for (int j = 0; j < strDateFrom.length; j++) {
+                numDateCheck[j] = Integer.parseInt(strDateCheck[j]);
+            }
+            if (numDateCheck[0] >= numDateFrom[0] && numDateCheck[0] <= numDateTo[0]
+                    && numDateCheck[1] >= numDateFrom[1] && numDateCheck[1] <= numDateTo[1]
+                    && numDateCheck[2] >= numDateFrom[2] && numDateCheck[2] <= numDateTo[2]) {
+                for (int j = 0; j < names.length; j++) {
+                    if (names[j].equals(dataName[i][1])) {
+                        salary[j] += Integer.parseInt(dataName[i][2])
+                                * Integer.parseInt(dataName[i][3]);
+                    }
+                }
+            }
+        }
+        String result = "Отчёт за период " + dateFrom + " - " + dateTo + "\n";
+        for (int i = 0; i < names.length; i++) {
+            result = result + names[i] + " - " + salary[i] + "\n";
+        }
+        return result;
     }
 }
