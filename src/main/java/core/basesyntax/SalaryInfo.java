46f2a23fec32ff0c1e5b895b11ced25ca558a0f8
ElvenNurse <58395217+ElvenNurse@users.noreply.github.com>
2019-12-05 15:19:48 +0200

    Added solution for hw 4.2

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,5 +1,10 @@
 package core.basesyntax;
 
+import core.basesyntax.exception.IllegalDateParametersException;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 public class SalaryInfo {
     /**
      * <p>Реализуйте метод getSalaryInfo(String[] names, String[] data,
@@ -40,7 +45,41 @@ public class SalaryInfo {
      * София - 900</p>
      */
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo)
-            throws Exception {
-        return null;
+            throws IllegalDateParametersException {
+        Calendar calendarFrom = getDate(dateFrom);
+        Calendar calendarTo = getDate(dateTo);
+        if (calendarFrom.after(calendarTo)) {
+            throw new IllegalDateParametersException("Wrong parameters");
+        }
+
+        String[][] dataSplit = new String[data.length][4];
+        for (int i = 0; i < dataSplit.length; i++) {
+            dataSplit[i] = data[i].split(" ");
+        }
+
+        StringBuilder salaryInfo =
+                new StringBuilder("Отчёт за период " + dateFrom + " - " + dateTo + "\n");
+        for (String name : names) {
+            int salary = 0;
+            for (String[] strings : dataSplit) {
+                if (name.equals(strings[1])) {
+                    if (!getDate(strings[0]).after(calendarTo)
+                            && !getDate(strings[0]).before(calendarFrom)) {
+                        salary += Integer.parseInt(strings[2])
+                                * Integer.parseInt(strings[3]);
+                    }
+                }
+            }
+            salaryInfo.append(name).append(" - ").append(salary).append("\n");
+        }
+        return salaryInfo.toString();
+    }
+
+    public Calendar getDate(String s) {
+        String[] split = s.split("\\.");
+        int year = Integer.parseInt(split[2]);
+        int month = Integer.parseInt(split[1]) - 1;
+        int day = Integer.parseInt(split[0]);
+        return new GregorianCalendar(year, month, day);
     }
 }
