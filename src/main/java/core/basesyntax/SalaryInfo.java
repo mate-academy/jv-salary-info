ff55baaaeb532ab25d1ae12fea3d7dcf7587a55f
Stanislav Karev <karsv.dvz@gmail.com>
2019-12-05 19:42:04 +0200

    del main, cahnge to StringBuilder & LocalDate

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -3,8 +3,8 @@ package core.basesyntax;
 import core.basesyntax.exception.IllegalDateParametersException;
 
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     /**
@@ -47,23 +47,24 @@ public class SalaryInfo {
      */
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo)
             throws IllegalDateParametersException, ParseException {
-        Date dateFromDate = new SimpleDateFormat("dd.MM.yyyy").parse(dateFrom);
-        Date dateToDate = new SimpleDateFormat("dd.MM.yyyy").parse(dateTo);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDate dateFromDate = LocalDate.parse(dateFrom, formatter);
+        LocalDate dateToDate = LocalDate.parse(dateTo, formatter);
         String[] parsedData = new String[4];
         int[] salary = new int[names.length];
 
-        if (dateFromDate.after(dateToDate)) {
+        if (dateFromDate.isAfter(dateToDate)) {
             throw new IllegalDateParametersException();
         }
 
         for (int i = 0; i < data.length; i++) {
             parsedData = data[i].split(" ");
-            Date dateTemp = null;
-            dateTemp = new SimpleDateFormat("dd.MM.yyyy").parse(parsedData[0]);
+            LocalDate dateTemp = null;
+            dateTemp = LocalDate.parse(parsedData[0], formatter);
             for (int j = 0; j < names.length; j++) {
                 if (names[j].equals(parsedData[1])) {
                     if (dateTemp.equals(dateFromDate) || dateTemp.equals(dateToDate)
-                            || dateTemp.after(dateFromDate) && dateTemp.before(dateToDate)) {
+                            || dateTemp.isAfter(dateFromDate) && dateTemp.isBefore(dateToDate)) {
                         salary[j] = salary[j] + (Integer.parseInt(parsedData[2])
                                 * Integer.parseInt(parsedData[3]));
                     }
@@ -75,34 +76,12 @@ public class SalaryInfo {
     }
 
     private String buildReport(String[] names, int[] salary, String dateFrom, String dateTo) {
-        String report = "Отчёт за период " + dateFrom + " - " + dateTo + "\n";
+        StringBuilder report = new StringBuilder();
+        report.append("Отчёт за период ").append(dateFrom).append(" - ")
+                .append(dateTo).append("\n");
         for (int i = 0; i < names.length; i++) {
-            report = report + names[i] + " - " + salary[i] + "\n";
+            report.append(names[i]).append(" - ").append(salary[i]).append("\n");
         }
-        return report;
-    }
-
-    public static void main(String[] args) {
-        String[] roles = {"Сергей", "Андрей", "София"};
-        String[] script = {
-                "25.04.2019 Сергей 60 50",
-                "25.04.2019 Андрей 3 200",
-                "25.04.2019 София 10 100",
-
-                "26.04.2019 Андрей 3 200",
-                "26.04.2019 София 9 100",
-
-                "27.04.2019 Сергей 7 100",
-                "27.04.2019 София 3 80",
-                "27.04.2019 Андрей 8 100"
-        };
-        SalaryInfo salaryInfo = new SalaryInfo();
-        try {
-            salaryInfo.getSalaryInfo(roles, script, "27.04.2019", "25.04.2019");
-        } catch (IllegalDateParametersException e) {
-            e.printStackTrace();
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
+        return report.toString();
     }
 }
