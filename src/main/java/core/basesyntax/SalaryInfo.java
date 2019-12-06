3d102af9c4a0e92cc6fa10499179d1431b4be4a5
Daniil Hovorun <hovorun.d@gmail.com>
2019-12-06 13:37:35 +0200

    hw:4.2 - changed the solution according to the provided suggestions

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,11 +1,8 @@
 package core.basesyntax;
 
 import core.basesyntax.exception.IllegalDateParametersException;
-
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     /**
@@ -46,12 +43,14 @@ public class SalaryInfo {
      * Андрей - 600
      * София - 900</p>
      */
-    private static DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+    private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
-    public static String getSalaryInfo(String[] names, String[] data,
+    public String getSalaryInfo(String[] names, String[] data,
                                        String dateFrom, String dateTo)
             throws IllegalDateParametersException {
-        if (!checkDate(dateFrom, dateTo)) {
+        LocalDate localDateFrom = LocalDate.parse(dateFrom, DATE_FORMAT);
+        LocalDate localDateTo = LocalDate.parse(dateTo, DATE_FORMAT);
+        if (localDateFrom.isAfter(localDateTo)) {
             throw new IllegalDateParametersException("Wrong parameters");
         }
         StringBuilder report = new StringBuilder("Отчёт за период " + dateFrom + " - " + dateTo);
@@ -60,7 +59,9 @@ public class SalaryInfo {
             for (int i = 0; i < data.length; i++) {
                 if (data[i].contains(name)) {
                     String[] dataRecord = data[i].split(" ");
-                    if (checkDate(dateFrom, dataRecord[0]) && checkDate(dataRecord[0], dateTo)) {
+                    if (localDateFrom.compareTo(LocalDate.parse(dataRecord[0], DATE_FORMAT)) <= 0
+                            && LocalDate.parse(dataRecord[0], DATE_FORMAT)
+                            .compareTo(localDateTo) <= 0) {
                         salary += Integer.parseInt(dataRecord[2]) * Integer.parseInt(dataRecord[3]);
                     }
                 }
@@ -71,44 +72,4 @@ public class SalaryInfo {
         return report.toString() + "\n";
     }
 
-    public static boolean checkDate(String dateFrom, String dateTo) {
-        try {
-            Date dateToF = dateFormat.parse(dateTo);
-            Date dateFromF = dateFormat.parse(dateFrom);
-            /*System.out.println(dateFromF);
-            System.out.println(dateToF);*/
-            return dateFromF.compareTo(dateToF) <= 0;
-
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        return true;
-    }
-
-    /*public static void main(String[] args) {
-        try {
-
-            System.out.println(getSalaryInfo(ROLES, SCRIPT_ARRAY,
-                    "27.03.2019", "26.04.2019"));
-
-        } catch (IllegalDateParametersException e) {
-            e.printStackTrace();
-        }
-
-        //System.out.println(checkDate("27.03.2019", "15.04.2019"));
-    }
-
-    private static final String[] ROLES = {"Сергей", "Андрей", "София"};
-    private static final String[] SCRIPT_ARRAY = {
-            "25.04.2019 Сергей 60 50",
-            "25.04.2019 Андрей 3 200",
-            "25.04.2019 София 10 100",
-
-            "26.04.2019 Андрей 3 200",
-            "26.04.2019 София 9 100",
-
-            "27.04.2019 Сергей 7 100",
-            "27.04.2019 София 3 80",
-            "27.04.2019 Андрей 8 100"
-    };*/
 }
