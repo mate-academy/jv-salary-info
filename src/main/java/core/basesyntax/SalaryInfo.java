ba18afbb203d140c615b107022517b608bc3f373
Sumned <vladar91@gmail.com>
2020-04-01 21:20:22 +0300

    demo

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,5 +1,10 @@
 package core.basesyntax;
 
+import core.basesyntax.exception.IllegalDateParametersException;
+
+import java.time.LocalDate;import java.time.format.DateTimeFormatter;
+
+
 public class SalaryInfo {
     /**
      * <p>Реализуйте метод getSalaryInfo(String[] names, String[] data,
@@ -11,7 +16,7 @@ public class SalaryInfo {
      * ставка за 1 час. Метод должен вернуть отчёт за период, который передали в метод
      * (обе даты включительно) составленный по следующей форме: Отчёт за период
      * #дата_1# - #дата_2# Имя сотрудника - сумма заработанных средств за этот период
-     * Создать пакет exception и в нём класс-ошибку IllegalDateParametersException. Сделать так,
+     * Создать пакет core.basesyntax.exception и в нём класс-ошибку IllegalDateParametersException. Сделать так,
      * чтобы метод getSalaryInfo выбрасывал IllegalDateParametersException,
      * если dateFrom > dateTo, с сообщнием "Wrong parameters"</p>
      *
@@ -40,7 +45,42 @@ public class SalaryInfo {
      * София - 900</p>
      */
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo)
-            throws Exception {
-        return null;
+            throws IllegalDateParametersException {
+        if(!(checkerDate(dateFrom, dateTo))) throw new IllegalDateParametersException();
+        StringBuilder main = new StringBuilder("Отчёт за период " + dateFrom + " - " + dateTo + "\n");
+        int count;
+
+        //System.out.println(Arrays.toString(data));
+
+        for(String i: names) {
+           main.append(i);
+           count = 0;
+           for(String j : data) {
+               //System.out.println(i);
+               //System.out.println(i.equals(j.split(" ")[1]));
+               //System.out.println(checkerDate(j.split(" ")[0], dateTo));
+               //System.out.println(dateTo);
+               //System.out.println(checkerDate(dateFrom, j.split(" ")[0]));
+               if(i.equals(j.split(" ")[1]) && checkerDate(j.split(" ")[0], dateTo) && checkerDate(dateFrom, j.split(" ")[0])) {
+                   count += Integer.parseInt(j.split(" ")[2]) * Integer.parseInt(j.split(" ")[3]);
+               }
+           }
+            main.append(" - ").append(count).append("\n");
+
+        }
+
+
+        return main.toString().substring(0,  main.length() - 1);
     }
+
+    public Boolean checkerDate(String dateFrom, String dateTo) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        return (dateFrom.equals(dateTo)) || LocalDate.parse(dateFrom, formatter).isBefore(LocalDate.parse(dateTo, formatter));
+
+    }
+
 }
+
+
+
+
