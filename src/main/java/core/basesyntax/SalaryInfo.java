4e2a74940f67e4ff6f8c80f0c1bfe97e1bac24f6
Polina Kulyk <polina.kulyk@protonmail.com>
2020-04-01 18:33:32 +0300

    replaced soluttion from n to n^2

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -3,9 +3,6 @@ package core.basesyntax;
 import core.basesyntax.exception.IllegalDateParametersException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
-import java.util.HashMap;
-import java.util.Map;
 
 public class SalaryInfo {
     /*
@@ -46,83 +43,33 @@ public class SalaryInfo {
      * Андрей - 600
      * София - 900</p>
      */
-    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final DateTimeFormatter FORMAT = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        LocalDate from = parseDate(dateFrom);
-        LocalDate to = parseDate(dateTo);
+        LocalDate from = LocalDate.parse(dateFrom, FORMAT);
+        LocalDate to = LocalDate.parse(dateTo, FORMAT);
         if (from.isAfter(to)) {
             throw new IllegalDateParametersException("Wrong parameters");
         }
-
-        Map<String, Integer> nameToEarnings = createNameToEarningsMap(names);
-
+        int[] result = new int[names.length];
         for (String record : data) {
-            EarningsRecord earningsRecord = EarningsRecord.create(record.split(" "));
-            if (filterEarningsRecord(earningsRecord, from, to, nameToEarnings)) {
-                processRecord(earningsRecord, nameToEarnings);
+            String[] values = record.split(" ");
+            LocalDate date = LocalDate.parse(values[0], FORMAT);
+            String name = values[1];
+            int hours = Integer.parseInt(values[2]);
+            int rate = Integer.parseInt(values[3]);
+            if (!date.isBefore(from) && !date.isAfter(to)) {
+                for (int i = 0; i < names.length; ++i) {
+                    if (name.equals(names[i])) {
+                        result[i] += hours * rate;
+                    }
+                }
             }
         }
-        return generateReport(names, dateFrom, dateTo, nameToEarnings);
-    }
-
-    private boolean filterEarningsRecord(
-            EarningsRecord earningsRecord,
-            LocalDate fromDate,
-            LocalDate toDate,
-            Map<String, Integer> nameToEarnings
-    ) {
-        return nameToEarnings.containsKey(earningsRecord.name)
-                && !earningsRecord.date.isBefore(fromDate)
-                && !earningsRecord.date.isAfter(toDate);
-    }
-
-    private void processRecord(EarningsRecord earningsRecord, Map<String, Integer> nameToEarnings) {
-        Integer earnings = nameToEarnings.get(earningsRecord.name)
-                + earningsRecord.hours * earningsRecord.hourlyRate;
-        nameToEarnings.put(earningsRecord.name, earnings);
-    }
-
-    private Map<String, Integer> createNameToEarningsMap(String[] names) {
-        Map<String, Integer> nameToEarnings = new HashMap<>();
-        for (String name : names) {
-            nameToEarnings.put(name, 0);
-        }
-        return nameToEarnings;
-    }
-
-    private String generateReport(String[] names, String dateFrom, String dateTo,
-            Map<String, Integer> nameToEarnings) {
         StringBuilder sb = new StringBuilder("Отчёт за период " + dateFrom + " - " + dateTo);
-        for (String name : names) {
-            sb.append("\n").append(name).append(" - ").append(nameToEarnings.get(name));
+        for (int i = 0; i < names.length; ++i) {
+            sb.append("\n").append(names[i]).append(" - ").append(result[i]);
         }
         return sb.toString();
     }
-
-    private static LocalDate parseDate(String date) {
-        try {
-            return LocalDate.parse(date, FORMATTER);
-        } catch (DateTimeParseException e) {
-            throw new IllegalDateParametersException("Wrong parameters");
-        }
-    }
-
-    private static class EarningsRecord {
-        LocalDate date;
-        String name;
-        int hours;
-        int hourlyRate;
-
-        static EarningsRecord create(String[] fields) {
-            EarningsRecord earningsRecord = new EarningsRecord();
-            earningsRecord.date = LocalDate.parse(fields[0], FORMATTER);
-            earningsRecord.name = fields[1];
-            earningsRecord.hours = Integer.parseInt(fields[2]);
-            earningsRecord.hourlyRate = Integer.parseInt(fields[3]);
-            return earningsRecord;
-        }
-
-    }
-
 }
