a2cce3d91f032f2b4afba25e687e51e72c3d578f
Polina Kulyk <polina.kulyk@protonmail.com>
2020-04-01 16:35:13 +0300

    put exception class into git

---
+++ src/main/java/core/basesyntax/exception/IllegalDateParametersException.java
@@ -0,0 +1,8 @@
+package core.basesyntax.exception;
+
+public class IllegalDateParametersException extends IllegalArgumentException {
+    public IllegalDateParametersException(String message) {
+        super(message);
+    }
+
+}
