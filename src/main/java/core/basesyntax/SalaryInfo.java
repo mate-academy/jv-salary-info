93ef8ff1dcc94a3c4444b803fa1a516689f06ffc
Nick97-git <nickarchangel97@gmail.com>
2020-04-01 13:22:40 +0300

    added solution

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,6 +1,11 @@
 package core.basesyntax;
 
+import core.basesyntax.exception.IllegalDateParametersException;
+import java.util.Arrays;
+import java.util.Calendar;
+
 public class SalaryInfo {
+
     /**
      * <p>Реализуйте метод getSalaryInfo(String[] names, String[] data,
      * String dateFrom, String dateTo)
@@ -40,7 +45,32 @@ public class SalaryInfo {
      * София - 900</p>
      */
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo)
-            throws Exception {
-        return null;
+            throws IllegalDateParametersException {
+        if (convertToMilliseconds(dateFrom) > convertToMilliseconds(dateTo)) {
+            throw new IllegalDateParametersException();
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Отчёт за период ").append(dateFrom)
+                .append(" - ").append(dateTo).append("\n");
+        for (String name : names) {
+            int salary = 0;
+            for (String line : data) {
+                String[] splitData = line.split(" ");
+                if (line.contains(name)
+                        && convertToMilliseconds(splitData[0]) >= convertToMilliseconds(dateFrom)
+                        && convertToMilliseconds(splitData[0]) <= convertToMilliseconds(dateTo)) {
+                    salary += Integer.parseInt(splitData[2]) * Integer.parseInt(splitData[3]);
+                }
+            }
+            stringBuilder.append(name).append(" - ").append(salary).append("\n");
+        }
+        return stringBuilder.toString().substring(0,stringBuilder.length() - 1);
+    }
+
+    private long convertToMilliseconds(String dateString) {
+        int[] values = Arrays.stream(dateString.split("\\.")).mapToInt(Integer::parseInt).toArray();
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(values[2],values[1],values[0]);
+        return calendar.getTimeInMillis();
     }
 }
