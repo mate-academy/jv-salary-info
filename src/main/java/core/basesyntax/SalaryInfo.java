0c7d14b4305c901b58cfefc94ca984e939aca1c7
Djeckkenni <djeck27kenni@gmail.com>
2020-04-02 11:27:30 +0300

    correct code-2

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -12,23 +12,23 @@ public class SalaryInfo {
         LocalDate dataFrom = LocalDate.parse(dateFrom, FORMATTER);
         LocalDate dataTo = LocalDate.parse(dateTo, FORMATTER);
         if (dataFrom.isAfter(dataTo)) {
-            throw new IllegalDateParametersException();
+            throw new IllegalDateParametersException("Wrong parameters");
         }
         StringBuilder heading = new StringBuilder();
         heading.append("Отчёт за период ").append(dateFrom).append(" - ")
                 .append(dateTo);
         for (String name : names) {
-            int zp = 0;
+            int salary = 0;
             for (String line : data) {
                 String[] splitData = line.split(" ");
                 LocalDate localDate = LocalDate.parse(splitData[0], FORMATTER);
                 if (line.contains(name) && (localDate.isAfter(dataFrom)
                         || localDate.isEqual(dataFrom))
                         && (localDate.isBefore(dataTo) || localDate.isEqual(dataTo))) {
-                    zp += Integer.parseInt(splitData[2]) * Integer.parseInt(splitData[3]);
+                    salary += Integer.parseInt(splitData[2]) * Integer.parseInt(splitData[3]);
                 }
             }
-            heading.append("\n").append(name).append(" - ").append(zp);
+            heading.append("\n").append(name).append(" - ").append(salary);
         }
         return heading.toString();
     }
