8c77e607a17a285ee7583a898896ea198ebae400
Andrey Tanchak <61573898+boterf@users.noreply.github.com>
2020-04-02 12:00:10 +0300

    DELETE ELSE

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -12,27 +12,26 @@ public class SalaryInfo {
         LocalDate localFrom = LocalDate.parse(dateFrom, DTF);
         LocalDate localTo = LocalDate.parse(dateTo, DTF);
         if (localTo.isBefore(localFrom)) {
-            throw new IllegalDateParametersException();
-        } else {
-            StringBuilder result = new StringBuilder();
-            result.append("Отчёт за период " + dateFrom + " - " + dateTo);
-            for (int i = 0; i < names.length; i++) {
-                int salary = 0;
-                String[] time;
-                for (int j = 0; j < data.length; j++) {
-                    time = data[j].split(" ");
-                    LocalDate localNow = LocalDate.parse(time[0], DTF);
-                    if (time[1].equals(names[i])
-                            && (localNow.isAfter(localFrom) || localNow.isEqual(localFrom))
-                            && (localNow.isBefore(localTo) || localNow.isEqual(localTo))) {
-                        salary = salary + Integer.parseInt(time[2]) * Integer.parseInt(time[3]);
-                    }
-
-                }
-                result.append("\n" + names[i] + " - " + salary);
-            }
-            return result.toString();
+            throw new IllegalDateParametersException("Wrong parameters");
         }
+        StringBuilder result = new StringBuilder();
+        result.append("Отчёт за период " + dateFrom + " - " + dateTo);
+        for (int i = 0; i < names.length; i++) {
+            int salary = 0;
+            String[] time;
+            for (int j = 0; j < data.length; j++) {
+                time = data[j].split(" ");
+                LocalDate localNow = LocalDate.parse(time[0], DTF);
+                if (time[1].equals(names[i])
+                        && (localNow.isAfter(localFrom) || localNow.isEqual(localFrom))
+                        && (localNow.isBefore(localTo) || localNow.isEqual(localTo))) {
+                    salary = salary + Integer.parseInt(time[2]) * Integer.parseInt(time[3]);
+                }
+
+            }
+            result.append("\n" + names[i] + " - " + salary);
+        }
+        return result.toString();
 
     }
 }
