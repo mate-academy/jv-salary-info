73e8a0358d160d2a6f042fa5c4c94ed9f1324540
Sumned <vladar91@gmail.com>
2020-04-02 14:11:24 +0300

    method removed, formatter is a constant field

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,34 +5,34 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo)
+
             throws IllegalDateParametersException {
-        if (!(checkerDate(dateFrom, dateTo))) {
+        if (!(LocalDate.parse(dateFrom, FORMATTER)
+                .isBefore(LocalDate.parse(dateTo, FORMATTER)))) {
             throw new IllegalDateParametersException();
         }
         StringBuilder main = new StringBuilder("Отчёт за период "
                 + dateFrom + " - " + dateTo + "\n");
         int count;
+        String[] str;
         for (String i : names) {
             main.append(i);
             count = 0;
             for (String j : data) {
-                if (i.equals(j.split(" ")[1]) && checkerDate(j.split(" ")[0], dateTo)
-                        && checkerDate(dateFrom, j.split(" ")[0])) {
-                    count += Integer.parseInt(j.split(" ")[2]) * Integer.parseInt(j.split(" ")[3]);
+                str = j.split(" ");
+                if (i.equals(str[1]) && ((LocalDate.parse(str[0], FORMATTER)
+                        .isBefore(LocalDate.parse(dateTo, FORMATTER)))
+                        && (LocalDate.parse(dateFrom, FORMATTER)
+                        .isBefore(LocalDate.parse(str[0], FORMATTER)))
+                        || (dateFrom.equals((str[0]))) || (dateTo.equals((str[0]))))) {
+                    count += Integer.parseInt(str[2]) * Integer.parseInt(str[3]);
                 }
             }
             main.append(" - ").append(count).append("\n");
         }
         return main.toString().substring(0, main.length() - 1);
     }
-
-    public Boolean checkerDate(String dateFrom, String dateTo) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        return (dateFrom.equals(dateTo))
-                || LocalDate.parse(dateFrom, formatter)
-                .isBefore(LocalDate.parse(dateTo, formatter));
-
-    }
-
 }
