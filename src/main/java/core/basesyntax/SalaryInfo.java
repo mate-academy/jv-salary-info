28e69c823cf9ba99cf28ea7ddf9a1f64c506015a
Максим Матуляк <maksymmatuliak@gmail.com>
2020-04-02 15:03:02 +0300

    deleted 100 chars in a row

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -43,7 +43,8 @@ public class SalaryInfo {
      * Андрей - 600
      * София - 900</p>
      */
-    private static final DateTimeFormatter PATTERN_OF_DATE = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final DateTimeFormatter PATTERN_OF_DATE =
+            DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo)
             throws IllegalDateParametersException {
