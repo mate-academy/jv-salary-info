f73f26ae30e6c12b5601d227854a820856e90983
ana-dav <opa.super.op@gmail.com>
2020-04-02 17:20:52 +0300

    It still doesn't work, I don't understand why

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,46 +1,39 @@
 package core.basesyntax;
 
+import exception.IllegalDateParametersException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
-    /**
-     * <p>Реализуйте метод getSalaryInfo(String[] names, String[] data,
-     * String dateFrom, String dateTo)
-     * вычисляющий зарплату сотрудников. На вход методу подаётся 2 массива и 2 даты,
-     * определяющие период за который надо вычислить зарплату, первый массив содержит имена
-     * сотрудников организации, второй массив информацию о рабочих часах и ставке. Формат данных
-     * второго массива следующий: дата, имя сотрудника, количество отработанных часов,
-     * ставка за 1 час. Метод должен вернуть отчёт за период, который передали в метод
-     * (обе даты включительно) составленный по следующей форме: Отчёт за период
-     * #дата_1# - #дата_2# Имя сотрудника - сумма заработанных средств за этот период
-     * Создать пакет exception и в нём класс-ошибку IllegalDateParametersException. Сделать так,
-     * чтобы метод getSalaryInfo выбрасывал IllegalDateParametersException,
-     * если dateFrom > dateTo, с сообщнием "Wrong parameters"</p>
-     *
-     * <p>Пример ввода: date from = 01.04.2019 date to = 30.04.2019</p>
-     *
-     * <p>names:
-     * Сергей
-     * Андрей
-     * София</p>
-     *
-     * <p>data:
-     * 26.04.2019 Сергей 60 50
-     * 26.04.2019 Андрей 3 200
-     * 26.04.2019 Сергей 7 100
-     * 26.04.2019 София 9 100
-     * 26.04.2019 Сергей 11 50
-     * 26.04.2019 Андрей 3 200
-     * 26.04.2019 Сергей 7 100
-     * 26.04.2019 София 9 100
-     * 26.04.2019 Сергей 11 50</p>
-     *
-     * <p>Пример вывода:
-     * Отчёт за период 01.04.2019  - 30.04.2019
-     * Сергей - 1550
-     * Андрей - 600
-     * София - 900</p>
-     */
+
+    private static final DateTimeFormatter FORMATTER =
+            DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo)
-            throws Exception {
-        return null;
+            throws IllegalDateParametersException {
+        LocalDate fromDate = parse(dateFrom, FORMATTER);
+        LocalDate toDate = parse(dateTo, FORMATTER);
+        if (fromDate.isAfter(toDate)) {
+            throw new IllegalDateParametersException("Wrong message!");
+        }
+
+        StringBuilder containerResult = new StringBuilder("Отчёт за период " + dateFrom + " - " + dateTo + "\n");
+
+        for (String n : names) {
+            int salary = 0;
+            for (String d : data) {
+                String[] dataArray = d.split(" ");
+                LocalDate dataDate = LocalDate.parse(dataArray[0], FORMATTER);
+                if (n.equals(dataArray[1])
+                        && dataDate.isAfter(fromDate)
+                        && dataDate.isEqual(fromDate)
+                        && dataDate.isBefore(toDate)
+                        && dataDate.isEqual(toDate)) {
+                    salary += Integer.parseInt(dataArray[2]) * Integer.parseInt(dataArray[3]);
+                }
+            }
+            containerResult.append(n).append(" - ").append(salary).append("\n");
+        }
+        return containerResult.toString().trim();
     }
 }
