3fc99c94bdcb4f39919961c8bc2b97176f186e5a
Dm_Grt <grytsishin@gmail.com>
2020-07-29 15:15:13 +0300

    Added solution 3.2

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,46 +1,35 @@
 package core.basesyntax;
 
+import core.basesyntax.exception.IllegalDateParametersException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
-    /**
-     * <p>Реализуйте метод getSalaryInfo(String[] names, String[] data,
-     * String dateFrom, String dateTo)
-     * вычисляющий зарплату сотрудников. На вход методу подаётся 2 массива и 2 даты,
-     * определяющие период за который надо вычислить зарплату, первый массив содержит имена
-     * сотрудников организации, второй массив информацию о рабочих часах и ставке. Формат данных
-     * второго массива следующий: дата, имя сотрудника, количество отработанных часов,
-     * ставка за 1 час. Метод должен вернуть отчёт за период, который передали в метод
-     * (обе даты включительно) составленный по следующей форме: Отчёт за период
-     * #дата_1# - #дата_2# Имя сотрудника - сумма заработанных средств за этот период
-     * Создать пакет exception и в нём класс-ошибку IllegalDateParametersException. Сделать так,
-     * чтобы метод getSalaryInfo выбрасывал IllegalDateParametersException,
-     * если dateFrom > dateTo, с сообщнием "Wrong parameters"</p>
-     *
-     * <p>Пример ввода: date from = 01.04.2019 date to = 30.04.2019</p>
-     *
-     * <p>names:
-     * Сергей
-     * Андрей
-     * София</p>
-     *
-     * <p>data:
-     * 26.04.2019 Сергей 60 50
-     * 26.04.2019 Андрей 3 200
-     * 26.04.2019 Сергей 7 100
-     * 26.04.2019 София 9 100
-     * 26.04.2019 Сергей 11 50
-     * 26.04.2019 Андрей 3 200
-     * 26.04.2019 Сергей 7 100
-     * 26.04.2019 София 9 100
-     * 26.04.2019 Сергей 11 50</p>
-     *
-     * <p>Пример вывода:
-     * Отчёт за период 01.04.2019  - 30.04.2019
-     * Сергей - 1550
-     * Андрей - 600
-     * София - 900</p>
-     */
+    private final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo)
-            throws Exception {
-        return null;
+            throws IllegalDateParametersException {
+        LocalDate startDate = LocalDate.parse(dateFrom, dateFormatter);
+        LocalDate stopDate = LocalDate.parse(dateTo, dateFormatter);
+
+        if (startDate.isAfter(stopDate)) {
+            throw new IllegalDateParametersException("Wrong parameters");
+        }
+        StringBuilder response = new StringBuilder("Отчёт за период ");
+        response.append(dateFrom).append(" - ").append(dateTo).append("\n");
+        for (String name : names) {
+            int salary = 0;
+            for (String currentData : data) {
+                String[] strData = currentData.split(" ");
+                LocalDate currentDate = LocalDate.parse(strData[0], dateFormatter);
+                if (name.equals(strData[1])
+                        && (currentDate.isAfter(startDate) || currentDate.isEqual(startDate))
+                        && (currentDate.isBefore(stopDate) || currentDate.isEqual(stopDate))) {
+                    salary += Integer.parseInt(strData[2]) * Integer.parseInt(strData[3]);
+                }
+            }
+            response.append(name).append(" - ").append(salary).append("\n");
+        }
+        return response.deleteCharAt(response.length() - 1).toString();
     }
 }
---
+++ src/main/java/core/basesyntax/exception/IllegalDateParametersException.java
@@ -0,0 +1,7 @@
+package core.basesyntax.exception;
+
+public class IllegalDateParametersException extends Exception {
+    public IllegalDateParametersException(String errorMessage) {
+        super(errorMessage);
+    }
+}
