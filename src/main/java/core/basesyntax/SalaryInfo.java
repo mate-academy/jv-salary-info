35b60510647bcb9c9fa6c9882655953d719ca372
tarkaiev <tarkaievd@gmail.com>
2020-07-29 13:28:08 +0300

    added solution

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,5 +1,9 @@
 package core.basesyntax;
 
+import core.basesyntax.exception.IllegalDateParametersException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
     /**
      * <p>Реализуйте метод getSalaryInfo(String[] names, String[] data,
@@ -40,7 +44,40 @@ public class SalaryInfo {
      * София - 900</p>
      */
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo)
-            throws Exception {
-        return null;
+            throws IllegalDateParametersException {
+
+        StringBuilder builder = new StringBuilder();
+        builder.append("Отчёт за период ")
+                .append(dateFrom).append(" - ")
+                .append(dateTo).append("\n");
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
+        LocalDate parsedDateFrom = LocalDate.parse(dateFrom, formatter);
+        LocalDate parsedDateTo = LocalDate.parse(dateTo, formatter);
+
+        if (parsedDateFrom.isAfter(parsedDateTo)) {
+            throw new IllegalDateParametersException("Wrong parameters");
+        }
+
+        for (String name : names) {
+            builder.append(name).append(" - ");
+            int result = 0;
+            for (String dataString : data) {
+                String[] parsedData = dataString.split(" ");
+
+                LocalDate currentDate = LocalDate.parse(parsedData[0], formatter);
+                if ((parsedData[1].equals(name)) && (currentDate.isAfter(parsedDateFrom)
+                        || currentDate.isEqual(parsedDateFrom))
+                        && (currentDate.isBefore(parsedDateTo)
+                        || currentDate.isEqual(parsedDateTo))) {
+                    result += Integer.parseInt(parsedData[2]) * Integer.parseInt(parsedData[3]);
+                }
+
+            }
+            builder.append(result).append("\n");
+        }
+        builder.deleteCharAt(builder.length() - 1);
+        return builder.toString();
     }
 }
