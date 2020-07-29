f9ec0f68979cbb22b14653dc92fd4f14fbda1529
MaliukDaria <maliuk.dasha@gmail.com>
2020-07-29 16:57:08 +0300

    added solution

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,5 +1,9 @@
 package core.basesyntax;
 
+import core.basesyntax.exception.IllegalDateParametersException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
     /**
      * <p>Реализуйте метод getSalaryInfo(String[] names, String[] data,
@@ -11,8 +15,8 @@ public class SalaryInfo {
      * ставка за 1 час. Метод должен вернуть отчёт за период, который передали в метод
      * (обе даты включительно) составленный по следующей форме: Отчёт за период
      * #дата_1# - #дата_2# Имя сотрудника - сумма заработанных средств за этот период
-     * Создать пакет exception и в нём класс-ошибку IllegalDateParametersException. Сделать так,
-     * чтобы метод getSalaryInfo выбрасывал IllegalDateParametersException,
+     * Создать пакет core.basesyntax.exception и в нём класс-ошибку IllegalDateParametersException.
+     * Сделать так, чтобы метод getSalaryInfo выбрасывал IllegalDateParametersException,
      * если dateFrom > dateTo, с сообщнием "Wrong parameters"</p>
      *
      * <p>Пример ввода: date from = 01.04.2019 date to = 30.04.2019</p>
@@ -39,8 +43,36 @@ public class SalaryInfo {
      * Андрей - 600
      * София - 900</p>
      */
+    static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("d.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo)
-            throws Exception {
-        return null;
+            throws IllegalDateParametersException {
+        if (LocalDate.parse(dateFrom, DATE_FORMAT).isAfter(LocalDate.parse(dateTo, DATE_FORMAT))) {
+            throw new IllegalDateParametersException("Wrong parameters");
+        }
+        StringBuilder report = new StringBuilder();
+        report.append("Отчёт за период ").append(dateFrom).append(" - ")
+                .append(dateTo).append("\n");
+        int totalSalaryForThePeriod = 0;
+        for (String employee : names
+        ) {
+            for (String infoAboutSalary : data
+            ) {
+                String[] arrayInfoAboutSalary = infoAboutSalary.split(" ");
+                if (LocalDate.parse(dateFrom, DATE_FORMAT).minusDays(1)
+                        .isBefore(LocalDate.parse(arrayInfoAboutSalary[0], DATE_FORMAT))
+                        && (LocalDate.parse(arrayInfoAboutSalary[0], DATE_FORMAT)
+                        .isBefore(LocalDate.parse(dateTo, DATE_FORMAT).plusDays(1)))) {
+                    if (employee.equals(arrayInfoAboutSalary[1])) {
+                        totalSalaryForThePeriod +=
+                                Integer.parseInt(arrayInfoAboutSalary[2])
+                                        * Integer.parseInt(arrayInfoAboutSalary[3]);
+                    }
+                }
+            }
+            report.append(employee).append(" - ").append(totalSalaryForThePeriod).append("\n");
+            totalSalaryForThePeriod = 0;
+        }
+        return report.toString().trim();
     }
 }
