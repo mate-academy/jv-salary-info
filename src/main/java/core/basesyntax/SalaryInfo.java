afe49f44a5dcf5d84071ba95f7086bccd07cd935
ROMASHSX <andriiromash@gmail.com>
2020-07-29 18:52:14 +0300

    code update

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,7 +5,7 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo)
             throws IllegalDateParametersException {
@@ -17,12 +17,11 @@ public class SalaryInfo {
         StringBuilder result = new StringBuilder();
         result.append("Отчёт за период ").append(dateFrom)
                 .append(" - ").append(dateTo);
-        LocalDate currentDate;
-        int salary = 0;
         for (String name : names) {
+            int salary = 0;
             for (String info : data) {
                 String[] information = info.split(" ");
-                currentDate = LocalDate.parse(information[0], DATE_FORMAT);
+                LocalDate currentDate = LocalDate.parse(information[0], DATE_FORMAT);
                 if (name.equals(information[1])
                         && (currentDate.isBefore(endDate) || currentDate.equals(endDate))
                         && (currentDate.isAfter(startDate) || currentDate.equals(startDate))) {
@@ -30,7 +29,6 @@ public class SalaryInfo {
                 }
             }
             result.append("\n").append(name).append(" - ").append(salary);
-            salary = 0;
         }
         return new String(result);
     }
