c21f0811d4a9eae88e0c8d8fae7f641073b136bf
Victoria-Moiseienko <teasevic@gmail.com>
2020-07-30 12:28:42 +0300

    updated without Class Worker

--- src/main/java/core/basesyntax/DataLineInfo.java
+++ src/main/java/core/basesyntax/DataLineInfo.java
@@ -1,16 +1,32 @@
 package core.basesyntax;
 
 public class DataLineInfo {
-    String date;
-    String name;
-    int hours;
-    int payment;
+    private String date;
+    private String name;
+    private int hours;
+    private int payment;
 
     public DataLineInfo(String data) {
         this.date = data.split(" ")[0];
         this.name = data.split(" ")[1];
         this.hours = Integer.parseInt(data.split(" ")[3]);
         this.payment = Integer.parseInt(data.split(" ")[2]);
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHours() {
+        return hours;
+    }
+
+    public int getPayment() {
+        return payment;
     }
 }
 
--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,76 +1,33 @@
 package core.basesyntax;
 
 import core.basesyntax.exception.IllegalDateParametersException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    /**
-     * <p>Реализуйте метод getSalaryInfo(String[] names, String[] data,
-     * String dateFrom, String dateTo)
-     * вычисляющий зарплату сотрудников. На вход методу подаётся 2 массива и 2 даты,
-     * определяющие период за который надо вычислить зарплату, первый массив содержит имена
-     * сотрудников организации, второй массив информацию о рабочих часах и ставке. Формат данных
-     * второго массива следующий: дата, имя сотрудника, количество отработанных часов,
-     * ставка за 1 час. Метод должен вернуть отчёт за период, который передали в метод
-     * (обе даты включительно) составленный по следующей форме: Отчёт за период
-     * #дата_1# - #дата_2# Имя сотрудника - сумма заработанных средств за этот период
-     * Создать пакет exception и в нём класс-ошибку IllegalDateParametersException. Сделать так,
-     * чтобы метод getSalaryInfo выбрасывал IllegalDateParametersException,
-     * если dateFrom > dateTo, с сообщнием "Wrong parameters"</p>
-     *
-     * <p>Пример ввода: date from = 01.04.2019 date to = 30.04.2019</p>
-     *
-     * <p>names:
-     * Сергей
-     * Андрей
-     * София</p>
-     *
-     * <p>data:
-     * 26.04.2019 Сергей 60 50
-     * 26.04.2019 Андрей 3 200
-     * 26.04.2019 Сергей 7 100
-     * 26.04.2019 София 9 100
-     * 26.04.2019 Сергей 11 50
-     * 26.04.2019 Андрей 3 200
-     * 26.04.2019 Сергей 7 100
-     * 26.04.2019 София 9 100
-     * 26.04.2019 Сергей 11 50</p>
-     *
-     * <p>Пример вывода:
-     * Отчёт за период 01.04.2019  - 30.04.2019
-     * Сергей - 1550
-     * Андрей - 600
-     * София - 900</p>
-     */
+    private static final DateTimeFormatter FORMATOFDATE = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data,
                                 String dateFrom, String dateTo) throws Exception {
         checkInputDates(dateFrom, dateTo);
 
-        Worker[] workers = new Worker[names.length];
-
-        for (int i = 0; i < names.length; i++) {
-            Worker worker = new Worker(names[i]);
-            workers[i] = worker;
-        }
+        int[] salaries = new int[names.length];
 
         for (String singleData : data) {
             DataLineInfo dataLineInfo = new DataLineInfo(singleData);
-            if (isNeedDate(dateFrom, dateTo, dataLineInfo.date)) {
-                for (Worker worker : workers) {
-                    if (worker.name.equals(dataLineInfo.name)) {
-                        worker.salary += dataLineInfo.hours * dataLineInfo.payment;
+            if (isNeedDate(dateFrom, dateTo, dataLineInfo.getDate())) {
+                for (int i = 0; i < names.length; i++) {
+                    if (names[i].equals(dataLineInfo.getName())) {
+                        salaries[i] += dataLineInfo.getHours() * dataLineInfo.getPayment();
                     }
                 }
             }
         }
 
-        String report = makeReport(dateFrom, dateTo, workers);
-        return report;
+        return makeReport(dateFrom, dateTo, names, salaries);
     }
 
-    private String makeReport(String dateFrom, String dateTo, Worker[] workers) {
+    private String makeReport(String dateFrom, String dateTo, String[] names, int[] salaries) {
         StringBuilder result = new StringBuilder();
 
         result.append("Отчёт за период ")
@@ -79,19 +36,18 @@ public class SalaryInfo {
                 .append(dateTo)
                 .append("\n");
 
-        for (Worker worker : workers) {
-            result.append(worker.name).append(" - ").append(worker.salary).append("\n");
-            ;
+        for (int i = 0; i < names.length; i++) {
+            result.append(names[i]).append(" - ").append(salaries[i]).append("\n");
         }
+
         result.append("\n");
 
         return result.toString().replace("\n\n", "");
     }
 
     private void checkInputDates(String dateFrom, String dateTo) throws Exception {
-        SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
-        Date from = sdf.parse(dateFrom);
-        Date to = sdf.parse(dateTo);
+        LocalDate from = LocalDate.parse(dateFrom, FORMATOFDATE);
+        LocalDate to = LocalDate.parse(dateTo, FORMATOFDATE);
 
         if (from.compareTo(to) > 0) {
             throw new IllegalDateParametersException("Wrong parameters");
@@ -100,10 +56,9 @@ public class SalaryInfo {
 
     private static boolean isNeedDate(String dateFrom, String dateTo,
                                       String currentDate) throws Exception {
-        SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
-        Date from = sdf.parse(dateFrom);
-        Date to = sdf.parse(dateTo);
-        Date current = sdf.parse(currentDate);
+        LocalDate from = LocalDate.parse(dateFrom, FORMATOFDATE);
+        LocalDate to = LocalDate.parse(dateTo, FORMATOFDATE);
+        LocalDate current = LocalDate.parse(currentDate, FORMATOFDATE);
 
         if (current.compareTo(from) >= 0
                 && current.compareTo(to) <= 0) {
