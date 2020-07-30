7784d42390590bfb10697a7727df214b16929312
YaTut1901 <59802427+YaTut1901@users.noreply.github.com>
2020-07-30 11:57:32 +0300

    hw-3.2

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,5 +1,9 @@
 package core.basesyntax;
 
+import core.basesyntax.core.basesyntax.exception.IllegalDateParametersException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
     /**
      * <p>Реализуйте метод getSalaryInfo(String[] names, String[] data,
@@ -11,7 +15,8 @@ public class SalaryInfo {
      * ставка за 1 час. Метод должен вернуть отчёт за период, который передали в метод
      * (обе даты включительно) составленный по следующей форме: Отчёт за период
      * #дата_1# - #дата_2# Имя сотрудника - сумма заработанных средств за этот период
-     * Создать пакет exception и в нём класс-ошибку IllegalDateParametersException. Сделать так,
+     * Создать пакет core.basesyntax.exception и в нём класс-ошибку IllegalDateParametersException.
+     * Сделать так,
      * чтобы метод getSalaryInfo выбрасывал IllegalDateParametersException,
      * если dateFrom > dateTo, с сообщнием "Wrong parameters"</p>
      *
@@ -39,8 +44,31 @@ public class SalaryInfo {
      * Андрей - 600
      * София - 900</p>
      */
-    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo)
-            throws Exception {
-        return null;
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("d.MM.yyyy");
+
+    public String getSalaryInfo(String[] names, String[] data, String dateFrom,
+                                String dateTo) throws IllegalDateParametersException {
+        StringBuilder builder = new StringBuilder("Отчёт за период " + dateFrom + " - " + dateTo);
+        LocalDate dateStart = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate dateFinish = LocalDate.parse(dateTo, FORMATTER);
+
+        if (dateStart.isAfter(dateFinish)) {
+            throw new IllegalDateParametersException("Wrong parameters");
+        }
+        for (int i = 0; i < names.length; i++) {
+            int fee = 0;
+            for (int j = 0; j < data.length; j++) {
+                if ((LocalDate.parse(data[j].split(" ")[0], FORMATTER).isAfter(dateStart)
+                        || LocalDate.parse(data[j].split(" ")[0], FORMATTER).isEqual(dateStart))
+                        && (LocalDate.parse(data[j].split(" ")[0], FORMATTER).isBefore(dateFinish)
+                        || LocalDate.parse(data[j].split(" ")[0], FORMATTER).isEqual(dateFinish))
+                        && names[i].equals(data[j].split(" ")[1])) {
+                    fee += Integer.parseInt(data[j].split(" ")[2])
+                            * Integer.parseInt(data[j].split(" ")[3]);
+                }
+            }
+            builder.append("\n").append(names[i] + " - ").append(fee);
+        }
+        return builder.toString();
     }
 }
---
+++ src/main/java/core/basesyntax/core/basesyntax/exception/IllegalDateParametersException.java
@@ -0,0 +1,8 @@
+package core.basesyntax.core.basesyntax.exception;
+
+public class IllegalDateParametersException extends Exception {
+    public IllegalDateParametersException(String str) {
+        super(str);
+    }
+}
+
