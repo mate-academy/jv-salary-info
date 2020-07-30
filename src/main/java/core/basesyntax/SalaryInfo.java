fdc9e3a3a475b82af1a903a8f77a984e45435059
margarita-ignatieva <ignatevamargarita950@gmail.com>
2020-07-30 10:36:34 +0300

    date changed to localdate

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -2,16 +2,18 @@
 package core.basesyntax;
 
 import core.basesyntax.exception.IllegalDateParametersException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    private static final DateTimeFormatter FORMATTER =
+            DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo)
             throws Exception {
 
-        Date dateStart = new SimpleDateFormat("dd.MM.yyyy").parse(dateFrom);
-        Date dateEnd = new SimpleDateFormat("dd.MM.yyyy").parse(dateTo);
+        LocalDate dateStart = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate dateEnd = LocalDate.parse(dateTo, FORMATTER);
         if (dateEnd.compareTo(dateStart) == -1) {
             throw new IllegalDateParametersException("Wrong parameters");
         }
@@ -20,7 +22,7 @@ public class SalaryInfo {
             int salary = 0;
             for (int i = 0;i < data.length; i++) {
                 String[] str = data[i].split(" ");
-                Date actual = new SimpleDateFormat("dd.MM.yyyy").parse(str[0]);
+                LocalDate actual = LocalDate.parse(str[0], FORMATTER);
                 if (str[1].equals(n) && dateEnd.compareTo(actual) >= 0
                         && dateStart.compareTo(actual) <= 0) {
                     salary += Integer.parseInt(str[2]) * Integer.parseInt(str[3]);
