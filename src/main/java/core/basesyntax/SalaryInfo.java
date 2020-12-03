7dd3aa6276ee63f178b9b44f128d79a9ce4e72fd
Oleh Kravets <oleg.kravets92@gmail.com>
2020-12-03 22:32:46 +0200

    John doesn't have salary for one test.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -13,30 +13,46 @@ public class SalaryInfo {
                 .append(dateFrom).append(" - ").append(dateTo);
 
         for (String name : names) {
-            int salary = getSalaryForPeriod(getDataForPeriod(name, data, dateFrom, dateTo));
+            String[] sorted = getDataByName(name, data);
+            sorted = getDataByPeriod(sorted, dateFrom, dateTo);
+            int salary = getSalaryForPeriod(sorted);
             result.append("\n").append(name).append(" - ").append(salary);
         }
 
         return result.toString();
     }
 
-    private int getSalaryForPeriod(String[][] data) {
+    private int getSalaryForPeriod(String[] dataLines) {
         int salaryForPeriod = 0;
-        for (String[] input : data) {
-            salaryForPeriod += Integer.parseInt(input[2]) * Integer.parseInt(input[3]);
+
+        for (String data: dataLines) {
+            int[] values = getHoursAndIncome(data);
+            salaryForPeriod += values[values.length - 1] * values[values.length - 2];
         }
         return salaryForPeriod;
     }
 
-    private String[][] getDataForPeriod(String name, String[] data,
-                                        String dateFrom, String dateTo) {
-        return Arrays.stream(data).filter(i -> i.contains(name))
-                .map(i -> i.split(" ")).filter(i -> convertToDate(i[0])
-                                .isAfter(convertToDate(dateFrom)) && convertToDate(i[0])
-                                .isBefore(convertToDate(dateTo))).toArray(String[][]::new);
+    private String[] getDataByPeriod(String[] data, String dateFrom, String dateTo) {
+        return Arrays.stream(data).filter(i -> convertToDate(split(i)[0])
+                .isBefore(convertToDate(dateTo)) && convertToDate(split(i)[0])
+                .isAfter(convertToDate(dateFrom))).toArray(String[]::new);
     }
 
-    public LocalDate convertToDate(String date) {
+    private String[] getDataByName(String name, String[] data) {
+        return Arrays.stream(data).filter(i -> i.contains(name)).toArray(String[]::new);
+    }
+
+    private LocalDate convertToDate(String date) {
         return LocalDate.parse(date, FORMATTER);
+    }
+
+    private String[] split(String dataLine) {
+        return dataLine.split(" ");
+    }
+
+    private int[] getHoursAndIncome(String dataLine) {
+        String[] split = split(dataLine);
+        return new int[]{Integer.parseInt(split[split.length - 1]),
+                Integer.parseInt(split[split.length - 2])};
     }
 }
