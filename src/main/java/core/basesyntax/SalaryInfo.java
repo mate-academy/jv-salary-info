472453cf587bf78243959cbfc8b33ab47a8ccbc8
Alex <65593125+Artificial-Friend@users.noreply.github.com>
2020-12-03 15:54:36 +0200

    It is bad but it is solved

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,8 +1,36 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+    private final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        final LocalDate from = LocalDate.parse(dateFrom, dateFormatter);
+        final LocalDate to = LocalDate.parse(dateTo, dateFormatter);
+
+        StringBuilder salaryResult = new StringBuilder().append("Report for period ")
+                                                        .append(dateFrom)
+                                                        .append(" - ")
+                                                        .append(dateTo);
+        for (String name : names) {
+            int nameSalary = 0;
+            for (String dateData : data) {
+                String[] dataNameHoursSalary = dateData.split(" ");
+                LocalDate day = LocalDate.parse(dataNameHoursSalary[0], dateFormatter);
+                if (dataNameHoursSalary[1].equals(name)
+                        && ((day.isAfter(from) || day.isEqual(from))
+                        && ((day.isBefore(to)) || day.isEqual(to)))) {
+                    nameSalary += Integer.parseInt(dataNameHoursSalary[2])
+                            * Integer.parseInt(dataNameHoursSalary[3]);
+                }
+            }
+            salaryResult.append("\n")
+                        .append(name)
+                        .append(" - ")
+                        .append(nameSalary);
+        }
+        return salaryResult.toString();
     }
 }
