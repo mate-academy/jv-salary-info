d08e03d3c78bfa6f54d083150bd3b3fc0f190f06
Rostyslav Mandybur <mandyburostyk@gmail.com>
2020-12-03 15:02:30 +0200

    Change date format

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,36 +1,30 @@
 package core.basesyntax;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    private static final DateTimeFormatter DATA_FORMAT = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder finalReport = new StringBuilder("Report for period "
                 + dateFrom + " - " + dateTo);
-        LocalDate startDate = LocalDate.of(Integer.parseInt(dateFrom.substring(6,10)),
-                Integer.parseInt(dateFrom.substring(3, 5)),
-                Integer.parseInt(dateFrom.substring(0, 2)));
-        LocalDate endDate = LocalDate.of(Integer.parseInt(dateTo.substring(6,10)),
-                Integer.parseInt(dateTo.substring(3, 5)),
-                Integer.parseInt(dateTo.substring(0, 2)));
+        LocalDate startDate = LocalDate.parse(dateFrom, DATA_FORMAT);
+        LocalDate endDate = LocalDate.parse(dateTo, DATA_FORMAT);
         for (int i = 0; i < names.length; i++) {
             int userSalary = 0;
             for (int j = 0; j < data.length; j++) {
-
-                LocalDate localDate = LocalDate.of(Integer.parseInt(data[j].substring(6,10)),
-                        Integer.parseInt(data[j].substring(3, 5)),
-                        Integer.parseInt(data[j].substring(0, 2)));
+                String[] userData = data[j].split(" ");
+                LocalDate localDate = LocalDate.parse(userData[0], DATA_FORMAT);
                 if ((localDate.equals(startDate) || localDate.equals(endDate)
                         || localDate.isAfter(startDate) && localDate.isBefore(endDate))
                         && data[j].contains(names[i])) {
-                    String[] userData = data[j].split(" ");
                     userSalary += Integer.parseInt(userData[2])
                             * Integer.parseInt(userData[3]);
                 }
             }
             finalReport.append("\n").append(names[i]).append(" - ").append(userSalary);
         }
-
         return finalReport.toString();
     }
 }
