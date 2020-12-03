e4cf7088e997d70c29ec62d4e9422d59426439b7
Yura <yuramitryahin@gmail.com>
2020-12-03 20:55:34 +0200

    fixed all bags

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,15 +1,13 @@
 package core.basesyntax;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder result = new StringBuilder().append("Report for period ")
-                .append(dateFrom + " - " + dateTo);
-
+                .append(dateFrom).append(" - ").append(dateTo);
         for (String name : names) {
             int sum = 0;
             for (String information : data) {
@@ -18,27 +16,18 @@ public class SalaryInfo {
                     sum += Integer.parseInt(parseData[2]) * Integer.parseInt(parseData[3]);
                 }
             }
-            result.append("\n" + name).append(" - " + sum);
+            result.append("\n").append(name).append(" - ").append(sum);
         }
         return result.toString();
     }
 
     private boolean checkDate(String dateFrom, String dateTo, String date) {
-        Date startDate = null;
-        Date finalDate = null;
-        Date localDate = null;
-        try {
-            startDate = new SimpleDateFormat("dd.MM.yyyy").parse(dateFrom);
-            finalDate = new SimpleDateFormat("dd.MM.yyyy").parse(dateTo);
-            localDate = new SimpleDateFormat("dd.MM.yyyy").parse(date);
 
-        } catch (ParseException e) {
-            System.out.println("Wrong date format!");
-        }
-        if ((localDate.after(startDate) && localDate.before(finalDate))
-                || localDate.equals(finalDate)) {
-            return true;
-        }
-        return false;
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDate startDate = LocalDate.parse(dateFrom, dtf);
+        LocalDate finalDate = LocalDate.parse(dateTo, dtf);
+        LocalDate localDate = LocalDate.parse(date, dtf);
+        return (localDate.isAfter(startDate) && localDate.isBefore(finalDate))
+                || localDate.isEqual(finalDate);
     }
 }
