f723549defbbe5cfaf8c0f2fcf8dcd1b1c09ee92
Andrii Bardin <andrii.bardin@gmail.com>
2020-12-03 20:02:49 +0200

    created required logic

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,8 +1,33 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+    private static final DateTimeFormatter FORMAT = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        LocalDate periodStart = LocalDate.parse(dateFrom, FORMAT);
+        LocalDate periodEnd = LocalDate.parse(dateTo, FORMAT);
+
+        StringBuilder report = new StringBuilder("Report for period ").append(dateFrom).append(" - ").append(dateTo);
+
+        for (String name : names) {
+            int income = 0;
+            for (String info : data) {
+                String[] splitInfo = info.split(" ");
+                LocalDate workingDate = LocalDate.parse(splitInfo[0], FORMAT);
+                String reportName = splitInfo[1];
+                int hoursWorked = Integer.parseInt(splitInfo[2]);
+                int hourlyRate = Integer.parseInt(splitInfo[3]);
+
+                if (reportName.equals(name) && (workingDate.isAfter(periodStart) || workingDate.isEqual(periodStart)) && (workingDate.isBefore(periodEnd) || workingDate.isEqual(periodEnd))) {
+                    income += hoursWorked * hourlyRate;
+                }
+            }
+            report.append("\n").append(name).append(" - ").append(income);
+        }
+        return report.toString();
+
     }
 }
