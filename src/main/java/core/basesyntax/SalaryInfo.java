cd0b32458850361613e7dbacd114866878355039
Rostyslav Mandybur <mandyburostyk@gmail.com>
2020-12-03 16:33:21 +0200

    Do small changes.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,18 +4,21 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private static final DateTimeFormatter DATA_FORMAT = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        StringBuilder finalReport = new StringBuilder("Report for period "
-                + dateFrom + " - " + dateTo);
-        LocalDate startDate = LocalDate.parse(dateFrom, DATA_FORMAT);
-        LocalDate endDate = LocalDate.parse(dateTo, DATA_FORMAT);
+        StringBuilder finalReport = new StringBuilder();
+        finalReport.append("Report for period ")
+                .append(dateFrom)
+                .append(" - ")
+                .append(dateTo);
+        LocalDate startDate = LocalDate.parse(dateFrom, DATE_FORMAT);
+        LocalDate endDate = LocalDate.parse(dateTo, DATE_FORMAT);
         for (int i = 0; i < names.length; i++) {
             int userSalary = 0;
             for (int j = 0; j < data.length; j++) {
                 String[] userData = data[j].split(" ");
-                LocalDate localDate = LocalDate.parse(userData[0], DATA_FORMAT);
+                LocalDate localDate = LocalDate.parse(userData[0], DATE_FORMAT);
                 if ((localDate.equals(startDate) || localDate.equals(endDate)
                         || localDate.isAfter(startDate) && localDate.isBefore(endDate))
                         && data[j].contains(names[i])) {
