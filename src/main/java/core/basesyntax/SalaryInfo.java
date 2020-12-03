5b383671e07de34830daf27ae57073ccd7c10d22
Okulov Vadim <Vedroidua@gmail.com>
2020-12-03 16:56:34 +0200

    Fix

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -7,26 +7,27 @@ public class SalaryInfo {
     private static final String DATE_FORMAT = "d.MM.yyyy";
     
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        StringBuilder report = new StringBuilder("Report for period " + dateFrom + " - " + dateTo);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_FORMAT);
         LocalDate localDateFrom = LocalDate.parse(dateFrom, formatter);
         LocalDate localDateTo = LocalDate.parse(dateTo, formatter);
-        
+        StringBuilder report = new StringBuilder()
+                .append("Report for period ")
+                .append(dateFrom)
+                .append(" - ")
+                .append(dateTo);
         for (String name : names) {
             int salary = 0;
-            
             for (String lineOfData : data) {
                 String[] info = lineOfData.split(" ");
-                LocalDate dateFromData = LocalDate.parse(info[0], formatter);
+                LocalDate currentDate = LocalDate.parse(info[0], formatter);
                 String nameFromData = info[1];
                 
                 if (nameFromData.equals(name)
-                        && (dateFromData.isAfter(localDateFrom)
-                        || dateFromData.isEqual(localDateFrom))
-                        && (dateFromData.isBefore(localDateTo)
-                        || dateFromData.isEqual(localDateTo))) {
-                    salary += Integer.parseInt(info[2])
-                            * Integer.parseInt(info[3]);
+                        && (currentDate.isAfter(localDateFrom)
+                        || currentDate.isEqual(localDateFrom))
+                        && (currentDate.isBefore(localDateTo)
+                        || currentDate.isEqual(localDateTo))) {
+                    salary += Integer.parseInt(info[2]) * Integer.parseInt(info[3]);
                 }
             }
             report.append("\n").append(name).append(" - ").append(salary);
