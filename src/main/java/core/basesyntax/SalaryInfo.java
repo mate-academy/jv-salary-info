5e390ac005d7c453389325a56c8a6e3ec3de12df
nikelok01@gmail.com <163amefeZ>
2020-12-03 17:08:27 +0200

    Fixed mistakes

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,22 +4,23 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate from = LocalDate.parse(dateFrom, DATE_FORMAT);
         LocalDate to = LocalDate.parse(dateTo, DATE_FORMAT);
-        StringBuilder reportPeriod = new StringBuilder("Report for period "
-                + from.format(DATE_FORMAT)
-                + " - " + to.format(DATE_FORMAT));
+        StringBuilder reportPeriod = new StringBuilder("Report for period ")
+                .append(from.format(DATE_FORMAT))
+                .append(" - ")
+                .append(to.format(DATE_FORMAT));
         for (String name : names) {
             int salaryBound = 0;
-            for (String information : data) {
-                String[] employeeSalary = information.split(" ");
+            for (String row : data) {
+                String[] employeeSalary = row.split(" ");
                 LocalDate workDay =
                         LocalDate.parse(employeeSalary[0], DATE_FORMAT);
                 if (name.equals(employeeSalary[1])
-                        && (from.isBefore(workDay) || from.isEqual(workDay))
+                        && (from.isBefore(workDay) || from.isEqual(workDay))    
                         && (to.isAfter(workDay) || to.isEqual(workDay))) {
                     salaryBound += Integer.parseInt(employeeSalary[2])
                             * Integer.parseInt(employeeSalary[3]);
