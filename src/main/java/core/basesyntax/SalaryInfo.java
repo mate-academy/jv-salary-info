a308438658029413b26a78f4bfff5577cbd99669
m.kislukhina <marharita.kislukhina@gmail.com>
2020-12-03 17:32:47 +0100

    a lot of appends. solution added

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,8 +1,36 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+    private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        StringBuilder result = new StringBuilder();
+        result.append("Report for period ");
+        result.append(dateFrom);
+        result.append(" - ");
+        result.append(dateTo);
+        LocalDate startDate = LocalDate.parse(dateFrom, DATE_FORMAT);
+        LocalDate endDate = LocalDate.parse(dateTo, DATE_FORMAT);
+
+        for (int i = 0; i < names.length; i++) {
+            int salary = 0;
+            for (int k = 0; k < data.length; k++) {
+                String[] dataPats = data[k].split(" ");
+                LocalDate localDate = LocalDate.parse(dataPats[0], DATE_FORMAT);
+                if (dataPats[1].equals(names[i]) && (localDate.equals(startDate)
+                        || localDate.equals(endDate) || localDate.isAfter(startDate)
+                        && localDate.isBefore(endDate))) {
+                    salary += Integer.parseInt(dataPats[2]) * Integer.parseInt(dataPats[3]);
+                }
+            }
+            result.append("\n");
+            result.append(names[i]);
+            result.append(" - ");
+            result.append(salary);
+        }
+        return result.toString();
     }
 }
