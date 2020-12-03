11cb1b9f0b3e7eb4af887105dd739c558773e4d4
ilya-brinchuk <ilya.brinchuk@gmail.com>
2020-12-03 19:26:02 +0200

    add corretion add StringBuilder

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,24 +4,26 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("d.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d.MM.yyyy");
-        LocalDate from = LocalDate.parse(dateFrom, formatter);
-        LocalDate to = LocalDate.parse(dateTo, formatter).plusDays(1);
-        String result = "Report for period " + dateFrom + " - " + dateTo + "\n";
+
+        LocalDate from = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate to = LocalDate.parse(dateTo, FORMATTER).plusDays(1);
+        StringBuilder result = new StringBuilder("Report for period " + dateFrom
+                + " - " + dateTo + "\n");
         for (String name : names) {
             int salary = 0;
             for (String informData : data) {
                 String[] line = informData.split(" ");
-                LocalDate factDate = LocalDate.parse(line[0], formatter);
+                LocalDate factDate = LocalDate.parse(line[0], FORMATTER);
                 if (line[1].equals(name) && (factDate.isBefore(to) && factDate.isAfter(from))) {
                     salary += Integer.parseInt(line[2]) * Integer.parseInt(line[3]);
                 }
             }
-            result += name + " - " + salary + "\n";
+            result.append(name).append(" - ").append(salary).append("\n");
         }
 
-        return result.substring(0, result.length() - 1);
+        return result.toString().trim();
     }
 }
