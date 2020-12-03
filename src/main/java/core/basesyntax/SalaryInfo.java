b73934de92b1f625f8fe4d0d3dc507a4cdbeaa25
IvanLuchkin <id.luchkin@gmail.com>
2020-12-03 20:25:58 +0200

    Rewritten using foreach loops

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -2,9 +2,6 @@ package core.basesyntax;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class SalaryInfo {
 
@@ -14,38 +11,28 @@ public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder report = new StringBuilder("Report for period ");
         report.append(dateFrom).append(" - ").append(dateTo).append("\n");
-        List<String> processedNames = new ArrayList<>();
-        Arrays.stream(data)
-                .forEach(dataRow -> {
-                    String rowName = dataRow.split(" ")[1];
-                    if (Arrays.asList(names).contains(rowName)
-                            && !processedNames.contains(rowName)) {
-                        report.append(rowName).append(" - ")
-                                .append(getIndividualSalary(data, dateFrom, dateTo, rowName))
-                                .append("\n");
-                        processedNames.add(rowName);
-                    }
-                });
+        for (String name : names) {
+            report.append(name).append(" - ")
+                    .append(getIndividualSalary(data, dateFrom, dateTo, name))
+                    .append("\n");
+        }
         return report.deleteCharAt(report.length() - 1).toString();
     }
 
     private static int getIndividualSalary(
             String[] data, String dateFrom, String dateTo, String rowName) {
-        return Arrays.stream(data)
-                .filter(row -> row.split(" ")[1].equals(rowName))
-                .mapToInt((row -> {
-                    String[] dividedRow = row.split(" ");
-                    LocalDate rowDate =
-                            LocalDate.parse(dividedRow[0], DATE_FORMATTER);
-                    return (rowDate
-                                .isAfter(LocalDate.parse(dateFrom, DATE_FORMATTER))
-                            && rowDate
-                                .isBefore(LocalDate.parse(dateTo, DATE_FORMATTER)
-                                    .plusDays(1)))
-                            ? Integer.parseInt(dividedRow[2])
-                                * Integer.parseInt(dividedRow[3])
-                            : 0;
-                }))
-                .sum();
+        int salary = 0;
+        for (String row : data) {
+            String[] dividedRow = row.split(" ");
+            LocalDate rowDate =
+                    LocalDate.parse(dividedRow[0], DATE_FORMATTER);
+            if (rowDate.isAfter(LocalDate.parse(dateFrom, DATE_FORMATTER))
+                    && rowDate.isBefore(LocalDate.parse(dateTo, DATE_FORMATTER).plusDays(1))
+                    && dividedRow[1].equals(rowName)) {
+                salary += Integer.parseInt(dividedRow[2])
+                        * Integer.parseInt(dividedRow[3]);
+            }
+        }
+        return salary;
     }
 }
