217824e636d4a0d0a7fcada36c7486c5f641f8bc
RuslanIbIsh <ruslan8ishankulov@gmail.com>
2020-12-04 17:16:01 +0200

    suggestions implemented

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,62 +1,46 @@
 package core.basesyntax;
 
-import java.text.ParseException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
 
+    private static final DateTimeFormatter DATE_TIME_FORMATTER =
+            DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        try {
-            LocalDate calendarFrom = convertDataToLocalDate(dateFrom);
-            LocalDate calendarTo = convertDataToLocalDate(dateTo);
 
-            StringBuilder employeeSalary = new StringBuilder();
-            employeeSalary.append("Report for period " + dateFrom + " - " + dateTo);
+        LocalDate calendarFrom = convertDataToLocalDate(dateFrom, DATE_TIME_FORMATTER);
+        LocalDate calendarTo = convertDataToLocalDate(dateTo, DATE_TIME_FORMATTER);
+        StringBuilder employeeSalary = new StringBuilder();
+        employeeSalary.append("Report for period " + dateFrom + " - " + dateTo);
 
-            for (String name: names) {
+        for (String name : names) {
+            int salary = 0;
+            for (String eachLine : data) {
+                String[] splitedData = eachLine.split(" ");
+                if (splitedData[1].equals(name)) {
+                    LocalDate currentDate =
+                            convertDataToLocalDate(splitedData[0], DATE_TIME_FORMATTER);
+                    if ((currentDate.isAfter(calendarFrom)
+                            || currentDate.isEqual(calendarFrom))
+                            && (currentDate.isBefore(calendarTo)
+                            || currentDate.isEqual(calendarTo))) {
 
-                int calcSalary = 0;
+                        salary += Integer.valueOf(splitedData[2])
+                                * Integer.valueOf(splitedData[3]);
 
-                for (String eachLine : data) {
-
-                    String[] parsedEchLine = eachLine.split(" ");
-
-                    if (parsedEchLine[1].equals(name)) {
-
-                        LocalDate currentDate = convertDataToLocalDate(parsedEchLine[0]);
-
-                        if ((currentDate.isAfter(calendarFrom)
-                                || currentDate.isEqual(calendarFrom))
-                                && (currentDate.isBefore(calendarTo)
-                                || currentDate.isEqual(calendarTo))) {
-
-                            calcSalary += Integer.valueOf(parsedEchLine[2])
-                                    * Integer.valueOf(parsedEchLine[3]);
-
-                        }
                     }
                 }
-
-                employeeSalary.append("\n" + name + " - " + calcSalary);
             }
-
-            return employeeSalary.toString();
-
-        } catch (ParseException e) {
-            System.out.println("Wrong format of date");
-            throw new RuntimeException();
+            employeeSalary.append("\n" + name + " - " + salary);
         }
+        return employeeSalary.toString();
     }
 
-    private LocalDate convertDataToLocalDate(String date) throws ParseException {
-
-        DateTimeFormatter sdf = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-
-        LocalDate localDate = LocalDate.parse(date, sdf);
-
+    private LocalDate convertDataToLocalDate(String date, DateTimeFormatter dateTimeFormatter) {
+        LocalDate localDate = LocalDate.parse(date, dateTimeFormatter);
         return localDate;
-
     }
 }
 
