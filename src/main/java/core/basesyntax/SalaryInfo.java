126d872c3867e8e42c703b16c76174506ac4e5e0
Baldinini <baldinat063@gmail.com>
2020-12-04 12:13:09 +0200

    bugs fixed

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,11 +4,11 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    static final DateTimeFormatter FORMATTER_DATE = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        final DateTimeFormatter formatterDate = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        LocalDate dateStart = LocalDate.parse(dateFrom, formatterDate);
-        LocalDate dateEnd = LocalDate.parse(dateTo, formatterDate);
+        LocalDate dateStart = LocalDate.parse(dateFrom, FORMATTER_DATE);
+        LocalDate dateEnd = LocalDate.parse(dateTo, FORMATTER_DATE);
         StringBuilder result = new StringBuilder();
         result.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
 
@@ -16,7 +16,7 @@ public class SalaryInfo {
             int sumSalary = 0;
             for (String date : data) {
                 String[] parameter = date.split(" ");
-                LocalDate parameterDate = LocalDate.parse(parameter[0], formatterDate);
+                LocalDate parameterDate = LocalDate.parse(parameter[0], FORMATTER_DATE);
                 if (name.equals(parameter[1])
                         && (parameterDate.isAfter(dateStart) || parameterDate.isEqual(dateStart))
                         && (parameterDate.isBefore(dateEnd) || parameterDate.isEqual(dateEnd))) {
@@ -24,7 +24,6 @@ public class SalaryInfo {
                 }
             }
             result.append("\n").append(name).append(" - ").append(sumSalary);
-
         }
         return result.toString();
     }
