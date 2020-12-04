01cd4cc68cc487a7403bedc6c3245af51d641d22
Alex <koleksandr4@gmail.com>
2020-12-04 12:24:04 +0200

    fixed code

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,10 +4,11 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    static final DateTimeFormatter FORMAT = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        LocalDate from = LocalDate.parse(dateFrom, DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-        LocalDate to = LocalDate.parse(dateTo, DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        LocalDate from = LocalDate.parse(dateFrom, FORMAT);
+        LocalDate to = LocalDate.parse(dateTo, FORMAT);
         StringBuilder infoAboutPeople = new StringBuilder("Report for period "
                 + dateFrom + " - " + dateTo);
         for (String name : names) {
@@ -18,8 +19,7 @@ public class SalaryInfo {
                 String personName = salary[1];
                 String hours = salary[2];
                 String moneyPerHour = salary[3];
-                LocalDate localDate = LocalDate.parse(date,
-                        DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+                LocalDate localDate = LocalDate.parse(date, FORMAT);
                 if (name.equals(personName)
                         && (localDate.isAfter(from) && localDate.isBefore(to)
                         || localDate.equals(from) || localDate.equals(to))) {
