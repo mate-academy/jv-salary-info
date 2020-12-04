05147a095672bbaed6a28b50e0231f0f9eebdf52
Alekearl <alexandrhurko@gmail.com>
2020-12-04 16:22:26 +0200

    implemented method getSalaryInfo

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,8 +1,29 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
 
-    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+    String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDate beginFrom = LocalDate.parse(dateFrom, formatter);
+        LocalDate endTo = LocalDate.parse(dateTo, formatter);
+        StringBuilder builder = new StringBuilder("Report for period " + dateFrom + " - " + dateTo);
+
+        for (String name : names) {
+            int counter = 0;
+            for (String matcher : data) {
+                LocalDate date = LocalDate.parse(matcher.split(" ")[0], formatter);
+                if (name.equals(matcher.split(" ")[1])
+                        && (date.isAfter(beginFrom) && date.isBefore(endTo)
+                        || date.isEqual(beginFrom) || date.isEqual(endTo))) {
+                    counter += Integer.parseInt(matcher.split(" ")[2])
+                            * Integer.parseInt(matcher.split(" ")[3]);
+                }
+            }
+            builder.append("\n").append(name).append(" - ").append(counter);
+        }
+        return builder.toString();
     }
 }
