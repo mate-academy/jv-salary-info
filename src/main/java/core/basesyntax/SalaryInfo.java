94da159c50efa36973eff006618434a4f288f08e
OleksandrPokotylenko <aln.nko.of@gmail.com>
2020-12-04 14:13:19 +0200

    fixed

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,37 +4,27 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    private static final DateTimeFormatter DMY = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        DateTimeFormatter dmy = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        LocalDate firstDate = LocalDate.parse(dateFrom, dmy);
-        LocalDate secondDate = LocalDate.parse(dateTo, dmy);
-        int[] totalSalary = new int[names.length];
+        LocalDate firstDate = LocalDate.parse(dateFrom, DMY);
+        LocalDate secondDate = LocalDate.parse(dateTo, DMY);
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Report for period ").append(firstDate.format(DMY)).append(" - ")
+                .append(secondDate.format(DMY));
 
-        for (int i = 0; i < names.length; i++) {
+        for (String name : names) {
+            int totalSalary = 0;
             for (String record : data) {
                 String[] buffer = record.split(" ");
-                if (names[i].equals(buffer[1])) {
-                    LocalDate currentDate = LocalDate.parse(buffer[0], dmy);
-                    if (currentDate.isAfter(firstDate)
-                            && currentDate.isBefore(secondDate)
-                            || currentDate.isEqual(firstDate)
-                            || currentDate.isEqual(secondDate)) {
-                        totalSalary[i] += Integer.parseInt(buffer[2]) * Integer.parseInt(buffer[3]);
-                    }
+                LocalDate currentDate = LocalDate.parse(buffer[0], DMY);
+                if (name.equals(buffer[1]) && (currentDate.isAfter(firstDate)
+                        && currentDate.isBefore(secondDate) || currentDate.isEqual(firstDate)
+                        || currentDate.isEqual(secondDate))) {
+                    totalSalary += Integer.parseInt(buffer[2]) * Integer.parseInt(buffer[3]);
                 }
             }
-        }
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("Report for period ")
-                      .append(firstDate.format(dmy))
-                      .append(" - ")
-                      .append(secondDate.format(dmy));
-        for (int i = 0; i < names.length; i++) {
-            stringBuilder.append("\n")
-                         .append(names[i])
-                         .append(" - ")
-                         .append(totalSalary[i]);
+            stringBuilder.append("\n").append(name).append(" - ").append(totalSalary);
         }
         return stringBuilder.toString();
     }
