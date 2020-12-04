62cc8752dea592f38a928489da132bdb22031407
Vitalik Kopach <apple_air@ukr.net>
2020-12-04 12:12:53 +0200

    added constant value FORMATTER and some changes in method getSalaryInfo

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,33 +5,31 @@ import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
 
+    public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         int salary;
 
-        final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        LocalDate from = LocalDate.parse(dateFrom, dateFormatter);
-        LocalDate to = LocalDate.parse(dateTo,dateFormatter);
-        StringBuilder info = new StringBuilder("Report for period "
-                                                + dateFrom + " - " + dateTo + "\n");
+        LocalDate from = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate to = LocalDate.parse(dateTo, FORMATTER);
+        StringBuilder info = new StringBuilder();
+        info.append("Report for period " + dateFrom + " - " + dateTo);
 
         for (int i = 0; i < names.length; i++) {
-            //update variable
             salary = 0;
             for (int k = 0; k < data.length; k++) {
                 String[] date = data[k].split(" "); // parse string to part "25.04.2019 John 60 50"
-                LocalDate current = LocalDate.parse(date[0], dateFormatter);
+                LocalDate current = LocalDate.parse(date[0], FORMATTER);
 
-                if (names[i].equals(date[1])) {
-                    if (from.isBefore(current) && current.isBefore(to)
-                            || from.isBefore(current) && current.equals(to)) {
-                        int hours = Integer.parseInt(date[2]);
-                        int salaryPerHour = Integer.parseInt(date[3]);
-                        salary = salary + (salaryPerHour * hours);
-                    }
+                if (names[i].equals(date[1]) && (from.isBefore(current)
+                        && (current.isBefore(to) | current.equals(to)))) {
+                    int hours = Integer.parseInt(date[2]);
+                    int salaryPerHour = Integer.parseInt(date[3]);
+                    salary += (salaryPerHour * hours);
                 }
             }
-            info.append(names[i]).append(" - ").append(salary).append("\n");
+            info.append("\n" + names[i]).append(" - ").append(salary);
         }
-        return info.toString().trim();
+        return info.toString();
     }
 }
