5489f04567f1b680ad273a5d79d681e899edd462
Fedenko Maksym <fedenko.maxim.a@gmail.com>
2020-12-04 11:59:04 +0200

    fixed remarks

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -6,7 +6,7 @@ import java.time.format.DateTimeFormatter;
 public class SalaryInfo {
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        DateTimeFormatter formatDate = DateTimeFormatter.ofPattern("d.MM.yyyy");
+        final DateTimeFormatter formatDate = DateTimeFormatter.ofPattern("d.MM.yyyy");
         StringBuilder returnValue = new StringBuilder();
         returnValue = returnValue.append("Report for period ").append(dateFrom)
                 .append(" - ").append(dateTo).append("\n");
@@ -20,21 +20,15 @@ public class SalaryInfo {
                 listFromData = data[j].split("\\s+");
                 dateWhenWorking = LocalDate.parse(listFromData[0], formatDate);
                 if (names[i].equals(listFromData[1])
-                        && (dateWhenWorking.isAfter(dateFromWorking)
-                        || dateWhenWorking.compareTo(dateFromWorking) == 0)
-                        && (dateWhenWorking.isBefore(dateToWorking)
-                        || dateWhenWorking.compareTo(dateToWorking) == 0)) {
+                        && dateWhenWorking.compareTo(dateFromWorking) >= 0
+                        && dateWhenWorking.compareTo(dateToWorking) <= 0) {
                     salary += Integer.parseInt(listFromData[2])
                             * Integer.parseInt(listFromData[3]);
                 }
             }
-            if (i == names.length - 1) {
-                returnValue.append(names[i] + " - " + salary);
-            } else {
-                returnValue.append(names[i] + " - " + salary + "\n");
-            }
+            returnValue.append(names[i] + " - " + salary + "\n");
             salary = 0;
         }
-        return returnValue.toString();
+        return returnValue.toString().trim();
     }
 }
