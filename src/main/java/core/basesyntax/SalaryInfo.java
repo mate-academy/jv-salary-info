b165bc0aee7de48933df97a258ceb2944c13d2a1
Serhii Semenykhin <semyonich@users.noreply.github.com>
2020-12-04 08:03:57 +0200

    DataFormatter used, unneccessary loop removed

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,31 +1,21 @@
 package core.basesyntax;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         int[] salaries = new int[names.length];
-        String[] stringDateFromArray = dateFrom.split("\\.");
-        String[] stringDateToArray = dateTo.split("\\.");
-        LocalDate dateStart = LocalDate.of(Integer.parseInt(stringDateFromArray[2]),
-                Integer.parseInt(stringDateFromArray[1]),
-                Integer.parseInt(stringDateFromArray[0]));
-        LocalDate dateEnd = LocalDate.of(Integer.parseInt(stringDateToArray[2]),
-                Integer.parseInt(stringDateToArray[1]),
-                Integer.parseInt(stringDateToArray[0]));
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDate dateStart = LocalDate.parse(dateFrom, dateFormatter).minusDays(1);
+        LocalDate dateEnd = LocalDate.parse(dateTo, dateFormatter).plusDays(1);
+        LocalDate currentDate;
         for (int i = 0; i < names.length; i++) {
             for (int j = 0; j < data.length; j++) {
                 String[] currentLine = data[j].split(" ");
-                int[] currentDateInt = new int[3];
-                String[] currentDateArray = currentLine[0].split("\\.");
-                for (int k = 0; k < 3; k++) {
-                    currentDateInt[k] = Integer.parseInt(currentDateArray[k]);
-                }
-                LocalDate currentDate = LocalDate.of(currentDateInt[2], currentDateInt[1],
-                        currentDateInt[0]);
-                if (currentDate.isAfter(dateStart.minusDays(1))
-                        && currentDate.isBefore(dateEnd.plusDays(1))
+                currentDate = LocalDate.parse(currentLine[0], dateFormatter);
+                if (currentDate.isAfter(dateStart) && currentDate.isBefore(dateEnd)
                         && currentLine[1].equals(names[i])) {
                     salaries[i] += Integer.parseInt(currentLine[2])
                             * Integer.parseInt(currentLine[3]);
