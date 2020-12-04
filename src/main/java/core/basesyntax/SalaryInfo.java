9e982af9a0cd30d1280c3611a9b29ebc1bfdbfed
Fedenko Maksym <fedenko.maxim.a@gmail.com>
2020-12-04 15:05:00 +0200

    updated to constant

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,21 +4,21 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("d.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        final DateTimeFormatter formatDate = DateTimeFormatter.ofPattern("d.MM.yyyy");
         StringBuilder returnValue = new StringBuilder();
         returnValue = returnValue.append("Report for period ").append(dateFrom)
                 .append(" - ").append(dateTo).append("\n");
         int salary = 0;
         String[] listFromData;
         LocalDate dateWhenWorking;
-        LocalDate dateFromWorking = LocalDate.parse(dateFrom, formatDate);
-        LocalDate dateToWorking = LocalDate.parse(dateTo, formatDate);
+        LocalDate dateFromWorking = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate dateToWorking = LocalDate.parse(dateTo, FORMATTER);
         for (int i = 0; i < names.length; i++) {
             for (int j = 0; j < data.length; j++) {
                 listFromData = data[j].split("\\s+");
-                dateWhenWorking = LocalDate.parse(listFromData[0], formatDate);
+                dateWhenWorking = LocalDate.parse(listFromData[0], FORMATTER);
                 if (names[i].equals(listFromData[1])
                         && dateWhenWorking.compareTo(dateFromWorking) >= 0
                         && dateWhenWorking.compareTo(dateToWorking) <= 0) {
