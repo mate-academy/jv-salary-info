3696181d3f2f3115d151a15a96df9f2c17a482dc
sanek98cv <73754789+sanek98cv@users.noreply.github.com>
2020-12-04 08:45:30 +0200

    Solution

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,8 +1,31 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("d.MM.yyyy");
+        LocalDate startWork = LocalDate.parse(dateFrom, dateFormatter);
+        LocalDate finishWork = LocalDate.parse(dateTo, dateFormatter);
+        StringBuilder infoList = new StringBuilder();
+        infoList.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
+
+        for (String name : names) {
+            int employeesSalary = 0;
+            for (String currentData : data) {
+                String[] dateResult = currentData.split(" ");
+                LocalDate localDate = LocalDate.parse(dateResult[0], dateFormatter);
+                if (dateResult[1].equals(name) && ((localDate.isAfter(startWork)
+                        && localDate.isBefore(finishWork)) || localDate.isEqual(startWork)
+                        || localDate.isEqual(finishWork))) {
+                    employeesSalary += Integer.parseInt(dateResult[2])
+                            * Integer.parseInt(dateResult[3]);
+                }
+            }
+            infoList.append("\n").append(name).append(" - ").append(employeesSalary);
+        }
+        return infoList.toString();
     }
 }
