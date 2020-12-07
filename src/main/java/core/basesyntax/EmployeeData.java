f2d55c41740595ea9d8ddf776d2e805dbe132c69
tbondare <tanya.bon2016@gmail.com>
2020-12-07 15:23:51 +0200

    solution was added firstly

---
+++ src/main/java/core/basesyntax/EmployeeData.java
@@ -0,0 +1,10 @@
+package core.basesyntax;
+
+import java.util.Date;
+
+public class EmployeeData {
+    public Date currentDate;
+    public String name;
+    public int hours;
+    public int income;
+}
