6fa6929e8e3380891217610845f05778fb8a6fba
Tetiana Chupika <mtv1301@gmail.com>
2021-02-17 15:13:22 +0200

    added solution to home work salary info

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -14,7 +14,7 @@ public class SalaryInfo {
         formatter.format(parseDateFrom);
         LocalDate parseDateTo = LocalDate.parse(dateTo, formatter);
         formatter.format(parseDateTo);
-        int[] saleryArray = new int[3];
+        int[] salaryArray = new int[3];
         StringBuilder builder = new StringBuilder();
         for (String datum : data) {
             String dateStringValue = datum.substring(0, DATE_PATTERN.length());
@@ -23,25 +23,25 @@ public class SalaryInfo {
                     || (salaryDate.isEqual(parseDateTo))) {
                 String[] substringSalaryArray = datum.substring(DATE_PATTERN.length() + 1)
                         .split(" ");
-                String[] namesAndSaleryArray = new String[substringSalaryArray.length - 1];
-                namesAndSaleryArray[0] = substringSalaryArray[0];
-                namesAndSaleryArray[1] = Integer.toString(
+                String[] namesAndSalaryArray = new String[substringSalaryArray.length - 1];
+                namesAndSalaryArray[0] = substringSalaryArray[0];
+                namesAndSalaryArray[1] = Integer.toString(
                         Integer.parseInt(substringSalaryArray[1])
                                 * Integer.parseInt(substringSalaryArray[2]));
 
                 if (substringSalaryArray[0].equals(names[0])) {
-                    saleryArray[0] += Integer.parseInt(namesAndSaleryArray[1]);
+                    salaryArray[0] += Integer.parseInt(namesAndSalaryArray[1]);
                 } else if (substringSalaryArray[0].equals(names[1])) {
-                    saleryArray[1] += Integer.parseInt(namesAndSaleryArray[1]);
+                    salaryArray[1] += Integer.parseInt(namesAndSalaryArray[1]);
                 } else if (substringSalaryArray[0].equals(names[2])) {
-                    saleryArray[2] += Integer.parseInt(namesAndSaleryArray[1]);
+                    salaryArray[2] += Integer.parseInt(namesAndSalaryArray[1]);
                 }
             }
         }
         return (builder.append("Report for period ").append(dateFrom).append(" - ")
-                .append(dateTo).append("\n").append(names[0]).append(" - ").append(saleryArray[0])
-                .append("\n").append(names[1]).append(" - ").append(saleryArray[1])
-                .append("\n").append(names[2]).append(" - ").append(saleryArray[2]).toString());
+                .append(dateTo).append("\n").append(names[0]).append(" - ").append(salaryArray[0])
+                .append("\n").append(names[1]).append(" - ").append(salaryArray[1])
+                .append("\n").append(names[2]).append(" - ").append(salaryArray[2]).toString());
 
     }
 }
