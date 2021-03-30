5ca873e005fe9333015dd82a7eeec37f5d487f32
dant0n <dmibuc@gmail.com>
2021-03-30 23:47:43 +0300

    Finish homework requirements.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,8 +1,32 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+    private final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        String initialString = "Report for period " + dateFrom + " - " + dateTo + "\n";
+        StringBuilder stringBuilder = new StringBuilder(initialString);
+        LocalDate startDate = LocalDate.parse(dateFrom, dateTimeFormatter);
+        LocalDate endDate = LocalDate.parse(dateTo, dateTimeFormatter);
+        for (String name : names) {
+            int totalSalary = 0;
+            for (String line : data) {
+                String[] entities = line.split(" ");
+                LocalDate dateWorked = LocalDate.parse(entities[0], dateTimeFormatter);
+                if (name.equals(entities[1])
+                        && !dateWorked.isBefore(startDate)
+                        && !dateWorked.isAfter(endDate)) {
+                    int hoursWorked = Integer.parseInt(entities[2]);
+                    int ratePerHour = Integer.parseInt(entities[3]);
+                    totalSalary += hoursWorked * ratePerHour;
+                }
+            }
+            String resultString = name + " - " + totalSalary + "\n";
+            stringBuilder.append(resultString);
+        }
+        return stringBuilder.toString().trim();
     }
 }
