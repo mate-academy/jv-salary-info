296862f59253f59cb8aff3268917004886a6d929
dant0n <dmibuc@gmail.com>
2021-03-31 11:53:17 +0300

    Make DateTimeFormatter a constant.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,18 +4,19 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final DateTimeFormatter DATE_TIME_FORMATTER =
+            DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         String initialString = "Report for period " + dateFrom + " - " + dateTo + "\n";
         StringBuilder stringBuilder = new StringBuilder(initialString);
-        LocalDate startDate = LocalDate.parse(dateFrom, dateTimeFormatter);
-        LocalDate endDate = LocalDate.parse(dateTo, dateTimeFormatter);
+        LocalDate startDate = LocalDate.parse(dateFrom, DATE_TIME_FORMATTER);
+        LocalDate endDate = LocalDate.parse(dateTo, DATE_TIME_FORMATTER);
         for (String name : names) {
             int totalSalary = 0;
             for (String line : data) {
                 String[] entities = line.split(" ");
-                LocalDate dateWorked = LocalDate.parse(entities[0], dateTimeFormatter);
+                LocalDate dateWorked = LocalDate.parse(entities[0], DATE_TIME_FORMATTER);
                 if (name.equals(entities[1])
                         && !dateWorked.isBefore(startDate)
                         && !dateWorked.isAfter(endDate)) {
