07106ce79c6e7d003944d99ded2c2efc13d2e3a9
dvdovynets <vdovinec@gmail.com>
2021-03-31 12:03:07 +0300

    cleared try-catch and add modifier

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -2,41 +2,35 @@ package core.basesyntax;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
 
 public class SalaryInfo {
-    static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter
+            .ofPattern("dd.MM.yyyy");
 
-    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo)
-            throws DateTimeParseException {
+    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
+        int[] salarySums = new int[names.length];
+        LocalDate dateStart = LocalDate.parse(dateFrom, DATE_FORMATTER);
+        LocalDate dateFinish = LocalDate.parse(dateTo, DATE_FORMATTER);
 
-        try {
-            int[] salarySums = new int[names.length];
-            LocalDate dateStart = LocalDate.parse(dateFrom, DATE_FORMATTER);
-            LocalDate dateFinish = LocalDate.parse(dateTo, DATE_FORMATTER);
-
-            for (String value : data) {
-                String[] separateValues = value.split(" ");
-                LocalDate currentDate = LocalDate.parse(separateValues[0], DATE_FORMATTER);
-                if (dateStart.compareTo(currentDate) <= 0
-                        && dateFinish.compareTo(currentDate) >= 0) {
-                    for (int j = 0; j < names.length; j++) {
-                        if (names[j].equals(separateValues[1])) {
-                            salarySums[j] += Integer.parseInt(separateValues[2])
-                                    * Integer.parseInt(separateValues[3]);
-                        }
+        for (String value : data) {
+            String[] separateValues = value.split(" ");
+            LocalDate currentDate = LocalDate.parse(separateValues[0], DATE_FORMATTER);
+            if (dateStart.compareTo(currentDate) <= 0
+                    && dateFinish.compareTo(currentDate) >= 0) {
+                for (int j = 0; j < names.length; j++) {
+                    if (names[j].equals(separateValues[1])) {
+                        salarySums[j] += Integer.parseInt(separateValues[2])
+                                * Integer.parseInt(separateValues[3]);
                     }
                 }
             }
-            StringBuilder salarySummary = new StringBuilder(
-                    String.format("Report for period %s - %s\n", dateFrom, dateTo));
-
-            for (int i = 0; i < names.length; i++) {
-                salarySummary.append(String.format("%s - %d\n", names[i], salarySums[i]));
-            }
-            return salarySummary.toString().trim();
-        } catch (DateTimeParseException ex) {
-            throw new RuntimeException("Date doesn't match format dd.mm.yyyy " + ex);
         }
+        StringBuilder salarySummary = new StringBuilder(
+                String.format("Report for period %s - %s\n", dateFrom, dateTo));
+
+        for (int i = 0; i < names.length; i++) {
+            salarySummary.append(String.format("%s - %d\n", names[i], salarySums[i]));
+        }
+        return salarySummary.toString().trim();
     }
 }
