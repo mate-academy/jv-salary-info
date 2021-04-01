bd4538b253b2e9c9851874d9e9c4e6f60eb29647
Stanislav Aleksandrovych <astanislav2361@gmail.com>
2021-04-01 22:49:56 +0300

    almost done

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,8 +1,45 @@
 package core.basesyntax;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+    private static final DateTimeFormatter FORMAT = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int DAYS_INDEX = 2;
+    private static final int INCOME_INDEX = 3;
+    private static final int NEW_ARRAY_INDEX = 2;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        LocalDate firstDate = LocalDate.parse(dateFrom, FORMAT);
+        LocalDate lastDate = LocalDate.parse(dateTo, FORMAT);
+        String[] newData = new String[NEW_ARRAY_INDEX];
+        StringBuilder finalList = new StringBuilder();
+
+        finalList.append("Report for period").append(" ")
+                .append(dateFrom).append(" - ").append(dateTo);
+
+        for (int i = 0; i < data.length; i++) {
+            String[] employeesData = data[i].split(" ");
+
+            LocalDate actualDate = LocalDate.parse(employeesData[DATE_INDEX], FORMAT);
+            String employeeName = employeesData[NAME_INDEX];
+
+            if (actualDate.isAfter(firstDate) && actualDate.isBefore(lastDate)) {
+                for (int y = 0; y < names.length; y++) {
+                    if (names[y].equals(employeeName)) {
+                        finalList.append("\n").append(employeeName).append(" ")
+                                .append(Integer.toString(Integer.parseInt(employeesData[DAYS_INDEX])
+                                * Integer.parseInt(employeesData[INCOME_INDEX])));
+
+                    }
+                }
+            }
+        }
+        return finalList.toString();
     }
 }
