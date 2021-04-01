d59029d774ad1e5d7b89eb257a472ff0c0aa1f87
Den4eg007 <68178197+Den4eg007@users.noreply.github.com>
2021-04-01 23:53:08 +0300

    Added implementation for SalaryInfo class

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,8 +1,42 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+    private final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Report for period ").append(dateFrom)
+                .append(" - ").append(dateTo).append("\n");
+        LocalDate dateFromLocalTime = LocalDate.parse(dateFrom, dateTimeFormatter);
+        LocalDate dateToLocalTime = LocalDate.parse(dateTo, dateTimeFormatter);
+        LocalDate currentEmployeeDate;
+        String[] currentEmployee;
+        int hoursWorked;
+        int moneyPerHour;
+        int totalIncome;
+
+        for (String name : names) {
+            totalIncome = 0;
+            for (String datum : data) {
+                if (datum.contains(name)) {
+                    currentEmployee = datum.split(" ");
+                    currentEmployeeDate = LocalDate.parse(currentEmployee[0],
+                            dateTimeFormatter);
+                    if ((currentEmployeeDate.isAfter(dateFromLocalTime)
+                            && currentEmployeeDate.isBefore(dateToLocalTime))
+                            || currentEmployeeDate.equals(dateToLocalTime)) {
+                        hoursWorked = Integer.parseInt(currentEmployee[2]);
+                        moneyPerHour = Integer.parseInt(currentEmployee[3]);
+                        totalIncome += hoursWorked * moneyPerHour;
+                    }
+                }
+            }
+            stringBuilder.append(name).append(" - ").append(totalIncome).append("\n");
+        }
+
+        return stringBuilder.toString().trim();
     }
 }
