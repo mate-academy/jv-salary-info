ba3ffadded8c666a56ef7a0b0503d346e807ff79
safordog <safordog@gmail.com>
2021-04-01 15:26:59 +0300

    added constants

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,14 +4,19 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private final String formatter = "dd.MM.yyyy";
+    private static final String DATE_PATTERN = "dd.MM.yyyy";
+    private static final String LINE_SEPARATOR = "\n";
+    private static final String HEADER_OF_RESPONSE = "Report for period ";
+    private static final String DASH = " - ";
+    private static final String HOURS_PATTERN = "[\\d\\.]+[\\D]+ ([\\d]+)[\\s\\d]+$";
+    private static final String RATE_PATTERN = "[\\d\\.]+[\\D]+ [\\d]+\\s([\\d]+)$";
+    private static final String PATTERN_FOR_CHECK_DATE = "([\\d\\.]+)[\\w\\d\\s]+";
+    private static final String ONLY_LETTERS_PATTERN = "[^A-Za-z]+";
 
     protected String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        if (names == null || data == null || dateFrom == null || dateTo == null) {
-            return "Incorrect input data";
-        }
         StringBuilder sb = new StringBuilder();
-        sb.append("Report for period ").append(dateFrom).append(" - ").append(dateTo).append("\n");
+        sb.append(HEADER_OF_RESPONSE).append(dateFrom)
+                .append(DASH).append(dateTo).append(LINE_SEPARATOR);
         LocalDate localDateFrom = parseStringToLocalDate(dateFrom);
         LocalDate localDateTo = parseStringToLocalDate(dateTo);
         int salarySummary;
@@ -20,23 +25,23 @@ public class SalaryInfo {
             for (int j = 0; j < data.length; j++) {
                 if (checkName(names[i], data[j])) {
                     if (checkDate(localDateFrom, localDateTo.plusDays(1), data[j])) {
-                        salarySummary += parseAndCalcSalary(data[j]);
+                        salarySummary += calculateSalary(data[j]);
                     }
                 }
             }
-            sb.append(names[i]).append(" - ").append(salarySummary).append("\n");
+            sb.append(names[i]).append(DASH).append(salarySummary).append(LINE_SEPARATOR);
         }
         sb.setLength(sb.length() - 1);
         return sb.toString();
     }
 
     private LocalDate parseStringToLocalDate(String date) {
-        return LocalDate.parse(date, DateTimeFormatter.ofPattern(formatter));
+        return LocalDate.parse(date, DateTimeFormatter.ofPattern(DATE_PATTERN));
     }
 
     private boolean checkName(String name, String data) {
         if (name != null && data != null) {
-            data = data.replaceAll("[^A-Za-z]+", "");
+            data = data.replaceAll(ONLY_LETTERS_PATTERN, "");
             if (data.equals(name)) {
                 return true;
             }
@@ -46,7 +51,7 @@ public class SalaryInfo {
 
     private boolean checkDate(LocalDate dateFrom, LocalDate dateTo, String data) {
         if (data != null) {
-            data = data.replaceAll("([\\d\\.]+)[\\w\\d\\s]+", "$1");
+            data = data.replaceAll(PATTERN_FOR_CHECK_DATE, "$1");
             LocalDate dateFromData = parseStringToLocalDate(data);
             if (dateFromData.isAfter(dateFrom) && dateFromData.isBefore(dateTo)) {
                 return true;
@@ -55,10 +60,9 @@ public class SalaryInfo {
         return false;
     }
 
-    private int parseAndCalcSalary(String data) {
-        int hours = Integer.parseInt(data.replaceAll("[\\d\\.]+[\\D]+ ([\\d]+)[\\s\\d]+$", "$1"));
-        int rate = Integer.parseInt(data.replaceAll("[\\d\\.]+[\\D]+ [\\d]+\\s([\\d]+)$", "$1"));
+    private int calculateSalary(String data) {
+        int hours = Integer.parseInt(data.replaceAll(HOURS_PATTERN, "$1"));
+        int rate = Integer.parseInt(data.replaceAll(RATE_PATTERN, "$1"));
         return hours * rate;
     }
-
 }
