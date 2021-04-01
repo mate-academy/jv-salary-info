fc28ec9be904c15edba6a2486ca7c75da73af1b2
Dmitriy <dmitriy4ever97@gmail.com>
2021-04-01 19:45:40 +0300

    Done Homework.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,8 +1,35 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+    private static final DateTimeFormatter DATE_TIME_FORMATTER =
+            DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        LocalDate beginDate = LocalDate.parse(dateFrom, DATE_TIME_FORMATTER);
+        LocalDate endDate = LocalDate.parse(dateTo, DATE_TIME_FORMATTER);
+        StringBuilder reportString = new StringBuilder("Report for period "
+                + dateFrom + " - " + dateTo + "\n");
+        int fullSalary = 0;
+        int workingHours;
+        int paymentPerHour;
+        for (String name : names) {
+            for (String dataInfo : data) {
+                if (name.equals(dataInfo.split(" ")[1])
+                        && !LocalDate.parse(dataInfo.split(" ")[0],
+                        DATE_TIME_FORMATTER).isBefore(beginDate)
+                        && !LocalDate.parse(dataInfo.split(" ")[0],
+                        DATE_TIME_FORMATTER).isAfter(endDate)) {
+                    workingHours = Integer.parseInt(dataInfo.split(" ")[2]);
+                    paymentPerHour = Integer.parseInt(dataInfo.split(" ")[3]);
+                    fullSalary += workingHours * paymentPerHour;
+                }
+            }
+            reportString.append(name).append(" - ").append(fullSalary).append("\n");
+            fullSalary = 0;
+        }
+        return reportString.toString().trim();
     }
 }
