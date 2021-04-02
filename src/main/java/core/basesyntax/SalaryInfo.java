57531775a023f530d317d3aa17e820654eb8725b
Den4eg007 <68178197+Den4eg007@users.noreply.github.com>
2021-04-02 11:41:22 +0300

    Fixed some style issues

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,30 +4,29 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final DateTimeFormatter TIME_FORMATTER =
+            DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append("Report for period ").append(dateFrom)
                 .append(" - ").append(dateTo).append("\n");
-        LocalDate dateFromLocalTime = LocalDate.parse(dateFrom, dateTimeFormatter);
-        LocalDate dateToLocalTime = LocalDate.parse(dateTo, dateTimeFormatter);
-        LocalDate currentEmployeeDate;
-        String[] currentEmployee;
-        int hoursWorked;
-        int moneyPerHour;
-        int totalIncome;
+        LocalDate dateFromTime = LocalDate.parse(dateFrom, TIME_FORMATTER);
+        LocalDate dateToTime = LocalDate.parse(dateTo, TIME_FORMATTER);
 
         for (String name : names) {
-            totalIncome = 0;
+            int hoursWorked;
+            int moneyPerHour;
+            int totalIncome = 0;
             for (String datum : data) {
                 if (datum.contains(name)) {
-                    currentEmployee = datum.split(" ");
-                    currentEmployeeDate = LocalDate.parse(currentEmployee[0],
-                            dateTimeFormatter);
-                    if ((currentEmployeeDate.isAfter(dateFromLocalTime)
-                            && currentEmployeeDate.isBefore(dateToLocalTime))
-                            || currentEmployeeDate.equals(dateToLocalTime)) {
+                    String[] currentEmployee = datum.split(" ");
+                    LocalDate currentEmployeeDate = LocalDate.parse(currentEmployee[0],
+                            TIME_FORMATTER);
+                    if ((currentEmployeeDate.isAfter(dateFromTime)
+                            && currentEmployeeDate.isBefore(dateToTime))
+                            || currentEmployeeDate.equals(dateFromTime)
+                            || currentEmployeeDate.equals(dateToTime)) {
                         hoursWorked = Integer.parseInt(currentEmployee[2]);
                         moneyPerHour = Integer.parseInt(currentEmployee[3]);
                         totalIncome += hoursWorked * moneyPerHour;
@@ -36,7 +35,6 @@ public class SalaryInfo {
             }
             stringBuilder.append(name).append(" - ").append(totalIncome).append("\n");
         }
-
         return stringBuilder.toString().trim();
     }
 }
