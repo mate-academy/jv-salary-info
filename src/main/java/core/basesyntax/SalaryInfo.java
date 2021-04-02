1401672f5a306ea4ff73bda208fd08adeb03ca80
the5tranger <mishaantonyuk00@gmail.com>
2021-04-02 16:40:39 +0300

    Homework_salary_v1.2

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -12,8 +12,13 @@ public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate startDate = LocalDate.parse(dateFrom, DATE_FORMAT);
         LocalDate endDate = LocalDate.parse(dateTo, DATE_FORMAT);
-        int[] salary = new int[names.length];
+        StringBuilder result = new StringBuilder();
+        int[] totalSalary = new int[names.length];
         int countOfSalary = 0;
+        result.append("Report for period ")
+                .append(dateFrom).append(" - ")
+                .append(dateTo)
+                .append("\n");
 
         for (String name : names) {
             for (String dataEmployee : data) {
@@ -21,25 +26,14 @@ public class SalaryInfo {
                     String[] employee = dataEmployee.split(" ");
                     LocalDate workDay = LocalDate.parse(employee[DATE], DATE_FORMAT);
                     if (startDate.compareTo(workDay) <= 0 && endDate.compareTo(workDay) >= 0) {
-                        salary[countOfSalary] += Integer.parseInt(employee[WORKED_HOURS])
+                        totalSalary[countOfSalary] += Integer.parseInt(employee[WORKED_HOURS])
                                 * Integer.parseInt(employee[INCOME_PER_HOUR]);
                     }
                 }
             }
-            countOfSalary++;
-        }
-
-        countOfSalary = 0;
-        StringBuilder result = new StringBuilder("Report for period "
-                + dateFrom
-                + " - "
-                + dateTo
-                + "\n");
-
-        for (String name : names) {
             result.append(name)
                     .append(" - ")
-                    .append(salary[countOfSalary])
+                    .append(totalSalary[countOfSalary])
                     .append("\n");
             countOfSalary++;
         }
