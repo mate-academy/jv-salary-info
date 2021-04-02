351afe1fefbc0e55c899f8f6aabc35f5795a22be
Alina <vasylyshyn.alina@gmail.com>
2021-04-02 15:52:48 +0300

    added FORMATTER, changed dateFrom and dateTo to object class LocalDate, added new array salary ..

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -7,31 +7,30 @@ public class SalaryInfo {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        LocalDate dateStart = LocalDate.parse(dateFrom, FORMATTER);
+        StringBuilder workerSalary =
+                new StringBuilder();
+        workerSalary.append("Report for period ")
+                .append(dateFrom).append(" - ").append(dateTo).append("\n");
+        LocalDate dateStart =
+                LocalDate.parse(dateFrom, FORMATTER);
         LocalDate dateFinish = LocalDate.parse(dateTo, FORMATTER);
-        int[] salary = new int[names.length];
         for (String name : names) {
-            for (int i = 0; i < data.length; i++) {
-                if (name.contains(data[i])) {
-                    String takeDate = data[i].split(" ")[0];
+            int totalSalary = 0;
+            for (String datum : data) {
+                if (datum.contains(name)) {
+                    String takeDate = datum.split(" ")[0];
                     LocalDate dateWorker = LocalDate.parse(takeDate, FORMATTER);
-                    if ((!dateWorker.isBefore(dateStart)) && (dateWorker.isBefore(dateFinish))) {
-                        String timeWorker = data[i].split(" ")[2];
-                        String priceForOneHour = data[i].split(" ")[3];
-                        salary[i] += Integer.parseInt(timeWorker)
+                    if (dateWorker.compareTo(dateStart) >= 0
+                            && dateWorker.compareTo(dateFinish) <= 0) {
+                        String timeWorker = datum.split(" ")[2];
+                        String priceForOneHour = datum.split(" ")[3];
+                        totalSalary += Integer.parseInt(timeWorker)
                                 * Integer.parseInt(priceForOneHour);
                     }
                 }
             }
+            workerSalary.append(name).append(" - ").append(totalSalary).append("\n");
         }
-        System.out.println("Repost for period " + dateStart + "-" + dateFrom);
-        StringBuilder workerSalary = new StringBuilder();
-        for (int i = 0; i < names.length; i++) {
-            workerSalary.append(names[i]);
-            workerSalary.append(" - ");
-            workerSalary.append(salary[i]);
-            workerSalary.append("\n");
-        }
-        return workerSalary.toString();
+        return workerSalary.toString().trim();
     }
 }
