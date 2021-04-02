302dfc145269b8019306f4a8a6222b962366b736
Lugos <lugos94@gmail.com>
2021-04-02 15:07:31 +0300

    Create HW-5

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,8 +1,37 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+    private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter
+            .ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        LocalDate localDateFrom = LocalDate.parse(dateFrom, DATE_FORMAT);
+        LocalDate localDateTo = LocalDate.parse(dateTo, DATE_FORMAT);
+        StringBuilder builder = new StringBuilder();
+
+        builder.append("Report ").append("for ").append("period ")
+                .append(localDateFrom.format(DATE_FORMAT)).append(" - ")
+                .append(localDateTo.format(DATE_FORMAT));
+
+        for (String name : names) {
+            int salary = 0;
+            for (String dates: data) {
+                String[] splitData = dates.split(" ");
+                LocalDate localDate = LocalDate.parse(splitData[0], DATE_FORMAT);
+
+                if (splitData[1].equals(name)
+                        && (localDate.isAfter(localDateFrom) || localDate.isEqual(localDateFrom))
+                        && (localDate.isBefore(localDateTo) || localDate.isEqual(localDateTo))
+                        && !localDateFrom.isEqual(localDateTo)) {
+                    salary += Integer.parseInt(splitData[2]) * Integer.parseInt(splitData[3]);
+                }
+            }
+            builder.append("\n").append(name).append(" - ").append(salary);
+        }
+
+        return builder.toString();
     }
 }
