4bae03a99b4bbc0640510711ad5a44a40b14a1b8
Aalderaan <chyregd@gmail.com>
2021-04-02 15:49:12 +0200

    added solition.Salary info

---
+++ src/main/java/Main.java
@@ -0,0 +1,25 @@
+package core.basesyntax;
+
+public class Main {
+
+    public static void main(String[] args) {
+        SalaryInfo info = new SalaryInfo();
+        System.out.println(info.getSalaryInfo(new String[]{"John", "Andrew", "Kate"},
+                new String[]{
+                        "13.07.2019 John 60 50",
+                        "15.07.2019 Andrew 3 200",
+                        "15.07.2019 Kate 10 100",
+
+                        "16.07.2019 Andrew 3 200",
+                        "16.07.2019 Kate 9 100",
+
+                        "10.08.2019 John 7 100",
+                        "08.08.2019 Kate 3 80",
+                        "11.08.2019 Andrew 8 100"
+                },
+                "14.07.2019", "10.08.2019"));
+
+    }
+}
+
+
