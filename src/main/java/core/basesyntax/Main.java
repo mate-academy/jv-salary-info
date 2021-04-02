67fe7fbab8c1093dfeae9c7fa25cc9fefbdded40
the5tranger <mishaantonyuk00@gmail.com>
2021-04-02 16:25:53 +0300

    Homework_salary

---
+++ src/main/java/core/basesyntax/Main.java
@@ -0,0 +1,28 @@
+package core.basesyntax;
+
+public class Main {
+    private static final String[] sampleNames = {"John", "Andrew", "Kate"};
+    private static final String[] scriptArray = {
+            "25.04.2019 John 60 50",
+            "25.04.2019 Andrew 3 200",
+            "25.04.2019 Kate 10 100",
+
+            "26.04.2019 Andrew 3 200",
+            "26.04.2019 Kate 9 100",
+
+            "27.04.2019 John 7 100",
+            "27.04.2019 Kate 3 80",
+            "27.04.2019 Andrew 8 100"
+    };
+    private static final String[] dates = {
+            "24.04.2019",
+            "25.04.2019",
+            "26.04.2019",
+            "27.04.2019"
+    };
+    public static void main(String[] args) {
+        SalaryInfo salaryInfo = new SalaryInfo();
+        //salaryInfo.getSalaryInfo(null, null, "01.04.2019", "30.04.2019");
+        salaryInfo.getSalaryInfo(sampleNames, scriptArray,"01.04.2019", "30.04.2019" );
+    }
+}
--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,8 +1,55 @@
 package core.basesyntax;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
 public class SalaryInfo {
+    private static final int DATE = 0;
+    private static final int NAME = 1;
+    private static final int WORKED_HOURS = 2;
+    private static final int INCOME_PER_HOUR = 3;
+    private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        LocalDate startDate = LocalDate.parse(dateFrom, DATE_FORMAT);
+        LocalDate endDate = LocalDate.parse(dateTo, DATE_FORMAT);
+        int[] salary = new int[names.length];
+        int countOfSalary = 0;
+
+        for (String name : names) {
+            for (String dataEmployee : data) {
+                if (dataEmployee.contains(name)) {
+                    String[] employee = dataEmployee.split(" ");
+                    LocalDate workDay = LocalDate.parse(employee[DATE], DATE_FORMAT);
+                    if (startDate.compareTo(workDay) <= 0 && endDate.compareTo(workDay) >= 0) {
+                        salary[countOfSalary] += Integer.parseInt(employee[WORKED_HOURS])
+                                * Integer.parseInt(employee[INCOME_PER_HOUR]);
+                    }
+                }
+            }
+            countOfSalary++;
+        }
+
+        countOfSalary = 0;
+        StringBuilder result = new StringBuilder("Report for period "
+                + dateFrom
+                + " - "
+                + dateTo
+                + "\n");
+
+        for (String name : names) {
+            result.append(name)
+                    .append(" - ")
+                    .append(salary[countOfSalary])
+                    .append("\n");
+            countOfSalary++;
+        }
+
+        return result.toString().trim();
     }
 }
