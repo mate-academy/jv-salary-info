10430230a00566732f098280068ac3b20c9d8c95
Vlad-Andr <andreev.v99@gmail.com>
2021-04-02 15:14:11 +0300

    fixed required mistakes

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -9,35 +9,32 @@ public class SalaryInfo {
 
     public static String getSalaryInfo(String[] names, String[] data,
                                        String dateFrom, String dateTo) {
-        LocalDate date;
         LocalDate dateFromParsed = LocalDate.parse(dateFrom, FORMATION_WITH_DOT);
         LocalDate dateToParsed = LocalDate.parse(dateTo, FORMATION_WITH_DOT);
-        int timesGotSalary;
-        int salary;
-        StringBuilder str = new StringBuilder();
-        str.append("Report for period ").append(dateFrom).append(" - ").append(dateTo).append("\n");
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Report for period ").append(dateFrom)
+                .append(" - ").append(dateTo).append("\n");
 
         for (String name: names) {
             int employeeSalary = 0;
             for (String datum : data) {
                 String[] employeesInformation = datum.split(" ");
                 String nameOfEmployee = employeesInformation[1];
-                timesGotSalary = Integer.parseInt(employeesInformation[2]);
-                salary = Integer.parseInt(employeesInformation[3]);
+                int timesGotSalary = Integer.parseInt(employeesInformation[2]);
+                int salary = Integer.parseInt(employeesInformation[3]);
                 String dateWithoutDot = employeesInformation[0];
-                date = LocalDate.parse(dateWithoutDot, FORMATION_WITH_DOT);
+                LocalDate date = LocalDate.parse(dateWithoutDot, FORMATION_WITH_DOT);
 
                 if (dateFromParsed.minusDays(1).isBefore(date)
-                        && dateToParsed.plusDays(1).isAfter(date)) {
-                    if (nameOfEmployee.equals(name)) {
-                        employeeSalary += timesGotSalary * salary;
-                    } else {
-                        employeeSalary += 0;
-                    }
+                        && dateToParsed.plusDays(1).isAfter(date)
+                        && nameOfEmployee.equals(name)) {
+                    employeeSalary += timesGotSalary * salary;
+                } else {
+                    employeeSalary += 0;
                 }
             }
-            str.append(name).append(" - ").append(employeeSalary).append("\n");
+            stringBuilder.append(name).append(" - ").append(employeeSalary).append("\n");
         }
-        return str.toString().trim();
+        return stringBuilder.toString().trim();
     }
 }
