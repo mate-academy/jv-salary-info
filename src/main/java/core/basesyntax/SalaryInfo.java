09af914fb57bad8151cad87f7565f4de2b326b72
unknown <vovazalli@gmail.com>
2021-04-02 12:03:15 +0300

    Class Date changed to LocalDate, constants implemented

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,38 +1,34 @@
 package core.basesyntax;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
+        LocalDate dateStart = LocalDate.parse(dateFrom, DATE_FORMAT);
+        LocalDate dateEnd = LocalDate.parse(dateTo, DATE_FORMAT);
 
-        try {
-            Date dateStart = new SimpleDateFormat("dd.MM.yyyy").parse(dateFrom);
-            Date dateEnd = new SimpleDateFormat("dd.MM.yyyy").parse(dateTo);
-            String[] splitData = new String[4];
+        String[] splitData;
+        int[] salaries = new int[3];
 
-            int[] salaries = new int[3];
-            for (String info : data) {
-                splitData = info.split(" ");
-                Date newDate = new SimpleDateFormat("dd.MM.yyyy").parse(splitData[0]);
-                if ((newDate.equals(dateEnd) || newDate.before(dateEnd))
-                        && (newDate.after(dateStart) || newDate.equals(dateStart))) {
-                    for (int i = 0; i < 3; i++) {
-                        if (names[i].equals(splitData[1])) {
-                            salaries[i] += Integer.parseInt(splitData[2])
-                                    * Integer.parseInt(splitData[3]);
-                            break;
-                        }
+        for (String info : data) {
+            splitData = info.split(" ");
+            LocalDate newDate = LocalDate.parse(splitData[0], DATE_FORMAT);
+            if ((newDate.equals(dateEnd) || newDate.isBefore(dateEnd))
+                    && (newDate.isAfter(dateStart) || newDate.equals(dateStart))) {
+                for (int i = 0; i < 3; i++) {
+                    if (names[i].equals(splitData[1])) {
+                        salaries[i] += Integer.parseInt(splitData[2])
+                                * Integer.parseInt(splitData[3]);
+                        break;
                     }
                 }
             }
-            return "Report for period " + dateFrom + " - " + dateTo + "\nJohn - "
-                    + salaries[0] + "\nAndrew - " + salaries[1] + "\nKate - " + salaries[2];
-
-        } catch (ParseException e) {
-            throw new RuntimeException();
         }
+        return "Report for period " + dateFrom + " - " + dateTo + "\nJohn - "
+                + salaries[0] + "\nAndrew - " + salaries[1] + "\nKate - " + salaries[2];
     }
 }
+
