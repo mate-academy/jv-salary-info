89d3b553979bc90913dff2a5ed118f074cdd6e03
DanyloIT <krupnyk.danylo@gmail.com>
2021-04-02 11:37:03 +0300

    all fixed, added minus and plus days, tested)) one private method instead of two build success

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,38 +4,36 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder result = new StringBuilder();
         result.append("Report for period ").append(dateFrom).append(" - ")
-                .append(dateTo);
-        LocalDate start = getStartDate(dateFrom);
-        LocalDate end = getEndDate(dateTo);
+                .append(dateTo).append("\n");
+
+        LocalDate start = getDate(dateFrom);
+        LocalDate end = getDate(dateTo);
 
         for (String name : names) {
             int salary = 0;
             for (String info : data) {
                 if (info.contains(name)) {
                     String[] information = info.split(" ");
-                    if (getStartDate(information[0]).isAfter(start)
-                                && getEndDate(information[0]).isBefore(start)) {
+                    if (getDate(information[0]).isAfter(start.minusDays(1))
+                                && getDate(information[0]).isBefore(end.plusDays(1))) {
                         salary += Integer.parseInt(information[2])
                                 * Integer.parseInt(information[3]);
                     }
                 }
             }
-            result.append(name).append(" - ").append(salary);
+            result.append(name).append(" - ").append(salary).append("\n");
         }
-        return result.toString();
+        return result.toString().trim();
     }
 
-    private LocalDate getEndDate(String dateTo) {
-        return LocalDate.parse(dateTo, FORMATTER);
-    }
-
-    private LocalDate getStartDate(String dateFrom) {
-        return LocalDate.parse(dateFrom, FORMATTER);
+    private LocalDate getDate(String date) {
+        return LocalDate.parse(date, FORMATTER);
     }
 }
 
