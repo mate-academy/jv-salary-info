9f1f40f9243dd8a5a40901795e0eadf448015a2f
Darina <to.darina.to>
2021-04-02 11:27:02 +0300

    3) change constanta

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,14 +4,15 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private static final DateTimeFormatter DATA_TIME = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final DateTimeFormatter DATA_TIME_FORMATTER =
+            DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder builder = new StringBuilder();
         builder.append("Report for period ").append(dateFrom)
                 .append(" - ").append(dateTo).append("\n");
-        LocalDate dateFromTime = LocalDate.parse(dateFrom, DATA_TIME);
-        LocalDate dateToTime = LocalDate.parse(dateTo, DATA_TIME);
+        LocalDate dateFromTime = LocalDate.parse(dateFrom, DATA_TIME_FORMATTER);
+        LocalDate dateToTime = LocalDate.parse(dateTo, DATA_TIME_FORMATTER);
         String[] employee;
         LocalDate nowData;
         int getMoney;
@@ -20,7 +21,7 @@ public class SalaryInfo {
             for (int j = 0; j < data.length; j++) {
                 if (data[j].contains(names[i])) {
                     employee = data[j].split(" ");
-                    nowData = LocalDate.parse(employee[0], DATA_TIME);
+                    nowData = LocalDate.parse(employee[0], DATA_TIME_FORMATTER);
                     if (nowData.isAfter(dateFromTime) && nowData.isBefore(dateToTime)
                             || nowData.equals(dateToTime)) {
                         getMoney += Integer.parseInt(employee[2])
