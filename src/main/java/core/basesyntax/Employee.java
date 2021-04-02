2265cf85d842d20b7153db23d7b0bdf26411313e
MakeDreamCode <65960125+MakeDreamCode@users.noreply.github.com>
2021-04-03 01:28:24 +0300

    Added constants + StringBuilder()

--- src/main/java/core/basesyntax/Employee.java
+++ src/main/java/core/basesyntax/Employee.java
@@ -1,28 +1,28 @@
 package core.basesyntax;
 
 public class Employee {
-  private String name;
-  private int salaryFromTo;
+    private String name;
+    private int salaryFromTo;
 
-  public Employee(String name, int totalSalaryFromTo) {
-    this.name = name;
-    this.salaryFromTo = totalSalaryFromTo;
-  }
+    public Employee(String name, int totalSalaryFromTo) {
+        this.name = name;
+        this.salaryFromTo = totalSalaryFromTo;
+    }
 
-  public String getName() {
-    return name;
-  }
+    public String getName() {
+        return name;
+    }
 
-  public int getSalaryFromTo() {
-    return salaryFromTo;
-  }
+    public int getSalaryFromTo() {
+        return salaryFromTo;
+    }
 
-  public void setSalaryFromTo(int salaryFromTo) {
-    this.salaryFromTo = salaryFromTo;
-  }
+    public void setSalaryFromTo(int salaryFromTo) {
+        this.salaryFromTo = salaryFromTo;
+    }
 
-  @Override
-  public String toString() {
-    return name + " - " + salaryFromTo;
-  }
+    @Override
+    public String toString() {
+        return name + " - " + salaryFromTo;
+    }
 }
