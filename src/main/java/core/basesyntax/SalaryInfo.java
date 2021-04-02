df9c6ba8e770b1d0321d6046a29e1b73e97ef0e7
anufriiev <dimaanufriev1012@gmail.com>
2021-04-02 14:32:56 +0300

    HW-4. SalaryInfo.Fixed issues.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,35 +4,31 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-
     private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-    private StringBuilder builder = new StringBuilder();
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-
+        StringBuilder builder = new StringBuilder();
+        builder.append("Report for period ").append(dateFrom)
+                .append(" - ").append(dateTo).append("\n");
         LocalDate dateFirst = LocalDate.parse(dateFrom, DATE_FORMAT);
         LocalDate dateSecond = LocalDate.parse(dateTo, DATE_FORMAT);
-        int result = 0;
-
-        for (String dat : data) {
-            String[] currentData = dat.split(" ");
-            for (int i = 0; i < names.length; i++) {
-                if (currentData[i].contains(names[i])) {
-                    LocalDate dayDate = LocalDate.parse(currentData[0], DATE_FORMAT);
-                    if (dayDate.isAfter(dateFirst)
-                            || dayDate.isBefore(dateSecond)
-                            || dayDate.equals(dateFirst)
-                            || dayDate.equals(dateSecond)) {
+        for (String name : names) {
+            int result = 0;
+            for (int i = 0; i < data.length; i++) {
+                if (data[i].contains(name)) {
+                    String[] currentData = data[i].split(" ");
+                    LocalDate localDate = LocalDate.parse(currentData[0], DATE_FORMAT);
+                    if (localDate.isAfter(dateFirst)
+                            && localDate.isBefore(dateSecond)
+                            || localDate.equals(dateFirst)
+                            || localDate.equals(dateSecond)) {
                         result += Integer.parseInt(currentData[2])
                                 * Integer.parseInt(currentData[3]);
                     }
                 }
             }
+            builder.append(name).append(" - ").append(String.valueOf(result)).append("\n");
         }
-        for (int i = 0; i < names.length; i++) {
-            builder.append("REport for period ").append(dateFrom)
-                    .append(" - ").append(dateTo).append("\n").append(names[i]).append(result);
-        }
-        return builder.toString();
+        return builder.toString().trim();
     }
 }
