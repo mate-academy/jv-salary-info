2d9307a5c4f2c03f20c12e984378bca892e6ae44
Alina <vasylyshyn.alina@gmail.com>
2021-04-02 13:50:51 +0300

    added FORMATTER, changed dateFrom and dateTo to object class LocalDate, added new array salary

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,8 +1,37 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        LocalDate dateStart = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate dateFinish = LocalDate.parse(dateTo, FORMATTER);
+        int[] salary = new int[names.length];
+        for (String name : names) {
+            for (int i = 0; i < data.length; i++) {
+                if (name.contains(data[i])) {
+                    String takeDate = data[i].split(" ")[0];
+                    LocalDate dateWorker = LocalDate.parse(takeDate, FORMATTER);
+                    if ((!dateWorker.isBefore(dateStart)) && (dateWorker.isBefore(dateFinish))) {
+                        String timeWorker = data[i].split(" ")[2];
+                        String priceForOneHour = data[i].split(" ")[3];
+                        salary[i] += Integer.parseInt(timeWorker)
+                                * Integer.parseInt(priceForOneHour);
+                    }
+                }
+            }
+        }
+        System.out.println("Repost for period " + dateStart + "-" + dateFrom);
+        StringBuilder workerSalary = new StringBuilder();
+        for (int i = 0; i < names.length; i++) {
+            workerSalary.append(names[i]);
+            workerSalary.append(" - ");
+            workerSalary.append(salary[i]);
+            workerSalary.append("\n");
+        }
+        return workerSalary.toString();
     }
 }
