bfb76ea9e3dd910aad6d8ae5e8d417f9b84b0ef6
MakeDreamCode <65960125+MakeDreamCode@users.noreply.github.com>
2021-04-05 17:51:00 +0300

    Completely rewritten code.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -2,68 +2,71 @@ package core.basesyntax;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.List;
 
 public class SalaryInfo {
-
+    private static final int DATE_IN_DATA_POSITION = 0;
+    private static final int NAME_IN_REPORT_POSITION = 0;
+    private static final int SALARY_RATE_PER_HOUR_POSITION = 3;
+    private static final int TOTAL_SALARY_IN_REPORT_POSITION = 2;
+    private static final int WORKED_HOURS_POSITION = 2;
+    private static final int ZERO_VALUE = 0;
     private static final String DATE_FORMAT = "dd.MM.yyyy";
     private static final String MINUS_DELIMITER = "-";
     private static final String NEW_STRING_DELIMITER = "\n";
     private static final String SPACE_DELIMITER = " ";
     private static final String TITLE_OF_REPORT = "Report for period";
+    private static final String INSERT_BETWEEN_VALUES = SPACE_DELIMITER + MINUS_DELIMITER
+            + SPACE_DELIMITER;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_FORMAT);
         LocalDate startOfPeriod = LocalDate.parse(dateFrom, formatter);
         LocalDate endOfPeriod = LocalDate.parse(dateTo, formatter);
-
         if (!startOfPeriod.equals(endOfPeriod)) {
             endOfPeriod = endOfPeriod.plusDays(1);
         }
-
-        List<Employee> listEmployee = new ArrayList<>();
-
-        for (String name : names) {
-            listEmployee.add(new Employee(name, 0));
+        String[] report = new String[names.length];
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < names.length; i++) {
+            report[i] = stringBuilder.append(names[i]).append(INSERT_BETWEEN_VALUES)
+                                     .append(ZERO_VALUE).toString();
+            stringBuilder.setLength(ZERO_VALUE);
         }
-
-        List<DataOfEmployees> dataEmployees = new ArrayList<>();
-
-        for (String dataString : data) {
-            String[] fieldsOfDataEmployee = dataString.split(SPACE_DELIMITER);
-            LocalDate date = LocalDate.parse(fieldsOfDataEmployee[0], formatter);
-            String employeeName = fieldsOfDataEmployee[1];
-            int workedHours = Integer.parseInt(fieldsOfDataEmployee[2]);
-            int salaryRate = Integer.parseInt(fieldsOfDataEmployee[3]);
-            dataEmployees.add(new DataOfEmployees(date, employeeName, workedHours, salaryRate));
-        }
-
-        for (DataOfEmployees dataEmployee : dataEmployees) {
-            if (dataEmployee.getDate().isAfter(startOfPeriod)
-                    && dataEmployee.getDate().isBefore(endOfPeriod)) {
-                for (Employee employee : listEmployee) {
-                    if (employee.getName().equals(dataEmployee.getName())) {
-                        int newSum = employee.getSalaryFromTo()
-                                + (dataEmployee.getWorkedHours() * dataEmployee.getSalaryRate());
-                        employee.setSalaryFromTo(newSum);
+        for (int i = 0; i < names.length; i++) {
+            for (String line : data) {
+                String[] fieldsOfData = line.split(SPACE_DELIMITER);
+                LocalDate date =
+                        LocalDate.parse(fieldsOfData[DATE_IN_DATA_POSITION], formatter);
+                if (line.contains(names[i]) && (date.isAfter(startOfPeriod)
+                        && date.isBefore(endOfPeriod))) {
+                    int workedHours = Integer.parseInt(fieldsOfData[WORKED_HOURS_POSITION]);
+                    int salaryRate =
+                            Integer.parseInt(fieldsOfData[SALARY_RATE_PER_HOUR_POSITION]);
+                    for (int j = 0; j < report.length; j++) {
+                        if (report[j].contains(names[i])) {
+                            String[] fieldsOfReport = report[i].split(SPACE_DELIMITER);
+                            int totalSalaryPerPeriod = Integer.parseInt(
+                                    fieldsOfReport[TOTAL_SALARY_IN_REPORT_POSITION]);
+                            totalSalaryPerPeriod += workedHours * salaryRate;
+                            report[j] =
+                                    stringBuilder.append(fieldsOfReport[NAME_IN_REPORT_POSITION])
+                                                     .append(INSERT_BETWEEN_VALUES)
+                                                     .append(totalSalaryPerPeriod)
+                                                     .toString();
+                            stringBuilder.setLength(ZERO_VALUE);
+                        }
                     }
                 }
             }
         }
-
-        StringBuilder sb = new StringBuilder();
-        sb.append(TITLE_OF_REPORT + SPACE_DELIMITER)
-                .append(dateFrom)
-                .append(SPACE_DELIMITER + MINUS_DELIMITER + SPACE_DELIMITER)
-                .append(dateTo)
-                .append(NEW_STRING_DELIMITER);
-
-        for (Employee employees : listEmployee) {
-            sb.append(employees.toString()).append(NEW_STRING_DELIMITER);
+        stringBuilder.append(TITLE_OF_REPORT + SPACE_DELIMITER).append(dateFrom)
+                     .append(INSERT_BETWEEN_VALUES).append(dateTo).append(NEW_STRING_DELIMITER);
+        for (int i = 0; i < report.length; i++) {
+            stringBuilder.append(report[i]);
+            if (i < report.length - 1) {
+                stringBuilder.append(NEW_STRING_DELIMITER);
+            }
         }
-
-        return sb.toString().trim();
+        return stringBuilder.toString();
     }
 }
