9856ed56eade57b20f6b0b64aeed8b05de9c76a9
forsfakel <81535727+forsfakel@users.noreply.github.com>
2021-05-10 00:41:00 +0300

    date1 - begin date; date2 - end date; format - for change type String to Date; result - string for return; temp - one string of data; date - part of temp, date salary; name - part of temp, name employee; hour - part of temp, hour work in day work; salaryHour - part of temp, salary per hour on this date; part - method that divides string temp; newText - method that creates a new line without a part; formatResult - method that creates line result for return

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,8 +1,74 @@
 package core.basesyntax;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 public class SalaryInfo {
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        String result = "Report for period ";
+        int[] salary = new int[names.length];
+
+        Date date1 = null;
+        Date date2 = null;
+
+        SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy", Locale.ENGLISH);
+        try {
+            date1 = format.parse(dateFrom);
+            date2 = format.parse(dateTo);
+        } catch (ParseException e) {
+            System.out.println("Date no correct");
+        }
+        result = result + dateFrom + " - " + dateTo + "\n";
+
+        for (String temp:data) {
+            Date date = null;
+            try {
+                date = format.parse(part(temp));
+            } catch (ParseException e) {
+                System.out.println("Date no correct");
+            }
+            temp = newText(temp);
+            String name = part(temp);
+            temp = newText(temp);
+            int hour;
+            int salaryHour;
+            hour = Integer.parseInt(part(temp));
+            temp = newText(temp);
+            salaryHour = Integer.parseInt(temp);
+            try {
+                assert date != null;
+                if (date.after(date1) || date.equals(date1)) {
+                    if (date.before(date2) || date.equals(date2)) {
+                        for (int j = 0; j < names.length; j++) {
+                            if (names[j].equals(name)) {
+                                salary[j] = salary[j] + (salaryHour * hour);
+                            }
+                        }
+                    }
+                }
+            } catch (NullPointerException e) {
+                e.getMessage();
+            }
+
+        }
+        for (int i = 0; i < names.length; i++) {
+            result = formatResult(result, names[i], salary[i]);
+        }
+        return result.strip();
+    }
+
+    private String part(String parameter) {
+        return parameter.substring(0, parameter.indexOf(' ')).strip();
+    }
+
+    private String newText(String oldText) {
+        return oldText.replaceFirst(oldText.substring(0, oldText.indexOf(' ')), "").strip();
+    }
+
+    private String formatResult(String text, String name, int salary) {
+        return text + name + " - " + salary + "\n";
     }
 }
