a9f1b9c0cc642b39e0f0d1d16a8bfc2cdf551c4c
forsfakel <81535727+forsfakel@users.noreply.github.com>
2021-05-12 20:16:16 +0300

    Changed parameters: date1 - dateBegin; date2 - dateEnd; temp - tempDataString;
    Changed method:
    part() - getParameter();
    newText() - removeParameterFromText();
    
    Changed type Date to LocalDate;
    Changed parameter format to formatter, with type SimpleDateFormat to DateTimeFormatter;
    
    improved the body of the loop "for (String tempDataString : data)"
    
    Edited lines:
    return result.strip(); -->> return result;
    return text + name + " - " + salary + "\n"; -->> return text + "\n" + name + " - " + salary;
    
    result = result + dateFrom + " - " + dateTo + "\n"; -->> result = result + dateFrom + " - " + dateTo;

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,74 +1,52 @@
 package core.basesyntax;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         String result = "Report for period ";
         int[] salary = new int[names.length];
+        final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
-        Date date1 = null;
-        Date date2 = null;
+        LocalDate dateBegin = LocalDate.parse(dateFrom, formatter);
+        LocalDate dateEnd = LocalDate.parse(dateTo, formatter);
 
-        SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy", Locale.ENGLISH);
-        try {
-            date1 = format.parse(dateFrom);
-            date2 = format.parse(dateTo);
-        } catch (ParseException e) {
-            System.out.println("Date no correct");
-        }
-        result = result + dateFrom + " - " + dateTo + "\n";
+        result = result + dateFrom + " - " + dateTo;
 
-        for (String temp:data) {
-            Date date = null;
-            try {
-                date = format.parse(part(temp));
-            } catch (ParseException e) {
-                System.out.println("Date no correct");
-            }
-            temp = newText(temp);
-            String name = part(temp);
-            temp = newText(temp);
-            int hour;
-            int salaryHour;
-            hour = Integer.parseInt(part(temp));
-            temp = newText(temp);
-            salaryHour = Integer.parseInt(temp);
-            try {
-                assert date != null;
-                if (date.after(date1) || date.equals(date1)) {
-                    if (date.before(date2) || date.equals(date2)) {
-                        for (int j = 0; j < names.length; j++) {
-                            if (names[j].equals(name)) {
-                                salary[j] = salary[j] + (salaryHour * hour);
-                            }
-                        }
+        for (String tempDataString : data) {
+            LocalDate date = LocalDate.parse(getParameter(tempDataString), formatter);
+            if ((date.isAfter(dateBegin) || date.equals(dateBegin))
+                    && (date.isBefore(dateEnd) || date.equals(dateEnd))) {
+                tempDataString = removeParameterFromText(tempDataString);
+                String name = getParameter(tempDataString);
+                for (int j = 0; j < names.length; j++) {
+                    if (names[j].equals(name)) {
+                        tempDataString = removeParameterFromText(tempDataString);
+                        int hour = Integer.parseInt(getParameter(tempDataString));
+                        tempDataString = removeParameterFromText(tempDataString);
+                        int salaryHour = Integer.parseInt(tempDataString);
+                        salary[j] = salary[j] + (salaryHour * hour);
                     }
                 }
-            } catch (NullPointerException e) {
-                e.getMessage();
             }
-
         }
         for (int i = 0; i < names.length; i++) {
             result = formatResult(result, names[i], salary[i]);
         }
-        return result.strip();
+        return result;
     }
 
-    private String part(String parameter) {
+    private String getParameter(String parameter) {
         return parameter.substring(0, parameter.indexOf(' ')).strip();
     }
 
-    private String newText(String oldText) {
+    private String removeParameterFromText(String oldText) {
         return oldText.replaceFirst(oldText.substring(0, oldText.indexOf(' ')), "").strip();
     }
 
     private String formatResult(String text, String name, int salary) {
-        return text + name + " - " + salary + "\n";
+        return text + "\n" + name + " - " + salary;
     }
 }
