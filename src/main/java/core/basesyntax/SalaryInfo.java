1a9c8166ef16898e9918ca42a195617baa7fea63
Пользователь <karaed.ml@mail.ru>
2021-05-31 20:31:00 +0300

    1 - deleted empty lines 2 - used "StringBuilder" instead "String"; 3 - deleted unnecessary variable; 4 - deleted unnecessary checks; 5 - used "equals" instead "contains".

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -7,13 +7,12 @@ public class SalaryInfo {
 
     private static final DateTimeFormatter DATE_TIME_FORMATTER
             = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-    private String salaryDate;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
 
         LocalDate dateStart = LocalDate.parse(dateFrom, DATE_TIME_FORMATTER);
         LocalDate dateEnd = LocalDate.parse(dateTo, DATE_TIME_FORMATTER);
-        String salaryFinal = new String();
+        StringBuilder salaryFinal = new StringBuilder();
         for (int i = 0; i < names.length; i++) {
             int salary = 0;
             for (int j = 0; j < data.length; j++) {
@@ -21,17 +20,14 @@ public class SalaryInfo {
                 LocalDate localDate = LocalDate.parse(usersInformation[0], DATE_TIME_FORMATTER);
                 if ((localDate.equals(dateStart) || localDate.equals(dateEnd)
                         || localDate.isAfter(dateStart) && localDate.isBefore(dateEnd))
-                        && data[i] != null
-                        && names[i] != null && data[j].contains(names[i])) {
+                        && usersInformation[1].equals(names[i])) {
 
                     salary += Integer.parseInt(usersInformation[2])
                             * Integer.parseInt(usersInformation[3]);
                 }
             }
-            salaryFinal += "\n" + names[i] + " - " + salary;
+            salaryFinal.append("\n").append(names[i]).append(" - ").append(salary);
         }
-        return salaryDate = "Report for period " + dateFrom + " - " + dateTo + salaryFinal;
+        return "Report for period " + dateFrom + " - " + dateTo + salaryFinal;
     }
 }
-
-
