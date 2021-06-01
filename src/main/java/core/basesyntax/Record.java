5a03b9d9ec54de8a03eee216e93456b267e09b49
Ruslan Malyavsky <ruslan-gorod@ukr.net>
2021-06-01 17:07:20 +0300

    Created all needed class and methods

---
+++ src/main/java/core/basesyntax/Record.java
@@ -0,0 +1,60 @@
+package core.basesyntax;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Record {
+    private final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private String originalData;
+    private LocalDate date;
+    private String userName;
+    private int countDays;
+    private double salary;
+
+    public Record(String data) {
+        this.originalData = data;
+        createFields();
+    }
+
+    private void createFields() {
+        if (originalData != null) {
+            String[] inputData = originalData.split(" ");
+            this.date = LocalDate.parse(inputData[0], FORMATTER);
+            this.userName = inputData[1];
+            this.countDays = Integer.parseInt(inputData[2]);
+            this.salary = Double.parseDouble(inputData[3]);
+        }
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public int getCountDays() {
+        return countDays;
+    }
+
+    public void setCountDays(int countDays) {
+        this.countDays = countDays;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,8 +1,40 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
 public class SalaryInfo {
+    private final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        LocalDate from = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate to = LocalDate.parse(dateTo, FORMATTER);
+        List<String> reportRecords = new ArrayList<>();
+        for (String name : names) {
+            double sum = 0;
+            for (String value : data) {
+                Record record = new Record(value);
+                if (name.equals(record.getUserName()) && record.getDate().isAfter(from) && record.getDate().isBefore(to)) {
+                    sum += record.getSalary() * record.getCountDays();
+                }
+            }
+            reportRecords.add(name + " - " + sum);
+        }
+        return createReport(reportRecords, from, to);
+    }
+
+    private String createReport(List<String> reportRecords, LocalDate from, LocalDate to) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Report for period ");
+        stringBuilder.append(from.format(FORMATTER));
+        stringBuilder.append(" - ");
+        stringBuilder.append(to.format(FORMATTER));
+        stringBuilder.append("\n");
+        for (String reportRecord : reportRecords) {
+            stringBuilder.append(reportRecord).append("\n");
+        }
+        return stringBuilder.toString();
     }
 }
