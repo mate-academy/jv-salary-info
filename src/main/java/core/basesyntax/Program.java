e8f5db8bcea4517d059551ff93640a1aa933c724
Ruslan Malyavsky <ruslan-gorod@ukr.net>
2021-06-01 17:17:59 +0300

    Changed main method

--- src/main/java/core/basesyntax/Program.java
+++ src/main/java/core/basesyntax/Program.java
@@ -3,10 +3,13 @@ package core.basesyntax;
 public class Program {
     public static void main(String[] args) {
         String[] names = {"John", "Andrew", "Kate"};
-        String[] data = {"26.04.2019 John 4 50", "05.04.2019 Andrew 3 200", "10.04.2019 John 7 100", "22.04.2019 Kate 9 100",
-                "25.06.2019 John 11 50", "26.04.2019 Andrew 3 150", "13.02.2019 John 7 100", "26.04.2019 Kate 9 100"};
+        String[] data = {"26.04.2019 John 4 50", "05.04.2019 Andrew 3 200",
+                "10.04.2019 John 7 100", "22.04.2019 Kate 9 100",
+                "25.06.2019 John 11 50", "26.04.2019 Andrew 3 150",
+                "13.02.2019 John 7 100", "26.04.2019 Kate 9 100"};
 
         SalaryInfo salaryInfo = new SalaryInfo();
-        System.out.println(salaryInfo.getSalaryInfo(names, data, "01.04.2019", "30.04.2019"));
+        System.out.println(salaryInfo.getSalaryInfo(names, data,
+                "01.04.2019", "30.04.2019"));
     }
 }
