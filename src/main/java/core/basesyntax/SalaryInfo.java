8fb3d2945f9c5ff6248f2fd67d3597a38ed49160
Борис Скворцов <dhakmar@gmail.com>
2021-06-02 14:35:13 +0300

    Complited solution (created a method with the requested logic)

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,8 +1,33 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        StringBuilder result = new StringBuilder();
+        result.append("Report for period ")
+                .append(dateFrom).append(" - ")
+                .append(dateTo).append("\n");
+
+        LocalDate from = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate to = LocalDate.parse(dateTo, FORMATTER);
+
+        for (String name : names) {
+            int amountSalary = 0;
+            for (String info : data) {
+                if (info.contains(name)) {
+                    String[] array = info.split(" ");
+                    LocalDate day = LocalDate.parse(array[0], FORMATTER);
+                    if (day.compareTo(from) >= 0 && day.compareTo(to) <= 0) {
+                        amountSalary += Integer.parseInt(array[2]) * Integer.parseInt(array[3]);
+                    }
+                }
+            }
+            result.append(name).append(" - ").append(amountSalary).append("\n");
+        }
+        return result.toString().trim();
     }
 }
