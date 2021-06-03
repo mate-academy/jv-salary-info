b5148c02d3934a3d85c7da323bf6737ba2c365f1
Veronika-Alekseienko <alekseienko.n@gmail.com>
2021-06-03 08:25:23 +0300

    added solution

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,8 +1,35 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final String HEADER = "Report for period %s - %s";
+    private static final int DATE_INDEX = 0;
+    private static final int COUNT_OF_HOURS_INDEX = 2;
+    private static final int SALARY_PER_HOUR_INDEX = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        LocalDate fromDate = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate toDate = LocalDate.parse(dateTo, FORMATTER);
+        StringBuilder buildInfo = new StringBuilder(String.format(HEADER, dateFrom, dateTo));
+        for (String name : names) {
+            int sumOfSalary = 0;
+            buildInfo.append("\n").append(name)
+                    .append(" ").append("-").append(" ");
+            for (String info : data) {
+                if (info.contains(name)) {
+                    LocalDate currentDate = LocalDate.parse(info.split(" ")[DATE_INDEX], FORMATTER);
+                    if ((fromDate.isBefore(currentDate) || fromDate.isEqual(currentDate))
+                            && (toDate.isAfter(currentDate) || toDate.isEqual(currentDate))) {
+                        sumOfSalary += Integer.parseInt(info.split(" ")[COUNT_OF_HOURS_INDEX])
+                                * Integer.parseInt(info.split(" ")[SALARY_PER_HOUR_INDEX]);
+                    }
+                }
+            }
+            buildInfo.append(sumOfSalary);
+        }
+        return buildInfo.toString();
     }
 }
