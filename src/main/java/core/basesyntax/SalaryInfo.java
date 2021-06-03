03fea48274f412df955ee0d80454b0aec383f17f
Борис Скворцов <dhakmar@gmail.com>
2021-06-03 17:54:06 +0300

    Added fix: renamed magic number renamed var day to date refactoring .append()

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,12 +5,15 @@ import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int INDEX_OF_DATA = 0;
+    private static final int INDEX_OF_SALARY_PER_HOUR = 2;
+    private static final int INDEX_OF_AMOUNT_HOURS = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder result = new StringBuilder();
         result.append("Report for period ")
                 .append(dateFrom).append(" - ")
-                .append(dateTo).append("\n");
+                .append(dateTo);
 
         LocalDate from = LocalDate.parse(dateFrom, FORMATTER);
         LocalDate to = LocalDate.parse(dateTo, FORMATTER);
@@ -20,14 +23,14 @@ public class SalaryInfo {
             for (String info : data) {
                 if (info.contains(name)) {
                     String[] array = info.split(" ");
-                    LocalDate day = LocalDate.parse(array[0], FORMATTER);
-                    if (day.compareTo(from) >= 0 && day.compareTo(to) <= 0) {
-                        amountSalary += Integer.parseInt(array[2]) * Integer.parseInt(array[3]);
+                    LocalDate date = LocalDate.parse(array[INDEX_OF_DATA], FORMATTER);
+                    if (date.compareTo(from) >= 0 && date.compareTo(to) <= 0) {
+                        amountSalary += Integer.parseInt(array[INDEX_OF_SALARY_PER_HOUR]) * Integer.parseInt(array[INDEX_OF_AMOUNT_HOURS]);
                     }
                 }
             }
-            result.append(name).append(" - ").append(amountSalary).append("\n");
+            result.append("\n").append(name).append(" - ").append(amountSalary);
         }
-        return result.toString().trim();
+        return result.toString();
     }
 }
