c696207286ad56a545e9fe7226676d274f390106
Daniel Kuruch <kuruch1004@gmail.com>
2021-06-04 15:13:11 +0300

    remove UtilsData.java,new constants for data array, optimization loop

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,43 +5,36 @@ import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("d.MM.yyyy");
-    private UtilsData utilsData = new UtilsData();
+    private static final int POSITION_DATE = 0;
+    private static final int POSITION_NAME = 1;
+    private static final int POSITION_HOUR = 2;
+    private static final int POSITION_PAY = 3;
+    private StringBuilder stringBuilder;
+    private String[] elementFromData;
+    private LocalDate localDateData;
+    private int salary;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder = new StringBuilder();
+        stringBuilder.append(String.format("Report for period %s - %s\n", dateFrom, dateTo));
 
         LocalDate localDateFrom = LocalDate.parse(dateFrom, FORMATTER);
         LocalDate localDateTo = LocalDate.parse(dateTo, FORMATTER);
 
-        int[] salaryEmployees = new int[names.length];
-
-        String name;
-        LocalDate localDateData;
-
-        for (int i = 0; i < names.length; i++) {
-            name = names[i];
+        for (String name : names) {
+            salary = 0;
             for (String datum : data) {
-                if (!name.equals(utilsData.getNameFromDate(datum))) {
-                    continue;
-                }
-                localDateData = utilsData.getDateFromDate(datum, FORMATTER);
-                if (localDateData.isAfter(localDateFrom.minusDays(1))
+                elementFromData = datum.split(" ");
+                if (name.equals(elementFromData[POSITION_NAME])) {
+                    localDateData = LocalDate.parse(elementFromData[POSITION_DATE], FORMATTER);
+                    if (localDateData.isAfter(localDateFrom.minusDays(1))
                         && localDateData.isBefore(localDateTo.plusDays(1))) {
-                    salaryEmployees[i] +=
-                        utilsData.getWorkingHourFromDate(datum) * utilsData
-                            .getIncomePerHourFromDate(datum);
+                        salary += Integer.parseInt(elementFromData[POSITION_HOUR])
+                            * Integer.parseInt(elementFromData[POSITION_PAY]);
+                    }
                 }
             }
-        }
-
-        stringBuilder.append("Report for period ")
-            .append(localDateFrom.format(FORMATTER))
-            .append(" - ").append(localDateTo.format(FORMATTER)).append("\n");
-
-        for (int i = 0; i < names.length; i++) {
-            stringBuilder.append(names[i])
-                .append(" - ")
-                .append(salaryEmployees[i]).append("\n");
+            stringBuilder.append(String.format("%s - %d\n", name, salary));
         }
 
         return stringBuilder.toString().trim();
