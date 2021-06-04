13cc629721f446da4f91c59d131f62ba11b7fd8d
Denis <deniskichan@gmail.com>
2021-06-04 13:51:31 +0300

    added magic numbers as constants, and renamed some variables

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,26 +5,29 @@ import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int DATE_INDEX = 0;
+    private static final int WORK_HOURS_INDEX = 2;
+    private static final int INCOME_PER_HOUR_INDEX = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        StringBuilder sb = new StringBuilder("Report for period ");
-        sb.append(dateFrom).append(" - ").append(dateTo).append("\n");
+        StringBuilder reportForPeriod = new StringBuilder("Report for period ");
+        reportForPeriod.append(dateFrom).append(" - ").append(dateTo);
         LocalDate tempDateFrom = LocalDate.parse(dateFrom, FORMATTER);
         LocalDate tempDateTo = LocalDate.parse(dateTo, FORMATTER);
         for (String name : names) {
             int totalIncome = 0;
             for (String dataInfo : data) {
-                String[] temp = dataInfo.split(" ");
-                LocalDate tempDate = LocalDate.parse(temp[0], FORMATTER);
+                String[] splittedData = dataInfo.split(" ");
+                LocalDate tempDate = LocalDate.parse(splittedData[DATE_INDEX], FORMATTER);
                 if (dataInfo.contains(name)
                         && (tempDate.isAfter(tempDateFrom) || tempDate.isEqual(tempDateFrom))
                         && (tempDate.isBefore(tempDateTo) || tempDate.isEqual(tempDateTo))) {
-                    totalIncome += Integer.parseInt(temp[2]) * Integer.parseInt(temp[3]);
+                    totalIncome += Integer.parseInt(splittedData[WORK_HOURS_INDEX])
+                            * Integer.parseInt(splittedData[INCOME_PER_HOUR_INDEX]);
                 }
             }
-            sb.append(name).append(" - ").append(totalIncome).append("\n");
+            reportForPeriod.append("\n").append(name).append(" - ").append(totalIncome);
         }
-        System.out.println(sb.toString());
-        return sb.toString().trim();
+        return reportForPeriod.toString();
     }
 }
