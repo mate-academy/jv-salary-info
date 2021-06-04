de378ad6173c6b80f310e6de182e2aad1108d3c0
yuriiadam <dr.damba@outlook.com>
2021-06-04 15:53:27 +0300

    method getSalaryInfo calculating salary is working

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,8 +1,40 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+    static final int WHICH_DATE = 0;
+    private static final int HOURS = 2;
+    private static final int SALARY_PER_HOUR = 3;
+    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        StringBuilder result = new StringBuilder();
+        LocalDate datFrom = LocalDate.parse(dateFrom, formatter);
+        LocalDate datTo = LocalDate.parse(dateTo, formatter);
+        result.append("Report for period ")
+                .append(formatter.format(datFrom))
+                .append(" - ")
+                .append(formatter.format(datTo));
+        for (String name : names) {
+            int salaryCounter = 0;
+            for (String date : data) {
+                if (date.contains(name)) {
+                    String[] arrayOfDate = date.split(" ");
+                    LocalDate datIs = LocalDate.parse(arrayOfDate[WHICH_DATE], formatter);
+                    if ((datIs.isEqual(datFrom) || datIs.isAfter(datFrom))
+                            && (datIs.isEqual(datTo) || datIs.isBefore(datTo))) {
+                        salaryCounter += (Integer.parseInt(arrayOfDate[HOURS])
+                                * Integer.parseInt(arrayOfDate[SALARY_PER_HOUR]));
+                    }
+                }
+            }
+            result.append("\n")
+                    .append(name)
+                    .append(" - ")
+                    .append(salaryCounter);
+        }
+        return result.toString();
     }
 }
