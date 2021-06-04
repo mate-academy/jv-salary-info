0c60d99b648dbda835b7d2c009520623a968fdd5
Volodymyr Bilodon <vovabilodon@gmail.com>
2021-06-04 17:07:17 +0300

    added constants for indexes, changed privacy modifiers for some classes

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -6,6 +6,10 @@ import java.time.format.DateTimeFormatter;
 public class SalaryInfo {
 
     private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int WORKING_HOUR_INDEX = 2;
+    private static final int INCOME_INDEX = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
 
@@ -18,10 +22,11 @@ public class SalaryInfo {
             int sumSalary = 0;
             for (String currentData : data) {
                 String[] splitData = currentData.split(" ");
-                if (splitData[1].equals(name) && isBetween(getDateFromString(dateFrom),
+                if (splitData[NAME_INDEX].equals(name) && isBetween(getDateFromString(dateFrom),
                         getDateFromString(dateTo),
-                        getDateFromString(splitData[0]))) {
-                    sumSalary += Integer.parseInt(splitData[2]) * Integer.parseInt(splitData[3]);
+                        getDateFromString(splitData[DATE_INDEX]))) {
+                    sumSalary += Integer.parseInt(splitData[WORKING_HOUR_INDEX])
+                            * Integer.parseInt(splitData[INCOME_INDEX]);
                 }
             }
             resultMessage
@@ -33,11 +38,11 @@ public class SalaryInfo {
         return resultMessage.toString();
     }
 
-    public LocalDate getDateFromString(String date) {
+    private LocalDate getDateFromString(String date) {
         return LocalDate.parse(date, DATE_FORMAT);
     }
 
-    public boolean isBetween(LocalDate startData, LocalDate endData, LocalDate currentDate) {
+    private boolean isBetween(LocalDate startData, LocalDate endData, LocalDate currentDate) {
         return currentDate.isBefore(endData)
                 && currentDate.isAfter(startData)
                 || startData.equals(currentDate)
