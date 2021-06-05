c172afd596b8736f2e7bbd18d4b2e4dc82c52e20
Chupiak <chupyak@gmail.com>
2021-06-05 11:53:07 +0300

    Implementation of getSalaryInfo method.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,8 +1,36 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        return String.format("Report for period %s - %s", dateFrom, dateTo)
+                + calculateSalaries(names,
+                        data,
+                        LocalDate.parse(dateFrom, FORMATTER),
+                        LocalDate.parse(dateTo, FORMATTER));
+    }
+
+    private String calculateSalaries(String[] names, String[] data, LocalDate from, LocalDate to) {
+        StringBuilder result = new StringBuilder();
+        for (String name : names) {
+            int salary = 0;
+            for (String line : data) {
+                String[] fields = line.split(" ");
+                if (!name.equals(fields[1])) {
+                    continue;
+                }
+                LocalDate date = LocalDate.parse(fields[0], FORMATTER);
+                if (date.compareTo(from) < 0 || date.compareTo(to) > 0) {
+                    continue;
+                }
+                salary += Integer.parseInt(fields[2]) * Integer.parseInt(fields[3]);
+            }
+            result.append("\n").append(name).append(" - ").append(salary);
+        }
+        return result.toString();
     }
 }
