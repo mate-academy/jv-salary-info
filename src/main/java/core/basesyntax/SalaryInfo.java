ffd8204c577c097cd50222ba16b4f9ddd1a81cae
vKutik <84785564+vKutik@users.noreply.github.com>
2021-06-07 08:49:08 +0300

    remove String.format() add final variable indexes

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -6,34 +6,31 @@ import java.time.format.DateTimeFormatter;
 public class SalaryInfo {
     public static final DateTimeFormatter DATA_FORMAT =
             DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    public static final int INDEX_DATE = 0;
+    public static final int INDEX_NAME = 1;
+    public static final int INDEX_PRICE = 2;
+    public static final int INDEX_HOURS = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        LocalDate dateFromLocalData = LocalDate.parse(dateFrom, DATA_FORMAT);
-        LocalDate dateToLocalData = LocalDate.parse(dateTo, DATA_FORMAT);
+        final LocalDate dateFromLocal = LocalDate.parse(dateFrom, DATA_FORMAT);
+        final LocalDate dateToLocal = LocalDate.parse(dateTo, DATA_FORMAT);
         LocalDate dateWork;
         String[] dataOfMen = new String[4];
-        int salaryMen = 0;
-
-        StringBuilder salaryInfo = new StringBuilder(String.format("Report for period %s - %s",
-                                                                                      dateFrom,
-                                                                                      dateTo));
+        StringBuilder salaryInfo = new StringBuilder();
+        salaryInfo.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
         for (String name : names) {
+            int salaryMen = 0;
             for (String info : data) {
                 dataOfMen = info.split(" ");
-                dateWork = LocalDate.parse(dataOfMen[0], DATA_FORMAT);
-                if (name.equals(dataOfMen[1])) {
-                    if ((dateToLocalData.isAfter(dateWork) || (dateToLocalData.isEqual(dateWork))
-                            && dateFromLocalData.isBefore(dateWork)
-                            || dateFromLocalData.isEqual(dateWork))) {
-                        salaryMen += Integer.parseInt(dataOfMen[2])
-                                   * Integer.parseInt(dataOfMen[3]);
+                dateWork = LocalDate.parse(dataOfMen[INDEX_DATE], DATA_FORMAT);
+                if (name.equals(dataOfMen[INDEX_NAME])) {
+                    if (!dateWork.isBefore(dateFromLocal) && !dateWork.isAfter(dateToLocal)) {
+                        salaryMen += Integer.parseInt(dataOfMen[INDEX_HOURS])
+                                   * Integer.parseInt(dataOfMen[INDEX_PRICE]);
                     }
-                }else {
-                    continue;
                 }
             }
             salaryInfo.append("\n").append(name).append(" - ").append(salaryMen);
-            salaryMen = 0;
         }
         return salaryInfo.toString();
     }
