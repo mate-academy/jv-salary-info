674a7c662fe59c3a5a31a73655bcfc59d8d7588c
Vladyslav Baydak <74090608+Optipist@users.noreply.github.com>
2021-06-07 16:24:39 +0300

    Update src/main/java/core/basesyntax/SalaryInfo.java
    Co-authored-by: KseniiaMakarova <59459757+KseniiaMakarova@users.noreply.github.com>

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -15,9 +15,6 @@ public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate fromDate = LocalDate.parse(dateFrom, DOT_FORMAT);
         LocalDate toDate = LocalDate.parse(dateTo, DOT_FORMAT);
-        LocalDate date = null;
-        String[] splited;
-        int salarySum = 0;
         StringBuilder builder = new StringBuilder("Report for period ").append(dateFrom)
                 .append(" - ").append(dateTo);
         for (String name: names) {
