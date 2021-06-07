255cafea1e4e698a905d8e3d9161dd77a4f6371c
Optipist <vlad.baydak28@gmail.com>
2021-06-07 17:57:45 +0300

    Solved all Travis problems

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,15 +4,16 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private static final DateTimeFormatter DOT_FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final DateTimeFormatter
+            DOT_FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
     private static final int NAME_INDEX = 1;
     private static final int DATE_INDEX = 0;
     private static final int SALARY_INDEX = 3;
     private static final int HOUR_INDEX = 2;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        LocalDate fromDate = LocalDate.parse(dateFrom, DOT_FORMAT);
-        LocalDate toDate = LocalDate.parse(dateTo, DOT_FORMAT);
+        LocalDate fromDate = LocalDate.parse(dateFrom, DOT_FORMATTER);
+        LocalDate toDate = LocalDate.parse(dateTo, DOT_FORMATTER);
         StringBuilder builder = new StringBuilder("Report for period ").append(dateFrom)
                 .append(" - ").append(dateTo);
         for (String name: names) {
@@ -20,7 +21,7 @@ public class SalaryInfo {
             for (String line: data) {
                 String[] splited = line.split(" ");
                 if (name.equals(splited[NAME_INDEX])) {
-                    LocalDate date = date.parse(splited[DATE_INDEX], DOT_FORMAT);
+                    LocalDate date = LocalDate.parse(splited[DATE_INDEX], DOT_FORMATTER);
                     if ((fromDate.isEqual(date) || fromDate.isBefore(date))
                             && (toDate.isEqual(date) || toDate.isAfter(date))) {
                         salarySum += Integer.parseInt(splited[SALARY_INDEX])
