32a268fc43c10c5f3cad8af12b8c0846b22d3580
Optipist <vlad.baydak28@gmail.com>
2021-06-07 10:58:37 +0300

    Added requested fixes

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,26 +4,26 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private static StringBuilder builder;
     private static final DateTimeFormatter DOT_FORMAT = DateTimeFormatter.ofPattern("dd.MM.yyyy");
     private static final int NAME_INDEX = 1;
     private static final int DATE_INDEX = 0;
     private static final int SALARY_INDEX = 3;
     private static final int HOUR_INDEX = 2;
-    private static LocalDate date;
 
-    private String[] splited;
-    private int salarySum;
+
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate fromDate = LocalDate.parse(dateFrom, DOT_FORMAT);
         LocalDate toDate = LocalDate.parse(dateTo, DOT_FORMAT);
-        builder = new StringBuilder("Report for period ").append(dateFrom)
+        LocalDate date = null;
+        String[] splited;
+        int salarySum = 0;
+        StringBuilder builder = new StringBuilder("Report for period ").append(dateFrom)
                 .append(" - ").append(dateTo);
-        for (String namesRunner: names) {
-            for (String dataRunner: data) {
-                splited = dataRunner.split(" ");
-                if (namesRunner.equals(splited[NAME_INDEX])) {
+        for (String name: names) {
+            for (String line: data) {
+                splited = line.split(" ");
+                if (name.equals(splited[NAME_INDEX])) {
                     date = date.parse(splited[DATE_INDEX], DOT_FORMAT);
                     if ((fromDate.isEqual(date) || fromDate.isBefore(date))
                             && (toDate.isEqual(date) || toDate.isAfter(date))) {
@@ -32,7 +32,7 @@ public class SalaryInfo {
                     }
                 }
             }
-            builder.append(String.format("\n%s - %d", namesRunner, salarySum));
+            builder.append(String.format("\n%s - %d", name, salarySum));
             salarySum = 0;
         }
         return builder.toString();
