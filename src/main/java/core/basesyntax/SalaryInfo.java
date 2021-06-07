184cb31245919497ea6408a5930ff1dc0a0faeaa
docer <vlasovengineer@gmail.com>
2021-06-07 12:51:24 +0300

    add modifiers constants rename more informative variables

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,41 +4,39 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    static final String FORMAT_DATE = "dd.MM.yyyy";
-    static final int POSITION_DATE_IN_ARRAY_DATA = 0;
-    static final int POSITION_NAME_IN_ARRAY_DATE = 1;
-    static final int POSITION_HOURS_IN_ARRAY_DATE = 2;
-    static final int POSITION_SALARY_IN_ARRAY_DATE = 3;
+    private static final String DATE_FORMAT = "dd.MM.yyyy";
+    private static final int POSITION_DATE_IN_ARRAY_DATA = 0;
+    private static final int POSITION_NAME_IN_ARRAY_DATA = 1;
+    private static final int POSITION_HOURS_IN_ARRAY_DATA = 2;
+    private static final int POSITION_SALARY_IN_ARRAY_DATA = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate dateFromSalaryInfo = LocalDate.parse(dateFrom,
-                DateTimeFormatter.ofPattern(FORMAT_DATE));
-        LocalDate dateToSalaryInfo = LocalDate.parse(dateTo,
-                DateTimeFormatter.ofPattern(FORMAT_DATE));
+                DateTimeFormatter.ofPattern(DATE_FORMAT));
         LocalDate dateWorkUser;
-        StringBuilder finishInformation = new StringBuilder("Report for period ");
-        finishInformation.append(dateFrom).append(" - ").append(dateTo);
-        String[] tempArray;
-        int allSalaryUser;
+        StringBuilder resultInformation = new StringBuilder("Report for period ");
+        resultInformation.append(dateFrom).append(" - ").append(dateTo);
 
         for (String nameUser : names) {
-            allSalaryUser = 0;
+            int allSalaryUser = 0;
             for (String dataWorkUser : data) {
-                tempArray = dataWorkUser.split(" ");
-                if (tempArray[POSITION_NAME_IN_ARRAY_DATE].equals(nameUser)) {
+                String[] tempArray = dataWorkUser.split(" ");
+                if (tempArray[POSITION_NAME_IN_ARRAY_DATA].equals(nameUser)) {
                     dateWorkUser = LocalDate.parse(tempArray[POSITION_DATE_IN_ARRAY_DATA],
-                            DateTimeFormatter.ofPattern(FORMAT_DATE));
+                            DateTimeFormatter.ofPattern(DATE_FORMAT));
+                    LocalDate currentDate = LocalDate.parse(dateTo,
+                            DateTimeFormatter.ofPattern(DATE_FORMAT));
                     if (dateWorkUser.isAfter(dateFromSalaryInfo)
-                            && dateWorkUser.isBefore(dateToSalaryInfo)
+                            && dateWorkUser.isBefore(currentDate)
                             || dateWorkUser.equals(dateFromSalaryInfo)
-                            || dateWorkUser.equals(dateToSalaryInfo)) {
-                        allSalaryUser += Integer.parseInt(tempArray[POSITION_HOURS_IN_ARRAY_DATE])
-                                * Integer.parseInt(tempArray[POSITION_SALARY_IN_ARRAY_DATE]);
+                            || dateWorkUser.equals(currentDate)) {
+                        allSalaryUser += Integer.parseInt(tempArray[POSITION_HOURS_IN_ARRAY_DATA])
+                                * Integer.parseInt(tempArray[POSITION_SALARY_IN_ARRAY_DATA]);
                     }
                 }
             }
-            finishInformation.append("\n").append(nameUser).append(" - ").append(allSalaryUser);
+            resultInformation.append("\n").append(nameUser).append(" - ").append(allSalaryUser);
         }
-        return finishInformation.toString();
+        return resultInformation.toString();
     }
 }
