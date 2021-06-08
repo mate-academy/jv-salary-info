8acea054b76f0472cfce2538163d885e04b65cc9
“Valerii <torsher09@gmail.com>
2021-06-08 14:27:43 +0300

    changed DATE_FORMAT to constant; changed fori to foreach; changed all spaces for SPACE constant; changed dash to DASH constant;

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,21 +5,23 @@ import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     private static final String SPACE = " ";
+    private static final String DASH = " - ";
+    private static final String DATE_FORMAT = "dd.MM.yyyy";
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_FORMAT);
         LocalDate sdateFrom = LocalDate.parse(dateFrom, formatter);
         LocalDate sdateTo = LocalDate.parse(dateTo, formatter);
         int[] totalSalaryForPerson = new int[names.length];
-        for (int i = 0; i < data.length; i++) {
-            String[] dates = data[i].split(" ");
+        for (String inputData : data) {
+            String[] dates = inputData.split(SPACE);
             LocalDate inputDateString = LocalDate.parse(dates[0], formatter);
             if ((inputDateString.isBefore(sdateTo) && inputDateString.isAfter(sdateFrom))
-            || inputDateString.isEqual(sdateTo) || inputDateString.isEqual(sdateFrom)) {
+                    || inputDateString.isEqual(sdateTo) || inputDateString.isEqual(sdateFrom)) {
                 for (int j = 0; j < names.length; j++) {
-                    if (names[j].equals(getName(data[i]))) {
+                    if (names[j].equals(getName(inputData))) {
                         totalSalaryForPerson[j] +=
-                                getPayPerDay(data[i]) * getDaysOfWork(data[i]);
+                                getPayPerDay(inputData) * getDaysOfWork(inputData);
                         break;
                     }
                 }
@@ -30,10 +32,10 @@ public class SalaryInfo {
                 .append(dateTo).append("\n");
         for (int i = 0; i < names.length; i++) {
             if (i == names.length - 1) {
-                stringBuilder.append(names[i]).append(" - ")
+                stringBuilder.append(names[i]).append(DASH)
                         .append(totalSalaryForPerson[i]);
             } else {
-                stringBuilder.append(names[i]).append(" - ")
+                stringBuilder.append(names[i]).append(DASH)
                         .append(totalSalaryForPerson[i]).append("\n");
             }
         }
@@ -53,7 +55,7 @@ public class SalaryInfo {
     }
 
     private int getDaysOfWork(String string) {
-        String[] array = string.split(" ");
+        String[] array = string.split(SPACE);
         int payPerDay = Integer.parseInt(array[2]);
         return payPerDay;
     }
