097c544c6c02c1d90f3185b957cee81c0b241dda
PashaTerletskyi <p.terletsky@gmail.com>
2021-06-08 12:36:58 +0300

    Changed program logic from indexOf() to element.split(DELIMITER);

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,8 +4,11 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    public static final String DATA_SEPARATOR = " ";
-    public static final int DATA_SEPARATOR_LENGTH = DATA_SEPARATOR.length();
+    public static final String DELIMITER = " ";
+    public static final int DATA_INDEX = 0;
+    public static final int NAME_INDEX = 1;
+    public static final int HOURS_INDEX = 2;
+    public static final int SALARY_PER_HOUR_INDEX = 3;
 
     public String getSalaryInfo(String[] names, String[] data,
                                        String dateFrom, String dateTo) {
@@ -16,23 +19,16 @@ public class SalaryInfo {
                 .append("Report for period ")
                 .append(dateFrom).append(" - ")
                 .append(dateTo);
+        String[] splitLine;
         for (String name : names) {
             int earnedPerName = 0;
             for (String element : data) {
-                if (name.equals(element.substring(element.indexOf(DATA_SEPARATOR, 0)
-                                + DATA_SEPARATOR_LENGTH,
-                        element.indexOf(DATA_SEPARATOR, element.indexOf(DATA_SEPARATOR, 0)
-                                + DATA_SEPARATOR_LENGTH)))
-                        && isDateInPeriod(element.substring(0,element.indexOf(DATA_SEPARATOR, 0)),
-                        dateTimeFormatter,dateFromWithFormatter, dateToWithFormatter)) {
-                    earnedPerName += Integer.parseInt(element.substring(element
-                            .indexOf(DATA_SEPARATOR,
-                                    element.indexOf(DATA_SEPARATOR, 0)
-                                            + DATA_SEPARATOR_LENGTH) + DATA_SEPARATOR_LENGTH,
-                            element.lastIndexOf(DATA_SEPARATOR)))
-                            * Integer.parseInt(element.substring(element.lastIndexOf(DATA_SEPARATOR)
-                            + DATA_SEPARATOR_LENGTH, element.length()));
-                    System.out.println(earnedPerName);
+                splitLine = element.split(DELIMITER);
+                if (name.equals(splitLine[NAME_INDEX])
+                        && isDateInPeriod(splitLine[DATA_INDEX],
+                        dateTimeFormatter, dateFromWithFormatter, dateToWithFormatter)) {
+                    earnedPerName += Integer.parseInt(splitLine[HOURS_INDEX])
+                            * Integer.parseInt(splitLine[SALARY_PER_HOUR_INDEX]);
                 }
             }
             salaryInfo = salaryInfo.append("\n")
