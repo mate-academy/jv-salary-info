32db697d80a540efe7d62cdd08ce0b7502b1e880
vKutik <84785564+vKutik@users.noreply.github.com>
2021-06-08 15:43:35 +0300

    create constant variable

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -10,12 +10,12 @@ public class SalaryInfo {
     public static final int INDEX_NAME = 1;
     public static final int INDEX_PRICE = 2;
     public static final int INDEX_HOURS = 3;
-
+    public static final int SIZE_DATA_ARRAY = 4;
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         final LocalDate dateFromLocal = LocalDate.parse(dateFrom, DATA_FORMAT);
         final LocalDate dateToLocal = LocalDate.parse(dateTo, DATA_FORMAT);
         LocalDate dateWork;
-        String[] dataOfMen = new String[4];
+        String[] dataOfMen = new String[SIZE_DATA_ARRAY];
         StringBuilder salaryInfo = new StringBuilder();
         salaryInfo.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
         for (String name : names) {
