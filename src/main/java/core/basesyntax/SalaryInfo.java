4253bf64474533265ef926a8f4468826ece806b7
Chupiak <chupyak@gmail.com>
2021-06-09 19:23:18 +0300

    add recommendations

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,32 +5,34 @@ import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int INDEX_DATA = 0;
+    private static final int INDEX_SALARY_PERHOUR = 2;
+    private static final int INDEX_AMOUNT_HOURS = 3;
+
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return String.format("Report for period %s - %s", dateFrom, dateTo)
-                + calculateSalaries(names,
-                        data,
-                        LocalDate.parse(dateFrom, FORMATTER),
-                        LocalDate.parse(dateTo, FORMATTER));
-    }
 
-    private String calculateSalaries(String[] names, String[] data, LocalDate from, LocalDate to) {
+        LocalDate from = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate to = LocalDate.parse(dateTo, FORMATTER);
+
         StringBuilder result = new StringBuilder();
-        for (String name : names) {
-            int salary = 0;
-            for (String line : data) {
-                String[] fields = line.split(" ");
-                if (!name.equals(fields[1])) {
-                    continue;
+        result.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
+
+        for (int j = 0; j<names.length; j++) {
+            int amountSalary = 0;
+            for (int i = 0; i < data.length; i++) {
+                if (data[i].contains(names[j])) {
+                    String[] array = data[i].split(" ");
+                    LocalDate date = LocalDate.parse(array[INDEX_DATA], FORMATTER);
+                    if (date.compareTo(from) >= 0 && date.compareTo(to) <= 0) {
+                        amountSalary += Integer.valueOf(array[INDEX_SALARY_PERHOUR])
+                                * Integer.valueOf(array[INDEX_AMOUNT_HOURS]);
+                    }
                 }
-                LocalDate date = LocalDate.parse(fields[0], FORMATTER);
-                if (date.compareTo(from) < 0 || date.compareTo(to) > 0) {
-                    continue;
-                }
-                salary += Integer.parseInt(fields[2]) * Integer.parseInt(fields[3]);
             }
-            result.append("\n").append(name).append(" - ").append(salary);
+            result.append("\n").append(names[j]).append(" - ").append(amountSalary);
         }
         return result.toString();
     }
 }
+
