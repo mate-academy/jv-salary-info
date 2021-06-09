51e828d2c933f791b051a44e83d45e3943cdfe57
Chupiak <chupyak@gmail.com>
2021-06-09 21:32:42 +0300

    add recommendations and rewrite code

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -6,31 +6,30 @@ import java.time.format.DateTimeFormatter;
 public class SalaryInfo {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
     private static final int INDEX_DATA = 0;
-    private static final int INDEX_SALARY_PERHOUR = 2;
+    private static final int INDEX_FROM_DATA = 1;
+    private static final int INDEX_SALARY_PER_HOUR = 2;
     private static final int INDEX_AMOUNT_HOURS = 3;
 
-
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-
-        LocalDate from = LocalDate.parse(dateFrom, FORMATTER);
-        LocalDate to = LocalDate.parse(dateTo, FORMATTER);
 
         StringBuilder result = new StringBuilder();
         result.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
 
-        for (int j = 0; j<names.length; j++) {
-            int amountSalary = 0;
-            for (int i = 0; i < data.length; i++) {
-                if (data[i].contains(names[j])) {
-                    String[] array = data[i].split(" ");
-                    LocalDate date = LocalDate.parse(array[INDEX_DATA], FORMATTER);
-                    if (date.compareTo(from) >= 0 && date.compareTo(to) <= 0) {
-                        amountSalary += Integer.valueOf(array[INDEX_SALARY_PERHOUR])
-                                * Integer.valueOf(array[INDEX_AMOUNT_HOURS]);
-                    }
+        LocalDate datefrom = LocalDate.parse(dateFrom,FORMATTER);
+        LocalDate dateto = LocalDate.parse(dateTo,FORMATTER);
+
+        for (String name : names) {
+            int salary = 0;
+            for (String datas : data) {
+                String [] massive = datas.split(" ");
+                LocalDate now = LocalDate.parse(massive[INDEX_DATA],FORMATTER);
+                if (name.equals(massive[INDEX_FROM_DATA]) && now.compareTo(datefrom) >= 0
+                        && now.compareTo(dateto) <= 0) {
+                    salary += Integer.parseInt(massive[INDEX_SALARY_PER_HOUR])
+                            * Integer.parseInt(massive[INDEX_AMOUNT_HOURS]);
                 }
             }
-            result.append("\n").append(names[j]).append(" - ").append(amountSalary);
+            result.append("\n").append(name).append(" - ").append(salary);
         }
         return result.toString();
     }
