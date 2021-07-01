6d32ce1706a69f2996592a54149daa5435364ac1
Olexandr <olesukmail@gmail.com>
2021-07-01 10:39:53 +0300

    Fixed name of field for result to report, add split method.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,23 +5,24 @@ import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        StringBuilder result = new StringBuilder();
-        result.append("Report for period ").append(dateFrom).append(" - ")
+        StringBuilder report = new StringBuilder();
+        report.append("Report for period ").append(dateFrom).append(" - ")
                 .append(dateTo);
         for (String name : names) {
             int salaryCount = 0;
             for (String datum : data) {
-                if (name.equals(datum.substring(11, 11 + name.length()))
-                        && !getDate(datum.substring(0, 10)).isBefore(getDate(dateFrom))
-                        && !getDate(datum.substring(0, 10)).isAfter(getDate(dateTo))) {
+                String[] datumParts = datum.split(" ");
+                if (name.equals(datumParts[1])
+                        && !getDate(datumParts[0]).isBefore(getDate(dateFrom))
+                        && !getDate(datumParts[0]).isAfter(getDate(dateTo))) {
                     int hours = Integer.parseInt(datum.split(" ")[2]);
                     int salary = Integer.parseInt(datum.split(" ")[3]);
                     salaryCount += hours * salary;
                 }
             }
-            result.append(System.lineSeparator()).append(name).append(" - ").append(salaryCount);
+            report.append(System.lineSeparator()).append(name).append(" - ").append(salaryCount);
         }
-        return result.toString().trim();
+        return report.toString().trim();
     }
 
     public final LocalDate getDate(String date) {
