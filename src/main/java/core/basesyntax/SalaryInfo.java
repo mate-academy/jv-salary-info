d3efe18d9d86434e9c341765b070114c7a759bec
Vitalii Martynov <VitaliiMvDeveloper@gmail.com>
2021-07-12 10:09:39 +0300

    The variable's access was changed. The redundant conditions and try blocks were removed.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -2,61 +2,37 @@ package core.basesyntax;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
 
 public class SalaryInfo {
-    static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        LocalDate localDateFrom = LocalDate.now();
-        LocalDate localDateTo = LocalDate.now();
-        try {
-            localDateFrom = LocalDate.parse(dateFrom, FORMATTER);
-        } catch (DateTimeParseException e) {
-            System.out.println("Wrong format of dateFrom!");
-        }
-        try {
-            localDateTo = LocalDate.parse(dateTo, FORMATTER);
-        } catch (DateTimeParseException e) {
-            System.out.println("Wrong format of dateTo!");
-        }
+        LocalDate localDateFrom = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate localDateTo = LocalDate.parse(dateTo, FORMATTER);
 
-        StringBuilder sb = new StringBuilder();
-        sb.append("Report for period " + localDateFrom.format(FORMATTER) + " - "
+        StringBuilder report = new StringBuilder();
+        report.append("Report for period " + localDateFrom.format(FORMATTER) + " - "
                 + localDateTo.format(FORMATTER));
 
         for (String name : names) {
-            sb.append(System.lineSeparator());
+            report.append(System.lineSeparator());
             int salary = 0;
             for (String element : data) {
                 String[] parsedData = element.split(" ");
-                if (parsedData.length != 4) {
-                    continue;
-                }
-                if (! parsedData[1].equals(name)) {
-                    continue;
-                }
 
-                LocalDate workingDate;
-                int workingHours = 0;
-                int ratePerHour = 0;
+                LocalDate workingDate = LocalDate.parse(parsedData[0], FORMATTER);
+                int workingHours = Integer.valueOf(parsedData[2]);
+                int ratePerHour = Integer.valueOf(parsedData[3]);
 
-                try {
-                    workingDate = LocalDate.parse(parsedData[0], FORMATTER);
-                    workingHours = Integer.valueOf(parsedData[2]);
-                    ratePerHour = Integer.valueOf(parsedData[3]);
-                } catch (NumberFormatException | DateTimeParseException e) {
-                    System.out.println("There are some problems with data: " + element);
-                    continue;
-                }
-
-                if ((workingDate.isAfter(localDateFrom) && workingDate.isBefore(localDateTo))
-                        || workingDate.isEqual(localDateFrom) || workingDate.isEqual(localDateTo)) {
+                if (parsedData[1].equals(name) && (
+                        (workingDate.isAfter(localDateFrom) && workingDate.isBefore(localDateTo))
+                        || workingDate.isEqual(localDateFrom)
+                        || workingDate.isEqual(localDateTo))) {
                     salary += workingHours * ratePerHour;
                 }
             }
-            sb.append(name + " - " + salary);
+            report.append(name + " - " + salary);
         }
-        return sb.toString();
+        return report.toString();
     }
 }
