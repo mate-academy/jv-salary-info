cc25b00318500a7f47d8c870a34c5b839262eb00
Lena Kravchenko <cupp.lenashapoval@gmail.com>
2021-07-19 22:17:35 +0300

    Fixes after review

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -11,16 +11,16 @@ public class SalaryInfo {
         result.append("Report for period ").append(dateFrom).append(" - ")
                 .append(dateTo).append(System.lineSeparator());
 
-        LocalDate start = getDate(dateFrom);
-        LocalDate end = getDate(dateTo);
+        LocalDate start = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate end = LocalDate.parse(dateTo, FORMATTER);;
 
         for (String name : names) {
             int salary = 0;
             for (String row : data) {
-                if (row.contains(name)) {
-                    String[] info = row.trim().split(" ");
-                    if (getDate(info[0]).isAfter(start.minusDays(1))
-                            && getDate(info[0]).isBefore(end.plusDays(1))) {
+                String[] info = row.trim().split(" ");
+                if (info[1].equals(name)) {
+                    if (LocalDate.parse(info[0], FORMATTER).isAfter(start.minusDays(1))
+                            && LocalDate.parse(info[0], FORMATTER).isBefore(end.plusDays(1))) {
                         salary += Integer.parseInt(info[2]) * Integer.parseInt(info[3]);
                     }
                 }
@@ -28,9 +28,5 @@ public class SalaryInfo {
             result.append(name).append(" - ").append(salary).append(System.lineSeparator());
         }
         return result.toString().trim();
-    }
-
-    private LocalDate getDate(String date) {
-        return LocalDate.parse(date, FORMATTER);
     }
 }
