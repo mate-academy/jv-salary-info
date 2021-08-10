3aba5d1e735f13d6cbf88e1b529a7f20c07eaaf2
vadim9482 <vadim9482@gmail.com>
2021-08-10 22:55:02 +0300

    Implement append for all parts of report

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -7,7 +7,8 @@ public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate from = DateParser.parse(dateFrom);
         LocalDate to = DateParser.parse(dateTo);
-        StringBuilder report = new StringBuilder("Report for period " + dateFrom + " - " + dateTo);
+        StringBuilder report = new StringBuilder("Report for period ")
+                .append(dateFrom).append(" - ").append(dateTo);
         for (String name : names) {
             int salary = 0;
             for (String datum : data) {
