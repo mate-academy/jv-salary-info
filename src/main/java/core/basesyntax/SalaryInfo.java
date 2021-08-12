ccfe138d62cf091a73da44d276f0a9acb6f5a004
Oleg Kvasha <oleg.kvasha.00@gmail.com>
2021-08-12 18:37:59 +0300

    Added first checks

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,29 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class SalaryInfo {
+    public static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d.M.y");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
+        StringBuilder result = new StringBuilder();
+        LocalDate localDateFrom;
+        LocalDate localDateTo;
+        if (names == null || data == null || dateFrom == null || dateTo == null) {
+            throw new RuntimeException("Null argument");
+        }
+        try {
+            localDateFrom = LocalDate.parse(dateFrom, formatter);
+            localDateTo = LocalDate.parse(dateTo, formatter);
+        } catch (DateTimeParseException dateTimeParseException) {
+            throw new RuntimeException("Invalid dateFrom or dateTo");
+        }
+
+        if (!localDateFrom.isBefore(localDateTo)) {
+            throw new RuntimeException("Invalid date range");
+        }
         return null;
     }
 }
