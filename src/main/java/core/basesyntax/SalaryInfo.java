12879e078a96ac3e383cb35f75916c11517564e7
Oleg Kvasha <oleg.kvasha.00@gmail.com>
2021-08-13 14:31:23 +0300

    Fixed method getSalaryInfo in SalaryInfo class

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,12 +5,18 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
 public class SalaryInfo {
-    public static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d.M.y");
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int numberOfColumns = 4;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        StringBuilder result = new StringBuilder();
         LocalDate localDateFrom;
         LocalDate localDateTo;
+        String[] itemDataArray;
+        LocalDate itemDate;
+        String itemName;
+        int numberOfDays;
+        int[] totalSalaries = new int[names.length];
+        int itemSalary;
         if (names == null || data == null || dateFrom == null || dateTo == null) {
             throw new RuntimeException("Null argument");
         }
@@ -21,9 +27,55 @@ public class SalaryInfo {
             throw new RuntimeException("Invalid dateFrom or dateTo");
         }
 
-        if (!localDateFrom.isBefore(localDateTo)) {
+        if (localDateFrom.compareTo(localDateTo) > 0) {
             throw new RuntimeException("Invalid date range");
         }
-        return null;
+
+        StringBuilder result = new StringBuilder();
+
+        result.append("Report for period "
+                + dateFrom
+                + " - "
+                + dateTo
+                + System.lineSeparator());
+        for (String itemData : data) {
+            itemDataArray = itemData.split(" ");
+
+            if (itemDataArray.length != numberOfColumns) {
+                throw new RuntimeException("Invalid data row");
+            }
+            try {
+                itemDate = LocalDate.parse(itemDataArray[0], formatter);
+                numberOfDays = Integer.parseInt(itemDataArray[2]);
+                itemSalary = Integer.parseInt(itemDataArray[3]);
+            } catch (DateTimeParseException dateTimeParseException) {
+                throw new RuntimeException("Invalid date in the row in the data");
+            } catch (NumberFormatException numberFormatException) {
+                throw new RuntimeException("Invalid format number of days or salary");
+            }
+            if (numberOfDays < 0) {
+                throw new RuntimeException("Invalid value of number of days");
+            } else if (itemSalary < 0) {
+                throw new RuntimeException("Invalid value of salary");
+            }
+            itemName = itemDataArray[1];
+            if (itemDate.compareTo(localDateFrom) >= 0 && itemDate.compareTo(localDateTo) <= 0) {
+                for (int i = 0; i < names.length; i++) {
+                    if (names[i].equals(itemName)) {
+                        totalSalaries[i] += numberOfDays * itemSalary;
+                    }
+                }
+            }
+
+        }
+        for (int i = 0; i < names.length; i++) {
+            result.append(names[i])
+                    .append(" - ")
+                    .append(totalSalaries[i]);
+            if (i != names.length - 1) {
+                result.append(System.lineSeparator());
+            }
+        }
+        return result.toString();
     }
 }
