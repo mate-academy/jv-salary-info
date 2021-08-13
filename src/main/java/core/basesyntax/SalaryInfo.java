24fa2ff7a26e2b75d5b6e6fabb77156c65967cd6
Ihor Khominich <ikhominitch@gmail.com>
2021-08-13 13:13:08 +0300

    variables changed, commentaries removed

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,21 +5,19 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
 public class SalaryInfo {
-    public static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-    private LocalDate dateFrom;
-    private LocalDate dateTo;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        int currentSum;
-        int searchInDataResult;
+        final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDate localDateTo;
+        LocalDate localDateFrom;
         LocalDate currentDate;
         try {
-            this.dateFrom = LocalDate.parse(dateFrom, formatter);
+            localDateFrom = LocalDate.parse(dateFrom, formatter);
         } catch (DateTimeParseException e) {
             return "Start date is incorrect!";
         }
         try {
-            this.dateTo = LocalDate.parse(dateTo, formatter);
+            localDateTo = LocalDate.parse(dateTo, formatter);
         } catch (DateTimeParseException e) {
             return "Final date is incorrect!";
         }
@@ -27,21 +25,19 @@ public class SalaryInfo {
         resultBuilder.append(dateFrom).append(" - ").append(dateTo);
         for (int i = 0; i < names.length; i++) {
             resultBuilder.append(System.lineSeparator()).append(names[i]).append(" - ");
-            currentSum = 0;
+            int currentSum = 0;
             for (int j = 0; j < data.length; j++) {
-                searchInDataResult = data[j].indexOf(names[i]); // points to the Name position
-                if (searchInDataResult >= 0) { // name found in the data string
+                int searchInDataResult = data[j].indexOf(names[i]);
+                if (searchInDataResult >= 0) {
                     try {
                         currentDate = LocalDate.parse(data[j]
                                 .substring(0, searchInDataResult - 1), formatter);
                     } catch (DateTimeParseException e) {
                         return "Date is incorrect! " + data[j];
                     }
-                    if (! this.dateFrom.isAfter(currentDate)
-                            && ! this.dateTo.isBefore(currentDate)) {
-                        // date is in range
+                    if (! localDateFrom.isAfter(currentDate)
+                            && ! localDateTo.isBefore(currentDate)) {
                         searchInDataResult = searchInDataResult + names[i].length() + 1;
-                        // searchInDataResult now points to the number of hours in the data string
                         currentSum += Integer.parseInt(data[j].substring(searchInDataResult,
                                 data[j].indexOf(' ', searchInDataResult)))
                                 * Integer.parseInt(data[j].substring(data[j].indexOf(' ',
