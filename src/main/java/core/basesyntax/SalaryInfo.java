07e086b803b9d10ae954ed2b10cddebfac154f15
Oleksandr Rozvazhaiev <alexrozv@gmail.com>
2021-08-16 20:23:14 +0300

    created getSalaryInfo and isCorrectDate. create main class to test if everything works

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,39 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+    private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        StringBuilder report = new StringBuilder().append("Report for period ")
+                .append(dateFrom)
+                .append(" - ")
+                .append(dateTo);
+
+        for (String name : names) {
+            int sum = 0;
+
+            for (String datum : data) {
+                String date = datum.split(" ")[0];
+                String worker = datum.split(" ")[1];
+                int hours = Integer.parseInt(datum.split(" ")[2]);
+                int rate = Integer.parseInt(datum.split(" ")[3]);
+
+                if (name.equals(worker) && isCorrectDate(dateFrom, dateTo, date)) {
+                    sum += hours * rate;
+                }
+            }
+            report.append("\n").append(name).append(" - ").append(sum);
+        }
+        return report.toString();
+    }
+
+    public boolean isCorrectDate(String startInput, String stopInput, String workDate) {
+        LocalDate start = LocalDate.parse( startInput , DATE_FORMAT) ;
+        LocalDate stop = LocalDate.parse( stopInput , DATE_FORMAT) ;
+        LocalDate work = LocalDate.parse( workDate , DATE_FORMAT) ;
+        return start.isBefore(work) && work.isBefore(stop);
     }
 }
