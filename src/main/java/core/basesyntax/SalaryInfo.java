0ad868590e652a87b7ce369217ab056869c00572
Yurii <wervmer>
2021-08-16 11:10:03 +0300

    changed 'builder' to 'report' in 10 line, added array Draw with information about employee, deleted method getName and getDate

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -7,45 +7,34 @@ public class SalaryInfo {
     private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        StringBuilder builder = new StringBuilder();
-        builder.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
+        StringBuilder report = new StringBuilder();
+        report.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
         for (String name: names) {
             int salary = 0;
-            builder.append(System.lineSeparator()).append(name).append(" - ");
+            report.append(System.lineSeparator()).append(name).append(" - ");
             for (String info : data) {
-
-                if ((formatter(getDate(info)).isAfter(formatter(dateFrom))
-                        || formatter(getDate(info)).equals(formatter(dateFrom)))
-                        && (formatter(getDate(info)).equals(formatter(dateTo))
-                        || formatter(getDate(info)).isBefore(formatter(dateTo)))
-                        && nameFromInfo(info).equals(name)) {
-                    salary += getSalaryFromInfo(info);
+                String[] Draw = info.split(" ");
+                if ((formatter(Draw[0]).isAfter(formatter(dateFrom))
+                        || formatter(Draw[0]).equals(formatter(dateFrom)))
+                        && (formatter(Draw[0]).equals(formatter(dateTo))
+                        || formatter(Draw[0]).isBefore(formatter(dateTo)))
+                        && Draw[1].equals(name)) {
+                    salary += getSalaryFromInfo(Draw);
                 }
             }
-            builder.append(salary);
+            report.append(salary);
 
         }
-        return builder.toString();
-    }
-
-    private String getDate(String info) {
-        return info.substring(0, info.indexOf(' '));
+        return report.toString();
     }
 
     private LocalDate formatter(String info) {
         return LocalDate.parse(info, formatter);
     }
 
-    private String nameFromInfo(String info) {
-        return info.substring(info.indexOf(' ') + 1, info.indexOf(' ', info.indexOf(' ') + 1));
-    }
-
-    private int getSalaryFromInfo(String info) {
-        String hoursAndValue = info.substring(info.indexOf(' ',
-                info.indexOf(' ', info.indexOf(' ') + 1)) + 1);
-
-        int hours = Integer.parseInt(hoursAndValue.substring(0, hoursAndValue.indexOf(' ')));
-        int value = Integer.parseInt(hoursAndValue.substring(hoursAndValue.indexOf(' ') + 1));
+    private int getSalaryFromInfo(String[] Draw) {
+        int hours = Integer.parseInt(Draw[2]);
+        int value = Integer.parseInt(Draw[3]);
         return hours * value;
     }
 }
