1497b0a48423d08625879fce53e4ad0d2094a0f9
Dima Petrushenko <dmytro.petrushenko@gmail.com>
2021-08-16 14:05:30 +0300

    teacher's notices were fixed(formatter & split())

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,23 +4,24 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder report = new StringBuilder();
         report.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
         for (String name: names) {
             String nameCheked = name.replaceAll("[^A-Za-z]", "");
-            report.append(System.lineSeparator()).append(nameCheked);
+            report.append(System.lineSeparator()).append(name.replaceAll("[^A-Za-z]", ""));
             int amount = 0;
             for (String dayData: data) {
-                int spaceSecond = (dayData.substring(11)).indexOf(' ') + 11;
-                int spacelast = dayData.lastIndexOf(' ');
+                String[] dayDataArray = dayData.split(" ");
+                String dateDay = dayDataArray[0];
+                String nameDay = dayDataArray[1];
+                int hoursDay = Integer.parseInt(dayDataArray[2]);
+                int salaryDay = Integer.parseInt(dayDataArray[3]);
 
-                if (checkDate(dayData, dateTo, dateFrom) && nameCheked.equals(dayData
-                        .substring(11, spaceSecond))) {
-                    amount += Integer.parseInt(dayData.substring(spaceSecond + 1, spacelast)
-                            .replaceAll("[^0-9]", ""))
-                            * Integer.parseInt(dayData.substring(spacelast + 1)
-                            .replaceAll("[^0-9]",""));
+                if (checkDate(dateDay, dateTo, dateFrom) && nameCheked.equals(nameDay)) {
+                    amount += hoursDay * salaryDay;
                 }
             }
             report.append(" - ").append(amount);
@@ -29,17 +30,15 @@ public class SalaryInfo {
     }
 
     public LocalDate toDateFormat(String date) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
         return LocalDate.parse(date, formatter);
     }
 
-    public boolean checkDate(String dayData, String dateTo, String dateFrom) {
-        LocalDate date = toDateFormat(dayData.substring(0, 10));
+    public boolean checkDate(String dateDay, String dateTo, String dateFrom) {
+        LocalDate date = toDateFormat(dateDay);
         LocalDate dateToLoc = toDateFormat(dateTo);
         LocalDate dateFromLoc = toDateFormat(dateFrom);
 
         return (date.isAfter(dateFromLoc) && date.isBefore(dateToLoc)) || date.isEqual(dateFromLoc)
                 || date.isEqual(dateToLoc);
-
     }
 }
