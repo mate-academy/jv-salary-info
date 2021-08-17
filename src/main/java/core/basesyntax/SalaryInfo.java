622d4456e45f44e0f161862c16bbbd71886a3f42
Vladimir Nedashkovskiy <vo-v4ik@hotmail.com>
2021-08-17 09:00:57 +0300

    replace \n to lineSeparator()

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -8,28 +8,32 @@ public class SalaryInfo {
         String[] from = dateFrom.split("\\.");
 
         StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("Report for period ");
-        stringBuilder.append(dateFrom);
-        stringBuilder.append(" - ");
-        stringBuilder.append(dateTo);
+        stringBuilder.append("Report for period ")
+        .append(dateFrom)
+        .append(" - ")
+        .append(dateTo);
 
-        LocalDate start = LocalDate.of(Integer.parseInt(from[2]), Integer.parseInt(from[1]), Integer.parseInt(from[0]));
-        LocalDate stop = LocalDate.of(Integer.parseInt(to[2]), Integer.parseInt(to[1]), Integer.parseInt(to[0]));
+        LocalDate start = LocalDate.of(Integer.parseInt(from[2]), Integer.parseInt(from[1]),
+                Integer.parseInt(from[0]));
+        LocalDate stop = LocalDate.of(Integer.parseInt(to[2]), Integer.parseInt(to[1]),
+                Integer.parseInt(to[0]));
 
         for (String name : names) {
             int salary = 0;
             for (String day : data) {
                 String[] info = day.split(" ");
                 String[] date = info[0].split("\\.");
-                LocalDate workDay = LocalDate.of(Integer.parseInt(date[2]), Integer.parseInt(date[1]), Integer.parseInt(date[0]));
-                if (info[1].equals(name) & (workDay.isEqual(start) | workDay.isEqual(stop) | (workDay.isAfter(start) & workDay.isBefore(stop)))) {
+                LocalDate workDay = LocalDate.of(Integer.parseInt(date[2]),
+                        Integer.parseInt(date[1]), Integer.parseInt(date[0]));
+                if (info[1].equals(name) & (workDay.isEqual(start) | workDay.isEqual(stop)
+                        | (workDay.isAfter(start) & workDay.isBefore(stop)))) {
                     salary += Integer.parseInt(info[2]) * Integer.parseInt(info[3]);
                 }
             }
-            stringBuilder.append("\n");
-            stringBuilder.append(name);
-            stringBuilder.append(" - ");
-            stringBuilder.append(salary);
+            stringBuilder.append(System.lineSeparator())
+            .append(name)
+            .append(" - ")
+            .append(salary);
         }
 
         return stringBuilder.toString();
