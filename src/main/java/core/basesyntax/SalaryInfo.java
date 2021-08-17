49075bfe32efa901d852b9053c638a33bcf89b13
Viktor Yukhno <pingvinomania@gmail.com>
2021-08-17 14:16:15 +0300

    fixed of the name of constant

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,14 +4,14 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         if (names.length == 0 || data.length == 0 || dateFrom.isEmpty() || dateTo.isEmpty()) {
             return "";
         }
-        LocalDate fromDate = LocalDate.parse(dateFrom, formatter).minusDays(1);
-        LocalDate toDate = LocalDate.parse(dateTo, formatter).plusDays(1);
+        LocalDate fromDate = LocalDate.parse(dateFrom, FORMATTER).minusDays(1);
+        LocalDate toDate = LocalDate.parse(dateTo, FORMATTER).plusDays(1);
         StringBuilder report = new StringBuilder().append("Report for period ")
                 .append(dateFrom).append(" - ")
                 .append(dateTo).append(System.lineSeparator());
@@ -20,7 +20,7 @@ public class SalaryInfo {
             int salary = 0;
             for (int i = 0; i < data.length; i++) {
                 String[] rowData = data[i].split(" ");
-                LocalDate date = LocalDate.parse(rowData[0], formatter);
+                LocalDate date = LocalDate.parse(rowData[0], FORMATTER);
                 if (name.equals(rowData[1]) && date.isAfter(fromDate) && date.isBefore(toDate)) {
                     salary += Integer.parseInt(rowData[2]) * Integer.parseInt(rowData[3]);
                 }
