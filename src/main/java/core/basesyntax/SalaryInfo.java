6c1c89a01c3728953a8fc10adfa7e90742ca8757
mishamolnar <michaellmuller423@gmail.com>
2021-08-17 12:59:45 +0300

    data converting done

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,19 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import javax.xml.crypto.Data;
+import java.text.SimpleDateFormat;
+
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        StringBuilder result = new StringBuilder("");
+
+        for (String name : names) {
+            int bufferSum = 0;
+            for (String datum : data) {
+                String[] personalData = datum.split(" ");
+                if (name.equals(personalData[1]) &&)
+            }
+        }
     }
 }
