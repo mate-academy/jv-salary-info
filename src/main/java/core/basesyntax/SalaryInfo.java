62bbd65c0109bf860e607a79a656e879447d2b4b
Vladimir Nedashkovskiy <vo-v4ik@hotmail.com>
2021-08-17 21:58:49 +0300

    rework and testing of a new way

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,19 +1,25 @@
 package core.basesyntax;
 
+import java.text.DateFormat;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        String[] to = dateTo.split("\\.");
-        String[] from = dateFrom.split("\\.");
+        //String[] to = dateTo.split("\\.");
+        LocalDate to = LocalDate.parse(dateTo, formatter);
+        LocalDate from = LocalDate.parse(dateFrom, formatter);
+        //String[] from = dateFrom.split("\\.");
 
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
 
-        LocalDate start = LocalDate.of(Integer.parseInt(from[2]), Integer.parseInt(from[1]),
-                Integer.parseInt(from[0]));
-        LocalDate stop = LocalDate.of(Integer.parseInt(to[2]), Integer.parseInt(to[1]),
-                Integer.parseInt(to[0]));
+        //LocalDate start = LocalDate.of(Integer.parseInt(from[2]), Integer.parseInt(from[1]),
+        //        Integer.parseInt(from[0]));
+        //LocalDate stop = LocalDate.of(Integer.parseInt(to[2]), Integer.parseInt(to[1]),
+        //        Integer.parseInt(to[0]));
 
         for (String name : names) {
             int salary = 0;
@@ -22,8 +28,8 @@ public class SalaryInfo {
                 String[] date = info[0].split("\\.");
                 LocalDate workDay = LocalDate.of(Integer.parseInt(date[2]),
                         Integer.parseInt(date[1]), Integer.parseInt(date[0]));
-                if (info[1].equals(name) & (workDay.isEqual(start) | workDay.isEqual(stop)
-                        | (workDay.isAfter(start) & workDay.isBefore(stop)))) {
+                if (info[1].equals(name) & (workDay.isEqual(from) | workDay.isEqual(to)
+                        | (workDay.isAfter(from) & workDay.isBefore(to)))) {
                     salary += Integer.parseInt(info[2]) * Integer.parseInt(info[3]);
                 }
             }
