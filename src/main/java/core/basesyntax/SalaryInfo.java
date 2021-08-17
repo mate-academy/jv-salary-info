a40736b2d5f76f48a1f89e771af63e8b31322b36
mishamolnar <michaellmuller423@gmail.com>
2021-08-17 15:31:50 +0300

    StringBuilder created

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -3,17 +3,32 @@ package core.basesyntax;
 import java.time.LocalDate;
 import javax.xml.crypto.Data;
 import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        StringBuilder result = new StringBuilder("");
+        StringBuilder result = new StringBuilder();
+        final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        int sum = 0;
 
+        result.append("Report for period ")
+                .append(dateFrom)
+                .append(" - ")
+                .append(dateTo)
+                .append(System.getProperty("line.separator"));
         for (String name : names) {
             int bufferSum = 0;
             for (String datum : data) {
                 String[] personalData = datum.split(" ");
-                if (name.equals(personalData[1]) &&)
+                if (name.equals(personalData[1])
+                        && LocalDate.parse(dateFrom, formatter).isBefore(LocalDate.parse(personalData[0], formatter))
+                        && LocalDate.parse(dateTo, formatter).isAfter((LocalDate.parse(personalData[0], formatter)))
+                ) {
+                    sum += Integer.parseInt(personalData[2]) * Integer.parseInt(personalData[3]);
+                }
             }
+            result.append(name).append(" - ").append(sum).append(System.getProperty("line.separator"));
         }
+        return result.toString();
     }
 }
