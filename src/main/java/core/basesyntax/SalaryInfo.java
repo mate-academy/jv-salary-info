d6d2516624f76e2e749a46b50d8f47f2e5bb159c
mishamolnar <michaellmuller423@gmail.com>
2021-08-18 14:50:25 +0300

    build success

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -8,9 +8,10 @@ public class SalaryInfo {
     private static final int NAME_INDEX = 1;
     private static final int HOURS_INFO = 2;
     private static final int PRICE_PER_HOUR_INFO = 3;
+    private static final int DIFFERENCE = 1;
+    private static String FORMATTER = "dd.MM.yyyy";
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        String FORMATTER = "dd.MM.yyyy";
         StringBuilder result = new StringBuilder();
         final DateTimeFormatter formatter = DateTimeFormatter.ofPattern(FORMATTER);
 
@@ -39,13 +40,15 @@ public class SalaryInfo {
         return result.toString().trim();
     }
 
-
-    public boolean isValidDate(String dateFrom, String[] personalData, String dateTo, String FORMATTER) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(FORMATTER);
-        int DIFFERENCE = 1;
-        return LocalDate.parse(dateFrom, DateTimeFormatter.ofPattern(FORMATTER))
-                .isBefore(LocalDate.parse(personalData[DATE_INDEX], DateTimeFormatter.ofPattern(FORMATTER)))
-                && LocalDate.parse(dateTo, DateTimeFormatter.ofPattern(FORMATTER))
-                .plusDays(DIFFERENCE).isAfter((LocalDate.parse(personalData[DATE_INDEX], DateTimeFormatter.ofPattern(FORMATTER))));
+    public boolean isValidDate(String dateFrom,
+                               String[] personalData,
+                               String dateTo,
+                               String formatter) {
+        return LocalDate.parse(dateFrom, DateTimeFormatter.ofPattern(formatter))
+                .isBefore(LocalDate.parse(
+                        personalData[DATE_INDEX], DateTimeFormatter.ofPattern(formatter)))
+                && LocalDate.parse(dateTo, DateTimeFormatter.ofPattern(formatter))
+                .plusDays(DIFFERENCE).isAfter((LocalDate.parse(
+                        personalData[DATE_INDEX], DateTimeFormatter.ofPattern(formatter))));
     }
 }
