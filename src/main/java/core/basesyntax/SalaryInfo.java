ef8aee8dbe2c4a7ff639f576ea7dfdf3b399fcdc
Maksym Mazlov <mazlovmaxim@gmail.com>
2021-08-19 23:56:05 +0300

    implemented method getSalaryInfo (calculate salary for employees)

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,40 @@
 package core.basesyntax;
 
+import core.basesyntax.utils.ParserStringDateToLocalDate;
+import java.time.LocalDate;
+
 public class SalaryInfo {
+    private final ParserStringDateToLocalDate parser = new ParserStringDateToLocalDate();
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        StringBuilder builder = new StringBuilder();
+        int salaryFull = 0;
+        for (String name : names) {
+            for (int i = 0; i < data.length; i++) {
+                String[] partsData = data[i].split(" ");
+
+                LocalDate dateStart = parser.stringToLocalDate(dateFrom);
+                LocalDate dateData = parser.stringToLocalDate(partsData[0]);
+                LocalDate dateEnd = parser.stringToLocalDate(dateTo);
+
+                if (name.equals(partsData[1])) {
+                    if ((dateStart.isBefore(dateData)) || (dateStart.isEqual(dateData))) {
+                        if ((dateEnd.isAfter(dateData)) || (dateEnd.isEqual(dateData))) {
+                            if (partsData[1].equals(name)) {
+                                int hours = Integer.parseInt(partsData[2]);
+                                int salary = Integer.parseInt(partsData[3]);
+                                salaryFull += hours * salary;
+                            }
+                        }
+                    }
+                }
+            }
+            builder.append(System.lineSeparator());
+            builder.append(name);
+            builder.append(" - ");
+            builder.append(salaryFull);
+            salaryFull = 0;
+        }
+        return "Report for period " + dateFrom + " - " + dateTo + builder;
     }
 }
---
+++ src/main/java/core/basesyntax/utils/ParserStringDateToLocalDate.java
@@ -0,0 +1,12 @@
+package core.basesyntax.utils;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class ParserStringDateToLocalDate {
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
+    public LocalDate stringToLocalDate(String dateString) {
+        return LocalDate.parse(dateString, FORMATTER);
+    }
+}
