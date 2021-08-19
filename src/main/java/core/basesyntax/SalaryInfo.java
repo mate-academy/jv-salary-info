670cb24194f79704630f1e68dc219bfc0756fb3c
Kostia Dementiev <konstde00@gmail.com>
2021-08-19 17:39:18 +0300

    added required constants\

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,27 +4,27 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    static final int DATEINDEX = 0;
+    static final int NAMEINDEX = 1;
+    static final int HOURSPOSITION = 2;
+    static final int SALARYPOSITION = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append("Report for period ").append(dateFrom).append(" - ").append(dateTo)
                 .append(System.lineSeparator());
         for (String name : names) {
-            final int dateIndex = 0;
-            final int nameIndex = 1;
-            final int hoursPosition = 2;
-            final int salaryPosition = 3;
             int salarySum = 0;
             for (String datum : data) {
                 String[] splittedDate = datum.split(" ");
-                if (name.equals(splittedDate[nameIndex])
-                        && !LocalDate.parse(splittedDate[dateIndex], formatter)
-                        .isBefore(LocalDate.parse(dateFrom, formatter))
-                        && !LocalDate.parse(splittedDate[dateIndex], formatter)
-                        .isAfter(LocalDate.parse(dateTo, formatter))) {
-                    int hours = Integer.parseInt(splittedDate[hoursPosition]);
-                    int salary = Integer.parseInt(splittedDate[salaryPosition]);
+                if (name.equals(splittedDate[NAMEINDEX])
+                        && !LocalDate.parse(splittedDate[DATEINDEX], FORMATTER)
+                        .isBefore(LocalDate.parse(dateFrom, FORMATTER))
+                        && !LocalDate.parse(splittedDate[DATEINDEX], FORMATTER)
+                        .isAfter(LocalDate.parse(dateTo, FORMATTER))) {
+                    int hours = Integer.parseInt(splittedDate[HOURSPOSITION]);
+                    int salary = Integer.parseInt(splittedDate[SALARYPOSITION]);
                     salarySum += (hours * salary);
                 }
             }
