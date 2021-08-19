3c6c0fbc2ad34e8e7f665a5df37459c239e72021
Kostia Dementiev <konstde00@gmail.com>
2021-08-19 22:45:43 +0300

    implemented date validation method

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -18,11 +18,8 @@ public class SalaryInfo {
             int salarySum = 0;
             for (String datum : data) {
                 String[] splittedDate = datum.split(" ");
-                if (name.equals(splittedDate[NAMEINDEX])
-                        && !LocalDate.parse(splittedDate[DATEINDEX], FORMATTER)
-                        .isBefore(LocalDate.parse(dateFrom, FORMATTER))
-                        && !LocalDate.parse(splittedDate[DATEINDEX], FORMATTER)
-                        .isAfter(LocalDate.parse(dateTo, FORMATTER))) {
+                if (isValidDate(name, dateFrom, splittedDate,
+                        DATEINDEX, dateTo, FORMATTER)) {
                     int hours = Integer.parseInt(splittedDate[HOURSPOSITION]);
                     int salary = Integer.parseInt(splittedDate[SALARYPOSITION]);
                     salarySum += (hours * salary);
@@ -32,5 +29,18 @@ public class SalaryInfo {
                     .append(System.lineSeparator());
         }
         return stringBuilder.toString().trim();
+    }
+
+    public boolean isValidDate(String name, String dateFrom, String[] splittedDate,
+                               int DATEINDEX, String dateTo, DateTimeFormatter FORMATTER) {
+        if (name.equals(splittedDate[NAMEINDEX])
+                && !LocalDate.parse(splittedDate[DATEINDEX], FORMATTER)
+                .isBefore(LocalDate.parse(dateFrom, FORMATTER))
+                && !LocalDate.parse(splittedDate[DATEINDEX], FORMATTER)
+                .isAfter(LocalDate.parse(dateTo, FORMATTER))) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
