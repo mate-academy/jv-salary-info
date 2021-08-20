eacf06141c2de0aeac8164403191e9c674dcac07
Yehor Zapara <egor.zapara1994@gmail.com>
2021-08-20 13:01:34 +0300

    constants was added

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -6,6 +6,10 @@ import java.time.format.DateTimeParseException;
 
 public class SalaryInfo {
     public static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    public static final int POSITION_OF_DATE = 0;
+    public static final int POSITION_OF_NAME = 1;
+    public static final int POSITION_OF_HOURS = 2;
+    public static final int POSITION_OF_INCOME_PER_HOUR = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder stringBuilder = new StringBuilder("Report for period ")
@@ -17,11 +21,12 @@ public class SalaryInfo {
                 int salary = 0;
                 for (int j = 0; j < data.length; j++) {
                     String[] dataOfDay = data[j].split("\\s");
-                    LocalDate localDate = LocalDate.parse(dataOfDay[0], DATE_FORMAT);
-                    if (names[i].equals(dataOfDay[1])
+                    LocalDate localDate = LocalDate.parse(dataOfDay[POSITION_OF_DATE], DATE_FORMAT);
+                    if (names[i].equals(dataOfDay[POSITION_OF_NAME])
                             && !localDate.isBefore(dateFromFormatted)
                             && !localDate.isAfter(dateToFormatted)) {
-                        salary += Integer.parseInt(dataOfDay[2]) * Integer.parseInt(dataOfDay[3]);
+                        salary += Integer.parseInt(dataOfDay[POSITION_OF_HOURS])
+                                * Integer.parseInt(dataOfDay[POSITION_OF_INCOME_PER_HOUR]);
                     }
                 }
                 stringBuilder.append(System.lineSeparator())
