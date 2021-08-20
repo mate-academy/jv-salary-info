fda13fea861c34eced3033e1f97cfa5b22be37aa
dogovor <dogovor@lan.com.ua>
2021-08-20 19:54:25 +0300

    parsed string to date and implemented method

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,36 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+    private final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        StringBuilder stringBuilder = new StringBuilder();
+        LocalDate beginPeriod = LocalDate.parse(dateFrom, dateTimeFormatter);
+        LocalDate endPeriod = LocalDate.parse(dateTo, dateTimeFormatter);
+        stringBuilder.append("Report for period ")
+                    .append(dateFrom).append(" - ")
+                    .append(dateTo)
+                    .append(System.lineSeparator());
+        for (String name : names) {
+            int salary = 0;
+            for (String datum : data) {
+                String[] partsOfData = datum.split("\\s");
+                if (partsOfData[1].equals(name)
+                            && LocalDate.parse(partsOfData[0],
+                            dateTimeFormatter).isBefore(endPeriod.plusDays(1))
+                            && LocalDate.parse(partsOfData[0],
+                            dateTimeFormatter).isAfter(beginPeriod.minusDays(1))) {
+                    salary += (Integer.parseInt(partsOfData[2]) * Integer.parseInt(partsOfData[3]));
+                }
+            }
+            stringBuilder.append(name)
+                        .append(" - ")
+                        .append(salary)
+                        .append(System.lineSeparator());
+        }
+        return stringBuilder.toString().trim();
     }
 }
