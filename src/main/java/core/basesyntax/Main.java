75cc6fac1fbd886a7e411f221ac7f1cb02ab1a06
Vitalii Mykh <vitaliymykh@gmail.com>
2021-08-20 19:20:24 +0300

    created method which gather salary info from data array, based on name and date;

---
+++ src/main/java/core/basesyntax/Main.java
@@ -0,0 +1,17 @@
+package core.basesyntax;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] names = new String[]{ "John", "Andrew", "Kate" };
+        String[] data = new String[]{ "26.04.2019 John 4 50",
+                "05.04.2019 Andrew 3 200",
+                "10.04.2019 John 7 100",
+                "22.04.2019 Kate 7 100",
+                "25.06.2019 John 11 50" };
+        String dateFrom = "01.04.2019";
+        String dateTo = "30.04.2019";
+
+        SalaryInfo salaryInfo = new SalaryInfo();
+        System.out.println(salaryInfo.getSalaryInfo(names,data,dateFrom, dateTo));
+    }
+}
--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,32 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        DateTimeFormatter formater = DateTimeFormatter.ofPattern("d.MM.yyyy");
+        LocalDate fromDate = LocalDate.parse(dateFrom, formater);
+        LocalDate toDate = LocalDate.parse(dateTo, formater);
+        String result = "Report for period " + dateFrom + " - " + dateTo + "\r\n";
+        StringBuilder stringBuilder = new StringBuilder();
+        int[] salaryArr = new int[names.length];
+        for (int i = 0; i < names.length; i++) {
+            for (int j = 0; j < data.length; j++) {
+                if (data[j].contains(names[i])) {
+                    String [] lineOfData = data[j].split(" ");
+                    LocalDate day = LocalDate.parse(lineOfData[0],formater);
+                    if (!day.isBefore(fromDate) && !day.isAfter(toDate)) {
+                        String[] dataLineArr = data[j].split(" ");
+                        int hours = Integer.parseInt(dataLineArr[2]);
+                        int money = Integer.parseInt(dataLineArr[3]);
+                        salaryArr[i] = salaryArr[i] + hours * money;
+                    }
+                }
+            }
+            stringBuilder.append(names[i]).append(" - ").append(salaryArr[i]).append("\r\n");
+        }
+        result = result + stringBuilder.toString();
+        return result.substring(0, result.length() - 2);
     }
 }
