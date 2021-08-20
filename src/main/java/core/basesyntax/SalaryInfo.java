6d846a65b4d1dbb7a98833b31c555cbacdb23f1b
Vladyslav Gural <vladgural@gmail.com>
2021-08-20 12:36:17 +0300

    removed DateTimeFormatter to field class

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,26 +4,25 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private static final String EUROPEAN_FORMAT = "dd.MM.yyyy";
+    private static final DateTimeFormatter EUROPEAN_FORMAT = DateTimeFormatter
+            .ofPattern("dd.MM.yyyy");
     private static final int CURRENT_DATE = 0;
     private static final int NAME = 1;
     private static final int HOURS = 2;
     private static final int INCOME_PER_HOUR = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        final DateTimeFormatter eurFormat = DateTimeFormatter
-                .ofPattern(EUROPEAN_FORMAT);
         String[] compareData;
         int[] countSalaries = new int[names.length];
         LocalDate currentDate;
-        LocalDate fromDate = LocalDate.parse(dateFrom, eurFormat);
-        LocalDate toDate = LocalDate.parse(dateTo, eurFormat);
+        LocalDate fromDate = LocalDate.parse(dateFrom, EUROPEAN_FORMAT);
+        LocalDate toDate = LocalDate.parse(dateTo, EUROPEAN_FORMAT);
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append("Report for period ").append(dateFrom)
                 .append(" - ").append(dateTo).append(System.lineSeparator());
         for (String splitedDate : data) {
             compareData = splitedDate.split(" ");
-            currentDate = LocalDate.parse(compareData[CURRENT_DATE], eurFormat);
+            currentDate = LocalDate.parse(compareData[CURRENT_DATE], EUROPEAN_FORMAT);
             for (int j = 0; j < names.length; j++) {
                 if (fromDate.compareTo(currentDate) <= 0
                         && toDate.compareTo(currentDate) >= 0
