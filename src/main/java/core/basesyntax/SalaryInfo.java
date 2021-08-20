ed4bc58b7476634d28c190ba46ea34e8e1a699f8
Roman Legendzevych <green3942@gmail.com>
2021-08-20 11:52:11 +0300

    fixed problems after mentor review

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,33 +4,33 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private static final int DATE = 0;
-    private static final int NAME = 1;
-    private static final int MULTIPLE = 2;
-    private static final int SALARY = 3;
-    private static final DateTimeFormatter DATE_COUNT = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int MULTIPLE_INDEX = 2;
+    private static final int SALARY_INDEX = 3;
+    private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        StringBuilder printer = new StringBuilder();
-        printer.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
+        StringBuilder reportBuilder = new StringBuilder();
+        reportBuilder.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
         for (String name : names) {
             int money = 0;
             for (String checkData : data) {
-                String[] dates = checkData.split(" ");
-                String date = dates[DATE];
-                String checkName = dates[NAME];
-                int multiple = Integer.parseInt(dates[MULTIPLE]);
-                int salary = Integer.parseInt(dates[SALARY]);
-                LocalDate start = LocalDate.parse(dateFrom, DATE_COUNT).minusDays(1);
-                LocalDate process = LocalDate.parse(date, DATE_COUNT);
-                LocalDate finish = LocalDate.parse(dateTo, DATE_COUNT).plusDays(1);
+                String[] rowData = checkData.split(" ");
+                String date = rowData[DATE_INDEX];
+                String checkName = rowData[NAME_INDEX];
+                int multiple = Integer.parseInt(rowData[MULTIPLE_INDEX]);
+                int salary = Integer.parseInt(rowData[SALARY_INDEX]);
+                LocalDate start = LocalDate.parse(dateFrom, DATE_FORMAT).minusDays(1);
+                LocalDate process = LocalDate.parse(date, DATE_FORMAT);
+                LocalDate finish = LocalDate.parse(dateTo, DATE_FORMAT).plusDays(1);
                 if (name.equals(checkName)
-                        && start.isBefore(process) & process.isBefore(finish)) {
+                        && start.isBefore(process) && process.isBefore(finish)) {
                     money += salary * multiple;
                 }
             }
-            printer.append(System.lineSeparator()).append(name).append(" - ").append(money);
+            reportBuilder.append(System.lineSeparator()).append(name).append(" - ").append(money);
         }
-        return printer.toString();
+        return reportBuilder.toString();
     }
 }
