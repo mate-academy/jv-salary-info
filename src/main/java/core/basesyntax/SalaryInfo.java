d70ad8ac0cc4befc257d76d4f3c2703eaec6af1e
Sasha Hordenin <sg743148@gmail.com>
2021-08-20 14:19:31 +0300

    realized jv-salary-info App

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,38 @@
 package core.basesyntax;
 
+import static java.time.LocalDate.parse;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        StringBuilder reportBuilder = new StringBuilder();
+        reportBuilder.append("Report for period ").append(dateFrom).append(" - ")
+                .append(dateTo).append(System.lineSeparator());
+        LocalDate startDate = parseStringToDate(dateFrom);
+        LocalDate endDate = parseStringToDate(dateTo);
+        for (String employeeName : names) {
+            reportBuilder.append(employeeName).append(" - ");
+            int salaryOfEmployee = 0;
+            for (String employeeInfo : data) {
+                String[] employeeInfoArray = employeeInfo.split(" ");
+                LocalDate dateOfDataArray = parseStringToDate(employeeInfoArray[0]);
+                if (employeeName.equals(employeeInfoArray[1])
+                        && dateOfDataArray.isAfter(startDate)
+                        && dateOfDataArray.isBefore(endDate.plusDays(1))) {
+                    salaryOfEmployee += Integer.parseInt(employeeInfoArray[2])
+                            * Integer.parseInt(employeeInfoArray[3]);
+                }
+            }
+            reportBuilder.append(salaryOfEmployee).append(System.lineSeparator());
+        }
+        return reportBuilder.toString().trim();
+    }
+
+    private LocalDate parseStringToDate(String date) {
+        return parse(date, FORMATTER);
     }
 }
