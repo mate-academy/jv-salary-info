58c6d6331554dd4cf2c4d3a198ad6e809ed36a18
SergiiLonskyi <sergii.lonskyi@gmail.com>
2021-08-20 22:30:03 +0300

    implement getSalaryInfo method, corrected the constant

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -6,7 +6,7 @@ import java.time.format.DateTimeFormatter;
 public class SalaryInfo {
     public static final DateTimeFormatter DATE_TIME_FORMATTER =
             DateTimeFormatter.ofPattern("dd.MM.yyyy");
-    public static final int DATA_INDEX = 0;
+    public static final int DATE_INDEX = 0;
     public static final int NAME_OF_EMPLOYEE = 1;
     public static final int WORKING_HOURS = 2;
     public static final int MONEY_PER_HOUR = 3;
@@ -25,7 +25,7 @@ public class SalaryInfo {
             int totalSalary = 0;
             for (String datum : data) {
                 String[] infoArray = datum.split(" ");
-                LocalDate currentDate = LocalDate.parse(infoArray[DATA_INDEX],
+                LocalDate currentDate = LocalDate.parse(infoArray[DATE_INDEX],
                         DATE_TIME_FORMATTER);
                 if ((nameOfEmployee.equals(infoArray[NAME_OF_EMPLOYEE]))
                         && currentDate.isAfter(localDateFrom)
