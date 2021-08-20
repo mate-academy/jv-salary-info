9b4b399d191d5ecf555f7520810141faf411a427
Kostia Dementiev <konstde00@gmail.com>
2021-08-20 13:02:00 +0300

    fixed access modifiers

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,11 +4,11 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
     static final int DATE_INDEX = 0;
     static final int NAME_INDEX = 1;
     static final int HOURSPOSITION = 2;
     static final int SALARYPOSITION = 3;
+    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder stringBuilder = new StringBuilder();
