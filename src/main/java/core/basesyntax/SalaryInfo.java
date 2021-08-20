41552fdd5e431e84e4a393ce935e50c4eda9ebaf
Vitalii Mykh <vitaliymykh@gmail.com>
2021-08-20 19:46:11 +0300

    created method which gather salary info, based on name and date;

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -2,12 +2,23 @@ package core.basesyntax;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         DateTimeFormatter formater = DateTimeFormatter.ofPattern("d.MM.yyyy");
-        LocalDate fromDate = LocalDate.parse(dateFrom, formater);
-        LocalDate toDate = LocalDate.parse(dateTo, formater);
+        LocalDate fromDate;
+        LocalDate toDate;
+        try {
+            fromDate = LocalDate.parse(dateFrom, formater);
+        } catch (DateTimeParseException e) {
+            throw new RuntimeException("Exception while parsing string to date ", e);
+        }
+        try {
+            toDate = LocalDate.parse(dateTo, formater);
+        } catch (DateTimeParseException e) {
+            throw new RuntimeException("Exception while parsing string to date ", e);
+        }
         String result = "Report for period " + dateFrom + " - " + dateTo + "\r\n";
         StringBuilder stringBuilder = new StringBuilder();
         int[] salaryArr = new int[names.length];
@@ -15,7 +26,12 @@ public class SalaryInfo {
             for (int j = 0; j < data.length; j++) {
                 if (data[j].contains(names[i])) {
                     String [] lineOfData = data[j].split(" ");
-                    LocalDate day = LocalDate.parse(lineOfData[0],formater);
+                    LocalDate day;
+                    try {
+                        day = LocalDate.parse(lineOfData[0],formater);
+                    } catch (DateTimeParseException e) {
+                        throw new RuntimeException("Exception while parsing string to date ", e);
+                    }
                     if (!day.isBefore(fromDate) && !day.isAfter(toDate)) {
                         String[] dataLineArr = data[j].split(" ");
                         int hours = Integer.parseInt(dataLineArr[2]);
@@ -30,6 +46,6 @@ public class SalaryInfo {
             return "String is null";
         }
         result = result + stringBuilder.toString();
-        return result.substring(0, result.length() - 1);
+        return result.substring(0, result.length() - 2);
     }
 }
