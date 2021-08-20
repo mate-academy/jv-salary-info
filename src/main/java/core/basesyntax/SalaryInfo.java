942e01e2a6e1b56073bf8ae757cea9bc4a31fbd6
Vladyslav Gural <vladgural@gmail.com>
2021-08-20 12:13:05 +0300

    implemented DateTimeFormatter in Method getSalaryInfo

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,33 +1,28 @@
 package core.basesyntax;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    private static final String EUROPEAN_FORMAT = "dd.MM.yyyy";
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
+        final DateTimeFormatter eurFormat = DateTimeFormatter
+                .ofPattern(EUROPEAN_FORMAT);
         String[] compareData;
-        String[] localdates;
         int[] countSalaries = new int[names.length];
-        LocalDate compareDate;
-        localdates = dateFrom.split("\\.");
-        LocalDate fromDate = LocalDate.of(Integer.parseInt(localdates[2]),
-                Integer.parseInt(localdates[1]),
-                Integer.parseInt(localdates[0]));
-        localdates = dateTo.split("\\.");
-        LocalDate toDate = LocalDate.of(Integer.parseInt(localdates[2]),
-                Integer.parseInt(localdates[1]),
-                Integer.parseInt(localdates[0]));
+        LocalDate currentDate;
+        LocalDate fromDate = LocalDate.parse(dateFrom, eurFormat);
+        LocalDate toDate = LocalDate.parse(dateTo, eurFormat);
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append("Report for period ").append(dateFrom)
                 .append(" - ").append(dateTo).append(System.lineSeparator());
         for (String splitedDate : data) {
             compareData = splitedDate.split(" ");
-            localdates = compareData[0].split("\\.");
-            compareDate = LocalDate.of(Integer.parseInt(localdates[2]),
-                    Integer.parseInt(localdates[1]),
-                    Integer.parseInt(localdates[0]));
+            currentDate = LocalDate.parse(compareData[0], eurFormat);
             for (int j = 0; j < names.length; j++) {
-                if (fromDate.compareTo(compareDate) <= 0
-                        && toDate.compareTo(compareDate) >= 0
+                if (fromDate.compareTo(currentDate) <= 0
+                        && toDate.compareTo(currentDate) >= 0
                         && names[j].equals(compareData[1])) {
                     countSalaries[j] += Integer.parseInt(compareData[2])
                             * Integer.parseInt(compareData[3]);
