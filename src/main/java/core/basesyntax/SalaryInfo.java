1ee61fef571812765a8690fe4b9f60e12d62441d
Roman Kozhevnikow <roman.kozhevnikow@gmail.com>
2021-08-21 20:26:13 +0300

    Implemented getSalaryInfo method

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,30 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        final DateTimeFormatter f = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        final LocalDate start = LocalDate.parse(dateFrom, f).minusDays(1);
+        final LocalDate end = LocalDate.parse(dateTo, f).plusDays(1);
+        int d = 0;
+        StringBuilder builder = new StringBuilder();
+        builder.append("Report for period ").append(dateFrom).append(" - ")
+                .append(dateTo).append(System.lineSeparator());
+        for (String name : names) {
+            for (String datum : data) {
+                String[] splitArray = datum.split(" ");
+                if (splitArray[1].equals(name)
+                        && (LocalDate.parse(splitArray[0], f).isAfter(start)
+                        && LocalDate.parse(splitArray[0], f).isBefore(end))) {
+                    d += Integer.parseInt(splitArray[2]) * Integer.parseInt(splitArray[3]);
+                }
+            }
+            builder.append(name).append(" - ")
+                    .append(d).append(System.lineSeparator());
+            d = 0;
+        }
+        return builder.toString().trim();
     }
 }
