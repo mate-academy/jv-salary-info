b2a9473482bbb0fdc67ffe3b9e5c64de90992fa9
P.Zbarazkyy <mtu09@ukr.net>
2021-08-21 21:34:41 +0300

    Implemented the getSalaryInfo method in the SalaryInfo class. It accepts two String dates from which period to build salary data, it also accepts an array of String salary data (date, name, working time and price per hour) and accepts an array of Names from whom to calculate.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,40 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private Integer salary;
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        StringBuilder salaryInfo = new StringBuilder();
+        salaryInfo.append("Report for period ");
+        salaryInfo.append(dateFrom);
+        salaryInfo.append(" - ");
+        salaryInfo.append(dateTo);
+        salaryInfo.append(System.lineSeparator());
+        LocalDate localDateFrom = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate localDateTo = LocalDate.parse(dateTo, FORMATTER);
+        for (String iterationNames : names) {
+            salary = 0;
+            for (String iterationDate : data) {
+                String[] splitData = iterationDate.split(" ");
+                if (iterationNames.equals(splitData[1])) {
+                    LocalDate localDate = LocalDate.parse(splitData[0], FORMATTER);
+                    if ((localDate.isAfter(localDateFrom) && localDate.isBefore(localDateTo))
+                            || localDate.isEqual(localDateFrom) || localDate.isEqual(localDateTo)) {
+                        this.salary +=
+                                Integer.parseInt(splitData[2]) * Integer.parseInt(splitData[3]);
+                    }
+                }
+            }
+            salaryInfo.append(iterationNames);
+            salaryInfo.append(" - ");
+            salaryInfo.append(this.salary);
+            salaryInfo.append(System.lineSeparator());
+
+        }
+        return salaryInfo.toString().trim();
     }
 }
