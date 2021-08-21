ff48ae213a26db066a978f8dc9bd57512c4d3b71
Roman Radikhovskiy <radihovskiyroman@gmail.com>
2021-08-22 00:57:14 +0300

    Improved for reading

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,28 +5,26 @@ import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-    private static final int INDEX3 = 3;
-    private static final int INDEX2 = 2;
-    private static final int INDEX1 = 1;
-    private static final int INDEX0 = 0;
+    private static final int RATE = 3;
+    private static final int HOURS = 2;
+    private static final int NAME = 1;
+    private static final int DATE = 0;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder builder = new StringBuilder();
         builder.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
-        int salary = 0;
-        for (int name = 0; name < names.length; name++) {
-            for (int i = 0; i < data.length; i++) {
-                if (names[name].equals(data[i].split(" ")[INDEX1])
-                        && LocalDate.parse(data[i].split(" ")[INDEX0], DATE_FORMAT)
+        for (int i = 0, salary = 0; i < names.length; i++, salary = 0) {
+            for (int num = 0; num < data.length; num++) {
+                if (names[i].equals(data[num].split(" ")[NAME])
+                        && LocalDate.parse(data[num].split(" ")[DATE], DATE_FORMAT)
                         .isAfter(LocalDate.parse(dateFrom, DATE_FORMAT))
-                        && (LocalDate.parse(data[i].split(" ")[INDEX0], DATE_FORMAT))
+                        && (LocalDate.parse(data[num].split(" ")[DATE], DATE_FORMAT))
                         .isBefore((LocalDate.parse(dateTo, DATE_FORMAT)).plusDays(1))) {
-                    salary += Integer.parseInt(data[i].split(" ")[INDEX2])
-                            * Integer.parseInt(data[i].split(" ")[INDEX3]);
+                    salary += Integer.parseInt(data[num].split(" ")[HOURS])
+                            * Integer.parseInt(data[num].split(" ")[RATE]);
                 }
             }
-            builder.append("\n").append(names[name]).append(" - ").append(salary);
-            salary = 0;
+            builder.append("\n").append(names[i]).append(" - ").append(salary);
         }
         return builder.toString();
     }
