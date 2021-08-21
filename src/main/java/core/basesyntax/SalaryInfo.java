ef3a7c4d61eaf9c2b4af9a15a86612873d9be62d
Maxim Moshenskiy <osidiv@ukr.net>
2021-08-21 15:11:02 +0300

    fixed bugs

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,14 +4,13 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private static final String FORMATTER = "dd.MM.yyyy";
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
     private static final int DATE_INFO = 0;
     private static final int NAME_INFO = 1;
     private static final int HOURS_INFO = 2;
     private static final int SALARY_FOR_HOURS = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        final DateTimeFormatter formatter = DateTimeFormatter.ofPattern(FORMATTER);
         StringBuilder result = new StringBuilder();
         result.append("Report for period ")
                 .append(dateFrom)
@@ -23,7 +22,7 @@ public class SalaryInfo {
             for (String dataTotal : data) {
                 String[] dataInfo = dataTotal.split(" ");
                 if (name.equals(dataInfo[NAME_INFO])
-                        && isCorrectDate(dateFrom, dataInfo, dateTo, FORMATTER)) {
+                        && isCorrectDate(dateFrom, dataInfo, dateTo)) {
                     totalAmount += Integer.parseInt(dataInfo[HOURS_INFO])
                             * Integer.parseInt(dataInfo[SALARY_FOR_HOURS]);
                 }
@@ -37,14 +36,13 @@ public class SalaryInfo {
     }
 
     public boolean isCorrectDate(String dateFrom,
-                               String[] dataInfo,
-                               String dateTo,
-                               String formatter) {
-        return LocalDate.parse(dateFrom, DateTimeFormatter.ofPattern(formatter))
+                                 String[] dataInfo,
+                                 String dateTo) {
+        return LocalDate.parse(dateFrom, FORMATTER)
                 .isBefore(LocalDate.parse(
-                        dataInfo[DATE_INFO], DateTimeFormatter.ofPattern(formatter)))
-                && LocalDate.parse(dateTo, DateTimeFormatter.ofPattern(formatter))
+                        dataInfo[DATE_INFO], FORMATTER))
+                && LocalDate.parse(dateTo, FORMATTER)
                 .plusDays(1).isAfter((LocalDate.parse(
-                        dataInfo[DATE_INFO], DateTimeFormatter.ofPattern(formatter))));
+                        dataInfo[DATE_INFO], FORMATTER)));
     }
 }
