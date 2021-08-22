b954bc34f541b3415b16da2a6b1c9300641acbbf
Lekhter <lehter319@gmail.com>
2021-08-22 19:55:23 +0300

    Removed redundant if, added magic number to constant, fixed unnecessary "plus one day".

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,29 +5,28 @@ import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     public static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    public static final int FOR_GET_SALARY = 1;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        LocalDate dateFromDateFormat = LocalDate.parse(dateFrom, DATE_FORMAT);
-        LocalDate dateToDateFormat = LocalDate.parse(dateTo, DATE_FORMAT).plusDays(1);
+        LocalDate localDateFrom = LocalDate.parse(dateFrom, DATE_FORMAT);
+        LocalDate localDateTo = LocalDate.parse(dateTo, DATE_FORMAT);
         StringBuilder builder = new StringBuilder("Report for period "
                                                   + dateFrom + " - " + dateTo);
-        int salary = 0;
         for (String name : names) {
+            int salary = 0;
             for (String datum : data) {
                 LocalDate changingDate = LocalDate.parse(
                         datum.substring(0, datum.indexOf(" ")), DATE_FORMAT);
-                if (changingDate.isAfter(dateFromDateFormat)
-                        && changingDate.isBefore(dateToDateFormat)) {
-                    if (datum.contains(name)) {
-                        String[] hoursAndSalary = datum.substring(datum.indexOf(" ") + 1)
-                                                                        .split(" ");
-                        salary += Integer.parseInt(hoursAndSalary[1])
-                                * Integer.parseInt(hoursAndSalary[2]);
-                    }
+                if (changingDate.compareTo(localDateFrom) >= 0
+                        && changingDate.compareTo(localDateTo) <= 0
+                        && datum.contains(name)) {
+                    String[] hoursAndSalary = datum.substring(datum.indexOf(" ") + 1)
+                                                                    .split(" ");
+                    salary += Integer.parseInt(hoursAndSalary[FOR_GET_SALARY])
+                            * Integer.parseInt(hoursAndSalary[FOR_GET_SALARY + 1]);
                 }
             }
             builder.append(System.lineSeparator()).append(name).append(" - ").append(salary);
-            salary = 0;
         }
         return builder.toString();
     }
