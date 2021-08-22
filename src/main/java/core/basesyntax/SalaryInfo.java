24759bc212feb07aa6551bd16e0cc95ccd7ed479
Rostyslav Fostyk <rostyslavfostykjob@gmail.com>
2021-08-22 16:58:08 +0300

    created array rowInData  for splitting data and created constants for array

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -7,6 +7,10 @@ import java.time.format.DateTimeFormatter;
 public class SalaryInfo {
     private static final DateTimeFormatter FORMATTER =
             DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int DATE_FROM_DATA = 0;
+    private static final int NAME_FROM_DATA = 1;
+    private static final int HOURS_FROM_DATA = 2;
+    private static final int SALARY_FROM_DATA = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo)
             throws DateTimeException {
@@ -26,22 +30,20 @@ public class SalaryInfo {
         for (int i = 0; i < names.length; i++) {
             int salary = 0;
             for (int j = 0; j < data.length; j++) {
+                String[] rowInData = data[j].split(" ");
                 try {
-                    dateInArray = LocalDate.parse(data[j].split(" ")[0], FORMATTER);
+                    dateInArray = LocalDate.parse(rowInData[DATE_FROM_DATA], FORMATTER);
                 } catch (DateTimeException e) {
                     throw new DateTimeException("Cannot parse date from data."
                             + "Format of date should be dd.mm.yyyy");
-                } finally {
-                    dateInArray = LocalDate.parse(data[j].split(" ")[0], FORMATTER);
-                    String nameFromData = data[j].split(" ")[1];
-                    int hours = Integer.parseInt(data[j].split(" ")[2]);
-                    int salaryPerHour = Integer.parseInt(data[j].split(" ")[3]);
-
-                    if (nameFromData.equals(names[i])
-                            && (dateInArray.isAfter(startDate) || dateInArray.equals(startDate))
-                            && (dateInArray.isBefore(endDate) || dateInArray.equals(endDate))) {
-                        salary += hours * salaryPerHour;
-                    }
+                }
+                String nameFromData = rowInData[NAME_FROM_DATA];
+                int hours = Integer.parseInt(rowInData[HOURS_FROM_DATA]);
+                int salaryPerHour = Integer.parseInt(rowInData[SALARY_FROM_DATA]);
+                if (nameFromData.equals(names[i])
+                        && (dateInArray.isAfter(startDate) || dateInArray.equals(startDate))
+                        && (dateInArray.isBefore(endDate) || dateInArray.equals(endDate))) {
+                    salary += hours * salaryPerHour;
                 }
             }
             salaryInfo.append(names[i]).append(" - ").append(salary)
