563b397dbb0095b59531f10902752bf2a2087a6b
Andrii Tyndiuk <andriytundyk@gmail.com>
2021-08-22 23:01:11 +0300

    implement method getSalaryInfo + tests

---
+++ src/main/java/core/basesyntax/Main.java
@@ -0,0 +1,16 @@
+package core.basesyntax;
+
+public class Main {
+    public static void main(String[] args) {
+        SalaryInfo salaryInfo = new SalaryInfo();
+        String[]names = new String[]{"John", "Andrew", "Kate"};
+        String[]data = new String[]{"26.04.2019 John 4 50",
+        "05.04.2019 Andrew 3 200", "10.04.2019 John 7 100",
+        "22.04.2019 Kate 9 100", "25.06.2019 John 11 50",
+        "26.04.2019 Andrew 3 150", "13.02.2019 John 7 100",
+        "26.04.2019 Kate 9 100"};
+        String dateFrom = "date from = `01.04.2019 `";
+        String dateTo = " date to = `30.04.2019` ";
+        System.out.println(salaryInfo.getSalaryInfo(names,data, dateFrom,dateTo));
+    }
+}
--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,38 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+    public static final String FORMAT = "dd.MM.yyyy";
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        StringBuilder builder = new StringBuilder();
+        String onlyDateFrom = dateFrom.split("\\s")[3].substring(1, 11);
+        String onlyDateTo = dateTo.split("\\s")[4].substring(1, 11);
+        builder.append("Report for period #").append(onlyDateFrom).append("# - #").append(onlyDateTo).append("#");
+        for(int i = 0; i < names.length; i++ ){
+            int sumSalary = 0;
+            for(int j = 0; j < data.length; j++){
+                String[]elements = data[j].split("\\s");
+                if(names[i].equals(elements[1]) && dateIsInRange(onlyDateFrom, onlyDateTo, elements[0])){
+                    sumSalary += Integer.parseInt(elements[2])
+                            * Integer.parseInt(elements[3]);
+                }
+            }
+            builder.append(System.lineSeparator()).append(names[i]).append(" - ").append(sumSalary);
+        }
+
+        return builder.toString();
+    }
+    private boolean dateIsInRange(String onlyDateFrom, String onlyDateTo, String workDay) {
+        LocalDate from = getDate(onlyDateFrom);
+        LocalDate to = getDate(onlyDateTo);
+        LocalDate day = getDate(workDay);
+        return (from.equals(day) && to.equals(day)) || (day.isBefore(to) && day.isAfter(from));
+
+    }
+    private LocalDate getDate(String date){
+        return LocalDate.parse(date, DateTimeFormatter.ofPattern(FORMAT));
     }
 }
