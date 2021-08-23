737fbafa1b33a525d5f0363d4c7ad15593a1246e
Andruwa808 <legacy112233@gmail.com>
2021-08-23 15:42:42 +0300

    optimized the code

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,23 +4,21 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        final int zeroConstant = 0;
-        final int oneConstant = 1;
-        final int twoConstant = 2;
-        final int threeConstant = 3;
+    private final DateTimeFormatter dataFormat = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private final int zeroConstant = 0;
+    private final int oneConstant = 1;
+    private final int twoConstant = 2;
+    private final int threeConstant = 3;
 
-        LocalDate dateFromConvert = LocalDate.parse(dateFrom,
-                DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-        LocalDate dateToConvert = LocalDate.parse(dateTo,
-                DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
+        LocalDate dateFromConvert = LocalDate.parse(dateFrom, dataFormat);
+        LocalDate dateToConvert = LocalDate.parse(dateTo, dataFormat);
 
         int[] salary = new int[names.length];
 
         for (int i = 0; i < names.length; i++) {
             for (int j = 0; j < data.length; j++) {
-                LocalDate checkDate = LocalDate.parse(data[j].split(" ")[zeroConstant],
-                        DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+                LocalDate checkDate = LocalDate.parse(data[j].split(" ")[zeroConstant], dataFormat);
                 if (!checkDate.isBefore(dateFromConvert) && !checkDate.isAfter(dateToConvert)) {
                     if (data[j].split(" ")[oneConstant].equals(names[i])) {
                         salary[i] += Integer.parseInt(data[j].split(" ")[twoConstant])
