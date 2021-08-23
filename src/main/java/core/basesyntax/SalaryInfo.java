90b06ea9c47ed6645200c8943c09b28420842053
Andruwa808 <legacy112233@gmail.com>
2021-08-23 11:57:01 +0300

    add constants and formatter

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,31 +1,30 @@
 package core.basesyntax;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        String[] dateFromSplit = dateFrom.split("\\.");
-        String[] dateToSplit = dateTo.split("\\.");
+        final int zeroConstant = 0;
+        final int oneConstant = 1;
+        final int twoConstant = 2;
+        final int threeConstant = 3;
 
-        LocalDate dateFromConvert = LocalDate.of(Integer.parseInt(dateFromSplit[2]),
-                Integer.parseInt(dateFromSplit[1]),
-                Integer.parseInt(dateFromSplit[0]));
-        LocalDate dateToConvert = LocalDate.of(Integer.parseInt(dateToSplit[2]),
-                Integer.parseInt(dateToSplit[1]),
-                Integer.parseInt(dateToSplit[0]));
+        LocalDate dateFromConvert = LocalDate.parse(dateFrom,
+                DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        LocalDate dateToConvert = LocalDate.parse(dateTo,
+                DateTimeFormatter.ofPattern("dd.MM.yyyy"));
 
         int[] salary = new int[names.length];
 
         for (int i = 0; i < names.length; i++) {
             for (int j = 0; j < data.length; j++) {
-                LocalDate checkDate =
-                        LocalDate.of(Integer.parseInt(data[j].split(" ")[0].split("\\.")[2]),
-                        Integer.parseInt(data[j].split(" ")[0].split("\\.")[1]),
-                        Integer.parseInt(data[j].split(" ")[0].split("\\.")[0]));
+                LocalDate checkDate = LocalDate.parse(data[j].split(" ")[zeroConstant],
+                        DateTimeFormatter.ofPattern("dd.MM.yyyy"));
                 if (!checkDate.isBefore(dateFromConvert) && !checkDate.isAfter(dateToConvert)) {
-                    if (data[j].split(" ")[1].equals(names[i])) {
-                        salary[i] += Integer.parseInt(data[j].split(" ")[2])
-                                * Integer.parseInt(data[j].split(" ")[3]);
+                    if (data[j].split(" ")[oneConstant].equals(names[i])) {
+                        salary[i] += Integer.parseInt(data[j].split(" ")[twoConstant])
+                                * Integer.parseInt(data[j].split(" ")[threeConstant]);
                     }
                 }
             }
