4cc8425ea6c1252590c84bb8e84539ba8abe1869
Alexandr <netiazhevskyi@gmail.com>
2021-08-23 18:18:12 +0300

    made variables and indexes

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,7 +4,11 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int DAY_INDEX = 2;
+    private static final int SALARY_INDEX = 3;
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder report = new StringBuilder();
@@ -15,19 +19,15 @@ public class SalaryInfo {
         for (String name : names) {
             int totalSalary = 0;
             for (String datum : data) {
-                if (name.equals(datum.substring(11, 11 + name.length()))) {
-                    if (LocalDate.parse(datum.substring(0, 10), formatter)
-                            .isAfter(formatDateFrom)
-                            && LocalDate.parse(datum.substring(0, 10), formatter)
-                            .isBefore(formatDateTo)
-                            || LocalDate.parse(datum.substring(0, 10), formatter)
-                            .isEqual(formatDateFrom)
-                            || LocalDate.parse(datum.substring(0, 10), formatter)
-                            .isEqual(formatDateTo)) {
-                        int days = Integer.parseInt(datum.substring(11 + name.length() + 1,
-                                datum.lastIndexOf(" ")));
-                        int salary = Integer.parseInt(datum.substring(datum.lastIndexOf(" ") + 1));
-                        totalSalary += days * salary;
+                String[] temporary = datum.split(" ");
+                LocalDate dateFromRow = LocalDate.parse(temporary[DATE_INDEX], formatter);
+                if (name.equals(temporary[NAME_INDEX])) {
+                    if (dateFromRow.isAfter(formatDateFrom)
+                            && dateFromRow.isBefore(formatDateTo)
+                            || dateFromRow.isEqual(formatDateFrom)
+                            || dateFromRow.isEqual(formatDateTo)) {
+                        totalSalary += Integer.parseInt(temporary[DAY_INDEX])
+                                * Integer.parseInt(temporary[SALARY_INDEX]);
                     }
                 }
             }
