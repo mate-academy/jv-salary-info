a597f55ccf9d57673c9f5189ee03039313ee609a
Maxim Moshenskiy <osidiv@ukr.net>
2021-08-23 15:40:28 +0300

    fixed bugs

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -21,11 +21,12 @@ public class SalaryInfo {
             int money = 0;
             for (String dataInf : data) {
                 String[] dataInfo = dataInf.split(" ");
-                LocalDate start = LocalDate.parse(dateFrom, FORMATTER).minusDays(1);
-                LocalDate process = LocalDate.parse(dataInfo[DATE_INDEX], FORMATTER);
-                LocalDate finish = LocalDate.parse(dateTo, FORMATTER).plusDays(1);
-                if (name.equals(dataInfo[NAME_INDEX])
-                        && process.isAfter(start) && process.isBefore(finish)) {
+                LocalDate startDate = LocalDate.parse(dateFrom, FORMATTER);
+                LocalDate dateToProcess = LocalDate.parse(dataInfo[DATE_INDEX], FORMATTER);
+                LocalDate finishDate = LocalDate.parse(dateTo, FORMATTER);
+                if (name.equals(dataInfo[NAME_INDEX]) && (dateToProcess.equals(startDate)
+                        || dateToProcess.equals(finishDate) || dateToProcess.isAfter(startDate)
+                        && dateToProcess.isBefore(finishDate))) {
                     money += Integer.parseInt(dataInfo[HOURS_INDEX])
                             * Integer.parseInt(dataInfo[SALARY_PER_HOURS_INDEX]);
                 }
