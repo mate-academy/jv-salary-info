b40af0b686e61e9c3ccdecdbbbfbdd4eb84f84a7
Kristina Voytikh <61868920+kvoytikh@users.noreply.github.com>
2021-08-23 13:52:20 +0300

    references added
    references added

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -8,11 +8,11 @@ public class SalaryInfo {
     private static final int WORKING_HOURS_INDEX = 2;
     private static final int RATE_PER_HOURS_INDEX = 3;
     private static final int EMPLOYEE_NAME_INDEX = 1;
-    private static final int ACTUAL_DATE = 0;
+    private static final int ACTUAL_DATE_INDEX = 0;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("Report for period ")
+        StringBuilder report = new StringBuilder();
+        report.append("Report for period ")
                 .append(dateFrom)
                 .append(" - ")
                 .append(dateTo);
@@ -21,18 +21,18 @@ public class SalaryInfo {
             for (String line : data) {
                 String[] lineWords = line.split(" ");
                 if (lineWords[EMPLOYEE_NAME_INDEX].equals(name)
-                        && isValidDate(dateFrom, dateTo, lineWords[ACTUAL_DATE])) {
+                        && isValidDate(dateFrom, dateTo, lineWords[ACTUAL_DATE_INDEX])) {
                     int hours = Integer.parseInt(lineWords[WORKING_HOURS_INDEX]);
                     int salaryPerHour = Integer.parseInt(lineWords[RATE_PER_HOURS_INDEX]);
                     salary += hours * salaryPerHour;
                 }
             }
-            stringBuilder.append(System.lineSeparator())
+            report.append(System.lineSeparator())
                     .append(name)
                     .append(" - ")
                     .append(salary);
         }
-        return stringBuilder.toString();
+        return report.toString();
     }
 
     public boolean isValidDate(String dateFrom, String dateTo, String actualDate) {
