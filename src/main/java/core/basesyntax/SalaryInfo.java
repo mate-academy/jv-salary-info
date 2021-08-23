c49024266e41741bceb725574213de63356bee94
Alex Hazniuk <ohazniuk@gmail.com>
2021-08-23 10:48:55 +0300

    fixed mistakes

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,10 +4,11 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        DateTimeFormatter format = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        LocalDate parsedMinDate = LocalDate.parse(dateFrom, format);
-        LocalDate parsedMaxDate = LocalDate.parse(dateTo, format);
+        LocalDate parsedMinDate = LocalDate.parse(dateFrom, DATE_FORMAT);
+        LocalDate parsedMaxDate = LocalDate.parse(dateTo, DATE_FORMAT);
         StringBuilder builder = new StringBuilder();
         builder.append("Report for period ")
                 .append(dateFrom)
@@ -16,13 +17,14 @@ public class SalaryInfo {
         for (String name : names) {
             int totalIncome = 0;
             for (String value : data) {
-                String[] valueArr = value.split(" ");
-                DateTimeFormatter formatDateOfValue = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-                LocalDate parsedDateOfValue = LocalDate.parse(valueArr[0], formatDateOfValue);
-                if (parsedDateOfValue.compareTo(parsedMinDate) >= 0
-                        && parsedDateOfValue.compareTo(parsedMaxDate) <= 0
-                        && name.equals(valueArr[1])) {
-                    totalIncome += Integer.parseInt(valueArr[2]) * Integer.parseInt(valueArr[3]);
+                String[] values = value.split(" ");
+                LocalDate parsedDateOfValue = LocalDate.parse(values[0], DATE_FORMAT);
+                if ((parsedDateOfValue.isAfter(parsedMinDate)
+                        || parsedDateOfValue.isEqual(parsedMinDate))
+                        && (parsedDateOfValue.isBefore(parsedMaxDate)
+                        || parsedDateOfValue.isEqual(parsedMaxDate))
+                        && name.equals(values[1])) {
+                    totalIncome += Integer.parseInt(values[2]) * Integer.parseInt(values[3]);
                 }
             }
             builder.append(System.lineSeparator()).append(name).append(" - ").append(totalIncome);
