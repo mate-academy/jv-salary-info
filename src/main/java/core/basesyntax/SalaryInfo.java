23312f4e0e264a8a402a46b1d34b9350628e4083
Roman Radikhovskiy <radihovskiyroman@gmail.com>
2021-08-23 11:18:28 +0300

    mistakes fixed

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -12,19 +12,26 @@ public class SalaryInfo {
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder builder = new StringBuilder();
-        builder.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
+        builder.append("Report for period ")
+                .append(dateFrom)
+                .append(" - ")
+                .append(dateTo);
         for (int i = 0, salary = 0; i < names.length; i++, salary = 0) {
-            for (int num = 0; num < data.length; num++) {
-                if (names[i].equals(data[num].split(" ")[NAME])
-                        && LocalDate.parse(data[num].split(" ")[DATE], DATE_FORMAT)
+            for (int dataElement = 0; dataElement < data.length; dataElement++) {
+                String[] dataArray = data[dataElement].split(" ");
+                if (names[i].equals(dataArray[NAME])
+                        && LocalDate.parse(dataArray[DATE], DATE_FORMAT)
                         .isAfter(LocalDate.parse(dateFrom, DATE_FORMAT))
-                        && (LocalDate.parse(data[num].split(" ")[DATE], DATE_FORMAT))
+                        && (LocalDate.parse(dataArray[DATE], DATE_FORMAT))
                         .isBefore((LocalDate.parse(dateTo, DATE_FORMAT)).plusDays(1))) {
-                    salary += Integer.parseInt(data[num].split(" ")[HOURS])
-                            * Integer.parseInt(data[num].split(" ")[RATE]);
+                    salary += Integer.parseInt(dataArray[HOURS])
+                            * Integer.parseInt(dataArray[RATE]);
                 }
             }
-            builder.append("\n").append(names[i]).append(" - ").append(salary);
+            builder.append(System.lineSeparator())
+                    .append(names[i])
+                    .append(" - ")
+                    .append(salary);
         }
         return builder.toString();
     }
