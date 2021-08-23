8d8511b19daf848e89a2b27d101d8c7fad225b89
Vitalii Mykh <vitaliymykh@gmail.com>
2021-08-23 15:24:52 +0300

    renamed variable, deleted redundant variables

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -6,35 +6,35 @@ import java.time.format.DateTimeFormatter;
 public class SalaryInfo {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
-    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo)
-            throws RuntimeException {
+    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate fromDate = LocalDate.parse(dateFrom, FORMATTER);
         LocalDate toDate = LocalDate.parse(dateTo, FORMATTER);
         String separator = System.lineSeparator();
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("Report for period ").append(dateFrom).append(" - ")
-                .append(dateTo).append(separator);
+        StringBuilder report = new StringBuilder();
+        report.append("Report for period ")
+                .append(dateFrom)
+                .append(" - ")
+                .append(dateTo)
+                .append(separator);
         int[] salaryArr = new int[names.length];
         for (int name = 0; name < names.length; name++) {
             for (int record = 0; record < data.length; record++) {
                 if (data[record].contains(names[name])) {
-                    String [] lineOfData = data[record].split(" ");
-                    LocalDate day = LocalDate.parse(lineOfData[0], FORMATTER);;
+                    LocalDate day = LocalDate.parse((data[record].split(" "))[0], FORMATTER);
                     if (!day.isBefore(fromDate) && !day.isAfter(toDate)) {
-                        String[] dataLineArr = data[record].split(" ");
-                        salaryArr[name] = salaryArr[name] + Integer.parseInt(dataLineArr[2])
-                                * Integer.parseInt(dataLineArr[3]);
+                        salaryArr[name] = salaryArr[name]
+                                    + Integer.parseInt(data[record].split(" ")[2])
+                                    * Integer.parseInt(data[record].split(" ")[3]);
                     }
                 }
             }
             if (name == names.length - 1) {
-                stringBuilder.append(names[name]).append(" - ").append(salaryArr[name]);
+                report.append(names[name]).append(" - ").append(salaryArr[name]);
             } else {
-                stringBuilder.append(names[name]).append(" - ")
+                report.append(names[name]).append(" - ")
                         .append(salaryArr[name]).append(separator);
             }
         }
-        String result = stringBuilder.toString();
-        return result;
+        return report.toString();
     }
 }
