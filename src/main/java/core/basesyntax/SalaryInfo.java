33cce90f2ff8c827e9b1160de34dad3995ee506d
Roman Kozhevnikow <roman.kozhevnikow@gmail.com>
2021-08-23 13:44:35 +0300

    refactored the code

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -10,29 +10,34 @@ public class SalaryInfo {
     private static final int HOURS_INDEX = 2;
     private static final int RATE_INDEX = 3;
     private static final String DATA_SEPARATOR = " ";
+    private static final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern(FORMATTER);
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern(FORMATTER);
-        LocalDate start = LocalDate.parse(dateFrom, dateFormatter).minusDays(1);
-        LocalDate end = LocalDate.parse(dateTo, dateFormatter).plusDays(1);
+        LocalDate dateStart = LocalDate.parse(dateFrom, dateFormatter);
+        LocalDate dateEnd = LocalDate.parse(dateTo, dateFormatter);
         int earnedMoney = 0;
-        StringBuilder builder = new StringBuilder();
-        builder.append("Report for period ").append(dateFrom).append(" - ")
-                .append(dateTo).append(System.lineSeparator());
+        StringBuilder reportBuilder = new StringBuilder();
+        reportBuilder.append("Report for period ")
+                .append(dateFrom)
+                .append(" - ")
+                .append(dateTo)
+                .append(System.lineSeparator());
         for (String name : names) {
             for (String datum : data) {
                 String[] splitArray = datum.split(DATA_SEPARATOR);
+                LocalDate arrayDate = LocalDate.parse(splitArray[DATE_INDEX], dateFormatter);
                 if (splitArray[NAME_INDEX].equals(name)
-                        && (LocalDate.parse(splitArray[DATE_INDEX], dateFormatter).isAfter(start)
-                        && LocalDate.parse(splitArray[DATE_INDEX], dateFormatter).isBefore(end))) {
+                        && (arrayDate.isAfter(dateStart) && !dateEnd.isBefore(arrayDate))) {
                     earnedMoney += Integer.parseInt(splitArray[HOURS_INDEX])
                             * Integer.parseInt(splitArray[RATE_INDEX]);
                 }
             }
-            builder.append(name).append(" - ")
-                    .append(earnedMoney).append(System.lineSeparator());
+            reportBuilder.append(name)
+                    .append(" - ")
+                    .append(earnedMoney)
+                    .append(System.lineSeparator());
             earnedMoney = 0;
         }
-        return builder.toString().trim();
+        return reportBuilder.toString().trim();
     }
 }
