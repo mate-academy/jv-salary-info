8852f3b2c6cbd2b4acb74d58e9d3e42e0831ded2
Yuliia Mysova-Shreder <yuliiamysova@gmail.com>
2021-08-23 13:17:18 +0300

    Constants for variables were created

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -2,44 +2,34 @@ package core.basesyntax;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
 
 public class SalaryInfo {
     private static final DateTimeFormatter TIME_FORMATTER =
             DateTimeFormatter.ofPattern("d.MM.yyyy");
+    private static final int DATE = 0;
+    private static final int EMPLOYEE_NAME = 1;
+    private static final int WORK_HOURS = 2;
+    private static final int SALARY_PER_HOURS = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         String separator = System.lineSeparator();
-        LocalDate localDateFrom;
-        LocalDate localDateTo;
-        LocalDate period;
+        LocalDate localDateFrom = LocalDate.parse(dateFrom, TIME_FORMATTER);
+        LocalDate localDateTo = LocalDate.parse(dateTo, TIME_FORMATTER);
         StringBuilder stringBuilder = new StringBuilder("Report for period ")
-                .append(dateFrom).append(" - ").append(dateTo);
-
-        try {
-            localDateFrom = LocalDate.parse(dateFrom, TIME_FORMATTER);
-        } catch (DateTimeParseException e) {
-            System.out.println(dateFrom + " is not parsable!");
-            throw e;
-        }
-        try {
-            localDateTo = LocalDate.parse(dateTo, TIME_FORMATTER);
-
-        } catch (DateTimeParseException e) {
-            System.out.println(dateTo + " is not parsable!");
-            throw e;
-        }
+                .append(dateFrom)
+                .append(" - ")
+                .append(dateTo);
 
         for (String name : names) {
             int salary = 0;
             for (int j = 0; j < data.length; j++) {
-                String[] arrayFromData = data[j].split(" ");
-                period = LocalDate.parse(arrayFromData[0], TIME_FORMATTER);
-                if ((name.equals(arrayFromData[1]))
+                String[] record = data[j].split(" ");
+                LocalDate period = LocalDate.parse(record[DATE], TIME_FORMATTER);
+                if ((name.equals(record[EMPLOYEE_NAME]))
                         && ((period.isAfter(localDateFrom) && period.isBefore(localDateTo))
                         || period.isEqual(localDateFrom)
                         || period.isEqual(localDateTo))) {
-                    salary += Integer.valueOf(arrayFromData[2]) * Integer.valueOf(arrayFromData[3]);
+                    salary += Integer.valueOf(record[WORK_HOURS]) * Integer.valueOf(record[SALARY_PER_HOURS]);
                 }
             }
             stringBuilder.append(separator).append(name).append(" - ").append(salary);
