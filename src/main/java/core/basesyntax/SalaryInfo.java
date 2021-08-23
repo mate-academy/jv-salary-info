6da1d59465d5346c09ba8b1f39a1d302bac87332
Maxim Moshenskiy <osidiv@ukr.net>
2021-08-23 13:51:54 +0300

    fixed bugs

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,44 +5,36 @@ import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-    private static final int DATE_INFO = 0;
-    private static final int NAME_INFO = 1;
-    private static final int HOURS_INFO = 2;
-    private static final int SALARY_FOR_HOURS = 3;
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int HOURS_INDEX = 2;
+    private static final int SALARY_FOR_HOURS_INDEX = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        StringBuilder result = new StringBuilder();
-        result.append("Report for period ")
+        StringBuilder report = new StringBuilder();
+        report.append("Report for period ")
                 .append(dateFrom)
                 .append(" - ")
                 .append(dateTo)
                 .append(System.lineSeparator());
         for (String name : names) {
-            int totalAmount = 0;
-            for (String dataTotal : data) {
-                String[] dataInfo = dataTotal.split(" ");
-                if (name.equals(dataInfo[NAME_INFO])
-                        && isCorrectDate(dateFrom, dataInfo, dateTo)) {
-                    totalAmount += Integer.parseInt(dataInfo[HOURS_INFO])
-                            * Integer.parseInt(dataInfo[SALARY_FOR_HOURS]);
+            int money = 0;
+            for (String dataInf : data) {
+                String[] dataInfo = dataInf.split(" ");
+                LocalDate start = LocalDate.parse(dateFrom, FORMATTER).minusDays(1);
+                LocalDate process = LocalDate.parse(dataInfo[DATE_INDEX], FORMATTER);
+                LocalDate finish = LocalDate.parse(dateTo, FORMATTER).plusDays(1);
+                if (name.equals(dataInfo[NAME_INDEX])
+                        && process.isAfter(start) && process.isBefore(finish)) {
+                    money += Integer.parseInt(dataInfo[HOURS_INDEX])
+                            * Integer.parseInt(dataInfo[SALARY_FOR_HOURS_INDEX]);
                 }
             }
-            result.append(name)
+            report.append(name)
                     .append(" - ")
-                    .append(totalAmount)
+                    .append(money)
                     .append(System.lineSeparator());
         }
-        return result.toString().trim();
-    }
-
-    public boolean isCorrectDate(String dateFrom,
-                                 String[] dataInfo,
-                                 String dateTo) {
-        return LocalDate.parse(dateFrom, FORMATTER)
-                .isBefore(LocalDate.parse(
-                        dataInfo[DATE_INFO], FORMATTER))
-                && LocalDate.parse(dateTo, FORMATTER)
-                .plusDays(1).isAfter((LocalDate.parse(
-                        dataInfo[DATE_INFO], FORMATTER)));
+        return report.toString().trim();
     }
 }
