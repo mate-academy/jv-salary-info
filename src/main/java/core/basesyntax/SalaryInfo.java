287002904c97d65e253c460a7391cb01c468fb98
Evgen Krasnenkov <krasnenkov.evgen@gmail.com>
2021-08-23 14:49:34 +0300

    successfull variant with constants and unchecked exceptions, suggested changes from Vlad has been implemented

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -2,54 +2,46 @@ package core.basesyntax;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
 
 public class SalaryInfo {
     public static final DateTimeFormatter
-            TIME_FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+            DATE_FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
     public static final int NAME = 1;
     public static final int PERIOD = 0;
     public static final int HOURS = 2;
     public static final int SALARY = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        LocalDate localDateFrom;
-        LocalDate localDateTo;
-        LocalDate localDateData;
+        LocalDate formattedDateFrom;
+        LocalDate formattedDateTo;
+        LocalDate formattedDateData;
 
-        try {
-            localDateFrom = LocalDate.parse(dateFrom, TIME_FORMATTER);
-            localDateTo = LocalDate.parse(dateTo, TIME_FORMATTER);
-        } catch (DateTimeParseException exc) {
-            throw new RuntimeException("Period format for scanning is not valid", exc);
-        }
-        StringBuilder builder = new StringBuilder();
-        builder.append("Report for period ")
+        formattedDateFrom = LocalDate.parse(dateFrom, DATE_FORMATTER);
+        formattedDateTo = LocalDate.parse(dateTo, DATE_FORMATTER);
+        StringBuilder builderReport = new StringBuilder();
+        builderReport.append("Report for period ")
                         .append(dateFrom)
                         .append(" - ")
                         .append(dateTo);
-        long[] countSalary = new long[names.length];
+        long[] salaries = new long[names.length];
         for (int i = 0; i < names.length; i++) {
             for (int j = 0; j < data.length; j++) {
                 String[] lineOfDate = data[j].split(" ");
-                try {
-                    localDateData = LocalDate.parse(lineOfDate[PERIOD], TIME_FORMATTER);
-                } catch (DateTimeParseException exc) {
-                    throw new RuntimeException("Date format of USER is not valid", exc);
-                }
-                if ((localDateData.isEqual(localDateFrom) || localDateData.isAfter(localDateFrom))
-                        && (localDateData.isBefore(localDateTo)
-                        || localDateData.isEqual(localDateTo))
+                formattedDateData = LocalDate.parse(lineOfDate[PERIOD], DATE_FORMATTER);
+                if ((formattedDateData.isEqual(formattedDateFrom)
+                        || formattedDateData.isAfter(formattedDateFrom))
+                        && (formattedDateData.isBefore(formattedDateTo)
+                        || formattedDateData.isEqual(formattedDateTo))
                         && names[i].equals(lineOfDate[NAME])) {
-                    countSalary[i] += Long.parseLong(lineOfDate[HOURS])
+                    salaries[i] += Long.parseLong(lineOfDate[HOURS])
                             * Long.parseLong(lineOfDate[SALARY]);
                 }
             }
-            builder.append(System.lineSeparator())
+            builderReport.append(System.lineSeparator())
                    .append(names[i])
                    .append(" - ")
-                    .append(countSalary[i]);
+                    .append(salaries[i]);
         }
-        return builder.toString();
+        return builderReport.toString();
     }
 }
