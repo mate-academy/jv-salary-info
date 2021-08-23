ad2ca293e8aa219f2a8d3fabcb1c56f9faeb8a63
Lekhter <lehter319@gmail.com>
2021-08-23 13:34:29 +0300

    Changed methods in if statement. Fixed styles mistakes, changed way for parse of LocalDate.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -6,6 +6,7 @@ import java.time.format.DateTimeFormatter;
 public class SalaryInfo {
     public static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("dd.MM.yyyy");
     public static final int FOR_GET_SALARY = 1;
+    public static final int FOR_RECORD = 0;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate localDateFrom = LocalDate.parse(dateFrom, DATE_FORMAT);
@@ -15,10 +16,11 @@ public class SalaryInfo {
         for (String name : names) {
             int salary = 0;
             for (String datum : data) {
-                LocalDate changingDate = LocalDate.parse(
-                        datum.substring(0, datum.indexOf(" ")), DATE_FORMAT);
-                if (changingDate.compareTo(localDateFrom) >= 0
-                        && changingDate.compareTo(localDateTo) <= 0
+                String[] record = datum.split(" ");
+                LocalDate changingDate = LocalDate.parse(record[FOR_RECORD], DATE_FORMAT);
+                if (changingDate.isAfter(localDateFrom)
+                        && (changingDate.isBefore(localDateTo)
+                        || changingDate.isEqual(localDateTo))
                         && datum.contains(name)) {
                     String[] hoursAndSalary = datum.substring(datum.indexOf(" ") + 1)
                                                                     .split(" ");
@@ -26,7 +28,10 @@ public class SalaryInfo {
                             * Integer.parseInt(hoursAndSalary[FOR_GET_SALARY + 1]);
                 }
             }
-            builder.append(System.lineSeparator()).append(name).append(" - ").append(salary);
+            builder.append(System.lineSeparator())
+                    .append(name)
+                    .append(" - ")
+                    .append(salary);
         }
         return builder.toString();
     }
