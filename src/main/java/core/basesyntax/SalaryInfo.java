0e8d55057d29fc1856b16ddcbec17a4b15631ce5
misha8096 <sichmisha@gmail.com>
2021-08-23 11:54:37 +0300

    implemented  method getSalaryInfo() and created new fields, renamed the constants, changed the if-else condition

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,14 +4,13 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private static final String FORMATTER = "dd.MM.yyyy";
-    private static final int DATE_INDEX = 0;
-    private static final int NAME_INDEX = 1;
-    private static final int HOUR_INDEX = 2;
-    private static final int MONEY_INDEX = 3;
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int DATE = 0;
+    private static final int NAME = 1;
+    private static final int HOUR = 2;
+    private static final int MONEY_PER_HOUR = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(FORMATTER);
         StringBuilder result = new StringBuilder();
         result.append("Report for period ")
                 .append(dateFrom)
@@ -23,18 +22,17 @@ public class SalaryInfo {
             int money = 0;
             for (String datum : data) {
                 String[] splitData = datum.split(" ");
-                LocalDate date = LocalDate.parse(splitData[DATE_INDEX], formatter);
-                LocalDate localDateFrom = LocalDate.parse(dateFrom, formatter);
-                LocalDate localDateTo = LocalDate.parse(dateTo, formatter);
-                if (name.equals(splitData[NAME_INDEX])
-                        && date.isAfter(localDateFrom.minusDays(1))
-                        && date.isBefore(localDateTo.plusDays(1))) {
-                    money += Integer.parseInt(splitData[HOUR_INDEX])
-                            * Integer.parseInt(splitData[MONEY_INDEX]);
+                LocalDate date = LocalDate.parse(splitData[DATE], FORMATTER);
+                LocalDate localDateFrom = LocalDate.parse(dateFrom, FORMATTER);
+                LocalDate localDateTo = LocalDate.parse(dateTo, FORMATTER);
+                if (name.equals(splitData[NAME]) && (date.equals(localDateTo)
+                        || date.equals(localDateFrom) || date.isAfter(localDateFrom)
+                        && date.isBefore(localDateTo))) {
+                    money += Integer.parseInt(splitData[HOUR])
+                            * Integer.parseInt(splitData[MONEY_PER_HOUR]);
                 }
             }
-            result
-                    .append(name)
+            result.append(name)
                     .append(" - ")
                     .append(money)
                     .append(System.lineSeparator());
