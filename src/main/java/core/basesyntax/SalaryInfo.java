13ab37bed8d04608ca99ca6e9f6dcb1f4cdf9721
Evgen Krasnenkov <krasnenkov.evgen@gmail.com>
2021-08-23 11:19:02 +0300

    successfull variant with constants and unchecked exceptions

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,7 +5,8 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
 public class SalaryInfo {
-    public static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    public static final DateTimeFormatter
+            TIME_FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
     public static final int NAME = 1;
     public static final int PERIOD = 0;
     public static final int HOURS = 2;
@@ -28,8 +29,8 @@ public class SalaryInfo {
                         .append(" - ")
                         .append(dateTo);
         long[] countSalary = new long[names.length];
-        for (int i = 0; i < names.length; i++){
-            for(int j = 0; j < data.length; j++){
+        for (int i = 0; i < names.length; i++) {
+            for (int j = 0; j < data.length; j++) {
                 String[] lineOfDate = data[j].split(" ");
                 try {
                     localDateData = LocalDate.parse(lineOfDate[PERIOD], TIME_FORMATTER);
@@ -37,15 +38,17 @@ public class SalaryInfo {
                     throw new RuntimeException("Date format of USER is not valid", exc);
                 }
                 if ((localDateData.isEqual(localDateFrom) || localDateData.isAfter(localDateFrom))
-                        && (localDateData.isBefore(localDateTo) || localDateData.isEqual(localDateTo))
+                        && (localDateData.isBefore(localDateTo)
+                        || localDateData.isEqual(localDateTo))
                         && names[i].equals(lineOfDate[NAME])) {
-                    countSalary[i] += Long.parseLong(lineOfDate[HOURS]) * Long.parseLong(lineOfDate[SALARY]);
+                    countSalary[i] += Long.parseLong(lineOfDate[HOURS])
+                            * Long.parseLong(lineOfDate[SALARY]);
                 }
             }
             builder.append(System.lineSeparator())
                    .append(names[i])
                    .append(" - ")
-                   .append(countSalary[i]);
+                    .append(countSalary[i]);
         }
         return builder.toString();
     }
