c90497f013bb9e03fc6411cd97b14478025797bd
pustoj0 <pustoj0@gmail.com>
2021-08-23 10:09:02 +0300

    Corrected naming; Using isEqual; Deleted variable that was not used.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,41 +5,41 @@ import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     private static final DateTimeFormatter FORMAT = DateTimeFormatter.ofPattern("d.MM.yyyy");
-    private static final int DATE_IND = 0;
-    private static final int NAME_IND = 1;
-    private static final int HOUR_QUANTITY_IND = 2;
-    private static final int SALARY_PER_HOUR_IND = 3;
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int HOUR_QUANTITY_INDEX = 2;
+    private static final int SALARY_PER_HOUR_INDEX = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        StringBuilder res = new StringBuilder();
-        res.append("Report for period ")
-                .append(dateFrom).append(" - ")
+        StringBuilder salaryInfo = new StringBuilder();
+        salaryInfo.append("Report for period ")
+                .append(dateFrom)
+                .append(" - ")
                 .append(dateTo)
                 .append(System.lineSeparator());
         int totalSalary;
-        int[] totalMoney = new int[names.length];
         for (String name : names) {
             totalSalary = 0;
             for (String dataLine : data) {
                 String[] dataElements = dataLine.split("\\s");
-                if (name.equals(dataElements[NAME_IND])
-                        && isDateInRange(dataElements[DATE_IND], dateFrom, dateTo)) {
-                    totalSalary += Integer.parseInt(dataElements[HOUR_QUANTITY_IND])
-                            * Integer.parseInt(dataElements[SALARY_PER_HOUR_IND]);
+                if (name.equals(dataElements[NAME_INDEX])
+                        && isDateInRange(dataElements[DATE_INDEX], dateFrom, dateTo)) {
+                    totalSalary += Integer.parseInt(dataElements[HOUR_QUANTITY_INDEX])
+                            * Integer.parseInt(dataElements[SALARY_PER_HOUR_INDEX]);
                 }
             }
-            res.append(name)
+            salaryInfo.append(name)
                     .append(" - ")
                     .append(totalSalary)
                     .append(System.lineSeparator());
         }
-        return res.toString().trim();
+        return salaryInfo.toString().trim();
     }
 
     private boolean isDateInRange(String date, String dateFrom, String dateTo) {
-        return LocalDate.parse(date, FORMAT)
-                .isAfter(LocalDate.parse(dateFrom, FORMAT).minusDays(1))
-                && LocalDate.parse(date, FORMAT)
-                .isBefore(LocalDate.parse(dateTo, FORMAT).plusDays(1));
+        return LocalDate.parse(date, FORMAT).isEqual(LocalDate.parse(dateFrom, FORMAT))
+                || LocalDate.parse(date, FORMAT).isEqual(LocalDate.parse(dateTo, FORMAT))
+                || LocalDate.parse(date, FORMAT).isBefore(LocalDate.parse(dateTo, FORMAT))
+                && LocalDate.parse(date, FORMAT).isAfter(LocalDate.parse(dateFrom, FORMAT));
     }
 }
