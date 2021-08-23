a196bc8cd30b6ec5fe603466c35f5a6245dbbf4d
V <v.ivashchenko42@protonmail.com>
2021-08-23 14:41:51 +0300

    corrected comments from PR

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,33 +5,38 @@ import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     public static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-    private static final int DATE_INDEX = 0;
-    private static final int NAME_INDEX = 1;
-    private static final int HOURS_INDEX = 2;
-    private static final int SALARY_INDEX = 3;
+    private static final int DATE = 0;
+    private static final int NAME = 1;
+    private static final int HOURS = 2;
+    private static final int SALARY = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        int[] salary = new int[names.length];
+        StringBuilder answer = new StringBuilder();
 
-        for (String oneLine : data) {
-            String[] dataArray = oneLine.split(" ");
-            for (int i = 0; i < names.length; i++) {
-                if (!LocalDate.parse(dataArray[DATE_INDEX], DATE_FORMAT)
-                        .isBefore(LocalDate.parse(dateFrom, DATE_FORMAT))
-                        && !LocalDate.parse(dataArray[DATE_INDEX], DATE_FORMAT)
-                        .isAfter(LocalDate.parse(dateTo, DATE_FORMAT))
-                        && dataArray[NAME_INDEX].equals(names[i])) {
-                    salary[i] += Integer.parseInt(dataArray[HOURS_INDEX])
-                            * Integer.parseInt(dataArray[SALARY_INDEX]);
+        answer.append("Report for period ")
+                .append(dateFrom)
+                .append(" - ")
+                .append(dateTo)
+                .append(System.lineSeparator());
+        for (String name : names) {
+            int salary = 0;
+            for (String oneLine : data) {
+                String[] dataArray = oneLine.split(" ");
+                LocalDate date = LocalDate.parse(dataArray[DATE], DATE_FORMAT);
+                if (!date.isBefore(LocalDate.parse(dateFrom, DATE_FORMAT))
+                        && !date.isAfter(LocalDate.parse(dateTo, DATE_FORMAT))
+                        && dataArray[NAME].equals(name)) {
+                    salary += Integer.parseInt(dataArray[HOURS])
+                            * Integer.parseInt(dataArray[SALARY]);
                 }
             }
+            answer.append(name)
+                    .append(" - ")
+                    .append(salary)
+                    .append(System.lineSeparator());
+
         }
-        StringBuilder answer = new StringBuilder();
-        answer.append("Report for period ").append(dateFrom).append(" - ")
-                .append(dateTo);
-        for (int i = 0; i < names.length; i++) {
-            answer.append(System.lineSeparator()).append(names[i]).append(" - ").append(salary[i]);
-        }
-        return answer.toString();
+
+        return answer.toString().trim();
     }
 }
