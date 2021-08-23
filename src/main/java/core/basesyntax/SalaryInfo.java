d41a2e25caa2449e75ca3cdbddf3889d3a929c5f
The Reaper <dimonnumber1@gmail.com>
2021-08-23 14:09:04 +0300

    Update

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -6,13 +6,13 @@ import java.time.format.DateTimeFormatter;
 public class SalaryInfo {
     private static final int DATE_INDEX = 0;
     private static final int NAME_INDEX = 1;
-    private static final int HOURSPOSITION = 2;
-    private static final int SALARYPOSITION = 3;
+    private static final int HOURS_POSITION = 2;
+    private static final int SALARY_POSITION = 3;
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("Report for period ").append(dateFrom).append(" - ").append(dateTo)
+        StringBuilder report = new StringBuilder();
+        report.append("Report for period ").append(dateFrom).append(" - ").append(dateTo)
                 .append(System.lineSeparator());
         for (String name : names) {
             int salarySum = 0;
@@ -20,28 +20,24 @@ public class SalaryInfo {
                 String[] splittedDate = datum.split(" ");
                 if (isValidDate(name, dateFrom, splittedDate,
                         DATE_INDEX, dateTo, FORMATTER)) {
-                    int hours = Integer.parseInt(splittedDate[HOURSPOSITION]);
-                    int salary = Integer.parseInt(splittedDate[SALARYPOSITION]);
+                    int hours = Integer.parseInt(splittedDate[HOURS_POSITION]);
+                    int salary = Integer.parseInt(splittedDate[SALARY_POSITION]);
                     salarySum += (hours * salary);
                 }
             }
-            stringBuilder.append(name).append(" - ").append(salarySum)
+            report.append(name).append(" - ").append(salarySum)
                     .append(System.lineSeparator());
         }
-        return stringBuilder.toString().trim();
+        return report.toString().trim();
     }
 
     public boolean isValidDate(String name, String dateFrom, String[] splittedDate,
                                int dateindex, String dateTo, DateTimeFormatter formatter) {
-        if (name.equals(splittedDate[NAME_INDEX])
+        return (name.equals(splittedDate[NAME_INDEX])
                 && !LocalDate.parse(splittedDate[dateindex], formatter)
                 .isBefore(LocalDate.parse(dateFrom, formatter))
                 && !LocalDate.parse(splittedDate[dateindex], formatter)
-                .isAfter(LocalDate.parse(dateTo, formatter))) {
-            return true;
-        } else {
-            return false;
-        }
+                .isAfter(LocalDate.parse(dateTo, formatter)));
     }
 }
 
