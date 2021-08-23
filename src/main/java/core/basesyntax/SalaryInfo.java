d2b7588ecbb30a6de55c0217dcf0323e9291aa7e
Yuliya Senchenko <usencenko70@gmail.com>
2021-08-23 13:30:04 +0300

    Created "jv-salary-info" project

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -9,23 +9,21 @@ public class SalaryInfo {
             DateTimeFormatter.ofPattern(pattern);
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        int moneyEarned;
-        String[] dataOfIndex;
-        LocalDate dateFrom2 = LocalDate.parse(dateFrom, SIMPLE_DATE_FORMAT).minusDays(1);
-        LocalDate dateTo2 = LocalDate.parse(dateTo, SIMPLE_DATE_FORMAT).plusDays(1);
-        LocalDate dateOfWork;
+        LocalDate localDateFrom = LocalDate.parse(dateFrom, SIMPLE_DATE_FORMAT).minusDays(1);
+        LocalDate localDateTo = LocalDate.parse(dateTo, SIMPLE_DATE_FORMAT).plusDays(1);
         StringBuilder reportForPeriod = new StringBuilder()
-                .append("Report for period ").append(dateFrom)
+                .append("Report for period ")
+                .append(dateFrom)
                 .append(" - ").append(dateTo);
         for (String name : names) {
-            moneyEarned = 0;
+            int moneyEarned = 0;
             for (String datum : data) {
-                dataOfIndex = datum.split(" ");
-                dateOfWork = LocalDate.parse(dataOfIndex[0], SIMPLE_DATE_FORMAT);
-                if (name.equals(dataOfIndex[1]) && dateOfWork.isAfter(dateFrom2)
-                        && dateOfWork.isBefore(dateTo2)) {
+                String[] record = datum.split(" ");
+                LocalDate dateOfWork = LocalDate.parse(record[0], SIMPLE_DATE_FORMAT);
+                if (name.equals(record[1]) && dateOfWork.isAfter(localDateFrom)
+                        && dateOfWork.isBefore(localDateTo)) {
                     moneyEarned +=
-                            Integer.parseInt(dataOfIndex[2]) * Integer.parseInt(dataOfIndex[3]);
+                            Integer.parseInt(record[2]) * Integer.parseInt(record[3]);
                 }
             }
             reportForPeriod.append(System.lineSeparator())
