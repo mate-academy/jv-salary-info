6f1aca4771d18acd2467bab4f8f90726df9b1c45
dogovor <dogovor@lan.com.ua>
2021-08-25 12:29:33 +0300

    chenged constans and condition in if-block

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,17 +4,17 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int DATE = 0;
+    private static final int WORKER = 1;
+    private static final int HOURS = 2;
+    private static final int MONEY = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        int date = 0;
-        int worker = 1;
-        int hours = 2;
-        int money = 3;
-        StringBuilder stringBuilder = new StringBuilder();
-        LocalDate beginPeriod = LocalDate.parse(dateFrom, dateTimeFormatter);
-        LocalDate endPeriod = LocalDate.parse(dateTo, dateTimeFormatter);
-        stringBuilder.append("Report for period ")
+        StringBuilder report = new StringBuilder();
+        LocalDate beginPeriod = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate endPeriod = LocalDate.parse(dateTo, FORMATTER);
+        report.append("Report for period ")
                     .append(dateFrom).append(" - ")
                     .append(dateTo)
                     .append(System.lineSeparator());
@@ -22,20 +22,19 @@ public class SalaryInfo {
             int salary = 0;
             for (String datum : data) {
                 String[] partsOfData = datum.split("\\s");
-                if (partsOfData[worker].equals(name)
-                            && !LocalDate.parse(partsOfData[date],
-                            dateTimeFormatter).isAfter(endPeriod)
-                            && !LocalDate.parse(partsOfData[date],
-                            dateTimeFormatter).isBefore(beginPeriod)) {
-                    salary += (Integer.parseInt(partsOfData[hours])
-                                * Integer.parseInt(partsOfData[money]));
+                LocalDate currentPeriod = LocalDate.parse(partsOfData[DATE], FORMATTER);
+                boolean isInPeriod = !currentPeriod.isAfter(endPeriod)
+                            && !currentPeriod.isBefore(beginPeriod);
+                if (partsOfData[WORKER].equals(name) && isInPeriod) {
+                    salary += (Integer.parseInt(partsOfData[HOURS])
+                                * Integer.parseInt(partsOfData[MONEY]));
                 }
             }
-            stringBuilder.append(name)
+            report.append(name)
                         .append(" - ")
                         .append(salary)
                         .append(System.lineSeparator());
         }
-        return stringBuilder.toString().trim();
+        return report.toString().trim();
     }
 }
