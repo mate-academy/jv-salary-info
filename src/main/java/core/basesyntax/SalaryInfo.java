7d8f2d3b271b287f679425e5139c8b5d767d4ee3
Svatos <fursin997@gmail.com>
2021-08-25 15:41:41 +0300

    additional variables were created

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -6,6 +6,11 @@ import java.time.format.DateTimeFormatter;
 public class SalaryInfo {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
+    private static final int DATE = 0;
+    private static final int NAME = 1;
+    private static final int HOURS = 2;
+    private static final int SALARY_PER_MONTH = 3;
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate localDateFrom = LocalDate.parse(dateFrom, FORMATTER);
         LocalDate localDateTo = LocalDate.parse(dateTo, FORMATTER);
@@ -16,12 +21,12 @@ public class SalaryInfo {
             int totalSalary = 0;
             for (String datum : data) {
                 String[] record = datum.split(" ");
-                LocalDate currentDate = LocalDate.parse(record[0], FORMATTER);
-                if (name.equals(record[1])
+                LocalDate currentDate = LocalDate.parse(record[DATE], FORMATTER);
+                if (name.equals(record[NAME])
                             && !currentDate.isBefore(localDateFrom)
                         && !currentDate.isAfter(localDateTo)) {
-                    totalSalary += Integer.parseInt(record[2])
-                            * Integer.parseInt(record[3]);
+                    totalSalary += Integer.parseInt(record[HOURS])
+                            * Integer.parseInt(record[SALARY_PER_MONTH]);
                 }
             }
             report.append(System.lineSeparator())
