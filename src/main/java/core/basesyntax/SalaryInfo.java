f0af83064025d3cdb62ba5d4c39120100c6526bb
Oleksandr Sydorskyi <oleksandrsydorskyi@gmail.com>
2021-09-10 17:26:54 +0300

    Oleksandr Sydorskyi Salary Info 10.09.2021  Throws ParseException - was removed. A try/catch block was placed around the code that might generate ParseException.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,22 +5,32 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class SalaryInfo {
-    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo)
-            throws ParseException {
+    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder report = new StringBuilder();
         report.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
-        Date startDate = new SimpleDateFormat("dd.MM.yyy").parse(dateFrom);
-        Date endDate = new SimpleDateFormat("dd.MM.yyy").parse(dateTo);
+        Date startDate = null;
+        Date endDate = null;
+        try {
+            startDate = new SimpleDateFormat("dd.MM.yyy").parse(dateFrom);
+            endDate = new SimpleDateFormat("dd.MM.yyy").parse(dateTo);
+        } catch (ParseException e) {
+            System.out.println("ParseException");
+        }
         for (String name : names) {
             int employeeSalary = 0;
             for (String datum : data) {
                 String[] employeeData = datum.split(" ");
-                Date currentDate = new SimpleDateFormat("dd.MM.yyy").parse(employeeData[0]);
+                Date currentDate = null;
+                try {
+                    currentDate = new SimpleDateFormat("dd.MM.yyy").parse(employeeData[0]);
+                } catch (ParseException e) {
+                    System.out.println("ParseException");
+                }
                 if ((currentDate.equals(startDate) || currentDate.equals(endDate)
-                        || currentDate.after(startDate) && currentDate.before(endDate))
-                        && employeeData[1].contains(name)) {
+                            || currentDate.after(startDate) && currentDate.before(endDate))
+                            && employeeData[1].contains(name)) {
                     employeeSalary += Integer.parseInt(employeeData[2])
-                            * Integer.parseInt(employeeData[3]);
+                                * Integer.parseInt(employeeData[3]);
                 }
             }
             report.append(System.lineSeparator()).append(name).append(" - ").append(employeeSalary);
@@ -28,3 +38,4 @@ public class SalaryInfo {
         return report.toString();
     }
 }
+
