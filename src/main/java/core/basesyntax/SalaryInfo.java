55fa00bf03e9f40cb6440c3968b0aeff9d18796e
Artem Kushniruk <artem.kushniruck@gmail.com>
2021-09-15 11:32:42 +0300

    all mistakes were fixed

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,7 +5,12 @@ import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
 public class SalaryInfo {
+    private static final DateTimeFormatter FORMATTER =
+            DateTimeFormatter.ofPattern("dd.MM.yyyy", Locale.ENGLISH);
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
+        LocalDate from = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate to = LocalDate.parse(dateTo, FORMATTER);
         StringBuilder builder = new StringBuilder();
         builder.append("Report for period ")
                 .append(dateFrom)
@@ -16,16 +21,15 @@ public class SalaryInfo {
             int totalIncome = 0;
 
             for (String userData : data) {
-                String userName = getName(userData);
-                String userDate = getDate(userData);
-                int userWorkingHours = getWorkingHours(userData);
-                int ratePerHour = getRatePerHour(userData);
-                LocalDate checkDate = convertDate(userDate);
-                LocalDate from = convertDate(dateFrom);
-                LocalDate to = convertDate(dateTo);
+                String[] userDataParts = userData.split(" ");
+                String userDate = userDataParts[0];
+                String userName = userDataParts[1];
+                int userWorkingHours = Integer.parseInt(userDataParts[2]);
+                int ratePerHour = Integer.parseInt(userDataParts[3]);
+                LocalDate checkDate = LocalDate.parse(userDate, FORMATTER);
 
                 if (name.equals(userName) && isInDates(checkDate, from, to)) {
-                    totalIncome += calculateDayIncome(userWorkingHours, ratePerHour);
+                    totalIncome += userWorkingHours * ratePerHour;
                 }
             }
             builder.append(System.lineSeparator())
@@ -36,39 +40,10 @@ public class SalaryInfo {
         return builder.toString();
     }
 
-    private String getDate(String userData) {
-        String[] parts = userData.split(" ");
-        return parts[0];
-    }
-
-    private String getName(String data) {
-        String[] parts = data.split(" ");
-        return parts[1];
-    }
-
-    private int getWorkingHours(String data) {
-        String[] parts = data.split(" ");
-        return Integer.parseInt(parts[2]);
-    }
-
-    private int getRatePerHour(String data) {
-        String[] parts = data.split(" ");
-        return Integer.parseInt(parts[3]);
-    }
-
-    private LocalDate convertDate(String date) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy", Locale.ENGLISH);
-        return LocalDate.parse(date, formatter);
-    }
-
     private boolean isInDates(LocalDate date, LocalDate from, LocalDate to) {
         return (date.isAfter(from)
                 && date.isBefore(to)
                 || date.isEqual(from)
                 || date.isEqual(to));
-    }
-
-    private int calculateDayIncome(int workingHours, int incomePerHour) {
-        return workingHours * incomePerHour;
     }
 }
