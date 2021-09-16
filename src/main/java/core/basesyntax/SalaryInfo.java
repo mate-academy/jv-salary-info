f1cb48d4889f812cb605811b7e4b96cfb50bdf25
Yaroslav Yarmoshyk <yarmoshyk96@gmail.com>
2021-09-16 11:23:51 +0300

    added fixes after second review

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -8,33 +8,26 @@ public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder report = new StringBuilder("Report for period " + dateFrom + " - " + dateTo);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        LocalDate dateF = LocalDate.parse(dateFrom,formatter);
-        LocalDate dateT = LocalDate.parse(dateTo,formatter);
+        LocalDate from = LocalDate.parse(dateFrom,formatter);
+        LocalDate to = LocalDate.parse(dateTo,formatter);
 
         for (int i = 0; i < names.length; i++) {
             int fullSalary = 0;
             for (int j = 0; j < data.length; j++) {
-                if (data[j] == null) {
-                    continue;
-                }
                 String[] words = data[j].split(" ",0);
                 LocalDate date = LocalDate.parse(words[0],formatter);
                 String checkedName = words[1];
                 int hours = Integer.parseInt(words[2]);
                 int salaryPerHour = Integer.parseInt(words[3]);
                 int salary = hours * salaryPerHour;
-                boolean a = !date.isBefore(dateF);
-                boolean b = !date.isAfter(dateT);
                 if (checkedName.equals(names[i])
-                            && !date.isBefore(dateF) && !date.isAfter(dateT)) {
+                            && !date.isBefore(from) && !date.isAfter(to)) {
                     fullSalary = salary + fullSalary;
                 }
-                if (j == data.length - 1) {
-                    StringBuilder resName = new StringBuilder(names[i] + " - " + fullSalary);
-                    report.append(System.lineSeparator());
-                    report.append(resName);
-                }
             }
+            StringBuilder resName = new StringBuilder(names[i] + " - " + fullSalary);
+            report.append(System.lineSeparator());
+            report.append(resName);
         }
         return report.toString();
     }
