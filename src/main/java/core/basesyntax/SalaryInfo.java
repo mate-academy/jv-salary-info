4b5f71b397a3a1e277a268933524e5220d1570a1
Mykola Skrypka <mykolaskrypka@gmail.com>
2021-09-22 22:05:55 +0300

    try catch removed, constant variables moved to class fields

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,39 +5,23 @@ import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
 public class SalaryInfo {
-    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        String patternFormat = "dd.MM.yyyy";
-        Locale localeUa = new Locale("ua", "UA");
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(patternFormat)
-                .withLocale(localeUa);
+    private String patternFormat = "dd.MM.yyyy";
+    private Locale localeUa = new Locale("ua", "UA");
+    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern(patternFormat)
+            .withLocale(localeUa);
 
-        LocalDate dateBegin;
-        try {
-            dateBegin = LocalDate.parse(dateFrom, formatter);
-        } catch (Exception e) {
-            System.out.println("Wrong dateFrom! " + e);
-            return "";
-        }
-        LocalDate dateEnd;
-        try {
-            dateEnd = LocalDate.parse(dateTo, formatter);
-        } catch (Exception e) {
-            System.out.println("Wrong dateTo! " + e);
-            return "";
-        }
+    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
+
+        LocalDate dateBegin = LocalDate.parse(dateFrom, formatter);
+        LocalDate dateEnd = LocalDate.parse(dateTo, formatter);
+
         StringBuilder report = new StringBuilder();
         report.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
         for (String name : names) {
             int amount = 0;
             for (String employeeData : data) {
                 String[] dataArray = employeeData.split(" ");
-                LocalDate dataDate;
-                try {
-                    dataDate = LocalDate.parse(dataArray[0], formatter);
-                } catch (Exception e) {
-                    System.out.println("Wrong date! " + e);
-                    continue;
-                }
+                LocalDate dataDate = LocalDate.parse(dataArray[0], formatter);
                 if (employeeData.contains(name) && !dataDate.isBefore(dateBegin)
                         && !dataDate.isAfter(dateEnd)) {
                     amount += Integer.parseInt(dataArray[2]) * Integer.parseInt(dataArray[3]);
