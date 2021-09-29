909b16041109fdc614d637f27095df26a233a6fc
Maryna Blank <marinablank96@gmail.com>
2021-09-29 12:06:59 +0300

    Created a class SalaryInfo with a method implementing salary calculation.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,36 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        StringBuilder salaryInfo = new StringBuilder();
+        int salary = 0;
+        for (String name : names) {
+            for (int i = 0; i < data.length; i++) {
+                String[] partsData = data[i].split(" ");
+
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+                LocalDate dateStart = LocalDate.parse(dateFrom, formatter);
+                LocalDate dateData = LocalDate.parse(partsData[0], formatter);
+                LocalDate dateEnd = LocalDate.parse(dateTo, formatter);
+
+                if (name.equals(partsData[1])) {
+                    if ((dateStart.isBefore(dateData)) || (dateStart.isEqual(dateData))) {
+                        if ((dateEnd.isAfter(dateData)) || (dateEnd.isEqual(dateData))) {
+                            if (partsData[1].equals(name)) {
+                                int hours = Integer.parseInt(partsData[2]);
+                                int rate = Integer.parseInt(partsData[3]);
+                                salary += hours * rate;
+                            }
+                        }
+                    }
+                }
+            }
+            salaryInfo.append(System.lineSeparator()).append(name).append(" - ").append(salary);
+            salary = 0;
+        }
+        return "Report for period " + dateFrom + " - " + dateTo + salaryInfo;
     }
 }
