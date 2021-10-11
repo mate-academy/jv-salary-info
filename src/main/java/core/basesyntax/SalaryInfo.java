5db15598500fe5b4c43fafc9a2e9d2169d0b4ee4
Viktor Stadnik <stadhik.victor@gmail.com>
2021-10-11 11:13:58 +0300

    Finished Homework. Created DataTime Formatter for convert String to DataFormat. Used StringBuilder for connect words

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,29 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDate fromDateFrom = LocalDate.parse(dateFrom, formatter);
+        LocalDate toDateTo = LocalDate.parse(dateTo, formatter);
+        StringBuilder result = new StringBuilder("Report for period " + dateFrom + " - " + dateTo);
+        for (String name : names) {
+            int salary = 0;
+            for (int i = 0; i < data.length; i++) {
+                String[] dataInfo = data[i].split(" ");
+                LocalDate dataInfoData = LocalDate.parse(dataInfo[0], formatter);
+                if (dataInfoData.isAfter(fromDateFrom)
+                        && dataInfoData.isBefore(toDateTo.plusDays(1))
+                        && dataInfo[1].equals(name)) {
+                    salary += Integer.parseInt(dataInfo[2]) * Integer.parseInt(dataInfo[3]);
+                }
+            }
+            result.append(System.lineSeparator());
+            result.append(name).append(" - ").append(salary);
+        }
+        return result.toString();
     }
 }
