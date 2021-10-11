7af72cf62be86cfa439b5694b1df9b52e38b994c
bukoz <bukoz@meta.ua>
2021-10-11 13:28:09 +0300

    removed DateTimeFormatter to class variable

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,14 +4,16 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
         LocalDate fromDate = LocalDate.parse(dateFrom, formatter);
         LocalDate toDate = LocalDate.parse(dateTo, formatter);
         StringBuilder builder = new StringBuilder("Report for period " + dateFrom + " - " + dateTo);
 
         for (int i = 0; i < names.length; i++) {
             int salary = 0;
+
             for (int j = 0; j < data.length; j++) {
                 String[] dataAll = data[j].split(" ");
                 if (LocalDate.parse(dataAll[0], formatter).isBefore(toDate.plusDays(1))
