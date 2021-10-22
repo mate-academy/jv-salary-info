b10dac1b261b360964a07770f501edcf3f3524ed
Sergii Svystun <sergiisvystun@gmail.com>
2021-10-22 18:39:34 +0300

    Removed array "names1", created "salary" array and its counting at second "for loop", added constants to generalInfo method

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,39 +4,35 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-
-    static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final DateTimeFormatter DATE_FORMATTER =
+            DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int INDEX_HOURS_PER_DAY = 2;
+    private static final int INDEX_OF_NAME = 1;
+    private static final int INDEX_SALARY_PER_DAY = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        String[] names1 = new String[names.length];
-        for (int i = 0; i < names1.length; i++) {
-            names1[i] = new StringBuilder().append(names[i]).append(" - ").append("0").toString();
-        }
-
+        int[] salary = new int[names.length];
         for (int i = 0; i < data.length; i++) {
             LocalDate dateData = LocalDate
-                    .parse(data[i].substring(0, data[i].indexOf(' ')), FORMATTER);
-            if (dateData.isAfter(LocalDate.parse(dateFrom, FORMATTER).minusDays(1))
-                    && dateData.isBefore(LocalDate.parse(dateTo, FORMATTER).plusDays(1))) {
-                for (int j = 0; j < names1.length; j++) {
+                    .parse(data[i].substring(0, data[i].indexOf(' ')), DATE_FORMATTER);
+            if (dateData.isAfter(LocalDate.parse(dateFrom, DATE_FORMATTER).minusDays(1))
+                    && dateData.isBefore(LocalDate.parse(dateTo, DATE_FORMATTER).plusDays(1))) {
+                for (int j = 0; j < names.length; j++) {
                     String[] generalInfo = data[i].split(" ");
-                    if (names1[j].substring(0, names1[j].indexOf(' ')).equals(generalInfo[1])) {
-                        names1[j] = new StringBuilder(names1[j])
-                        .replace(names1[j].split(" ")[0].length() + 3, names1[j].length(),
-                        Integer.toString(Integer.parseInt(generalInfo[2])
-                        * Integer.parseInt(generalInfo[3])
-                        + Integer.parseInt(names1[j].split(" ")[2]))).toString();
+                    if (names[j].equals(generalInfo[INDEX_OF_NAME])) {
+                        salary[j] += Integer.parseInt(generalInfo[INDEX_HOURS_PER_DAY])
+                                * Integer.parseInt(generalInfo[INDEX_SALARY_PER_DAY]);
                     }
                 }
             }
         }
-        StringBuilder finalInfo = new StringBuilder();
-        finalInfo.append("Report for period ").append(dateFrom).append(" - ")
-                .append(dateTo).append(System.lineSeparator());
-        for (String totalInfo:names1) {
-            finalInfo.append(totalInfo).append(System.lineSeparator());
+        StringBuilder reportBuilder = new StringBuilder();
+        reportBuilder.append("Report for period ").append(dateFrom).append(" - ")
+                .append(dateTo);
+        for (int i = 0; i < names.length; i++) {
+            reportBuilder.append(System.lineSeparator()).append(names[i])
+                    .append(" - ").append(salary[i]);
         }
-        return finalInfo.toString().trim();
+        return reportBuilder.toString();
     }
 }
-
