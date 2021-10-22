f9b8606e2bb7744f01b5bb5626f208d21cd5824c
Oleg Shiriaev <mystidreamer@gmail.com>
2021-10-22 14:33:42 +0300

    all problems was fixed

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -2,8 +2,6 @@ package core.basesyntax;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
-import java.util.Collections;
 
 public class SalaryInfo {
 
@@ -12,7 +10,7 @@ public class SalaryInfo {
         LocalDate from = LocalDate.parse(dateFrom, df);
         LocalDate to = LocalDate.parse(dateTo, df);
         StringBuilder sb = new StringBuilder("Report for period "
-                + dateFrom + " - " + dateTo + "\n");
+                + dateFrom + " - " + dateTo + System.lineSeparator());
         int salary = 0;
 
         for (String name : names) {
