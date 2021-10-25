ce034e0c181d7e5833cbbe8c4b29e8ac56888d85
nishchenkokateryna <kateryna.nishchenko@gmail.com>
2021-10-25 16:21:50 +0300

    Deleted unnecessary final identifier, renamed variable str to more informed variable

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -10,15 +10,14 @@ public class SalaryInfo {
     private static final int INDEX_OF_WORKING_HOURS = 2;
     private static final int INDEX_OF_INCOME_PER_HOUR = 3;
 
-    public String getSalaryInfo(final String[] names, final String[] data,
-                                final String dateFrom, final String dateTo) {
+    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         final LocalDate startDate = LocalDate.parse(dateFrom, DATE_FORMAT);
         final LocalDate endDate = LocalDate.parse(dateTo, DATE_FORMAT);
         final int[] salaries = new int[names.length];
 
         for (int i = 0; i < names.length; i++) {
-            for (String str : data) {
-                final String[] tempDataPerDate = str.split(" ");
+            for (String dataElement : data) {
+                final String[] tempDataPerDate = dataElement.split(" ");
                 salaries[i] += checkDateAndCalculateSalary(startDate, endDate,
                         tempDataPerDate, names[i]);
             }
@@ -26,8 +25,8 @@ public class SalaryInfo {
         return printInfo(names, salaries, dateFrom, dateTo);
     }
 
-    private int checkDateAndCalculateSalary(final LocalDate dateFrom, final LocalDate dateTo,
-                                            final String[] data, final String currentName) {
+    private int checkDateAndCalculateSalary(LocalDate dateFrom, LocalDate dateTo,
+                                            String[] data, String currentName) {
         final LocalDate currentDate = LocalDate.parse(data[INDEX_OF_DATE], DATE_FORMAT);
         final String name = data[INDEX_OF_NAME];
         if (currentDate.isBefore(dateTo.plusDays(1)) && currentDate.isAfter(dateFrom.minusDays(1))
@@ -39,8 +38,7 @@ public class SalaryInfo {
         return 0;
     }
 
-    private String printInfo(final String[] names, final int[] incomes,
-                             final String startDate, final String endDate) {
+    private String printInfo(String[] names, int[] incomes, String startDate, String endDate) {
         final String hyphenAppender = " - ";
         final StringBuilder builder = new StringBuilder();
         builder.append("Report for period ")
