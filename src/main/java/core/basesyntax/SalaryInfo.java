39c643b830b1bb1aeb0ab34f4a111c7de99ee27a
roman-kalinin <roman.kalinin.w@gmail.com>
2021-10-29 01:15:33 +0300

    Getter class added, PersonalSalaryCalc added

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,20 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDate from = LocalDate.parse(dateFrom, format);
+        LocalDate to = LocalDate.parse(dateTo, format);
+
+        StringBuilder a = new StringBuilder();
+        a.append("Report for period " + dateFrom + " - " + dateTo + System.lineSeparator());
+        for (String name : names) {
+            a.append(new PersonalSalaryCalc().getPersonalSalary(data, name, from, to) 
+                    + System.lineSeparator());
+        }
+        return a.toString().trim();
     }
 }
