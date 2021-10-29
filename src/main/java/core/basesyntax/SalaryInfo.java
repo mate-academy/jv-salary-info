9fbd03c20d5ba2eab8d8e15bf330b122fd94a2a2
Yaroslav Kavun <yka40znp@gmail.com>
2021-10-29 14:13:05 +0300

    added new constants and additional equals checks

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -6,6 +6,11 @@ import java.time.format.DateTimeFormatter;
 public class SalaryInfo {
     private static final DateTimeFormatter DATE_TIME_FORMATTER = 
             DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int DATE = 0;
+    private static final int NAME_OF_EMPLOYEE = 1;
+    private static final int NUMBER_OF_DAYS = 1;
+    private static final int WORKING_HOURS = 2;
+    private static final int SALARY_PER_HOUR = 3;
     
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate startDate = LocalDate.parse(dateFrom, DATE_TIME_FORMATTER);
@@ -16,12 +21,13 @@ public class SalaryInfo {
             int amontOfMoney = 0;
             for (String datum : data) {
                 String[] split = datum.split(" ");
-                LocalDate separateDate = LocalDate.parse(split[0], DATE_TIME_FORMATTER);
-                if (name.equals(split[1])) {
-                    if (separateDate.isAfter(startDate) && separateDate
-                            .isBefore(finishDate.plusDays(1))) {
-                        amontOfMoney += Integer.parseInt(split[2]) * Integer.parseInt(split[3]);
-                    }
+                LocalDate separateDate = LocalDate.parse(split[DATE], DATE_TIME_FORMATTER);
+                if (name.equals(split[NAME_OF_EMPLOYEE])
+                        && (separateDate.isAfter(startDate) || separateDate.equals(startDate))
+                        && (separateDate.isBefore(finishDate.plusDays(NUMBER_OF_DAYS))
+                        || separateDate.equals(finishDate))) {
+                    amontOfMoney += Integer.parseInt(split[WORKING_HOURS])
+                            * Integer.parseInt(split[SALARY_PER_HOUR]);
                 }
             }
             reportBuilder.append(System.lineSeparator()).append(name)
