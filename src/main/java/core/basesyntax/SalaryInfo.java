33386877dfc9e508b69a5a8558fc1f58f0561212
KostyNikiforov <www.kosta.kon@gmail.com>
2021-11-01 17:41:32 +0200

    Constants were created. Creating vars was moved to the loop.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -7,37 +7,35 @@ import java.util.HashMap;
 public class SalaryInfo {
     private static final DateTimeFormatter DATE_TIME_FORMATTER =
             DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int DATE = 0;
+    private static final int NAME = 1;
+    private static final int HOURS = 2;
+    private static final int INCOME_PER_HOUR = 3;
 
     public static String getSalaryInfo(String[] names, String[] data,
                                        String dateFrom, String dateTo) {
         HashMap<String, Integer> employeesSalary = new HashMap<>();
-        Integer moneyPerDay;
-        Integer currentValue;
         for (String date: data) {
             String[] splitData = date.split(" ");
-            if (LocalDate.parse(splitData[0], DATE_TIME_FORMATTER)
+            if (LocalDate.parse(splitData[DATE], DATE_TIME_FORMATTER)
                     .compareTo(LocalDate.parse(dateFrom, DATE_TIME_FORMATTER)) >= 0
-                    && LocalDate.parse(splitData[0], DATE_TIME_FORMATTER)
+                    && LocalDate.parse(splitData[DATE], DATE_TIME_FORMATTER)
                     .compareTo(LocalDate.parse(dateTo, DATE_TIME_FORMATTER)) <= 0) {
-                moneyPerDay = Integer.parseInt(splitData[2]) * Integer.parseInt(splitData[3]);
-                // Get current value or put a new name with a value
-                // if this name was not there before
-                currentValue = employeesSalary.putIfAbsent(splitData[1], moneyPerDay);
+                Integer moneyPerDay = Integer.parseInt(splitData[HOURS])
+                                * Integer.parseInt(splitData[INCOME_PER_HOUR]);
+                Integer currentValue = employeesSalary.putIfAbsent(splitData[NAME], moneyPerDay);
                 if (currentValue != null) {
-                    // Update current value
-                    employeesSalary.replace(splitData[1], currentValue + moneyPerDay);
+                    employeesSalary.replace(splitData[NAME], currentValue + moneyPerDay);
                 }
             }
         }
         StringBuilder result = new StringBuilder(
-                String.format("Report for period %s - %s%s", dateFrom, dateTo,
-                        System.lineSeparator()));
+                String.format("Report for period %s - %s", dateFrom, dateTo));
         for (String name : names) {
-            result.append(name)
+            result.append(System.lineSeparator()).append(name)
                     .append(" - ")
-                    .append(employeesSalary.getOrDefault(name, 0))
-                    .append(System.lineSeparator());
+                    .append(employeesSalary.getOrDefault(name, 0));
         }
-        return result.toString().trim();
+        return result.toString();
     }
 }
