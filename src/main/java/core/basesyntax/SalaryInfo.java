0ff44352a4175ba01f849cc0fb6a116753e555bb
Dima Barshay <barshayerum8@gmail.com>
2021-11-01 16:45:21 +0200

    renamed local dates

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -12,8 +12,8 @@ public class SalaryInfo {
     private static final int INCOME_POSITION = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        LocalDate localFrom = LocalDate.parse(dateFrom, DATE_FORMATTER);
-        LocalDate localTo = LocalDate.parse(dateTo, DATE_FORMATTER);
+        LocalDate localDateFrom = LocalDate.parse(dateFrom, DATE_FORMATTER);
+        LocalDate localDateTo = LocalDate.parse(dateTo, DATE_FORMATTER);
         StringBuilder builder = new StringBuilder("Report for period ").append(dateFrom)
                 .append(" - ").append(dateTo);
 
@@ -22,9 +22,9 @@ public class SalaryInfo {
             for (String line : data) {
                 String[] record = line.split(" ");
                 LocalDate recordDate = LocalDate.parse(record[DATE_POSITION], DATE_FORMATTER);
-                if (name.equals(record[NAME_POSITION]) && (recordDate.isAfter(localFrom)
-                        || recordDate.equals(localFrom)) && (recordDate.isBefore(localTo)
-                        || recordDate.equals(localTo))) {
+                if (name.equals(record[NAME_POSITION]) && (recordDate.isAfter(localDateFrom)
+                        || recordDate.equals(localDateFrom)) && (recordDate.isBefore(localDateTo)
+                        || recordDate.equals(localDateTo))) {
                     money += Integer.parseInt(record[HOURS_POSITION])
                             * Integer.parseInt(record[INCOME_POSITION]);
                 }
