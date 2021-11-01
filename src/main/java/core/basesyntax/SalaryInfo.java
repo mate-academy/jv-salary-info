73dde489e5c8a86dcde62fda6cb598986dbddd63
Dima Barshay <barshayerum8@gmail.com>
2021-11-01 16:42:02 +0200

    fixed mistakes

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,23 +4,24 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final DateTimeFormatter DATE_FORMATTER =
+            DateTimeFormatter.ofPattern("dd.MM.yyyy");
     private static final int DATE_POSITION = 0;
     private static final int NAME_POSITION = 1;
     private static final int HOURS_POSITION = 2;
     private static final int INCOME_POSITION = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        LocalDate localFrom = LocalDate.parse(dateFrom, FORMATTER);
-        LocalDate localTo = LocalDate.parse(dateTo, FORMATTER);
+        LocalDate localFrom = LocalDate.parse(dateFrom, DATE_FORMATTER);
+        LocalDate localTo = LocalDate.parse(dateTo, DATE_FORMATTER);
         StringBuilder builder = new StringBuilder("Report for period ").append(dateFrom)
                 .append(" - ").append(dateTo);
-        int money = 0;
 
         for (String name : names) {
+            int money = 0;
             for (String line : data) {
                 String[] record = line.split(" ");
-                LocalDate recordDate = LocalDate.parse(record[DATE_POSITION], FORMATTER);
+                LocalDate recordDate = LocalDate.parse(record[DATE_POSITION], DATE_FORMATTER);
                 if (name.equals(record[NAME_POSITION]) && (recordDate.isAfter(localFrom)
                         || recordDate.equals(localFrom)) && (recordDate.isBefore(localTo)
                         || recordDate.equals(localTo))) {
@@ -28,8 +29,7 @@ public class SalaryInfo {
                             * Integer.parseInt(record[INCOME_POSITION]);
                 }
             }
-            builder.append("\n").append(name).append(" - ").append(money);
-            money = 0;
+            builder.append(System.lineSeparator()).append(name).append(" - ").append(money);
         }
         return builder.toString();
     }
