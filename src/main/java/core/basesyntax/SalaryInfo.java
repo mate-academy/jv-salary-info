250ff2070e8a3e322f614608ed937af683204098
morozovyrv <72643876+morozovyrv@users.noreply.github.com>
2021-11-08 21:43:51 +0200

    DateTimeFormatter FORMATTER -> class field localDateFrom + localDateTo -> remove final

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,10 +4,11 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    private final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo)  {
-        final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        final LocalDate FROM_LOCAL_DATE = LocalDate.parse(dateFrom,FORMATTER);
-        final LocalDate TO_LOCAL_DATE = LocalDate.parse(dateTo,FORMATTER);
+        LocalDate localDateFrom = LocalDate.parse(dateFrom,FORMATTER);
+        LocalDate localDateTo = LocalDate.parse(dateTo,FORMATTER);
         StringBuilder stringBuilder = new StringBuilder(String.format("Report for period %s - %s",dateFrom,dateTo));
         for (String name : names) {
             int employeeSalaryForPeriod = 0;
@@ -16,7 +17,7 @@ public class SalaryInfo {
                 String currentEmployeeName = employeeData[1];
                 if (name.equals(currentEmployeeName)) {
                     LocalDate currentDate = LocalDate.parse(employeeData[0],FORMATTER);
-                    if ((currentDate.isAfter(FROM_LOCAL_DATE) || currentDate.equals(FROM_LOCAL_DATE)) && (currentDate.isBefore(TO_LOCAL_DATE) || currentDate.equals(TO_LOCAL_DATE))) {
+                    if ((currentDate.isAfter(localDateFrom) || currentDate.equals(localDateFrom)) && (currentDate.isBefore(localDateTo) || currentDate.equals(localDateTo))) {
                         int employeeWorkedHours = Integer.parseInt(employeeData[2]);
                         int employeeSalaryPerHour = Integer.parseInt(employeeData[3]);
                         employeeSalaryForPeriod += employeeWorkedHours * employeeSalaryPerHour;
