a8fece5e7ac0763e455b79125bf2e0b39c43580a
Vladyslav Chyver <lione2009lione@gmail.com>
2021-11-11 13:46:24 +0200

    SalaryInfo was implemented

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,37 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter
+            .ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        StringBuilder result = new StringBuilder().append("Report for period ")
+                .append(dateFrom).append(" - ").append(dateTo);
+        LocalDate dateFromInLocalDate = LocalDate.parse(dateFrom, DATE_FORMATTER);
+        LocalDate dateToInLocalDate = LocalDate. parse(dateTo, DATE_FORMATTER);
+        for (String name : names) {
+            int totalSalary = 0;
+            for (String stringOfData : data) {
+                LocalDate salaryDate = LocalDate.parse(stringOfData
+                        .substring(0, stringOfData.indexOf(" ")), DATE_FORMATTER);
+                if (stringOfData.contains(name)
+                        && salaryDate.compareTo(dateFromInLocalDate) >= 0
+                        && salaryDate.compareTo(dateToInLocalDate) <= 0) {
+                    Integer hoursPerDay = Integer.parseInt(stringOfData.substring(
+                            stringOfData.lastIndexOf(name) + name.length() + 1,
+                            stringOfData.lastIndexOf(" ")));
+                    Integer incomePerHour =
+                            Integer.parseInt(stringOfData.substring(stringOfData
+                                    .lastIndexOf(" ") + 1));
+                    totalSalary += hoursPerDay * incomePerHour;
+                }
+            }
+            result.append(System.lineSeparator()).append(name).append(" - ").append(totalSalary);
+        }
+
+        return result.toString();
     }
 }
