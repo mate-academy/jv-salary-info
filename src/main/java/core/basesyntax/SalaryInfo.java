200529401bd30bcac9e64023168dd6fb80043b38
morozovyrv <72643876+morozovyrv@users.noreply.github.com>
2021-11-17 14:42:31 +0200

    fixes for travis

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,12 +4,14 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
-    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo)  {
+    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate localDateFrom = LocalDate.parse(dateFrom,FORMATTER);
         LocalDate localDateTo = LocalDate.parse(dateTo,FORMATTER);
-        StringBuilder stringBuilder = new StringBuilder(String.format("Report for period %s - %s",dateFrom,dateTo));
+        StringBuilder stringBuilder = new StringBuilder(
+                String.format("Report for period %s - %s",dateFrom,dateTo)
+        );
         for (String name : names) {
             int employeeSalaryForPeriod = 0;
             for (String employeesData : data) {
@@ -17,14 +19,19 @@ public class SalaryInfo {
                 String currentEmployeeName = employeeData[1];
                 if (name.equals(currentEmployeeName)) {
                     LocalDate currentDate = LocalDate.parse(employeeData[0],FORMATTER);
-                    if ((currentDate.isAfter(localDateFrom) || currentDate.equals(localDateFrom)) && (currentDate.isBefore(localDateTo) || currentDate.equals(localDateTo))) {
+                    if ((currentDate.isAfter(localDateFrom)
+                            || currentDate.equals(localDateFrom))
+                            && (currentDate.isBefore(localDateTo)
+                            || currentDate.equals(localDateTo))) {
                         int employeeWorkedHours = Integer.parseInt(employeeData[2]);
                         int employeeSalaryPerHour = Integer.parseInt(employeeData[3]);
                         employeeSalaryForPeriod += employeeWorkedHours * employeeSalaryPerHour;
                     }
                 }
             }
-            stringBuilder.append(String.format("%s%s - %d",System.lineSeparator(),name,employeeSalaryForPeriod));
+            stringBuilder.append(
+                    String.format("%s%s - %d",System.lineSeparator(),name,employeeSalaryForPeriod)
+            );
         }
         return stringBuilder.toString();
     }
