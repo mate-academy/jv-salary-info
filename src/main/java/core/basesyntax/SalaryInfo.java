52138c170c4578b1eb3d1bd9fb49875e596a6dc9
Roman Okhrymovych <okhrymovych11@gmail.com>
2021-11-27 20:19:51 +0200

    All test & Travis checked.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,33 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDate startDate = LocalDate.parse(dateFrom, formatter).minusDays(1);
+        LocalDate finishDate = LocalDate.parse(dateTo, formatter).plusDays(1);
+        int[]salary = new int[names.length];
+        StringBuilder builder = new StringBuilder("Report for period " + dateFrom + " - " + dateTo);
+        for (int i = 0; i < names.length; i++) {
+            for (String datum : data) {
+                int days = Integer.parseInt(datum.split(" ")[2]);
+                int price = Integer.parseInt(datum.split(" ")[3]);
+                String nameTemp = datum.substring(11, 11 + names[i].length());
+                LocalDate dataTemp = LocalDate.parse(datum.split(" ")[0], formatter);
+                if (dataTemp.isAfter(startDate) && dataTemp.isBefore(finishDate)
+                                                && names[i].equals(nameTemp)) {
+                    salary[i] += days * price;
+                }
+            }
+        }
+        for (int i = 0; i < names.length; i++) {
+            builder.append(System.lineSeparator());
+            builder.append(names[i]);
+            builder.append(" - ");
+            builder.append(salary[i]);
+        }
+        return builder.toString();
     }
 }
