073c42a2cae3e8ba18868ce54d18759bdf146cc3
Roman Okhrymovych <okhrymovych11@gmail.com>
2021-11-27 21:08:52 +0200

    Only two loops & builder. All test passed.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -8,25 +8,23 @@ public class SalaryInfo {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
         LocalDate startDate = LocalDate.parse(dateFrom, formatter).minusDays(1);
         LocalDate finishDate = LocalDate.parse(dateTo, formatter).plusDays(1);
-        int[]salary = new int[names.length];
         StringBuilder builder = new StringBuilder("Report for period " + dateFrom + " - " + dateTo);
-        for (int i = 0; i < names.length; i++) {
+        for (String name : names) {
+            int salaryTotal = 0;
             for (String datum : data) {
                 int days = Integer.parseInt(datum.split(" ")[2]);
                 int price = Integer.parseInt(datum.split(" ")[3]);
-                String nameTemp = datum.substring(11, 11 + names[i].length());
+                String nameTemp = datum.substring(11, 11 + name.length());
                 LocalDate dataTemp = LocalDate.parse(datum.split(" ")[0], formatter);
                 if (dataTemp.isAfter(startDate) && dataTemp.isBefore(finishDate)
-                                                && names[i].equals(nameTemp)) {
-                    salary[i] += days * price;
+                        && name.equals(nameTemp)) {
+                    salaryTotal += days * price;
                 }
             }
-        }
-        for (int i = 0; i < names.length; i++) {
             builder.append(System.lineSeparator());
-            builder.append(names[i]);
+            builder.append(name);
             builder.append(" - ");
-            builder.append(salary[i]);
+            builder.append(salaryTotal);
         }
         return builder.toString();
     }
