84fc18e464bd647633fe3950ab33f390eff8fe33
Roman Okhrymovych <okhrymovych11@gmail.com>
2021-11-29 20:27:56 +0200

    Remove exception, changed DATE_FORMAT, salaryPerDay

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -2,40 +2,32 @@ package core.basesyntax;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
 
 public class SalaryInfo {
-    static final String CONSTANT_FORMAT = "dd.MM.yyyy";
+    static final String DATE_FORMAT = "dd.MM.yyyy";
 
-    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo)
-            throws DateTimeParseException {
-        String result = "";
-        try {
-            StringBuilder builder = new StringBuilder();
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern(CONSTANT_FORMAT);
-            LocalDate startDate = LocalDate.parse(dateFrom, formatter).minusDays(1);
-            LocalDate finishDate = LocalDate.parse(dateTo, formatter).plusDays(1);
-            for (String name : names) {
-                int salaryTotal = 0;
-                for (String datum : data) {
-                    int days = Integer.parseInt(datum.split(" ")[2]);
-                    int price = Integer.parseInt(datum.split(" ")[3]);
-                    String nameTemp = datum.split(" ")[1];
-                    LocalDate dateTemp = LocalDate.parse(datum.split(" ")[0], formatter);
-                    if (dateTemp.isAfter(startDate) && dateTemp.isBefore(finishDate)
-                            && name.equals(nameTemp)) {
-                        salaryTotal += days * price;
-                    }
+    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_FORMAT);
+        LocalDate startDate = LocalDate.parse(dateFrom, formatter).minusDays(1);
+        LocalDate finishDate = LocalDate.parse(dateTo, formatter).plusDays(1);
+        StringBuilder builder = new StringBuilder("Report for period " + dateFrom + " - " + dateTo);
+        for (String name : names) {
+            int salaryTotal = 0;
+            for (String datum : data) {
+                int salaryPerDay = Integer.parseInt(datum.split(" ")[2])
+                                 * Integer.parseInt(datum.split(" ")[3]);
+                String nameTemp = datum.split(" ")[1];
+                LocalDate dataTemp = LocalDate.parse(datum.split(" ")[0], formatter);
+                if (dataTemp.isAfter(startDate) && dataTemp.isBefore(finishDate)
+                        && name.equals(nameTemp)) {
+                    salaryTotal += salaryPerDay;
                 }
-                builder.append(System.lineSeparator());
-                builder.append(name);
-                builder.append(" - ");
-                builder.append(salaryTotal);
             }
-            result = "Report for period " + dateFrom + " - " + dateTo + builder;
-        } catch (DateTimeParseException exc) {
-            System.out.println("Date Parameters are incorrect.");
+            builder.append(System.lineSeparator());
+            builder.append(name);
+            builder.append(" - ");
+            builder.append(salaryTotal);
         }
-        return result;
+        return builder.toString();
     }
 }
