3c763fd5a744164d1c587dea9c03a5ccdaee4ef3
Evgen Lanovenko <ev.lanovenko@gmail.com>
2021-11-29 19:46:58 +0200

    changes made

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -2,7 +2,6 @@ package core.basesyntax;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
 
 public class SalaryInfo {
     private final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
@@ -11,36 +10,31 @@ public class SalaryInfo {
         if (names == null || data == null || dateFrom == null || dateTo == null) {
             return "";
         }
-        StringBuilder sb = new StringBuilder();
+        StringBuilder report = new StringBuilder();
 
-        sb.append("Report for period ").append(dateFrom).append(" - ")
+        report.append("Report for period ").append(dateFrom).append(" - ")
                 .append(dateTo).append(System.lineSeparator());
         for (int i = 0; i < names.length; i++) {
             int sum = 0;
-            sb.append(names[i]).append(" - ");
+            report.append(names[i]).append(" - ");
             for (int j = 0; j < data.length; j++) {
-                try {
-                    LocalDate start = LocalDate.parse(dateFrom, dateTimeFormatter);
-                    LocalDate end = LocalDate.parse(dateTo, dateTimeFormatter);
-                    LocalDate dateOfArray =
-                            LocalDate.parse(data[j].split(" ", 4)[0], dateTimeFormatter);
-                    if ((dateOfArray.isAfter(start) || start.equals(dateOfArray))
+                String [] result = data[j].split(" ");
+                LocalDate start = LocalDate.parse(dateFrom, dateTimeFormatter);
+                LocalDate end = LocalDate.parse(dateTo, dateTimeFormatter);
+                LocalDate dateOfArray =
+                        LocalDate.parse(result[0], dateTimeFormatter);
+                if ((dateOfArray.isAfter(start) || start.equals(dateOfArray))
                             && (dateOfArray.isBefore(end) || end.equals(dateOfArray))) {
-                        if (names[i].equals(data[j].split(" ", 4)[1])) {
-                            sum = sum + (Integer.parseInt(data[j].split(" ", 4)[2]))
-                                    * (Integer.parseInt(data[j].split(" ", 4)[3]));
-                        }
+                    if (names[i].equals(result[1])) {
+                        sum = sum + (Integer.parseInt(result[2]))
+                                    * (Integer.parseInt(result[3]));
                     }
-                } catch (DateTimeParseException e) {
-                    throw new RuntimeException("Wrong date",e);
-                } catch (NumberFormatException e) {
-                    throw new RuntimeException("Wrong date",e);
                 }
             }
-            sb.append(sum).append(System.lineSeparator());
+            report.append(sum).append(System.lineSeparator());
         }
-        sb.setLength(sb.length() - 2);
-        System.out.println(sb);
-        return sb.toString();
+        report.setLength(report.length() - 2);
+        System.out.println(report);
+        return report.toString();
     }
 }
