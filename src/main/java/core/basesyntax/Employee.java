011151beb478eedec2272f5806e7bd754542add2
oleg <9213195@gmail.com>
2021-11-30 19:43:46 +0200

    classes Main and Employee created

---
+++ src/main/java/core/basesyntax/Employee.java
@@ -0,0 +1,6 @@
+package core.basesyntax;
+
+public class Employee {
+    private String name;
+    private int salary;
+}
---
+++ src/main/java/core/basesyntax/Main.java
@@ -0,0 +1,10 @@
+package core.basesyntax;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] name = new String[]{};
+        String[] data = new String[]{};
+        String dateFrom = "01.04.2019";
+        String dateTo = "30.04.2019";
+    }
+}
