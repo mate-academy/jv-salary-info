e7bb8cf214be55e504f013229d84ca6fd312b24b
Vitalii Moisieiev <vitalij.mojsejiv@gmail.com>
2021-12-11 23:52:49 +0200

    Implemented method getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo); it calculates salary for employees for given period inclusively and returns result as a String.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,43 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("d.M.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        LocalDate localDateFrom = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate localDateTo = LocalDate.parse(dateTo, FORMATTER);
+        String[] dataLineSplit;
+        LocalDate dataLineDate;
+        int[] salary = new int[names.length];
+        for (int i = 0; i < names.length; i++) {
+            salary[i] = 0;
+            for (String dataLine : data) {
+                dataLineSplit = dataLine.split(" ");
+                dataLineDate = LocalDate.parse(dataLineSplit[0], FORMATTER);
+                if ((dataLineDate.isAfter(localDateFrom)
+                        || dataLineDate.equals(localDateFrom))
+                        && (dataLineDate.isBefore(localDateTo)
+                        || dataLineDate.equals(localDateTo))
+                        && dataLineSplit[1].equals(names[i])) {
+                    salary[i] += Integer.parseInt(dataLineSplit[2])
+                            * Integer.parseInt(dataLineSplit[3]);
+                }
+            }
+        }
+        StringBuilder builder = new StringBuilder("");
+        builder.append("Report for period ").append(dateFrom).append(" - ")
+                .append(dateTo).append(System.lineSeparator());
+        for (int i = 0; i < names.length; i++) {
+            if (i != names.length - 1) {
+                builder.append(names[i]).append(" - ").append(salary[i])
+                        .append(System.lineSeparator());
+            } else {
+                builder.append(names[i]).append(" - ").append(salary[i]);
+            }
+        }
+        return builder.toString();
     }
 }
