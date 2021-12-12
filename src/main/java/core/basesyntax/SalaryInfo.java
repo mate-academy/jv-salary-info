114680b20e9678a9b212a29a6965e46a620e80d9
Vitaliy Zahrenko <vzagorenko@gmail.com>
2021-12-12 23:33:29 +0200

    all test have been passed

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,49 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+
+        //result string initiated by  head "Report for period dd.mm.yyyy  - dd.mm.yyyy  "
+        StringBuilder reportString = new StringBuilder("Report for period ");
+        reportString.append(dateFrom).append(" - ").append(dateTo);
+
+        //set global formatter for date
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDate startDate = LocalDate.parse(dateFrom,formatter);
+        LocalDate endDate = LocalDate.parse(dateTo,formatter);
+        LocalDate currDate;
+
+        //salary array same as manes relation index to index
+        int[] salarySum = new int[names.length];
+
+        for (String record : data) {
+            String[] recordString = record.split(" ");
+
+            try {
+                currDate = LocalDate.parse(recordString[0],formatter);
+            } catch (Exception e) {
+                continue;
+            }
+            if ((currDate.equals(startDate) || currDate.equals(endDate))
+                    || (currDate.isAfter(startDate) && currDate.isBefore(endDate))) {
+                for (int i = 0; i < names.length; i++) {
+                    if (recordString[1].equals(names[i])) {
+                        salarySum[i] = salarySum[i]
+                                + Integer.parseInt(recordString[2])
+                                * Integer.parseInt(recordString[3]);
+                        break;
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < names.length; i++) {
+            reportString.append("\n").append(names[i]).append(" - ").append(salarySum[i]);
+        }
+
+        return reportString.toString();
     }
 }
