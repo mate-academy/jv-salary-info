bf631701214c377b9876faf9a2f4c1092f106e08
Tsymbal Sergey <sfisher@ukr.net>
2021-12-12 18:44:28 +0200

    salary info is done

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,32 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+    public static final DateTimeFormatter FORMAT = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        LocalDate localDateFrom = LocalDate.parse(dateFrom, FORMAT);
+        LocalDate localDateTo = LocalDate.parse(dateTo, FORMAT);
+        StringBuilder result = new StringBuilder("Report for period " + dateFrom + " - " + dateTo);
+        for (String n : names) {
+            String salaryForName = n + " - " + 0;
+            int salary = 0;
+            for (String d : data) {
+                String[] parts = d.split(" ");
+                LocalDate now = LocalDate.parse(parts[0], FORMAT);
+                if (n.equals(parts[1])
+                        && (now.isAfter(localDateFrom) || now.isEqual(localDateFrom))
+                        && (now.isBefore(localDateTo) || now.isEqual(localDateTo))
+                ) {
+                    salary += Integer.parseInt(parts[2]) * Integer.parseInt(parts[3]);
+                    salaryForName = n + " - " + salary;
+                }
+            }
+            result.append(System.lineSeparator()).append(salaryForName);
+        }
+
+        return result.toString();
     }
 }
