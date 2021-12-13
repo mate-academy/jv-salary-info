0a00b3c5b1b6820c46781cbedd4222dc69c9e554
Tsymbal Sergey <sfisher@ukr.net>
2021-12-13 20:53:36 +0200

    added fixes after review

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,14 +4,13 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    public static final DateTimeFormatter FORMAT = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final DateTimeFormatter FORMAT = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate localDateFrom = LocalDate.parse(dateFrom, FORMAT);
         LocalDate localDateTo = LocalDate.parse(dateTo, FORMAT);
         StringBuilder result = new StringBuilder("Report for period " + dateFrom + " - " + dateTo);
         for (String name : names) {
-            String salaryForName = name + " - " + 0;
             int salary = 0;
             for (String d : data) {
                 String[] parts = d.split(" ");
@@ -21,10 +20,10 @@ public class SalaryInfo {
                         && (now.isBefore(localDateTo) || now.isEqual(localDateTo))
                 ) {
                     salary += Integer.parseInt(parts[2]) * Integer.parseInt(parts[3]);
-                    salaryForName = name + " - " + salary;
+
                 }
             }
-            result.append(System.lineSeparator()).append(salaryForName);
+            result.append(System.lineSeparator()).append(name).append(" - ").append(salary);
         }
 
         return result.toString();
