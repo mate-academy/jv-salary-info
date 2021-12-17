51ab066310d55dcbcab9c06e04a08e0ad6a87ec6
Vitaliy Bushovskiy <Bushovskiy@gmail.com>
2021-12-17 08:06:58 +0200

    fixedAfterReviewVers1

--- src/main/java/core/basesyntax/academy/SalaryInfo.java
+++ src/main/java/core/basesyntax/academy/SalaryInfo.java
@@ -7,49 +7,32 @@ import java.time.format.DateTimeParseException;
 public class SalaryInfo {
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        StringBuilder stringBuilder = new StringBuilder();
-        for (String str : data) {
-            stringBuilder.append(str).append(" ");
-        }
-        String strResult = stringBuilder.toString();
-        String[] matrixFromStrResult = strResult.split(" ");
-        StringBuilder stringBuilderForPeriod = new StringBuilder();
-        final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.uuuu");
-        LocalDate start = LocalDate.parse(dateFrom, formatter);
-        LocalDate stop = LocalDate.parse(dateTo, formatter);
-        for (int k = 0; k < matrixFromStrResult.length; k += 4) {
-            try {
-                LocalDate today = LocalDate.parse(matrixFromStrResult[k], formatter);
-                boolean intervalContainsToday = ((today.isAfter(start) || start.equals(today))
-                        && (today.isBefore(stop) || stop.equals(today)));
-                if (intervalContainsToday) {
-                    String str1 = matrixFromStrResult[k];
-                    String str2 = matrixFromStrResult[k + 1];
-                    String str3 = matrixFromStrResult[k + 2];
-                    String str4 = matrixFromStrResult[k + 3];
-                    stringBuilderForPeriod.append(str1).append(" ").append(str2).append(" ")
-                            .append(str3).append(" ").append(str4).append(" ");
-                }
-            } catch (DateTimeParseException exc) {
-                throw new RuntimeException("Wrong  format data", exc);
-            }
-        }
-        String strForPeriod = stringBuilderForPeriod.toString();
-        String[] matrixStrForPeriod = strForPeriod.split(" ");
-        StringBuilder stringBuilderResult = new StringBuilder("Report for period " + dateFrom
+        StringBuilder stringBuilder = new StringBuilder("Report for period " + dateFrom
                 + " - " + dateTo);
-        for (String name : names) {
-            int salary = 0;
-            for (int i = 1; i < matrixStrForPeriod.length; i += 4) {
-                if (matrixStrForPeriod[i].equals(name)) {
-                    salary = salary + (Integer.parseInt(matrixStrForPeriod[i + 1])
-                                * Integer.parseInt(matrixStrForPeriod[i + 2]));
+        final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.uuuu");
+        try {
+            LocalDate start = LocalDate.parse(dateFrom, formatter);
+            LocalDate stop = LocalDate.parse(dateTo, formatter);
+            for (String name : names) {
+                int salary = 0;
+                for (String datum : data) {
+                    String[] matrixFromstrData = datum.split(" ");
+                    LocalDate today = LocalDate.parse(matrixFromstrData[0], formatter);
+                    if ((matrixFromstrData[1].equals(name)) && (today.isAfter(start)
+                            || start.equals(today))
+                            && (today.isBefore(stop) || stop.equals(today))) {
+                        salary = salary + (Integer.parseInt(matrixFromstrData[2])
+                                * Integer.parseInt(matrixFromstrData[3]));
+                    }
                 }
+                stringBuilder.append(System.lineSeparator()).append(name).append(" - ")
+                        .append(salary);
             }
-            stringBuilderResult.append(System.lineSeparator()).append(name).append(" - ")
-                    .append(salary);
+        } catch (DateTimeParseException exc) {
+            throw new RuntimeException("Wrong  format data", exc);
         }
-        System.out.println(stringBuilderResult);
-        return stringBuilderResult.toString();
+        System.out.println(stringBuilder);
+        return stringBuilder.toString();
     }
 }
+
