f647f157eb384dd6e6c7d5d3784a37b943eede30
Vitaliy Bushovskiy <Bushovskiy@gmail.com>
2021-12-17 11:32:28 +0200

    delSout_delTryCatch

--- src/main/java/core/basesyntax/academy/SalaryInfo.java
+++ src/main/java/core/basesyntax/academy/SalaryInfo.java
@@ -2,7 +2,6 @@ package core.basesyntax.academy;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
 
 public class SalaryInfo {
 
@@ -10,29 +9,23 @@ public class SalaryInfo {
         StringBuilder stringBuilder = new StringBuilder("Report for period " + dateFrom
                 + " - " + dateTo);
         final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.uuuu");
-        try {
-            LocalDate start = LocalDate.parse(dateFrom, formatter);
-            LocalDate stop = LocalDate.parse(dateTo, formatter);
-            for (String name : names) {
-                int salary = 0;
-                for (String datum : data) {
-                    String[] matrixFromstrData = datum.split(" ");
-                    LocalDate today = LocalDate.parse(matrixFromstrData[0], formatter);
-                    if ((matrixFromstrData[1].equals(name)) && (today.isAfter(start)
+        LocalDate start = LocalDate.parse(dateFrom, formatter);
+        LocalDate stop = LocalDate.parse(dateTo, formatter);
+        for (String name : names) {
+            int salary = 0;
+            for (String datum : data) {
+                String[] matrixFromstrData = datum.split(" ");
+                LocalDate today = LocalDate.parse(matrixFromstrData[0], formatter);
+                if ((matrixFromstrData[1].equals(name)) && (today.isAfter(start)
                             || start.equals(today))
                             && (today.isBefore(stop) || stop.equals(today))) {
-                        salary = salary + (Integer.parseInt(matrixFromstrData[2])
+                    salary = salary + (Integer.parseInt(matrixFromstrData[2])
                                 * Integer.parseInt(matrixFromstrData[3]));
-                    }
                 }
-                stringBuilder.append(System.lineSeparator()).append(name).append(" - ")
-                        .append(salary);
             }
-        } catch (DateTimeParseException exc) {
-            throw new RuntimeException("Wrong  format data", exc);
+            stringBuilder.append(System.lineSeparator()).append(name).append(" - ")
+                        .append(salary);
         }
-        System.out.println(stringBuilder);
         return stringBuilder.toString();
     }
 }
-
