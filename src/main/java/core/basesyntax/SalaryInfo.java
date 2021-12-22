d0e6a31698f05f644eaec1b91b960ca6b9a9544d
Mykhailo Tereshchenkov <mtwib.re@gmail.com>
2021-12-22 22:25:56 +0200

    HashMap removed

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,34 +1,37 @@
 package core.basesyntax;
 
 import java.time.LocalDate;
-import java.util.HashMap;
-import java.util.Map;
 
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate dateF = parseDate(dateFrom);
         LocalDate dateT = parseDate(dateTo);
-        Map<String, Integer> map = new HashMap<>();
-        for (String name : names) {
-            map.put(name, 0);
-        }
         for (String info : data) {
             String[] infoArr = info.split(" ");
             if ((parseDate(infoArr[0]).isAfter(dateF)
                     || parseDate(infoArr[0]).equals(dateF))
                     && (parseDate(infoArr[0]).isBefore(dateT)
                     || parseDate(infoArr[0]).equals(dateF))) {
-                if(map.get(infoArr[1]) != null) {
-                    Integer newValue = map.get(infoArr[1])
-                            + Integer.valueOf(infoArr[2]) * Integer.valueOf(infoArr[3]);
-                    map.put(infoArr[1], newValue);
+                for (int i = 0; i < names.length; i++) {
+                    String[] name = names[i].split(" ");
+                    if (name[0].equals(infoArr[1])) {
+                        names[i] = names[i] + " " + String.valueOf(Integer.valueOf(infoArr[2])
+                                * Integer.valueOf(infoArr[3]));
+                    }
                 }
             }
         }
         StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("Report for period " + dateFrom + " - " + dateTo + "\n");
-        for (Map.Entry entry : map.entrySet()) {
-            stringBuilder.append(entry.getKey() + " - " + String.valueOf(entry.getValue()) + "\n");
+        stringBuilder.append("Report for period " + dateFrom + " - " + dateTo + System.lineSeparator());
+        int sum = 0;
+        for (int i = 0; i < names.length; i++) {
+            String[] money = names[i].split(" ");
+            stringBuilder.append(money[0] + " - ");
+            sum = 0;
+            for (int j = 1; j < money.length; j++) {
+                sum += Integer.valueOf(money[j]);
+            }
+            stringBuilder.append(String.valueOf(sum) + System.lineSeparator());
         }
         return stringBuilder.toString();
     }
