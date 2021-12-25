f27bbf384b6b4c79b6f5f4e078afe6fbe8a6dbb5
Natalia Kurhanska <kurhnatali@gmail.com>
2021-12-25 22:45:10 +0200

    implemented getSalaryInfo(), added MainSalaryInfo class

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,36 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class SalaryInfo {
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        LocalDate dateFromPeriod = LocalDate.parse(dateFrom, formatter);
+        LocalDate dateToPeriod = LocalDate.parse(dateTo, formatter);
+        StringBuilder builder = new StringBuilder(" Report for period " + dateFrom + " - " + dateTo
+                + System.lineSeparator());
+        for (String name : names) {
+            int salary = 0;
+            builder.append(name).append(" - ");
+            for (String pr : data) {
+                String[] array = pr.split(" ");
+                if (name.equals(array[1])) {
+                    try {
+                        LocalDate date = LocalDate.parse(array[0], formatter);
+                        if (date.isAfter(dateFromPeriod) && date.isBefore(dateToPeriod)
+                                || date.equals(dateFromPeriod) || date.equals(dateToPeriod)) {
+                            salary += Integer.parseInt(array[2]) * Integer.parseInt(array[3]);
+                        }
+                    } catch (DateTimeParseException e) {
+                        throw new IllegalArgumentException("Invalid format for date", e);
+                    }
+                }
+            }
+            builder.append(salary).append(System.lineSeparator());
+        }
+        return builder.toString().trim();
     }
 }
