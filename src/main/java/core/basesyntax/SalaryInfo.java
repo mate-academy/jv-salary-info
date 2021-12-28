93ac94aa02920ff7a90d07ec0c5eab55cea4f4c6
Sonya Ryabchun <ryabchunsofa@gmail.com>
2021-12-28 22:56:37 +0200

    commit before adding exception

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,50 @@
 package core.basesyntax;
 
 public class SalaryInfo {
+    public boolean isLessOrEqual(String firstDate, String secondDate) {
+        String[] first = firstDate.split("\\.");
+        String[] second = secondDate.split("\\.");
+        if (Integer.parseInt(first[2]) < Integer.parseInt(second[2])) {
+            return true;
+        }
+        if (Integer.parseInt(first[2]) > Integer.parseInt(second[2])) {
+            return false;
+        }
+        if (Integer.parseInt(first[1]) < Integer.parseInt(second[1])) {
+            return true;
+        }
+        if (Integer.parseInt(first[1]) > Integer.parseInt(second[1])) {
+            return false;
+        }
+        if (Integer.parseInt(first[0]) < Integer.parseInt(second[1])) {
+            return true;
+        }
+        return Integer.parseInt(first[0]) <= Integer.parseInt(second[0]);
+    }
+
+    public int findIndexOfName(String[] names, String name) {
+        for (int i = 0; i < names.length; i++) {
+            if (names[i].equals(name)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        int[] salaries = new int[names.length];
+        for (String info : data) {
+            String[] separatedInfo = info.split(" ");
+            if (isLessOrEqual(separatedInfo[0], dateTo)
+                    && isLessOrEqual(dateFrom, separatedInfo[0])) {
+                salaries[findIndexOfName(names, separatedInfo[1])]
+                        += Integer.parseInt(separatedInfo[2]) * Integer.parseInt(separatedInfo[3]);
+            }
+        }
+        String result = "Report for period " + dateFrom + " - " + dateTo;
+        for (int i = 0; i < names.length; i++) {
+            result += System.lineSeparator() + names[i] + " - " + salaries[i];
+        }
+        return result;
     }
 }
