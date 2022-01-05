0476e10ea59525b418d7f2ca7bd223ee828e7e8d
Nikita Melnikov <bam19921303@gmail.com>
2022-01-05 19:03:53 +0200

    -Created the first loop to select a name. -The second loop determines the required dates and calculates the salary.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,9 +5,9 @@ import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        LocalDate dateFromDate = LocalDate.parse(dateFrom, FORMATTER);
-        LocalDate dateToDate = LocalDate.parse(dateTo, FORMATTER);
+        final DateTimeFormatter Formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDate dateFromDate = LocalDate.parse(dateFrom, Formatter);
+        LocalDate dateToDate = LocalDate.parse(dateTo, Formatter);
         StringBuilder salaryInfo = new StringBuilder("Report for period "
                 + dateFrom + " - " + dateTo);
         for (String namesEmployees : names) {
@@ -15,7 +15,7 @@ public class SalaryInfo {
             salaryInfo.append(System.lineSeparator()).append(namesEmployees).append(" - ");
             for (int d = 0; d < data.length; d++) {
                 String[] period = data[d].split(" ");
-                LocalDate dateNow = LocalDate.parse(period[0], FORMATTER);
+                LocalDate dateNow = LocalDate.parse(period[0], Formatter);
                 if (namesEmployees.equals(period[1]) && dateNow.isAfter(dateFromDate)
                         && dateNow.isBefore(dateToDate.plusDays(1))) {
                     salary = salary + (Integer.parseInt(period[2]) * Integer.parseInt(period[3]));
