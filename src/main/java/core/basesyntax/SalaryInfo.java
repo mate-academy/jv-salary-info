76d7bb629cd1b0f7607deadb299273170194c743
Nikita Melnikov <bam19921303@gmail.com>
2022-01-06 13:09:06 +0200

    Added constant field FORMATTER

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,10 +4,11 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        final DateTimeFormatter Formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        LocalDate dateFromDate = LocalDate.parse(dateFrom, Formatter);
-        LocalDate dateToDate = LocalDate.parse(dateTo, Formatter);
+        LocalDate dateFromDate = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate dateToDate = LocalDate.parse(dateTo, FORMATTER);
         StringBuilder salaryInfo = new StringBuilder("Report for period "
                 + dateFrom + " - " + dateTo);
         for (String namesEmployees : names) {
@@ -15,7 +16,7 @@ public class SalaryInfo {
             salaryInfo.append(System.lineSeparator()).append(namesEmployees).append(" - ");
             for (int d = 0; d < data.length; d++) {
                 String[] period = data[d].split(" ");
-                LocalDate dateNow = LocalDate.parse(period[0], Formatter);
+                LocalDate dateNow = LocalDate.parse(period[0], FORMATTER);
                 if (namesEmployees.equals(period[1]) && dateNow.isAfter(dateFromDate)
                         && dateNow.isBefore(dateToDate.plusDays(1))) {
                     salary = salary + (Integer.parseInt(period[2]) * Integer.parseInt(period[3]));
