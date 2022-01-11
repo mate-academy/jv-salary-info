ec714d52e6f0bf2f992aaf186b6e5b28d911c16d
Ira Zatsnova <a.ir@outlook.com>
2022-01-11 21:26:26 +0200

    Implemented method getSalaryInfo

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,38 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class SalaryInfo {
+    public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        StringBuilder builder = new StringBuilder();
+        int salaryDay;
+        for (String name : names) {
+            salaryDay = 0;
+            for (String dataValue : data) {
+                String[] dataSplit = dataValue.split(" ");
+                boolean cd1 = compareDates(dataSplit[0], dateFrom) >= 0;
+                boolean cd2 = compareDates(dataSplit[0], dateTo) <= 0;
+                if (cd1 && cd2 && dataSplit[1].equals(name)) {
+                    int hour = Integer.parseInt(dataSplit[2]);
+                    int income = Integer.parseInt(dataSplit[3]);
+                    salaryDay = salaryDay + hour * income;
+                }
+            }
+            builder.append(System.lineSeparator()).append(name).append(" - ").append(salaryDay);
+        }
+        return "Report for period " + dateFrom + " - " + dateTo + builder;
+    }
+
+    public int compareDates(String date1, String date2) {
+        try {
+            return LocalDate.parse(date1, FORMATTER).compareTo(LocalDate.parse(date2, FORMATTER));
+        } catch (DateTimeParseException exc) {
+            System.out.println("Strings cannot be parsed");
+            throw exc;
+        }
     }
 }
