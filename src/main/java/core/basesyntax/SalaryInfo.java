19b1332e7a654d30cefa4a577057fa38864318b3
savchenkoarts <251088>
2022-01-12 13:43:46 +0200

    Fixed after review

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,39 +4,42 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        int income;
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
+
+        LocalDate parseDateFrom = LocalDate.parse(dateFrom, formatter);
+        LocalDate parseDateTo = LocalDate.parse(dateTo, formatter);
+
+        StringBuilder report = new StringBuilder();
+        report.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
         for (String name : names) {
-            income = 0;
+            int income = 0;
             for (String datum : data) {
                 String[] getInfoWithDate = datum.split(" ");
-                if (checkDate(name, getInfoWithDate[1], getInfoWithDate[0], dateFrom, dateTo)) {
+                if (checkDate(name, getInfoWithDate[1], getInfoWithDate[0],
+                        parseDateFrom, parseDateTo)) {
                     if (name.equals(getInfoWithDate[1])) {
                         income = income + (Integer.parseInt(getInfoWithDate[2])
                                 * Integer.parseInt(getInfoWithDate[3]));
                     }
                 }
             }
-            if (stringBuilder.length() == 1) {
-                stringBuilder.append(name).append(" - ").append(income);
+            if (report.length() == 1) {
+                report.append(name).append(" - ").append(income);
             } else {
-                stringBuilder.append("\n").append(name).append(" - ").append(income);
+                report.append("\n").append(name).append(" - ").append(income);
             }
         }
-        return stringBuilder.toString();
+        return report.toString();
     }
 
-    protected boolean checkDate(String name, String nameDate, String date,
-                                String dateFrom, String dateTo) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private boolean checkDate(String name, String nameDate, String date,
+                              LocalDate dateFrom, LocalDate dateTo) {
         LocalDate parseDate = LocalDate.parse(date, formatter);
-        LocalDate parseDateFrom = LocalDate.parse(dateFrom, formatter);
-        LocalDate parseDateTo = LocalDate.parse(dateTo, formatter);
 
-        if (parseDate.isAfter(parseDateFrom) && parseDate.isBefore(parseDateTo)
-                || parseDate.equals(parseDateTo) && name.equals(nameDate)) {
+        if (parseDate.isAfter(dateFrom) && parseDate.isBefore(dateTo)
+                || parseDate.equals(dateTo) && name.equals(nameDate)) {
             return true;
         } else {
             return false;
