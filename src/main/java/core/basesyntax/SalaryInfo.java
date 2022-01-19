3d594ff9a6798c36e1b7830deb75d824aa91352b
LinkClink <shekaqwe@gmail.com>
2022-01-19 18:24:02 +0200

    mvn fix

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,7 +4,6 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate dateFromD = LocalDate.parse(dateFrom, DateTimeFormatter.ofPattern("dd.MM.yyyy"));
         LocalDate dateToD = LocalDate.parse(dateTo, DateTimeFormatter.ofPattern("dd.MM.yyyy"));
@@ -13,12 +12,15 @@ public class SalaryInfo {
         for (String namesS : names) {
             int salary = 0;
             result.append(System.lineSeparator()).append(namesS).append(" - ");
-            for (int d = 0; d < data.length; d++) {
-                String[] datePeriod = data[d].split(" ");
-                LocalDate dateNow = LocalDate.parse(datePeriod[0], DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-                if (namesS.equals(datePeriod[1]) && dateNow.isAfter(dateFromD)
+            for (String datum : data) {
+                String[] datePeriod = datum.split(" ");
+                LocalDate dateNow = LocalDate.parse(datePeriod[0],
+                        DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+                if (namesS.equals(datePeriod[1])
+                        && dateNow.isAfter(dateFromD)
                         && dateNow.isBefore(dateToD.plusDays(1))) {
-                    salary = salary + (Integer.parseInt(datePeriod[2]) * Integer.parseInt(datePeriod[3]));
+                    salary = salary + (Integer.parseInt(datePeriod[2])
+                            * Integer.parseInt(datePeriod[3]));
                 }
             }
             result.append(salary);
