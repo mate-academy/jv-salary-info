23d414ec232bbb3abfab498e9fdaf389cf7bc913
Iryna Khrustalova <blackkitty2204@gmail.com>
2022-01-19 15:20:30 +0100

    checkDate-method-was-added

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,32 +1,23 @@
 package core.basesyntax;
 
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        Date dateFromToDate = createDate(dateFrom);
-        Date dateToToDate = createDate(dateTo);
         StringBuilder builder = new StringBuilder();
         builder.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
-        if (dateFromToDate != null && dateToToDate != null && names != null && data != null) {
+        if (names != null && data != null) {
             for (String name: names) {
                 int moneyEarned = 0;
                 builder.append(System.getProperty("line.separator"));
                 for (String dates : data) {
                     String[] dataToMath = dates.split(" ");
+                    boolean dateCheck = checkDate(dateFrom, dateTo, dataToMath[0]);
                     if (name.equals(dataToMath[1])) {
-                        Date dayOfWork = createDate(dataToMath[0]);
-                        if (dayOfWork != null) {
-                            if (dayOfWork.equals(dateFromToDate)
-                                    || dayOfWork.after(dateFromToDate)) {
-                                if (dayOfWork.equals(dateToToDate)
-                                        || dayOfWork.before(dateToToDate)) {
-                                    moneyEarned += Integer.parseInt(dataToMath[2])
-                                            * Integer.parseInt(dataToMath[3]);
-                                }
-                            }
+                        if (dateCheck) {
+                            moneyEarned += Integer.parseInt(dataToMath[2])
+                                    * Integer.parseInt(dataToMath[3]);
                         }
                     }
                 }
@@ -36,13 +27,30 @@ public class SalaryInfo {
         return builder.toString();
     }
 
-    private Date createDate(String dateInString) {
+    private boolean checkDate(String dateFrom, String dateTo, String dateOfWork) {
+        Date dateFromToDate = createDate(dateFrom);
+        Date dateToToDate = createDate(dateTo);
+        Date dayOfWork = createDate(dateOfWork);
+        if (dateFromToDate != null && dateToToDate != null) {
+            if (dayOfWork != null) {
+                if (dayOfWork.equals(dateFromToDate)
+                        || dayOfWork.after(dateFromToDate)) {
+                    if (dayOfWork.equals(dateToToDate)
+                            || dayOfWork.before(dateToToDate)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    private final Date createDate(String dateInString) {
         try {
             Date date = new SimpleDateFormat("dd.MM.yyyy").parse(dateInString);
             return date;
-        } catch (ParseException e) {
-            e.printStackTrace();
-            return null;
+        } catch (Exception e) {
+            throw new RuntimeException("Date is incorrect", e);
         }
     }
 }
