e496ff25db30260600db49f758c8215c9c656407
Roman Kobryn <romankobryn.work@gmail.com>
2022-01-20 12:11:02 +0200

    1st commit, implemented working solution, build success with maven

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,40 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
-    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+    private static final String DATE_PATTERN = "dd.MM.yyyy";
+
+    public static String getSalaryInfo(String[] names, String[] data, String dateFrom,
+                                       String dateTo) {
+        DateTimeFormatter formatter;
+        formatter = DateTimeFormatter.ofPattern(DATE_PATTERN);
+        StringBuilder builder = new StringBuilder("Report for period " + dateFrom + " - "
+                + dateTo + System.lineSeparator());
+
+        LocalDate fromDay = LocalDate.parse(dateFrom, formatter);
+        LocalDate toDay = LocalDate.parse(dateTo, formatter);
+
+        for (int j = 0; j < names.length; j++) {
+            int totalSalary = 0;
+            for (int i = 0; i < data.length; i++) {
+                String[] info = data[i].split(" ");
+                LocalDate givenDay = LocalDate.parse(info[0], formatter);
+                if (names[j].equals(info[1])
+                        && (givenDay.isAfter(fromDay) || givenDay.isEqual(fromDay))
+                        && (givenDay.isBefore(toDay) || givenDay.isEqual(toDay))) {
+                    totalSalary += Integer.parseInt(info[2]) * Integer.parseInt(info[3]);
+                }
+            }
+            if (j == names.length - 1) {
+                builder.append(names[j]).append(" - ").append(totalSalary);
+            } else {
+                builder.append(names[j]).append(" - ").append(totalSalary)
+                        .append(System.lineSeparator());
+            }
+        }
+        String result = builder.toString();
+        return result;
     }
 }
