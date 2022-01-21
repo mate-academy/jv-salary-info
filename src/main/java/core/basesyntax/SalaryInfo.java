cbffb374b779b7492c6157726bf9b320c3cb71f2
Vladyslav Chyver <vladyslav.chyver@gmail.com>
2022-01-21 21:31:38 +0200

    code was optimized

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,41 +4,31 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter
-            .ofPattern("dd.MM.yyyy");
-    private static final int INDEX_SALARY_DATE = 0;
-    private static final int INDEX_HOURS_PER_DAY = 1;
-    private static final int INDEX_INCOME_PER_HOUR = 2;
+    private static final int WORKING_DAY = 0;
+    private static final int NAME = 1;
+    private static final int HOURS = 2;
+    private static final int INCOME = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        LocalDate dateFromInLocalDate = LocalDate.parse(dateFrom, DATE_FORMATTER);
-        LocalDate dateToInLocalDate = LocalDate.parse(dateTo, DATE_FORMATTER);
-        StringBuilder result = new StringBuilder().append("Report for period ")
-                .append(dateFrom).append(" - ").append(dateTo);
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.uuuu");
+        LocalDate localDateFrom = LocalDate.parse(dateFrom, dateTimeFormatter);
+        LocalDate localDateTo = LocalDate.parse(dateTo, dateTimeFormatter);
+        StringBuilder result = new StringBuilder();
+        result.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
         for (String name : names) {
-            int totalSalary = 0;
-            for (String stringOfData : data) {
-                LocalDate salaryDate = LocalDate
-                        .parse(parseData(stringOfData)[INDEX_SALARY_DATE], DATE_FORMATTER);
-                if (stringOfData.contains(name)
-                        && salaryDate.compareTo(dateFromInLocalDate) >= 0
-                        && salaryDate.compareTo(dateToInLocalDate) <= 0) {
-                    Integer hoursPerDay = Integer
-                            .parseInt(parseData(stringOfData)[INDEX_HOURS_PER_DAY]);
-                    Integer incomePerHour = Integer
-                            .parseInt(parseData(stringOfData)[INDEX_INCOME_PER_HOUR]);
-                    totalSalary += hoursPerDay * incomePerHour;
+            int salary = 0;
+            for (String line : data) {
+                String[] elements = line.split(" ");
+                if (elements[NAME].equals(name)) {
+                    LocalDate localDateInLine = LocalDate.parse(elements[WORKING_DAY], dateTimeFormatter);
+                    if (localDateInLine.compareTo(localDateFrom) >= 0
+                            && localDateInLine.compareTo(localDateTo) <= 0) {
+                        salary += Integer.parseInt(elements[INCOME]) * Integer.parseInt(elements[HOURS]);
+                    }
                 }
             }
-            result.append(System.lineSeparator()).append(name).append(" - ").append(totalSalary);
+            result.append(System.lineSeparator()).append(name).append(" - ").append(salary);
         }
         return result.toString();
-    }
-
-    private String[] parseData(String stringOfData) {
-        String salaryDate = stringOfData.substring(0, stringOfData.indexOf(" "));
-        String hoursPerDay = stringOfData.replaceAll(".*\\w+ (\\d+) .*", "$1");
-        String incomePerHour = stringOfData.substring(stringOfData.lastIndexOf(" ") + 1);
-        return new String[]{salaryDate, hoursPerDay, incomePerHour};
     }
 }
