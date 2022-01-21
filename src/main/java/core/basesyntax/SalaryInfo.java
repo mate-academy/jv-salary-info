7299549d45adb2f873c8f4406f154e897d79d66a
Yurii Tomashevskyi <niteris92@gmail.com>
2022-01-21 15:12:38 +0200

    added solution

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,36 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        int salary;
+        int hours;
+        LocalDate fromDate = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate toDate = LocalDate.parse(dateTo, FORMATTER);
+        StringBuilder stringBuilder =
+                new StringBuilder("Report for period ")
+                        .append(dateFrom).append(" - ").append(dateTo);
+        for (String string : names) {
+            int sumSalary = 0;
+            String name;
+            LocalDate date;
+            for (String datum : data) {
+                name = datum.split(" ")[1];
+                date = LocalDate.parse(datum.split(" ")[0], FORMATTER);
+                hours = Integer.parseInt(datum.split(" ")[2]);
+                salary = Integer.parseInt(datum.split(" ")[3]);
+                if ((date.isAfter(fromDate) && date.isBefore(toDate)
+                        || date.isEqual(toDate)) && string.equals(name)) {
+                    sumSalary += hours * salary;
+                }
+            }
+            stringBuilder.append(System.lineSeparator()).append(string)
+                    .append(" - ").append(sumSalary);
+        }
+        return stringBuilder.toString();
     }
 }
