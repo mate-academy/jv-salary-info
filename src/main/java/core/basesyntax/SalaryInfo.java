2f77b70cda5a1e1e7d8ce17bea7e8436e8689aa0
Nazarii Yatsiuk <nazariiyatsiuk@gmail.com>
2022-01-23 01:27:23 +0200

    Moved declaration of varuable salaryAmount from method into class body and made some style fixes

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -9,14 +9,16 @@ public class SalaryInfo {
     private static final int SALARY_INDEX = 2;
     private static final int HOURS_INDEX = 3;
     private static final DateTimeFormatter FORMAT = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private int salaryAmount;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder builder = new StringBuilder()
                 .append("Report for period ").append(dateFrom)
                 .append(" - ").append(dateTo);
         for (int i = 0; i < names.length; i++) {
-            int salaryAmount = 0;
-            builder.append(System.lineSeparator()).append(names[i]).append(" - ");
+            salaryAmount = 0;
+            builder.append(System.lineSeparator())
+                    .append(names[i]).append(" - ");
             for (int j = 0; j < data.length; j++) {
                 if (names[i].equals(data[j].split(" ")[NAME_INDEX])) {
                     if (LocalDate.parse(data[j].split(" ")[DATE_INDEX], FORMAT)
