4d9cba67b3d77d5b0980643e5846fe75d651412b
Ivan Pavluchuk <java.python10@gmail.com>
2022-01-23 07:57:15 +0200

    added solution

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,35 @@
 package core.basesyntax;
 
 public class SalaryInfo {
-    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+
+    static String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
+        String [] b = {};
+        int sal = 0;
+        String result = "";
+        result = "Report for period " + dateFrom + " - " + dateTo + System.lineSeparator();
+        for (int j = 0; j < names.length; j++) {
+            sal = 0;
+            for (int i = 0; i < data.length; i++) {
+                b = data[i].split(" ");
+                if ((names [j].equals(b [1])
+                        && ((Integer.parseInt(data [i].substring(3, 5))
+                        < Integer.parseInt(dateTo.substring(3, 5)))
+                        || (Integer.parseInt(data [i].substring(3, 5))
+                        == Integer.parseInt(dateTo.substring(3, 5)))
+                        && Integer.parseInt(data [i].substring(0, 2))
+                        <= Integer.parseInt(dateTo.substring(0, 2))))
+                        && ((Integer.parseInt(data [i].substring(3, 5))
+                        > Integer.parseInt(dateFrom.substring(3, 5)))
+                        || (Integer.parseInt(data [i].substring(3, 5))
+                        == Integer.parseInt(dateFrom.substring(3, 5)))
+                        && (Integer.parseInt(data [i].substring(0, 2))
+                        >= Integer.parseInt(dateFrom.substring(0, 2))))) {
+                    sal = sal + Integer.parseInt(b [2]) * Integer.parseInt(b [3]);
+                }
+            }
+            result = (j != names.length - 1) ? result + names [j] + " - "
+                    + sal + System.lineSeparator() : result + names [j] + " - " + sal;
+        }
+        return result;
     }
 }
