fc168c560f28c5d6202b42dc2e6fb6d0109829f8
DenTima <tymoshchuk.den@gmail.com>
2022-01-23 22:24:53 +0200

    Comleted task

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,31 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDate dateF = LocalDate.parse(dateFrom, dateTimeFormatter);
+        LocalDate dateT = LocalDate.parse(dateTo, dateTimeFormatter);
+        StringBuilder dataResult = new StringBuilder("Report for period "
+                + dateFrom + " - " + dateTo);
+        for (String name : names) {
+            dataResult.append(System.lineSeparator()).append(name + " - ");
+            int salarySum = 0;
+            for (String dataCount : data) {
+                LocalDate currentDate = LocalDate.parse(dataCount.split(" ")[0], dateTimeFormatter);
+                String currentName = dataCount.split(" ")[1];
+                if (currentName.equals(name) && (dateF.isBefore(currentDate)
+                        && dateT.isAfter(currentDate)
+                        || dateF.isEqual(currentDate)
+                        || dateT.isEqual(currentDate))) {
+                    salarySum += Integer.parseInt(dataCount.split(" ")[3])
+                        * Integer.parseInt(dataCount.split(" ")[2]);
+                }
+            }
+            dataResult.append(salarySum);
+        }
+        return dataResult.toString();
     }
 }
