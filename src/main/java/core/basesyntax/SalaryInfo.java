797825996184b87c1d7158f04f51b10bad4d2cb5
igoryan <zimyshkaa@gmail.com>
2022-01-23 19:14:39 +0200

    method get salary info was implemented

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,31 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
-    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter
+            .ofPattern("dd.MM.yyyy");
+
+    public static String getSalaryInfo(String[] names, String[] data,
+                                       String dateFrom, String dateTo) {
+        LocalDate localDateFrom = LocalDate.parse(dateFrom, DATE_TIME_FORMATTER);
+        LocalDate localDateTo = LocalDate.parse(dateTo, DATE_TIME_FORMATTER);
+        StringBuilder report = new StringBuilder("Report for period ")
+                .append(dateFrom).append(" - ").append(dateTo);
+        for (String name : names) {
+            int salary = 0;
+            for (String datum : data) {
+                String[] splitData = datum.split(" ");
+                LocalDate date = LocalDate.parse(splitData[0], DATE_TIME_FORMATTER);
+                if (name.equals(splitData[1])
+                        && ((date.isAfter(localDateFrom) && date.isBefore(localDateTo))
+                        || date.equals(localDateFrom) || date.equals(localDateTo))) {
+                    salary += Integer.parseInt(splitData[2]) * Integer.parseInt(splitData[3]);
+                }
+            }
+            report.append(System.lineSeparator()).append(name).append(" - ").append(salary);
+        }
+        return report.toString();
     }
 }
