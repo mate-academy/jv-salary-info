253efa1625dd1cdefae6405a957d0b60ef6d23a9
Dmytro Lemeshchenko <dimmonishe@gmail.com>
2022-01-25 00:24:37 +0200

    Project started

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -2,6 +2,7 @@ package core.basesyntax;
 
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+
+        return "";
     }
 }
