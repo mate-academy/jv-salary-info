8f54df13d601748a9d4bb7a581294b4439b8ebd3
Sasha Bachynskyi <allfred12@gmail.com>
2022-01-24 13:12:37 +0200

    added private static final fields

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,40 +4,43 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int INDEX_FOR_NAMES = 0;
+    private static final int INDEX_FOR_DATA = 0;
+    private static final int INDEX_FOR_DATE_IN_DATA = 0;
+    private static final int INDEX_FOR_NAME_IN_DATA = 1;
+    private static final int INDEX_FOR_HOURS_IN_DATA = 2;
+    private static final int INDEX_FOR_SALARY_PER_HOUR_IN_DATA = 3;
+
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         int[] salaries = new int[names.length];
-
         LocalDate localDateFrom = LocalDate.parse(dateFrom, FORMATTER);
         LocalDate localDateTo = LocalDate.parse(dateTo, FORMATTER);
-
-        for (int i = 0; i < names.length; i++) {
-            for (int j = 0; j < data.length; j++) {
+        for (int i = INDEX_FOR_NAMES; i < names.length; i++) {
+            for (int j = INDEX_FOR_DATA; j < data.length; j++) {
                 String[] temp = data[j].split(" ");
-                LocalDate currentDate = LocalDate.parse(temp[0], FORMATTER);
-
-                if (temp[1].equals(names[i])
+                LocalDate currentDate = LocalDate.parse(temp[INDEX_FOR_DATE_IN_DATA], FORMATTER);
+                if (temp[INDEX_FOR_NAME_IN_DATA].equals(names[i])
                         && ((currentDate.isEqual(localDateFrom) || currentDate.isEqual(localDateTo))
                         || (currentDate.isAfter(localDateFrom)
                         && currentDate.isBefore(localDateTo)))) {
-                    salaries[i] += Integer.parseInt(temp[2]) * Integer.parseInt(temp[3]);
+                    salaries[i] += Integer.parseInt(temp[INDEX_FOR_HOURS_IN_DATA])
+                            * Integer.parseInt(temp[INDEX_FOR_SALARY_PER_HOUR_IN_DATA]);
                 }
             }
         }
-
         StringBuilder builder = new StringBuilder();
         builder.append("Report for period ")
                 .append(dateFrom)
                 .append(" - ")
                 .append(dateTo);
-        for (int i = 0; i < salaries.length; i++) {
+        for (int i = INDEX_FOR_NAMES; i < names.length; i++) {
             builder.append(System.lineSeparator())
                     .append(names[i])
                     .append(" - ")
                     .append(salaries[i]);
         }
-
         return builder.toString();
     }
 }
