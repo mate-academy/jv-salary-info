ba26200e5329eb648acc391ae7adf4f7a4f19421
anuytamysko <a_misko@ukr.net>
2022-01-24 13:59:18 +0200

    added constant

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,21 +5,26 @@ import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    public static final int DATE = 0;
+    public static final int NAME = 1;
+    public static final int WORKING_HOUR = 2;
+    public static final int INCOME_PER_OUR = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate dateBegin = LocalDate.parse(dateFrom, FORMATTER);
         LocalDate dateEnd = LocalDate.parse(dateTo, FORMATTER);
         StringBuilder report = new StringBuilder();
         report.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
-
+        String[] dataForDay = null;
         for (String name : names) {
             int salary = 0;
             for (String datum : data) {
-                String[] dataForDay = datum.split(" ");
-                LocalDate currentDate = LocalDate.parse(dataForDay[0], FORMATTER);
+                dataForDay = datum.split(" ");
+                LocalDate currentDate = LocalDate.parse(dataForDay[DATE], FORMATTER);
                 if ((currentDate.isAfter(dateBegin) && (currentDate.isBefore(dateEnd))
-                        || currentDate.isEqual(dateEnd)) && name.equals(dataForDay[1])) {
-                    salary += Integer.parseInt(dataForDay[2]) * Integer.parseInt(dataForDay[3]);
+                        || currentDate.isEqual(dateEnd)) && name.equals(dataForDay[NAME])) {
+                    salary += Integer.parseInt(dataForDay[INCOME_PER_OUR])
+                            * Integer.parseInt(dataForDay[WORKING_HOUR]);
                 }
             }
             report.append(System.lineSeparator()).append(name).append(" - ").append(salary);
