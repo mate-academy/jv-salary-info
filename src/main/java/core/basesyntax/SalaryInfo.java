6c999f117b0d0a229799beb8704ef56001ee5532
Alex Drapikovsky <drapikovskyan@gmail.com>
2022-01-24 14:41:46 +0200

    JC-SalaryInfo. fixed 2.0. I hope I understood correctly

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,6 +4,13 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    private static final DateTimeFormatter DATE_TIME_FORMATTER
+            = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int INDEX_WORK_DAY = 0;
+    private static final int INDEX_NAME = 1;
+    private static final int INDEX_WORKING_HOUR = 2;
+    private static final int INDEX_INCOME = 3;
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
@@ -15,12 +22,12 @@ public class SalaryInfo {
             int salary = 0;
             for (String s : data) {
                 String[] splitDataUnit = s.split(" ");
-                if (name.equals(splitDataUnit[1])) {
-                    workDayDate = getDateFromString(splitDataUnit[0]);
+                if (name.equals(splitDataUnit[INDEX_NAME])) {
+                    workDayDate = getDateFromString(splitDataUnit[INDEX_WORK_DAY]);
                     if (workDayDate.compareTo(dateFrom1) >= 0
                             && workDayDate.compareTo(dateTo1) <= 0) {
-                        salary += (Integer.parseInt(splitDataUnit[2])
-                                * Integer.parseInt(splitDataUnit[3]));
+                        salary += (Integer.parseInt(splitDataUnit[INDEX_WORKING_HOUR])
+                                * Integer.parseInt(splitDataUnit[INDEX_INCOME]));
                     }
                 }
             }
@@ -30,7 +37,6 @@ public class SalaryInfo {
     }
 
     private static LocalDate getDateFromString(String value) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        return LocalDate.from(formatter.parse(value));
+        return LocalDate.from(DATE_TIME_FORMATTER.parse(value));
     }
 }
