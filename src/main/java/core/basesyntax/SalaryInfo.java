7b9d6a54ab495801916be011a38fa1740ae386ab
Liliia Ponomarenko <liliya97ua@gmail.com>
2022-01-24 12:27:42 +0200

    added fixes after review

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -9,24 +9,15 @@ public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate from = LocalDate.parse(dateFrom, FORMATTER);
         LocalDate to = LocalDate.parse(dateTo, FORMATTER);
-        String[] info = new String[data.length * 4];
-        int c = 0;
-        for (String d : data) {
-            String[] one = d.split(" ");
-            info[c] = one[0];
-            info[c + 1] = one[1];
-            info[c + 2] = one[2];
-            info[c + 3] = one [3];
-            c += 4;
-        }
         StringBuilder builder = new StringBuilder("Report for period " + dateFrom + " - " + dateTo);
         int salary = 0;
         for (int i = 0; i < names.length; i++) {
-            for (int j = 0; j < info.length; j += 4) {
-                LocalDate date = LocalDate.parse(info[j], FORMATTER);
-                if (names[i].equals(info[j + 1]) && (date.isAfter(from) && date.isBefore(to)
+            for (int j = 0; j < data.length; j++) {
+                String[] lineInfo = data[j].split(" ");
+                LocalDate date = LocalDate.parse(lineInfo[0], FORMATTER);
+                if (names[i].equals(lineInfo[1]) && (date.isAfter(from) && date.isBefore(to)
                         || date.isEqual(to))) {
-                    salary += Integer.parseInt(info[j + 2]) * Integer.parseInt(info[j + 3]);
+                    salary += Integer.parseInt(lineInfo[2]) * Integer.parseInt(lineInfo[3]);
                 }
             }
             builder.append(System.lineSeparator()).append(names[i]).append(" - ").append(salary);
