7fd5de13d975b61a242c94790b69dd020494e4a8
Misha Dzhuzha <dzhuzhamichael@gmail.com>
2022-01-24 12:20:43 +0200

    class SalaryInfo fixed according to common mistakes file

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,29 +1,35 @@
 package core.basesyntax;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
-import java.util.Date;
+import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM.yyyy");
+    private static final DateTimeFormatter DATE_TIME_FORMATTER
+            = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int WORKING_HOURS_INDEX = 2;
+    private static final int INCOME_INDEX = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
-        Date startDate = parseDate(dateFrom);
-        Date endDate = parseDate(dateTo);
+        LocalDate startDate = LocalDate.parse(dateFrom, DATE_TIME_FORMATTER);;
+        LocalDate endDate = LocalDate.parse(dateTo, DATE_TIME_FORMATTER);
         for (int i = 0; i < names.length; i++) {
             int salary = 0;
             for (int j = 0; j < data.length; j++) {
                 String[] dataPart = data[j].split(" ");
-                if (dataPart[1].equals(names[i])) {
-                    Date dateFromData = parseDate(dataPart[0]);
+                if (dataPart[NAME_INDEX]
+                        .equals(names[i])) {
+                    LocalDate dateFromData = LocalDate
+                            .parse(dataPart[DATE_INDEX], DATE_TIME_FORMATTER);
                     if ((dateFromData.equals(startDate) & dateFromData.equals(endDate))
-                            || (dateFromData.after(startDate) & dateFromData.before(endDate))
-                            || (dateFromData.equals(startDate) & dateFromData.before(endDate))
-                            || (dateFromData.after(startDate) & dateFromData.equals(endDate))) {
-                        salary += Integer.parseInt(dataPart[2]) * Integer.parseInt(dataPart[3]);
+                            || ((dateFromData.isAfter(startDate) & dateFromData.isBefore(endDate))
+                            || (dateFromData.isEqual(startDate) & dateFromData.isBefore(endDate))
+                            || (dateFromData.isAfter(startDate) & dateFromData.isEqual(endDate)))) {
+                        salary += Integer.parseInt(dataPart[WORKING_HOURS_INDEX])
+                                * Integer.parseInt(dataPart[INCOME_INDEX]);
                     }
                 }
             }
@@ -31,14 +37,5 @@ public class SalaryInfo {
                     .append(" - ").append(salary);
         }
         return stringBuilder.toString();
-    }
-
-    private Date parseDate(String date) {
-        try {
-            Date parsedDate = simpleDateFormat.parse(date);
-            return parsedDate;
-        } catch (ParseException e) {
-            throw new RuntimeException("I can`t parse date" + date, e);
-        }
     }
 }
