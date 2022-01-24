17d01b22e597ca407d4e2aac20b74921dfd511b4
Ivan Pavluchuk <java.python10@gmail.com>
2022-01-24 16:08:00 +0200

    re-added solution 01

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -3,28 +3,28 @@ package core.basesyntax;
 public class SalaryInfo {
 
     static String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        String [] b = {};
+        String [] detail = {};
         int sal = 0;
         String result = "";
         result = "Report for period " + dateFrom + " - " + dateTo + System.lineSeparator();
         for (int j = 0; j < names.length; j++) {
             sal = 0;
             for (int i = 0; i < data.length; i++) {
-                b = data[i].split(" ");
-                if ((names [j].equals(b [1])
-                        && ((Integer.parseInt(data [i].substring(3, 5))
+                detail = data[i].split(" ");
+                if ((names[j].equals(detail[1])
+                        && ((Integer.parseInt(data[i].substring(3, 5))
                         < Integer.parseInt(dateTo.substring(3, 5)))
-                        || (Integer.parseInt(data [i].substring(3, 5))
+                        || (Integer.parseInt(data[i].substring(3, 5))
                         == Integer.parseInt(dateTo.substring(3, 5)))
-                        && Integer.parseInt(data [i].substring(0, 2))
+                        && Integer.parseInt(data[i].substring(0, 2))
                         <= Integer.parseInt(dateTo.substring(0, 2))))
-                        && ((Integer.parseInt(data [i].substring(3, 5))
+                        && ((Integer.parseInt(data[i].substring(3, 5))
                         > Integer.parseInt(dateFrom.substring(3, 5)))
-                        || (Integer.parseInt(data [i].substring(3, 5))
+                        || (Integer.parseInt(data[i].substring(3, 5))
                         == Integer.parseInt(dateFrom.substring(3, 5)))
-                        && (Integer.parseInt(data [i].substring(0, 2))
+                        && (Integer.parseInt(data[i].substring(0, 2))
                         >= Integer.parseInt(dateFrom.substring(0, 2))))) {
-                    sal = sal + Integer.parseInt(b [2]) * Integer.parseInt(b [3]);
+                    sal = sal + Integer.parseInt(detail[2]) * Integer.parseInt(detail[3]);
                 }
             }
             result = (j != names.length - 1) ? result + names [j] + " - "
