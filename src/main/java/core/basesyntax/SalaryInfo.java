87d6a0ac74ced4b2c10e237ff3b9f61bf1ed96b3
Alex Drapikovsky <drapikovskyan@gmail.com>
2022-01-24 13:23:38 +0200

    JC-SalaryInfo. fixed

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,25 +1,24 @@
 package core.basesyntax;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
-        Date workDayDate;
-        Date dateFrom1 = getDateFromString(dateFrom);
-        Date dateTo1 = getDateFromString(dateTo);
+        LocalDate workDayDate;
+        LocalDate dateFrom1 = getDateFromString(dateFrom);
+        LocalDate dateTo1 = getDateFromString(dateTo);
         for (String name : names) {
             stringBuilder.append(System.lineSeparator()).append(name).append(" - ");
             int salary = 0;
-            for (String dataUnit : data) {
-                String[] splitDataUnit = dataUnit.split(" ");
+            for (String s : data) {
+                String[] splitDataUnit = s.split(" ");
                 if (name.equals(splitDataUnit[1])) {
                     workDayDate = getDateFromString(splitDataUnit[0]);
-                    if ((workDayDate.after(dateFrom1) | (workDayDate.equals(dateFrom1)))
-                            && (workDayDate.before(dateTo1) | workDayDate.equals(dateTo1))) {
+                    if (workDayDate.compareTo(dateFrom1) >= 0
+                            && workDayDate.compareTo(dateTo1) <= 0) {
                         salary += (Integer.parseInt(splitDataUnit[2])
                                 * Integer.parseInt(splitDataUnit[3]));
                     }
@@ -30,14 +29,8 @@ public class SalaryInfo {
         return stringBuilder.toString();
     }
 
-    private static Date getDateFromString(String value) {
-        SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyyy");
-        Date date = new Date();
-        try {
-            date = formatter.parse(value);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        return date;
+    private static LocalDate getDateFromString(String value) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        return LocalDate.from(formatter.parse(value));
     }
 }
