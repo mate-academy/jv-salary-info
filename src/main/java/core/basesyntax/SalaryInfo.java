badfa129589763b4eaf8db01dbc60024a10e5aaa
Anastasia <nastyaprokhorenko527@gmail.com>
2022-01-24 13:03:15 +0200

    fixed mistakes

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,34 +4,30 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private static final DateTimeFormatter format = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder salaryInfo = new StringBuilder();
         salaryInfo.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
-        int[] countSalary = new int[names.length];
-        LocalDate fromD = LocalDate.parse(dateFrom, format);
-        LocalDate toD = LocalDate.parse(dateTo, format);
+        int salary;
+        LocalDate fromDate = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate toDate = LocalDate.parse(dateTo, FORMATTER);
         String[] dataDetail;
         LocalDate date;
 
-        for (int i = 0; i < data.length; i++) {
-            dataDetail = data[i].split(" ");
-            date = LocalDate.parse(dataDetail[0], format);
-            if (!(date.isBefore(fromD) || date.isAfter(toD))) {
-                for (int j = 0; j < names.length; j++) {
-                    if (dataDetail[1].equals(names[j])) {
-                        countSalary[j] += Integer.parseInt(dataDetail[2])
+        for (int i = 0; i < names.length; i++) {
+            salary = 0;
+            for (int j = 0; j < data.length; j++) {
+                dataDetail = data[j].split(" ");
+                date = LocalDate.parse(dataDetail[0], FORMATTER);
+                if (dataDetail[1].equals(names[i]) && !(date.isBefore(fromDate)
+                        || date.isAfter(toDate))) {
+                    salary += Integer.parseInt(dataDetail[2])
                                 * Integer.parseInt(dataDetail[3]);
-                        break;
-                    }
                 }
             }
-        }
-
-        for (int i = 0; i < names.length; i++) {
             salaryInfo.append(System.lineSeparator()).append(names[i]).append(" - ")
-                    .append(countSalary[i]);
+                    .append(salary);
         }
 
         return salaryInfo.toString();
