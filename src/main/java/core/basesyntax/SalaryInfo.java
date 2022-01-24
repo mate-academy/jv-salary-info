09069460b9b64fdb5948a3d1336468260480cc8f
Vadym <vadya829@gmail.com>
2022-01-24 16:35:06 +0100

    Created constants for indexes of splitRowOfData

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,7 +5,11 @@ import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-    
+    public static final int DATE_INDEX = 0;
+    public static final int NAME_INDEX = 1;
+    public static final int HOURS_INDEX = 2;
+    public static final int INCOME_PER_HOUR_INDEX = 3;
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         String[] splitRowOfData;
         StringBuilder stringBuilder = new StringBuilder("Report for period "
@@ -23,10 +27,10 @@ public class SalaryInfo {
             stringBuilder.append(" - ");
             for (String dataRow : data) {
                 splitRowOfData = dataRow.split(" ");
-                dataDate = LocalDate.parse(splitRowOfData[0], FORMATTER);
-                dataName = splitRowOfData[1];
-                dataHours = Integer.parseInt(splitRowOfData[2]);
-                dataIncomePerHour = Integer.parseInt(splitRowOfData[3]);
+                dataDate = LocalDate.parse(splitRowOfData[DATE_INDEX], FORMATTER);
+                dataName = splitRowOfData[NAME_INDEX];
+                dataHours = Integer.parseInt(splitRowOfData[HOURS_INDEX]);
+                dataIncomePerHour = Integer.parseInt(splitRowOfData[INCOME_PER_HOUR_INDEX]);
                 if (name.equals(dataName)
                         && (dataDate.isAfter(dateF)
                         && dataDate.isBefore(dateT)
