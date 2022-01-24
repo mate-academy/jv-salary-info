3f40bf0e9340355977e0cd5a08bb6f52bbe3e7b4
Liubov Chernohor <liubovchernohor@gmail.com>
2022-01-24 15:50:01 +0200

    getSalaryInfo method was updated

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -8,34 +8,29 @@ public class SalaryInfo {
     static final int NAME_NUMBER_IN_LINE = 1;
     static final int HOURS_NUMBER_IN_LINE = 2;
     static final int MONEY_PER_HOUR_NUMBER_IN_LINE = 3;
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private LocalDate startDate;
+    private LocalDate finishDate;
+    private LocalDate currentDate;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        LocalDate startDate;
-        LocalDate finishDate;
-        LocalDate carrentDate;
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
         startDate = LocalDate.parse(dateFrom, formatter);
         finishDate = LocalDate.parse(dateTo, formatter);
         StringBuilder report = new StringBuilder("Report for period ")
                 .append(dateFrom)
                 .append(" - ").append(dateTo)
                 .append(System.lineSeparator());
-        String [] dataNames = new String[data.length];
         String[] personData;
-        for (int i = 0; i < data.length; i++) {
-            personData = data[i].split(" ");
-            dataNames[i] = personData[NAME_NUMBER_IN_LINE];
-        }
         for (String name : names) {
             int salary = 0;
-            for (int j = 0; j < dataNames.length; j++) {
-                if (name.equals(dataNames[j])) {
-                    personData = data[j].split(" ");
-                    carrentDate = LocalDate.parse(personData[DATA_NUMBER_IN_LINE], formatter);
-                    if ((carrentDate.isAfter(startDate)
-                            || (carrentDate.equals(startDate)))
-                            && (carrentDate.isBefore(finishDate))
-                            || (carrentDate.equals(finishDate))) {
+            for (int i = 0; i < data.length; i++) {
+                personData = data[i].split(" ");
+                if (name.equals(personData[NAME_NUMBER_IN_LINE])) {
+                    currentDate = LocalDate.parse(personData[DATA_NUMBER_IN_LINE], formatter);
+                    if ((currentDate.isAfter(startDate)
+                            || (currentDate.equals(startDate)))
+                            && (currentDate.isBefore(finishDate))
+                            || (currentDate.equals(finishDate))) {
                         salary += Integer.parseInt(personData[HOURS_NUMBER_IN_LINE])
                                 * Integer.parseInt(personData[MONEY_PER_HOUR_NUMBER_IN_LINE]);
                     }
