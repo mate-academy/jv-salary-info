15d8f3e4e35a0089cadd790be3bc0b7c94c638e3
AprioriKS <sounddummies@gmail.com>
2022-01-24 11:49:43 +0200

    solved hw, added some methods + repair mistakes

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -2,45 +2,34 @@ package core.basesyntax;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
 
 public class SalaryInfo {
+
+    public static final DateTimeFormatter FORMATTER =
+            DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         return calculate(names, data, dateFrom, dateTo);
-    }
-
-    public LocalDate parseData(String input) {
-        final String pattern_data = "dd.MM.yyyy";
-        try {
-            DateTimeFormatter formatter =
-                    DateTimeFormatter.ofPattern(pattern_data);
-            LocalDate date = LocalDate.parse(input, formatter);
-            return date;
-        } catch (DateTimeParseException exc) {
-            System.out.printf("%s is not parsable!%n", input);
-            throw exc;
-        }
     }
 
     public String calculate(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder calculation = new StringBuilder();
         calculation.append("Report for period ")
                 .append(dateFrom).append(" - ").append(dateTo);
-        for (String inputNames : names) {
+        for (String name : names) {
             int finalSalary = 0;
-            calculation.append(System.lineSeparator()).append(inputNames).append(" - ");
-            for (int d = 0; d < data.length; d++) {
-                String[] period = data[d].split(" ");
-                LocalDate today = parseData(period[0]);
-                if (inputNames.equals(period[1]) && today.isAfter(parseData(dateFrom))
-                        && today.isBefore(parseData(dateTo).plusDays(1))) {
+            calculation.append(System.lineSeparator()).append(name).append(" - ");
+            for (int i = 0; i < data.length; i++) {
+                String[] period = data[i].split(" ");
+                LocalDate today = LocalDate.parse(period[0], FORMATTER);
+                if (name.equals(period[1]) && today.isAfter(LocalDate.parse(dateFrom, FORMATTER))
+                        && today.isBefore(LocalDate.parse(dateTo,FORMATTER).plusDays(1))) {
                     finalSalary = finalSalary
                             + (Integer.parseInt(period[2]) * Integer.parseInt(period[3]));
                 }
             }
             calculation.append(finalSalary);
         }
-
         return calculation.toString();
     }
 }
