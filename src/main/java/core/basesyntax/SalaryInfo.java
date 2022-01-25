8a343a18590a0572f848d0232adc92d6e10601d4
Viktoriia <viksonchik1996@ukr.net>
2022-01-25 22:15:01 +0200

    -created getSalaryInfo method in which grouped data for a specific period of time

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,38 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
-    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    public static final String SEPARATOR = " ";
+    public static final int DATA = 0;
+    public static final int NAME = 1;
+    public static final int HOUR = 2;
+    public static final int P_HOUR = 3;
+
+    public static String getSalaryInfo(String[] names, String[] data,
+                                       String dateFrom, String dateTo) {
+        LocalDate dateF = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate dateT = LocalDate.parse(dateTo, FORMATTER);
+        StringBuilder builder = new StringBuilder();
+        builder.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
+        int salary;
+        for (String name: names) {
+            salary = 0;
+            for (String info: data) {
+                String[] elements = info.split(SEPARATOR);
+                if (name.equals(elements[NAME])) {
+                    LocalDate hoursPerDay = LocalDate.parse(elements[DATA], FORMATTER);
+                    if (dateF.isBefore(hoursPerDay) && dateT.isAfter(hoursPerDay)
+                            || dateF.isEqual(hoursPerDay) || dateT.equals(hoursPerDay)) {
+                        salary += Integer.parseInt(elements[HOUR])
+                                * Integer.parseInt(elements[P_HOUR]);
+                    }
+                }
+            }
+            builder.append(System.lineSeparator()).append(name).append(" - ").append(salary);
+        }
+        return builder.toString();
     }
 }
