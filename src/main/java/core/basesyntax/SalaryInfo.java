d33bb7e66385d17fca8cbbe39ae4f4f7d8da0157
Valera Lemberov <valera.lemberov1993@gmail.com>
2022-01-26 12:39:29 +0200

    create constants

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -7,6 +7,9 @@ import java.util.Arrays;
 public class SalaryInfo {
     private static final DateTimeFormatter DATE_TIME_FORMATTER
             = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final String SPLETERATOR = " ";
+    private static final int INDEX_WORKING_HOUR = 2;
+    private static final int INDEX_INCOME_PER_HOUR = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder salaryBilder = new StringBuilder("Report for period "
@@ -14,9 +17,9 @@ public class SalaryInfo {
         int paimant = 0;
         for (String name : names) {
             for (String datum : data) {
-                int workingHour = Integer.parseInt(datum.split(" ")[2]);
-                int incomePerHour = Integer.parseInt(datum.split(" ")[3]);
-                boolean found = Arrays.asList(datum.split(" ")).contains(name);
+                int workingHour = Integer.parseInt(datum.split(SPLETERATOR)[INDEX_WORKING_HOUR ]);
+                int incomePerHour = Integer.parseInt(datum.split(SPLETERATOR)[INDEX_INCOME_PER_HOUR]);
+                boolean found = Arrays.asList(datum.split(SPLETERATOR)).contains(name);
                 if (found) {
                     if (getDay(datum).compareTo(getDay(dateFrom)) >= 0
                             & getDay(dateTo).compareTo(getDay(datum)) >= 0) {
@@ -31,7 +34,7 @@ public class SalaryInfo {
     }
 
     private LocalDate getDay(String value) {
-        String onlyDate = value.split(" ")[0];
+        String onlyDate = value.split(SPLETERATOR)[0];
         return LocalDate.from(DATE_TIME_FORMATTER.parse(onlyDate));
     }
 
