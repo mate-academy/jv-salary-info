33822ce91b7a667daebb03e2e447bc1b4ba87075
Sergii Turchyn <turchyn13@gmail.com>
2022-01-31 22:40:13 +0200

    fixed loops_2 loops

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -10,30 +10,32 @@ public class SalaryInfo {
         LocalDate parsedDateFrom = LocalDate.parse(dateFrom, FORMATTER);
         LocalDate parsedDateTo = LocalDate.parse(dateTo, FORMATTER);
         int[] salaryArr = new int [names.length];
-        for (int i = 0; i < data.length; i++) {
-            String[] splitData = data[i].split(" ");
-            LocalDate dataDate = LocalDate.parse(splitData[0], FORMATTER);
-            int parsedHours = Integer.parseInt(splitData[2]);
-            int parsedMoney = Integer.parseInt(splitData[3]);
-            if ((dataDate.isAfter(parsedDateFrom) || dataDate.isEqual(parsedDateFrom))
-                    && (dataDate.isBefore(parsedDateTo) || dataDate.isEqual(parsedDateTo))) {
-                for (int j = 0; j < names.length; j++) {
+
+        StringBuilder header = new StringBuilder("Report for period ");
+        header.append(parsedDateFrom.format(FORMATTER))
+                .append(" - ")
+                .append(parsedDateTo.format(FORMATTER));
+
+        for (int j = 0; j < names.length; j++) {
+            for (int i = 0; i < data.length; i++) {
+                String[] splitData = data[i].split(" ");
+                LocalDate dataDate = LocalDate.parse(splitData[0], FORMATTER);
+                int parsedHours = Integer.parseInt(splitData[2]);
+                int parsedMoney = Integer.parseInt(splitData[3]);
+
+                if ((dataDate.isAfter(parsedDateFrom) || dataDate.isEqual(parsedDateFrom))
+                        && (dataDate.isBefore(parsedDateTo) || dataDate.isEqual(parsedDateTo))) {
                     if (names[j].equals(splitData[1])) {
                         salaryArr[j] = salaryArr[j] + parsedHours * parsedMoney;
                     }
                 }
             }
-        }
-        StringBuilder header = new StringBuilder("Report for period ");
-        header.append(parsedDateFrom.format(FORMATTER))
-                .append(" - ")
-                .append(parsedDateTo.format(FORMATTER));
-        for (int i = 0; i < names.length; i++) {
             header.append(System.lineSeparator())
-                  .append(names[i])
+                    .append(names[j])
                     .append(" - ")
-                    .append(salaryArr[i]);
+                    .append(salaryArr[j]);
         }
+
         return header.toString();
     }
 
