14cb7cd0e993b15f509d17b55d130b694f5e97ee
Dmytro Chernenko <mitresko12@gmail.com>
2022-02-02 02:29:58 +0200

    check null as arguments

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -2,6 +2,10 @@ package core.basesyntax;
 
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
+        if (names == null || data == null || dateFrom == null || dateTo == null) {
+            throw new IllegalArgumentException("Arguements cannot be null");
+        }
+
         return null;
     }
 }
