121be36a83ff8a8c9daff7aba2a2b93cc0571a6e
Dmytro <mitresko12@gmail.com>
2022-02-03 18:27:11 +0200

    fixed spelling mistake

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         if (names == null || data == null || dateFrom == null || dateTo == null) {
-            throw new IllegalArgumentException("Arguements cannot be null");
+            throw new IllegalArgumentException("Argugitments cannot be null");
         }
 
         return null;
