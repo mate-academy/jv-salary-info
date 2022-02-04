fb72deae0c17486dc7de273ae46bda98d4f2a075
Kate Chernyavska <ccarts.kpi@gmail.com>
2022-02-04 20:51:55 +0200

    created private field DateTimeFormatter formatter. In method getSalaryInfo converted two date from String to LocalDate and created array Lacaldate where saved dates from array data, also created 3 arrays String for save other information from array data as names, hours, salary per hour. Also created 2 array int for save information about salary. Created loop for fill up arrays information from array data. In the other loop calculated salary and save in array salary. In the next loop build last part stringbuilder for fill up all information about employees. Print all information.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,50 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        LocalDate startDate = LocalDate.parse(dateFrom, formatter);
+        LocalDate endDate = LocalDate.parse(dateTo, formatter);
+        LocalDate[] datesFromArray = new LocalDate[data.length];
+        String[] namesFromArray = new String[data.length];
+        String[] hoursFromArray = new String[data.length];
+        String[] salaryFromArray = new String[data.length];
+        int[] salarys = new int[data.length];
+        int[] salary = new int[names.length];
+        String line = System.lineSeparator();
+        StringBuilder str = new StringBuilder("Report for period " + dateFrom + " - " + dateTo);
+
+        for (int i = 0; i < data.length; i++) {
+            String[] arr = data[i].split(" ");
+            datesFromArray[i] = LocalDate.parse(arr[0], formatter);
+            namesFromArray[i] = arr[1];
+            hoursFromArray[i] = arr[2];
+            salaryFromArray[i] = arr[3];
+        }
+
+        for (int i = 0; i < data.length; i++) {
+            for (int j = 0; j < names.length; j++) {
+                if ((datesFromArray[i].isEqual(startDate)
+                        || datesFromArray[i].isAfter(startDate))
+                        && (datesFromArray[i].isEqual(endDate)
+                        || datesFromArray[i].isBefore(endDate))) {
+                    salarys[i] = Integer.parseInt(hoursFromArray[i])
+                            * Integer.parseInt(salaryFromArray[i]);
+                }
+                if (names[j].equals(namesFromArray[i])) {
+                    salary[j] += salarys[i];
+                }
+            }
+        }
+        for (int i = 0; i < names.length; i++) {
+            str.append(line).append(names[i]).append(" - ").append(salary[i]);
+        }
+        System.out.println(str);
+
+        return str.toString();
     }
 }
