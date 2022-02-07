4c8358c1a2e8c2d18eca08af537dfe7a91574d57
Kate Chernyavska <ccarts.kpi@gmail.com>
2022-02-07 20:08:54 +0200

    deleted variables that initialized in the method as datesFromArray, namesFromArray, hoursFromArray, salaryFromArray, salarys. Initialization array salary moved to the outer loop. In the inner loop calculated salary for every name from names array. Construction strinbuilder was moved to same outer loop

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -9,41 +9,24 @@ public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate startDate = LocalDate.parse(dateFrom, formatter);
         LocalDate endDate = LocalDate.parse(dateTo, formatter);
-        LocalDate[] datesFromArray = new LocalDate[data.length];
-        String[] namesFromArray = new String[data.length];
-        String[] hoursFromArray = new String[data.length];
-        String[] salaryFromArray = new String[data.length];
-        int[] salarys = new int[data.length];
-        int[] salary = new int[names.length];
-        String line = System.lineSeparator();
         StringBuilder str = new StringBuilder("Report for period " + dateFrom + " - " + dateTo);
 
-        for (int i = 0; i < data.length; i++) {
-            String[] arr = data[i].split(" ");
-            datesFromArray[i] = LocalDate.parse(arr[0], formatter);
-            namesFromArray[i] = arr[1];
-            hoursFromArray[i] = arr[2];
-            salaryFromArray[i] = arr[3];
-        }
-
-        for (int i = 0; i < data.length; i++) {
-            for (int j = 0; j < names.length; j++) {
-                if ((datesFromArray[i].isEqual(startDate)
-                        || datesFromArray[i].isAfter(startDate))
-                        && (datesFromArray[i].isEqual(endDate)
-                        || datesFromArray[i].isBefore(endDate))) {
-                    salarys[i] = Integer.parseInt(hoursFromArray[i])
-                            * Integer.parseInt(salaryFromArray[i]);
-                }
-                if (names[j].equals(namesFromArray[i])) {
-                    salary[j] += salarys[i];
+        for (int i = 0; i < names.length; i++) {
+            int[] salary = new int[names.length];
+            for (String datum : data) {
+                String[] arr = datum.split(" ");
+                if ((LocalDate.parse(arr[0], formatter).isEqual(startDate)
+                        || LocalDate.parse(arr[0], formatter).isAfter(startDate))
+                        && (LocalDate.parse(arr[0], formatter).isEqual(endDate)
+                        || LocalDate.parse(arr[0], formatter).isBefore(endDate))) {
+                    if (names[i].equals(arr[1])) {
+                        salary[i] += Integer.parseInt(arr[2])
+                                * Integer.parseInt(arr[3]);
+                    }
                 }
             }
+            str.append(System.lineSeparator()).append(names[i]).append(" - ").append(salary[i]);
         }
-        for (int i = 0; i < names.length; i++) {
-            str.append(line).append(names[i]).append(" - ").append(salary[i]);
-        }
-        System.out.println(str);
 
         return str.toString();
     }
