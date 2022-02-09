dbf861b62d8951f1cca5c0be1c26fd05e2246a45
Dmytro <mitresko12@gmail.com>
2022-02-09 13:24:27 +0200

    Implemented SallaryInfo method

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,11 +1,54 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+
+    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        if (names == null || data == null || dateFrom == null || dateTo == null) {
-            throw new IllegalArgumentException("Argugitments cannot be null");
+        int[] totalIncomesByName = new int[names.length];
+
+        for (String datum : data) {
+            String[] words = datum.split(" ");
+
+            String date = words[0];
+            String name = words[1];
+
+            if (isDateBetween(date, dateFrom, dateTo)) {
+                for (int i = 0; i < names.length; i++) {
+                    if (name.equals(names[i])) {
+                        int hours = Integer.parseInt(words[2]);
+                        int tax = Integer.parseInt(words[3]);
+                        totalIncomesByName[i] += hours * tax;
+                    }
+                }
+            }
         }
 
-        return null;
+        StringBuilder builder = new StringBuilder();
+        builder.append("Report for period ")
+                .append(dateFrom + " - " + dateTo)
+                .append(System.lineSeparator());
+
+        for (int i = 0; i < names.length; i++) {
+            builder.append(names[i])
+                    .append(" - ")
+                    .append(totalIncomesByName[i]);
+            if (i < names.length - 1) {
+                builder.append(System.lineSeparator());
+            }
+        }
+        return builder.toString();
+    }
+
+    private boolean isDateBetween(String date, String dateFrom, String dateTo) {
+        LocalDate target = LocalDate.parse(date, formatter);
+        LocalDate from = LocalDate.parse(dateFrom, formatter);
+        LocalDate to = LocalDate.parse(dateTo, formatter);
+
+        return (target.isAfter(from) || target.isEqual(from))
+                && (target.isBefore(to) || target.isEqual(to));
     }
 }
