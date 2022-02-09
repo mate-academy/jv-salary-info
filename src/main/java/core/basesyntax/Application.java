627598ce93a1efa9510f80064cb612d38e729287
dmytro.sylenok <ulinmob@gmail.com>
2022-02-09 09:00:49 +0200

    implemented salary calculations in a given time frame.

---
+++ src/main/java/core/basesyntax/Application.java
@@ -0,0 +1,27 @@
+package core.basesyntax;
+
+public class Application {
+    public static void main(String[] args) {
+        String[] sampleNames = {"John", "Andrew", "Kate"};
+        String[] scriptArray = {
+                "25.04.2019 John 60 50",
+                "25.04.2019 Andrew 3 200",
+                "25.04.2019 Kate 10 100",
+
+                "26.04.2019 Andrew 3 200",
+                "26.04.2019 Kate 9 100",
+
+                "27.04.2019 John 7 100",
+                "27.04.2019 Kate 3 80",
+                "27.04.2019 Andrew 8 100"
+        };
+        String[] dates = {
+                "24.04.2019",
+                "25.04.2019",
+                "26.04.2019",
+                "27.04.2019"
+        };
+
+        System.out.println(new SalaryInfo().getSalaryInfo(sampleNames,scriptArray, dates[0], dates[1]));
+    }
+}
--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,31 @@
 package core.basesyntax;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        LocalDate startDate = LocalDate.parse(dateFrom, formatter);
+        LocalDate endDate = LocalDate.parse(dateTo, formatter);
+        StringBuilder result = new StringBuilder("Report for period " + dateFrom + " - " + dateTo);
+        int[] payment = new int[names.length];
+
+        for (int i = 0; i < names.length; i++) {
+            for (String data_record : data) {
+                String[] arr = data_record.split(" ");
+                if ((LocalDate.parse(arr[0], formatter).isEqual(startDate)
+                        || LocalDate.parse(arr[0], formatter).isAfter(startDate))
+                        && (LocalDate.parse(arr[0], formatter).isEqual(endDate)
+                        || LocalDate.parse(arr[0], formatter).isBefore(endDate))) {
+                    if (names[i].equals(arr[1])) {
+                        payment[i] += Integer.parseInt(arr[2])
+                                * Integer.parseInt(arr[3]);
+                    }
+                }
+            }
+            result.append(System.lineSeparator()).append(names[i]).append(" - ").append(payment[i]);
+        }
+        return result.toString();
     }
 }
