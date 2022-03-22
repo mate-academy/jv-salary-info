f187c7e26194f88849c4a98b387a03b0ebd952b7
Volodymyr Klimyuk <klimyuk054@gmail.com>
2022-03-22 17:41:00 +0200

    Implemented all necessary classes.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,58 @@
 package core.basesyntax;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class SalaryInfo {
+    SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        Employee[] arrayEmployee = new Employee[names.length];
+        for (int i = 0; i < arrayEmployee.length; i++) {
+            arrayEmployee[i] = new Employee(names[i]);
+        }
+
+        Date dateStart = getDate(dateFrom);
+        Date dateEnd = getDate(dateTo);
+
+        for (int i = 0; i < names.length; i++) {
+            int sunSalary = 0;
+            for (int j = 0; j < data.length; j++) {
+                String[] arrayData = data[j].split(" ");
+                if (names[i].equals(arrayData[1])
+                        && getDate(arrayData[0]).compareTo(dateStart) >= 0
+                        && getDate(arrayData[0]).compareTo(dateEnd) <= 0) {
+
+                    sunSalary += Integer.parseInt(arrayData[2])
+                            * Integer.parseInt(arrayData[3]);
+                }
+                arrayEmployee[i].setSalary(sunSalary);
+            }
+        }
+        return getOutputString(arrayEmployee, dateFrom, dateTo);
+    }
+
+    public Date getDate(String dateInString){
+        Date date = new Date();
+        try {
+            date = formatter.parse(dateInString);
+            return date;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return date;
+    }
+
+    public String getOutputString(Employee[] arrayEmployee, String dateFrom, String dateTo){
+        String nextLine = System.lineSeparator();
+        StringBuilder text = new StringBuilder("Report for period " + dateFrom + " - " + dateTo + nextLine);
+
+        for (int i = 0; i < arrayEmployee.length; i++) {
+            text.append(arrayEmployee[i].getName())
+                    .append(" - ")
+                    .append(arrayEmployee[i].getSalary())
+                    .append(nextLine);
+        }
+        return text.substring(0,text.length() -2);
     }
 }
