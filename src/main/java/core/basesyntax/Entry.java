c2b91700c7ac1f727e2872dd2b1bce5846589054
Dmytro Skarzhynets <d.skarzh@protonmail.com>
2022-03-22 18:35:49 +0200

    Refactor loops and parsing

--- src/main/java/core/basesyntax/Entry.java
+++ src/main/java/core/basesyntax/Entry.java
@@ -5,20 +5,16 @@ import java.time.format.DateTimeFormatter;
 
 public class Entry {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-    private static final int INDEX_OF_DATE = 0;
-    private static final int INDEX_OF_NAME = 1;
-    private static final int INDEX_OF_HOURS_WORKED = 2;
-    private static final int INDEX_OF_HOURLY_RATE = 3;
     private LocalDate date;
     private String name;
     private int hoursWorked;
     private int hourlyRate;
 
-    public Entry(String[] entryData) {
-        this.date = LocalDate.parse(entryData[INDEX_OF_DATE], FORMATTER);
-        this.name = entryData[INDEX_OF_NAME];
-        this.hoursWorked = Integer.parseInt(entryData[INDEX_OF_HOURS_WORKED]);
-        this.hourlyRate = Integer.parseInt(entryData[INDEX_OF_HOURLY_RATE]);
+    public Entry(LocalDate date, String name, int hoursWorked, int hourlyRate) {
+        this.date = date;
+        this.name = name;
+        this.hoursWorked = hoursWorked;
+        this.hourlyRate = hourlyRate;
     }
 
     public LocalDate getDate() {
--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,41 +4,51 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private StringBuilder builder = new StringBuilder();
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final String SEPARATOR = System.lineSeparator();
+    private static final int INDEX_OF_DATE = 0;
+    private static final int INDEX_OF_NAME = 1;
+    private static final int INDEX_OF_HOURS_WORKED = 2;
+    private static final int INDEX_OF_HOURLY_RATE = 3;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private String name;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        LocalDate startDate = LocalDate.parse(dateFrom, FORMATTER);
-        LocalDate endDate = LocalDate.parse(dateTo, FORMATTER);
-        int[] salaries = new int[names.length];
-        for (String entry : data) {
-            Entry currentEntry = parseEntry(entry);
-            for (int i = 0; i < names.length; i++) {
-                if (isEligible(currentEntry, names[i], startDate, endDate)) {
-                    salaries[i] += currentEntry.getMoneyEarned();
+        StringBuilder builder = new StringBuilder();
+        startDate = LocalDate.parse(dateFrom, FORMATTER);
+        endDate = LocalDate.parse(dateTo, FORMATTER);
+        int salary;
+        builder.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
+        for (String name : names) {
+            salary = 0;
+            this.name = name;
+            for (String entry : data) {
+                Entry parsedEntry = parseEntry(entry);
+                if (isEligible(parsedEntry)) {
+                    salary += parsedEntry.getMoneyEarned();
                 }
             }
-        }
-        builder.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
-        for (int i = 0; i < names.length; i++) {
-            builder.append(System.lineSeparator())
-                    .append(names[i])
-                    .append(" - ")
-                    .append(salaries[i]);
+            builder.append(SEPARATOR).append(name).append(" - ").append(salary);
         }
         return builder.toString();
     }
 
     private Entry parseEntry(String entry) {
-        String[] splitted = entry.split(" ");
-        return new Entry(splitted);
+        String[] splittedData = entry.split(" ");
+        LocalDate date = LocalDate.parse(splittedData[INDEX_OF_DATE], FORMATTER);
+        String name = splittedData[INDEX_OF_NAME];
+        int hoursWorked = Integer.parseInt(splittedData[INDEX_OF_HOURS_WORKED]);
+        int hourlyRate = Integer.parseInt(splittedData[INDEX_OF_HOURLY_RATE]);
+        return new Entry(date, name, hoursWorked, hourlyRate);
     }
 
-    private boolean isWithinRange(LocalDate date, LocalDate dateFrom, LocalDate dateTo) {
+    private boolean isWithinDateRange(LocalDate date, LocalDate dateFrom, LocalDate dateTo) {
         return !(date.isBefore(dateFrom) || date.isAfter(dateTo));
     }
 
-    private boolean isEligible(Entry entry, String name, LocalDate startDate, LocalDate endDate) {
-        return entry.getName().equals(name) && isWithinRange(entry.getDate(), startDate, endDate);
+    private boolean isEligible(Entry entry) {
+        return entry.getName().equals(name)
+             & isWithinDateRange(entry.getDate(), startDate, endDate);
     }
 }
