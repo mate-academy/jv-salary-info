7d9784b543c4efc866562a474c374268a6f5eceb
yuriikozar <yuriikozar@gmail.com>
2022-03-22 22:29:00 +0200

    Commit 2 Checkstyle Fixed v1

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,10 +1,8 @@
 package core.basesyntax;
 
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
-//God help me
+
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder builder = new StringBuilder("Report for period " + dateFrom + " - " + dateTo);
@@ -16,8 +14,9 @@ public class SalaryInfo {
 
             for (String datum : data) {
                 array = datum.split(" ");
-                if (getDateFromString(dateFrom, format).isAfter(getDateFromString(array[0], format)) ||
-                        getDateFromString(dateTo, format).isBefore(getDateFromString(array[0], format))) {
+                if (getDateFromString(dateFrom, format).isAfter(getDateFromString(array[0], format))
+                        || getDateFromString(dateTo, format)
+                        .isBefore(getDateFromString(array[0], format))) {
                     continue;
                 }
                 if (array[1].equals(name)) {
@@ -30,8 +29,8 @@ public class SalaryInfo {
         }
         return builder.toString();
     }
-    public static LocalDate getDateFromString(String string, DateTimeFormatter format)
-    {
+
+    public static LocalDate getDateFromString(String string, DateTimeFormatter format) {
         return LocalDate.parse(string, format);
     }
 }
