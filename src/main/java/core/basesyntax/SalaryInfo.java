f625fe13ebe3620278b02818cf884f301bdde9b6
AndrewLinskii <tolstuy1996@gmail.com>
2022-03-22 17:25:27 +0200

    make constant field and make more informative massage in exception

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,23 +4,25 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
 
-        DateTimeFormatter fomater = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        LocalDate localeFrom = LocalDate.parse(dateFrom, fomater);
-        LocalDate localeTo = LocalDate.parse(dateTo, fomater);
+        LocalDate localeFrom = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate localeTo = LocalDate.parse(dateTo, FORMATTER);
         int salary = 0;
         StringBuilder report = new StringBuilder(" Report for period "
                 + dateFrom + " - "
                 + dateTo + System.lineSeparator());
-
         if (localeFrom.isAfter(localeTo)) {
-            throw new RuntimeException("Invalid dates");
+            throw new RuntimeException("Invalid dates. Date from: "
+                    + localeFrom + " could'd be after  date to: "
+                    + localeTo);
         }
         for (String name : names) {
             for (String temp : data) {
                 String[] dateName = temp.split(" ");
-                LocalDate tempData = LocalDate.parse(dateName[0], fomater);
+                LocalDate tempData = LocalDate.parse(dateName[0], FORMATTER);
                 if (((tempData.isBefore(localeTo)
                         && tempData.isAfter(localeFrom))
                         || tempData.equals(localeTo))
@@ -31,7 +33,6 @@ public class SalaryInfo {
             report.append(name).append(" - ").append(salary).append(System.lineSeparator());
             salary = 0;
         }
-
         return report.toString().trim();
     }
 }
