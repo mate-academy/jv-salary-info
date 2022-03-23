05100222f17dff643d721e27430e3c9aa98c215c
Tatka <tlogatskaya96@gmail.com>
2022-03-23 17:53:30 +0200

    implemented changes

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,6 +1,5 @@
 package core.basesyntax;
 
-import java.time.DateTimeException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
@@ -10,7 +9,6 @@ public class SalaryInfo {
     private static final int NAME_INDEX = 1;
     private static final int WORKING_HOURS_INDEX = 2;
     private static final int SALARY_PER_HOUR_INDEX = 3;
-    private static final String SEPARATOR = System.lineSeparator();
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate startDate;
@@ -20,28 +18,25 @@ public class SalaryInfo {
         StringBuilder report = new StringBuilder();
         report.append("Report for period ")
                 .append(dateFrom).append(" - ")
-                .append(dateTo).append(SEPARATOR);
-        try {
-            for (String name : names) {
-                int earnedMoney = 0;
-                for (String element : data) {
-                    String[] dataLine = element.split(" ");
-                    startDate = LocalDate.parse(dateFrom, FORMATTER);
-                    endDate = LocalDate.parse(dateTo, FORMATTER);
-                    workDate = LocalDate.parse(dataLine[DATE_INDEX], FORMATTER);
-                    if (name.equals(dataLine[NAME_INDEX])
-                            && workDate.isAfter(startDate.minusDays(1))
-                            && workDate.isBefore(endDate.plusDays(1))) {
-                        earnedMoney += Integer.parseInt(dataLine[WORKING_HOURS_INDEX])
-                                * Integer.parseInt(dataLine[SALARY_PER_HOUR_INDEX]);
-                    }
+                .append(dateTo).append(System.lineSeparator());
+
+        for (String name : names) {
+            int earnedMoney = 0;
+            for (String element : data) {
+                String[] dataLine = element.split(" ");
+                startDate = LocalDate.parse(dateFrom, FORMATTER);
+                endDate = LocalDate.parse(dateTo, FORMATTER);
+                workDate = LocalDate.parse(dataLine[DATE_INDEX], FORMATTER);
+                if (name.equals(dataLine[NAME_INDEX])
+                        && workDate.isAfter(startDate.minusDays(1))
+                        && workDate.isBefore(endDate.plusDays(1))) {
+                    earnedMoney += Integer.parseInt(dataLine[WORKING_HOURS_INDEX])
+                            * Integer.parseInt(dataLine[SALARY_PER_HOUR_INDEX]);
                 }
-                report.append(name).append(" - ")
-                        .append(earnedMoney).append(SEPARATOR);
             }
-            return report.toString().trim();
-        } catch (DateTimeException e) {
-            throw new RuntimeException("Wrong data format");
+            report.append(name).append(" - ")
+                    .append(earnedMoney).append(System.lineSeparator());
         }
+        return report.toString().trim();
     }
 }
