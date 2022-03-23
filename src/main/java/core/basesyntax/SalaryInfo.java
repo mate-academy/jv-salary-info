4b6ef9749861cf92a21f5ee97e39d0b4329ce86a
Dmytro Skarzhynets <d.skarzh@protonmail.com>
2022-03-23 21:37:59 +0200

    Fix typo, refactor constants and fields

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,29 +5,26 @@ import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-    private static final String SEPARATOR = System.lineSeparator();
     private static final int INDEX_OF_DATE = 0;
     private static final int INDEX_OF_NAME = 1;
     private static final int INDEX_OF_HOURS_WORKED = 2;
     private static final int INDEX_OF_HOURLY_RATE = 3;
-    private LocalDate startDate;
-    private LocalDate endDate;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder builder = new StringBuilder();
-        startDate = LocalDate.parse(dateFrom, FORMATTER);
-        endDate = LocalDate.parse(dateTo, FORMATTER);
+        LocalDate startDate = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate endDate = LocalDate.parse(dateTo, FORMATTER);
         int salary;
         builder.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
         for (String name : names) {
             salary = 0;
             for (String entry : data) {
                 Entry parsedEntry = parseEntry(entry);
-                if (isEligible(parsedEntry, name)) {
+                if (isEligible(parsedEntry, name, startDate, endDate)) {
                     salary += parsedEntry.getMoneyEarned();
                 }
             }
-            builder.append(SEPARATOR).append(name).append(" - ").append(salary);
+            builder.append(System.lineSeparator()).append(name).append(" - ").append(salary);
         }
         return builder.toString();
     }
@@ -45,8 +42,8 @@ public class SalaryInfo {
         return !(date.isBefore(dateFrom) || date.isAfter(dateTo));
     }
 
-    private boolean isEligible(Entry entry, String name) {
+    private boolean isEligible(Entry entry, String name, LocalDate startDate, LocalDate endDate) {
         return entry.getName().equals(name)
-             & isWithinDateRange(entry.getDate(), startDate, endDate);
+            && isWithinDateRange(entry.getDate(), startDate, endDate);
     }
 }
