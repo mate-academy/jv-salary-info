d3380c7dfd09f62d278aa26dc65c8f35df30fb20
JadeRowan <rougesoul.m@gmail.com>
2022-03-23 14:20:31 +0200

    Method is implemented. All goals achived

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,42 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
-    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
+    public String getSalaryInfo(String[] names, String[] data,
+                                String dateFromString, String dateToString) {
+        LocalDate dateFrom = LocalDate.parse(dateFromString, formatter);
+        LocalDate dateTo = LocalDate.parse(dateToString, formatter);
+        StringBuilder resultMessage = new StringBuilder();
+        resultMessage.append("Report for period ").append(dateFromString)
+                .append(" - ").append(dateToString);
+        String[] recordValues;
+        int salaryPerHour;
+        int hours;
+        int totalSalary;
+        for (String name : names) {
+            totalSalary = 0;
+            for (String record : data) {
+                if (record.contains(name)
+                        && isDateActual(record.substring(0, 10), dateFrom, dateTo)) {
+                    recordValues = record.split(" ");
+                    salaryPerHour = Integer.parseInt(recordValues[2]);
+                    hours = Integer.parseInt(recordValues[3]);
+                    totalSalary += salaryPerHour * hours;
+                }
+            }
+            resultMessage.append(System.lineSeparator()).append(name)
+                    .append(" - ").append(totalSalary);
+        }
+        return resultMessage.toString();
+    }
+
+    private boolean isDateActual(String undefinedDateString, LocalDate dateFrom, LocalDate dateTo) {
+        LocalDate undefinedDate = LocalDate.parse(undefinedDateString, formatter);
+        return dateFrom.isBefore(undefinedDate) && undefinedDate.isBefore(dateTo)
+                || undefinedDate.isEqual(dateFrom) || undefinedDate.isEqual(dateTo);
     }
 }
