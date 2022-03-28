7d95db385f3ed1c8ebb41c213d2059719cbc5072
GerasimovTA-92 <olexandr.gerasimov.02@gmail.com>
2022-03-28 15:33:53 +0300

    deleted variable list

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -2,10 +2,8 @@ package core.basesyntax;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.List;
 
-public class SalaryInfo {
+public class SalaryInfo extends Exception {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
     private static final int INDEX_OF_NAME = 1;
     private static final int INDEX_OF_DATE = 0;
@@ -13,7 +11,6 @@ public class SalaryInfo {
     private static final int INDEX_OF_SALARY_PER_HOUR = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        List<String> list = new ArrayList<>();
         LocalDate from = LocalDate.parse(dateFrom, FORMATTER);
         LocalDate to = LocalDate.parse(dateTo, FORMATTER);
         StringBuilder info = new StringBuilder("Report for period ")
