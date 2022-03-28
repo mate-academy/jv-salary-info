e87f55e9d56cc700ac55d10869dc831c24130b56
Lyudmila D <dunda94lyudmila@gmail.com>
2022-03-28 19:45:57 +0300

    added static final to DateTimeFormatter

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -8,7 +8,7 @@ public class SalaryInfo {
     private static final int NAME_INDEX = 1;
     private static final int DAY_INDEX = 2;
     private static final int INCOME_INDEX = 3;
-    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate localDateFrom = LocalDate.parse(dateFrom, formatter);
