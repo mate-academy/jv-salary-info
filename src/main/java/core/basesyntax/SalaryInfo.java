d7dc723ef364fed8af6b20735f165ca6ca6fc65b
GerasimovTA-92 <olexandr.gerasimov.02@gmail.com>
2022-03-28 13:04:40 +0300

    fixed mistakes

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -2,30 +2,37 @@ package core.basesyntax;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SalaryInfo {
-    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int INDEX_OF_NAME = 1;
+    private static final int INDEX_OF_DATE = 0;
+    private static final int INDEX_OF_COUNT_HOURS = 2;
+    private static final int INDEX_OF_SALARY_PER_HOUR = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        //DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        LocalDate from = LocalDate.parse(dateFrom, formatter);
-        LocalDate to = LocalDate.parse(dateTo, formatter);
+        List<String> list = new ArrayList<>();
+        LocalDate from = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate to = LocalDate.parse(dateTo, FORMATTER);
         StringBuilder info = new StringBuilder("Report for period ")
                 .append(dateFrom).append(" - ").append(dateTo);
         int sum;
-        for (int i = 0; i < names.length; i++) {
+        for (String name : names) {
             sum = 0;
-            for (int j = 0; j < data.length; j++) {
-                String[] line = data[j].split(" ");
-                if (line[1].equals(names[i])
-                        && ((LocalDate.parse(line[0], formatter).isAfter(from)
-                        || from.equals(LocalDate.parse(line[0], formatter)))
-                        && (to.isAfter(LocalDate.parse(line[0], formatter))
-                        || to.equals(LocalDate.parse(line[0], formatter))))) {
-                    sum += Integer.parseInt(line[2]) * Integer.parseInt(line[3]);
+            for (String value : data) {
+                String[] line = value.split(" ");
+                if (line[INDEX_OF_NAME].equals(name)
+                        && ((LocalDate.parse(line[INDEX_OF_DATE], FORMATTER).isAfter(from)
+                        || from.equals(LocalDate.parse(line[INDEX_OF_DATE], FORMATTER)))
+                        && (to.isAfter(LocalDate.parse(line[INDEX_OF_DATE], FORMATTER))
+                        || to.equals(LocalDate.parse(line[INDEX_OF_DATE], FORMATTER))))) {
+                    sum += Integer.parseInt(line[INDEX_OF_COUNT_HOURS])
+                            * Integer.parseInt(line[INDEX_OF_SALARY_PER_HOUR]);
                 }
             }
-            info.append(System.lineSeparator()).append(names[i]).append(" - ").append(sum);
+            info.append(System.lineSeparator()).append(name).append(" - ").append(sum);
         }
         return info.toString();
     }
