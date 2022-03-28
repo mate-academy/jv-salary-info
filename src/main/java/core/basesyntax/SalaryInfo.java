52a4f354171031862f249015d2517ca9a53182bd
Lyudmila D <dunda94lyudmila@gmail.com>
2022-03-28 15:38:46 +0300

    created Global Constants

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,36 +4,33 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private DateTimeFormatter formatter;
-
-    public SalaryInfo() {
-        formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-    }
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int DAY_INDEX = 2;
+    private static final int INCOME_INDEX = 3;
+    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate localDateFrom = LocalDate.parse(dateFrom, formatter);
         LocalDate localDateTo = LocalDate.parse(dateTo, formatter);
 
-        StringBuilder reportStringBuilder = new StringBuilder("Report for period ");
-        reportStringBuilder.append(dateFrom).append(" - ").append(dateTo);
+        StringBuilder reportBuilder = new StringBuilder("Report for period ");
+        reportBuilder.append(dateFrom).append(" - ").append(dateTo);
         for (String name : names) {
             int salary = 0;
             for (int i = 0; i < data.length; i++) {
                 String[] splittedData = data[i].split(" ");
-                final String Date_Index = splittedData[0];
-                final String Name_Index = splittedData[1];
-                final String Day_Index = splittedData[2];
-                final String Income_Index = splittedData[3];
-                LocalDate date = LocalDate.parse(Date_Index, formatter);
+                LocalDate date = LocalDate.parse(splittedData[DATE_INDEX], formatter);
                 if ((localDateFrom.isBefore(date) || localDateFrom.equals(date))
                         && (localDateTo.isAfter(date) || localDateTo.equals(date))
-                        && name.equals(Name_Index)) {
-                    salary += Integer.parseInt(Day_Index) * Integer.parseInt(Income_Index);
+                        && name.equals(splittedData[NAME_INDEX])) {
+                    salary += Integer.parseInt(splittedData[DAY_INDEX])
+                            * Integer.parseInt(splittedData[INCOME_INDEX]);
                 }
             }
-            reportStringBuilder.append(System.lineSeparator())
+            reportBuilder.append(System.lineSeparator())
                     .append(name).append(" - ").append(salary);
         }
-        return reportStringBuilder.toString();
+        return reportBuilder.toString();
     }
 }
