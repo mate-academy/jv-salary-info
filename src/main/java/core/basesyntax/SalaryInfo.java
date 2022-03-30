5920ee57e616ec2d4ed822fc86607b985e69dfb3
Roma Yarishkin <romarijkov@gmail.com>
2022-03-30 15:03:02 +0300

    Task complete changed 2

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,24 +5,27 @@ import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int DATE_INDEX1 = 0;
+    private static final int DATE_INDEX2 = 1;
+    private static final int DATE_INDEX3 = 2;
+    private static final int DATE_INDEX4 = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        LocalDate parseDateFrom = LocalDate.parse(dateFrom, FORMATTER);
-        LocalDate parseDateTo = LocalDate.parse(dateTo, FORMATTER);
+        LocalDate parsedDateFrom = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate parsedDateTo = LocalDate.parse(dateTo, FORMATTER);
         StringBuilder report = new StringBuilder().append("Report for period ")
                 .append(dateFrom).append(" - ").append(dateTo);
-        String[] getInfoWithDate;
 
         for (String name : names) {
             int salary = 0;
             for (String dat : data) {
-                getInfoWithDate = dat.split(" ");
-                LocalDate currentDate = LocalDate.parse(getInfoWithDate[0], FORMATTER);
-                if (parseDateFrom.compareTo(currentDate) <= 0
-                        && parseDateTo.compareTo(currentDate) >= 0
-                        && getInfoWithDate[1].equals(name)) {
-                    salary += (Integer.parseInt(getInfoWithDate[3])
-                            * Integer.parseInt(getInfoWithDate[2]));
+                String[] line = dat.split(" ");
+                LocalDate currentDate = LocalDate.parse(line[DATE_INDEX1], FORMATTER);
+                if ((currentDate.isAfter(parsedDateFrom) || currentDate.isEqual(parsedDateFrom))
+                        && (currentDate.isBefore(parsedDateTo) || currentDate.isEqual(parsedDateTo))
+                        && name.equals(line[DATE_INDEX2])) {
+                    salary += (Integer.parseInt(line[DATE_INDEX4])
+                            * Integer.parseInt(line[DATE_INDEX3]));
                 }
             }
             report.append(System.lineSeparator()).append(name).append(" - ").append(salary);
