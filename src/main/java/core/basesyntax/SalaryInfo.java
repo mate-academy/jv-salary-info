db28aaab1a2fc537591fb62684ad15be9d9906dc
Vladyslav Fomenko <with.god.victorius@gmail.com>
2022-03-30 22:35:46 +0300

    constants, joined if statement, added StringBuilder instead of plain concatenation

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,33 +5,36 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class SalaryInfo {
+    private static Date FIRST_DATE;
+    private static Date SECOND_DATE;
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
+        try {
+            FIRST_DATE = new SimpleDateFormat("dd.MM.yyyy").parse(dateFrom);
+            SECOND_DATE = new SimpleDateFormat("dd.MM.yyyy").parse(dateTo);
+        } catch (ParseException e) {
+            throw new RuntimeException("Could not parse", e);
+        }
         StringBuilder builder = new StringBuilder();
         try {
-            Date firstDate = new SimpleDateFormat("dd.MM.yyyy").parse(dateFrom);
-            Date secondDate = new SimpleDateFormat("dd.MM.yyyy").parse(dateTo);
             for (String name: names) {
                 builder.append(System.lineSeparator());
                 int salary = 0;
                 for (int i = 0; i < data.length; i++) {
                     String[] dataFormat = data[i].split(" ");
                     Date tempDate = new SimpleDateFormat("dd.MM.yyyy").parse(dataFormat[0]);
-                    if (name.equals(dataFormat[1])
-                            && (firstDate.equals(tempDate) || secondDate.equals(tempDate))) {
-                        salary += Integer.parseInt(dataFormat[2]) * Integer.parseInt(dataFormat[3]);
-                    }
-                    if (name.equals(dataFormat[1])
-                            && tempDate.after(firstDate) && tempDate.before(secondDate)) {
+                    if ((name.equals(dataFormat[1])
+                            && tempDate.after(FIRST_DATE) && tempDate.before(SECOND_DATE))
+                            || (name.equals(dataFormat[1])
+                            && (FIRST_DATE.equals(tempDate) || SECOND_DATE.equals(tempDate)))) {
                         salary += Integer.parseInt(dataFormat[2]) * Integer.parseInt(dataFormat[3]);
                     }
                 }
                 builder.append(name).append(" - ").append(salary);
             }
         } catch (ParseException e) {
-            System.out.println("Something may be wrong!");
-            e.printStackTrace();
+            throw new RuntimeException("Can not parse", e);
         }
-        return "Report for period " + dateFrom + " - " + dateTo
-                + builder.toString();
+        return builder.insert(0,"Report for period " + dateFrom + " - " + dateTo).toString();
     }
 }
