ad30ebbd0f1912d20783e456abcf23fa93f5e194
Vasiliy Sheretko <vasil.sheretko@yahoo.com>
2022-03-31 13:27:10 +0300

    Made a formatter constant field

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,10 +4,11 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    private static final String PATTERN = "dd.MM.yyyy";
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern(PATTERN);
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        String pattern = "dd.MM.yyyy";
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
-        LocalDate dayFrom = LocalDate.parse(dateFrom, formatter);
+        LocalDate dayFrom = LocalDate.parse(dateFrom, FORMATTER);
         StringBuilder result = new StringBuilder();
         result.append("Report for period ").append(dateFrom).append(" - ").append(dateTo)
                 .append(System.lineSeparator());
@@ -21,8 +22,8 @@ public class SalaryInfo {
                     if (!(result.toString().contains(name))) {
                         result.append(name).append(" - ");
                     }
-                    LocalDate dayTo = LocalDate.parse(dateTo, formatter);
-                    LocalDate empDays = LocalDate.parse(processData[0], formatter);
+                    LocalDate dayTo = LocalDate.parse(dateTo, FORMATTER);
+                    LocalDate empDays = LocalDate.parse(processData[0], FORMATTER);
                     if (empDays.compareTo(dayFrom) >= 0 && (dayTo.compareTo(empDays) >= 0)) {
                         salary += (Integer.parseInt(processData[2])
                                 * Integer.parseInt(processData[3]));
