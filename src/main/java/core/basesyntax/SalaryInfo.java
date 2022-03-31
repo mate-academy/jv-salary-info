20e555a5d4c45e8fcc78581ccd1a2228f5868943
Vladyslav Fomenko <with.god.victorius@gmail.com>
2022-03-31 18:57:38 +0300

    improved readability, fixed constant,lessened try catch wrap

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,36 +5,30 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class SalaryInfo {
-    private static Date FIRST_DATE;
-    private static Date SECOND_DATE;
+    private static final SimpleDateFormat FORMAT = new SimpleDateFormat("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        try {
-            FIRST_DATE = new SimpleDateFormat("dd.MM.yyyy").parse(dateFrom);
-            SECOND_DATE = new SimpleDateFormat("dd.MM.yyyy").parse(dateTo);
-        } catch (ParseException e) {
-            throw new RuntimeException("Could not parse", e);
-        }
         StringBuilder builder = new StringBuilder();
-        try {
-            for (String name: names) {
-                builder.append(System.lineSeparator());
-                int salary = 0;
-                for (int i = 0; i < data.length; i++) {
-                    String[] dataFormat = data[i].split(" ");
-                    Date tempDate = new SimpleDateFormat("dd.MM.yyyy").parse(dataFormat[0]);
+        for (String name : names) {
+            builder.append(System.lineSeparator());
+            int salary = 0;
+            for (String oneString: data) {
+                String[] dataFormat = oneString.split(" ");
+                try {
+                    Date tempDate = FORMAT.parse(dataFormat[0]);
+                    Date begin = FORMAT.parse(dateFrom);
+                    Date end = FORMAT.parse(dateTo);
                     if ((name.equals(dataFormat[1])
-                            && tempDate.after(FIRST_DATE) && tempDate.before(SECOND_DATE))
-                            || (name.equals(dataFormat[1])
-                            && (FIRST_DATE.equals(tempDate) || SECOND_DATE.equals(tempDate)))) {
+                            && ((tempDate).after(begin) && tempDate.before(end)
+                            || (begin.equals(tempDate) || tempDate.equals(end))))) {
                         salary += Integer.parseInt(dataFormat[2]) * Integer.parseInt(dataFormat[3]);
                     }
+                } catch (ParseException e) {
+                    throw new RuntimeException("Could not parse date", e);
                 }
-                builder.append(name).append(" - ").append(salary);
             }
-        } catch (ParseException e) {
-            throw new RuntimeException("Can not parse", e);
+            builder.append(name).append(" - ").append(salary);
         }
-        return builder.insert(0,"Report for period " + dateFrom + " - " + dateTo).toString();
+        return builder.insert(0, "Report for period " + dateFrom + " - " + dateTo).toString();
     }
 }
