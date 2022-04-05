1c26afe30a8055cf08cefb4e82b10c4abd07c0e3
Lev Ber <levberezov01@gmail.com>
2022-04-05 10:14:48 +0300

    added 3  private static final int variable, change compareTo to isBefore, isAfter and isEqual.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,30 +4,31 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    private static final int NAME_INDEX_1 = 1;
+    private static final int DATE_INDEX_2 = 2;
+    private static final int PRISE_INDEX_3 = 3;
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-
         LocalDate localDateFrom = LocalDate.parse(dateFrom, FORMATTER);
         LocalDate localDateTo = LocalDate.parse(dateTo,FORMATTER);
-        StringBuilder stringBuilder = new StringBuilder().append("Report for period ")
+        StringBuilder reportBuilder = new StringBuilder().append("Report for period ")
                 .append(dateFrom).append(" - ").append(dateTo);
-        String[] arrayOfData;
-
+        String[] splitterLine;
         for (String name: names) {
             int salary = 0;
             for (String line: data) {
-                arrayOfData = line.split(" ");
-                LocalDate correctData = LocalDate.parse(arrayOfData[0],FORMATTER);
-                if (localDateFrom.compareTo(correctData) <= 0
-                        && localDateTo.compareTo(correctData) >= 0
-                        && arrayOfData[1].equals(name)) {
-                    salary += (Integer.parseInt(arrayOfData[3])
-                            * Integer.parseInt(arrayOfData[2]));
+                splitterLine = line.split(" ");
+                LocalDate date = LocalDate.parse(splitterLine[0],FORMATTER);
+                if ((localDateFrom.isBefore(date) || localDateFrom.isEqual(date))
+                        && (localDateTo.isAfter(date) || localDateTo.isEqual(date))
+                        && splitterLine[NAME_INDEX_1].equals(name)) {
+                    salary += (Integer.parseInt(splitterLine[PRISE_INDEX_3])
+                            * Integer.parseInt(splitterLine[DATE_INDEX_2]));
                 }
             }
-            stringBuilder.append(System.lineSeparator()).append(name).append(" - ").append(salary);
+            reportBuilder.append(System.lineSeparator()).append(name).append(" - ").append(salary);
         }
-        return stringBuilder.toString();
+        return reportBuilder.toString();
     }
 }
