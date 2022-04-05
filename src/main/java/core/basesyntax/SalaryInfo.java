4e52a9d0e662628d50af4dd97601fe5531cd9376
SerhiiPankov <pankoff.serg@gmail.com>
2022-04-05 16:44:14 +0300

    fix bag - add LocalDate and DateTimeFormatter for jv-salary-info

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,37 +1,28 @@
 package core.basesyntax;
 
-import java.util.Calendar;
-import java.util.GregorianCalendar;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private static final Calendar dateFromGet = new GregorianCalendar();
-    private static final Calendar dateToGet = new GregorianCalendar();
-    private static final Calendar date = new GregorianCalendar();
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder salary = new StringBuilder();
-        dateFromGet.set(Integer.parseInt(dateFrom.split("\\.")[2]),
-                Integer.parseInt(dateFrom.split("\\.")[1]) - 1,
-                Integer.parseInt(dateFrom.split("\\.")[0]));
-        dateToGet.set(Integer.parseInt(dateTo.split("\\.")[2]),
-                Integer.parseInt(dateTo.split("\\.")[1]) - 1,
-                Integer.parseInt(dateTo.split("\\.")[0]) + 1);
+        LocalDate dateFromGet = LocalDate.parse(dateFrom, formatter);
+        LocalDate dateToGet = LocalDate.parse(dateTo, formatter);
         salary.append("Report for period ")
                 .append(dateFrom)
                 .append(" - ")
                 .append(dateTo);
-
         for (String name : names) {
             int salaryCounter = 0;
             salary.append(System.lineSeparator())
                     .append(name);
             for (String datum : data) {
-                date.set(Integer.parseInt(datum.split(" ")[0].split("\\.")[2]),
-                        Integer.parseInt(datum.split(" ")[0].split("\\.")[1]) - 1,
-                        Integer.parseInt(datum.split(" ")[0].split("\\.")[0]));
+                LocalDate date = LocalDate.parse(datum.split(" ")[0], formatter);
                 if (datum.split(" ")[1].equals(name)
-                        && date.after(dateFromGet)
-                        && date.before(dateToGet)) {
+                        && date.isAfter(dateFromGet)
+                        && date.isBefore(dateToGet.plusDays(1))) {
                     salaryCounter = Integer.parseInt(datum.split(" ")[2])
                             * Integer.parseInt(datum.split(" ")[3])
                             + salaryCounter;
@@ -41,7 +32,6 @@ public class SalaryInfo {
                     .append(salaryCounter);
 
         }
-
         return salary.toString();
     }
 }
