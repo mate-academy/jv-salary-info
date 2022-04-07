7713eab856ae5260caf2d100c71a5d7b0c719e17
dippp <Dipppp84@gmail.com>
2022-04-07 16:12:01 +0300

    myDecision

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,30 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
 public class SalaryInfo {
+    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        LocalDate localDateFrom = LocalDate.parse(dateFrom, formatter).minus(1, ChronoUnit.DAYS);
+        LocalDate localDateTo = LocalDate.parse(dateTo, formatter).plus(1, ChronoUnit.DAYS);
+        StringBuilder sb = new StringBuilder("Report for period " + dateFrom + " - " + dateTo);
+        for (String name : names) {
+            int moneyEarned = 0;
+            for (String workData : data) {
+                String[] split = workData.split(" ");
+                LocalDate workDay = LocalDate.parse(split[0], formatter);
+                if (name.equals(split[1]) && workDay.isAfter(localDateFrom) &&
+                        workDay.isBefore(localDateTo)) {
+                    int hours = Integer.parseInt(split[2]);
+                    int incomePerHour = Integer.parseInt(split[3]);
+                    moneyEarned += hours * incomePerHour;
+                }
+            }
+            sb.append(System.lineSeparator()).append(name).append(" - ").append(moneyEarned);
+        }
+        return sb.toString();
     }
 }
