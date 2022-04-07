6a5a16626e9e3cf2441bc5f157ad93e40e0aa540
Dmytro Dmytrenko <dmitrynetfx@gmail.com>
2022-04-07 14:55:39 +0300

    Init commit

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -2,6 +2,18 @@ package core.basesyntax;
 
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        String result;
+        if (dateFrom != null && dateTo != null) {
+            for (String name : names) {
+                for (String dataStr : data) {
+
+                }
+            }
+
+            return result;
+        } else {
+            return null;
+        }
     }
 }
+
