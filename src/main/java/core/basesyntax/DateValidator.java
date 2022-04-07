9b6a9a99d6853b005f640e4bdcc593a743a7daee
Dmytro Dmytrenko <dmitrynetfx@gmail.com>
2022-04-07 18:54:54 +0300

    Date Validator

---
+++ src/main/java/core/basesyntax/DateValidator.java
@@ -0,0 +1,19 @@
+package core.basesyntax;
+import java.util.Date;
+public class DateValidator {
+    private Date startDate;
+    private Date endDate;
+
+    public DateValidator(Date startDate, Date endDate) {
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+    public boolean isWithinRange(Date salaryDate) {
+        boolean result = false;
+        if ((salaryDate.equals(startDate) || salaryDate.equals(endDate)) ||
+                (salaryDate.after(startDate) && salaryDate.before(endDate))) {
+            result = true;
+        }
+        return result;
+    }
+}
--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,11 +1,30 @@
 package core.basesyntax;
 
+
+
+import java.text.SimpleDateFormat;
+import java.time.DateTimeException;
+
+import java.util.Date;
+
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         String result;
+        try {
+            SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy" );
+            Date startDate = dateFormat.parse(dateFrom);
+            Date endDate = dateFormat.parse(dateTo);
+            Date salaryDate = dateFormat.parse(dateFrom);
+            System.out.println("Report for period " + dateFormat.format(startDate) + " - " + dateFormat.format(endDate));
+        }
+        catch (DateTimeException exc) {
+            System.out.printf("%s Date is wrong formatted!%n");
+            throw exc;
+        }
         if (dateFrom != null && dateTo != null) {
             for (String name : names) {
                 for (String dataStr : data) {
+
 
                 }
             }
