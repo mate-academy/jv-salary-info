96116145378d7139e8a84d27e44de264e1951bae
dippp <Dipppp84@gmail.com>
2022-04-09 21:06:06 +0300

    fix 2: made "format" variable a constant

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,17 +5,17 @@ import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 
 public class SalaryInfo {
-    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        LocalDate localDateFrom = LocalDate.parse(dateFrom, formatter).minus(1, ChronoUnit.DAYS);
-        LocalDate localDateTo = LocalDate.parse(dateTo, formatter).plus(1, ChronoUnit.DAYS);
+        LocalDate localDateFrom = LocalDate.parse(dateFrom, FORMATTER).minus(1, ChronoUnit.DAYS);
+        LocalDate localDateTo = LocalDate.parse(dateTo, FORMATTER).plus(1, ChronoUnit.DAYS);
         StringBuilder sb = new StringBuilder("Report for period " + dateFrom + " - " + dateTo);
         for (String name : names) {
             int moneyEarned = 0;
             for (String workData : data) {
                 String[] split = workData.split(" ");
-                LocalDate workDay = LocalDate.parse(split[0], formatter);
+                LocalDate workDay = LocalDate.parse(split[0], FORMATTER);
                 if (name.equals(split[1]) && workDay.isAfter(localDateFrom)
                         && workDay.isBefore(localDateTo)) {
                     int hours = Integer.parseInt(split[2]);
