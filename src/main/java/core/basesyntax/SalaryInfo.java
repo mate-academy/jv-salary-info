634b5633b71df1a66e2c2914a59119da06cc0de9
Dmytro Dmytrenko <dmitrynetfx@gmail.com>
2022-04-11 21:30:25 +0300

    renamed constants

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,34 +4,34 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private static final DateTimeFormatter DATEFORMAT = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
     private static final int DATE_INDEX = 0;
     private static final int NAME_INDEX = 1;
-    private static final int H_INDEX = 2;
-    private static final int IPH_IND = 3;
+    private static final int HOURS_INDEX = 2;
+    private static final int COST_INDEX = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
 
         StringBuilder reportBuilder = new StringBuilder();
-        LocalDate startDate = LocalDate.parse(dateFrom, DATEFORMAT);
-        LocalDate endDate = LocalDate.parse(dateTo, DATEFORMAT);
+        LocalDate startDate = LocalDate.parse(dateFrom, DATE_FORMATTER);
+        LocalDate endDate = LocalDate.parse(dateTo, DATE_FORMATTER);
         LocalDate salaryDate = null;
         boolean isWithinRange = false;
         reportBuilder.append("Report for period ")
-                     .append(DATEFORMAT.format(startDate))
+                     .append(DATE_FORMATTER.format(startDate))
                      .append(" - ")
-                     .append(DATEFORMAT.format(endDate));
+                     .append(DATE_FORMATTER.format(endDate));
         String[] fields = new String[4];
         int salary;
         for (String name : names) {
             salary = 0;
             for (String dataStr : data) {
                 fields = dataStr.split(" ");
-                salaryDate = LocalDate.parse(fields[DATE_INDEX], DATEFORMAT);
+                salaryDate = LocalDate.parse(fields[DATE_INDEX], DATE_FORMATTER);
                 isWithinRange = ((salaryDate.equals(startDate) || salaryDate.equals(endDate))
                         || (salaryDate.isAfter(startDate) && salaryDate.isBefore(endDate)));
                 if (isWithinRange && name.equals(fields[NAME_INDEX])) {
-                    salary += Integer.parseInt(fields[H_INDEX]) * Integer.parseInt(fields[IPH_IND]);
+                    salary += Integer.parseInt(fields[HOURS_INDEX]) * Integer.parseInt(fields[COST_INDEX]);
                 }
             }
             reportBuilder.append(System.lineSeparator())
@@ -42,4 +42,3 @@ public class SalaryInfo {
         return reportBuilder.toString();
     }
 }
-
