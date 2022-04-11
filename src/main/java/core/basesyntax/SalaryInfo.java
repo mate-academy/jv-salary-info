4c4f704cf76bf701f44e97dd6c41538786d6273c
Dmytro Dmytrenko <dmitrynetfx@gmail.com>
2022-04-11 19:24:08 +0300

    Removed check (null)

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -3,8 +3,6 @@ package core.basesyntax;
 import java.text.ParseException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
-
 
 public class SalaryInfo {
     private static final DateTimeFormatter DATEFORMAT = DateTimeFormatter.ofPattern("dd.MM.yyyy");
@@ -13,16 +11,6 @@ public class SalaryInfo {
     private static final int HOURS_INDEX = 2;
     private static final int INCOME_PER_HOUR_INDEX = 3;
 
-    public boolean isWithinRange(LocalDate salaryDate) {
-        boolean result = false;
-        LocalDate startDate = null;
-        LocalDate endDate = null;
-        if ((salaryDate.equals(startDate) || salaryDate.equals(endDate))
-                || (salaryDate.isAfter(startDate) && salaryDate.isBefore(endDate))) {
-            result = true;
-        }
-        return result;
-    }
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
 
@@ -31,27 +19,27 @@ public class SalaryInfo {
             LocalDate startDate = LocalDate.parse(dateFrom, DATEFORMAT);
             LocalDate endDate = LocalDate.parse(dateTo, DATEFORMAT);
             LocalDate salaryDate = null;
+            boolean isWithinRange = ((salaryDate.equals(startDate) || salaryDate.equals(endDate))
+            || (salaryDate.isAfter(startDate) && salaryDate.isBefore(endDate)));
             reportBuilder.append("Report for period ")
                          .append(DATEFORMAT.format(startDate))
                          .append(" - ")
                          .append(DATEFORMAT.format(endDate));
             String[] fields = new String[4];
             int salary;
-            if (dateFrom != null && dateTo != null) {
-                for (String name : names) {
-                    salary = 0;
-                    for (String dataStr : data) {
-                        fields = dataStr.split(" ");
-                        salaryDate = LocalDate.parse(fields[DATE_INDEX], DATEFORMAT);
-                        if (isWithinRange(salaryDate) && name.equals(fields[NAME_INDEX])) {
-                            salary += Integer.parseInt(fields[HOURS_INDEX]) * Integer.parseInt(fields[INCOME_PER_HOUR_INDEX]);
-                        }
+            for (String name : names) {
+                salary = 0;
+                for (String dataStr : data) {
+                    fields = dataStr.split(" ");
+                    salaryDate = LocalDate.parse(fields[DATE_INDEX], DATEFORMAT);
+                    if (isWithinRange && name.equals(fields[NAME_INDEX])) {
+                        salary += Integer.parseInt(fields[HOURS_INDEX]) * Integer.parseInt(fields[INCOME_PER_HOUR_INDEX]);
                     }
-                    reportBuilder.append(System.lineSeparator())
-                            .append(name)
-                            .append(" - ")
-                            .append(salary);
                 }
+                reportBuilder.append(System.lineSeparator())
+                        .append(name)
+                        .append(" - ")
+                        .append(salary);
             }
         } catch (ParseException e) {
             System.out.println("Failed to parse date");
