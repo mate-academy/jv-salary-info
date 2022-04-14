5222db0643c65986478b32d6dd304b20d852cd1e
Maksym <Maksym_Personal@ip-192-168-0-72.eu-central-1.compute.internal>
2022-04-14 21:37:58 +0300

    3rd. Removed excessive parsing and constants, fixed naming

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -8,48 +8,42 @@ public class SalaryInfo {
     public static final int NAME_INDEX = 1;
     public static final int HOURS_INDEX = 2;
     public static final int SALARY_RATE_INDEX = 3;
-    public static final int TOTAL_PARAMETERS = 4;
-
     private final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        LocalDate localDate1 = LocalDate.parse(dateFrom, dateTimeFormatter);
-        LocalDate localDate2 = LocalDate.parse(dateTo, dateTimeFormatter);
-        int[] salaryTotal = new int[names.length];
+        LocalDate localDateFrom = LocalDate.parse(dateFrom, dateTimeFormatter);
+        LocalDate localDateTo = LocalDate.parse(dateTo, dateTimeFormatter);
+        int[] salaries = new int[names.length];
+        for (String datum : data) {
 
-        for (int i = 0; i < data.length; i++) {
-            String date = data[i].split(" ")[DATE_INDEX];
-            LocalDate localDate = LocalDate.parse(date, dateTimeFormatter);
-            String name = data[i].split(" ")[NAME_INDEX];
-            String hours = data[i].split(" ")[HOURS_INDEX];
-            String salary = data[i].split(" ")[SALARY_RATE_INDEX];
+            String[] dataParsed = datum.split(" ");
             for (int j = 0; j < names.length; j++) {
-                if (names[j].equals(name)
-                        && localDate.isAfter(localDate1)
-                        && (localDate.isBefore(localDate2) || (localDate.equals(localDate2)))) {
-                    salaryTotal[j] += Integer.parseInt(hours) * Integer.parseInt(salary);
+                if (names[j].equals(dataParsed[NAME_INDEX])
+                        && LocalDate.parse(dataParsed[DATE_INDEX],
+                        dateTimeFormatter).isAfter(localDateFrom)
+                        && (LocalDate.parse(dataParsed[DATE_INDEX],
+                        dateTimeFormatter).isBefore(localDateTo)
+                        || (LocalDate.parse(dataParsed[DATE_INDEX],
+                        dateTimeFormatter).equals(localDateTo)))) {
+                    salaries[j] += Integer.parseInt(dataParsed[HOURS_INDEX])
+                            * Integer.parseInt(dataParsed[SALARY_RATE_INDEX]);
                 }
             }
         }
 
-        //      Creating resulting StringBuilder
-        StringBuilder stringBuilderNames = new StringBuilder();
+        StringBuilder reportBuilder = new StringBuilder("Report for period ");
+        reportBuilder.append(dateFrom);
+        reportBuilder.append(" - ");
+        reportBuilder.append(dateTo);
+        reportBuilder.append(System.lineSeparator());
         for (int i = 0; i < names.length; i++) {
-            stringBuilderNames.append(names[i]);
-            stringBuilderNames.append(" - ");
-            stringBuilderNames.append(salaryTotal[i]);
+            reportBuilder.append(names[i]);
+            reportBuilder.append(" - ");
+            reportBuilder.append(salaries[i]);
             if (i != names.length - 1) {
-                stringBuilderNames.append("\n");
+                reportBuilder.append("\n");
             }
         }
-
-        StringBuilder stringBuilderResult = new StringBuilder("Report for period ");
-        stringBuilderResult.append(dateFrom);
-        stringBuilderResult.append(" - ");
-        stringBuilderResult.append(dateTo);
-        stringBuilderResult.append(System.lineSeparator());
-        stringBuilderResult.append(stringBuilderNames);
-
-        return stringBuilderResult.toString();
+        return reportBuilder.toString();
     }
 }
