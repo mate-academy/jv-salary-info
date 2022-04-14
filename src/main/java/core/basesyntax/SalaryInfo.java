9dc15b8076f2363ffdc37ec1014e0b9cf549322b
Maksym <Maksym_Personal@ip-192-168-0-72.eu-central-1.compute.internal>
2022-04-14 20:49:01 +0300

    2st. Formatter is constant, no 3D array, LocalDate used

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,8 +1,7 @@
 package core.basesyntax;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     public static final int DATE_INDEX = 0;
@@ -11,63 +10,46 @@ public class SalaryInfo {
     public static final int SALARY_RATE_INDEX = 3;
     public static final int TOTAL_PARAMETERS = 4;
 
+    private final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-
-        Date date1 = new Date();
-        Date date2 = new Date();
-
-        try {
-            date1 = new SimpleDateFormat("dd.MM.yyyy").parse(dateFrom);
-            date2 = new SimpleDateFormat("dd.MM.yyyy").parse(dateTo);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-
-        //      Parse data into 3D Array
-        String[][] dataParsed = new String[data.length][TOTAL_PARAMETERS];
-        for (int i = 0; i < data.length; i++) {
-            dataParsed[i] = data[i].split(" ");
-        }
-
+        LocalDate localDate1 = LocalDate.parse(dateFrom, dateTimeFormatter);
+        LocalDate localDate2 = LocalDate.parse(dateTo, dateTimeFormatter);
         int[] salaryTotal = new int[names.length];
 
-        for (int i = 0; i < names.length; i++) {
-            for (int k = 0; k < data.length; k++) {
-                Date date = new Date();
-                try {
-                    date = new SimpleDateFormat("dd.MM.yyyy").parse(dataParsed[k][DATE_INDEX]);
-                } catch (ParseException e) {
-                    e.printStackTrace();
-                }
-                if (date.after(date1)
-                        && (date.equals(date2) || date.before(date2))
-                        && names[i].equals(dataParsed[k][NAME_INDEX])) {
-                    salaryTotal[i] += Integer.parseInt(dataParsed[k][HOURS_INDEX])
-                            * Integer.parseInt(dataParsed[k][SALARY_RATE_INDEX]);
+        for (int i = 0; i < data.length; i++) {
+            String date = data[i].split(" ")[DATE_INDEX];
+            LocalDate localDate = LocalDate.parse(date, dateTimeFormatter);
+            String name = data[i].split(" ")[NAME_INDEX];
+            String hours = data[i].split(" ")[HOURS_INDEX];
+            String salary = data[i].split(" ")[SALARY_RATE_INDEX];
+            for (int j = 0; j < names.length; j++) {
+                if (names[j].equals(name)
+                        && localDate.isAfter(localDate1)
+                        && (localDate.isBefore(localDate2) || (localDate.equals(localDate2)))) {
+                    salaryTotal[j] += Integer.parseInt(hours) * Integer.parseInt(salary);
                 }
             }
         }
 
         //      Creating resulting StringBuilder
-        StringBuilder stringBuilder = new StringBuilder();
+        StringBuilder stringBuilderNames = new StringBuilder();
         for (int i = 0; i < names.length; i++) {
-            stringBuilder.append(names[i]);
-            stringBuilder.append(" - ");
-            stringBuilder.append(salaryTotal[i]);
+            stringBuilderNames.append(names[i]);
+            stringBuilderNames.append(" - ");
+            stringBuilderNames.append(salaryTotal[i]);
             if (i != names.length - 1) {
-                stringBuilder.append("\n");
+                stringBuilderNames.append("\n");
             }
         }
 
-        String result = (
-                "Report for period "
-                + dateFrom
-                + " - "
-                + dateTo
-                + "\n"
-                + stringBuilder.toString()
-        );
+        StringBuilder stringBuilderResult = new StringBuilder("Report for period ");
+        stringBuilderResult.append(dateFrom);
+        stringBuilderResult.append(" - ");
+        stringBuilderResult.append(dateTo);
+        stringBuilderResult.append(System.lineSeparator());
+        stringBuilderResult.append(stringBuilderNames);
 
-        return result;
+        return stringBuilderResult.toString();
     }
 }
