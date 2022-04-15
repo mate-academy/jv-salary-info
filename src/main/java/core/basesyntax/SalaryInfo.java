c01a52ee72b07a1d2580b7b7a3ddfd2667be8828
Ihor Dmytrush <ihor.dm.a@gmail.com>
2022-04-15 11:49:39 +0300

    3th try: hiding private method and constants, private method moved after public, other mistakes fixed

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -2,43 +2,41 @@ package core.basesyntax;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
 
 public class SalaryInfo {
-    static final String DATE_FORMAT_PATTERN = "dd.MM.yyyy";
-
-    public boolean inPeriod(String date, String dateFrom, String dateTo) {
-        try {
-            LocalDate checkDate = LocalDate.parse(date,
-                    DateTimeFormatter.ofPattern(DATE_FORMAT_PATTERN));
-            LocalDate dateStart = LocalDate.parse(dateFrom,
-                    DateTimeFormatter.ofPattern(DATE_FORMAT_PATTERN));
-            LocalDate dateEnd = LocalDate.parse(dateTo,
-                    DateTimeFormatter.ofPattern(DATE_FORMAT_PATTERN));
-            return (checkDate.isAfter(dateStart)
-                    && checkDate.isBefore(dateEnd))
-                    || checkDate.isEqual(dateStart)
-                    || checkDate.isEqual(dateEnd);
-        } catch (DateTimeParseException e) {
-            throw new RuntimeException("Error of parsing string to date!");
-        }
-    }
+    private static final String DATE_FORMAT_PATTERN = "dd.MM.yyyy";
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int HOUR_COUNT_INDEX = 2;
+    private static final int SALARY_VALUE_INDEX = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder result = new StringBuilder("Report for period ")
                 .append(dateFrom).append(" - ").append(dateTo);
-        int salarySum;
         for (String name : names) {
-            salarySum = 0;
+            int salarySum = 0;
             for (String dataRow : data) {
-                if (inPeriod(dataRow.split(" ")[0], dateFrom, dateTo)
-                        && name.equals(dataRow.split(" ")[1])) {
-                    salarySum += Integer.parseInt(dataRow.split(" ")[2])
-                        * Integer.parseInt(dataRow.split(" ")[3]);
+                if (inPeriod(dataRow.split(" ")[DATE_INDEX], dateFrom, dateTo)
+                        && name.equals(dataRow.split(" ")[NAME_INDEX])) {
+                    salarySum += Integer.parseInt(dataRow.split(" ")[HOUR_COUNT_INDEX])
+                        * Integer.parseInt(dataRow.split(" ")[SALARY_VALUE_INDEX]);
                 }
             }
             result.append(System.lineSeparator()).append(name).append(" - ").append(salarySum);
         }
         return result.toString();
+    }
+
+    private boolean inPeriod(String date, String dateFrom, String dateTo) {
+        LocalDate checkedDate = LocalDate.parse(date,
+                DateTimeFormatter.ofPattern(DATE_FORMAT_PATTERN));
+        LocalDate dateStart = LocalDate.parse(dateFrom,
+                DateTimeFormatter.ofPattern(DATE_FORMAT_PATTERN));
+        LocalDate dateEnd = LocalDate.parse(dateTo,
+                DateTimeFormatter.ofPattern(DATE_FORMAT_PATTERN));
+        return (checkedDate.isAfter(dateStart)
+                && checkedDate.isBefore(dateEnd))
+                || checkedDate.isEqual(dateStart)
+                || checkedDate.isEqual(dateEnd);
     }
 }
