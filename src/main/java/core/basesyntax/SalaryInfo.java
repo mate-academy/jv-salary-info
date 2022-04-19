49262cedd4371785823e898ecb84b16f91c0408a
Denys Skrinnik <skrinnikden2000@gmail.com>
2022-04-19 15:35:02 +0300

    another right solution using "split"

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -2,49 +2,41 @@ package core.basesyntax;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Date;
 
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-
-        ArrayList<String> dataList = new ArrayList<>(Arrays.asList(data));
+        long start = System.nanoTime();
         final SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.y");
-
+        int[] salaries = new int[names.length];
         try {
             Date dateFromDataType = formatter.parse(dateFrom);
             Date dateToDataType = formatter.parse(dateTo);
 
-            for (int i = 0; i < dataList.size(); i++) {
-                if (dateFromDataType.after(formatter.parse(dataList.get(i).substring(0,
-                        dataList.get(i).indexOf(' ')))) || dateToDataType.before(formatter.parse(
-                                dataList.get(i).substring(0, dataList.get(i).indexOf(' '))))) {
-                    dataList.remove(i);
-                    i--;
-                } else {
-                    dataList.set(i, dataList.get(i).substring(dataList.get(i).indexOf(' ') + 1));
+            for (int i = 0; i < data.length; i++) {
+                String[] separateData = data[i].split(" ");
+                if ((formatter.parse(separateData[0]).after(dateFromDataType)
+                        || formatter.parse(separateData[0]).equals(dateFromDataType))
+                        && (formatter.parse(separateData[0]).before(dateToDataType)
+                        || formatter.parse(separateData[0]).equals(dateToDataType))) {
+                    for (int j = 0; j < names.length; j++) {
+                        if (names[j].equals(separateData[1])) {
+                            salaries[j] += Integer.parseInt(separateData[2])
+                                    * Integer.parseInt(separateData[3]);
+                        }
+                    }
                 }
             }
         } catch (ParseException e) {
             throw new RuntimeException("Sorry, unparseable date");
-        }
-        int[] salaries = new int[names.length];
-        for (int i = 0; i < names.length; i++) {
-            for (int j = 0; j < dataList.size(); j++) {
-                if (names[i].equals(dataList.get(j).substring(0, dataList.get(j).indexOf(' ')))) {
-                    dataList.set(j, dataList.get(j).substring(dataList.get(j).indexOf(' ') + 1));
-                    salaries[i] += Integer.parseInt(dataList.get(j).substring(
-                            0, dataList.get(j).indexOf(' '))) * Integer.parseInt(
-                                    dataList.get(j).substring(dataList.get(j).indexOf(' ') + 1));
-                }
-            }
         }
         StringBuilder answer = new StringBuilder("Report for period " + dateFrom + " - " + dateTo);
         for (int i = 0; i < names.length; i++) {
             answer.append(System.lineSeparator()).append(names[i]).append(" - ")
                     .append(salaries[i]);
         }
+        long end = System.nanoTime();
+        System.out.println(end - start);
         return answer.toString();
     }
 }
