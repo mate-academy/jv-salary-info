11d56cf1f0628648fc57406c0689df4e23ff3930
pavel <pavelpm1971@gmail.com>
2022-05-03 19:02:30 +0200

    I made "getSalaryInfo" method which calculate employees salary between two dates

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,30 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        long fromDate = LocalDate.parse(dateFrom, formatter).toEpochDay();
+        long toDate = LocalDate.parse(dateTo, formatter).toEpochDay();
+        StringBuilder reportBuilder = new StringBuilder();
+        reportBuilder.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
+        for (String name: names) {
+            int salary = 0;
+            for (String dat: data) {
+                String[] aboutEmployee = dat.split(" ");
+                if (name.equals(aboutEmployee[1])) {
+                    long workDate = LocalDate.parse(aboutEmployee[0], formatter).toEpochDay();
+                    if (workDate >= fromDate && workDate <= toDate) {
+                        salary += Integer.parseInt(aboutEmployee[3])
+                                * Integer.parseInt(aboutEmployee[2]);
+                    }
+                }
+            }
+            reportBuilder.append(System.lineSeparator()).append(name).append(" - ")
+                    .append(salary);
+        }
+        return reportBuilder.toString();
     }
 }
