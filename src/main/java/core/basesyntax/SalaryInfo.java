391a5524df3e82816df85934d0d699dba6ae9928
pavel <pavelpm1971@gmail.com>
2022-05-03 22:36:47 +0200

    I made "getSalaryInfo" method which calculate employees salary between two dates Corrected double if msde constant field

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,17 +4,18 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        long fromDate = LocalDate.parse(dateFrom, formatter).toEpochDay();
-        long toDate = LocalDate.parse(dateTo, formatter).toEpochDay();
+        long fromDate = LocalDate.parse(dateFrom, FORMATTER).toEpochDay();
+        long toDate = LocalDate.parse(dateTo, FORMATTER).toEpochDay();
         StringBuilder reportBuilder = new StringBuilder();
         reportBuilder.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
         for (String name: names) {
             int salary = 0;
             for (String dat: data) {
                 String[] aboutEmployee = dat.split(" ");
-                long workDate = LocalDate.parse(aboutEmployee[0], formatter).toEpochDay();
+                long workDate = LocalDate.parse(aboutEmployee[0], FORMATTER).toEpochDay();
                 if (name.equals(aboutEmployee[1]) && (workDate >= fromDate && workDate <= toDate)) {
                     salary += Integer.parseInt(aboutEmployee[3])
                             * Integer.parseInt(aboutEmployee[2]);
