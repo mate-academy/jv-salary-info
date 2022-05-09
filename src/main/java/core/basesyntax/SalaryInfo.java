c8ed517e2046a2d5155aaae22db8100cfab5e200
Maksim Tsvetkov <tsm_2001@ukr.net>
2022-05-09 16:02:33 +0300

    jv-salary solution

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,33 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Report for period ")
+                .append(dateFrom)
+                .append(" - ")
+                .append(dateTo);
+        DateTimeFormatter form = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDate fromDate = LocalDate.parse(dateFrom, form);
+        LocalDate toDate = LocalDate.parse(dateTo, form);
+        for (String name : names) {
+            int salary = 0;
+            for (String dat : data) {
+                String[] splitData = dat.split(" ");
+                if (name.equals(splitData[1])) {
+                    LocalDate dataDate = LocalDate.parse(splitData[0], form);
+                    if ((dataDate.isEqual(fromDate) || dataDate.isAfter(fromDate))
+                            && (dataDate.isEqual(toDate) || dataDate.isBefore(toDate))) {
+                        salary += Integer.valueOf(splitData[2])
+                                * Integer.valueOf(splitData[3]);
+                    }
+                }
+            }
+            stringBuilder.append("\r\n").append(name).append(" - ").append(salary);
+        }
+        return stringBuilder.toString();
     }
 }
