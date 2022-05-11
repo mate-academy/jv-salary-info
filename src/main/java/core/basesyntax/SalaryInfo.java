2408b8faaced9adb79da3578bf59999de8a9a802
Viacheslav Bondar <mcx.bondar@outlook.com>
2022-05-11 10:43:38 +0300

    changed constant names and used another comparing methods

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,9 +4,10 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private static final int FIRST_VALUE = 0;
-    private static final int PENULTIMATE_VALUE = 2;
-    private static final int LAST_VALUE = 3;
+    private static final int START_INDEX = 0;
+    private static final int FIRST_ELEMENT_INDEX = 1;
+    private static final int PENULTIMATE_INDEX = 2;
+    private static final int LAST_INDEX = 3;
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
@@ -19,12 +20,12 @@ public class SalaryInfo {
             int salary = 0;
             for (String line : data) {
                 dataArray = line.split(" ");
-                LocalDate currentDate = LocalDate.parse(dataArray[FIRST_VALUE], FORMATTER);
-                if (localDateFrom.compareTo(currentDate) <= 0
-                        && localDateTo.compareTo(currentDate) >= 0
-                        && dataArray[1].equals(name)) {
-                    salary += Integer.parseInt(dataArray[LAST_VALUE])
-                            * Integer.parseInt(dataArray[PENULTIMATE_VALUE]);
+                LocalDate currentDate = LocalDate.parse(dataArray[START_INDEX], FORMATTER);
+                if ((localDateFrom.isBefore(currentDate) || localDateFrom.isEqual(currentDate))
+                        && (localDateTo.isAfter(currentDate) || localDateTo.isEqual(currentDate))
+                        && dataArray[FIRST_ELEMENT_INDEX].equals(name)) {
+                    salary += Integer.parseInt(dataArray[LAST_INDEX])
+                            * Integer.parseInt(dataArray[PENULTIMATE_INDEX]);
                 }
             }
             result.append(System.lineSeparator()).append(name).append(" - ").append(salary);
