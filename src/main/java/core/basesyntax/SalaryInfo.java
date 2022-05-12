7def08f1272dbbdb31ed437ed76bc4b3264c2366
Maksim Tsvetkov <tsm_2001@ukr.net>
2022-05-12 22:28:11 +0300

    jv-salary solution 2 - constants added

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,30 +4,36 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter
+            .ofPattern("dd.MM.yyyy");
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int HOURS_INDEX = 2;
+    private static final int INCOME_INDEX = 3;
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("Report for period ")
+        StringBuilder reportBuilder = new StringBuilder();
+        reportBuilder.append("Report for period ")
                 .append(dateFrom)
                 .append(" - ")
                 .append(dateTo);
-        DateTimeFormatter form = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        LocalDate fromDate = LocalDate.parse(dateFrom, form);
-        LocalDate toDate = LocalDate.parse(dateTo, form);
+        LocalDate fromDate = LocalDate.parse(dateFrom, DATE_FORMATTER);
+        LocalDate toDate = LocalDate.parse(dateTo, DATE_FORMATTER);
         for (String name : names) {
             int salary = 0;
-            for (String dat : data) {
-                String[] splitData = dat.split(" ");
-                if (name.equals(splitData[1])) {
-                    LocalDate dataDate = LocalDate.parse(splitData[0], form);
+            for (String line : data) {
+                String[] splittedLine = line.split(" ");
+                if (name.equals(splittedLine[NAME_INDEX])) {
+                    LocalDate dataDate = LocalDate.parse(splittedLine[DATE_INDEX], DATE_FORMATTER);
                     if ((dataDate.isEqual(fromDate) || dataDate.isAfter(fromDate))
                             && (dataDate.isEqual(toDate) || dataDate.isBefore(toDate))) {
-                        salary += Integer.valueOf(splitData[2])
-                                * Integer.valueOf(splitData[3]);
+                        salary += Integer.valueOf(splittedLine[HOURS_INDEX])
+                                * Integer.valueOf(splittedLine[INCOME_INDEX]);
                     }
                 }
             }
-            stringBuilder.append(System.lineSeparator()).append(name).append(" - ").append(salary);
+            reportBuilder.append(System.lineSeparator()).append(name).append(" - ").append(salary);
         }
-        return stringBuilder.toString();
+        return reportBuilder.toString();
     }
 }
