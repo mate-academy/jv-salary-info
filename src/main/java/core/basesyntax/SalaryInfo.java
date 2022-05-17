4533212a4597f71869d0a11d23a84ad6edca6926
Mykola Dubich <nikolay19888@hotmail.com>
2022-05-17 14:07:08 +0300

    Replace all /n on System.lineSeparator(). Refactored getReport method. Refactored getSalaryInfo, added framework - LinkedHashMap

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,6 +1,9 @@
 package core.basesyntax;
 
 import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class SalaryInfo {
     private static final int DAY = 0;
@@ -14,19 +17,19 @@ public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate from = parseToLocalDate(dateFrom);
         LocalDate to = parseToLocalDate(dateTo);
-        int[] salary = new int[names.length];
+        HashMap<String, Integer> workersSalary = getNamesOnHash(names);
+
         for (int i = 0; i < data.length; i++) {
-            String[] splittedData = data[i].split(" ");
-            for (int j = 0; j < names.length; j++) {
-                LocalDate dateOfWork = parseToLocalDate(splittedData[WORK_DAY]);
-                if (dateOfWork.compareTo(from) >= 0 && dateOfWork.compareTo(to) <= 0
-                        && splittedData[EMP_NAME].equals(names[j])) {
-                    salary[j] += Integer.parseInt(splittedData[WORK_HOURS])
-                            * Integer.parseInt(splittedData[PER_HOUR]);
-                }
+            String[] splitData = data[i].split(" ");
+            LocalDate dateOfWork = parseToLocalDate(splitData[WORK_DAY]);
+            if (dateOfWork.compareTo(from) >= 0 && dateOfWork.compareTo(to) <= 0) {
+                workersSalary.merge(splitData[EMP_NAME],
+                        Integer.parseInt(splitData[WORK_HOURS])
+                                * Integer.parseInt(splitData[PER_HOUR]),
+                        Integer::sum);
             }
         }
-        return getReport(names, salary, dateFrom, dateTo);
+        return getReport(workersSalary, dateFrom, dateTo);
     }
 
     private LocalDate parseToLocalDate(String date) {
@@ -38,14 +41,25 @@ public class SalaryInfo {
         return localDate;
     }
 
-    private String getReport(String[] names, int[] salary, String dateFrom, String dateTo) {
+    private HashMap<String, Integer> getNamesOnHash(String[] names) {
+        HashMap<String, Integer> hashMap = new LinkedHashMap<>(16, 0.75f, false);
+        for (int i = 0; i < names.length; i++) {
+            hashMap.put(names[i], 0);
+        }
+        return hashMap;
+    }
+
+    private String getReport(HashMap<String, Integer> workers, String dateFrom, String dateTo) {
         StringBuilder salaryInfo = new StringBuilder("Report for period "
                 + dateFrom
                 + " - "
-                + dateTo + "\n");
-        for (int i = 0; i < names.length; i++) {
-            salaryInfo.append(names[i] + " - " + salary[i] + "\n");
+                + dateTo);
+        for (Map.Entry<String, Integer> entry : workers.entrySet()) {
+            salaryInfo.append(System.lineSeparator())
+                    .append(entry.getKey())
+                    .append(" - ")
+                    .append(entry.getValue());
         }
-        return salaryInfo.toString().trim();
+        return salaryInfo.toString();
     }
 }
