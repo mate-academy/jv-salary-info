33799a7499f268262bac4e3032005da7a165f3a9
Andrey Chumak <chumakandrey7@gmail.com>
2022-05-18 08:27:28 +0300

    Implemented method "getSalaryInfo" Implemented additional method "getSalaryInfo" to compare dates.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,42 @@
 package core.basesyntax;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class SalaryInfo {
+    private boolean isDateValid(String userDate, String dateFrom, String dateTo) {
+        try {
+            Date user = new SimpleDateFormat("dd.MM.yyyy").parse(userDate);
+            Date from = new SimpleDateFormat("dd.MM.yyyy").parse(dateFrom);
+            Date to = new SimpleDateFormat("dd.MM.yyyy").parse(dateTo);
+            return ((user.after(from) && user.before(to)) || (user.equals(from) && user.before(to))
+                    || (user.after(from) && user.equals(to)));
+        } catch (ParseException e) {
+            System.out.println("Date is not valid");
+        }
+        return false;
+    }
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        int[] salary = new int[names.length];
+        StringBuilder resultsMessage = new StringBuilder("Report for period ");
+        resultsMessage.append(dateFrom).append(" - ").append(dateTo);
+
+        for (int j = 0; j < names.length; j++) {
+            for (String datum : data) {
+                String[] person = datum.split(" ");
+
+                if (names[j].equals(person[1])
+                        && isDateValid(person[0], dateFrom, dateTo)) {
+                    salary[j] += Integer.parseInt(person[2]) * Integer.parseInt(person[3]);
+                }
+            }
+        }
+
+        for (int i = 0; i < names.length; i++) {
+            resultsMessage.append("\n").append(names[i]).append(" - ").append(salary[i]);
+        }
+        return resultsMessage.toString();
     }
 }
