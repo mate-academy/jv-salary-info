89421d1f9a92c36c798840b6e113a844a7c9223d
Andrey Chumak <chumakandrey7@gmail.com>
2022-05-18 11:22:19 +0300

    Created only one instance of SimpleDateFormat in method "isDateValid".

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -6,10 +6,11 @@ import java.util.Date;
 
 public class SalaryInfo {
     private boolean isDateValid(String userDate, String dateFrom, String dateTo) {
+        final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM.yyyy");
         try {
-            Date user = new SimpleDateFormat("dd.MM.yyyy").parse(userDate);
-            Date from = new SimpleDateFormat("dd.MM.yyyy").parse(dateFrom);
-            Date to = new SimpleDateFormat("dd.MM.yyyy").parse(dateTo);
+            Date user = simpleDateFormat.parse(userDate);
+            Date from = simpleDateFormat.parse(dateFrom);
+            Date to = simpleDateFormat.parse(dateTo);
             return ((user.after(from) && user.before(to)) || (user.equals(from) && user.before(to))
                     || (user.after(from) && user.equals(to)));
         } catch (ParseException e) {
@@ -33,7 +34,6 @@ public class SalaryInfo {
                 }
             }
         }
-
         for (int i = 0; i < names.length; i++) {
             resultsMessage.append("\n").append(names[i]).append(" - ").append(salary[i]);
         }
