07c1be90adc599ea765e719fc622a96a5e0bbbc0
Konstantin Miroshnichenko <bigfishnotbad@gmail.com>
2022-05-18 11:24:20 +0300

    problem with magic numbers were fixed

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -3,6 +3,14 @@ package core.basesyntax;
 import java.time.LocalDate;
 
 public class SalaryInfo {
+    private static final int DATE_CELL = 0;
+    private static final int NAME_CELL = 1;
+    private static final int WORKING_HOURS_CELL = 2;
+    private static final int PAY_PER_HOUR_CELL = 3;
+    private static final int DAY_CELL = 0;
+    private static final int MONTH_CELL = 1;
+    private static final int YEAR_CELL = 2;
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder builder = new StringBuilder();
         builder.append("Report for period ").append(dateFrom). append(" - ").append(dateTo);
@@ -21,10 +29,10 @@ public class SalaryInfo {
         String[] dayNotations;
         for (int i = 0; i < data.length; i++) {
             dayNotations = data[i].split(" ");
-            if (name.equals(dayNotations[1])) {
-                if (dataValidate(getLocalDate(dayNotations[0]), dateFrom, dateTo)) {
-                    totalWorkingHours += Integer.parseInt(dayNotations[2])
-                            * Integer.parseInt(dayNotations[3]);
+            if (name.equals(dayNotations[NAME_CELL])) {
+                if (dataValidate(getLocalDate(dayNotations[DATE_CELL]), dateFrom, dateTo)) {
+                    totalWorkingHours += Integer.parseInt(dayNotations[WORKING_HOURS_CELL])
+                            * Integer.parseInt(dayNotations[PAY_PER_HOUR_CELL]);
                 }
             }
         }
@@ -34,8 +42,8 @@ public class SalaryInfo {
 
     private LocalDate getLocalDate(String date) {
         String[] dateMassive = date.split("\\.");
-        return LocalDate.of(Integer.parseInt(dateMassive[2]),
-                Integer.parseInt(dateMassive[1]), Integer.parseInt(dateMassive[0]));
+        return LocalDate.of(Integer.parseInt(dateMassive[YEAR_CELL]),
+                Integer.parseInt(dateMassive[MONTH_CELL]), Integer.parseInt(dateMassive[DAY_CELL]));
     }
 
     private boolean dataValidate(LocalDate dataFromList,
