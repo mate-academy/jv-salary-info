e3ceefb7becfe3030c68988681d934645771c5d4
Ivan Yuriv <yuriv1024@gmail.com>
2022-05-18 12:26:01 +0300

    Magic with number constants was added =)

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -6,6 +6,10 @@ import java.time.format.DateTimeFormatter;
 public class SalaryInfo {
     private static final DateTimeFormatter dateTimeFormatter
             = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int DATE_FROM_ARRAY = 0;
+    private static final int NAME_OF_EMPLOYEE = 1;
+    private static final int WORKING_HOURS = 2;
+    private static final int INCOME_PER_HOUR = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate dateFromLocalDate = LocalDate.parse(dateFrom, dateTimeFormatter);
@@ -17,10 +21,11 @@ public class SalaryInfo {
             int salary = 0;
             for (String elementFromData : data) {
                 String[] arrayFromData = elementFromData.split(" ");
-                LocalDate dateFromArray = LocalDate.parse(arrayFromData[0], dateTimeFormatter);
-                String nameOfEmployee = arrayFromData[1];
-                int workingHours = Integer.parseInt(arrayFromData[2]);
-                int incomePerHour = Integer.parseInt(arrayFromData[3]);
+                LocalDate dateFromArray = LocalDate.parse(arrayFromData[DATE_FROM_ARRAY],
+                        dateTimeFormatter);
+                String nameOfEmployee = arrayFromData[NAME_OF_EMPLOYEE];
+                int workingHours = Integer.parseInt(arrayFromData[WORKING_HOURS]);
+                int incomePerHour = Integer.parseInt(arrayFromData[INCOME_PER_HOUR]);
                 if (name.equals(nameOfEmployee) && ((dateFromArray.isAfter(dateFromLocalDate)
                         || dateFromArray.equals(dateToLocalDate))
                         && (dateFromArray.isBefore(dateToLocalDate)
