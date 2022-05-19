6b12eb0f8200d2dbe672e93f865b198c4435c730
Zinchuk-Yevhenii-Mate <zenya87@gmail.com>
2022-05-19 18:11:51 +0300

    Implement method getsalaryinfo and calculate salary for employers

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,31 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        DateTimeFormatter formatDate = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDate startDate = LocalDate.parse(dateFrom, formatDate);
+        LocalDate endDate = LocalDate.parse(dateTo, formatDate);
+        StringBuilder stringPeriod = new StringBuilder();
+        stringPeriod.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
+        for (String name : names) {
+            int resultSalary = 0;
+            for (String rowData : data) {
+                String[] rowSplit = rowData.split(" ");
+                if (name.equals(rowSplit[1])) {
+                    LocalDate dateFromData = LocalDate.parse(rowSplit[0], formatDate);
+                    if ((dateFromData.isAfter(startDate) || dateFromData.isEqual(startDate))
+                            && (dateFromData.isBefore(endDate) || dateFromData.isEqual(endDate))) {
+                        resultSalary += Integer.parseInt(rowSplit[2])
+                                * Integer.parseInt(rowSplit[3]);
+                    }
+                }
+            }
+            stringPeriod.append(System.lineSeparator()).append(name).append(" - ")
+                    .append(resultSalary);
+        }
+        return stringPeriod.toString();
     }
 }
