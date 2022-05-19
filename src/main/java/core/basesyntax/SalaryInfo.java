7600bc6a3277e5df2fa0f035b9dc06d1d85f9f94
Zinchuk-Yevhenii-Mate <zenya87@gmail.com>
2022-05-19 18:56:15 +0300

    Added four constant

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,8 +4,13 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    private static final int INDEX_PERIOD = 0;
+    private static final int INDEX_NAME = 1;
+    private static final int INDEX_SALARY = 2;
+    private static final int INDEX_INCOME = 3;
+    private static final String FORMAT_DATE = "dd.MM.yyyy";
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        DateTimeFormatter formatDate = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        DateTimeFormatter formatDate = DateTimeFormatter.ofPattern(FORMAT_DATE);
         LocalDate startDate = LocalDate.parse(dateFrom, formatDate);
         LocalDate endDate = LocalDate.parse(dateTo, formatDate);
         StringBuilder stringPeriod = new StringBuilder();
@@ -14,12 +19,12 @@ public class SalaryInfo {
             int resultSalary = 0;
             for (String rowData : data) {
                 String[] rowSplit = rowData.split(" ");
-                if (name.equals(rowSplit[1])) {
-                    LocalDate dateFromData = LocalDate.parse(rowSplit[0], formatDate);
+                if (name.equals(rowSplit[INDEX_NAME])) {
+                    LocalDate dateFromData = LocalDate.parse(rowSplit[INDEX_PERIOD], formatDate);
                     if ((dateFromData.isAfter(startDate) || dateFromData.isEqual(startDate))
                             && (dateFromData.isBefore(endDate) || dateFromData.isEqual(endDate))) {
-                        resultSalary += Integer.parseInt(rowSplit[2])
-                                * Integer.parseInt(rowSplit[3]);
+                        resultSalary += Integer.parseInt(rowSplit[INDEX_SALARY])
+                                * Integer.parseInt(rowSplit[INDEX_INCOME]);
                     }
                 }
             }
