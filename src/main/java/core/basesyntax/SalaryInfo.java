82e908b4eaf4de0034c37e7d2f8cdb69b8806b30
Zinchuk-Yevhenii-Mate <zenya87@gmail.com>
2022-05-20 11:57:51 +0300

    made formatdate constant

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -9,9 +9,9 @@ public class SalaryInfo {
     private static final int INDEX_SALARY = 2;
     private static final int INDEX_INCOME = 3;
     private static final String FORMAT_DATE = "dd.MM.yyyy";
+    private static final DateTimeFormatter formatDate = DateTimeFormatter.ofPattern(FORMAT_DATE);
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        DateTimeFormatter formatDate = DateTimeFormatter.ofPattern(FORMAT_DATE);
         LocalDate startDate = LocalDate.parse(dateFrom, formatDate);
         LocalDate endDate = LocalDate.parse(dateTo, formatDate);
         StringBuilder stringPeriod = new StringBuilder();
