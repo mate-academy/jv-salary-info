ba642d229c287facbe3519e8264bf6daf0d1ac35
Алена <258258>
2022-05-20 19:47:27 +0300

    Created getSalaryInfo method.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -23,17 +23,18 @@ public class SalaryInfo {
         System.out.println(getSalaryInfo(names, data, dateFrom, dateTo));
     }*/
 
-    public static String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
+    public static String getSalaryInfo(String[] names, String[] data,
+                                       String dateFrom, String dateTo) {
         String[] splitDataToArray;
         int[] salaryInfo = new int[names.length];
-        final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        final LocalDate fromDate = LocalDate.parse(dateFrom, DATE_FORMATTER);
-        final LocalDate toDate = LocalDate.parse(dateTo, DATE_FORMATTER);
+        final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        final LocalDate fromDate = LocalDate.parse(dateFrom, dateFormatter);
+        final LocalDate toDate = LocalDate.parse(dateTo, dateFormatter);
 
         for (int j = 0; j < names.length; j++) {
             for (String datum : data) {
                 splitDataToArray = datum.split(" ");
-                LocalDate currentDate = LocalDate.parse(splitDataToArray[0], DATE_FORMATTER);
+                LocalDate currentDate = LocalDate.parse(splitDataToArray[0], dateFormatter);
                 if (names[j].equals(splitDataToArray[1])
                         && (currentDate.isAfter(fromDate) && currentDate.isBefore(toDate)
                         && names[j].equals(splitDataToArray[1])
@@ -46,7 +47,8 @@ public class SalaryInfo {
         }
         StringBuilder builder = new StringBuilder("Report for period " + dateFrom + " - " + dateTo);
         for (int i = 0; i < names.length; i++) {
-            builder.append(System.lineSeparator()).append(names[i]).append(" - ").append(salaryInfo[i]);
+            builder.append(System.lineSeparator()).append(names[i])
+                    .append(" - ").append(salaryInfo[i]);
         }
         return builder.toString();
     }
