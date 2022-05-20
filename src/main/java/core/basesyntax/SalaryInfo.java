1382312317adf68342bd047b1fa09ce03b419fa3
Anton Karpov <antonkarpov547@gmail.com>
2022-05-20 13:52:04 +0200

    Checkstyle improved

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -26,7 +26,7 @@ public class SalaryInfo {
             output.append(names[i]).append(" - ")
                     .append(salaries[i]).append(System.lineSeparator());
         }
-        output.deleteCharAt(output.length() - 2).deleteCharAt(output.length() - 1);
+        output.delete(output.length() - 2, output.length());
         return output.toString();
     }
 }
