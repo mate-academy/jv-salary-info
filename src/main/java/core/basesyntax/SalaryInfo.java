3ec33e077d8924232c5828e99a6976429c278afc
Maksym Karamavrov <makcumkaka@gmail.com>
2022-05-20 19:11:18 +0300

    Method getSalaryInfo was implemented.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,26 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        long localDateFrom = LocalDate.parse(dateFrom, formatter).toEpochDay();
+        long localDateTo = LocalDate.parse(dateTo, formatter).toEpochDay();
+        StringBuilder builder = new StringBuilder();
+        builder.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
+        for (String name: names) {
+            int employeesSalary = 0;
+            for (String dataInfo: data) {
+                String[] dataSplitted = dataInfo.split(" ");
+                long localDateNow = LocalDate.parse(dataSplitted[0], formatter).toEpochDay();
+                if (name.equals(dataSplitted[1]) && (localDateNow >= localDateFrom && localDateNow <= localDateTo)) {
+                    employeesSalary += Integer.parseInt(dataSplitted[2]) * Integer.parseInt(dataSplitted[3]);
+                }
+            }
+            builder.append(System.lineSeparator()).append(name).append(" - ").append(employeesSalary);
+        }
+        return  builder.toString();
     }
 }
