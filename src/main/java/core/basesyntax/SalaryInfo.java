5a7eeaeb1edd6c2e6b8a2578fb951cd0ff294152
Oleksii Kordysh <aleksey.kordysh@gmail.com>
2022-05-20 19:45:50 +0300

    solution implemented, build success

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,39 @@
 package core.basesyntax;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 public class SalaryInfo {
+    private static final SimpleDateFormat SDF = new SimpleDateFormat("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        StringBuilder getSalaryInfo = new StringBuilder("Report for period "
+                                                            + dateFrom + " - " + dateTo);
+        int[] salariesInfo = new int[names.length];
+
+        for (int i = 0; i < names.length; i++) {
+            for (String datum : data) {
+                if (dateNotInRange(datum.split(" ")[0], dateFrom, dateTo)) {
+                    continue;
+                }
+                if (datum.split(" ")[1].equals(names[i])) {
+                    salariesInfo[i] += Integer.parseInt(datum.split(" ")[2])
+                            * Integer.parseInt(datum.split(" ")[3]);
+                }
+            }
+            getSalaryInfo.append(System.lineSeparator()).append(names[i])
+                    .append(" - ").append(salariesInfo[i]);
+        }
+        return getSalaryInfo.toString();
+    }
+
+    private boolean dateNotInRange(String dateChecked, String dateFrom, String dateTo) {
+        try {
+            return SDF.parse(dateChecked).before(SDF.parse(dateFrom))
+                 || SDF.parse(dateChecked).after(SDF.parse(dateTo));
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
+
