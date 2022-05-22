c04a24acfe3daa4bfcea48fa29045b1471df0cec
Maksym Karamavrov <makcumkaka@gmail.com>
2022-05-22 12:57:07 +0300

    formatter was changed for a constatnt field

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,17 +4,18 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        long localDateFrom = LocalDate.parse(dateFrom, formatter).toEpochDay();
-        long localDateTo = LocalDate.parse(dateTo, formatter).toEpochDay();
+        long localDateFrom = LocalDate.parse(dateFrom, FORMATTER).toEpochDay();
+        long localDateTo = LocalDate.parse(dateTo, FORMATTER).toEpochDay();
         StringBuilder builder = new StringBuilder();
         builder.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
         for (String name: names) {
             int employeesSalary = 0;
             for (String dataInfo: data) {
                 String[] dataSplitted = dataInfo.split(" ");
-                long localDateNow = LocalDate.parse(dataSplitted[0], formatter).toEpochDay();
+                long localDateNow = LocalDate.parse(dataSplitted[0], FORMATTER).toEpochDay();
                 if (name.equals(dataSplitted[1])
                         && (localDateNow >= localDateFrom && localDateNow <= localDateTo)) {
                     employeesSalary += Integer.parseInt(dataSplitted[2])
