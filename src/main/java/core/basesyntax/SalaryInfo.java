00f13f10717b7431d2612884913c0b08425497c1
Алена <258258>
2022-05-23 12:55:04 +0300

    Added changes after review.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,30 +4,31 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-        private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        private static final int INDEX_DATA_IN_ARRAY = 0;
-        private static final int INDEX_NAME_IN_ARRAY = 1;
-        private static final int INDEX_WORKING_HOUR_IN_ARRAY = 2;
-        private static final int INDEX_PER_HOUR_IN_ARRAY = 3;
-
+        private static final DateTimeFormatter DATE_FORMATTER
+                = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        private static final int INDEX_DATA = 0;
+        private static final int INDEX_NAME = 1;
+        private static final int INDEX_HOUR = 2;
+        private static final int INDEX_PER_HOUR = 3;
 
         public static String getSalaryInfo(String[] names, String[] data,
                                        String dateFrom, String dateTo) {
         String[] splitDataToArray;
         int[] salaryInfo = new int[names.length];
-        final LocalDate fromDate = LocalDate.parse(dateFrom, DATE_TIME_FORMATTER);
-        final LocalDate toDate = LocalDate.parse(dateTo, DATE_TIME_FORMATTER);
+        final LocalDate fromDate = LocalDate.parse(dateFrom, DATE_FORMATTER);
+        final LocalDate toDate = LocalDate.parse(dateTo, DATE_FORMATTER);
 
         for (int j = 0; j < names.length; j++) {
             for (String datum : data) {
                 splitDataToArray = datum.split(" ");
-                LocalDate currentDate = LocalDate.parse(splitDataToArray[INDEX_DATA_IN_ARRAY], DATE_TIME_FORMATTER);
-                if (names[j].equals(splitDataToArray[INDEX_NAME_IN_ARRAY])
+                LocalDate currentDate
+                        = LocalDate.parse(splitDataToArray[INDEX_DATA], DATE_FORMATTER);
+                if (names[j].equals(splitDataToArray[INDEX_NAME])
                         && (currentDate.isAfter(fromDate) && currentDate.isBefore(toDate)
                         || currentDate.equals(fromDate)
                         || currentDate.equals(toDate))) {
-                    salaryInfo[j] = salaryInfo[j] + (Integer.parseInt(splitDataToArray[INDEX_WORKING_HOUR_IN_ARRAY])
-                            * Integer.parseInt(splitDataToArray[INDEX_PER_HOUR_IN_ARRAY]));
+                    salaryInfo[j] = salaryInfo[j] + (Integer.parseInt(splitDataToArray[INDEX_HOUR])
+                            * Integer.parseInt(splitDataToArray[INDEX_PER_HOUR]));
                 }
             }
         }
