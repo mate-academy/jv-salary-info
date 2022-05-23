7343a5c1afd747421b1c1d106311b327cad77409
Maksym Karamavrov <makcumkaka@gmail.com>
2022-05-23 12:15:42 +0300

    constant fields were addded, LocalDate class and its methods were implemented.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,22 +4,28 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int INDEX_FOR_DATE = 0;
+    private static final int INDEX_HOURS_PER_DAY = 2;
+    private static final int INDEX_SALARY_PER_HOUR = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        long localDateFrom = LocalDate.parse(dateFrom, FORMATTER).toEpochDay();
-        long localDateTo = LocalDate.parse(dateTo, FORMATTER).toEpochDay();
+        LocalDate localDateFrom = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate localDateTo = LocalDate.parse(dateTo, FORMATTER);
         StringBuilder builder = new StringBuilder();
         builder.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
         for (String name: names) {
             int employeesSalary = 0;
             for (String dataInfo: data) {
                 String[] dataSplitted = dataInfo.split(" ");
-                long localDateNow = LocalDate.parse(dataSplitted[0], FORMATTER).toEpochDay();
+                LocalDate localDateNow = LocalDate.parse(dataSplitted[INDEX_FOR_DATE], FORMATTER);
                 if (name.equals(dataSplitted[1])
-                        && (localDateNow >= localDateFrom && localDateNow <= localDateTo)) {
-                    employeesSalary += Integer.parseInt(dataSplitted[2])
-                                    * Integer.parseInt(dataSplitted[3]);
+                        && (localDateNow.isEqual(localDateFrom)
+                        || localDateNow.isAfter(localDateFrom)
+                        && localDateNow.isBefore(localDateTo)
+                        || localDateNow.isEqual(localDateTo))) {
+                    employeesSalary += Integer.parseInt(dataSplitted[INDEX_HOURS_PER_DAY])
+                            * Integer.parseInt(dataSplitted[INDEX_SALARY_PER_HOUR]);
                 }
             }
             builder.append(System.lineSeparator()).append(name)
