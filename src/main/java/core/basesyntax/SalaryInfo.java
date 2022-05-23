7cebab3d9eb94fc319005e361d30e5bc39d05d31
Dima Povkh <dimapovkh@gmail.com>
2022-05-23 15:00:38 +0200

    FROM_DATA -> _INDEX getSalaryInfo -> reportBuilder SDF -> simpleDateFormat comments were removed mvn clean package -> BUILD SUCCESS

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,38 +4,42 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 public class SalaryInfo {
-    private static final SimpleDateFormat SDF = new SimpleDateFormat("dd.MM.yyyy");
-    private static final int DATE_FROM_DATA = 0;
-    private static final int NAME_FROM_DATA = 1;
-    private static final int HOURS_FROM_DATA = 2;
-    private static final int SALARY_PER_HOUR_FROM_DATA = 3;
+    private static final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM.yyyy");
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int HOURS_INDEX = 2;
+    private static final int SALARY_PER_HOUR_INDEX = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        StringBuilder getSalaryInfo = new StringBuilder("Report for period "
-                + dateFrom + " - " + dateTo);
+        StringBuilder reportBuilder = new StringBuilder()
+                .append("Report for period ")
+                .append(dateFrom)
+                .append(" - ")
+                .append(dateTo);
         int[] salariesInfo = new int[names.length];
+
         for (int i = 0; i < names.length; i++) {
-            for (String currentData : data) {
-                if (dateNotInRange(currentData.split(" ")[DATE_FROM_DATA], dateFrom, dateTo)) {
+            for (String line : data) {
+                if (dateNotInRange(line.split(" ")[DATE_INDEX], dateFrom, dateTo)) {
                     continue;
                 }
-                //System.out.println("#1 " + currentData);
-                if (currentData.split(" ")[NAME_FROM_DATA].equals(names[i])) {
-                    salariesInfo[i] += Integer.parseInt(currentData.split(" ")[HOURS_FROM_DATA])
-                            * Integer.parseInt(currentData.split(" ")[SALARY_PER_HOUR_FROM_DATA]);
+                if (line.split(" ")[NAME_INDEX].equals(names[i])) {
+                    salariesInfo[i] += Integer.parseInt(line.split(" ")[HOURS_INDEX])
+                            * Integer.parseInt(line.split(" ")[SALARY_PER_HOUR_INDEX]);
                 }
             }
-            getSalaryInfo.append(System.lineSeparator()).append(names[i])
-                    .append(" - ").append(salariesInfo[i]);
+            reportBuilder.append(System.lineSeparator())
+                    .append(names[i])
+                    .append(" - ")
+                    .append(salariesInfo[i]);
         }
-        System.out.println("#2 " + getSalaryInfo);
-        return getSalaryInfo.toString();
+        return reportBuilder.toString();
     }
 
     private boolean dateNotInRange(String dateChecked, String dateFrom, String dateTo) {
         try {
-            return SDF.parse(dateChecked).before(SDF.parse(dateFrom))
-                    || SDF.parse(dateChecked).after(SDF.parse(dateTo));
+            return simpleDateFormat.parse(dateChecked).before(simpleDateFormat.parse(dateFrom))
+                    || simpleDateFormat.parse(dateChecked).after(simpleDateFormat.parse(dateTo));
         } catch (ParseException e) {
             throw new RuntimeException(e);
         }
