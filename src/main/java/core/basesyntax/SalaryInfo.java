45489715a0c7d2a6f37c033d112ce23c86ba3862
Vasylyi Bevz <bevzvasya@gmail.com>
2022-05-23 15:01:46 +0300

    optimization of code was done

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,32 +4,39 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    private static final String SEPARATOR = " - ";
+    private static final String SPACE = " ";
+    private static final DateTimeFormatter FORMATER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int NEXT_DAY = 1;
+    private static final int DATE_OF_WORK = 0;
+    private static final int NAME_OF_EMPLOYER = 1;
+    private static final int TIME_OF_WORK = 2;
+    private static final int EARN_PER_HOUR = 3;
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
         StringBuilder builder = new StringBuilder();
         int[] empolyerSalaryPeriod = new int[names.length];
         String[] dataUser;
-        LocalDate firstDate = LocalDate.parse(dateFrom, formatter);
-        LocalDate lastDate = LocalDate.parse(dateTo, formatter);
+        LocalDate firstDate = LocalDate.parse(dateFrom, FORMATER);
+        LocalDate lastDate = LocalDate.parse(dateTo, FORMATER);
         LocalDate currentDate;
-        for (String dt : data) {
-            dataUser = dt.split(" ");
-            currentDate = LocalDate.parse(dataUser[0], formatter);
-            if (currentDate.isAfter(firstDate) && currentDate.isBefore(lastDate.plusDays(1))) {
-                for (int j = 0; j < names.length; j++) {
-                    if (names[j].equals(dataUser[1])) {
-                        empolyerSalaryPeriod[j] += Integer.parseInt(dataUser[2])
-                                * Integer.parseInt(dataUser[3]);
+        builder.append("Report for period ").append(dateFrom)
+                .append(SEPARATOR).append(dateTo);
+        for (int i = 0; i < names.length; i++) {
+            for (String rowData : data) {
+                dataUser = rowData.split(SPACE);
+                currentDate = LocalDate.parse(dataUser[DATE_OF_WORK], FORMATER);
+                if (currentDate.isAfter(firstDate)
+                        && currentDate.isBefore(lastDate.plusDays(NEXT_DAY))) {
+                    if (names[i].equals(dataUser[NAME_OF_EMPLOYER])) {
+                        empolyerSalaryPeriod[i] += Integer.parseInt(dataUser[TIME_OF_WORK])
+                                * Integer.parseInt(dataUser[EARN_PER_HOUR]);
                     }
                 }
             }
+            builder.append(System.lineSeparator()).append(names[i]).append(SEPARATOR)
+                    .append(empolyerSalaryPeriod[i]);
         }
-        builder.append("Report for period ").append(dateFrom)
-                .append(" - ").append(dateTo).append(System.lineSeparator());
-        for (int i = 0; i < names.length; i++) {
-            builder.append(names[i]).append(" - ")
-                    .append(empolyerSalaryPeriod[i]).append(System.lineSeparator());
-        }
-        return builder.toString().trim();
+        return builder.toString();
     }
 }
