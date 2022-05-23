0f016e94ebd87e58ef9795393453b6ea6f1d97f2
Mykola Yakymiv <mykola8426@gmail.com>
2022-05-23 12:35:05 +0300

    added fixes after solution

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,14 +4,16 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private static int salary = 0;
+    private static int salary;
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static int INDEX_OF_HOURS = 2;
+    private static int INDEX_OF_SALARY_PER_HOUR = 3;
 
     public static String getSalaryInfo(String[] names, String[] data,
                                        String dateFrom, String dateTo) {
 
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        LocalDate fromDate = LocalDate.parse(dateFrom,formatter);
-        LocalDate toDate = LocalDate.parse(dateTo,formatter);
+        LocalDate fromDate = LocalDate.parse(dateFrom,FORMATTER);
+        LocalDate toDate = LocalDate.parse(dateTo,FORMATTER);
         StringBuilder builder = new StringBuilder();
 
         builder.append("Report for period ").append(dateFrom)
@@ -21,17 +23,17 @@ public class SalaryInfo {
             builder.append(System.lineSeparator()).append(name).append(" - ");
             for (String datum : data) {
                 String[] arr = datum.split(" ");
-                LocalDate currentDate = LocalDate.parse(arr[0], formatter);
+                LocalDate currentDate = LocalDate.parse(arr[0], FORMATTER);
                 if (arr[1].equals(name)) {
                     if ((currentDate.isAfter(fromDate) || currentDate.isEqual(fromDate))
                             && (currentDate.isBefore(toDate) || currentDate.isEqual(toDate))) {
-                        salary += Integer.parseInt(arr[2]) * Integer.parseInt(arr[3]);
+                        salary += Integer.parseInt(arr[INDEX_OF_HOURS])
+                                * Integer.parseInt(arr[INDEX_OF_SALARY_PER_HOUR]);
                     }
                 }
             }
             builder.append(salary);
             salary = 0;
-
         }
         return builder.toString();
     }
