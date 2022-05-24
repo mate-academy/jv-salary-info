1731c6708b3a498aef566a06ada8f01137578b9f
Oleksii Kordysh <aleksey.kordysh@gmail.com>
2022-05-24 15:51:47 +0300

    from local to class constants

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,28 +5,27 @@ import java.text.SimpleDateFormat;
 
 public class SalaryInfo {
     private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd.MM.yyyy");
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int HOURS_INDEX = 2;
+    private static final int SALARY_INDEX = 3;
+    private static final String SEPARATOR = " - ";
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        final int dateIndex = 0;
-        final int nameIndex = 1;
-        final int hoursIndex = 2;
-        final int salaryIndex = 3;
-        final String dateSeparator = " - ";
-
         StringBuilder getSalaryInfo = new StringBuilder("Report for period "
-                + dateFrom + dateSeparator + dateTo);
+                + dateFrom + SEPARATOR + dateTo);
         int[] salariesInfo = new int[names.length];
 
         for (int i = 0; i < names.length; i++) {
             for (String datum : data) {
-                if (isDateInRange(datum.split(" ")[dateIndex], dateFrom, dateTo)
-                        && datum.split(" ")[nameIndex].equals(names[i])) {
-                    salariesInfo[i] += Integer.parseInt(datum.split(" ")[hoursIndex])
-                            * Integer.parseInt(datum.split(" ")[salaryIndex]);
+                if (isDateInRange(datum.split(" ")[DATE_INDEX], dateFrom, dateTo)
+                        && datum.split(" ")[NAME_INDEX].equals(names[i])) {
+                    salariesInfo[i] += Integer.parseInt(datum.split(" ")[HOURS_INDEX])
+                            * Integer.parseInt(datum.split(" ")[SALARY_INDEX]);
                 }
             }
             getSalaryInfo.append(System.lineSeparator()).append(names[i])
-                    .append(" - ").append(salariesInfo[i]);
+                    .append(SEPARATOR).append(salariesInfo[i]);
         }
         return getSalaryInfo.toString();
     }
