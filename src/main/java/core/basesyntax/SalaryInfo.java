8dc91bc2b749628605091bba68fbcc173d089537
Ilya <zimmboco@gmail.com>
2022-05-24 12:31:55 +0300

    hw-5

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -6,10 +6,10 @@ import java.time.format.DateTimeFormatter;
 public class SalaryInfo {
     private static final DateTimeFormatter DATE_TIME_FORMATTER =
             DateTimeFormatter.ofPattern("dd.MM.yyyy");
-    private static final byte dateIndex = 0;
-    private static final byte nameIndex = 1;
-    private static final byte workingHoursIndex = 2;
-    private static final byte salaryPerHourIndex = 3;
+    private static final short DATE_INDEX = 0;
+    private static final short NAME_INDEX = 1;
+    private static final short WORKING_HOURS_INDEX = 2;
+    private static final short SALARY_PER_HOUR_INDEX = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate localDateFrom = LocalDate.parse(dateFrom, DATE_TIME_FORMATTER);
@@ -17,19 +17,18 @@ public class SalaryInfo {
         StringBuilder result = new StringBuilder();
 
         result.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
-        String[] arrayOfData;
 
         for (String name : names) {
             int salary = 0;
             for (String line : data) {
-                arrayOfData = line.split(" ");
-                LocalDate currentDate = LocalDate.parse(arrayOfData[dateIndex],
+                String[] arrayOfData = line.split(" ");
+                LocalDate currentDate = LocalDate.parse(data[DATE_INDEX],
                         DATE_TIME_FORMATTER);
                 if (localDateFrom.compareTo(currentDate) <= 0
                         && localDateTo.compareTo(currentDate) >= 0
-                        && arrayOfData[nameIndex].equals(name)) {
-                    salary = salary + (Integer.parseInt(arrayOfData[salaryPerHourIndex])
-                            * Integer.parseInt(arrayOfData[workingHoursIndex]));
+                        && arrayOfData[NAME_INDEX].equals(name)) {
+                    salary = salary + (Integer.parseInt(arrayOfData[SALARY_PER_HOUR_INDEX])
+                            * Integer.parseInt(arrayOfData[WORKING_HOURS_INDEX]));
                 }
             }
             result.append('\n').append(name).append(" - ").append(salary);
