ac3596f17af82928a3e02a8783116dc40ac55fcb
Dima Povkh <dimapovkh@gmail.com>
2022-05-25 17:56:46 +0200

    line.split(" ") -> splittedLine LocalDate

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,47 +1,39 @@
 package core.basesyntax;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private static final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM.yyyy");
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
     private static final int DATE_INDEX = 0;
     private static final int NAME_INDEX = 1;
     private static final int HOURS_INDEX = 2;
     private static final int SALARY_PER_HOUR_INDEX = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
+        LocalDate localDateFrom = LocalDate.parse(dateFrom, formatter);
+        LocalDate localDateTo = LocalDate.parse(dateTo, formatter);
         StringBuilder reportBuilder = new StringBuilder()
                 .append("Report for period ")
                 .append(dateFrom)
                 .append(" - ")
                 .append(dateTo);
-        int[] salariesInfo = new int[names.length];
-        for (int i = 0; i < names.length; i++) {
+
+        String[] dataArray;
+        for (String name : names) {
+            int salary = 0;
             for (String line : data) {
-                String[] splittedLine = line.split(" ");
-                if (dateNotInRange(splittedLine[DATE_INDEX], dateFrom, dateTo)) {
-                    continue;
-                }
-                if (splittedLine[NAME_INDEX].equals(names[i])) {
-                    salariesInfo[i] += Integer.parseInt(splittedLine[HOURS_INDEX])
-                            * Integer.parseInt(splittedLine[SALARY_PER_HOUR_INDEX]);
+                dataArray = line.split(" ");
+                LocalDate currentDate = LocalDate.parse(dataArray[DATE_INDEX], formatter);
+                if (((localDateFrom.isBefore(currentDate)) || localDateFrom.isEqual(currentDate))
+                        && ((localDateTo.isAfter(currentDate)) || localDateTo.isEqual(currentDate))
+                        && (dataArray[NAME_INDEX].equals(name))) {
+                    salary += Integer.parseInt(dataArray[HOURS_INDEX])
+                            * Integer.parseInt(dataArray[SALARY_PER_HOUR_INDEX]);
                 }
             }
-            reportBuilder.append(System.lineSeparator())
-                    .append(names[i])
-                    .append(" - ")
-                    .append(salariesInfo[i]);
+            reportBuilder.append(System.lineSeparator()).append(name).append(" - ").append(salary);
         }
         return reportBuilder.toString();
-    }
-
-    private boolean dateNotInRange(String dateChecked, String dateFrom, String dateTo) {
-        try {
-            return simpleDateFormat.parse(dateChecked).before(simpleDateFormat.parse(dateFrom))
-                    || simpleDateFormat.parse(dateChecked).after(simpleDateFormat.parse(dateTo));
-        } catch (ParseException e) {
-            throw new RuntimeException(e);
-        }
     }
 }
