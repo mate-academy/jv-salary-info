194b1e895519b274f5f588292dccb48832de768d
Sergey Gordeychuk <ya.gordeichuk@gmail.com>
2022-05-31 10:58:00 +0300

    ÐeDleted Main and variable.. Changed the names of the variables.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -12,32 +12,30 @@ public class SalaryInfo {
             DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        StringBuilder salaryWorker = new StringBuilder();
+        StringBuilder reportBuilder = new StringBuilder();
         LocalDate localDateFrom = LocalDate.parse(dateFrom, DATE_FORMATTER);
         LocalDate localDateTo = LocalDate.parse(dateTo, DATE_FORMATTER);
-        String name;
-        salaryWorker.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
-        for (String nameWorkers : names) {
-            name = nameWorkers;
+        reportBuilder.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
+        for (String name : names) {
             int moneyEarned = 0;
             for (String personData : data) {
-                String[] getSalaryInfo = personData.split(" ");
-                LocalDate localDateStartWork =
-                        LocalDate.parse(getSalaryInfo[DAY_START_WORK_INDEX], DATE_FORMATTER);
-                String nameWorker = getSalaryInfo[NAME_INDEX].toString();
-                int hoursOfWorkInDay = Integer.parseInt(getSalaryInfo[HOURS_OF_WORK_IN_DAY_INDEX]);
-                int salaryInHour = Integer.parseInt(getSalaryInfo[SALARY_IN_HOUR_INDEX]);
-                if (name.equals(nameWorker)
-                        && (localDateStartWork.isAfter(localDateFrom)
-                        || localDateStartWork.equals(localDateFrom))
-                        && (localDateStartWork.isBefore(localDateTo)
-                        || localDateStartWork.equals(localDateTo))) {
-                    moneyEarned += hoursOfWorkInDay * salaryInHour;
+                String[] splitedPersonData = personData.split(" ");
+                LocalDate workStartDate =
+                        LocalDate.parse(splitedPersonData[DAY_START_WORK_INDEX], DATE_FORMATTER);
+                String workerName = splitedPersonData[NAME_INDEX];
+                int hoursOfWorkInDay = Integer.parseInt(splitedPersonData[HOURS_OF_WORK_IN_DAY_INDEX]);
+                int salaryPerHour = Integer.parseInt(splitedPersonData[SALARY_IN_HOUR_INDEX]);
+                if (name.equals(workerName)
+                        && (workStartDate.isAfter(localDateFrom)
+                        || workStartDate.equals(localDateFrom))
+                        && (workStartDate.isBefore(localDateTo)
+                        || workStartDate.equals(localDateTo))) {
+                    moneyEarned += hoursOfWorkInDay * salaryPerHour;
                 }
             }
-            salaryWorker.append(System.lineSeparator())
+            reportBuilder.append(System.lineSeparator())
                     .append(name).append(" - ").append(moneyEarned);
         }
-        return salaryWorker.toString();
+        return reportBuilder.toString();
     }
 }
