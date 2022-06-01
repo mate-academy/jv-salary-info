82a74dd400f6bbc6941f5e392b5573424a037104
Sergii Uglovyi <s.uglovyi@gmail.com>
2022-06-01 13:39:40 +0300

    moved the magic numbers to constants

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,12 +4,15 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    static final String DATE_FORMAT = "dd.MM.yyyy";
+    static final String SPACE_SPLITTER = " ";
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         String[] result = new String[names.length + 1];
         String[] dataDB;
         int salary;
 
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_FORMAT);
         LocalDate fromAsDate = LocalDate.parse(dateFrom, formatter);
         LocalDate toAsDate = LocalDate.parse(dateTo, formatter);
 
@@ -17,7 +20,7 @@ public class SalaryInfo {
         for (int i = 0; i < names.length; i++) {
             salary = 0;
             for (String dateElem:data) {
-                dataDB = dateElem.split(" ");
+                dataDB = dateElem.split(SPACE_SPLITTER);
                 boolean dateInRange = LocalDate.parse(dataDB[0], formatter).isAfter(fromAsDate)
                         && LocalDate.parse(dataDB[0], formatter).isBefore(toAsDate)
                         || LocalDate.parse(dataDB[0], formatter).isEqual(fromAsDate)
