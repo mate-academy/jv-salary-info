5789bc20736dcd0bbc63992ef41d53d0b9ad7b52
Sergii Uglovyi <s.uglovyi@gmail.com>
2022-06-01 22:18:36 +0300

    fix Dasha remark 2

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -13,25 +13,28 @@ public class SalaryInfo {
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder result = new StringBuilder();
-        String[] dataBase;
+        String[] rowDataElements;
         int salary;
-
         LocalDate fromAsDate = LocalDate.parse(dateFrom, formatter);
         LocalDate toAsDate = LocalDate.parse(dateTo, formatter);
-
         result.append(String.format("Report for period %s - %s", dateFrom, dateTo));
         for (String name : names) {
             salary = 0;
-            for (String dateElem : data) {
-                dataBase = dateElem.split(SPACE_SPLITTER);
-                boolean dateInRange
-                        = LocalDate.parse(dataBase[DATE_COLUMN], formatter).isAfter(fromAsDate)
-                        && LocalDate.parse(dataBase[DATE_COLUMN], formatter).isBefore(toAsDate)
-                        || LocalDate.parse(dataBase[DATE_COLUMN], formatter).isEqual(fromAsDate)
-                        || LocalDate.parse(dataBase[DATE_COLUMN], formatter).isEqual(toAsDate);
-                if (!name.isEmpty() && name.equals(dataBase[EMPLOYEE_COLUMN]) && dateInRange) {
-                    salary += Integer.parseInt(dataBase[HOUR_COLUMN])
-                            * Integer.parseInt(dataBase[SALARY_COLUMN]);
+            for (String rowData : data) {
+                rowDataElements = rowData.split(SPACE_SPLITTER);
+                boolean isDateSuitable
+                        = LocalDate.parse(rowDataElements[DATE_COLUMN],
+                        formatter).isAfter(fromAsDate)
+                        && LocalDate.parse(rowDataElements[DATE_COLUMN],
+                        formatter).isBefore(toAsDate)
+                        || LocalDate.parse(rowDataElements[DATE_COLUMN],
+                        formatter).isEqual(fromAsDate)
+                        || LocalDate.parse(rowDataElements[DATE_COLUMN],
+                        formatter).isEqual(toAsDate);
+                if (!name.isEmpty() && name.equals(rowDataElements[EMPLOYEE_COLUMN])
+                        && isDateSuitable) {
+                    salary += Integer.parseInt(rowDataElements[HOUR_COLUMN])
+                            * Integer.parseInt(rowDataElements[SALARY_COLUMN]);
                 }
             }
             result.append(System.lineSeparator()).append(String.format("%s - %d", name, salary));
