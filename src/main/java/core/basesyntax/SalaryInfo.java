f84213001484e17ec3ed8bf15b76bd9cc373b8ae
Sergii Uglovyi <s.uglovyi@gmail.com>
2022-06-01 16:32:08 +0300

    fix Dasha remark

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,33 +4,38 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    static final String DATE_FORMAT = "dd.MM.yyyy";
-    static final String SPACE_SPLITTER = " ";
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final String SPACE_SPLITTER = " ";
+    private static final int DATE_COLUMN = 0;
+    private static final int EMPLOYEE_COLUMN = 1;
+    private static final int HOUR_COLUMN = 2;
+    private static final int SALARY_COLUMN = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        String[] result = new String[names.length + 1];
-        String[] dataDB;
+        StringBuilder result = new StringBuilder();
+        String[] dataBase;
         int salary;
 
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_FORMAT);
         LocalDate fromAsDate = LocalDate.parse(dateFrom, formatter);
         LocalDate toAsDate = LocalDate.parse(dateTo, formatter);
 
-        result[0] = String.format("Report for period %s - %s", dateFrom, dateTo);
-        for (int i = 0; i < names.length; i++) {
+        result.append(String.format("Report for period %s - %s", dateFrom, dateTo));
+        for (String name : names) {
             salary = 0;
-            for (String dateElem:data) {
-                dataDB = dateElem.split(SPACE_SPLITTER);
-                boolean dateInRange = LocalDate.parse(dataDB[0], formatter).isAfter(fromAsDate)
-                        && LocalDate.parse(dataDB[0], formatter).isBefore(toAsDate)
-                        || LocalDate.parse(dataDB[0], formatter).isEqual(fromAsDate)
-                        || LocalDate.parse(dataDB[0], formatter).isEqual(toAsDate);
-                if (!names[i].isEmpty() && names[i].equals(dataDB[1]) && dateInRange) {
-                    salary += Integer.parseInt(dataDB[2]) * Integer.parseInt(dataDB[3]);
+            for (String dateElem : data) {
+                dataBase = dateElem.split(SPACE_SPLITTER);
+                boolean dateInRange
+                        = LocalDate.parse(dataBase[DATE_COLUMN], formatter).isAfter(fromAsDate)
+                        && LocalDate.parse(dataBase[DATE_COLUMN], formatter).isBefore(toAsDate)
+                        || LocalDate.parse(dataBase[DATE_COLUMN], formatter).isEqual(fromAsDate)
+                        || LocalDate.parse(dataBase[DATE_COLUMN], formatter).isEqual(toAsDate);
+                if (!name.isEmpty() && name.equals(dataBase[EMPLOYEE_COLUMN]) && dateInRange) {
+                    salary += Integer.parseInt(dataBase[HOUR_COLUMN])
+                            * Integer.parseInt(dataBase[SALARY_COLUMN]);
                 }
             }
-            result[i + 1] = String.format("%s - %d", names[i], salary);
+            result.append(System.lineSeparator()).append(String.format("%s - %d", name, salary));
         }
-        return String.join(System.lineSeparator(), result);
+        return result.toString();
     }
 }
