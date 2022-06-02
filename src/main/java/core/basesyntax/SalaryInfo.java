d3a494cef9887032b03f7cf65fb1c601b645f947
Nata <natserbo@gmail.com>
2022-06-02 18:37:31 +0300

    add Main temporary

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -6,16 +6,19 @@ public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate fromDate = LocalDate.parse(dateFrom);
         LocalDate toDate = LocalDate.parse(dateTo);
-        for (int i = 0; i < data.length; i++) {
+        int i = 0;
+        StringBuilder[] namesHoursMoneyData = new StringBuilder[0];
+        for (String dateElement : data) {
             String[] dataInfo = data[i].split(" ");
             LocalDate workDate = LocalDate.parse(dataInfo[0]);
 
             if (workDate.isBefore(toDate) && workDate.isAfter(fromDate)
                     || workDate.equals(toDate) || workDate.equals(fromDate)) {
-
+                namesHoursMoneyData[i].append(dataInfo[1]).append(dataInfo[2]).append(dataInfo[3]);
+                i++;
             }
-
-
-            return null;
         }
+
+        return namesHoursMoneyData.toString();
     }
+}
