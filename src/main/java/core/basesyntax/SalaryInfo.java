77a0804a363fd1539ea7406281aaf357b98b063d
Nata <natserbo@gmail.com>
2022-06-02 18:12:14 +0300

    date sorting realisation

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,21 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        LocalDate fromDate = LocalDate.parse(dateFrom);
+        LocalDate toDate = LocalDate.parse(dateTo);
+        for (int i = 0; i < data.length; i++) {
+            String[] dataInfo = data[i].split(" ");
+            LocalDate workDate = LocalDate.parse(dataInfo[0]);
+
+            if (workDate.isBefore(toDate) && workDate.isAfter(fromDate)
+                    || workDate.equals(toDate) || workDate.equals(fromDate)) {
+
+            }
+
+
+            return null;
+        }
     }
-}
