f3ee7a0852c537857d55f051c1bfd8802d02c722
Nata <natserbo@gmail.com>
2022-06-03 17:54:38 +0300

    getSalaryInfo method realisation

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,24 +1,36 @@
 package core.basesyntax;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        LocalDate fromDate = LocalDate.parse(dateFrom);
-        LocalDate toDate = LocalDate.parse(dateTo);
-        int i = 0;
-        StringBuilder[] namesHoursMoneyData = new StringBuilder[0];
-        for (String dateElement : data) {
-            String[] dataInfo = data[i].split(" ");
-            LocalDate workDate = LocalDate.parse(dataInfo[0]);
-
+        LocalDate fromDate = LocalDate.parse(dateFrom,DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        LocalDate toDate = LocalDate.parse(dateTo, DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        int[] salary = new int[names.length];
+        StringBuilder resultString = new StringBuilder();
+        for (String datum : data) {
+            String[] splitedData = datum.split(" ");
+            LocalDate workDate = LocalDate.parse(splitedData[0],
+                    DateTimeFormatter.ofPattern("dd.MM.yyyy"));
             if (workDate.isBefore(toDate) && workDate.isAfter(fromDate)
                     || workDate.equals(toDate) || workDate.equals(fromDate)) {
-                namesHoursMoneyData[i].append(dataInfo[1]).append(dataInfo[2]).append(dataInfo[3]);
-                i++;
+                for (int k = 0; k < names.length; k++) {
+                    if (splitedData[1].equals(names[k])) {
+                        salary[k] += Integer.parseInt(splitedData[2])
+                                * Integer.parseInt(splitedData[3]);
+                    }
+                }
             }
         }
+        for (int i = 0; i < names.length; i++) {
+            resultString.append(System.lineSeparator())
+                    .append(names[i])
+                    .append(" - ")
+                    .append(salary[i]);
+        }
 
-        return namesHoursMoneyData.toString();
+        return "Report for period " + dateFrom + " - " + dateTo
+                + resultString;
     }
 }
