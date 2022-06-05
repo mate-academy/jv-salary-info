452f5e77b9e5c1f4ef57decacb9eff2eef469534
Nata <natserbo@gmail.com>
2022-06-05 14:11:06 +0300

    rename salary ->salaryCounter

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -9,7 +9,7 @@ public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate fromDate = LocalDate.parse(dateFrom, DateTimeFormatter.ofPattern(formatter));
         LocalDate toDate = LocalDate.parse(dateTo, DateTimeFormatter.ofPattern(formatter));
-        int[] salary = new int[names.length];
+        int[] salaryCounter = new int[names.length];
         StringBuilder resultString = new StringBuilder();
         for (int k = 0; k < names.length; k++) {
             for (String datum : data) {
@@ -19,18 +19,15 @@ public class SalaryInfo {
                 if (workDay.isBefore(toDate) && workDay.isAfter(fromDate)
                         || workDay.equals(toDate) || workDay.equals(fromDate)) {
                     if (splitData[1].equals(names[k])) {
-                        salary[k] += Integer.parseInt(splitData[2])
+                        salaryCounter[k] += Integer.parseInt(splitData[2])
                                 * Integer.parseInt(splitData[3]);
-
                     }
                 }
-
             }
             resultString.append(System.lineSeparator())
                     .append(names[k])
                     .append(" - ")
-                    .append(salary[k]);
-
+                    .append(salaryCounter[k]);
         }
         return "Report for period " + dateFrom + " - " + dateTo
                 + resultString;
