b037ff78902a536126e6f7eda53120fa7584506c
Nata <natserbo@gmail.com>
2022-06-05 12:26:09 +0300

    formatter -> constant, rename splitData

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -2,23 +2,25 @@ package core.basesyntax;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Formatter;
 
 public class SalaryInfo {
+    String formatter = "dd.MM.yyyy";
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        LocalDate fromDate = LocalDate.parse(dateFrom,DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-        LocalDate toDate = LocalDate.parse(dateTo, DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        LocalDate fromDate = LocalDate.parse(dateFrom,DateTimeFormatter.ofPattern(formatter));
+        LocalDate toDate = LocalDate.parse(dateTo, DateTimeFormatter.ofPattern(formatter));
         int[] salary = new int[names.length];
         StringBuilder resultString = new StringBuilder();
         for (String datum : data) {
-            String[] splitedData = datum.split(" ");
-            LocalDate workDate = LocalDate.parse(splitedData[0],
+            String[] splitData = datum.split(" ");
+            LocalDate workDate = LocalDate.parse(splitData[0],
                     DateTimeFormatter.ofPattern("dd.MM.yyyy"));
             if (workDate.isBefore(toDate) && workDate.isAfter(fromDate)
                     || workDate.equals(toDate) || workDate.equals(fromDate)) {
                 for (int k = 0; k < names.length; k++) {
-                    if (splitedData[1].equals(names[k])) {
-                        salary[k] += Integer.parseInt(splitedData[2])
-                                * Integer.parseInt(splitedData[3]);
+                    if (splitData[1].equals(names[k])) {
+                        salary[k] += Integer.parseInt(splitData[2])
+                                * Integer.parseInt(splitData[3]);
                     }
                 }
             }
