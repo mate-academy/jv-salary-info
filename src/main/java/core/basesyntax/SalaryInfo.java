f3ae91555f610abf97b771a37fc4fa61e0ae205c
Nata <natserbo@gmail.com>
2022-06-05 13:22:26 +0300

    replace loops

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -2,36 +2,36 @@ package core.basesyntax;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Formatter;
 
 public class SalaryInfo {
-    String formatter = "dd.MM.yyyy";
+    private static final String formatter = "dd.MM.yyyy";
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        LocalDate fromDate = LocalDate.parse(dateFrom,DateTimeFormatter.ofPattern(formatter));
+        LocalDate fromDate = LocalDate.parse(dateFrom, DateTimeFormatter.ofPattern(formatter));
         LocalDate toDate = LocalDate.parse(dateTo, DateTimeFormatter.ofPattern(formatter));
         int[] salary = new int[names.length];
         StringBuilder resultString = new StringBuilder();
-        for (String datum : data) {
-            String[] splitData = datum.split(" ");
-            LocalDate workDay = LocalDate.parse(splitData[0],
-                    DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-            if (workDay.isBefore(toDate) && workDay.isAfter(fromDate)
-                    || workDay.equals(toDate) || workDay.equals(fromDate)) {
-                for (int k = 0; k < names.length; k++) {
+        for (int k = 0; k < names.length; k++) {
+            for (String datum : data) {
+                String[] splitData = datum.split(" ");
+                LocalDate workDay = LocalDate.parse(splitData[0],
+                        DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+                if (workDay.isBefore(toDate) && workDay.isAfter(fromDate)
+                        || workDay.equals(toDate) || workDay.equals(fromDate)) {
                     if (splitData[1].equals(names[k])) {
                         salary[k] += Integer.parseInt(splitData[2])
                                 * Integer.parseInt(splitData[3]);
+
                     }
                 }
-            }
-        }
-        for (int i = 0; i < names.length; i++) {
-            resultString.append(System.lineSeparator())
-                    .append(names[i])
-                    .append(" - ")
-                    .append(salary[i]);
-        }
 
+            }
+            resultString.append(System.lineSeparator())
+                    .append(names[k])
+                    .append(" - ")
+                    .append(salary[k]);
+
+        }
         return "Report for period " + dateFrom + " - " + dateTo
                 + resultString;
     }
