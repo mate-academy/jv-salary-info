7aaa7ecb4326aa1cc98d890a7623561adcaf9e38
Misha Polunin <polunin1986@gmail.com>
2022-06-09 11:55:12 +0300

    - done

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,37 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int START_WORK = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int HOURS_OF_WORK = 2;
+    private static final int SALARY_FOR_HOUR = 3;
+    private static final String SEPARATOR = System.lineSeparator();
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        LocalDate localDateFrom = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate localDateTo = LocalDate.parse(dateTo, FORMATTER);
+        StringBuilder result = new StringBuilder().append("Report for period ")
+                .append(dateFrom).append(" - ").append(dateTo);
+        String[] arraysOfData;
+
+        for (String name : names) {
+            int salary = 0;
+            for (String line : data) {
+                arraysOfData = line.split(" ");
+                LocalDate current = LocalDate.parse(arraysOfData[START_WORK], FORMATTER);
+                if (arraysOfData[NAME_INDEX].equals(name) && (current.isAfter(localDateFrom)
+                        || current.isEqual(localDateFrom)) && (current.isBefore(localDateTo)
+                        || current.isEqual(localDateTo))) {
+                    salary += Integer.parseInt(arraysOfData[SALARY_FOR_HOUR])
+                            * Integer.parseInt(arraysOfData[HOURS_OF_WORK]);
+                }
+            }
+            result.append(SEPARATOR).append(name).append(" - ").append(salary);
+        }
+        return result.toString();
     }
 }
