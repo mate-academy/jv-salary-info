575dd59b618b1193fd8ae2a2cd709109152ff9b2
Roman Voitalik <voitalik1@gmail.com>
2022-06-14 14:56:00 +0300

    fix changes, use LocalDate methods to compare dates

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -7,6 +7,10 @@ import java.time.format.DateTimeParseException;
 public class SalaryInfo {
     private static final String DATA_SEPARATOR = " ";
     private static final String MINUS_SEPARATOR = " - ";
+    private static final int DATE_INDEX = 0;
+    private static final int PERSON_INDEX = 1;
+    private static final int HOUR_INDEX = 2;
+    private static final int PAY_FOR_HOUR_INDEX = 3;
     private static final DateTimeFormatter formatter =
             DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
@@ -15,28 +19,28 @@ public class SalaryInfo {
         int i = 0;
         StringBuilder report = new StringBuilder("Report for period ");
         report.append(dateFrom).append(MINUS_SEPARATOR).append(dateTo);
-        // initiate date variables
         LocalDate startDate;
         LocalDate endDate;
+        LocalDate currentDate;
         try {
             startDate = LocalDate.parse(dateFrom, formatter);
             endDate = LocalDate.parse(dateTo, formatter);
         } catch (DateTimeParseException e) {
-            throw new RuntimeException("Date is not parsable.");
+            throw new RuntimeException("Date is not parsable.", e);
         }
         for (String name : names) {
             for (String dataValue : data) {
                 String[] splittedData = dataValue.split(DATA_SEPARATOR);
-                LocalDate currentDate = LocalDate.parse(splittedData[0], formatter);
                 try {
-                    LocalDate.parse(splittedData[0], formatter);
+                    currentDate = LocalDate.parse(splittedData[DATE_INDEX], formatter);
                 } catch (DateTimeParseException e) {
-                    throw new RuntimeException("Date is not parsable.");
+                    throw new RuntimeException("Date is not parsable.", e);
                 }
-                if (name.equals(splittedData[1]) && currentDate.compareTo(startDate) >= 0
-                        && currentDate.compareTo(endDate) <= 0) {
-                    int hour = Integer.parseInt(splittedData[2]);
-                    int payPerHour = Integer.parseInt(splittedData[3]);
+                if (name.equals(splittedData[PERSON_INDEX])
+                        && currentDate.isAfter(startDate.minusDays(1))
+                        && currentDate.isBefore(endDate.plusDays(1))) {
+                    int hour = Integer.parseInt(splittedData[HOUR_INDEX]);
+                    int payPerHour = Integer.parseInt(splittedData[PAY_FOR_HOUR_INDEX]);
                     sum[i] += hour * payPerHour;
                 }
             }
