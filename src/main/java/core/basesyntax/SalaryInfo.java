967920b99813351dfcf56e07f63bdb8c2dde6f60
SergiiUstinchenko <s.ustinchenko@gmail.com>
2022-06-24 13:15:15 +0300

    Fixed after viewing

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,10 +5,10 @@ import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-    final static int FIRST_ELEMENT_OF_ARRAY = 0;
-    final static int SECOND_ELEMENT_OF_ARRAY = 1;
-    final static int THIRD_ELEMENT_OF_ARRAY = 2;
-    final static int FOURTH_ELEMENT_OF_ARRAY = 3;
+    private static final int FIRST_ELEMENT_OF_ARRAY = 0;
+    private static final int SECOND_ELEMENT_OF_ARRAY = 1;
+    private static final int THIRD_ELEMENT_OF_ARRAY = 2;
+    private static final int FOURTH_ELEMENT_OF_ARRAY = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder employeeSalaryOfPeriod = new StringBuilder("Report for period "
@@ -22,7 +22,8 @@ public class SalaryInfo {
             for (int i = 0; i < data.length; i++) {
                 String[] temp = data[i].split(" ");
                 if (LocalDate.parse(temp[FIRST_ELEMENT_OF_ARRAY], FORMATTER).isAfter(dateBegin)
-                        && LocalDate.parse(temp[FIRST_ELEMENT_OF_ARRAY], FORMATTER).isBefore(dateEnd)
+                        && LocalDate.parse(temp[FIRST_ELEMENT_OF_ARRAY], FORMATTER)
+                        .isBefore(dateEnd)
                         && name.equals(temp[SECOND_ELEMENT_OF_ARRAY])) {
                     int hours = Integer.parseInt(temp[THIRD_ELEMENT_OF_ARRAY]);
                     int hourSalary = Integer.parseInt(temp[FOURTH_ELEMENT_OF_ARRAY]);
