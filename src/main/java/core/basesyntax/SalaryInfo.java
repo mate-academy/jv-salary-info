7ff2a9996e2b138339e33d29a49c110b195ad8f2
Azarov Artur <azarovartyr@gmail.com>
2022-06-28 02:50:06 +0300

    renamed constant field in upper case and refactored switch case

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,32 +4,22 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        LocalDate boundDateFrom = LocalDate.parse(dateFrom,formatter);
-        LocalDate boundDateTo = LocalDate.parse(dateTo,formatter);
+        LocalDate boundDateFrom = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate boundDateTo = LocalDate.parse(dateTo, FORMATTER);
         int[] sumSalary = new int[names.length];
         for (String employeeData : data) {
             String[] employeeDataArray = employeeData.split(" ");
-            LocalDate workingDate = LocalDate.parse(employeeDataArray[0],formatter);
+            LocalDate workingDate = LocalDate.parse(employeeDataArray[0], FORMATTER);
             if ((workingDate.isEqual(boundDateFrom) || workingDate.isAfter(boundDateFrom))
                     && (workingDate.isEqual(boundDateTo) || workingDate.isBefore(boundDateTo))) {
-                switch (employeeDataArray[1]) {
-                    case("John"):
-                        sumSalary[0] += Integer.parseInt(employeeDataArray[2])
+                for (int i = 0; i < names.length; i++) {
+                    if (names[i].equals(employeeDataArray[1])) {
+                        sumSalary[i] += Integer.parseInt(employeeDataArray[2])
                                 * Integer.parseInt(employeeDataArray[3]);
-                        break;
-                    case("Andrew"):
-                        sumSalary[1] += Integer.parseInt(employeeDataArray[2])
-                                * Integer.parseInt(employeeDataArray[3]);
-                        break;
-                    case("Kate"):
-                        sumSalary[2] += Integer.parseInt(employeeDataArray[2])
-                                * Integer.parseInt(employeeDataArray[3]);
-                        break;
-                    default:
-                        break;
+                    }
                 }
             }
         }
