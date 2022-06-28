3b0b175e563512a613a556995ed70e352f3e5c25
Yurii Lisovskyi <yuri.lisovski@gmail.com>
2022-06-28 21:21:11 +0300

    Clean and optimize the code

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,19 +5,28 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class SalaryInfo {
+
+    private static final byte DATE = 0;
+    private static final byte WORKERNAME = 1;
+    private static final byte HOURS = 2;
+    private static final byte PAYPERHOUR = 3;
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder resultSalaryReport =
-                new StringBuilder("Report for period "
-                        + dateFrom + " - " + dateTo);
+                new StringBuilder()
+                        .append("Report for period ")
+                        .append(dateFrom)
+                        .append(" - ")
+                        .append(dateTo);
         try {
             Date dateBegin = new SimpleDateFormat("dd.MM.yyyy").parse(dateFrom);
             Date dateEnd = new SimpleDateFormat("dd.MM.yyyy").parse(dateTo);
-            for (String name : names) {
+            for (String workerName : names) {
                 resultSalaryReport
-                        .append("\r\n")
-                        .append(name)
+                        .append(System.lineSeparator())
+                        .append(workerName)
                         .append(" - ")
-                        .append(salaryCounter(name, data, dateBegin, dateEnd));
+                        .append(workerSalaryCalculator(workerName, data, dateBegin, dateEnd));
             }
         } catch (ParseException e) {
             System.out.println("bad data parameters format");
@@ -27,23 +36,22 @@ public class SalaryInfo {
         return resultSalaryReport.toString();
     }
 
-    int salaryCounter(String name, String[] data, Date dateBegin, Date dateEnd) {
-        int result = 0;
-        for (String jobInfo : data) {
-            String[] dataParsedString = jobInfo.split(" ");
+    int workerSalaryCalculator(String workerName, String[] data, Date dateBegin, Date dateEnd) {
+        int totalWorkerSalary = 0;
+        for (String workerJobInfo : data) {
+            String[] dataParsedString = workerJobInfo.split(" ");
             try {
                 Date salaryDate = new SimpleDateFormat("dd.MM.yyyy")
-                        .parse(dataParsedString[0]);
-                if ((salaryDate.after(dateBegin) || salaryDate.equals(dateBegin))
+                        .parse(dataParsedString[DATE]);
+                totalWorkerSalary += ((salaryDate.after(dateBegin) || salaryDate.equals(dateBegin))
                         && (salaryDate.before(dateEnd) || salaryDate.equals(dateEnd))
-                        && name.equals(dataParsedString[1])) {
-                    result += Integer.parseInt(dataParsedString[2])
-                            * Integer.parseInt(dataParsedString[3]);
-                }
+                        && workerName.equals(dataParsedString[WORKERNAME])) ?
+                        Integer.parseInt(dataParsedString[HOURS])
+                        * Integer.parseInt(dataParsedString[PAYPERHOUR]) : 0 ;
             } catch (ParseException e) {
                 System.out.println("bad user data format");
             }
         }
-        return result;
+        return totalWorkerSalary;
     }
 }
