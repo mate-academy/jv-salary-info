2e75a02d07f252695a6d673c2dad784a59dcf8fd
Nikita Ostrenskyi <nikostrik2003@gmail.com>
2022-06-29 16:37:13 +0300

    fixed mistakes

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,16 +4,16 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private static final int DATA = 0;
-    private static final int EMPLOYEE = 1;
-    private static final int HOURS = 2;
-    private static final int SALARY = 3;
-    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int DATE_INDEX = 0;
+    private static final int EMPLOYEE_NAME_INDEX = 1;
+    private static final int HOURS_INDEX = 2;
+    private static final int SALARY_PER_HOUR_INDEX = 3;
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         int fullSalary = 0;
-        LocalDate dayFrom = LocalDate.parse(dateFrom, formatter);
-        LocalDate dayTo = LocalDate.parse(dateTo, formatter);
+        LocalDate dayFrom = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate dayTo = LocalDate.parse(dateTo, FORMATTER);
         StringBuilder employeeInfo = new StringBuilder()
                 .append("Report for period ")
                 .append(dateFrom)
@@ -23,12 +23,13 @@ public class SalaryInfo {
         for (String employeeName : names) {
             for (String dayInfo : data) {
                 String[] arrayInfo = dayInfo.split(" ");
-                LocalDate currentData = LocalDate.parse(arrayInfo[DATA], formatter);
-                fullSalary += ((dayFrom.isBefore(currentData)) || dayFrom.equals(currentData))
+                LocalDate currentData = LocalDate.parse(arrayInfo[DATE_INDEX], FORMATTER);
+                if (((dayFrom.isBefore(currentData)) || dayFrom.equals(currentData))
                             && ((dayTo.isAfter(currentData)) || dayTo.equals(currentData))
-                            && (arrayInfo[EMPLOYEE].equals(employeeName))
-                            ? Integer.parseInt(arrayInfo[HOURS])
-                            * Integer.parseInt(arrayInfo[SALARY]) : 0;
+                            && (arrayInfo[EMPLOYEE_NAME_INDEX].equals(employeeName))) {
+                    fullSalary += Integer.parseInt(arrayInfo[HOURS_INDEX])
+                                * Integer.parseInt(arrayInfo[SALARY_PER_HOUR_INDEX]);
+                }
             }
             employeeInfo.append(System.lineSeparator())
                         .append(employeeName)
