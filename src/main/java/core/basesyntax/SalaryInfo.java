e864aa8175e34b801b9147851d031bb4e3432425
DOM <kasinets.ulia@gmail.com>
2022-06-30 10:03:30 +0300

    made magic numbers constants

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,6 +5,10 @@ import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int WORKING_HOURS_INDEX = 2;
+    private static final int SALARY_INDEX = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder employeeSalaryOfPeriod = new StringBuilder("Report for period "
@@ -21,10 +25,10 @@ public class SalaryInfo {
             for (int i = 0; i < data.length; i++) {
                 String[][] dataInfo = new String[data.length][4];
                 dataInfo[i] = data[i].split(" ");
-                localDate = LocalDate.parse(dataInfo[i][0], FORMATTER);
-                String employeeNames = dataInfo[i][1];
-                int workingHours = Integer.parseInt(dataInfo[i][2]);
-                int salaryForHours = Integer.parseInt(dataInfo[i][3]);
+                localDate = LocalDate.parse(dataInfo[i][DATE_INDEX], FORMATTER);
+                String employeeNames = dataInfo[i][NAME_INDEX];
+                int workingHours = Integer.parseInt(dataInfo[i][WORKING_HOURS_INDEX]);
+                int salaryForHours = Integer.parseInt(dataInfo[i][SALARY_INDEX]);
                 if (names[j].equals(employeeNames) && localDate.plusDays(1).isAfter(localDateFrom)
                         && localDate.minusDays(1).isBefore(localDateTo)) {
                     salary += workingHours * salaryForHours;
