386512d1c401bfdd03117bf5733fc0d441084952
Oleksandr <dovy_al@ukr.net>
2022-06-30 11:50:51 +0300

    fixes after review

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -6,6 +6,13 @@ import java.time.format.DateTimeFormatter;
 public class SalaryInfo {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
+    private static class Data {
+        private static final int date = 0;
+        private static final int name = 1;
+        private static final int workingHours = 2;
+        private static final int incomePerHour = 3;
+    }
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate localDateFrom = LocalDate.parse(dateFrom, FORMATTER);
         LocalDate localDateTo = LocalDate.parse(dateTo, FORMATTER);
@@ -17,12 +24,12 @@ public class SalaryInfo {
             int salary = 0;
             for (String info : data) {
                 arrayInfo = info.split(" ");
-                LocalDate currentDate = LocalDate.parse(arrayInfo[0], FORMATTER);
+                LocalDate currentDate = LocalDate.parse(arrayInfo[Data.date], FORMATTER);
                 if (localDateFrom.compareTo(currentDate) <= 0
                         && localDateTo.compareTo(currentDate) >= 0
-                        && arrayInfo[1].equals(name)) {
-                    salary += (Integer.parseInt(arrayInfo[2])
-                            * Integer.parseInt(arrayInfo[3]));
+                        && arrayInfo[Data.name].equals(name)) {
+                    salary += (Integer.parseInt(arrayInfo[Data.workingHours])
+                            * Integer.parseInt(arrayInfo[Data.incomePerHour]));
                 }
             }
             salaryInfo.append(System.lineSeparator()).append(name).append(" - ").append(salary);
