dea162153123e9fc67ef49a503f9f2418fcd24c5
Valerii-Sh <val-sh@ukr.net>
2022-07-02 14:55:26 +0300

    implement method getSalaryInfo

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,42 @@
 package core.basesyntax;
 
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDate;
+
 public class SalaryInfo {
-    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+    private static final DateTimeFormatter DATE_PATTERN = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int INDEX_OF_DATE = 0;
+    private static final int INDEX_OF_NAME = 1;
+    private static final int INDEX_OF_HOUR = 2;
+    private static final int INDEX_OF_INCOME_PER_HOUR = 3;
+
+
+
+    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo){
+        LocalDate startDate = LocalDate.parse(dateFrom, DATE_PATTERN);
+        LocalDate endDate = LocalDate.parse(dateTo, DATE_PATTERN);
+        StringBuilder result = new StringBuilder();
+        result.append("Report for period ")
+                .append(dateFrom)
+                .append(" - ")
+                .append(dateTo);
+        for (String name : names) {
+            result.append(System.getProperty("line.separator"));
+            int salaryEmployee = 0;
+            result.append(name).append(" - ");
+            for (String recordData : data) {
+                String[] arrayData = recordData.split(" ");
+                LocalDate date = LocalDate.parse(arrayData[INDEX_OF_DATE], DATE_PATTERN);
+                boolean isRequiredName = name.equals(arrayData[INDEX_OF_NAME]);
+                boolean isRequiredDate = ((date.isEqual(startDate) || date.isAfter(startDate))
+                        && (date.isBefore(endDate) || date.isEqual(endDate)));
+                if (isRequiredName && isRequiredDate) {
+                    salaryEmployee += (Integer.parseInt(arrayData[INDEX_OF_HOUR])
+                            * Integer.parseInt(arrayData[INDEX_OF_INCOME_PER_HOUR]));
+                }
+            }
+            result.append(salaryEmployee);
+        }
+        return result.toString();
     }
 }
