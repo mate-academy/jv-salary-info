0d5a722b708d932eeafe686fd469ea239fe4425e
Vasyl Piznak <piznak3145@gmail.com>
2022-07-05 20:48:19 +0300

    Rename splettedLine. Add modifier to constant FORMATTER.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,7 +4,7 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate localDateFrom = LocalDate.parse(dateFrom,FORMATTER);
@@ -17,12 +17,13 @@ public class SalaryInfo {
         for (String name : names) {
             int salary = 0;
             for (String lines:data) {
-                String[] split = lines.split(" ");
-                LocalDate currentDate = LocalDate.parse(split[0], FORMATTER);
+                String[] splittedLine = lines.split(" ");
+                LocalDate currentDate = LocalDate.parse(splittedLine[0], FORMATTER);
                 if (currentDate.compareTo(localDateFrom) >= 0
                         && currentDate.compareTo(localDateTo) <= 0
-                        && split[1].equals(name)) {
-                    salary = salary + Integer.parseInt(split[2]) * Integer.parseInt(split[3]);
+                        && splittedLine[1].equals(name)) {
+                    salary = salary + Integer.parseInt(splittedLine[2])
+                            * Integer.parseInt(splittedLine[3]);
                 }
             }
             result.append(System.lineSeparator()).append(name).append(" - ").append(salary);
