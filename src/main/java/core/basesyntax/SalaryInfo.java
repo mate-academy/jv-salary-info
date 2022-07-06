a54def5205d35fe4fa623d1ca45d4e98334805bf
Vasyl Piznak <piznak3145@gmail.com>
2022-07-06 19:22:17 +0300

    Use method equals,isAfter and isBefore. Create constants.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,6 +5,10 @@ import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int INDEX_DATA = 0;
+    private static final int INDEX_NAME = 1;
+    private static final int INDEX_HOURES = 2;
+    private static final int INDEX_INCOME_PER_HOUR = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate localDateFrom = LocalDate.parse(dateFrom,FORMATTER);
@@ -18,12 +22,12 @@ public class SalaryInfo {
             int salary = 0;
             for (String lines:data) {
                 String[] splittedLine = lines.split(" ");
-                LocalDate currentDate = LocalDate.parse(splittedLine[0], FORMATTER);
-                if (currentDate.compareTo(localDateFrom) >= 0
-                        && currentDate.compareTo(localDateTo) <= 0
-                        && splittedLine[1].equals(name)) {
-                    salary = salary + Integer.parseInt(splittedLine[2])
-                            * Integer.parseInt(splittedLine[3]);
+                LocalDate currentDate = LocalDate.parse(splittedLine[INDEX_DATA], FORMATTER);
+                if ((currentDate.equals(localDateFrom) || currentDate.isAfter(localDateFrom))
+                        && (currentDate.equals(localDateTo) || currentDate.isBefore(localDateTo))
+                        && splittedLine[INDEX_NAME].equals(name)) {
+                    salary = salary + Integer.parseInt(splittedLine[INDEX_HOURES])
+                            * Integer.parseInt(splittedLine[INDEX_INCOME_PER_HOUR]);
                 }
             }
             result.append(System.lineSeparator()).append(name).append(" - ").append(salary);
