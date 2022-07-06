3c53d6c19cb7479613122708845d3a0e78a0dfdb
Andrii Zakhaarchenko <C:\Progs\BatBase>
2022-07-06 22:28:41 +0300

    jv-salary-info-hw6-done-common-mistakes-fixed

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,14 +1,17 @@
 package core.basesyntax;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    private static final String DATE_FORMAT = "dd.MM.yyyy";
+    private static final String HEAD_MESSAGE = "Report for period ";
+    private static final String IOEXEPTION_MESSAGE = "We have IOExeption";
     private StringBuilder totalSalaryInfo = new StringBuilder();
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         totalSalaryInfo.delete(0, totalSalaryInfo.length());
-        totalSalaryInfo.append("Report for period " + dateFrom + " - " + dateTo);
+        totalSalaryInfo.append(HEAD_MESSAGE + dateFrom + " - " + dateTo);
         for (String name : names) {
             int usersSalary = 0;
             if (name == null || name.equals("")) {
@@ -17,22 +20,22 @@ public class SalaryInfo {
             for (int i = 0; i < data.length; i++) {
                 try {
                     String[] dataParse = data[i].split("\\s");
-                    Date dataParseDate =
-                            new SimpleDateFormat("dd.MM.yyyy").parse(dataParse[0]);
-                    Date dateFromDat =
-                            new SimpleDateFormat("dd.MM.yyyy").parse(dateFrom);
-                    Date dateToDate =
-                            new SimpleDateFormat("dd.MM.yyyy").parse(dateTo);
-                    if ((dataParseDate.after(dateFromDat)
+                    LocalDate dataParseDate =
+                            LocalDate.parse(dataParse[0], DateTimeFormatter.ofPattern(DATE_FORMAT));
+                    LocalDate dateFromDat =
+                            LocalDate.parse(dateFrom, DateTimeFormatter.ofPattern(DATE_FORMAT));
+                    LocalDate dateToDate =
+                            LocalDate.parse(dateTo, DateTimeFormatter.ofPattern(DATE_FORMAT));
+                    if ((dataParseDate.isAfter(dateFromDat)
                             || dataParseDate.equals(dateFromDat))
-                            && (dataParseDate.before(dateToDate)
+                            && (dataParseDate.isBefore(dateToDate)
                             || dataParseDate.equals(dateToDate))
                             && name.equals(dataParse[1])) {
                         usersSalary += Integer.valueOf(dataParse[2])
                                 * Integer.valueOf(dataParse[3]);
                     }
                 } catch (Exception e) {
-                    System.out.println("We have IOExeption");
+                    System.out.println(IOEXEPTION_MESSAGE);
                 }
             }
             totalSalaryInfo.append(System.lineSeparator() + name + " - " + usersSalary);
