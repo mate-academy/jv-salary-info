e9b44ab6effdf44ba1cc0838d1ba03d40a3ebf9f
Maksym Bakunov <mpbakunov.it@gmail.com>
2022-07-10 19:26:25 +0200

    implementation of the task condition

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,44 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class SalaryInfo {
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        final String FORMAT_STRING ="dd.MM.yyyy";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(FORMAT_STRING);
+        String [] parsedData;
+        LocalDate dateFromD, dateToD, Day;
+        int employeeSalary;
+
+        try {
+            dateFromD = LocalDate.parse(dateFrom, formatter);
+            dateToD = LocalDate.parse(dateTo, formatter);
+        } catch (DateTimeParseException ex) {
+            System.out.println("Not correct date!");
+            return null;
+        }
+        StringBuilder salaryInfo = new StringBuilder("Report for period " + dateFrom + " - " + dateTo);
+
+        for (String name : names) {
+            employeeSalary = 0;
+            for (String str : data) {
+                parsedData = str.split(" ");
+                if (name.equals(parsedData[1])) {
+                    try {
+                        Day = LocalDate.parse(parsedData[0], formatter);
+                        if ((dateFromD.isBefore(Day) || dateFromD.equals(Day)) && (dateToD.isAfter(Day) || dateToD.equals(Day))) {
+                            employeeSalary = employeeSalary + Integer.parseInt(parsedData[2]) * Integer.parseInt(parsedData[3]);
+                        }
+                    } catch (DateTimeParseException ex) {
+                        System.out.println("Not correct day!");
+                    }
+                }
+            }
+            salaryInfo.append(System.lineSeparator()).append(name).append(" - ").append(employeeSalary);
+        }
+        return salaryInfo.toString();
     }
 }
