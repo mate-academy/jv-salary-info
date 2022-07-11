e9e6bf4f4e972bc3c37baf303df3ab0536715745
Andrii Zakhaarchenko <C:\Progs\BatBase>
2022-07-11 20:56:34 +0300

    jv-salary-info-hw6-done-with-0-1-2-3-as-constant-after-missunderstanding

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -6,6 +6,10 @@ import java.time.format.DateTimeFormatter;
 public class SalaryInfo {
     private static final DateTimeFormatter
             DATETIME_FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int WORKING_DAY_DATE = 0;
+    private static final int WORKER_NAME = 1;
+    private static final int WORKING_HOUR_PER_DAY = 2;
+    private static final int WORKING_SALARY_PER_HOUR = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         final String headMessage = "Report for period ";
@@ -15,12 +19,9 @@ public class SalaryInfo {
             int usersSalary = 0;
             for (int i = 0; i < data.length; i++) {
                 String[] dataParse = data[i].split("\\s");
-                String workingDayDate = dataParse[0];
-                String workerName = dataParse[1];
-                String workingHourPerDay = dataParse[2];
-                String workingSalaryPerHour = dataParse[3];
+
                 LocalDate dataParseDate =
-                        LocalDate.parse(workingDayDate, DATETIME_FORMATTER);
+                        LocalDate.parse(dataParse[WORKING_DAY_DATE], DATETIME_FORMATTER);
                 LocalDate dateFromDat =
                         LocalDate.parse(dateFrom, DATETIME_FORMATTER);
                 LocalDate dateToDate =
@@ -29,9 +30,9 @@ public class SalaryInfo {
                         || dataParseDate.equals(dateFromDat))
                         && (dataParseDate.isBefore(dateToDate)
                         || dataParseDate.equals(dateToDate))
-                        && name.equals(workerName)) {
-                    usersSalary += Integer.valueOf(workingHourPerDay)
-                            * Integer.valueOf(workingSalaryPerHour);
+                        && name.equals(dataParse[WORKER_NAME])) {
+                    usersSalary += Integer.valueOf(dataParse[WORKING_HOUR_PER_DAY])
+                            * Integer.valueOf(dataParse[WORKING_SALARY_PER_HOUR]);
                 }
             }
             totalSalaryInfo.append(System.lineSeparator() + name + " - " + usersSalary);
