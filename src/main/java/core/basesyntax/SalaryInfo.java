c7be58a7c9a538ab6d8a59dafe25ef8a2c2a36a2
Andrii Zakhaarchenko <C:\Progs\BatBase>
2022-07-11 16:17:57 +0300

    jv-salary-info-hw6-done-Formatter-as-Class-constant-and-some-small-fixes

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,34 +4,34 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    private static final DateTimeFormatter
+            DATETIME_FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        final String dateFormat = "dd.MM.yyyy";
         final String headMessage = "Report for period ";
-        final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(dateFormat);
         StringBuilder totalSalaryInfo = new StringBuilder();
-        totalSalaryInfo.delete(0, totalSalaryInfo.length());
         totalSalaryInfo.append(headMessage + dateFrom + " - " + dateTo);
         for (String name : names) {
             int usersSalary = 0;
-            if (name == null || name.equals("")) {
-                continue;
-            }
             for (int i = 0; i < data.length; i++) {
                 String[] dataParse = data[i].split("\\s");
+                String workingDayDate = dataParse[0];
+                String workerName = dataParse[1];
+                String workingHourPerDay = dataParse[2];
+                String workingSalaryPerHour = dataParse[3];
                 LocalDate dataParseDate =
-                        LocalDate.parse(dataParse[0], dateTimeFormatter);
+                        LocalDate.parse(workingDayDate, DATETIME_FORMATTER);
                 LocalDate dateFromDat =
-                        LocalDate.parse(dateFrom, dateTimeFormatter);
+                        LocalDate.parse(dateFrom, DATETIME_FORMATTER);
                 LocalDate dateToDate =
-                        LocalDate.parse(dateTo, dateTimeFormatter);
+                        LocalDate.parse(dateTo, DATETIME_FORMATTER);
                 if ((dataParseDate.isAfter(dateFromDat)
                         || dataParseDate.equals(dateFromDat))
                         && (dataParseDate.isBefore(dateToDate)
                         || dataParseDate.equals(dateToDate))
-                        && name.equals(dataParse[1])) {
-                    usersSalary += Integer.valueOf(dataParse[2])
-                            * Integer.valueOf(dataParse[3]);
+                        && name.equals(workerName)) {
+                    usersSalary += Integer.valueOf(workingHourPerDay)
+                            * Integer.valueOf(workingSalaryPerHour);
                 }
             }
             totalSalaryInfo.append(System.lineSeparator() + name + " - " + usersSalary);
