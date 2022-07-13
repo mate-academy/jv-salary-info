0aa4299f40bbc8906d9678a17b6517a83a37d454
Andrii Malenko <andmalenko@gmail.com>
2022-07-13 16:45:30 +0300

    I've done this task. Please check

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,28 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        StringBuilder builder = new StringBuilder("Report for period "
+                + dateFrom + " - " + dateTo);
+        for (String array:names) {
+            builder.append(System.lineSeparator()).append(array).append(" - ");
+            int sum = 0;
+            for (int i = 0; i < data.length; i++) {
+                String[] newArray = data[i].split(" ");
+                LocalDate date = LocalDate.parse(newArray[0], FORMATTER);
+                if (date.isAfter(LocalDate.parse(dateFrom, FORMATTER))
+                        && !date.isAfter(LocalDate.parse(dateTo, FORMATTER))
+                        && array.equals(newArray[1])) {
+                    sum += Integer.parseInt(newArray[2]) * Integer.parseInt(newArray[3]);
+                }
+            }
+            builder.append(sum);
+        }
+        return builder.toString();
     }
 }
