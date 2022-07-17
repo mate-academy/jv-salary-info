354b89b30c97e141bc9c1c3897563b59f96c14a0
Dmytro Stakhiv <dstahiv@gmail.com>
2022-07-17 18:41:47 +0300

    Homework Salary Info

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,36 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+    private static final DateTimeFormatter FORMATT = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int ZERO = 0;
+    private static final int ONE = 1;
+    private static final int TWO = 2;
+    private static final int THREE = 3;
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        LocalDate localDateFrom = LocalDate.parse(dateFrom, FORMATT);
+        LocalDate localDateTo = LocalDate.parse(dateTo, FORMATT);
+        StringBuilder info = new StringBuilder().append("Report for period ")
+                .append(dateFrom).append(" - ").append(dateTo);
+        String[] arrayOfData;
+
+        for (String name : names) {
+            int salary = 0;
+            for (String dat : data) {
+                arrayOfData = dat.split(" ");
+                LocalDate today = LocalDate.parse(arrayOfData[ZERO], FORMATT);
+                if (localDateFrom.compareTo(today) <= 0
+                        && localDateTo.compareTo(today) >= 0
+                        && arrayOfData[ONE].equals(name)) {
+                    salary += (Integer.parseInt(arrayOfData[THREE])
+                            * Integer.parseInt(arrayOfData[TWO]));
+                }
+            }
+            info.append(System.lineSeparator()).append(name).append(" - ").append(salary);
+        }
+        return info.toString();
     }
 }
