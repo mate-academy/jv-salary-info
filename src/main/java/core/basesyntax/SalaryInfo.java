ecf6b7f21bf33b82614aae56ea64f8621d1343b1
Serhii Davydov <serhii.davydov@gmail.com>
2022-07-18 16:31:51 +0300

    Replace compareTo() to isEqual(), isAfter() and isBefore()

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,45 +5,36 @@ import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     private static final String PATTERN_OF_DATE = "dd.MM.yyyy";
-    private static final String REGEX_OF_DATA = " ";
-    private static final int INDEX_DATE = 0;
-    private static final int INDEX_NAME_EMPLOYEE = 1;
-    private static final int INDEX_SALARY = 2;
-    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern(PATTERN_OF_DATE);
+    private static final String LINE_SEPARATOR = " ";
+    private static final int INDEX_OF_DATE = 0;
+    private static final int INDEX_OF_NAME = 1;
+    private static final int INDEX_OF_HOURS = 2;
+    private static final int INDEX_OF_SALARY = 3;
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern(PATTERN_OF_DATE);
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        LocalDate startDate = LocalDate.parse(dateFrom, formatter);
-        LocalDate endDate = LocalDate.parse(dateTo, formatter);
-        StringBuilder builder = new StringBuilder("Report for period ");
-        builder.append(dateFrom).append(" - ").append(dateTo);
+        LocalDate startDate = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate endDate = LocalDate.parse(dateTo, FORMATTER);
+        StringBuilder reportBuilder = new StringBuilder("Report for period ");
+        reportBuilder.append(dateFrom).append(" - ").append(dateTo);
+
         for (String name : names) {
             int salary = 0;
-            for (String dataOfEmployee : data) {
-                if (getNameFromData(dataOfEmployee).equals(name)
-                        && getDateFromData(dataOfEmployee).compareTo(startDate) >= 0
-                        && getDateFromData(dataOfEmployee).compareTo(endDate) <= 0) {
-                    salary += getSalaryFromDay(dataOfEmployee);
+            for (String line : data) {
+                String[] dataOfEmployee = line.split(LINE_SEPARATOR);
+                LocalDate dateOfWork = LocalDate.parse(dataOfEmployee[INDEX_OF_DATE], FORMATTER);
+                String nameEmployee = dataOfEmployee[INDEX_OF_NAME];
+                int amountHoursWork = Integer.parseInt(dataOfEmployee[INDEX_OF_HOURS]);
+                int salaryPerHour = Integer.parseInt(dataOfEmployee[INDEX_OF_SALARY]);
+
+                if (nameEmployee.equals(name) && (dateOfWork.isEqual(startDate)
+                        || dateOfWork.isAfter(startDate)) && (dateOfWork.isBefore(endDate)
+                        || dateOfWork.isEqual(endDate))) {
+                    salary += amountHoursWork * salaryPerHour;
                 }
             }
-            builder.append(System.lineSeparator()).append(name).append(" - ").append(salary);
+            reportBuilder.append(System.lineSeparator()).append(name).append(" - ").append(salary);
         }
-        return builder.toString();
-    }
-
-    public String getNameFromData(String dataOfEmployee) {
-        String[] arrayOfData = dataOfEmployee.split(REGEX_OF_DATA);
-        return arrayOfData[INDEX_NAME_EMPLOYEE];
-
-    }
-
-    public LocalDate getDateFromData(String dataOfEmployee) {
-        String[] arrayOfData = dataOfEmployee.split(REGEX_OF_DATA);
-        return LocalDate.parse(arrayOfData[INDEX_DATE], formatter);
-
-    }
-
-    public int getSalaryFromDay(String dataOfEmployee) {
-        String[] arrayOfData = dataOfEmployee.split(REGEX_OF_DATA);
-        return Integer.valueOf(arrayOfData[INDEX_SALARY]) * Integer.valueOf(arrayOfData[3]);
+        return reportBuilder.toString();
     }
 }
