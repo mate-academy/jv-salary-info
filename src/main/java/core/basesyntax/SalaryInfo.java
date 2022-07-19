e2e03658fcbbfd5babf29bb5851d4909a21f15f8
Oleksandr Gotin <alekander.gotin@gmail.com>
2022-07-19 20:25:39 +0300

    code changed after mentor's remarks

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -6,30 +6,30 @@ import java.time.format.DateTimeFormatter;
 public class SalaryInfo {
     private static final int DATE_INDEX = 0;
     private static final int NAME_INDEX = 1;
-    private static final int HOUR_INDEX_ = 2;
+    private static final int HOUR_INDEX = 2;
     private static final int SALARY_INDEX = 3;
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
+        int salary = 0;
         LocalDate startDate = LocalDate.parse(dateFrom, FORMATTER);
-        LocalDate endDate = LocalDate.parse(dateTo, FORMATTER).plusDays(1);
-        StringBuilder report = new StringBuilder("Report for period ")
+        LocalDate endDate = LocalDate.parse(dateTo, FORMATTER);
+        StringBuilder reportBuilder = new StringBuilder("Report for period ")
                 .append(dateFrom).append(" - ").append(dateTo);
         for (String name : names) {
-            int salary = 0;
-            report.append(System.lineSeparator());
             for (String line : data) {
-                String[] value = line.split(" ");
-                if (value[1].equals(name)) {
-                    LocalDate workDay = LocalDate.parse(value[0], FORMATTER);
-                    if (workDay.isAfter(startDate) && workDay.isBefore(endDate)) {
-                        salary = salary + Integer.parseInt(value[2])
-                                * Integer.parseInt(value[3]);
-                    }
+                String[] splittedLine = line.split(" ");
+                LocalDate workDay = LocalDate.parse(splittedLine[DATE_INDEX], FORMATTER);
+                if (((startDate.isBefore(workDay)) || startDate.equals(workDay))
+                        && ((endDate.isAfter(workDay)) || endDate.equals(workDay))
+                        && (splittedLine[NAME_INDEX].equals(name))) {
+                    salary += Integer.parseInt(splittedLine[HOUR_INDEX])
+                            * Integer.parseInt(splittedLine[SALARY_INDEX]);
                 }
             }
-            report.append(name).append(" - ").append(salary);
+            reportBuilder.append(System.lineSeparator()).append(name).append(" - ").append(salary);
+            salary = 0;
         }
-        return report.toString();
+        return reportBuilder.toString();
     }
 }
