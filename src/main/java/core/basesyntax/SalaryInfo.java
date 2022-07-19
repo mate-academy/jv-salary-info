9631b73e07ae5effd0029608b02a11956da64170
Mykyta Hulianytskyi <yadiver82@gmail.com>
2022-07-19 21:58:51 +0300

    v2.0 fixed names of constant variables and some modifiers, fixed append-concatenating, and so on

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,30 +4,32 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    static final byte dateIndex = 0;
-    static final byte nameIndex = 1;
-    static final byte workingDaysIndex = 2;
-    static final byte hourlyRateIndex = 3;
-    static final DateTimeFormatter formatter =
+    private static final byte DATE_INDEX = 0;
+    private static final byte NAME_INDEX = 1;
+    private static final byte WORKING_DAYS_INDEX = 2;
+    private static final byte HOURLY_RATE_INDEX = 3;
+    private static final DateTimeFormatter DATE_FORMATTER =
             DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        StringBuilder report = new StringBuilder("Report for period " + dateFrom
-                + " - " + dateTo);
-        LocalDate dtFrom = LocalDate.parse(dateFrom, formatter).minusDays(1);
-        LocalDate dtTo = LocalDate.parse(dateTo, formatter).plusDays(1);
-        int[] salary = new int[names.length];
-        for (int i = 0; i < names.length; i++) {
-            for (String z:data) {
-                String[] dataArray = z.split(" ");
-                LocalDate neededDt = LocalDate.parse(dataArray[dateIndex], formatter);
-                if (neededDt.isAfter(dtFrom) && neededDt.isBefore(dtTo)
-                        && names[i].equals(dataArray[nameIndex])) {
-                    salary[i] += Integer.parseInt(dataArray[workingDaysIndex])
-                            * Integer.parseInt(dataArray[hourlyRateIndex]);
+        StringBuilder report = new StringBuilder("Report for period ")
+                .append(dateFrom).append(" - ").append(dateTo);
+        // minus and plus one day because we need theese days inclusively,
+        // and I think it's better than to use two more .equals() in if() statement in a doubleloop
+        LocalDate localDateFrom = LocalDate.parse(dateFrom, DATE_FORMATTER).minusDays(1);
+        LocalDate localDateTo = LocalDate.parse(dateTo, DATE_FORMATTER).plusDays(1);
+        for (String name : names) {
+            int salary = 0;
+            for (String line : data) {
+                String[] splittedLine = line.split(" ");
+                LocalDate currentDate = LocalDate.parse(splittedLine[DATE_INDEX], DATE_FORMATTER);
+                if (currentDate.isAfter(localDateFrom) && currentDate.isBefore(localDateTo)
+                        && name.equals(splittedLine[NAME_INDEX])) {
+                    salary += Integer.parseInt(splittedLine[WORKING_DAYS_INDEX])
+                            * Integer.parseInt(splittedLine[HOURLY_RATE_INDEX]);
                 }
             }
-            report.append(System.lineSeparator() + names[i] + " - " + salary[i]);
+            report.append(System.lineSeparator()).append(name).append(" - ").append(salary);
         }
         return report.toString();
     }
