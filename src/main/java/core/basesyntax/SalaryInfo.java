7d145341447358616536540063a36a47e1a86e79
Serhii Nazarenko <nasarenko82@gmail.com>
2022-07-20 16:43:44 +0300

    performed for the first time

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -3,44 +3,35 @@ package core.basesyntax;
 import static java.lang.Integer.parseInt;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
+        StringBuilder reportBuilder = new StringBuilder();
+        reportBuilder.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
         for (String name : names) {
             int salary = 0;
-            for (String periodFromData : data) {
-                int indexDate = periodFromData.indexOf(" ");
-                int indexName = periodFromData.substring(indexDate + 1)
-                        .indexOf(" ") + indexDate + 1;
-                int indexHour = periodFromData.substring(indexName + 1)
-                        .indexOf(" ") + indexName + 1;
-                LocalDate workday = getLocalDate(periodFromData.substring(0, indexDate));
-                String nameUser = periodFromData.substring(indexDate + 1, indexName);
-                String hourFromArray = periodFromData.substring(indexName + 1, indexHour);
-                String salaryFromArray = periodFromData.substring(indexHour + 1);
+            for (String line : data) {
+                String[] arrayLine = line.split(" ");
+                LocalDate workday = getLocalDate(arrayLine[0]);
                 if ((getLocalDate(dateFrom).isBefore(workday)
                         || getLocalDate(dateFrom).isEqual(workday))
                         && (getLocalDate(dateTo).isAfter(workday)
                         || getLocalDate(dateTo).isEqual(workday))) {
-                    if (name.equals(nameUser)) {
-                        salary = salary + parseInt(salaryFromArray)
-                                * parseInt(hourFromArray);
+                    if (name.equals(arrayLine[1])) {
+                        salary = salary + parseInt(arrayLine[3])
+                                * parseInt(arrayLine[2]);
                     }
                 }
             }
-            stringBuilder.append(System.lineSeparator()).append(name).append(" - ").append(salary);
+            reportBuilder.append(System.lineSeparator()).append(name).append(" - ").append(salary);
         }
-        return stringBuilder.toString();
+        return reportBuilder.toString();
     }
 
     public LocalDate getLocalDate(String dateString) {
-        int separator1 = dateString.indexOf(".");
-        int separator2 = dateString.substring(separator1 + 1).indexOf(".") + separator1 + 1;
-        int day = parseInt(dateString.substring(0, separator1));
-        int month = parseInt(dateString.substring(separator1 + 1, separator2));
-        int year = parseInt(dateString.substring(separator2 + 1));
-        return LocalDate.of(year, month, day);
+        LocalDate localDate = LocalDate
+                .parse(dateString, DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        return localDate;
     }
 }
