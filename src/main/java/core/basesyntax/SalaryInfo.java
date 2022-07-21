3856073acb5ef1f15472351c0e256b8b72935187
Anna Popova <anitapopova1996@gmail.com>
2022-07-21 12:38:10 +0300

    Implemented getSalaryInfo() in SalaryInfo class.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,48 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
-    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+
+    public static String getSalaryInfo(String[] names, String[] data, String dateFrom,
+                                       String dateTo) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d.MM.yyyy");
+        LocalDate from = LocalDate.parse(dateFrom, formatter);
+        LocalDate to = LocalDate.parse(dateTo, formatter);
+        StringBuilder reportForPeriod = new StringBuilder()
+                .append("Report for period ")
+                .append(dateFrom)
+                .append(" - ")
+                .append(dateTo)
+                .append(System.lineSeparator());
+
+        for (int i = 0; i < names.length; i++) {
+            int totalSumForPeriod = 0;
+            for (int j = 0; j < data.length; j++) {
+                if (data[j].contains(names[i])) {
+                    String[] splittedDataArray = data[j].split(" ");
+                    LocalDate workDay = LocalDate.parse(splittedDataArray[0], formatter);
+                    if (workDay.isAfter(from) && workDay.isBefore(to)
+                            || workDay.isEqual(from) || workDay.isEqual(to)) {
+                        int[] paymentPerHour = new int[]{
+                                Integer.parseInt(splittedDataArray[2]),
+                                Integer.parseInt(splittedDataArray[3])
+                        };
+                        totalSumForPeriod = paymentPerHour[0] * paymentPerHour[1]
+                                + totalSumForPeriod;
+                    }
+                }
+            }
+            reportForPeriod
+                    .append(names[i])
+                    .append(" - ")
+                    .append(totalSumForPeriod)
+                    .append(System.lineSeparator());
+        }
+        return reportForPeriod
+                .delete(reportForPeriod.length() - 2, reportForPeriod.length())
+                .toString();
     }
+
 }
