374e27de8c7f9738a060a7f202a58c42cd562817
l_osyash <los.nazar24@gmail.com>
2022-07-21 19:25:18 +0300

    Changed logic. Added fixes.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,80 +1,79 @@
 package core.basesyntax;
 
-import core.basesyntax.db.Storage;
+import core.basesyntax.model.Employee;
 import core.basesyntax.model.EmployeeWorkDayInfo;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
 import java.util.stream.Collectors;
 
 public class SalaryInfo {
-    private static final int INDEX_OF_DATES = 0;
-    private static final int INDEX_OF_NAMES = 1;
-    private static final int INDEX_OF_HOURS = 2;
-    private static final int INDEX_OF_WAGES = 3;
-    private static final DateTimeFormatter TIME_FORMATTER
+    private static final int INDEX_OF_DATE = 0;
+    private static final int INDEX_OF_NAME = 1;
+    private static final int INDEX_OF_HOUR = 2;
+    private static final int INDEX_OF_WAGE = 3;
+    private static final DateTimeFormatter DATE_FORMATTER
             = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        List<EmployeeWorkDayInfo> employeeWorkDayInfoList = parseData(data);
-        filterByDate(employeeWorkDayInfoList, dateFrom, dateTo);
-        Queue<String> statistic = calculate(names);
-        Storage.getEmployeeWorkDayInfo().clear();
-        return makeReport(statistic, names, dateFrom, dateTo);
+        List<EmployeeWorkDayInfo> parsedData = parseData(data);
+        List<EmployeeWorkDayInfo> filteredData = filterByDate(parsedData, dateFrom, dateTo);
+        List<Employee> calculatedInfo = calculate(filteredData, names);
+        return makeReport(calculatedInfo, names, dateFrom, dateTo);
     }
 
-    public String makeReport(Queue<String> statistic, String[] names,
+    public String makeReport(List<Employee> employees, String[] names,
                              String dateFrom, String dateTo) {
-        String header = "Report for period " + dateFrom + " - " + dateTo + System.lineSeparator();
-        StringBuilder builder = new StringBuilder(header);
+        StringBuilder reportBuilder = new StringBuilder();
+        reportBuilder.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
         Arrays.stream(names).forEach(name -> {
-            builder.append(statistic.poll());
-            if (statistic.peek() != null) {
-                builder.append(System.lineSeparator());
-            }
+            Employee employee = employees.stream()
+                    .filter(person -> person.getName().equals(name))
+                    .findFirst()
+                    .get();
+            reportBuilder.append(System.lineSeparator())
+                    .append(employee.getName()).append(" - ")
+                    .append(employee.getSalary());
+
         });
-        return builder.toString();
+        return reportBuilder.toString();
     }
 
-    public Queue<String> calculate(String[] names) {
-        Queue<String> salaryStatistic = new LinkedList<>();
+    public List<Employee> calculate(List<EmployeeWorkDayInfo> filteredData, String[] names) {
+        List<Employee> employeesInfo = new ArrayList<>();
         Arrays.stream(names).forEach(name -> {
-            Integer salary = Storage.getEmployeeWorkDayInfo().stream()
+            Integer salary = filteredData.stream()
                     .filter(employeeWorkDayInfo ->
                             employeeWorkDayInfo.getEmployeeName().equals(name))
                     .map(employeeWorkDayInfo -> employeeWorkDayInfo.getHours()
                             * employeeWorkDayInfo.getIncomePerHour())
                     .reduce(0, Integer::sum);
-            String employeeSalary = name + " - " + salary;
-            salaryStatistic.add(employeeSalary);
+            employeesInfo.add(new Employee(name, salary));
         });
-        return salaryStatistic;
+        return employeesInfo;
     }
 
-    public void filterByDate(List<EmployeeWorkDayInfo> employeeWorkDayInfoList,
+    public List<EmployeeWorkDayInfo> filterByDate(List<EmployeeWorkDayInfo> employeeWorkDayInfoList,
                                                   String dateFrom, String dateTo) {
-        LocalDate parsedDateFrom = LocalDate.parse(dateFrom, TIME_FORMATTER);
-        LocalDate parsedDateTo = LocalDate.parse(dateTo, TIME_FORMATTER);
-        List<EmployeeWorkDayInfo> filteredInfo = employeeWorkDayInfoList.stream()
+        LocalDate parsedDateFrom = LocalDate.parse(dateFrom, DATE_FORMATTER);
+        LocalDate parsedDateTo = LocalDate.parse(dateTo, DATE_FORMATTER);
+        return employeeWorkDayInfoList.stream()
                 .filter(employeeWorkDayInfo ->
                         (!parsedDateFrom.isAfter(employeeWorkDayInfo.getDate()))
                         && !parsedDateTo.isBefore(employeeWorkDayInfo.getDate()))
                 .collect(Collectors.toList());
-        Storage.getEmployeeWorkDayInfo().addAll(filteredInfo);
     }
 
     public List<EmployeeWorkDayInfo> parseData(String[] data) {
         List<EmployeeWorkDayInfo> employeeWorkDayInfoList = new ArrayList<>();
-        for (String datum : data) {
-            String[] split = datum.split(" ");
-            employeeWorkDayInfoList.add(new EmployeeWorkDayInfo(split[INDEX_OF_NAMES],
-                    LocalDate.parse(split[INDEX_OF_DATES], TIME_FORMATTER),
-                    Integer.parseInt(split[INDEX_OF_HOURS]),
-                    Integer.parseInt(split[INDEX_OF_WAGES])));
+        for (String line : data) {
+            String[] splitLine = line.split(" ");
+            employeeWorkDayInfoList.add(new EmployeeWorkDayInfo(splitLine[INDEX_OF_NAME],
+                    LocalDate.parse(splitLine[INDEX_OF_DATE], DATE_FORMATTER),
+                    Integer.parseInt(splitLine[INDEX_OF_HOUR]),
+                    Integer.parseInt(splitLine[INDEX_OF_WAGE])));
         }
         return employeeWorkDayInfoList;
     }
