46f1ef34087ed43c9c01711a699af220521f8d4c
Anna Popova <anitapopova1996@gmail.com>
2022-07-21 13:29:04 +0300

    Removed method main

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,23 +4,6 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    public static void main(String[] args) {
-        String[] names = {"John", "Andrew", "Kate"};
-        String[] data = {
-                "25.04.2019 John 60 50",
-                "25.04.2019 Andrew 3 200",
-                "25.04.2019 Kate 10 100",
-
-                "26.04.2019 Andrew 3 200",
-                "26.04.2019 Kate 9 100",
-
-                "27.04.2019 John 7 100",
-                "27.04.2019 Kate 3 80",
-                "27.04.2019 Andrew 8 100"
-        };
-        System.out.println(getSalaryInfo(names,data,"24.04.2019","24.04.2019"));
-    }
-
     public static String getSalaryInfo(String[] names, String[] data, String dateFrom,
                                        String dateTo) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d.MM.yyyy");
@@ -59,6 +42,7 @@ public class SalaryInfo {
         return reportForPeriod
                 .delete(reportForPeriod.length() - 1, reportForPeriod.length())
                 .toString();
+
     }
 
 }
