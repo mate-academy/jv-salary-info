69d9fa96c53f68165da40619bf89370f413169eb
Maksym Kulakov <kulakovmaksym0@gmail.com>
2022-07-21 16:57:36 +0200

    SalaryInfo1

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,39 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+    private static final String DATE_PATTERN = "dd.MM.yyyy";
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        StringBuilder builderListOfWorkers = new StringBuilder();
+        builderListOfWorkers.append("Report for period ").append(dateFrom)
+                .append(" - ").append(dateTo);
+        int index = 0;
+        for (String name : names) {
+            int salaryTotalAtWorkPeriod = 0;
+            for (String dataRow : data) {
+                String[] dataArrayRow = dataRow.split(" ");
+                LocalDate workDate = getFormatDate(dataArrayRow[0]);
+                String nameOfWorker = dataArrayRow[1];
+                int hoursPerDay = Integer.parseInt(dataArrayRow[2]);
+                int salaryPerHour = Integer.parseInt(dataArrayRow[3]);
+                boolean atWorkPeriod = workDate.isAfter(getFormatDate(dateFrom).minusDays(1))
+                        & workDate.isBefore(getFormatDate(dateTo).plusDays(1));
+                if (name.equals(nameOfWorker) && atWorkPeriod) {
+                    salaryTotalAtWorkPeriod += hoursPerDay * salaryPerHour;
+                }
+            }
+            builderListOfWorkers.append(System.lineSeparator()).append(names[index])
+                    .append(" - ").append(salaryTotalAtWorkPeriod);
+            index++;
+        }
+        return builderListOfWorkers.toString();
+    }
+
+    private LocalDate getFormatDate(String date) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_PATTERN);
+        return LocalDate.parse(date, formatter);
     }
 }
