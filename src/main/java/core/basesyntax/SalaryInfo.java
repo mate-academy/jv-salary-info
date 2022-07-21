f049eb38db26d325c2bafd81ee359ed3ae60e4ae
Serhii Nazarenko <nasarenko82@gmail.com>
2022-07-21 14:32:44 +0300

    error corrections al

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -10,20 +10,22 @@ public class SalaryInfo {
     private static final int INDEX_OF_NAME = 1;
     private static final int INDEX_OF_HOUR = 2;
     private static final int INDEX_OF_INCOME_PER_HOUR = 3;
+    private static DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-
+        LocalDate localDateFrom = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate localDateTo = LocalDate.parse(dateTo, FORMATTER);
         StringBuilder reportBuilder = new StringBuilder();
         reportBuilder.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
         for (String name : names) {
             int salary = 0;
             for (String line : data) {
                 String[] arrayLine = line.split(" ");
-                LocalDate workday = getLocalDate(arrayLine[INDEX_OF_DATA]);
-                if ((getLocalDate(dateFrom).isBefore(workday)
-                        || getLocalDate(dateFrom).isEqual(workday))
-                        && (getLocalDate(dateTo).isAfter(workday)
-                        || getLocalDate(dateTo).isEqual(workday))) {
+                LocalDate workday = LocalDate.parse(arrayLine[INDEX_OF_DATA], FORMATTER);
+                if ((localDateFrom.isBefore(workday)
+                        || localDateFrom.isEqual(workday))
+                        && (localDateTo.isAfter(workday)
+                        || localDateTo.isEqual(workday))) {
                     if (name.equals(arrayLine[INDEX_OF_NAME])) {
                         salary = salary + parseInt(arrayLine[INDEX_OF_INCOME_PER_HOUR])
                                 * parseInt(arrayLine[INDEX_OF_HOUR]);
@@ -33,11 +35,5 @@ public class SalaryInfo {
             reportBuilder.append(System.lineSeparator()).append(name).append(" - ").append(salary);
         }
         return reportBuilder.toString();
-    }
-
-    public LocalDate getLocalDate(String dateString) {
-        LocalDate localDate = LocalDate
-                .parse(dateString, DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-        return localDate;
     }
 }
