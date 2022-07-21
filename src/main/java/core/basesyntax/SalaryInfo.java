76513a4bcb03e212f777deecc81123f4339fd215
Serhii Nazarenko <nasarenko82@gmail.com>
2022-07-21 13:41:50 +0300

    error corrections al

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -6,21 +6,27 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    private static final int INDEX_OF_DATA = 0;
+    private static final int INDEX_OF_NAME = 1;
+    private static final int INDEX_OF_HOUR = 2;
+    private static final int INDEX_OF_INCOME_PER_HOUR = 3;
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
+
         StringBuilder reportBuilder = new StringBuilder();
         reportBuilder.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
         for (String name : names) {
             int salary = 0;
             for (String line : data) {
                 String[] arrayLine = line.split(" ");
-                LocalDate workday = getLocalDate(arrayLine[0]);
+                LocalDate workday = getLocalDate(arrayLine[INDEX_OF_DATA]);
                 if ((getLocalDate(dateFrom).isBefore(workday)
                         || getLocalDate(dateFrom).isEqual(workday))
                         && (getLocalDate(dateTo).isAfter(workday)
                         || getLocalDate(dateTo).isEqual(workday))) {
-                    if (name.equals(arrayLine[1])) {
-                        salary = salary + parseInt(arrayLine[3])
-                                * parseInt(arrayLine[2]);
+                    if (name.equals(arrayLine[INDEX_OF_NAME])) {
+                        salary = salary + parseInt(arrayLine[INDEX_OF_INCOME_PER_HOUR])
+                                * parseInt(arrayLine[INDEX_OF_HOUR]);
                     }
                 }
             }
