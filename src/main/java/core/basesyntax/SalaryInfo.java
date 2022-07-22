5516d1670b85c2489f32a8ef9afc466d68f4bf7d
Dmytro Stakhiv <dstahiv@gmail.com>
2022-07-22 20:30:14 +0300

    Changes in Salary Info

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,33 +4,32 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private static final DateTimeFormatter FORMATT = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-    private static final int ZERO = 0;
-    private static final int ONE = 1;
-    private static final int TWO = 2;
-    private static final int THREE = 3;
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int LINE_FIRST = 0;
+    private static final int LINE_SECOND = 1;
+    private static final int LINE_THIRD = 2;
+    private static final int LINE_FOURTH = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        LocalDate localDateFrom = LocalDate.parse(dateFrom, FORMATT);
-        LocalDate localDateTo = LocalDate.parse(dateTo, FORMATT);
-        StringBuilder info = new StringBuilder().append("Report for period ")
+        LocalDate localDateFrom = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate localDateTo = LocalDate.parse(dateTo, FORMATTER);
+        StringBuilder reportBuilder = new StringBuilder().append("Report for period ")
                 .append(dateFrom).append(" - ").append(dateTo);
-        String[] arrayOfData;
 
         for (String name : names) {
             int salary = 0;
-            for (String dat : data) {
-                arrayOfData = dat.split(" ");
-                LocalDate today = LocalDate.parse(arrayOfData[ZERO], FORMATT);
-                if (localDateFrom.compareTo(today) <= 0
-                        && localDateTo.compareTo(today) >= 0
-                        && arrayOfData[ONE].equals(name)) {
-                    salary += (Integer.parseInt(arrayOfData[THREE])
-                            * Integer.parseInt(arrayOfData[TWO]));
+            for (String line : data) {
+                String[] splittedLine = line.split(" ");
+                LocalDate currentDate = LocalDate.parse(splittedLine[LINE_FIRST], FORMATTER);
+                if ((currentDate.isEqual(localDateFrom) || currentDate.isAfter(localDateFrom))
+                        && (currentDate.isBefore(localDateTo) || currentDate.isEqual(localDateTo))
+                        && splittedLine[LINE_SECOND].equals(name)) {
+                    salary += (Integer.parseInt(splittedLine[LINE_FOURTH])
+                            * Integer.parseInt(splittedLine[LINE_THIRD]));
                 }
             }
-            info.append(System.lineSeparator()).append(name).append(" - ").append(salary);
+            reportBuilder.append(System.lineSeparator()).append(name).append(" - ").append(salary);
         }
-        return info.toString();
+        return reportBuilder.toString();
     }
 }
