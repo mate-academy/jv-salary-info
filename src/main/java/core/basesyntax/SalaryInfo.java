ba5d11092c2179c1fdb6d819a151a04eadc31750
Andrii Kovalskyi <kovalskyi.andrii90@gmail.com>
2022-07-24 16:14:41 +0200

    Salary is calculated correctly

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,39 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+
+    private final DateTimeFormatter format = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+
+        LocalDate formattedDateFrom = LocalDate.parse(dateFrom, format);
+        LocalDate formattedDateTo = LocalDate.parse(dateTo, format);
+        StringBuilder builder = new StringBuilder();
+        builder.append("Report for period ")
+                .append(dateFrom)
+                .append(" - ").append(dateTo);
+        for (String name : names) {
+            int salaryPerPeriod = 0;
+            for (String newData : data) {
+                String[] seperatedData = newData.split(" ");
+                LocalDate dateTime = LocalDate.parse(seperatedData[0], format);
+                if ((dateTime.isAfter(formattedDateFrom) && dateTime.isBefore(formattedDateTo)
+                        || dateTime.equals(formattedDateFrom)
+                        || dateTime.equals(formattedDateTo))
+                        && name.equals(seperatedData[1])) {
+                    int workingHuors = Integer.parseInt(seperatedData[2]);
+                    int salaryPerHour = Integer.parseInt(seperatedData[3]);
+                    salaryPerPeriod = salaryPerPeriod + (salaryPerHour * workingHuors);
+                }
+            }
+            builder.append(System.lineSeparator())
+                    .append(name)
+                    .append(" - ")
+                    .append(salaryPerPeriod);
+        }
+        return builder.toString();
     }
 }
