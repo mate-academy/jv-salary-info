96dabd3b7f2d6fc7f1ce49bfa8071a70f74797b0
Andrii Kovalskyi <kovalskyi.andrii90@gmail.com>
2022-07-25 15:33:44 +0200

    Ading chsnges after review v1.1

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,8 +4,11 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-
     private final DateTimeFormatter format = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int WORKING_HOURS_INDEX = 2;
+    private static final int SALARY_PER_HOUR_INDEX = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
 
@@ -19,13 +22,13 @@ public class SalaryInfo {
             int salaryPerPeriod = 0;
             for (String newData : data) {
                 String[] seperatedData = newData.split(" ");
-                LocalDate dateTime = LocalDate.parse(seperatedData[0], format);
+                LocalDate dateTime = LocalDate.parse(seperatedData[DATE_INDEX], format);
                 if ((dateTime.isAfter(formattedDateFrom) && dateTime.isBefore(formattedDateTo)
                         || dateTime.equals(formattedDateFrom)
                         || dateTime.equals(formattedDateTo))
-                        && name.equals(seperatedData[1])) {
-                    int workingHuors = Integer.parseInt(seperatedData[2]);
-                    int salaryPerHour = Integer.parseInt(seperatedData[3]);
+                        && name.equals(seperatedData[NAME_INDEX])) {
+                    int workingHuors = Integer.parseInt(seperatedData[WORKING_HOURS_INDEX]);
+                    int salaryPerHour = Integer.parseInt(seperatedData[SALARY_PER_HOUR_INDEX]);
                     salaryPerPeriod = salaryPerPeriod + (salaryPerHour * workingHuors);
                 }
             }
