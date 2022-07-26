a9bbc5642a4d611819de35ac8961330c2eae37f5
Andrii Kuzmych <andrii.kuzmych@gmail.com>
2022-07-26 11:25:10 +0300

    Working solution, changed SimpleDateFormat to LocalDate

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,38 +1,38 @@
 package core.basesyntax;
 
-
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private final int DATES_LIST = 0;
-    private final int NAMES_LIST = 1;
-    private final int WORKING_HOURS = 2;
-    private final int INCOME_PER_HOUR = 3;
+    private static final int DATES_LIST = 0;
+    private static final int NAMES_LIST = 1;
+    private static final int WORKING_HOURS = 2;
+    private static final int INCOME_PER_HOUR = 3;
 
-    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) throws ParseException {
-        SimpleDateFormat formatDate = new SimpleDateFormat("dd.MM.yyyy");
-        Date dateFromFormated = formatDate.parse(dateFrom);
-        Date dateToFormated = formatDate.parse(dateTo);
+    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDate dateFromFormated = LocalDate.parse(dateFrom,formatter);
+        LocalDate dateToFormated = LocalDate.parse(dateTo, formatter);
         StringBuilder result = new StringBuilder().append("Report for period ")
                 .append(dateFrom).append(" - ").append(dateTo);
 
         for (int j = 0; j < names.length; j++) {
             String name = names[j];
-            int salary =0;
+            int salary = 0;
 
             for (int i = 0; i < data.length; i++) {
                 String[] dataSeparate = data[i].split(" ");
-                Date dateFromData = formatDate.parse(dataSeparate[DATES_LIST]);
+                LocalDate dateFromData = LocalDate.parse(dataSeparate[DATES_LIST], formatter);
 
                 if (name.equals(dataSeparate[NAMES_LIST])
-                        && dateFromData.before(dateToFormated)
-                        && dateFromData.after(dateFromFormated) ) {
+                        && (dateFromData.isBefore(dateToFormated)
+                        || dateFromData.isEqual(dateToFormated))
+                        && (dateFromData.isAfter(dateFromFormated)
+                        || dateFromData.isEqual(dateFromFormated))) {
                     salary += Integer.parseInt(dataSeparate[WORKING_HOURS])
                             * Integer.parseInt(dataSeparate[INCOME_PER_HOUR]);
-                    }
                 }
+            }
             result.append(System.lineSeparator()).append(name).append(" - ").append(salary);
         }
         return result.toString();
