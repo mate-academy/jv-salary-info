f666ab5c302e94f986e6abf1bf5538e49b18ddca
Mikhail Maiorov <majorov.michail@gmail.com>
2022-07-26 15:11:48 +0300

    method getSalaryInfo() was refactored

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,10 +1,16 @@
 package core.basesyntax;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    private static final String stringSeparator = " ";
+    private static final DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static String dateFromData;
+    private static String employeeName;
+    private static int hoursWorkedPerDay;
+    private static int incomePerHour;
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder salaryInfo = new StringBuilder("Report for period "
                 + dateFrom + " - " + dateTo);
@@ -13,9 +19,9 @@ public class SalaryInfo {
             int salary = 0;
 
             for (String getData : data) {
-                String[] infoData = getData.split(" ");
-                if (isInDate(infoData[0], dateFrom, dateTo)) {
-                    salary += calculateSalary(infoData, name);
+                splitData(getData);
+                if (isInDate(dateFromData, dateFrom, dateTo)) {
+                    salary += calculateSalary(name);
                 }
             }
             salaryInfo.append(System.lineSeparator()).append(name).append(" - ").append(salary);
@@ -24,27 +30,24 @@ public class SalaryInfo {
         return salaryInfo.toString();
     }
 
-    private int calculateSalary(String[] infoData, String name) {
-        if (infoData[1].equals(name)) {
-            return Integer.parseInt(infoData[2]) * Integer.parseInt(infoData[3]);
-        }
-        return 0;
+    private int calculateSalary(String name) {
+        return employeeName.equals(name) ? hoursWorkedPerDay * incomePerHour : 0;
     }
 
     private boolean isInDate(String dataDate, String dateFrom, String dateTo) {
-        Date parseDateData = null;
-        Date parseDateFrom = null;
-        Date parseDateTo = null;
+        LocalDate dateFromData = LocalDate.parse(dataDate, dateFormat);
+        LocalDate dateInRangeFrom = LocalDate.parse(dateFrom, dateFormat);
+        LocalDate dateInRangeTo = LocalDate.parse(dateTo, dateFormat);
 
-        try {
-            parseDateData = new SimpleDateFormat("dd.MM.yyyy").parse(dataDate);
-            parseDateFrom = new SimpleDateFormat("dd.MM.yyyy").parse(dateFrom);
-            parseDateTo = new SimpleDateFormat("dd.MM.yyyy").parse(dateTo);
-        } catch (ParseException e) {
-            System.out.println("Date format parsing exception");
-        }
+        return dateFromData != null && dateFromData.compareTo(dateInRangeFrom) >= 0
+                && dateFromData.compareTo(dateInRangeTo) <= 0;
+    }
 
-        return parseDateTo != null && parseDateData.compareTo(parseDateFrom) >= 0
-                && parseDateData.compareTo(parseDateTo) <= 0;
+    private void splitData(String data) {
+        String[] infoData = data.split(stringSeparator);
+        dateFromData = infoData[0];
+        employeeName = infoData[1];
+        hoursWorkedPerDay = Integer.parseInt(infoData[2]);
+        incomePerHour = Integer.parseInt(infoData[3]);
     }
 }
