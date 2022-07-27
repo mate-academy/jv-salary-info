57f69bcec155ef05c811615fc4679a2f95e24759
Ivan Litvynets <Litvyvanya@gmail.com>
2022-07-27 20:02:00 +0300

    added final fields and fixed methods

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,62 +1,46 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
-    private static final short NAME_INDEX = 0;
-    private static final short SALARY_INDEX = 1;
     private static final short DATE_INDEX = 0;
+    private static final short NAME_INDEX = 1;
+    private static final short NUMBER_OF_HOURS_INDEX = 2;
+    private static final short HOURLY_PAY_INDEX = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-
-        String[][] salary = new String[names.length][2];
-        for (int i = 0; i < names.length; i++) {
-            salary[i][NAME_INDEX] = names[i];
-            salary[i][SALARY_INDEX] = "0";
-        }
-        int digitDateFrom = this.digitalDate(dateFrom);
-        int digitDateTo = this.digitalDate(dateTo);
-        for (String dat: data) {
-            if (digitDateFrom <= this.digitalDate(dat.split(" ")[DATE_INDEX])
-                    && digitDateTo >= this.digitalDate(dat.split(" ")[DATE_INDEX])) {
-                for (int i = 0; i < salary.length; i++) {
-                    /** Index "1" - date of working hours
-                    Index "2" - working hours
-                    Index "3" - hourly pay **/
-                    if (salary[i][NAME_INDEX].equals(dat.split(" ")[1])) {
-                        salary[i][1] = String.valueOf(Integer.parseInt(salary[i][1])
-                                + (Integer.parseInt(dat.split(" ")[2])
-                                * Integer.parseInt(dat.split(" ")[3])));
-                    }
-                }
-            }
-        }
+        LocalDate digitDateFrom = digitalDate(dateFrom);
+        LocalDate digitDateTo = digitalDate(dateTo);
         StringBuilder report = new StringBuilder();
         report.append("Report for period ")
                 .append(dateFrom)
                 .append(" - ")
                 .append(dateTo);
-        for (int i = 0; i < salary.length; i++) {
+        for (String name: names) {
+            int salary = 0;
+            for (String dataElement: data) {
+                String[] dataFields = dataElement.split(" ");
+                if (name.equals(dataFields[NAME_INDEX])) {
+                    if ((digitalDate(dataFields[DATE_INDEX]).isAfter(digitDateFrom)
+                            && digitalDate(dataFields[DATE_INDEX]).isBefore(digitDateTo))
+                            || (digitalDate(dataFields[DATE_INDEX]).equals(digitDateFrom)
+                            || digitalDate(dataFields[DATE_INDEX]).equals(digitDateTo))) {
+                        salary += Integer.parseInt(dataFields[NUMBER_OF_HOURS_INDEX])
+                                * Integer.parseInt(dataFields[HOURLY_PAY_INDEX]);
+                    }
+                }
+            }
             report.append(System.lineSeparator())
-                    .append(salary[i][0])
+                    .append(name)
                     .append(" - ")
-                    .append(salary[i][1]);
+                    .append(salary);
         }
         return report.toString();
     }
 
-    private int digitalDate(String date) {
-        String[] stringDate = date.split("\\.");
-        if (stringDate.length == 0) {
-            return 0;
-        }
-        int day = Integer.valueOf(stringDate[0]);
-        int mounth = Integer.valueOf(stringDate[1]);
-        int year = Integer.valueOf(stringDate[2]);
-        int res = 0;
-        for (int i = 1; i < mounth; i++) {
-            res += (28 + (i + i / 8) % 2 + 2 % i + 1 / i * 2) + 1;
-            //formula for calculating the number of days in a month
-        }
-        res += day + (year * 365);
-        return res;
+    private LocalDate digitalDate(String date) {
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        return LocalDate.parse(date, timeFormatter);
     }
 }
