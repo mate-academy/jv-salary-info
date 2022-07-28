de6ddf7c8c816a260db128cea1a101d26f77e6ae
Igor Petrichenko <i.petrichenko@gmail.com>
2022-07-28 10:00:15 +0300

    add constant number 0,1,2,3; access modifier Private to DATE_FORMAT, and modified method checkWorkingDay

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,16 +5,19 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class SalaryInfo {
-    static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd.MM.yyyy");
+    private static final int DATE_OF_WORK = 0;
+    private static final int NAME_WORKER = 1;
+    private static final int WORKING_HOUR = 2;
+    private static final int INCOME_PER_DAY = 3;
+
+    private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder report = new StringBuilder();
         report.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
         for (String name: names) {
-            if (name != null) {
-                String salary = getSalaryEmployee(name, data, dateFrom, dateTo);
-                report.append(System.lineSeparator()).append(salary);
-            }
+            String salary = getSalaryEmployee(name, data, dateFrom, dateTo);
+            report.append(System.lineSeparator()).append(salary);
         }
         return report.toString();
     }
@@ -23,27 +26,31 @@ public class SalaryInfo {
         int salaryEmployeeByDay = 0;
         for (String workDate: data) {
             String[] dataInfo = workDate.split(" ");
-            String nameWorker = dataInfo[1];
-            String workingDay = dataInfo[0];
-            int workingHour = Integer.parseInt(dataInfo[2]);
-            int incomePerHour = Integer.parseInt(dataInfo[3]);
-            if (nameWorker.equals(name)) {
-                salaryEmployeeByDay = checkWorkingDay(workingDay, dateFrom, dateTo)
-                        ? salaryEmployeeByDay + workingHour * incomePerHour : salaryEmployeeByDay;
+            String nameWorker = dataInfo[NAME_WORKER];
+            String workingDay = dataInfo[DATE_OF_WORK];
+            int workingHour = Integer.parseInt(dataInfo[WORKING_HOUR]);
+            int incomePerHour = Integer.parseInt(dataInfo[INCOME_PER_DAY]);
+            if (checkWorkingDay(workingDay, dateFrom, dateTo, nameWorker, name)) {
+                salaryEmployeeByDay += workingHour * incomePerHour;
             }
         }
         return name + " - " + salaryEmployeeByDay;
     }
 
-    private boolean checkWorkingDay(String workingDay, String dateFrom, String dateTo) {
-        try {
-            Date workDate = DATE_FORMAT.parse(workingDay);
-            Date dateStart = DATE_FORMAT.parse(dateFrom);
-            Date dateEnd = DATE_FORMAT.parse(dateTo);
-            return workDate.after(dateStart) && workDate.before(dateEnd)
-                    || workDate.equals(dateEnd);
-        } catch (ParseException e) {
-            throw new RuntimeException("Date format isn't correct (dd.MM.yyyy)", e);
+    private boolean checkWorkingDay(String workingDay, String dateFrom, String dateTo,
+                                    String nameWorker, String name) {
+        if (nameWorker.equals(name)) {
+            try {
+                Date workDate = DATE_FORMAT.parse(workingDay);
+                Date dateStart = DATE_FORMAT.parse(dateFrom);
+                Date dateEnd = DATE_FORMAT.parse(dateTo);
+                return workDate.after(dateStart) && workDate.before(dateEnd)
+                        || workDate.equals(dateEnd);
+            } catch (ParseException e) {
+                throw new RuntimeException("Date format isn't correct (dd.MM.yyyy)", e);
+            }
+        } else {
+            return false;
         }
     }
 }
