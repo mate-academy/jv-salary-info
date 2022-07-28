1b453e16ed15a54a2d0b685b25c19a09b4d983f5
Ivan Litvynets <Litvyvanya@gmail.com>
2022-07-28 20:37:44 +0300

    "digitalDate" method removed & final field added for "DataTimeFormatter" parameter

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,15 +4,17 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    private static final String DATE_FORMAT = "dd.MM.yyyy";
     private static final short DATE_INDEX = 0;
     private static final short NAME_INDEX = 1;
     private static final short NUMBER_OF_HOURS_INDEX = 2;
     private static final short HOURLY_PAY_INDEX = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        LocalDate digitDateFrom = digitalDate(dateFrom);
-        LocalDate digitDateTo = digitalDate(dateTo);
         StringBuilder report = new StringBuilder();
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern(DATE_FORMAT);
+        LocalDate digitDateFrom = LocalDate.parse(dateFrom, dateFormatter);
+        LocalDate digitDateTo = LocalDate.parse(dateTo, dateFormatter);
         report.append("Report for period ")
                 .append(dateFrom)
                 .append(" - ")
@@ -21,14 +23,17 @@ public class SalaryInfo {
             int salary = 0;
             for (String dataElement: data) {
                 String[] dataFields = dataElement.split(" ");
-                if (name.equals(dataFields[NAME_INDEX])) {
-                    if ((digitalDate(dataFields[DATE_INDEX]).isAfter(digitDateFrom)
-                            && digitalDate(dataFields[DATE_INDEX]).isBefore(digitDateTo))
-                            || (digitalDate(dataFields[DATE_INDEX]).equals(digitDateFrom)
-                            || digitalDate(dataFields[DATE_INDEX]).equals(digitDateTo))) {
-                        salary += Integer.parseInt(dataFields[NUMBER_OF_HOURS_INDEX])
-                                * Integer.parseInt(dataFields[HOURLY_PAY_INDEX]);
-                    }
+                if (name.equals(dataFields[NAME_INDEX])
+                        && ((LocalDate.parse(dataFields[DATE_INDEX],dateFormatter)
+                        .isAfter(digitDateFrom)
+                        && LocalDate.parse(dataFields[DATE_INDEX],dateFormatter)
+                        .isBefore(digitDateTo))
+                        || (LocalDate.parse(dataFields[DATE_INDEX],dateFormatter)
+                        .equals(digitDateFrom)
+                        || LocalDate.parse(dataFields[DATE_INDEX],dateFormatter)
+                        .equals(digitDateTo)))) {
+                    salary += Integer.parseInt(dataFields[NUMBER_OF_HOURS_INDEX])
+                            * Integer.parseInt(dataFields[HOURLY_PAY_INDEX]);
                 }
             }
             report.append(System.lineSeparator())
@@ -37,10 +42,5 @@ public class SalaryInfo {
                     .append(salary);
         }
         return report.toString();
-    }
-
-    private LocalDate digitalDate(String date) {
-        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        return LocalDate.parse(date, timeFormatter);
     }
 }
