2097bb5d0c0133d6e6b764e8e228243fdad8429a
Anatoliy Olkhovskiy <Anatoltbcm@gmail.com>
2022-07-29 13:42:29 +0300

    implement getSalaryInfo(...)

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,35 @@
 package core.basesyntax;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        StringBuilder string = new StringBuilder();
+        string.append("Report for period ").append(dateFrom).append(" - ")
+                .append(dateTo).append(System.lineSeparator());
+        for (int i = 0; i < names.length; i++) {
+            int money = 0;
+            for (String eachData : data) {
+                String[] eachDataArray = eachData.split(" ");
+                if (names[i].equals(eachDataArray[1])
+                        && (getDate(eachDataArray[0]).equals(getDate(dateFrom))
+                        || getDate(eachDataArray[0]).after(getDate(dateFrom)))
+                        && (getDate(eachDataArray[0]).equals(getDate(dateTo))
+                        || getDate(eachDataArray[0]).before(getDate(dateTo)))) {
+                    money += Integer.parseInt(eachDataArray[2])
+                            * Integer.parseInt(eachDataArray[3]);
+                }
+            }
+            string.append(names[i]).append(" - ").append(money).append(System.lineSeparator());
+        }
+        string.deleteCharAt(string.length() - 1).deleteCharAt(string.length() - 1);
+        return string.toString();
+    }
+
+    private static Calendar getDate(String string) {
+        String[] date = string.split("\\.");
+        return new GregorianCalendar(Integer.parseInt(date[2]),
+                Integer.parseInt(date[1]), Integer.parseInt(date[0]));
     }
 }
