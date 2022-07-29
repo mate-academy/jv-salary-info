d77f411980a57d81852bd8ef4977e3ec519b92d8
max-bolotin <realprof.mx@gmail.com>
2022-07-29 22:48:55 +0300

    all tests passed and checkstyle fixed

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -8,22 +8,23 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class SalaryInfo {
-    Map<String, Integer> employees = new HashMap<>();
+    private Map<String, Integer> employees = new HashMap<>();
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         for (String name : names) {
             employees.put(name, 0);
         }
-        StringBuilder res = new StringBuilder("Report for period ").append(dateFrom).append(" - ").append(dateTo);
+        StringBuilder res = new StringBuilder("Report for period ").append(dateFrom)
+                .append(" - ").append(dateTo);
         for (String employeeData : data) {
-          String[] details = employeeData.split(" ");
-          Date date = getDate(details[0]);
-          String nameOfEmployee = details[1];
-          int dailySalary;
-          if (date.compareTo(getDate(dateFrom)) >= 0 && date.compareTo(getDate(dateTo)) <= 0) {
-              dailySalary = Integer.parseInt(details[2]) * Integer.parseInt(details[3]);
-              employees.put(nameOfEmployee, employees.get(nameOfEmployee) + dailySalary);
-          }
+            String[] details = employeeData.split(" ");
+            Date date = getDate(details[0]);
+            String nameOfEmployee = details[1];
+            int dailySalary;
+            if (date.compareTo(getDate(dateFrom)) >= 0 && date.compareTo(getDate(dateTo)) <= 0) {
+                dailySalary = Integer.parseInt(details[2]) * Integer.parseInt(details[3]);
+                employees.put(nameOfEmployee, employees.get(nameOfEmployee) + dailySalary);
+            }
 
         }
         for (String name : names) {
