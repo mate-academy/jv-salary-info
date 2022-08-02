dd73f56ed2835a3ea45f5bb1958f834b91c5dc28
buuufy <s1mpleworkpost@gmail.com>
2022-08-03 00:11:44 +0300

    fix all logic and syntax using mistakes checklist

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,49 +1,33 @@
 package core.basesyntax;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        String res = "Report for period " + dateFrom + " - " + dateTo + System.lineSeparator();
-        StringBuilder builder = new StringBuilder(res);
-        SimpleDateFormat format = new SimpleDateFormat();
-        format.applyPattern("dd.MM.yyyy");
-        Date fromDate = null;
-        try {
-            fromDate = format.parse(dateFrom);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        Date toDate = null;
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
-        try {
-            toDate = format.parse(dateTo);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        int[] allSalaries = new int[names.length];
-        for (int j = 0; j < data.length; j++) {
-            Date currentDate = null;
-            try {
-                currentDate = format.parse(data[j].split(" ")[0]);
-            } catch (ParseException e) {
-                e.printStackTrace();
-            }
-            int hoursCounter = Integer.parseInt(data[j].split(" ")[2]);
-            int cashPerHour = Integer.parseInt(data[j].split(" ")[3]);
-            for (int i = 0; i < names.length; i++) {
-                if (data[j].split(" ")[1].equals(names[i]) && (currentDate.equals(fromDate)
-                        || currentDate.after(fromDate)) && (currentDate.equals(toDate)
-                        || currentDate.before(toDate))) {
-                    allSalaries[i] += hoursCounter * cashPerHour;
+    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
+        StringBuilder builder = new StringBuilder("Report for period ")
+                .append(dateFrom).append(" - ").append(dateTo);
+        LocalDate fromDate = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate toDate = LocalDate.parse(dateTo, FORMATTER);
+        for (String name : names) {
+            int currentSalary = 0;
+            for (String currentData : data) {
+                String[] splitValues = currentData.split(" ");
+                LocalDate currentDate = LocalDate.parse(splitValues[0], FORMATTER);
+                String currentName = splitValues[1];
+                int hoursCounter = Integer.parseInt(splitValues[2]);
+                int cashPerHour = Integer.parseInt(splitValues[3]);
+
+                if (currentName.equals(name)
+                        && (currentDate.equals(fromDate) || currentDate.isAfter(fromDate))
+                        && (currentDate.equals(toDate) || currentDate.isBefore(toDate))) {
+                    currentSalary += hoursCounter * cashPerHour;
                 }
             }
+            builder.append(System.lineSeparator()).append(name).append(" - ").append(currentSalary);
         }
-        for (int i = 0; i < names.length; i++) {
-            builder.append(names[i] + " - " + allSalaries[i] + System.lineSeparator());
-        }
-        return builder.toString().trim();
+        return builder.toString();
     }
 }
