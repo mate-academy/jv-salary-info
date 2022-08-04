60fe27ab32d0007243c69d2a3de2e0b24d8784b6
Vasyl Shevtsiv <vasyl.shevtsiv@gmail.com>
2022-08-04 13:26:32 +0300

    improving readability with constants

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,22 +4,27 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int DATE = 0;
+    private static final int NAME = 1;
+    private static final int HOURS = 2;
+    private static final int PAY_PER_HOUR = 3;
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        LocalDate startDate = LocalDate.parse(dateFrom, formatter);
-        LocalDate endDate = LocalDate.parse(dateTo, formatter);
+        LocalDate startDate = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate endDate = LocalDate.parse(dateTo, FORMATTER);
         int[] salaries = new int[names.length];
         for (String datum : data) {
             String[] temp = datum.split(" ");
-            if (temp[0].length() < 10) {
-                temp[0] = "0" + temp[0];
+            if (temp[DATE].length() < 10) {
+                temp[DATE] = "0" + temp[DATE];
             }
-            LocalDate date = LocalDate.parse(temp[0], formatter);
+            LocalDate date = LocalDate.parse(temp[0], FORMATTER);
             if (date.compareTo(startDate) >= 0 && (date.compareTo(endDate) <= 0)) {
                 for (int i = 0; i < names.length; i++) {
-                    if (names[i].equals(temp[1])) {
-                        salaries[i] = salaries[i] + Integer.parseInt(temp[2])
-                                * Integer.parseInt(temp[3]);
+                    if (names[i].equals(temp[NAME])) {
+                        salaries[i] = salaries[i] + Integer.parseInt(temp[HOURS])
+                                * Integer.parseInt(temp[PAY_PER_HOUR]);
                     }
                 }
             }
@@ -29,7 +34,6 @@ public class SalaryInfo {
         for (int j = 0; j < names.length; j++) {
             report.append(names[j]).append(" - ").append(salaries[j])
                     .append(System.lineSeparator());
-
         }
         return report.toString().trim();
     }
