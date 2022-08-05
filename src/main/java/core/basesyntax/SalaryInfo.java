74371f92bd5ce5dfea07bfed95037df0be8317a8
amarkelovUA <java.developer.aleksandr@gmail.com>
2022-08-05 13:16:56 +0300

    The redundant method stringToDate() is removed

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -6,10 +6,11 @@ import java.time.format.DateTimeFormatter;
 public class SalaryInfo {
     private static final String SEPARATOR = " ";
     private static final String START_TOPIC = "Report for period ";
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.M.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        LocalDate from = stringToDate(dateFrom);
-        LocalDate to = stringToDate(dateTo);
+        LocalDate from = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate to = LocalDate.parse(dateTo, FORMATTER);
         StringBuilder builder = new StringBuilder(
                 START_TOPIC + dateFrom + " - " + dateTo + System.lineSeparator()
         );
@@ -19,22 +20,16 @@ public class SalaryInfo {
             for (String record : data) {
                 String[] info = record.split(SEPARATOR);
                 if (name.equals(info[1])) {
-                    LocalDate date = stringToDate(info[0]);
+                    LocalDate date = LocalDate.parse(info[0], FORMATTER);
                     if (date.compareTo(from) >= 0 && date.compareTo(to) <= 0) {
                         salary += Integer.parseInt(info[2]) * Integer.parseInt(info[3]);
                     }
                 }
             }
             builder.append(name + " - " + salary + System.lineSeparator());
-            salary = 0;
         }
 
         int indexOfSeparatpr = builder.lastIndexOf(System.lineSeparator());
         return builder.subSequence(0, indexOfSeparatpr).toString();
-    }
-
-    private LocalDate stringToDate(String date) {
-        DateTimeFormatter formater = DateTimeFormatter.ofPattern("dd.M.yyyy");
-        return LocalDate.parse(date, formater);
     }
 }
