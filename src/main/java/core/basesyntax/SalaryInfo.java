ec2bddc39ab5ad470283b3ec721c6effbb2e4bee
max-bolotin <realprof.mx@gmail.com>
2022-08-08 13:17:58 +0300

    mentor's notes fixed, checkstyle errors fixed

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,24 +1,23 @@
 package core.basesyntax;
 
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
 
 public class SalaryInfo {
+    private static final String DATE_FORMAT = "dd.MM.yyyy";
     private Map<String, Integer> employees = new HashMap<>();
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         for (String name : names) {
             employees.put(name, 0);
         }
-        StringBuilder res = new StringBuilder("Report for period ").append(dateFrom)
+        StringBuilder reportForPeriod = new StringBuilder("Report for period ").append(dateFrom)
                 .append(" - ").append(dateTo);
         for (String employeeData : data) {
             String[] details = employeeData.split(" ");
-            Date date = getDate(details[0]);
+            LocalDate date = getDate(details[0]);
             String nameOfEmployee = details[1];
             int dailySalary;
             if (date.compareTo(getDate(dateFrom)) >= 0 && date.compareTo(getDate(dateTo)) <= 0) {
@@ -28,20 +27,16 @@ public class SalaryInfo {
 
         }
         for (String name : names) {
-            res.append(System.lineSeparator()).append(name)
+            reportForPeriod.append(System.lineSeparator()).append(name)
                     .append(" - ").append(employees.get(name));
         }
-        return res.toString();
+        return reportForPeriod.toString();
     }
 
-    private Date getDate(String input) {
-        DateFormat formatter = new SimpleDateFormat("dd.MM.yyyy");
-        Date date;
-        try {
-            date = formatter.parse(input);
-        } catch (ParseException e) {
-            throw new RuntimeException(e);
-        }
+    private LocalDate getDate(String input) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_FORMAT);
+        LocalDate date;
+        date = LocalDate.parse(input, formatter);
         return date;
     }
 }
