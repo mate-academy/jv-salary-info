547e89324e4958028604edd112b9727d5fe847e7
Aptypio13 <aptypio668@gmail.com>
2022-08-08 09:31:02 +0300

    move DateTimeFormatter to class level and make it as a constant Change checkDatePeriod method change "+" to .append in StringBuilder I'm sorry I accidentally changed the test class during the commit

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -9,14 +9,18 @@ public class SalaryInfo {
     private static final int HOURS_OF_WORK_INDEX = 2;
     private static final int SALARY_PER_HOUR_INDEX = 3;
     private static final int DAY_TO_PLUS = 1;
+    private static final String DATE_FORMAT_PATTERN = "dd.MM.yyyy";
+    private static final DateTimeFormatter dateTimeFormatter
+            = DateTimeFormatter.ofPattern(DATE_FORMAT_PATTERN);
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder stringBuilder = new StringBuilder(
                 "Report for period " + dateFrom + " - " + dateTo + "\n");
         for (String employee: names) {
-            stringBuilder.append(
-                    employee + " - "
-                            + employeeSalaryChecker(employee, data, dateFrom, dateTo) + "\n");
+            stringBuilder.append(employee)
+                    .append(" - ")
+                    .append(employeeSalaryChecker(employee, data, dateFrom, dateTo))
+                    .append(System.lineSeparator());
 
         }
         return stringBuilder.toString().trim();
@@ -24,10 +28,12 @@ public class SalaryInfo {
 
     private int employeeSalaryChecker(String name, String[] data, String dateFrom, String dateTo) {
         int salary = 0;
+        LocalDate localDateFrom = LocalDate.parse(dateFrom, dateTimeFormatter);
+        LocalDate localDateTo = LocalDate.parse(dateTo, dateTimeFormatter);
         for (String line: data) {
             String[] arrayOfLines = line.split(" ");
-            if (dateChecker(
-                    arrayOfLines[START_DATE_INDEX], dateFrom, dateTo)
+            if (checkDatePeriod(
+                    arrayOfLines[START_DATE_INDEX], localDateFrom, localDateTo)
                     && arrayOfLines[NAME_INDEX].equals(name)) {
                 salary += Integer.parseInt(arrayOfLines[HOURS_OF_WORK_INDEX])
                      * Integer.parseInt(arrayOfLines[SALARY_PER_HOUR_INDEX]);
@@ -36,12 +42,11 @@ public class SalaryInfo {
         return salary;
     }
 
-    private static boolean dateChecker(String dataToCheck, String dateFrom, String dateTo) {
-        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        LocalDate localDateFrom = LocalDate.parse(dateFrom, dateTimeFormatter);
-        LocalDate localDateTo = LocalDate.parse(dateTo, dateTimeFormatter).plusDays(DAY_TO_PLUS);
+    private static boolean checkDatePeriod(
+            String dataToCheck, LocalDate dateFrom, LocalDate dateTo) {
+        dateTo = dateTo.plusDays(DAY_TO_PLUS);
         LocalDate localDateToCheck = LocalDate.parse(dataToCheck, dateTimeFormatter);
-        return localDateToCheck.isBefore(localDateTo) && localDateToCheck.isAfter(localDateFrom);
+        return localDateToCheck.isBefore(dateTo) && localDateToCheck.isAfter(dateFrom);
     }
 
 }
