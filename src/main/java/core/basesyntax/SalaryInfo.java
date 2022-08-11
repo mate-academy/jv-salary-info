e1537c0f330ee53ebd8d10b44573238ab825c8f6
GolDenik <denik.golibroda@gmail.com>
2022-08-11 17:01:43 +0300

    save before some changes

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -2,6 +2,84 @@ package core.basesyntax;
 
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        StringBuilder builder = new StringBuilder();
+        builder.append("Report for period ")
+                .append(dateFrom)
+                .append(" - ")
+                .append(dateTo)
+                .append(System.lineSeparator());
+
+        int yearFrom = Integer.parseInt(dateFrom.split("\\.")[2]);
+        int monthFrom = Integer.parseInt(dateFrom.split("\\.")[1]);
+        int dayFrom = Integer.parseInt(dateFrom.split("\\.")[0]);
+
+        int yearTo = Integer.parseInt(dateTo.split("\\.")[2]);
+        int monthTo = Integer.parseInt(dateTo.split("\\.")[1]);
+        int dayTo = Integer.parseInt(dateTo.split("\\.")[0]);
+
+        for (int i = 0; i < names.length; i++) {
+            int counter = 0;
+            for (String datum : data) {
+                String[] parts = datum.split(" ");
+                int year = Integer.parseInt(parts[0].split("\\.")[2]);
+                int month = Integer.parseInt(parts[0].split("\\.")[1]);
+                int day = Integer.parseInt(parts[0].split("\\.")[0]);
+                if (year == yearFrom && year == yearTo) {
+                    if (month == monthFrom && month == monthTo) {
+                        if (day < dayFrom || day > dayTo) {
+                            continue;
+                        } else {
+                            if (parts[1].equals(names[i])) {
+                                counter += Integer.parseInt(parts[2]) * Integer.parseInt(parts[3]);
+                            }
+                        }
+                    } else {
+                        if (month < monthFrom || month > monthTo) {
+                            continue;
+                        } else {
+                            if (month == monthFrom) {
+                                if (day < dayFrom) {
+                                    continue;
+                                } else {
+                                    if (parts[1].equals(names[i])) {
+                                        counter += Integer.parseInt(parts[2])
+                                                * Integer.parseInt(parts[3]);
+                                    }
+                                }
+                            } else if (month == monthTo) {
+                                if (day > dayTo) {
+                                    continue;
+                                } else {
+                                    if (parts[1].equals(names[i])) {
+                                        counter += Integer.parseInt(parts[2])
+                                                * Integer.parseInt(parts[3]);
+                                    }
+                                }
+                            } else {
+                                if (parts[1].equals(names[i])) {
+                                    counter += Integer.parseInt(parts[2])
+                                            * Integer.parseInt(parts[3]);
+                                }
+                            }
+                        }
+                    }
+                } else {
+                    if (year < yearFrom || year > yearTo) {
+                        continue;
+                    } else {
+                        if (parts[1].equals(names[i])) {
+                            counter += Integer.parseInt(parts[2]) * Integer.parseInt(parts[3]);
+                        }
+                    }
+                }
+            }
+            builder.append(names[i])
+                    .append(" - ")
+                    .append(counter);
+            if (i != names.length - 1) {
+                builder.append(System.lineSeparator());
+            }
+        }
+        return builder.toString();
     }
 }
