7bf746bd899525824802b7971a7662aafe81cb99
Andrii Shevchuk <zovten156@gmail.com>
2022-08-13 15:56:46 +0300

    correct naming, access modifier

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,34 +4,35 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-    static final int FIRST_INDEX = 0;
-    static final int SECOND_INDEX = 1;
-    static final int THIRD_INDEX = 2;
-    static final int FOURTH_INDEX = 3;
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int WORK_DATE_INDEX = 0;
+    private static final int NAME_OF_WORKER_INDEX = 1;
+    private static final int HOURS_PER_DAY_INDEX = 2;
+    private static final int SALARY_PER_HOUR_INDEX = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate firstDate = LocalDate.parse(dateFrom, FORMATTER);
         LocalDate lastDate = LocalDate.parse(dateTo, FORMATTER);
-        int[] money = new int[names.length];
-        StringBuilder builder = new StringBuilder();
-        builder.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
+        int[] totalIncome = new int[names.length];
+        StringBuilder reportBuilder = new StringBuilder();
+        reportBuilder.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
         for (String newData : data) {
             String[] salaryInfo = newData.split(" ");
-            LocalDate workDate = LocalDate.parse(salaryInfo[FIRST_INDEX], FORMATTER);
+            LocalDate workDate = LocalDate.parse(salaryInfo[WORK_DATE_INDEX], FORMATTER);
             if (!(workDate.isBefore(firstDate)) && !(workDate.isAfter(lastDate))) {
                 for (int i = 0; i < names.length; i++) {
-                    if (names[i].equals(salaryInfo[SECOND_INDEX])) {
-                        money[i] = money[i] + Integer.parseInt(salaryInfo[THIRD_INDEX])
-                                * Integer.parseInt(salaryInfo[FOURTH_INDEX]);
+                    if (names[i].equals(salaryInfo[NAME_OF_WORKER_INDEX])) {
+                        totalIncome[i] = totalIncome[i]
+                                + Integer.parseInt(salaryInfo[HOURS_PER_DAY_INDEX])
+                                * Integer.parseInt(salaryInfo[SALARY_PER_HOUR_INDEX]);
                     }
                 }
             }
         }
         for (int i = 0; i < names.length; i++) {
-            builder.append(System.lineSeparator()).append(names[i]).append(" - ")
-                    .append(money[i]);
+            reportBuilder.append(System.lineSeparator()).append(names[i]).append(" - ")
+                    .append(totalIncome[i]);
         }
-        return builder.toString();
+        return reportBuilder.toString();
     }
 }
