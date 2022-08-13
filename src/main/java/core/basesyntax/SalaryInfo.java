fd05ae00e155d3fe48c76793be0470cab31d1a01
Valentyn Slutskyi <vaaalik45@gmail.com>
2022-08-13 23:04:09 +0300

    Was deleted redundant methods and created constants

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,14 +1,37 @@
 package core.basesyntax;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 public class SalaryInfo {
+    private static final String FORMATTER = "dd.MM.yyyy";
+    private static final int DATA = 0;
+    private static final int NAME = 1;
+    private static final int HOURS = 2;
+    private static final int price = 3;
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        CompareTwoDates compareTwoDates = new CompareTwoDates();
+        SimpleDateFormat format = new SimpleDateFormat(FORMATTER);
         int[] salary = new int[names.length];
         for (int i = 0; i < data.length; i++) {
-            String[] temp = data[i].split(" ");
-            if (compareTwoDates.comparator(dateFrom, temp[0], dateTo)) {
-                salary[foundIndex(names, temp[1])] +=
-                        Integer.parseInt(temp[2]) * Integer.parseInt(temp[3]);
+            String[] dataLine = data[i].split(" ");
+            boolean correctDate;
+            try {
+                correctDate = format.parse(dateFrom).compareTo(format.parse(dataLine[DATA])) <= 0
+                        && format.parse(dataLine[DATA]).compareTo(format.parse(dateTo)) <= 0;
+            } catch (ParseException e) {
+                throw new RuntimeException(e);
+            }
+            if (correctDate) {
+                int index = -1;
+                for (int j = 0; j < names.length; j++) {
+                    if (names[j].equals(dataLine[NAME])) {
+                        index = j;
+                        break;
+                    }
+                }
+                salary[index] += Integer.parseInt(dataLine[HOURS])
+                        * Integer.parseInt(dataLine[price]);
             }
         }
         StringBuilder result = new StringBuilder("Report for period ");
@@ -17,14 +40,5 @@ public class SalaryInfo {
             result.append(System.lineSeparator()).append(names[i]).append(" - ").append(salary[i]);
         }
         return result.toString();
-    }
-
-    public int foundIndex(String[] names, String name) {
-        for (int i = 0; i < names.length; i++) {
-            if (names[i].equals(name)) {
-                return i;
-            }
-        }
-        return 0;
     }
 }
