f67f0f8558a8e22733edf9cb22914c6974843d76
Kostiantyn Sacheiev <dynamofan1927@gmail.com>
2022-08-15 12:54:03 +0200

    Add solution: 1. Make constants; 2. Strings with dates was converted to LocalDate format; 3. Was added StringBuilder 4. Was added loops with implementation method getSalaryInfo

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,37 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+    private static final int ORDER_DATE_OF_STRING = 0;
+    private static final int ORDER_NAME_OF_STRING = 1;
+    private static final int ORDER_WORKING_HOURS_OF_STRING = 2;
+    private static final int ORDER_SALARY_OF_HOUR_OF_STRING = 3;
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        LocalDate localDateFrom = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate localDateTo = LocalDate.parse(dateTo, FORMATTER);
+        StringBuilder result = new StringBuilder().append("Report for period ")
+                .append(dateFrom).append(" - ").append(dateTo);
+        String[] arrayOfData;
+
+        for (String name : names) {
+            int salary = 0;
+            for (String strings : data) {
+                arrayOfData = strings.split(" ");
+                LocalDate currentDate = LocalDate.parse(arrayOfData[ORDER_DATE_OF_STRING], FORMATTER);
+                if (localDateFrom.compareTo(currentDate) <= 0
+                && localDateTo.compareTo(currentDate) >= 0
+                && arrayOfData[ORDER_NAME_OF_STRING].equals(name)) {
+                    salary += (Integer.parseInt(arrayOfData[ORDER_SALARY_OF_HOUR_OF_STRING])
+                    * Integer.parseInt(arrayOfData[ORDER_WORKING_HOURS_OF_STRING]));
+                }
+            }
+            result.append(System.lineSeparator()).append(name).append(" - ").append(salary);
+        }
+        return result.toString();
     }
 }
+
