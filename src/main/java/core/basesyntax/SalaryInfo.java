33b049b5d5ec961f758998ad7309c4029b83a9f6
Valentyn Chepiha <valentyn.chepiha@gmail.com>
2022-08-18 13:42:46 +0300

    Deleted services. Refactored SalaryInfo

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,39 +1,51 @@
 package core.basesyntax;
 
-import core.basesyntax.services.ArrayService;
-import core.basesyntax.services.CompareDate;
-import core.basesyntax.services.ParseService;
-import core.basesyntax.services.PrintService;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
 
 public class SalaryInfo {
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         int[] resultSalary = new int[names.length];
-        ArrayService arrayService = new ArrayService(names);
-        ParseService parseService = new ParseService();
-        CompareDate compareDate = new CompareDate(
-                parseService.stringToDate(dateFrom), parseService.stringToDate(dateTo));
+
+        StringBuilder result = new StringBuilder();
+        result.append("Report for period ")
+                .append(dateFrom)
+                .append(" - ")
+                .append(dateTo);
+
+        LocalDate startDate = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate endDate = LocalDate.parse(dateTo, FORMATTER);
 
         for (String rowData : data) {
             String[] valuesData = rowData.split(" ");
-            if (valuesData[0] == null || valuesData[1] == null
-                    || valuesData[2] == null || valuesData[3] == null) {
+            boolean isNull = false;
+            for (String value : valuesData) {
+                if (value == null) {
+                    isNull = true;
+                    break;
+                }
+            }
+            if (isNull) {
                 continue;
             }
 
-            int index = arrayService.indexByValue(valuesData[1]);
-            if (index < 0 || !compareDate.isBetween(parseService.stringToDate(valuesData[0]))) {
-                continue;
+            int index = Arrays.asList(names).indexOf(valuesData[1]);
+            LocalDate currentDate = LocalDate.parse(valuesData[0], FORMATTER);
+            if (index >= 0 && !currentDate.isBefore(startDate)
+                    && !currentDate.isAfter(endDate)) {
+                resultSalary[index] += Integer.parseInt(valuesData[2])
+                        * Integer.parseInt(valuesData[3]);
             }
-
-            resultSalary[index] += Integer.parseInt(valuesData[2])
-                    * Integer.parseInt(valuesData[3]);
         }
 
-        StringBuilder result = new StringBuilder();
-        result.append(PrintService.printHead(dateFrom, dateTo));
         for (int i = 0; i < names.length; i++) {
             result.append(System.lineSeparator())
-                    .append(PrintService.printRow(names[i], String.valueOf(resultSalary[i])));
+                    .append(names[i])
+                    .append(" - ")
+                    .append(resultSalary[i]);
         }
         return result.toString();
     }
