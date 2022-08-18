d800db89dd67866126601d56fd06a56b9ff1d922
AndriiP <andrey.pslk@gmail.com>
2022-08-18 16:50:25 +0300

    getSalaryInfo method refactored: splited dataline is passed to method calculateSalaryForEmployee(); for local date compareTo replaced by isEqual, isAfter, isBefore appropriately. Indexes moved to constants.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,19 +4,22 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-    static final int FIRST_SPACE_INDEX = 10;
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final String SPACE_REGEX = "\\s";
+    private static final int INDEX_OF_DATE = 0;
+    private static final int INDEX_OF_WAGE = 2;
+    private static final int INDEX_OF_HOURS_WORKED = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("Report for period " + dateFrom + " - " + dateTo);
+        StringBuilder reportBuilder = new StringBuilder();
+        reportBuilder.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
         LocalDate startDate = LocalDate.parse(dateFrom, formatter);
         LocalDate endDate = LocalDate.parse(dateTo, formatter);
         for (String name : names) {
-            stringBuilder.append(System.lineSeparator());
-            stringBuilder.append(calculateSalaryForEmployee(data, name, startDate, endDate));
+            reportBuilder.append(System.lineSeparator());
+            reportBuilder.append(calculateSalaryForEmployee(data, name, startDate, endDate));
         }
-        return stringBuilder.toString();
+        return reportBuilder.toString();
     }
 
     private String calculateSalaryForEmployee(String[] data, String name,
@@ -24,22 +27,21 @@ public class SalaryInfo {
         int salary = 0;
         for (String dataLine : data) {
             if (dataLine.contains(name)) {
-                salary += calculateIfApplicable(dataLine, startDate, endDate);
+                salary += getSalary(dataLine.split(SPACE_REGEX), startDate, endDate);
             }
         }
         return name + " - " + salary;
     }
 
-    private int calculateIfApplicable(String dataLine, LocalDate startDate, LocalDate endDate) {
-        String currentDateString = dataLine.substring(0, FIRST_SPACE_INDEX);
-        LocalDate currentDate = LocalDate.parse(currentDateString, formatter);
-        return (currentDate.compareTo(startDate) >= 0 && currentDate.compareTo(endDate) <= 0)
-                ? salaryForCurrentDate(dataLine) : 0;
+    private int getSalary(String[] line, LocalDate startDate, LocalDate endDate) {
+        LocalDate currentDate = LocalDate.parse(line[INDEX_OF_DATE], formatter);
+        return ((currentDate.isEqual(startDate) || currentDate.isAfter(startDate))
+                && (currentDate.isEqual(endDate) || currentDate.isBefore(endDate)))
+                ? getSalaryForCurrentDate(line) : 0;
     }
 
-    private int salaryForCurrentDate(String dataLine) {
-        String[] digitsStrings = dataLine.split("[A-Z,a-z]+\\s");
-        String[] hoursAndWage = digitsStrings[digitsStrings.length - 1].split("\\s");
-        return Integer.parseInt(hoursAndWage[0]) * Integer.parseInt(hoursAndWage[1]);
+    private int getSalaryForCurrentDate(String[] line) {
+        return Integer.parseInt(line[INDEX_OF_WAGE])
+                * Integer.parseInt(line[INDEX_OF_HOURS_WORKED]);
     }
 }
