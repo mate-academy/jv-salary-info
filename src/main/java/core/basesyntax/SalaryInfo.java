7b8b358387aa4becd44692addea48b78b13b3f48
Riki Maru <qui.eg0m@gmail.com>
2022-08-18 15:45:35 +0300

    fix code for linter

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,41 @@
 package core.basesyntax;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class SalaryInfo {
+    private final SimpleDateFormat dateFormatter = new SimpleDateFormat("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        try {
+            final Date dateFromFormatted = dateFormatter.parse(dateFrom);
+            final Date dateToFormatted = dateFormatter.parse(dateTo);
+            StringBuilder salaryInfo = new StringBuilder("Report for period ")
+                    .append(dateFrom)
+                    .append(" - ")
+                    .append(dateTo);
+            for (String name : names) {
+                int salarySum = 0;
+                for (String emplData : data) {
+                    String[] emplDataSplitted = emplData.split(" ");
+                    Date dataSalaryFormatted = dateFormatter.parse(emplDataSplitted[0]);
+                    if (name.equals(emplDataSplitted[1])
+                            && (dateFromFormatted.compareTo(dataSalaryFormatted) < 1
+                            && dateToFormatted.compareTo(dataSalaryFormatted) > -1)
+                    ) {
+                        salarySum += Integer.parseInt(emplDataSplitted[2])
+                                * Integer.parseInt(emplDataSplitted[3]);
+                    }
+                }
+                salaryInfo.append(System.lineSeparator())
+                        .append(name)
+                        .append(" - ")
+                        .append(salarySum);
+            }
+            return salaryInfo.toString();
+        } catch (ParseException e) {
+            return "Wrongs data";
+        }
     }
 }
