a97db17a44e1f3a6a58756ea4306a24ae5fbfeab
Valentyn Chepiha <valentyn.chepiha@gmail.com>
2022-08-18 18:48:19 +0300

    Refactored SalaryInfo: deleted array

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -7,12 +7,12 @@ public class SalaryInfo {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        int[] resultSalary = new int[names.length];
+        int totalSalary = 0;
         StringBuilder result = new StringBuilder();
         LocalDate startDate = LocalDate.parse(dateFrom, FORMATTER);
         LocalDate endDate = LocalDate.parse(dateTo, FORMATTER);
         result.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
-        for (int i = 0; i < names.length; i++) {
+        for (String name : names) {
             for (String record : data) {
                 String[] valuesData = record.split(" ");
                 boolean isNull = false;
@@ -26,14 +26,15 @@ public class SalaryInfo {
                     continue;
                 }
                 LocalDate currentDate = LocalDate.parse(valuesData[0], FORMATTER);
-                if (names[i].equals(valuesData[1])
+                if (name.equals(valuesData[1])
                         && !currentDate.isBefore(startDate) && !currentDate.isAfter(endDate)) {
-                    resultSalary[i] += Integer.parseInt(valuesData[2])
+                    totalSalary += Integer.parseInt(valuesData[2])
                             * Integer.parseInt(valuesData[3]);
                 }
             }
-            result.append(System.lineSeparator()).append(names[i]).append(" - ")
-                    .append(resultSalary[i]);
+            result.append(System.lineSeparator()).append(name).append(" - ")
+                    .append(totalSalary);
+            totalSalary = 0;
         }
         return result.toString();
     }
