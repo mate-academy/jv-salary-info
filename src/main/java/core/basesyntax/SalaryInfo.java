f1108e52dbbfb11f1e823dd62168ea8c28e4045a
Riki Maru <qui.eg0m@gmail.com>
2022-08-19 09:53:27 +0300

    hw fix

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,41 +1,44 @@
 package core.basesyntax;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private final SimpleDateFormat dateFormatter = new SimpleDateFormat("dd.MM.yyyy");
+    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter
+            .ofPattern("dd.MM.yyyy");
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int HOURS_INDEX = 2;
+    private static final int SALARY_INDEX = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        try {
-            final Date dateFromFormatted = dateFormatter.parse(dateFrom);
-            final Date dateToFormatted = dateFormatter.parse(dateTo);
-            StringBuilder salaryInfo = new StringBuilder("Report for period ")
-                    .append(dateFrom)
-                    .append(" - ")
-                    .append(dateTo);
-            for (String name : names) {
-                int salarySum = 0;
-                for (String emplData : data) {
-                    String[] emplDataSplitted = emplData.split(" ");
-                    Date dataSalaryFormatted = dateFormatter.parse(emplDataSplitted[0]);
-                    if (name.equals(emplDataSplitted[1])
-                            && (dateFromFormatted.compareTo(dataSalaryFormatted) < 1
-                            && dateToFormatted.compareTo(dataSalaryFormatted) > -1)
-                    ) {
-                        salarySum += Integer.parseInt(emplDataSplitted[2])
-                                * Integer.parseInt(emplDataSplitted[3]);
-                    }
+        LocalDate dateFromFormatted = LocalDate.parse(dateFrom, DATE_FORMATTER);
+        LocalDate dateToFormatted = LocalDate.parse(dateTo, DATE_FORMATTER);
+        StringBuilder salaryInfo = new StringBuilder("Report for period ")
+                .append(dateFrom)
+                .append(" - ")
+                .append(dateTo);
+        for (String name : names) {
+            int salarySum = 0;
+            for (String datas : data) {
+                String[] datasSplitted = datas.split(" ");
+                LocalDate dataSalaryFormatted = LocalDate.parse(
+                        datasSplitted[DATE_INDEX], DATE_FORMATTER);
+                int dateFromCompare = dateFromFormatted.compareTo(dataSalaryFormatted);
+                int dateToCompare = dateToFormatted.compareTo(dataSalaryFormatted);
+                int datasHours = Integer.parseInt(datasSplitted[HOURS_INDEX]);
+                int salaryPerHour = Integer.parseInt(datasSplitted[SALARY_INDEX]);
+                if (name.equals(datasSplitted[NAME_INDEX])
+                        && (dateFromCompare < 1 && dateToCompare > -1)
+                ) {
+                    salarySum += datasHours * salaryPerHour;
                 }
-                salaryInfo.append(System.lineSeparator())
-                        .append(name)
-                        .append(" - ")
-                        .append(salarySum);
             }
-            return salaryInfo.toString();
-        } catch (ParseException e) {
-            return "Wrongs data";
+            salaryInfo.append(System.lineSeparator())
+                    .append(name)
+                    .append(" - ")
+                    .append(salarySum);
         }
+        return salaryInfo.toString();
     }
 }
