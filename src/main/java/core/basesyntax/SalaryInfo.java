1cbaebb80321dfc8e311a5b4480ace1430be6cef
Roman Denysiuk <r.v.denysiuk@gmail.com>
2022-08-23 09:44:33 +0300

    checkstyle changes

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -6,9 +6,10 @@ import java.time.format.DateTimeFormatter;
 public class SalaryInfo {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
     private static final int DATA = 0;
-    public static final int NAME = 1;
-    public static final int HOUR = 2;
-    public static final int  SALARY_PER_HOUR = 3;
+    private static final int NAME = 1;
+    private static final int HOUR = 2;
+    private static final int SALARY_PER_HOUR = 3;
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
 
         LocalDate from = LocalDate.parse(dateFrom, FORMATTER);
@@ -19,15 +20,14 @@ public class SalaryInfo {
                 .append(" - ")
                 .append(dateTo);
 
-
-        for(String name: names) {
+        for (String name: names) {
             int salary = 0;
-            for(String lineOfData: data) {
+            for (String lineOfData: data) {
                 String[] line = lineOfData.split(" ");
                 if (name.equals(line[NAME])) {
                     LocalDate date = LocalDate.parse(line[DATA], FORMATTER);
                     if (date.compareTo(from) >= 0 && date.compareTo(to) <=0) {
-                        salary += (Integer.parseInt(line[HOUR])* Integer.parseInt(line[SALARY_PER_HOUR]));
+                        salary += (Integer.parseInt(line[HOUR]) * Integer.parseInt(line[SALARY_PER_HOUR]));
                         builder.append(System.lineSeparator())
                                 .append(name)
                                 .append(" - ")
