a6558edfdfa32e3e00919d4dc94d968662fd8c59
Valeriy M <valeriym250@gmail.com>
2022-08-24 18:34:02 +0300

    Completed task according to your instance

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,81 +1,37 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int HOURS_INDEX = 2;
+    private static final int SALARY_INDEX = 3;
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        String account = new String();
-        String compareName = new String();
-        int[][][] calculation = new int[names.length + 2][2][3];
-        int index = 0;
-        /*
-       calculation =
-        { [0] [0] [0 - 2]- date from
-          [0] [1] [0 - 2] - date to
 
-          [1] [0] [0 - 2] - String data[i] 10.04.2019 John 7 100 = date only: 10 04 2019
-          [1] [1] [0 - 1] - String data[i] 10.04.2019 John 7 100 = only: 7 100
+        LocalDate from = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate to = LocalDate.parse(dateTo, FORMATTER);
 
-          [2] [0] [0] - result of user -  names[0];
-          [3] [0] [0] - result of user2 - names[1];
-          [4] [0] [0] - result of user3 - names[2];
-        */
+        StringBuilder reportBuilder = new StringBuilder();
+        reportBuilder.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
 
-        //send day "From" and "To" to int array
-        calculation[0][0] = toIntArray(dateFrom, "\\.");
-        calculation[0][1] = toIntArray(dateTo, "\\.");
-        // Validation: input Data, bounds
-        if (!validationData(calculation[0][0], calculation[0][1])) {
-            return ("Data \"from\" can`t be bigger than data \"to\"");
-        }
-        for (int i = 0; i < data.length; i++) {
-            // String - Array of day, month and year without current employee
-            index = data[i].indexOf(' ');
-            account = data[i].substring(0, index);
-            calculation[1][0] = toIntArray(account, "\\.");
-            // Data check
-            if (!validationData(calculation[0][0], calculation[1][0])
-                    || !validationData(calculation[1][0], calculation[0][1])) {
-                continue;
-            }
-            // if data was applied, lets find person and write result
-            account = data[i].substring(index + 1); //  "13.02.2019 John 7 100" = John 7 100;
-            index = account.indexOf(' ');
-            compareName = account.substring(0, index);
-            account = account.substring(index + 1);
-            calculation[1][1] = toIntArray(account, "\\ "); // calculation[1][1] = 7 100;
-            for (int j = 0; j < names.length; j++) {
-                if (!names[j].equals(compareName)) {
-                    continue;
+        for (String name : names) {
+            int salary = 0;
+            for (String lines : data) {
+                String[] line = lines.split(" ");
+                if (name.equals(line[NAME_INDEX])) {
+                    LocalDate date = LocalDate.parse(line[DATE_INDEX],FORMATTER);
+                    if (date.compareTo(from) >= 0 && date.compareTo(to) <= 0) {
+                        salary += Integer.parseInt(line[HOURS_INDEX]) * Integer.parseInt(line[SALARY_INDEX]);
+                    }
                 }
-                calculation[j + 2][0][0] += calculation[1][1][0] * calculation[1][1][1]; // result
             }
+            reportBuilder.append(System.lineSeparator()).append(name). append(" - ").append(salary);
         }
-        //Out information
-        account = "";
-        for (int i = 0; i < 3; i++) {
-            account += System.lineSeparator() + names[i] + " - " + calculation[i + 2][0][0];
-        }
-        return "Report for period " + dateFrom + " - " + dateTo + account;
-    }
-
-    private boolean validationData(int [] smallerValues, int [] biggerValues) {
-        for (int i = smallerValues.length - 1; i > -1; i--) {
-            if (biggerValues[i] > smallerValues[i]) {
-                return true;
-            }
-            if (biggerValues[i] < smallerValues[i]) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private int [] toIntArray(String input, String divide) {
-        String [] stringArray = input.split(divide);
-        int [] outArray = new int [stringArray.length];
-
-        for (int i = 0; i < stringArray.length; i++) {
-            outArray[i] = Integer.parseInt(stringArray[i]);
-        }
-        return outArray;
+        return reportBuilder.toString();
     }
 }
