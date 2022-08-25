89779d2c1b6bde14db5110c17579037a0474dfca
Andrii Petruk <andrzej.petruk@bk.ru>
2022-08-25 22:00:05 +0200

    task solved

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,41 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int HOUR_INDEX = 2;
+    private static final int SALARY_PER_HOUR_INDEX = 3;
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        LocalDate from = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate to = LocalDate.parse(dateTo, FORMATTER);
+        StringBuilder reportBuilder = new StringBuilder();
+        reportBuilder.append("Report for period ")
+                .append(dateFrom)
+                .append(" - ")
+                .append(dateTo);
+
+        for (String name: names) {
+            int salary = 0;
+            for (String lineOfData: data) {
+                String[] line = lineOfData.split(" ");
+                if (name.equals(line[NAME_INDEX])) {
+                    LocalDate date = LocalDate.parse(line[DATE_INDEX], FORMATTER);
+                    if (date.compareTo(from) >= 0 && date.compareTo(to) <= 0) {
+                        salary += (Integer.parseInt(line[HOUR_INDEX])
+                                * Integer.parseInt(line[SALARY_PER_HOUR_INDEX]));
+                    }
+                }
+            }
+            reportBuilder.append(System.lineSeparator())
+                    .append(name)
+                    .append(" - ")
+                    .append(salary);
+        }
+        return reportBuilder.toString();
     }
 }
