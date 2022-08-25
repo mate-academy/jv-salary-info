b1ec27ef1f2978de2c405e5049a192df9aa72b7f
Oleksandr Kravchenko <Pokemonishche@me.com>
2022-08-25 17:45:45 +0300

    Mission accomplished

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,34 @@
 package core.basesyntax;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 public class SalaryInfo {
+    private static final SimpleDateFormat time = new SimpleDateFormat("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
+        for (int i = 0; i < names.length; i++) {
+            int sum = 0;
+            stringBuilder.append(System.lineSeparator());
+            for (int j = 0; j < data.length; j++) {
+                String[] table = data[j].split(" ");
+                if (names[i].equals(table[1])) {
+                    try {
+                        if (time.parse(dateFrom).equals(time.parse(table[0]))
+                                || time.parse(dateTo).equals(time.parse(table[0]))
+                                || (time.parse(table[0]).after(time.parse(dateFrom))
+                                && time.parse(table[0]).before(time.parse(dateTo)))) {
+                            sum += Integer.parseInt(table[2]) * Integer.parseInt(table[3]);
+                        }
+                    } catch (ParseException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+            stringBuilder.append(names[i]).append(" - ").append(sum);
+        }
+        return stringBuilder.toString();
     }
 }
