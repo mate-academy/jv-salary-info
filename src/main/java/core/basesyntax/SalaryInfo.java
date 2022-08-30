2345ba7d9cd635bed21618c9065a81be1e4f1c3d
Oleg Pruh <olegpruh1@gmail.com>
2022-08-30 17:54:12 +0100

    Code for calculating salaries had been written

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,39 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        LocalDate fromDate = parseDate(dateFrom);
+        LocalDate toDate = parseDate(dateTo);
+        var result = new StringBuilder();
+        int[] salariesArr = new int[names.length];
+
+        for (String person : data) {
+            String[] personData = person.split(" ");
+            LocalDate dataDate = parseDate(personData[0]);
+            int salary = Integer.parseInt(personData[2]) * Integer.parseInt(personData[3]);
+            if (dataDate.compareTo(fromDate) >= 0
+                    && dataDate.compareTo(toDate) <= 0) {
+                salariesArr[Arrays.asList(names).indexOf(personData[1])] += salary;
+            }
+        }
+
+        result.append("Report for period ").append(dateFrom).append(" - ")
+                .append(dateTo);
+
+        for (int i = 0; i < names.length; i++) {
+            result.append(System.lineSeparator()).append(names[i])
+                    .append(" - ").append(salariesArr[i]);
+        }
+
+        return result.toString();
     }
+
+    public static LocalDate parseDate(String date) {
+        return LocalDate.parse(date, DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
 }
