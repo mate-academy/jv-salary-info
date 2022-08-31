ddbe61b153bec6d68d486a210f7bcf947c46caf4
Maksym Ivanov <ivanov1maxim@gmail.com>
2022-08-31 18:06:07 +0300

    Simplify logic by using Formatter

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,94 +1,42 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder result = new StringBuilder("Report for period ");
-        String lineSeparator = System.lineSeparator();
+        LocalDate from = LocalDate.parse(dateFrom, formatter);
+        LocalDate to = LocalDate.parse(dateTo, formatter);
 
         result.append(dateFrom)
                 .append(" - ")
-                .append(dateTo)
-                .append(lineSeparator);
+                .append(dateTo);
 
-        int[] salaries = this.countSalaries(names, data, dateFrom, dateTo);
+        for (final String name : names) {
+            result.append(System.lineSeparator());
+            int salary = 0;
 
-        for (int i = 0; i < names.length; i++) {
-            result.append(names[i])
-                    .append(" - ")
-                    .append(salaries[i]);
-            if (i != names.length - 1) {
-                result.append(lineSeparator);
-            }
-        }
-
-        return result.toString();
-    }
-
-    private int[] convertDateToIntArray(String date) {
-        String[] dateArray = date.split("\\.");
-        int[] result = new int[dateArray.length];
-        for (int i = 0; i < dateArray.length; i++) {
-            result[i] = Integer.parseInt(dateArray[i]);
-        }
-        return result;
-    }
-
-    private boolean isDateValid(String dateActual, String dateFrom, String dateTo) {
-
-        int[] dateActualArray = this.convertDateToIntArray(dateActual);
-        int[] dateFromArray = this.convertDateToIntArray(dateFrom);
-        int[] dateToArray = this.convertDateToIntArray(dateTo);
-
-        boolean result = false;
-
-        if (dateActualArray[2] >= dateFromArray[2]) {
-            if (dateActualArray[2] > dateFromArray[2]) {
-                result = true;
-            } else if (dateActualArray[1] > dateFromArray[1]) {
-                result = true;
-            } else if (dateActualArray[1] == dateFromArray[1]) {
-                result = dateActualArray[0] >= dateFromArray[0];
-            }
-        }
-
-        if (result) {
-            if (dateActualArray[2] <= dateToArray[2]) {
-                if (dateActualArray[2] < dateToArray[2]) {
-                    result = true;
-                } else if (dateActualArray[1] < dateToArray[1]) {
-                    result = true;
-                } else if (dateActualArray[1] == dateToArray[1]) {
-                    result = dateActualArray[0] <= dateToArray[0];
-                } else {
-                    result = false;
-                }
-            } else {
-                result = false;
-            }
-        }
-        return result;
-    }
-
-    private int[] countSalaries(String[] names, String[] data, String dateFrom, String dateTo) {
-        int[] result = new int[names.length];
-
-        for (int i = 0; i < names.length; i++) {
             for (final String datum : data) {
-                String[] dataSplit = datum.split(" ");
-                if (names[i].equals(dataSplit[1])) {
+                String[] datumSplit = datum.split(" ");
+                LocalDate current = LocalDate.parse(datumSplit[0], formatter);
+
+                if (name.equals(datumSplit[1])) {
                     if (
-                            this.isDateValid(
-                                    dataSplit[0],
-                                    dateFrom,
-                                    dateTo
-                            )
+                            (current.isAfter(from) || current.isEqual(from))
+                                    && (current.isBefore(to) || current.isEqual(to))
                     ) {
-                        result[i] += Integer.parseInt(dataSplit[3])
-                                * Integer.parseInt(dataSplit[2]);
+                        salary += Integer.parseInt(datumSplit[2]) * Integer.parseInt(datumSplit[3]);
                     }
                 }
             }
+            result.append(name)
+                    .append(" - ")
+                    .append(salary);
         }
-        return result;
+        return result.toString();
     }
 }
