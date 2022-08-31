18eb3c417328ccae67b71ff40f004e2511514b9e
Maksym Ivanov <ivanov1maxim@gmail.com>
2022-08-31 19:09:46 +0300

    Add index constant variables

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,13 +4,16 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-
     private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder result = new StringBuilder("Report for period ");
         LocalDate from = LocalDate.parse(dateFrom, formatter);
         LocalDate to = LocalDate.parse(dateTo, formatter);
+        final int dateIndex = 0;
+        final int nameIndex = 1;
+        final int hoursIndex = 2;
+        final int salaryIndex = 3;
 
         result.append(dateFrom)
                 .append(" - ")
@@ -22,14 +25,15 @@ public class SalaryInfo {
 
             for (final String datum : data) {
                 String[] datumSplit = datum.split(" ");
-                LocalDate current = LocalDate.parse(datumSplit[0], formatter);
+                LocalDate current = LocalDate.parse(datumSplit[dateIndex], formatter);
 
-                if (name.equals(datumSplit[1])) {
+                if (name.equals(datumSplit[nameIndex])) {
                     if (
                             (current.isAfter(from) || current.isEqual(from))
                                     && (current.isBefore(to) || current.isEqual(to))
                     ) {
-                        salary += Integer.parseInt(datumSplit[2]) * Integer.parseInt(datumSplit[3]);
+                        salary += Integer.parseInt(datumSplit[hoursIndex])
+                                * Integer.parseInt(datumSplit[salaryIndex]);
                     }
                 }
             }
