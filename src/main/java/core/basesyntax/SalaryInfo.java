eaa932ce590cdf1a09f625768768a27077aed2c7
Maksym Ivanov <ivanov1maxim@gmail.com>
2022-09-01 17:11:05 +0300

    Add index constant variables as class fields

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,36 +4,31 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    static final int DATE_INDEX = 0;
+    static final int NAME_INDEX = 1;
+    static final int HOURS_INDEX = 2;
+    static final int SALARY_INDEX = 3;
+
     private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder result = new StringBuilder("Report for period ");
         LocalDate from = LocalDate.parse(dateFrom, formatter);
         LocalDate to = LocalDate.parse(dateTo, formatter);
-        final int dateIndex = 0;
-        final int nameIndex = 1;
-        final int hoursIndex = 2;
-        final int salaryIndex = 3;
-
         result.append(dateFrom)
                 .append(" - ")
                 .append(dateTo);
-
         for (final String name : names) {
             result.append(System.lineSeparator());
             int salary = 0;
-
             for (final String datum : data) {
                 String[] datumSplit = datum.split(" ");
-                LocalDate current = LocalDate.parse(datumSplit[dateIndex], formatter);
-
-                if (name.equals(datumSplit[nameIndex])) {
-                    if (
-                            (current.isAfter(from) || current.isEqual(from))
-                                    && (current.isBefore(to) || current.isEqual(to))
-                    ) {
-                        salary += Integer.parseInt(datumSplit[hoursIndex])
-                                * Integer.parseInt(datumSplit[salaryIndex]);
+                LocalDate current = LocalDate.parse(datumSplit[DATE_INDEX], formatter);
+                if (name.equals(datumSplit[NAME_INDEX])) {
+                    if ((current.isAfter(from) || current.isEqual(from))
+                            && (current.isBefore(to) || current.isEqual(to))) {
+                        salary += Integer.parseInt(datumSplit[HOURS_INDEX])
+                                * Integer.parseInt(datumSplit[SALARY_INDEX]);
                     }
                 }
             }
