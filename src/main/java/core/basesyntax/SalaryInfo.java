32ace9f6b402d1004b35aca46bada574c0ae6719
max-bolotin <realprof.mx@gmail.com>
2022-09-03 14:55:32 +0300

    map removed, logic implemented via two cycles

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -2,34 +2,33 @@ package core.basesyntax;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.HashMap;
-import java.util.Map;
 
 public class SalaryInfo {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
     private static final String SPLITTER = " ";
     private static final String HYPHEN = " - ";
-    private final Map<String, Integer> employees = new HashMap<>();
+    private static final String HEADER = "Report for period ";
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        for (String name : names) {
-            employees.put(name, 0);
-        }
-        StringBuilder reportForPeriod = new StringBuilder("Report for period ").append(dateFrom)
+        LocalDate localDateFrom = getDate(dateFrom);
+        LocalDate localDateTo = getDate(dateTo);
+        StringBuilder reportForPeriod = new StringBuilder(HEADER).append(dateFrom)
                 .append(HYPHEN).append(dateTo);
-        for (String employeeData : data) {
-            String[] details = employeeData.split(SPLITTER);
-            LocalDate date = getDate(details[0]);
-            String nameOfEmployee = details[1];
+        for (String employee: names) {
             int dailySalary;
-            if (date.compareTo(getDate(dateFrom)) >= 0 && date.compareTo(getDate(dateTo)) <= 0) {
-                dailySalary = Integer.parseInt(details[2]) * Integer.parseInt(details[3]);
-                employees.put(nameOfEmployee, employees.get(nameOfEmployee) + dailySalary);
+            int salary = 0;
+            for (String employeeData : data) {
+                String[] details = employeeData.split(SPLITTER);
+                if (employee.equals(details[1])) {
+                    LocalDate date = getDate(details[0]);
+                    if (date.compareTo(localDateFrom) >= 0 && date.compareTo(localDateTo) <= 0) {
+                        dailySalary = Integer.parseInt(details[2]) * Integer.parseInt(details[3]);
+                        salary += dailySalary;
+                    }
+                }
             }
-        }
-        for (String name : names) {
-            reportForPeriod.append(System.lineSeparator()).append(name)
-                    .append(HYPHEN).append(employees.get(name));
+            reportForPeriod.append(System.lineSeparator()).append(employee)
+                        .append(HYPHEN).append(salary);
         }
         return reportForPeriod.toString();
     }
