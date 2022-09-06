461d5d976ec0b7411471537d450e256216d92a56
Dima Senko <d.senko@i.ua>
2022-09-06 12:46:14 +0300

    Calculated the salary of each employee for a given period of time

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,27 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        LocalDate dateOne = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate dateTwo = LocalDate.parse(dateTo, FORMATTER);
+        StringBuilder result = new StringBuilder("Report for period " + dateFrom + " - " + dateTo);
+        for (String name: names) {
+            int salary = 0;
+            for (String line: data) {
+                String[] partsLine = line.split(" ");
+                LocalDate date = LocalDate.parse(partsLine[0], FORMATTER);
+                if (name.equals(partsLine[1]) && dateOne.compareTo(date) <= 0
+                        && dateTwo.compareTo(date) >= 0) {
+                    salary += Integer.valueOf(partsLine[2]) * Integer.valueOf(partsLine[3]);
+                }
+            }
+            result.append(System.lineSeparator()).append(name).append(" - ").append(salary);
+        }
+        return result.toString();
     }
 }
