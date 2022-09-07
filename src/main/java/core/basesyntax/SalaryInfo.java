06787229b8e9ba022c74180a5971dd88818ddd78
Dima Senko <d.senko@i.ua>
2022-09-07 12:49:04 +0300

    constant for indexes

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,6 +5,10 @@ import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int DATE = 0;
+    private static final int NAME = 1;
+    private static final int HOUR = 2;
+    private static final int INCOME = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate dateOne = LocalDate.parse(dateFrom, FORMATTER);
@@ -14,10 +18,10 @@ public class SalaryInfo {
             int salary = 0;
             for (String line: data) {
                 String[] partsLine = line.split(" ");
-                LocalDate date = LocalDate.parse(partsLine[0], FORMATTER);
-                if (name.equals(partsLine[1]) && dateOne.compareTo(date) <= 0
+                LocalDate date = LocalDate.parse(partsLine[DATE], FORMATTER);
+                if (name.equals(partsLine[NAME]) && dateOne.compareTo(date) <= 0
                         && dateTwo.compareTo(date) >= 0) {
-                    salary += Integer.valueOf(partsLine[2]) * Integer.valueOf(partsLine[3]);
+                    salary += Integer.valueOf(partsLine[HOUR]) * Integer.valueOf(partsLine[INCOME]);
                 }
             }
             result.append(System.lineSeparator()).append(name).append(" - ").append(salary);
