71a4f6614cc16bc675e58335813772205b495f7b
Ilja Nekrituk <Nekrit99@gmail.com>
2022-09-13 16:10:29 +0300

    Fixed

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,40 +1,37 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
     private static final Integer HOURS_OF_WORK_INDEX = 2;
     private static final Integer SALARY_PER_HOUR_INDEX = 3;
     private static final Integer POSITION_OF_DATE_IN_STRING = 0;
-    private static final Integer START_DAY_INDEX = 0;
-    private static final Integer START_MONTH_INDEX = 2;
-    private static final Integer END_DATE_INDEX = 3;
-    private static final Integer END_MONTH_INDEX = 5;
-    private static final Integer DAYS_IN_MONTH = 30;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        int startDay = calculateDays(dateFrom);
-        int endDay = calculateDays(dateTo);
-        StringBuilder sb = new StringBuilder("Report for period " + dateFrom + " - " + dateTo);
+        DateTimeFormatter formatter1 = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDate startCounting = LocalDate.parse(dateFrom, formatter1);
+        LocalDate endCounting = LocalDate.parse(dateTo, formatter1);
+        StringBuilder stringBuilder = new StringBuilder("Report for period "
+                + dateFrom + " - " + dateTo);
         for (String name : names) {
             int sumForName = 0;
-            for (String string : data) {
-                String[] dataString = string.split(" ");
-                if (string.contains(name)) {
-                    int sum = Integer.parseInt(dataString[HOURS_OF_WORK_INDEX])
-                            * Integer.parseInt(dataString[SALARY_PER_HOUR_INDEX]);
-                    if (calculateDays(dataString[POSITION_OF_DATE_IN_STRING]) >= startDay
-                            && calculateDays(dataString[POSITION_OF_DATE_IN_STRING]) <= endDay) {
+            for (String line : data) {
+                String[] splittedLine = line.split(" ");
+                if (line.contains(name)) {
+                    LocalDate date = LocalDate
+                            .parse(splittedLine[POSITION_OF_DATE_IN_STRING], formatter1);
+                    int sum = Integer.parseInt(splittedLine[HOURS_OF_WORK_INDEX])
+                            * Integer.parseInt(splittedLine[SALARY_PER_HOUR_INDEX]);
+                    if ((date.isAfter(startCounting) || date.isEqual(startCounting))
+                            && (date.isBefore(endCounting) || date.isEqual(endCounting))) {
                         sumForName += sum;
                     }
                 }
             }
-            sb.append(System.lineSeparator()).append(name).append(" - ").append(sumForName);
+            stringBuilder.append(System.lineSeparator())
+                    .append(name).append(" - ").append(sumForName);
         }
-        return sb.toString();
-    }
-
-    private static Integer calculateDays(String string) {
-        return Integer.parseInt(string.substring(END_DATE_INDEX, END_MONTH_INDEX))
-                * DAYS_IN_MONTH
-                + Integer.parseInt(string.substring(START_DAY_INDEX, START_MONTH_INDEX));
+        return stringBuilder.toString();
     }
 }
