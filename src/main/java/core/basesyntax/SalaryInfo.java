2e2df6ab85bb52d62b32020b92acaf88fab85135
sasha <radovietsoleksandr@gmail.com>
2022-09-13 18:12:21 +0300

    salaryinfo00

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,33 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
-    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+    static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("d.MM.yyyy");
+
+    public static String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
+        LocalDate from = LocalDate.parse(dateFrom, dateTimeFormatter);
+        LocalDate to = LocalDate.parse(dateTo, dateTimeFormatter);
+        StringBuilder builder = new StringBuilder("Report for period ");
+        builder.append(dateFrom).append(" - ").append(dateTo).append(System.lineSeparator());
+        for (String name : names) {
+            long salary = 0;
+            for (String line : data) {
+                String[] lineInfo = line.split(" ");
+                LocalDate workingDate = LocalDate.parse(lineInfo[0], dateTimeFormatter);
+                String employeeName = lineInfo[1];
+                int rate = Integer.parseInt(lineInfo[2]);
+                int money = Integer.parseInt(lineInfo[3]);
+                if (name.equals(employeeName)) {
+                    if ((workingDate.isAfter(from) || workingDate.equals(from))
+                            && (workingDate.isBefore(to) || workingDate.equals(to))) {
+                        salary += rate * money;
+                    }
+                }
+            }
+            builder.append(name).append(" - ").append(salary).append(System.lineSeparator());
+        }
+        return builder.toString().trim();
     }
 }
