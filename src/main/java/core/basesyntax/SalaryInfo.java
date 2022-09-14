74e262513be82dce538b31606168db55b5b5a807
Ilja Nekrituk <Nekrit99@gmail.com>
2022-09-14 21:33:37 +0300

    Done

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,14 +4,14 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
     private static final Integer HOURS_OF_WORK_INDEX = 2;
     private static final Integer SALARY_PER_HOUR_INDEX = 3;
-    private static final Integer POSITION_OF_DATE_IN_STRING = 0;
+    private static final Integer DATE_INDEX = 0;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        DateTimeFormatter formatter1 = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        LocalDate startCounting = LocalDate.parse(dateFrom, formatter1);
-        LocalDate endCounting = LocalDate.parse(dateTo, formatter1);
+        LocalDate startCounting = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate endCounting = LocalDate.parse(dateTo, FORMATTER);
         StringBuilder stringBuilder = new StringBuilder("Report for period "
                 + dateFrom + " - " + dateTo);
         for (String name : names) {
@@ -20,7 +20,7 @@ public class SalaryInfo {
                 String[] splittedLine = line.split(" ");
                 if (line.contains(name)) {
                     LocalDate date = LocalDate
-                            .parse(splittedLine[POSITION_OF_DATE_IN_STRING], formatter1);
+                            .parse(splittedLine[DATE_INDEX], FORMATTER);
                     int sum = Integer.parseInt(splittedLine[HOURS_OF_WORK_INDEX])
                             * Integer.parseInt(splittedLine[SALARY_PER_HOUR_INDEX]);
                     if ((date.isAfter(startCounting) || date.isEqual(startCounting))
