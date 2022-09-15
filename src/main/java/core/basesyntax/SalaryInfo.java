cfa5e3c64e6d8e02f16c6061d035563e8fe8e66e
Oleksandr Fedchik <fed4iksasha@gmail.com>
2022-09-15 22:23:12 +0300

    Used DateForTimeFormatter to get the date from String dateFrom, String dateTo Deleted unnecessary arrays and rewrote loop for via splitData[..]

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,40 +1,23 @@
 package core.basesyntax;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        LocalDate[] dateOfCalendar = new LocalDate[data.length];
-        String[] nameOfEmployee = new String[data.length];
-        int[] workingHours = new int[data.length];
-        int[] incomePerHour = new int[data.length];
         int[] salaryOfEmployee = new int[names.length];
-        LocalDate dateFromCalendar = LocalDate.of(
-                Integer.parseInt(dateFrom.substring(6, 10)),
-                Integer.parseInt(dateFrom.substring(3, 5)),
-                Integer.parseInt(dateFrom.substring(0, 2)) - 1);
-
-        LocalDate dateToCalendar = LocalDate.of(
-                Integer.parseInt(dateTo.substring(6, 10)),
-                Integer.parseInt(dateTo.substring(3, 5)),
-                Integer.parseInt(dateTo.substring(0, 2)) + 1);
-
-        for (int i = 0; i < data.length; i++) {
-            String[] splitData = data[i].split("\\s");
-            dateOfCalendar[i] = LocalDate.of(Integer.parseInt(data[i].substring(6, 10)),
-                    Integer.parseInt(data[i].substring(3, 5)),
-                    Integer.parseInt(data[i].substring(0, 2)));
-            nameOfEmployee[i] = splitData[1];
-            workingHours[i] = Integer.parseInt(splitData[2]);
-            incomePerHour[i] = Integer.parseInt(splitData[3]);
-        }
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDate dateFromCalendar = LocalDate.parse(dateFrom, formatter).minusDays(1);
+        LocalDate dateToCalendar = LocalDate.parse(dateTo, formatter).plusDays(1);
 
         for (int i = 0; i < names.length; i++) {
             for (int j = 0; j < data.length; j++) {
-                if ((names[i].equals(nameOfEmployee[j])) && dateOfCalendar[j]
+                String[] splitData = data[j].split("\\s");
+                if ((names[i].equals(splitData[1])) && LocalDate.parse(splitData[0], formatter)
                         .isAfter(dateFromCalendar)
-                        && dateOfCalendar[j].isBefore(dateToCalendar)) {
-                    salaryOfEmployee[i] += workingHours[j] * incomePerHour[j];
+                        && LocalDate.parse(splitData[0], formatter).isBefore(dateToCalendar)) {
+                    salaryOfEmployee[i] += Integer.parseInt(splitData[2])
+                            * Integer.parseInt(splitData[3]);
                 }
             }
         }
