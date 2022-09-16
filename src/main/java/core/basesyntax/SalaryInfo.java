98b787783063746196e97244cd5f7fe2be012c0d
X3NON-135 <immort4ld3momo@gmail.com>
2022-09-16 20:08:58 +0100

    fixed maven

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -69,15 +69,15 @@ public class SalaryInfo {
     public static void main(String[] args) {
         String[] names = {"John", "Andrew", "Kate"};
         String[] data = {"13.07.2019 John 60 50",
-                        "15.07.2019 Andrew 3 200",
-                        "15.07.2019 Kate 10 100",
+                "15.07.2019 Andrew 3 200",
+                "15.07.2019 Kate 10 100",
 
-                        "16.07.2019 Andrew 3 200",
-                        "16.07.2019 Kate 9 100",
+                "16.07.2019 Andrew 3 200",
+                "16.07.2019 Kate 9 100",
 
-                        "10.08.2019 John 7 100",
-                        "08.08.2019 Kate 3 80",
-                        "11.08.2019 Andrew 8 100"};
+                "10.08.2019 John 7 100",
+                "08.08.2019 Kate 3 80",
+                "11.08.2019 Andrew 8 100"};
 
         String dateFrom = "14.07.2019";
         String dateTo = "10.08.2019";
