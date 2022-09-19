c4062976471438d61edd2a9ae1bb070b6bc8df05
sasha <radovietsoleksandr@gmail.com>
2022-09-19 06:08:12 +0300

    salaryinfo11

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -6,6 +6,9 @@ import java.time.format.DateTimeFormatter;
 public class SalaryInfo {
     public static final DateTimeFormatter
             DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("d.MM.yyyy");
+    public static final int EMPLOYEE_NAME = 1;
+    public static final int RATE = 2;
+    public static final int MONEY = 3;
 
     public static String getSalaryInfo(String[] names,
                                        String[] data, String dateFrom, String dateTo) {
@@ -18,9 +21,9 @@ public class SalaryInfo {
             for (String line : data) {
                 String[] lineInfo = line.split(" ");
                 LocalDate workingDate = LocalDate.parse(lineInfo[0], DATE_TIME_FORMATTER);
-                String employeeName = lineInfo[1];
-                int rate = Integer.parseInt(lineInfo[2]);
-                long money = Integer.parseInt(lineInfo[3]);
+                String employeeName = lineInfo[EMPLOYEE_NAME];
+                int rate = Integer.parseInt(lineInfo[RATE]);
+                long money = Integer.parseInt(lineInfo[MONEY]);
                 if (name.equals(employeeName)) {
                     if ((workingDate.isAfter(from) || workingDate.equals(from))
                             && (workingDate.isBefore(to) || workingDate.equals(to))) {
