9c1a4381ef37afcf8956d881df7bc7ff94b3fde2
Denis Kalyakin <denis.kalyakin999@gmail.com>
2022-09-24 18:37:15 +0300

    fixed const & removing redundant arrays

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,35 +4,21 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-
-    public DateTimeFormatter getFormatter() {
-        return formatter;
-    }
-
-    public void setFormatter(DateTimeFormatter formatter) {
-        this.formatter = formatter;
-    }
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        LocalDate startDate = LocalDate.parse(dateFrom, formatter);
-        LocalDate endDate = LocalDate.parse(dateTo, formatter).plusDays(1);
-        LocalDate[] dates = new LocalDate[data.length];
-        String[] namesOfData = new String[data.length];
-        int[] hours = new int[data.length];
-        int[] rate = new int[data.length];
+        LocalDate startDate = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate endDate = LocalDate.parse(dateTo, FORMATTER).plusDays(1);
         int[] salary = new int[names.length];
 
         for (int i = 0; i < data.length; i++) {
             String[] datas = data[i].split(" ");
-            dates[i] = LocalDate.parse(datas[0], formatter);
-            namesOfData[i] = datas[1];
-            hours[i] = Integer.parseInt(datas[2]);
-            rate[i] = Integer.parseInt(datas[3]);
             for (int j = 0; j < names.length; j++) {
-                if (names[j].equals(namesOfData[i])
-                        && (dates[i].isAfter(startDate) && dates[i].isBefore(endDate))) {
-                    salary[j] = (hours[i] * rate[i]) + salary[j];
+                if (names[j].equals(datas[1])
+                        && (LocalDate.parse(datas[0], FORMATTER).isAfter(startDate)
+                        && LocalDate.parse(datas[0], FORMATTER).isBefore(endDate))) {
+                    salary[j] = (Integer.parseInt(datas[2]) * Integer.parseInt(datas[3]))
+                            + salary[j];
                 }
             }
         }
