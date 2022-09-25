8be4e353b5ee22d544a4f9ed59a4c0c0929b6297
Denis Kalyakin <denis.kalyakin999@gmail.com>
2022-09-25 13:13:57 +0300

    added fixes after review again

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,10 +5,10 @@ import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-    private static final byte DATE = 0;
-    private static final byte NAME = 1;
-    private static final byte HOUR = 2;
-    private static final byte RATE = 3;
+    private static final byte DATE_INDEX = 0;
+    private static final byte NAME_INDEX = 1;
+    private static final byte HOUR_INDEX = 2;
+    private static final byte RATE_INDEX = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate startDate = LocalDate.parse(dateFrom, FORMATTER);
@@ -17,21 +17,21 @@ public class SalaryInfo {
         builder.append("Report for period ")
                 .append(dateFrom).append(" - ")
                 .append(dateTo);
-        int salary = 0;
-        for (int j = 0; j < names.length; j++) {
-            for (int i = 0; i < data.length; i++) {
-                String[] datum = data[i].split(" ");
-                if (names[j].equals(datum[NAME])
-                        && (LocalDate.parse(datum[DATE], FORMATTER).isAfter(startDate)
-                        && LocalDate.parse(datum[DATE], FORMATTER).isBefore(endDate))) {
-                    salary = salary + Integer.parseInt(datum[HOUR]) * Integer.parseInt(datum[RATE]);
+
+        for (String name : names) {
+            int salary = 0;
+            for (String record : data) {
+                String[] datum = record.split(" ");
+                if (name.equals(datum[NAME_INDEX])
+                        && (LocalDate.parse(datum[DATE_INDEX], FORMATTER).isAfter(startDate)
+                        && LocalDate.parse(datum[DATE_INDEX], FORMATTER).isBefore(endDate))) {
+                    salary = salary + Integer.parseInt(datum[HOUR_INDEX]) * Integer.parseInt(datum[RATE_INDEX]);
                 }
             }
             builder.append(System.lineSeparator())
-                    .append(names[j])
+                    .append(name)
                     .append(" - ")
                     .append(salary);
-            salary = 0;
         }
         return builder.toString();
     }
