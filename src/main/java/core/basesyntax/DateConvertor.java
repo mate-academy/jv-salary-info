6cdd0b77b863812c1463015365873359dd74973b
Anatolii Bespalko <tolyan9901@gmail.com>
2022-09-27 12:22:15 +0300

    Add new constant DATE_ELEMENT, fix DateConvertor method

--- src/main/java/core/basesyntax/DateConvertor.java
+++ src/main/java/core/basesyntax/DateConvertor.java
@@ -7,7 +7,6 @@ public class DateConvertor {
     public static final DateTimeFormatter DT_FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public LocalDate convert(String strDate) {
-        LocalDate localDate = LocalDate.parse(strDate, DT_FORMATTER);
-        return localDate;
+        return LocalDate.parse(strDate, DT_FORMATTER);
     }
 }
