932c278b38124d79cde7f2cdce16fd14da938d11
EvgenyTyshchenko <evgeny.tyshchenko@gmail.com>
2022-09-27 13:44:01 +0300

    Varsion 2. Fixed remarks

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,47 +1,61 @@
 package core.basesyntax;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 public class SalaryInfo {
-    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        Date dateFromDate = new Date();
-        Date dateToDate = new Date();
-        Date date = new Date();
-        StringBuilder builder = new StringBuilder("Report for period ")
-                .append(dateFrom).append(" - ").append(dateTo);
-        try {
-            dateFromDate = new SimpleDateFormat("dd.MM.yyyy").parse(dateFrom);
-        } catch (ParseException e) {
-            System.out.println("Date from parsed error: " + dateFrom);
-        }
-        try {
-            dateToDate = new SimpleDateFormat("dd.MM.yyyy").parse(dateTo);
-        } catch (ParseException e) {
-            System.out.println("Date to parsed error: " + dateTo);
-        }
-        for (String name : names) {
-            builder.append(System.lineSeparator()).append(name).append(" - ");
-            int totalSallery = 0;
-            for (String dataRow : data) {
-                if (dataRow.isEmpty()) {
-                    continue;
-                }
-                String[] dataRowArray = dataRow.split(" ");
-                try {
-                    date = new SimpleDateFormat("dd.MM.yyyy").parse(dataRowArray[0]);
-                } catch (ParseException e) {
-                    System.out.println("Date parsed error: " + dataRowArray[0]);
-                }
-                if ((date.after(dateFromDate) || date.equals(dateFromDate))
-                        && (date.before(dateToDate) || date.equals(dateToDate))
-                        && dataRowArray[1].equals(name)) {
-                    totalSallery += Integer.parseInt(dataRowArray[2])
-                            * Integer.parseInt(dataRowArray[3]);
-                }
+    public static final String DATE_FORMAT = "d.MM.yyyy";
+    public static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_FORMAT);
+    public static final String DATA_SEPARATOR = " - ";
+    public static final String COLUMN_SEPARATOR = " ";
+    public static final String REPORT_HEADER = "Report for period ";
+    public static final int DATE_COLUMN_INDEX = 0;
+    public static final int NAME_COLUMN_INDEX = 1;
+    public static final int QOTY_COLUMN_INDEX = 2;
+    public static final int SUM_COLUMN_INDEX = 3;
+
+    private int getTotalSalaryByName(String[] data, String name,
+                                     LocalDate dateFromLocalDate, LocalDate dateToLocalDate) {
+        int totalSalary = 0;
+        for (String dataRow : data) {
+            if (dataRow.isEmpty()) {
+                continue;
             }
-            builder.append(totalSallery);
+            String[] dataRowArray = dataRow.split(COLUMN_SEPARATOR);
+            LocalDate date;
+            try {
+                date = LocalDate.parse(dataRowArray[DATE_COLUMN_INDEX], formatter);
+            } catch (DateTimeParseException e) {
+                throw new DateTimeParseException("Error paring date in data rows",
+                        e.getParsedString(), e.getErrorIndex());
+            }
+            if ((date.isAfter(dateFromLocalDate) || date.equals(dateFromLocalDate))
+                    && (date.isBefore(dateToLocalDate) || date.equals(dateToLocalDate))
+                    && dataRowArray[NAME_COLUMN_INDEX].equals(name)) {
+                totalSalary += Integer.parseInt(dataRowArray[QOTY_COLUMN_INDEX])
+                        * Integer.parseInt(dataRowArray[SUM_COLUMN_INDEX]);
+            }
+        }
+        return totalSalary;
+    }
+
+    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
+        LocalDate dateFromLocalDate;
+        LocalDate dateToLocalDate;
+        try {
+            dateFromLocalDate = LocalDate.parse(dateFrom, formatter);
+            dateToLocalDate = LocalDate.parse(dateTo, formatter);
+        } catch (DateTimeParseException e) {
+            throw new DateTimeParseException("Error paring input period",
+                    e.getParsedString(), e.getErrorIndex());
+        }
+
+        StringBuilder builder = new StringBuilder(REPORT_HEADER)
+                .append(dateFrom).append(DATA_SEPARATOR).append(dateTo);
+        for (String name : names) {
+            builder.append(System.lineSeparator()).append(name).append(DATA_SEPARATOR);
+            builder.append(getTotalSalaryByName(data, name, dateFromLocalDate, dateToLocalDate));
         }
         return builder.toString();
     }
