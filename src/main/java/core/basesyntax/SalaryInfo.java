74353baf60acad09591870e23bf6a22d997d2b0e
Volodymyr Oliinyk <volodymyr.oliinyk89@gmail.com>
2022-09-28 12:13:48 +0300

    added fixes after review

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,18 +1,34 @@
 package core.basesyntax;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    public static final int DATE_INDEX = 0;
+    public static final int HOURS_WORKED_INDEX = 2;
+    public static final int INCOME_PER_HOUR_INDEX = 3;
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
+        LocalDate dateFromFormat = parseDate(dateFrom);
+        LocalDate dateToFormat = parseDate(dateTo);
         StringBuilder result = new StringBuilder("Report for period ");
         result.append(dateFrom).append(" - ").append(dateTo).append(System.lineSeparator());
         for (String name : names) {
             result.append(name).append(" - ");
             int sum = 0;
             for (String info : data) {
-                if (info.contains(name) && dateValid(dateFrom, info.substring(0, 10), dateTo)) {
-                    sum += Integer.parseInt(info.split(" ")[2])
-                            * Integer.parseInt(info.split(" ")[3]);
+                if (!info.contains(name)) {
+                    continue;
+                } else {
+                    String[] infoSplit = info.split(" ");
+                    int hoursWorked = Integer.parseInt(infoSplit[HOURS_WORKED_INDEX]);
+                    int incomePerHour = Integer.parseInt(infoSplit[INCOME_PER_HOUR_INDEX]);
+                    LocalDate workedDate = parseDate(infoSplit[DATE_INDEX]);
+                    if (workedDate.isAfter(dateFromFormat) && workedDate.isBefore(dateToFormat)
+                            || workedDate.isEqual(dateFromFormat)
+                            || workedDate.isEqual(dateToFormat)) {
+                        sum += hoursWorked * incomePerHour;
+                    }
                 }
             }
             result.append(sum).append(System.lineSeparator());
@@ -20,17 +36,8 @@ public class SalaryInfo {
         return result.toString().trim();
     }
 
-    public static boolean dateValid(String dateFrom, String date, String dateTo) {
-        LocalDate dateNew = LocalDate.of(Integer.parseInt(date.substring(6)),
-                Integer.parseInt(date.substring(3, 5)),
-                Integer.parseInt(date.substring(0, 2)));
-        LocalDate dateFromNew = LocalDate.of(Integer.parseInt(dateFrom.substring(6)),
-                Integer.parseInt(dateFrom.substring(3, 5)),
-                Integer.parseInt(dateFrom.substring(0, 2)));
-        LocalDate dateToNew = LocalDate.of(Integer.parseInt(dateTo.substring(6)),
-                Integer.parseInt(dateTo.substring(3, 5)),
-                Integer.parseInt(dateTo.substring(0, 2)));
-        return (dateNew.isAfter(dateFromNew) && dateNew.isBefore(dateToNew))
-                || dateNew.isEqual(dateFromNew) || dateNew.isEqual(dateToNew);
+    public static LocalDate parseDate(String date) {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        return LocalDate.parse(date, dateTimeFormatter);
     }
 }
