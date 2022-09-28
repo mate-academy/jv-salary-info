8d0cd4859c50a63b9fd4e729693135309d6fef5f
EvgenyTyshchenko <evgeny.tyshchenko@gmail.com>
2022-09-28 12:53:50 +0300

    Varsion 3. Private constants, condition simplification, methods order and redundant try-catch blocks removed

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -2,18 +2,29 @@ package core.basesyntax;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
 
 public class SalaryInfo {
-    public static final String DATE_FORMAT = "d.MM.yyyy";
-    public static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_FORMAT);
-    public static final String DATA_SEPARATOR = " - ";
-    public static final String COLUMN_SEPARATOR = " ";
-    public static final String REPORT_HEADER = "Report for period ";
-    public static final int DATE_COLUMN_INDEX = 0;
-    public static final int NAME_COLUMN_INDEX = 1;
-    public static final int QOTY_COLUMN_INDEX = 2;
-    public static final int SUM_COLUMN_INDEX = 3;
+    private static final String DATE_FORMAT = "d.MM.yyyy";
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_FORMAT);
+    private static final String DATA_SEPARATOR = " - ";
+    private static final String COLUMN_SEPARATOR = " ";
+    private static final String REPORT_HEADER = "Report for period ";
+    private static final int DATE_COLUMN_INDEX = 0;
+    private static final int NAME_COLUMN_INDEX = 1;
+    private static final int QOTY_COLUMN_INDEX = 2;
+    private static final int SUM_COLUMN_INDEX = 3;
+
+    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
+        LocalDate dateFromLocalDate = LocalDate.parse(dateFrom, formatter);
+        LocalDate dateToLocalDate = LocalDate.parse(dateTo, formatter);
+        StringBuilder builder = new StringBuilder(REPORT_HEADER)
+                .append(dateFrom).append(DATA_SEPARATOR).append(dateTo);
+        for (String name : names) {
+            builder.append(System.lineSeparator()).append(name).append(DATA_SEPARATOR);
+            builder.append(getTotalSalaryByName(data, name, dateFromLocalDate, dateToLocalDate));
+        }
+        return builder.toString();
+    }
 
     private int getTotalSalaryByName(String[] data, String name,
                                      LocalDate dateFromLocalDate, LocalDate dateToLocalDate) {
@@ -23,40 +34,14 @@ public class SalaryInfo {
                 continue;
             }
             String[] dataRowArray = dataRow.split(COLUMN_SEPARATOR);
-            LocalDate date;
-            try {
-                date = LocalDate.parse(dataRowArray[DATE_COLUMN_INDEX], formatter);
-            } catch (DateTimeParseException e) {
-                throw new DateTimeParseException("Error paring date in data rows",
-                        e.getParsedString(), e.getErrorIndex());
-            }
-            if ((date.isAfter(dateFromLocalDate) || date.equals(dateFromLocalDate))
-                    && (date.isBefore(dateToLocalDate) || date.equals(dateToLocalDate))
+            LocalDate date = LocalDate.parse(dataRowArray[DATE_COLUMN_INDEX], formatter);
+            if ((date.isAfter(dateFromLocalDate) && (date.isBefore(dateToLocalDate))
+                    || date.equals(dateToLocalDate))
                     && dataRowArray[NAME_COLUMN_INDEX].equals(name)) {
                 totalSalary += Integer.parseInt(dataRowArray[QOTY_COLUMN_INDEX])
                         * Integer.parseInt(dataRowArray[SUM_COLUMN_INDEX]);
             }
         }
         return totalSalary;
-    }
-
-    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        LocalDate dateFromLocalDate;
-        LocalDate dateToLocalDate;
-        try {
-            dateFromLocalDate = LocalDate.parse(dateFrom, formatter);
-            dateToLocalDate = LocalDate.parse(dateTo, formatter);
-        } catch (DateTimeParseException e) {
-            throw new DateTimeParseException("Error paring input period",
-                    e.getParsedString(), e.getErrorIndex());
-        }
-
-        StringBuilder builder = new StringBuilder(REPORT_HEADER)
-                .append(dateFrom).append(DATA_SEPARATOR).append(dateTo);
-        for (String name : names) {
-            builder.append(System.lineSeparator()).append(name).append(DATA_SEPARATOR);
-            builder.append(getTotalSalaryByName(data, name, dateFromLocalDate, dateToLocalDate));
-        }
-        return builder.toString();
     }
 }
