f98e0bc69ed3286c9be8d12b9c7bbb453889b964
Alex Anofriev <alex.linkoln7391@gmail.com>
2022-09-28 13:43:44 +0300

    used Stringbuilder and loop to create returning report

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,17 +4,21 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-    private final int dateOfPay = 10;
-    private final int hoursWorked = 2;
-    private final int salaryPerHour = 3;
-    private final int dayToAdd = 1;
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int DATE_OF_PAY = 10;
+    private static final int HOURS_WORKED = 2;
+    private static final int SALARY_PER_HOUR = 3;
+    private static final int DAY_TO_ADD = 1;
+    private static final int START_OF_RANGE = 0;
+    private static final String SPACE = " ";
+    private static final String REPORT_MESSAGE = "Report for period ";
+    private static final String SEPARATE = " - ";
     private StringBuilder sb = new StringBuilder();
     private int salary = 0;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        LocalDate dateStart = LocalDate.parse(dateFrom, formatter);
-        LocalDate dateLast = LocalDate.parse(dateTo, formatter);
+        LocalDate dateStart = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate dateLast = LocalDate.parse(dateTo, FORMATTER);
         String[] resultToReport = new String[names.length];
 
         for (int i = 0; i < names.length; i++) {
@@ -22,31 +26,25 @@ public class SalaryInfo {
             for (String dataArr : data) {
                 if (dataArr.contains(names[i])) {
                     LocalDate currentDate = LocalDate
-                               .parse(dataArr.substring(0, dateOfPay), formatter);
-                    sb.delete(0, sb.length());
-                    if (dateLast.plusDays(dayToAdd).isAfter(currentDate)
-                            && dateStart.minusDays(dayToAdd).isBefore(currentDate)) {
-                        String[] arr = dataArr.split(" ");
-                        salary += Integer.parseInt(arr[hoursWorked])
-                                * Integer.parseInt(arr[salaryPerHour]);
-                        resultToReport[i] = sb.append(names[i])
-                                .append(" - ").append(salary).toString();
-                    } else {
-                        resultToReport[i] = sb.append(names[i])
-                                .append(" - ").append(salary).toString();
+                            .parse(dataArr.substring(START_OF_RANGE, DATE_OF_PAY), FORMATTER);
+                    sb.delete(START_OF_RANGE, sb.length());
+                    if (dateLast.plusDays(DAY_TO_ADD).isAfter(currentDate)
+                            && dateStart.minusDays(DAY_TO_ADD).isBefore(currentDate)) {
+                        String[] splittedData = dataArr.split(SPACE);
+                        salary += Integer.parseInt(splittedData[HOURS_WORKED])
+                                * Integer.parseInt(splittedData[SALARY_PER_HOUR]);
                     }
+                    resultToReport[i] = sb.append(names[i])
+                            .append(SEPARATE).append(salary).toString();
                 }
             }
         }
 
-        String report = "Report for period " + dateFrom + " - " + dateTo
-                + System.lineSeparator()
-                + resultToReport[0]
-                + System.lineSeparator()
-                + resultToReport[1]
-                + System.lineSeparator()
-                + resultToReport[2];
+        sb = new StringBuilder(REPORT_MESSAGE).append(dateFrom).append(SEPARATE).append(dateTo);
+        for (String result : resultToReport) {
+            sb.append(System.lineSeparator()).append(result);
+        }
 
-        return report;
+        return sb.toString();
     }
 }
