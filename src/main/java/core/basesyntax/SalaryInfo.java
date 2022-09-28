044499a5a6bbc0bca5f37f918cc2ff52ac0c39fb
alexeyukrainets <qaaq.com.ua@gmail.com>
2022-09-28 13:28:40 +0200

    Final version

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,10 +5,10 @@ import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-    private static final int INDEX_FOR_ARRAY_ZERO = 0;
-    private static final int INDEX_FOR_ARRAY_ONE = 1;
-    private static final int INDEX_FOR_ARRAY_TWO = 2;
-    private static final int INDEX_FOR_ARRAY_THREE = 3;
+    private static final int DATE_INDEX = 0;
+    private static final int DATE_INDEX1 = 1;
+    private static final int DATE_INDEX2 = 2;
+    private static final int DATE_INDEX3 = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate localDateFrom = LocalDate.parse(dateFrom, FORMATTER);
@@ -20,16 +20,16 @@ public class SalaryInfo {
             int salary = 0;
             for (String line: data) {
                 arrayOfData = line.split(" ");
-                LocalDate currentDate = LocalDate.parse(arrayOfData[INDEX_FOR_ARRAY_ZERO],
+                LocalDate currentDate = LocalDate.parse(arrayOfData[DATE_INDEX],
                         FORMATTER);
                 if (localDateFrom.compareTo(currentDate) <= 0
                         && localDateTo.compareTo(currentDate) >= 0
-                        && arrayOfData[INDEX_FOR_ARRAY_ONE].equals(name)) {
-                    salary += (Integer.parseInt(arrayOfData[INDEX_FOR_ARRAY_THREE])
-                            * Integer.parseInt(arrayOfData[INDEX_FOR_ARRAY_TWO]));
+                        && arrayOfData[DATE_INDEX1].equals(name)) {
+                    salary += (Integer.parseInt(arrayOfData[DATE_INDEX3])
+                            * Integer.parseInt(arrayOfData[DATE_INDEX2]));
                 }
             }
-            result.append('\n').append(name).append(" - ").append(salary);
+            result.append(System.lineSeparator()).append(name).append(" - ").append(salary);
         }
         return result.toString();
     }
