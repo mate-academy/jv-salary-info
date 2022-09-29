66c6ea87bc2080068ae7c46aeb26998575cd9feb
Anton Pavliuk <anton.pavliuk20@gmail.com>
2022-09-29 22:35:14 +0300

    Some changes according to Common mistakes

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,41 +1,37 @@
 package core.basesyntax;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int INFO_FORMAT_INDEX_OF_NAME = 0;
+    private static final int INFO_FORMAT_INDEX_OF_WORKING_HOURS = 2;
+    private static final int INFO_FORMAT_INDEX_OF_PAYMENT = 3;
+    private static final String INFO_FORMAT_DEFAULT_SPLITTER = " ";
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder sb = new StringBuilder();
         sb.append("Report for period " + dateFrom + " - " + dateTo);
 
-        SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
-        Date beginDate;
-        Date endDate;
-        try {
-            beginDate = dateFormat.parse(dateFrom);
-            endDate = dateFormat.parse(dateTo);
-        } catch (ParseException e) {
-            throw new RuntimeException("Wrong date format in argument dateFrom or dateTo", e);
-        }
+        LocalDate beginDate;
+        LocalDate endDate;
+        beginDate = LocalDate.parse(dateFrom, DATE_FORMAT);
+        endDate = LocalDate.parse(dateTo, DATE_FORMAT);
 
         for (String name : names) {
             sb.append(System.lineSeparator());
             int salary = 0;
             for (String s : data) {
-                String[] info = s.split(" ");
+                String[] info = s.split(INFO_FORMAT_DEFAULT_SPLITTER);
 
-                Date workDay;
-                try {
-                    workDay = dateFormat.parse(info[0]);
-                } catch (ParseException e) {
-                    throw new RuntimeException("Wrong date format in data array", e);
-                }
+                LocalDate workDay;
+                workDay = LocalDate.parse(info[INFO_FORMAT_INDEX_OF_NAME], DATE_FORMAT);
 
                 if (info[1].equals(name)
                         && (workDay.compareTo(beginDate) >= 0 && workDay.compareTo(endDate) <= 0)) {
-                    int hours = Integer.parseInt(info[2]);
-                    int paymentPerHour = Integer.parseInt(info[3]);
+                    int hours = Integer.parseInt(info[INFO_FORMAT_INDEX_OF_WORKING_HOURS]);
+                    int paymentPerHour = Integer.parseInt(info[INFO_FORMAT_INDEX_OF_PAYMENT]);
                     salary += hours * paymentPerHour;
                 }
             }
