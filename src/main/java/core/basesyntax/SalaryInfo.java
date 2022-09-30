d800a9f38a3bd15e68d23a76ef8d8ca6946eaafd
Anton Pavliuk <anton.pavliuk20@gmail.com>
2022-09-30 21:20:58 +0300

    Im a bit confused. This is the last moment when everything works.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -8,35 +8,59 @@ public class SalaryInfo {
     private static final int INFO_FORMAT_INDEX_OF_NAME = 0;
     private static final int INFO_FORMAT_INDEX_OF_WORKING_HOURS = 2;
     private static final int INFO_FORMAT_INDEX_OF_PAYMENT = 3;
+    private static final int INFO_FORMAT_INDEX_OF_ARRAY_NAME = 1;
     private static final String INFO_FORMAT_DEFAULT_SPLITTER = " ";
+    private static final String INFO_FORMAT_DATE_SPLITTER = " - ";
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        StringBuilder sb = new StringBuilder();
-        sb.append("Report for period " + dateFrom + " - " + dateTo);
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Report for period " + dateFrom + INFO_FORMAT_DATE_SPLITTER + dateTo);
 
+
+
+        for (String name : names) {
+            stringBuilder.append(findInfoAboutName(name, data, dateFrom, dateTo));
+        }
+        return stringBuilder.toString();
+    }
+
+    public String findInfoAboutName(String name, String[] data, String dateFrom, String dateTo) {
+
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        stringBuilder.append(System.lineSeparator());
+
+
+        stringBuilder.append(getSalaryInfoByDate(name, data, dateFrom, dateTo));
+
+        return stringBuilder.toString();
+    }
+
+    private String getSalaryInfoByDate(String name, String[] data, String dateFrom, String dateTo) {
         LocalDate beginDate;
         LocalDate endDate;
         beginDate = LocalDate.parse(dateFrom, DATE_FORMAT);
         endDate = LocalDate.parse(dateTo, DATE_FORMAT);
 
-        for (String name : names) {
-            sb.append(System.lineSeparator());
-            int salary = 0;
-            for (String s : data) {
-                String[] info = s.split(INFO_FORMAT_DEFAULT_SPLITTER);
+        int salary = 0;
+        for (String s : data) {
+            String[] info = s.split(INFO_FORMAT_DEFAULT_SPLITTER);
 
-                LocalDate workDay;
-                workDay = LocalDate.parse(info[INFO_FORMAT_INDEX_OF_NAME], DATE_FORMAT);
+            LocalDate workDay;
+            workDay = LocalDate.parse(info[INFO_FORMAT_INDEX_OF_NAME], DATE_FORMAT);
 
-                if (info[1].equals(name)
-                        && (workDay.compareTo(beginDate) >= 0 && workDay.compareTo(endDate) <= 0)) {
-                    int hours = Integer.parseInt(info[INFO_FORMAT_INDEX_OF_WORKING_HOURS]);
-                    int paymentPerHour = Integer.parseInt(info[INFO_FORMAT_INDEX_OF_PAYMENT]);
-                    salary += hours * paymentPerHour;
-                }
+            if (info[INFO_FORMAT_INDEX_OF_ARRAY_NAME].equals(name)
+                    && (workDay.compareTo(beginDate) >= 0 && workDay.compareTo(endDate) <= 0)) {
+                salary += getSalaryFromDataLine(info);
             }
-            sb.append(name + " - " + salary);
         }
-        return sb.toString();
+        return name + INFO_FORMAT_DATE_SPLITTER + salary;
+    }
+
+    private int getSalaryFromDataLine(String[] info) {
+        int hours = Integer.parseInt(info[INFO_FORMAT_INDEX_OF_WORKING_HOURS]);
+        int paymentPerHour = Integer.parseInt(info[INFO_FORMAT_INDEX_OF_PAYMENT]);
+        return hours * paymentPerHour;
     }
 }
