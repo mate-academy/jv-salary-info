0f16899e8b86901412c6cc6490bcbc4f229373d7
Anton Pavliuk <anton.pavliuk20@gmail.com>
2022-09-30 21:42:41 +0300

    Changes after mentor review
    1. Correct abbreaviation in StringBuilder() name
    
    2. Create constants for name index from array, and for string " - "
    
    3. Divide program logic

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -16,8 +16,6 @@ public class SalaryInfo {
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append("Report for period " + dateFrom + INFO_FORMAT_DATE_SPLITTER + dateTo);
 
-
-
         for (String name : names) {
             stringBuilder.append(findInfoAboutName(name, data, dateFrom, dateTo));
         }
@@ -25,40 +23,32 @@ public class SalaryInfo {
     }
 
     public String findInfoAboutName(String name, String[] data, String dateFrom, String dateTo) {
-
-
-        StringBuilder stringBuilder = new StringBuilder();
-
-        stringBuilder.append(System.lineSeparator());
-
-
-        stringBuilder.append(getSalaryInfoByDate(name, data, dateFrom, dateTo));
-
-        return stringBuilder.toString();
-    }
-
-    private String getSalaryInfoByDate(String name, String[] data, String dateFrom, String dateTo) {
-        LocalDate beginDate;
-        LocalDate endDate;
-        beginDate = LocalDate.parse(dateFrom, DATE_FORMAT);
-        endDate = LocalDate.parse(dateTo, DATE_FORMAT);
-
         int salary = 0;
         for (String s : data) {
-            String[] info = s.split(INFO_FORMAT_DEFAULT_SPLITTER);
-
-            LocalDate workDay;
-            workDay = LocalDate.parse(info[INFO_FORMAT_INDEX_OF_NAME], DATE_FORMAT);
-
-            if (info[INFO_FORMAT_INDEX_OF_ARRAY_NAME].equals(name)
-                    && (workDay.compareTo(beginDate) >= 0 && workDay.compareTo(endDate) <= 0)) {
-                salary += getSalaryFromDataLine(info);
-            }
+            salary += getSalaryForName(name, s, dateFrom, dateTo);
         }
-        return name + INFO_FORMAT_DATE_SPLITTER + salary;
+        return System.lineSeparator() + name + INFO_FORMAT_DATE_SPLITTER + salary;
     }
 
-    private int getSalaryFromDataLine(String[] info) {
+    private int getSalaryForName(String name, String dataLine, String dateFrom, String dateTo) {
+        LocalDate beginDate = LocalDate.parse(dateFrom, DATE_FORMAT);
+        LocalDate endDate = LocalDate.parse(dateTo, DATE_FORMAT);
+        int salary = 0;
+
+        String[] line = dataLine.split(INFO_FORMAT_DEFAULT_SPLITTER);
+
+        LocalDate workDay;
+        workDay = LocalDate.parse(line[INFO_FORMAT_INDEX_OF_NAME], DATE_FORMAT);
+
+        if (line[INFO_FORMAT_INDEX_OF_ARRAY_NAME].equals(name)
+                && (workDay.compareTo(beginDate) >= 0 && workDay.compareTo(endDate) <= 0)) {
+            salary = getSalaryFromLine(line);
+        }
+
+        return salary;
+    }
+
+    private int getSalaryFromLine(String[] info) {
         int hours = Integer.parseInt(info[INFO_FORMAT_INDEX_OF_WORKING_HOURS]);
         int paymentPerHour = Integer.parseInt(info[INFO_FORMAT_INDEX_OF_PAYMENT]);
         return hours * paymentPerHour;
