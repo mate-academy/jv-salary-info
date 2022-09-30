767b9a2db714fe8a90809653f4e3cf67c363889c
Nazarii Pertsak <pertsak98@gmail.com>
2022-09-30 13:09:17 +0300

    implemented getSalaryInfo method

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,35 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        LocalDate fromDate = LocalDate.parse(dateFrom, formatter);
+        LocalDate toDate = LocalDate.parse(dateTo, formatter);
+        StringBuilder stringBuilder = new StringBuilder("Report for period ");
+        stringBuilder.append(dateFrom)
+                .append(" - ")
+                .append(dateTo);
+        for (String name : names) {
+            int salary = 0;
+            for (int i = 0; i < data.length; i++) {
+                String[] slrInfo = data[i].split(" ");
+                LocalDate slrDate = LocalDate.parse(slrInfo[0], formatter);
+                int hoursOfWork = Integer.parseInt(slrInfo[2]);
+                int salaryPerHour = Integer.parseInt(slrInfo[3]);
+                if (name.equals(slrInfo[1])
+                        && !slrDate.isAfter(toDate) && !slrDate.isBefore(fromDate)) {
+                    salary += hoursOfWork * salaryPerHour;
+                }
+            }
+            stringBuilder.append(System.lineSeparator())
+                    .append(name)
+                    .append(" - ")
+                    .append(salary);
+        }
+        return stringBuilder.toString();
     }
 }
