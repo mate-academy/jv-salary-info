3768e36f32e1f93d04aee7e4ef368930f9c419e7
Bodddd <skrypnikn@gmail.com>
2022-09-30 18:43:25 +0300

    combine conditionals and public->private

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,19 +5,19 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
 public class SalaryInfo {
-    public static final String DATE_FORM = "d.M.yyyy";
-    public static final int DATE_INDEX = 0;
-    public static final int NAME_INDEX = 1;
-    public static final int COUNT_SALARY_INDEX = 2;
-    public static final int SALARY_INDEX = 3;
-    private final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(DATE_FORM);
+    private static final String DATE_FORM = "d.M.yyyy";
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int COUNT_SALARY_INDEX = 2;
+    private static final int SALARY_INDEX = 3;
+    private static final DateTimeFormatter dateTimeForm = DateTimeFormatter.ofPattern(DATE_FORM);
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate from;
         LocalDate to;
         try {
-            from = LocalDate.parse(dateFrom, dateTimeFormatter);
-            to = LocalDate.parse(dateTo, dateTimeFormatter);
+            from = LocalDate.parse(dateFrom, dateTimeForm);
+            to = LocalDate.parse(dateTo, dateTimeForm);
             if (from.isAfter(to)) {
                 throw new DateTimeErrorPosition("Date from is bigger than date to");
             }
@@ -42,11 +42,9 @@ public class SalaryInfo {
         int salary = 0;
         for (String employerDate : date) {
             String[] lineDateArray = employerDate.split(" ");
-            if (!name.equals(lineDateArray[NAME_INDEX])) {
-                continue;
-            }
-            LocalDate dateFromArray = LocalDate.parse(lineDateArray[DATE_INDEX], dateTimeFormatter);
-            if (dateFromArray.isBefore(dateFrom) || dateFromArray.isAfter(dateTo)) {
+            LocalDate dateFromArray = LocalDate.parse(lineDateArray[DATE_INDEX], dateTimeForm);
+            if (!name.equals(lineDateArray[NAME_INDEX]) || dateFromArray.isBefore(dateFrom)
+                    || dateFromArray.isAfter(dateTo)) {
                 continue;
             }
             int count = Integer.parseInt(lineDateArray[COUNT_SALARY_INDEX]);
