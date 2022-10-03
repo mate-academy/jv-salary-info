85207d68a4bd17af610ebc1b3a0294881d52f40b
Yura Hryniv <superyura444@gmail.com>
2022-10-03 12:21:33 +0300

    Added constants and used properly System.lineSeparator()

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -6,31 +6,34 @@ import java.time.format.DateTimeFormatter;
 public class SalaryInfo {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
     private static final DataChecker dataChecker = new DataChecker();
+    private static final String DATA_SEPARATOR = " ";
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        LocalDate formattedDateFrom = LocalDate.parse(dateFrom, FORMATTER);
-        LocalDate formattedDateTo = LocalDate.parse(dateTo, FORMATTER);
+        final LocalDate formattedDateFrom = LocalDate.parse(dateFrom, FORMATTER);
+        final LocalDate formattedDateTo = LocalDate.parse(dateTo, FORMATTER);
         StringBuilder stringBuilder = new StringBuilder();
 
-        stringBuilder.append("Report for period " + dateFrom + " - "
-                + dateTo + System.lineSeparator());
+        stringBuilder.append("Report for period " + dateFrom + " - " + dateTo);
 
         int sum;
         for (int i = 0; i < names.length; i++) {
             sum = 0;
             for (int j = 0; j < data.length; j++) {
                 if (data[j].contains(names[i])) {
-                    String [] splitedData = data[j].split(" ");
-                    LocalDate localDate = LocalDate.parse(splitedData[0], FORMATTER);
-                    if (dataChecker.isWithinRange(formattedDateFrom,localDate,formattedDateTo)) {
-                        sum += Integer.parseInt(splitedData[splitedData.length - 1])
-                                * Integer.parseInt(splitedData[splitedData.length - 2]);
-                    }
 
+                    final String [] splitedData = data[j].split(DATA_SEPARATOR);
+                    final String zero_data_index = splitedData[0];
+                    final LocalDate localDate = LocalDate.parse(zero_data_index, FORMATTER);
+                    final int workingHour = Integer.parseInt(splitedData[splitedData.length - 1]);
+                    final int incomePerHour = Integer.parseInt(splitedData[splitedData.length - 2]);
+
+                    if (dataChecker.isWithinRange(formattedDateFrom,localDate,formattedDateTo)) {
+                        sum += workingHour * incomePerHour;
+                    }
                 }
             }
-            stringBuilder.append(names[i] + " - " + sum + System.lineSeparator());
+            stringBuilder.append(System.lineSeparator() + names[i] + " - " + sum);
         }
-        return stringBuilder.toString().trim();
+        return stringBuilder.toString();
     }
 }
