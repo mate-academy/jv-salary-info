08f66eddd5802580fa27f0cf785ae43df62077af
Oksana Rudenko <0ksaha_rudenko@ukr.net>
2022-10-03 22:35:30 +0300

    jv-salary-info solution rename constants

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,11 +5,10 @@ import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-    private static final int ZERO_INDEX = 0;
-    private static final int FIRST_INDEX = 1;
-    private static final int SECOND_INDEX = 2;
-    private static final int THIRD_INDEX = 3;
-    private static final int DAYS_BOUNDS = 1;
+    private static final int WORKING_DAY_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int WORKING_HOURS_INDEX = 2;
+    private static final int HOUR_WAGE_INDEX = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate parseDateFrom = LocalDate.parse(dateFrom, FORMATTER);
@@ -22,17 +21,17 @@ public class SalaryInfo {
             int total = 0;
             for (String line : data) {
                 String[] lines = line.split(" ");
-                workingDay = LocalDate.parse(lines[ZERO_INDEX], FORMATTER);
+                workingDay = LocalDate.parse(lines[WORKING_DAY_INDEX], FORMATTER);
                 int salary = 0;
-                if (name.equals(lines[FIRST_INDEX])
-                        && parseDateFrom.minusDays(DAYS_BOUNDS).isBefore(workingDay)
-                        && workingDay.isBefore(parseDateTo.plusDays(DAYS_BOUNDS))) {
-                    salary = Integer.parseInt(lines[SECOND_INDEX])
-                            * Integer.parseInt(lines[THIRD_INDEX]);
+                if (name.equals(lines[NAME_INDEX])
+                        && parseDateFrom.isBefore(workingDay) | parseDateFrom.isEqual(workingDay)
+                        && workingDay.isBefore(parseDateTo) | workingDay.isEqual(parseDateTo)) {
+                    salary = Integer.parseInt(lines[WORKING_HOURS_INDEX])
+                            * Integer.parseInt(lines[HOUR_WAGE_INDEX]);
                 }
                 total = total + salary;
             }
-            builder.append(String.valueOf(total));
+            builder.append(total);
         }
         return builder.toString();
     }
--- src/test/java/core/basesyntax/SalaryInfoTest.java
+++ src/test/java/core/basesyntax/SalaryInfoTest.java
@@ -82,7 +82,8 @@ public class SalaryInfoTest {
     @Test
     public void getSalaryInfoByMonth() {
         SalaryInfo salary = new SalaryInfo();
-        for (int i = 0; i < dates.length; i++) {String actualResult = salary.getSalaryInfo(sampleNames, scriptArray, dates[0], dates[i]);
+        for (int i = 0; i < dates.length; i++) {
+            String actualResult = salary.getSalaryInfo(sampleNames, scriptArray, dates[0], dates[i]);
             String expectedResult = SalaryInfoTest.exceptedReports[i];
 
             Assert.assertEquals(
