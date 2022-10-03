1bedcc51ffe7115b776bb7492648ba63de214eba
Yura Hryniv <superyura444@gmail.com>
2022-10-03 13:43:29 +0300

    Used constants properly this time

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -7,6 +7,9 @@ public class SalaryInfo {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
     private static final DataChecker dataChecker = new DataChecker();
     private static final String DATA_SEPARATOR = " ";
+    private static final int DATE_INDEX = 0;
+    private static final int WORKING_HOURS_INDEX = 1;
+    private static final int INCOME_PER_HOUR_INDEX = 1;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         final LocalDate formattedDateFrom = LocalDate.parse(dateFrom, FORMATTER);
@@ -20,15 +23,14 @@ public class SalaryInfo {
             sum = 0;
             for (int j = 0; j < data.length; j++) {
                 if (data[j].contains(names[i])) {
-
-                    final String [] splitedData = data[j].split(DATA_SEPARATOR);
-                    final String zero_data_index = splitedData[0];
-                    final LocalDate localDate = LocalDate.parse(zero_data_index, FORMATTER);
-                    final int workingHour = Integer.parseInt(splitedData[splitedData.length - 1]);
-                    final int incomePerHour = Integer.parseInt(splitedData[splitedData.length - 2]);
+                    String [] splitedData = data[j].split(DATA_SEPARATOR);
+                    String targetDateStr = splitedData[DATE_INDEX];
+                    LocalDate localDate = LocalDate.parse(targetDateStr, FORMATTER);
+                    int workingHours = Integer.parseInt(splitedData[splitedData.length - WORKING_HOURS_INDEX]);
+                    int incomePerHour = Integer.parseInt(splitedData[splitedData.length - INCOME_PER_HOUR_INDEX]);
 
                     if (dataChecker.isWithinRange(formattedDateFrom,localDate,formattedDateTo)) {
-                        sum += workingHour * incomePerHour;
+                        sum += workingHours * incomePerHour;
                     }
                 }
             }
