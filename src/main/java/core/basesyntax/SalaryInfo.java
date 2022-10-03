51b607ca0c43eca3e3f117de4d4bb2713a665a10
Zoreslav Todorenko <wzxzzx@gmail.com>
2022-10-03 23:01:24 +0300

    Redundant loops was redundant

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,57 +4,38 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    public static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-    // create formatter
+    public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+    public static final int DATA_INDEX = 0;
+    public static final int NAME_INDEX = 1;
+    public static final int HOUR_INDEX = 2;
+    public static final int PAY_INDEX = 3;
+    public static final int EXTRA_DAY = 1;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        LocalDate dateF = LocalDate.parse(dateFrom.replace(".", "-"), formatter);
-        LocalDate dateT = LocalDate.parse(dateTo.replace(".", "-"), formatter);
-        // parse string "dateFrom" and "datwTo" to LocalDate format
-
-        LocalDate[] dateArray = new LocalDate[data.length];
-        String[] nameArray = new String[data.length];
-        int[] hoursArray = new int[data.length];
-        int [] paymentArray = new int[data.length];
-        String[] totalArray;
-        //  Сreate arrays for the future division of the "date array" into components
-
-        for (int i = 0; i < data.length; i++) {
-            totalArray = data[i].split(" ");
-            for (int j = 0; j < 4; j++) {
-                dateArray[i] = LocalDate.parse(totalArray[0].replace(".", "-"), formatter);
-                nameArray[i] = totalArray[1];
-                hoursArray[i] = Integer.parseInt(totalArray[2]);
-                paymentArray[i] = Integer.parseInt(totalArray[3]);
-            }
-        }
-        // Divide "date array" into components
-
-        int [] totalSalary = new int[names.length];
-        for (int i = 0; i < names.length; i++) {
-            for (int j = 0; j < data.length; j++) {
-                if (names[i].equals(nameArray[j]) && (dateArray[j].isAfter(dateF)
-                        && dateArray[j].isBefore(dateT.plusDays(1)))) {
-                    totalSalary[i] += hoursArray[j] * paymentArray[j];
+        LocalDate dateF = LocalDate.parse(dateFrom.replace(".", "-"), FORMATTER);
+        LocalDate dateT = LocalDate.parse(dateTo.replace(".", "-"), FORMATTER);
+        int totalSalary;
+        String[] array;
+        String result = "";
+        for (String name : names) {
+            totalSalary = 0;
+            for (String datum : data) {
+                array = datum.split(" ");
+                if (name.equals(array[NAME_INDEX])
+                        && (LocalDate.parse(array[DATA_INDEX].replace(".", "-"), FORMATTER)
+                        .isAfter(dateF)
+                        && LocalDate.parse(array[DATA_INDEX].replace(".", "-"), FORMATTER)
+                        .isBefore(dateT.plusDays(EXTRA_DAY)))) {
+                    totalSalary += Integer.parseInt(array[HOUR_INDEX])
+                            * Integer.parseInt(array[PAY_INDEX]);
                 }
             }
-        }
-        // Check name & data and calculate total salary for period
-
-        String result = "";
-        for (int i = 0; i < names.length; i++) {
             StringBuilder builder = new StringBuilder();
-            builder.append(names[i]).append(" - ").append(totalSalary[i]);
+            builder.append(name).append(" - ").append(totalSalary);
             result += builder.toString() + System.lineSeparator();
         }
-// Convert data to general string
-
         return "Report for period " + dateFrom + " - " + dateTo
                 + System.lineSeparator()
                 + result.trim();
     }
 }
-
-
-
-
