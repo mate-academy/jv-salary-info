ad58dd1b468cf6adf87f013d5d1b189ebe4a7fe7
Pavlo.Polovyi <pasha32662@gmail.com>
2022-10-03 17:59:07 +0300

    Made constant field CONVERTER with DateConverter, made constant field SYSTEM_LINE_SEPARATOR with line separator.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,16 +4,18 @@ import java.time.LocalDate;
 import java.util.stream.Stream;
 
 public class SalaryInfo {
-    private static final String DATE_PATTERN = "d.M.y";
     private static final int DATE_INDEX = 0;
     private static final int HOURS_INDEX = 2;
     private static final int SALARY_INDEX = 3;
+    private static final String DATE_PATTERN = "d.M.y";
     private static final String REPORT_DELIMITER = " - ";
     private static final String REPORT_HEADING = "Report for period ";
+    private static final String SYSTEM_LINE_SEPARATOR = System.lineSeparator();
+    private static final DateConverter CONVERTER = new DateConverter(DATE_PATTERN);
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder builder = new StringBuilder(REPORT_HEADING
-                + dateFrom + REPORT_DELIMITER + dateTo + System.lineSeparator());
+                + dateFrom + REPORT_DELIMITER + dateTo + SYSTEM_LINE_SEPARATOR);
         for (String name : names) {
             builder.append(getIndividualReport(name, data, dateFrom, dateTo));
         }
@@ -22,23 +24,22 @@ public class SalaryInfo {
 
     private StringBuilder getIndividualReport(String name, String[] data,
                                               String dateFrom, String dateTo) {
-        DateConverter converter = new DateConverter(DATE_PATTERN);
-        LocalDate from = converter.stringDateToLocale(dateFrom);
-        LocalDate to = converter.stringDateToLocale(dateTo);
+        LocalDate from = CONVERTER.stringDateToLocale(dateFrom);
+        LocalDate to = CONVERTER.stringDateToLocale(dateTo);
         StringBuilder builder = new StringBuilder(name + REPORT_DELIMITER);
-        int salaryForPeriod = getSalaryForPeriod(name, data, from, to, converter);
+        int salaryForPeriod = getSalaryForPeriod(name, data, from, to);
         builder.append(salaryForPeriod)
-                .append(System.lineSeparator());
+                .append(SYSTEM_LINE_SEPARATOR);
         return builder;
     }
 
     private int getSalaryForPeriod(String name, String[] data, LocalDate from,
-                                   LocalDate to, DateConverter converter) {
+                                   LocalDate to) {
         return Stream.of(data)
                 .filter(x -> x.contains(name))
                 .mapToInt(x -> {
                     String[] tmp = x.split(" ");
-                    LocalDate tempDate = converter.stringDateToLocale(tmp[DATE_INDEX]);
+                    LocalDate tempDate = CONVERTER.stringDateToLocale(tmp[DATE_INDEX]);
                     if ((tempDate.isAfter(from) || tempDate.equals(from))
                             && (tempDate.isBefore(to) || tempDate.equals(to))) {
                         return Integer.parseInt(tmp[HOURS_INDEX])
