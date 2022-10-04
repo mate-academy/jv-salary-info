2d843524368fd056e84f7a55a11786359f103b62
thebitler <yurchyknikita@gmail.com>
2022-10-04 16:32:24 +0200

    Fixed typo

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -10,10 +10,10 @@ public class SalaryInfo {
     private static final int SALARY_INDEX = 2;
     private static final int SALARY_MULTIPLIER_INDEX = 3;
     private static final String DATA_SEPARATOR = " ";
-    private static final String LOG_INFO_DELIMETER = " - ";
+    private static final String LOG_INFO_DELIMITER = " - ";
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        StringBuilder appender = new StringBuilder("Report for period ").append(dateFrom).append(LOG_INFO_DELIMETER).append(dateTo);
+        StringBuilder appender = new StringBuilder("Report for period ").append(dateFrom).append(LOG_INFO_DELIMITER).append(dateTo);
         LocalDate dateFromParsed = LocalDate.parse(dateFrom, DATE_TIME_FORMATTER);
         LocalDate dateToParsed = LocalDate.parse(dateTo, DATE_TIME_FORMATTER);
         for (String name : names) {
@@ -32,7 +32,7 @@ public class SalaryInfo {
                 }
                 sum += Integer.parseInt(info[SALARY_INDEX]) * Integer.parseInt(info[SALARY_MULTIPLIER_INDEX]);
             }
-            appender.append(System.lineSeparator()).append(name).append(LOG_INFO_DELIMETER).append(sum);
+            appender.append(System.lineSeparator()).append(name).append(LOG_INFO_DELIMITER).append(sum);
         }
         return appender.toString();
     }
