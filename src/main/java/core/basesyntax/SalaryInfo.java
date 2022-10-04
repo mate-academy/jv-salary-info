e6017de9de45a61754cb78c4c5796c94e5015df9
yehbreus <yeh.breus@gmail.com>
2022-10-04 11:37:01 +0300

    fixed some issues

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -3,41 +3,45 @@ package core.basesyntax;
 import java.time.LocalDate;
 
 public class SalaryInfo {
-    private static final int FIRST_ARRAY_ELEMENT = 0;
-    private static final int SECOND_ARRAY_ELEMENT = 1;
-    private static final int THIRD_ARRAY_ELEMENT = 2;
-    private static final int FOURTH_ARRAY_ELEMENT = 3;
+    private static final int DATE_INDEX = 0;
+    private static final int YEAR_INDEX = 2;
+    private static final int MONTH_INDEX = 1;
+    private static final int DAY_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int WORK_HOURS_INDEX = 2;
+    private static final int SALARY_PER_HOUR = 3;
     private static final int ONE_DAY = 1;
+    private static final String LINE_SEPARATOR = " ";
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        StringBuilder sb = new StringBuilder("Report for period " + dateFrom + " - " + dateTo);
+        StringBuilder result = new StringBuilder("Report for period " + dateFrom + " - " + dateTo);
         String[] splitDateFrom = dateFrom.split("\\.");
         String[] splitDateTo = dateTo.split("\\.");
-        LocalDate dateBefore = LocalDate.of(Integer.parseInt(splitDateFrom[THIRD_ARRAY_ELEMENT]),
-                Integer.parseInt(splitDateFrom[SECOND_ARRAY_ELEMENT]),
-                Integer.parseInt(splitDateFrom[FIRST_ARRAY_ELEMENT]));
-        LocalDate dateAfter = LocalDate.of(Integer.parseInt(splitDateTo[THIRD_ARRAY_ELEMENT]),
-                Integer.parseInt(splitDateTo[SECOND_ARRAY_ELEMENT]),
-                Integer.parseInt(splitDateTo[FIRST_ARRAY_ELEMENT]));
+        LocalDate dateBefore = LocalDate.of(Integer.parseInt(splitDateFrom[YEAR_INDEX]),
+                Integer.parseInt(splitDateFrom[MONTH_INDEX]),
+                Integer.parseInt(splitDateFrom[DAY_INDEX]));
+        LocalDate dateAfter = LocalDate.of(Integer.parseInt(splitDateTo[YEAR_INDEX]),
+                Integer.parseInt(splitDateTo[MONTH_INDEX]),
+                Integer.parseInt(splitDateTo[DAY_INDEX]));
 
-        for (String name:names) {
+        for (String name : names) {
             int sumSalary = 0;
             for (String dataLine:data) {
-                String[] dataLineParts = dataLine.split(" ");
-                String[] dataLinePartWithDate = dataLineParts[FIRST_ARRAY_ELEMENT].split("\\.");
+                String[] dataLineParts = dataLine.split(LINE_SEPARATOR);
+                String[] dataLinePartWithDate = dataLineParts[DATE_INDEX].split("\\.");
                 LocalDate workDate = LocalDate.of(Integer
-                        .parseInt(dataLinePartWithDate[THIRD_ARRAY_ELEMENT]),
-                        Integer.parseInt(dataLinePartWithDate[SECOND_ARRAY_ELEMENT]),
-                        Integer.parseInt(dataLinePartWithDate[FIRST_ARRAY_ELEMENT]));
-                if (dataLineParts[SECOND_ARRAY_ELEMENT].equals(name)
+                        .parseInt(dataLinePartWithDate[YEAR_INDEX]),
+                        Integer.parseInt(dataLinePartWithDate[MONTH_INDEX]),
+                        Integer.parseInt(dataLinePartWithDate[DAY_INDEX]));
+                if (dataLineParts[NAME_INDEX].equals(name)
                         && workDate.isAfter(dateBefore)
                         && workDate.isBefore(dateAfter.plusDays(ONE_DAY))) {
-                    sumSalary = sumSalary + Integer.parseInt(dataLineParts[THIRD_ARRAY_ELEMENT])
-                            * Integer.parseInt(dataLineParts[FOURTH_ARRAY_ELEMENT]);
+                    sumSalary = sumSalary + Integer.parseInt(dataLineParts[WORK_HOURS_INDEX])
+                            * Integer.parseInt(dataLineParts[SALARY_PER_HOUR]);
                 }
             }
-            sb.append(System.lineSeparator()).append(name).append(" - ").append(sumSalary);
+            result.append(System.lineSeparator()).append(name).append(" - ").append(sumSalary);
         }
-        return sb.toString();
+        return result.toString();
     }
 }
