44a9a67cd7c15af35d0951c027815683903102d3
thebitler <yurchyknikita@gmail.com>
2022-10-04 16:24:41 +0200

    Removed empty lines

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -10,35 +10,29 @@ public class SalaryInfo {
     public static final int SALARY_INDEX = 2;
     public static final int SALARY_MULTIPLIER_INDEX = 3;
     public static final String DATA_SEPARATOR = " ";
+    public static final String LOG_INFO_DELIMETER = " - ";
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        StringBuilder appender = new StringBuilder("Report for period ").append(dateFrom).append(" - ").append(dateTo);
+        StringBuilder appender = new StringBuilder("Report for period ").append(dateFrom).append(LOG_INFO_DELIMETER).append(dateTo);
         LocalDate dateFromParsed = LocalDate.parse(dateFrom, DATE_TIME_FORMATTER);
         LocalDate dateToParsed = LocalDate.parse(dateTo, DATE_TIME_FORMATTER);
-
         for (String name : names) {
             int sum = 0;
-
             for (String row : data) {
                 String[] info = row.split(DATA_SEPARATOR);
-
                 if (!info[NAME_INDEX].equals(name)) {
                     continue;
                 }
-
                 LocalDate date = LocalDate.parse(info[DATE_INDEX], DATE_TIME_FORMATTER);
                 boolean gte = date.isAfter(dateFromParsed) || date.isEqual(dateFromParsed);
                 boolean lessThenOrEqual = date.isBefore(dateToParsed) || date.isEqual(dateToParsed);
                 boolean dateBetween = gte && lessThenOrEqual;
-
                 if (!dateBetween) {
                     continue;
                 }
-
                 sum += Integer.parseInt(info[SALARY_INDEX]) * Integer.parseInt(info[SALARY_MULTIPLIER_INDEX]);
             }
-
-            appender.append(System.lineSeparator()).append(name).append(" - ").append(sum);
+            appender.append(System.lineSeparator()).append(name).append(LOG_INFO_DELIMETER).append(sum);
         }
         return appender.toString();
     }
