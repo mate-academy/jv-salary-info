788671f6db17646b6ab07b980b73bdfcabbc73f8
thebitler <yurchyknikita@gmail.com>
2022-10-04 16:35:52 +0200

    Fixed maven checkstyle problems with max line length

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,7 +4,8 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final DateTimeFormatter DATE_TIME_FORMATTER =
+            DateTimeFormatter.ofPattern("dd.MM.yyyy");
     private static final int DATE_INDEX = 0;
     private static final int NAME_INDEX = 1;
     private static final int SALARY_INDEX = 2;
@@ -13,7 +14,8 @@ public class SalaryInfo {
     private static final String LOG_INFO_DELIMITER = " - ";
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        StringBuilder appender = new StringBuilder("Report for period ").append(dateFrom).append(LOG_INFO_DELIMITER).append(dateTo);
+        StringBuilder appender = new StringBuilder("Report for period ")
+                .append(dateFrom).append(LOG_INFO_DELIMITER).append(dateTo);
         LocalDate dateFromParsed = LocalDate.parse(dateFrom, DATE_TIME_FORMATTER);
         LocalDate dateToParsed = LocalDate.parse(dateTo, DATE_TIME_FORMATTER);
         for (String name : names) {
@@ -30,9 +32,14 @@ public class SalaryInfo {
                 if (!dateBetween) {
                     continue;
                 }
-                sum += Integer.parseInt(info[SALARY_INDEX]) * Integer.parseInt(info[SALARY_MULTIPLIER_INDEX]);
+                sum += Integer.parseInt(info[SALARY_INDEX])
+                        * Integer.parseInt(info[SALARY_MULTIPLIER_INDEX]);
             }
-            appender.append(System.lineSeparator()).append(name).append(LOG_INFO_DELIMITER).append(sum);
+            appender
+                    .append(System.lineSeparator())
+                    .append(name)
+                    .append(LOG_INFO_DELIMITER)
+                    .append(sum);
         }
         return appender.toString();
     }
