185988d9e9d62fd42c8250e3ad6a9325c58dee9d
MuzykaVP <muzykavp.work@gmail.com>
2022-10-04 16:44:51 +0300

    constant fields was created

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,21 +5,26 @@ import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("d.MM.yyyy");
-    private LocalDate currentDate;
-    private String[] userData;
-    private int income;
+    private static final int DAY = 0;
+    private static final int NAME = 1;
+    private static final int WORK_HOURS = 2;
+    private static final int SALARY = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder tempSB = new StringBuilder();
+        LocalDate currentDate;
+        String[] userData;
+        int income = 0;
         tempSB.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
         for (String name : names) {
             for (String info : data) {
                 userData = info.split(" ");
-                currentDate = LocalDate.parse(userData[0], FORMATTER);
-                if (userData[1].equals(name) && dateFrom != null && dateTo != null
+                currentDate = LocalDate.parse(userData[DAY], FORMATTER);
+                if (userData[NAME].equals(name) && dateFrom != null && dateTo != null
                         && (!currentDate.isBefore(LocalDate.parse(dateFrom, FORMATTER))
                         && !currentDate.isAfter(LocalDate.parse(dateTo, FORMATTER)))) {
-                    income += Integer.parseInt(userData[2]) * Integer.parseInt(userData[3]);
+                    income += Integer.parseInt(userData[WORK_HOURS])
+                            * Integer.parseInt(userData[SALARY]);
                 }
             }
             tempSB.append(System.lineSeparator()).append(name).append(" - ").append(income);
