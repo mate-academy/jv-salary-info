a64fc15b70296ec6bfcd17cf16256e49a2febaac
Artem Kurinniy <artem.kyrinniy@gmail.com>
2022-10-04 16:51:00 +0300

    added empty line/ added constants

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,28 +4,30 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int SALARY_INDEX = 2;
+    private static final int HOURS_INDEX = 3;
+    private static final String SPLITTER = " ";
+
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate startWorkDate = LocalDate.parse(dateFrom, FORMATTER);
-        // converted from string dateFrom date to Date format
         LocalDate finishWorkDate = LocalDate.parse(dateTo, FORMATTER);
-        // converted from string dateTo date to Date format
         StringBuilder salaryInfo = new StringBuilder("Report for period "
                     + dateFrom + " - "
                     + dateTo);
         for (String name : names) {
             int salary = 0;
             for (String allData : data) {
-                String[] dataInArray = allData.split(" ");
-                LocalDate certainDay = LocalDate.parse(dataInArray[0], FORMATTER);
-                // converted from string date in data to Date
-                int salaryPerDay = Integer.parseInt(dataInArray[2])
-                            * Integer.parseInt(dataInArray[3]);
-                // product of hours and pay
+                String[] dataInArray = allData.split(SPLITTER);
+                LocalDate certainDay = LocalDate.parse(dataInArray[DATE_INDEX], FORMATTER);
+                int salaryPerDay = Integer.parseInt(dataInArray[SALARY_INDEX])
+                            * Integer.parseInt(dataInArray[HOURS_INDEX]);
                 if (startWorkDate.getDayOfYear() < certainDay.getDayOfYear()
                         && certainDay.getDayOfYear() <= finishWorkDate.getDayOfYear()) {
-                    if (dataInArray[1].equals(name)) {
+                    if (dataInArray[NAME_INDEX].equals(name)) {
                         salary += salaryPerDay;
                     }
                 }
