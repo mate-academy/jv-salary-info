4193b2e01311d8ec702f8bd7b2cbc843379bcc0d
thebitler <yurchyknikita@gmail.com>
2022-10-04 16:19:49 +0200

    Added constants

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,12 +4,14 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    public static final DateTimeFormatter DATE_TIME_FORMATTER =
-            DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    public static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    public static final int SALARY_INDEX = 2;
+    public static final int SALARY_MULTIPLIER_INDEX = 3;
+    public static final int DATE_INDEX = 0;
+    public static final String DATA_SEPARATOR = " ";
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        StringBuilder appender = new StringBuilder("Report for period ")
-                .append(dateFrom).append(" - ").append(dateTo);
+        StringBuilder appender = new StringBuilder("Report for period ").append(dateFrom).append(" - ").append(dateTo);
         LocalDate dateFromParsed = LocalDate.parse(dateFrom, DATE_TIME_FORMATTER);
         LocalDate dateToParsed = LocalDate.parse(dateTo, DATE_TIME_FORMATTER);
 
@@ -17,13 +19,13 @@ public class SalaryInfo {
             int sum = 0;
 
             for (String row : data) {
-                String[] info = row.split(" ");
+                String[] info = row.split(DATA_SEPARATOR);
 
                 if (!info[1].equals(name)) {
                     continue;
                 }
 
-                LocalDate date = LocalDate.parse(info[0], DATE_TIME_FORMATTER);
+                LocalDate date = LocalDate.parse(info[DATE_INDEX], DATE_TIME_FORMATTER);
                 boolean gte = date.isAfter(dateFromParsed) || date.isEqual(dateFromParsed);
                 boolean lessThenOrEqual = date.isBefore(dateToParsed) || date.isEqual(dateToParsed);
                 boolean dateBetween = gte && lessThenOrEqual;
@@ -32,7 +34,7 @@ public class SalaryInfo {
                     continue;
                 }
 
-                sum += Integer.parseInt(info[2]) * Integer.parseInt(info[3]);
+                sum += Integer.parseInt(info[SALARY_INDEX]) * Integer.parseInt(info[SALARY_MULTIPLIER_INDEX]);
             }
 
             appender.append(System.lineSeparator()).append(name).append(" - ").append(sum);
