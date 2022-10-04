6e6acbb16f242d5f2c00d7aaa9fbcc36e2f0f4ca
o-shyshkan <o.shyshkan@gmail.com>
2022-10-04 20:09:58 +0300

    Deleted one loop

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -14,32 +14,33 @@ public class SalaryInfo {
     private static final int POSITION_INCOME_PER_HOUR = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        int[] resultSalary = new int[names.length];
+
         StringBuilder report = new StringBuilder();
         LocalDate dateFromLocalDate = LocalDate.parse(dateFrom, formatter);
         LocalDate dateToLocalDate = LocalDate.parse(dateTo, formatter);
-        for (String record : data) {
-            if (record.isBlank()) {
-                continue;
-            }
-            String[] parts = record.split(DELIMITER);
-            LocalDate dateRecord = LocalDate.parse(parts[POSITION_DATE], formatter);
-            if ((dateRecord.isAfter(dateFromLocalDate) && dateRecord.isBefore(dateToLocalDate))
-                    || (dateRecord.equals(dateFromLocalDate))
-                    || (dateRecord.equals(dateToLocalDate))) {
-                for (int i = 0; i < names.length; i++) {
-                    if (names[i].equals(parts[POSITION_NAME])) {
-                        resultSalary[i] += Integer.parseInt(parts[POSITION_WORKING_HOUR])
-                                * Integer.parseInt(parts[POSITION_INCOME_PER_HOUR]);
-                    }
-                }
-            }
-        }
         report.append(REPORT_HEAD).append(dateFrom).append(SEPARATOR).append(dateTo);
         for (int i = 0; i < names.length; i++) {
-            report.append(System.lineSeparator()).append(names[i]).append(SEPARATOR)
-                    .append(String.valueOf(resultSalary[i]));
+            int resultSalary = 0;
+            for (String record : data) {
+                if (record.isBlank()) {
+                    continue;
+                }
+                String[] parts = record.split(DELIMITER);
+                LocalDate dateRecord = LocalDate.parse(parts[POSITION_DATE], formatter);
+                if ((dateRecord.isAfter(dateFromLocalDate) && dateRecord.isBefore(dateToLocalDate))
+                        || (dateRecord.equals(dateFromLocalDate))
+                        || (dateRecord.equals(dateToLocalDate))) {
+
+                        if (names[i].equals(parts[POSITION_NAME])) {
+                            resultSalary += Integer.parseInt(parts[POSITION_WORKING_HOUR])
+                                    * Integer.parseInt(parts[POSITION_INCOME_PER_HOUR]);
+                        }
+                    }
+            }
+        report.append(System.lineSeparator()).append(names[i]).append(SEPARATOR)
+                .append(String.valueOf(resultSalary));
+
         }
-        return report.toString();
+    return report.toString();
     }
 }
