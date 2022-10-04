d20bb23fc782b8c8341ef8e830a2d1c19fa70fc4
MuzykaVP <muzykavp.work@gmail.com>
2022-10-04 17:15:26 +0300

    fields was edited

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,10 +5,11 @@ import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("d.MM.yyyy");
-    private static final int DAY = 0;
-    private static final int NAME = 1;
-    private static final int WORK_HOURS = 2;
-    private static final int SALARY = 3;
+    private static final int DAY_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int WORK_HOURS_INDEX = 2;
+    private static final int SALARY_INDEX = 3;
+    private static final String SPLITTER = " ";
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder tempSB = new StringBuilder();
@@ -18,13 +19,13 @@ public class SalaryInfo {
         tempSB.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
         for (String name : names) {
             for (String info : data) {
-                userData = info.split(" ");
-                currentDate = LocalDate.parse(userData[DAY], FORMATTER);
-                if (userData[NAME].equals(name) && dateFrom != null && dateTo != null
+                userData = info.split(SPLITTER);
+                currentDate = LocalDate.parse(userData[DAY_INDEX], FORMATTER);
+                if (userData[NAME_INDEX].equals(name) && dateFrom != null && dateTo != null
                         && (!currentDate.isBefore(LocalDate.parse(dateFrom, FORMATTER))
                         && !currentDate.isAfter(LocalDate.parse(dateTo, FORMATTER)))) {
-                    income += Integer.parseInt(userData[WORK_HOURS])
-                            * Integer.parseInt(userData[SALARY]);
+                    income += Integer.parseInt(userData[WORK_HOURS_INDEX])
+                            * Integer.parseInt(userData[SALARY_INDEX]);
                 }
             }
             tempSB.append(System.lineSeparator()).append(name).append(" - ").append(income);
