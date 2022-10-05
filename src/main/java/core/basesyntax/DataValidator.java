5ec1992660c40e0f36bf3809a3996558fa037deb
Roman Novosad <roman.novosad@yahoo.com>
2022-10-05 15:29:44 +0200

    refactored code after review

--- src/main/java/core/basesyntax/DataValidator.java
+++ src/main/java/core/basesyntax/DataValidator.java
@@ -1,28 +1,41 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class DataValidator {
-    private static final String LINE_SEPARATOR = System.lineSeparator();
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public void validate(String[] names, String[] data, String dateFrom, String dateTo) {
         if (dateFrom == null || dateTo == null) {
             throw new DataValidationException("Input data is incorrect: "
-            + LINE_SEPARATOR + "Date From and Date To cannot be null");
+             + "Date From and Date To cannot be null");
         }
         if (names.length == 0 || data.length == 0) {
             throw new DataValidationException("Input data is incorrect: "
-                    + LINE_SEPARATOR + "Names and Data arrays cannot be empty");
+                     + "Names and Data arrays cannot be empty");
         }
         for (String name : names) {
-            if (name == null || name.length() == 0) {
+            if (name == null || name.isEmpty()) {
                 throw new DataValidationException("Input data is incorrect: "
-                        + LINE_SEPARATOR + "Elements in array Names cannot be null or empty");
+                        + "Elements in array Names cannot be null or empty");
             }
         }
         for (String datum : data) {
-            if (datum == null || datum.length() == 0) {
+            if (datum == null || datum.isEmpty()) {
                 throw new DataValidationException("Input data is incorrect: "
-                        + LINE_SEPARATOR + "Elements in array Data cannot be null or empty");
+                        + "Elements in array Data cannot be null or empty");
             }
+        }
+
+        try {
+            LocalDate.parse(dateFrom, FORMATTER);
+            LocalDate.parse(dateTo, FORMATTER);
+        } catch (DateTimeParseException e) {
+            throw new IllegalArgumentException("Format of date is invalid (should be dd.MM.yyyy): "
+                     + "Date from: " + dateFrom + " "
+                     + "Date to: " + dateTo);
         }
     }
 }
--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -6,65 +6,49 @@ import java.time.format.DateTimeParseException;
 
 public class SalaryInfo {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-    private static final String LINE_SEPARATOR = System.lineSeparator();
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int HOURS_INDEX = 2;
+    private static final int INCOME_INDEX = 3;
+    private static final String DASH_WITH_SPACES = " - ";
+    private static final String SPLIT = " ";
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         DataValidator dataValidator = new DataValidator();
-        try {
-            dataValidator.validate(names, data, dateFrom, dateTo);
-        } catch (DataValidationException e) {
-            return String.valueOf(e);
-        }
+        dataValidator.validate(names, data, dateFrom, dateTo);
 
-        LocalDate localDateFrom;
-        LocalDate localDateTo;
-        try {
-            localDateFrom = LocalDate.parse(dateFrom, FORMATTER);
-            localDateTo = LocalDate.parse(dateTo, FORMATTER);
-        } catch (DateTimeParseException ex) {
-            throw new IllegalArgumentException("Format of date is invalid (should be dd.MM.yyyy):"
-                    + LINE_SEPARATOR + "Date from: " + dateFrom + " "
-                    + LINE_SEPARATOR + "Date to: " + dateTo);
-        }
-        int dataArrayLength = data.length;
-        LocalDate[] datesArray = new LocalDate[dataArrayLength];
-        String[] namesArray = new String[dataArrayLength];
-        Integer[] hoursArray = new Integer[dataArrayLength];
-        Integer[] incomeArray = new Integer[dataArrayLength];
+        LocalDate localDateFrom = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate localDateTo = LocalDate.parse(dateTo, FORMATTER);
 
-        int arrayIndex = 0;
+        StringBuilder builder = new StringBuilder("Report for period "
+                + FORMATTER.format(localDateFrom) + DASH_WITH_SPACES
+                + FORMATTER.format(localDateTo));
 
-        for (String datum : data) {
-            String[] splitData = datum.split(" ");
-            if (arrayIndex < dataArrayLength) {
+        for (String name : names) {
+            int salary = 0;
+            for (String datum : data) {
+                String[] splitData = datum.split(SPLIT);
                 try {
-                    datesArray[arrayIndex] = LocalDate.parse(splitData[0], FORMATTER);
-                    namesArray[arrayIndex] = splitData[1];
-                    hoursArray[arrayIndex] = Integer.parseInt(splitData[2]);
-                    incomeArray[arrayIndex] = Integer.parseInt(splitData[3]);
-                    arrayIndex++;
+                    LocalDate date = LocalDate.parse(splitData[DATE_INDEX], FORMATTER);
+                    Integer hours = Integer.parseInt(splitData[HOURS_INDEX]);
+                    Integer income = Integer.parseInt(splitData[INCOME_INDEX]);
+                    if (name.equals(splitData[NAME_INDEX])
+                            && date.compareTo(localDateFrom) >= 0
+                            && date.compareTo(localDateTo) <= 0) {
+                        salary += hours * income;
+                    }
                 } catch (DateTimeParseException ex) {
                     throw new IllegalArgumentException("Format of date is invalid "
-                            + "(should be dd.MM.yyyy):" + LINE_SEPARATOR + datum);
+                            + "(should be dd.MM.yyyy): " + datum);
                 } catch (NumberFormatException ex) {
                     throw new IllegalArgumentException("Format of working hours and income "
-                            + "per hour is invalid (should be number):"
-                            + LINE_SEPARATOR + datum);
+                            + "per hour is invalid (should be number): " + datum);
                 }
             }
-        }
-        StringBuilder builder = new StringBuilder("Report for period "
-                + FORMATTER.format(localDateFrom) + " - " + FORMATTER.format(localDateTo));
-        int namesArrayLength = names.length;
-        int[] salary = new int[namesArrayLength];
-        for (int i = 0; i < namesArrayLength; i++) {
-            for (int j = 0; j < dataArrayLength; j++) {
-                if (names[i].equals(namesArray[j]) && datesArray[j].compareTo(localDateFrom) >= 0
-                        && datesArray[j].compareTo(localDateTo) <= 0) {
-                    salary[i] += hoursArray[j] * incomeArray[j];
-                }
-            }
-            builder.append(LINE_SEPARATOR).append(names[i]).append(" - ").append(salary[i]);
+            builder.append(System.lineSeparator())
+                    .append(name)
+                    .append(DASH_WITH_SPACES)
+                    .append(salary);
         }
         return builder.toString();
     }
