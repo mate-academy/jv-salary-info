d0bb9d3cf1a51f07a1f1387b72c622a012de7ac8
Sasha Vradiy <alexvradii@gmail.com>
2022-10-05 13:46:03 +0300

    Removed the class Employee.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -9,10 +9,10 @@ public class SalaryInfo {
             = DateTimeFormatter.ofPattern("dd.MM.yyyy");
     private static final String REPORT_FOR_PERIOD = "Report for period ";
     private static final String DATE_TIME_SEPARATOR = " - ";
-    private static final int DATE_OF_START_WORK = 0;
-    private static final int WORKER_NAME = 1;
-    private static final int WORKDAY = 2;
-    private static final int WORKING_HOURS = 3;
+    private static final int DATE_OF_START_WORK_INDEX = 0;
+    private static final int WORKER_NAME_INDEX = 1;
+    private static final int WORKDAY_INDEX = 2;
+    private static final int WORKING_HOURS_INDEX = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate from = LocalDate.parse(dateFrom, DATE_TIME_FORMAT);
@@ -27,12 +27,12 @@ public class SalaryInfo {
             int salary = 0;
             for (String record : data) {
                 String[] info = record.split(" ");
-                LocalDate parsedDate = LocalDate.parse(info[DATE_OF_START_WORK], DATE_TIME_FORMAT);
-                if (info[WORKER_NAME].equals(name)
+                LocalDate parsedDate = LocalDate.parse(info[DATE_OF_START_WORK_INDEX], DATE_TIME_FORMAT);
+                if (info[WORKER_NAME_INDEX].equals(name)
                         && (parsedDate.compareTo(from) >= 0
                         && parsedDate.compareTo(to) <= 0)) {
-                    int salaryForDay = Integer.parseInt(info[WORKDAY])
-                            * Integer.parseInt(info[WORKING_HOURS]);
+                    int salaryForDay = Integer.parseInt(info[WORKDAY_INDEX])
+                            * Integer.parseInt(info[WORKING_HOURS_INDEX]);
                     salary = salary + salaryForDay;
                 }
             }
