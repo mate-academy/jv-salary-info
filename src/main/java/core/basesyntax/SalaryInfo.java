eed05e45484198c885f53beef3af1d7eb9541031
Artur Markoboh <arthur4655@gmail.com>
2022-10-05 14:05:51 +0300

    Changed position values in array to constants

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -7,8 +7,11 @@ public class SalaryInfo {
     public static final DateTimeFormatter dateFormatter =
             DateTimeFormatter.ofPattern("dd.MM.yyyy");
     public static final String DIVIDER = " - ";
-    public static final String INTRODUCTORY_PHRASE = "Report for period ";
     public static final String SPLITTER = " ";
+    public static final int DATE_POSITION = 0;
+    public static final int NAME_POSITION = 1;
+    public static final int HOURS_POSITION = 2;
+    public static final int SALARY_POSITION = 3;
 
     private LocalDate searchFrom;
     private LocalDate searchTo;
@@ -16,7 +19,7 @@ public class SalaryInfo {
     private int sum;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        StringBuilder dataBuilder = new StringBuilder(INTRODUCTORY_PHRASE);
+        StringBuilder dataBuilder = new StringBuilder("Report for period ");
         dataBuilder.append(dateFrom).append(DIVIDER).append(dateTo);
         searchFrom = LocalDate.parse(dateFrom, dateFormatter);
         searchTo = LocalDate.parse(dateTo, dateFormatter);
@@ -25,10 +28,11 @@ public class SalaryInfo {
                     .append(DIVIDER);
             for (String inf : data) {
                 String[] str = inf.split(SPLITTER);
-                currentDate = LocalDate.parse(str[0], dateFormatter);
-                if (str[1].equals(name) && currentDate.compareTo(searchFrom)
+                currentDate = LocalDate.parse(str[DATE_POSITION], dateFormatter);
+                if (str[NAME_POSITION].equals(name) && currentDate.compareTo(searchFrom)
                         * currentDate.compareTo(searchTo) <= 0) {
-                    sum += Integer.parseInt(str[2]) * Integer.parseInt(str[3]);
+                    sum += Integer.parseInt(str[HOURS_POSITION])
+                            * Integer.parseInt(str[SALARY_POSITION]);
                 }
             }
             dataBuilder.append(sum);
