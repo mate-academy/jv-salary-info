4132a59b1014fa1db307124c192d66a3d38c33ba
maksim-untilov <maksimuntilov7@gmail.com>
2022-10-06 22:28:45 +0300

    Implement method getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo). It should calculate salary for employees. As input you receive two arrays and two dates in String format.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,34 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        LocalDate localDateFrom = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate localDateTo = LocalDate.parse(dateTo, FORMATTER);
+        StringBuilder result = new StringBuilder()
+                .append("Report for period ")
+                .append(dateFrom)
+                .append(" - ")
+                .append(dateTo);
+        String[] dateArray;
+        for (String name : names) {
+            int salary = 0;
+            for (String dates : data) {
+                dateArray = dates.split(" ");
+                if (name.equals(dateArray[1])) {
+                    LocalDate dateFromData = LocalDate.parse(dateArray[0], FORMATTER);
+                    if ((dateFromData.isBefore(localDateTo) && dateFromData.isAfter(localDateFrom))
+                            || dateFromData.isEqual(localDateTo)) {
+                        salary += Integer.parseInt(dateArray[2]) * Integer.parseInt(dateArray[3]);
+                    }
+                }
+            }
+            result.append(System.lineSeparator()).append(name).append(" - ").append(salary);
+        }
+        return result.toString();
     }
 }
