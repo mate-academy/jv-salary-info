1bb78e1d6e3759037d29afd89b6e30789503b39d
Oleksandr Drobot <drob4@ukr.net>
2022-10-08 14:26:37 +0300

    replacing the Index constant

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,7 +5,10 @@ import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-    private static final int[] INDEX = {0, 1, 2, 3};
+    private static final int INDEX0 = 0;
+    private static final int INDEX1 = 1;
+    private static final int INDEX2 = 2;
+    private static final int INDEX3 = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder builder = new StringBuilder("Report for period ");
@@ -18,14 +21,14 @@ public class SalaryInfo {
         for (String name : names) {
             for (String strData : data) {
                 String[] array = strData.split(" ");
-                LocalDate date = LocalDate.parse(array[INDEX[0]], FORMATTER);
-                if (name.equals(array[INDEX[1]])
+                LocalDate date = LocalDate.parse(array[INDEX0], FORMATTER);
+                if (name.equals(array[INDEX1])
                         && !dateF.isEqual(dateT)
                         && (date.isAfter(dateF)
                         || date.isEqual(dateF))
                         && (date.isBefore(dateT)
                         || date.isEqual(dateT))) {
-                    salary += Integer.parseInt(array[INDEX[2]]) * Integer.parseInt(array[INDEX[3]]);
+                    salary += Integer.parseInt(array[INDEX2]) * Integer.parseInt(array[INDEX3]);
                 }
             }
             builder.append(System.lineSeparator());
