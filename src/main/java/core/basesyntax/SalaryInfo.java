313b3701156111af3fa9e321731c7bb4f0ac72ff
Oleksandr Drobot <drob4@ukr.net>
2022-10-08 15:00:18 +0300

    renamed constants

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,10 +5,10 @@ import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-    private static final int INDEX0 = 0;
-    private static final int INDEX1 = 1;
-    private static final int INDEX2 = 2;
-    private static final int INDEX3 = 3;
+    private static final int DATE = 0;
+    private static final int NAME = 1;
+    private static final int MULTIPLIER = 2;
+    private static final int AMOUNT = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder builder = new StringBuilder("Report for period ");
@@ -21,14 +21,14 @@ public class SalaryInfo {
         for (String name : names) {
             for (String strData : data) {
                 String[] array = strData.split(" ");
-                LocalDate date = LocalDate.parse(array[INDEX0], FORMATTER);
-                if (name.equals(array[INDEX1])
+                LocalDate date = LocalDate.parse(array[DATE], FORMATTER);
+                if (name.equals(array[NAME])
                         && !dateF.isEqual(dateT)
                         && (date.isAfter(dateF)
                         || date.isEqual(dateF))
                         && (date.isBefore(dateT)
                         || date.isEqual(dateT))) {
-                    salary += Integer.parseInt(array[INDEX2]) * Integer.parseInt(array[INDEX3]);
+                    salary += Integer.parseInt(array[MULTIPLIER]) * Integer.parseInt(array[AMOUNT]);
                 }
             }
             builder.append(System.lineSeparator());
