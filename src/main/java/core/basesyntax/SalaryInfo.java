9088ca259310d333e2b3e0a38550d2d247d1d04d
fantom <mironovanton5511@gmail.com>
2022-10-10 23:47:07 +0300

    added constants

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,9 +5,9 @@ import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     public static final int DATE_INDEX = 0;
-    public static final int NAME_INDEX = 0;
-    public static final int HOURS_INDEX = 0;
-    public static final int SALARY_INDEX = 0;
+    public static final int NAME_INDEX = 1;
+    public static final int HOURS_INDEX = 2;
+    public static final int SALARY_INDEX = 3;
     public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
@@ -20,11 +20,11 @@ public class SalaryInfo {
             int salary = 0;
             for (int j = 0; j < data.length; j++) {
                 String[] splitData = data[j].split(" ");
-                LocalDate currentDate = LocalDate.parse(splitData[0], FORMATTER);
+                LocalDate currentDate = LocalDate.parse(splitData[DATE_INDEX], FORMATTER);
                 if (currentDate.compareTo(localDateFrom) >= 0
                         && currentDate.compareTo(localDateTo) <= 0
-                        && names[i].equals(splitData[1])) {
-                    salary += Integer.parseInt(splitData[2]) * Integer.parseInt(splitData[3]);
+                        && names[i].equals(splitData[NAME_INDEX])) {
+                    salary += Integer.parseInt(splitData[HOURS_INDEX]) * Integer.parseInt(splitData[SALARY_INDEX]);
                 }
             }
             builder.append('\n').append(names[i]).append(" - ").append(salary);
