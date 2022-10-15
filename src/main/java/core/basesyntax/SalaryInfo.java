0afc1ba4f2b5138652a5f031a4f87beadeec48d9
Dmytro Busyhin <zza4etik@gmail.com>
2022-10-15 21:37:21 +0300

    removed redundant loop

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -7,28 +7,27 @@ public class SalaryInfo {
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        final int countOfNames = names.length;
-        final int[] employeesSalaryCount = new int[countOfNames];
         final LocalDate dateFromFormatted = LocalDate.parse(dateFrom, formatter);
         final LocalDate dateToFormatted = LocalDate.parse(dateTo, formatter);
-        StringBuilder resultForPeriod = new StringBuilder();
-        resultForPeriod.append("Report for period " + dateFrom + " - " + dateTo);
-        for (String datum : data) {
-            LocalDate currentDate = LocalDate.parse(
-                    datum.substring(0, datum.indexOf(" ")), formatter);
-            if (dateCheck(dateFromFormatted, dateToFormatted, currentDate)) {
-                for (int j = 0; j < names.length; j++) {
-                    if (datum.contains(names[j])) {
-                        employeesSalaryCount[j] += dailySalaryCalculation(datum);
+        final int[] employeesSalaryCount = new int[names.length];
+        int countOfNames = 0;
+        StringBuilder reportBuilder = new StringBuilder();
+        reportBuilder.append("Report for period " + dateFrom + " - " + dateTo);
+        for (String name : names) {
+            for (String datum : data) {
+                LocalDate currentDate = LocalDate.parse(
+                        datum.substring(0, datum.indexOf(" ")), formatter);
+                if (dateCheck(dateFromFormatted, dateToFormatted, currentDate)) {
+                    if (datum.contains(name)) {
+                        employeesSalaryCount[countOfNames] += dailySalaryCalculation(datum);
                     }
                 }
             }
+            reportBuilder.append(
+                    System.lineSeparator() + name + " - " + employeesSalaryCount[countOfNames]);
+            countOfNames++;
         }
-        for (int i = 0; i < countOfNames; i++) {
-            resultForPeriod.append(
-                    System.lineSeparator() + names[i] + " - " + employeesSalaryCount[i]);
-        }
-        return resultForPeriod.toString();
+        return reportBuilder.toString();
     }
 
     private boolean dateCheck(LocalDate dateFrom, LocalDate dateTo, LocalDate date) {
