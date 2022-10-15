aa30cdeff23c340140c972c46aa01154fbb302a4
Katya Humeniuk <kapelka.boo@gmail.com>
2022-10-15 16:25:03 +0300

    Fixed after second review.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,10 +5,10 @@ import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-    private static final int ZERO = 0;
-    private static final int ONE = 1;
-    private static final int TWO = 2;
-    private static final int THREE = 3;
+    private static final int INDEX_OF_DATE = 0;
+    private static final int INDEX_OF_NAME = 1;
+    private static final int INDEX_OF_HOURS = 2;
+    private static final int INDEX_OF_SALARY = 3;
     private static final String HYPHEN = " - ";
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
@@ -21,14 +21,12 @@ public class SalaryInfo {
             int salary = 0;
             for (String line : data) {
                 String[] dataFromFile = line.split(" ");
-                if (name.equals(dataFromFile[ONE])) {
-                    LocalDate dateCurrent = LocalDate.parse(dataFromFile[ZERO], FORMATTER);
-                    if ((dateFromDate.equals(dateCurrent) || dateFromDate.isBefore(dateCurrent))
-                            && (dateToDate.equals(dateCurrent)
-                            || dateToDate.isAfter(dateCurrent))) {
-                        salary = salary + (Integer.parseInt(dataFromFile[TWO])
-                                * Integer.parseInt(dataFromFile[THREE]));
-                    }
+                LocalDate dateCurrent = LocalDate.parse(dataFromFile[INDEX_OF_DATE], FORMATTER);
+                if (name.equals(dataFromFile[INDEX_OF_NAME]) && ((dateFromDate.equals(dateCurrent)
+                        || dateFromDate.isBefore(dateCurrent)) && (dateToDate.equals(dateCurrent)
+                        || dateToDate.isAfter(dateCurrent)))) {
+                    salary = salary + (Integer.parseInt(dataFromFile[INDEX_OF_HOURS])
+                                * Integer.parseInt(dataFromFile[INDEX_OF_SALARY]));
                 }
             }
             result.append(System.lineSeparator()).append(name).append(HYPHEN).append(salary);
