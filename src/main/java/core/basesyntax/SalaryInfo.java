7d6ca4e8ca1c9c91b95ea463e030fda9808c2bd3
Ilya Samilyak <samilyakilya@gmail.com>
2022-10-17 17:50:15 +0300

    HW salary

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,39 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
-    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+    private static final int DATE_INDEX = 0;
+    private static final int HOURS_INDEX = 2;
+    private static final int PAY_FOR_HOUR_INDEX = 3;
+    private static final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
+    public String getSalaryInfo(String[] names, String[] data,
+                                String dateFrom, String dateTo) {
+        LocalDate parseDateFrom = LocalDate.parse(dateFrom, dtf);
+        LocalDate parseDateTo = LocalDate.parse(dateTo, dtf);
+
+        StringBuilder builder = new StringBuilder();
+        builder.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
+
+        for (String name : names) {
+            int salary = 0;
+            for (String element : data) {
+                LocalDate date = LocalDate.parse(element.split(" ")[DATE_INDEX], dtf);
+                String[] splitData = element.split(" ");
+                if (element.contains(name)) {
+                    if (date.compareTo(parseDateFrom) >= 0
+                            && parseDateTo.compareTo(date) >= 0) {
+                        salary += Integer.parseInt(splitData[HOURS_INDEX])
+                                * Integer.parseInt(splitData[PAY_FOR_HOUR_INDEX]);
+                    }
+                }
+            }
+            builder.append(System.lineSeparator()).append(name).append(" - ").append(salary);
+        }
+        return builder.toString();
     }
 }
+
+
