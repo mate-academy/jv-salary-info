3f4d12dd52e6346249a9aa16dd1e21584c29b703
Vlad Andriichuk <vlad150096@gmail.com>
2022-10-17 20:34:57 +0200

    Changed variables DATE_INDEX, NAME_INDEX, HOURS_INDEX, PAYMENT_INDEX and DATE_FORMATTER to constant. Optimize local variables paymentForHour, hoursWorked, and date.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,35 +4,30 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    static final int DATE_INDEX = 0;
+    static final int NAME_INDEX = 1;
+    static final int HOURS_INDEX = 2;
+    static final int PAYMENT_INDEX = 3;
+    static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        final int dateIndex = 0;
-        final int nameIndex = 1;
-        final int hoursIndex = 2;
-        final int paymentIndex = 3;
         StringBuilder employeeSalaryInfo = new StringBuilder();
-        int paymentForHour;
-        int salaryForDay = 0;
-        int hoursWorked;
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        LocalDate date;
-
         for (String name : names) {
+            int salaryForDay = 0;
             employeeSalaryInfo.append(System.lineSeparator()).append(name).append(" - ");
             for (String record : data) {
-                paymentForHour = Integer.parseInt(record.split(" ")[paymentIndex]);
-                hoursWorked = Integer.parseInt(record.split(" ")[hoursIndex]);
-                date = LocalDate.parse(record.split(" ")[dateIndex], formatter);
-                if (date.isEqual(LocalDate.parse(dateFrom, formatter))
-                        || date.isAfter(LocalDate.parse(dateFrom, formatter))
-                        && !date.isAfter(LocalDate.parse(dateTo, formatter))) {
-                    if (name.equals(record.split(" ")[nameIndex])) {
+                int paymentForHour = Integer.parseInt(record.split(" ")[PAYMENT_INDEX]);
+                int hoursWorked = Integer.parseInt(record.split(" ")[HOURS_INDEX]);
+                LocalDate date = LocalDate.parse(record.split(" ")[DATE_INDEX], DATE_FORMATTER);
+                if (date.isEqual(LocalDate.parse(dateFrom, DATE_FORMATTER))
+                        || date.isAfter(LocalDate.parse(dateFrom, DATE_FORMATTER))
+                        && !date.isAfter(LocalDate.parse(dateTo, DATE_FORMATTER))) {
+                    if (name.equals(record.split(" ")[NAME_INDEX])) {
                         salaryForDay += hoursWorked * paymentForHour;
                     }
                 }
             }
             employeeSalaryInfo.append(salaryForDay);
-            salaryForDay = 0;
         }
         return "Report for period " + dateFrom + " - " + dateTo + employeeSalaryInfo;
     }
