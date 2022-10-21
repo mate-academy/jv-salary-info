1609fb2aa9ef070d40f57d35a8f10f4c9472f0e7
AndriySichniy <sichniy.andriy@gmail.com>
2022-10-21 19:55:07 +0300

    3nd commit - mentor recommendation

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,54 +1,58 @@
 package core.basesyntax;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 public class SalaryInfo {
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         String[] tmpStrArr;
-        SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyyy");
         int len = names.length;
         int[] tmpIntArr = new int[len];
         StringBuilder stringBuilder =
                 new StringBuilder("Report for period " + dateFrom + " - " + dateTo);
 
-        Date tmpDate;
-        Date tmpDateFrom;
-        Date tmpDateTo;
+        LocalDate convertDate;
+        LocalDate convertDateFrom;
+        LocalDate convertDateTo;
         try {
-            tmpDateFrom = formatter.parse(dateFrom);
-        } catch (ParseException e) {
-            tmpDateFrom = new Date();
+            convertDateFrom = LocalDate.parse(dateFrom, formatter);
+        } catch (DateTimeParseException e) {
+            convertDateFrom = LocalDate.EPOCH;
         }
         try {
-            tmpDateTo = formatter.parse(dateTo);
-        } catch (ParseException e) {
-            tmpDateTo = new Date();
+            convertDateTo = LocalDate.parse(dateTo, formatter);
+        } catch (DateTimeParseException e) {
+            convertDateTo = LocalDate.EPOCH;
         }
 
-        for (String d: data) {
-            tmpStrArr = d.split(" ");
-            try {
-                tmpDate = formatter.parse(tmpStrArr[0]);
-            } catch (ParseException e) {
-                tmpDate = new Date();
-            }
-            if (tmpDate.after(tmpDateFrom) && tmpDate.before(tmpDateTo)
-                    || tmpDate.compareTo(tmpDateFrom) == 0
-                    || tmpDate.compareTo(tmpDateTo) == 0) {
-                for (int i = 0; i < len; ++i) {
-                    if (names[i].equals(tmpStrArr[1])) {
+        int index = 0;
+        for (String name: names) {
+            for (String record: data) {
+                tmpStrArr = record.split(" ");
+                if (tmpStrArr[1].equals(name)) {
+                    try {
+                        convertDate = LocalDate.parse(tmpStrArr[0], formatter);
+                    } catch (DateTimeParseException e) {
+                        convertDate = LocalDate.EPOCH;
+                    }
+                    if (convertDate.isAfter(convertDateFrom) && convertDate.isBefore(convertDateTo)
+                            || convertDate.isEqual(convertDateFrom)
+                            || convertDate.isEqual(convertDateTo)) {
                         try {
-                            tmpIntArr[i] +=
+                            tmpIntArr[index] +=
                                     Integer.parseInt(tmpStrArr[2]) * Integer.parseInt(tmpStrArr[3]);
                         } catch (NumberFormatException e) {
-                            tmpIntArr[i] += 0;
+                            tmpIntArr[index] += 0;
                         }
                     }
                 }
             }
+            ++index;
         }
+
         for (int i = 0; i < len; ++i) {
             stringBuilder.append(System.lineSeparator())
                     .append(names[i])
