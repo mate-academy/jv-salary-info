53b1b8ac454ae1b191da44f9239ee0ea41cd05d1
Alona Vyrodova <alenavirodova@gmail.com>
2022-10-30 20:06:19 +0200

    change style's for constants

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,10 +4,10 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private static final int positionOfDate = 0;
-    private static final int positionOfName = 1;
-    private static final int positionOfHours = 2;
-    private static final int positionOfSalary = 3;
+    private static final int POSITION_OF_DATE= 0;
+    private static final int POSITION_OF_NAME = 1;
+    private static final int POSITION_OF_HOURS = 2;
+    private static final int POSITION_OF_SALARY= 3;
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
@@ -20,11 +20,11 @@ public class SalaryInfo {
             int salary = 0;
             for (String line : data) {
                 arrayData = line.split(" ");
-                LocalDate currentDate = LocalDate.parse(arrayData[positionOfDate], FORMATTER);
+                LocalDate currentDate = LocalDate.parse(arrayData[POSITION_OF_DATE], FORMATTER);
                 if (localDateFrom.compareTo(currentDate) <= 0 && localDateTo
-                        .compareTo(currentDate) >= 0 && arrayData[positionOfName].equals(name)) {
-                    salary = salary + (Integer.parseInt(arrayData[positionOfSalary])
-                            * Integer.parseInt(arrayData[positionOfHours]));
+                        .compareTo(currentDate) >= 0 && arrayData[POSITION_OF_NAME].equals(name)) {
+                    salary = salary + (Integer.parseInt(arrayData[POSITION_OF_SALARY])
+                            * Integer.parseInt(arrayData[POSITION_OF_HOURS]));
                 }
             }
             result.append(System.lineSeparator()).append(name).append(" - ").append(salary);
