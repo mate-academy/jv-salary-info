5f85b55029c49bf5e8b05ca25a5d3840fa68384f
Yaroslav Liashchenko <liaroswin@gmail.com>
2022-11-04 14:19:05 +0200

    modified getSalaryInfo and compareDates methods, deleted excess method

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,38 +1,40 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
+        final int indexOfDate = 0;
+        final int indexOfName = 1;
+        final int indexOfHours = 2;
+        final int indexOfTariff = 3;
         StringBuilder builder = new StringBuilder("Report for period ")
                                                     .append(dateFrom).append(" - ")
                                                     .append(dateTo);
-        int[] salary = new int[names.length];
-        for (int i = 0; i < salary.length; i++) {
+        int salary;
+        for (int i = 0; i < names.length; i++) {
+            salary = 0;
             for (String info : data) {
                 String[] splitedInfo = info.split(" ");
-                if (names[i].equals(splitedInfo[1])
-                        && compareDates(splitedInfo[0], dateFrom, dateTo)) {
-                    salary[i] += (Integer.parseInt(splitedInfo[2])
-                                * Integer.parseInt(splitedInfo[3]));
+                if (names[i].equals(splitedInfo[indexOfName])
+                        && compareDates(splitedInfo[indexOfDate], dateFrom, dateTo)) {
+                    salary += (Integer.parseInt(splitedInfo[indexOfHours])
+                            * Integer.parseInt(splitedInfo[indexOfTariff]));
                 }
             }
-            builder.append(System.lineSeparator()).append(names[i]).append(" - ").append(salary[i]);
+            builder.append(System.lineSeparator()).append(names[i]).append(" - ").append(salary);
         }
         return builder.toString();
     }
 
     private boolean compareDates(String workDate, String dateFrom, String dateTo) {
-        String[] splitedWorkDate = workDate.split("\\.");
-        String[] splitedDateFrom = dateFrom.split("\\.");
-        String[] splitedDateTo = dateTo.split("\\.");
-        int daysInWorkDate = countDaysInDate(splitedWorkDate);
-        int daysInDateFrom = countDaysInDate(splitedDateFrom);
-        int daysInDateTo = countDaysInDate(splitedDateTo);
-        return daysInWorkDate >= daysInDateFrom && daysInWorkDate <= daysInDateTo ? true : false;
-    }
-
-    private int countDaysInDate(String[] date) {
-        return Integer.parseInt(date[0])
-            + (Integer.parseInt(date[1]) * 30)
-            + (Integer.parseInt(date[2]) * 365);
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDate localWorkDate = LocalDate.parse(workDate, dateTimeFormatter);
+        LocalDate localDateFrom = LocalDate.parse(dateFrom, dateTimeFormatter);
+        LocalDate localDateTo = LocalDate.parse(dateTo, dateTimeFormatter);
+        return localWorkDate.equals(localDateFrom)
+                || localWorkDate.isAfter(localDateFrom) && localWorkDate.isBefore(localDateTo)
+                || localWorkDate.equals(localDateTo) ? true : false;
     }
 }
