9c712478e6530cdb86413fc856c635dd04ff1412
Yaroslav Liashchenko <liaroswin@gmail.com>
2022-11-05 22:02:54 +0200

    changed formatter to constant, for loop to for each, return statement in compareDates method

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -8,33 +8,33 @@ public class SalaryInfo {
     private static final int NAME_INDEX = 1;
     private static final int HOURS_INDEX = 2;
     private static final int TARIFF_INDEX = 3;
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder builder = new StringBuilder("Report for period ")
                                                     .append(dateFrom).append(" - ")
                                                     .append(dateTo);
-        for (int i = 0; i < names.length; i++) {
+        for (String name : names) {
             int salary = 0;
             for (String info : data) {
                 String[] splitedInfo = info.split(" ");
-                if (names[i].equals(splitedInfo[NAME_INDEX])
+                if (name.equals(splitedInfo[NAME_INDEX])
                         && compareDates(splitedInfo[DATE_INDEX], dateFrom, dateTo)) {
                     salary += (Integer.parseInt(splitedInfo[HOURS_INDEX])
                             * Integer.parseInt(splitedInfo[TARIFF_INDEX]));
                 }
             }
-            builder.append(System.lineSeparator()).append(names[i]).append(" - ").append(salary);
+            builder.append(System.lineSeparator()).append(name).append(" - ").append(salary);
         }
         return builder.toString();
     }
 
     private boolean compareDates(String workDate, String dateFrom, String dateTo) {
-        final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        LocalDate localWorkDate = LocalDate.parse(workDate, dateTimeFormatter);
-        LocalDate localDateFrom = LocalDate.parse(dateFrom, dateTimeFormatter);
-        LocalDate localDateTo = LocalDate.parse(dateTo, dateTimeFormatter);
+        LocalDate localWorkDate = LocalDate.parse(workDate, FORMATTER);
+        LocalDate localDateFrom = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate localDateTo = LocalDate.parse(dateTo, FORMATTER);
         return localWorkDate.equals(localDateFrom)
                 || localWorkDate.isAfter(localDateFrom) && localWorkDate.isBefore(localDateTo)
-                || localWorkDate.equals(localDateTo) ? true : false;
+                || localWorkDate.equals(localDateTo);
     }
 }
