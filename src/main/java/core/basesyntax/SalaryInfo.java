5ab37f5a4dbdf8ef3eb81128c7e63122aca539aa
Diana <dlomei283@gmail.com>
2022-11-05 16:34:50 +0200

    i change name of constans.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,10 +4,10 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private static final int NUMBER_ZERO = 0;
-    private static final int NUMBER1 = 1;
-    private static final int NUMBER2 = 2;
-    private static final int NUMBER3 = 3;
+    private static final int INDEX_OF_NAME_ZERO = 0;
+    private static final int INDEX_OF_NAME_FIRST = 1;
+    private static final int INDEX_OF_NAME_SECOND = 2;
+    private static final int INDEX_OF_NAME_THIRD = 3;
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
@@ -21,12 +21,12 @@ public class SalaryInfo {
             int salary = 0;
             for (String line : data) {
                 arrayOfData = line.split(" ");
-                LocalDate currentDate = LocalDate.parse(arrayOfData[NUMBER_ZERO], FORMATTER);
+                LocalDate currentDate = LocalDate.parse(arrayOfData[INDEX_OF_NAME_ZERO], FORMATTER);
                 if (localDateFrom.compareTo(currentDate) <= 0
                         && localDateTo.compareTo(currentDate) >= 0
-                        && arrayOfData[NUMBER1].equals(name)) {
-                    salary += (Integer.parseInt(arrayOfData[NUMBER3])
-                            * Integer.parseInt(arrayOfData[NUMBER2]));
+                        && arrayOfData[INDEX_OF_NAME_FIRST].equals(name)) {
+                    salary += (Integer.parseInt(arrayOfData[INDEX_OF_NAME_THIRD])
+                            * Integer.parseInt(arrayOfData[INDEX_OF_NAME_SECOND]));
                 }
             }
             result.append(System.lineSeparator()).append(name).append(" - ").append(salary);
