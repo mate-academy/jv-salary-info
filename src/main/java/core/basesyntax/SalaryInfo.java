e19eceb572655fc113bc0a1400739b71f8574f8e
Diana <dlomei283@gmail.com>
2022-11-05 15:19:17 +0200

    i do this task, everything works.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,36 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+    private static final int NUMBER_ZERO = 0;
+    private static final int NUMBER1 = 1;
+    private static final int NUMBER2 = 2;
+    private static final int NUMBER3 = 3;
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        LocalDate localDateFrom = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate localDateTo = LocalDate.parse(dateTo, FORMATTER);
+        StringBuilder result = new StringBuilder().append("Report for period ")
+                .append(dateFrom).append(" - ").append(dateTo);
+        String[] arrayOfData;
+
+        for (String name: names) {
+            int salary = 0;
+            for (String line : data) {
+                arrayOfData = line.split(" ");
+                LocalDate currentDate = LocalDate.parse(arrayOfData[NUMBER_ZERO], FORMATTER);
+                if (localDateFrom.compareTo(currentDate) <= 0
+                        && localDateTo.compareTo(currentDate) >= 0
+                        && arrayOfData[NUMBER1].equals(name)) {
+                    salary += (Integer.parseInt(arrayOfData[NUMBER3])
+                            * Integer.parseInt(arrayOfData[NUMBER2]));
+                }
+            }
+            result.append(System.lineSeparator()).append(name).append(" - ").append(salary);
+        }
+        return result.toString();
     }
 }
