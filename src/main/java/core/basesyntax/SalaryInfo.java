12e1c6d08f3184c47600abf88a9e8d5c7876e676
Danylo Borysenko <borysenkodanyl@gmail.com>
2022-11-07 10:31:20 +0200

    method getSalaryInfo() was created

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,34 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
-    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+    private static DateTimeFormatter formatter =
+            DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
+    public String getSalaryInfo(String[] names, String[] data, String dateFrom,
+                                String dateTo) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
+        LocalDate periodFrom = LocalDate.parse(dateFrom, formatter);
+        LocalDate periodTo = LocalDate.parse(dateTo, formatter);
+        for (int i = 0; i < names.length; i++) {
+            int salary = 0;
+            for (int y = 0; y < data.length; y++) {
+                String[] partsOfData = data[y].split(" ");
+                LocalDate workingDayOfEmployee = LocalDate.parse(partsOfData[0],formatter);
+                boolean isDayInThePeriod = workingDayOfEmployee.compareTo(periodFrom) >= 0
+                        && workingDayOfEmployee.compareTo(periodTo) <= 0;
+                if (isDayInThePeriod && names[i].equals(partsOfData[1])) {
+                    int workingHours = Integer.parseInt(partsOfData[2]);
+                    int incomePerHour = Integer.parseInt(partsOfData[3]);
+                    salary += workingHours * incomePerHour;
+                }
+            }
+            stringBuilder.append(System.lineSeparator())
+                    .append(names[i]).append(" - ").append(salary);
+        }
+        return stringBuilder.toString();
     }
 }
