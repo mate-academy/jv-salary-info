2997c833385ff14829565b2ed49af53d101c517f
Danylo Borysenko <borysenkodanyl@gmail.com>
2022-11-07 10:39:38 +0200

    keyword final was added to the FORMATTER

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,20 +4,20 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private static DateTimeFormatter formatter =
+    private static final DateTimeFormatter FORMATTER =
             DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom,
                                 String dateTo) {
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
-        LocalDate periodFrom = LocalDate.parse(dateFrom, formatter);
-        LocalDate periodTo = LocalDate.parse(dateTo, formatter);
+        LocalDate periodFrom = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate periodTo = LocalDate.parse(dateTo, FORMATTER);
         for (int i = 0; i < names.length; i++) {
             int salary = 0;
             for (int y = 0; y < data.length; y++) {
                 String[] partsOfData = data[y].split(" ");
-                LocalDate workingDayOfEmployee = LocalDate.parse(partsOfData[0],formatter);
+                LocalDate workingDayOfEmployee = LocalDate.parse(partsOfData[0], FORMATTER);
                 boolean isDayInThePeriod = workingDayOfEmployee.compareTo(periodFrom) >= 0
                         && workingDayOfEmployee.compareTo(periodTo) <= 0;
                 if (isDayInThePeriod && names[i].equals(partsOfData[1])) {
