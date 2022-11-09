b1fefa90ea8b8e2c5015c57cf8d59819f8c9577c
Oksana Domanchuk <dvoynikova@gmail.com>
2022-11-09 10:56:44 +0200

    implemented getSalaryInfo method

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -6,12 +6,6 @@ import java.time.format.DateTimeFormatter;
 public class SalaryInfo {
     private static final DateTimeFormatter FORMATTER
             = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-    private static String[] arrayOfDataElements;
-    private static final LocalDate CURRENT_DATE
-            = LocalDate.parse(arrayOfDataElements[0], FORMATTER);
-    private static final String EMPLOYER_NAME = arrayOfDataElements[1];
-    private static final int WORKING_HOURS = Integer.parseInt(arrayOfDataElements[2]);
-    private static final int INCOME_PER_HOUR = Integer.parseInt(arrayOfDataElements[3]);
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate localDateFrom = LocalDate.parse(dateFrom, FORMATTER);
@@ -20,14 +14,21 @@ public class SalaryInfo {
                 .append(dateFrom)
                 .append(" - ")
                 .append(dateTo);
+
         for (String name : names) {
             int salary = 0;
             for (String dataElement : data) {
-                arrayOfDataElements = dataElement.split(" ");
-                if (localDateFrom.compareTo(CURRENT_DATE) <= 0
-                        && localDateTo.compareTo(CURRENT_DATE) >= 0
-                        && EMPLOYER_NAME.equals(name)) {
-                    salary += WORKING_HOURS * INCOME_PER_HOUR;
+                String[] arrayOfDataElements = dataElement.split(" ");
+                LocalDate currentDate
+                        = LocalDate.parse(arrayOfDataElements[0], FORMATTER);
+                String employerName = arrayOfDataElements[1];
+                int workingHours = Integer.parseInt(arrayOfDataElements[2]);
+                int incomePerHour = Integer.parseInt(arrayOfDataElements[3]);
+
+                if (localDateFrom.compareTo(currentDate) <= 0
+                        && localDateTo.compareTo(currentDate) >= 0
+                        && employerName.equals(name)) {
+                    salary += workingHours * incomePerHour;
                 }
             }
             result.append(System.lineSeparator()).append(name).append(" - ").append(salary);
