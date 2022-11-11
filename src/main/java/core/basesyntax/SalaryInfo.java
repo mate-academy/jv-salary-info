e05c15ebcc4b468e37b98e66e0cc7087074a945d
AndriiBohun <poikka69@gmail.com>
2022-11-11 13:29:16 +0200

    My-Commit002

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,8 +5,8 @@ import static java.lang.Integer.parseInt;
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
 
-        StringBuilder sB_basic = new StringBuilder();
-        StringBuilder sB_done = new StringBuilder();
+        StringBuilder sbBasic = new StringBuilder();
+        StringBuilder sbDone = new StringBuilder();
         GetDateInNumbers getDateInNumbers = new GetDateInNumbers();
         Selection selection = new Selection();
 
@@ -18,18 +18,19 @@ public class SalaryInfo {
             int theCurrentDate = getDateInNumbers.getDateInNumbers(arrayAllInformationData[0]);
 
             if (theCurrentDate >= dateMin && theCurrentDate <= dateMax) {
-                int counter = parseInt(arrayAllInformationData[2]) * parseInt(arrayAllInformationData[3]);
+                int counter = parseInt(arrayAllInformationData[2]) *
+                        parseInt(arrayAllInformationData[3]);
                 for (String name : names) {
                     if (arrayAllInformationData[1].equals(name)) {
                         String cc = (counter + " " + name);
-                        sB_basic.append(cc).append("/");
+                        sbBasic.append(cc).append("/");
                     }
                 }
             }
         }
-        String[] LastArray = sB_basic.toString().split("/");
+        String[] lastArray = sbBasic.toString().split("/");
 
-        sB_done.append("Report for period ")
+        sbDone.append("Report for period ")
                 .append(dateFrom)
                 .append(" - ")
                 .append(dateTo)
@@ -41,10 +42,10 @@ public class SalaryInfo {
                 indentDown = "";
             }
 
-            sB_done.append(names[i]).append(" - ")
-                    .append(selection.selection(LastArray, names, i))
+            sbDone.append(names[i]).append(" - ")
+                    .append(selection.selection(lastArray, names, i))
                     .append(indentDown);
         }
-        return sB_done.toString();
+        return sbDone.toString();
     }
 }
