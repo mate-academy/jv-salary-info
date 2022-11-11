26704248edeeb0a56aa7f936296b641a4ab5d3a8
andrii-kobryn <andrzjha@gmail.com>
2022-11-11 14:05:01 +0200

    moved indexes to constants

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -8,6 +8,10 @@ public class SalaryInfo {
             = DateTimeFormatter.ofPattern("dd.MM.yyyy");
     private static final String SEPARATOR = " ";
     private static final long AND_ONE_MORE_DAY = 1L;
+    private static final int EMPLOYEE_DATA_INDEX = 0;
+    private static final int EMPLOYEE_NAME_INDEX = 1;
+    private static final int WORKING_OURS = 2;
+    private static final int INCOME_PER_OUR = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder stringBuilder = new StringBuilder("Report for period ");
@@ -16,10 +20,11 @@ public class SalaryInfo {
             int salary = 0;
             for (String record : data) {
                 var separatedData = record.split(SEPARATOR);
-                var employeeData = LocalDate.parse(separatedData[0], DATE_TIME_FORMATTER);
-                var employeeName = separatedData[1];
-                var workingOurs = Integer.parseInt(separatedData[2]);
-                var incomePerOur = Integer.parseInt(separatedData[3]);
+                var employeeData = LocalDate.parse(separatedData[EMPLOYEE_DATA_INDEX],
+                        DATE_TIME_FORMATTER);
+                var employeeName = separatedData[EMPLOYEE_NAME_INDEX];
+                var workingOurs = Integer.parseInt(separatedData[WORKING_OURS]);
+                var incomePerOur = Integer.parseInt(separatedData[INCOME_PER_OUR]);
                 if (name.equals(employeeName)
                         && employeeData.isAfter(LocalDate.parse(dateFrom, DATE_TIME_FORMATTER)
                         .minusDays(AND_ONE_MORE_DAY))
