c041e888921f498f058fa38f3910b8bf2b5e039e
Artem Bordichuk <artyombordichuk@gmail.com>
2022-11-12 14:06:47 +0200

    The method getSalaryInfo() was implemented.It calculates employee's salary for a certain period of time. LocalDate class and DateTimeFormatter was used to comparing input data parameters. The calculation results were combined using object of StringBuilder class.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,37 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+    private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d.MM.yyyy");
+    private LocalDate fromDate;
+    private LocalDate toDate;
+    private StringBuilder builder = new StringBuilder();
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        int result = 0;
+        StringBuilder builder = new StringBuilder("Report for period " + dateFrom + " - " + dateTo);
+        LocalDate employeeWorkingDay;
+        fromDate = LocalDate.parse(dateFrom, formatter);
+        toDate = LocalDate.parse(dateTo, formatter);
+
+        for (String name : names) {
+            for (String date : data) {
+                String [] dataSplit = date.split(" ");
+                employeeWorkingDay = LocalDate.parse(dataSplit[0], formatter);
+
+                if ((name.equals(dataSplit[1]))
+                        && (employeeWorkingDay.isEqual(fromDate)
+                        || employeeWorkingDay.isAfter(fromDate))
+                        && (employeeWorkingDay.isEqual(toDate)
+                        || employeeWorkingDay.isBefore(toDate))) {
+                    result += Integer.valueOf(dataSplit[2]) * Integer.valueOf(dataSplit[3]);
+                }
+            }
+            builder.append(System.lineSeparator()).append(name).append(" - ").append(result);
+            result = 0;
+        }
+        return builder.toString();
     }
 }
