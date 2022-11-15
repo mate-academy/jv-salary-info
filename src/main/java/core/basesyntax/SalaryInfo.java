a4f185a11cfca79286ba711ddd3322be135963d9
Bohdan Shevtsov <shevtsov.b@gmail.com>
2022-11-15 18:07:48 +0200

    before making 2 loops

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -6,13 +6,16 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int NAME_INDEX = 0;
+    private static final int SALARY_INDEX = 1;
+
 
     public String getSalaryInfo(String[]names, String[]data, String dateFrom, String dateTo) {
         String[][] resultArray = new String[names.length][2];
         for (int i = 0; i < names.length; i++) { //feel names in result array
-            resultArray[i][0] = names[i];
-            resultArray[i][1] = "0";
+            resultArray[i][NAME_INDEX] = names[i];
+            resultArray[i][SALARY_INDEX] = "0";
         }
         for (int i = 0; i < data.length; i++) {
             String[] divideByWhitespace = data[i].split(" ");
@@ -23,8 +26,8 @@ public class SalaryInfo {
                     || ddate.isEqual(ddateFrom) || ddate.isEqual(ddateTo)) {
                 String name = divideByWhitespace[1];
                 for (int j = 0; j < names.length; j++) {
-                    if (name.equals(resultArray[j][0])) {
-                        resultArray[j][1] = valueOf(Integer.parseInt(resultArray[j][1])
+                    if (name.equals(resultArray[j][NAME_INDEX])) {
+                        resultArray[j][1] = valueOf(Integer.parseInt(resultArray[j][SALARY_INDEX])
                                 + Integer.parseInt(divideByWhitespace[2])
                                 * Integer.parseInt(divideByWhitespace[3]));
                     }
@@ -35,11 +38,11 @@ public class SalaryInfo {
         resultBuilder.append("Report for period ").append(dateFrom).append(" - ").append(dateTo)
                         .append(System.lineSeparator());
         for (int i = 0; i < names.length - 1; i++) {
-            resultBuilder.append(resultArray[i][0]).append(" - ").append(resultArray[i][1])
+            resultBuilder.append(resultArray[i][NAME_INDEX]).append(" - ").append(resultArray[i][1])
                     .append(System.lineSeparator());
         }
-        resultBuilder.append(resultArray[names.length - 1][0]).append(" - ")
-                .append(resultArray[names.length - 1][1]);
+        resultBuilder.append(resultArray[names.length - 1][NAME_INDEX]).append(" - ")
+                .append(resultArray[names.length - 1][SALARY_INDEX]);
         String resultString = resultBuilder.toString();
         return resultString;
     }
