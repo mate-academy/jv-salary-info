1249bdb6ddf8177670b4b57e634d17be36e34e02
Bohdan Shevtsov <shevtsov.b@gmail.com>
2022-11-16 17:51:31 +0200

    created constant for all indexes

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -9,6 +9,10 @@ public class SalaryInfo {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
     private static final int NAME_INDEX = 0;
     private static final int SALARY_INDEX = 1;
+    private static final int DIVIDER_DATE = 0;
+    private static final int DIVIDER_NAME = 1;
+    private static final int DIVIDER_HOURS = 2;
+    private static final int DIVIDER_INCOME = 3;
 
     public String getSalaryInfo(String[]names, String[]data, String dateFrom, String dateTo) {
         String[][] resultArray = new String[names.length][2];
@@ -19,20 +23,20 @@ public class SalaryInfo {
             resultArray[i][SALARY_INDEX] = "0";
             for (int j = 0; j < data.length; j++) {
                 String[] divideByWhitespace = data[j].split(" ");
-                LocalDate ddate = LocalDate.parse(divideByWhitespace[0], FORMATTER);
+                LocalDate ddate = LocalDate.parse(divideByWhitespace[DIVIDER_DATE], FORMATTER);
                 LocalDate ddateFrom = LocalDate.parse(dateFrom, FORMATTER);
                 LocalDate ddateTo = LocalDate.parse(dateTo, FORMATTER);
                 if (((ddate.isAfter(ddateFrom) && ddate.isBefore(ddateTo))
                         || ddate.isEqual(ddateFrom) || ddate.isEqual(ddateTo))
-                        && divideByWhitespace[1].equals(resultArray[i][NAME_INDEX])) {
+                        && divideByWhitespace[DIVIDER_NAME].equals(resultArray[i][NAME_INDEX])) {
                     resultArray[i][SALARY_INDEX] = valueOf(Integer
                             .parseInt(resultArray[i][SALARY_INDEX])
-                            + Integer.parseInt(divideByWhitespace[2])
-                            * Integer.parseInt(divideByWhitespace[3]));
+                            + Integer.parseInt(divideByWhitespace[DIVIDER_HOURS])
+                            * Integer.parseInt(divideByWhitespace[DIVIDER_INCOME]));
                 }
             }
             resultBuilder.append(System.lineSeparator()).append(resultArray[i][NAME_INDEX])
-                    .append(" - ").append(resultArray[i][1]);
+                    .append(" - ").append(resultArray[i][SALARY_INDEX]);
         }
         return resultBuilder.toString();
     }
