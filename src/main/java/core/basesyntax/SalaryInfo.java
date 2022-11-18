bed9e97eb606f41305bbd8537980083ff09262c6
Nikita Kolesnikov <kit1datasvit@gmail.com>
2022-11-18 12:50:46 +0200

    hw-1-solution-fix-mistakes-1

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,28 +5,32 @@ import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
 
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int HOURS_INDEX = 2;
+    private static final int INCOME_PER_HOUR_INDEX = 3;
     private static final DateTimeFormatter DATE_FORMATTER =
             DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate from = LocalDate.parse(dateFrom, DATE_FORMATTER);
         LocalDate to = LocalDate.parse(dateTo, DATE_FORMATTER);
-        StringBuilder salaryResult = new StringBuilder().append("Report for period ")
+        StringBuilder salaryResultBuilder = new StringBuilder().append("Report for period ")
                 .append(dateFrom).append(" - ").append(dateTo);
         for (String name : names) {
             int nameSalary = 0;
             for (String dateData : data) {
                 String[] dataNameHoursSalary = dateData.split(" ");
-                LocalDate day = LocalDate.parse(dataNameHoursSalary[0], DATE_FORMATTER);
-                if (dataNameHoursSalary[1].equals(name)
+                LocalDate day = LocalDate.parse(dataNameHoursSalary[DATE_INDEX], DATE_FORMATTER);
+                if (dataNameHoursSalary[NAME_INDEX].equals(name)
                         && ((day.isAfter(from) || day.isEqual(from))
                         && ((day.isBefore(to)) || day.isEqual(to)))) {
-                    nameSalary += Integer.parseInt(dataNameHoursSalary[2])
-                            * Integer.parseInt(dataNameHoursSalary[3]);
+                    nameSalary += Integer.parseInt(dataNameHoursSalary[HOURS_INDEX])
+                            * Integer.parseInt(dataNameHoursSalary[INCOME_PER_HOUR_INDEX]);
                 }
             }
-            salaryResult.append("\n").append(name).append(" - ").append(nameSalary);
+            salaryResultBuilder.append("\n").append(name).append(" - ").append(nameSalary);
         }
-        return salaryResult.toString();
+        return salaryResultBuilder.toString();
     }
 }
