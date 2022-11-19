9778296c2e22ca54148a270f38ed4c61c39b7c8b
Nazarii Kharyshyn <nazariykharyshyn@gmail.com>
2022-11-20 01:53:15 +0200

    Created for and foreach loops to go through names and data arrays in order to check and calculate salary info for every employee. Parsed String with date to LocalDate in order to check if date limits are correct. Returned salary info by using StringBuilder class

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,44 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class SalaryInfo {
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        final StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(String.format("Report for period %s - %s", dateFrom, dateTo))
+                .append(System.lineSeparator());
+        int[] salaryInfo = new int[names.length];
+        for (int i = 0; i < names.length; i++) {
+            for (String checkedData : data) {
+                String[] info = checkedData.split(" ");
+                if (names[i].equals(info[1])) {
+                    try {
+                        LocalDate dateAfter = LocalDate.parse(dateFrom, formatter);
+                        LocalDate dateBefore = LocalDate.parse(dateTo, formatter);
+                        LocalDate date = LocalDate.parse(info[0], formatter);
+                        if (date.compareTo(dateAfter) >= 0
+                                && date.compareTo(dateBefore) <= 0) {
+                            int hour = Integer.parseInt(info[2]);
+                            int income = Integer.parseInt(info[3]);
+                            salaryInfo[i] += hour * income;
+                        }
+                    } catch (DateTimeParseException e) {
+                        System.out.println("Date is not parsable");
+                        throw e;
+                    }
+                }
+            }
+            stringBuilder.append(names[i])
+                    .append(" - ")
+                    .append(salaryInfo[i]);
+            if (i < names.length - 1) {
+                stringBuilder.append(System.lineSeparator());
+            }
+        }
+        return stringBuilder.toString();
     }
 }
