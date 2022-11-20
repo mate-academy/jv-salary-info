f1e0a3d21843c397b857f0a6a99cd7a2a0c18cff
Yurii Storozhuk <95564174+urlo0k@users.noreply.github.com>
2022-11-20 22:16:15 +0200

    fix(all): break long line

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,41 +4,39 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private static final int DATE = 0;
-    private static final int NAME = 1;
-    private static final int HOUR = 2;
-    private static final int INCOME = 3;
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int HOUR_INDEX = 2;
+    private static final int INCOME_INDEX = 3;
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        LocalDate date1 = LocalDate.parse(dateFrom, FORMATTER);
-        LocalDate date2 = LocalDate.parse(dateTo, FORMATTER);
-        int[] salary = new int[names.length];
-        for (int i = 0; i < names.length; i++) {
-            salary[i] = 0;
-            for (int j = 0; j < data.length; j++) {
-                String line = data[j];
-                String[] lines = line.split(" ");
-                LocalDate dateLine = LocalDate.parse(lines[DATE], FORMATTER);
-                if (((dateLine.isAfter(date1)) || (dateLine.equals(date1)))
-                        && ((dateLine.isBefore(date2)) || (dateLine.equals(date2)))) {
-                    if (lines[NAME].equals(names[i])) {
-                        int sum = Integer.parseInt(lines[HOUR]) * Integer.parseInt(lines[INCOME]);
-                        salary[i] = salary[i] + sum;
-                    }
-                }
-            }
-        }
+        LocalDate localDateFrom = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate localDateTo = LocalDate.parse(dateTo, FORMATTER);
         StringBuilder builder = new StringBuilder();
         builder.append("Report for period ");
         builder.append(dateFrom);
         builder.append(" - ");
         builder.append(dateTo);
-        for (int i = 0; i < names.length; i++) {
+        for (String name : names) {
+            int salary = 0;
+            for (String item : data) {
+                String[] lines = item.split(" ");
+                LocalDate currentData = LocalDate.parse(lines[DATE_INDEX], FORMATTER);
+                if (((currentData.isAfter(localDateFrom))
+                        || (currentData.equals(localDateFrom)))
+                        && ((currentData.isBefore(localDateTo))
+                        || (currentData.equals(localDateTo)))
+                        && (lines[NAME_INDEX].equals(name))) {
+                    int sum = Integer.parseInt(lines[HOUR_INDEX])
+                            * Integer.parseInt(lines[INCOME_INDEX]);
+                    salary = salary + sum;
+                }
+            }
             builder.append(System.lineSeparator());
-            builder.append(names[i]);
+            builder.append(name);
             builder.append(" - ");
-            builder.append(salary[i]);
+            builder.append(salary);
         }
         return builder.toString();
     }
