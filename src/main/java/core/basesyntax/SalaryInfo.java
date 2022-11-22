6b443e1679b4a056ea05d17b752c48e14db1ad23
Volodymyr Mykychak <leadervlo.d@gmail.com>
2022-11-22 19:01:57 +0200

    doing four commit

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,43 +1,41 @@
 package core.basesyntax;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    private static final int DATE = 0;
+    private static final int NUMBER_OF_MONTH = 2;
+    private static final int SALARY = 3;
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        String nameAndSalarySum = "";
+        StringBuilder nameAndSalarySum = new StringBuilder();
         for (int i = 0; i < names.length; i++) {
             int salarySum = 0;
-            for (String s : data) {
-                String[] splitData = s.split(" ");
-                if (dateRange(dateFrom, splitData[0], dateTo) == true
+            for (String record : data) {
+                String[] splitData = record.split(" ");
+                if (dateRange(dateFrom, splitData[DATE], dateTo) == true
                         && splitData[1].equals(names[i])) {
-                    salarySum += getMonthSalary(splitData[2], splitData[3]);
+                    salarySum += getMonthSalary(splitData[NUMBER_OF_MONTH], splitData[SALARY]);
                 }
             }
             if (i == names.length - 1) {
-                nameAndSalarySum += names[i] + " - " + salarySum;
+                nameAndSalarySum.append(names[i]).append(" - ").append(salarySum);
             } else {
-                nameAndSalarySum += names[i] + " - " + salarySum + "\n";
+                nameAndSalarySum.append(names[i]).append(" - ").append(salarySum).append("\n");
             }
         }
         return String.format("Report for period %s - %s\n%s", dateFrom, dateTo, nameAndSalarySum);
     }
 
-    private Date getDateFormat(String stringDate) {
-        SimpleDateFormat format = new SimpleDateFormat();
-        format.applyPattern("dd.MM.yyyy");
-        try {
-            return format.parse(stringDate);
-        } catch (ParseException e) {
-            throw new RuntimeException(e);
-        }
+    private LocalDate getDateFormat(String stringDate) {
+        return LocalDate.parse(stringDate, FORMATTER);
     }
 
     private boolean dateRange(String dateFrom, String correctDate, String dateTo) {
-        return !getDateFormat(correctDate).before(getDateFormat(dateFrom))
-                && !getDateFormat(correctDate).after(getDateFormat(dateTo));
+        return !getDateFormat(correctDate).isBefore(getDateFormat(dateFrom))
+                && !getDateFormat(correctDate).isAfter(getDateFormat(dateTo));
     }
 
     private int getMonthSalary(String splitData2, String splitData3) {
