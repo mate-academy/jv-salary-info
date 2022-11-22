6f528daff1cc3f073307a86ed8ff888623217b56
Igor Puhach <igorpuhach21@gmail.com>
2022-11-22 09:13:03 +0200

    Added class DateTimeFormatter, created two loops, used StringBuilder,System.Lineseparator  #2

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -29,7 +29,6 @@ public class SalaryInfo {
                             * Integer.parseInt(dataNew[PAY_HOURS]);
                 }
             }
-
             array.append(System.lineSeparator())
                     .append(name)
                     .append(" - ")
