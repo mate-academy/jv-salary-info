0a2a6160d9913ab96e64301c5bf6ccb28196a179
Nazarii Kharyshyn <nazariykharyshyn@gmail.com>
2022-11-27 18:24:00 +0200

    added all fixes after review

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -8,36 +8,30 @@ public class SalaryInfo {
     private static final int INDEX_OF_NAME = 1;
     private static final int INDEX_OF_HOUR = 2;
     private static final int INDEX_OF_INCOME = 3;
-    private static final int INDEX_OF_EMPLOYEE = -1;
-    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         final StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append(String.format("Report for period %s - %s", dateFrom, dateTo))
-                .append(System.lineSeparator());
-        int[] salaryInfo = new int[names.length];
-        LocalDate localDateFrom = LocalDate.parse(dateFrom, formatter);
-        LocalDate localDateTo = LocalDate.parse(dateTo, formatter);
-        int indexEmployee = INDEX_OF_EMPLOYEE;
+        stringBuilder.append(String.format("Report for period %s - %s", dateFrom, dateTo));
+        LocalDate localDateFrom = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate localDateTo = LocalDate.parse(dateTo, FORMATTER);
         for (String name : names) {
-            indexEmployee++;
+            int salary = 0;
             for (String line : data) {
                 String[] info = line.split(" ");
-                LocalDate date = LocalDate.parse(info[INDEX_OF_DATE], formatter);
+                LocalDate date = LocalDate.parse(info[INDEX_OF_DATE], FORMATTER);
                 if (date.compareTo(localDateFrom) >= 0
                         && date.compareTo(localDateTo) <= 0
                         && name.equals(info[INDEX_OF_NAME])) {
                     int hour = Integer.parseInt(info[INDEX_OF_HOUR]);
                     int income = Integer.parseInt(info[INDEX_OF_INCOME]);
-                    salaryInfo[indexEmployee] += hour * income;
+                    salary += hour * income;
                 }
             }
-            stringBuilder.append(name)
+            stringBuilder.append(System.lineSeparator())
+                    .append(name)
                     .append(" - ")
-                    .append(salaryInfo[indexEmployee]);
-            if (indexEmployee < names.length - 1) {
-                stringBuilder.append(System.lineSeparator());
-            }
+                    .append(salary);
         }
         return stringBuilder.toString();
     }
