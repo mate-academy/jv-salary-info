8352c431d5e7ccf1775a1bc1cb7e641e01d567b9
Nazarii Kharyshyn <nazariykharyshyn@gmail.com>
2022-11-27 17:47:50 +0200

    Replaced for loop by foreach, fixed checkstyle mistakes, added constants for magic numbers, removed unneeded try catch

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -2,9 +2,13 @@ package core.basesyntax;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
 
 public class SalaryInfo {
+    private static final int INDEX_OF_DATE = 0;
+    private static final int INDEX_OF_NAME = 1;
+    private static final int INDEX_OF_HOUR = 2;
+    private static final int INDEX_OF_INCOME = 3;
+    private static final int INDEX_OF_EMPLOYEE = -1;
     private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
@@ -12,30 +16,26 @@ public class SalaryInfo {
         stringBuilder.append(String.format("Report for period %s - %s", dateFrom, dateTo))
                 .append(System.lineSeparator());
         int[] salaryInfo = new int[names.length];
-        for (int i = 0; i < names.length; i++) {
-            for (String checkedData : data) {
-                String[] info = checkedData.split(" ");
-                if (names[i].equals(info[1])) {
-                    try {
-                        LocalDate dateAfter = LocalDate.parse(dateFrom, formatter);
-                        LocalDate dateBefore = LocalDate.parse(dateTo, formatter);
-                        LocalDate date = LocalDate.parse(info[0], formatter);
-                        if (date.compareTo(dateAfter) >= 0
-                                && date.compareTo(dateBefore) <= 0) {
-                            int hour = Integer.parseInt(info[2]);
-                            int income = Integer.parseInt(info[3]);
-                            salaryInfo[i] += hour * income;
-                        }
-                    } catch (DateTimeParseException e) {
-                        System.out.println("Date is not parsable");
-                        throw e;
-                    }
+        LocalDate localDateFrom = LocalDate.parse(dateFrom, formatter);
+        LocalDate localDateTo = LocalDate.parse(dateTo, formatter);
+        int indexEmployee = INDEX_OF_EMPLOYEE;
+        for (String name : names) {
+            indexEmployee++;
+            for (String line : data) {
+                String[] info = line.split(" ");
+                LocalDate date = LocalDate.parse(info[INDEX_OF_DATE], formatter);
+                if (date.compareTo(localDateFrom) >= 0
+                        && date.compareTo(localDateTo) <= 0
+                        && name.equals(info[INDEX_OF_NAME])) {
+                    int hour = Integer.parseInt(info[INDEX_OF_HOUR]);
+                    int income = Integer.parseInt(info[INDEX_OF_INCOME]);
+                    salaryInfo[indexEmployee] += hour * income;
                 }
             }
-            stringBuilder.append(names[i])
+            stringBuilder.append(name)
                     .append(" - ")
-                    .append(salaryInfo[i]);
-            if (i < names.length - 1) {
+                    .append(salaryInfo[indexEmployee]);
+            if (indexEmployee < names.length - 1) {
                 stringBuilder.append(System.lineSeparator());
             }
         }
