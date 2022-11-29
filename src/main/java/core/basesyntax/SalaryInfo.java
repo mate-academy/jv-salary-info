468af60ee2398071590507d0de830a24fb92fdc3
Iskamele <iskamele@gmail.com>
2022-11-29 12:30:55 +0200

    Added class-level variables for formatting string responses; Indices of information positions in the provided data array data; Date formatting, whose name had to be shortened to D_FORMATTER due to a build error that limits the string length to 100 characters.
    In the getSalaryInfo() method, we first parse the date into LocalDate.
    We form part of the response using the above-declared REPORT_TITLE_PATTERN pattern.
    And we add the resulting response to the declared StringBuilder.
    
    For further calculations, we add the salary variable.
    
    The first loop iterates over the names array.
    Inside the first cycle, the second cycle is triggered, which goes through the elements of the data array.
    For each iteration, we split the data element.
    If the name in the splitLine matches the name in the name array and the weekday date is in the correct range (this is checked by a separate isBetween() method), then we calculate the salary using the getSalary() method.
    We carry out these calculations in the try-catch block in case of a ParseException error.
    After exiting the second loop, we create a formattedString string, in which we add data according to the PERSON_DESCRIPTION_PATTERN pattern.
    The result is added to StringBuilder.
    Reset the salary counter.
    At the end of the first loop, remove the extra lineSeparator() and return the response as a String.
    
    The isBetween() method compares whether the date was greater than the start date and less than the end date, or whether it coincides with them.
    
    The getSalary() method parses the values from the income and hours strings and multiplies them with each other.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,58 @@
 package core.basesyntax;
 
+import java.text.ParseException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+    public static final String REPORT_TITLE_PATTERN = "Report for period %s - %s";
+    public static final String PERSON_DESCRIPTION_PATTERN = "%s - %s";
+    public static final String DATA_SEPARATOR = " ";
+    public static final int DATE_POSITION = 0;
+    public static final int NAME_POSITION = 1;
+    public static final int HOURS_POSITION = 2;
+    public static final int INCOME_POSITION = 3;
+    private static final DateTimeFormatter D_FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        final LocalDate from = LocalDate.parse(dateFrom, D_FORMATTER);
+        final LocalDate to = LocalDate.parse(dateTo, D_FORMATTER);
+        final String title = String.format(REPORT_TITLE_PATTERN, dateFrom, dateTo);
+        final StringBuilder builder = new StringBuilder(title)
+                .append(System.lineSeparator());
+        int salary = 0;
+        for (String name : names) {
+            for (String dataElement : data) {
+                String[] splitLine = dataElement.split(DATA_SEPARATOR);
+                try {
+                    if (splitLine[NAME_POSITION].equals(name)
+                            && isBetween(from, to, splitLine[DATE_POSITION])) {
+                        salary += getSalary(splitLine[INCOME_POSITION], splitLine[HOURS_POSITION]);
+                    }
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+            }
+            String formattedString = String.format(PERSON_DESCRIPTION_PATTERN, name, salary);
+            builder.append(formattedString)
+                    .append(System.lineSeparator());
+            salary = 0;
+        }
+        builder.delete(builder.length() - 2, builder.length());
+        return builder.toString();
+    }
+
+    private boolean isBetween(LocalDate from, LocalDate to, String splitDate)
+            throws ParseException {
+        LocalDate date = LocalDate.parse(splitDate, D_FORMATTER);
+        return date.isAfter(from)
+                && date.isBefore(to)
+                || date.isEqual(from)
+                || date.isEqual(to);
+    }
+
+    private int getSalary(String income, String hours)
+            throws ParseException {
+        return Integer.parseInt(income) * Integer.parseInt(hours);
     }
 }
