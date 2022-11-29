de5f7ff151d8e55edffe6595ab02bc9e0cef0105
Oleksii Parkhomenko <kcas.pilot@gmail.com>
2022-11-29 20:53:09 +0200

    added constants, renamed variable, removed checking dates

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,45 +5,45 @@ import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final String NAME_REPORT = "Report for period ";
+    private static final String STRING_SEP = "\\s";
+    private static final String INFO_DELIMITER = " - ";
+    private static final byte DATE_INDEX = 0;
+    private static final byte NAME_INDEX = 1;
+    private static final byte HOURS_INDEX = 2;
+    private static final byte SALARY_INDEX = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate startDate = LocalDate.parse(dateFrom, DATE_FORMAT);
         LocalDate endDate = LocalDate.parse(dateTo, DATE_FORMAT);
-        StringBuilder result = new StringBuilder("Report for period ");
+        StringBuilder reportBuilder = new StringBuilder(NAME_REPORT);
         String[] personalInfo;
         LocalDate personalDate;
         int personalSalary = 0;
 
-        if (startDate.isAfter(endDate)) {
-            startDate = endDate;
-            endDate = LocalDate.parse(dateFrom, DATE_FORMAT);
-            result.append(dateTo)
-                    .append(" - ")
-                    .append(dateFrom);
-        } else {
-            result.append(dateFrom)
-                    .append(" - ")
-                    .append(dateTo);
-        }
+        reportBuilder.append(dateFrom)
+                .append(INFO_DELIMITER)
+                .append(dateTo);
+
         for (String name : names) {
             for (String datum : data) {
-                personalInfo = datum.split("\\s");
-                if (!name.equals(personalInfo[1])) {
+                personalInfo = datum.split(STRING_SEP);
+                if (!name.equals(personalInfo[NAME_INDEX])) {
                     continue;
                 }
-                personalDate = LocalDate.parse(personalInfo[0], DATE_FORMAT);
+                personalDate = LocalDate.parse(personalInfo[DATE_INDEX], DATE_FORMAT);
                 if (!personalDate.isBefore(startDate)
                         && !personalDate.isAfter(endDate)) {
-                    personalSalary += Integer.parseInt(personalInfo[2])
-                            * Integer.parseInt(personalInfo[3]);
+                    personalSalary += Integer.parseInt(personalInfo[HOURS_INDEX])
+                            * Integer.parseInt(personalInfo[SALARY_INDEX]);
                 }
             }
-            result.append(System.lineSeparator())
+            reportBuilder.append(System.lineSeparator())
                     .append(name)
-                    .append(" - ")
+                    .append(INFO_DELIMITER)
                     .append(personalSalary);
             personalSalary = 0;
         }
-        return result.toString();
+        return reportBuilder.toString();
     }
 }
