364ee0b14757f7db36951cdb56a2f972397ddb47
Artem Otsabera <eskvair86@gmail.com>
2022-11-30 23:01:50 +0200

    added five constants

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,23 +4,28 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    public static final int DATE = 0;
+    public static final int NAME = 1;
+    public static final int HOURS = 2;
+    public static final int MONEY = 3;
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder builder = new StringBuilder();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        LocalDate dateFromLocal = LocalDate.parse(dateFrom, formatter);
-        LocalDate dateToLocal = LocalDate.parse(dateTo, formatter);
+        LocalDate dateFromLocal = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate dateToLocal = LocalDate.parse(dateTo, FORMATTER);
 
         for (String name : names) {
             int salary = 0;
             for (String date : data) {
                 String[] newData = date.split(" ");
-                LocalDate localDate = LocalDate.parse(newData[0], formatter);
-                String nameData = newData[1];
+                LocalDate localDate = LocalDate.parse(newData[DATE], FORMATTER);
+                String nameData = newData[NAME];
                 if (name.equals(nameData)
                         && (localDate.isEqual(dateFromLocal) || localDate.isAfter(dateFromLocal))
                         && (localDate.isEqual(dateToLocal) || localDate.isBefore(dateToLocal))) {
-                    salary = salary + Integer.parseInt((newData[2].trim()))
-                            * Integer.parseInt(newData[3].trim());
+                    salary = salary + Integer.parseInt((newData[HOURS].trim()))
+                            * Integer.parseInt(newData[MONEY].trim());
                 }
             }
             builder.append(System.lineSeparator()).append(name).append(" - ").append(salary);
