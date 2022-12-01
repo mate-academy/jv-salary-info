dd4412a10dd853b4d4fcb74cd0189b6d60534ba4
Yevhenii Kilovyi <jenya.kilevoy@gmail.com>
2022-12-01 20:53:05 +0200

    third try of solution with requested changes

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -6,6 +6,10 @@ import java.time.format.DateTimeFormatter;
 public class SalaryInfo {
     private static final DateTimeFormatter DATE_FORMATTER
             = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int DATE_POSITION = 0;
+    private static final int NAME_POSITION = 1;
+    private static final int WORKING_HOURS_POSITION = 2;
+    private static final int SALARY_PER_HOUR_POSITION = 3;
 
     public String getSalaryInfo(String[] names, String[] data,
                                 String dateFrom, String dateTo) {
@@ -14,28 +18,27 @@ public class SalaryInfo {
         StringBuilder report = new StringBuilder("Report for period ")
                 .append(dateFrom)
                 .append(" - ")
-                .append(dateTo)
-                .append(System.lineSeparator());
+                .append(dateTo);
         for (String name :
                 names) {
             int salaryCounter = 0;
             for (String line :
                     data) {
                 String [] arrayOfData = line.split(" ");
-                LocalDate currentDate = LocalDate.parse(arrayOfData[0], DATE_FORMATTER);
-                if (arrayOfData[1].equals(name)
+                LocalDate currentDate = LocalDate.parse(arrayOfData[DATE_POSITION], DATE_FORMATTER);
+                if (arrayOfData[NAME_POSITION].equals(name)
                         && ((currentDate.isAfter(firstDate)) || (currentDate.isEqual(firstDate)))
                         && (currentDate.isBefore(lastDate) || currentDate.isEqual(lastDate))) {
                     salaryCounter = salaryCounter
-                            + Integer.parseInt(arrayOfData[2])
-                                    * Integer.parseInt(arrayOfData[3]);
+                            + Integer.parseInt(arrayOfData[WORKING_HOURS_POSITION])
+                                    * Integer.parseInt(arrayOfData[SALARY_PER_HOUR_POSITION]);
                 }
             }
-            report.append(name)
+            report.append(System.lineSeparator())
+                    .append(name)
                     .append(" - ")
-                    .append(salaryCounter)
-                    .append(System.lineSeparator());
+                    .append(salaryCounter);
         }
-        return report.toString().substring(0,report.length() - 1);
+        return report.toString();
     }
 }
