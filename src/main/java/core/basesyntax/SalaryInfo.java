5a13abeec6cc34c979ada77789570ad979d79a2a
Volodymyr Isakov <ive.ukr@gmail.com>
2022-12-02 00:25:01 +0200

    Remove redundant array add variable instead change const names

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,30 +5,30 @@ import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-    private static final int DATE = 0;
-    private static final int NAME = 1;
-    private static final int HOURS = 2;
-    private static final int INCOME = 3;
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int HOURS_INDEX = 2;
+    private static final int INCOME_INDEX = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         final LocalDate parsedDateFrom = LocalDate.parse(dateFrom, FORMATTER);
         final LocalDate parsedDateTo = LocalDate.parse(dateTo, FORMATTER);
-        int[] salary = new int[names.length];
         StringBuilder builder = new StringBuilder();
         builder.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
         for (int i = 0; i < names.length; i++) {
+            int salary = 0;
             for (int j = 0; j < data.length; j++) {
                 String[] dataSplit = data[j].split(" ");
-                LocalDate currentDate = LocalDate.parse(dataSplit[DATE], FORMATTER);
-                if (names[i].equals(dataSplit[NAME])) {
+                LocalDate currentDate = LocalDate.parse(dataSplit[DATE_INDEX], FORMATTER);
+                if (names[i].equals(dataSplit[NAME_INDEX])) {
                     if (!currentDate.isBefore(parsedDateFrom)
                             && !currentDate.isAfter(parsedDateTo)) {
-                        salary[i] += Integer.parseInt(dataSplit[HOURS])
-                                * Integer.parseInt(dataSplit[INCOME]);
+                        salary += Integer.parseInt(dataSplit[HOURS_INDEX])
+                                * Integer.parseInt(dataSplit[INCOME_INDEX]);
                     }
                 }
             }
-            builder.append(System.lineSeparator()).append(names[i]).append(" - ").append(salary[i]);
+            builder.append(System.lineSeparator()).append(names[i]).append(" - ").append(salary);
         }
         return builder.toString();
     }
