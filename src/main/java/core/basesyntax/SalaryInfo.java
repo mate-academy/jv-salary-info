e8ddb3a379fe6db4fc612d6a44e8e01302643e7c
Andrei_Bondarenko <bondorol007@gmail.com>
2022-12-02 14:11:11 +0200

    Check pleas

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,15 +1,17 @@
 package core.basesyntax;
 
+import java.util.Arrays;
+
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder builder = new StringBuilder();
         String infoSalary = null;
-        int salary = 0;
+        int[] salary = new int[names.length];
 
         int yearFrom = Integer.parseInt(dateFrom.substring(6, 10));
         int yearTo = Integer.parseInt(dateTo.substring(6, 10));
         int monthFrom = Integer.parseInt(dateFrom.substring(3, 5));
-        int monthTo = Integer.parseInt(dateFrom.substring(3, 5));
+        int monthTo = Integer.parseInt(dateTo.substring(3, 5));
         int dayFrom = Integer.parseInt(dateFrom.substring(0, 2));
         int dayTo = Integer.parseInt(dateTo.substring(0, 2));
 
@@ -17,13 +19,15 @@ public class SalaryInfo {
             int dataYear = Integer.parseInt(data[i].substring(6, 10));
             int dataMonth = Integer.parseInt(data[i].substring(3, 5));
             int dataDay = Integer.parseInt(data[i].substring(0, 2));
-            if (dataYear != yearFrom
-                    || dataYear != yearTo
-                    || dataMonth != monthFrom
-                    || dataMonth != monthTo
-                    || dataDay < dayFrom
-                    || dataDay > dayTo) {
+            if (dataYear != yearFrom && dataYear != yearTo) {
                 data[i] = null;
+                continue;
+            } else if (dataMonth != monthFrom && dataMonth != monthTo) {
+                data[i] = null;
+                continue;
+            } else if  (dataDay < dayFrom || dataDay > dayTo) {
+                data[i] = null;
+                continue;
             }
         }
         for (int i = 0; i < names.length; i++) {
@@ -34,22 +38,14 @@ public class SalaryInfo {
                     String[] splitName = data[j].split(" ");
                     for (int k = 0; k < splitName.length; k++) {
                         if (splitName[k].equals(names[i])) {
-                            salary = salary + Integer.parseInt(splitName[2]) * Integer.parseInt(splitName[3]);
+                            salary[i] = salary[i] + Integer.parseInt(splitName[2]) * Integer.parseInt(splitName[3]);
                         }
                     }
                 }
             }
-            infoSalary = builder.append("Report for period").append(" ").append(dateFrom)
-                    .append(" - ").append(dateTo).append("\n").append(names[i])
-                    .append(" - ").append(salary).append("\n").toString();
+            infoSalary = builder.append("\n").append(names[i])
+                    .append(" - ").append(salary[i]).toString();
         }
-        return infoSalary;
+        return "Report for period " + dateFrom + " - " +  dateTo  +  infoSalary;
     }
 }
-
-
-
-
-
-  /* Report for period #date_1# - #date_2#
-           #Name of employee# - #money earned#*/
