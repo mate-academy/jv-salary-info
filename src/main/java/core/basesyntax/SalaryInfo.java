7f9cc5db28a696dedc787673c69540176d8632d9
Dmytro Katrevych <katrevichdv@gmail.com>
2022-12-02 12:33:52 +0200

    DateTimeFormatter was made constant.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -7,17 +7,14 @@ public class SalaryInfo {
     private static final int DATE_INDEX = 0;
     private static final int NAME_INDEX = 1;
     private static final int HOURS_INDEX = 2;
-    private static final int PAYRATE_INDEX = 3;
-    private static final String PATTERN = "dd.MM.yyyy";
-
-    private LocalDate dateFrom;
-    private LocalDate dateTo;
+    private static final int PAY_RATE_INDEX = 3;
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        var formatter = DateTimeFormatter.ofPattern(PATTERN);
-        this.dateFrom = LocalDate.parse(dateFrom, formatter);
-        this.dateTo = LocalDate.parse(dateTo, formatter);
+        var fromDate = LocalDate.parse(dateFrom, formatter);
+        var toDate = LocalDate.parse(dateTo, formatter);
         var report = new StringBuilder();
+
         report.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
 
         for (var name : names) {
@@ -27,9 +24,9 @@ public class SalaryInfo {
                 String[] values = row.split(" ");
                 if (name.equals(values[NAME_INDEX])) {
                     var date = LocalDate.parse(values[DATE_INDEX], formatter);
-                    if (isInPeriod(date)) {
+                    if (isInPeriod(date, fromDate, toDate)) {
                         count += Integer.valueOf(values[HOURS_INDEX]) 
-                                * Integer.valueOf(values[PAYRATE_INDEX]);
+                                * Integer.valueOf(values[PAY_RATE_INDEX]);
                     }
                 }
 
@@ -39,8 +36,8 @@ public class SalaryInfo {
         return report.toString();
     }
 
-    private boolean isInPeriod(LocalDate date) {
-        return (dateFrom.isEqual(date) || dateFrom.isBefore(date)) 
+    private boolean isInPeriod(LocalDate date, LocalDate dateFrom, LocalDate dateTo) {
+        return (dateFrom.isEqual(date) || dateFrom.isBefore(date))
                 && (dateTo.isEqual(date) || dateTo.isAfter(date));
     }
 }
