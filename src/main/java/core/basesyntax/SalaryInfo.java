8f52667a343e1c0bca54258b5df3095926c74ee8
Алексей <m.leshka955@gmail.com>
2022-12-03 21:24:26 +0200

    checkstyle fix

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -8,7 +8,8 @@ public class SalaryInfo {
     private static final int NAME_INDEX = 1;
     private static final int OPENING_HOURS = 2;
     private static final int SALARY_PER_HOR = 3;
-    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final DateTimeFormatter DATE_FORMATTER =
+            DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder stringBuilder = new StringBuilder("Report for period ")
@@ -22,9 +23,10 @@ public class SalaryInfo {
             int salary = 0;
             for (String dates : data) {
                 String[] sortArrayData = dates.split(" ");
-                LocalDate dateWhenWorked = LocalDate.parse(sortArrayData[DATE_INDEX], DATE_FORMATTER);
-                if (sortArrayData[NAME_INDEX].equals(name) &&
-                        ((dateWhenWorked.isAfter(startDate)) || (dateWhenWorked.isEqual(startDate)))
+                LocalDate dateWhenWorked =
+                        LocalDate.parse(sortArrayData[DATE_INDEX], DATE_FORMATTER);
+                if (sortArrayData[NAME_INDEX].equals(name)
+                        && ((dateWhenWorked.isAfter(startDate)) || (dateWhenWorked.isEqual(startDate)))
                         && (dateWhenWorked.isBefore(endDate) || dateWhenWorked.isEqual(endDate))) {
                     salary += Integer.parseInt(sortArrayData[OPENING_HOURS])
                             * Integer.parseInt(sortArrayData[SALARY_PER_HOR]);
