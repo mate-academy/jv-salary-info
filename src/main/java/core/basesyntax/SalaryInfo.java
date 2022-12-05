0acefcafe7d62dda7598fa1aae43dc2927f6006f
Andrei_Bondarenko <bondorol007@gmail.com>
2022-12-05 20:49:44 +0200

    salary info1

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -2,26 +2,27 @@ package core.basesyntax;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
 
 public class SalaryInfo {
-    final DateTimeFormatter DTF = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    final private DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder builder = new StringBuilder("Report for period " + dateFrom + " - " + dateTo);
-        LocalDate localDateFrom = LocalDate.parse(dateFrom, DTF);
-        LocalDate localDateTo = LocalDate.parse(dateTo, DTF);
+        LocalDate localDateFrom = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate localDateTo = LocalDate.parse(dateTo, FORMATTER);
         String infoSalary = null;
         int[] salary = new int[names.length];
 
         for (int i = 0; i < names.length; i++) {
             for (int j = 0; j < data.length; j++) {
                 String[] splitData = data[j].split(" ");
-                LocalDate localSplitDate = LocalDate.parse(splitData[0], DTF);
-                if ((localDateFrom.isBefore(localSplitDate) || localDateTo.isEqual(localSplitDate))
+                LocalDate localSplitDate = LocalDate.parse(splitData[0], FORMATTER);
+                if ((localDateFrom.isBefore(localSplitDate)
+                        || localDateTo.isEqual(localSplitDate))
                         && (localDateTo.isAfter(localSplitDate) || localDateTo.isEqual(localSplitDate))
                         && names[i].equals(splitData[1])) {
-                    salary[i] = salary[i] + Integer.parseInt(splitData[2]) * Integer.parseInt(splitData[3]);
+                    salary[i] = salary[i] +
+                            Integer.parseInt(splitData[2]) * Integer.parseInt(splitData[3]);
                 }
             }
             infoSalary = builder.append(System.lineSeparator()).append(names[i])
