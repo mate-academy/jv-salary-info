08d6a18ae77d6d9e8e6098e3eb4e2b35cbaf2ecc
Vladyslav Fesenko <vlad.11.fesenko@gmail.com>
2022-12-05 17:54:03 +0200

    fixed code style

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,15 +4,15 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-    private static final int datePosition = 0;
-    private static final int namePosition = 1;
-    private static final int hoursWorkedPosition = 2;
-    private static final int salaryPerHourPosition = 3;
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int DATE_POSITION = 0;
+    private static final int NAME_POSITION = 1;
+    private static final int HOURS_WORKED_POSITION = 2;
+    private static final int SALARY_PER_HOUR_POSITION = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        StringBuilder stringBuilder = new StringBuilder("Report for period "
-                + dateFrom + " - " + dateTo);
+        StringBuilder stringBuilder = new StringBuilder("Report for period ");
+        stringBuilder.append(dateFrom).append(" - ").append(dateTo);
         for (String name: names) {
             int salary = 0;
             for (String record: data) {
@@ -27,21 +27,21 @@ public class SalaryInfo {
     }
 
     private String getName(String data) {
-        return data.split(" ")[namePosition];
+        return data.split(" ")[NAME_POSITION];
     }
 
     private boolean validateDate(String data, String dateFrom, String dateTo) {
-        String dateString = data.split(" ")[datePosition];
-        LocalDate date = LocalDate.parse(dateString, formatter);
-        LocalDate dateStart = LocalDate.parse(dateFrom, formatter);
-        LocalDate dateEnd = LocalDate.parse(dateTo, formatter);
+        String dateString = data.split(" ")[DATE_POSITION];
+        LocalDate date = LocalDate.parse(dateString, FORMATTER);
+        LocalDate dateStart = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate dateEnd = LocalDate.parse(dateTo, FORMATTER);
 
         return !date.isBefore(dateStart) & !date.isAfter(dateEnd);
     }
 
     private int getDailySalary(String data) {
-        int numberOfHoursWorked = Integer.parseInt(data.split(" ")[hoursWorkedPosition]);
-        int salaryPerHour = Integer.parseInt(data.split(" ")[salaryPerHourPosition]);
+        int numberOfHoursWorked = Integer.parseInt(data.split(" ")[HOURS_WORKED_POSITION]);
+        int salaryPerHour = Integer.parseInt(data.split(" ")[SALARY_PER_HOUR_POSITION]);
         return numberOfHoursWorked * salaryPerHour;
     }
 }
