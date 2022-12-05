45135bfc827c4b5a0bf4c3f389092000468aa573
Andrei_Bondarenko <bondorol007@gmail.com>
2022-12-05 20:28:06 +0200

    salary info

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,49 +1,32 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 
 public class SalaryInfo {
+    final DateTimeFormatter DTF = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        StringBuilder builder = new StringBuilder();
+        StringBuilder builder = new StringBuilder("Report for period " + dateFrom + " - " + dateTo);
+        LocalDate localDateFrom = LocalDate.parse(dateFrom, DTF);
+        LocalDate localDateTo = LocalDate.parse(dateTo, DTF);
         String infoSalary = null;
         int[] salary = new int[names.length];
-        int yearFrom = Integer.parseInt(dateFrom.substring(6, 10));
-        int yearTo = Integer.parseInt(dateTo.substring(6, 10));
-        int monthFrom = Integer.parseInt(dateFrom.substring(3, 5));
-        int monthTo = Integer.parseInt(dateTo.substring(3, 5));
-        int dayFrom = Integer.parseInt(dateFrom.substring(0, 2));
-        int dayTo = Integer.parseInt(dateTo.substring(0, 2));
-        for (int i = 0; i < data.length; i++) {
-            int dataYear = Integer.parseInt(data[i].substring(6, 10));
-            int dataMonth = Integer.parseInt(data[i].substring(3, 5));
-            int dataDay = Integer.parseInt(data[i].substring(0, 2));
-            if (dataYear != yearFrom && dataYear != yearTo) {
-                data[i] = null;
-                continue;
-            } else if (dataMonth != monthFrom && dataMonth != monthTo) {
-                data[i] = null;
-                continue;
-            } else if  (dataDay < dayFrom || dataDay > dayTo) {
-                data[i] = null;
-                continue;
-            }
-        }
+
         for (int i = 0; i < names.length; i++) {
             for (int j = 0; j < data.length; j++) {
-                if (data[j] == null) {
-                    continue;
-                } else {
-                    String[] splitName = data[j].split(" ");
-                    for (int k = 0; k < splitName.length; k++) {
-                        if (splitName[k].equals(names[i])) {
-                            salary[i] = salary[i] + Integer.parseInt(splitName[2]) * Integer.parseInt(splitName[3]);
-                        }
-                    }
+                String[] splitData = data[j].split(" ");
+                LocalDate localSplitDate = LocalDate.parse(splitData[0], DTF);
+                if ((localDateFrom.isBefore(localSplitDate) || localDateTo.isEqual(localSplitDate))
+                        && (localDateTo.isAfter(localSplitDate) || localDateTo.isEqual(localSplitDate))
+                        && names[i].equals(splitData[1])) {
+                    salary[i] = salary[i] + Integer.parseInt(splitData[2]) * Integer.parseInt(splitData[3]);
                 }
             }
-            infoSalary = builder.append("\n").append(names[i])
+            infoSalary = builder.append(System.lineSeparator()).append(names[i])
                     .append(" - ").append(salary[i]).toString();
         }
-        return "Report for period " + dateFrom + " - " +  dateTo  +  infoSalary;
+        return builder.toString();
     }
 }
