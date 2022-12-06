a5c90c831514e3cf67c0f402e41c9c790c17d671
Inna Diachkina <innadyachkina@gmail.com>
2022-12-06 08:21:53 +0200

    create field FORMATTER

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -7,18 +7,17 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-
+    static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder report = new StringBuilder("Report for period ").append(dateFrom)
                 .append(" - ").append(dateTo);
-        final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        LocalDate from = parse(dateFrom, formatter);
-        LocalDate to = parse(dateTo, formatter);
+        LocalDate from = parse(dateFrom, FORMATTER);
+        LocalDate to = parse(dateTo, FORMATTER);
         for (String name : names) {
             int salary = 0;
             for (String daySalary : data) {
                 String[] dataOneDay = daySalary.split(" ");
-                LocalDate day = parse(dataOneDay[0], formatter);
+                LocalDate day = parse(dataOneDay[0], FORMATTER);
                 if (name.equals(dataOneDay[1]) && ((day.isAfter(from) || day.equals(from)))
                         && (day.isBefore(to) || day.equals(to))) {
                     salary += parseInt(dataOneDay[2]) * parseInt(dataOneDay[3]);
