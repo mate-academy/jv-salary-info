da2e53a06a31b7ee31928b4cf7ca9f91bd003b19
Dmytro M <dmytro.malinnikov@gmail.com>
2022-12-06 14:40:31 +0200

    refactor getSalaryInfo method

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,45 +1,51 @@
 package core.basesyntax;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private static final String pattern = "dd.MM.yyyy";
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int WORKING_HOURS_INDEX = 2;
+    private static final int INCOME_PER_HOUR_INDEX = 3;
+    private static final String DATA_SEPARATOR = " ";
+    private static final String DATE_PATTERN = "dd.MM.yyyy";
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        List<String> namesList = Arrays.asList(names);
         int[] salaries = new int[names.length];
-        for (String dataRow : data) {
-            String[] dataArr = dataRow.split(" ");
-            if (namesList.contains(dataArr[1]) && isDateInRange(dataArr[0], dateFrom, dateTo)) {
-                int hours = Integer.parseInt(dataArr[2]);
-                int amount = Integer.parseInt(dataArr[3]);
-                int index = namesList.indexOf(String.valueOf(dataArr[1]));
-                salaries[index] += hours * amount;
+        for (String line : data) {
+            String[] splitLine = line.split(DATA_SEPARATOR);
+            int index = getNameIndex(names, splitLine[NAME_INDEX]);
+            if (index != -1 && isDateInRange(splitLine[DATE_INDEX], dateFrom, dateTo)) {
+                int workingHours = Integer.parseInt(splitLine[WORKING_HOURS_INDEX]);
+                int incomePerHour = Integer.parseInt(splitLine[INCOME_PER_HOUR_INDEX]);
+                salaries[index] += workingHours * incomePerHour;
             }
         }
-        StringBuilder sb = new StringBuilder("Report for period ");
-        sb.append(dateFrom).append(" - ").append(dateTo);
-        for (int i = 0; i < namesList.size(); i++) {
-            sb.append(System.lineSeparator());
-            sb.append(namesList.get(i)).append(" - ").append(salaries[i]);
+        StringBuilder reportBuilder = new StringBuilder("Report for period ");
+        reportBuilder.append(dateFrom).append(" - ").append(dateTo);
+        for (int i = 0; i < salaries.length; i++) {
+            reportBuilder.append(System.lineSeparator());
+            reportBuilder.append(names[i]).append(" - ").append(salaries[i]);
         }
-        return sb.toString();
+        return reportBuilder.toString();
     }
 
-    private boolean isDateInRange(String dateToCheck, String dateFrom, String dateTo) {
-        SimpleDateFormat dateFormat = new SimpleDateFormat();
-        dateFormat.applyPattern(pattern);
-        try {
-            Date from = dateFormat.parse(dateFrom);
-            Date to = dateFormat.parse(dateTo);
-            Date date = dateFormat.parse(dateToCheck);
-            return date.getTime() >= from.getTime() && date.getTime() <= to.getTime();
-        } catch (ParseException e) {
-            throw new RuntimeException("Date doesn't match " + pattern + " pattern ", e);
+    private int getNameIndex(String[] names, String nameToCheck) {
+        for (int i = 0; i < names.length; i++) {
+            if (names[i].equals(nameToCheck)) {
+                return i;
+            }
         }
+        return -1;
+    }
+
+    private boolean isDateInRange(String dateToCheck, String dateFromString, String dateToString) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_PATTERN);
+        LocalDate dateFrom = LocalDate.parse(dateFromString, formatter);
+        LocalDate dateTo = LocalDate.parse(dateToString, formatter);
+        LocalDate date = LocalDate.parse(dateToCheck, formatter);
+        return (date.isEqual(dateFrom) || date.isAfter(dateFrom))
+                && (date.isBefore(dateTo) || date.isEqual(dateTo));
     }
 }
