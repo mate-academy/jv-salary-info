d82b2a660943bca4ce3654acff11ac4791697a1a
Inna Diachkina <innadyachkina@gmail.com>
2022-12-06 16:35:10 +0200

    fixed mistakes

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -7,25 +7,29 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        StringBuilder report = new StringBuilder("Report for period ").append(dateFrom)
+        StringBuilder reportBuilder = new StringBuilder("Report for period ").append(dateFrom)
                 .append(" - ").append(dateTo);
-        LocalDate from = parse(dateFrom, FORMATTER);
-        LocalDate to = parse(dateTo, FORMATTER);
+        LocalDate firstDay = parse(dateFrom, FORMATTER);
+        LocalDate lastDay = parse(dateTo, FORMATTER);
         for (String name : names) {
             int salary = 0;
-            for (String daySalary : data) {
-                String[] dataOneDay = daySalary.split(" ");
-                LocalDate day = parse(dataOneDay[0], FORMATTER);
-                if (name.equals(dataOneDay[1]) && ((day.isAfter(from) || day.equals(from)))
-                        && (day.isBefore(to) || day.equals(to))) {
-                    salary += parseInt(dataOneDay[2]) * parseInt(dataOneDay[3]);
+            for (String line : data) {
+                String[] splittedLine = line.split(" ");
+                String dayOfData = splittedLine[0];
+                String nameOfData = splittedLine[1];
+                String workTime = splittedLine[2];
+                String salaryPerHour = splittedLine[3];
+                LocalDate day = parse(dayOfData, FORMATTER);
+                if (name.equals(nameOfData) && ((day.isAfter(firstDay) || day.equals(firstDay)))
+                        && (day.isBefore(lastDay) || day.equals(lastDay))) {
+                    salary += parseInt(workTime) * parseInt(salaryPerHour);
                 }
             }
-            report.append(System.lineSeparator()).append(name).append(" - ").append(salary);
+            reportBuilder.append(System.lineSeparator()).append(name).append(" - ").append(salary);
         }
-        return report.toString();
+        return reportBuilder.toString();
     }
 }
