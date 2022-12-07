50f890e4d97818f213fa5b8061ede4d41e7685f3
Andrei_Bondarenko <bondorol007@gmail.com>
2022-12-07 14:48:23 +0200

    salary info4.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -9,6 +9,7 @@ public class SalaryInfo {
     private static final int EMPLOYEE_NAME = 1;
     private static final int WORK_HOURS = 2;
     private static final int SALARY_HOUR = 3;
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder builder = new StringBuilder("Report for period " + dateFrom + " - " + dateTo);
         LocalDate localDateFrom = LocalDate.parse(dateFrom, FORMATTER);
