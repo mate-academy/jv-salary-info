17c272b70c5a18d74518ad3d231859450e571990
Inna Diachkina <innadyachkina@gmail.com>
2022-12-07 16:57:08 +0200

    added static fields

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -8,6 +8,10 @@ import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int DAY_OF_DATA = 0;
+    private static final int NAME_OF_DATA = 1;
+    private static final int WORK_TIME = 2;
+    private static final int SALARY_PER_HOUR = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder reportBuilder = new StringBuilder("Report for period ").append(dateFrom)
@@ -18,14 +22,12 @@ public class SalaryInfo {
             int salary = 0;
             for (String line : data) {
                 String[] splittedLine = line.split(" ");
-                String dayOfData = splittedLine[0];
-                String nameOfData = splittedLine[1];
-                String workTime = splittedLine[2];
-                String salaryPerHour = splittedLine[3];
-                LocalDate day = parse(dayOfData, FORMATTER);
-                if (name.equals(nameOfData) && ((day.isAfter(firstDay) || day.equals(firstDay)))
-                        && (day.isBefore(lastDay) || day.equals(lastDay))) {
-                    salary += parseInt(workTime) * parseInt(salaryPerHour);
+                LocalDate day = parse(splittedLine[DAY_OF_DATA], FORMATTER);
+                if (name.equals(splittedLine[NAME_OF_DATA]) && ((day.isAfter(firstDay)
+                        || day.equals(firstDay))) && (day.isBefore(lastDay)
+                        || day.equals(lastDay))) {
+                    salary += parseInt(splittedLine[WORK_TIME])
+                            * parseInt(splittedLine[SALARY_PER_HOUR]);
                 }
             }
             reportBuilder.append(System.lineSeparator()).append(name).append(" - ").append(salary);
