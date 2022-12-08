1537a9187ad4434aa46d373252388f299ff24921
mykoladymchenko <mykola.dymchenko@insightsoftware.com>
2022-12-08 11:15:03 +0200

    Added task implementation in the class SalaryInfo: - getSalaryInfo() method implemented the main logic of the task - getParseDate() method converted String to Date - addOneDayDateTo() - added one to the dateFrom - overriding toString method because I had a problem with the displaying data in the loop (test 2: getSalaryInfoByMonth())

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,65 @@
 package core.basesyntax;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class SalaryInfo {
+    private Date newDateTo;
+    private int salary = 0;
+    private String stringResult = "";
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        try {
+            stringResult = "Report for period " + dateFrom + " - " + dateTo;
+            Date newDateFrom = getParseDate(dateFrom);
+            newDateTo = getParseDate(dateTo);
+
+            if (!dateFrom.equals(dateTo)) {
+                addOneDayDateTo();
+            }
+
+            for (int i = 0; i <= names.length; i++) {
+                if (i > 0) {
+                    stringResult += "\n" + names[i - 1] + " - " + salary;
+                    salary = 0;
+                    if (i == names.length) {
+                        salary = 0;
+                        break;
+                    }
+                }
+                for (int j = 0; j < data.length; j++) {
+                    String[] tempArray = data[j].split(" ");
+                    Date employeeDate = getParseDate(tempArray[0]);
+                    if (names[i].equals(tempArray[1])) {
+                        if (employeeDate.after(newDateFrom)
+                                && employeeDate.before(newDateTo)) {
+                            salary += Integer.parseInt(tempArray[2])
+                                    * Integer.parseInt(tempArray[3]);
+                        }
+                    }
+                }
+            }
+            return stringResult;
+        } catch (ParseException exp) {
+            throw new RuntimeException("Wrong date", exp);
+        }
+    }
+
+    private static Date getParseDate(String dateFrom) throws ParseException {
+        return new SimpleDateFormat("dd.MM.yyyy").parse(dateFrom);
+    }
+
+    private void addOneDayDateTo() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(newDateTo);
+        calendar.add(Calendar.DAY_OF_MONTH, 1);
+        newDateTo = calendar.getTime();
+    }
+
+    @Override
+    public String toString() {
+        return stringResult;
     }
 }
