40d735d873c1a997f409a9099453da5a05a99052
mykoladymchenko <mykola.dymchenko@insightsoftware.com>
2022-12-08 20:50:04 +0200

    added fixes after review: remove ParseException from the methods, create salary variable in the loop, remove reset salary variable, created constant variable for the array index

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,52 +1,47 @@
 package core.basesyntax;
 
-import java.text.ParseException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    public static final int indexOfArray = 0;
     public static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        try {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Report for period ")
+                .append(dateFrom)
+                .append(" - ")
+                .append(dateTo);
+        LocalDate newDateFrom = getParseDate(dateFrom);
+        LocalDate newDateTo = getParseDate(dateTo);
+
+        if (!dateFrom.equals(dateTo)) {
+            newDateTo = newDateTo.plusDays(1);
+        }
+
+        for (int i = 0; i < names.length; i++) {
             int salary = 0;
-            StringBuilder stringBuilder = new StringBuilder();
-            stringBuilder.append("Report for period ")
-                    .append(dateFrom)
-                    .append(" - ")
-                    .append(dateTo);
-            LocalDate newDateFrom = getParseDate(dateFrom);
-            LocalDate newDateTo = getParseDate(dateTo);
-
-            if (!dateFrom.equals(dateTo)) {
-                newDateTo = newDateTo.plusDays(1);
-            }
-
-            for (int i = 0; i < names.length; i++) {
-                for (int j = 0; j < data.length; j++) {
-                    String[] tempArray = data[j].split(" ");
-                    LocalDate employeeDate = getParseDate(tempArray[0]);
-                    if (names[i].equals(tempArray[1])) {
-                        if (employeeDate.isAfter(newDateFrom)
-                                && employeeDate.isBefore(newDateTo)) {
-                            salary += Integer.parseInt(tempArray[2])
-                                    * Integer.parseInt(tempArray[3]);
-                        }
+            for (int j = 0; j < data.length; j++) {
+                String[] tempArray = data[j].split(" ");
+                LocalDate employeeDate = getParseDate(tempArray[indexOfArray]);
+                if (names[i].equals(tempArray[indexOfArray + 1])) {
+                    if (employeeDate.isAfter(newDateFrom)
+                            && employeeDate.isBefore(newDateTo)) {
+                        salary += Integer.parseInt(tempArray[indexOfArray + 2])
+                                * Integer.parseInt(tempArray[indexOfArray + 3]);
                     }
                 }
-                stringBuilder.append("\n")
-                        .append(names[i])
-                        .append(" - ")
-                        .append(salary);
-                salary = 0;
             }
-            return stringBuilder.toString();
-        } catch (ParseException exp) {
-            throw new RuntimeException("Wrong date", exp);
+            stringBuilder.append("\n")
+                    .append(names[i])
+                    .append(" - ")
+                    .append(salary);
         }
+        return stringBuilder.toString();
     }
 
-    private LocalDate getParseDate(String dateFrom) throws ParseException {
+    private LocalDate getParseDate(String dateFrom) {
         return LocalDate.parse(dateFrom, formatter);
     }
 }
