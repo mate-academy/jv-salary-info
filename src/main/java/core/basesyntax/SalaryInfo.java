55aae8cfffab8bf2b2a36a026f1e59d2a7ad7fb5
Ihor Vynogradskyi <avassorv@gmail.com>
2022-12-09 13:02:31 +0200

    bugs are fixed

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,31 +4,30 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private static final DateTimeFormatter OUR_FORMAT = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
     private static final int INDEX_OF_DATE = 0;
     private static final int INDEX_OF_NAME = 1;
     private static final int INDEX_OF_AMOUNT_OF_HOURS = 2;
     private static final int INDEX_OF_INCOME_PER_HOUR = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        StringBuilder stringBuilder = new StringBuilder("Report for period ");
-        stringBuilder.append(dateFrom).append(" - ").append(dateTo);
+        StringBuilder reportBuilder = new StringBuilder("Report for period ");
+        reportBuilder.append(dateFrom).append(" - ").append(dateTo);
         for (String name: names) {
             int salary = 0;
-            for (String workInfo: data) {
-                String[] dateOfWork = workInfo.split(" ");
-                if (name.equals(dateOfWork[INDEX_OF_NAME])) {
-                    if (!LocalDate.parse(dateOfWork[INDEX_OF_DATE], OUR_FORMAT)
-                            .isBefore(LocalDate.parse(dateFrom, OUR_FORMAT))
-                            && !LocalDate.parse(dateOfWork[INDEX_OF_DATE], OUR_FORMAT)
-                            .isAfter(LocalDate.parse(dateTo, OUR_FORMAT))) {
-                        salary += Integer.parseInt(dateOfWork[INDEX_OF_AMOUNT_OF_HOURS])
-                                * Integer.parseInt(dateOfWork[INDEX_OF_INCOME_PER_HOUR]);
+            for (String line: data) {
+                String[] splitteddLine = line.split(" ");
+                LocalDate dateOfWork = LocalDate.parse(splitteddLine[INDEX_OF_DATE], FORMATTER);
+                if (name.equals(splitteddLine[INDEX_OF_NAME])) {
+                    if (!dateOfWork.isBefore(LocalDate.parse(dateFrom, FORMATTER))
+                            && !dateOfWork.isAfter(LocalDate.parse(dateTo, FORMATTER))) {
+                        salary += Integer.parseInt(splitteddLine[INDEX_OF_AMOUNT_OF_HOURS])
+                                * Integer.parseInt(splitteddLine[INDEX_OF_INCOME_PER_HOUR]);
                     }
                 }
             }
-            stringBuilder.append(System.lineSeparator()).append(name).append(" - ").append(salary);
+            reportBuilder.append(System.lineSeparator()).append(name).append(" - ").append(salary);
         }
-        return stringBuilder.toString();
+        return reportBuilder.toString();
     }
 }
