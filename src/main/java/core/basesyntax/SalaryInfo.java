89bbf503f28750f61451cc0a8053b3d7508a5572
Ihor Vynogradskyi <avassorv@gmail.com>
2022-12-09 12:21:33 +0200

    codestyle is fixed

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,27 +1,34 @@
 package core.basesyntax;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo)
-            throws ParseException {
-        SimpleDateFormat df = new SimpleDateFormat("dd.MM.yyyy");
-        String report = "Report for period " + dateFrom + " - " + dateTo;
+    private static final DateTimeFormatter OUR_FORMAT = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int INDEX_OF_DATE = 0;
+    private static final int INDEX_OF_NAME = 1;
+    private static final int INDEX_OF_AMOUNT_OF_HOURS = 2;
+    private static final int INDEX_OF_INCOME_PER_HOUR = 3;
 
-        for (int i = 0; i < names.length; i++) {
+    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
+        StringBuilder stringBuilder = new StringBuilder("Report for period ");
+        stringBuilder.append(dateFrom).append(" - ").append(dateTo);
+        for (String name: names) {
             int salary = 0;
-            for (int k = 0; k < data.length; k++) {
-                String[] workInfo = data[k].split(" ");
-                if (names[i].equals(workInfo[1])) {
-                    if (!df.parse(workInfo[0]).before(df.parse(dateFrom))
-                            && !df.parse(workInfo[0]).after(df.parse(dateTo))) {
-                        salary += Integer.parseInt(workInfo[2]) * Integer.parseInt(workInfo[3]);
+            for (String workInfo: data) {
+                String[] dateOfWork = workInfo.split(" ");
+                if (name.equals(dateOfWork[INDEX_OF_NAME])) {
+                    if (!LocalDate.parse(dateOfWork[INDEX_OF_DATE], OUR_FORMAT)
+                            .isBefore(LocalDate.parse(dateFrom, OUR_FORMAT))
+                            && !LocalDate.parse(dateOfWork[INDEX_OF_DATE], OUR_FORMAT)
+                            .isAfter(LocalDate.parse(dateTo, OUR_FORMAT))) {
+                        salary += Integer.parseInt(dateOfWork[INDEX_OF_AMOUNT_OF_HOURS])
+                                * Integer.parseInt(dateOfWork[INDEX_OF_INCOME_PER_HOUR]);
                     }
                 }
             }
-            report += System.lineSeparator() + names[i] + " - " + salary;
+            stringBuilder.append(System.lineSeparator()).append(name).append(" - ").append(salary);
         }
-        return report;
+        return stringBuilder.toString();
     }
 }
