ccb1f7121c99b43767a0fc1ae121528a2523b9c2
Alina Smirnova <0970352020a@gmail.com>
2022-12-10 13:57:30 +0300

    jv-salary-info fixed

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -2,15 +2,15 @@ package core.basesyntax;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
 
 public class SalaryInfo {
-    private final DateTimeFormatter MY_DATE_FORMAT = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        LocalDate dateFromFormat = LocalDate.parse(dateFrom, MY_DATE_FORMAT);
-        LocalDate dateToFormat = LocalDate.parse(dateTo, MY_DATE_FORMAT);
+
+        LocalDate dateFromFormat = LocalDate.parse(dateFrom, DATE_FORMAT);
+        LocalDate dateToFormat = LocalDate.parse(dateTo, DATE_FORMAT);
         String userInfo = "";
         StringBuilder builder = new StringBuilder();
 
@@ -20,16 +20,16 @@ public class SalaryInfo {
             int totalSalary = 0;
             for (int j = 0; j < data.length; j++) {
                 String[] arrDataSplit = data[j].split(" ");
-                final LocalDate INPUT_DATE = LocalDate.parse(arrDataSplit[0], MY_DATE_FORMAT);
+                final LocalDate INPUT_DATE = LocalDate.parse(arrDataSplit[0], DATE_FORMAT);
                 final String INPUT_NAME = arrDataSplit[1];
-                final int INPUT_WORKING_DAYS = Integer.parseInt(arrDataSplit[2]);
+                final int INPUT_DAYS = Integer.parseInt(arrDataSplit[2]);
                 final int SALARY_RATE = Integer.parseInt(arrDataSplit[3]);
                 if (name.equals(INPUT_NAME) && (
                         INPUT_DATE.isAfter(dateFromFormat)
                                 && INPUT_DATE.isBefore(dateToFormat)
                                 || INPUT_DATE.equals(dateFromFormat)
                                 || INPUT_DATE.equals(dateToFormat))) {
-                    totalSalary += INPUT_WORKING_DAYS * SALARY_RATE;
+                    totalSalary += INPUT_DAYS * SALARY_RATE;
                 }
                 userInfo = (name + " - " + totalSalary);
             }
