1bec3aba245b3b1718610558565b788eeb364e3a
Alina Smirnova <0970352020a@gmail.com>
2022-12-10 13:51:56 +0300

    jv-salary-info fixed

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,44 +5,36 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
 public class SalaryInfo {
+    private final DateTimeFormatter MY_DATE_FORMAT = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        try {
-            LocalDate dateFromFormat = getParsedDate(dateFrom);
-            LocalDate dateToFormat = getParsedDate(dateTo);
-            String userInfo = "";
-            StringBuilder strBld = new StringBuilder();//to append result
-            String result = "";
+        LocalDate dateFromFormat = LocalDate.parse(dateFrom, MY_DATE_FORMAT);
+        LocalDate dateToFormat = LocalDate.parse(dateTo, MY_DATE_FORMAT);
+        String userInfo = "";
+        StringBuilder builder = new StringBuilder();
 
-            strBld.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
+        builder.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
 
-            for (int i = 0; i < names.length; i++) {
-                int totalSalary = 0;
-                for (int j = 0; j < data.length; j++) {
-                    String[] arrDataSplit = data[j].split(" ");
-                    LocalDate userDate = getParsedDate(arrDataSplit[0]);
-                    if (names[i].equals(arrDataSplit[1]) && (
-                            userDate.isAfter(dateFromFormat)
-                                    && userDate.isBefore(dateToFormat)
-                                    || userDate.equals(dateFromFormat)
-                                    || userDate.equals(dateToFormat))) {
-                        totalSalary += (Integer.parseInt(arrDataSplit[2]))
-                                * (Integer.parseInt(arrDataSplit[3]));
-                    }
-                    userInfo = (names[i] + " - " + totalSalary);
+        for (String name: names) {
+            int totalSalary = 0;
+            for (int j = 0; j < data.length; j++) {
+                String[] arrDataSplit = data[j].split(" ");
+                final LocalDate INPUT_DATE = LocalDate.parse(arrDataSplit[0], MY_DATE_FORMAT);
+                final String INPUT_NAME = arrDataSplit[1];
+                final int INPUT_WORKING_DAYS = Integer.parseInt(arrDataSplit[2]);
+                final int SALARY_RATE = Integer.parseInt(arrDataSplit[3]);
+                if (name.equals(INPUT_NAME) && (
+                        INPUT_DATE.isAfter(dateFromFormat)
+                                && INPUT_DATE.isBefore(dateToFormat)
+                                || INPUT_DATE.equals(dateFromFormat)
+                                || INPUT_DATE.equals(dateToFormat))) {
+                    totalSalary += INPUT_WORKING_DAYS * SALARY_RATE;
                 }
-                strBld.append(System.lineSeparator()).append(userInfo);
+                userInfo = (name + " - " + totalSalary);
             }
-
-            result = strBld.toString();
-            System.out.println(result);
-            return result;
-        } catch (DateTimeParseException e) {
-            throw new RuntimeException("Wrong date format", e);
+            builder.append(System.lineSeparator()).append(userInfo);
         }
-    }
-
-    private LocalDate getParsedDate(String dateForm) throws DateTimeParseException {
-        DateTimeFormatter myDateFormat = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        return LocalDate.parse(dateForm, myDateFormat);
+        return builder.toString();
     }
 }
