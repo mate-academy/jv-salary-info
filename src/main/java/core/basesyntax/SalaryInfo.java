18dec42fc6208c4cb66fc1c329201dd04ff80fc0
Alina Smirnova <0970352020a@gmail.com>
2022-12-10 14:41:16 +0300

    jv-salary-info fixed

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,31 +5,30 @@ import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     private final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-
+    private final static int INDEX_ZERO = 0;
+    private final static int INDEX_ONE = 1;
+    private final static int INDEX_TWO = 2;
+    private final static int INDEX_THREE = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-
         LocalDate dateFromFormat = LocalDate.parse(dateFrom, DATE_FORMAT);
         LocalDate dateToFormat = LocalDate.parse(dateTo, DATE_FORMAT);
+
         String userInfo = "";
         StringBuilder builder = new StringBuilder();
-
         builder.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
-
         for (String name : names) {
             int totalSalary = 0;
             for (int j = 0; j < data.length; j++) {
                 String[] arrDataSplit = data[j].split(" ");
-                final LocalDate INPUT_DATE = LocalDate.parse(arrDataSplit[0], DATE_FORMAT);
-                final String INPUT_NAME = arrDataSplit[1];
-                final int INPUT_DAYS = Integer.parseInt(arrDataSplit[2]);
-                final int SALARY_RATE = Integer.parseInt(arrDataSplit[3]);
-                if (name.equals(INPUT_NAME) && (
-                        INPUT_DATE.isAfter(dateFromFormat)
-                                && INPUT_DATE.isBefore(dateToFormat)
-                                || INPUT_DATE.equals(dateFromFormat)
-                                || INPUT_DATE.equals(dateToFormat))) {
-                    totalSalary += INPUT_DAYS * SALARY_RATE;
+                LocalDate userDate = LocalDate.parse(arrDataSplit[INDEX_ZERO], DATE_FORMAT);
+                if (name.equals(arrDataSplit[INDEX_ONE]) && (
+                        userDate.isAfter(dateFromFormat)
+                                && userDate.isBefore(dateToFormat)
+                                || userDate.equals(dateFromFormat)
+                                || userDate.equals(dateToFormat))) {
+                    totalSalary += (Integer.parseInt(arrDataSplit[INDEX_TWO]))
+                            * (Integer.parseInt(arrDataSplit[INDEX_THREE]));
                 }
                 userInfo = (name + " - " + totalSalary);
             }
