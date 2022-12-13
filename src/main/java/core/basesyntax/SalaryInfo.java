e46df99a06c5a4ac03fb71df1bf13d9c4d0e8ce7
Dmitriy Krasnopir <dmitriy.krasnopir@gmail.com>
2022-12-13 14:15:32 +0200

    mistakes was fixed

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -8,9 +8,9 @@ import java.time.format.DateTimeFormatter;
 public class SalaryInfo {
     private static final int DAY_INDEX = 0;
     private static final int NAME_INDEX = 1;
-    private static final int WORKING_HOURS = 2;
-    private static final int SALARY_PER_HOUR = 3;
-    private DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int WORKING_HOURS_INDEX = 2;
+    private static final int SALARY_PER_HOUR_INDEX = 3;
+    private final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate dateFormatFrom = parse(dateFrom, dateTimeFormatter);
@@ -22,15 +22,15 @@ public class SalaryInfo {
         for (String name : names) {
             int totalSalary = 0;
             for (String n : data) {
-                String[] s = n.split(" ");
-                LocalDate day = parse(s[DAY_INDEX], dateTimeFormatter);
-                if (s[NAME_INDEX].equals(name)) {
+                String[] oneDate = n.split(" ");
+                LocalDate day = parse(oneDate[DAY_INDEX], dateTimeFormatter);
+                if (oneDate[NAME_INDEX].equals(name)) {
                     if ((day.isAfter(dateFormatFrom)
                             || day.isEqual(dateFormatFrom))
                             && (day.isBefore(dateFormatTo)
                             || day.isEqual(dateFormatTo))) {
-                        totalSalary += Integer.parseInt(s[WORKING_HOURS])
-                                * Integer.parseInt(s[SALARY_PER_HOUR]);
+                        totalSalary += Integer.parseInt(oneDate[WORKING_HOURS_INDEX])
+                                * Integer.parseInt(oneDate[SALARY_PER_HOUR_INDEX]);
                     }
                 }
             }
@@ -39,7 +39,6 @@ public class SalaryInfo {
                     .append(" - ")
                     .append(totalSalary);
             totalSalary = 0;
-
         }
         return builder.toString();
     }
