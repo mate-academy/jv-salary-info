4954286a367bb0305e0f852748fc9e2c92c9db9c
Denis Pugovkin <den.pugovkin@gmail.com>
2022-12-14 18:30:23 +0200

    1. Method for return Integer from String date was created. 2. Method for return Integer from String income was created. 3. getSalaryInfo method was created.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,30 @@
 package core.basesyntax;
 
 public class SalaryInfo {
+    private int dateInt(String date) {
+        String[] dataArray = date.split("\\.");
+        return Integer.parseInt(dataArray[2] + dataArray[1] + dataArray[0]);
+    }
+
+    private int incomeToInt(String income) {
+        return Integer.parseInt(income);
+    }
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        StringBuilder builder = new StringBuilder();
+        builder.append("Report for period ").append(dateFrom).append(" - ")
+                .append(dateTo);
+        for (String name : names) {
+            int income = 0;
+            for (String datForSplit : data) {
+                String[] dat = datForSplit.split(" ");
+                if (name.equals(dat[1]) && dateInt(dat[0]) >= dateInt(dateFrom)
+                        && dateInt(dat[0]) <= dateInt(dateTo)) {
+                    income = income + incomeToInt(dat[3]) * incomeToInt(dat[2]);
+                }
+            }
+            builder.append(System.lineSeparator()).append(name).append(" - ").append(income);
+        }
+        return builder.toString();
     }
 }
