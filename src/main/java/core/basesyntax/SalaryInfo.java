0242520b5fd5706d4feda18d0344d5b231329bc8
Denis Pugovkin <den.pugovkin@gmail.com>
2022-12-15 01:36:50 +0200

    1. Removed private method after public. 2. Renamed 'dat' variable. 3. Created constants for arrays indexes. 4.Separated method for convert String to Integer was deleted. 5. Renamed dateInt to parseDateToInt.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,30 +1,33 @@
 package core.basesyntax;
 
 public class SalaryInfo {
-    private int dateInt(String date) {
-        String[] dataArray = date.split("\\.");
-        return Integer.parseInt(dataArray[2] + dataArray[1] + dataArray[0]);
-    }
-
-    private int incomeToInt(String income) {
-        return Integer.parseInt(income);
-    }
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder builder = new StringBuilder();
         builder.append("Report for period ").append(dateFrom).append(" - ")
                 .append(dateTo);
+        final int index0 = 0;
+        final int index1 = 1;
+        final int index2 = 2;
+        final int index3 = 3;
         for (String name : names) {
             int income = 0;
-            for (String datForSplit : data) {
-                String[] dat = datForSplit.split(" ");
-                if (name.equals(dat[1]) && dateInt(dat[0]) >= dateInt(dateFrom)
-                        && dateInt(dat[0]) <= dateInt(dateTo)) {
-                    income = income + incomeToInt(dat[3]) * incomeToInt(dat[2]);
+            for (String dataForSplit : data) {
+                String[] dataArray = dataForSplit.split(" ");
+                if (name.equals(dataArray[index1]) && parseDateToInt(dataArray[index0])
+                        >= parseDateToInt(dateFrom) && parseDateToInt(dataArray[index0])
+                        <= parseDateToInt(dateTo)) {
+                    income = income + Integer.parseInt(dataArray[index3])
+                            * Integer.parseInt(dataArray[index2]);
                 }
             }
             builder.append(System.lineSeparator()).append(name).append(" - ").append(income);
         }
         return builder.toString();
+    }
+
+    private int parseDateToInt(String date) {
+        String[] dateArray = date.split("\\.");
+        return Integer.parseInt(dateArray[2] + dateArray[1] + dateArray[0]);
     }
 }
