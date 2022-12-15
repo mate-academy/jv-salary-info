30619d9c820635bc3f596c7aed464929f807dec4
Denis Pugovkin <den.pugovkin@gmail.com>
2022-12-15 22:00:30 +0200

    1. Replaced constants from method to class. 2. Changed parseToInt method and comparison of dates on LocalDate. 3. Replaced each .append from new line.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,33 +1,39 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+    private final int index0 = 0;
+    private final int index1 = 1;
+    private final int index2 = 2;
+    private final int index3 = 3;
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
+        LocalDate dateFromLD = LocalDate.parse(dateFrom,formatter).minusDays(1);
+        LocalDate dateToLD = LocalDate.parse(dateTo,formatter).plusDays(1);
         StringBuilder builder = new StringBuilder();
-        builder.append("Report for period ").append(dateFrom).append(" - ")
+        builder.append("Report for period ")
+                .append(dateFrom)
+                .append(" - ")
                 .append(dateTo);
-        final int index0 = 0;
-        final int index1 = 1;
-        final int index2 = 2;
-        final int index3 = 3;
         for (String name : names) {
             int income = 0;
             for (String dataForSplit : data) {
                 String[] dataArray = dataForSplit.split(" ");
-                if (name.equals(dataArray[index1]) && parseDateToInt(dataArray[index0])
-                        >= parseDateToInt(dateFrom) && parseDateToInt(dataArray[index0])
-                        <= parseDateToInt(dateTo)) {
+                LocalDate date = LocalDate.parse(dataArray[index0],formatter);
+                if (name.equals(dataArray[index1]) && dateFromLD.isBefore(date)
+                        && dateToLD.isAfter(date)) {
                     income = income + Integer.parseInt(dataArray[index3])
                             * Integer.parseInt(dataArray[index2]);
                 }
             }
-            builder.append(System.lineSeparator()).append(name).append(" - ").append(income);
+            builder.append(System.lineSeparator())
+                    .append(name)
+                    .append(" - ")
+                    .append(income);
         }
         return builder.toString();
-    }
-
-    private int parseDateToInt(String date) {
-        String[] dateArray = date.split("\\.");
-        return Integer.parseInt(dateArray[2] + dateArray[1] + dateArray[0]);
     }
 }
