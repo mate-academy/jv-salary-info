21696d397237fcc932e3c0cb9ca7520c95e90ee2
Roman Usyk <roman.usik.93@ukr.net>
2022-12-16 11:50:16 +0200

    the mistakes were corrected

---
+++ src/main/java/core/basesyntax/EmployeeWorkingDay.java
@@ -0,0 +1,46 @@
+package core.basesyntax;
+
+import java.time.LocalDate;
+
+public class EmployeeWorkingDay {
+    private LocalDate date;
+    private String name;
+    private int numberOfHours;
+    private int salaryPerHour;
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumberOfHours() {
+        return numberOfHours;
+    }
+
+    public void setNumberOfHours(int numberOfHours) {
+        this.numberOfHours = numberOfHours;
+    }
+
+    public int getSalaryPerHour() {
+        return salaryPerHour;
+    }
+
+    public void setSalaryPerHour(int salaryPerHour) {
+        this.salaryPerHour = salaryPerHour;
+    }
+
+    public int getSalaryPerDay() {
+        return getSalaryPerHour() * getNumberOfHours();
+    }
+}
--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -6,59 +6,61 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SalaryInfo {
-    private static final String PATTERN_FOR_DATE = "dd.MM.yyyy";
+    private static final DateTimeFormatter DATE_TIME_FORMATTER =
+            DateTimeFormatter.ofPattern("dd.MM.yyyy");
     private static final String TITLE_FOR_REPORT = "Report for period ";
     private static final String DASH_FOR_REPORT = " - ";
     private static final String NEW_LINE = System.lineSeparator();
     private static final String STRING_FOR_SPLIT = " ";
+    private static final int NUMBER_FOR_CORRECTLY_DATE = 1;
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int NUMBER_OF_HOURS_INDEX = 2;
+    private static final int SALARY_PER_HOUR = 3;
 
-    public static String getSalaryInfo(String[] names, String[] data,
-                                       String dateFrom, String dateTo) {
-        LocalDate fromDate = LocalDate.parse(dateFrom,
-                DateTimeFormatter.ofPattern(PATTERN_FOR_DATE));
-        LocalDate toDate = LocalDate.parse(dateTo, DateTimeFormatter.ofPattern(PATTERN_FOR_DATE));
-        List<PeoplesSalary> peoplesSalaries = writeDataToListPeoplesSalary(data);
-        System.out.println(getSalary(names, peoplesSalaries, fromDate, toDate));
+    public String getSalaryInfo(String[] names, String[] data,
+                                String dateFrom, String dateTo) {
+        LocalDate fromDate = LocalDate.parse(dateFrom, DATE_TIME_FORMATTER);
+        LocalDate toDate = LocalDate.parse(dateTo, DATE_TIME_FORMATTER);
+        List<EmployeeWorkingDay> peoplesSalaries = parseEmployeeWorkingDay(data);
         return getSalary(names, peoplesSalaries, fromDate, toDate);
     }
 
-    private static String getSalary(String[] names, List<PeoplesSalary> peoplesSalaries,
-                                    LocalDate fromDate, LocalDate toDate) {
+    private String getSalary(String[] names, List<EmployeeWorkingDay> peoplesSalaries,
+                             LocalDate fromDate, LocalDate toDate) {
         StringBuilder report = new StringBuilder(TITLE_FOR_REPORT);
-        report.append(fromDate.format(DateTimeFormatter.ofPattern(PATTERN_FOR_DATE)))
+        report.append(fromDate.format(DATE_TIME_FORMATTER))
                 .append(DASH_FOR_REPORT)
-                .append(toDate.format(DateTimeFormatter.ofPattern(PATTERN_FOR_DATE)))
+                .append(toDate.format(DATE_TIME_FORMATTER))
                 .append(NEW_LINE);
-        for (int i = 0; i < names.length; i++) {
-            String name = names[i];
+        for (String name : names) {
             int salary = peoplesSalaries.stream()
                     .filter(s -> name.equals(s.getName())
-                            && s.getWorkDay().isAfter(fromDate.minusDays(1))
-                            && s.getWorkDay().isBefore(toDate.plusDays(1)))
-                    .map(s -> (s.getNumberOfHours() * s.getSalaryForHours()))
+                            && s.getDate().isAfter(fromDate.minusDays(NUMBER_FOR_CORRECTLY_DATE))
+                            && s.getDate().isBefore(toDate.plusDays(NUMBER_FOR_CORRECTLY_DATE)))
+                    .map(EmployeeWorkingDay::getSalaryPerDay)
                     .mapToInt(s -> s).sum();
-            report.append(names[i]).append(DASH_FOR_REPORT).append(salary)
+            report.append(name).append(DASH_FOR_REPORT).append(salary)
                     .append(System.lineSeparator());
         }
         return report.toString().trim();
     }
 
-    private static List<PeoplesSalary> writeDataToListPeoplesSalary(String[] data) {
-        List<PeoplesSalary> salaries = new ArrayList<>();
+    private List<EmployeeWorkingDay> parseEmployeeWorkingDay(String[] data) {
+        List<EmployeeWorkingDay> salaries = new ArrayList<>();
         for (String datum : data) {
             salaries.add(writeDataToPeoplesSalary(datum));
         }
         return salaries;
     }
 
-    private static PeoplesSalary writeDataToPeoplesSalary(String data) {
+    private EmployeeWorkingDay writeDataToPeoplesSalary(String data) {
         String[] dataArray = data.split(STRING_FOR_SPLIT);
-        PeoplesSalary peoplesSalary = new PeoplesSalary();
-        peoplesSalary.setWorkDay(LocalDate.parse(dataArray[0],
-                DateTimeFormatter.ofPattern(PATTERN_FOR_DATE)));
-        peoplesSalary.setName(dataArray[1]);
-        peoplesSalary.setNumberOfHours(Integer.parseInt(dataArray[2]));
-        peoplesSalary.setSalaryForHours(Integer.parseInt(dataArray[3]));
+        EmployeeWorkingDay peoplesSalary = new EmployeeWorkingDay();
+        peoplesSalary.setDate(LocalDate.parse(dataArray[DATE_INDEX], DATE_TIME_FORMATTER));
+        peoplesSalary.setName(dataArray[NAME_INDEX]);
+        peoplesSalary.setNumberOfHours(Integer.parseInt(dataArray[NUMBER_OF_HOURS_INDEX]));
+        peoplesSalary.setSalaryPerHour(Integer.parseInt(dataArray[SALARY_PER_HOUR]));
         return peoplesSalary;
     }
 }
