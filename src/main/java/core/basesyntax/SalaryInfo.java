c1d61ebed3e003157312f764c03df30d180e8ead
Petro Korets <petrkorets@gmail.com>
2022-12-21 18:19:56 +0200

    Removed extra StringBuilder var. Removed INITIALIZATOR of for-loops. Added one more constant for DAYS.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,29 +4,30 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private static final int INITIALIZOR = 0;
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int INDEX_OF_DAY = 0;
     private static final int INDEX_OF_NAME = 1;
     private static final int INDEX_OF_HOURS = 2;
     private static final int INDEX_OF_SALARY = 3;
-    private LocalDate dateOfEmployeeLD = null;
-    private LocalDate dateFromLD = null;
-    private LocalDate dateToLD = null;
-    private StringBuilder stb = null;
-    private StringBuilder stb2 = null;
+    LocalDate dateOfEmployeeLD = null;
 
     public String getSalaryInfo(String[] names,
                                 String[] data,
                                 String dateFrom,
                                 String dateTo) {
-        stb = new StringBuilder();
-        stb2 = new StringBuilder();
-        dateFromLD = LocalDate.parse(dateFrom, FORMATTER).minusDays(1);
-        dateToLD = LocalDate.parse(dateTo, FORMATTER).plusDays(1);
+        StringBuilder reportBuffer = new StringBuilder();
+        LocalDate dateFromLD = LocalDate.parse(dateFrom, FORMATTER).minusDays(1);
+        LocalDate dateToLD = LocalDate.parse(dateTo, FORMATTER).plusDays(1);
         int [] salaryNumbers = new int [names.length];
-        for (int i = INITIALIZOR; i < names.length; i++) {
-            for (int j = INITIALIZOR; j < data.length; j++) {
-                dateOfEmployeeLD = LocalDate.parse(data[j].split(" ")[0], FORMATTER);
+        reportBuffer.append("Report for period ")
+                .append(dateFrom)
+                .append(" ")
+                .append("-")
+                .append(" ")
+                .append(dateTo);
+        for (int i = 0; i < names.length; i++) {
+            for (int j = 0; j < data.length; j++) {
+                dateOfEmployeeLD = LocalDate.parse(data[j].split(" ")[INDEX_OF_DAY], FORMATTER);
                 if ((names[i].equals(data[j].split(" ")[INDEX_OF_NAME]))
                         && ((dateOfEmployeeLD.isAfter(dateFromLD))
                                 && dateOfEmployeeLD.isBefore(dateToLD))) {
@@ -34,20 +35,13 @@ public class SalaryInfo {
                             * Integer.parseInt(data[j].split(" ")[INDEX_OF_SALARY]);
                 }
             }
-            stb2.append(System.lineSeparator())
+            reportBuffer.append(System.lineSeparator())
                     .append(names[i])
                     .append(" ")
                     .append("-")
                     .append(" ")
                     .append(salaryNumbers[i]);
         }
-        stb.append("Report for period ")
-                .append(dateFrom)
-                .append(" ")
-                .append("-")
-                .append(" ")
-                .append(dateTo)
-                .append(stb2);
-        return stb.toString();
+        return reportBuffer.toString();
     }
 }
