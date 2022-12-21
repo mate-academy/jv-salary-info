543298f7d68ddadc7897b28b6b947e73a270f063
Petro Korets <petrkorets@gmail.com>
2022-12-21 10:41:13 +0200

    Edited static modifiers for constants according to JSL. Edited append chaining.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,48 +4,48 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private final static int INITIALIZOR = 0;
-    private final static DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-    private final static int INDEX_OF_NAME = 1;
-    private final static int INDEX_OF_HOURS = 2;
-    private final static int INDEX_OF_SALARY = 3;
+    private static final int INITIALIZOR = 0;
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int INDEX_OF_NAME = 1;
+    private static final int INDEX_OF_HOURS = 2;
+    private static final int INDEX_OF_SALARY = 3;
     private LocalDate dateOfEmployeeLD = null;
     private LocalDate dateFromLD = null;
     private LocalDate dateToLD = null;
+    private StringBuilder stb = new StringBuilder();
+    private StringBuilder stb2 = new StringBuilder();
 
     public String getSalaryInfo(String[] names,
                                 String[] data,
                                 String dateFrom,
                                 String dateTo) {
-        StringBuilder stb = new StringBuilder();
-        StringBuilder stb2 = new StringBuilder();
         dateFromLD = LocalDate.parse(dateFrom, FORMATTER).minusDays(1);
         dateToLD = LocalDate.parse(dateTo, FORMATTER).plusDays(1);
         int [] salaryNumbers = new int [names.length];
         for (int i = INITIALIZOR; i < names.length; i++) {
             for (int j = INITIALIZOR; j < data.length; j++) {
                 dateOfEmployeeLD = LocalDate.parse(data[j].split(" ")[0], FORMATTER);
-                if ((names[i].equals(data[j].split(" ")[INDEX_OF_NAME])) &&
-                        ((dateOfEmployeeLD.isAfter(dateFromLD))
+                if ((names[i].equals(data[j].split(" ")[INDEX_OF_NAME]))
+                        && ((dateOfEmployeeLD.isAfter(dateFromLD))
                                 && dateOfEmployeeLD.isBefore(dateToLD))) {
-                    salaryNumbers[i] += Integer.parseInt(data[j].split(" ")[INDEX_OF_HOURS]) *
-                            Integer.parseInt(data[j].split(" ")[INDEX_OF_SALARY]);
+                    salaryNumbers[i] += Integer.parseInt(data[j].split(" ")[INDEX_OF_HOURS])
+                            * Integer.parseInt(data[j].split(" ")[INDEX_OF_SALARY]);
                 }
             }
-            stb2.append(System.lineSeparator()).
-                    append(names[i]).
-                    append(" ").
-                    append("-").
-                    append(" ").
-                    append(+ salaryNumbers[i]);
+            stb2.append(System.lineSeparator())
+                    .append(names[i])
+                    .append(" ")
+                    .append("-")
+                    .append(" ")
+                    .append(+ salaryNumbers[i]);
         }
-        stb.append("Report for period ").
-                append(dateFrom).
-                append(" ").
-                append("-").
-                append(" ").
-                append(dateTo).
-                append(stb2);
+        stb.append("Report for period ")
+                .append(dateFrom)
+                .append(" ")
+                .append("-")
+                .append(" ")
+                .append(dateTo)
+                .append(stb2);
         return stb.toString();
     }
 }
