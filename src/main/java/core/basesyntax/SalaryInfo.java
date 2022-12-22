4b394df31ea5fe6145336d1bc2cc7f1c4ac57cd1
Volodymyr Gaievsky <mr.gvvd@gmail.com>
2022-12-23 00:25:16 +0200

    Fix of request issues#1.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,7 +4,7 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private static final int PLUS_ONE_DAY = 1;
+    private static final int ONE_DAY = 1;
     private static final int DATE_IN_DATA = 0;
     private static final int NAME_IN_DATA = 1;
     private static final int HOURS_IN_DATA = 2;
@@ -20,8 +20,8 @@ public class SalaryInfo {
 
         for (String name : names) {
             int salary = 0;
-            for (String date : data) {
-                String[] dataInput = date.split(" ");
+            for (String inputValue : data) {
+                String[] dataInput = inputValue.split(" ");
                 if (name.equals(dataInput[NAME_IN_DATA])
                         && isBetween(dataInput[DATE_IN_DATA], dateFrom, dateTo)) {
                     salary += Integer.parseInt(dataInput[HOURLY_INCOME_IN_DATA])
@@ -33,11 +33,10 @@ public class SalaryInfo {
         return result.toString();
     }
 
-    private boolean isBetween(String data, String dateFrom, String dateTo) {
-        String[] inputData = data.split(" ");
-        return LocalDate.parse(inputData[0], DATE_TIME_FORMATTER)
-               .isAfter(LocalDate.parse(dateFrom,DATE_TIME_FORMATTER).minusDays(PLUS_ONE_DAY))
-               && LocalDate.parse(inputData[0], DATE_TIME_FORMATTER)
-               .isBefore(LocalDate.parse(dateTo,DATE_TIME_FORMATTER).plusDays(PLUS_ONE_DAY));
+    private boolean isBetween(String inputDate, String dateFrom, String dateTo) {
+        return LocalDate.parse(inputDate, DATE_TIME_FORMATTER)
+               .isAfter(LocalDate.parse(dateFrom,DATE_TIME_FORMATTER).minusDays(ONE_DAY))
+               && LocalDate.parse(inputDate, DATE_TIME_FORMATTER)
+               .isBefore(LocalDate.parse(dateTo,DATE_TIME_FORMATTER).plusDays(ONE_DAY));
     }
 }
