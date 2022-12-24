0e641a7d74fed208ebab2da2eaef86bec482fa4a
Марк Міллер <youtte1g@gmail.com>
2022-12-24 10:18:26 +0200

    created constans, remove Ð one if

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -6,41 +6,31 @@ import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    static final int FIRST_VALUE = 0;
+    static final int SECOND_VALUE = 1;
+    static final int THIRD_VALUE = 2;
+    static final int FOUR_VALUE = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate fromThisDate = LocalDate.parse(dateFrom, FORMATTER);
         LocalDate toThisDate = LocalDate.parse(dateTo, FORMATTER);
         StringBuilder builder = new StringBuilder();
         for (String name : names) {
-            String testString = "";
             int employeeSalary = 0;
             for (String dates : data) {
                 String[] split = dates.split(" ");
-                LocalDate date = LocalDate.parse(split[0], FORMATTER);
-                if (date.isEqual(fromThisDate) || date.isEqual(toThisDate)
-                        || date.isAfter(fromThisDate) && date.isBefore(toThisDate)) {
-                    if (name.equals(split[1])) {
-                        int workedHours = Integer.parseInt(split[2]);
-                        int payPerHour = Integer.parseInt(split[3]);
-                        int result = workedHours * payPerHour;
-                        employeeSalary += result;
-                        if (!testString.equals(name)) {
-                            testString += name;
-                            builder.append(System.lineSeparator())
-                                    .append(name)
-                                    .append(" - ");
-                        }
-                    }
-                } else {
-                    if (!testString.equals(name)) {
-                        testString += name;
-                        builder.append(System.lineSeparator())
-                                .append(name)
-                                .append(" - ");
-                    }
+                LocalDate date = LocalDate.parse(split[FIRST_VALUE], FORMATTER);
+                if (name.equals(split[SECOND_VALUE])
+                        && (date.isEqual(fromThisDate) || date.isEqual(toThisDate)
+                        || date.isAfter(fromThisDate) && date.isBefore(toThisDate))) {
+                    int workedHours = Integer.parseInt(split[THIRD_VALUE]);
+                    int payPerHour = Integer.parseInt(split[FOUR_VALUE]);
+                    int result = workedHours * payPerHour;
+                    employeeSalary += result;
                 }
             }
-            builder.append(employeeSalary);
+            builder.append(System.lineSeparator()).append(name).append(" - ")
+                    .append(employeeSalary);
         }
         return "Report for period " + dateFrom + " - " + dateTo + builder;
     }
