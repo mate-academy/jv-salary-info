5b2bce10b310794e4d112614086cf1db73579b10
Марк Міллер <youtte1g@gmail.com>
2022-12-25 22:47:55 +0200

    renamed constans, variables, optimized StringBuilder

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -6,25 +6,25 @@ import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-    static final int FIRST_VALUE = 0;
-    static final int SECOND_VALUE = 1;
-    static final int THIRD_VALUE = 2;
-    static final int FOUR_VALUE = 3;
+    static final int DATE_INDEX = 0;
+    static final int NAME_INDEX = 1;
+    static final int HOURS_INDEX = 2;
+    static final int INCOME_PER_HOURS_INDEX = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate fromThisDate = LocalDate.parse(dateFrom, FORMATTER);
         LocalDate toThisDate = LocalDate.parse(dateTo, FORMATTER);
-        StringBuilder builder = new StringBuilder();
+        StringBuilder builder = new StringBuilder("Report for period " + dateFrom + " - " + dateTo);
         for (String name : names) {
             int employeeSalary = 0;
-            for (String dates : data) {
-                String[] split = dates.split(" ");
-                LocalDate date = LocalDate.parse(split[FIRST_VALUE], FORMATTER);
-                if (name.equals(split[SECOND_VALUE])
+            for (String line : data) {
+                String[] split = line.split(" ");
+                LocalDate date = LocalDate.parse(split[DATE_INDEX], FORMATTER);
+                if (name.equals(split[NAME_INDEX])
                         && (date.isEqual(fromThisDate) || date.isEqual(toThisDate)
                         || date.isAfter(fromThisDate) && date.isBefore(toThisDate))) {
-                    int workedHours = Integer.parseInt(split[THIRD_VALUE]);
-                    int payPerHour = Integer.parseInt(split[FOUR_VALUE]);
+                    int workedHours = Integer.parseInt(split[HOURS_INDEX]);
+                    int payPerHour = Integer.parseInt(split[INCOME_PER_HOURS_INDEX]);
                     int result = workedHours * payPerHour;
                     employeeSalary += result;
                 }
@@ -32,7 +32,7 @@ public class SalaryInfo {
             builder.append(System.lineSeparator()).append(name).append(" - ")
                     .append(employeeSalary);
         }
-        return "Report for period " + dateFrom + " - " + dateTo + builder;
+        return builder.toString();
     }
 }
 
