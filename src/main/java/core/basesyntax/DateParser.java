5f0139dc13679acfdf226a03266a9e253884bc2c
IlkoGood <ilko.engineering@gmail.com>
2022-12-27 16:06:07 +0200

    Salary calc completed

---
+++ src/main/java/core/basesyntax/DateParser.java
@@ -0,0 +1,19 @@
+package core.basesyntax;
+
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.text.ParseException;
+
+public class DateParser {
+    public Date date(String date) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM.yyyy");
+        Date correctDate = new Date();
+// try-catch may be rewritten, here is just a simple plug for current task
+        try {
+            correctDate = simpleDateFormat.parse(date);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+        return correctDate;
+    }
+}
--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,29 @@
 package core.basesyntax;
 
+import java.util.Date;
+
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        DateParser dateParser = new DateParser();
+        Date startDate = dateParser.date(dateFrom);
+        Date stopDate = dateParser.date(dateTo);
+        int [] salaries = new int[names.length];
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < names.length; i++) {
+            for (String datum : data) {
+                if (names[i].equals(datum.split(" ")[1]) &&
+                        dateParser.date(datum.split(" ")[0]).compareTo(startDate) >= 0 &&
+                        dateParser.date(datum.split(" ")[0]).compareTo(stopDate) <= 0) {
+                    salaries[i] += Integer.parseInt(datum.split(" ")[2]) *
+                            Integer.parseInt(datum.split(" ")[3]);
+                }
+            }
+            sb.append(names[i]).append(" - ").append(salaries[i]);
+            if (i < names.length - 1) {
+                sb.append(System.lineSeparator());
+            }
+        }
+        return "Report for period " + dateFrom + " - " + dateTo +
+                System.lineSeparator() + sb;
     }
 }
