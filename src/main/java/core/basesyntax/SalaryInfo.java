a5b8b0592d93209ab269d5f89abb84b9aac608a6
Hryhorii Chernolutskyi <iamchgi1976@gmail.com>
2023-01-02 20:49:59 +0200

    moved several operators , create index constants

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -6,24 +6,28 @@ import java.time.format.DateTimeFormatter;
 public class SalaryInfo {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int HOURS_INDEX = 2;
+    private static final int SALARY_PER_HOUR_INDEX = 3;
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder report = new StringBuilder();
-        int employSalary;
+        report.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
         LocalDate localDateTo = LocalDate.parse(dateTo, FORMATTER);
         LocalDate localDateFrom = LocalDate.parse(dateFrom, FORMATTER);
         String[] parsedEmployData;
         LocalDate localEmployDate;
-        report.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
         for (String employName : names) {
-            employSalary = 0;
+            int employSalary = 0;
             for (String employData : data) {
                 parsedEmployData = employData.split(" ");
-                if (employName.equals(parsedEmployData[1])) {
-                    localEmployDate = LocalDate.parse(parsedEmployData[0], FORMATTER);
+                if (employName.equals(parsedEmployData[NAME_INDEX])) {
+                    localEmployDate = LocalDate.parse(parsedEmployData[DATE_INDEX], FORMATTER);
                     if ((localEmployDate.compareTo(localDateFrom) >= 0)
                             & (localEmployDate.compareTo(localDateTo) <= 0)) {
-                        employSalary += Integer.parseInt(parsedEmployData[2])
-                                * Integer.parseInt(parsedEmployData[3]);
+                        employSalary += Integer.parseInt(parsedEmployData[HOURS_INDEX])
+                                * Integer.parseInt(parsedEmployData[SALARY_PER_HOUR_INDEX]);
                     }
                 }
             }
