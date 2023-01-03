261b80df26a32e111e11e36102abff4dbf2166f3
Myhailo Mykhasov <mikhailmikhasev@gmail.com>
2023-01-03 20:29:13 +0200

    Removed redundunt classes, used LocalDate in Salary info class.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,36 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        final LocalDate dateLast = LocalDate.parse(dateTo, formatter);
+        final LocalDate dateFirst = LocalDate.parse(dateFrom, formatter);
+        StringBuilder sb = new StringBuilder();
+        int[] income = new int[names.length];
+        String[] temp = new String[4];
+        sb.append("Report for period " + dateFrom + " - " + dateTo + System.lineSeparator());
+        for (int j = 0; j < names.length; j++) {
+            for (int i = 0; i < data.length; i++) {
+                temp = data[i].split(" ");
+                if ((temp[1].equals(names[j]))
+                        && (LocalDate.parse(temp[0], formatter).isAfter(dateFirst)
+                        || LocalDate.parse(temp[0], formatter).isEqual(dateFirst))
+                        && (LocalDate.parse(temp[0], formatter).isBefore(dateLast)
+                        || LocalDate.parse(temp[0], formatter).isEqual(dateLast))) {
+                    income[j] += Integer.parseInt(temp[2]) * Integer.parseInt(temp[3]);
+                }
+            }
+            sb.append(names[j] + " - " + income[j]);
+            if (j == names.length - 1) {
+                break;
+            }
+            sb.append(System.lineSeparator());
+        }
+
+        return sb.toString();
     }
 }
