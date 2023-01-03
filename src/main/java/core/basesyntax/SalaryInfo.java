0d2bffc1c03bd8e54d23e20a72d020adca619ea5
Oleksandr Kravchenko <oleksandr.kravchenko@spirent.com>
2023-01-03 22:14:27 +0200

    feat: jv-salary-info #comment fixed code after review

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -2,38 +2,33 @@ package core.basesyntax;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
 
 public class SalaryInfo {
-    private static final String FORMAT = "dd.MM.yyyy";
-    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern(FORMAT);
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int HOURS_INDEX = 2;
+    private static final int INCOME_INDEX = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         int[] salaryCounter = new int[names.length];
-        try {
-            LocalDate localDateFrom = LocalDate.parse(dateFrom, formatter);
-            LocalDate localDateTo = LocalDate.parse(dateTo, formatter);
+        LocalDate localDateFrom = LocalDate.parse(dateFrom, formatter);
+        LocalDate localDateTo = LocalDate.parse(dateTo, formatter);
 
-            for (String record : data) {
-                String[] splitRecord = record.split(" ");
-                LocalDate date = LocalDate.parse(splitRecord[0], formatter);
+        for (String record : data) {
+            String[] splitRecord = record.split(" ");
+            LocalDate date = LocalDate.parse(splitRecord[DATE_INDEX], formatter);
 
-                if (!localDateFrom.isAfter(date) && !localDateTo.isBefore(date)) {
-                    for (int i = 0; i < names.length; i++) {
-                        if (names[i].equals(splitRecord[1])) {
-                            salaryCounter[i] +=
-                              Integer.parseInt(splitRecord[2]) * Integer.parseInt(splitRecord[3]);
-                            break;
-                        }
+            if (!localDateFrom.isAfter(date) && !localDateTo.isBefore(date)) {
+                for (int i = 0; i < names.length; i++) {
+                    if (names[i].equals(splitRecord[NAME_INDEX])) {
+                        salaryCounter[i] +=
+                          Integer.parseInt(splitRecord[HOURS_INDEX])
+                            * Integer.parseInt(splitRecord[INCOME_INDEX]);
+                        break;
                     }
-
                 }
             }
-
-        } catch (DateTimeParseException e) {
-            System.out.println("Can not parse date");
-        } catch (NumberFormatException e) {
-            System.out.println("Can not parse salary values");
         }
 
         StringBuilder builder = new StringBuilder("Report for period ")
