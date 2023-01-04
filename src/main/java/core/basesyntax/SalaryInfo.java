7326411f8080f1ad7c506e4670cec011118e241c
Myhailo Mykhasov <mikhailmikhasev@gmail.com>
2023-01-04 23:44:49 +0200

    Made changhes according to comments.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,33 +4,41 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    public static final String DASH = " - ";
+    public static final String REGEX = " ";
+    public static final String INTRO = "Report for period ";
+    public static final int DATE_INDEX = 0;
+    public static final int NAME_INDEX = 1;
+    public static final int HOURS_INDEX = 2;
+    public static final int INCOME_INDEX = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        final LocalDate dateLast = LocalDate.parse(dateTo, formatter);
-        final LocalDate dateFirst = LocalDate.parse(dateFrom, formatter);
-        StringBuilder sb = new StringBuilder();
+        final LocalDate dateLast = LocalDate.parse(dateTo, FORMATTER);
+        final LocalDate dateFirst = LocalDate.parse(dateFrom, FORMATTER);
+        StringBuilder stringBuilder = new StringBuilder();
         int[] income = new int[names.length];
-        String[] temp = new String[4];
-        sb.append("Report for period " + dateFrom + " - " + dateTo + System.lineSeparator());
+        String[] dataRow;
+        stringBuilder.append(INTRO + dateFrom + DASH + dateTo + System.lineSeparator());
         for (int j = 0; j < names.length; j++) {
             for (int i = 0; i < data.length; i++) {
-                temp = data[i].split(" ");
-                if ((temp[1].equals(names[j]))
-                        && (LocalDate.parse(temp[0], formatter).isAfter(dateFirst)
-                        || LocalDate.parse(temp[0], formatter).isEqual(dateFirst))
-                        && (LocalDate.parse(temp[0], formatter).isBefore(dateLast)
-                        || LocalDate.parse(temp[0], formatter).isEqual(dateLast))) {
-                    income[j] += Integer.parseInt(temp[2]) * Integer.parseInt(temp[3]);
+                dataRow = data[i].split(REGEX);
+                if ((dataRow[NAME_INDEX].equals(names[j]))
+                        && (LocalDate.parse(dataRow[DATE_INDEX], FORMATTER).isAfter(dateFirst)
+                        || LocalDate.parse(dataRow[DATE_INDEX], FORMATTER).isEqual(dateFirst))
+                        && (LocalDate.parse(dataRow[DATE_INDEX], FORMATTER).isBefore(dateLast)
+                        || LocalDate.parse(dataRow[DATE_INDEX], FORMATTER).isEqual(dateLast))) {
+                    income[j] += Integer.parseInt(dataRow[HOURS_INDEX])
+                        * Integer.parseInt(dataRow[INCOME_INDEX]);
                 }
             }
-            sb.append(names[j] + " - " + income[j]);
+            stringBuilder.append(names[j] + DASH + income[j]);
             if (j == names.length - 1) {
                 break;
             }
-            sb.append(System.lineSeparator());
+            stringBuilder.append(System.lineSeparator());
         }
 
-        return sb.toString();
+        return stringBuilder.toString();
     }
 }
