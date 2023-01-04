164b416f52953a71770dd16603d13f008ce3ced4
Denys Dominskyi <dominskyj@gmail.com>
2023-01-04 16:13:45 +0200

    Solution1

---
+++ src/main/java/core/basesyntax/Info.java
@@ -0,0 +1,25 @@
+package core.basesyntax;
+
+import java.util.Date;
+
+public class Info {
+    String[] information;
+
+    public Info(String[] information) {
+        this.information = information;
+    }
+
+    void splitInfo(String[] information) {
+        for (int i = 0; i < information.length; i++) {
+            String[] dates = information[i].split(" ");
+
+        }
+
+
+
+    }
+
+
+}
+
+
--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,40 @@
 package core.basesyntax;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        int[] res = new int[names.length];
+        DateFormat format = new SimpleDateFormat("dd.MM.yyyy");
+        Date date1;
+        Date date2;
+        try {
+            date1 = format.parse(dateFrom);
+            date2 = format.parse(dateTo);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+        for (int i = 0; i < names.length; i++) {
+            for (String datum : data) {
+                String[] dates = datum.split(" ");
+                try {
+                    if (names[i].equals(dates[1]) && format.parse(dates[0]).getTime() >= date1.getTime()
+                            && format.parse(dates[0]).getTime() <= date2.getTime()) {
+                        res[i] += (Integer.parseInt(dates[2]) * Integer.parseInt(dates[3]));
+                    }
+                } catch (ParseException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+        String result = "Report for period " + dateFrom + " - " + dateTo;
+        for (int i = 0; i < res.length; i++) {
+            result += "\r\n" + names[i] + " - " + res[i];
+        }
+        return result.trim();
     }
 }
+
