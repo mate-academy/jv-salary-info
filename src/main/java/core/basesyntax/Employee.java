ce18d3ec342f59845dc0d0b21a69b3261ef0f377
Igor Parovenko <igorparovenko@yahoo.com>
2023-01-04 19:28:04 +0100

    Solution without Stringbuilder

---
+++ src/main/java/core/basesyntax/Employee.java
@@ -0,0 +1,28 @@
+package core.basesyntax;
+
+public class Employee {
+    private final String name;
+    private int salary;
+
+    public Employee(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return name + "-" + salary + System.lineSeparator();
+    }
+
+}
--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,38 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.util.Arrays;
+
 public class SalaryInfo {
+    static final String REPORT_PERIOD_MESSAGE = "Report for period: ";
+    static final DateTimeFormatter formatter = new DateTimeFormatterBuilder().appendPattern("dd.MM.yyyy").toFormatter();
+
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        LocalDate startDate = LocalDate.parse(dateFrom, formatter);
+        LocalDate endDate = LocalDate.parse(dateTo, formatter);
+        Employee[] employees = new Employee[names.length];
+        for (int i = 0; i < names.length; i++) {
+            employees[i] = new Employee(names[i]);
+        }
+        for (String d:data) {
+            String[] split = d.split(" ");
+            LocalDate arrayDate = LocalDate.parse(split[0], formatter);
+            if (arrayDate.isAfter(startDate) && arrayDate.isBefore(endDate) | arrayDate.isEqual(endDate)) {
+                for (Employee employee : employees) {
+                    if (split[1].equals(employee.getName())) {
+                        employee.setSalary(employee.getSalary()
+                                + Integer.parseInt(split[2]) * Integer.parseInt(split[3]));
+                    }
+                }
+          }
+        }
+        return  REPORT_PERIOD_MESSAGE + dateFrom + " - "  + dateTo
+                + System.lineSeparator()
+                + Arrays.toString(employees).replace("[", "")
+                .replace("]", "").replace(",", "");
     }
+
 }
