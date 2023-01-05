faffcf4ee5ca04db0a84ddc0190168d61c6a079e
Igor Parovenko <igorparovenko@yahoo.com>
2023-01-05 18:24:34 +0100

    solution simplified

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,36 +5,32 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeFormatterBuilder;
 
 public class SalaryInfo {
-    static final String REPORT_PERIOD_MSG = "Report for period ";
-    static final DateTimeFormatter formatter = new DateTimeFormatterBuilder()
+    private static final String REPORT_PERIOD_MSG = "Report for period ";
+    private static final DateTimeFormatter formatter = new DateTimeFormatterBuilder()
             .appendPattern("dd.MM.yyyy").toFormatter();
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate startDate = LocalDate.parse(dateFrom, formatter);
         LocalDate endDate = LocalDate.parse(dateTo, formatter);
-        Employee[] employees = new Employee[names.length];
         StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < names.length; i++) {
-            employees[i] = new Employee(names[i]);
-        }
-        for (String d:data) {
-            String[] split = d.split(" ");
+        int[] salaries = new int[names.length];
+        for (String array:data) {
+            String[] split = array.split(" ");
             LocalDate arrayDate = LocalDate.parse(split[0], formatter);
             if (arrayDate.isAfter(startDate)
                     && arrayDate.isBefore(endDate) | arrayDate.isEqual(endDate)) {
-                for (Employee employee : employees) {
-                    if (split[1].equals(employee.getName())) {
-                        employee.setSalary(employee.getSalary()
-                                + Integer.parseInt(split[2]) * Integer.parseInt(split[3]));
+                for (int i = 0; i < names.length; i++) {
+                    if (names[i].equals(split[1])) {
+                        salaries[i] += Integer.parseInt(split[2]) * Integer.parseInt(split[3]);
                     }
                 }
             }
         }
-        for (Employee employee:employees) {
-            stringBuilder.append(employee);
+        for (int i = 0; i < names.length; i++) {
+            stringBuilder.append(System.lineSeparator()).append(names[i])
+                    .append(" - ").append(salaries[i]);
         }
-        return REPORT_PERIOD_MSG + dateFrom + " - " + dateTo
-                + System.lineSeparator() + stringBuilder.substring(0, stringBuilder.length() - 2);
+        return REPORT_PERIOD_MSG + dateFrom + " - " + dateTo + stringBuilder;
     }
 }
 
