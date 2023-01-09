94a4634a985f57f354b79a254e54a5a6b02f02b9
Ilia Budenkov <i.budenkov@gmail.com>
2023-01-09 09:33:25 +0200

    Added constants, fixed requirements from mentor

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,29 +1,36 @@
 package core.basesyntax;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
 public class SalaryInfo {
     private static final String FORMAT_STRING = "dd.MM.yyyy";
+    private static final String DELIMITER = " ";
+    private static final int DATA_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int HOUR_INDEX = 2;
+    private static final int SALARY_INDEX = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder builder = new StringBuilder();
-        final DataParserFormatter dataParser = new DataParserFormatter(FORMAT_STRING);
+        DateTimeFormatter dateParserFormatter = DateTimeFormatter.ofPattern(FORMAT_STRING);
         try {
-            LocalDate localDateFrom = dataParser.parseDate(dateFrom);
-            LocalDate localDateTo = dataParser.parseDate(dateTo);
-            builder.append("Report for period ").append(dataParser.formatDate(localDateFrom))
-            .append(" - ").append(dataParser.formatDate(localDateTo));
+            LocalDate localDateFrom = LocalDate.parse(dateFrom, dateParserFormatter);
+            LocalDate localDateTo = LocalDate.parse(dateTo, dateParserFormatter);
+            builder.append("Report for period ").append(localDateFrom.format(dateParserFormatter))
+            .append(" - ").append(localDateTo.format(dateParserFormatter));
             for (String name: names) {
                 int totalSum = 0;
-                for (String row: data) {
-                    String [] input = row.split(" ");
-                    if (name.equals(input[1])) {
-                        LocalDate inputDate = dataParser.parseDate(input[0]);
+                for (String row : data) {
+                    String [] input = row.split(DELIMITER);
+                    if (name.equals(input[NAME_INDEX])) {
+                        LocalDate inputDate = LocalDate.parse(input[DATA_INDEX],
+                                dateParserFormatter);
                         if (inputDate.compareTo(localDateFrom) >= 0
                                 && inputDate.compareTo(localDateTo) <= 0) {
-                            totalSum += dataParser.parseInt(input[2])
-                                                * dataParser.parseInt(input[3]);
+                            totalSum += Integer.valueOf(input[HOUR_INDEX])
+                                                * Integer.valueOf(input[SALARY_INDEX]);
                         }
                     }
                 }
