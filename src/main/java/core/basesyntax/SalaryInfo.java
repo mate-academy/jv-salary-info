f0050ba35896d9ea39725b4fd2fb25e9edc6b235
Yaroslav Ometiukh <yaroslav.ometiukh@outlook.com>
2023-01-11 19:05:46 +0200

    +created constants, the order of loops was changed

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,31 +4,31 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private final DateTimeFormatter format = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int WORKED_DATE = 0;
+    private static final int EMPLOYEE_NAME = 1;
+    private static final int WORKED_HOURS = 2;
+    private static final int RATE_PER_HOUR = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
+        LocalDate fromDate = LocalDate.parse(dateFrom, DATE_FORMAT);
+        LocalDate toDate = LocalDate.parse(dateTo, DATE_FORMAT);
+        String[] workSchedule;
         StringBuilder sb = new StringBuilder();
-        int[] totalSalary = new int[names.length];
-        String[] employeeData;
-        LocalDate fromDate = LocalDate.parse(dateFrom, format);
-        LocalDate toDate = LocalDate.parse(dateTo, format);
-        for (String employee : data
-        ) {
-            employeeData = employee.split(" ", 4);
-            LocalDate currentDate = LocalDate.parse(employeeData[0], format);
-            for (int i = 0; i < names.length; i++) {
-                if (employeeData[1].equals(names[i])
-                        & !currentDate.isBefore(fromDate)
-                        & !currentDate.isAfter(toDate)) {
-                    totalSalary[i] += (Integer.parseInt(employeeData[2])
-                            * Integer.parseInt(employeeData[3]));
+        sb.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
+        for (String implName : names) {
+            int totalSalary = 0;
+            for (String implData : data) {
+                workSchedule = implData.split(" ", 4);
+                LocalDate checkDate = LocalDate.parse(workSchedule[WORKED_DATE], DATE_FORMAT);
+                if (implName.equals(workSchedule[EMPLOYEE_NAME])
+                        && !checkDate.isBefore(fromDate)
+                        && !checkDate.isAfter(toDate)) {
+                    totalSalary += (Integer.parseInt(workSchedule[WORKED_HOURS])
+                            * Integer.parseInt(workSchedule[RATE_PER_HOUR]));
                 }
             }
-        }
-        sb.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
-        for (int i = 0; i < names.length; i++) {
-            sb.append(System.lineSeparator()).append(names[i])
-                    .append(" - ").append(totalSalary[i]);
+            sb.append(System.lineSeparator()).append(implName).append(" - ").append(totalSalary);
         }
         return sb.toString();
     }
