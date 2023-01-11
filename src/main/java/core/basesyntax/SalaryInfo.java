30c16ac19da57d820f8574b376d383b9ed5e53e6
Yaroslav Ometiukh <yaroslav.ometiukh@outlook.com>
2023-01-11 21:12:38 +0200

    Edited according to comments

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,30 +5,30 @@ import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-    private static final int WORKED_DATE = 0;
-    private static final int EMPLOYEE_NAME = 1;
-    private static final int WORKED_HOURS = 2;
-    private static final int RATE_PER_HOUR = 3;
+    private static final int WORKED_DATE_INDEX = 0;
+    private static final int EMPLOYEE_NAME_INDEX = 1;
+    private static final int WORKED_HOURS_INDEX = 2;
+    private static final int RATE_PER_HOUR_INDEX = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate fromDate = LocalDate.parse(dateFrom, DATE_FORMAT);
         LocalDate toDate = LocalDate.parse(dateTo, DATE_FORMAT);
         String[] workSchedule;
-        StringBuilder sb = new StringBuilder();
-        sb.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
-        for (String implName : names) {
+        StringBuilder sb = new StringBuilder().append("Report for period ")
+                .append(dateFrom).append(" - ").append(dateTo);
+        for (String name : names) {
             int totalSalary = 0;
-            for (String implData : data) {
-                workSchedule = implData.split(" ", 4);
-                LocalDate checkDate = LocalDate.parse(workSchedule[WORKED_DATE], DATE_FORMAT);
-                if (implName.equals(workSchedule[EMPLOYEE_NAME])
+            for (String line : data) {
+                workSchedule = line.split(" ", 4);
+                LocalDate checkDate = LocalDate.parse(workSchedule[WORKED_DATE_INDEX], DATE_FORMAT);
+                if (name.equals(workSchedule[EMPLOYEE_NAME_INDEX])
                         && !checkDate.isBefore(fromDate)
                         && !checkDate.isAfter(toDate)) {
-                    totalSalary += (Integer.parseInt(workSchedule[WORKED_HOURS])
-                            * Integer.parseInt(workSchedule[RATE_PER_HOUR]));
+                    totalSalary += (Integer.parseInt(workSchedule[WORKED_HOURS_INDEX])
+                            * Integer.parseInt(workSchedule[RATE_PER_HOUR_INDEX]));
                 }
             }
-            sb.append(System.lineSeparator()).append(implName).append(" - ").append(totalSalary);
+            sb.append(System.lineSeparator()).append(name).append(" - ").append(totalSalary);
         }
         return sb.toString();
     }
