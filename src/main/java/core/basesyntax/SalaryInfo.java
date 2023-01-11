fe0f79dd68cfbab163292cc04adb81e115c7f830
Anna Melnychuk <annamelnichuk2512@gmail.com>
2023-01-11 18:41:02 +0200

    Fixed mistakes, make constants and change return

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -7,24 +7,34 @@ import java.time.format.DateTimeFormatter;
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        LocalDate dateFrom1 = LocalDate.parse(dateFrom, formatter);
-        LocalDate dateTo1 = LocalDate.parse(dateTo, formatter);
-        StringBuilder report = new StringBuilder();
+        LocalDate currentDateFrom = LocalDate.parse(dateFrom, formatter);
+        LocalDate currentDateTo = LocalDate.parse(dateTo, formatter);
+        StringBuilder report = new StringBuilder("Report for period ")
+                .append(dateFrom)
+                .append(" - ")
+                .append(dateTo);
+        final String White_Space = " ";
+        final int Date_Index = 0;
+        final int Name_Index = 1;
+        final int Hours_Index = 2;
+        final int Salary_Index = 3;
 
         for (String name : names) {
             int salaryCount = 0;
             for (String date : data) {
-                String[] splitData = date.split(" ");
-                LocalDate currentDate = LocalDate.parse(splitData[0], formatter);
-                if (splitData[1].equals(name)
-                        && ((currentDate.isAfter(dateFrom1)) || (currentDate.isEqual(dateFrom1)))
-                        && (currentDate.isBefore(dateTo1) || currentDate.isEqual(dateTo1))) {
-                    salaryCount = salaryCount + Integer.parseInt(splitData[2])
-                            * Integer.parseInt(splitData[3]);
+                String[] splitData = date.split(White_Space);
+                final LocalDate currentDate = LocalDate.parse(splitData[Date_Index], formatter);
+                if (splitData[Name_Index].equals(name)
+                        && ((currentDate.isAfter(currentDateFrom))
+                        || (currentDate.isEqual(currentDateFrom)))
+                        && (currentDate.isBefore(currentDateTo)
+                        || currentDate.isEqual(currentDateTo))) {
+                    salaryCount = salaryCount + Integer.parseInt(splitData[Hours_Index])
+                            * Integer.parseInt(splitData[Salary_Index]);
                 }
             }
             report.append(System.lineSeparator()).append(name).append(" - ").append(salaryCount);
         }
-        return "Report for period " + dateFrom + " - " + dateTo + report;
+        return report.toString();
     }
 }
