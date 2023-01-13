fba6bf9becddc4797105e5b60275f5e74814c49b
Kostia Vershynin <vershynin.kostia@gmail.com>
2023-01-13 20:43:06 +0200

    fixed realization getSalaryInfo

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,18 +4,27 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int ARRAY_INDEX_ZERO = 0;
+    private static final int ARRAY_INDEX_ONE = 1;
+    private static final int ARRAY_INDEX_TWO = 2;
+    private static final int ARRAY_INDEX_TREE = 3;
+    private static final String DELIMITER = " ";
+    private static final String DASH = " - ";
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder result = new StringBuilder("Report for period " + dateFrom + " - " + dateTo);
         int[] salary = new int[names.length];
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        String[] str;
 
         for (String datum : data) {
-            String[] str = datum.split(" ");
-            String name = str[1];
-            LocalDate date = LocalDate.parse(str[0], formatter);
-            LocalDate localDateFrom = LocalDate.parse(dateFrom, formatter);
-            LocalDate localDateTo = LocalDate.parse(dateTo, formatter);
-            int salaryInDay = (Integer.parseInt(str[2]) * Integer.parseInt(str[3]));
+            str = datum.split(DELIMITER);
+            String name = str[ARRAY_INDEX_ONE];
+            LocalDate date = LocalDate.parse(str[ARRAY_INDEX_ZERO], FORMATTER);
+            LocalDate localDateFrom = LocalDate.parse(dateFrom, FORMATTER);
+            LocalDate localDateTo = LocalDate.parse(dateTo, FORMATTER);
+            int salaryInDay = (Integer.parseInt(str[ARRAY_INDEX_TWO]) * Integer.parseInt(str[ARRAY_INDEX_TREE]));
+
             if ((date.isAfter(localDateFrom) || date.isEqual(localDateFrom))
                     && (date.isBefore(localDateTo) || date.isEqual(localDateTo))) {
                 for (int j = 0; j < names.length; j++) {
@@ -28,7 +37,7 @@ public class SalaryInfo {
         for (int i = 0; i < names.length; i++) {
             result.append(System.lineSeparator())
                     .append(names[i])
-                    .append(" - ")
+                    .append(DASH)
                     .append(salary[i]);
         }
         return result.toString();
