a7907ed82435ee6439d3b092d4cc48df9789b870
Kostia Vershynin <vershynin.kostia@gmail.com>
2023-01-13 21:53:28 +0200

    fixed realization getSalaryInfo

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,26 +5,26 @@ import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-    private static final int ARRAY_INDEX_ZERO = 0;
-    private static final int ARRAY_INDEX_ONE = 1;
-    private static final int ARRAY_INDEX_TWO = 2;
-    private static final int ARRAY_INDEX_TREE = 3;
+    private static final int OPERATION_INDEX_ZERO = 0;
+    private static final int OPERATION_INDEX_ONE = 1;
+    private static final int OPERATION_INDEX_TWO = 2;
+    private static final int OPERATION_INDEX_TREE = 3;
     private static final String DELIMITER = " ";
     private static final String DASH = " - ";
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder result = new StringBuilder("Report for period " + dateFrom + " - " + dateTo);
         int[] salary = new int[names.length];
-        String[] str;
+        String[] arrayData;
 
         for (String datum : data) {
-            str = datum.split(DELIMITER);
-            String name = str[ARRAY_INDEX_ONE];
-            LocalDate date = LocalDate.parse(str[ARRAY_INDEX_ZERO], FORMATTER);
+            arrayData = datum.split(DELIMITER);
+            String name = arrayData[OPERATION_INDEX_ONE];
+            LocalDate date = LocalDate.parse(arrayData[OPERATION_INDEX_ZERO], FORMATTER);
             LocalDate localDateFrom = LocalDate.parse(dateFrom, FORMATTER);
             LocalDate localDateTo = LocalDate.parse(dateTo, FORMATTER);
-            int salaryInDay = (Integer.parseInt(str[ARRAY_INDEX_TWO])
-                    * Integer.parseInt(str[ARRAY_INDEX_TREE]));
+            int salaryInDay = (Integer.parseInt(arrayData[OPERATION_INDEX_TWO])
+                    * Integer.parseInt(arrayData[OPERATION_INDEX_TREE]));
 
             if ((date.isAfter(localDateFrom) || date.isEqual(localDateFrom))
                     && (date.isBefore(localDateTo) || date.isEqual(localDateTo))) {
