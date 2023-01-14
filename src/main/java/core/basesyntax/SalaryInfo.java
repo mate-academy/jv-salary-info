1676dca52bfbd9ed164e6a70ebcc04dfc215b585
Kostia Vershynin <vershynin.kostia@gmail.com>
2023-01-14 21:27:40 +0200

    fixed realization getSalaryInfo

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,41 +5,39 @@ import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-    private static final int OPERATION_INDEX_ZERO = 0;
-    private static final int OPERATION_INDEX_ONE = 1;
-    private static final int OPERATION_INDEX_TWO = 2;
-    private static final int OPERATION_INDEX_TREE = 3;
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int HOURS_INDEX = 2;
+    private static final int PER_HOUR_INDEX = 3;
     private static final String DELIMITER = " ";
     private static final String DASH = " - ";
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder result = new StringBuilder("Report for period " + dateFrom + " - " + dateTo);
-        int[] salary = new int[names.length];
         String[] arrayData;
 
-        for (String datum : data) {
-            arrayData = datum.split(DELIMITER);
-            String name = arrayData[OPERATION_INDEX_ONE];
-            LocalDate date = LocalDate.parse(arrayData[OPERATION_INDEX_ZERO], FORMATTER);
-            LocalDate localDateFrom = LocalDate.parse(dateFrom, FORMATTER);
-            LocalDate localDateTo = LocalDate.parse(dateTo, FORMATTER);
-            int salaryInDay = (Integer.parseInt(arrayData[OPERATION_INDEX_TWO])
-                    * Integer.parseInt(arrayData[OPERATION_INDEX_TREE]));
+        for (String nameWorker : names) {
+            int salary = 0;
+            for (String datum : data) {
+                arrayData = datum.split(DELIMITER);
+                String name = arrayData[NAME_INDEX];
+                LocalDate date = LocalDate.parse(arrayData[DATE_INDEX], FORMATTER);
+                LocalDate localDateFrom = LocalDate.parse(dateFrom, FORMATTER);
+                LocalDate localDateTo = LocalDate.parse(dateTo, FORMATTER);
+                int salaryInDay = (Integer.parseInt(arrayData[HOURS_INDEX])
+                        * Integer.parseInt(arrayData[PER_HOUR_INDEX]));
 
-            if ((date.isAfter(localDateFrom) || date.isEqual(localDateFrom))
-                    && (date.isBefore(localDateTo) || date.isEqual(localDateTo))) {
-                for (int j = 0; j < names.length; j++) {
-                    if (names[j].equals(name)) {
-                        salary[j] += salaryInDay;
+                if (nameWorker.equals(name)) {
+                    if ((date.isAfter(localDateFrom) || date.isEqual(localDateFrom))
+                            && (date.isBefore(localDateTo) || date.isEqual(localDateTo))) {
+                        salary += salaryInDay;
                     }
                 }
             }
-        }
-        for (int i = 0; i < names.length; i++) {
             result.append(System.lineSeparator())
-                    .append(names[i])
+                    .append(nameWorker)
                     .append(DASH)
-                    .append(salary[i]);
+                    .append(salary);
         }
         return result.toString();
     }
