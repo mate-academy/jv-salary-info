fd4cce5c9d951cc3297f8d0488720fe4355a5b6e
Andrii Varlanovych <andrew_exp@ukr.net>
2023-01-17 10:28:20 +0200

    changes were added according Aleksandr's request

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -2,52 +2,41 @@ package core.basesyntax;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
 
 public class SalaryInfo {
-    private static final String TITLE_LINE = "Report for period";
-    private static final int DATA_INDEX = 0;
+    private static final DateTimeFormatter DATE_FORMATTER
+            = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final String STRING_FROM_TITLE_LINE = "Report for period";
+    private static final int DATE_INDEX = 0;
     private static final int NAME_INDEX = 1;
-    private static final int WORKING_OUR_INDEX = 2;
+    private static final int WORKING_HOUR_INDEX = 2;
     private static final int INCOME_INDEX = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder stringBuilder = new StringBuilder();
-        return stringBuilder.append(TITLE_LINE)
-                .append(" " + dateFrom + " - " + dateTo)
-                .append(getData(names, data, dateFrom, dateTo)).toString();
-    }
-
-    private String getData(String[] names, String[] data, String dateFrom, String dateTo) {
-        StringBuilder stringBuilder = new StringBuilder();
-        int salary;
+        stringBuilder = stringBuilder.append(STRING_FROM_TITLE_LINE)
+                .append(" " + dateFrom + " - " + dateTo);
 
         for (int i = 0; i < names.length; i++) {
-            salary = 0;
-            stringBuilder.append(System.lineSeparator()).append(names[i]).append(" - ");
+            int salary = 0;
+            stringBuilder.append(System.lineSeparator())
+                    .append(names[i])
+                    .append(" - ");
             for (int j = 0; j < data.length; j++) {
                 String[] valuesFromData = data[j].split(" ");
-                if (getLocalDate(valuesFromData[DATA_INDEX]).isAfter(getLocalDate(dateFrom))
-                        && (getLocalDate(valuesFromData[DATA_INDEX]).isBefore(getLocalDate(dateTo))
-                        || getLocalDate(valuesFromData[DATA_INDEX]).isEqual(getLocalDate(dateTo)))
+                LocalDate dateToCheck = LocalDate.parse(valuesFromData[DATE_INDEX], DATE_FORMATTER);
+                LocalDate dateStart = LocalDate.parse(dateFrom, DATE_FORMATTER);
+                LocalDate dateEnd = LocalDate.parse(dateTo, DATE_FORMATTER);
+
+                if (dateToCheck.isAfter(dateStart)
+                        && (dateToCheck.isBefore(dateEnd) || dateToCheck.isEqual(dateEnd))
                         && valuesFromData[NAME_INDEX].equals(names[i])) {
-                    salary += Integer.parseInt(valuesFromData[WORKING_OUR_INDEX])
+                    salary += Integer.parseInt(valuesFromData[WORKING_HOUR_INDEX])
                             * Integer.parseInt(valuesFromData[INCOME_INDEX]);
                 }
             }
             stringBuilder.append(salary);
         }
         return stringBuilder.toString();
-    }
-
-    private LocalDate getLocalDate(String dataInString) {
-        LocalDate date;
-        try {
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-            date = LocalDate.parse(dataInString, formatter);
-        } catch (DateTimeParseException e) {
-            throw new RuntimeException(dataInString + " is not parsable", e);
-        }
-        return date;
     }
 }
