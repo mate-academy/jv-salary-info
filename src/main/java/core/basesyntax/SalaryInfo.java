e8e66fd4e7a4ec4918e79b6f92ad6d7494fe93ae
slaybrute <slaybrute@gmail.com>
2023-01-17 20:14:17 +0200

    changes after verification

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,6 +1,7 @@
 package core.basesyntax;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 
 public class SalaryInfo {
@@ -17,33 +18,32 @@ public class SalaryInfo {
         StringBuilder stringBuilder = new StringBuilder("Report for period "
                 + dateFrom + " - " + dateTo);
         int[] salary = new int[names.length];
-        for (int i = 0; i < data.length; i++) {
-            String[] generalInfo = data[i].split(" ");
-            String[] dateMain = generalInfo[INDEX_OF_DATA].split("\\.");
-            String[] dateStarted = dateFrom.split("\\.");
-            String[] dateFinished = dateTo.split("\\.");
-            LocalDate mainDate = LocalDate.of(Integer.parseInt(dateMain[INDEX_OF_YEAR]),
-                    Integer.parseInt(dateMain[INDEX_OF_MONTH]),
-                    Integer.parseInt(dateMain[INDEX_OF_DAY]));
-            LocalDate startedDate = LocalDate.of(Integer.parseInt(dateStarted[INDEX_OF_YEAR]),
-                    Integer.parseInt(dateStarted[INDEX_OF_MONTH]),
-                    Integer.parseInt(dateStarted[INDEX_OF_DAY]));
-            LocalDate finishedDate = LocalDate.of(Integer.parseInt(dateFinished[INDEX_OF_YEAR]),
-                    Integer.parseInt(dateFinished[INDEX_OF_MONTH]),
-                    Integer.parseInt(dateFinished[INDEX_OF_DAY]));
-            for (String name : names) {
-                if (name.equals(generalInfo[INDEX_OF_NAME]) && ((mainDate.isAfter(startedDate)
-                        && mainDate.isBefore(finishedDate)) || mainDate.equals(startedDate)
-                        || mainDate.equals(finishedDate))) {
-                    salary[Arrays.asList(names).indexOf(name)]
-                            += Integer.parseInt(generalInfo[INDEX_OF_WORKED_HOURES])
-                            * Integer.parseInt(generalInfo[INDEX_OF_SALARY]);
-                }
-                if (i == data.length - 1) {
-                    stringBuilder.append(System.lineSeparator()).append(name).append(" - ")
-                            .append(salary[Arrays.asList(names).indexOf(name)]);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        try {
+            LocalDate startedDate = LocalDate.parse(dateFrom, formatter);
+            LocalDate finishedDate = LocalDate.parse(dateTo, formatter);
+            for (int i = 0; i < data.length; i++) {
+                String[] generalInfo = data[i].split(" ");
+                String[] dateMain = generalInfo[INDEX_OF_DATA].split("\\.");
+                LocalDate mainDate = LocalDate.of(Integer.parseInt(dateMain[INDEX_OF_YEAR]),
+                        Integer.parseInt(dateMain[INDEX_OF_MONTH]),
+                        Integer.parseInt(dateMain[INDEX_OF_DAY]));
+                for (String name : names) {
+                    if (name.equals(generalInfo[INDEX_OF_NAME]) && ((mainDate.isAfter(startedDate)
+                            && mainDate.isBefore(finishedDate)) || mainDate.equals(startedDate)
+                            || mainDate.equals(finishedDate))) {
+                        salary[Arrays.asList(names).indexOf(name)]
+                                += Integer.parseInt(generalInfo[INDEX_OF_WORKED_HOURES])
+                                * Integer.parseInt(generalInfo[INDEX_OF_SALARY]);
+                    }
+                    if (i == data.length - 1) {
+                        stringBuilder.append(System.lineSeparator()).append(name).append(" - ")
+                                .append(salary[Arrays.asList(names).indexOf(name)]);
+                    }
                 }
             }
+        } catch (RuntimeException e) {
+            throw new RuntimeException("can't parse date", e);
         }
         return stringBuilder.toString();
     }
