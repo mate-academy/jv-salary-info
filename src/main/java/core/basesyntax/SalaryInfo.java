ca8407711c801075c745d4fbc4cbe69731102c01
kromch <94765048+kromch@users.noreply.github.com>
2023-01-18 11:44:57 +0200

    Update SalaryInfo.java

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,32 +1,32 @@
 package core.basesyntax;
 
-import java.text.SimpleDateFormat;
 import java.text.ParseException;
-import java.util.Date;
+import java.text.SimpleDateFormat;
 import java.text.DateFormat;
+import java.util.Date;
 import java.util.Locale;
 
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo)
-    throws ParseException {
+            throws ParseException {
         StringBuilder res = new StringBuilder();
         try {
             SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyyy");
-            Date date_1 = formatter.parse(dateFrom);
-            Date date_2 = formatter.parse(dateTo);
-            if (date_2.compareTo(date_1) < 0) {
+            Date date1 = formatter.parse(dateFrom);
+            Date date2 = formatter.parse(dateTo);
+            if (date2.compareTo(date1) < 0) {
                 throw new ParseException("DateFrom occurs after DateTo", 0);
             }
             DateFormat dateFormat;
             dateFormat = DateFormat.getDateInstance(DateFormat.MEDIUM, Locale.GERMANY);
-            res.append("Report for period " + dateFormat.format(date_1) + " - " + dateFormat.format(date_2) + "\n");
+            res.append("Report for period " + dateFormat.format(date1) + " - " + dateFormat.format(date2) + "\n");
             for (String n: names) {
                 int money = 0;
                 for (String d: data) {
                     var temp = d.split(" ");
-                    if ((date_1.compareTo(formatter.parse(temp[0])) < 0 || date_1.compareTo(formatter.parse(temp[0])) == 0) &&
-                        (date_2.compareTo(formatter.parse(temp[0])) > 0 || date_2.compareTo(formatter.parse(temp[0])) == 0) &&
-                        n.trim().equals(temp[1].trim())) {
+                    if ((date1.compareTo(formatter.parse(temp[0])) < 0 || date1.compareTo(formatter.parse(temp[0])) == 0) 
+                        && (date2.compareTo(formatter.parse(temp[0])) > 0 || date2.compareTo(formatter.parse(temp[0])) == 0)
+                        && n.trim().equals(temp[1].trim())) {
                         money += Integer.parseInt(temp[2]) * Integer.parseInt(temp[3]);
                     }
                 }
