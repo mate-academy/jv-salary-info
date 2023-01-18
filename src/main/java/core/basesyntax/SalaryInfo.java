cf6144b9f569b6df30b0c8b1f8d7bcbfe09e13c9
Yuriy Kononchuk <y_kononchuk@ukr.net>
2023-01-18 11:33:50 +0200

    prepared solution for task salary-info

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,39 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+    protected int totalSalary;
+    protected int workingHours;
+    protected int salaryPerHour;
+    protected LocalDate workingDateFrom;
+    protected LocalDate workingDateTo;
+    protected LocalDate workingDay;
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private final String separator = System.lineSeparator();
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        workingDateFrom = LocalDate.from(formatter.parse(dateFrom));
+        workingDateTo = LocalDate.from(formatter.parse(dateTo));
+        StringBuilder employeeSalary = new StringBuilder("Report for period " + dateFrom
+                + " - " + dateTo);
+        for (String name : names) {
+            totalSalary = 0;
+            for (String dataField : data) {
+                String[] dataFields = dataField.split(" ");
+                workingDay = LocalDate.from(formatter.parse(dataFields[0]));
+                String employeeName = dataFields[1];
+                workingHours = Integer.parseInt(dataFields[2]);
+                salaryPerHour = Integer.parseInt(dataFields[3]);
+                boolean validWorkingDay = workingDay.compareTo(workingDateFrom) >= 0
+                        && workingDay.compareTo(workingDateTo) <= 0;
+                if (name.equals(employeeName) && validWorkingDay) {
+                    totalSalary += workingHours * salaryPerHour;
+                }
+            }
+            employeeSalary.append(separator).append(name).append(" - ").append(totalSalary);
+        }
+        return employeeSalary.toString();
     }
 }
