227adb295bb2ac329463c0f064c4aac459245191
Yuriy Kononchuk <y_kononchuk@ukr.net>
2023-01-19 21:20:58 +0200

    added changes after 1st review

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,28 +4,27 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    protected int totalSalary;
-    protected int workingHours;
-    protected int salaryPerHour;
-    protected LocalDate workingDateFrom;
-    protected LocalDate workingDateTo;
-    protected LocalDate workingDay;
-    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
     private final String separator = System.lineSeparator();
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        workingDateFrom = LocalDate.from(formatter.parse(dateFrom));
-        workingDateTo = LocalDate.from(formatter.parse(dateTo));
+        int totalSalary;
+        LocalDate workingDateFrom = LocalDate.from(FORMATTER.parse(dateFrom));
+        LocalDate workingDateTo = LocalDate.from(FORMATTER.parse(dateTo));
         StringBuilder employeeSalary = new StringBuilder("Report for period " + dateFrom
                 + " - " + dateTo);
         for (String name : names) {
             totalSalary = 0;
             for (String dataField : data) {
+                final int dateIndex = 0;
+                final int nameIndex = 1;
+                final int workingHoursIndex = 2;
+                final int salaryPerHourIndex = 3;
                 String[] dataFields = dataField.split(" ");
-                workingDay = LocalDate.from(formatter.parse(dataFields[0]));
-                String employeeName = dataFields[1];
-                workingHours = Integer.parseInt(dataFields[2]);
-                salaryPerHour = Integer.parseInt(dataFields[3]);
+                LocalDate workingDay = LocalDate.from(FORMATTER.parse(dataFields[dateIndex]));
+                String employeeName = dataFields[nameIndex];
+                int workingHours = Integer.parseInt(dataFields[workingHoursIndex]);
+                int salaryPerHour = Integer.parseInt(dataFields[salaryPerHourIndex]);
                 boolean validWorkingDay = workingDay.compareTo(workingDateFrom) >= 0
                         && workingDay.compareTo(workingDateTo) <= 0;
                 if (name.equals(employeeName) && validWorkingDay) {
