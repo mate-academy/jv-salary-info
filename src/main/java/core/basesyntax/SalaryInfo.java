04afe9dbee196753813bdd421dba234e083ae39c
Artur Muliukov <arthur.muliukov@gmail.com>
2023-01-19 15:17:15 +0200

    Solved 1.1

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,14 +1,21 @@
 package core.basesyntax;
 
+import java.util.Calendar;
+
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder builder =
                 new StringBuilder("Report for period" + " " + dateFrom + " - " + dateTo);
-
         int[] datefromint = info(dateFrom);
         int[] datetoint = info(dateTo);
+        Calendar calendarmin = Calendar.getInstance();
+        calendarmin.set(datefromint[2], datefromint[1], datefromint[0]);
+        Calendar calendarmax = Calendar.getInstance();
+        calendarmax.set(datetoint[2], datetoint[1], datetoint[0]);
+        calendarmax.add(Calendar.DATE, 1);
         String[] datawithsplit = new String[4];
         int[] datadate = new int[3];
+        Calendar calendarX = Calendar.getInstance();
         int startbalance = 0;
 
         for (String name : names) {
@@ -17,9 +24,8 @@ public class SalaryInfo {
                 datawithsplit = datas.split(" ");
                 if (datawithsplit[1].equals(name)) {
                     datadate = info(datawithsplit[0]);
-                    if (datadate[2] <= datetoint[2] && datadate[2] >= datefromint[2]
-                            && datadate[1] <= datetoint[1] && datadate[1] >= datefromint[1]
-                            && datadate[0] <= datetoint[0] && datadate[0] >= datefromint[0]) {
+                    calendarX.set(datadate[2], datadate[1], datadate[0]);
+                    if (calendarX.after(calendarmin) && calendarX.before(calendarmax)){
                         startbalance = startbalance + Integer.parseInt(datawithsplit[2])
                                 * Integer.parseInt(datawithsplit[3]);
                     }
