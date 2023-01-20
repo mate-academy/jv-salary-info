a6b730f613c0773612910ea0e28661fe5c0243f0
Yuriy Kononchuk <y_kononchuk@ukr.net>
2023-01-20 13:31:55 +0200

    fixed after 2nd review: relocated constants to class level, used dedicated methods, system separator used in code not as constant

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,8 +4,11 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int WORKING_HOURS_INDEX = 2;
+    private static final int SALARY_PER_HOUR_INDEX = 3;
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-    private final String separator = System.lineSeparator();
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         int totalSalary;
@@ -16,22 +19,20 @@ public class SalaryInfo {
         for (String name : names) {
             totalSalary = 0;
             for (String dataField : data) {
-                final int dateIndex = 0;
-                final int nameIndex = 1;
-                final int workingHoursIndex = 2;
-                final int salaryPerHourIndex = 3;
                 String[] dataFields = dataField.split(" ");
-                LocalDate workingDay = LocalDate.from(FORMATTER.parse(dataFields[dateIndex]));
-                String employeeName = dataFields[nameIndex];
-                int workingHours = Integer.parseInt(dataFields[workingHoursIndex]);
-                int salaryPerHour = Integer.parseInt(dataFields[salaryPerHourIndex]);
-                boolean validWorkingDay = workingDay.compareTo(workingDateFrom) >= 0
-                        && workingDay.compareTo(workingDateTo) <= 0;
+                LocalDate workingDay = LocalDate.from(FORMATTER.parse(dataFields[DATE_INDEX]));
+                String employeeName = dataFields[NAME_INDEX];
+                int workingHours = Integer.parseInt(dataFields[WORKING_HOURS_INDEX]);
+                int salaryPerHour = Integer.parseInt(dataFields[SALARY_PER_HOUR_INDEX]);
+                boolean validWorkingDay = workingDay.isAfter(workingDateFrom)
+                        && workingDay.isBefore(workingDateTo) || workingDay.isEqual(workingDateFrom)
+                        || workingDay.isEqual(workingDateTo);
                 if (name.equals(employeeName) && validWorkingDay) {
                     totalSalary += workingHours * salaryPerHour;
                 }
             }
-            employeeSalary.append(separator).append(name).append(" - ").append(totalSalary);
+            employeeSalary.append(System.lineSeparator()).append(name).append(" - ")
+                    .append(totalSalary);
         }
         return employeeSalary.toString();
     }
