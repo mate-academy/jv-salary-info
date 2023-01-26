10be5896465cf8f7b78ba706e908ac30e67e488b
Volodymyr Sakalosh <sva2005@ukr.net>
2023-01-26 22:30:12 +0200

    Implemented method getSalaryInfo

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -2,6 +2,22 @@ package core.basesyntax;
 
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        String sallaryInfo = "";
+        int sallary = 0;
+        dateFrom = dateFrom.trim();
+        dateTo = dateTo.trim();
+
+        for (String name : names) {
+            for (String getData : data) {
+                if (getData.contains(name) && (dateFrom.contains(getData.substring(0,10))
+                || dateTo.contains(getData.substring(0, 10))
+                        || dateFrom.contains(getData.substring(3, 10)))) {
+                    sallary += Integer.parseInt(getData.substring(getData
+                            .lastIndexOf(' ') + 1, getData.length()));
+                }
+            }
+            sallaryInfo = name + " - " + sallary + "\n";
+        }
+        return "Report of period " + dateFrom + " - " + dateTo + "\n" + sallaryInfo;
     }
 }
