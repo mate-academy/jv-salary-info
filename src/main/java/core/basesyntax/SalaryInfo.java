c18e41fc888140950a3759f5b9f3342de9ae6611
Ihor Sydorenko <sidorenko.igorek@gmail.com>
2023-01-29 22:12:23 +0200

    valid solution

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,41 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int DATE_INDEX = 0;
+    private static final int EMPLOYEE_INDEX = 1;
+    private static final int WORK_HOURS_INDEX = 2;
+    private static final int SALARY_PER_HOUR_INDEX = 3;
+
+    private LocalDate getParsedDay(String date) {
+        return LocalDate.parse(date, FORMATTER);
+    }
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        StringBuilder reportFromPeriod = new StringBuilder()
+                .append("Report for period ")
+                .append(dateFrom)
+                .append(" - ")
+                .append(dateTo);
+        LocalDate localDateFrom = getParsedDay(dateFrom);
+        LocalDate localDateTo = getParsedDay(dateTo);
+        for (String name : names) {
+            int salary = 0;
+            for (String employeeInfo : data) {
+                String[] employeeSplitInfo = employeeInfo.split(" ");
+                LocalDate foundDate = getParsedDay(employeeSplitInfo[DATE_INDEX]);
+                if (name.equals(employeeSplitInfo[EMPLOYEE_INDEX])
+                        && !foundDate.isAfter(localDateTo)
+                        && !foundDate.isBefore(localDateFrom)) {
+                    salary += Integer.parseInt(employeeSplitInfo[WORK_HOURS_INDEX])
+                            * Integer.parseInt(employeeSplitInfo[SALARY_PER_HOUR_INDEX]);
+                }
+            }
+            reportFromPeriod.append(System.lineSeparator()).append(name).append(" - ").append(salary);
+        }
+        return reportFromPeriod.toString();
     }
 }
