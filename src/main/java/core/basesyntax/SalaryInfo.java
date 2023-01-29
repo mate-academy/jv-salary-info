05ebc66ebf67c8f5de08e81950f16031f0802390
Volodymyr Sakalosh <sva2005@ukr.net>
2023-01-29 12:54:24 +0200

    Replaced substring() with split() and deleted two fields

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,37 +4,37 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    static final StringBuilder STRING_BUILDER = new StringBuilder();
     static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-    private LocalDate localDate;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
+        LocalDate localDate;
         LocalDate localDateFrom = getLocalDate(dateFrom);
         LocalDate localDateTo = getLocalDate(dateTo);
+        StringBuilder builder = new StringBuilder();
         int salary = 0;
-        STRING_BUILDER.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
+        builder.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
 
         for (String name : names) {
             for (String line : data) {
-                localDate = getLocalDate(line.substring(0, 10));
+                localDate = getLocalDate(line);
                 if ((localDateFrom.equals(localDate) || localDate.equals(localDateTo)
                         || (localDate.isAfter(localDateTo) && localDate.isBefore(localDateFrom)))
                         && line.contains(name)) {
                     salary += getSalaryOfDay(line, name);
                 }
             }
-            STRING_BUILDER.append(System.lineSeparator()).append(name).append(" - ").append(salary);
+            builder.append(System.lineSeparator()).append(name).append(" - ").append(salary);
         }
-        return STRING_BUILDER.toString();
+        return builder.toString();
     }
 
-    public LocalDate getLocalDate(String date) {
-        return localDate = LocalDate.parse(date, FORMATTER);
+    public LocalDate getLocalDate(String line) {
+        String[] date = line.split(" ");
+        return LocalDate.parse(date[0], FORMATTER);
     }
 
     public int getSalaryOfDay(String line, String name) {
-        return Integer.parseInt(line.substring(line.lastIndexOf(' ')
-                + 1, line.length())) * Integer.parseInt(line.substring(line
-                .indexOf(name) + name.length() + 1, line.lastIndexOf(' ')));
+        String[] dataLine = line.split(" ");
+        return Integer.parseInt(dataLine[3]) * Integer.parseInt(dataLine[2]);
     }
 }
