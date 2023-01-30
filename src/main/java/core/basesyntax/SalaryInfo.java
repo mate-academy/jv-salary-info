1cf6ea4d9546a569962e095d0ee9e4824705f971
Yehor-Cherkashyn <gooooodvin1995@gmail.com>
2023-01-30 17:40:20 +0200

    Deleted two-dimensian array

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,35 +5,35 @@ import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
         LocalDate dateFromFormatted = LocalDate.parse(dateFrom, formatter);
         LocalDate dateToFormatted = LocalDate.parse(dateTo, formatter);
 
         StringBuilder reportForPeriod = new StringBuilder();
+        reportForPeriod.append("Report for period ").append(dateFrom)
+                .append(" - ").append(dateTo);
 
         int[] salary = new int[names.length];
-        String[][] dataSplit = new String[data.length][4];
 
-        for (int i = 0; i < data.length; i++) {
-            dataSplit[i] = data[i].split(" ");
-            for (int j = 0; j < names.length; j++) {
-                if ((LocalDate.parse(dataSplit[i][0], formatter).isAfter(dateFromFormatted)
-                        || LocalDate.parse(dataSplit[i][0], formatter).isEqual(dateFromFormatted))
-                        && (LocalDate.parse(dataSplit[i][0], formatter).isBefore(dateToFormatted)
-                        || LocalDate.parse(dataSplit[i][0], formatter).isEqual(dateToFormatted))
-                        && names[j].equals(dataSplit[i][1])) {
-                    salary[j] += Integer.parseInt(dataSplit[i][2])
-                            * Integer.parseInt(dataSplit[i][3]);
+        for (int i = 0; i < names.length; i++) {
+            for (String dataLoop : data) {
+                if ((LocalDate.parse(dataLoop.split(" ")[0], formatter)
+                        .isAfter(dateFromFormatted)
+                        || LocalDate.parse(dataLoop.split(" ")[0], formatter)
+                        .isEqual(dateFromFormatted))
+                        && (LocalDate.parse(dataLoop.split(" ")[0], formatter)
+                        .isBefore(dateToFormatted)
+                        || LocalDate.parse(dataLoop.split(" ")[0], formatter)
+                        .isEqual(dateToFormatted))
+                        && names[i].equals(dataLoop.split(" ")[1])) {
+                    salary[i] += Integer.parseInt(dataLoop.split(" ")[2])
+                            * Integer.parseInt(dataLoop.split(" ")[3]);
                 }
             }
+            reportForPeriod.append(System.lineSeparator())
+                    .append(names[i]).append(" - ").append(salary[i]);
         }
-
-        return reportForPeriod.append("Report for period ").append(dateFrom)
-                .append(" - ").append(dateTo).append(System.lineSeparator())
-                .append(names[0]).append(" - ").append(salary[0])
-                .append(System.lineSeparator()).append(names[1]).append(" - ").append(salary[1])
-                .append(System.lineSeparator()).append(names[2]).append(" - ").append(salary[2])
-                .toString();
+        return reportForPeriod.toString();
     }
 }
