88a7d4c8cc555d7152147385374f9843343752a3
Liubomyr Kniazkyi <knyazkyylubomir@gmail.com>
2023-02-03 10:22:13 +0200

    declared variables at the place of usage, changed constant to more informative

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -10,29 +10,24 @@ public class SalaryInfo {
     private static final int NAME_INDEX = 1;
     private static final int WORKING_HOURS_INDEX = 2;
     private static final int INCOME_PER_HOUR_INDEX = 3;
-    private static final String SPLIT_BY_SPACE = " ";
+    private static final String SPACE_SEPARATOR = " ";
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder builder = new StringBuilder("Report for period " + dateFrom + " - " + dateTo);
         LocalDate localDateFrom = LocalDate.parse(dateFrom, DATE_TIME_FORMATTER);
         LocalDate localDateTo = LocalDate.parse(dateTo, DATE_TIME_FORMATTER);
-        String[] dataArrays;
-        LocalDate workDay;
-        int salary;
-        int workingHours;
-        int incomePerHour;
 
         for (int i = 0; i < names.length; i++) {
-            salary = 0;
+            int salary = 0;
             for (int j = 0; j < data.length; j++) {
-                dataArrays = data[j].split(SPLIT_BY_SPACE);
-                workDay = LocalDate.parse(dataArrays[WORK_DAY_INDEX], DATE_TIME_FORMATTER);
-                workingHours = Integer.parseInt(dataArrays[WORKING_HOURS_INDEX]);
-                incomePerHour = Integer.parseInt(dataArrays[INCOME_PER_HOUR_INDEX]);
+                String[] dataArrays = data[j].split(SPACE_SEPARATOR);
+                LocalDate workDay
+                        = LocalDate.parse(dataArrays[WORK_DAY_INDEX], DATE_TIME_FORMATTER);
+                int workingHours = Integer.parseInt(dataArrays[WORKING_HOURS_INDEX]);
+                int incomePerHour = Integer.parseInt(dataArrays[INCOME_PER_HOUR_INDEX]);
 
                 if (localDateFrom.isBefore(workDay)
-                        && (localDateTo.isAfter(workDay)
-                        || localDateTo.equals(workDay))
+                        && (localDateTo.isAfter(workDay) || localDateTo.equals(workDay))
                         && names[i].equals(dataArrays[NAME_INDEX])) {
                     salary += workingHours * incomePerHour;
                 }
