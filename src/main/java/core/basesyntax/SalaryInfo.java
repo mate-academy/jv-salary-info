02a41a236459f466bc64e0159831339f22f306f9
MaksymHorielikov <hmv.fermats@gmail.com>
2023-02-06 14:55:05 +0200

    fixed reqested changes (use LocalDate, make DATE_FORMATTER, add method convertStringToLocalDate, StringBuilder instead concatenation)

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,46 +1,42 @@
 package core.basesyntax;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    private static final DateTimeFormatter DATE_FORMATTER =
+            DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int INDEX_DATE = 0;
+    private static final int INDEX_NAME = 1;
+    private static final int INDEX_HOURS = 2;
+    private static final int INDEX_COST = 3;
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM.yyyy");
-        Date from;
-        Date to;
-        try {
-            from = simpleDateFormat.parse(dateFrom);
-        } catch (ParseException e) {
-            throw new RuntimeException("Invalid date format [" + dateFrom + "]", e);
-        }
-        try {
-            to = simpleDateFormat.parse(dateTo);
-        } catch (ParseException e) {
-            throw new RuntimeException("Invalid date format [" + dateTo + "]", e);
-        }
-        StringBuilder result = new StringBuilder("Report for period " + dateFrom + " - "
-                + dateTo + System.lineSeparator());
+        LocalDate from = convertStringToLocalDate(dateFrom);
+        LocalDate to = convertStringToLocalDate(dateTo);
+        StringBuilder result = new StringBuilder("Report for period ")
+                .append(dateFrom)
+                .append(" - ")
+                .append(dateTo)
+                .append(System.lineSeparator());
         for (int i = 0; i < names.length; i++) {
             int salary = 0;
-            result.append(names[i]);
-            result.append(" - ");
+            result.append(names[i]).append(" - ");
             for (String record: data) {
                 String [] dates = record.split(" ");
+                LocalDate currentDate = convertStringToLocalDate(dates[INDEX_DATE]);
                 try {
-                    Date current = simpleDateFormat.parse(dates[0]);
-                    if (current.compareTo(from) != -1 && current.compareTo(to) != 1
-                            && dates[1].equals(names[i])) {
-                        int hours = Integer.parseInt(dates[2]);
-                        int cost = Integer.parseInt(dates[3]);
-                        salary += hours * cost;
+                    if (!currentDate.isBefore(from) && !currentDate.isAfter(to)
+                            && dates[INDEX_NAME].equals(names[i])) {
+                        salary += Integer.parseInt(dates[INDEX_HOURS])
+                                * Integer.parseInt(dates[INDEX_COST]);
                     }
-                } catch (ParseException e) {
-                    throw new RuntimeException("Invalid date format [" + dates[0] + "]", e);
                 } catch (NumberFormatException e) {
                     throw new RuntimeException("Invalid number format hours or cost", e);
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    throw new RuntimeException("Invalid format record in data", e);
                 }
-
             }
             result.append(salary);
             if (i != names.length - 1) {
@@ -48,5 +44,15 @@ public class SalaryInfo {
             }
         }
         return result.toString();
+    }
+
+    private LocalDate convertStringToLocalDate(String date) {
+        LocalDate result;
+        try {
+            result = LocalDate.parse(date, DATE_FORMATTER);
+        } catch (DateTimeException e) {
+            throw new RuntimeException("Invalid date format - [" + date + "]", e);
+        }
+        return result;
     }
 }
