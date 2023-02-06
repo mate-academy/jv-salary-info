f8e4e93eda2d4dcd816211c5f61d96a1fba961a2
Anatolii Nazarov <anatoly_n@ukr.net>
2023-02-06 10:53:10 +0200

    Fixe according remarks

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,14 +4,15 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    private static String whiteSpace = " ";
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d.MM.yyyy");
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
+        LocalDate dateFromLocal = LocalDate.parse(dateFrom, formatter);
+        LocalDate dateToLocal = LocalDate.parse(dateTo, formatter);
         int[] salaryAll = new int[names.length];
         for (int i = 0; i < names.length; i++) {
             for (String datas : data) {
-                String[] datasPart = datas.split(" ");
-                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d.MM.yyyy");
-                LocalDate dateFromLocal = LocalDate.parse(dateFrom, formatter);
-                LocalDate dateToLocal = LocalDate.parse(dateTo, formatter);
+                String[] datasPart = datas.split(whiteSpace);
                 LocalDate dateData = LocalDate.parse(datasPart[0], formatter);
                 if ((names[i].equals(datasPart[1])) && (dateFromLocal.isBefore(dateData)
                         || dateFromLocal.equals(dateData)) && ((dateData.isBefore(dateToLocal)
@@ -21,9 +22,10 @@ public class SalaryInfo {
                 }
             }
         }
-        return "Report for period " + dateFrom + " - " + dateTo + System.lineSeparator()
-                + names[0] + " - " + salaryAll[0] + System.lineSeparator()
-                + names[1] + " - " + salaryAll[1] + System.lineSeparator()
-                + names[2] + " - " + salaryAll[2];
+        StringBuilder builder = new StringBuilder();
+        for (int i=0; i < names.length; i++) {
+            builder.append(System.lineSeparator()).append(names[i]).append(" - ").append(salaryAll[i]);
+        }
+        return "Report for period" + dateFrom + " - " + dateTo + builder.toString();
     }
 }
