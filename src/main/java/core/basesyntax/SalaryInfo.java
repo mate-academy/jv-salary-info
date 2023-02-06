15a6d5903d722709e4e862c27922fc23aea35c4c
WallRun <dimonty2003@gmail.com>
2023-02-06 19:37:28 +0200

    replaced magic numbers with constants

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,6 +5,11 @@ import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int WORK_HOURS_INDEX = 2;
+    private static final int HOURLY_SALARY_INDEX = 3;
+
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate beginDate = LocalDate.parse(dateFrom, FORMATTER);
@@ -17,10 +22,10 @@ public class SalaryInfo {
             int currentEmployeeSalary = 0;
             for (String currentData : data) {
                 String[] dataArray = currentData.split(" ");
-                LocalDate currentDate = LocalDate.parse(dataArray[0], FORMATTER);
-                String nameData = dataArray[1];
-                int hours = Integer.parseInt(dataArray[2]);
-                int salary = Integer.parseInt(dataArray[3]);
+                LocalDate currentDate = LocalDate.parse(dataArray[DATE_INDEX], FORMATTER);
+                String nameData = dataArray[NAME_INDEX];
+                int hours = Integer.parseInt(dataArray[WORK_HOURS_INDEX]);
+                int salary = Integer.parseInt(dataArray[HOURLY_SALARY_INDEX]);
 
                 if (nameData.equals(name)) {
                     if (!currentDate.isAfter(endDate) && !currentDate.isBefore(beginDate)) {
