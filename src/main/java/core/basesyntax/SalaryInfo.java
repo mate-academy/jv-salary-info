f8bd9a87da70b311b465c06baff1deef5e5ac899
Anatolii Nazarov <anatoly_n@ukr.net>
2023-02-07 16:54:55 +0200

    Fixed

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -10,23 +10,21 @@ public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate dateFromLocal = LocalDate.parse(dateFrom, formatter);
         LocalDate dateToLocal = LocalDate.parse(dateTo, formatter);
-        int[] salaryAll = new int[names.length];
+        StringBuilder builder = new StringBuilder();
+        //int[] salaryAll = new int[names.length];
         for (int i = 0; i < names.length; i++) {
+            int salary = 0;
             for (String dat : data) {
                 String[] datPart = dat.split(whiteSpace);
                 LocalDate dateData = LocalDate.parse(datPart[0], formatter);
                 if ((names[i].equals(datPart[1])) && (dateFromLocal.isBefore(dateData)
                         || dateFromLocal.equals(dateData)) && ((dateData.isBefore(dateToLocal)
                         || (dateToLocal.equals(dateData))))) {
-                    salaryAll[i] = salaryAll[i] + Integer.parseInt(datPart[2])
+                    salary = salary + Integer.parseInt(datPart[2])
                             * Integer.parseInt(datPart[3]);
                 }
             }
-        }
-        StringBuilder builder = new StringBuilder();
-        for (int i = 0; i < names.length; i++) {
-            builder.append(System.lineSeparator()).append(names[i])
-                    .append(" - ").append(salaryAll[i]);
+            builder.append(System.lineSeparator()).append(names[i]+" - "+salary);
         }
         return "Report for period " + dateFrom + " - " + dateTo + builder;
     }
