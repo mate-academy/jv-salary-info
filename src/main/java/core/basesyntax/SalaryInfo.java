9e7e5be83d5b370f91b367e8b81e22d86580ddd4
industrialeng <semonenko055@gmail.com>
2023-02-07 12:28:33 +0200

    The decision was added into the SalaryInfo

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,35 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+    public static final int DAY = 0;
+    public static final int NAME = 1;
+    public static final int HOUR = 2;
+    public static final int MONEY = 3;
+    public static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        String enter = System.lineSeparator();
+        StringBuilder result = new StringBuilder("Report for period " + dateFrom + " - " + dateTo);
+        LocalDate dayFrom = LocalDate.parse(dateFrom,formatter);
+        LocalDate dayTo = LocalDate.parse(dateTo,formatter);
+        LocalDate day;
+        String [] lineArray = new String[4];
+        for (String name : names) {
+            int salary = 0;
+            for (String line : data) {
+                lineArray = line.split(" ");
+                day = LocalDate.parse(lineArray[DAY],formatter);
+                if (name.equals(lineArray[NAME]) && (day.isAfter(dayFrom) || day.isEqual(dayFrom))
+                        && (day.isBefore(dayTo) || day.isEqual(dayTo))) {
+                    salary = salary + (Integer.parseInt(lineArray[HOUR])
+                            * Integer.parseInt(lineArray[MONEY]));
+                }
+            }
+            result.append(enter + name + " - " + salary);
+        }
+        return result.toString();
     }
 }
