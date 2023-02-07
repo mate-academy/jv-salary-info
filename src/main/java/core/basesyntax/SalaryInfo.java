2a8b3528136a76e5b296fae96c1d99c3d3639fdb
industrialeng <semonenko055@gmail.com>
2023-02-07 13:33:43 +0200

    The decision was added into the SalaryInfo 3.0

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -8,27 +8,28 @@ public class SalaryInfo {
     public static final int INDEX_OF_NAME = 1;
     public static final int INDEX_OF_HOURS = 2;
     public static final int INDEX_OF_MONEY = 3;
-    public static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-    public static final String enter = System.lineSeparator();
+    public static final int LENGTH_OF_SEPARATED_LINE = 4;
+    public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    public static final String SEPARATOR = System.lineSeparator();
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder result = new StringBuilder("Report for period " + dateFrom + " - " + dateTo);
-        LocalDate dayFrom = LocalDate.parse(dateFrom,formatter);
-        LocalDate dayTo = LocalDate.parse(dateTo,formatter);
-        String [] lineArray = new String[4];
+        LocalDate dayFrom = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate dayTo = LocalDate.parse(dateTo, FORMATTER);
+        String [] splittedLine = new String[LENGTH_OF_SEPARATED_LINE];
         for (String name : names) {
             int salary = 0;
             for (String line : data) {
-                lineArray = line.split(" ");
-                LocalDate day = LocalDate.parse(lineArray[INDEX_OF_DAY],formatter);
-                if (name.equals(lineArray[INDEX_OF_NAME])
+                splittedLine = line.split(" ");
+                LocalDate day = LocalDate.parse(splittedLine[INDEX_OF_DAY], FORMATTER);
+                if (name.equals(splittedLine[INDEX_OF_NAME])
                         && (day.isAfter(dayFrom) || day.isEqual(dayFrom))
                         && (day.isBefore(dayTo) || day.isEqual(dayTo))) {
-                    salary = salary + (Integer.parseInt(lineArray[INDEX_OF_HOURS])
-                            * Integer.parseInt(lineArray[INDEX_OF_MONEY]));
+                    salary = salary + (Integer.parseInt(splittedLine[INDEX_OF_HOURS])
+                            * Integer.parseInt(splittedLine[INDEX_OF_MONEY]));
                 }
             }
-            result.append(enter + name + " - " + salary);
+            result.append(SEPARATOR + name + " - " + salary);
         }
         return result.toString();
     }
