7e45234ccba905b27e0b20c2c9ff76ddff5f765b
Potseluiko Vitalii <vetaliy063@gmail.com>
2023-02-07 23:01:38 +0200

    Made formatter field like constant

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -6,17 +6,17 @@ import java.time.format.DateTimeFormatter;
 public class SalaryInfo {
     private static final String DATE_FORMAT = "dd.MM.yyyy";
     private static final String HEADING = "Report for period %s - %s";
-    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_FORMAT);
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern(DATE_FORMAT);
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder info = new StringBuilder(String.format(HEADING, dateFrom, dateTo));
-        LocalDate firstLimit = LocalDate.parse(dateFrom, formatter);
-        LocalDate secondLimit = LocalDate.parse(dateTo, formatter);
+        LocalDate firstLimit = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate secondLimit = LocalDate.parse(dateTo, FORMATTER);
         for (int i = 0; i < names.length; i++) {
             int generalEmployeeSalary = 0;
             for (int j = 0; j < data.length; j++) {
                 String[] employeeData = data[j].split(" ");
-                LocalDate date = LocalDate.parse(employeeData[0], formatter);
+                LocalDate date = LocalDate.parse(employeeData[0], FORMATTER);
                 String employeeName = employeeData[1];
                 int countMonths = Integer.parseInt(employeeData[2]);
                 int salary = Integer.parseInt(employeeData[3]);
