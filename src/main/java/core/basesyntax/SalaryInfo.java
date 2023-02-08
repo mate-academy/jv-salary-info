4d6541148642b0a732fb7a95c04bf28c56597e0d
Yehor-Cherkashyn <gooooodvin1995@gmail.com>
2023-02-08 15:58:37 +0200

    Corrected naming, added access modifier

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,7 +4,11 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int DATE_INDEX_0 = 0;
+    private static final int DATE_INDEX_1 = 1;
+    private static final int DATE_INDEX_2 = 2;
+    private static final int DATE_INDEX_3 = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate dateFromFormatted = LocalDate.parse(dateFrom, FORMATTER);
@@ -17,16 +21,16 @@ public class SalaryInfo {
         int[] salary = new int[names.length];
 
         for (int i = 0; i < names.length; i++) {
-            for (String dataLoop : data) {
-                String dataLoopSplit = dataLoop.split(" ")[0];
-                LocalDate localDateParse = LocalDate.parse(dataLoopSplit, FORMATTER);
+            for (String row : data) {
+                String date = row.split(" ")[DATE_INDEX_0];
+                LocalDate localDateParse = LocalDate.parse(date, FORMATTER);
                 if ((localDateParse.isAfter(dateFromFormatted)
                         || localDateParse.isEqual(dateFromFormatted))
                         && (localDateParse.isBefore(dateToFormatted)
                         || localDateParse.isEqual(dateToFormatted))
-                        && names[i].equals(dataLoop.split(" ")[1])) {
-                    salary[i] += Integer.parseInt(dataLoop.split(" ")[2])
-                            * Integer.parseInt(dataLoop.split(" ")[3]);
+                        && names[i].equals(row.split(" ")[DATE_INDEX_1])) {
+                    salary[i] += Integer.parseInt(row.split(" ")[DATE_INDEX_2])
+                            * Integer.parseInt(row.split(" ")[DATE_INDEX_3]);
                 }
             }
             reportForPeriod.append(System.lineSeparator())
