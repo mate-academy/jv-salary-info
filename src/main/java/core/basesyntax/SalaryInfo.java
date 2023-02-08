1bab4f9772e01e134c0406380c619c01efd9f872
potrap <potrapmax@gmail.com>
2023-02-08 21:14:36 +0200

    some mistakes was fixed

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -2,22 +2,29 @@ package core.basesyntax;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 public class SalaryInfo {
 
+    private static final int DATE_FROM_DATA = 0;
+    private static final int NAME_FROM_DATA = 1;
+    private static final int HOURS_FROM_DATA = 2;
+    private static final int SALARY_FROM_DATA = 3;
     private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
-    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
+    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo)
+            throws ArrayIndexOutOfBoundsException {
         int[] salary = new int[names.length];
-        LocalDate from = LocalDate.parse(dateFrom, formatter);
-        LocalDate to = LocalDate.parse(dateTo, formatter);
+        LocalDate from = parseDate(dateFrom, formatter);
+        LocalDate to = parseDate(dateTo, formatter);
         for (String information : data) {
             String[] formattedData = information.split(" ");
-            LocalDate workDay = LocalDate.parse(formattedData[0], formatter);
+            LocalDate workDay = parseDate(formattedData[DATE_FROM_DATA], formatter);
             for (int i = 0; i < names.length; i++) {
-                if (names[i].equals(formattedData[1]) && compareDate(workDay, from, to)) {
-                    int hours = Integer.parseInt(formattedData[2]);
-                    int salaryPerHour = Integer.parseInt(formattedData[3]);
+                if (names[i].equals(formattedData[NAME_FROM_DATA])
+                        && compareDate(workDay, from, to)) {
+                    int hours = Integer.parseInt(formattedData[HOURS_FROM_DATA]);
+                    int salaryPerHour = Integer.parseInt(formattedData[SALARY_FROM_DATA]);
                     salary[i] += hours * salaryPerHour;
                     break;
                 }
@@ -26,14 +33,14 @@ public class SalaryInfo {
         return formatInfo(salary, names, dateFrom, dateTo);
     }
 
-    private static boolean compareDate(LocalDate date, LocalDate from, LocalDate to) {
-        return (date.isAfter(from) || date.isEqual(from))
-                && (date.isEqual(to) || date.isBefore(to));
+    private boolean compareDate(LocalDate date, LocalDate from, LocalDate to) {
+        return !date.isBefore(from) && !date.isAfter(to);
 
     }
 
-    private static String formatInfo(int[] salary, String[] names, String from, String to) {
-        StringBuilder info = new StringBuilder("Report for period " + from + " - " + to);
+    private String formatInfo(int[] salary, String[] names, String from, String to) {
+        StringBuilder info = new StringBuilder("Report for period ");
+        info.append(from).append(" - ").append(to);
         for (int i = 0; i < names.length; i++) {
             info.append(System.lineSeparator())
                     .append(names[i]).append(" - ").append(salary[i]);
@@ -41,5 +48,11 @@ public class SalaryInfo {
         return info.toString();
     }
 
+    private LocalDate parseDate(String date, DateTimeFormatter formatter) {
+        try {
+            return LocalDate.parse(date, formatter);
+        } catch (DateTimeParseException e) {
+            throw new RuntimeException("wrong date format");
+        }
+    }
 }
-
