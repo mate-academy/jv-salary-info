169913316489d7371ce83dc647fcb4ddecdd65f1
Sergiy Golubchenko <galuba1@gmail.com>
2023-02-09 00:30:19 +0200

    fix after riding common mistakes

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,45 +1,38 @@
 package core.basesyntax;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 public class SalaryInfo {
+    static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         String[] tempData;
         int salarySumm;
-        Date firstDate = new Date();
-        Date secondDate = new Date();
-        SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyyy");
+        StringBuilder result = new StringBuilder("Report for period ")
+                .append(dateFrom).append(" - ").append(dateTo);
         try {
-            firstDate = formatter.parse(dateFrom);
-            secondDate = formatter.parse(dateTo);
-        } catch (ParseException e) {
-            System.out.println("Incorrect data");
-        }
-
-        Date workDate = null;
-        StringBuilder result = new StringBuilder("Report for period ");
-        result.append(dateFrom).append(" - ").append(dateTo);
-        for (int i = 0; i < names.length; i++) {
-            result.append(System.lineSeparator());
-            salarySumm = 0;
-            for (int j = 0; j < data.length; j++) {
-                tempData = data[j].split(" ");
-                try {
-                    workDate = formatter.parse(tempData[0]);
-                } catch (ParseException e) {
-                    System.out.println("Incorrect data");
-                }
-
-                if (names[i].equals(tempData[1])
-                        && workDate.compareTo(firstDate) >= 0
-                        && workDate.compareTo(secondDate) <= 0) {
-                    salarySumm = salarySumm
+            LocalDate firstDate = LocalDate.parse(dateFrom, FORMATTER);
+            LocalDate secondDate = LocalDate.parse(dateTo, FORMATTER);
+            LocalDate workDate = LocalDate.now();
+            for (int i = 0; i < names.length; i++) {
+                result.append(System.lineSeparator());
+                salarySumm = 0;
+                for (int j = 0; j < data.length; j++) {
+                    tempData = data[j].split(" ");
+                    workDate = LocalDate.parse(tempData[0], FORMATTER);
+                    if (names[i].equals(tempData[1])
+                            && workDate.compareTo(firstDate) >= 0
+                            && workDate.compareTo(secondDate) <= 0) {
+                        salarySumm = salarySumm
                             + Integer.parseInt(tempData[2]) * Integer.parseInt(tempData[3]);
+                    }
                 }
+                result.append(names[i]).append(" - ").append(salarySumm);
             }
-            result.append(names[i]).append(" - ").append(salarySumm);
+        } catch (DateTimeParseException e) {
+            System.out.println("Incorrect data");
         }
         return result.toString();
     }
