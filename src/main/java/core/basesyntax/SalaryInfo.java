7f09e4c931729aa246b114c3af3a0879d22140d0
kromch <94765048+kromch@users.noreply.github.com>
2023-02-08 13:23:43 +0200

    Update SalaryInfo.java

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -7,7 +7,7 @@ import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.Date;
 import java.util.Locale;
-pattern
+
 public class SalaryInfo {
     private final String pattern = "dd.MM.yyyy";
     private final SimpleDateFormat formatter = new SimpleDateFormat(pattern);
