f39d3ae07f3d0ff6b7d1e96292df3d8573da39d6
Ilko Good <ilko.engineering@gmail.com>
2023-02-09 14:47:23 +0200

    Minor codestyle fix

--- src/main/java/core/basesyntax/DateParser.java
+++ src/main/java/core/basesyntax/DateParser.java
@@ -4,8 +4,9 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class DateParser {
+    private final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public LocalDate date(String date) {
-        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
         return LocalDate.parse(date, dateTimeFormatter);
     }
 }
