74c2294d9d77eb83bc5cb99884be996b04c8b7b8
KosMarch <kosmarchenko98@gmail.com>
2023-02-12 17:32:11 +0200

    fixed common mistakes

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,55 +1,35 @@
 package core.basesyntax;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    private static final int DATA_POSITION_0_DATES = 0;
+    private static final int DATA_POSITION_1_NAMES = 1;
+    private static final int DATA_POSITION_2_HOURS = 2;
+    private static final int DATA_POSITION_3_PER_HOURS = 3;
+    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        int sumJohn = 0;
-        int sumAndrew = 0;
-        int sumKate = 0;
-
-        StringBuilder sb = new StringBuilder();
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM.yyyy");
-        Date to;
-        Date from;
-        try {
-            to = simpleDateFormat.parse(dateTo);
-            from = simpleDateFormat.parse(dateFrom);
-        } catch (ParseException e) {
-            throw new RuntimeException(e);
-        }
-
-        for (String datum : data) {
-            String[] dates = datum.split(" ");
-            Date d1;
-            try {
-                d1 = simpleDateFormat.parse(dates[0]);
-            } catch (ParseException e) {
-                throw new RuntimeException(e);
-            }
-            if (d1.compareTo(from) >= 0 && d1.compareTo(to) <= 0) {
-                switch (dates[1]) {
-                    case "John":
-                        sumJohn += Integer.parseInt(dates[2]) * Integer.parseInt(dates[3]);
-                        break;
-                    case "Andrew":
-                        sumAndrew += Integer.parseInt(dates[2]) * Integer.parseInt(dates[3]);
-                        break;
-                    case "Kate":
-                        sumKate += Integer.parseInt(dates[2]) * Integer.parseInt(dates[3]);
-                        break;
-                    default:
-                        break;
+        LocalDate limitFrom = LocalDate.parse(dateFrom, formatter);
+        LocalDate limitTo = LocalDate.parse(dateTo, formatter);
+        StringBuilder stringBuilder = new StringBuilder("Report for period "
+                + dateFrom + " - " + dateTo);
+        for (String name : names) {
+            int salary = 0;
+            for (String datum : data) {
+                String[] dates = datum.split(" ");
+                if (name.equals(dates[DATA_POSITION_1_NAMES])) {
+                    LocalDate salaryDate = LocalDate.parse(dates[DATA_POSITION_0_DATES], formatter);
+                    if (salaryDate.isAfter(limitFrom) && salaryDate.isBefore(limitTo)
+                            || salaryDate.isEqual(limitFrom) || salaryDate.isEqual(limitTo)) {
+                        salary += Integer.parseInt(dates[DATA_POSITION_2_HOURS])
+                                * Integer.parseInt(dates[DATA_POSITION_3_PER_HOURS]);
+                    }
                 }
             }
+            stringBuilder.append(System.lineSeparator()).append(name).append(" - ").append(salary);
         }
-        return sb.append("Report for period ")
-                .append(dateFrom).append(" - ").append(dateTo).append(System.lineSeparator())
-                .append(names[0]).append(" - ").append(sumJohn).append(System.lineSeparator())
-                .append(names[1]).append(" - ").append(sumAndrew).append(System.lineSeparator())
-                .append(names[2]).append(" - ").append(sumKate)
-                .toString();
+        return stringBuilder.toString();
     }
 }
