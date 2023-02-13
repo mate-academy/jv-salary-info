eebf1efcc6ed9d9b3fc9948d4165b70e160426bd
Oleksandr Karpiuk <karpo99old@gmail.com>
2023-02-13 17:40:38 +0200

    Change method order for better understanding;

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -9,13 +9,6 @@ public class SalaryInfo {
     public static final String DATE_FORMAT = "dd.MM.yyyy";
     private DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_FORMAT);
 
-    private boolean getCompare(String dateFrom, String dateTo, String forCompare) {
-        LocalDate start = LocalDate.parse(dateFrom, formatter);
-        LocalDate end = LocalDate.parse(dateTo, formatter);
-        LocalDate actual = LocalDate.parse(forCompare, formatter);
-        return actual.compareTo(start) >= 0 && actual.compareTo(end) <= 0;
-    }
-
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder builder = new StringBuilder("Report for period "
                 + dateFrom + " - " + dateTo + System.lineSeparator());
@@ -36,5 +29,12 @@ public class SalaryInfo {
             builder.append(employee).append(" - ").append(salary).append(System.lineSeparator());
         }
         return builder.toString().trim();
+    }
+
+    private boolean getCompare(String dateFrom, String dateTo, String forCompare) {
+        LocalDate start = LocalDate.parse(dateFrom, formatter);
+        LocalDate end = LocalDate.parse(dateTo, formatter);
+        LocalDate actual = LocalDate.parse(forCompare, formatter);
+        return actual.compareTo(start) >= 0 && actual.compareTo(end) <= 0;
     }
 }
