6c023e0322b44cfd08b8bcb83d7dfcf19f6b5159
“Ymishok321 <kkoiku3@gmail.com>
2023-02-13 14:54:54 +0200

    the reading method has been changed & the array indexes have been moved to constants

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -2,50 +2,41 @@ package core.basesyntax;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
 
 public class SalaryInfo {
+    public static final int DATE_COLUMN = 0;
+    public static final int NAME_COLUMN = 1;
+    public static final int HOURS_COLUMN = 2;
+    public static final int SALARY_COLUMN = 3;
     private static final DateTimeFormatter FORMATTER =
             DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private LocalDate dateFromParsed;
+    private LocalDate dateToParsed;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
+        dateFromParsed = LocalDate.parse(dateFrom, FORMATTER);
+        dateToParsed = LocalDate.parse(dateTo, FORMATTER);
         int[] salariesInfo = new int[names.length];
         StringBuilder result = new StringBuilder()
                 .append("Report for period " + dateFrom + " - " + dateTo + System.lineSeparator());
-
         for (int i = 0; i < data.length; i++) {
             String[] currentRow = data[i].split("\\s+");
-            if (isDateInRange(currentRow[0], dateFrom, dateTo)) {
-                int salaryIndex = getIndex(currentRow[1], names);
-                salariesInfo[salaryIndex] += Integer.valueOf(currentRow[2])
-                        * Integer.valueOf(currentRow[3]);
+            if (isDateInRange(currentRow[DATE_COLUMN])) {
+                int salaryIndex = getIndex(currentRow[NAME_COLUMN], names);
+                salariesInfo[salaryIndex] += Integer.valueOf(currentRow[HOURS_COLUMN])
+                        * Integer.valueOf(currentRow[SALARY_COLUMN]);
             }
         }
-
         for (int i = 0; i < names.length; i++) {
             result.append(names[i] + " - " + salariesInfo[i] + System.lineSeparator());
         }
-
         return result.toString().trim();
     }
 
-    private boolean isDateInRange(String dateToCheckString, String dateFromString,
-                                  String dateToString) {
-        LocalDate dateFrom = null;
-        LocalDate dateTo = null;
-        LocalDate dateToCheck = null;
-
-        try {
-            dateFrom = LocalDate.parse(dateFromString, FORMATTER);
-            dateTo = LocalDate.parse(dateToString, FORMATTER);
-            dateToCheck = LocalDate.parse(dateToCheckString, FORMATTER);
-        } catch (DateTimeParseException exc) {
-            System.out.println("Error while parsing date");
-        }
-
-        return dateFrom == null || dateTo == null || dateToCheck == null ? false :
-                dateToCheck.isAfter(dateFrom.minusDays(1))
-                        && dateToCheck.isBefore(dateTo.plusDays(1));
+    private boolean isDateInRange(String dateToCheckString) {
+        LocalDate dateToCheck = LocalDate.parse(dateToCheckString, FORMATTER);
+        return dateToCheck.isAfter(dateFromParsed.minusDays(1))
+                        && dateToCheck.isBefore(dateToParsed.plusDays(1));
     }
 
     private int getIndex(String element, String[] array) {
