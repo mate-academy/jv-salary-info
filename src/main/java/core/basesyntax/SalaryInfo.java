d4cb849d4c2a5968c81ab6347eb38f88b80cce2d
Anna Shokun <anne.shokun@gmail.com>
2023-02-13 22:34:53 +0200

    added constants for indexes; moved conversion of dateFrom and dateTo in the beginning of method

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,10 +5,17 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
 public class SalaryInfo {
-    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int HOURS_INDEX = 2;
+    private static final int INCOME_INDEX = 3;
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         int salary = 0;
+        LocalDate localDateFrom = dateConvert(dateFrom);
+        LocalDate localDateTo = dateConvert(dateTo);
+        LocalDate particularDay;
         String[] info;
         StringBuilder employeeSalary = new StringBuilder("Report for period "
                 + dateFrom + " - " + dateTo);
@@ -16,11 +23,13 @@ public class SalaryInfo {
         for (String name: names) {
             for (String dataPerDay: data) {
                 info = dataPerDay.split(" ");
+                particularDay = dateConvert(info[DATE_INDEX]);
 
-                if (name.equals(info[1])
-                        && dateConvert(info[0]).compareTo(dateConvert(dateFrom)) >= 0
-                        && dateConvert(info[0]).compareTo(dateConvert(dateTo)) <= 0) {
-                    salary += Integer.parseInt(info[2]) * Integer.parseInt(info[3]);
+                if (name.equals(info[NAME_INDEX])
+                        && particularDay.compareTo(localDateFrom) >= 0
+                        && particularDay.compareTo(localDateTo) <= 0) {
+                    salary += Integer.parseInt(info[HOURS_INDEX])
+                            * Integer.parseInt(info[INCOME_INDEX]);
                 }
             }
             employeeSalary.append(System.lineSeparator())
@@ -33,7 +42,7 @@ public class SalaryInfo {
 
     private LocalDate dateConvert(String dateString) {
         try {
-            return LocalDate.parse(dateString, formatter);
+            return LocalDate.parse(dateString, FORMATTER);
         } catch (DateTimeParseException e) {
             System.out.println(dateString + " is not parsable!");
             throw e;
