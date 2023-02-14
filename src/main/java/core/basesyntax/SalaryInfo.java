fd2169438fbb547ac9070a4360690e60fc7b6a41
Elvira Solnyshkina <elvira.solnyshkina@gmail.com>
2023-02-14 11:47:56 +0200

    fixed mistakes, delete redundant class fields, remove resetSalary method

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,47 +4,42 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("d.MM.yyyy");
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
     private static final int DATE_INDEX = 0;
     private static final int NAME_INDEX = 1;
     private static final int HOURS_INDEX = 2;
     private static final int INCOME_INDEX = 3;
-    private LocalDate startPeriod;
-    private LocalDate endPeriod;
-    private int totalSalary;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         String[] salaryInfo = new String[names.length];
-        initializeLocalDates(dateFrom, dateTo);
-        for (int i = 0; i < names.length; i++) {
-            for (int y = 0; y < data.length; y++) {
-                String[] info = data[y].split(" ");
+        LocalDate startPeriod = initializeLocalDates(dateFrom);
+        LocalDate endPeriod = initializeLocalDates(dateTo);
+        int index = 0;
+        for (String name : names) {
+            int totalSalary = 0;
+            for (String datum : data) {
+                String[] info = datum.split(" ");
                 LocalDate singleDay = LocalDate.parse(info[DATE_INDEX], FORMATTER);
-                if (dateIsValid(singleDay, startPeriod, endPeriod) && nameIsValid(names[i], info)) {
+                if (isDateInRange(singleDay, startPeriod, endPeriod) && isNameFound(name, info)) {
                     totalSalary += calculateSalary(info);
                 }
-                salaryInfo[i] = names[i] + " - " + totalSalary;
             }
-            resetSalary();
+            salaryInfo[index++] = name + " - " + totalSalary;
         }
         return buildReport(salaryInfo, dateFrom, dateTo);
     }
 
-    private void initializeLocalDates(String dateFrom, String dateTo) {
-        startPeriod = LocalDate.parse(dateFrom, FORMATTER);
-        endPeriod = LocalDate.parse(dateTo, FORMATTER);
+    private LocalDate initializeLocalDates(String date) {
+        return LocalDate.parse(date, FORMATTER);
+
     }
 
-    private boolean dateIsValid(LocalDate singleDay, LocalDate startPeriod, LocalDate endPeriod) {
+    private boolean isDateInRange(LocalDate singleDay, LocalDate startPeriod, LocalDate endPeriod) {
         return !singleDay.isBefore(startPeriod) && !singleDay.isAfter(endPeriod);
     }
 
-    private boolean nameIsValid(String name, String[] data) {
+    private boolean isNameFound(String name, String[] data) {
         return name.equals(data[NAME_INDEX]);
-    }
-
-    private void resetSalary() {
-        totalSalary = 0;
     }
 
     private int calculateSalary(String[] data) {
