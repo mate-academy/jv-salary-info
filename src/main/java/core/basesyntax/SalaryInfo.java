a5bbe1596360a5aa816c3d46707b7704d90b9919
Mykyta <nikita.nikonets@gmail.com>
2023-02-14 16:52:01 +0200

    added constants and replaced redundant variable with StringBuilder

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,26 +5,24 @@ import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    public static final String DAY_DELIMITER = " ";
+    public static final int DATE_INDEX = 0;
+    public static final int NAME_INDEX = 1;
+    public static final int HOURS_INDEX = 2;
+    public static final int WAGE_INDEX = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate startDateFormatted = LocalDate.parse(dateFrom, FORMATTER);
         LocalDate endDateFormatted = LocalDate.parse(dateTo, FORMATTER);
-        String[] namesAndSalaries = new String[names.length];
-
-        for (int i = 0; i < names.length; i++) {
-            int salary = getEmployeeSalary(names[i], data, startDateFormatted, endDateFormatted);
-            namesAndSalaries[i] = names[i] + " - " + salary;
-        }
-
         StringBuilder str = new StringBuilder();
         str.append("Report for period ").append(dateFrom)
                 .append(" - ")
                 .append(dateTo)
                 .append(System.lineSeparator());
-        for (String nameAndSalary : namesAndSalaries) {
-            str.append(nameAndSalary).append(System.lineSeparator());
+        for (String name : names) {
+            int salary = getEmployeeSalary(name, data, startDateFormatted, endDateFormatted);
+            str.append(name).append(" - ").append(salary).append(System.lineSeparator());
         }
-
         return str.toString().stripTrailing();
     }
 
@@ -32,11 +30,11 @@ public class SalaryInfo {
                                   LocalDate dateFrom, LocalDate dateTo) {
         int salarySum = 0;
         for (String day : data) {
-            String[] values = day.split(" ");
-            LocalDate date = LocalDate.parse(values[0], FORMATTER);
-            String name = values[1];
-            Integer hours = Integer.parseInt(values[2]);
-            Integer wage = Integer.parseInt(values[3]);
+            String[] values = day.split(DAY_DELIMITER);
+            LocalDate date = LocalDate.parse(values[DATE_INDEX], FORMATTER);
+            String name = values[NAME_INDEX];
+            Integer hours = Integer.parseInt(values[HOURS_INDEX]);
+            Integer wage = Integer.parseInt(values[WAGE_INDEX]);
             boolean dateWithinRange = (date.isAfter(dateFrom) || date.isEqual(dateFrom))
                     && (date.isBefore(dateTo) || date.isEqual(dateTo));
 
