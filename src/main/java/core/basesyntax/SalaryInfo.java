3d00e7dbdcba71804e6ecc5c9c15e2edcc62f535
Ilko Good <ilko.engineering@gmail.com>
2023-02-14 11:55:50 +0200

    requested changes implemented

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -3,6 +3,13 @@ package core.basesyntax;
 import java.time.LocalDate;
 
 public class SalaryInfo {
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int HOURS_INDEX = 2;
+    private static final int SALARY_INDEX = 3;
+    private static final String INFO_SEPARATOR = " ";
+    private static final String EXPECTED_SEPARATOR = " - ";
+
     private final DateParser dateParser = new DateParser();
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
@@ -17,19 +24,21 @@ public class SalaryInfo {
         */
         for (int i = 0; i < names.length; i++) {
             for (String datum : data) {
-                if (names[i].equals(datum.split(" ")[1])
-                        && dateParser.date(datum.split(" ")[0]).compareTo(startDate) >= 0
-                        && dateParser.date(datum.split(" ")[0]).compareTo(stopDate) <= 0) {
-                    salaries[i] += Integer.parseInt(datum.split(" ")[2])
-                            * Integer.parseInt(datum.split(" ")[3]);
+                String[] entries = datum.split(INFO_SEPARATOR);
+                LocalDate currentDate = dateParser.date(entries[DATE_INDEX]);
+                if (names[i].equals(entries[NAME_INDEX])
+                        && currentDate.compareTo(startDate) >= 0
+                        && currentDate.compareTo(stopDate) <= 0) {
+                    salaries[i] += Integer.parseInt(entries[HOURS_INDEX])
+                            * Integer.parseInt(entries[SALARY_INDEX]);
                 }
             }
-            sb.append(names[i]).append(" - ").append(salaries[i]);
+            sb.append(names[i]).append(EXPECTED_SEPARATOR).append(salaries[i]);
             if (i < names.length - 1) {
                 sb.append(System.lineSeparator());
             }
         }
-        return "Report for period " + dateFrom + " - " + dateTo
+        return "Report for period " + dateFrom + EXPECTED_SEPARATOR + dateTo
                 + System.lineSeparator() + sb;
     }
 }
