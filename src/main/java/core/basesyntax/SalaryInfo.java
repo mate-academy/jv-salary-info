47afa17a6815168017340e5ef61aca6392992263
Oleksandr Karpiuk <karpo99old@gmail.com>
2023-02-14 12:20:04 +0200

    Introduce Constant, change method name

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -6,32 +6,34 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    public static final int DAYS = 0;
+    public static final int WORKER_NAME = 1;
+    public static final int HOURS = 2;
+    public static final int HOURLY_PAY = 3;
+    public static final String REGEX = " ";
+    public static final String REGEX_RESULT = " - ";
     public static final String DATE_FORMAT = "dd.MM.yyyy";
     private DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_FORMAT);
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder builder = new StringBuilder("Report for period "
-                + dateFrom + " - " + dateTo + System.lineSeparator());
-        final int days = 0;
-        final int workerName = 1;
-        final int hours = 2;
-        final int hourlyPay = 3;
-        String regex = " ";
+                + dateFrom + REGEX_RESULT + dateTo + System.lineSeparator());
         for (String employee : names) {
             int salary = 0;
             for (String actualData : data) {
-                String[] dateSplit = actualData.split(regex);
-                if (employee.equals(dateSplit[workerName])
-                        && getCompare(dateFrom, dateTo, dateSplit[days])) {
-                    salary += parseInt(dateSplit[hourlyPay]) * parseInt(dateSplit[hours]);
+                String[] dateSplit = actualData.split(REGEX);
+                if (employee.equals(dateSplit[WORKER_NAME])
+                        && isBetween(dateFrom, dateTo, dateSplit[DAYS])) {
+                    salary += parseInt(dateSplit[HOURLY_PAY]) * parseInt(dateSplit[HOURS]);
                 }
             }
-            builder.append(employee).append(" - ").append(salary).append(System.lineSeparator());
+            builder.append(employee).append(REGEX_RESULT)
+                    .append(salary).append(System.lineSeparator());
         }
         return builder.toString().trim();
     }
 
-    private boolean getCompare(String dateFrom, String dateTo, String forCompare) {
+    private boolean isBetween(String dateFrom, String dateTo, String forCompare) {
         LocalDate start = LocalDate.parse(dateFrom, formatter);
         LocalDate end = LocalDate.parse(dateTo, formatter);
         LocalDate actual = LocalDate.parse(forCompare, formatter);
