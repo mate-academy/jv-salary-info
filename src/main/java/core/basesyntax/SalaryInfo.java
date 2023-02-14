446cd081afeb487648434646e52efa8b65ae24b8
Nickolay Voloshchuk <124300295+NickolayVoloshchuk@users.noreply.github.com>
2023-02-14 12:00:41 +0200

    fix reject problem

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,53 +1,48 @@
 package core.basesyntax;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     private static final String DATE_PATTERN = "dd.MM.yyy";
+    private static final String EMPTY_SEPARATOR = " ";
+    private static final String REPORT_SEPARATOR = " - ";
     private static final short LINE_DATE = 0;
     private static final short LINE_NAME = 1;
     private static final short LINE_HOURS = 2;
     private static final short LINE_SALARY = 3;
-    private final SimpleDateFormat simpleDateFormat = new SimpleDateFormat(DATE_PATTERN);
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_PATTERN);
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder report = new StringBuilder("Report for period ")
-                .append(dateFrom).append(" - ").append(dateTo).append(System.lineSeparator());
+                .append(dateFrom).append(REPORT_SEPARATOR)
+                .append(dateTo).append(System.lineSeparator());
         for (String name : names) {
-            report.append(name).append(" - ")
-                    .append(getSalary(name, data, dateFrom, dateTo)).append(System.lineSeparator());
+            report.append(name).append(REPORT_SEPARATOR)
+                    .append(calculateSalary(name, data, dateFrom, dateTo))
+                    .append(System.lineSeparator());
         }
         return report.toString().trim();
     }
 
-    private int getSalary(String name, String[] data, String fromDate, String toDate) {
+    private int calculateSalary(String name, String[] data, String fromDate, String toDate) {
         int salary = 0;
         String[] table;
         for (String tableLine : data) {
-            table = tableLine.trim().split(" ");
+            table = tableLine.trim().split(EMPTY_SEPARATOR);
             if ((name.equals(table[LINE_NAME]))
-                    && (isSuitableDate(table[LINE_DATE], fromDate, toDate))) {
+                    && (isDateInRange(table[LINE_DATE], fromDate, toDate))) {
                 salary += Integer.decode(table[LINE_HOURS]) * Integer.decode(table[LINE_SALARY]);
             }
         }
         return salary;
     }
 
-    private boolean isSuitableDate(String lineString, String fromString, String toString) {
-        Date lineDate;
-        Date fromDate;
-        Date toDate;
-        try {
-            lineDate = simpleDateFormat.parse(lineString);
-            fromDate = simpleDateFormat.parse(fromString.trim());
-            toDate = simpleDateFormat.parse(toString.trim());
-        } catch (ParseException e) {
-            System.out.println("Date parse error!");
-            return false;
-        }
-        return ((lineDate.getTime() >= fromDate.getTime())
-                && (lineDate.getTime() <= toDate.getTime()));
+    private boolean isDateInRange(String lineString, String fromString, String toString) {
+        LocalDate lineDate = LocalDate.parse(lineString.trim(), formatter);
+        LocalDate fromDate = LocalDate.parse(fromString.trim(), formatter);
+        LocalDate toDate = LocalDate.parse(toString.trim(), formatter);
+        return lineDate.isAfter(fromDate.minusDays(1))
+                && lineDate.isBefore(toDate.plusDays(1));
     }
 }
