b7968816ac859d743d1bd21eb199c757b50eaacb
Potseluiko Vitalii <vetaliy063@gmail.com>
2023-02-14 18:12:07 +0200

    Added constants instead of magic numbers. Changed for loop with index on for each. Rename local variables.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -7,27 +7,31 @@ public class SalaryInfo {
     private static final String DATE_FORMAT = "dd.MM.yyyy";
     private static final String HEADING = "Report for period %s - %s";
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern(DATE_FORMAT);
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int MONTH_INDEX = 2;
+    private static final int SALARY_INDEX = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder info = new StringBuilder(String.format(HEADING, dateFrom, dateTo));
-        LocalDate firstLimit = LocalDate.parse(dateFrom, FORMATTER);
-        LocalDate secondLimit = LocalDate.parse(dateTo, FORMATTER);
-        for (int i = 0; i < names.length; i++) {
-            int generalEmployeeSalary = 0;
-            for (int j = 0; j < data.length; j++) {
-                String[] employeeData = data[j].split(" ");
-                LocalDate date = LocalDate.parse(employeeData[0], FORMATTER);
-                String employeeName = employeeData[1];
-                int countMonths = Integer.parseInt(employeeData[2]);
-                int salary = Integer.parseInt(employeeData[3]);
-                if (names[i].equals(employeeName)
-                        && date.isAfter(firstLimit.minusDays(1))
-                        && date.isBefore(secondLimit.plusDays(1))) {
-                    generalEmployeeSalary += countMonths * salary;
+        LocalDate startDate = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate endDate = LocalDate.parse(dateTo, FORMATTER);
+        for (String name : names) {
+            int totalSalary = 0;
+            for (String currentData : data) {
+                String[] employeeData = currentData.split(" ");
+                LocalDate date = LocalDate.parse(employeeData[DATE_INDEX], FORMATTER);
+                String employeeName = employeeData[NAME_INDEX];
+                int countMonths = Integer.parseInt(employeeData[MONTH_INDEX]);
+                int salary = Integer.parseInt(employeeData[SALARY_INDEX]);
+                if (name.equals(employeeName)
+                        && date.isAfter(startDate.minusDays(1))
+                        && date.isBefore(endDate.plusDays(1))) {
+                    totalSalary += countMonths * salary;
                 }
             }
-            info.append(System.lineSeparator()).append(names[i]).append(" - ");
-            info.append(generalEmployeeSalary);
+            info.append(System.lineSeparator()).append(name).append(" - ");
+            info.append(totalSalary);
         }
         return info.toString();
     }
