24d8143738cf5141077e8283ca79c4e2f13bc30f
kromchk <kromchyk@yahoo.com>
2023-02-16 20:40:54 +0200

    refactoring code #1

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,52 +1,49 @@
 package core.basesyntax;
 
-import java.text.DateFormat;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
-import java.time.ZoneId;
-import java.util.Date;
-import java.util.Locale;
+import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private final String pattern = "dd.MM.yyyy";
-    private final SimpleDateFormat formatter = new SimpleDateFormat(pattern);
+    private static final String DELIMITER = " ";
+    private static final String SIGN = " - ";
+    private static final String REPORT_MESSAGE = "Report for period ";
+    private static final int DATE_VALUE = 0;
+    private static final int EMPLOYER = 1;
+    private static final int HOURS = 2;
+    private static final int PRICE = 3;
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.uuuu");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        StringBuilder res = new StringBuilder();
+        StringBuilder result = new StringBuilder();
         try {
-            LocalDate date1 = formatter.parse(dateFrom).toInstant()
-                    .atZone(ZoneId.systemDefault()).toLocalDate();
-            LocalDate date2 = formatter.parse(dateTo).toInstant()
-                    .atZone(ZoneId.systemDefault()).toLocalDate();
-            if (date2.compareTo(date1) < 0) {
+            final LocalDate fromDate = LocalDate.parse(dateFrom, formatter);
+            final LocalDate toDate = LocalDate.parse(dateTo, formatter);
+            if (toDate.compareTo(fromDate) < 0) {
                 throw new ParseException("DateFrom occurs after DateTo", 0);
             }
-            DateFormat dateFormat;
-            dateFormat = DateFormat.getDateInstance(DateFormat.MEDIUM, Locale.GERMANY);
-            res.append("Report for period " + dateFormat.format(Date.from(date1.atStartOfDay()
-                    .atZone(ZoneId.systemDefault()).toInstant())) + " - "
-                    + dateFormat.format(Date.from(date2.atStartOfDay()
-                    .atZone(ZoneId.systemDefault()).toInstant())));
-            for (String n: names) {
+            result.append(REPORT_MESSAGE).append(fromDate.format(formatter))
+                    .append(SIGN).append(toDate.format(formatter));
+            for (String name: names) {
                 int money = 0;
-                for (String d: data) {
-                    var temp = d.split(" ");
-                    var dtmp = formatter.parse(temp[0]).toInstant().atZone(ZoneId.systemDefault())
-                            .toLocalDate();
-                    if ((date1.compareTo(dtmp) < 0 || date1.compareTo(dtmp) == 0) 
-                            && (date2.compareTo(dtmp) > 0 || date2.compareTo(dtmp) == 0)
-                            && n.trim().equals(temp[1].trim())) {
-                        money += Integer.parseInt(temp[2]) * Integer.parseInt(temp[3]);
+                for (String dat: data) {
+                    var record = dat.split(DELIMITER);
+                    var dataRecord = LocalDate.parse(record[DATE_VALUE], formatter);
+                    if ((fromDate.compareTo(dataRecord) < 0 || fromDate.compareTo(dataRecord) == 0)
+                            && (toDate.compareTo(dataRecord) > 0
+                            || toDate.compareTo(dataRecord) == 0)
+                            && name.trim().equals(record[EMPLOYER].trim())) {
+                        money += Integer.parseInt(record[HOURS]) * Integer.parseInt(record[PRICE]);
                     }
                 }
-                res.append("\n" + n.trim() + " - " + String.format("%d", money));
+                result.append(System.lineSeparator()).append(name.trim())
+                        .append(SIGN).append(money);
             }
         } catch (NumberFormatException e) {
             System.out.println(e.getMessage());
         } catch (ParseException e) {
             System.out.println(e.getMessage());
         }
-        return res.toString();
+        return result.toString();
     }
 }
