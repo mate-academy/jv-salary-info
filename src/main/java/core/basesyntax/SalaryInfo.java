036e3d019e8be23f58bd3ca7e382d7d1d8b7437f
Richard <richarddkochut@gmail.com>
2023-02-16 09:16:50 +0200

    refactor

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -2,33 +2,42 @@ package core.basesyntax;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 import java.util.List;
 
 public class SalaryInfo {
+
+    public static final String EMPTY_SPACE = " ";
+
+    public static final String REPORT_STRING = "Report for period ";
+    public static final String DASH = " - ";
+    public static final int INDEX_OF_DATE = 0;
+    public static final int INDEX_OF_NAME = 1;
+    public static final int INDEX_HOUR_AMOUNT = 2;
+    public static final int INDEX_SALARY_BY_HOUR = 3;
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        StringBuilder result = new StringBuilder();
-        int[] arrayResultInt = new int[names.length];
-        List<String> list = new ArrayList<>(List.of(names));
+        StringBuilder builderOfResult = new StringBuilder();
+        int[] arrayOfSalaries = new int[names.length];
+        List<String> listOfNames = List.of(names);
         LocalDate startDate = LocalDate.parse(dateFrom, formatter);
         LocalDate endDate = LocalDate.parse(dateTo, formatter);
-        for (String s : data) {
-            String[] split = s.split(" ");
-            LocalDate localDate = LocalDate.parse(split[0], formatter);
-            if ((localDate.isAfter(startDate) || localDate.isEqual(startDate))
-                    && (localDate.isBefore(endDate) || localDate.isEqual(endDate))) {
-                int salarySum = Integer.parseInt(split[2]) * Integer.parseInt(split[3]);
-                arrayResultInt[list.indexOf(split[1])] += salarySum;
+        for (String dataPerDay : data) {
+            String[] split = dataPerDay.split(EMPTY_SPACE);
+            LocalDate localDateInData = LocalDate.parse(split[INDEX_OF_DATE], formatter);
+            if ((localDateInData.isAfter(startDate) || localDateInData.isEqual(startDate))
+                    && (localDateInData.isBefore(endDate) || localDateInData.isEqual(endDate))) {
+                int salarySum = Integer.parseInt(split[INDEX_HOUR_AMOUNT])
+                        * Integer.parseInt(split[INDEX_SALARY_BY_HOUR]);
+                arrayOfSalaries[listOfNames.indexOf(split[INDEX_OF_NAME])] += salarySum;
             }
         }
-        result.append("Report for period ")
-                .append(dateFrom).append(" - ").append(dateTo).append(System.lineSeparator());
-        for (int i = 0; i < list.size(); i++) {
-            result.append(list.get(i)).append(" - ")
-                    .append(arrayResultInt[i]).append(System.lineSeparator());
+        builderOfResult.append(REPORT_STRING)
+                .append(dateFrom).append(DASH).append(dateTo).append(System.lineSeparator());
+        for (int i = 0; i < listOfNames.size(); i++) {
+            builderOfResult.append(listOfNames.get(i)).append(" - ")
+                    .append(arrayOfSalaries[i]).append(System.lineSeparator());
         }
-        return result.toString().trim();
+        return builderOfResult.toString().trim();
     }
 }
