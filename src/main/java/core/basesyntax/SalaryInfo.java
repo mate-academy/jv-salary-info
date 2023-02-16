d8213b52e013e94e91dfd0c7b81442fdcf20e08b
irynamatveieva <irina.m2004@icloud.com>
2023-02-16 14:04:24 +0200

    made formatter a constant

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,8 +4,9 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 public class SalaryInfo {
+    private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy");
         StringBuilder builder = new StringBuilder("Report for period " + dateFrom + " - " + dateTo);
         for (String name : names) {
             builder.append(System.lineSeparator() + name + " - ");
@@ -14,16 +15,16 @@ public class SalaryInfo {
                 String date = line.substring(0, 10);
                 if (line.matches("(.*)" + name + "(.*)")) {
                     try {
-                        if (format.parse(date).after(format.parse(dateFrom))
-                                && format.parse(date).before(format.parse(dateTo))
-                                || format.parse(date).equals(format.parse(dateTo))) {
+                        if (DATE_FORMAT.parse(date).after(DATE_FORMAT.parse(dateFrom))
+                                && DATE_FORMAT.parse(date).before(DATE_FORMAT.parse(dateTo))
+                                || DATE_FORMAT.parse(date).equals(DATE_FORMAT.parse(dateTo))) {
                             String[] hoursAndPerHour = line.split(name + " ")[1].split(" ");
                             int hours = Integer.parseInt(hoursAndPerHour[0]);
                             int perHour = Integer.parseInt(hoursAndPerHour[1]);
                             salary += hours * perHour;
                         }
                     } catch (ParseException e) {
-                        e.printStackTrace();
+                        throw new RuntimeException("Cannot parse the date from data");
                     }
                 }
             }
