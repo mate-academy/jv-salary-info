6491a2e40b6fa1b97031df287febd64b29a44e2c
irynamatveieva <irina.m2004@icloud.com>
2023-02-16 16:29:53 +0200

    used LocalDateFormatter and remove try/catch constructor

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,30 +1,33 @@
 package core.basesyntax;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd.MM.yyyy");
+    private static final String DATE_PATTERN = "dd.MM.yyyy";
+    private static final DateTimeFormatter DATE_FORMATTER
+            = DateTimeFormatter.ofPattern(DATE_PATTERN);
+    private static final int FIRST_ELEMENT = 0;
+    private static final int SECOND_ELEMENT = 1;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder builder = new StringBuilder("Report for period " + dateFrom + " - " + dateTo);
+        LocalDate fromDate = LocalDate.parse(dateFrom, DATE_FORMATTER);
+        LocalDate toDate = LocalDate.parse(dateTo, DATE_FORMATTER);
         for (String name : names) {
             builder.append(System.lineSeparator() + name + " - ");
             int salary = 0;
             for (String line : data) {
-                String date = line.substring(0, 10);
                 if (line.matches("(.*)" + name + "(.*)")) {
-                    try {
-                        if (DATE_FORMAT.parse(date).after(DATE_FORMAT.parse(dateFrom))
-                                && DATE_FORMAT.parse(date).before(DATE_FORMAT.parse(dateTo))
-                                || DATE_FORMAT.parse(date).equals(DATE_FORMAT.parse(dateTo))) {
-                            String[] hoursAndPerHour = line.split(name + " ")[1].split(" ");
-                            int hours = Integer.parseInt(hoursAndPerHour[0]);
-                            int perHour = Integer.parseInt(hoursAndPerHour[1]);
-                            salary += hours * perHour;
-                        }
-                    } catch (ParseException e) {
-                        throw new RuntimeException("Cannot parse the date from data");
+                    String date = line.split(" ")[FIRST_ELEMENT];
+                    LocalDate currentDate = LocalDate.parse(date, DATE_FORMATTER);
+                    if (currentDate.isAfter(fromDate) && currentDate.isBefore(toDate)
+                            || currentDate.isEqual(toDate)) {
+                        String[] hoursAndPerHour
+                                = line.split(name + " ")[SECOND_ELEMENT].split(" ");
+                        int hours = Integer.parseInt(hoursAndPerHour[FIRST_ELEMENT]);
+                        int perHour = Integer.parseInt(hoursAndPerHour[SECOND_ELEMENT]);
+                        salary += hours * perHour;
                     }
                 }
             }
