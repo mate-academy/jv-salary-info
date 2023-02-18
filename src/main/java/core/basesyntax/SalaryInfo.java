50af9ca992421362fc0a2582fc345cb219f52ef3
irynamatveieva <irina.m2004@icloud.com>
2023-02-18 16:49:12 +0200

    created some variables for code simplicity and remove one check

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -7,8 +7,10 @@ public class SalaryInfo {
     private static final String DATE_PATTERN = "dd.MM.yyyy";
     private static final DateTimeFormatter DATE_FORMATTER
             = DateTimeFormatter.ofPattern(DATE_PATTERN);
-    private static final int FIRST_ELEMENT = 0;
-    private static final int SECOND_ELEMENT = 1;
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int HOURS_INDEX = 2;
+    private static final int PER_HOUR_INDEX = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder builder = new StringBuilder("Report for period " + dateFrom + " - " + dateTo);
@@ -18,17 +20,17 @@ public class SalaryInfo {
             builder.append(System.lineSeparator() + name + " - ");
             int salary = 0;
             for (String line : data) {
-                if (line.matches("(.*)" + name + "(.*)")) {
-                    String date = line.split(" ")[FIRST_ELEMENT];
-                    LocalDate currentDate = LocalDate.parse(date, DATE_FORMATTER);
-                    if (currentDate.isAfter(fromDate) && currentDate.isBefore(toDate)
-                            || currentDate.isEqual(toDate)) {
-                        String[] hoursAndPerHour
-                                = line.split(name + " ")[SECOND_ELEMENT].split(" ");
-                        int hours = Integer.parseInt(hoursAndPerHour[FIRST_ELEMENT]);
-                        int perHour = Integer.parseInt(hoursAndPerHour[SECOND_ELEMENT]);
-                        salary += hours * perHour;
-                    }
+                String[] splited = line.split(" ");
+                String dateOnLine = splited[DATE_INDEX];
+                String nameOnLine = splited[NAME_INDEX];
+                String hoursOnLine = splited[HOURS_INDEX];
+                String perHourOnLine = splited[PER_HOUR_INDEX];
+                LocalDate currentDate = LocalDate.parse(dateOnLine, DATE_FORMATTER);
+                if ((currentDate.isAfter(fromDate) && currentDate.isBefore(toDate)
+                        || currentDate.isEqual(toDate)) && nameOnLine.equals(name)) {
+                    int hours = Integer.parseInt(hoursOnLine);
+                    int perHour = Integer.parseInt(perHourOnLine);
+                    salary += hours * perHour;
                 }
             }
             builder.append(salary);
