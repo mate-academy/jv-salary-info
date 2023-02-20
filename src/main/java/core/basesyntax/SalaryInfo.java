7f02f335ea5a56d16bb4d03ec29184ea5373a508
Oleg Ravlyk <oleg.ravlyk2005@gmail.com>
2023-02-20 19:52:57 +0200

    added and tested solution

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -3,24 +3,38 @@ package core.basesyntax;
 import java.util.Date;
 
 public class SalaryInfo {
-    StringBuilder result = new StringBuilder();
-    private static final int ONE_DAY = 3_600_000;
-    private static final int HOURS_PER_DAY = 24;
-    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        result.append("Result for period" ).append(dateFrom).append(" - ").append(dateTo).append(System.lineSeparator());
-        String[] dateFromSeparateArray = dateFrom.split("\\.");
-        String[] dateToSeparateArray = dateTo.split("\\.");
-        int[] dateFromNumber = new int[dateFromSeparateArray.length];
-        int[] dateToNumber = new int[dateToSeparateArray.length];
-        for (int i = 0; i < 3; i++) {
-            dateFromNumber[i] =  Integer.parseInt(dateFromSeparateArray[i]);
-            dateToNumber[i] =  Integer.parseInt(dateToSeparateArray[i]);
-        }
-        Date date1 = new Date(dateFromNumber[2], dateFromNumber[1], dateFromNumber[0]);
-        Date date2 = new Date(dateToNumber[2], dateToNumber[1], dateToNumber[0]);
-        long totalHours = date2.getTime() - date1.getTime();
-        int dayCount = (int) totalHours / ONE_DAY / HOURS_PER_DAY;
-        return "";
+    private static final byte DATE_LENGTH = 3;
 
+    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
+        StringBuilder result = new StringBuilder();
+        result.append("Report for period ").append(dateFrom).append(" - ").append(dateTo).append(System.lineSeparator());
+        Date date1 = convertStringToDate(dateFrom);
+        Date date2 = convertStringToDate(dateTo);
+
+        for (int i = 0; i < names.length; i++) {
+            int totalSalary = 0;
+            for (int k = 0; k < data.length; k++) {
+                if (data[k].contains(names[i])) {
+                    String[] separateData = data[k].split(" ");
+                    Date date = convertStringToDate(separateData[0]);
+                    if (date.compareTo(date1) >= 0 && date.compareTo(date2) <= 0) {
+                        totalSalary += Integer.parseInt(separateData[2]) * Integer.parseInt(separateData[3]);
+                    }
+                }
+            }
+            result.append(names[i]).append(" - ").append(totalSalary).append(System.lineSeparator());
+        }
+        result.delete(result.length() - 2, result.length());
+        return result.toString();
+
+    }
+
+    private static final Date convertStringToDate(String date) {
+        String[] dateSeparateArray = date.split("\\.");
+        int[] dateNumber = new int[dateSeparateArray.length];
+        for (int i = 0; i < DATE_LENGTH; i++) {
+            dateNumber[i] = Integer.parseInt(dateSeparateArray[i]);
+        }
+        return new Date(dateNumber[2], dateNumber[1], dateNumber[0]);
     }
 }
