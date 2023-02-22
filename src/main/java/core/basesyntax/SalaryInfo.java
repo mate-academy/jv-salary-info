e4243119d1f0a2a08a4292651ce84b1b43631bfa
Oleg Ravlyk <oleg.ravlyk2005@gmail.com>
2023-02-22 02:26:35 +0200

    replaced LocalDate instead of Date, added constants

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,9 +1,13 @@
 package core.basesyntax;
 
-import java.util.Date;
+import java.time.LocalDate;
 
 public class SalaryInfo {
     private static final byte DATE_LENGTH = 3;
+    private static final byte YEAR_ARRAY_INDEX = 0;
+    private static final byte WORK_HOURS_ARRAY_INDEX = 2;
+    private static final byte SALARY_PERHOUR_ARRAY_INDEX = 3;
+    private static final byte EQUAL_YEAR_CONDITION = 0;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder result = new StringBuilder();
@@ -11,19 +15,19 @@ public class SalaryInfo {
                 .append(dateFrom)
                 .append(" - ")
                 .append(dateTo);
-        Date date1 = convertStringToDate(dateFrom);
-        Date date2 = convertStringToDate(dateTo);
+        LocalDate date1 = convertStringToDate(dateFrom);
+        LocalDate date2 = convertStringToDate(dateTo);
 
         for (int i = 0; i < names.length; i++) {
             int totalSalary = 0;
             for (int k = 0; k < data.length; k++) {
                 if (data[k].contains(names[i])) {
                     String[] separateData = data[k].split(" ");
-                    Date date = convertStringToDate(separateData[0]);
-                    if (date.compareTo(date1) >= 0
-                            && date.compareTo(date2) <= 0) {
-                        totalSalary += Integer.parseInt(separateData[2])
-                                * Integer.parseInt(separateData[3]);
+                    LocalDate date = convertStringToDate(separateData[YEAR_ARRAY_INDEX]);
+                    if (date.compareTo(date1) >= EQUAL_YEAR_CONDITION
+                            && date.compareTo(date2) <= EQUAL_YEAR_CONDITION) {
+                        totalSalary += Integer.parseInt(separateData[WORK_HOURS_ARRAY_INDEX])
+                                * Integer.parseInt(separateData[SALARY_PERHOUR_ARRAY_INDEX]);
                     }
                 }
             }
@@ -36,12 +40,12 @@ public class SalaryInfo {
 
     }
 
-    private static final Date convertStringToDate(String date) {
+    private static final LocalDate convertStringToDate(String date) {
         String[] dateSeparateArray = date.split("\\.");
         int[] dateNumber = new int[dateSeparateArray.length];
         for (int i = 0; i < DATE_LENGTH; i++) {
             dateNumber[i] = Integer.parseInt(dateSeparateArray[i]);
         }
-        return new Date(dateNumber[2], dateNumber[1], dateNumber[0]);
+        return LocalDate.of(dateNumber[2], dateNumber[1], dateNumber[0]);
     }
 }
