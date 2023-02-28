ee77b25cea4ba77e68bf63b38da2f56b28378301
Kostia Demchuk <kostiademchuk23@gmail.com>
2023-02-28 21:48:47 +0200

    fixed after review

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -6,24 +6,29 @@ import java.time.format.DateTimeParseException;
 
 public class SalaryInfo {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-    private int salary = 0;
-    private LocalDate localDate;
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int WORKING_HOUR_INDEX = 2;
+    private static final int INCOME_PER_HOUR_INDEX = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
+        int salary = 0;
 
         StringBuilder builder =
                 new StringBuilder("Report for period " + dateFrom + " - " + dateTo);
 
         for (String name : names) {
-
             for (String info : data) {
                 String[] employeeInfo = info.split(" ");
 
-                if (name.equals(employeeInfo[1])
-                        && strToDate(employeeInfo[0]).plusDays(1).isAfter(strToDate(dateFrom))
-                        && strToDate(employeeInfo[0]).minusDays(1).isBefore(strToDate(dateTo))) {
+                if (name.equals(employeeInfo[NAME_INDEX])
+                        && (strToDate(employeeInfo[DATE_INDEX]).isAfter(strToDate(dateFrom))
+                        || strToDate(employeeInfo[DATE_INDEX]).isEqual(strToDate(dateFrom)))
+                        && (strToDate(employeeInfo[DATE_INDEX]).isBefore(strToDate(dateTo))
+                        || strToDate(employeeInfo[DATE_INDEX]).isEqual(strToDate(dateTo)))) {
 
-                    salary += Integer.parseInt(employeeInfo[2]) * Integer.parseInt(employeeInfo[3]);
+                    salary += Integer.parseInt(employeeInfo[WORKING_HOUR_INDEX])
+                            * Integer.parseInt(employeeInfo[INCOME_PER_HOUR_INDEX]);
                 }
             }
 
@@ -35,6 +40,7 @@ public class SalaryInfo {
     }
 
     private LocalDate strToDate(String inputDate) {
+        LocalDate localDate = null;
         try {
             localDate = LocalDate.parse(inputDate, FORMATTER);
         } catch (DateTimeParseException e) {
