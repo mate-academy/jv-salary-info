5dc46ed9ee112953f5d532031cd7d7a626dceea3
Artem <artempharm83@gmail.com>
2023-02-28 16:08:52 +0200

    Implementation of the getSalaryInfo method: import java.time.LocalDate import java.time.format.DateTimeFormatter added constants: private static final DateTimeFormatter FORMATTER private static final int DATE private static final int NAME private static final int HOUR private static final int INCOME Used LocalDate for input dates dateFrom and dateTo

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,32 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int DATE = 0;
+    private static final int NAME = 1;
+    private static final int HOUR = 2;
+    private static final int INCOME = 3;
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        LocalDate dateOne = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate dateTwo = LocalDate.parse(dateTo, FORMATTER);
+        StringBuilder result = new StringBuilder("Report for period " + dateFrom + " - " + dateTo);
+        for (String name: names) {
+            int salary = 0;
+            for (String line: data) {
+                String [] parseLine = line.split(" ");
+                LocalDate dates = LocalDate.parse(parseLine[DATE], FORMATTER);
+                if (name.equals(parseLine[NAME]) && dateOne.compareTo(dates) <= 0
+                            && dateTwo.compareTo(dates) >= 0) {
+                    salary += Integer.valueOf(parseLine[HOUR]) * Integer.valueOf(parseLine[INCOME]);
+                }
+            }
+            result.append(System.lineSeparator()).append(name).append(" - ").append(salary);
+        }
+        return result.toString();
+
     }
 }
