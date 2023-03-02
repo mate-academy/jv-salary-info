099da60a1c7beea8a5096138fba7b6ba218e56dc
Rule <amirparadice@gmail.com>
2023-03-01 19:20:07 -0500

    1.Initiating of  StringBuilder;\n 2.Formating dates, using Local date Formatter, and importing two packages;\n 3.Creating loops for parsing Arrays, creating if statement checking by names;\n 4.Spliting data Array into 4 columns, naming and formatting of each column;\n 5. Using of second conditional for checking dates, and writing formula to calculate salary;/n 6.finishing String by SttringBuilder, and returning result.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,42 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        StringBuilder report = new StringBuilder("Report for period ");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDate from = LocalDate.parse(dateFrom, formatter);
+        LocalDate to = LocalDate.parse(dateTo, formatter);
+        report.append(dateFrom)
+                .append(" - ")
+                .append(dateTo);
+
+        for (String name : names) {
+            int salary = 0;
+            for (int i = 0; i < data.length; i++) {
+                if (data[i].contains(name)) {
+                    String[] b = data[i].split(" ");
+                    String[][] dt = new String[data.length][4];
+                    // just to have possibility to call each element;
+                    dt[i][0] = b[0];
+                    dt[i][1] = b[1];
+                    dt[i][2] = b[2];
+                    dt[i][3] = b[3];
+                    int hoursWorking = Integer.parseInt(dt[i][2]);
+                    int moneyPerHour = Integer.parseInt(dt[i][3]);
+                    LocalDate date = LocalDate.parse(dt[i][0], formatter);
+                    if (!date.isBefore(from) && !date.isAfter(to)) {
+                        salary += hoursWorking * moneyPerHour;
+                    }
+                }
+            }
+            report.append(System.lineSeparator())
+                    .append(name)
+                    .append(" - ")
+                    .append(salary);
+        }
+        return report.toString();
     }
 }
