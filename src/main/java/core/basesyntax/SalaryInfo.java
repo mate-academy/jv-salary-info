eb6328160f870cfaf2a0621c846542fd6c453a01
Vitalii Romanyuk <vitaliy37777@gmail.com>
2023-03-03 23:53:49 +0200

    fixed all comments from Mentor

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,62 +1,44 @@
 package core.basesyntax;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private final SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy");
+    private static final String SEPARATOR = " ";
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        String[] checkLine;
         StringBuilder finalReport = new StringBuilder();
+        String[] checkLine;
+        boolean isDateInRange;
+        LocalDate dateRangeBegin;
+        LocalDate dateRangeEnd;
+        LocalDate dataSalary;
+
         finalReport.append("Report for period ")
                 .append(dateFrom).append(" - ")
-                .append(dateTo).append("\r");
+                .append(dateTo);
 
-        for (String name: names) {
-            finalReport.append("\n").append(name).append(" - ");
+        for (String name : names) {
+            finalReport.append(System.lineSeparator()).append(name).append(" - ");
             int salary = 0;
 
             for (String dateString : data) {
-                checkLine = splitString(dateString, " ");
-                if (name.equals(checkLine[1])) {
-                    if (isDateInRange(dateFrom, dateTo, checkLine[0])) {
-                        salary = salary
-                                + (Integer.parseInt(checkLine[2]) * Integer.parseInt(checkLine[3]));
-                    }
+                checkLine = dateString.split(SEPARATOR);
+                dateRangeBegin = LocalDate.parse(dateFrom, formatter);
+                dateRangeEnd = LocalDate.parse(dateTo, formatter);
+                dataSalary = LocalDate.parse(checkLine[0], formatter);
+                isDateInRange = dateRangeBegin.isBefore(dataSalary)
+                        && dateRangeEnd.isAfter(dataSalary)
+                        || dateRangeBegin.equals(dataSalary)
+                        || dateRangeEnd.equals(dataSalary);
+
+                if (name.equals(checkLine[1]) && isDateInRange) {
+                    salary += (Integer.parseInt(checkLine[2]) * Integer.parseInt(checkLine[3]));
                 }
             }
-
-            if (!name.equals(names[names.length - 1])) {
-                finalReport.append(salary).append("\r");
-            } else {
-                finalReport.append(salary);
-            }
-
+            finalReport.append(salary);
         }
-
         return finalReport.toString();
-    }
-
-    public boolean isDateInRange(String dateRangeBegin, String dateRangeEnd, String dataSalary) {
-        try {
-            Date date1 = format.parse(dateRangeBegin);
-            Date date2 = format.parse(dateRangeEnd);
-            Date date3 = format.parse(dataSalary);
-
-            if (date1.before(date3) && date2.after(date3)
-                    || date1.equals(date3) || date2.equals(date3)) {
-                return true;
-            }
-        } catch (ParseException e) {
-            System.out.println("Format date is not right");;
-        }
-
-        return false;
-    }
-
-    public String[] splitString(String textForSplit, String splitSymbol) {
-        return textForSplit.split(splitSymbol);
     }
 }
