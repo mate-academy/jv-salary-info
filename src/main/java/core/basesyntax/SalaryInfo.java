6378aaeb7378e4c0e75587de69f58ce5f7165abe
Vitalii Romanyuk <vitaliy37777@gmail.com>
2023-03-04 22:29:54 +0200

    fixed all comments from Mentor (olekskov)

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -8,27 +8,18 @@ public class SalaryInfo {
     private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        StringBuilder finalReport = new StringBuilder();
-        String[] checkLine;
-        boolean isDateInRange;
-        LocalDate dateRangeBegin;
-        LocalDate dateRangeEnd;
-        LocalDate dataSalary;
-
-        finalReport.append("Report for period ")
-                .append(dateFrom).append(" - ")
-                .append(dateTo);
+        StringBuilder finalReport = new StringBuilder("Report for period ")
+                .append(dateFrom).append(" - ").append(dateTo);
+        LocalDate dateRangeBegin = LocalDate.parse(dateFrom, formatter);
+        LocalDate dateRangeEnd = LocalDate.parse(dateTo, formatter);
 
         for (String name : names) {
-            finalReport.append(System.lineSeparator()).append(name).append(" - ");
             int salary = 0;
 
             for (String dateString : data) {
-                checkLine = dateString.split(SEPARATOR);
-                dateRangeBegin = LocalDate.parse(dateFrom, formatter);
-                dateRangeEnd = LocalDate.parse(dateTo, formatter);
-                dataSalary = LocalDate.parse(checkLine[0], formatter);
-                isDateInRange = dateRangeBegin.isBefore(dataSalary)
+                String[] checkLine = dateString.split(SEPARATOR);
+                LocalDate dataSalary = LocalDate.parse(checkLine[0], formatter);
+                boolean isDateInRange = dateRangeBegin.isBefore(dataSalary)
                         && dateRangeEnd.isAfter(dataSalary)
                         || dateRangeBegin.equals(dataSalary)
                         || dateRangeEnd.equals(dataSalary);
@@ -37,7 +28,7 @@ public class SalaryInfo {
                     salary += (Integer.parseInt(checkLine[2]) * Integer.parseInt(checkLine[3]));
                 }
             }
-            finalReport.append(salary);
+            finalReport.append(System.lineSeparator()).append(name).append(" - ").append(salary);
         }
         return finalReport.toString();
     }
