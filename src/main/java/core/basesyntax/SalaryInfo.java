db28580ede38f1735ed0e7e80aec409ec4225f26
Valerii Kulibchenko <kulibchenkovalera@gmail.com>
2023-03-07 15:20:56 +0200

    3rd commit (fix index names and formatter as constant)

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -6,23 +6,23 @@ import java.time.format.DateTimeFormatter;
 public class SalaryInfo {
     private static final int DATE_INDEX = 0;
     private static final int NAME_INDEX = 1;
-    private static final int NUMBERS_INDEX = 2;
-    private static final int VALUE_INDEX = 3;
+    private static final int AMOUNT_INDEX_1 = 2;
+    private static final int AMOUNT_INDEX_2 = 3;
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate localDate;
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
         StringBuilder result = new StringBuilder("Report for period " + dateFrom + " - " + dateTo);
         int[] sum = new int[names.length];
         for (String string: data) {
             String[] split = string.split(" ");
-            localDate = LocalDate.parse(split[DATE_INDEX],formatter);
-            if (!localDate.isAfter(LocalDate.parse(dateTo,formatter))
-                    && !localDate.isBefore(LocalDate.parse(dateFrom,formatter))) {
+            localDate = LocalDate.parse(split[DATE_INDEX], FORMATTER);
+            if (!localDate.isAfter(LocalDate.parse(dateTo, FORMATTER))
+                    && !localDate.isBefore(LocalDate.parse(dateFrom, FORMATTER))) {
                 for (int i = 0; i < names.length; i++) {
                     if (names[i].equals(split[NAME_INDEX])) {
-                        sum[i] += Integer.parseInt(split[NUMBERS_INDEX])
-                                * Integer.parseInt(split[VALUE_INDEX]);
+                        sum[i] += Integer.parseInt(split[AMOUNT_INDEX_1])
+                                * Integer.parseInt(split[AMOUNT_INDEX_2]);
                     }
                 }
             }
