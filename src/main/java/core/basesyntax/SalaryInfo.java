c0cd80287d09beede312c083430e24758d243af5
Victor Rudkovsky <vpcomtek@gmail.com>
2023-03-10 00:22:26 +0200

    HW jv-salary-info v1.1

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,39 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+    private static final int DATE = 0;
+    private static final int NAME = 1;
+    private static final int WORK_HOURS = 2;
+    private static final int RATE = 3;
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        LocalDate from = LocalDate.parse(dateFrom, formatter);
+        LocalDate to = LocalDate.parse(dateTo, formatter);
+        StringBuilder salaryInfo = new StringBuilder();
+
+        salaryInfo.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
+
+        for (String name : names) {
+            int salary = 0;
+            for (String d : data) {
+                String[] emData = d.split(" ");
+                if (name.equals(emData[NAME]) && isInPeriod(emData[DATE], from, to)) {
+                    salary += Integer.parseInt(emData[WORK_HOURS]) * Integer.parseInt(emData[RATE]);
+                }
+            }
+            salaryInfo.append('\n').append(name).append(" - ").append(salary);
+        }
+
+        return salaryInfo.toString();
+    }
+
+    private boolean isInPeriod(String date, LocalDate from, LocalDate to) {
+        LocalDate workingDay = LocalDate.parse(date, formatter);
+        return (workingDay.isEqual(from) || workingDay.isAfter(from))
+                && (workingDay.isBefore(to) || workingDay.isEqual(to));
     }
 }
