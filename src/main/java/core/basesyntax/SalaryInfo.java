2ae175f106bcb46fe4640b18a5427d569c4528a1
Andrii <subashandrii@gmail.com>
2023-03-10 19:51:37 +0200

    hw-jv-salary-info

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,33 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDate startDate = LocalDate.parse(dateFrom, formatter);
+        LocalDate lastDate = LocalDate.parse(dateTo, formatter);
+
+        StringBuilder result = new StringBuilder("Report for period ").append(dateFrom)
+                .append(" - ").append(dateTo);
+
+        for (String name: names) {
+            int totalSalary = 0;
+            result.append(System.lineSeparator()).append(name).append(" - ");
+
+            for (int i = 0; i < data.length; i++) {
+                String[] arrayElements = data[i].split(" ");
+                LocalDate dataDate = LocalDate.parse(arrayElements[0], formatter);
+                if (arrayElements[1].equals(name)
+                        && (dataDate.isAfter(startDate) || dataDate.isEqual(startDate))
+                        && (dataDate.isBefore(lastDate) || dataDate.isEqual(lastDate))) {
+                    totalSalary += Integer.valueOf(arrayElements[2])
+                            * Integer.valueOf(arrayElements[3]);
+                }
+            }
+            result.append(totalSalary);
+        }
+        return result.toString();
     }
 }
