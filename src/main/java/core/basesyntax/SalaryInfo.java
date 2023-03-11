9fd82109bc4bfd7401a5c33d7bf8854bee4f504c
Vyacheslav Sukhov <makasin_12@hotmail.com>
2023-03-11 22:55:02 +0100

    refactored attempt jv.salary-info task

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,30 +4,28 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-
         StringBuilder result = new StringBuilder("Report for period "
                 + dateFrom + " - "
-                + dateTo + System.lineSeparator());
+                + dateTo);
+        LocalDate fromDate = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate toDate = LocalDate.parse(dateTo, FORMATTER);
         int income = 0;
-        LocalDate fromDate = LocalDate.parse(dateFrom, formatter);
-        LocalDate toDate = LocalDate.parse(dateTo, formatter);
-
         for (String name : names) {
             for (String dataString : data) {
                 String[] splitData = dataString.split(" ");
                 int hoursInData = Integer.parseInt(splitData[2]);
                 int incomePerHourInData = Integer.parseInt(splitData[3]);
-                LocalDate date = LocalDate.parse(splitData[0], formatter);
+                LocalDate date = LocalDate.parse(splitData[0], FORMATTER);
                 String nameInData = splitData[1];
-
-                if (nameInData.equals(name) && fromDate.isBefore(date) && toDate.isAfter(date)) {
-                    income += hoursInData * incomePerHourInData;
+                if (nameInData.equals(name) && fromDate.isBefore(date)
+                        && (toDate.isEqual(date) || toDate.isAfter(date))) {
+                    income = income + (hoursInData * incomePerHourInData);
                 }
             }
-            result.append(name).append(" - ").append(income).append(System.lineSeparator());
+            result.append(System.lineSeparator()).append(name).append(" - ").append(income);
             income = 0;
         }
         return result.toString();
