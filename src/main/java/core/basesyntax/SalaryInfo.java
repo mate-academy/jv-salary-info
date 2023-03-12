c7a70908eddf16011c2bd839f22dfeafa4efa196
Andrii <subashandrii@gmail.com>
2023-03-12 11:41:13 +0200

    hw-jv-salary-info/ fix bugs

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,6 +4,11 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    private static final int DATE = 0;
+    private static final int NAME = 1;
+    private static final int HOURS = 2;
+    private static final int HOURLY_WAGE = 3;
+    
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
         LocalDate startDate = LocalDate.parse(dateFrom, formatter);
@@ -12,21 +17,21 @@ public class SalaryInfo {
         StringBuilder result = new StringBuilder("Report for period ").append(dateFrom)
                 .append(" - ").append(dateTo);
 
-        for (String name: names) {
+        for (String name : names) {
             int totalSalary = 0;
-            result.append(System.lineSeparator()).append(name).append(" - ");
 
             for (int i = 0; i < data.length; i++) {
-                String[] arrayElements = data[i].split(" ");
-                LocalDate dataDate = LocalDate.parse(arrayElements[0], formatter);
-                if (arrayElements[1].equals(name)
-                        && (dataDate.isAfter(startDate) || dataDate.isEqual(startDate))
-                        && (dataDate.isBefore(lastDate) || dataDate.isEqual(lastDate))) {
-                    totalSalary += Integer.valueOf(arrayElements[2])
-                            * Integer.valueOf(arrayElements[3]);
+                String[] employeeInformation = data[i].split(" ");
+                LocalDate workDayDate = LocalDate.parse(employeeInformation[DATE], formatter);
+                
+                if (employeeInformation[NAME].equals(name)
+                        && (workDayDate.isAfter(startDate) || workDayDate.isEqual(startDate))
+                        && (workDayDate.isBefore(lastDate) || workDayDate.isEqual(lastDate))) {
+                    totalSalary += Integer.valueOf(employeeInformation[HOURS])
+                            * Integer.valueOf(employeeInformation[HOURLY_WAGE]);
                 }
             }
-            result.append(totalSalary);
+            result.append(System.lineSeparator()).append(name).append(" - ").append(totalSalary);
         }
         return result.toString();
     }
