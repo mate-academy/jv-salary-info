67d495f9d9e65c5bb934fe07c7137475c1509086
Sergey Shestaka <sergey.shestaka@gmail.com>
2023-03-12 09:44:29 +0100

    deleted useless variables / implemented logic without arrays / created constants for all magic numbers /

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,52 +1,43 @@
 package core.basesyntax;
 
+import java.time.DateTimeException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 public class SalaryInfo {
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int DATE = 0;
+    private static final int NAME = 1;
+    private static final int HOURS = 2;
+    private static final int PAY = 3;
 
-    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
+    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo)
+            throws DateTimeParseException, DateTimeException {
         final LocalDate dateStart = LocalDate.parse(dateFrom, formatter);
         final LocalDate dateFinish = LocalDate.parse(dateTo, formatter);
-        final int namesNumber = names.length;
-        LocalDate dateEmployee;
-        StringBuilder[] results = new StringBuilder[namesNumber + 1];
-        int[] salary = new int[namesNumber];
-
-        results[0] = new StringBuilder("Report for period ");
-        results[0].append(dateFrom).append(" - ").append(dateTo);
+        LocalDate localDate;
+        StringBuilder report = new StringBuilder("Report for period " + dateFrom + " - " + dateTo);
 
         if (data == null) {
             return "";
         }
 
         for (int j = 0; j < names.length; j++) {
+            int salary = 0;
             for (int i = 0; i < data.length; i++) {
                 String[] record = data[i].split(" ");
-                dateEmployee = LocalDate.parse(record[0], formatter);
-                if (names[j].equals(record[1])
-                        && (dateEmployee.isEqual(dateStart) || dateEmployee.isAfter(dateStart))
-                        && (dateEmployee.isEqual(dateFinish)
-                        || dateEmployee.isBefore(dateFinish))) {
-                    results[j + 1] = new StringBuilder(names[j] + " - ");
-                    salary[j] += Integer.parseInt(record[2]) * Integer.parseInt(record[3]);
-                } else if (!dateEmployee.isBefore(dateFinish)) {
-                    results[j + 1] = new StringBuilder(names[j] + " - ");
-                    salary[j] += 0;
+                localDate = LocalDate.parse(record[DATE], formatter);
+                if (names[j].equals(record[NAME])
+                        && (localDate.isEqual(dateStart) || localDate.isAfter(dateStart))
+                        && (localDate.isEqual(dateFinish)
+                        || localDate.isBefore(dateFinish))) {
+                    salary += Integer.parseInt(record[HOURS]) * Integer.parseInt(record[PAY]);
                 }
             }
+            report.append(System.lineSeparator())
+                    .append(names[j]).append(" - ").append(salary);
         }
-
-        for (int i = 0; i < results.length - 1; i++) {
-            results[i + 1].append(salary[i]);
-        }
-
-        for (int i = 1; i <= results.length - 1; i++) {
-            results[0].append(System.lineSeparator())
-                    .append(results[i]);
-        }
-
-        return new String(results[0].toString());
+        return report.toString();
     }
 }
