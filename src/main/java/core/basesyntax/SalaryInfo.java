c836e5274e5057848fb291d1fa5c72f01bd73095
Lev Shtana <120460187+CapybaraFrog@users.noreply.github.com>
2023-03-23 15:34:24 +0300

    new solution

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -8,8 +8,7 @@ public class SalaryInfo {
             DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        StringBuilder result = new StringBuilder("Report for period "
-                + dateFrom + " - "
+        StringBuilder result = new StringBuilder("Report for period " + dateFrom + " - "
                 + dateTo);
         LocalDate fromDate = LocalDate.parse(dateFrom, DATE_FORMATTER);
         LocalDate toDate = LocalDate.parse(dateTo, DATE_FORMATTER);
@@ -17,13 +16,14 @@ public class SalaryInfo {
         for (String name : names) {
             for (String dataString : data) {
                 String[] splitData = dataString.split(" ");
-                int hoursInData = Integer.parseInt(splitData[2]);
-                int incomePerHourInData = Integer.parseInt(splitData[3]);
                 LocalDate date = LocalDate.parse(splitData[0], DATE_FORMATTER);
                 String nameInData = splitData[1];
                 if (nameInData.equals(name) && fromDate.isBefore(date)
                         && (toDate.isEqual(date) || toDate.isAfter(date))) {
-                    salary = salary + (hoursInData * incomePerHourInData);
+                    int hoursInData = Integer.parseInt(splitData[2]);
+                    int incomePerHourInData = Integer.parseInt(splitData[3]);
+                    salary += hoursInData * incomePerHourInData;
+
                 }
             }
             result.append(System.lineSeparator()).append(name).append(" - ").append(salary);
