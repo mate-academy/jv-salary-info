e9f697e0147b54bcbf48018ef5cb7c47fc7d673a
Andrii First <andre_first@ukr.net>
2023-03-23 22:51:52 +0000

    Implemented getSalaryInfo method. Use two loops and stringBuilder. Attempt 2.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -10,20 +10,17 @@ public class SalaryInfo {
     private static final int NAME_INDEX = 1;
     private static final int HOURS_INDEX = 2;
     private static final int SALARY_INDEX = 3;
-    private static final int INITIAL_SALARY_AMOUNT = 0;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        String[] splitedData;
         LocalDate fromDate = LocalDate.parse(dateFrom, DATE_FORMATER);
         LocalDate toDate = LocalDate.parse(dateTo, DATE_FORMATER);
-        int salary;
         StringBuilder builder = new StringBuilder("Report for period ")
                 .append(dateFrom).append(" - ")
                 .append(dateTo);
         for (String name : names) {
-            salary = INITIAL_SALARY_AMOUNT;
-            for (String s : data) {
-                splitedData = s.split(" ");
+            int salary = 0;
+            for (String salaryItem : data) {
+                String[] splitedData = salaryItem.split(" ");
                 LocalDate tempDate = LocalDate.parse(splitedData[DATE_INDEX], DATE_FORMATER);
                 if (splitedData[NAME_INDEX].equals(name)
                         && !tempDate.isBefore(fromDate)
