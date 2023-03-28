4937501e6610e5d8d75b06cfd6c2f9eb2d4eaa37
Serhii Serdiuk <serhii.serdiuk@gmail.com>
2023-03-28 02:03:35 +0200

    Create getSalaryInfo method 2

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,57 +1,43 @@
 package core.basesyntax;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
+    public static String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         String[] countData = new String[data.length];
         int[] wages = new int[names.length];
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM.yyyy");
-        Date fromDate;
-        try {
-            fromDate = simpleDateFormat.parse(dateFrom);
-        } catch (ParseException e) {
-            throw new RuntimeException(e);
-        }
-        Date toDate;
-        try {
-            toDate = simpleDateFormat.parse(dateTo);
-        } catch (ParseException e) {
-            throw new RuntimeException(e);
-        }
-        Date compareDate;
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDate fromDate = LocalDate.parse(dateFrom, dateTimeFormatter);
+        LocalDate toDate = LocalDate.parse(dateTo, dateTimeFormatter);
+        LocalDate compareDate;
         String withoutName;
         int hours;
         int money;
-        int wage;
+        int wage = 0;
         int i = 0;
         for (String date : data) {
-            try {
-                compareDate = simpleDateFormat.parse(date.substring(0, 10));
-            } catch (ParseException e) {
-                throw new RuntimeException(e);
-            }
-            if (fromDate.getTime() <= compareDate.getTime()
-                    && compareDate.getTime() <= toDate.getTime()) {
+            compareDate = LocalDate.parse(date.substring(0, 10), dateTimeFormatter);
+            if ((fromDate.isBefore(compareDate) || fromDate.isEqual(compareDate))
+                    && (toDate.isAfter(compareDate) || toDate.isEqual(compareDate))) {
                 countData[i] = date.substring(10);
                 i++;
             }
         }
         i = 0;
-        for (String s : countData) {
-            for (String name : names) {
-                if (s.contains(name)) {
-                    withoutName = s.substring(name.length()).trim();
-                    hours = Integer.parseInt(withoutName.substring(0, withoutName.indexOf(' ')));
+        for (String name : names) {
+            for (String s : countData) {
+                if (s != null && s.contains(name)) {
+                    withoutName = s.substring(name.length() + 1).trim();
+                    hours = Integer.parseInt(withoutName.substring(0, withoutName.indexOf(" ")));
                     money = Integer.parseInt(withoutName
-                            .substring(withoutName.indexOf(' ')).trim());
-                    wage = hours * money;
-                    wages[i] = wage;
-                    i++;
+                            .substring(withoutName.indexOf(" ")).trim());
+                    wage += hours * money;
                 }
             }
+            wages[i] = wage;
+            wage = 0;
+            i++;
         }
         StringBuilder output = new StringBuilder();
         output.append("Report for period ").append(dateFrom).append(" - ")
