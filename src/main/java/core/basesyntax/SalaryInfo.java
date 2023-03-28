fb0d3337c0c1a1229f4c188d10eef6fcaba059be
Sergey Zavalniuk <sergzavalnyuk@gmail.com>
2023-03-28 17:41:37 +0300

    Made method getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo). It calculates salary for employees

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,37 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
-    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final String REPORT_TITLE = "Report for period ";
+    private static final String SEPARATOR = " - ";
+    private static final String Empty = " ";
+
+    public static String getSalaryInfo(String[] names, String[] data,
+                                       String dateFrom, String dateTo) {
+        StringBuilder result = new StringBuilder(REPORT_TITLE
+                + dateFrom + SEPARATOR
+                + dateTo);
+        LocalDate fromDate = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate toDate = LocalDate.parse(dateTo, FORMATTER);
+        int income = 0;
+        for (String name : names) {
+            for (String dataString : data) {
+                String[] splitData = dataString.split(Empty);
+                int hoursInData = Integer.parseInt(splitData[2]);
+                int incomePerHourInData = Integer.parseInt(splitData[3]);
+                LocalDate date = LocalDate.parse(splitData[0], FORMATTER);
+                String nameInData = splitData[1];
+                if (nameInData.equals(name) && fromDate.isBefore(date)
+                        && (toDate.isEqual(date) || toDate.isAfter(date))) {
+                    income = income + (hoursInData * incomePerHourInData);
+                }
+            }
+            result.append(System.lineSeparator()).append(name).append(SEPARATOR).append(income);
+            income = 0;
+        }
+        return result.toString();
     }
 }
