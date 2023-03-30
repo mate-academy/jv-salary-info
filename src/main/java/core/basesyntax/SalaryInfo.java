7a001fc26f6dc2392ba0dc89e8edb8357cad339f
Nikita Sachik <nikitasachik@gmail.com>
2023-03-30 15:48:49 +0300

    Task complete with 2 loops and by using DateTimeFormatter for parsing date

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,51 +1,38 @@
 package core.basesyntax;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    private static final int WORKING_DATE_NUMBER = 0;
+    private static final int NAME_NUMBER = 1;
+    private static final int WORK_HOURS_PER_DAY_NUMBER = 2;
+    private static final int PRICE_FOR_ONE_WORK_HOUR = 3;
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder stringBuilder = new StringBuilder("Report for period ");
-        stringBuilder.append(dateFrom + " - " + dateTo);
-        final int dayNumber = 0;
-        final int monthNumber = 1;
-        final int yearNumber = 2;
-        String[] dateFromString = dateFrom.split("\\.");
-        String[] dateToString = dateTo.split("\\.");
-        LocalDate localDateFrom = LocalDate.of(Integer.valueOf(dateFromString[yearNumber]),
-                Integer.valueOf(dateFromString[monthNumber]),
-                Integer.valueOf(dateFromString[dayNumber]));
-        LocalDate localDateTo = LocalDate.of(Integer.valueOf(dateToString[yearNumber]),
-                Integer.valueOf(dateToString[monthNumber]),
-                Integer.valueOf(dateToString[dayNumber]));
+        stringBuilder.append(dateFrom).append(" - ").append(dateTo);
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDate localDateFrom = LocalDate.parse(dateFrom, dateTimeFormatter);
+        LocalDate localDateTo = LocalDate.parse(dateTo, dateTimeFormatter);
         LocalDate workingDate;
-        String[] workingDateToString;
         String[] employerInfo;
-        final int workingDateNumber = 0;
-        final int nameNumber = 1;
-        final int workHoursPerDayNumber = 2;
-        final int priceForOneWorkHour = 3;
-        int[] employersSalary = new int[names.length];
-
-        for (int i = 0; i < data.length; i++) {
-            employerInfo = data[i].split(" ");
-            workingDateToString = employerInfo[workingDateNumber].split("\\.");
-            workingDate =
-                    LocalDate.of(Integer.valueOf(workingDateToString[yearNumber]),
-                    Integer.valueOf(workingDateToString[monthNumber]),
-                            Integer.valueOf(workingDateToString[dayNumber]));
-            for (int j = 0; j < names.length; j++) {
-                if (employerInfo[nameNumber].equals(names[j])
+        for (String name : names) {
+            int salary = 0;
+            for (String dataString : data) {
+                employerInfo = dataString.split(" ");
+                workingDate = LocalDate.parse(employerInfo[WORKING_DATE_NUMBER], dateTimeFormatter);
+                if (name.equals(employerInfo[NAME_NUMBER])
                         && (workingDate.isAfter(localDateFrom)
-                        && workingDate.isBefore(localDateTo) || workingDate.equals(localDateTo)
+                        && workingDate.isBefore(localDateTo)
+                        || workingDate.equals(localDateTo)
                         || workingDate.equals(localDateFrom))) {
-                    employersSalary[j] += Integer.valueOf(employerInfo[priceForOneWorkHour])
-                            * Integer.valueOf(employerInfo[workHoursPerDayNumber]);
+                    salary += Integer.parseInt(employerInfo[PRICE_FOR_ONE_WORK_HOUR])
+                            * Integer.parseInt(employerInfo[WORK_HOURS_PER_DAY_NUMBER]);
                 }
             }
-        }
-        for (int i = 0; i < names.length; i++) {
             stringBuilder.append(System.lineSeparator());
-            stringBuilder.append(names[i] + " - " + employersSalary[i]);
+            stringBuilder.append(name).append(" - ").append(salary);
         }
         return stringBuilder.toString();
     }
