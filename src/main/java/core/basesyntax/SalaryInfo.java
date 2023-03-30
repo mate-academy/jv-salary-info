c0c6edf742dc250f357f54ffe668d6b448f43571
Nikita Sachik <nikitasachik@gmail.com>
2023-03-30 17:13:27 +0300

    Signature mistakes fixed

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,15 +4,17 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private static final int WORKING_DATE_NUMBER = 0;
-    private static final int NAME_NUMBER = 1;
-    private static final int WORK_HOURS_PER_DAY_NUMBER = 2;
-    private static final int PRICE_FOR_ONE_WORK_HOUR = 3;
+    private static final int WORKING_DATE_NUMBER_INDEX = 0;
+    private static final int NAME_NUMBER_INDEX = 1;
+    private static final int WORK_HOURS_PER_DAY_NUMBER_INDEX = 2;
+    private static final int PRICE_FOR_ONE_WORK_HOUR_INDEX = 3;
+    private static final String SPLIT_BY_THIS_STRING = " ";
+    private static final DateTimeFormatter dateTimeFormatter
+            = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        StringBuilder stringBuilder = new StringBuilder("Report for period ");
-        stringBuilder.append(dateFrom).append(" - ").append(dateTo);
-        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        StringBuilder stringBuilder = new StringBuilder("Report for period ")
+                .append(dateFrom).append(" - ").append(dateTo);
         LocalDate localDateFrom = LocalDate.parse(dateFrom, dateTimeFormatter);
         LocalDate localDateTo = LocalDate.parse(dateTo, dateTimeFormatter);
         LocalDate workingDate;
@@ -20,19 +22,19 @@ public class SalaryInfo {
         for (String name : names) {
             int salary = 0;
             for (String dataString : data) {
-                employerInfo = dataString.split(" ");
-                workingDate = LocalDate.parse(employerInfo[WORKING_DATE_NUMBER], dateTimeFormatter);
-                if (name.equals(employerInfo[NAME_NUMBER])
+                employerInfo = dataString.split(SPLIT_BY_THIS_STRING);
+                workingDate = LocalDate.parse(employerInfo[WORKING_DATE_NUMBER_INDEX],
+                        dateTimeFormatter);
+                if (name.equals(employerInfo[NAME_NUMBER_INDEX])
                         && (workingDate.isAfter(localDateFrom)
                         && workingDate.isBefore(localDateTo)
                         || workingDate.equals(localDateTo)
                         || workingDate.equals(localDateFrom))) {
-                    salary += Integer.parseInt(employerInfo[PRICE_FOR_ONE_WORK_HOUR])
-                            * Integer.parseInt(employerInfo[WORK_HOURS_PER_DAY_NUMBER]);
+                    salary += Integer.parseInt(employerInfo[PRICE_FOR_ONE_WORK_HOUR_INDEX])
+                            * Integer.parseInt(employerInfo[WORK_HOURS_PER_DAY_NUMBER_INDEX]);
                 }
             }
-            stringBuilder.append(System.lineSeparator());
-            stringBuilder.append(name).append(" - ").append(salary);
+            stringBuilder.append(System.lineSeparator()).append(name).append(" - ").append(salary);
         }
         return stringBuilder.toString();
     }
