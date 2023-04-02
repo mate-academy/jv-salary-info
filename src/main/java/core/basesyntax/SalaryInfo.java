1e4e633d199f988d8ab56724bc662adb53b48a95
petrozagorodnii <patriculusus@gmail.com>
2023-04-02 13:06:37 +0300

    Use LocalDate instead of Date Delete convertStringToDate method as it doesn't need anymore

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,11 +1,10 @@
 package core.basesyntax;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private static final SimpleDateFormat SIMPLE_DATE_FORMAT = new SimpleDateFormat("dd.MM.yyyy");
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
     private static final int DATE_POSITION = 0;
     private static final int NAME_POSITION = 1;
     private static final int HOURS_POSITION = 2;
@@ -13,18 +12,18 @@ public class SalaryInfo {
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         Employee[] employees = new Employee[names.length];
-        Date dateFromData = convertStringToDate(dateFrom);
-        Date dateToData = convertStringToDate(dateTo);
+        LocalDate dateFromData = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate dateToData = LocalDate.parse(dateTo, FORMATTER);
         for (int i = 0; i < names.length; i++) {
             employees[i] = new Employee(names[i]);
         }
         for (Employee employee : employees) {
             for (String dataString : data) {
                 String[] dataArray = dataString.split(" ");
-                Date date = convertStringToDate(dataArray[DATE_POSITION]);
+                LocalDate date = LocalDate.parse(dataArray[DATE_POSITION], FORMATTER);
                 if (employee.getName().equalsIgnoreCase(dataArray[NAME_POSITION])
-                        && (date.after(dateFromData) || date.equals(dateFromData))
-                        && (date.before(dateToData) || date.equals(dateToData))) {
+                        && (date.isAfter(dateFromData) || date.isEqual(dateFromData))
+                        && (date.isBefore(dateToData) || date.isEqual(dateToData))) {
                     employee.addEarnedMoney(Integer.parseInt(dataArray[HOURS_POSITION])
                             * Integer.parseInt(dataArray[MONEY_POSITION]));
                 }
@@ -38,15 +37,5 @@ public class SalaryInfo {
                     .append(employee.getEarnedMoney());
         }
         return stringBuilder.toString();
-    }
-
-    private Date convertStringToDate(String date) {
-        Date dateConverted;
-        try {
-            dateConverted = SIMPLE_DATE_FORMAT.parse(date);
-        } catch (ParseException e) {
-            throw new RuntimeException("Date can't be converted", e);
-        }
-        return dateConverted;
     }
 }
