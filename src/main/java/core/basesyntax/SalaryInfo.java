ae37ff105230924e84a380be99167082d8e3f9f7
Oleksandr Rolik <rolik.alexx@gmail.com>
2023-04-03 22:18:57 +0300

    fix issues with constants

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -6,10 +6,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class SalaryInfo {
+    private static final String SPLIT_SEPARATOR = " ";
+    private static final String DATE_PATTERN = "dd.MM.yyyy";
+    private static final String LINE_SEPARATOR = " - ";
 
     public static String getSalaryInfo(String[] names, String[] data,
                                        String dateFrom, String dateTo) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_PATTERN);
         LocalDate fromDate = LocalDate.parse(dateFrom, formatter);
         LocalDate toDate = LocalDate.parse(dateTo, formatter);
 
@@ -19,7 +22,7 @@ public class SalaryInfo {
         }
 
         for (String line : data) {
-            String[] parts = line.split(" ");
+            String[] parts = line.split(SPLIT_SEPARATOR);
             LocalDate date = LocalDate.parse(parts[0], formatter);
             if (!date.isBefore(fromDate) && !date.isAfter(toDate)) {
                 String name = parts[1];
@@ -30,12 +33,14 @@ public class SalaryInfo {
             }
         }
 
-        StringBuilder sb = new StringBuilder();
-        sb.append("Report for period ").append(dateFrom).append(" - ").append(dateTo)
+        StringBuilder builder = new StringBuilder();
+        builder.append("Report for period ").append(dateFrom).append(LINE_SEPARATOR).append(dateTo)
                 .append(System.lineSeparator());
         for (String name : names) {
-            sb.append(name).append(" - ").append(earnings.get(name)).append(System.lineSeparator());
+            builder.append(name).append(LINE_SEPARATOR)
+                    .append(earnings.get(name))
+                    .append(System.lineSeparator());
         }
-        return sb.toString().trim();
+        return builder.toString().trim();
     }
 }
