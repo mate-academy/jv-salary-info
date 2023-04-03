1e89e8434b0b2ecf31d4f81967ff2568af521262
Artem Kostenko <a_kostenco@yahoo.com>
2023-04-03 16:41:53 +0300

    second fix errors after mentor review

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -6,10 +6,10 @@ import java.time.format.DateTimeFormatter;
 public class SalaryInfo {
     private static final String pattern = "dd.MM.yyyy";
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
-    private static final String WHITE_SPASE = " ";
-    private static final int DATE = 0;
-    private static final int HOUR = 2;
-    private static final int INCOME_PER_HOUR = 3;
+    private static final String SEPARATOR = " ";
+    private static final int DATE_INDEX = 0;
+    private static final int HOUR_INDEX = 2;
+    private static final int INCOME_PER_HOUR_INDEX = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder stringBuilder = new StringBuilder().append("Report for period ")
@@ -18,17 +18,17 @@ public class SalaryInfo {
         LocalDate endDate = LocalDate.parse(dateTo, formatter);
         LocalDate currentDate;
 
-        for (String name: names) {
+        for (String name : names) {
             int sum = 0;
-            for (String dateLine: data) {
+            for (String dateLine : data) {
                 if (dateLine.contains(name)) {
-                    String[] record = dateLine.split(WHITE_SPASE);
-                    currentDate = LocalDate.parse(record[DATE], formatter);
+                    String[] record = dateLine.split(SEPARATOR);
+                    currentDate = LocalDate.parse(record[DATE_INDEX], formatter);
 
-                    if (currentDate.compareTo(beginDate) == 0 || currentDate.compareTo(endDate) == 0
+                    if (currentDate.isEqual(beginDate) || currentDate.isEqual(endDate)
                             || (currentDate.isAfter(beginDate) && currentDate.isBefore(endDate))) {
-                        sum += Integer.parseInt(record[HOUR])
-                                * Integer.parseInt(record[INCOME_PER_HOUR]);
+                        sum += Integer.parseInt(record[HOUR_INDEX])
+                                * Integer.parseInt(record[INCOME_PER_HOUR_INDEX]);
                     }
                 }
             }
