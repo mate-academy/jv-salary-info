9a75714479e065a7c47fc3eb275b5c17de982f83
Artem Kostenko <a_kostenco@yahoo.com>
2023-04-03 16:12:08 +0300

    fix errors after mentor review

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -6,9 +6,13 @@ import java.time.format.DateTimeFormatter;
 public class SalaryInfo {
     private static final String pattern = "dd.MM.yyyy";
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
+    private static final String WHITE_SPASE = " ";
+    private static final int DATE = 0;
+    private static final int HOUR  = 2;
+    private static final int INCOME_PER_HOUR = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        StringBuilder sb = new StringBuilder().append("Report for period ")
+        StringBuilder stringBuilder = new StringBuilder().append("Report for period ")
                 .append(dateFrom).append(" - ").append(dateTo);
         LocalDate beginDate = LocalDate.parse(dateFrom, formatter);
         LocalDate endDate = LocalDate.parse(dateTo, formatter);
@@ -18,17 +22,17 @@ public class SalaryInfo {
             int sum = 0;
             for (String dateLine: data) {
                 if (dateLine.contains(name)) {
-                    String[] record = dateLine.split(" ");
-                    currentDate = LocalDate.parse(record[0], formatter);
+                    String[] record = dateLine.split(WHITE_SPASE);
+                    currentDate = LocalDate.parse(record[DATE], formatter);
 
                     if (currentDate.compareTo(beginDate) == 0 || currentDate.compareTo(endDate) == 0
                             || (currentDate.isAfter(beginDate) && currentDate.isBefore(endDate))) {
-                        sum += Integer.parseInt(record[2]) * Integer.parseInt(record[3]);
+                        sum += Integer.parseInt(record[HOUR]) * Integer.parseInt(record[INCOME_PER_HOUR]);
                     }
                 }
             }
-            sb.append(System.lineSeparator()).append(name).append(" - ").append(sum);
+            stringBuilder.append(System.lineSeparator()).append(name).append(" - ").append(sum);
         }
-        return sb.toString();
+        return stringBuilder.toString();
     }
 }
