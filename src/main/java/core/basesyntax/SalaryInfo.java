b7594f480541cb9b95bdaaa1dc0713bf295150d5
Oleksandr Rolik <rolik.alexx@gmail.com>
2023-04-03 17:07:55 +0300

    initial commit

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,41 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
+
 public class SalaryInfo {
-    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+
+    public static String getSalaryInfo(String[] names, String[] data,
+                                       String dateFrom, String dateTo) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDate fromDate = LocalDate.parse(dateFrom, formatter);
+        LocalDate toDate = LocalDate.parse(dateTo, formatter);
+
+        Map<String, Integer> earnings = new HashMap<>();
+        for (String name : names) {
+            earnings.put(name, 0);
+        }
+
+        for (String line : data) {
+            String[] parts = line.split(" ");
+            LocalDate date = LocalDate.parse(parts[0], formatter);
+            if (!date.isBefore(fromDate) && !date.isAfter(toDate)) {
+                String name = parts[1];
+                int hours = Integer.parseInt(parts[2]);
+                int rate = Integer.parseInt(parts[3]);
+                int amount = hours * rate;
+                earnings.put(name, earnings.get(name) + amount);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("Report for period ").append(dateFrom).append(" - ").append(dateTo)
+                .append(System.lineSeparator());
+        for (String name : names) {
+            sb.append(name).append(" - ").append(earnings.get(name)).append(System.lineSeparator());
+        }
+        return sb.toString().trim();
     }
 }
