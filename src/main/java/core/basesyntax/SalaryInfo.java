db078b0275647bf47942bcf9601edc00cbd9df89
Artem Kostenko <a_kostenco@yahoo.com>
2023-04-03 14:09:25 +0300

    first commit, realise method getSalaryInfo

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,45 @@
 package core.basesyntax;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class SalaryInfo {
+    private static SimpleDateFormat format = new SimpleDateFormat();
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        sb.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
+        format.applyPattern("dd.MM.yyyy");
+        Date beginDate = null;
+        Date endDate = null;
+        try {
+            beginDate = format.parse(dateFrom);
+            endDate = format.parse(dateTo);
+        } catch (ParseException e) {
+            return "Format dates is unknown";
+        }
+        Date currentDate = null;
+
+        for (String name: names) {
+            int sum = 0;
+            for (String dateLine: data) {
+                if (dateLine.contains(name)) {
+                    String[] record = dateLine.split(" ");
+                    try {
+                        currentDate = format.parse(record[0]);
+                    } catch (ParseException e) {
+                        return "Format data record is unknown";
+                    }
+                    if (currentDate.compareTo(beginDate) == 0 || currentDate.compareTo(endDate) == 0
+                            || (currentDate.after(beginDate) && currentDate.before(endDate))) {
+                        sum += Integer.parseInt(record[2]) * Integer.parseInt(record[3]);
+                    }
+                }
+
+            }
+            sb.append("\r\n").append(name).append(" - ").append(sum);
+        }
+        return sb.toString();
     }
 }
