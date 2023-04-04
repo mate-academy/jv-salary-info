25311da15c176fee81d707946dd555229e01954f
Vladyslav <vladyslavsolianyk@gmail.com>
2023-04-04 18:45:34 +0300

    implemented the method "getSalaryInfo" which parses the necessary dates, and also calculates the total salary of each person for a separate period of time

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -6,27 +6,35 @@ import java.time.format.DateTimeFormatter;
 public class SalaryInfo {
     private static final String PATTERN_FORMAT = "dd.MM.yyyy";
 
+    private static final int CONSTANT_NUM = 1;
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern(PATTERN_FORMAT);
         LocalDate payPeriodStart = LocalDate.parse(dateFrom, formatter);
         LocalDate payPeriodEnd = LocalDate.parse(dateTo, formatter);
 
-        int totalSalary = 0;
         StringBuilder builder = new StringBuilder();
-        builder.append("Report for period ").append(payPeriodStart).append(" - ")
-                .append(payPeriodEnd).append(System.lineSeparator());
+        builder.append("Report for period ").append(dateFrom).append(" - ")
+                .append(dateTo).append(System.lineSeparator());
 
-        for (String name : names) {
-            for (int i = 0; i < data.length; i++) {
-                String[] splitArrayOfData = data[i].split(" ");
+        for (int i = 0; i < names.length; i++) {
+            int totalSalary = 0;
+            for (String dataOfPerson : data) {
+                String[] splitArrayOfData = dataOfPerson.split(" ");
                 LocalDate particularDay = LocalDate.parse(splitArrayOfData[0], formatter);
-                if (name.equals(splitArrayOfData[1]) &&
-                        particularDay.isAfter(payPeriodStart.minusDays(1))
-                        && particularDay.isBefore(payPeriodEnd.plusDays(1))) {
-                    totalSalary += (Integer.parseInt(splitArrayOfData[2]) * Integer.parseInt(splitArrayOfData[3]));
+                if (names[i].equals(splitArrayOfData[1])
+                        && particularDay.isAfter(payPeriodStart.minusDays(CONSTANT_NUM))
+                        && particularDay.isBefore(payPeriodEnd.plusDays(CONSTANT_NUM))) {
+                    totalSalary += (Integer.parseInt(splitArrayOfData[2])
+                            * Integer.parseInt(splitArrayOfData[3]));
                 }
             }
-            builder.append(name).append(" - ").append(totalSalary).append(System.lineSeparator());
+            if (i == names.length - 1) {
+                builder.append(names[i]).append(" - ").append(totalSalary);
+            } else {
+                builder.append(names[i]).append(" - ").append(totalSalary)
+                        .append(System.lineSeparator());
+            }
         }
         return builder.toString();
     }
