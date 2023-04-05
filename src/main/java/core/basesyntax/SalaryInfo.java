fde55ec7875bffa2224fdcca99a8122d85c360e3
Artem <artemenko.as@gmail.com>
2023-04-05 12:00:13 +0300

    Renamed all one or two letter names. Removed hardcoded indexes and replaced by constant values. Removed new line "\n" to System.lineSeparator().

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,6 +4,11 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    public static final int DATE_INDEX = 0;
+    public static final int NAME_INDEX = 1;
+    public static final int WORKING_HOURS_INDEX = 2;
+    public static final int HOURLY_PRICE_INDEX = 3;
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
         LocalDate startDate = LocalDate.parse(dateFrom, formatter);
@@ -12,19 +17,19 @@ public class SalaryInfo {
 
         for (String name : names) {
             int salary = 0;
-            for (String d : data) {
-                String[] split = d.split(" ");
-                LocalDate date = LocalDate.parse(split[0], formatter);
-                String workerName = split[1];
-                int hours = Integer.parseInt(split[2]);
-                int hourlySalary = Integer.parseInt(split[3]);
+            for (String newData : data) {
+                String[] split = newData.split(" ");
+                LocalDate date = LocalDate.parse(split[DATE_INDEX], formatter);
+                String workerName = split[NAME_INDEX];
+                int hours = Integer.parseInt(split[WORKING_HOURS_INDEX]);
+                int hourlySalary = Integer.parseInt(split[HOURLY_PRICE_INDEX]);
                 if (name.equals(workerName)
                         && !date.isBefore(startDate)
                         && !date.isAfter(endDate)) {
                     salary += hours * hourlySalary;
                 }
             }
-            builder.append("\n").append(name).append(" - ").append(salary);
+            builder.append(System.lineSeparator()).append(name).append(" - ").append(salary);
         }
         return builder.toString();
     }
