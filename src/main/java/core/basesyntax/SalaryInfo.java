2d76d2b59aa38734b216702d5a9616c185611e2f
Sergey Zavalniuk <sergzavalnyuk@gmail.com>
2023-04-09 18:57:50 +0300

    Used append in StringBuilder; Added array indexes to level constants;

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -8,29 +8,33 @@ public class SalaryInfo {
     private static final String REPORT_TITLE = "Report for period ";
     private static final String SEPARATOR = " - ";
     private static final String Empty = " ";
+    private static final int ZERO_POSITION = 0;
+    private static final int FIRST_POSITION = 1;
+    private static final int SECOND_POSITION = 2;
+    private static final int THIRD_POSITION = 3;
 
     public static String getSalaryInfo(String[] names, String[] data,
                                        String dateFrom, String dateTo) {
-        StringBuilder result = new StringBuilder(REPORT_TITLE
-                + dateFrom + SEPARATOR
-                + dateTo);
+        StringBuilder result = new StringBuilder(REPORT_TITLE)
+                .append(dateFrom)
+                .append(SEPARATOR)
+                .append(dateTo);
         LocalDate fromDate = LocalDate.parse(dateFrom, FORMATTER);
         LocalDate toDate = LocalDate.parse(dateTo, FORMATTER);
-        int income = 0;
         for (String name : names) {
+            int income = 0;
             for (String dataString : data) {
                 String[] splitData = dataString.split(Empty);
-                int hoursInData = Integer.parseInt(splitData[2]);
-                int incomePerHourInData = Integer.parseInt(splitData[3]);
-                LocalDate date = LocalDate.parse(splitData[0], FORMATTER);
-                String nameInData = splitData[1];
+                int hoursInData = Integer.parseInt(splitData[SECOND_POSITION]);
+                int incomePerHourInData = Integer.parseInt(splitData[THIRD_POSITION]);
+                LocalDate date = LocalDate.parse(splitData[ZERO_POSITION], FORMATTER);
+                String nameInData = splitData[FIRST_POSITION];
                 if (nameInData.equals(name) && fromDate.isBefore(date)
                         && (toDate.isEqual(date) || toDate.isAfter(date))) {
                     income = income + (hoursInData * incomePerHourInData);
                 }
             }
             result.append(System.lineSeparator()).append(name).append(SEPARATOR).append(income);
-            income = 0;
         }
         return result.toString();
     }
