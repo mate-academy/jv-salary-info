6ea536f5f2a6c237ca1ed5320e676a5163cf279c
Yurii Ivanov <ivanovyura76@gmail.com>
2023-04-13 16:34:46 +0300

    Make constants CONSTANT_CASE, rename variable line 17, 18

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -6,28 +6,29 @@ import java.time.format.DateTimeFormatter;
 public class SalaryInfo {
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
     private static final StringBuilder result = new StringBuilder();
-    private static final int dateIndex = 0;
-    private static final int nameIndex = 1;
-    private static final int workingHourIndex = 2;
-    private static final int incomePerDayIndex = 3;
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int WORKING_HOUR_INDEX = 2;
+    private static final int INCOME_PER_DAY_INDEX = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         int moneyEarned = 0;
         result.delete(0,result.length());
-        LocalDate dateFromDate = LocalDate.parse(dateFrom, formatter);
-        LocalDate dateToDate = LocalDate.parse(dateTo, formatter);
+        LocalDate outputdateFrom = LocalDate.parse(dateFrom, formatter);
+        LocalDate outputdateTo = LocalDate.parse(dateTo, formatter);
         result.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
 
         for (String nameCurrentEmployee : names) {
             moneyEarned = 0;
             for (String dateInfoCurrent : data) {
                 String[] currentDayInfo = dateInfoCurrent.split(" ");
-                LocalDate dateCurrentDate = LocalDate.parse(currentDayInfo[dateIndex], formatter);
-                if (dateCurrentDate.isAfter(dateFromDate) && (dateCurrentDate.isBefore(dateToDate)
-                        || dateCurrentDate.isEqual(dateToDate))
-                        && nameCurrentEmployee.equals(currentDayInfo[nameIndex])) {
-                    moneyEarned = moneyEarned + Integer.parseInt(currentDayInfo[workingHourIndex])
-                           * Integer.parseInt(currentDayInfo[incomePerDayIndex]);
+                LocalDate dateCurrentDate = LocalDate.parse(currentDayInfo[DATE_INDEX], formatter);
+                if (dateCurrentDate.isAfter(outputdateFrom)
+                        && (dateCurrentDate.isBefore(outputdateTo)
+                        || dateCurrentDate.isEqual(outputdateTo))
+                        && nameCurrentEmployee.equals(currentDayInfo[NAME_INDEX])) {
+                    moneyEarned = moneyEarned + Integer.parseInt(currentDayInfo[WORKING_HOUR_INDEX])
+                           * Integer.parseInt(currentDayInfo[INCOME_PER_DAY_INDEX]);
                 }
             }
             result.append(System.lineSeparator()).append(nameCurrentEmployee)
