466f88b5653f39f73a524d3c215b18675a59a230
Yurii Ivanov <ivanovyura76@gmail.com>
2023-04-13 04:10:14 +0300

    task solution

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,33 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        int moneyEarned = 0;
+        StringBuilder result = new StringBuilder();
+        LocalDate dateFromDate = LocalDate.parse(dateFrom, formatter);
+        LocalDate dateToDate = LocalDate.parse(dateTo, formatter);
+        result.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
+
+        for (String nameCurrentEmployee : names) {
+            moneyEarned = 0;
+            for (String dateInfoCurrent : data) {
+                String[] currentDayInfo = dateInfoCurrent.split(" ");
+                LocalDate dateCurrentDate = LocalDate.parse(currentDayInfo[0], formatter);
+                if (dateCurrentDate.isAfter(dateFromDate) && (dateCurrentDate.isBefore(dateToDate)
+                        || dateCurrentDate.isEqual(dateToDate))
+                        && nameCurrentEmployee.equals(currentDayInfo[1])) {
+                    moneyEarned = moneyEarned + Integer.parseInt(currentDayInfo[2])
+                           * Integer.parseInt(currentDayInfo[3]);
+                }
+            }
+            result.append(System.lineSeparator()).append(nameCurrentEmployee)
+                    .append(" - ").append(moneyEarned);
+        }
+        return result.toString();
     }
 }
