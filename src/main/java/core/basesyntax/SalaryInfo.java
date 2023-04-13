28a8aedfc6cb71e2b451cfa1d3a2bf0f40032c22
Yurii Ivanov <ivanovyura76@gmail.com>
2023-04-13 18:43:44 +0300

    result make local variable

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,7 +5,6 @@ import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     private static final String FORMAT_DATE = "dd.MM.yyyy";
-    private static final StringBuilder result = new StringBuilder();
     private static final int DATE_INDEX = 0;
     private static final int NAME_INDEX = 1;
     private static final int WORKING_HOUR_INDEX = 2;
@@ -15,7 +14,7 @@ public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern(FORMAT_DATE);
         int moneyEarned = 0;
-        result.delete(0, result.length());
+        StringBuilder result = new StringBuilder();
         LocalDate outputdateFrom = LocalDate.parse(dateFrom, formatter);
         LocalDate outputdateTo = LocalDate.parse(dateTo, formatter);
         result.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
