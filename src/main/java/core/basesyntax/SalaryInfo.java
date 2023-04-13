4b9aaec9e9fb5d462d2360f775c943e856c60f39
Yurii Ivanov <ivanovyura76@gmail.com>
2023-04-13 15:58:44 +0300

    Move indexes to the class constant

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -6,6 +6,10 @@ import java.time.format.DateTimeFormatter;
 public class SalaryInfo {
     private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
     private final StringBuilder result = new StringBuilder();
+    private final int dateIndex = 0;
+    private final int nameIndex = 1;
+    private final int workingHourIndex = 2;
+    private final int incomePerDayIndex = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         int moneyEarned = 0;
@@ -18,12 +22,12 @@ public class SalaryInfo {
             moneyEarned = 0;
             for (String dateInfoCurrent : data) {
                 String[] currentDayInfo = dateInfoCurrent.split(" ");
-                LocalDate dateCurrentDate = LocalDate.parse(currentDayInfo[0], formatter);
+                LocalDate dateCurrentDate = LocalDate.parse(currentDayInfo[dateIndex], formatter);
                 if (dateCurrentDate.isAfter(dateFromDate) && (dateCurrentDate.isBefore(dateToDate)
                         || dateCurrentDate.isEqual(dateToDate))
-                        && nameCurrentEmployee.equals(currentDayInfo[1])) {
-                    moneyEarned = moneyEarned + Integer.parseInt(currentDayInfo[2])
-                           * Integer.parseInt(currentDayInfo[3]);
+                        && nameCurrentEmployee.equals(currentDayInfo[nameIndex])) {
+                    moneyEarned = moneyEarned + Integer.parseInt(currentDayInfo[workingHourIndex])
+                           * Integer.parseInt(currentDayInfo[incomePerDayIndex]);
                 }
             }
             result.append(System.lineSeparator()).append(nameCurrentEmployee)
