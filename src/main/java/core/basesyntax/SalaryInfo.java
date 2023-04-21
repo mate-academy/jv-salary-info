30c6d0d91465e9113feb3a9c3c2827f6bbe51d51
Oleh Pryshchepa <poleja122@gmail.com>
2023-04-21 15:09:37 +0300

    corrected the comments

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,48 +4,52 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    private static final int INDEX_DATE = 0;
+    private static final int INDEX_NAME = 1;
+    private static final int INDEX_WORKING_HOURS = 2;
+    private static final int INDEX_AMOUNT = 3;
+    private static final String SEPARATOR = " ";
+    private static final String DELIMITER = " - ";
+    private static final String DATE_FORMAT = "dd.MM.yyyy";
+    private static final String MESSAGE_FOR_PERIOD = "Report for period ";
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        int[] yearnMoney = new int[names.length];
-        String[] information;
-        LocalDate compareDate;
-        DateTimeFormatter format = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        Employee[] employees = new Employee[names.length];
+        DateTimeFormatter format = DateTimeFormatter.ofPattern(DATE_FORMAT);
         LocalDate localDateFrom = LocalDate.parse(dateFrom, format);
         LocalDate localDateTo = LocalDate.parse(dateTo, format);
-
-        for (String dataString : data) {
-            information = dataString.split(" ");
-            compareDate = LocalDate.parse(information[0],format);
-            if (compareDates(compareDate, localDateFrom, localDateTo)) {
-                for (int i = 0; i < names.length; i++) {
-                    if (information[1].equals(names[i])) {
-                        yearnMoney[i] += Integer.parseInt(information[2])
-                                * Integer.parseInt(information[3]);
+        for (int i = 0; i < names.length; i++) {
+            employees[i] = new Employee(names[i]);
+            for (int j = 0; j < data.length; j++) {
+                String[] information = data[j].split(SEPARATOR);
+                LocalDate compareDate = LocalDate.parse(information[INDEX_DATE],format);
+                if (compareDates(compareDate, localDateFrom, localDateTo)) {
+                    if (information[INDEX_NAME].equals(names[i])) {
+                        employees[i].setName(names[i]);
+                        employees[i].setSalary(Integer.parseInt(information[INDEX_WORKING_HOURS])
+                                * Integer.parseInt(information[INDEX_AMOUNT]));
                     }
                 }
             }
         }
-        return getReport(names, yearnMoney, dateFrom, dateTo);
+        return createReport(employees, dateFrom, dateTo);
     }
 
-    private boolean compareDates(LocalDate firstDate, LocalDate dateFrom, LocalDate dateTo) {
-        if (firstDate == null || dateFrom == null || dateTo == null) {
+    private boolean compareDates(LocalDate date, LocalDate dateFrom, LocalDate dateTo) {
+        if (date == null || dateFrom == null || dateTo == null) {
             return false;
         }
-        if (firstDate.equals(dateFrom) || firstDate.isAfter(dateFrom)) {
-            if (firstDate.equals(dateTo) || firstDate.isBefore(dateTo)) {
-                return true;
-            }
-        }
-        return false;
+        return !date.isBefore(dateFrom) && !date.isAfter(dateTo);
     }
 
-    private String getReport(String[] names, int[] yearnMoney, String dateFrom, String dateTo) {
+    private String createReport(Employee[] employees, String dateFrom, String dateTo) {
         StringBuilder builder = new StringBuilder();
-        builder.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
-        for (int i = 0; i < names.length; i++) {
+        builder.append(MESSAGE_FOR_PERIOD).append(dateFrom).append(DELIMITER).append(dateTo);
+        for (int i = 0; i < employees.length; i++) {
             builder.append(System.lineSeparator())
-                    .append(names[i]).append(" - ").append(yearnMoney[i]);
+                    .append(employees[i].getName())
+                    .append(DELIMITER)
+                    .append(employees[i].getSalary());
         }
         return builder.toString();
     }
