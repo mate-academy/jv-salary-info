fb04c171822b36de489156acbccf8c1eacf78ea4
Yevhen Tolkunov <yevhentolkunov@gmail.com>
2023-04-22 12:01:24 +0300

    second commit

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,7 +4,8 @@ public class SalaryInfo {
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder builder = new StringBuilder();
-        builder.append("Report for period ").append(dateFrom).append(" - ").append(dateTo).append("\n");
+        builder.append("Report for period ").append(dateFrom)
+                .append(" - ").append(dateTo).append("\n");
         int sum = 0;
         for (int i = 0; i < names.length; i++) {
             for (int b = 0; b < data.length; b++) {
@@ -21,7 +22,7 @@ public class SalaryInfo {
         return builder.toString();
     }
 
-    public boolean compareDates (String dateFrom, String dateFromArray, String dateTo) {
+    public boolean compareDates(String dateFrom, String dateFromArray, String dateTo) {
         int dayFirstData = Integer.parseInt(dateFrom.substring(0, 2));
         int monthFirstData = Integer.parseInt(dateFrom.substring(3, 5));
         int yearFirstData = Integer.parseInt(dateFrom.substring(6, 10));
@@ -32,8 +33,12 @@ public class SalaryInfo {
         int monthThirdData = Integer.parseInt(dateTo.substring(3, 5));
         int yearThirdData = Integer.parseInt(dateTo.substring(6, 10));
 
-        if (yearFirstData <= yearSecondData && monthFirstData <= monthSecondData && dayFirstData <= daySecondData) {
-            if (yearSecondData <= yearThirdData && monthSecondData <= monthThirdData && daySecondData <= dayThirdData) {
+        if (yearFirstData <= yearSecondData
+                && monthFirstData <= monthSecondData
+                && dayFirstData <= daySecondData) {
+            if (yearSecondData <= yearThirdData
+                    && monthSecondData <= monthThirdData
+                    && daySecondData <= dayThirdData) {
                 return true;
             }
         }
