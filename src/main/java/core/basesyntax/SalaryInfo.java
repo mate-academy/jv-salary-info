9d6a6db341b2aa3b89caf8251065b7be9b56db97
User <Monsterhunter@ukr.net>
2023-04-25 18:01:39 +0300

    Counted salary using getSalaryInfo method

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -25,7 +25,7 @@ public class SalaryInfo {
     }
 
     public static boolean between(LocalDate d, LocalDate from, LocalDate to) {
-        return d.isAfter(from) || d.isEqual(from) && d.isBefore(to);
+        return (d.isAfter(from) || d.isEqual(from)) && (d.isBefore(to) || d.isEqual(to));
     }
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
@@ -35,7 +35,7 @@ public class SalaryInfo {
         Set<String> set = Set.of(names);
 
         Map<String, Integer> salaryMap = Arrays.stream(data)
-                .map(row -> parse(row))
+                .map(SalaryInfo::parse)
                 .filter(row -> set.contains(row.name()) && between(row.date(), from, to))
                 .collect(Collectors.groupingBy(
                         DataRow::name,
@@ -44,18 +44,18 @@ public class SalaryInfo {
 
         StringBuilder sb = new StringBuilder();
         sb.append("Report for period ")
-                .append(dateFrom).append(" - ").append(dateTo).append(" \n");
+                .append(dateFrom).append(" - ").append(dateTo).append("\r\n");
         Arrays.stream(names)
                 .forEach(name -> sb
                         .append(name)
                         .append(" - ")
                         .append(salaryMap.containsKey(name)
                                 ? String.valueOf(salaryMap.get(name))
-                                : "---"
+                                : "0"
                         )
-                        .append("\n")
+                        .append("\r\n")
                 );
-        return sb.toString();
+        return sb.toString().strip();
     }
 }
 
