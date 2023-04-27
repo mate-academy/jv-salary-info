af83372bdee6287e968a8b1c25dd5281ec22200f
Vladyslav <vladyslavsolianyk@gmail.com>
2023-04-27 14:09:46 +0300

    fixed all mistakes, removed the extra lines and created the necessary constants

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,31 +5,31 @@ import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     private static final String PATTERN_FORMAT = "dd.MM.yyyy";
-
-    private static final int CONSTANT_NUM = 1;
+    private static final int FIRST_ELEMENT = 1;
+    private static final int SECOND_ELEMENT = 2;
+    private static final int THIRD_ELEMENT = 3;
+    private static final int ZERO_INDEX = 0;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern(PATTERN_FORMAT);
         LocalDate payPeriodStart = LocalDate.parse(dateFrom, formatter);
         LocalDate payPeriodEnd = LocalDate.parse(dateTo, formatter);
-
         StringBuilder builder = new StringBuilder();
         builder.append("Report for period ").append(dateFrom).append(" - ")
                 .append(dateTo).append(System.lineSeparator());
-
         for (int i = 0; i < names.length; i++) {
             int totalSalary = 0;
             for (String dataOfPerson : data) {
                 String[] splitArrayOfData = dataOfPerson.split(" ");
-                LocalDate particularDay = LocalDate.parse(splitArrayOfData[0], formatter);
-                if (names[i].equals(splitArrayOfData[1])
-                        && particularDay.isAfter(payPeriodStart.minusDays(CONSTANT_NUM))
-                        && particularDay.isBefore(payPeriodEnd.plusDays(CONSTANT_NUM))) {
-                    totalSalary += (Integer.parseInt(splitArrayOfData[2])
-                            * Integer.parseInt(splitArrayOfData[3]));
+                LocalDate particularDay = LocalDate.parse(splitArrayOfData[ZERO_INDEX], formatter);
+                if (names[i].equals(splitArrayOfData[FIRST_ELEMENT])
+                        && particularDay.isAfter(payPeriodStart.minusDays(FIRST_ELEMENT))
+                        && particularDay.isBefore(payPeriodEnd.plusDays(FIRST_ELEMENT))) {
+                    totalSalary += (Integer.parseInt(splitArrayOfData[SECOND_ELEMENT])
+                            * Integer.parseInt(splitArrayOfData[THIRD_ELEMENT]));
                 }
             }
-            if (i == names.length - 1) {
+            if (i == names.length - FIRST_ELEMENT) {
                 builder.append(names[i]).append(" - ").append(totalSalary);
             } else {
                 builder.append(names[i]).append(" - ").append(totalSalary)
