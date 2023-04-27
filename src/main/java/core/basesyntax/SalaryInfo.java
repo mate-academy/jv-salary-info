62adf861ae5a116f314e05f15518a0dea3d4e4a0
Vladyslav Krutyus <kijdron@gmail.com>
2023-04-27 11:51:36 +0300

    task fixed after review

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,29 +5,33 @@ import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
 public class SalaryInfo {
+    private static final DateTimeFormatter FORMATTER =
+            DateTimeFormatter.ofPattern("dd.MM.yyyy", Locale.ENGLISH);
+    private static final String HYPHEN = " - ";
+    private static final String SPACE = " ";
+    private static final int INDEX = 1;
+    private static final int HOURS = 2;
+    private static final int RATE = 3;
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         int[] earnings = new int[names.length];
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy", Locale.ENGLISH);
-        LocalDate fromDate = LocalDate.parse(dateFrom, formatter);
-        LocalDate toDate = LocalDate.parse(dateTo, formatter);
-
+        LocalDate fromDate = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate toDate = LocalDate.parse(dateTo, FORMATTER);
         for (String entry : data) {
-            String[] dataEntry = entry.split(" ");
-            LocalDate date = LocalDate.parse(dataEntry[0], formatter);
-
+            String[] dataEntry = entry.split(SPACE);
+            LocalDate date = LocalDate.parse(dataEntry[0], FORMATTER);
             if (!date.isBefore(fromDate) && !date.isAfter(toDate)) {
-                int index = getIndex(names, dataEntry[1]);
-                int hours = Integer.parseInt(dataEntry[2]);
-                int rate = Integer.parseInt(dataEntry[3]);
+                int index = getIndex(names, dataEntry[INDEX]);
+                int hours = Integer.parseInt(dataEntry[HOURS]);
+                int rate = Integer.parseInt(dataEntry[RATE]);
                 earnings[index] += hours * rate;
             }
         }
-
         StringBuilder report = new StringBuilder();
-        report.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
+        report.append("Report for period ").append(dateFrom).append(HYPHEN).append(dateTo);
         for (int i = 0; i < names.length; i++) {
             report.append(System.lineSeparator());
-            report.append(names[i]).append(" - ").append(earnings[i]);
+            report.append(names[i]).append(HYPHEN).append(earnings[i]);
         }
         return report.toString();
     }
@@ -38,7 +42,6 @@ public class SalaryInfo {
                 return i;
             }
         }
-
         return -1;
     }
 }
