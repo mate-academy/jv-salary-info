1b7eeffb8c856f449cd0c7e6d6dc64dedf99f1ae
Viktor Bykh <bykhviktor21@gmail.com>
2023-04-29 22:08:13 +0300

    hw jv-salary-info

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,46 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+    private static final String SPACE = " ";
+    private static final String DASH = " - ";
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        LocalDate localDateFrom = LocalDate.parse(dateFrom,
+                DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        LocalDate localDateTo = LocalDate.parse(dateTo,
+                DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        StringBuilder result = new StringBuilder("Report for period ")
+                .append(dateFrom)
+                .append(DASH)
+                .append(dateTo)
+                .append(System.lineSeparator());
+        for (String name : names) {
+            int sum = 0;
+            for (String test : data) {
+                if (test == null) {
+                    continue;
+                }
+                String[] parts = test.split(SPACE);
+                if (parts[1].equals(name)) {
+                    String date = parts[0];
+                    LocalDate localDate = LocalDate.parse(date,
+                            DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+                    if (localDate.compareTo(localDateFrom) >= 0
+                            && localDate.compareTo(localDateTo) <= 0) {
+                        int hours = Integer.parseInt(parts[2]);
+                        int incomePerHour = Integer.parseInt(parts[3]);
+                        sum += hours * incomePerHour;
+                    }
+                }
+            }
+            result.append(name)
+                    .append(" - ")
+                    .append(sum)
+                    .append(System.lineSeparator());
+        }
+        return result.toString().trim();
     }
 }
