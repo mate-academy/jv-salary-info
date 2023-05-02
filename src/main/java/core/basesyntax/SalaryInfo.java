10ee8e3f2424c3594728f7e9a8ae0f672f4e2dfa
Viktor Bykh <bykhviktor21@gmail.com>
2023-05-02 13:43:45 +0300

    update hw jv-salary-info

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -6,13 +6,19 @@ import java.time.format.DateTimeFormatter;
 public class SalaryInfo {
     private static final String SPACE = " ";
     private static final String DASH = " - ";
+    private static final String DATE_FORMAT = "dd.MM.yyyy";
+    private static final String REPORT_FOR_PERIOD = "Report for period ";
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int HOURS_INDEX = 2;
+    private static final int INCOME_PER_HOUR_INDEX = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate localDateFrom = LocalDate.parse(dateFrom,
-                DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+                DateTimeFormatter.ofPattern(DATE_FORMAT));
         LocalDate localDateTo = LocalDate.parse(dateTo,
-                DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-        StringBuilder result = new StringBuilder("Report for period ")
+                DateTimeFormatter.ofPattern(DATE_FORMAT));
+        StringBuilder result = new StringBuilder(REPORT_FOR_PERIOD)
                 .append(dateFrom)
                 .append(DASH)
                 .append(dateTo)
@@ -24,20 +30,20 @@ public class SalaryInfo {
                     continue;
                 }
                 String[] parts = test.split(SPACE);
-                if (parts[1].equals(name)) {
-                    String date = parts[0];
+                if (parts[NAME_INDEX].equals(name)) {
+                    String date = parts[DATE_INDEX];
                     LocalDate localDate = LocalDate.parse(date,
-                            DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+                            DateTimeFormatter.ofPattern(DATE_FORMAT));
                     if (localDate.compareTo(localDateFrom) >= 0
                             && localDate.compareTo(localDateTo) <= 0) {
-                        int hours = Integer.parseInt(parts[2]);
-                        int incomePerHour = Integer.parseInt(parts[3]);
+                        int hours = Integer.parseInt(parts[HOURS_INDEX]);
+                        int incomePerHour = Integer.parseInt(parts[INCOME_PER_HOUR_INDEX]);
                         sum += hours * incomePerHour;
                     }
                 }
             }
             result.append(name)
-                    .append(" - ")
+                    .append(DASH)
                     .append(sum)
                     .append(System.lineSeparator());
         }
