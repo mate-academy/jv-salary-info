89b16a2cc8a266942be8782c2b571471055ed87c
Vasya <xaski514@gmail.com>
2023-05-10 01:04:12 +0300

    Salary HW

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,43 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+    public static final DateTimeFormatter sdf = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        LocalDate startDate = LocalDate.parse(dateFrom, sdf);
+        LocalDate endDate = LocalDate.parse(dateTo,sdf);
+        String[] splitData;
+        LocalDate date;
+        String dataName;
+        int hour;
+        int wage;
+        int salary;
+
+        StringBuilder sb = new StringBuilder("Report for period " + dateFrom + " - " + dateTo);
+
+        for (int i = 0; i < names.length; i++) {
+            salary = 0;
+            for (String line: data) {
+                splitData = line.split(" ");
+                date = LocalDate.parse(splitData[0], sdf);
+                dataName = splitData[1];
+                hour = Integer.parseInt(splitData[2]);
+                wage = Integer.parseInt(splitData[3]);
+
+                if (dataName.equals(names[i])) {
+                    if (date.compareTo(startDate) >= 0 && date.compareTo(endDate) <= 0) {
+                        salary += wage * hour;
+                    }
+                }
+            }
+            sb.append(System.lineSeparator())
+                    .append(names[i])
+                    .append(" - ")
+                    .append(salary);
+        }
+        return sb.toString();
     }
 }
