7e0864bb6403b9f3a5a69517bbaab00e3686eb44
Vasya <xaski514@gmail.com>
2023-05-10 15:57:57 +0300

    Salary HW fix

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,11 +4,14 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    public static final DateTimeFormatter sdf = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    public static final DateTimeFormatter DATA_FORMATTER
+            = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
+    private static final int DATE_INDEX = 0;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        LocalDate startDate = LocalDate.parse(dateFrom, sdf);
-        LocalDate endDate = LocalDate.parse(dateTo,sdf);
+        LocalDate startDate = LocalDate.parse(dateFrom, DATA_FORMATTER);
+        LocalDate endDate = LocalDate.parse(dateTo,DATA_FORMATTER);
         String[] splitData;
         LocalDate date;
         String dataName;
@@ -22,13 +25,14 @@ public class SalaryInfo {
             salary = 0;
             for (String line: data) {
                 splitData = line.split(" ");
-                date = LocalDate.parse(splitData[0], sdf);
+                date = LocalDate.parse(splitData[DATE_INDEX], DATA_FORMATTER);
                 dataName = splitData[1];
                 hour = Integer.parseInt(splitData[2]);
                 wage = Integer.parseInt(splitData[3]);
 
                 if (dataName.equals(names[i])) {
-                    if (date.compareTo(startDate) >= 0 && date.compareTo(endDate) <= 0) {
+                    if (date.equals(startDate) || date.equals(endDate)
+                            || (date.isAfter(startDate) && date.isBefore(endDate))) {
                         salary += wage * hour;
                     }
                 }
