7f13150565351780ad98cfdd7182a9e4e5299821
Vasya <xaski514@gmail.com>
2023-05-11 00:25:07 +0300

    Salary HW repair

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -8,6 +8,11 @@ public class SalaryInfo {
             = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     private static final int DATE_INDEX = 0;
+    private static final String SEPARATOR = " ";
+    private static final int NAME_INDEX = 1;
+    private static final int HOUR_INDEX = 2;
+    private static final int WAGE_INDEX = 3;
+    private static final String DASH = " - ";
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate startDate = LocalDate.parse(dateFrom, DATA_FORMATTER);
@@ -19,16 +24,17 @@ public class SalaryInfo {
         int wage;
         int salary;
 
-        StringBuilder sb = new StringBuilder("Report for period " + dateFrom + " - " + dateTo);
+        StringBuilder reportBuilder = new StringBuilder("Report for period "
+                + dateFrom + " - " + dateTo);
 
         for (int i = 0; i < names.length; i++) {
             salary = 0;
             for (String line: data) {
-                splitData = line.split(" ");
+                splitData = line.split(SEPARATOR);
                 date = LocalDate.parse(splitData[DATE_INDEX], DATA_FORMATTER);
-                dataName = splitData[1];
-                hour = Integer.parseInt(splitData[2]);
-                wage = Integer.parseInt(splitData[3]);
+                dataName = splitData[NAME_INDEX];
+                hour = Integer.parseInt(splitData[HOUR_INDEX]);
+                wage = Integer.parseInt(splitData[WAGE_INDEX]);
 
                 if (dataName.equals(names[i])) {
                     if (date.equals(startDate) || date.equals(endDate)
@@ -37,11 +43,11 @@ public class SalaryInfo {
                     }
                 }
             }
-            sb.append(System.lineSeparator())
+            reportBuilder.append(System.lineSeparator())
                     .append(names[i])
-                    .append(" - ")
+                    .append(DASH)
                     .append(salary);
         }
-        return sb.toString();
+        return reportBuilder.toString();
     }
 }
