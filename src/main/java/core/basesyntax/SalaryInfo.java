b6d96e7aafd9357bab0c142cbe2bad6ae88fee58
marinakusniruk <Kushnirukmv@gmail.com>
2023-05-11 14:27:14 +0200

    Hometask Salary completed correct

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,27 +1,32 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+
+    private static final String DATE_PATTERN = "dd.MM.yyyy";
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern(DATE_PATTERN);
+    private static final String REPORT_TEXT = "Report for period ";
+    private static final String DASH = " - ";
+    private static final String LINE_BREAK = "\n";
+    private static final String WHITESPACE = " ";
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
+
         int sum = 0;
         int[] salary = new int[names.length];
         for (int i = 0; i < names.length; i++) {
             for (int j = 0; j < data.length; j++) {
-                String[] dataPerName = data[j].split(" ");
-                String[] dataOfWork = dataPerName[0].split("[.]");
-                String[] dateFr = dateFrom.split("[.]");
-                String[] dateToo = dateTo.split("[.]");
+                String[] dataPerName = data[j].split(WHITESPACE);
+                LocalDate formattedDate = LocalDate.parse(dataPerName[0], FORMATTER);
+                LocalDate formattedDateFrom = LocalDate.parse(dateFrom, FORMATTER);
+                LocalDate formattedDateTo = LocalDate.parse(dateTo, FORMATTER);
 
-                if (dataPerName[1].equals(names[i]) && ((Integer.parseInt(dataOfWork[0])
-                        >= Integer.parseInt(dateFr[0]) && Integer.parseInt(dataOfWork[0])
-                        <= Integer.parseInt(dateToo[0]) && Integer.parseInt(dataOfWork[1])
-                        == Integer.parseInt(dateFr[1]) && Integer.parseInt(dataOfWork[1])
-                        == Integer.parseInt(dateToo[1])) || (Integer.parseInt(dataOfWork[0])
-                        >= Integer.parseInt(dateFr[0]) && Integer.parseInt(dataOfWork[1])
-                        == Integer.parseInt(dateFr[1]) && Integer.parseInt(dataOfWork[1])
-                        != Integer.parseInt(dateToo[1])) || (Integer.parseInt(dataOfWork[0])
-                        <= Integer.parseInt(dateToo[0]) && Integer.parseInt(dataOfWork[1])
-                        != Integer.parseInt(dateFr[1]) && Integer.parseInt(dataOfWork[1])
-                        == Integer.parseInt(dateToo[1])))) {
+                if (dataPerName[1].equals(names[i]) && ((formattedDate.isAfter(formattedDateFrom)
+                        && formattedDate.isBefore(formattedDateTo))
+                        || (formattedDate.isEqual(formattedDateFrom)
+                        || formattedDate.isEqual(formattedDateTo)))) {
                     sum = sum + (Integer.parseInt(dataPerName[2])
                             * Integer.parseInt(dataPerName[3]));
                 }
@@ -30,13 +35,13 @@ public class SalaryInfo {
             sum = 0;
         }
         StringBuilder builder = new StringBuilder();
-        builder.append("Report for period ").append(dateFrom).append(" - ")
-                .append(dateTo).append("\n");
+        builder.append(REPORT_TEXT).append(dateFrom).append(DASH)
+                .append(dateTo).append(LINE_BREAK);
         for (int i = 0; i < names.length; i++) {
             for (int j = 0; j < salary.length; j++) {
                 if (i == j) {
-                    builder.append(names[i]).append(" - ")
-                            .append(salary[j]).append("\n");
+                    builder.append(names[i]).append(DASH)
+                            .append(salary[j]).append(LINE_BREAK);
                 }
             }
         }
