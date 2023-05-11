350f7916be3afa0cf3589b0d70f0b40e23a7c15a
Vasya <xaski514@gmail.com>
2023-05-11 20:02:06 +0300

    Salary HW hotfix

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -7,16 +7,15 @@ public class SalaryInfo {
     public static final DateTimeFormatter DATA_FORMATTER
             = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
+    private static final String FIELD_SEPARATOR = " ";
     private static final int DATE_INDEX = 0;
-    private static final String SEPARATOR = " ";
     private static final int NAME_INDEX = 1;
     private static final int HOUR_INDEX = 2;
     private static final int WAGE_INDEX = 3;
-    private static final String DASH = " - ";
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate startDate = LocalDate.parse(dateFrom, DATA_FORMATTER);
-        LocalDate endDate = LocalDate.parse(dateTo,DATA_FORMATTER);
+        LocalDate endDate = LocalDate.parse(dateTo, DATA_FORMATTER);
         String[] splitData;
         LocalDate date;
         String dataName;
@@ -24,13 +23,13 @@ public class SalaryInfo {
         int wage;
         int salary;
 
-        StringBuilder reportBuilder = new StringBuilder("Report for period "
-                + dateFrom + " - " + dateTo);
+        StringBuilder reportBuilder = new StringBuilder(String.format("Report for period %s - %s",
+                dateFrom, dateTo));
 
         for (int i = 0; i < names.length; i++) {
             salary = 0;
-            for (String line: data) {
-                splitData = line.split(SEPARATOR);
+            for (String line : data) {
+                splitData = line.split(FIELD_SEPARATOR);
                 date = LocalDate.parse(splitData[DATE_INDEX], DATA_FORMATTER);
                 dataName = splitData[NAME_INDEX];
                 hour = Integer.parseInt(splitData[HOUR_INDEX]);
@@ -43,9 +42,11 @@ public class SalaryInfo {
                     }
                 }
             }
+
+            final String recordSeparator = " - ";
             reportBuilder.append(System.lineSeparator())
                     .append(names[i])
-                    .append(DASH)
+                    .append(recordSeparator)
                     .append(salary);
         }
         return reportBuilder.toString();
