82c53a366518af83aa413859ba72945dec684b7a
Olena Protsailo <olenaprotsailo@gmail.com>
2023-05-12 15:33:48 +0300

    fixed try-catch,moved all indexes to class constants,moved the DateTimeFormatter initialization out of checkPeriod method and maked it a constant,

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -3,9 +3,15 @@ package core.basesyntax;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-public class SalaryInfo extends Exception {
+public class SalaryInfo {
+    private static final int INDEX_DATE = 0;
+    private static final int INDEX_NAME = 1;
+    private static final int INDEX_HOURS = 2;
+    private static final int INDEX_SALARY = 3;
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        int earnedsSalary = 0;
+        int earnedSalary = 0;
         StringBuilder report = new StringBuilder();
         String newLine = System.lineSeparator();
         report.append("Report for period ")
@@ -13,22 +19,22 @@ public class SalaryInfo extends Exception {
                 .append(dateTo);
         for (String name : names) {
             for (int i = 0; i < data.length; i++) {
-                String[] spBf = data[i].split(" ");
-                if (checkPeriod(dateFrom, dateTo, spBf[0])) {
-                    if (name.equals(spBf[1])) {
-                        earnedsSalary = earnedsSalary + Integer.parseInt(spBf[2])
-                                * Integer.parseInt(spBf[3]);
+                String[] bufferStringBase = data[i].split(" ");
+                if (checkPeriod(dateFrom, dateTo, bufferStringBase[INDEX_DATE])) {
+                    if (name.equals(bufferStringBase[INDEX_NAME])) {
+                        earnedSalary = earnedSalary
+                                + Integer.parseInt(bufferStringBase[INDEX_HOURS])
+                                * Integer.parseInt(bufferStringBase[INDEX_SALARY]);
                     }
                 }
             }
-            report.append(newLine).append(name).append(" - ").append(earnedsSalary);
-            earnedsSalary = 0;
+            report.append(newLine).append(name).append(" - ").append(earnedSalary);
+            earnedSalary = 0;
         }
         return report.toString();
     }
 
     private boolean checkPeriod(String dateFrom, String dateTo, String dateBase) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
         try {
             LocalDate formatDateFrom = LocalDate.parse(dateFrom, formatter);
             LocalDate formatDateTo = LocalDate.parse(dateTo, formatter);
@@ -36,8 +42,7 @@ public class SalaryInfo extends Exception {
             return (formatDateBase.compareTo(formatDateFrom) >= 0
                     && formatDateBase.compareTo(formatDateTo) <= 0);
         } catch (Exception e) {
-            e.getLocalizedMessage();
+            throw new RuntimeException("Error during date parsing", e);
         }
-        return false;
     }
 }
