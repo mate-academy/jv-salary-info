6294785e79e733265d8109efec2b213b81bcd36d
pochtalon <pochtalon1010@gmail.com>
2023-05-13 23:06:03 +0300

    Separated few one-lines to two. remove unused import separated methods by line

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,25 +1,29 @@
 package core.basesyntax;
 
 import java.time.LocalDate;
-import java.util.Arrays;
 
 public class SalaryInfo {
     private StringBuilder builder = new StringBuilder("Report for period");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder builder = new StringBuilder("Report for period ");
         builder.append(dateFrom).append(" - ").append(dateTo);
         String[] start = dateFrom.split("\\.");
         String[] end = dateTo.split("\\.");
-        LocalDate startToWork = LocalDate.of(Integer.valueOf(start[2]), Integer.valueOf(start[1]), Integer.valueOf(start[0]));
-        LocalDate endToWork = LocalDate.of(Integer.valueOf(end[2]), Integer.valueOf(end[1]), Integer.valueOf(end[0]));
+        LocalDate startToWork = LocalDate.of(Integer.valueOf(start[2]),
+                Integer.valueOf(start[1]), Integer.valueOf(start[0]));
+        LocalDate endToWork = LocalDate.of(Integer.valueOf(end[2]),
+                Integer.valueOf(end[1]), Integer.valueOf(end[0]));
         for (int i = 0; i < names.length; i++) {
             builder.append(System.lineSeparator()).append(names[i]).append(" - ");
             int sum = 0;
             for (int j = 0; j < data.length; j++) {
                 String[] dataSeparated = data[j].split(" ");
                 String[] workD = dataSeparated[0].split("\\.");
-                LocalDate workingDay = LocalDate.of(Integer.valueOf(workD[2]), Integer.valueOf(workD[1]), Integer.valueOf(workD[0]));
-                if (!(workingDay.getDayOfYear() >= startToWork.getDayOfYear() && workingDay.getDayOfYear() <= endToWork.getDayOfYear())) {
+                LocalDate workingDay = LocalDate.of(Integer.valueOf(workD[2]),
+                        Integer.valueOf(workD[1]), Integer.valueOf(workD[0]));
+                if (!(workingDay.getDayOfYear() >= startToWork.getDayOfYear() &&
+                        workingDay.getDayOfYear() <= endToWork.getDayOfYear())) {
                     continue;
                 }
                 if (names[i].equals(dataSeparated[1])) {
