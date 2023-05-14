ed8bf3f2c18e27688abb3da5ec1bbb10cf5e45c8
pochtalon <pochtalon1010@gmail.com>
2023-05-14 21:23:51 +0300

    Refactored variable's names for shorter lines

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -8,25 +8,26 @@ public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
         final String resultSeparator = " - ";
-        final int indexDate = 0;
-        final int indexName = 1;
-        final int indexHours = 2;
-        final int indexMoney = 3;
+        final int indDate = 0;
+        final int indName = 1;
+        final int indHours = 2;
+        final int indMoney = 3;
         StringBuilder builder = new StringBuilder("Report for period ")
                 .append(dateFrom).append(resultSeparator).append(dateTo);
-        LocalDate startToWork = LocalDate.parse(dateFrom, formatter);
-        LocalDate endToWork = LocalDate.parse(dateTo, formatter);
+        LocalDate startWork = LocalDate.parse(dateFrom, formatter);
+        LocalDate endWork = LocalDate.parse(dateTo, formatter);
         for (int i = 0; i < names.length; i++) {
             builder.append(System.lineSeparator()).append(names[i]).append(resultSeparator);
             int sum = 0;
             for (int j = 0; j < data.length; j++) {
-                String[] dataSeparated = data[j].split(" ");
-                LocalDate workingDay = LocalDate.parse(dataSeparated[indexDate], formatter);
-                boolean isPresent = workingDay.isAfter(startToWork) && workingDay.isBefore(endToWork)
-                        || workingDay.isEqual(startToWork) || workingDay.isEqual(endToWork);
-                boolean isNameMatch = names[i].equals(dataSeparated[indexName]);
+                String[] dataSepar = data[j].split(" ");
+                LocalDate workingDay = LocalDate.parse(dataSepar[indDate], formatter);
+                boolean isPresent = workingDay.isAfter(startWork) && workingDay.isBefore(endWork)
+                        || workingDay.isEqual(startWork) || workingDay.isEqual(endWork);
+                boolean isNameMatch = names[i].equals(dataSepar[indName]);
                 if (isPresent && isNameMatch) {
-                    sum += Integer.valueOf(dataSeparated[indexHours]) * Integer.valueOf(dataSeparated[indexMoney]);
+                    sum += Integer.valueOf(dataSepar[indHours])
+                            * Integer.valueOf(dataSepar[indMoney]);
                 }
             }
             builder.append(sum);
