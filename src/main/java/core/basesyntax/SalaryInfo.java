c6696eca37faac16e4238c96d52fb74c9a041cf0
Kristina Shpilchyna <kriss.mva@gmail.com>
2023-05-19 20:33:37 +0200

    removed utils Class DateFormatted and added formatter to SalaryInfo class updated verification equals instead of contains

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,17 +1,19 @@
 package core.basesyntax;
 
-import core.utils.DateFormatter;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder builder = new StringBuilder("Report for period " + dateFrom + " - " + dateTo);
 
         for (String name : names) {
             int totalSalary = 0;
             for (String dates : data) {
-                if (dates.contains(name)) {
-                    String[] employeeInfo = dates.split(" ");
+                String[] employeeInfo = dates.split(" ");
+                if (employeeInfo[1].equals(name)) {
                     if (isDateTheSameOrBetween(employeeInfo[0], dateFrom, dateTo)) {
                         totalSalary += Integer.parseInt(employeeInfo[2])
                                 * Integer.parseInt(employeeInfo[3]);
@@ -24,9 +26,9 @@ public class SalaryInfo {
     }
 
     private boolean isDateTheSameOrBetween(String dateValidate, String dateFrom, String dateTo) {
-        LocalDate validateDate = DateFormatter.dateFormatter(dateValidate);
-        LocalDate fromDate = DateFormatter.dateFormatter(dateFrom);
-        LocalDate toDate = DateFormatter.dateFormatter(dateTo);
+        LocalDate validateDate = LocalDate.parse(dateValidate, FORMATTER);
+        LocalDate fromDate = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate toDate = LocalDate.parse(dateTo, FORMATTER);;
 
         return validateDate.equals(fromDate) || validateDate.isAfter(fromDate)
                 && validateDate.isBefore(toDate) || validateDate.equals(toDate);
