2baba5a1c2ca80d5748f0d6da46d9fe57e06a1f2
Kristina Shpilchyna <kriss.mva@gmail.com>
2023-05-20 20:49:00 +0200

    extracted hardcoded indexes to constants

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,6 +5,10 @@ import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int WORKED_HOURS_INDEX = 2;
+    private static final int PER_HOUR_SALARY_INDEX = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder builder = new StringBuilder("Report for period " + dateFrom + " - " + dateTo);
@@ -13,11 +17,10 @@ public class SalaryInfo {
             int totalSalary = 0;
             for (String dates : data) {
                 String[] employeeInfo = dates.split(" ");
-                if (employeeInfo[1].equals(name)) {
-                    if (isDateTheSameOrBetween(employeeInfo[0], dateFrom, dateTo)) {
-                        totalSalary += Integer.parseInt(employeeInfo[2])
-                                * Integer.parseInt(employeeInfo[3]);
-                    }
+                if (employeeInfo[NAME_INDEX].equals(name)
+                        && isDateTheSameOrBetween(employeeInfo[DATE_INDEX], dateFrom, dateTo)) {
+                    totalSalary += Integer.parseInt(employeeInfo[WORKED_HOURS_INDEX])
+                            * Integer.parseInt(employeeInfo[PER_HOUR_SALARY_INDEX]);
                 }
             }
             builder.append(System.lineSeparator()).append(name).append(" - ").append(totalSalary);
