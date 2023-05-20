4aea6d5a292f0f4471fc90c96f9e2b6e18fe25bb
Andrii Khomenko <andpey.homenko@gmail.com>
2023-05-20 19:47:12 +0300

    changed constants and LocalDate names

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,36 +4,35 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private static final int WORKING_DAY_DATE = 0;
-    private static final int WORKERS_NAME = 1;
-    private static final int WORK_TIME = 2;
-    private static final int PER_HOUR_SALARY = 3;
+    private static final int WORKING_DAY_DATE_INDEX = 0;
+    private static final int WORKERS_NAME_INDEX = 1;
+    private static final int WORK_TIME_INDEX = 2;
+    private static final int PER_HOUR_SALARY_INDEX = 3;
     private DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        LocalDate dateFromFormat = LocalDate.parse(dateFrom, dtf);
-        LocalDate dateToFormat = LocalDate.parse(dateTo, dtf);
+        LocalDate dateFromFormatted = LocalDate.parse(dateFrom, dtf);
+        LocalDate dateToFormatted = LocalDate.parse(dateTo, dtf);
         StringBuilder result = new StringBuilder("Report for period ");
         result.append(dateFrom).append(" - ").append(dateTo);
-        int salary = 0;
         for (String name : names) {
+            int salary = 0;
             for (String dataElement : data) {
                 String[] newDataElement = dataElement.split(" ");
-                LocalDate workDay = LocalDate.parse(newDataElement[WORKING_DAY_DATE], dtf);
-                if ((newDataElement[WORKERS_NAME].equals(name))
-                        && (workDay.isAfter(dateFromFormat)
-                        || workDay.isEqual(dateFromFormat))
-                        && (workDay.isBefore(dateToFormat)
-                        || workDay.isEqual(dateToFormat))) {
-                    salary += Integer.parseInt(newDataElement[WORK_TIME])
-                            * Integer.parseInt(newDataElement[PER_HOUR_SALARY]);
+                LocalDate workDay = LocalDate.parse(newDataElement[WORKING_DAY_DATE_INDEX], dtf);
+                if ((newDataElement[WORKERS_NAME_INDEX].equals(name))
+                        && (workDay.isAfter(dateFromFormatted)
+                        || workDay.isEqual(dateFromFormatted))
+                        && (workDay.isBefore(dateToFormatted)
+                        || workDay.isEqual(dateToFormatted))) {
+                    salary += Integer.parseInt(newDataElement[WORK_TIME_INDEX])
+                            * Integer.parseInt(newDataElement[PER_HOUR_SALARY_INDEX]);
                 }
             }
             result.append(System.lineSeparator())
                     .append(name)
                     .append(" - ")
                     .append(salary);
-            salary = 0;
         }
         return result.toString();
     }
