bc78bfe10b14568f17a9f317a7476c851ee7e1b4
OLYA <Ole4kastar79@gmail.com>
2023-06-02 00:36:17 +0300

    my first commit. SalaryInfoTest was completed

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,43 @@
 package core.basesyntax;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 public class SalaryInfo {
-    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+    private static final DateTimeFormatter formatter =
+            DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
+    public static String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
+        if (names.length == 0 || data.length == 0){
+            return "";
+        }
+        try {
+            LocalDate date1 = LocalDate.parse(dateFrom, formatter);
+            LocalDate date2 = LocalDate.parse(dateTo, formatter);
+            StringBuilder builder = new StringBuilder("Report for period ");
+            builder.append(dateFrom).append(" - ").append(dateTo);
+            int sampleSalary;
+            for (String sampleName : names) {
+                sampleSalary = 0;
+                for (String datas : data) {
+                    String[] record = datas.split(" ");
+                    if (record[1].equals(sampleName)) {
+                        LocalDate dateRecord = LocalDate.parse(record[0], formatter);
+                        if (dateRecord.isAfter(date1) && dateRecord.isBefore(date2) || dateRecord.equals(date1) || dateRecord.equals(date2)) {
+                            int recordSalary = Integer.parseInt( record[2]) *Integer.parseInt(record[3]);
+                            sampleSalary += recordSalary;
+                        }
+                    }
+                }
+                builder.append(System.lineSeparator());
+                builder.append(sampleName).append(" - ");
+                builder.append(sampleSalary);
+            }
+            return builder.toString();
+        }
+        catch (DateTimeParseException exc) {
+            System.out.printf("%s is not parsable!%n", dateFrom);
+            throw exc;      // Rethrow the exception.
+        }
     }
 }
