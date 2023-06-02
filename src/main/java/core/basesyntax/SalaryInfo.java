2aa4734dde63f7af25148f38572f42c3f9d946b0
Sashchenko Nikita <saschenko7@gmail.com>
2023-06-02 18:42:48 +0300

    add constants and remove unnecessary condition

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -6,37 +6,36 @@ import java.time.format.DateTimeFormatter;
 public class SalaryInfo {
     private static final DateTimeFormatter DATE_FORMATTER
             = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int HOURS_WORKED_INDEX = 2;
+    private static final int HOURLY_RATE_INDEX = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate fromDate = LocalDate.parse(dateFrom, DATE_FORMATTER);
         LocalDate toDate = LocalDate.parse(dateTo, DATE_FORMATTER);
 
         StringBuilder report = new StringBuilder();
-        report.append("Report for period ")
-                .append(dateFrom)
-                .append(" - ")
-                .append(dateTo)
-                .append(System.lineSeparator());
-        for (int i = 0;i < names.length; i++) {
-            String name = names[i];
+        report.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
 
+        for (String name : names) {
             int totalSalary = 0;
             for (String entry : data) {
                 String[] entryParts = entry.split(" ");
-                LocalDate entryDate = LocalDate.parse(entryParts[0], DATE_FORMATTER);
+                LocalDate entryDate = LocalDate.parse(entryParts[DATE_INDEX], DATE_FORMATTER);
 
-                if (entryParts[1].equals(name) && isDateInRange(entryDate, fromDate, toDate)) {
-                    int hoursWorked = Integer.parseInt(entryParts[2]);
-                    int hourlyRate = Integer.parseInt(entryParts[3]);
+                if (entryParts[NAME_INDEX].equals(name)
+                        && isDateInRange(entryDate, fromDate, toDate)) {
+                    int hoursWorked = Integer.parseInt(entryParts[HOURS_WORKED_INDEX]);
+                    int hourlyRate = Integer.parseInt(entryParts[HOURLY_RATE_INDEX]);
                     totalSalary += hoursWorked * hourlyRate;
                 }
             }
-            StringBuilder result = report.append(name)
-                                         .append(" - ")
-                                         .append(totalSalary);
-            if (i != names.length - 1) {
-                result.append(System.lineSeparator());
-            }
+            report
+                    .append(System.lineSeparator())
+                    .append(name)
+                    .append(" - ")
+                    .append(totalSalary);
         }
         return report.toString();
     }
