79762a47dd22c43b14f26f6424342e89ac63d44b
OLYA <Ole4kastar79@gmail.com>
2023-06-02 22:33:52 +0300

    fixed: СI

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -2,17 +2,16 @@ package core.basesyntax;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
 
 public class SalaryInfo {
     private static final DateTimeFormatter formatter =
             DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int HOURS_INDEX = 2;
+    private static final int INCOME_PER_HOUR_INDEX = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        final int DATE_INDEX = 0;
-        final int NAME_INDEX = 1;
-        final int HOURS_INDEX = 2;
-        final int INCOME_PER_HOUR_INDEX = 3;
         if (names.length == 0 || data.length == 0) {
             return "";
         }
