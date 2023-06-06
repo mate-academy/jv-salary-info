023e56798641f700840159fcfc19f42f952ad52c
Stanislav Golubkov <golubkovstas83@gmail.com>
2023-06-06 22:10:24 +0300

    version5-stas

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,15 +4,15 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("dd.MM.yyyy");
     private static final int DATE_INDEX = 0;
     private static final int NAME_INDEX = 1;
     private static final int HOURS_WORKED_INDEX = 2;
     private static final int INCOME_INDEX = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        LocalDate dateStart = LocalDate.parse(dateFrom, DATE_FORMATTER);
-        LocalDate dateFinish = LocalDate.parse(dateTo, DATE_FORMATTER);
+        LocalDate dateStart = LocalDate.parse(dateFrom, DATE_FORMAT);
+        LocalDate dateFinish = LocalDate.parse(dateTo, DATE_FORMAT);
         int salary = 0;
         StringBuilder builder = new StringBuilder("Report for period ");
         builder.append(dateFrom)
@@ -21,7 +21,7 @@ public class SalaryInfo {
         for (String name : names) {
             for (String component : data) {
                 String[] dataComponents = component.split(" ");
-                LocalDate dateOfWork = LocalDate.parse(dataComponents[DATE_INDEX], DATE_FORMATTER);
+                LocalDate dateOfWork = LocalDate.parse(dataComponents[DATE_INDEX], DATE_FORMAT);
                 String nameFromData = dataComponents[NAME_INDEX];
                 int workingHour = Integer.parseInt(dataComponents[HOURS_WORKED_INDEX]);
                 int income = Integer.parseInt(dataComponents[INCOME_INDEX]);
