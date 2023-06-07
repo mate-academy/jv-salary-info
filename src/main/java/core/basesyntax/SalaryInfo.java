74c25ddc0210ae001110c816589885b2baeec10d
Yura Khilko <yurakhilko@gmail.com>
2023-06-07 11:36:51 +0300

    Correct constants' names (to uppercase).

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,16 +4,16 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    static final DateTimeFormatter formatter =
+    static final DateTimeFormatter FORMATTER =
             DateTimeFormatter.ofPattern("dd.MM.yyyy");
-    static final int firstWord = 0;
-    static final int secondWord = 1;
-    static final int thirdWord = 2;
-    static final int forthWord = 3;
+    static final int FIRST_WORD = 0;
+    static final int SECOND_WORD = 1;
+    static final int THIRD_WORD = 2;
+    static final int FORTH_WORD = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        LocalDate dateFromLocal = LocalDate.parse(dateFrom, formatter);
-        LocalDate dateToLocal = LocalDate.parse(dateTo, formatter);
+        LocalDate dateFromLocal = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate dateToLocal = LocalDate.parse(dateTo, FORMATTER);
 
         StringBuilder result = new StringBuilder("Report for period ").append(dateFrom)
                 .append(" - ").append(dateTo);
@@ -21,12 +21,12 @@ public class SalaryInfo {
             int sum = 0;
             for (String dataRow : data) {
                 String[] dataRowSplit = dataRow.split(" ");
-                LocalDate workingDate = LocalDate.parse(dataRowSplit[firstWord], formatter);
-                if (name.equals(dataRowSplit[secondWord]) && workingDate.isAfter(dateFromLocal)
+                LocalDate workingDate = LocalDate.parse(dataRowSplit[FIRST_WORD], FORMATTER);
+                if (name.equals(dataRowSplit[SECOND_WORD]) && workingDate.isAfter(dateFromLocal)
                         && (workingDate.isBefore(dateToLocal)
                         || workingDate.isEqual(dateToLocal))) {
-                    sum += Integer.parseInt(dataRowSplit[thirdWord])
-                            * Integer.parseInt(dataRowSplit[forthWord]);
+                    sum += Integer.parseInt(dataRowSplit[THIRD_WORD])
+                            * Integer.parseInt(dataRowSplit[FORTH_WORD]);
                 }
             }
             result.append(System.lineSeparator()).append(name).append(" - ").append(sum);
