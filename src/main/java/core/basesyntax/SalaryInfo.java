52df4ecc28624f616bb159ce0cb7e3194e858a9f
Dmytro Hanshyn <di.hanshyn@gmail.com>
2023-06-09 14:15:17 +0300

    Fixed after review. Deleted class DataRangeValidator. Create methods: dataReangeValidator, isWithinRange.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,33 +1,54 @@
 package core.basesyntax;
 
-public class SalaryInfo {
-    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        int i = 0;
-        int lengthArrayNames = names.length;
-        int[] salary = new int[lengthArrayNames];
-        DataRangeValidator dates = new DataRangeValidator();
-        dates.dateRangeValidator(dateFrom, dateTo);
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
-        for (String name : names) {
-            for (String employeeData : data) {
-                String[] employee = employeeData.split(" ");
-                if (name.equals(employee[1])) {
-                    if (dates.isWithinRange(employee[0])) {
-                        salary[i] += Integer.parseInt(employee[2]) * Integer.parseInt(employee[3]);
-                    }
-                }
-            }
-            i++;
-        }
+public class SalaryInfo {
+    static final int INDEX_DATE = 0;
+    static final int INDEX_NAME = 1;
+
+    static final int INDEX_HOUR = 2;
+    static final int INDEX_SALARY = 3;
+    static final String SEPARATOR = " ";
+    private LocalDate fromDate;
+    private LocalDate toDate;
+    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
+    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
+        dateRangeValidator(dateFrom, dateTo);
         StringBuilder reportString = new StringBuilder("Report for period ");
         reportString.append(dateFrom).append(" - ").append(dateTo).append(System.lineSeparator());
 
-        for (int j = 0; j < lengthArrayNames; j++) {
-            reportString.append(names[j]).append(" - ").append(salary[j])
-                    .append(System.lineSeparator());
+        for (String name : names) {
+            int salary = 0;
+            for (String employeeData : data) {
+                String[] employee = employeeData.split(SEPARATOR);
+                if (name.equals(employee[INDEX_NAME])) {
+                    if (isWithinRange(employee[INDEX_DATE])) {
+                        salary += Integer.parseInt(employee[INDEX_HOUR])
+                                * Integer.parseInt(employee[INDEX_SALARY]);
+                    }
+                }
+            }
+            reportString.append(name).append(" - ").append(salary).append(System.lineSeparator());
         }
-
         reportString.deleteCharAt(reportString.length() - 1);
         return reportString.toString();
+    }
+
+    public void dateRangeValidator(String fromDate, String toDate) {
+        this.fromDate = LocalDate.parse(fromDate, formatter);
+        this.toDate = LocalDate.parse(toDate, formatter);
+
+    }
+
+    public boolean isWithinRange(String employeeJobDate) {
+        LocalDate dates = LocalDate.parse(employeeJobDate, formatter);
+
+        if ((dates.isEqual(fromDate) || dates.isEqual(toDate))
+                || (dates.isAfter(fromDate) && dates.isBefore(toDate))) {
+            return true;
+        }
+        return false;
     }
 }
