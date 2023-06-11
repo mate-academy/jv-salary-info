f75d91733ec014bf988546bddb2d903139bdad8c
Evhenii Polishuchenko <sds191sds@gmail.com>
2023-06-11 21:18:25 +0300

    fixed mistakes and used LocalDate

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,37 +1,47 @@
 package core.basesyntax;
 
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalDate;
 
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
-        DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
-        try {
-            for (String name : names) {
-                int salary = 0;
-                for (String employeeData : data) {
-                    String[] separatedData = employeeData.split(" ");
-                    Date localDate = dateFormat.parse(separatedData[0]);
-                    Date fromDate = dateFormat.parse(dateFrom);
-                    Date toDate = dateFormat.parse(dateTo);
-                    if (!localDate.before(fromDate) && !localDate.after(toDate)) {
-                        if (separatedData[1].equals(name)) {
-                            int localSalary = Integer.parseInt(separatedData[2])
-                                    * Integer.parseInt(separatedData[3]);
-                            salary += localSalary;
-                        }
+        for (String name : names) {
+            int salary = 0;
+            for (String employeeData : data) {
+                String[] separatedData = employeeData.split(" ");
+                String date = separatedData[0];
+                String employeeName = separatedData[1];
+                int hours = Integer.parseInt(separatedData[2]);
+                int moneyPerHour = Integer.parseInt(separatedData[3]);
+                LocalDate localDate = LocalDate.of(getYear(date), getMonth(date), getDay(date));
+                LocalDate fromDate = LocalDate.of(getYear(dateFrom),
+                        getMonth(dateFrom), getDay(dateFrom));
+                LocalDate toDate = LocalDate.of(getYear(dateTo), getMonth(dateTo), getDay(dateTo));
+                if (!localDate.isBefore(fromDate) && !localDate.isAfter(toDate)) {
+                    if (employeeName.equals(name)) {
+                        salary += hours * moneyPerHour;
                     }
                 }
-                stringBuilder.append(System.lineSeparator()).append(name)
-                        .append(" - ").append(salary);
             }
-        } catch (ParseException e) {
-            System.out.println("Incorrect date format!");
+            stringBuilder.append(System.lineSeparator()).append(name)
+                    .append(" - ").append(salary);
         }
         return stringBuilder.toString();
+    }
+
+    public int getDay(String date) {
+        String[] dates = date.split("\\.");
+        return Integer.parseInt(dates[0]);
+    }
+
+    public int getMonth(String date) {
+        String[] dates = date.split("\\.");
+        return Integer.parseInt(dates[1]);
+    }
+
+    public int getYear(String date) {
+        String[] dates = date.split("\\.");
+        return Integer.parseInt(dates[2]);
     }
 }
