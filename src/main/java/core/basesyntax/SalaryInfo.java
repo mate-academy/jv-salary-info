c3cd6c35b268bf89e33ba7c6c358b8334f2c06a8
VladSchweps <vladschweps1@gmail.com>
2023-06-11 20:57:09 +0300

    Added changes after review

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,40 +1,42 @@
 package core.basesyntax;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    static final String DELIMITER = " ";
+    static final int DATE = 0;
+    static final int NAME = 1;
+    static final int HOURS_WORKED = 2;
+    static final int SALARY_PER_HOUR = 3;
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        StringBuilder result = new StringBuilder();
-        result.append("Report for period ").append(dateFrom).append(" - ").append(dateTo)
-                .append(System.lineSeparator());
-        try {
-            Date fromDate = new SimpleDateFormat("dd.MM.yyyy").parse(dateFrom);
-            Date toDate = new SimpleDateFormat("dd.MM.yyyy").parse(dateTo);
-            for (String name : names) {
-                int totalSalary = 0;
-                for (String record : data) {
-                    String[] recordData = record.split(" ");
-                    Date recordDate = new SimpleDateFormat("dd.MM.yyyy").parse(recordData[0]);
-                    if (recordDate.compareTo(fromDate) >= 0 && recordDate.compareTo(toDate) <= 0) {
-                        if (recordData[1].equals(name)) {
-                            int hoursWorked = Integer.parseInt(recordData[2]);
-                            int salaryPerHour = Integer.parseInt(recordData[3]);
-                            int salaryPerDay = hoursWorked * salaryPerHour;
-                            totalSalary += salaryPerDay;
-                        }
+        StringBuilder result = new StringBuilder()
+                .append("Report for period ")
+                .append(dateFrom)
+                .append(" - ")
+                .append(dateTo);
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDate fromDate = LocalDate.parse(dateFrom, formatter);
+        LocalDate toDate = LocalDate.parse(dateTo, formatter);
+
+        for (String name : names) {
+            int totalSalary = 0;
+            for (String record : data) {
+                String[] recordData = record.split(DELIMITER);
+                LocalDate recordDate = LocalDate.parse(recordData[DATE], formatter);
+                if (!recordDate.isBefore(fromDate) && !recordDate.isAfter(toDate)) {
+                    if (recordData[NAME].equals(name)) {
+                        int hoursWorked = Integer.parseInt(recordData[HOURS_WORKED]);
+                        int salaryPerHour = Integer.parseInt(recordData[SALARY_PER_HOUR]);
+                        int salaryPerDay = hoursWorked * salaryPerHour;
+                        totalSalary += salaryPerDay;
+
                     }
                 }
-                if (names[2].equals(name)) {
-                    result.append(name).append(" - ").append(totalSalary);
-                } else {
-                    result.append(name).append(" - ").append(totalSalary)
-                            .append(System.lineSeparator());
-                }
-
             }
-        } catch (Exception e) {
-            e.printStackTrace();
+            result.append(System.lineSeparator()).append(name).append(" - ").append(totalSalary);
         }
         return result.toString();
     }
