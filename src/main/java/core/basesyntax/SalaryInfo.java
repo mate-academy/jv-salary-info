eee8d245f5312a0b3e82d71e9b8c32bb20ea1c57
mIst <bogdankharchenko77@gmail.com>
2023-06-19 15:06:24 +0300

    added fixes after review

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -6,18 +6,18 @@ import java.time.format.DateTimeFormatter;
 public class SalaryInfo {
     private static final DateTimeFormatter DATE_TIME_FORMATTER
             = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int DATE_INDEX = 0;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder builder = new StringBuilder("Report for period ");
-        builder.append(dateFrom).append(" - ").append(dateTo).append(System.lineSeparator());
+        builder.append(dateFrom).append(" - ").append(dateTo);
         LocalDate parseDateFrom = LocalDate.parse(dateFrom, DATE_TIME_FORMATTER);
         LocalDate parseDateTo = LocalDate.parse(dateTo, DATE_TIME_FORMATTER);
-        for (int i = 0; i < names.length; i++) {
-            builder.append(names[i]).append(" - ");
+        for (String name : names) {
             int salary = 0;
             for (String dataElement : data) {
                 String[] arrayOfDataElements = dataElement.split(" ");
-                LocalDate parseParticularDate = LocalDate.parse(arrayOfDataElements[0],
+                LocalDate parseParticularDate = LocalDate.parse(arrayOfDataElements[DATE_INDEX],
                         DATE_TIME_FORMATTER);
                 if (parseParticularDate.isAfter(parseDateFrom)
                         && parseParticularDate.isBefore(parseDateTo)
@@ -25,15 +25,12 @@ public class SalaryInfo {
                         || parseParticularDate.isEqual(parseDateTo)) {
                     int incomePerDay = Integer.parseInt(arrayOfDataElements[2])
                             * Integer.parseInt(arrayOfDataElements[3]);
-                    if (names[i].equals(arrayOfDataElements[1])) {
+                    if (name.equals(arrayOfDataElements[1])) {
                         salary += incomePerDay;
                     }
                 }
             }
-            builder.append(salary);
-            if (i != names.length - 1) {
-                builder.append(System.lineSeparator());
-            }
+            builder.append(System.lineSeparator()).append(name).append(" - ").append(salary);
         }
         return builder.toString();
     }
