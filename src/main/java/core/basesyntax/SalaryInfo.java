12b5bf4ae852342df889e5aeeb23fcd3434dd426
Efim Turkovskiy <turkovskiy.efim@outlook.com>
2023-06-19 10:44:22 +0300

    addedConstantFields

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,6 +5,10 @@ import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int WORKING_HOUR_INDEX = 2;
+    private static final int INCOME_PER_HOUR_INDEX = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder builder = new StringBuilder("Report for period ");
@@ -16,21 +20,20 @@ public class SalaryInfo {
             int salary = 0;
             for (String dataElement : data) {
                 String[] dataPerDataElementArray = dataElement.split(" ");
-                LocalDate localData = LocalDate.parse(dataPerDataElementArray[0], formatter);
+                LocalDate localData = LocalDate.parse(dataPerDataElementArray[DATE_INDEX],
+                        formatter);
                 if (localData.isAfter(localDateFrom) && localData.isBefore(localDateTo)
                         ||
                         localData.isEqual(localDateFrom)
                         ||
                         localData.isEqual(localDateTo)) {
-                    int income = Integer.parseInt(dataPerDataElementArray[2])
-                            *
-                            Integer.parseInt(dataPerDataElementArray[3]);
-                    if (name.equals(dataPerDataElementArray[1])) {
-                        salary += income;
+
+                    if (name.equals(dataPerDataElementArray[NAME_INDEX])) {
+                        salary += Integer.parseInt(dataPerDataElementArray[WORKING_HOUR_INDEX])
+                                *
+                                Integer.parseInt(dataPerDataElementArray[INCOME_PER_HOUR_INDEX]);
                     }
-
                 }
-
             }
             builder.append(salary);
         }
