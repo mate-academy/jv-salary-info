062b04cd9685d85f42121fe2d3537b8e96e33833
ОЛЕГ <avanesovob@gmail.com>
2023-06-21 14:42:46 +0300

    Made constants

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,19 +4,25 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int INDEX_ZERO = 0;
+    private static final int INDEX_ONE = 1;
+    private static final int INDEX_TWO = 2;
+    private static final int INDEX_THREE = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
+
         int[] sum = new int[names.length];
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        LocalDate beginningOfPeriod = LocalDate.parse(dateFrom, formatter);
-        LocalDate endOfPeriod = LocalDate.parse(dateTo,formatter);
+        LocalDate beginningOfPeriod = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate endOfPeriod = LocalDate.parse(dateTo,FORMATTER);
         for (int i = 0; i < data.length; i++) {
             String[] dataArray = data[i].split(" ");
-            LocalDate workingDay = LocalDate.parse(dataArray[0], formatter);
+            LocalDate workingDay = LocalDate.parse(dataArray[INDEX_ZERO], FORMATTER);
             if (!workingDay.isBefore(beginningOfPeriod) && !workingDay.isAfter(endOfPeriod)) {
                 for (int k = 0; k < names.length; k++) {
-                    if (names[k].equals(dataArray[1])) {
-                        sum[k] += Integer.parseInt(dataArray[2]) * Integer.parseInt(dataArray[3]);
+                    if (names[k].equals(dataArray[INDEX_ONE])) {
+                        sum[k] += Integer.parseInt(dataArray[INDEX_TWO])
+                                * Integer.parseInt(dataArray[INDEX_THREE]);
                     }
                 }
             }
