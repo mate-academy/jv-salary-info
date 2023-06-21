464db393c7211cc5144ff79c6833ac7b896b9e69
Hlib Bykovskyi <minexgleb@gmail.com>
2023-06-21 17:08:43 +0300

    Replace Date with LocalDate

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,11 +1,11 @@
 package core.basesyntax;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 public class SalaryInfo {
-    private static final SimpleDateFormat dateFormatter = new SimpleDateFormat("dd.MM.yyyy");
+    private static final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder resultListBuilder = new StringBuilder("Report for period "
@@ -13,10 +13,8 @@ public class SalaryInfo {
                 + " - "
                 + dateTo
                 + System.lineSeparator());
-
-        try {
-            Date firstDate = dateFormatter.parse(dateFrom);
-            Date secondDate = dateFormatter.parse(dateTo);
+            LocalDate firstDate = LocalDate.parse(dateFrom, dateFormatter);
+            LocalDate secondDate = LocalDate.parse(dateTo, dateFormatter);
 
             for (String name : names) {
                 int totalSalary = calculateTotalSalary(data, firstDate, secondDate, name);
@@ -27,30 +25,28 @@ public class SalaryInfo {
                         .append(System.lineSeparator());
             }
 
-        } catch (ParseException e) {
-            System.out.println("Error occurred while parsing date: " + e.getMessage());
-        }
-
         String result = resultListBuilder.toString();
         return removeTrailingLineSeparator(result);
     }
 
-    private int calculateTotalSalary(String[] data, Date firstDate, Date secondDate, String name)
-            throws ParseException {
+    private int calculateTotalSalary(String[] data, LocalDate firstDate, LocalDate secondDate, String name) {
         int totalSalary = 0;
         for (String element : data) {
-            Date workDate = dateFormatter.parse(element.split(" ")[0]);
-            String employeeName = element.split(" ")[1];
-            int workingHours = Integer.parseInt(element.split(" ")[2]);
-            int moneyPerHourEarned = Integer.parseInt(element.split(" ")[3]);
+            String[] parts = element.split(" ");
+            LocalDate workDate = LocalDate.parse(parts[0], dateFormatter);
+            String employeeName = parts[1];
+            int workingHours = Integer.parseInt(parts[2]);
+            int moneyPerHourEarned = Integer.parseInt(parts[3]);
 
-            if ((workDate.equals(firstDate) || (workDate.after(firstDate)
-                    && workDate.before(secondDate) || workDate.equals(secondDate)))
-                    && name.equals(employeeName)) {
+            if (isWithinDateRange(workDate, firstDate, secondDate) && name.equals(employeeName)) {
                 totalSalary += moneyPerHourEarned * workingHours;
             }
         }
         return totalSalary;
+    }
+
+    private boolean isWithinDateRange(LocalDate date, LocalDate startDate, LocalDate endDate) {
+        return !date.isBefore(startDate) && !date.isAfter(endDate);
     }
 
     private String removeTrailingLineSeparator(String result) {
