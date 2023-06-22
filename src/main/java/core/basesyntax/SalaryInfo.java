5d9df71fe7bef83c2de8024e3507cb07da48e5e7
Anton Uzhva <anton.itec91@gmail.com>
2023-06-23 00:44:36 +0300

    Completed task

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,59 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class SalaryInfo {
+    private static final DateTimeFormatter DATE_TIME_FORMATTER =
+            DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        LocalDate from = LocalDate.parse(dateFrom, DATE_TIME_FORMATTER);
+        LocalDate to = LocalDate.parse(dateTo, DATE_TIME_FORMATTER);
+        List<String> list = new ArrayList<>(new ArrayList<>(Arrays.asList(data)));
+        int[] countedSalary = extracted(names, from, to, list);
+        return formReport(names, dateFrom, dateTo, countedSalary);
+    }
+
+    private int[] extracted(String[] names, LocalDate from, LocalDate to, List<String> list) {
+        int[] countedSalary = new int[names.length];
+        LocalDate dateToCompare;
+        for (String s : list) {
+            String[] strings = s.split("\\s");
+            dateToCompare = LocalDate.parse(strings[0], DATE_TIME_FORMATTER);
+            if (isMatchedDate(from, to, dateToCompare)) {
+                for (int j = 0; j < names.length; j++) {
+                    if (names[j].contains(strings[1])) {
+                        countedSalary[j] += multiplyHoursOnRate(strings);
+                    }
+                }
+            }
+        }
+        return countedSalary;
+    }
+
+    private int multiplyHoursOnRate(String[] strings) {
+        return Integer.parseInt(strings[2])
+                * Integer.parseInt(strings[3]);
+    }
+
+    private boolean isMatchedDate(LocalDate from, LocalDate to, LocalDate dateToCompare) {
+        return dateToCompare.compareTo(from) >= 0
+                && dateToCompare.compareTo(to) <= 0;
+    }
+
+    private String formReport(String[] names, String dateFrom, String dateTo, int[] countedSalary) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(String.format("Report for period %s - %s", dateFrom, dateTo))
+                .append(System.lineSeparator());
+        for (int i = 0; i < names.length; i++) {
+            stringBuilder.append(names[i]).append(" - ").append(countedSalary[i])
+                    .append(System.lineSeparator());
+        }
+        return stringBuilder.toString().trim();
     }
 }
+
