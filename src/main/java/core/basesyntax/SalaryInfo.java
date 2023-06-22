dac8e5425ccc92e52e1cf7d2ae8fa4e9339d6c8c
ОЛЕГ <avanesovob@gmail.com>
2023-06-22 14:04:35 +0300

    Remove one loop

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,33 +5,28 @@ import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-    private static final int INDEX_ZERO = 0;
-    private static final int INDEX_ONE = 1;
-    private static final int INDEX_TWO = 2;
-    private static final int INDEX_THREE = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
 
         int[] sum = new int[names.length];
         LocalDate beginningOfPeriod = LocalDate.parse(dateFrom, FORMATTER);
         LocalDate endOfPeriod = LocalDate.parse(dateTo,FORMATTER);
-        for (int i = 0; i < data.length; i++) {
-            String[] dataArray = data[i].split(" ");
-            LocalDate workingDay = LocalDate.parse(dataArray[INDEX_ZERO], FORMATTER);
-            if (!workingDay.isBefore(beginningOfPeriod) && !workingDay.isAfter(endOfPeriod)) {
-                for (int k = 0; k < names.length; k++) {
-                    if (names[k].equals(dataArray[INDEX_ONE])) {
-                        sum[k] += Integer.parseInt(dataArray[INDEX_TWO])
-                                * Integer.parseInt(dataArray[INDEX_THREE]);
-                    }
-                }
-            }
-        }
         StringBuilder result = new StringBuilder();
         result.append("Report for period " + dateFrom + " - " + dateTo + System.lineSeparator());
         for (int i = 0; i < names.length; i++) {
+            for (int j = 0; j < data.length; j++) {
+                String[] dataArray = data[j].split(" ");
+                LocalDate workingDay = LocalDate.parse(dataArray[0], FORMATTER);
+                if (!workingDay.isBefore(beginningOfPeriod) && !workingDay.isAfter(endOfPeriod)) {
+                    if (names[i].equals(dataArray[1])) {
+                        sum[i] += Integer.parseInt(dataArray[2])
+                                * Integer.parseInt(dataArray[3]);
+                    }
+                }
+            }
             result.append(names[i] + " - " + sum[i] + System.lineSeparator());
         }
+
         return result.toString().trim();
     }
 }
