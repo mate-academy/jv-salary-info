e52a708769309932f9ae4c62e5c537b9fc5ef358
Anton Uzhva <anton.itec91@gmail.com>
2023-06-23 16:08:50 +0300

    --ammend

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -13,7 +13,7 @@ public class SalaryInfo {
     private static final int DATE_INDEX = 0;
     private static final int NAME_INDEX = 1;
     private static final int WORKED_HOURS_INDEX = 2;
-    private static final int RATE = 3;
+    private static final int RATE_INDEX = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate from;
@@ -60,7 +60,7 @@ public class SalaryInfo {
 
     private int multiplyHoursOnRate(String[] strings) {
         int hours = Integer.parseInt(strings[WORKED_HOURS_INDEX]);
-        int rate = Integer.parseInt(strings[RATE]);
+        int rate = Integer.parseInt(strings[RATE_INDEX]);
         return hours * rate;
     }
 
