1e29c4a9da5ba11433d1994346da80749b5ea61e
Jean-Mark Kimbembe <marselium@gmail.com>
2023-06-25 19:28:39 +0300

    Fixed constants, reduces loops to 2, fixed isDateBetweenTwoDays method

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -8,52 +8,41 @@ public class SalaryInfo {
     public static final int NAME_INDEX = 1;
     public static final int AMOUNT_INDEX = 2;
     public static final int PAY_FOR_HOUR_INDEX = 3;
+    public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    public static final String SEPARATOR = " ";
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        int [] salary = new int[names.length];
         StringBuilder reportBuilder = new StringBuilder(
-                String.format("Report for period %1$s - %2$s", dateFrom, dateTo))
-                .append(System.lineSeparator());
+                String.format("Report for period %1$s - %2$s", dateFrom, dateTo));
         LocalDate localDateFrom = stringToLocalDate(dateFrom);
         LocalDate localDateTo = stringToLocalDate(dateTo);
         for (int i = 0; i < names.length; i++) {
+            int salary = 0;
             for (int j = 0; j < data.length; j++) {
-                String [] recordSplitter = data[j].split(" ");
+                String [] recordSplitter = data[j].split(SEPARATOR);
                 if (recordSplitter[NAME_INDEX].equals(names[i])) {
                     if (isDateBetweenTwoDays(localDateFrom, localDateTo,
                             stringToLocalDate(recordSplitter[DATE_INDEX]))) {
-                        salary[i] = salary[i] + (Integer.parseInt(recordSplitter[AMOUNT_INDEX])
+                        salary = salary + (Integer.parseInt(recordSplitter[AMOUNT_INDEX])
                                 * Integer.parseInt(recordSplitter[PAY_FOR_HOUR_INDEX]));
                     }
                 }
             }
-        }
-
-        for (int i = 0; i < names.length; i++) {
-            if (i + 1 < names.length) {
-                reportBuilder.append(names[i])
-                        .append(" - ")
-                        .append(salary[i])
-                        .append(System.lineSeparator());
-            } else {
-                reportBuilder.append(names[i])
-                        .append(" - ")
-                        .append(salary[i]);
-            }
+            reportBuilder
+                    .append(System.lineSeparator())
+                    .append(names[i])
+                    .append(" - ")
+                    .append(salary);
         }
         return reportBuilder.toString();
     }
 
     private boolean isDateBetweenTwoDays(LocalDate startDate, LocalDate endDate,
                                          LocalDate targetDate) {
-        if (targetDate.equals(endDate)) {
-            return true;
-        }
-        return (!targetDate.isBefore(startDate)) && (targetDate.isBefore(endDate));
+        return !targetDate.isBefore(startDate) && !targetDate.isAfter(endDate);
     }
 
     private LocalDate stringToLocalDate(String date) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        return LocalDate.parse(date, formatter);
+        return LocalDate.parse(date, FORMATTER);
     }
 }
