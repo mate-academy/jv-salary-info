b0258055005d4e6a3b26ee49052cd2f452ed954a
Bohdan Kaziamyr <notpadumink01@gmail.com>
2023-06-25 12:46:15 +0300

    added constant variables

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,6 +4,10 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    private static final int DATE_FROM_DATA = 0;
+    private static final int NAME_FROM_DATA = 1;
+    private static final int WORKING_HOURS_FROM_DATA = 2;
+    private static final int PER_HOUR_FROM_DATA = 3;
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.uuuu");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
@@ -15,10 +19,10 @@ public class SalaryInfo {
             int sumSalary = 0;
             for (String record : data) {
                 String[] splitedRecord = record.split(" ");
-                LocalDate recordsDate = LocalDate.parse(splitedRecord[0], formatter);
-                String recordsName = splitedRecord[1];
-                int recordsHours = Integer.parseInt(splitedRecord[2]);
-                int recordsSalary = Integer.parseInt(splitedRecord[3]);
+                LocalDate recordsDate = LocalDate.parse(splitedRecord[DATE_FROM_DATA], formatter);
+                String recordsName = splitedRecord[NAME_FROM_DATA];
+                int recordsHours = Integer.parseInt(splitedRecord[WORKING_HOURS_FROM_DATA]);
+                int recordsSalary = Integer.parseInt(splitedRecord[PER_HOUR_FROM_DATA]);
                 if ((recordsDate.isAfter(dateFromParsed) || recordsDate.isEqual(dateFromParsed))
                         & (recordsDate.isBefore(dateToParsed) || recordsDate.isEqual(dateToParsed))
                         && name.equals(recordsName)) {
