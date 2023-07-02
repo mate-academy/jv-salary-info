a4a305aac170fe76f47ddf9316802d2b4149087e
vova5179 <vova5179@gmail.com>
2023-07-02 10:01:14 +0300

    changed constant names

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,11 +5,13 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
 public class SalaryInfo {
-    private static final String PATTERN = "dd.MM.yyyy";
-    private static final int DATE = 0;
-    private static final int NAME = 1;
-    private static final int WORKING_HOURS = 2;
-    private static final int PER_HOUR_SALARY = 3;
+
+    private static final DateTimeFormatter DATE_TIME_FORMATTER
+            = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int WORKING_HOURS_INDEX = 2;
+    private static final int PER_HOUR_SALARY_INDEX = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate from = parseDate(dateFrom);
@@ -19,11 +21,11 @@ public class SalaryInfo {
             int sumOfEarnedMoney = 0;
             for (String strData : data) {
                 String[] split = strData.split(" ");
-                LocalDate date = parseDate(split[DATE]);
+                LocalDate date = parseDate(split[DATE_INDEX]);
                 if (date.isAfter(from) && date.minusDays(1).isBefore(to)) {
-                    if (name.equals(split[NAME])) {
-                        sumOfEarnedMoney += Integer.parseInt(split[WORKING_HOURS])
-                                          * Integer.parseInt(split[PER_HOUR_SALARY]);
+                    if (name.equals(split[NAME_INDEX])) {
+                        sumOfEarnedMoney += Integer.parseInt(split[WORKING_HOURS_INDEX])
+                                          * Integer.parseInt(split[PER_HOUR_SALARY_INDEX]);
                     }
                 }
             }
@@ -40,9 +42,7 @@ public class SalaryInfo {
 
     private static LocalDate parseDate(String date) {
         try {
-            DateTimeFormatter formatter =
-                    DateTimeFormatter.ofPattern(PATTERN);
-            return LocalDate.parse(date, formatter);
+            return LocalDate.parse(date, DATE_TIME_FORMATTER);
         } catch (DateTimeParseException e) {
             throw new DateTimeParseException("Date is is not parsable!",
                     date, e.getErrorIndex(), e);
