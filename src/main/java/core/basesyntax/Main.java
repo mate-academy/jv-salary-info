0151308f8e05eb07b8c4b60dcc9c0d67e6ebe7e7
Dmytro Varukha <dmytro.varukha@gmail.com>
2023-07-02 16:00:41 +0300

    implemented salary info application

---
+++ src/main/java/core/basesyntax/Main.java
@@ -0,0 +1,52 @@
+package core.basesyntax;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] dataArr = new String[]{
+                "26.04.2019 John 4 50",
+                "05.04.2019 Andrew 3 200",
+                "10.04.2019 John 7 100",
+                "22.04.2019 Kate 9 100",
+                "25.06.2019 John 11 50",
+                "26.04.2019 Andrew 3 150",
+                "13.02.2019 John 7 100",
+                "26.04.2019 Kate 9 100"
+        };
+
+        final String[] secondScriptArray = {
+                "13.07.2019 John 60 50",
+                "15.07.2019 Andrew 3 200",
+                "15.07.2019 Kate 10 100",
+
+                "16.07.2019 Andrew 3 200",
+                "16.07.2019 Kate 9 100",
+
+                "10.08.2019 John 7 100",
+                "08.08.2019 Kate 3 80",
+                "11.08.2019 Andrew 8 100"
+        };
+
+        final String[] secondDates = {
+                "14.07.2019",
+                "10.08.2019",
+        };
+
+        final String[] dates = {
+                "24.04.2019",
+                "25.04.2019",
+                "26.04.2019",
+                "27.04.2019"
+        };
+
+        String[] namesArr = new String[]{
+                "John",
+                "Andrew",
+                "Kate"
+        };
+        SalaryInfo salaryInfo = new SalaryInfo();
+        String result = salaryInfo.getSalaryInfo(namesArr, secondScriptArray,
+                secondDates[0], secondDates[1]);
+        System.out.println(result);
+    }
+}
+
--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,48 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
-    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+    public String getSalaryInfo(String[] names, String[] data,
+                                String dateFrom, String dateTo) {
+        StringBuilder reportBuilder = new StringBuilder();
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDate startDate;
+        LocalDate endDate;
+        startDate = LocalDate.parse(dateFrom, dateFormatter);
+        endDate = LocalDate.parse(dateTo, dateFormatter);
+        reportBuilder.append("Report for period ")
+                .append(dateFrom)
+                .append(" - ")
+                .append(dateTo)
+                .append(System.lineSeparator());
+        for (String name : names) {
+            int totalSalary = 0;
+            for (String entryData : data) {
+                String[] dataParts = entryData.split(" ");
+                String dateStr = dataParts[0];
+                String employeeName = dataParts[1];
+                int employeeWorkedHours = Integer.parseInt(dataParts[2]);
+                int employeeIncomePerHour = Integer.parseInt(dataParts[3]);
+                LocalDate entryDateByUserName = LocalDate.parse(dateStr, dateFormatter);
+                if (entryDateByUserName.isEqual(startDate)
+                        || entryDateByUserName.isAfter(startDate)) {
+                    if (entryDateByUserName.isBefore(endDate)
+                            || entryDateByUserName.isEqual(endDate)) {
+                        if (employeeName.equals(name)) {
+                            totalSalary += employeeWorkedHours * employeeIncomePerHour;
+                        }
+                    }
+                }
+            }
+            reportBuilder.append(name)
+                    .append(" - ")
+                    .append(totalSalary)
+                    .append(System.lineSeparator());
+        }
+        return reportBuilder.toString().trim();
     }
 }
+
+
