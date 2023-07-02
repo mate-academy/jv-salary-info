3e0fc6966a90199a959dcac4d15ce2e5d2eada97
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-07-02 18:21:19 +0300

    added changes in the class SalaryInfo

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,35 +4,43 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private static final String REPORT_HEADER_FORMAT = "Report for period %s - %s&n";
+    private static final String LINE_SEPARATOR = System.lineSeparator();
+    private static final String LINE = " - ";
+    private static final String REPORT_HEADER_FORMAT = "Report for period %s - %s%n";
     private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate fromDate = LocalDate.parse(dateFrom, DATE_FORMAT);
         LocalDate toDate = LocalDate.parse(dateTo, DATE_FORMAT);
         int[] salaries = new int[names.length];
-        for (int i = 0; i < data.length; i++) {
-            String[] recordData = data[i].split(" ");
+        for (String record : data) {
+            String[] recordData = record.split(" ");
             LocalDate recordDate = LocalDate.parse(recordData[0], DATE_FORMAT);
-            if (recordDate.compareTo(fromDate) >= 0 && recordDate.compareTo(toDate) <= 0) {
+            if (!recordDate.isBefore(fromDate) && !recordDate.isAfter(toDate)) {
                 String name = recordData[1];
                 int hours = Integer.parseInt(recordData[2]);
                 int rate = Integer.parseInt(recordData[3]);
 
-                for (int j = 0; j < names.length; j++) {
-                    if (names[j].equals(name)) {
-                        salaries[j] += hours * rate;
-                        break;
-                    }
+                int index = getIndex(names, name);
+                if (index != -1) {
+                    salaries[index] += hours * rate;
                 }
             }
         }
         StringBuilder reportBuilder = new StringBuilder();
-        reportBuilder.append(REPORT_HEADER_FORMAT).append(dateFrom).append(" - ").append(dateTo);
+        reportBuilder.append(String.format(REPORT_HEADER_FORMAT, dateFrom, dateTo));
         for (int i = 0; i < names.length; i++) {
-            reportBuilder.append(System.lineSeparator())
-                    .append(names[i]).append(" - ").append(salaries[i]);
+            reportBuilder.append(names[i]).append(LINE).append(salaries[i]).append(LINE_SEPARATOR);
         }
-        return reportBuilder.toString();
+        return reportBuilder.toString().trim();
+    }
+
+    private int getIndex(String[] names, String name) {
+        for (int i = 0; i < names.length; i++) {
+            if (names[i].equals(name)) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
