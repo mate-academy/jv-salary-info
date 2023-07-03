1b8bce4559f32b2c1fd7b5e2672520b72a0b3117
Oleg Turyk <oleh.turyk.ki.2019@lpnu.ua>
2023-07-03 13:19:12 +0300

    implemented getSalaryInfo method, added isWithinRange method

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,40 @@
 package core.basesyntax;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.text.ParseException;
+
+import java.util.Arrays;
 
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
+        for (String name : names) {
+            int salary = 0;
+            for (int i = 0; i < data.length; i++) {
+                String[] parts = data[i].split(" ");
+                if (isWithinRange(parts[0], dateFrom, dateTo) && name.equals(parts[1])) {
+                    int hours = Integer.parseInt(parts[2]);
+                    int rate = Integer.parseInt(parts[3]);
+                    salary += hours * rate;
+                }
+            }
+            sb.append(System.lineSeparator()).append(name).append(" - ").append(salary);
+        }
+        return sb.toString();
+    }
+
+    private static boolean isWithinRange(String date, String dateFrom, String dateTo) {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
+        try {
+            Date d = sdf.parse(date);
+            Date from = sdf.parse(dateFrom);
+            Date to = sdf.parse(dateTo);
+            return d.compareTo(from) >= 0 && d.compareTo(to) <= 0;
+        } catch (ParseException e) {
+            System.out.println(e);
+        }
+        return false;
     }
 }
