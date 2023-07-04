81bf51d3204896f37f1bb69c1c970d941bd77860
Denis Balako <denisarius2000s@gmail.com>
2023-07-04 23:52:29 +0300

    fixed minor checkstyle issues

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,6 +4,11 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int HOURS_INDEX = 2;
+    private static final int SALARY_INDEX = 3;
+    private static final String REPORT_FORMAT = "%s - %d";
     private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data,
@@ -11,7 +16,6 @@ public class SalaryInfo {
         StringBuilder builder = new StringBuilder();
         LocalDate localDateFrom = parseDate(dateFrom);
         LocalDate localDateTo = parseDate(dateTo);
-        int salary = 0;
 
         builder.append("Report for period ")
                 .append(dateFrom)
@@ -19,28 +23,35 @@ public class SalaryInfo {
                 .append(dateTo);
 
         for (String name : names) {
-            for (String line : data) {
-                String[] parsedLine = line.split(" ");
-
-                if (!name.equals(parsedLine[1])) {
-                    continue;
-                }
-
-                LocalDate dateInDataLine = parseDate(parsedLine[0]);
-                boolean localDateFromCheck = dateInDataLine.isAfter(localDateFrom)
-                        || dateInDataLine.equals(localDateFrom);
-                boolean localDateToCheck = (dateInDataLine.isBefore(localDateTo)
-                        || dateInDataLine.equals(localDateTo));
-                if (localDateFromCheck && localDateToCheck) {
-                    salary += Integer.parseInt(parsedLine[3]) * Integer.parseInt(parsedLine[2]);
-                }
-            }
+            int salary = getSalarySumFromData(data, name, localDateFrom, localDateTo);
             builder.append(System.lineSeparator())
-                    .append(String.format("%s - %d", name, salary));
-            salary = 0;
+                    .append(String.format(REPORT_FORMAT, name, salary));
         }
-
         return builder.toString();
+    }
+
+    private int getSalarySumFromData(String[] data, String name,
+                                     LocalDate localDateFrom,
+                                     LocalDate localDateTo) {
+        int salary = 0;
+        for (String line : data) {
+            String[] parsedLine = line.split(" ");
+
+            if (!name.equals(parsedLine[NAME_INDEX])) {
+                continue;
+            }
+
+            LocalDate dateInDataLine = parseDate(parsedLine[DATE_INDEX]);
+            boolean localDateFromCheck = dateInDataLine.isAfter(localDateFrom)
+                    || dateInDataLine.equals(localDateFrom);
+            boolean localDateToCheck = (dateInDataLine.isBefore(localDateTo)
+                    || dateInDataLine.equals(localDateTo));
+            if (localDateFromCheck && localDateToCheck) {
+                salary += Integer.parseInt(parsedLine[SALARY_INDEX])
+                        * Integer.parseInt(parsedLine[HOURS_INDEX]);
+            }
+        }
+        return salary;
     }
 
     private LocalDate parseDate(String date) {
