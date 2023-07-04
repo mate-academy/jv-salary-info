1be65c7ac86083037975adf1c9b2c5148fcbf0d6
Mykola Horianin <anyavic2379@gmail.com>
2023-07-04 15:16:22 +0300

    Changed variable names and added constants

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,7 +4,7 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
     private static final String NEW_LINE = System.lineSeparator();
     private static final String SPACE_SEPARATOR = " ";
     private static final String DASH_SEPARATOR = " - ";
@@ -14,13 +14,13 @@ public class SalaryInfo {
     private static final int PER_HOUR_SALARY_INDEX = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        LocalDate formattedDateFrom = LocalDate.parse(dateFrom, formatter);
-        LocalDate formattedDateTo = LocalDate.parse(dateTo, formatter).plusDays(1);
+        LocalDate formattedDateFrom = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate formattedDateTo = LocalDate.parse(dateTo, FORMATTER).plusDays(1);
         int[] salaries = new int[names.length];
         for (int i = 0; i < names.length; i++) {
             for (String record: data) {
                 String[] splitRecord = record.split(SPACE_SEPARATOR);
-                LocalDate dateWork = LocalDate.parse(splitRecord[DATE_INDEX], formatter);
+                LocalDate dateWork = LocalDate.parse(splitRecord[DATE_INDEX], FORMATTER);
                 if (names[i].equals(splitRecord[NAME_INDEX]) && dateWork.isAfter(formattedDateFrom)
                         && dateWork.isBefore(formattedDateTo)) {
                     salaries[i] += Integer.parseInt(splitRecord[WORKED_HOURS_INDEX])
