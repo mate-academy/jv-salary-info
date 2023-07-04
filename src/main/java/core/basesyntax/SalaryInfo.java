62752a6de56c2baf68a0c99318bc5cf4f89abf2f
Sergii Dancha <sergdan666@gmail.com>
2023-07-04 11:58:55 +0300

    fixed SalaryInfo

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -7,32 +7,33 @@ import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
 public class SalaryInfo {
+    private static final String DATE_FORMAT = "dd.MM.yyyy";
+    private static final String REPORT_HEADER = "Report for period ";
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder report = new StringBuilder();
-        report.append("Report for period ").append(dateFrom).append(" - ").append(dateTo).append("\n");
-        LocalDate fromDate = LocalDate.parse(dateFrom, DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-        LocalDate toDate = LocalDate.parse(dateTo, DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        report.append(REPORT_HEADER).append(dateFrom).append(" - ").append(dateTo).append("\n");
+        LocalDate fromDate = LocalDate.parse(dateFrom, DateTimeFormatter.ofPattern(DATE_FORMAT));
+        LocalDate toDate = LocalDate.parse(dateTo, DateTimeFormatter.ofPattern(DATE_FORMAT));
 
         for (String name : names) {
             int totalSalary = 0;
             for (String record : data) {
-                String[] fields = record.split("\\s+");
-                LocalDate recordDate = LocalDate.parse(fields[0],
-                        DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-                if (fields[1].equals(name) && !recordDate.isBefore(fromDate)
-                        && !recordDate.isAfter(toDate)) {
+                String[] fields = record.split(" ");
+                LocalDate recordDate = LocalDate.parse(fields[0], DateTimeFormatter.ofPattern(DATE_FORMAT));
+                if (fields[1].equals(name) && !recordDate.isBefore(fromDate) && !recordDate.isAfter(toDate)) {
                     int hours = Integer.parseInt(fields[2]);
                     int rate = Integer.parseInt(fields[3]);
                     int salary = hours * rate;
                     totalSalary += salary;
                 }
             }
-            report.append(name).append(" - ").append(formatSalary(totalSalary)).append("\n");
+            report.append(name).append(" - ").append(formatSalary(totalSalary)).append(System.lineSeparator());
         }
-        return report.toString();
+        return report.toString().trim();
     }
 
-    private String formatSalary(double salary) {
+    private String formatSalary(int salary) {
         NumberFormat formatter = NumberFormat.getCurrencyInstance(new Locale("en", "US"));
         DecimalFormat decimalFormat = (DecimalFormat) formatter;
         decimalFormat.applyPattern("0");
