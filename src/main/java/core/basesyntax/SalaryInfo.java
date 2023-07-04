031ede0710a8eb77b50717f5bf46c3a5a69fbdd8
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-07-04 14:26:27 +0300

    added changes to the class SalaryInfo

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -16,35 +16,28 @@ public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate fromDate = LocalDate.parse(dateFrom, DATE_FORMAT);
         LocalDate toDate = LocalDate.parse(dateTo, DATE_FORMAT);
-        int[] salaries = new int[names.length];
-        for (String record : data) {
-            String[] recordData = record.split(" ");
-            LocalDate recordDate = LocalDate.parse(recordData[RECORD_DATE_INDEX], DATE_FORMAT);
-            if (!recordDate.isBefore(fromDate) && !recordDate.isAfter(toDate)) {
-                String name = recordData[NAME_INDEX];
-                int hours = Integer.parseInt(recordData[HOURS_INDEX]);
-                int rate = Integer.parseInt(recordData[RATE_INDEX]);
-
-                int index = getIndex(names, name);
-                if (index != -1) {
-                    salaries[index] += hours * rate;
-                }
-            }
-        }
         StringBuilder reportBuilder = new StringBuilder();
         reportBuilder.append(String.format(REPORT_HEADER_FORMAT, dateFrom, dateTo));
-        for (int i = 0; i < names.length; i++) {
-            reportBuilder.append(names[i]).append(LINE).append(salaries[i]).append(LINE_SEPARATOR);
-        }
-        return reportBuilder.toString().trim();
-    }
 
-    private int getIndex(String[] names, String name) {
-        for (int i = 0; i < names.length; i++) {
-            if (names[i].equals(name)) {
-                return i;
+        for (String name : names) {
+            int salary = 0;
+            for (String record : data) {
+                String[] recordData = record.split(" ");
+                LocalDate recordDate = LocalDate.parse(recordData[RECORD_DATE_INDEX], DATE_FORMAT);
+
+                if (!recordDate.isBefore(fromDate) && !recordDate.isAfter(toDate)) {
+                    String recordName = recordData[NAME_INDEX];
+                    if (recordName.equals(name)) {
+                        int hours = Integer.parseInt(recordData[HOURS_INDEX]);
+                        int rate = Integer.parseInt(recordData[RATE_INDEX]);
+                        salary += hours * rate;
+                    }
+                }
             }
+
+            reportBuilder.append(name).append(LINE).append(salary).append(LINE_SEPARATOR);
         }
-        return -1;
+
+        return reportBuilder.toString().trim();
     }
 }
