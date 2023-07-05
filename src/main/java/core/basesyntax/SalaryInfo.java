6f5d958cd2314c3286f7d36a88b424d613e8fa09
Sieryi D <hunterst@ukr.net>
2023-07-05 04:02:03 +0300

    reducing the content of the block try/catch

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,45 +5,53 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class SalaryInfo {
+    private static final String PATTERN = "dd.MM.yyyy";
+    private SimpleDateFormat dateFormat = new SimpleDateFormat(PATTERN);
+    private Date startWork;
+    private Date finishWork;
+    private Date dayWork;
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        String pattern = "dd.MM.yyyy";
         try {
-            SimpleDateFormat sdf = new SimpleDateFormat(pattern);
-            Date startWork = sdf.parse(dateFrom);
-            Date finishWork = sdf.parse(dateTo);
-            String[] returnStrings = new String[names.length];
-            int[] sums = new int[names.length];
-            for (int a = 0; a < names.length; a++) {
-                String nameAtIndex  = names[a];
-                sums[a] = 0;
-                for (int b = 0; b < data.length; b++) {
-                    String dataString = data[b];
-                    if (dataString.contains(nameAtIndex )) {
-                        String[] someData = dataString.split(" ");
-                        Date dayWork = sdf.parse(someData[0]);
-                        int ret1 = dayWork.compareTo(startWork);
-                        int ret2 = dayWork.compareTo(finishWork);
-                        if (ret1 >= 0 && ret2 <= 0) {
-                            int c = Integer.parseInt(someData[2]);
-                            int d = Integer.parseInt(someData[3]);
-                            sums[a] = sums[a] + c * d;
-                        }
-                    }
-                }
-                String output1 = names[a];
-                int output2 = sums[a];
-                StringBuilder outputStringBuilder = new StringBuilder();
-                outputStringBuilder.append(output1).append(" - ").append(output2);
-                returnStrings[a] = outputStringBuilder.toString();
-            }
-            StringBuilder outputAnswer = new StringBuilder();
-            outputAnswer.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
-            for (int f = 0; f < returnStrings.length; f++) {
-                outputAnswer.append(System.lineSeparator()).append(returnStrings[f]);
-            }
-            return outputAnswer.toString();
+            startWork = dateFormat.parse(dateFrom);
+            finishWork = dateFormat.parse(dateTo);
         } catch (ParseException pe) {
             return "exception-wrecker" + pe;
         }
+        String[] returnStrings = new String[names.length];
+        int[] sums = new int[names.length];
+        for (int a = 0; a < names.length; a++) {
+            String nameAtIndex = names[a];
+            sums[a] = 0;
+            for (int b = 0; b < data.length; b++) {
+                String dataString = data[b];
+                if (dataString.contains(nameAtIndex)) {
+                    String[] someData = dataString.split(" ");
+                    try {
+                        dayWork = dateFormat.parse(someData[0]);
+                    } catch (ParseException pe) {
+                        return "exception-wrecker" + pe;
+                    }
+                    int ret1 = dayWork.compareTo(startWork);
+                    int ret2 = dayWork.compareTo(finishWork);
+                    if (ret1 >= 0 && ret2 <= 0) {
+                        int c = Integer.parseInt(someData[2]);
+                        int d = Integer.parseInt(someData[3]);
+                        sums[a] = sums[a] + c * d;
+                    }
+                }
+            }
+            String output1 = names[a];
+            int output2 = sums[a];
+            StringBuilder outputStringBuilder = new StringBuilder();
+            outputStringBuilder.append(output1).append(" - ").append(output2);
+            returnStrings[a] = outputStringBuilder.toString();
+        }
+        StringBuilder outputAnswer = new StringBuilder();
+        outputAnswer.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
+        for (int f = 0; f < returnStrings.length; f++) {
+            outputAnswer.append(System.lineSeparator()).append(returnStrings[f]);
+        }
+        return outputAnswer.toString();
     }
 }
