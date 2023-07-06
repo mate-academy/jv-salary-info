55466034b51771dc3dccb8ab8b8f27ad23b19fd6
yermilov.m <yermilov.m@hotmail.com>
2023-07-06 23:45:56 +0200

    /Initial commit/

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -2,6 +2,5 @@ package core.basesyntax;
 
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
     }
 }
