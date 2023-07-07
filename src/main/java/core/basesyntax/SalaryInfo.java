c81cdd990457152aa524845565587165a2ee3f7d
Sieryi D <hunterst@ukr.net>
2023-07-07 18:25:04 +0300

    deleted block try/catch

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -7,6 +7,9 @@ public class SalaryInfo {
     private static final String PATTERN = "dd.MM.yyyy";
     private static final String SEPARATOR = " ";
     private DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(PATTERN);
+    private static final int DAY_INDEX_TO_STRING = 0;
+    private static final int HOURS_INDEX_TO_STRING = 2;
+    private static final int PAY_INDEX_TO_STRING = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate startWork = LocalDate.parse(dateFrom, dateTimeFormatter);
@@ -20,13 +23,13 @@ public class SalaryInfo {
                 String dataString = data[b];
                 if (dataString.contains(workerName)) {
                     String[] daysData = dataString.split(SEPARATOR);
-                    String stringDayWork = daysData[0];
+                    String stringDayWork = daysData[DAY_INDEX_TO_STRING];
                     LocalDate dayWork = LocalDate.parse(stringDayWork, dateTimeFormatter);
                     int daysAfterStartWork = dayWork.compareTo(startWork);
                     int daysBeforeFinishWork = dayWork.compareTo(finishWork);
                     if (daysAfterStartWork >= 0 && daysBeforeFinishWork <= 0) {
-                        int numberOfHours = Integer.parseInt(daysData[2]);
-                        int hourlyPay = Integer.parseInt(daysData[3]);
+                        int numberOfHours = Integer.parseInt(daysData[HOURS_INDEX_TO_STRING]);
+                        int hourlyPay = Integer.parseInt(daysData[PAY_INDEX_TO_STRING]);
                         wages[a] = wages[a] + numberOfHours * hourlyPay;
                     }
                 }
