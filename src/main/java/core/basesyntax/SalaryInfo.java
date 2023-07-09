7e9d77527fe02667807fcb16e0bfd19418f2eae8
Taras Nedaskivskyi <nadashkivskyitv@gmail.com>
2023-07-09 20:17:07 +0300

    errors correction - added constants for magic values

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,30 +4,40 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private final String datePattern = "dd.MM.yyyy";
+    private final int workDateIndex = 0;
+    private final int workHoursIndex = 2;
+    private final int hourSalaryIndex = 3;
+    private final String dataSeparator = " ";
+    private final String reportText = "Report for period ";
+    private final String reportTextSeparator = " - ";
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern(datePattern);
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate startDate = LocalDate.parse(dateFrom, formatter).minusDays(1);
         LocalDate finishDate = LocalDate.parse(dateTo, formatter).plusDays(1);
 
-        StringBuilder result = new StringBuilder("Report for period ")
+        StringBuilder result = new StringBuilder(reportText)
                 .append(dateFrom)
-                .append(" - ")
+                .append(reportTextSeparator)
                 .append(dateTo);
         int daySalary = 0;
 
         for (String name : names) {
             for (String employeeWorkingDayDatum : data) {
-                String[] employeeInfo = employeeWorkingDayDatum.split(" ");
-                LocalDate workDate = LocalDate.parse(employeeInfo[0], formatter);
+                String[] employeeInfo = employeeWorkingDayDatum.split(dataSeparator);
+                LocalDate workDate = LocalDate.parse(employeeInfo[workDateIndex], formatter);
                 if (employeeWorkingDayDatum.contains(name)
                         && workDate.isAfter(startDate)
                         && workDate.isBefore(finishDate)) {
-                    daySalary += Integer.parseInt(employeeInfo[2])
-                            * Integer.parseInt(employeeInfo[3]);
+                    daySalary += Integer.parseInt(employeeInfo[workHoursIndex])
+                            * Integer.parseInt(employeeInfo[hourSalaryIndex]);
                 }
             }
-            result.append(System.lineSeparator()).append(name).append(" - ").append(daySalary);
+            result.append(System.lineSeparator())
+                    .append(name)
+                    .append(reportTextSeparator)
+                    .append(daySalary);
             daySalary = 0;
         }
 
