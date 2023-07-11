8c442cf89b5740a5282f680e66a293775c1e5cbe
Evhenii Polishuchenko <sds191sds@gmail.com>
2023-07-11 17:34:15 +0300

    used the formatter

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,18 +1,16 @@
 package core.basesyntax;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     private static final String SPACE = " ";
-    private static final String DOT = "\\.";
     private static final String DASH = " - ";
-    private static final int DAY_INDEX = 0;
-    private static final int MONTH_INDEX = 1;
-    private static final int YEAR_INDEX = 2;
     private static final int DATE_INDEX = 0;
     private static final int EMPLOYEE_NAME_INDEX = 1;
     private static final int HOURS_INDEX = 2;
     private static final int MONEY_PER_HOUR_INDEX = 3;
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder stringBuilder = new StringBuilder();
@@ -25,34 +23,17 @@ public class SalaryInfo {
                 String employeeName = separatedData[EMPLOYEE_NAME_INDEX];
                 int hours = Integer.parseInt(separatedData[HOURS_INDEX]);
                 int moneyPerHour = Integer.parseInt(separatedData[MONEY_PER_HOUR_INDEX]);
-                LocalDate localDate = LocalDate.of(getYear(date), getMonth(date), getDay(date));
-                LocalDate fromDate = LocalDate.of(getYear(dateFrom),
-                        getMonth(dateFrom), getDay(dateFrom));
-                LocalDate toDate = LocalDate.of(getYear(dateTo), getMonth(dateTo), getDay(dateTo));
-                if (!localDate.isBefore(fromDate) && !localDate.isAfter(toDate)) {
-                    if (employeeName.equals(name)) {
+                LocalDate localDate = LocalDate.parse(date, FORMATTER);
+                LocalDate fromDate = LocalDate.parse(dateFrom, FORMATTER);
+                LocalDate toDate = LocalDate.parse(dateTo, FORMATTER);
+                if (!localDate.isBefore(fromDate) && !localDate.isAfter(toDate)
+                        && employeeName.equals(name)) {
                         salary += hours * moneyPerHour;
-                    }
                 }
             }
             stringBuilder.append(System.lineSeparator())
                     .append(name).append(DASH).append(salary);
         }
         return stringBuilder.toString();
-    }
-
-    private int getDay(String date) {
-        String[] dates = date.split(DOT);
-        return Integer.parseInt(dates[DAY_INDEX]);
-    }
-
-    private int getMonth(String date) {
-        String[] dates = date.split(DOT);
-        return Integer.parseInt(dates[MONTH_INDEX]);
-    }
-
-    private int getYear(String date) {
-        String[] dates = date.split(DOT);
-        return Integer.parseInt(dates[YEAR_INDEX]);
     }
 }
