174f499caab86246670d97f2da4aa424d8c5c647
Oleg Turyk <oleh.turyk.ki.2019@lpnu.ua>
2023-07-12 15:33:25 +0300

    fixed errors

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,35 +5,39 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class SalaryInfo {
-    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+    private static final int DATE_PART = 0;
+    private static final int NAME_PART = 1;
+    private static final int HOURS_PART = 2;
+    private static final int RATE_PART = 3;
 
-        StringBuilder sb = new StringBuilder();
-        sb.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
+    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
+        StringBuilder report = new StringBuilder();
+        report.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
         for (String name : names) {
             int salary = 0;
-            for (int i = 0; i < data.length; i++) {
-                String[] parts = data[i].split(" ");
-                if (isWithinRange(parts[0], dateFrom, dateTo) && name.equals(parts[1])) {
-                    int hours = Integer.parseInt(parts[2]);
-                    int rate = Integer.parseInt(parts[3]);
+            for (String item : data) {
+                String[] parts = item.split(" ");
+                if (isWithinRange(parts[DATE_PART], dateFrom, dateTo)
+                        && name.equals(parts[NAME_PART])) {
+                    int hours = Integer.parseInt(parts[HOURS_PART]);
+                    int rate = Integer.parseInt(parts[RATE_PART]);
                     salary += hours * rate;
                 }
             }
-            sb.append(System.lineSeparator()).append(name).append(" - ").append(salary);
+            report.append(System.lineSeparator()).append(name).append(" - ").append(salary);
         }
-        return sb.toString();
+        return report.toString();
     }
 
     private static boolean isWithinRange(String date, String dateFrom, String dateTo) {
-        SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
         try {
-            Date d = sdf.parse(date);
-            Date from = sdf.parse(dateFrom);
-            Date to = sdf.parse(dateTo);
-            return d.compareTo(from) >= 0 && d.compareTo(to) <= 0;
+            Date current = dateFormat.parse(date);
+            Date from = dateFormat.parse(dateFrom);
+            Date to = dateFormat.parse(dateTo);
+            return current.compareTo(from) >= 0 && current.compareTo(to) <= 0;
         } catch (ParseException e) {
-            System.out.println(e);
+            throw new RuntimeException("Can't get date, ", e);
         }
-        return false;
     }
 }
