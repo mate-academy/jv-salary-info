0f36bbb36a741216feb35b6c56146eb0bb00e8f1
Vladyslav Ihnatiuk <123470878+Someboty@users.noreply.github.com>
2023-07-12 13:56:58 +0300

    isolated getting personal salary into separated method, added more constants, renamed formatter into FORMATTER, changed substring into trim in the last result

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,44 +4,49 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-
-    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int COEFFICIENT_INDEX = 2;
+    private static final int VALUE_INDEX = 3;
+    private static final String HYPHEN_AND_SPACES = " - ";
+    private static final String REPORT_START_TEXT = "Report for period ";
+    private static final String SPACE_REGEX = " ";
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        LocalDate localFrom = LocalDate.parse(dateFrom, formatter);
-        LocalDate localTo = LocalDate.parse(dateTo, formatter);
-        StringBuilder result = new StringBuilder("Report for period ")
-                .append(dateFrom).append(" - ")
+        LocalDate localFrom = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate localTo = LocalDate.parse(dateTo, FORMATTER);
+        StringBuilder result = new StringBuilder(REPORT_START_TEXT)
+                .append(dateFrom).append(HYPHEN_AND_SPACES)
                 .append(dateTo)
-                .append(System.lineSeparator())
-                .append(String.join(" - 0" + System.lineSeparator(), names))
-                .append(" - 0")
                 .append(System.lineSeparator());
+        for (String name : names) {
+            result.append(getPersonalSalary(name, data, localFrom, localTo));
+        }
+        return result.toString().trim();
+    }
+
+    private String getPersonalSalary(
+            String name, String[] data, LocalDate fromDate, LocalDate toDate) {
+        int sum = 0;
         for (String dataUnit : data) {
-            String[] dataSeparated = dataUnit.split(" ");
-            LocalDate dataDate = LocalDate.parse(dataSeparated[0], formatter);
-            if (dataDate.isAfter(localFrom)
-                    && dataDate.isBefore(localTo)
-                    || dataDate.isEqual(localFrom)
-                    || dataDate.isEqual(localTo)) {
-                for (String name : names) {
-                    if (dataSeparated[1].equals(name)) {
-                        int startIndex =
-                                result.indexOf(dataSeparated[1]) + dataSeparated[1].length() + 3;
-                        int endIndex = result.indexOf(System.lineSeparator(), startIndex);
-                        result.replace(
-                                startIndex,
-                                endIndex,
-                                String.valueOf(
-                                        Integer.parseInt(dataSeparated[2])
-                                        * Integer.parseInt(dataSeparated[3])
-                                        + Integer.parseInt(result.substring(startIndex, endIndex)
-                                        )));
-                        break;
-                    }
+            String[] dataSeparated = dataUnit.split(SPACE_REGEX);
+            LocalDate dataDate = LocalDate.parse(dataSeparated[DATE_INDEX], FORMATTER);
+            if (dataDate.isAfter(fromDate)
+                    && dataDate.isBefore(toDate)
+                    || dataDate.isEqual(fromDate)
+                    || dataDate.isEqual(toDate)) {
+                if (dataSeparated[NAME_INDEX].equals(name)) {
+                    sum += Integer.parseInt(dataSeparated[COEFFICIENT_INDEX])
+                            * Integer.parseInt(dataSeparated[VALUE_INDEX]);
                 }
             }
         }
-        return result.substring(0, result.lastIndexOf(System.lineSeparator()));
+        StringBuilder builder = new StringBuilder();
+        return builder.append(name)
+                .append(HYPHEN_AND_SPACES)
+                .append(sum)
+                .append(System.lineSeparator())
+                .toString();
     }
 }
