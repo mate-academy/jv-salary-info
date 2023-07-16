1c9a6bf4d2818d5068be0e81604c2ce387562f8f
Bohdan Kaziamyr <notpadumink01@gmail.com>
2023-07-16 13:34:31 +0300

    renamed the constant field

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -8,18 +8,18 @@ public class SalaryInfo {
     private static final int NAME_FROM_DATA = 1;
     private static final int WORKING_HOURS_FROM_DATA = 2;
     private static final int PER_HOUR_FROM_DATA = 3;
-    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.uuuu");
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.uuuu");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        LocalDate dateFromParsed = LocalDate.parse(dateFrom, formatter);
-        LocalDate dateToParsed = LocalDate.parse(dateTo, formatter);
+        LocalDate dateFromParsed = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate dateToParsed = LocalDate.parse(dateTo, FORMATTER);
         StringBuilder output = new StringBuilder();
         output.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
         for (String name : names) {
             int sumSalary = 0;
             for (String record : data) {
                 String[] splitedRecord = record.split(" ");
-                LocalDate recordsDate = LocalDate.parse(splitedRecord[DATE_FROM_DATA], formatter);
+                LocalDate recordsDate = LocalDate.parse(splitedRecord[DATE_FROM_DATA], FORMATTER);
                 String recordsName = splitedRecord[NAME_FROM_DATA];
                 int recordsHours = Integer.parseInt(splitedRecord[WORKING_HOURS_FROM_DATA]);
                 int recordsSalary = Integer.parseInt(splitedRecord[PER_HOUR_FROM_DATA]);
