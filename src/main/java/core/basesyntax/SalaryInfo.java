5815b937c24bffefdadb6b3dc535ee48e554c2e8
Volodymyr Shashkov <finansist33333324@gmail.com>
2023-07-21 10:59:38 +0300

    jv-salary-info-solution

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,42 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
-    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+    private static final String REPORT_DELIMITER = " - ";
+
+    public String getSalaryInfo(String[] names, String[] date, String dateFrom, String dateTo) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDate dateStart = LocalDate.parse(dateFrom, formatter);
+        LocalDate dateFinish = LocalDate.parse(dateTo, formatter);
+
+        StringBuilder builder = new StringBuilder("Report for period "
+                + dateFrom + REPORT_DELIMITER + dateTo);
+
+        for (String name : names) {
+            int totalSalary = calculateTotalSalaryForEmployee(name, date, dateStart,
+                    dateFinish, formatter);
+            builder.append(System.lineSeparator()).append(name)
+                    .append(REPORT_DELIMITER).append(totalSalary);
+        }
+
+        return builder.toString();
+    }
+
+    private int calculateTotalSalaryForEmployee(String name, String[] date, LocalDate dateStart,
+                                                LocalDate dateFinish, DateTimeFormatter formatter) {
+        int totalSalary = 0;
+        for (String record : date) {
+            String[] recordParts = record.split(" ");
+            LocalDate targetDate = LocalDate.parse(recordParts[0], formatter);
+
+            if ((targetDate.isEqual(dateStart) || targetDate.isAfter(dateStart))
+                    && (targetDate.isEqual(dateFinish) || targetDate.isBefore(dateFinish))
+                    && name.equals(recordParts[1])) {
+                totalSalary += Integer.parseInt(recordParts[2]) * Integer.parseInt(recordParts[3]);
+            }
+        }
+        return totalSalary;
     }
 }
