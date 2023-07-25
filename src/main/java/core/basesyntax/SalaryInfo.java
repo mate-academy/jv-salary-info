be83085d8e2f38c02f3861e2718a6841447b3435
Oleksii Borysenko <343502597s@gmail.com>
2023-07-25 19:35:47 +0200

    mvn clear ok

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,30 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+    public static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        int salaryEmployee = 0;
+        LocalDate dateF = LocalDate.parse(dateFrom,formatter);
+        LocalDate dateT = LocalDate.parse(dateTo, formatter);
+        StringBuilder resultString = new StringBuilder("Report for period "
+                + dateFrom + " - " + dateTo);
+        for (int i = 0; i < names.length; i++) {
+            for (int j = 0; j < data.length; j++) {
+                String[] splitMassiv = data[j].split(" ");
+                if (LocalDate.parse(splitMassiv[0],formatter).isBefore(dateT.plusDays(1))
+                        && LocalDate.parse(splitMassiv[0], formatter).isAfter(dateF.minusDays(1))
+                        && splitMassiv[1].equals(names[i])) {
+                    salaryEmployee += Integer.parseInt(splitMassiv[2])
+                            * Integer.parseInt(splitMassiv[3]);
+                }
+            }
+            resultString.append(System.lineSeparator())
+                    .append(names[i]).append(" - ").append(salaryEmployee);
+            salaryEmployee = 0;
+        }
+        return resultString.toString();
     }
 }
