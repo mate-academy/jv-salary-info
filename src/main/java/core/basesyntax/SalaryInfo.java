adf230727a4fd490307d73890a5426b06db94d7e
Kolinko Oleksii <alexkolinko22@gmail.com>
2023-07-26 21:34:16 +0200

    HW6-solution-used-LocalDate-and-Formatter,-added-constant-index-Data

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,22 +1,26 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
     private static int salary;
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final String DATA_INDEX = " ";
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        String[] dateToSplit = dateTo.split("[.]");
-        String[] dateFromSplit = dateFrom.split("[.]");
-        int dateToKod = Integer.parseInt(dateToSplit[2] + dateToSplit[1] + dateToSplit[0]);
-        int dateFromKod = Integer.parseInt(dateFromSplit[2] + dateFromSplit[1] + dateFromSplit[0]);
+        LocalDate dateFromLocal = LocalDate.parse(dateFrom, formatter);
+        LocalDate dateToLocal = LocalDate.parse(dateTo, formatter);
         StringBuilder reportSalary = new StringBuilder("Report for period " + dateFrom + " - "
                 + dateTo);
         for (String name : names) {
             for (String informPerson : data) {
-                String[] splitData = informPerson.split(" ");
-                String[] splDate = splitData[0].split("[.]");
-                int dateWorkKod = Integer.parseInt(splDate[2] + splDate[1] + splDate[0]);
-                if (name.equals(splitData[1]) && dateToKod >= dateWorkKod
-                        && dateWorkKod >= dateFromKod) {
+                String[] splitData = informPerson.split(DATA_INDEX);
+                LocalDate dateWorkLocal = LocalDate.parse(splitData[0], formatter);
+                if (name.equals(splitData[1]) && ((dateFromLocal.isBefore(dateWorkLocal)
+                        && dateWorkLocal.isBefore(dateToLocal))
+                        || dateWorkLocal.isEqual(dateFromLocal)
+                        || dateWorkLocal.isEqual(dateToLocal))) {
                     salary = salary
                             + Integer.parseInt(splitData[2]) * Integer.parseInt(splitData[3]);
                 }
