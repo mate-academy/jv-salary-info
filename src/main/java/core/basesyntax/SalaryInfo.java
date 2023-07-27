5abbcecc46f450c84df8bac1e37ea8848df70f23
Alexander Karpenko <karalexander@gmail.com>
2023-07-28 01:36:52 +0300

    updated pom.xml from 4.12 to 4.13 Implemented method getSalaryInfo

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,35 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        StringBuilder report = new StringBuilder();
+        report.append("Report for period ")
+                .append(dateFrom)
+                .append(" - ")
+                .append(dateTo);
+
+        for (String name: names) {
+            int salary = 0;
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+            LocalDate fromDate = LocalDate.parse(dateFrom, formatter);
+            LocalDate toDate = LocalDate.parse(dateTo, formatter);
+
+            for (String databaseRecord : data) {
+                String[] dataRecord = databaseRecord.split(" ");
+                String employeeName = dataRecord[1];
+                LocalDate recordDate = LocalDate.parse(dataRecord[0], formatter);
+
+                if (employeeName.equals(name)
+                        && !recordDate.isBefore(fromDate)
+                        && !recordDate.isAfter(toDate)) {
+                    salary += Integer.parseInt(dataRecord[2]) * Integer.parseInt(dataRecord[3]);
+                }
+            }
+            report.append(System.lineSeparator()).append(name).append(" - ").append(salary);
+        }
+        return report.toString();
     }
 }
