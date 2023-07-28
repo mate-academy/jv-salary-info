f993ea0ac0bed22076d71569014652f2cd6c7b50
Alexander Karpenko <karalexander@gmail.com>
2023-07-28 23:13:34 +0300

    1) made formatter a constant field 2) converted magic numbers to constants

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,28 +4,32 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int HOURS_WORKED_INDEX = 2;
+    private static final int HOURLY_RATE_INDEX = 3;
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder report = new StringBuilder();
         report.append("Report for period ")
                 .append(dateFrom)
                 .append(" - ")
                 .append(dateTo);
-
         for (String name: names) {
             int salary = 0;
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-            LocalDate fromDate = LocalDate.parse(dateFrom, formatter);
-            LocalDate toDate = LocalDate.parse(dateTo, formatter);
-
+            LocalDate fromDate = LocalDate.parse(dateFrom, FORMATTER);
+            LocalDate toDate = LocalDate.parse(dateTo, FORMATTER);
             for (String databaseRecord : data) {
                 String[] dataRecord = databaseRecord.split(" ");
-                String employeeName = dataRecord[1];
-                LocalDate recordDate = LocalDate.parse(dataRecord[0], formatter);
-
+                String employeeName = dataRecord[NAME_INDEX];
+                LocalDate recordDate = LocalDate.parse(dataRecord[DATE_INDEX], FORMATTER);
                 if (employeeName.equals(name)
                         && !recordDate.isBefore(fromDate)
                         && !recordDate.isAfter(toDate)) {
-                    salary += Integer.parseInt(dataRecord[2]) * Integer.parseInt(dataRecord[3]);
+                    int hoursWorked = Integer.parseInt(dataRecord[HOURS_WORKED_INDEX]);
+                    int hourlyRate = Integer.parseInt(dataRecord[HOURLY_RATE_INDEX]);
+                    salary += hoursWorked * hourlyRate;
                 }
             }
             report.append(System.lineSeparator()).append(name).append(" - ").append(salary);
