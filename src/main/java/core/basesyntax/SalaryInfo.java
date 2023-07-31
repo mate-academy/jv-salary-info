d9e6776e4c896fb06a89115f596cef14b1ebeffa
Oleksandr Pidhaiko <iviviv1999@gmail.com>
2023-07-31 22:36:41 +0300

    fixed errors pointed out by mentor

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -6,6 +6,9 @@ import java.time.format.DateTimeParseException;
 
 public class SalaryInfo {
     private static final String REPORT_SEPARATOR = " - ";
+    private static final int DATE_INDEX = 0;
+    private static final int EMPLOYEE_WORKING_HOURS_INDEX = 2;
+    private static final int EMPLOYEE_INCOME_PER_HOUR_INDEX = 3;
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
@@ -24,27 +27,26 @@ public class SalaryInfo {
         for (String name : names) {
             int employeeSalary = 0;
             for (String dataRecord : data) {
-                if (!dataRecord.contains(name)) {
-                    continue;
-                }
-                final int dataEmployeeWorkingHoursIndex = 2;
-                final int dataEmployeeIncomePerHourIndex = 3;
-                LocalDate parsedDate;
+                LocalDate parsedDate = null;
                 String[] dataRecordSplit = dataRecord.split(" ");
-                String employeeWorkingHours = dataRecordSplit[dataEmployeeWorkingHoursIndex];
-                String employeeIncomePerHour = dataRecordSplit[dataEmployeeIncomePerHourIndex];
-                try {
-                    parsedDate = LocalDate.parse(dataRecordSplit[0], formatter);
-                    if (parsedDate.toEpochDay() < startDate.toEpochDay()
-                            || parsedDate.toEpochDay() > endDate.toEpochDay()) {
-                        continue;
+                String employeeWorkingHours = dataRecordSplit[EMPLOYEE_WORKING_HOURS_INDEX];
+                String employeeIncomePerHour = dataRecordSplit[EMPLOYEE_INCOME_PER_HOUR_INDEX];
+                if (dataRecord.contains(name)) {
+                    try {
+                        parsedDate = LocalDate.parse(dataRecordSplit[DATE_INDEX], formatter);
+                    } catch (DateTimeParseException e) {
+                        throw new RuntimeException("Wrong date formats in data.", e);
                     }
-                    employeeSalary += Integer.parseInt(employeeWorkingHours) * Integer.parseInt(
-                            employeeIncomePerHour);
-                } catch (DateTimeParseException exception) {
-                    throw new IllegalArgumentException("Wrong date formats in data.");
-                } catch (NumberFormatException exception) {
-                    throw new IllegalArgumentException("Wrong numeric values format in data.");
+                }
+                if (parsedDate != null && (parsedDate.isAfter(startDate) || parsedDate.isEqual(
+                        startDate)) && (parsedDate.isBefore(endDate) || parsedDate.isEqual(
+                        endDate))) {
+                    try {
+                        employeeSalary += Integer.parseInt(employeeWorkingHours) * Integer.parseInt(
+                                employeeIncomePerHour);
+                    } catch (DateTimeParseException e) {
+                        throw new RuntimeException("Wrong date formats in data.", e);
+                    }
                 }
             }
             report.append(System.lineSeparator());
