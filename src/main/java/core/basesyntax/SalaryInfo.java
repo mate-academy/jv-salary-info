40f0ad0115751391a3a8ff9097e70275df889e7c
Andrey Polezhaiev <polezhaievandrey28@gmail.com>
2023-07-31 15:35:03 +0300

    salaryInfo was successfully implemented

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -2,78 +2,67 @@ package core.basesyntax;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class SalaryInfo {
-    private Map<String, Integer> nameSalaryMap = new LinkedHashMap<>();
+    private static DateTimeFormatter DATE_TIME_FORMATTER
+            = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private String[] report;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        if (names == null || data == null || dateFrom == null || dateTo == null) {
-            return null;
-        }
+        String[] resultReport = new String[names.length];
+        report = new String[names.length];
 
-        Map<String, Integer> resultInfoMap = new LinkedHashMap<>();
+        LocalDate dateStart = LocalDate.parse(dateFrom, DATE_TIME_FORMATTER);
+        LocalDate dateEnd = LocalDate.parse(dateTo, DATE_TIME_FORMATTER);
 
-        LocalDate dateStart = findDate(dateFrom);
-        LocalDate dateEnd = findDate(dateTo);
-        StringBuffer stringBufferFull = new StringBuffer();
-        StringBuffer stringBufferEmpty = new StringBuffer();
-        stringBufferFull.append("Report for period " + dateFrom + " - " + dateTo);
-        stringBufferEmpty.append("Report for period " + dateFrom + " - " + dateTo);
+        StringBuilder stringBuilderFull = new StringBuilder();
+        StringBuilder stringBuilderEmpty = new StringBuilder();
+        StringBuilder builder = new StringBuilder();
+
+        stringBuilderFull.append("Report for period " + dateFrom + " - " + dateTo);
+        stringBuilderEmpty.append("Report for period " + dateFrom + " - " + dateTo);
 
         for (int i = 0; i < names.length; i++) {
-            nameSalaryMap.put(names[i], 0);
+            builder.append(names[i]).append(" ").append("0");
+            report[i] = builder.toString();
+            builder.setLength(0);
             int resultSalary = 0;
             for (int j = 0; j < data.length; j++) {
-                if ((findDate(data[j]).compareTo(dateStart) > 0
-                        && findDate(data[j]).compareTo(dateEnd) < 0)
-                        || findDate(data[j]).equals(dateStart)
-                        || findDate(data[j]).equals(dateEnd)) {
+                LocalDate currentDate = LocalDate.parse(data[j].split(" ")[0], DATE_TIME_FORMATTER);
+                if (currentDate.compareTo(dateStart) > 0
+                        && currentDate.compareTo(dateEnd) < 0
+                        || currentDate.equals(dateStart)
+                        || currentDate.equals(dateEnd)) {
                     if (data[j].contains(names[i])) {
                         String dataEmployee = data[j].substring(data[j].indexOf(names[i]));
                         String[] hourSalary = dataEmployee.split(" ");
                         int hour = Integer.parseInt(hourSalary[1]);
                         int salaryValue = Integer.parseInt(hourSalary[2]);
                         resultSalary = hour * salaryValue;
-                        Integer salary = nameSalaryMap.get(names[i]);
+                        Integer salary = Integer.parseInt(report[i].split(" ")[1]);
                         resultSalary += salary;
-                        resultInfoMap.put(names[i], resultSalary);
-                        nameSalaryMap.put(names[i], resultSalary);
+
+                        builder.append(names[i]).append(" ").append(resultSalary);
+                        resultReport[i] = builder.toString();
+                        builder.setLength(0);
+
+                        builder.append(names[i]).append(" ").append(resultSalary);
+                        report[i] = builder.toString();
+                        builder.setLength(0);
                     }
                 }
             }
-            stringBufferFull.append(System.lineSeparator())
+            stringBuilderFull.append(System.lineSeparator())
                     .append(names[i]).append(" - ").append(resultSalary);
 
-            stringBufferEmpty.append(System.lineSeparator())
+            stringBuilderEmpty.append(System.lineSeparator())
                     .append(names[i]).append(" - ").append(0);
         }
 
-        if (resultInfoMap.isEmpty()) {
-            return stringBufferEmpty.toString();
+        if (resultReport == null || resultReport.length == 0) {
+            return stringBuilderEmpty.toString();
         }
 
-        return stringBufferFull.toString();
-    }
-
-    private LocalDate findDate(String string) {
-        String regex = "\\b(\\d{2})\\.(\\d{2})\\.(\\d{4})\\b";
-
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(string);
-
-        while (matcher.find()) {
-            String day = matcher.group(1);
-            String month = matcher.group(2);
-            String year = matcher.group(3);
-
-            String dateString = day + "." + month + "." + year;
-            LocalDate date = LocalDate.parse(dateString, DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-            return date;
-        }
-        return null;
+        return stringBuilderFull.toString();
     }
 }
