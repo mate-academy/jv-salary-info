12a612c638a6883727d9d09c9dccb5960e722ea3
PavloSliepchenko <sliepchenkopavlo@gmail.com>
2023-08-02 14:09:14 -0500

    getDate() method was replated with DateTimeFormatter, constants for indexes were created

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,14 +1,20 @@
 package core.basesyntax;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    private static final int INDEX_OF_DATE = 0;
+    private static final int INDEX_OF_HOURS = 2;
+    private static final int INDEX_OF_RATE = 3;
+    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder result = new StringBuilder("Report for period ")
                 .append(dateFrom).append(" - ").append(dateTo);
 
-        LocalDate dateStart = getDate(dateFrom);
-        LocalDate dateEnd = getDate(dateTo);
+        LocalDate dateStart = LocalDate.parse(dateFrom, formatter);
+        LocalDate dateEnd = LocalDate.parse(dateTo, formatter);
 
         for (String name : names) {
             result.append(System.lineSeparator()).append(name).append(" - ");
@@ -16,12 +22,12 @@ public class SalaryInfo {
             for (String info : data) {
                 if (info.contains(name)) {
                     String[] infoArray = info.split(" ");
-                    LocalDate workDate = getDate(infoArray[0]);
+                    LocalDate workDate = LocalDate.parse(infoArray[INDEX_OF_DATE], formatter);
                     if (workDate.isEqual(dateStart)
                             || (workDate.isAfter(dateStart) && workDate.isBefore(dateEnd)
                             || workDate.isEqual(dateEnd))) {
-                        int hour = Integer.parseInt(infoArray[2]);
-                        int rate = Integer.parseInt(infoArray[3]);
+                        int hour = Integer.parseInt(infoArray[INDEX_OF_HOURS]);
+                        int rate = Integer.parseInt(infoArray[INDEX_OF_RATE]);
                         salary += hour * rate;
                     }
                 }
@@ -29,13 +35,5 @@ public class SalaryInfo {
             result.append(salary);
         }
         return result.toString();
-    }
-
-    private LocalDate getDate(String stringDate) {
-        String[] tempDateArray = stringDate.split("\\.");
-        int tempDay = Integer.parseInt(tempDateArray[0]);
-        int tempMonth = Integer.parseInt(tempDateArray[1]);
-        int tempYear = Integer.parseInt(tempDateArray[2]);
-        return LocalDate.of(tempYear, tempMonth, tempDay);
     }
 }
