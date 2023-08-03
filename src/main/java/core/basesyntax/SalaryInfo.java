3507400df003e753a63d644b0928aa969f1217f0
Iryna Mekh <iryna.mekh@gmail.com>
2023-08-03 17:37:33 +0300

    fixed mistakes after review

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,49 +1,47 @@
 package core.basesyntax;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM.yyyy");
+    private static final String COMA = ",";
+    private static final String HYPHEN = " - ";
+    private static final String GAP = " ";
+    private static final int ZERO_INDEX = 0;
+    private static final int FIRST_INDEX = 1;
+    private static final int SECOND_INDEX = 2;
+    private static final int THIRD_INDEX = 3;
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder periodData = new StringBuilder();
-        try {
-            Date fromDate = simpleDateFormat.parse(dateFrom);
-            Date toDate = simpleDateFormat.parse(dateTo);
-            for (String day : data) {
-                String[] string = day.split(" ");
-                Date date = simpleDateFormat.parse(string[0]);
-                if ((date.after(fromDate) || fromDate.equals(date))
-                        && (date.before(toDate) || date.equals(toDate))) {
-                    periodData.append(day).append(",");
-                }
+        LocalDate fromDate = LocalDate.parse(dateFrom, formatter);
+        LocalDate toDate = LocalDate.parse(dateTo, formatter);
+        for (String day : data) {
+            String[] string = day.split(GAP);
+            LocalDate date = LocalDate.parse(string[ZERO_INDEX], formatter);
+            if ((date.isAfter(fromDate) || fromDate.isEqual(date))
+                    && (date.isBefore(toDate) || date.isEqual(toDate))) {
+                periodData.append(day).append(COMA);
             }
-        } catch (ParseException e) {
-            System.out.println("Can't parse date");
         }
-        String[] periodDataArr = periodData.toString().split(",");
+        String[] periodDataArr = periodData.toString().split(COMA);
         int[] salsry = new int[names.length];
         StringBuilder builder = new StringBuilder().append("Report for period ")
-                .append(dateFrom).append(" - ")
-                .append(dateTo).append(System.lineSeparator());
+                .append(dateFrom).append(HYPHEN)
+                .append(dateTo);
         for (int i = 0; i < names.length; i++) {
             if (periodDataArr.length > 1) {
                 for (String personDataStr : periodDataArr) {
-                    String[] dataPerson = personDataStr.split(" ");
-                    if (dataPerson[1].equals(names[i])) {
-                        salsry[i] += Integer.parseInt(dataPerson[2])
-                                * Integer.parseInt(dataPerson[3]);
+                    String[] dataPerson = personDataStr.split(GAP);
+                    if (dataPerson[FIRST_INDEX].equals(names[i])) {
+                        salsry[i] += Integer.parseInt(dataPerson[SECOND_INDEX])
+                                * Integer.parseInt(dataPerson[THIRD_INDEX]);
                     }
                 }
             }
-            if (i < names.length - 1) {
-                builder.append(names[i]).append(" - ").append(salsry[i])
-                        .append(System.lineSeparator());
-            } else {
-                builder.append(names[i]).append(" - ").append(salsry[i]);
-            }
+            builder.append(System.lineSeparator()).append(names[i])
+                    .append(HYPHEN).append(salsry[i]);
         }
         return builder.toString();
     }
