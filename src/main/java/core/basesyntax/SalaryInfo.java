71a7a984df8add4cad0fff4ce3d969051c025203
Artem Hlukhoman <giantking1231@gmail.com>
2023-08-06 18:10:43 +0300

    SalaryInfo without hashMap

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -2,8 +2,6 @@ package core.basesyntax;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.HashMap;
-import java.util.Map;
 
 public class SalaryInfo {
     private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter
@@ -13,31 +11,27 @@ public class SalaryInfo {
         LocalDate fromDate = LocalDate.parse(dateFrom, DATE_TIME_FORMATTER);
         LocalDate toDate = LocalDate.parse(dateTo, DATE_TIME_FORMATTER);
 
-        Map<String, Integer> salaryMap = new HashMap<>();
-
-        for (String record: data) {
-            String[] recordParts = record.split(" ");
-            LocalDate date = LocalDate.parse(recordParts[0], DATE_TIME_FORMATTER);
-            String nameAtRecord = recordParts[1];
-            int hoursAtRecord = Integer.parseInt(recordParts[2]);
-            int moneyPerHour = Integer.parseInt(recordParts[3]);
-
-            if (date.isBefore(fromDate) || date.isAfter(toDate)) {
-                continue;
-            }
-
-            int salary = moneyPerHour * hoursAtRecord;
-            salaryMap.put(nameAtRecord,
-                    salaryMap.getOrDefault(nameAtRecord, 0) + salary);
-        }
-
         StringBuilder result = new StringBuilder();
         result.append("Report for period ").append(dateFrom)
                 .append(" - ").append(dateTo);
 
         for (String name : names) {
-            int salary = salaryMap.getOrDefault(name, 0);
-            result.append(System.lineSeparator()).append(name).append(" - ").append(salary);
+            int totalSalary = 0;
+            for (String record : data) {
+                String[] recordParts = record.split(" ");
+                LocalDate date = LocalDate.parse(recordParts[0], DATE_TIME_FORMATTER);
+                if (date.isBefore(fromDate) || date.isAfter(toDate)) {
+                    continue;
+                }
+                String nameAtRecord = recordParts[1];
+                if (name.equals(nameAtRecord)) {
+                    int hoursAtRecord = Integer.parseInt(recordParts[2]);
+                    int moneyPerHour = Integer.parseInt(recordParts[3]);
+                    int salary = moneyPerHour * hoursAtRecord;
+                    totalSalary += salary;
+                }
+            }
+            result.append(System.lineSeparator()).append(name).append(" - ").append(totalSalary);
         }
 
         return result.toString();
