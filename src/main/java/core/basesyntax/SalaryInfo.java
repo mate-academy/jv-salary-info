8a72c59eac88b5c90658a45275e774e9d2892cb5
Artem Hlukhoman <giantking1231@gmail.com>
2023-08-07 13:12:31 +0300

    Changed indexes and If

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -6,6 +6,10 @@ import java.time.format.DateTimeFormatter;
 public class SalaryInfo {
     private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter
             .ofPattern("dd.MM.yyyy");
+    private static final int INDEX_ZERO = 0;
+    private static final int INDEX_ONE = 1;
+    private static final int INDEX_TWO = 2;
+    private static final int INDEX_THREE = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate fromDate = LocalDate.parse(dateFrom, DATE_TIME_FORMATTER);
@@ -16,19 +20,20 @@ public class SalaryInfo {
                 .append(" - ").append(dateTo);
 
         for (String name : names) {
-            int totalSalary = 0;
+            int totalSalary = INDEX_ZERO;
             for (String record : data) {
                 String[] recordParts = record.split(" ");
-                LocalDate date = LocalDate.parse(recordParts[0], DATE_TIME_FORMATTER);
-                if (date.isBefore(fromDate) || date.isAfter(toDate)) {
-                    continue;
-                }
-                String nameAtRecord = recordParts[1];
-                if (name.equals(nameAtRecord)) {
-                    int hoursAtRecord = Integer.parseInt(recordParts[2]);
-                    int moneyPerHour = Integer.parseInt(recordParts[3]);
-                    int salary = moneyPerHour * hoursAtRecord;
-                    totalSalary += salary;
+                LocalDate date = LocalDate.parse(recordParts[INDEX_ZERO], DATE_TIME_FORMATTER);
+                if ((date.isEqual(toDate) || date.isEqual(fromDate))
+                        || (date.isAfter(fromDate) && date.isBefore(toDate))) {
+                    String nameAtRecord = recordParts[INDEX_ONE];
+
+                    if (name.equals(nameAtRecord)) {
+                        int hoursAtRecord = Integer.parseInt(recordParts[INDEX_TWO]);
+                        int moneyPerHour = Integer.parseInt(recordParts[INDEX_THREE]);
+                        int salary = moneyPerHour * hoursAtRecord;
+                        totalSalary += salary;
+                    }
                 }
             }
             result.append(System.lineSeparator()).append(name).append(" - ").append(totalSalary);
