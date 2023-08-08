afaf145f8237df35aee9f55d7625f3cc6e1d4328
Oleksandr Tymoshenko <aleksandrtimosenko089@gmail.com>
2023-08-08 20:51:24 +0300

    refactored code without unnecessary cycle

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,31 +5,31 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class SalaryInfo {
+    static final SimpleDateFormat FORMATTER = new SimpleDateFormat("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        final SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyyy");
         StringBuilder builder = new StringBuilder("Report for period ");
-        builder.append(dateFrom + " - ").append(dateTo);
-        Date dateFromD;
-        Date dateToD;
+        builder.append(dateFrom).append(" - ").append(dateTo);
+        Date localDateFrom;
+        Date localDateTo;
         try {
-            dateFromD = formatter.parse(dateFrom);
-            dateToD = formatter.parse(dateTo);
-            int[] salaries = new int[names.length];
-            for (int i = 0; i < data.length; i++) {
-                String[] dataForOnePeople = data[i].split(" ");
-                if (formatter.parse(dataForOnePeople[0]).compareTo(dateFromD) >= 0
-                        && formatter.parse(dataForOnePeople[0]).compareTo(dateToD) <= 0) {
-                    for (int j = 0; j < names.length; j++) {
-                        if (dataForOnePeople[1].equals(names[j])) {
-                            salaries[j] += Integer.parseInt(dataForOnePeople[2])
-                                    * Integer.parseInt(dataForOnePeople[3]);
+            localDateFrom = FORMATTER.parse(dateFrom);
+            localDateTo = FORMATTER.parse(dateTo);
+            for (int i = 0; i < names.length; i++) {
+                int tempSalaryForPerson = 0;
+                for (int j = 0; j < data.length; j++) {
+                    if (data[j].contains(names[i])) {
+                        String[] dataForOnePerson = data[j].split(" ");
+                        Date personDay = FORMATTER.parse(dataForOnePerson[0]);
+                        if (personDay.compareTo(localDateFrom) >= 0
+                                && personDay.compareTo(localDateTo) <= 0) {
+                            tempSalaryForPerson += Integer.parseInt(dataForOnePerson[2])
+                                    * Integer.parseInt(dataForOnePerson[3]);
                         }
                     }
                 }
-            }
-            for (int i = 0; i < names.length; i++) {
                 builder.append(System.lineSeparator()).append(names[i]).append(" - ")
-                        .append(salaries[i]);
+                        .append(tempSalaryForPerson);
             }
         } catch (ParseException e) {
             return null;
