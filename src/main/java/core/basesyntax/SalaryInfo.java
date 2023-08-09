51217aecb91fc0dcf07160e01935ed0b2c0a4a2d
Epikano <epikano@gmail.com>
2023-08-09 15:12:37 +0300

    Fixed calculateIncome method, added constants for indexes.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -6,6 +6,9 @@ import java.time.format.DateTimeParseException;
 
 public class SalaryInfo {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int DATE_INDEX = 0;
+    private static final int WORKING_HOURS_INDEX = 2;
+    private static final int HOURLY_WAGE_INDEX = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate localDateFrom = stringToLocalDate(dateFrom);
@@ -26,10 +29,11 @@ public class SalaryInfo {
 
     public int calculateIncome(String personData, LocalDate localDateFrom, LocalDate localDateTo) {
         String[] personDataArray = personData.split(" ");
-        LocalDate currentLocalDate = stringToLocalDate(personDataArray[0]);
-        return (currentLocalDate.compareTo(localDateFrom) > -1)
-                && (currentLocalDate.compareTo(localDateTo) < 1)
-                ? Integer.parseInt(personDataArray[2]) * Integer.parseInt(personDataArray[3])
+        LocalDate currentLocalDate = stringToLocalDate(personDataArray[DATE_INDEX]);
+        int workingHours = Integer.parseInt(personDataArray[WORKING_HOURS_INDEX]);
+        int hourlyWage = Integer.parseInt(personDataArray[HOURLY_WAGE_INDEX]);
+        return (!currentLocalDate.isBefore(localDateFrom) && !currentLocalDate.isAfter(localDateTo))
+                ? workingHours * hourlyWage
                 : 0;
     }
 
