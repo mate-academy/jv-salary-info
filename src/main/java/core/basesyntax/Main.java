d14efd7a2c821ea8d9b9d12409f946196df9e385
Teract10s <trs.leonov@gmail.com>
2023-08-10 21:44:59 +0300

    added implementation

---
+++ src/main/java/core/basesyntax/Main.java
@@ -0,0 +1,18 @@
+package core.basesyntax;
+
+public class Main {
+    public static void main(String[] args) {
+        SalaryInfo salaryInfo = new SalaryInfo();
+        String[] names = new String[]{"John", "Andrew", "Kate"};
+        String[] data = new String[]{
+                "25.04.2019 John 60 50",
+                "25.04.2019 Andrew 3 200",
+                "25.04.2019 Kate 10 100",
+                "26.04.2019 Andrew 3 200",
+                "26.04.2019 Kate 9 100",
+                "27.04.2019 John 7 100",
+                "27.04.2019 Kate 3 80",
+                "27.04.2019 Andrew 8 100"};
+        System.out.println(salaryInfo.getSalaryInfo(names, data, "25.04.2019", "25.04.2019"));
+    }
+}
--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,32 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        StringBuilder resultStr = new StringBuilder();
+        resultStr.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
+        for (String n : names) {
+            resultStr.append("\n").append(n).append(" - ");
+            int sum = 0;
+            for (String d : data) {
+                String[] splitData = d.split(" ");
+                if (splitData[1].equals(n) && isRelevantDate(splitData[0], dateFrom, dateTo)) {
+                    sum += Integer.parseInt(splitData[2]) * Integer.parseInt(splitData[3]);
+                }
+            }
+            resultStr.append(sum);
+        }
+        return resultStr.toString();
+    }
+
+    private boolean isRelevantDate(String dateStr, String dateFromStr, String dateToStr) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDate date = LocalDate.parse(dateStr, formatter);
+        LocalDate dateFrom = LocalDate.parse(dateFromStr, formatter);
+        LocalDate dateTo = LocalDate.parse(dateToStr, formatter);
+        return date.isAfter(dateFrom) && date.isBefore(dateTo)
+                || date.isEqual(dateFrom) || date.isEqual(dateTo);
     }
 }
