0a844413a4233d1da4fde8c08e622a841df2e85e
Teract10s <trs.leonov@gmail.com>
2023-08-10 21:54:30 +0300

    corrected some mistakes

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,15 +4,17 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder resultStr = new StringBuilder();
         resultStr.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
-        for (String n : names) {
-            resultStr.append("\n").append(n).append(" - ");
+        for (String name : names) {
+            resultStr.append(System.lineSeparator()).append(name).append(" - ");
             int sum = 0;
             for (String d : data) {
                 String[] splitData = d.split(" ");
-                if (splitData[1].equals(n) && isRelevantDate(splitData[0], dateFrom, dateTo)) {
+                if (splitData[1].equals(name) && isRelevantDate(splitData[0], dateFrom, dateTo)) {
                     sum += Integer.parseInt(splitData[2]) * Integer.parseInt(splitData[3]);
                 }
             }
@@ -22,10 +24,9 @@ public class SalaryInfo {
     }
 
     private boolean isRelevantDate(String dateStr, String dateFromStr, String dateToStr) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        LocalDate date = LocalDate.parse(dateStr, formatter);
-        LocalDate dateFrom = LocalDate.parse(dateFromStr, formatter);
-        LocalDate dateTo = LocalDate.parse(dateToStr, formatter);
+        LocalDate date = LocalDate.parse(dateStr, DATE_TIME_FORMATTER);
+        LocalDate dateFrom = LocalDate.parse(dateFromStr, DATE_TIME_FORMATTER);
+        LocalDate dateTo = LocalDate.parse(dateToStr, DATE_TIME_FORMATTER);
         return date.isAfter(dateFrom) && date.isBefore(dateTo)
                 || date.isEqual(dateFrom) || date.isEqual(dateTo);
     }
