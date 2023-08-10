5c33087c10e211ba11a5627437c55832af3f3b6f
Sonia Baranova <sofiasapsan@gmail.com>
2023-08-10 20:42:45 +0300

    cleaned code

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,25 +4,25 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-    private static final int amountOfDateIndex = 0;
-    private static final int amountOfHourIndex = 2;
-    private static final int amountOfMoneyIndex = 3;
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int AMOUNT_OF_DATE_INDEX = 0;
+    private static final int AMOUNT_OF_HOUR_INDEX = 2;
+    private static final int AMOUNT_OF_MONEY_INDEX = 3;
 
     public String getSalaryInfo(String[] names, String[] data,
                                 String dateFrom, String dateTo) {
         StringBuilder stringBuilder = new StringBuilder("Report for period "
                                             + dateFrom + " - " + dateTo);
-        LocalDate localDateFrom = LocalDate.parse(dateFrom, formatter);
-        LocalDate localDateTo = LocalDate.parse(dateTo, formatter);
+        LocalDate localDateFrom = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate localDateTo = LocalDate.parse(dateTo, FORMATTER);
         System.out.println(localDateFrom + " and " + localDateTo);
         for (int i = 0, fullSalary = 0; i < names.length; i++) {
             for (String pieceOfData : data) {
                 if (pieceOfData.contains(names[i])) {
                     String[] allInfo = pieceOfData.split(" ");
-                    if (isDateIncludes(localDateFrom, localDateTo, allInfo[amountOfDateIndex])) {
-                        fullSalary += Integer.parseInt(allInfo[amountOfHourIndex])
-                                * Integer.parseInt(allInfo[amountOfMoneyIndex]);
+                    if (isDateIncludes(localDateFrom, localDateTo, allInfo[AMOUNT_OF_DATE_INDEX])) {
+                        fullSalary += Integer.parseInt(allInfo[AMOUNT_OF_HOUR_INDEX])
+                                * Integer.parseInt(allInfo[AMOUNT_OF_MONEY_INDEX]);
                     }
                 }
             }
@@ -34,7 +34,7 @@ public class SalaryInfo {
     }
 
     private boolean isDateIncludes(LocalDate dateFrom, LocalDate dateTo, String currentDateString) {
-        LocalDate currentDate = LocalDate.parse(currentDateString, formatter);
+        LocalDate currentDate = LocalDate.parse(currentDateString, FORMATTER);
         return (currentDate.isAfter(dateFrom) && currentDate.isBefore(dateTo))
                 || (currentDate.isEqual(dateFrom) || currentDate.isEqual(dateTo));
     }
