e2f13579b66af826139ac3c0db6a7721969d26ff
Nataliia <ladna1984@gmail.com>
2023-08-11 21:13:30 +0300

    Renamed the variables: dateFrom1, dateTo1; Stringbuilder sb. Wrote two logical conditions instead of four. Thanks.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,35 +5,39 @@ import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     public static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-    private static final String INDEX = " ";
+    private static final String DELIMITER = " ";
+    private static final int WORKDAY = 0;
+    private static final int NAMEOFEMPLOYEE = 1;
+    private static final int WORKINGHOUR = 2;
+    private static final int INCOMEPERHOUSE = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        LocalDate dateFrom1 = LocalDate.parse(dateFrom, formatter);
-        LocalDate dateTo1 = LocalDate.parse(dateTo, formatter);
+        LocalDate from = LocalDate.parse(dateFrom, formatter);
+        LocalDate to = LocalDate.parse(dateTo, formatter);
         int salary = 0;
-        StringBuilder sb = new StringBuilder();
-        sb.append("Report for period ")
+        StringBuilder report = new StringBuilder();
+        report.append("Report for period ")
                 .append(dateFrom)
                 .append(" - ")
                 .append(dateTo);
         for (String name : names) {
             for (String dates : data) {
-                String[] splitData = dates.split(INDEX);
-                LocalDate dataWork = LocalDate.parse(splitData[0], formatter);
-                if (name.equals(splitData[1]) && ((dateFrom1.isBefore(dataWork)
-                        && dataWork.isBefore(dateTo1))
-                        || dataWork.isEqual(dateFrom1)
-                        || dataWork.isEqual(dateTo1))) {
+                String[] splitData = dates.split(DELIMITER);
+                LocalDate dataWork = LocalDate.parse(splitData[WORKDAY], formatter);
+                if (name.equals(splitData[NAMEOFEMPLOYEE])
+                        && ((from.minusDays(1).isBefore(dataWork)
+                        && dataWork.isBefore(to.plusDays(1))))) {
                     salary = salary
-                            + Integer.parseInt(splitData[2]) * Integer.parseInt(splitData[3]);
+                            + Integer.parseInt(splitData[WORKINGHOUR])
+                            * Integer.parseInt(splitData[INCOMEPERHOUSE]);
                 }
             }
-            sb.append(System.lineSeparator())
+            report.append(System.lineSeparator())
                     .append(name)
                     .append(" - ")
                     .append(salary);
             salary = 0;
         }
-        return sb.toString();
+        return report.toString();
     }
 }
