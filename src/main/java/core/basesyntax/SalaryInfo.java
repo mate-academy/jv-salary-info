b570e0ed27deb889dff5879fe51bf5a4eeb20fb3
Vlad Rublenko <vladik.rar@ukr.net>
2023-08-11 18:48:16 +0300

    solved it by using class SimpleDateFormat, Date, .after, .before, .equals

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,5 +1,10 @@
 package core.basesyntax;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class SalaryInfo {
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
@@ -7,91 +12,38 @@ public class SalaryInfo {
         StringBuilder info = new StringBuilder("Report for period ");
         info.append(dateFrom).append(" - ").append(dateTo);
 
-        for (int i = 0; i < names.length; i++) {
-            int tempSalaryInMonth = 0;
-            for (int j = 0; j < data.length; j++) {
-                String[] tempSpaceSeparatorData = data[j].split(" ");
-                String[] tempPointSeparatorData = tempSpaceSeparatorData[0].split("\\.");
+        try {
+            SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+            Date dateFromDate = dateFormat.parse(dateFrom);
+            Date dateToDate = dateFormat.parse(dateTo);
 
-                String[] tempSpaceSeparatorDateFrom = dateFrom.split(" ");
-                String[] tempPointSeparatorDateFrom = tempSpaceSeparatorDateFrom[0].split("\\.");
+            for (int i = 0; i < names.length; i++) {
+                int tempSalaryInMonth = 0;
+                for (int j = 0; j < data.length; j++) {
+                    String[] tempSpaceSeparatorData = data[j].split(" ");
 
-                String[] tempSpaceSeparatorDateTo = dateTo.split(" ");
-                String[] tempPointSeparatorDateTo = tempSpaceSeparatorDateTo[0].split("\\.");
+                    Date temporaryDate = dateFormat.parse(tempSpaceSeparatorData[0]);
 
-                int tempYearDateFrom = Integer.parseInt(tempPointSeparatorDateFrom[2]);
-                int tempMonthDateFrom = Integer.parseInt(tempPointSeparatorDateFrom[1]);
-                int tempDayDateFrom = Integer.parseInt(tempPointSeparatorDateFrom[0]);
+                    if ((temporaryDate.after(dateFromDate) || temporaryDate.equals(dateFromDate))
+                            && (temporaryDate.before(dateToDate) || temporaryDate.equals(dateToDate))) {
+                        if (data[j].contains(names[i])) {
+                            int tempHourInThatDay = Integer.parseInt(tempSpaceSeparatorData[2]);
+                            int tempSalaryPerHourInThatDay = Integer
+                                    .parseInt(tempSpaceSeparatorData[3]);
 
-                int tempYearDateTo = Integer.parseInt(tempPointSeparatorDateTo[2]);
-                int tempMonthDateTo = Integer.parseInt(tempPointSeparatorDateTo[1]);
-                int tempDayDateTo = Integer.parseInt(tempPointSeparatorDateTo[0]);
-
-                int tempYearData = Integer.parseInt(tempPointSeparatorData[2]);
-                int tempMonthData = Integer.parseInt(tempPointSeparatorData[1]);
-                int tempDayData = Integer.parseInt(tempPointSeparatorData[0]);
-
-                if (compareDate(tempYearData, tempYearDateFrom, tempYearDateTo, tempMonthData,
-                        tempMonthDateFrom, tempMonthDateTo, tempDayData,
-                        tempDayDateFrom, tempDayDateTo)) {
-                    if (data[j].contains(names[i])) {
-                        int tempHourInThatDay = Integer.parseInt(tempSpaceSeparatorData[2]);
-                        int tempSalaryPerHourInThatDay = Integer
-                                .parseInt(tempSpaceSeparatorData[3]);
-
-                        int tempSalarySumInThatDay = tempHourInThatDay * tempSalaryPerHourInThatDay;
-                        tempSalaryInMonth += tempSalarySumInThatDay;
+                            int tempSalarySumInThatDay = tempHourInThatDay * tempSalaryPerHourInThatDay;
+                            tempSalaryInMonth += tempSalarySumInThatDay;
+                        }
+                    }
+                    if (j == data.length - 1) {
+                        info.append(System.lineSeparator()).append(names[i])
+                                .append(" - ").append(tempSalaryInMonth);
                     }
                 }
-                if (j == data.length - 1) {
-                    info.append(System.lineSeparator()).append(names[i])
-                                    .append(" - ").append(tempSalaryInMonth);
-                }
             }
+        } catch (ParseException e) {
+            System.out.println("date in not valid");
         }
         return info.toString();
-    }
-
-    public static boolean compareDate(int year, int yearFrom, int yearTo,
-                                      int month, int monthFrom, int monthTo,
-                                      int day,int dayFrom, int dayTo) {
-
-        if (year > yearFrom && year < yearTo) {
-            return true;
-        }
-        if (!(year >= yearFrom && year <= yearTo)) {
-            return false;
-        }
-        if (year == yearFrom && year != yearTo) {
-            if (month > monthFrom) {
-                return true;
-            }
-            if (month == monthFrom && day >= dayFrom) {
-                return true;
-            }
-        }
-        if (year == yearTo && year != yearFrom) {
-            if (month < monthTo) {
-                return true;
-            }
-            if (month == monthTo && day <= dayTo) {
-                return true;
-            }
-        }
-        if (yearFrom == yearTo) {
-            if (month > monthFrom && month < monthTo) {
-                return true;
-            }
-            if (month == monthFrom && month != monthTo && day >= dayFrom) {
-                return true;
-            }
-            if (month == monthTo && month != monthFrom && day <= dayTo) {
-                return true;
-            }
-            if (monthFrom == monthTo && day >= dayFrom && day <= dayTo) {
-                return true;
-            }
-        }
-        return false;
     }
 }
