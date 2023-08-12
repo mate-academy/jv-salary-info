e4e819305e88d70786a7233b85797280b6520644
Vlad Rublenko <vladik.rar@ukr.net>
2023-08-12 17:40:15 +0300

    created 2 constans DATE_FORMAT and START_INDEX, changed Date to LocalDate, and deleted indexex(0) by changing it to constant variable. Also deleted try-catch block

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,49 +1,50 @@
 package core.basesyntax;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+
+    private static final DateTimeFormatter DATE_FORMAT =
+            DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
+    private static final int START_INDEX = 0;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
 
         StringBuilder info = new StringBuilder("Report for period ");
         info.append(dateFrom).append(" - ").append(dateTo);
 
-        try {
-            SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
-            Date dateFromDate = dateFormat.parse(dateFrom);
-            Date dateToDate = dateFormat.parse(dateTo);
+            LocalDate dateFromDate = LocalDate.parse(dateFrom, DATE_FORMAT);
+            LocalDate dateToDate = LocalDate.parse(dateTo, DATE_FORMAT);
 
-            for (int i = 0; i < names.length; i++) {
-                int tempSalaryInMonth = 0;
-                for (int j = 0; j < data.length; j++) {
-                    String[] tempSpaceSeparatorData = data[j].split(" ");
+        for (String name : names) {
+            int tempSalaryInMonth = 0;
+            for (int j = START_INDEX; j < data.length; j++) {
+                String[] tempSpaceSeparatorData = data[j].split(" ");
 
-                    Date temporaryDate = dateFormat.parse(tempSpaceSeparatorData[0]);
+                LocalDate temporaryDate = LocalDate
+                        .parse(tempSpaceSeparatorData[START_INDEX], DATE_FORMAT);
 
-                    if ((temporaryDate.after(dateFromDate) || temporaryDate.equals(dateFromDate))
-                            && (temporaryDate.before(dateToDate)
-                            || temporaryDate.equals(dateToDate))) {
-                        if (data[j].contains(names[i])) {
-                            int tempHourInThatDay = Integer.parseInt(tempSpaceSeparatorData[2]);
-                            int tempSalaryPerHourInThatDay = Integer
-                                    .parseInt(tempSpaceSeparatorData[3]);
+                if ((temporaryDate.isAfter(dateFromDate)
+                        || temporaryDate.equals(dateFromDate))
+                        && (temporaryDate.isBefore(dateToDate)
+                        || temporaryDate.equals(dateToDate))) {
+                    if (data[j].contains(name)) {
+                        int tempHourInThatDay = Integer.parseInt(tempSpaceSeparatorData[2]);
+                        int tempSalaryPerHourInThatDay = Integer
+                                .parseInt(tempSpaceSeparatorData[3]);
 
-                            int tempSalarySumInThatDay =
-                                    tempHourInThatDay * tempSalaryPerHourInThatDay;
-                            tempSalaryInMonth += tempSalarySumInThatDay;
-                        }
-                    }
-                    if (j == data.length - 1) {
-                        info.append(System.lineSeparator()).append(names[i])
-                                .append(" - ").append(tempSalaryInMonth);
+                        int tempSalarySumInThatDay =
+                                tempHourInThatDay * tempSalaryPerHourInThatDay;
+                        tempSalaryInMonth += tempSalarySumInThatDay;
                     }
                 }
+                if (j == data.length - 1) {
+                    info.append(System.lineSeparator()).append(name)
+                            .append(" - ").append(tempSalaryInMonth);
+                }
             }
-        } catch (ParseException e) {
-            System.out.println("date in not valid");
         }
         return info.toString();
     }
