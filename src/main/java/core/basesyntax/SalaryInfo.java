6ecdb18dad0c64b11d1a6708e1f49c9efd94f17e
Mykhailo <mykhailokuryk16@gmail.com>
2023-08-12 18:23:49 +0300

    fix my solution after review

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -6,6 +6,10 @@ import java.time.format.DateTimeFormatter;
 public class SalaryInfo {
     private static final DateTimeFormatter dateFormatter =
             DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int WORKING_HOURS_INDEX = 2;
+    private static final int INCOME_INDEX = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate fromDate = LocalDate.parse(dateFrom, dateFormatter);
@@ -20,10 +24,10 @@ public class SalaryInfo {
             int earnedMoney = 0;
             for (String part : data) {
                 String[] parts = part.split(" ");
-                LocalDate entryDate = LocalDate.parse(parts[0], dateFormatter);
-                String employeeName = parts[1];
-                int workingHours = Integer.parseInt(parts[2]);
-                int incomePerHour = Integer.parseInt(parts[3]);
+                LocalDate entryDate = LocalDate.parse(parts[DATE_INDEX], dateFormatter);
+                String employeeName = parts[NAME_INDEX];
+                int workingHours = Integer.parseInt(parts[WORKING_HOURS_INDEX]);
+                int incomePerHour = Integer.parseInt(parts[INCOME_INDEX]);
 
                 if (employeeName.equals(name) && !entryDate.isBefore(fromDate)
                         && !entryDate.isAfter(toDate)) {
@@ -35,7 +39,6 @@ public class SalaryInfo {
                     .append(earnedMoney)
                     .append(System.lineSeparator());
         }
-
         return builder.toString().trim();
     }
 }
