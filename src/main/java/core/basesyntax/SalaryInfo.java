cf14e1954555caf69bded819830501f4abbad27f
Oleh Oliiarnyk <oliver.aka.rid@gmail.com>
2023-08-15 18:22:53 +0300

    removed one circle and improved solution

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -11,22 +11,25 @@ public class SalaryInfo {
 
     public String getSalaryInfo(String[] names, String[] data,
                                        String dateFrom, String dateTo) {
-        int[] salaryCount = new int[names.length];
-        for (String datum : data) {
-            String[] dataAboutEmployee = datum.split(" ");
-            if (dataValidation(dataAboutEmployee[0], dateFrom, dateTo)) {
-                for (int i = 0; i < names.length; i++) {
-                    if (names[i].equals(dataAboutEmployee[1])) {
-                        salaryCount[i] += Integer.parseInt(dataAboutEmployee[2])
-                                * Integer.parseInt(dataAboutEmployee[3]);
-                    }
+        StringBuilder stringBuilder = new StringBuilder("Report for period " + dateFrom
+                + WORDS_SEPARATOR + dateTo);
+        for (String name : names) {
+            int salaryCount = 0;
+            for (String datum : data) {
+                String[] dataAboutEmployee = datum.split(" ");
+                if (dataValidation(dataAboutEmployee[0], dateFrom, dateTo)
+                        && name.equals(dataAboutEmployee[1])) {
+                    salaryCount += Integer.parseInt(dataAboutEmployee[2])
+                            * Integer.parseInt(dataAboutEmployee[3]);
                 }
             }
+            stringBuilder.append(SYSTEM_SEPARATOR).append(name)
+                    .append(WORDS_SEPARATOR).append(salaryCount);
         }
-        return getReport(names, salaryCount, dateFrom, dateTo);
+        return stringBuilder.toString();
     }
 
-    private boolean dataValidation(String date, String dateFrom, String dateTo) {
+    private static boolean dataValidation(String date, String dateFrom, String dateTo) {
         try {
             if (DATE_FORMAT.parse(date).compareTo(DATE_FORMAT.parse(dateFrom)) >= 0
                              && DATE_FORMAT.parse(date).compareTo(DATE_FORMAT.parse(dateTo)) <= 0) {
@@ -36,15 +39,5 @@ public class SalaryInfo {
             throw new RuntimeException("Invalid input date format", e);
         }
         return false;
-    }
-
-    private String getReport(String[] names, int[] salary, String dateFrom, String dateTo) {
-        StringBuilder stringBuilder = new StringBuilder("Report for period " + dateFrom
-                + WORDS_SEPARATOR + dateTo + SYSTEM_SEPARATOR);
-        for (int i = 0; i < names.length; i++) {
-            stringBuilder.append(names[i]).append(WORDS_SEPARATOR).append(salary[i])
-                    .append(SYSTEM_SEPARATOR);
-        }
-        return stringBuilder.toString().trim().replaceAll("\\n+$", "");
     }
 }
