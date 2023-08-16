db24e823017d349e0bc8eb1ad4b41dcbcbac2950
Vlad Rublenko <vladik.rar@ukr.net>
2023-08-16 19:16:40 +0300

    created new constants and changed numbers to them

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -9,11 +9,18 @@ public class SalaryInfo {
             DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     private static final int START_INDEX = 0;
+    private static final String SEPARATOR = " ";
+    private static final String DELIMITER = " - ";
+    private static final String HEADER = "Report for period ";
+    private static final int NUMBER_ONE = 1;
+    private static final int INDEX_TWO = 2;
+    private static final int INDEX_THREE = 3;
+
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
 
-        StringBuilder info = new StringBuilder("Report for period ");
-        info.append(dateFrom).append(" - ").append(dateTo);
+        StringBuilder info = new StringBuilder(HEADER);
+        info.append(dateFrom).append(DELIMITER).append(dateTo);
 
         LocalDate dateFromDate = LocalDate.parse(dateFrom, DATE_FORMAT);
         LocalDate dateToDate = LocalDate.parse(dateTo, DATE_FORMAT);
@@ -21,7 +28,7 @@ public class SalaryInfo {
         for (String name : names) {
             int tempSalaryInMonth = 0;
             for (int j = START_INDEX; j < data.length; j++) {
-                String[] tempSpaceSeparatorData = data[j].split(" ");
+                String[] tempSpaceSeparatorData = data[j].split(SEPARATOR);
 
                 LocalDate temporaryDate = LocalDate
                         .parse(tempSpaceSeparatorData[START_INDEX], DATE_FORMAT);
@@ -31,18 +38,18 @@ public class SalaryInfo {
                         && (temporaryDate.isBefore(dateToDate)
                         || temporaryDate.equals(dateToDate))) {
                     if (data[j].contains(name)) {
-                        int tempHourInThatDay = Integer.parseInt(tempSpaceSeparatorData[2]);
+                        int tempHourInThatDay = Integer.parseInt(tempSpaceSeparatorData[INDEX_TWO]);
                         int tempSalaryPerHourInThatDay = Integer
-                                .parseInt(tempSpaceSeparatorData[3]);
+                                .parseInt(tempSpaceSeparatorData[INDEX_THREE]);
 
                         int tempSalarySumInThatDay =
                                 tempHourInThatDay * tempSalaryPerHourInThatDay;
                         tempSalaryInMonth += tempSalarySumInThatDay;
                     }
                 }
-                if (j == data.length - 1) {
+                if (j == data.length - NUMBER_ONE) {
                     info.append(System.lineSeparator()).append(name)
-                            .append(" - ").append(tempSalaryInMonth);
+                            .append(DELIMITER).append(tempSalaryInMonth);
                 }
             }
         }
