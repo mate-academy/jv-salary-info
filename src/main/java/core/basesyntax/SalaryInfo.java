e5a4ba1ff70cbd1a3ef4be42db5bfd8deab0222d
Alexander <liero2D@gmail.com>
2023-08-17 13:38:08 +0300

    The getSalaryInfo method has been implemented.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,35 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        LocalDate localDateFrom = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate localDateTo = LocalDate.parse(dateTo, FORMATTER);
+        StringBuilder builder = new StringBuilder();
+        builder.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
+        String[] arrayOfData;
+
+        for (String name : names) {
+            int salary = 0;
+            builder.append(System.lineSeparator());
+            for (String line : data) {
+                arrayOfData = line.split(" ");
+                if (name.equals(arrayOfData[1])) {
+                    LocalDate currentDate = LocalDate.parse(arrayOfData[0], FORMATTER);
+                    if (localDateFrom.compareTo(currentDate) <= 0
+                            && localDateTo.compareTo(currentDate) >= 0
+                            && arrayOfData[1].equals(name)) {
+                        salary += Integer.parseInt(arrayOfData[2])
+                                * Integer.parseInt(arrayOfData[3]);
+                    }
+                }
+            }
+            builder.append(name).append(" - ").append(salary);
+        }
+        return builder.toString();
     }
 }
