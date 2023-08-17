e6800caac722a2c8f1b52365f02a03e1ddcdcf4b
Alexander <liero2D@gmail.com>
2023-08-17 20:37:44 +0300

    Added fixes after re-review.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,6 +5,7 @@ import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int DATE = 0;
     private static final int EMPLOYEE_NAME = 1;
     private static final int HOURS_WORKED = 2;
     private static final int HOURLY_WAGE = 3;
@@ -21,10 +22,10 @@ public class SalaryInfo {
             for (String line : data) {
                 String[] arrayOfData = line.split(" ");
                 if (name.equals(arrayOfData[EMPLOYEE_NAME])) {
-                    LocalDate currentDate = LocalDate.parse(arrayOfData[0], FORMATTER);
+                    LocalDate currentDate = LocalDate.parse(arrayOfData[DATE], FORMATTER);
                     if (localDateFrom.compareTo(currentDate) <= 0
                             && localDateTo.compareTo(currentDate) >= 0
-                            && arrayOfData[1].equals(name)) {
+                            && arrayOfData[EMPLOYEE_NAME].equals(name)) {
                         salary += Integer.parseInt(arrayOfData[HOURS_WORKED])
                                 * Integer.parseInt(arrayOfData[HOURLY_WAGE]);
                     }
