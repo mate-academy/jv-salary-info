da161bde6a61fb9fd67e432729dde7fbff9553cd
Stanislav Viner <stanislav.viner@gmail.com>
2023-08-18 15:29:37 +0300

    Added NEW jv-salary-info solution

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,38 +4,41 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    private static final DateTimeFormatter DATE_FORMATTER
+            = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int DATE = 0;
+    private static final int NAME = 1;
+    private static final int HOURS = 2;
+    private static final int INCOME = 3;
+    private static final String REG_EX = " ";
+    private static final String DASH = " - ";
+    private static final String LINE_SEPARATOR = System.lineSeparator();
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        StringBuilder result = new StringBuilder();
-        String[][] salary = new String[names.length][2];
-        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        LocalDate dateStart = LocalDate.parse(dateFrom, dateTimeFormatter);
-        LocalDate dateEnd = LocalDate.parse(dateTo, dateTimeFormatter);
+        StringBuilder result = new StringBuilder().append("Report for period ").append(dateFrom)
+                                                  .append(DASH).append(dateTo);
 
-        for (int i = 0; i < names.length; i++) {
-            salary[i][0] = names[i];
-            salary[i][1] = "0";
-        }
+        LocalDate firstDate = LocalDate.parse(dateFrom, DATE_FORMATTER);
+        LocalDate lastDate = LocalDate.parse(dateTo, DATE_FORMATTER);
 
-        for (String datum : data) {
-            String[] temp = datum.split(" ");
-            LocalDate dateTemp = LocalDate.parse(temp[0], dateTimeFormatter);
+        for (String name : names) {
+            int employeeSalary = 0;
 
-            if (!dateTemp.isBefore(dateStart) && !dateTemp.isAfter(dateEnd)) {
-                for (int j = 0; j < salary.length; j++) {
-                    if (temp[1].equals(salary[j][0])) {
-                        salary[j][1] = String.valueOf(Integer.parseInt(salary[j][1])
-                                + Integer.parseInt(temp[2])
-                                * Integer.parseInt(temp[3]));
-                    }
+            for (String datum : data) {
+                String[] employeeInfo = datum.split(REG_EX);
+
+                LocalDate employeeDate = LocalDate.parse(employeeInfo[DATE], DATE_FORMATTER);
+                String employeeName = employeeInfo[NAME];
+                int employeeHours = Integer.parseInt(employeeInfo[HOURS]);
+                int employeeIncome = Integer.parseInt(employeeInfo[INCOME]);
+
+                if (employeeName.equals(name) && !employeeDate.isBefore(firstDate)
+                                              && !employeeDate.isAfter(lastDate)) {
+                    employeeSalary += employeeHours * employeeIncome;
                 }
             }
+            result.append(LINE_SEPARATOR).append(name).append(DASH).append(employeeSalary);
         }
-
-        for (String[] finalSalary : salary) {
-            result.append(System.lineSeparator()).append(finalSalary[0])
-                                .append(" - ").append(finalSalary[1]);
-        }
-
-        return "Report for period " + dateFrom + " - " + dateTo + result;
+        return result.toString();
     }
 }
