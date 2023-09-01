efbcb9fe1c6ee5ecbc341799ec6f7646b661b35e
Vladyslav Herasymenko <dalv23092005@gmail.com>
2023-09-01 20:17:18 +0200

    Made some fields constant

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,26 +4,30 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    public static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
     public static final int DATA_LENGTH = 4;
+    public static final int DATE_INDEX = 0;
+    public static final int NAME_INDEX = 1;
+    public static final int HOUR_INDEX = 2;
+    public static final int SALARY_INDEX = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         int[] salaries = new int[names.length];
-        LocalDate to = LocalDate.parse(dateTo, formatter);
-        LocalDate from = LocalDate.parse(dateFrom, formatter);
+        LocalDate to = LocalDate.parse(dateTo, FORMATTER);
+        LocalDate from = LocalDate.parse(dateFrom, FORMATTER);
         LocalDate tempDate;
         String[] tempData;
 
         for (String datum : data) {
             tempData = datum.split(" ", DATA_LENGTH);
-            tempDate = LocalDate.parse(tempData[0], formatter);
+            tempDate = LocalDate.parse(tempData[DATE_INDEX], FORMATTER);
 
             if ((tempDate.isAfter(from) || tempDate.isEqual(from))
                     && (tempDate.isBefore(to) || tempDate.isEqual(to))) {
                 for (int i = 0; i < names.length; i++) {
-                    if (tempData[1].equals(names[i])) {
-                        salaries[i] += Integer.parseInt(tempData[3])
-                                * Integer.parseInt(tempData[2]);
+                    if (tempData[NAME_INDEX].equals(names[i])) {
+                        salaries[i] += Integer.parseInt(tempData[SALARY_INDEX])
+                                * Integer.parseInt(tempData[HOUR_INDEX]);
                     }
                 }
             }
