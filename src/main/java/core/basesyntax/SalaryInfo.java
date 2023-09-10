243b4121772a29c8bcc53664f0797193358802cc
Bohdan Maksymenko <bogdan.maksimenko.2002@gmail.com>
2023-09-10 17:47:54 +0300

    implement method for report of worker salary

--- src/main/java/core/basesyntax/SalaryInfo.java
+++
@@ -1,7 +0,0 @@
-package core.basesyntax;
-
-public class SalaryInfo {
-    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
-    }
-}
