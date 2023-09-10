686f46e13267fccf80c9a88f340d1a000beb0b5a
Vitalii Voitseshuk <voitseshuk.v@gmail.com>
2023-09-10 22:06:34 +0300

    rename variables

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -7,17 +7,18 @@ import java.time.format.DateTimeFormatter;
 public class SalaryInfo {
     private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
-    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo)
+    public String getSalaryInfo(String[] names, String[] data, String dateFromString,
+                                String dateToString)
             throws DateTimeException, NumberFormatException {
-        LocalDate from = LocalDate.parse(dateFrom, formatter);
-        LocalDate to = LocalDate.parse(dateTo, formatter);
-        if (from.isAfter(to)) {
+        LocalDate dateFrom = LocalDate.parse(dateFromString, formatter);
+        LocalDate dateTo = LocalDate.parse(dateToString, formatter);
+        if (dateFrom.isAfter(dateTo)) {
             throw new DateTimeException("Invalid period");
         }
-        StringBuilder result = new StringBuilder("Report for period ").append(dateFrom)
-                .append(" - ").append(dateTo);
+        StringBuilder result = new StringBuilder("Report for period ").append(dateFromString)
+                .append(" - ").append(dateToString);
         for (String name: names) {
-            int amount = 0;
+            int salary = 0;
             for (String record: data) {
                 String[] parts = record.split(" ");
                 if (parts.length != 4) {
@@ -27,12 +28,12 @@ public class SalaryInfo {
                     continue;
                 }
                 LocalDate recordDate = LocalDate.parse(parts[0], formatter);
-                if (from.isAfter(recordDate) || to.isBefore(recordDate)) {
+                if (dateFrom.isAfter(recordDate) || dateTo.isBefore(recordDate)) {
                     continue;
                 }
-                amount += Integer.parseInt(parts[2]) * Integer.parseInt(parts[3]);
+                salary += Integer.parseInt(parts[2]) * Integer.parseInt(parts[3]);
             }
-            result.append(System.lineSeparator()).append(name).append(" - ").append(amount);
+            result.append(System.lineSeparator()).append(name).append(" - ").append(salary);
         }
         return result.toString();
     }
