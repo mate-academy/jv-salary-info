a5368496ba98590e6d7e6bb9e965fe17c82f44dc
Bohdan Maksymenko <bogdan.maksimenko.2002@gmail.com>
2023-09-11 10:59:44 +0300

    new solution for salary info task

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -2,49 +2,35 @@ package core.basesyntax;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.LinkedHashMap;
-import java.util.Map;
 
 public class SalaryInfo {
     private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-    private static String WHITESPACE_DELIMITER = " ";
-    private Map<String, Integer> workerInfo = new LinkedHashMap<>();
+    private static String DATA_LINE_SEPARATOR = " ";
+    private static int INDEX_OF_DATE = 0;
+    private static int INDEX_OF_NAME = 1;
+    private static int INDEX_OF_WORKING_HOURS = 2;
+    private static int INDEX_OF_INCOME_PER_HOUR = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate localDateFrom = LocalDate.parse(dateFrom, formatter);
         LocalDate localDateTo = LocalDate.parse(dateTo, formatter);
-        setWorkersName(workerInfo,names);
-        for (String record : data) {
-            String[] parts = record.split(WHITESPACE_DELIMITER);
-            String employeeName = parts[1];
-            LocalDate parseDate = LocalDate.parse(parts[0], formatter);
-            int parsedHour = Integer.parseInt(parts[2]);
-            int parsedMoney = Integer.parseInt(parts[3]);
-            if (parseDate.isBefore(localDateTo.plusDays(1L)) && parseDate.isAfter(localDateFrom)) {
-                int earnAday = parsedHour * parsedMoney;
-                workerInfo.put(employeeName, workerInfo.get(employeeName) + earnAday);
+        StringBuilder result = new StringBuilder("Report for period ")
+                .append(dateFrom).append(" - ").append(dateTo);
+
+        for (String name : names) {
+            int sum = 0;
+            for (String datum : data) {
+                String[] lineFromData = datum.split(DATA_LINE_SEPARATOR);
+                LocalDate currentDate = LocalDate.parse(lineFromData[INDEX_OF_DATE], formatter);
+                if (name.equals(lineFromData[INDEX_OF_NAME]) && !currentDate.isAfter(localDateTo)
+                        && !currentDate.isBefore(localDateFrom)) {
+                    sum += Integer.parseInt(lineFromData[INDEX_OF_WORKING_HOURS])
+                            * Integer.parseInt(lineFromData[INDEX_OF_INCOME_PER_HOUR]);
+                }
             }
+            result.append(System.lineSeparator()).append(name).append(" - ").append(sum);
         }
-        return createReport(workerInfo, dateFrom, dateTo);
-    }
-
-    private Map<String, Integer> setWorkersName(Map<String, Integer> workerInfo,String[] names) {
-        for (String name: names) {
-            workerInfo.put(name,0);
-        }
-        return workerInfo;
-    }
-
-    private String createReport(Map<String,Integer> workerInfo,String dateFrom,String dateTo) {
-        StringBuilder resultString = new StringBuilder(
-                String.format("Report for period %s - %s",dateFrom,dateTo))
-                .append(System.lineSeparator());
-        for (Map.Entry<String, Integer> info : workerInfo.entrySet()) {
-            resultString.append(info.getKey() + " - " + info.getValue())
-                    .append(System.lineSeparator());
-        }
-        resultString.delete(resultString.length() - System.lineSeparator().length(),
-                resultString.length());
-        return resultString.toString();
+        return result.toString();
     }
 }
+
