aba122830817428fc8606210a76179e5c5eb2316
Dima Semeniuk <semeniuk.dima@ukr.net>
2023-09-11 16:11:00 +0300

    made the code more optimized

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -8,30 +8,31 @@ public class SalaryInfo {
     private static final int POSITION_OF_NAME = 1;
     private static final int POSITION_OF_HOURS = 2;
     private static final int POSITION_OF_MONEY_PER_HOUR = 3;
+    private static final String SEPARATOR = " ";
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        StringBuilder builder = new StringBuilder("Report for period ");
+        int moneyForPeriod;
+        StringBuilder builder = new StringBuilder("Report for period ")
+                .append(dateFrom).append(" - ").append(dateTo);;
         LocalDate localDateFrom = LocalDate.parse(dateFrom, FORMATTER);
         LocalDate localDateTo = LocalDate.parse(dateTo, FORMATTER);
-        builder.append(dateFrom).append(" - ").append(dateTo);
 
         for (String name : names) {
-            builder.append(System.lineSeparator()).append(name).append(" - ");
-            int moneyForPeriod = 0;
-
+            moneyForPeriod = 0;
             for (String record : data) {
-                String[] infoFromRecord = record.split(" ");
-                LocalDate date = LocalDate.parse(infoFromRecord[POSITION_OF_DATE], FORMATTER);
-
-                if (name.equals(infoFromRecord[POSITION_OF_NAME])
-                        && (date.isEqual(localDateFrom) || date.isEqual(localDateTo)
-                        || (date.isAfter(localDateFrom) && date.isBefore(localDateTo)))) {
-                    moneyForPeriod += Integer.parseInt(infoFromRecord[POSITION_OF_HOURS])
+                String[] infoFromRecord = record.split(SEPARATOR);
+                if (name.equals(infoFromRecord[POSITION_OF_NAME])) {
+                    LocalDate date = LocalDate.parse(infoFromRecord[POSITION_OF_DATE], FORMATTER);
+                    if (date.isEqual(localDateFrom) || date.isEqual(localDateTo)
+                            || (date.isAfter(localDateFrom) && date.isBefore(localDateTo))) {
+                        moneyForPeriod += Integer.parseInt(infoFromRecord[POSITION_OF_HOURS])
                                 * Integer.parseInt(infoFromRecord[POSITION_OF_MONEY_PER_HOUR]);
+                    }
                 }
             }
-            builder.append(moneyForPeriod);
+            builder.append(System.lineSeparator()).append(name).append(" - ")
+                    .append(moneyForPeriod);
         }
         return builder.toString();
     }
