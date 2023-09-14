4706b1f9e423384cf3ed8f8cb9fee9644514b098
Kseniia Stepanchenko <red.xantoria@gmail.com>
2023-09-14 18:58:06 +0300

    added fixes

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -7,33 +7,35 @@ public class SalaryInfo {
     private static final String FORMATTER = "dd.MM.yyyy";
     private static final String HEADER = "Report for period ";
     private static final String DELIMITER = " - ";
-    private static final int DATE = 0;
-    private static final int EMPLOYEE_NAME = 1;
-    private static final int HOURS = 2;
-    private static final int SALARY = 3;
+    private static final int DATE_INDEX = 0;
+    private static final int EMPLOYEE_NAME_INDEX = 1;
+    private static final int HOURS_INDEX = 2;
+    private static final int SALARY_INDEX = 3;
     private static final int DATA_CHANGING = 1;
+    public static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern(FORMATTER);
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate fromDate = dateFormat(dateFrom);
         LocalDate toDate = dateFormat(dateTo);
-        StringBuilder builder = new StringBuilder();
-        builder.append(HEADER).append(dateFrom).append(DELIMITER).append(dateTo);
+        StringBuilder builder = new StringBuilder()
+                .append(HEADER).append(dateFrom).append(DELIMITER).append(dateTo);
         LocalDate thisDate;
         for (String name : names) {
             int sum = 0;
-            builder.append(System.getProperty("line.separator"))
+            builder.append(System.lineSeparator())
                     .append(name)
                     .append(DELIMITER);
-            for (String dat : data) {
-                if (dat.isEmpty()) {
+            for (String line : data) {
+                if (line.isEmpty()) {
                     continue;
                 } else {
-                    String[] array = dat.split(" ");
-                    thisDate = dateFormat(array[DATE]);
+                    String[] array = line.split(" ");
+                    thisDate = dateFormat(array[DATE_INDEX]);
                     if (thisDate.isAfter(fromDate.minusDays(DATA_CHANGING))
                             && thisDate.isBefore(toDate.plusDays(DATA_CHANGING))
-                            && name.equals(array[EMPLOYEE_NAME])) {
-                        sum += (Integer.parseInt(array[HOURS]) * Integer.parseInt(array[SALARY]));
+                            && name.equals(array[EMPLOYEE_NAME_INDEX])) {
+                        sum += (Integer.parseInt(array[HOURS_INDEX])
+                                * Integer.parseInt(array[SALARY_INDEX]));
                     }
                 }
             }
@@ -43,6 +45,6 @@ public class SalaryInfo {
     }
 
     public static LocalDate dateFormat(String thisDate) {
-        return LocalDate.parse(thisDate, DateTimeFormatter.ofPattern(FORMATTER));
+        return LocalDate.parse(thisDate, DATE_FORMATTER);
     }
 }
