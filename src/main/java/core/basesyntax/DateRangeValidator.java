21f7bc93f59e7574432c4d43c094e93228b62797
Vadym Stanishevskyi <mainvadym@gmail.com>
2023-09-14 21:59:25 +0200

    added privat fields in class DateRangeValidator

--- src/main/java/core/basesyntax/DateRangeValidator.java
+++ src/main/java/core/basesyntax/DateRangeValidator.java
@@ -4,20 +4,18 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class DateRangeValidator {
-    private final String startDate;
-    private final String endDate;
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private final LocalDate startDate;
+    private final LocalDate endDate;
 
     public DateRangeValidator(String startDate, String endDate) {
-        this.startDate = startDate;
-        this.endDate = endDate;
+        this.startDate = LocalDate.parse(startDate, FORMATTER);
+        this.endDate = LocalDate.parse(endDate, FORMATTER);
     }
 
     public boolean isWithinRange(String particularDay) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        LocalDate particularDate = LocalDate.parse(particularDay, formatter);
-        LocalDate startDay = LocalDate.parse(startDate, formatter);
-        LocalDate endDay = LocalDate.parse(endDate, formatter);
-        return particularDate.isAfter(startDay) && particularDate.isBefore(endDay)
-                || particularDate.isEqual(startDay) || particularDate.isEqual(endDay);
+        LocalDate particularDate = LocalDate.parse(particularDay, FORMATTER);
+        return particularDate.isAfter(startDate) && particularDate.isBefore(endDate)
+                || particularDate.isEqual(startDate) || particularDate.isEqual(endDate);
     }
 }
