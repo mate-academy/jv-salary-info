0f2405eb30b08b04bed680b1178e83de5c8f8034
Rostyslav Kuzmych <rostislavkuzmic401@gmail.com>
2023-09-15 12:26:39 +0300

    fixed the cod

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,44 +1,42 @@
 package core.basesyntax;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
+import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    private static final DateTimeFormatter DATE_TIME_FORMATTER =
+            DateTimeFormatter.ofPattern("dd.MM.yyyy");
     private static final int DATE_INDEX = 0;
     private static final int NAME_INDEX = 1;
     private static final int HOURS_WORKED_INDEX = 2;
     private static final int HOURLY_RATE_INDEX = 3;
-    private static final int NUMBER_OF_MONTH = 0;
-    private static final int MONTH = 1;
-    private static final int YEAR = 2;
+    private static final String DATA_PARTS_SEPARATOR = " ";
+    private static final String REPORT_DATE_SEPARATOR = " - ";
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder resultBuilder = new StringBuilder();
-        resultBuilder.append("Report for period ").append(dateFrom).append(" - ").append(dateTo).append(System.lineSeparator());
-        String [] arrayDateFrom = dateFrom.split("\\.");
-        String [] arrayDateTo = dateTo.split("\\.");
-        LocalDate localDateFrom = LocalDate.of(Integer.parseInt(arrayDateFrom[YEAR]),
-                Integer.parseInt(arrayDateFrom[MONTH]), Integer.parseInt(arrayDateFrom[NUMBER_OF_MONTH]));
-        LocalDate localDateTo = LocalDate.of(Integer.parseInt(arrayDateTo[YEAR]),
-                Integer.parseInt(arrayDateTo[MONTH]), Integer.parseInt(arrayDateTo[NUMBER_OF_MONTH]));
-        for (int i = 0; i < names.length; i++) {
+        resultBuilder.append("Report for period ").append(dateFrom).append(REPORT_DATE_SEPARATOR)
+                .append(dateTo).append(System.lineSeparator());
+        LocalDate localDateFrom = LocalDate.parse(dateFrom, DATE_TIME_FORMATTER);
+        LocalDate localDateTo = LocalDate.parse(dateTo, DATE_TIME_FORMATTER);
+        for (String nameOfPeople : names) {
             int moneyEarned = 0;
-            for (int j = 0;j < data.length;j++) {
-            String[] parts = data[j].split(" ");
-            String date = parts[DATE_INDEX];
-            String [] arrayInputDate = date.split("\\.");
-            LocalDate localDate = LocalDate.of(Integer.parseInt(arrayInputDate[YEAR]),
-                    Integer.parseInt(arrayInputDate[MONTH]),Integer.parseInt(arrayInputDate[NUMBER_OF_MONTH]));
-            String name = parts[NAME_INDEX];
-            String hours = parts[HOURS_WORKED_INDEX];
-            String incomePerHour = parts[HOURLY_RATE_INDEX];
-                if (name.equals(names[i]) && !localDate.isAfter(localDateTo) && !localDate.isBefore(localDateFrom)) {
-                moneyEarned += Integer.parseInt(hours) * Integer.parseInt(incomePerHour);
+            for (String dataFromArray : data) {
+                String[] parts = dataFromArray.split(DATA_PARTS_SEPARATOR);
+                String name = parts[NAME_INDEX];
+                if (name.equals(nameOfPeople)) {
+                    String date = parts[DATE_INDEX];
+                    LocalDate localDate = LocalDate.parse(date, DATE_TIME_FORMATTER);
+                    String hours = parts[HOURS_WORKED_INDEX];
+                    String incomePerHour = parts[HOURLY_RATE_INDEX];
+                    if (!localDate.isAfter(localDateTo) && !localDate.isBefore(localDateFrom)) {
+                        moneyEarned += Integer.parseInt(hours) * Integer.parseInt(incomePerHour);
+                    }
+                }
             }
+            resultBuilder.append(nameOfPeople).append(REPORT_DATE_SEPARATOR)
+                    .append(moneyEarned).append(System.lineSeparator());
         }
-            resultBuilder.append(names[i]).append(" - ").append(moneyEarned).append(System.lineSeparator());
-
-    }
         return resultBuilder.toString().trim();
-}
+    }
 }
