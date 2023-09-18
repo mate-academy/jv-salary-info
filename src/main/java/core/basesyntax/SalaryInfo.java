e7eaed1c81a225a7bd25c3d8dbe2432b963164ff
testSouth <southparaplan@gmail.com>
2023-09-18 16:14:19 +0300

    fixed mentor remarks

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,17 +4,22 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-
     public static final String SEPARATOR = " - ";
     public static final String SPACE_SEPARATOR = " ";
+    public static final int RECORD_DATE_FIRST_ELEMENT = 0;
+    public static final int EMPLOYEE_NAME_SECOND_ELEMENT = 1;
+    public static final int HOURS_WORKED_THIRD_ELEMENT = 2;
+    public static final int HOURLY_RATE_FORTH_ELEMENT = 3;
+    private static final DateTimeFormatter dateFormatter =
+            DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
         LocalDate fromDate = LocalDate.parse(dateFrom, dateFormatter);
         LocalDate toDate = LocalDate.parse(dateTo, dateFormatter);
 
-        StringBuilder report = new StringBuilder();
-        report.append("Report for period ").append(dateFrom)
+        StringBuilder report = new StringBuilder()
+                .append("Report for period ").append(dateFrom)
                 .append(SEPARATOR).append(dateTo).append(System.lineSeparator());
 
         for (String name : names) {
@@ -22,10 +27,10 @@ public class SalaryInfo {
             for (String record : data) {
                 String[] parts = record.split(SPACE_SEPARATOR);
                 if (parts.length == 4) {
-                    String recordDateStr = parts[0];
-                    String employeeName = parts[1];
-                    int hoursWorked = Integer.parseInt(parts[2]);
-                    int hourlyRate = Integer.parseInt(parts[3]);
+                    String recordDateStr = parts[RECORD_DATE_FIRST_ELEMENT];
+                    String employeeName = parts[EMPLOYEE_NAME_SECOND_ELEMENT];
+                    int hoursWorked = Integer.parseInt(parts[HOURS_WORKED_THIRD_ELEMENT]);
+                    int hourlyRate = Integer.parseInt(parts[HOURLY_RATE_FORTH_ELEMENT]);
 
                     if (employeeName.equals(name)) {
                         LocalDate recordDate = LocalDate.parse(recordDateStr, dateFormatter);
@@ -38,24 +43,8 @@ public class SalaryInfo {
             report.append(name).append(SEPARATOR).append(totalSalary)
                     .append(System.lineSeparator());
         }
-
         return report.toString().trim();
     }
 }
-// Data:
-//   "26.04.2019 John 4 50"
-//   05.04.2019 Andrew 3 200
-//   10.04.2019 John 7 100
-//   22.04.2019 Kate 9 100
-//   25.06.2019 John 11 50
-//   26.04.2019 Andrew 3 150
-//   13.02.2019 John 7 100
-//   26.04.2019 Kate 9 100
-// Names:
-//     John
-//     Andrew
-//     Kate
-//  Method execution result:
-//       Report for period 01.04.2019  - 30.04.2019
-//       John - 900
+
 
