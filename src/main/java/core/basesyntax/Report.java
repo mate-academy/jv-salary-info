636f9350e4c40cf8deb8f44770326d162900ebe9
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2023-09-22 21:07:25 +0300

    Create the "Report" class. Override toString method to create report in format that described in README instruction.

---
+++ src/main/java/core/basesyntax/Report.java
@@ -0,0 +1,42 @@
+package core.basesyntax;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Report {
+
+    private static final String NAME_SALARY_SEPARATOR = " - ";
+    private final String dataFrom;
+    private final String dataTo;
+    private Map<String, Integer> workerNameSalaryMap;
+
+    public Report(String dataFrom, String dataTo, Map<String, Integer> workerNameSalaryMap) {
+        this.dataFrom = dataFrom;
+        this.dataTo = dataTo;
+        this.workerNameSalaryMap = workerNameSalaryMap;
+    }
+
+    public Map<String, Integer> getWorkerNameSalaryMap() {
+        return workerNameSalaryMap;
+    }
+
+    public void setWorkerNameSalaryMap(HashMap<String, Integer> workerNameSalaryMap) {
+        this.workerNameSalaryMap = workerNameSalaryMap;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder reportBuilder = new StringBuilder();
+        reportBuilder.append("Report for period ").append(dataFrom).append(" - ")
+                .append(dataTo).append('\n');
+        int reportHeaderLength = reportBuilder.length();
+        for (String name : workerNameSalaryMap.keySet()) {
+            StringBuilder workerSalaryBuilder = new StringBuilder();
+            workerSalaryBuilder.append(name).append(NAME_SALARY_SEPARATOR)
+                    .append(workerNameSalaryMap.get(name))
+                    .append('\n');
+            reportBuilder.insert(reportHeaderLength, workerSalaryBuilder);
+        }
+        return reportBuilder.toString().trim();
+    }
+}
--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,47 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
+
 public class SalaryInfo {
+    private static final int INIT_SALARY_AMOUNT = 0;
+    private static final String DATA_SEPARATOR = " ";
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private Report report;
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        Map<String, Integer> nameSalaryMap = fillMapWithKeys(names);
+        LocalDate dateFromBorder = stringDateIntoLocalDate(dateFrom).minusDays(1);
+        LocalDate dateToBorder = stringDateIntoLocalDate(dateTo).plusDays(1);
+        for (String line : data) {
+            String[] info = line.split(DATA_SEPARATOR);
+            LocalDate workDate = stringDateIntoLocalDate(info[0]);
+            if (workDate.isAfter(dateFromBorder) && workDate.isBefore(dateToBorder)) {
+                String workerName = info[1];
+                if (nameSalaryMap.containsKey(workerName)) {
+                    int workingHours = Integer.parseInt(info[2]);
+                    int payForHour = Integer.parseInt(info[3]);
+                    int currentSalary = nameSalaryMap.get(workerName);
+                    nameSalaryMap.put(workerName,currentSalary + workingHours * payForHour);
+                }
+            }
+        }
+        report = new Report(dateFrom, dateTo, nameSalaryMap);
+        return report.toString();
+    }
+
+    private Map<String, Integer> fillMapWithKeys(String[] names) {
+        Map<String, Integer> nameSalaryMap = new HashMap<String, Integer>();
+        for (String name : names) {
+            nameSalaryMap.put(name, INIT_SALARY_AMOUNT);
+        }
+        return nameSalaryMap;
+    }
+
+    private LocalDate stringDateIntoLocalDate(String date) {
+        return LocalDate.parse(date, FORMATTER);
     }
 }
+
