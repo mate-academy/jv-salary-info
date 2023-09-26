416b1b186388d3ea043d8a23dfb3ae4fcdb021f1
Roman Kuzyk <kuzya228337@mail.com>
2023-09-26 16:56:05 +0300

    Here's what has been done in this code:
    Imports:
    The java.text.ParseException and java.text.SimpleDateFormat classes are imported for working with dates and formatting date strings.
    Private field dateFormat:
    A private field dateFormat of type SimpleDateFormat has been created for date formatting. This object will be used to parse string dates into Date objects and vice versa. The date format is set to "dd.MM.yyyy".
    Method getSalaryInfo:
    This method takes the following parameters:
    An array of strings names, representing employee names.
    An array of strings entries, containing records of employee work.
    Strings fromDate and toDate, representing the start and end dates of the period for which salary needs to be calculated.
    Date Processing:
    In the try-catch block, string dates (fromDate and toDate) are processed using SimpleDateFormat. If the dates have an incorrect format, a ParseException exception will be thrown.
    Processing Work Entries:
    Next, the method iterates through each entry in the entries array, splitting it into separate parts (date, name, hours worked, and hourly rate).
    The date of the entry is converted into a Date object, and the other values are converted to their respective data types.
    Salary Calculation:
    Then, the method checks whether the entry date falls within the specified time period (between from and to) or is on the boundaries of this period. If the condition is met, the employee's salary is updated based on the hours worked and hourly rate.
    Report Generation:
    A report is then assembled as a string, which contains information about the period and the salary for each employee.
    Information is added to a StringBuilder object.
    Returning the Result:
    The report, formatted as a string, is returned from the method, with leading and trailing spaces removed using the trim() method.
    Exception Handling:
    If the input data has an incorrect date format, an IllegalArgumentException is thrown with the message "Invalid date format in input data," and the exception is propagated as a result of the parsing error (ParseException).

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,53 @@
 package core.basesyntax;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class SalaryInfo {
-    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+
+    public String getSalaryInfo(String[] names, String[] data, String fromDate, String toDate) {
+        try {
+            Date from = dateFormat.parse(fromDate);
+            Date to = dateFormat.parse(toDate);
+            String[] salaries = new String[names.length];
+
+            for (String entry : data) {
+                String[] parts = entry.split(" ");
+                Date entryDate = dateFormat.parse(parts[0]);
+                String name = parts[1];
+                int hoursWorked = Integer.parseInt(parts[2]);
+                int hourlyRate = Integer.parseInt(parts[3]);
+
+                if (entryDate.after(from) && entryDate.before(to)
+                        || entryDate.equals(from)
+                        || entryDate.equals(to)) {
+                    for (int i = 0; i < names.length; i++) {
+                        if (names[i].equals(name)) {
+                            int salary = Integer.parseInt(salaries[i] == null ? "0" : salaries[i]);
+                            salary += hoursWorked * hourlyRate;
+                            salaries[i] = String.valueOf(salary);
+                            break;
+                        }
+                    }
+                }
+            }
+
+            StringBuilder report = new StringBuilder("Report for period "
+                    + fromDate + " - "
+                    + toDate
+                    + System.lineSeparator());
+            for (int i = 0; i < names.length; i++) {
+                report.append(names[i])
+                        .append(" - ")
+                        .append(salaries[i] == null ? "0" : salaries[i])
+                        .append(System.lineSeparator());
+            }
+
+            return report.toString().trim();
+        } catch (ParseException e) {
+            throw new IllegalArgumentException("Invalid date format in input data", e);
+        }
     }
 }
