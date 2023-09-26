8f2e5ae1c35bd4e09af68e3e4da57b1376275067
Valerii Fedorovych <valera.fedorovich.2000@gmail.com>
2023-09-26 16:58:22 +0300

    fixed all mistakes

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,49 +1,35 @@
 package core.basesyntax;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
-public class SalaryInfo extends CheckNotNull {
+public class SalaryInfo {
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        checkNotNull(names, data, dateFrom, dateTo);
-        String[] fullInformation = new String[names.length];
-        StringBuilder builder;
-        StringBuilder finalBuilder = new StringBuilder();
-        SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy");
-        short builderCounter = 0;
+        StringBuilder builder = new StringBuilder();
+        builder.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
+        LocalDate parsedDateFrom = LocalDate.parse(dateFrom, formatter);
+        LocalDate parsedDateTo = LocalDate.parse(dateTo, formatter);
         for (String name : names) {
             int sum = 0;
-            builder = new StringBuilder();
             for (String note : data) {
-                if (!note.isEmpty()) {
-                    try {
-                        String[] dataSeparated = note.split(" ");
-                        Date parsedNoteDate = format.parse(dataSeparated[0]);
-                        Date parsedDateFrom = format.parse(dateFrom);
-                        Date parsedDateTo = format.parse(dateTo);
-                        if ((parsedNoteDate.after(parsedDateFrom)
-                                && parsedNoteDate.before(parsedDateTo))
-                                || (parsedNoteDate.equals(parsedDateFrom)
-                                || parsedNoteDate.equals((parsedDateTo)))) {
-                            if (dataSeparated[1].equals(name)) {
-                                sum += Integer.parseInt(dataSeparated[2])
-                                        * Integer.parseInt(dataSeparated[3]);
-                            }
+                try {
+                    String[] dataSeparated = note.split(" ");
+                    LocalDate parsedNoteDate = LocalDate.parse(dataSeparated[0], formatter);
+                    if (!parsedNoteDate.isBefore(parsedDateFrom)
+                            && !parsedNoteDate.isAfter(parsedDateTo)) {
+                        if (dataSeparated[1].equals(name)) {
+                            sum += Integer.parseInt(dataSeparated[2])
+                                    * Integer.parseInt(dataSeparated[3]);
                         }
-                    } catch (ParseException e) {
-                        System.out.println("Cannot Parse Date: ParseException");
                     }
+                } catch (EmptyStringException e) {
+                    System.out.println("Data in array cannot be empty.");
                 }
             }
-            fullInformation[builderCounter] = builder.append(name).append(" - ")
-                    .append(sum).toString();
-            builderCounter++;
+            builder.append(System.lineSeparator()).append(name).append(" - ").append(sum);
         }
-        finalBuilder.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
-        for (String employee : fullInformation) {
-            finalBuilder.append(System.lineSeparator()).append(employee);
-        }
-        return finalBuilder.toString();
+        return builder.toString();
     }
 }
