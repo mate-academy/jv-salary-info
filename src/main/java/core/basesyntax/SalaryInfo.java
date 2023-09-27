c8388125b460b70f17a9e4baac8277946274e6df
HovorukhaBohdan <hovorukha.bohdan@gmail.com>
2023-09-27 17:50:05 +0300

    Added logic to 'getSalaryInfo' method

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,42 +1,36 @@
 package core.basesyntax;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
+        String paragraph = System.lineSeparator();
         StringBuilder builder = new StringBuilder("Report for period ")
-                                        .append(dateFrom).append(" - ")
-                                        .append(dateTo);
-        dateFrom = dateFrom.replace('.', '/');
-        dateTo = dateTo.replace('.', '/');
+                .append(dateFrom).append(" - ")
+                .append(dateTo);
 
-        try {
-            Date startDate = new SimpleDateFormat("dd/MM/yyyy").parse(dateFrom);
-            Date endDate = new SimpleDateFormat("dd/MM/yyyy").parse(dateTo);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d.MM.yyyy");
+        LocalDate startDate = LocalDate.parse(dateFrom, formatter);
+        LocalDate endDate = LocalDate.parse(dateTo, formatter);
 
-            for (String name: names) {
-                int totalSalary = 0;
+        for (String name: names) {
+            int totalSalary = 0;
 
-                for (int i = 0; i < data.length; i++) {
-                    String[] employeeInfo = data[i].split(" ");
-                    Date workDay = new SimpleDateFormat("dd/MM/yyyy")
-                            .parse(employeeInfo[0].replace('.', '/'));
+            for (String employeeData: data) {
+                String[] employeeInfo = employeeData.split(" ");
+                LocalDate workDay = LocalDate.parse(employeeInfo[0], formatter);
 
-                    if (name.equals(employeeInfo[1])
-                            && workDay.compareTo(startDate) > 0
-                            && (workDay.compareTo(endDate) == 0
-                            || workDay.compareTo(endDate) < 0)) {
-                        totalSalary += Integer.parseInt(employeeInfo[2])
-                                * Integer.parseInt(employeeInfo[3]);
-                    }
+                if (name.equals(employeeInfo[1])
+                        && workDay.isAfter(startDate)
+                        && (workDay.isEqual(endDate)
+                        || workDay.isBefore(endDate))) {
+                    totalSalary += Integer.parseInt(employeeInfo[2])
+                            * Integer.parseInt(employeeInfo[3]);
                 }
-
-                builder.append("\n").append(name).append(" - ").append(totalSalary);
             }
-        } catch (ParseException e) {
-            System.out.println("Exception: " + e);
+
+            builder.append(paragraph).append(name).append(" - ").append(totalSalary);
         }
 
         return builder.toString();
