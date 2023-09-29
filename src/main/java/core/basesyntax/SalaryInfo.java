1093ffc0d0383996825f691a8494873ed2175a24
Vladyslav Druhov <space.drugov@gmail.com>
2023-09-29 14:44:18 +0300

    Remove try-catch blocks and make indexes to constants

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -8,50 +8,47 @@ public class SalaryInfo {
     private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter
             .ofPattern("dd.MM.yyyy", Locale.ENGLISH);
 
+    private static final int ENTRY_DATE_INDEX = 0;
+    private static final int ENTRY_NAME_INDEX = 1;
+    private static final int HOURS_INDEX = 2;
+    private static final int HOURLY_RATE_INDEX = 3;
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder result = new StringBuilder();
 
-        try {
-            LocalDate fromDate = LocalDate.parse(dateFrom, DATE_FORMATTER);
-            LocalDate toDate = LocalDate.parse(dateTo, DATE_FORMATTER);
+        LocalDate fromDate = LocalDate.parse(dateFrom, DATE_FORMATTER);
+        LocalDate toDate = LocalDate.parse(dateTo, DATE_FORMATTER);
 
-            result.append("Report for period ")
-                    .append(dateFrom)
-                    .append(" - ")
-                    .append(dateTo)
-                    .append(System.lineSeparator());
+        result.append("Report for period ")
+                .append(dateFrom)
+                .append(" - ")
+                .append(dateTo)
+                .append(System.lineSeparator());
 
-            for (String name : names) {
-                int totalSalary = 0;
+        for (String name : names) {
+            int totalSalary = 0;
 
-                for (String entry : data) {
-                    String[] entryParts = entry.split(" ");
-                    String entryDateStr = entryParts[0];
-                    String entryName = entryParts[1];
+            for (String entry : data) {
+                String[] entryParts = entry.split(" ");
+                String entryDateStr = entryParts[ENTRY_DATE_INDEX];
+                String entryName = entryParts[ENTRY_NAME_INDEX];
 
-                    try {
-                        int hours = Integer.parseInt(entryParts[2]);
-                        int hourlyRate = Integer.parseInt(entryParts[3]);
-                        LocalDate entryDate = LocalDate.parse(entryDateStr, DATE_FORMATTER);
+                int hours = Integer.parseInt(entryParts[HOURS_INDEX]);
+                int hourlyRate = Integer.parseInt(entryParts[HOURLY_RATE_INDEX]);
+                LocalDate entryDate = LocalDate.parse(entryDateStr, DATE_FORMATTER);
 
-                        if (entryName.equals(name) && !entryDate.isBefore(fromDate)
-                                && !entryDate.isAfter(toDate)) {
-                            totalSalary += hours * hourlyRate;
-                        }
-                    } catch (NumberFormatException e) {
-                        throw new NumberFormatException("Invalid number format");
-                    }
-                }
-
-                result.append(name)
-                        .append(" - ")
-                        .append(totalSalary);
-                if (!name.equals(names[names.length - 1])) {
-                    result.append(System.lineSeparator());
+                if (entryName.equals(name) && !entryDate.isBefore(fromDate)
+                        && !entryDate.isAfter(toDate)) {
+                    totalSalary += hours * hourlyRate;
                 }
             }
-        } catch (Exception e) {
-            throw new RuntimeException("Error occurred while calculating salaries");
+
+            result.append(name)
+                    .append(" - ")
+                    .append(totalSalary);
+            if (!name.equals(names[names.length - 1])) {
+                result.append(System.lineSeparator());
+            }
         }
 
         return result.toString();
