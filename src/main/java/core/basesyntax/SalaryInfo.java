bfbf2987532356d81138ae286d91c9644de0f7d2
Nataliia <gzodchie@gmail.com>
2023-09-29 11:57:06 +0300

    fixed jv-salary-info solution

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,7 +5,7 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
 public class SalaryInfo {
-    private final DateTimeFormatter formatter =
+    private static final DateTimeFormatter formatter =
             DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
@@ -14,18 +14,19 @@ public class SalaryInfo {
             LocalDate dateStart = LocalDate.parse(dateFrom, formatter);
             LocalDate dateFinish = LocalDate.parse(dateTo, formatter);
             int salary = 0;
-            builder.append("Report for period " + dateFrom + " - " + dateTo);
+            builder.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
 
             for (String name : names) {
                 for (String str : data) {
                     String[] values = str.split(" ");
                     LocalDate dateWork = LocalDate.parse(values[0], formatter);
-                    if (dateWork.compareTo(dateStart) >= 0 && dateWork.compareTo(dateFinish) <= 0
+                    if (dateWork.isAfter(dateStart.minusDays(1))
+                            && dateWork.isBefore(dateFinish.plusDays(1))
                             && name.equals(values[1])) {
                         salary += Integer.parseInt(values[2]) * Integer.parseInt(values[3]);
                     }
                 }
-                builder.append(System.lineSeparator() + name + " - " + salary);
+                builder.append(System.lineSeparator()).append(name).append(" - ").append(salary);
                 salary = 0;
             }
         } catch (DateTimeParseException exc) {
