4fbee5cce0057dacd304d785f1d14e257bdec293
Mateusz Rostek <17rostek17@gmail.com>
2023-10-01 13:11:29 +0200

    (WIP) Added 3 local dates, StringBuilder, String separator, constant date time formatter and "Report for period #date_1# - #date_2#" part of task

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,21 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        StringBuilder builder = new StringBuilder("Report for period ");
+        String separator = " - ";
+        LocalDate formattedDateFrom = LocalDate.parse(dateFrom, DATE_TIME_FORMATTER);
+        LocalDate formattedDateTo = LocalDate.parse(dateTo, DATE_TIME_FORMATTER);
+        LocalDate particularDay = null;
+
+        builder.append(dateFrom).append(separator).append(dateTo).append(System.lineSeparator());
+
+
+        System.out.println(builder);
+        return "hello";
     }
 }
