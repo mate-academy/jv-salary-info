ce1b4b8afd390bd00d74f6b8a51f07d63ab5f922
Mateusz Rostek <17rostek17@gmail.com>
2023-10-01 19:35:30 +0200

    Improved readability, added 2 methods and made more constants

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,33 +4,46 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final DateTimeFormatter DATE_TIME_FORMATTER =
+            DateTimeFormatter.ofPattern("dd.MM.yyyy");
     private static final int INDEX_OF_WORKING_HOURS = 2;
     private static final int INDEX_OF_INCOME_PER_HOUR = 3;
+    private static final String SEPARATOR = " - ";
+    private static final String REPORT_BEGINNING = "Report for period ";
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        StringBuilder builder = new StringBuilder("Report for period ");
-        String separator = " - ";
+        StringBuilder builder = new StringBuilder(REPORT_BEGINNING);
         LocalDate formattedDateFrom = LocalDate.parse(dateFrom, DATE_TIME_FORMATTER);
         LocalDate formattedDateTo = LocalDate.parse(dateTo, DATE_TIME_FORMATTER);
-        LocalDate particularDay = null;
+        LocalDate particularDay;
         int employeeMoneyEarned;
 
-        builder.append(dateFrom).append(separator).append(dateTo).append(System.lineSeparator());
+        builder.append(dateFrom).append(SEPARATOR).append(dateTo).append(System.lineSeparator());
         for (String name : names) {
             employeeMoneyEarned = 0;
-            builder.append(name).append(separator);
+            builder.append(name).append(SEPARATOR);
             for (String currentData : data) {
-                particularDay = LocalDate.parse(currentData.substring(0, currentData.indexOf(" ")), DATE_TIME_FORMATTER);
-                if (((particularDay.isAfter(formattedDateFrom) || particularDay.isEqual(formattedDateFrom))
-                        && (particularDay.isBefore(formattedDateTo) || particularDay.isEqual(formattedDateTo))) && currentData.contains(name)) {
+                particularDay = LocalDate.parse(
+                        currentData.substring(0, currentData.indexOf(" ")), DATE_TIME_FORMATTER);
+                if (isDateInRange(particularDay, formattedDateFrom, formattedDateTo)
+                        && currentData.contains(name)) {
                     String[] segregatedData = currentData.split(" ");
-                    employeeMoneyEarned += Integer.parseInt(segregatedData[INDEX_OF_WORKING_HOURS])
-                            * Integer.parseInt(segregatedData[INDEX_OF_INCOME_PER_HOUR]);
+                    employeeMoneyEarned += calculateDaySalary(segregatedData);
                 }
             }
             builder.append(employeeMoneyEarned).append(System.lineSeparator());
         }
-        return builder.substring(0, builder.length() - separator.length()+ 1).toString();
+        int trimmedReportLength = builder.length() - SEPARATOR.length() + 1;
+        return builder.substring(0, trimmedReportLength);
+    }
+
+    private boolean isDateInRange(LocalDate particularDay, LocalDate dateFrom, LocalDate dateTo) {
+        return (particularDay.isAfter(dateFrom) && (particularDay.isBefore(dateTo))
+                || particularDay.isEqual(dateFrom)) || particularDay.isEqual(dateTo);
+    }
+
+    private int calculateDaySalary(String[] segregatedData) {
+        return Integer.parseInt(segregatedData[INDEX_OF_WORKING_HOURS])
+                * Integer.parseInt(segregatedData[INDEX_OF_INCOME_PER_HOUR]);
     }
 }
