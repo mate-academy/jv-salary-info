4efecccabc0106c2185f9c54f4370d42a3f84108
Vlasenko Yevhenii <vlasenkoevy@gmail.com>
2023-10-03 13:38:38 +0300

    deleted SalaryInfo instances, renamed getLocalDate to parseDate

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -6,20 +6,19 @@ import java.time.format.DateTimeFormatter;
 public class SalaryInfo {
     private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
-    private LocalDate getLocalDate(String date) {
+    private LocalDate parseDate(String date) {
         return LocalDate.parse(date, formatter);
     }
 
     private boolean isValidDate(LocalDate startDate, LocalDate endDate, String checkedDate) {
-        LocalDate localDate = getLocalDate(checkedDate);
+        LocalDate localDate = parseDate(checkedDate);
         return localDate.isAfter(startDate.minusDays(1))
             && localDate.isBefore(endDate.plusDays(1));
     }
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        SalaryInfo salaryInfo = new SalaryInfo();
-        LocalDate startLocalDate = salaryInfo.getLocalDate(dateFrom);
-        LocalDate endLocalDate = salaryInfo.getLocalDate(dateTo);
+        LocalDate startLocalDate = parseDate(dateFrom);
+        LocalDate endLocalDate = parseDate(dateTo);
         int[] totalSalaries = new int[names.length];
 
         for (String datum : data) {
@@ -29,14 +28,14 @@ public class SalaryInfo {
             int workingHours = Integer.parseInt(splittedData[2]);
             int hourSalary = Integer.parseInt(splittedData[3]);
             for (int i = 0; i < names.length; i++) {
-                if (names[i].equals(name) && salaryInfo.isValidDate(startLocalDate,
+                if (names[i].equals(name) && isValidDate(startLocalDate,
                         endLocalDate, date)) {
                     totalSalaries[i] += workingHours * hourSalary;
                 }
             }
         }
-        StringBuilder stringBuilder = new StringBuilder(
-                "Report for period " + dateFrom + " - " + dateTo);
+        StringBuilder stringBuilder = new StringBuilder("Report for period "
+                + dateFrom + " - " + dateTo);
         for (int i = 0; i < names.length; i++) {
             stringBuilder.append(System.lineSeparator());
             stringBuilder.append(names[i]).append(" - ").append(totalSalaries[i]);
