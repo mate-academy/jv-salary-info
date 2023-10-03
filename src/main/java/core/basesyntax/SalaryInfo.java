eed7ef55c6349b77c16c1833be6b7532f460925f
Hlib Kulikov <forzen123212@proton.me>
2023-10-03 15:22:58 +0300

    Fixed codeStyle and deleted string concat

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -8,29 +8,32 @@ public class SalaryInfo {
     private static final int NAME_INDEX = 1;
     private static final int HOURS_INDEX = 2;
     private static final int PRICE_INDEX = 3;
-    private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final DateTimeFormatter DATE_FORMATTER =
+            DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         final int[] totalSalary = new int[names.length];
-        StringBuilder report = new StringBuilder();
-        LocalDate fromDate = LocalDate.parse(dateFrom, DATE_FORMAT);
-        LocalDate toDate = LocalDate.parse(dateTo, DATE_FORMAT);
+        StringBuilder reportSalaryInfo = new StringBuilder();
+        StringBuilder fullReport = new StringBuilder();
+        LocalDate fromDate = LocalDate.parse(dateFrom, DATE_FORMATTER);
+        LocalDate toDate = LocalDate.parse(dateTo, DATE_FORMATTER);
 
         for (int i = 0; i < names.length; i++) {
             for (String dateInfo : data) {
                 String[] salaryData = dateInfo.split(" ");
-                LocalDate date = LocalDate.parse(salaryData[DATE_INDEX], DATE_FORMAT);
+                LocalDate date = LocalDate.parse(salaryData[DATE_INDEX], DATE_FORMATTER);
                 if (date.isAfter(fromDate) && date.isBefore(toDate)
                         || date.equals(fromDate) || date.equals(toDate)) {
-                    int salaryPerDay = Integer.parseInt(salaryData[HOURS_INDEX])
-                                * Integer.parseInt(salaryData[PRICE_INDEX]);
                     if (names[i].equals(salaryData[NAME_INDEX])) {
+                        int salaryPerDay = Integer.parseInt(salaryData[HOURS_INDEX])
+                                * Integer.parseInt(salaryData[PRICE_INDEX]);
                         totalSalary[i] += salaryPerDay;
                     }
                 }
             }
-            report.append("\n").append(names[i]).append(" - ").append(totalSalary[i]);
+            reportSalaryInfo.append("\n").append(names[i]).append(" - ").append(totalSalary[i]);
         }
-        return "Report for period " + dateFrom + " - " + dateTo + report;
+        return fullReport.append("Report for period ").append(dateFrom).append(" - ")
+                .append(dateTo).append(reportSalaryInfo).toString();
     }
 }
