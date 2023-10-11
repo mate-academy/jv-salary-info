aab2ee486169542ae11d51f0880eb5b2ff2fc377
Kozharin-Vasul <kozharinvasyl@gmail.com>
2023-10-11 15:54:02 +0300

    change some levels

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,15 +1,14 @@
 package core.basesyntax;
 
+import static java.lang.Integer.parseInt;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-
-import static java.lang.Integer.parseInt;
 
 public class SalaryInfo {
     public static final DateTimeFormatter DDMMYYYY = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public static boolean between(LocalDate d, LocalDate from, LocalDate to) {
-        return (d.isAfter(from) || d.isEqual(from)) && (d.isBefore(to)|| d.isEqual(to));
+        return (d.isAfter(from) || d.isEqual(from)) && (d.isBefore(to) || d.isEqual(to));
     }
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
@@ -19,7 +18,7 @@ public class SalaryInfo {
         for (int i = 0; i < data.length; i++) {
             String[] pices = data[i].split(" ");
             LocalDate now = LocalDate.parse(pices[0], DDMMYYYY);
-             if ((between(now, from, to))) {
+            if ((between(now, from, to))) {
                 for (int j = 0; j < names.length; j++) {
                     if (pices[1].equals(names[j])) {
                         salary[j] += parseInt(pices[2]) * parseInt(pices[3]);
