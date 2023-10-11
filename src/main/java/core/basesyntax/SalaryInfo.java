b9c3a3f6762f9a01b9687640b015a96f9d9c8c89
Yevhenii Liashenko <paradoxrial@gmail.com>
2023-10-11 14:07:12 +0300

    Move all hardcoded values to constant fields

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -3,22 +3,27 @@ package core.basesyntax;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
+import java.util.Map;
 
 public class SalaryInfo {
-    static final String DATE_FORMAT = "dd.MM.yyyy";
+    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int HOURS_INDEX = 2;
+    private static final int INCOME_INDEX = 3;
+
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_FORMAT);
-        LocalDate fromDate = LocalDate.parse(dateFrom, formatter);
-        LocalDate toDate = LocalDate.parse(dateTo, formatter);
-        HashMap<String, Integer> earnings = new HashMap<>();
+        LocalDate fromDate = LocalDate.parse(dateFrom, DATE_FORMATTER);
+        LocalDate toDate = LocalDate.parse(dateTo, DATE_FORMATTER);
+        Map<String, Integer> earnings = new HashMap<>();
         for (String entry : data) {
             String[] parts = entry.split(" ");
-            String dateString = parts[0];
-            String name = parts[1];
-            int hours = Integer.parseInt(parts[2]);
-            int incomePerHour = Integer.parseInt(parts[3]);
-            LocalDate entryDate = LocalDate.parse(dateString, formatter);
+            String dateString = parts[DATE_INDEX];
+            String name = parts[NAME_INDEX];
+            int hours = Integer.parseInt(parts[HOURS_INDEX]);
+            int incomePerHour = Integer.parseInt(parts[INCOME_INDEX]);
+            LocalDate entryDate = LocalDate.parse(dateString, DATE_FORMATTER);
             if (entryDate.compareTo(fromDate) >= 0 && entryDate.compareTo(toDate) <= 0) {
                 int totalEarnings = earnings.getOrDefault(name, 0);
                 totalEarnings += hours * incomePerHour;
