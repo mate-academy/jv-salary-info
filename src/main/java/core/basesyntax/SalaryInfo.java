812e617eede8b666b0e62a902008e54f66a1e281
Leonid Bondarenko <leonidbondarenko69@gmail.com>
2023-10-15 22:36:18 +0300

    Done

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,22 +5,30 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class SalaryInfo {
+    private static final String HEADER = "Report for period ";
+    private static final String SEPARATOR = " - ";
+    private static final String SELIMITER = " ";
+    private static final int WORK_HOURS_INDEX_PARTS = 2;
+    private static final int HOURLY_RATE_INDEX_PARTS = 3;
+    private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd.MM.yyyy");
 
     public static String getSalaryInfo(String[] names, String[] data,
                                        String dateFrom, String dateTo) {
-        StringBuilder report = new StringBuilder("Report for period ")
+        StringBuilder report = new StringBuilder(HEADER)
                 .append(dateFrom)
-                .append(" - ")
+                .append(SEPARATOR)
                 .append(dateTo)
                 .append(System.lineSeparator());
         for (String name : names) {
             int totalEarnings = 0;
             for (String record : data) {
-                String[] parts = record.split(" ");
+                String[] parts = record.split(SELIMITER);
                 String employeeName = parts[1];
                 if (employeeName.equals(name)) {
                     if (isDateInRange(parts[0], dateFrom, dateTo)) {
-                        totalEarnings += Integer.parseInt(parts[2]) * Integer.parseInt(parts[3]);
+                        int workHours = Integer.parseInt(parts[WORK_HOURS_INDEX_PARTS]);
+                        int hourlyRate = Integer.parseInt(parts[HOURLY_RATE_INDEX_PARTS]);
+                        totalEarnings += workHours * hourlyRate;
                     }
                 }
             }
@@ -33,18 +41,13 @@ public class SalaryInfo {
     }
 
     private static boolean isDateInRange(String dateStr, String dateFrom, String dateTo) {
-        SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
         try {
-            Date date1 = sdf.parse(dateStr);
-            Date date2 = sdf.parse(dateFrom);
-            Date date3 = sdf.parse(dateTo);
-            if (date1.equals(date2) || date1.equals(date3)
-                    || (date1.after(date2) && date1.before(date3))) {
-                return true;
-            } else {
-                return false;
-            }
+            Date date = DATE_FORMAT.parse(dateStr);
+            Date fromDate = DATE_FORMAT.parse(dateFrom);
+            Date toDate = DATE_FORMAT.parse(dateTo);
+            return !date.before(fromDate) && !date.after(toDate);
         } catch (ParseException e) {
+            e.printStackTrace();
             return false;
         }
     }
