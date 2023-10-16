6353255534b2efac12581b275f6524f1df751fa8
Leonid Bondarenko <leonidbondarenko69@gmail.com>
2023-10-16 19:48:38 +0300

    Done 16/10/23

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,24 +1,23 @@
 package core.basesyntax;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 public class SalaryInfo {
+    private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("dd.MM.yyyy");
     private static final String HEADER = "Report for period ";
     private static final String SEPARATOR = " - ";
     private static final String SELIMITER = " ";
     private static final int WORK_HOURS_INDEX_PARTS = 2;
     private static final int HOURLY_RATE_INDEX_PARTS = 3;
-    private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd.MM.yyyy");
 
     public static String getSalaryInfo(String[] names, String[] data,
                                        String dateFrom, String dateTo) {
         StringBuilder report = new StringBuilder(HEADER)
                 .append(dateFrom)
                 .append(SEPARATOR)
-                .append(dateTo)
-                .append(System.lineSeparator());
+                .append(dateTo);
         for (String name : names) {
             int totalEarnings = 0;
             for (String record : data) {
@@ -32,21 +31,18 @@ public class SalaryInfo {
                     }
                 }
             }
-            report.append(name).append(" - ").append(totalEarnings);
-            if (!names[names.length - 1].equals(name)) {
-                report.append(System.lineSeparator());
-            }
+            report.append(System.lineSeparator()).append(name).append(" - ").append(totalEarnings);
         }
         return report.toString();
     }
 
-    private static boolean isDateInRange(String dateStr, String dateFrom, String dateTo) {
+    public static boolean isDateInRange(String dateStr, String dateFrom, String dateTo) {
         try {
-            Date date = DATE_FORMAT.parse(dateStr);
-            Date fromDate = DATE_FORMAT.parse(dateFrom);
-            Date toDate = DATE_FORMAT.parse(dateTo);
-            return !date.before(fromDate) && !date.after(toDate);
-        } catch (ParseException e) {
+            LocalDate date = LocalDate.parse(dateStr, DATE_FORMAT);
+            LocalDate fromDate = LocalDate.parse(dateFrom, DATE_FORMAT);
+            LocalDate toDate = LocalDate.parse(dateTo, DATE_FORMAT);
+            return !date.isBefore(fromDate) && !date.isAfter(toDate);
+        } catch (DateTimeParseException e) {
             e.printStackTrace();
             return false;
         }
