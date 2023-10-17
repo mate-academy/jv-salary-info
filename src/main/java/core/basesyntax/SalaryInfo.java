216362c48044c2163e595021eecac01a16609cff
Anton Babych <antonbabi13@gmail.com>
2023-10-17 19:04:02 +0300

    develop a function of SalaryInfo class. Develop function getSalaryInfo and companions with her calculateSakaryForPerson and isDateInRange

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,42 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+    public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        StringBuilder stringBuilder = new StringBuilder("Report for period ");
+        stringBuilder.append(dateFrom).append(" - ")
+                .append(dateTo).append(System.lineSeparator());
+        LocalDate startDate = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate endDate = LocalDate.parse(dateTo, FORMATTER);
+        for (int i = 0; i < names.length; i++) {
+            int salary = calculateSalaryForPerson(names[i], data, startDate, endDate);
+            stringBuilder.append(names[i]).append(" - ").append(salary);
+            if (i != names.length - 1) {
+                stringBuilder.append(System.lineSeparator());
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+    private int calculateSalaryForPerson(String name, String[] data,
+                                         LocalDate startDate, LocalDate endDate) {
+        int salary = 0;
+        for (String entry : data) {
+            String[] allData = entry.split(" ");
+            LocalDate dateData = LocalDate.parse(allData[0], FORMATTER);
+            if (name.equals(allData[1]) && isDateInRange(dateData, startDate, endDate)) {
+                salary += Integer.parseInt(allData[2]) * Integer.parseInt(allData[3]);
+            }
+        }
+        return salary;
+    }
+
+    private boolean isDateInRange(LocalDate date, LocalDate startDate, LocalDate endDate) {
+        return (date.isAfter(startDate) || date.isEqual(startDate))
+                && (date.isBefore(endDate) || date.isEqual(endDate));
     }
 }
