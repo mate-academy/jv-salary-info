520ae3587738e2ece0a55b656776c31a1918a9c5
Andriy-Hrynevetskyi <andrijgrineveckij@gmail.com>
2023-10-17 20:16:30 +0300

    minor changes after the code review

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -7,15 +7,19 @@ import java.time.format.DateTimeParseException;
 public class SalaryInfo {
     public static final DateTimeFormatter DATE_FORMATTER
             = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    public static final String DATA_SEPARATOR = " - ";
+    public static final String STRING_SPLIT_VALUE = " ";
+    public static final String START_STRING = "Report for period ";
+    public static final String ERROR_MSG = "%s is not parsable!";
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        StringBuilder builder = new StringBuilder("Report for period ").append(dateFrom)
-                .append(" - ").append(dateTo).append(System.lineSeparator());
+        StringBuilder builder = new StringBuilder(START_STRING).append(dateFrom)
+                .append(DATA_SEPARATOR).append(dateTo).append(System.lineSeparator());
         for (String name : names) {
             int salary = 0;
-            builder.append(name).append(" - ");
+            builder.append(name).append(DATA_SEPARATOR);
             for (String dataUnit : data) {
-                String[] dataUnitToArray = dataUnit.split(" ");
+                String[] dataUnitToArray = dataUnit.split(STRING_SPLIT_VALUE);
                 if (dataUnit.contains(name)
                         && isValidDate(dataUnitToArray[0], dateFrom, dateTo)) {
                     salary += Integer.parseInt(dataUnitToArray[2])
@@ -32,12 +36,12 @@ public class SalaryInfo {
             LocalDate currentDate = LocalDate.parse(date, DATE_FORMATTER);
             LocalDate dateStart = LocalDate.parse(dateFrom, DATE_FORMATTER);
             LocalDate dateEnd = LocalDate.parse(dateTo, DATE_FORMATTER);
-            return (currentDate.compareTo(dateStart) >= 0)
-                    && (currentDate.compareTo(dateEnd) <= 0);
+            return (!currentDate.isBefore(dateStart))
+                    && (!currentDate.isAfter(dateEnd));
         } catch (DateTimeParseException exc) {
-            System.out.printf("%s is not parsable!%n", date);
-            System.out.printf("%s is not parsable!%n", dateFrom);
-            System.out.printf("%s is not parsable!%n", dateTo);
+            System.out.printf(ERROR_MSG + System.lineSeparator(), date);
+            System.out.printf(ERROR_MSG + System.lineSeparator(), dateFrom);
+            System.out.printf(ERROR_MSG + System.lineSeparator(), dateTo);
         }
         return false;
     }
