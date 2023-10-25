1382c17a930bf707156648388b30237e86f0d0f2
almanac1914 <144943174+almanac1914@users.noreply.github.com>
2023-10-25 15:21:13 -0600

    task implemented, build success

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,42 @@
 package core.basesyntax;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        StringBuilder result = new StringBuilder();
+        result.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
+    
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+    
+        for (String name : names) {
+            int totalSalary = 0;
+    
+            for (String entry : data) {
+                String[] parts = entry.split(" ");
+                String entryDateStr = parts[0];
+                String employeeName = parts[1];
+                int hoursWorked = Integer.parseInt(parts[2]);
+                int hourlyRate = Integer.parseInt(parts[3]);
+    
+                try {
+                    Date entryDate = dateFormat.parse(entryDateStr);
+                    Date fromDate = dateFormat.parse(dateFrom);
+                    Date toDate = dateFormat.parse(dateTo);
+    
+                    if (employeeName.equals(name) && entryDate.compareTo(fromDate) 
+                            >= 0 && entryDate.compareTo(toDate) <= 0) {
+                        totalSalary += hoursWorked * hourlyRate;
+                    }
+                } catch (Exception e) {
+                    throw new RuntimeException("Can't process data", e);
+                }
+            }
+            
+            result.append(System.lineSeparator()).append(name).append(" - ").append(totalSalary);
+        }
+    
+        return result.toString();
     }
 }
