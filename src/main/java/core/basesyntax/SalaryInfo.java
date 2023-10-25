da98306e12e9fdf30b109e7a4a2ea8dde24a1c0d
Vitalii Kazymyrovych <impero44@gmail.com>
2023-10-25 12:27:11 +0300

    Changed constants names

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,10 +5,10 @@ import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-    private static final int ZERO = 0;
-    private static final int ONE = 1;
-    private static final int TWO = 2;
-    private static final int THREE = 3;
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int HOURS_INDEX = 2;
+    private static final int RATE_INDEX = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder stringBuilder = new StringBuilder("Report for period "
@@ -18,15 +18,15 @@ public class SalaryInfo {
         LocalDate tempDate;
         for (String name : names) {
             stringBuilder.append(System.lineSeparator()).append(name).append(" - ");
-            int nameSum = ZERO;
+            int nameSum = 0;
             for (String info : data) {
                 String[] tempInfo = info.split(" ");
-                tempDate = LocalDate.parse(tempInfo[ZERO], FORMATTER);
-                if (tempInfo[ONE].equals(name)
+                tempDate = LocalDate.parse(tempInfo[DATE_INDEX], FORMATTER);
+                if (tempInfo[NAME_INDEX].equals(name)
                         && (tempDate.equals(from) || tempDate.isAfter(from))
                         && (tempDate.equals(to) || tempDate.isBefore(to))) {
-                    nameSum += Integer.parseInt(tempInfo[TWO])
-                            * Integer.parseInt(tempInfo[THREE]);
+                    nameSum += Integer.parseInt(tempInfo[HOURS_INDEX])
+                            * Integer.parseInt(tempInfo[RATE_INDEX]);
                 }
             }
             stringBuilder.append(nameSum);
