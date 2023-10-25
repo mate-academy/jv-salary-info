1483de6605015c2e3bc359cf70757818b494f545
yevhenreuta <r3utaa@gmail.com>
2023-10-25 17:06:29 +0300

    implemented getSalaryInfo method

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,11 +4,11 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
     private static final int DATE_INDEX = 0;
     private static final int NAME_INDEX = 1;
     private static final int HOURS_WORKED_INDEX = 2;
     private static final int HOURLY_RATE_INDEX = 3;
+    private final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate dateStart = LocalDate.parse(dateFrom, dateFormatter);
@@ -21,21 +21,23 @@ public class SalaryInfo {
         for (String name : names) {
             int totalSalary = 0;
 
-            for (String workingTimeData : data){
+            for (String workingTimeData : data) {
                 String[] parsedData = workingTimeData.split(" ");
 
                 LocalDate dateToCheck = LocalDate.parse(parsedData[DATE_INDEX], dateFormatter);
                 String parsedName = parsedData[NAME_INDEX];
                 int hours = Integer.parseInt(parsedData[HOURS_WORKED_INDEX]);
                 int payForHourAmount = Integer.parseInt(parsedData[HOURLY_RATE_INDEX]);
-                if (parsedName.equals(name) &&  !dateToCheck.isBefore(dateStart)
+                if (parsedName.equals(name) && !dateToCheck.isBefore(dateStart)
                         && !dateToCheck.isAfter(dateEnd)) {
                     totalSalary += hours * payForHourAmount;
                 }
             }
-            employerInfo.append(name).append(" - ").append(totalSalary).append(System.lineSeparator());
+            employerInfo.append(name).append(" - ")
+                    .append(totalSalary).append(System.lineSeparator());
         }
-        employerInfo.delete(employerInfo.length() - System.lineSeparator().length(), employerInfo.length());
+        employerInfo.delete(employerInfo.length() - System.lineSeparator().length(),
+                employerInfo.length());
         return employerInfo.toString();
     }
 }
