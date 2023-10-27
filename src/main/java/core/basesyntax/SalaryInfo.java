0dcbf4a644336d8456016176522ac90d117d2c97
almanac1914 <144943174+almanac1914@users.noreply.github.com>
2023-10-26 19:51:37 -0600

    constant field are added, LocalDate is used

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,42 +1,47 @@
 package core.basesyntax;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter
+            .ofPattern("dd.MM.yyyy");
+    private static final int ENTRY_DATE_INDEX = 0;
+    private static final int EMPLOYEE_NAME_INDEX = 1;
+    private static final int HOURS_WORKED_INDEX = 2;
+    private static final int HOURLY_RATE_INDEX = 3;
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder result = new StringBuilder();
         result.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
-    
-        SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
-    
+
         for (String name : names) {
             int totalSalary = 0;
-    
+
             for (String entry : data) {
                 String[] parts = entry.split(" ");
-                String entryDateStr = parts[0];
-                String employeeName = parts[1];
-                int hoursWorked = Integer.parseInt(parts[2]);
-                int hourlyRate = Integer.parseInt(parts[3]);
-    
+                String entryDateStr = parts[ENTRY_DATE_INDEX];
+                String employeeName = parts[EMPLOYEE_NAME_INDEX];
+                int hoursWorked = Integer.parseInt(parts[HOURS_WORKED_INDEX]);
+                int hourlyRate = Integer.parseInt(parts[HOURLY_RATE_INDEX]);
+
                 try {
-                    Date entryDate = dateFormat.parse(entryDateStr);
-                    Date fromDate = dateFormat.parse(dateFrom);
-                    Date toDate = dateFormat.parse(dateTo);
-    
-                    if (employeeName.equals(name) && entryDate.compareTo(fromDate) 
-                            >= 0 && entryDate.compareTo(toDate) <= 0) {
+                    LocalDate entryDate = LocalDate.parse(entryDateStr, DATE_FORMATTER);
+                    LocalDate fromDate = LocalDate.parse(dateFrom, DATE_FORMATTER);
+                    LocalDate toDate = LocalDate.parse(dateTo, DATE_FORMATTER);
+
+                    if (employeeName.equals(name) && !entryDate.isBefore(fromDate) 
+                            && !entryDate.isAfter(toDate)) {
                         totalSalary += hoursWorked * hourlyRate;
                     }
                 } catch (Exception e) {
                     throw new RuntimeException("Can't process data", e);
                 }
             }
-            
+
             result.append(System.lineSeparator()).append(name).append(" - ").append(totalSalary);
         }
-    
+
         return result.toString();
     }
 }
