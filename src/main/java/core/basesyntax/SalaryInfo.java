ddee16e28c6acbf9fb298649f27249a042d6732b
Bohdan Kost <kostbogdan@icloud.com>
2023-10-29 17:48:52 +0200

    implemented separate method calculateSalary

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -6,7 +6,7 @@ import java.time.format.DateTimeFormatter;
 public class SalaryInfo {
     private static final DateTimeFormatter DATE_TIME_FORMATTER
             = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-    private static final String SPLITTER = " ";
+    private static final String SPACE_SPLITTER = " ";
     private static final int DATE_INDEX = 0;
     private static final int NAME_INDEX = 1;
     private static final int HOURS_INDEX = 2;
@@ -17,37 +17,35 @@ public class SalaryInfo {
         LocalDate localDateTo = LocalDate.parse(dateTo, DATE_TIME_FORMATTER);
 
         StringBuilder builder = new StringBuilder();
-
         builder.append("Report for period ")
                 .append(dateFrom)
                 .append(" - ")
                 .append(dateTo)
                 .append(System.lineSeparator());
-
         for (String name : names) {
-            int salary = 0;
-
-            for (String datum : data) {
-                String[] parts = datum.split(SPLITTER);
-                LocalDate date = LocalDate.parse(parts[DATE_INDEX], DATE_TIME_FORMATTER);
-                String employeeName = parts[NAME_INDEX];
-                int hours = Integer.parseInt(parts[HOURS_INDEX]);
-                int incomePerHour = Integer.parseInt(parts[INCOME_PER_HOUR_INDEX]);
-
-                if (employeeName.equals(name)
-                        && !date.isBefore(localDateFrom)
-                        && !date.isAfter(localDateTo)) {
-                    salary += hours * incomePerHour;
-                }
-            }
             builder.append(name)
                     .append(" - ")
-                    .append(salary)
+                    .append(calculateSalary(name, data, localDateFrom, localDateTo))
                     .append(System.lineSeparator());
         }
-        builder.delete(builder.length()
-                - System.lineSeparator().length(), builder.length());
+        return builder.delete(builder.length()
+                - System.lineSeparator().length(), builder.length()).toString();
+    }
 
-        return builder.toString();
+    private int calculateSalary(String name, String[] data, LocalDate dateFrom, LocalDate dateTo) {
+        int salary = 0;
+        for (String datum : data) {
+            String[] parts = datum.split(SPACE_SPLITTER);
+            LocalDate date = LocalDate.parse(parts[DATE_INDEX], DATE_TIME_FORMATTER);
+            String employeeName = parts[NAME_INDEX];
+            int hours = Integer.parseInt(parts[HOURS_INDEX]);
+            int incomePerHour = Integer.parseInt(parts[INCOME_PER_HOUR_INDEX]);
+            if (employeeName.equals(name)
+                    && !date.isBefore(dateFrom)
+                    && !date.isAfter(dateTo)) {
+                salary += hours * incomePerHour;
+            }
+        }
+        return salary;
     }
 }
