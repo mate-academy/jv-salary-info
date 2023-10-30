097be45978fd5c63627d2c1d19ccd4becf5f5e31
Bohdan <bohdan.g.54@gmail.com>
2023-10-30 02:17:30 +0200

    debugged and refactored

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,38 +5,39 @@ import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
+
         StringBuilder outputData = new StringBuilder("Report for period ");
+        String visualSeparator = " - ";
+        outputData.append(dateFrom).append(visualSeparator).append(dateTo).append(System.lineSeparator());
 
-        for (int i = 0; i < data.length; i++) {
-            String visualSeparator = " - ";
-            outputData.append(dateFrom).append(visualSeparator).append(dateTo);
-
-            String[] rowOfData = data[i].split(" ");
-            String name = rowOfData[1]; // Get name from row of data
+        for (String currentName : names) {
             int totalIncome = 0;
+            for (String dataArrayElement : data) {
+                String[] rowOfData = dataArrayElement.split(" ");
+                String name = rowOfData[1]; // Get name from row of data
 
-            for (int j = 0; j < names.length; j++) {
-                if (names[i].equals(name)) {
+                if (currentName.equals(name)) {
                     String dateFromRowOfDate = rowOfData[0];
                     LocalDate localDateFromRowOfDate = dateFromString(dateFromRowOfDate);
                     LocalDate localDateFrom = dateFromString(dateFrom);
-                    LocalDate localDateTo = dateFromString(dateFrom);
+                    LocalDate localDateTo = dateFromString(dateTo);
 
                     if (localDateFromRowOfDate.isAfter(localDateFrom)
                             && localDateFromRowOfDate.isBefore(localDateTo)) {
-                        int timesWasPaid = Integer.parseInt(rowOfData[3]);
-                        int paidSum = Integer.parseInt(rowOfData[4]);
-                        totalIncome+=timesWasPaid*paidSum;
+                        int timesWasPaid = Integer.parseInt(rowOfData[2]);
+                        int paidSum = Integer.parseInt(rowOfData[3]);
+                        totalIncome += timesWasPaid * paidSum;
                     }
-                    outputData.append(name).append(visualSeparator).append(totalIncome);
                 }
             }
+            outputData.append(currentName).append(visualSeparator).append(totalIncome).append(System.lineSeparator());
         }
         return outputData.toString();
     }
 
     public LocalDate dateFromString(String date) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        date = date.strip();
         try {
             // Parse the date string into a LocalDate using the specified format
             return LocalDate.parse(date, formatter);
