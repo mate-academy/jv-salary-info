ba0996f790a8a4b86b6be5fd6b5dd1a01361924b
Bohdan <bohdan.g.54@gmail.com>
2023-11-01 16:07:44 +0200

    reformating, adding proper constant names

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,46 +4,50 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-    // 26.04.2019 John 4 50
-    private static final int dateSubstring = 0;
-    private static final int nameSubstring = 1;
-    private static final int timesPaidSubstring = 2;
-    private static final int sumPaidSubstring = 3;
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int TIMES_PAID_INDEX = 2;
+    private static final int SUM_PAID_INDEX = 3;
+    private static final String VISUAL_SEPARATOR = " - ";
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         // Creating String builder with starting content
         StringBuilder outputData = new StringBuilder("Report for period ");
-        String visualSeparator = " - ";
         outputData.append(dateFrom)
-                .append(visualSeparator)
+                .append(VISUAL_SEPARATOR)
                 .append(dateTo);
+
+        // Get data from string parameters
+        LocalDate localDateFrom = dateFromString(dateFrom);
+        LocalDate localDateTo = dateFromString(dateTo);
 
         for (String currentName : names) {
             int totalIncome = 0;
             for (String dataArrayElement : data) {
                 String[] rowOfData = dataArrayElement.split(" ");
-                String name = rowOfData[nameSubstring]; // Get name from row of data
+                String name = rowOfData[NAME_INDEX]; // Get name from row of data
 
                 if (currentName.equals(name)) {
-                    String dateFromRowOfData = rowOfData[dateSubstring];
+                    // Getting data from current user's row of data
+                    String dateFromRowOfData = rowOfData[DATE_INDEX];
                     LocalDate localDateFromRowOfDate = dateFromString(dateFromRowOfData);
-                    LocalDate localDateFrom = dateFromString(dateFrom);
-                    LocalDate localDateTo = dateFromString(dateTo);
 
+                    // Checking if current data is inside set data period
                     if ((localDateFromRowOfDate.isEqual(localDateFrom)
                             || localDateFromRowOfDate.isAfter(localDateFrom))
                             && (localDateFromRowOfDate.isEqual(localDateTo)
                             || localDateFromRowOfDate.isBefore(localDateTo))) {
-                        int timesWasPaid = Integer.parseInt(rowOfData[timesPaidSubstring]);
-                        int paidSum = Integer.parseInt(rowOfData[sumPaidSubstring]);
+                        // If condition of data satisfied and users are the same, add his income to stat
+                        int timesWasPaid = Integer.parseInt(rowOfData[TIMES_PAID_INDEX]);
+                        int paidSum = Integer.parseInt(rowOfData[SUM_PAID_INDEX]);
                         totalIncome += timesWasPaid * paidSum;
                     }
                 }
             }
             outputData.append(System.lineSeparator())
                     .append(currentName)
-                    .append(visualSeparator)
+                    .append(VISUAL_SEPARATOR)
                     .append(totalIncome);
         }
         return outputData.toString();
@@ -53,7 +57,7 @@ public class SalaryInfo {
         date = date.strip();
         try {
             // Parse the date string into a LocalDate using the specified format
-            return LocalDate.parse(date, formatter);
+            return LocalDate.parse(date, FORMATTER);
         } catch (Exception e) {
             throw new RuntimeException("Error while parsing date from string");
         }
