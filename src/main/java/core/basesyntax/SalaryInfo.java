4ee2d2842d79231269b68d83c182fd6676532900
Roman K <romazan07sq@gmail.com>
2023-11-03 23:48:45 +0200

    rename some variables, simplify code, use for-each loops

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,39 +4,32 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private static final int ZERO_NUMBER = 0;
-    private static final int ONE_NUMBER = 1;
-    private static final int TWO_NUMBER = 2;
-    private static final int THREE_NUMBER = 3;
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int SALARY_INDEX = 2;
+    private static final int SALARY_MULTIPLIER_INDEX = 3;
+    private final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        LocalDate localDateFrom = LocalDate.parse(dateFrom, dateTimeFormatter);
-        LocalDate localDateTo = LocalDate.parse(dateTo, dateTimeFormatter);
-        StringBuilder stringBuilder = new StringBuilder("Report for period " + dateFrom
-                + " - " + dateTo + "\n");
-        for (int i = 0; i < names.length; i++) {
-            stringBuilder.append(names[i] + " - ");
+        LocalDate parsedDateFrom = LocalDate.parse(dateFrom, dateTimeFormatter);
+        LocalDate parseDateTo = LocalDate.parse(dateTo, dateTimeFormatter);
+        StringBuilder report = new StringBuilder();
+        report.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
+        for (String name : names) {
+            report.append(System.lineSeparator()).append(name).append(" - ");
             int sumSalary = 0;
-            for (int j = 0; j < data.length; j++) {
-                String[] dataToArray = data[j].split(" ");
-                LocalDate dateFromDate = LocalDate.parse(
-                        dataToArray[ZERO_NUMBER], dateTimeFormatter);
-                if (dateFromDate.compareTo(localDateFrom) >= ZERO_NUMBER
-                        && dateFromDate.compareTo(localDateTo) <= ZERO_NUMBER
-                        && dataToArray[1].equals(names[i])) {
-                    sumSalary += Integer.parseInt(data[j].split(" ")[TWO_NUMBER])
-                            * Integer.parseInt(data[j].split(" ")[THREE_NUMBER]);
-                }
-                if (j == data.length - ONE_NUMBER && i == names.length - ONE_NUMBER) {
-                    stringBuilder.append(sumSalary);
-                    continue;
-                }
-                if (j == data.length - 1) {
-                    stringBuilder.append(sumSalary + "\n");
+            for (String dataElement : data) {
+                String[] dataToArray = dataElement.split(" ");
+                LocalDate parsedDate = LocalDate.parse(
+                        dataToArray[DATE_INDEX], dateTimeFormatter);
+                if (!parsedDate.isBefore(parsedDateFrom) && !parsedDate.isAfter(parseDateTo)
+                        && dataToArray[NAME_INDEX].equals(name)) {
+                    sumSalary += Integer.parseInt(dataElement.split(" ")[SALARY_INDEX])
+                            * Integer.parseInt(dataElement.split(" ")[SALARY_MULTIPLIER_INDEX]);
                 }
             }
+            report.append(sumSalary);
         }
-        return stringBuilder.toString();
+        return report.toString();
     }
 }
