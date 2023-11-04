4e60e600b75470c42bed068af27bb7458c576073
Nazar Kolesnikov <nazarkolesnikov536@gmail.com>
2023-11-04 23:05:57 +0200

    implementaion of getSalaryInfo() method

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,45 +1,43 @@
 package core.basesyntax;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    private static final int DATE_POSITION = 0;
+    private static final int QUANTITY_POSITION = 2;
+    private static final int AMOUNT_POSITION = 3;
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
-
-        try {
-            Date fromDate = dateFormat.parse(dateFrom);
-            Date toDate = dateFormat.parse(dateTo);
-
-            Map<String, Integer> earnings = IntStream.range(0, data.length)
-                    .mapToObj(i -> data[i].split(" "))
-                    .filter(parts -> {
-                        try {
-                            Date recordDate = dateFormat.parse(parts[0]);
-                            return recordDate.compareTo(fromDate) >= 0 && recordDate.compareTo(toDate) <= 0;
-                        } catch (ParseException e) {
-                            e.printStackTrace();
-                            return false;
-                        }
-                    })
-                    .collect(Collectors.toMap(parts -> parts[1],
-                            parts -> Integer.parseInt(parts[2]) * Integer.parseInt(parts[3]),
-                            Integer::sum));
-
-            StringBuilder result = new StringBuilder("Report for period " + dateFrom + " - " + dateTo + "\n");
-            for (String name : names) {
-                int earnedMoney = earnings.getOrDefault(name, 0);
-                result.append(name).append(" - ").append(earnedMoney).append("\n");
-            }
-
-            return result.toString().trim();
-        } catch (ParseException e) {
-            e.printStackTrace();
-            return "Invalid date format";
+        StringBuilder builder = new StringBuilder("Report for period ")
+                .append(dateFrom)
+                .append(" - ")
+                .append(dateTo);
+        for (String name: names) {
+            int totalResourses = 0;
+            for (String dataLine: data) {
+                if (!dataLine.contains(name)) {
+                    continue;
+                }
+                String[] dataArray = dataLine.split(" ");
+                if (LocalDate
+                        .parse(dataArray[DATE_POSITION], formatter)
+                        .compareTo(LocalDate.parse(dateTo, formatter)) > 0 ||
+                        LocalDate
+                            .parse(dataArray[DATE_POSITION], formatter)
+                            .compareTo(LocalDate.parse(dateFrom, formatter)) < 0) {
+                    continue;
+                }
+                totalResourses += Integer.parseInt(dataArray[QUANTITY_POSITION])
+                        * Integer.parseInt(dataArray[AMOUNT_POSITION]);
+                }
+                builder
+                        .append(System.lineSeparator())
+                        .append(name)
+                        .append(" - ")
+                        .append(totalResourses);
         }
+        return builder.toString();
     }
 }
