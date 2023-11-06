f4c946326f4cbbe8cbce119f774e2593a2a4e837
Slava_Moiseev <qualityengineerinkom@gmail.com>
2023-11-06 12:35:22 +0200

    Replaced Date to LocalDate, change variables names.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,38 +1,29 @@
 package core.basesyntax;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Date;
 
 public class SalaryInfo {
     private static final String RETURN_ROW = "Report for period ";
     private static final String HYPHEN = " - ";
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
 
         int sum;
         String[] info;
         ArrayList<String[]> sortNames = new ArrayList<>();
-        Date from;
-        Date actualDate;
-        Date endDate;
 
-        for (String name: names) {
+        for (String name : names) {
             for (int i = 0; i < data.length; i++) {
                 info = data[i].split(" ");
-                try {
-                    SimpleDateFormat format = new SimpleDateFormat();
-                    format.applyPattern("dd.MM.yyyy");
-                    actualDate = format.parse(info[0]);
-                    from = format.parse(dateFrom);
-                    endDate = format.parse(dateTo);
-                } catch (ParseException e) {
-                    throw new RuntimeException(e);
-                }
+                LocalDate actualDate = LocalDate.parse(info[0], formatter);
+                LocalDate from = LocalDate.parse(dateFrom, formatter);
+                LocalDate endDate = LocalDate.parse(dateTo, formatter);
                 if (name.equals(info[1])) {
-                    if ((actualDate.after(from) || actualDate.equals(from))
-                            && (actualDate.before(endDate) || actualDate.equals(endDate))) {
+                    if ((actualDate.isAfter(from.minusDays(1)))
+                            && (actualDate.isBefore(endDate.plusDays(1)))) {
                         sum = Integer.parseInt(info[2]) * Integer.parseInt(info[3]);
                         String[] str = {name, Integer.toString(sum)};
                         sortNames.add(str);
@@ -49,24 +40,25 @@ public class SalaryInfo {
                 return emptyResult.toString();
             }
         }
-        ArrayList<String[]> secondArray = new ArrayList<>();
-        for (String[] row: sortNames) {
-            boolean b = false;
-            for (String[] rowUnique: secondArray) {
+        ArrayList<String[]> finalArray = new ArrayList<>();
+        for (String[] row : sortNames) {
+            boolean isEqualName = false;
+            for (String[] rowUnique : finalArray) {
                 if (row[0].equals(rowUnique[0])) {
-                    b = true;
+                    isEqualName = true;
                     rowUnique[1] = Integer.toString(Integer.parseInt(rowUnique[1])
                             + Integer.parseInt(row[1]));
                 }
             }
-            if (!b) {
-                secondArray.add(row);
+            if (!isEqualName) {
+                finalArray.add(row);
             }
         }
-        StringBuilder res = new StringBuilder(RETURN_ROW + dateFrom + HYPHEN + dateTo);
-        for (String[] rows: secondArray) {
-            res.append(System.lineSeparator()).append(rows[0]).append(HYPHEN).append(rows[1]);
+        StringBuilder resultOfMethod = new StringBuilder(RETURN_ROW + dateFrom + HYPHEN + dateTo);
+        for (String[] rows : finalArray) {
+            resultOfMethod.append(System.lineSeparator())
+                    .append(rows[0]).append(HYPHEN).append(rows[1]);
         }
-        return res.toString();
+        return resultOfMethod.toString();
     }
 }
