6b544e5b0998c06b9898f4e5c2c692aeabbf71e1
Slava_Moiseev <qualityengineerinkom@gmail.com>
2023-11-06 21:07:55 +0200

    Without ArrayList? only StringBuilder.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -2,63 +2,38 @@ package core.basesyntax;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 
 public class SalaryInfo {
     private static final String RETURN_ROW = "Report for period ";
     private static final String HYPHEN = " - ";
+    private static final String SEPARATOR = " ";
+    private static final int INDEX_OF_DATE = 0;
+    private static final int INDEX_OF_NAME = 1;
+    private static final int INDEX_OF_HOURS = 2;
+    private static final int INDEX_OF_SALARY = 3;
     private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
 
-        int sum;
+        StringBuilder builder = new StringBuilder(RETURN_ROW + dateFrom + HYPHEN + dateTo);
         String[] info;
-        ArrayList<String[]> sortNames = new ArrayList<>();
-
+        LocalDate from = LocalDate.parse(dateFrom, formatter);
+        LocalDate endDate = LocalDate.parse(dateTo, formatter);
         for (String name : names) {
-            for (int i = 0; i < data.length; i++) {
-                info = data[i].split(" ");
-                LocalDate actualDate = LocalDate.parse(info[0], formatter);
-                LocalDate from = LocalDate.parse(dateFrom, formatter);
-                LocalDate endDate = LocalDate.parse(dateTo, formatter);
-                if (name.equals(info[1])) {
-                    if ((actualDate.isAfter(from.minusDays(1)))
-                            && (actualDate.isBefore(endDate.plusDays(1)))) {
-                        sum = Integer.parseInt(info[2]) * Integer.parseInt(info[3]);
-                        String[] str = {name, Integer.toString(sum)};
-                        sortNames.add(str);
+            int sum = 0;
+            for (String datum : data) {
+                info = datum.split(SEPARATOR);
+                LocalDate actualDate = LocalDate.parse(info[INDEX_OF_DATE], formatter);
+                if ((actualDate.isAfter(from.minusDays(1)))
+                        && (actualDate.isBefore(endDate.plusDays(1)))) {
+                    if (name.equals(info[INDEX_OF_NAME])) {
+                        sum = sum + Integer.parseInt(info[INDEX_OF_HOURS])
+                                * Integer.parseInt(info[INDEX_OF_SALARY]);
                     }
                 }
             }
-            if (sortNames.isEmpty()) {
-                StringBuilder emptyResult = new StringBuilder(RETURN_ROW
-                        + dateFrom + HYPHEN + dateTo);
-                for (String name1 : names) {
-                    emptyResult.append(System.lineSeparator())
-                            .append(name1).append(HYPHEN).append("0");
-                }
-                return emptyResult.toString();
-            }
+            builder.append(System.lineSeparator()).append(name).append(HYPHEN).append(sum);
         }
-        ArrayList<String[]> finalArray = new ArrayList<>();
-        for (String[] row : sortNames) {
-            boolean isEqualName = false;
-            for (String[] rowUnique : finalArray) {
-                if (row[0].equals(rowUnique[0])) {
-                    isEqualName = true;
-                    rowUnique[1] = Integer.toString(Integer.parseInt(rowUnique[1])
-                            + Integer.parseInt(row[1]));
-                }
-            }
-            if (!isEqualName) {
-                finalArray.add(row);
-            }
-        }
-        StringBuilder resultOfMethod = new StringBuilder(RETURN_ROW + dateFrom + HYPHEN + dateTo);
-        for (String[] rows : finalArray) {
-            resultOfMethod.append(System.lineSeparator())
-                    .append(rows[0]).append(HYPHEN).append(rows[1]);
-        }
-        return resultOfMethod.toString();
+        return builder.toString();
     }
 }
