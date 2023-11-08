f5ce51993e5aa479740eca5626d8cd5fe631e20a
Eugene Bormotov <bormotovjenya2510@gmail.com>
2023-11-08 21:25:15 +0100

    Implemented a method getSalaryInfo that calculates employee salaries.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,33 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        LocalDate startDate = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate deadLineDate = LocalDate.parse(dateTo, FORMATTER);
+        StringBuilder builder = new StringBuilder();
+        builder.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
+        for (String name : names) {
+            int salary = 0;
+            for (String dataString : data) {
+                String[] dataStringArray = dataString.split(" ");
+                if (name.equals(dataStringArray[1])) {
+                    LocalDate rowDate = LocalDate.parse(dataStringArray[0], FORMATTER);
+                    if ((rowDate.isAfter(startDate) && rowDate.isBefore(deadLineDate))
+                            || rowDate.equals(startDate)
+                            || rowDate.equals(deadLineDate)) {
+                        salary += Integer.parseInt(dataStringArray[2])
+                                * Integer.parseInt(dataStringArray[3]);
+                    }
+                }
+            }
+            builder.append(System.lineSeparator()).append(name).append(" - ").append(salary);
+        }
+
+        return builder.toString();
     }
 }
