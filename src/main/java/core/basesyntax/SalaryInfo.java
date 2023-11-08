19dcc1b988ef6ec418b4ea954ae0df560b73de79
petro feniak <petro.fenyak@gmail.com>
2023-11-08 11:49:28 +0200

    in the implementation of the method used only two loops and formatter is a constant field

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,51 +5,53 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
 public class SalaryInfo {
-    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int HOUR_INDEX = 2;
+    private static final int SALARY_INDEX = 3;
+    private static final String SEPARATOR = " - ";
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate dateStart;
         LocalDate dateEnd;
         try {
             dateStart = LocalDate.parse(dateFrom, formatter);
             dateEnd = LocalDate.parse(dateTo, formatter);
         } catch (DateTimeParseException e) {
-            System.out.printf("%s is not parsable!%n", dateFrom);
-            System.out.printf("%s is not parsable!%n", dateTo);
+            System.out.printf("%s is not parsable!%n", dateFrom, dateTo);
             throw e;
         }
-        int[] totalSalaryPerWorker = new int[names.length];
-        for (String dataLine : data) {
-            String[] splitDataArray = dataLine.split(" ");
-            LocalDate workDate;
-            try {
-                workDate = LocalDate.parse(splitDataArray[0], formatter);
-            } catch (DateTimeParseException e) {
-                System.out.printf("%s is not parsable!%n", splitDataArray[0]);
-                throw e;
-            }
-            String workerName = splitDataArray[1];
-            int workerTime = Integer.parseInt(splitDataArray[2]);
-            int workerSalary = Integer.parseInt(splitDataArray[3]);
-            if (!workDate.isBefore(dateStart) && !workDate.isAfter(dateEnd)) {
-                for (int i = 0; i < names.length; i++) {
-                    if (workerName.equals(names[i])) {
-                        totalSalaryPerWorker [i] += workerTime * workerSalary;
-                    }
+
+        StringBuilder totalReport = new StringBuilder("Report for period "
+                + dateFrom
+                + SEPARATOR
+                + dateTo);
+        for (String name: names) {
+            int totalSalaryPerWorker = 0;
+            for (String dataLine : data) {
+                String[] splitDataArray = dataLine.split(" ");
+                LocalDate workDate;
+                try {
+                    workDate = LocalDate.parse(splitDataArray[DATE_INDEX], formatter);
+                } catch (DateTimeParseException e) {
+                    System.out.printf("%s is not parsable!%n", splitDataArray[DATE_INDEX]);
+                    throw e;
+                }
+                String workerName = splitDataArray[NAME_INDEX];
+                int workerTime = Integer.parseInt(splitDataArray[HOUR_INDEX]);
+                int workerSalary = Integer.parseInt(splitDataArray[SALARY_INDEX]);
+                if (!workDate.isBefore(dateStart)
+                        && !workDate.isAfter(dateEnd)
+                        && workerName.equals(name)) {
+                    totalSalaryPerWorker += workerTime * workerSalary;
                 }
             }
-        }
-        StringBuilder totalReport = new StringBuilder();
-        totalReport.append("Report for period ")
-                .append(dateFrom)
-                .append(" - ")
-                .append(dateTo);
-        for (int i = 0; i < names.length; i++) {
             totalReport.append(System.lineSeparator())
-                    .append(names[i])
-                    .append(" - ")
-                    .append(totalSalaryPerWorker [i]);
+                    .append(name).append(SEPARATOR)
+                    .append(totalSalaryPerWorker);
         }
+
         return totalReport.toString();
     }
 }
