d3453cf3a329026d2e62e983c1c04a2a44bad415
Ivan <lavrinenkoivan51@gmail.com>
2023-11-10 12:58:18 +0100

    expression with LocalDate changed and index constants added

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,6 +5,10 @@ import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int HOURS_INDEX = 2;
+    private static final int INCOME_PER_HOUR_INDEX = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate localDateDateFrom = LocalDate.parse(dateFrom, FORMATTER);
@@ -16,14 +20,14 @@ public class SalaryInfo {
             int salary = 0;
             for (String entry : data) {
                 String [] parts = entry.split(" ");
-                LocalDate dataDate = LocalDate.parse(parts[0], FORMATTER);
-                String employeeName = parts[1];
-                int hours = Integer.parseInt(parts[2]);
-                int salaryPerHour = Integer.parseInt(parts[3]);
+                LocalDate dataDate = LocalDate.parse(parts[DATE_INDEX], FORMATTER);
+                String employeeName = parts[NAME_INDEX];
+                int hours = Integer.parseInt(parts[HOURS_INDEX]);
+                int incomePerHour = Integer.parseInt(parts[INCOME_PER_HOUR_INDEX]);
                 if (name.equals(employeeName)
-                        && dataDate.isAfter(localDateDateFrom)
-                        && dataDate.isBefore(localDateDateTo.plusDays(1))) {
-                    salary += salaryPerHour * hours;
+                        && !dataDate.isBefore(localDateDateFrom)
+                        && !dataDate.isAfter(localDateDateTo)) {
+                    salary += incomePerHour * hours;
                 }
             }
             stringBuilder.append(System.lineSeparator()).append(name).append(" - ").append(salary);
