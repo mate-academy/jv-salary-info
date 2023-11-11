e57a17d82dc6d8a7f8d1d85efcc710dd59721a4e
mykolatsupryk <mykola.tsupryk1994@gmail.com>
2023-11-11 23:59:08 +0200

    solution salary info task

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,44 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+    private StringBuilder builder;
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        LocalDate localDateFrom = stringToDate(dateFrom);
+        LocalDate localDateTo = stringToDate(dateTo);
+        LocalDate localDateFromRecord;
+        builder = new StringBuilder();
+        int salary;
+        builder.append("Report for period ");
+        builder.append(dateFrom).append(" - ");
+        builder.append(dateTo);
+        for (String name : names) {
+            salary = 0;
+            builder.append("\n").append(name).append(" - ");
+            for (String record : data) {
+                String[] recordArray = record.split(" ");
+                localDateFromRecord = stringToDate(recordArray[0]);
+                if (name.equalsIgnoreCase(recordArray[1])) {
+                    if ((localDateFromRecord.isAfter(localDateFrom)
+                            || localDateFromRecord.isEqual(localDateFrom))
+                            && (localDateFromRecord.isBefore(localDateTo)
+                            || localDateFromRecord.isEqual(localDateTo))) {
+                        salary += Integer.parseInt(recordArray[2])
+                                * Integer.parseInt(recordArray[3]);
+                    }
+                }
+            }
+            builder.append(salary);
+        }
+        return builder.toString();
+    }
+
+    private LocalDate stringToDate(String dateString) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDate date = LocalDate.parse(dateString, formatter);
+        return date;
     }
 }
