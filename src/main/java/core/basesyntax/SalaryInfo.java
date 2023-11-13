0f98ee51e2f17159c5391427871e8ed0299d0149
Bohdan Chupika <boroda4436@users.noreply.github.com>
2023-11-13 13:04:38 +0200

    remove encoding config from checkstyle plugin

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,38 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+
+        StringBuilder result = new StringBuilder();
+        result.append("Report for period ").append(dateFrom).append(" - ").append(dateTo)
+                .append(System.lineSeparator());
+        for (String name : names) {
+            int userSalary = 0;
+            for (String currentData : data) {
+                String[] record = currentData.split(" ");
+                String workDate = record[0];
+                String employeeName = record[1];
+                String workHours = record[2];
+                String hourSalary = record[3];
+
+                if (name.equals(employeeName) && isDateIncluded(dateFrom, dateTo, workDate)) {
+                    userSalary += Integer.parseInt(workHours) * Integer.parseInt(hourSalary);
+                }
+            }
+            result.append(name).append(" - ").append(userSalary).append(System.lineSeparator());
+        }
+        return result.toString().trim();
+
+    }
+
+    public Boolean isDateIncluded(String dateFrom, String dateTo, String seachDate) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDate beginDate = LocalDate.parse(dateFrom, formatter);
+        LocalDate endDate = LocalDate.parse(dateTo, formatter);
+        LocalDate targetDate = LocalDate.parse(seachDate, formatter);
+        return targetDate.isEqual(beginDate) || targetDate.isEqual(endDate) || targetDate.isAfter(beginDate) && targetDate.isBefore(endDate);
     }
 }
+
