9e25fe10d295b50793a6e919450b0b7423a888c0
LeonIllya <dd0733567@gmail.com>
2023-11-13 16:50:15 +0200

    my solution

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,33 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+    private static final String SEPARATOR = " - ";
+    private final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+
+        LocalDate dateFromStartWork = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate dateToFinishWork = LocalDate.parse(dateTo, FORMATTER);
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Report for period ").append(dateFrom).append(SEPARATOR).append(dateTo);
+
+        for (String name : names) {
+            int salary = 0;
+            for (String dataRow : data) {
+                String[] dataRowArray = dataRow.split(" ");
+                if (dataRowArray[1].equals(name)) {
+                    LocalDate rowDate = LocalDate.parse(dataRowArray[0], FORMATTER);
+                    if ((rowDate.isBefore(dateToFinishWork) && rowDate.isAfter(dateFromStartWork))
+                        || rowDate.equals(dateFromStartWork) || rowDate.equals(dateToFinishWork)) {
+                        salary += Integer.parseInt(dataRowArray[2]) * Integer.parseInt(dataRowArray[3]);
+                    }
+                }
+            }
+            stringBuilder.append(System.lineSeparator()).append(name).append(SEPARATOR).append(salary);
+        }
+        return stringBuilder.toString();
     }
 }
