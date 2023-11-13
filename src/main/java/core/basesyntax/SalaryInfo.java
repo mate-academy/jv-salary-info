0fb69676a23e73769ff0fe90d8068691d5b04797
LeonIllya <dd0733567@gmail.com>
2023-11-13 16:57:17 +0200

    fixed maven

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,14 +5,17 @@ import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     private static final String SEPARATOR = " - ";
-    private final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
 
         LocalDate dateFromStartWork = LocalDate.parse(dateFrom, FORMATTER);
         LocalDate dateToFinishWork = LocalDate.parse(dateTo, FORMATTER);
         StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("Report for period ").append(dateFrom).append(SEPARATOR).append(dateTo);
+        stringBuilder.append("Report for period ")
+                     .append(dateFrom)
+                     .append(SEPARATOR)
+                     .append(dateTo);
 
         for (String name : names) {
             int salary = 0;
@@ -21,8 +24,9 @@ public class SalaryInfo {
                 if (dataRowArray[1].equals(name)) {
                     LocalDate rowDate = LocalDate.parse(dataRowArray[0], FORMATTER);
                     if ((rowDate.isBefore(dateToFinishWork) && rowDate.isAfter(dateFromStartWork))
-                        || rowDate.equals(dateFromStartWork) || rowDate.equals(dateToFinishWork)) {
-                        salary += Integer.parseInt(dataRowArray[2]) * Integer.parseInt(dataRowArray[3]);
+                            || rowDate.equals(dateFromStartWork) || rowDate.equals(dateToFinishWork)) {
+                        salary += Integer.parseInt(dataRowArray[2])
+                            * Integer.parseInt(dataRowArray[3]);
                     }
                 }
             }
