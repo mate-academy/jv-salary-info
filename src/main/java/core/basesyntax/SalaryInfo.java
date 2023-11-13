548de45f802cc0ef06b38fb56b1c388bfdf25320
LeonIllya <dd0733567@gmail.com>
2023-11-13 19:11:20 +0200

    fixed

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -6,6 +6,10 @@ import java.time.format.DateTimeFormatter;
 public class SalaryInfo {
     private static final String SEPARATOR = " - ";
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int HOURS_INDEX = 2;
+    private static final int INCOME_PER_HOUR_INDEX = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
 
@@ -21,13 +25,13 @@ public class SalaryInfo {
             int salary = 0;
             for (String dataRow : data) {
                 String[] dataRowArray = dataRow.split(" ");
-                if (dataRowArray[1].equals(name)) {
-                    LocalDate rowDate = LocalDate.parse(dataRowArray[0], FORMATTER);
+                if (dataRowArray[NAME_INDEX].equals(name)) {
+                    LocalDate rowDate = LocalDate.parse(dataRowArray[DATE_INDEX], FORMATTER);
                     if ((rowDate.isBefore(dateToFinishWork) && rowDate.isAfter(dateFromStartWork))
                             || rowDate.equals(dateFromStartWork)
                             || rowDate.equals(dateToFinishWork)) {
-                        salary += Integer.parseInt(dataRowArray[2])
-                            * Integer.parseInt(dataRowArray[3]);
+                        salary += Integer.parseInt(dataRowArray[HOURS_INDEX])
+                            * Integer.parseInt(dataRowArray[INCOME_PER_HOUR_INDEX]);
                     }
                 }
             }
