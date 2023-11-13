cc7539f87e39a40a376a9382c9fb5b5df05ed14f
Bohdan Chupika <boroda4436@users.noreply.github.com>
2023-11-13 13:04:38 +0200

    remove encoding config from checkstyle plugin

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,40 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+    private static final DateTimeFormatter DATE_FORMATTER =
+            DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
-    }
-}
+        LocalDate startDate = LocalDate.parse(dateFrom, DATE_FORMATTER);
+        LocalDate endDate = LocalDate.parse(dateTo, DATE_FORMATTER);
+
+        StringBuilder report = new StringBuilder("Report for period "
+                + dateFrom + " - " + dateTo);
+
+        for (String name : names) {
+            int totalSalary = 0;
+
+            for (String entry : data) {
+                String[] details = entry.split(" ");
+                LocalDate workDate = LocalDate.parse(details[0], DATE_FORMATTER);
+                String employeeName = details[1];
+                int hoursWorked = Integer.parseInt(details[2]);
+                int hourlyRate = Integer.parseInt(details[3]);
+
+                if (employeeName.equals(name)
+                        && (workDate.isEqual(startDate) || workDate.isAfter(startDate))
+                        && (workDate.isEqual(endDate) || workDate.isBefore(endDate))) {
+                    totalSalary += hoursWorked * hourlyRate;
+                }
+            }
+
+            report.append(System.lineSeparator())
+                    .append(name).append(" - ").append(totalSalary);
+        }
+
+        return report.toString();
+    } // This correctly closes the method
+} // This correctly closes the class
