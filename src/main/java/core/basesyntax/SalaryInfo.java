b06a280969d941f1df8ee73cd44539769bf27d23
artem <artemsa223@gmail.com>
2023-11-16 22:42:32 +0000

    Realized method getSalary()

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,46 @@
 package core.basesyntax;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class SalaryInfo {
-    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+    public static final int DATE_POSITION_IN_ARRAY = 0;
+    public static final int NAME_POSITION_IN_ARRAY = 1;
+    public static final int HOURS_POSITION_IN_ARRAY = 2;
+    public static final int HOURLY_RATE_POSITION_IN_ARRAY = 3;
+
+    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo)
+            throws ParseException {
+        Date dateto = new SimpleDateFormat("dd.MM.yyyy").parse(dateTo);
+        Date datefrom = new SimpleDateFormat("dd.MM.yyyy").parse(dateFrom);
+        String[] nameArray = new String[names.length];
+        Integer[] salaryArray = new Integer[names.length];
+        for (int i = 0; i < names.length; i++) {
+            String name = names[i];
+            salaryArray[i] = 0;
+            nameArray[i] = name;
+            for (String info : data) {
+                String[] parts = info.split(" ");
+                Date actualDate = new SimpleDateFormat("dd.MM.yyyy")
+                        .parse(parts[DATE_POSITION_IN_ARRAY]);
+                if (parts[NAME_POSITION_IN_ARRAY].equals(name)
+                        && ((actualDate.before(dateto) || actualDate.equals(dateto))
+                                && (actualDate.after(datefrom) || actualDate.equals(datefrom)))) {
+                    salaryArray[i] += Integer.parseInt(parts[HOURS_POSITION_IN_ARRAY])
+                            * Integer.parseInt(parts[HOURLY_RATE_POSITION_IN_ARRAY]);
+                }
+            }
+        }
+        StringBuilder report = new StringBuilder();
+        report.append("Report for period ").append(dateFrom).append(" - ").append(dateTo)
+                .append("\n");
+        for (int i = 0; i < names.length; i++) {
+            report.append(nameArray[i]).append(" - ").append(salaryArray[i]);
+            if (i != (names.length - 1)) {
+                report.append(System.lineSeparator());
+            }
+        }
+        return report.toString();
     }
 }
