b06a7f12ca65e834b4283d97c8cdb6161a3a4347
artem <artemsa223@gmail.com>
2023-11-16 23:03:00 +0000

    remove 1 for loop, add try catch block

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -10,20 +10,33 @@ public class SalaryInfo {
     public static final int HOURS_POSITION_IN_ARRAY = 2;
     public static final int HOURLY_RATE_POSITION_IN_ARRAY = 3;
 
-    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo)
-            throws ParseException {
-        Date dateto = new SimpleDateFormat("dd.MM.yyyy").parse(dateTo);
-        Date datefrom = new SimpleDateFormat("dd.MM.yyyy").parse(dateFrom);
+    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
+        Date dateto;
+        Date datefrom;
+        try {
+            dateto = new SimpleDateFormat("dd.MM.yyyy").parse(dateTo);
+            datefrom = new SimpleDateFormat("dd.MM.yyyy").parse(dateFrom);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
         String[] nameArray = new String[names.length];
         Integer[] salaryArray = new Integer[names.length];
+        StringBuilder report = new StringBuilder();
+        report.append("Report for period ").append(dateFrom).append(" - ").append(dateTo)
+                .append("\n");
         for (int i = 0; i < names.length; i++) {
             String name = names[i];
             salaryArray[i] = 0;
             nameArray[i] = name;
             for (String info : data) {
                 String[] parts = info.split(" ");
-                Date actualDate = new SimpleDateFormat("dd.MM.yyyy")
-                        .parse(parts[DATE_POSITION_IN_ARRAY]);
+                Date actualDate;
+                try {
+                    actualDate = new SimpleDateFormat("dd.MM.yyyy")
+                            .parse(parts[DATE_POSITION_IN_ARRAY]);
+                } catch (ParseException e) {
+                    throw new RuntimeException(e);
+                }
                 if (parts[NAME_POSITION_IN_ARRAY].equals(name)
                         && ((actualDate.before(dateto) || actualDate.equals(dateto))
                                 && (actualDate.after(datefrom) || actualDate.equals(datefrom)))) {
@@ -31,11 +44,6 @@ public class SalaryInfo {
                             * Integer.parseInt(parts[HOURLY_RATE_POSITION_IN_ARRAY]);
                 }
             }
-        }
-        StringBuilder report = new StringBuilder();
-        report.append("Report for period ").append(dateFrom).append(" - ").append(dateTo)
-                .append("\n");
-        for (int i = 0; i < names.length; i++) {
             report.append(nameArray[i]).append(" - ").append(salaryArray[i]);
             if (i != (names.length - 1)) {
                 report.append(System.lineSeparator());
