8d2dc95e01620771424538d1a503ca0c6b4579da
Maksym Mazlov <mazlovmaxim@gmail.com>
2023-11-21 18:13:05 +0200

    fix magic numbers

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,27 +4,32 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int HOURS_INDEX = 2;
+    private static final int HOURLY_PAY_INDEX = 3;
+
     public String getSalaryInfo(String[] names,
                                 String[] data,
                                 String dateFrom,
                                 String dateTo) {
         int salary = 0;
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d.MM.yyyy");
-        LocalDate dateStart = LocalDate.parse(dateFrom, formatter);
-        LocalDate dateFinish = LocalDate.parse(dateTo, formatter);
+        LocalDate dateStart = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate dateFinish = LocalDate.parse(dateTo, FORMATTER);
         LocalDate workDay;
         StringBuilder builder = new StringBuilder();
 
         for (String name : names) {
             for (String dayInfo : data) {
                 String[] columnsData = dayInfo.split(" ");
-                workDay = LocalDate.parse(columnsData[0], formatter);
+                workDay = LocalDate.parse(columnsData[DATE_INDEX], FORMATTER);
 
-                if ((name.equals(columnsData[1]))
+                if ((name.equals(columnsData[NAME_INDEX]))
                         && ((dateStart.isBefore(workDay) || dateStart.isEqual(workDay))
                         && dateFinish.isAfter(workDay) || dateFinish.isEqual(workDay))) {
-                    int hours = Integer.parseInt(columnsData[2]);
-                    int hourlyPay = Integer.parseInt(columnsData[3]);
+                    int hours = Integer.parseInt(columnsData[HOURS_INDEX]);
+                    int hourlyPay = Integer.parseInt(columnsData[HOURLY_PAY_INDEX]);
                     salary += hours * hourlyPay;
                 }
             }
