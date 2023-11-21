c94416f3cc478f1a4ba593a81bfde9ef91d23b0f
Oleh <mihailyakyb45@gmail.com>
2023-11-21 22:13:13 +0200

    fixed getSalaryInfo method

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -2,44 +2,44 @@ package core.basesyntax;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.HashMap;
-import java.util.Map;
 
 public class SalaryInfo {
+    private static final DateTimeFormatter DATE_TIME_FORMATTER =
+            DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         if (names == null || data == null || dateFrom.isEmpty() || dateTo.isEmpty()) {
             throw new IllegalArgumentException("Dates or data cannot be null or empty");
         }
 
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        LocalDate startDate = LocalDate.parse(dateFrom, formatter);
-        LocalDate endDate = LocalDate.parse(dateTo, formatter);
-        Map<String, Integer> salaryMap = new HashMap<>();
-
-        for (String name : names) {
-            salaryMap.put(name, 0);
-        }
-
-        for (String info : data) {
-            String[] parts = info.split(" ");
-            LocalDate date = LocalDate.parse(parts[0], formatter);
-            if ((date.isEqual(startDate) || date.isAfter(startDate))
-                    && (date.isEqual(endDate) || date.isBefore(endDate))) {
-                String name = parts[1];
-                int hoursWorked = Integer.parseInt(parts[2]);
-                int payPerHour = Integer.parseInt(parts[3]);
-                if (salaryMap.containsKey(name)) {
-                    salaryMap.put(name, salaryMap.get(name) + (hoursWorked * payPerHour));
-                }
-            }
-        }
+        LocalDate startDate = LocalDate.parse(dateFrom, DATE_TIME_FORMATTER);
+        LocalDate endDate = LocalDate.parse(dateTo, DATE_TIME_FORMATTER);
 
         StringBuilder sb = new StringBuilder("Report for period "
-                + dateFrom + " - " + dateTo + "\n");
-        for (String name : names) {
-            sb.append(name).append(" - ").append(salaryMap.get(name)).append("\n");
-        }
+                + dateFrom + " - " + dateTo + System.lineSeparator());
 
+        for (String name : names) {
+            int salary = 0;
+
+            for (String info : data) {
+                String[] parts = info.split(" ");
+                LocalDate date = LocalDate.parse(parts[0], DATE_TIME_FORMATTER);
+                String nameFromData = parts[1];
+                String hoursWorked = parts[2];
+                String paymentPerHour = parts[3];
+
+                if (nameFromData.equals(name)
+                        && (date.isEqual(startDate) || date.isAfter(startDate))
+                        && (date.isEqual(endDate) || date.isBefore(endDate))) {
+                    int hoursWorkedToInt = Integer.parseInt(hoursWorked);
+                    int paymentPerHourToInt = Integer.parseInt(paymentPerHour);
+
+                    salary += hoursWorkedToInt * paymentPerHourToInt;
+                }
+            }
+
+            sb.append(name).append(" - ").append(salary).append(System.lineSeparator());
+        }
         return sb.toString().trim();
     }
 }
