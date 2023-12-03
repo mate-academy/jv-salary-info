6eddfb0fa717c01f74fea919ac69c232a0c80b02
Roman Soproniuk <anteumbra@gmail.com>
2023-12-03 20:09:59 +0200

    1. create constant 2. change name datum - dataEntry 3. create magic number 4. redundant blank line 5. dont delete int[] countSalary and dont create int salary since program doesn t work es if should be

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,40 +4,35 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    private static final DateTimeFormatter df = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        DateTimeFormatter df = DateTimeFormatter.ofPattern("dd.MM.yyyy");
         LocalDate dateFromNow = LocalDate.parse(dateFrom, df);
         LocalDate dateToNow = LocalDate.parse(dateTo, df);
         int[] countSalary = new int [names.length];
-        for (String datum : data) {
-            String[] currentStringInfo = datum.split(" ");
+        for (String dataEntry : data) {
+            String[] currentStringInfo = dataEntry.split(" ");
+            int hourlyRate = Integer.parseInt(currentStringInfo[3]);
+            int hoursWorked = Integer.parseInt(currentStringInfo[2]);
             for (int j = 0; j < names.length; j++) {
                 LocalDate today = LocalDate.parse(currentStringInfo[0], df);
                 if (names[j].equals(currentStringInfo[1]) && !dateFromNow.isAfter(today)
                         && !(dateToNow.isBefore(today))) {
-
-                    countSalary[j] += Integer.parseInt(currentStringInfo[2])
-                            * Integer.parseInt(currentStringInfo[3]);
+                    countSalary[j] += hourlyRate * hoursWorked;
                 }
             }
         }
-        return new StringBuilder().append("Report for period ")
+        StringBuilder salaryBuilder = new StringBuilder();
+        salaryBuilder.append("Report for period ")
                 .append(dateFrom)
                 .append(" - ")
-                .append(dateTo)
-                .append(System.lineSeparator())
-                .append(names[0])
-                .append(" - ")
-                .append(countSalary[0])
-                .append(System.lineSeparator())
-                .append(names[1])
-                .append(" - ")
-                .append(countSalary[1])
-                .append(System.lineSeparator())
-                .append(names[2])
-                .append(" - ")
-                .append(countSalary[2])
-                .toString();
+                .append(dateTo);
+        for (int k = 0; k < names.length; k++) {
+            salaryBuilder.append(System.lineSeparator())
+                    .append(names[k])
+                    .append(" - ")
+                    .append(countSalary[k]);
+        }
+        return salaryBuilder.toString();
     }
 }
