085e3289e6dd8ebb96d5785fcba5e27e904d433e
Vlad Bondarev <bondarev.21@icloud.com>
2023-12-07 13:14:39 +0200

    solution after review

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,16 +4,13 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private String dateFrom;
-    private String dateTo;
-    private int amountOfMoney = 0;
-    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final String DASH = " - ";
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        this.dateFrom = dateFrom;
-        this.dateTo = dateTo;
         StringBuilder builder = new StringBuilder("Report for period ")
-                .append(dateFrom).append(" - ").append(dateTo);
+                .append(dateFrom).append(DASH).append(dateTo);
+        int amountOfMoney = 0;
         for (int i = 0; i < names.length; i++) {
             for (String info : data) {
                 if (names[i].equals(getName(info)) && fitsInTime(info, dateFrom, dateTo)) {
@@ -21,31 +18,35 @@ public class SalaryInfo {
                 }
             }
             builder.append(System.lineSeparator())
-                    .append(names[i]).append(" - ").append(amountOfMoney);
+                    .append(names[i]).append(DASH).append(amountOfMoney);
             amountOfMoney = 0;
         }
         return builder.toString();
     }
 
     public LocalDate getDate(String data) {
-        String result = data.split(" ")[0];
-        return LocalDate.parse(result, formatter);
+        final int index = 0;
+        String result = data.split(" ")[index];
+        return LocalDate.parse(result, FORMATTER);
     }
 
     public String getName(String data) {
-        return String.valueOf(data.split(" ")[1]);
+        final int index = 1;
+        return String.valueOf(data.split(" ")[index]);
     }
 
     public int getMoney(String data) {
+        final int hours_index = 2;
+        final int money_per_hour_index = 3;
         String[] divided = data.split(" ");
-        int hours = Integer.parseInt(divided[2]);
-        int moneyPerHour = Integer.parseInt(divided[3]);
+        int hours = Integer.parseInt(divided[hours_index]);
+        int moneyPerHour = Integer.parseInt(divided[money_per_hour_index]);
         return hours * moneyPerHour;
     }
 
     public boolean fitsInTime(String string, String dateFrom, String dateTo) {
-        LocalDate localDateFrom = LocalDate.parse(dateFrom, formatter);
-        LocalDate localDateTo = LocalDate.parse(dateTo, formatter);
+        LocalDate localDateFrom = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate localDateTo = LocalDate.parse(dateTo, FORMATTER);
         LocalDate givenDate = getDate(string);
         boolean fits = localDateTo.equals(givenDate)
                 || (localDateTo.isAfter(givenDate) && localDateFrom.isBefore(givenDate));
