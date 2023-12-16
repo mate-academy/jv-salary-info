8d9b8734bf0e28b3abf60660a5d479c225183688
Anna Suprun <evgenevnaanna989@gmail.com>
2023-12-16 16:26:55 +0200

    removed useless method, created variable income for better readability, created constant fields

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,7 +4,12 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int WORK_HOUR_INDEX = 2;
+    private static final int INCOME_PER_HOUR_INDEX = 3;
+
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder result = new StringBuilder("Report for period " + dateFrom + " - " + dateTo
@@ -12,14 +17,10 @@ public class SalaryInfo {
         LocalDate dateFromFormatted = LocalDate.parse(dateFrom, formatter);
         LocalDate dateToFormatted = LocalDate.parse(dateTo, formatter);
         for (String name : names) {
-            result.append(formatData(name, calculatePersonsIncome(name, data,
-                    dateFromFormatted, dateToFormatted)));
+            int income = calculatePersonsIncome(name, data, dateFromFormatted, dateToFormatted);
+            result.append(name).append(" - ").append(income).append(System.lineSeparator());
         }
         return result.toString().trim();
-    }
-
-    private String formatData(String name, int income) {
-        return name + " - " + income + System.lineSeparator();
     }
 
     private boolean isInDateLimit(LocalDate date, LocalDate dateFrom, LocalDate dateTo) {
@@ -32,10 +33,10 @@ public class SalaryInfo {
         int income = 0;
         for (String dataSample : data) {
             String[] dataArray = dataSample.split(" ");
-            LocalDate date = LocalDate.parse(dataArray[0], formatter);
-            String name = dataArray[1];
-            int workHour = Integer.parseInt(dataArray[2]);
-            int incomePerHour = Integer.parseInt(dataArray[3]);
+            LocalDate date = LocalDate.parse(dataArray[DATE_INDEX], formatter);
+            String name = dataArray[NAME_INDEX];
+            int workHour = Integer.parseInt(dataArray[WORK_HOUR_INDEX]);
+            int incomePerHour = Integer.parseInt(dataArray[INCOME_PER_HOUR_INDEX]);
             if (name.equals(person)
                     && isInDateLimit(date, dateFrom, dateTo)) {
                 income += workHour * incomePerHour;
