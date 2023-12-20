b5bf79911140c1f137a41a7ac56de6dffc1551c0
Ruslan Cherniavskiy <ruslan.od.ua@ukr.net>
2023-12-20 18:29:07 +0200

    added constants and throw the exception

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -3,24 +3,31 @@ package core.basesyntax;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+
 public class SalaryInfo {
+    static  final DateTimeFormatter FORMATER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    static final String[] EMPTY_ARRAY = {};
+    static final String EMPTY_STRING = null;
+    static final String HEADER = "Report for period ";
+    static final String DELIMITER = " - ";
+    static final String SEPARATOR = " ";
+    static final int DATE = 0;
+    static final int NAME = 1;
+    static final int HOUR = 2;
+    static final int SALARY = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        StringBuilder result = new StringBuilder("Report for period " + dateFrom + " - " + dateTo);
+        StringBuilder result = new StringBuilder(HEADER + dateFrom + DELIMITER + dateTo);
 
-        if (names != null && data != null && dateFrom != null && dateTo != null) {
-            try {
+        if (names != EMPTY_ARRAY && data != EMPTY_ARRAY && dateFrom != EMPTY_STRING && dateTo != EMPTY_STRING) {
                 LocalDate startDate = parseDate(dateFrom);
                 LocalDate endDate = parseDate(dateTo);
 
                 for (String name : names) {
                     int employeeSalary = calculateEmployeeSalary(data, name, startDate, endDate);
                     result.append(System.lineSeparator()).append(name)
-                            .append(" - ").append(employeeSalary);
+                            .append(DELIMITER).append(employeeSalary);
                 }
-            } catch (NumberFormatException e) {
-                result.append("\nError parsing number.");
-            }
         }
 
         return result.toString();
@@ -28,10 +35,9 @@ public class SalaryInfo {
 
     private LocalDate parseDate(String dateString) {
         try {
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-            return LocalDate.parse(dateString, formatter);
+            return LocalDate.parse(dateString, FORMATER);
         } catch (Exception e) {
-            throw new RuntimeException("Error parsing date.");
+            throw new RuntimeException("Error parsing date.", e);
         }
     }
 
@@ -40,17 +46,17 @@ public class SalaryInfo {
         int employeeSalary = 0;
 
         for (String day : data) {
-            String[] dataDay = day.split(" ");
+            String[] dataDay = day.split(SEPARATOR);
             try {
-                LocalDate itemDate = parseDate(dataDay[0]);
+                LocalDate itemDate = parseDate(dataDay[DATE]);
 
-                if (name.equals(dataDay[1]) && (itemDate.isEqual(startDate)
+                if (name.equals(dataDay[NAME]) && (itemDate.isEqual(startDate)
                         || (itemDate.isAfter(startDate) && itemDate.isBefore(endDate))
                         || itemDate.isEqual(endDate))) {
-                    employeeSalary += Integer.parseInt(dataDay[2]) * Integer.parseInt(dataDay[3]);
+                    employeeSalary += Integer.parseInt(dataDay[HOUR]) * Integer.parseInt(dataDay[SALARY]);
                 }
             } catch (NumberFormatException e) {
-                System.out.println("Error parsing number: ");
+                throw new NumberFormatException("Error parsing number: ");
             }
         }
 
