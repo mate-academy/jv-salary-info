7db4981f9ca1e48bcbe8e46250bf0e2694995058
Oleksandr Kalenyk <carbonmail88@gmail.com>
2023-12-31 10:32:25 +0200

    fixed StringBuilder "results", added constant indexes and made "formatter constant"

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,24 +4,29 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    public static final int DATA_DATE = 0;
+    public static final int DATA_NAME = 1;
+    public static final int DATA_HOURS = 2;
+    public static final int DATA_MONEY = 3;
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        StringBuilder results = new StringBuilder(
-                ("Report for period ") + (dateFrom) + (" - ") + (dateTo));
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        LocalDate localDateFrom = LocalDate.parse(dateFrom, formatter);
-        LocalDate localDateTo = LocalDate.parse(dateTo, formatter);
+        StringBuilder results = new StringBuilder("Report for period ")
+                .append(dateFrom).append(" - ").append(dateTo);
+        LocalDate localDateFrom = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate localDateTo = LocalDate.parse(dateTo, FORMATTER);
 
         for (int i = 0; i < names.length; i++) {
             results.append(System.lineSeparator()).append(names[i]);
             int salary = 0;
             for (int y = 0; y < data.length; y++) {
                 String[] nameData = data[y].split(" ");
-                if (nameData[1].equals(names[i])) {
-                    LocalDate date = LocalDate.parse(nameData[0], formatter);
+                if (nameData[DATA_NAME].equals(names[i])) {
+                    LocalDate date = LocalDate.parse(nameData[DATA_DATE], FORMATTER);
                     if (date.isAfter(localDateFrom)
                             && (date.isBefore(localDateTo) || date.isEqual(localDateTo))) {
-                        int hours = Integer.parseInt(nameData[2]);
-                        int money = Integer.parseInt(nameData[3]);
+                        int hours = Integer.parseInt(nameData[DATA_HOURS]);
+                        int money = Integer.parseInt(nameData[DATA_MONEY]);
                         salary += money * hours;
                     }
                 }
@@ -31,3 +36,4 @@ public class SalaryInfo {
         return results.toString();
     }
 }
+
