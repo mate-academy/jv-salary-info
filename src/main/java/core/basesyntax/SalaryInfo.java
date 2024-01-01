7dfd2870dd2062317bdb193cce60b9161300f2e9
Viktor Oblapenko <viktor.oblapenko@gmail.com>
2024-01-01 22:49:16 +0200

    temporary working code in an attempt to avoid HashMap data structure

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -2,23 +2,27 @@ package core.basesyntax;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.time.temporal.ChronoField;
+import java.time.temporal.TemporalField;
 import java.util.HashMap;
 import java.util.Map;
 
 public class SalaryInfo {
-    private final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM.yyyy");
+    private static final DateTimeFormatter SIMPLE_DATE_FORMAT = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         Map<String, Integer> employeesSalaries = getEmployeesSalaries(data, dateFrom, dateTo);
         StringBuilder result = new StringBuilder("Report for period " + dateFrom
-                + " - " + dateTo + "\n");
+                + " - " + dateTo);
         for (String name : names) {
             int totalSalary = employeesSalaries.getOrDefault(name,0);
-            result.append(name).append(" - ").append(totalSalary);
-            if (!name.equals(names[names.length - 1])) {
-                result.append("\n");
-            }
+            result.append(System.lineSeparator())
+                    .append(name)
+                    .append(" - ")
+                    .append(totalSalary);
         }
         return result.toString();
     }
@@ -27,24 +31,24 @@ public class SalaryInfo {
                                                      String dateFrom, String dateTo) {
         Map<String,Integer> localMap = new HashMap<>();
         try {
-            Date fromDate = simpleDateFormat.parse(dateFrom);
-            Date toDate = simpleDateFormat.parse(dateTo);
+            LocalDate fromDate = LocalDate.parse(dateFrom,SIMPLE_DATE_FORMAT);
+            LocalDate toDate = LocalDate.parse(dateTo,SIMPLE_DATE_FORMAT);
             for (String entry : data) {
                 String[] parts = entry.split(" ");
                 String dateString = parts[0];
                 String name = parts[1];
                 int workingHours = Integer.parseInt(parts[2]);
                 int incomePerHour = Integer.parseInt(parts[3]);
-                Date date = simpleDateFormat.parse(dateString);
-                if (date.after(fromDate) && date.before(toDate)
+                LocalDate date = LocalDate.parse(dateString,SIMPLE_DATE_FORMAT);
+                if (date.isAfter(fromDate) && date.isBefore(toDate)
                         || date.equals(fromDate) || date.equals(toDate)) {
                     int totalIncome = workingHours * incomePerHour;
                     localMap.put(name, localMap.getOrDefault(name,0)
                             + totalIncome);
                 }
             }
-        } catch (ParseException | NumberFormatException e) {
-            throw new RuntimeException(e);
+        } catch (DateTimeParseException | NumberFormatException e) {
+           e.printStackTrace();
         }
         return localMap;
     }
