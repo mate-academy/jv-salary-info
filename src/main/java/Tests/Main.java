031e529d8bd78b0feae061e70eb2e39897f59fc9
John <pandastars535@gmail.com>
2024-01-01 04:00:39 +0200

    done Test task need start to insert all solution to salaryinfo.class

--- src/main/java/Tests/Main.java
+++ src/main/java/Tests/Main.java
@@ -4,6 +4,8 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,19 +13,13 @@ public class Main {
                 "10.04.2019 John 7 100", "22.04.2019 Kate 9 100", "25.06.2019 John 11 50"
                 , "26.04.2019 Andrew 3 150", "13.02.2019 John 7 100", "26.04.2019 Kate 9 100"};
 
-        String[] names = new String[]{"john", "andrew", "alice", "bob", "tania", "tolia"};
+        String[] names = new String[]{"John", "Andrew", "Kate"};
         String dateFrom = "01.04.2019";
         String dateTo = "30.04.2019";
 
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
         StringBuilder builderOfValidRange = new StringBuilder();
-//        for (String dat : data ) {
-//            String[] parts = dat.split(" ");
-//            stringBuilder.append(parts[0]).append(" ").toString();
-//        }
-//        System.out.println(stringBuilder);
-//     // у цьому методі ми отримали всі дати із масива дат
-        int result = 0;
+
         for (String dataItem : data) {
             String[] parts = dataItem.split(" ");
             String dateString = parts[0];
@@ -39,20 +35,24 @@ public class Main {
                 System.out.println("date format exseption");
             }
         }
-        String[] arr = builderOfValidRange.toString().split(",");
-        StringBuilder builderForResult = new StringBuilder();
-        int index = 0;
-
-        for (String res : arr) {
-            String[] parts = res.split(" ");
-            if  (names[index].equals(parts[0])) {
-                result += Integer.parseInt(parts[1]);
-                builderForResult.append(names[index]).append(result).toString();
-                index++;
-                System.out.println(builderForResult);
-            }
+        StringBuilder fin = new StringBuilder("Report for period " + dateFrom + " - " + dateTo + "\n");
+        String[] records = builderOfValidRange.toString().split(",");
+        Map<String, Integer> totalSumMap = new HashMap<>();
+        for (String name : names) {
+            totalSumMap.put(name, 0);
         }
 
+        for (String record : records) {
+            String[] parts = record.split(" ");
+            String name = parts[0];
+            int amount = Integer.parseInt(parts[1].replace(",", ""));
+            totalSumMap.put(name, totalSumMap.get(name) + amount);
+        }
+        for (String name : names) {
+            fin.append(name ).append(" ").append(totalSumMap.get(name)).append("\n");
+        }
+        fin.toString();
+        System.out.println(fin);
     }
 }
 
