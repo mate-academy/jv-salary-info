f54da0bfdda51271d50eb58923ae431b9b406676
Kateryna Riznychenko <lyqvin.kr@gmail.com>
2024-01-04 16:39:29 +0200

    implemented task solution

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,58 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
-    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+
+    public static String getSalaryInfo(String[] names, String[] data,
+                                       String dateFrom, String dateTo) {
+        String[][] words = new String[data.length][4];
+
+        for (int i = 0; i < data.length; i++) {
+            words[i] = data[i].split(" ");
+        }
+
+        StringBuilder report = new StringBuilder();
+        report.append("Report for period ")
+                .append(dateFrom)
+                .append(" - ")
+                .append(dateTo)
+                .append("\n");
+
+        for (int i = 0; i < names.length; i++) {
+            int totalSalary = 0;
+
+            for (int j = 0; j < words.length; j++) {
+                if (names[i].equals(words[j][1]) && isDateInRange(words[j][0], dateFrom, dateTo)) {
+                    int hours = Integer.parseInt(words[j][2]);
+                    int salaryPerHour = Integer.parseInt(words[j][3]);
+                    int salary = hours * salaryPerHour;
+                    totalSalary += salary;
+                }
+            }
+            if (i == names.length - 1) {
+                report.append(names[i])
+                        .append(" - ")
+                        .append(totalSalary);
+            } else {
+                report.append(names[i])
+                        .append(" - ")
+                        .append(totalSalary)
+                        .append(System.lineSeparator());
+            }
+        }
+
+        return report.toString();
+    }
+
+    private static boolean isDateInRange(String dateString, String dateFrom, String dateTo) {
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDate date = LocalDate.parse(dateString, dateFormatter);
+        LocalDate fromDate = LocalDate.parse(dateFrom, dateFormatter);
+        LocalDate toDate = LocalDate.parse(dateTo, dateFormatter);
+
+        return (date.isEqual(fromDate) || date.isAfter(fromDate))
+                && (date.isEqual(toDate) || date.isBefore(toDate));
     }
 }
