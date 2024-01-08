53951ef11315d48538e8d95838d6aabf29ff51a4
Yana Stepanova <yanna.stepanova@gmail.com>
2024-01-08 14:26:17 +0200

    made constants for indexes

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -8,6 +8,10 @@ public class SalaryInfo {
     private static final String SEPARATOR = " ";
     private static final String HEADER = "Report for period ";
     private static final String DELIMITER = " - ";
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int SALARY_AMOUNT_INDEX = 2;
+    private static final int SALARY_INDEX = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder report = new StringBuilder();
@@ -19,11 +23,11 @@ public class SalaryInfo {
             for (int i = 0; i < names.length; i++) {
                 for (String line : data) {
                     String[] strings = line.split(SEPARATOR);
-                    LocalDate dateReport = LocalDate.parse(strings[0], PATTERN);
+                    LocalDate dateReport = LocalDate.parse(strings[DATE_INDEX], PATTERN);
                     if (!dateReport.isBefore(dateLocalFrom) && !dateReport.isAfter(dateLocalTo)) {
-                        int indexFromData = Integer.parseInt(strings[2]);
-                        int salaryFromData = Integer.parseInt(strings[3]);
-                        salaries[i] += (names[i].equals(strings[1]))
+                        int indexFromData = Integer.parseInt(strings[SALARY_AMOUNT_INDEX]);
+                        int salaryFromData = Integer.parseInt(strings[SALARY_INDEX]);
+                        salaries[i] += (names[i].equals(strings[NAME_INDEX]))
                                 ? (indexFromData * salaryFromData) : 0;
                     }
                 }
