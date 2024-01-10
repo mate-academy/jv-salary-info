eca1f8c1bc88b25df5571d5d11846c4b7994c818
Igor Hunder <gunderigorv@gmail.com>
2024-01-10 20:59:56 +0200

    made a salary calculation

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,33 +5,33 @@ import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int HOURS_WORKED_INDEX = 2;
+    private static final int HOURLY_RATE_INDEX = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        StringBuilder result = new StringBuilder();
+        StringBuilder result = new StringBuilder("Report for period ")
+                .append(dateFrom).append(" - ").append(dateTo);
         LocalDate fromDate = LocalDate.parse(dateFrom,DATE_FORMAT);
         LocalDate toDate = LocalDate.parse(dateTo, DATE_FORMAT);
-        if (data.length > 0) {
-            String reportPeriod = "Report for period " + dateFrom + " - " + dateTo;
-            result.insert(0, reportPeriod);
-            for (String name : names) {
-                StringBuilder nameResult = new StringBuilder();
-                int totalSalary = 0;
-                for (int i = 0; i < data.length; i++) {
-                    String[] date = data[i].split(" ");
-                    LocalDate currentDate = LocalDate.parse(date[0], DATE_FORMAT);
-                    String employeeName = date[1];
-                    int hoursWorked = Integer.parseInt(date[2]);
-                    int hourlyRate = Integer.parseInt(date[3]);
-                    int salary = hoursWorked * hourlyRate;
-                    if ((currentDate.isAfter(fromDate) || currentDate.equals(fromDate))
+        for (String name : names) {
+            int totalSalary = 0;
+            for (int i = 0; i < data.length; i++) {
+                String[] date = data[i].split(" ");
+                LocalDate currentDate = LocalDate.parse(date[DATE_INDEX], DATE_FORMAT);
+                String employeeName = date[NAME_INDEX];
+                int hoursWorked = Integer.parseInt(date[HOURS_WORKED_INDEX]);
+                int hourlyRate = Integer.parseInt(date[HOURLY_RATE_INDEX]);
+                int salary = hoursWorked * hourlyRate;
+                if ((currentDate.isAfter(fromDate) || currentDate.equals(fromDate))
                             && (currentDate.isBefore(toDate) || currentDate.equals(toDate))
                             && employeeName.equals(name)) {
-                        totalSalary += salary;
-                    }
+                    totalSalary += salary;
                 }
-                nameResult.append(name).append(" - ").append(totalSalary);
-                result.append(System.lineSeparator()).append(nameResult);
             }
+            result.append(System.lineSeparator()).append(name).append(" - ")
+                        .append(totalSalary);
         }
         return result.toString();
     }
