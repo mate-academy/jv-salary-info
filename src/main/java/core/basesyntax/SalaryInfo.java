7cd408089ada6bd4ed4ffb7bfc835fa90450e857
MolchanovAlexander <rybkolhoz@gmail.com>
2024-01-10 20:14:48 +0200

    build-success-first-commit

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,35 @@
 package core.basesyntax;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 public class SalaryInfo {
+    private static final SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        StringBuilder stringBuilder = new StringBuilder(
+                "Report for period " + dateFrom + " - " + dateTo);
+        String[] temp;
+        int sum = 0;
+        for (int i = 0; i < data.length; i++) {
+            for (String oneData: data) {
+                temp = oneData.split(" ");
+                try {
+                    if (names[i].equals(temp[1])
+                            && ((sdf.parse(temp[0]).compareTo(sdf.parse(dateFrom))) >= 0
+                            && (sdf.parse(temp[0]).compareTo(sdf.parse(dateTo))) <= 0)) {
+                        sum = sum + Integer.parseInt(temp[2]) * Integer.parseInt(temp[3]);
+                    }
+                } catch (ParseException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            stringBuilder.append("\n").append(names[i]).append(" - ").append(sum);
+            sum = 0;
+            if (i == names.length - 1) {
+                break;
+            }
+        }
+        return stringBuilder.toString();
     }
 }
