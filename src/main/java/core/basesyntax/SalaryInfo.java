514294d3077572871c0fe34e4869dc78bb8480b7
Alex Naumenko <alex_naumenko_@ukr.net>
2024-01-10 21:46:36 +0200

    refactored using of StringBuilder append() method, removed minusDays() and plusDays() methods, added constants DATE_INDEX, NAME_INDEX, WORK_HOURS_INDEX, INCOME_PER_HOUR_INDEX for better readability

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -7,6 +7,10 @@ import java.util.Locale;
 public class SalaryInfo {
     private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern(
             "dd.MM.yyyy", Locale.ENGLISH);
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int WORK_HOURS_INDEX = 2;
+    private static final int INCOME_PER_HOUR_INDEX = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         int[] salaryForPeriod = new int[names.length];
@@ -16,25 +20,30 @@ public class SalaryInfo {
             int employerSalary = 0;
             for (String datum : data) {
                 String[] dataFormatted = datum.split(" ");
-                LocalDate workingDayDate = LocalDate.parse(dataFormatted[0], DATE_TIME_FORMATTER);
-                if (workingDayDate.isAfter(beginDate.minusDays(1))
-                        && workingDayDate.isBefore(endDate.plusDays(1))
-                        && names[i].equals(dataFormatted[1])) {
-                    employerSalary = employerSalary + Integer.parseInt(dataFormatted[2])
-                            * Integer.parseInt(dataFormatted[3]);
+                LocalDate workingDayDate = LocalDate.parse(dataFormatted[DATE_INDEX],
+                        DATE_TIME_FORMATTER);
+                if (!workingDayDate.isAfter(endDate)
+                        && !workingDayDate.isBefore(beginDate)
+                        && names[i].equals(dataFormatted[NAME_INDEX])) {
+                    employerSalary = employerSalary
+                            + Integer.parseInt(dataFormatted[WORK_HOURS_INDEX])
+                            * Integer.parseInt(dataFormatted[INCOME_PER_HOUR_INDEX]);
                 }
             }
             salaryForPeriod[i] = employerSalary;
         }
 
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("Report for period ").append(dateFrom).append(" - ").append(dateTo)
-                .append(System.lineSeparator());
+        StringBuilder stringBuilder = new StringBuilder()
+                .append("Report for period ")
+                .append(dateFrom)
+                .append(" - ")
+                .append(dateTo);
         for (int i = 0; i < names.length; i++) {
-            stringBuilder.append(names[i]).append(" - ").append(salaryForPeriod[i]);
-            if (i < names.length - 1) {
-                stringBuilder.append(System.lineSeparator());
-            }
+            stringBuilder
+                    .append(System.lineSeparator())
+                    .append(names[i])
+                    .append(" - ")
+                    .append(salaryForPeriod[i]);
         }
 
         return stringBuilder.toString();
