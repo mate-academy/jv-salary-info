b81ef7dcd24686b5aca09d73f87bc3c29b2530ad
Liza <lizabalashova7@gmail.com>
2024-01-10 15:23:18 +0200

    fixed

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,13 +4,16 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        final String europeanDatePattern = "dd.MM.yyyy";
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int HOURS_INDEX = 2;
+    private static final int RATE_INDEX = 3;
+    private static final String RECORD_SEPARATOR = " ";
 
-        LocalDate fromDate = LocalDate.parse(dateFrom,
-                DateTimeFormatter.ofPattern(europeanDatePattern));
-        LocalDate toDate = LocalDate.parse(dateTo,
-                DateTimeFormatter.ofPattern(europeanDatePattern));
+    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
+        LocalDate fromDate = LocalDate.parse(dateFrom, formatter);
+        LocalDate toDate = LocalDate.parse(dateTo, formatter);
 
         StringBuilder report = new StringBuilder();
         report.append("Report for period ").append(dateFrom).append(" - ").append(dateTo)
@@ -19,14 +22,13 @@ public class SalaryInfo {
         for (String name : names) {
             int totalSalary = 0;
             for (String record : data) {
-                String[] recordString = record.split(" ");
-                String recordDate = recordString[0];
-                String recordName = recordString[1];
-                int hours = Integer.parseInt(recordString[2]);
-                int rate = Integer.parseInt(recordString[3]);
+                String[] recordString = record.split(RECORD_SEPARATOR);
+                String recordDate = recordString[DATE_INDEX];
+                String recordName = recordString[NAME_INDEX];
+                int hours = Integer.parseInt(recordString[HOURS_INDEX]);
+                int rate = Integer.parseInt(recordString[RATE_INDEX]);
 
-                LocalDate entryLocalDate = LocalDate.parse(recordDate,
-                        DateTimeFormatter.ofPattern(europeanDatePattern));
+                LocalDate entryLocalDate = LocalDate.parse(recordDate, formatter);
 
                 if (recordName.equals(name) && !entryLocalDate.isBefore(fromDate)
                         && !entryLocalDate.isAfter(toDate)) {
