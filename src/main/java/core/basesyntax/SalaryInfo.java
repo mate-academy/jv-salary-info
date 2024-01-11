a7d1fd16f759e99d3c08548cca3ce7567b60e908
Yuliia <innishastudy@gmail.com>
2024-01-11 14:00:55 +0200

    removed arrays and catch for RuntimeExceptions

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -2,50 +2,28 @@ package core.basesyntax;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
 
 public class SalaryInfo {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
     private String lineSeparator = System.lineSeparator();
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        if (validateInput(names, data, dateFrom, dateTo)) {
-            throw new DataFormatException("Data is empty");
-        }
-
-        LocalDate[] dataDate = new LocalDate[data.length];
-        String[] dataName = new String[data.length];
-        String[] dataHours = new String[data.length];
-        String[] dataSalaryPerHour = new String[data.length];
-
-        try {
-            for (int i = 0; i < data.length; i++) {
-                String[] dataSplit = data[i].split(" ");
-                dataDate[i] = LocalDate.parse(dataSplit[0], FORMATTER);
-                dataName[i] = dataSplit[1];
-                dataHours[i] = dataSplit[2];
-                dataSalaryPerHour[i] = dataSplit[3];
-            }
-        } catch (DateTimeParseException e) {
-            throw new DataFormatException("Wrong date format");
-        }
-
-        LocalDate dateFromLocalDate = parseDateFrom(dateFrom);
-        LocalDate dateToLocalDate = parseDateTo(dateTo);
-
-        StringBuilder result = new StringBuilder();
-        result.append("Report for period ")
-                .append(dateFrom)
+        validateInput(names, data, dateFrom, dateTo);
+        StringBuilder result = new StringBuilder("Report for period ");
+        result.append(dateFrom)
                 .append(" - ")
                 .append(dateTo);
 
         for (String name : names) {
             int sum = 0;
             for (int i = 0; i < data.length; i++) {
-                if (name.equals(dataName[i])
-                        && dataDate[i].compareTo(dateFromLocalDate) >= 0
-                        && dataDate[i].compareTo(dateToLocalDate) <= 0) {
-                    sum += Integer.valueOf(dataHours[i]) * Integer.valueOf(dataSalaryPerHour[i]);
+                String[] dataLine = data[i].split(" ");
+                if (dataLine[1].equals(name)
+                        && LocalDate.parse(dataLine[0], FORMATTER)
+                            .compareTo(LocalDate.parse(dateFrom, FORMATTER)) >= 0
+                        && LocalDate.parse(dataLine[0], FORMATTER)
+                            .compareTo(LocalDate.parse(dateTo, FORMATTER)) <= 0) {
+                    sum += Integer.valueOf(dataLine[2]) * Integer.valueOf(dataLine[3]);
                 }
             }
             result.append(lineSeparator)
@@ -56,23 +34,10 @@ public class SalaryInfo {
         return result.toString();
     }
 
-    public boolean validateInput(String[] names, String[] data, String dateFrom, String dateTo) {
-        return names.length == 0 || data.length == 0 || dateFrom == null || dateTo == null;
-    }
-
-    public LocalDate parseDateFrom(String dateFrom) {
-        try {
-            return LocalDate.parse(dateFrom, FORMATTER);
-        } catch (DateTimeParseException e) {
-            throw new DataFormatException("Wrong from date format");
-        }
-    }
-
-    public LocalDate parseDateTo(String dateTo) {
-        try {
-            return LocalDate.parse(dateTo, FORMATTER);
-        } catch (DateTimeParseException e) {
-            throw new DataFormatException("Wrong to date format");
+    public void validateInput(String[] names, String[] data, String dateFrom, String dateTo) {
+        if (names.length == 0 || data.length == 0 || dateFrom == null || dateTo == null) {
+            throw new DataFormatException("Data is empty");
         }
     }
 }
+
