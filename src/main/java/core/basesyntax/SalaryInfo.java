fcf75d50732ff5d68761bbd61b05e4bf8127bd49
bodya4243 <105318502+bodya4243@users.noreply.github.com>
2024-01-15 22:34:35 +0200

    the "getSalaryInfo" method is implemented in the "SalaryInfo" class.
    "DateTimeFormatter" was added.
    
    added "temp" array to work with
    with array "data".
    
    a logical block of salary calculation has been added.
    
    "StringBuilder" was added to generate the result

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,47 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDate firstDate = LocalDate.parse(dateFrom, formatter);
+        LocalDate secondDate = LocalDate.parse(dateTo, formatter);
+
+        String[] temp;
+        LocalDate dataDate;
+        int firstIncome;
+        int[] incomeInfo = new int[3];
+
+        for (int c = 0; c < names.length; c++) {
+            firstIncome = 0;
+
+            for (String datum : data) {
+                temp = datum.split(" ");
+                dataDate = LocalDate.parse(temp[0], formatter);
+
+                if (temp[1].equals(names[c])) {
+                    if ((dataDate.isEqual(firstDate) || dataDate.isEqual(secondDate))
+                            || (dataDate.isAfter(firstDate) && dataDate.isBefore(secondDate))) {
+                        firstIncome += Integer.parseInt(temp[2]) * Integer.parseInt(temp[3]);
+                        incomeInfo[c] = firstIncome;
+                    } else {
+                        incomeInfo[c] += 0;
+                    }
+                }
+            }
+        }
+
+        StringBuilder formattedInfo = new StringBuilder();
+        formattedInfo.append("Report for period ").append(dateFrom)
+                .append(" - ").append(dateTo).append(System.lineSeparator());
+
+        for (int i = 0; i < names.length; i++) {
+            formattedInfo.append(names[i]).append(" - ")
+                    .append(incomeInfo[i]).append(System.lineSeparator());
+        }
+
+        return formattedInfo.toString().trim();
     }
 }
