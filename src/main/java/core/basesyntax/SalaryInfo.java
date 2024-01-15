06a33d6748c63dd0373408c4669c8045136a546e
YEvhen Korobka <korobkaevgeny@gmail.com>
2024-01-15 21:59:41 +0200

    Fix constants and delete one "for" loop in getSalaryInfo

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,35 +5,34 @@ import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     public static final String FORMAT = "dd.MM.yyyy";
-    public static final int DATE = 0;
-    public static final int NAME = 1;
-    public static final int HOUR = 2;
-    public static final int SALARY = 3;
+    public static final String SEPARATOR = " ";
+    public static final int DATE_INDEX = 0;
+    public static final int NAME_INDEX = 1;
+    public static final int HOUR_INDEX = 2;
+    public static final int SALARY_INDEX = 3;
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern(FORMAT);
         LocalDate fromDate = LocalDate.parse(dateFrom, dateFormatter);
         LocalDate toDate = LocalDate.parse(dateTo, dateFormatter);
-        int[] totalSalary = new int[names.length];
-        for (int i = 0; i < data.length; i++) {
-            String[] splitDate = data[i].split(" ");
-            LocalDate actualDate = LocalDate.parse(splitDate[DATE], dateFormatter);
-            if (actualDate.isEqual(fromDate) || actualDate.isEqual(toDate)
-                    || (actualDate.isAfter(fromDate) && actualDate.isBefore(toDate))) {
-                for (int j = 0; j < names.length; j++) {
-                    if (splitDate[NAME].equals(names[j])) {
-                        totalSalary[j] += Integer.parseInt(splitDate[HOUR])
-                                * Integer.parseInt(splitDate[SALARY]);
-                        break;
+        StringBuilder result = new StringBuilder();
+        result.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
+        int totalSalary;
+        for (String name: names) {
+            totalSalary = 0;
+            for (String datum : data) {
+                String[] splitDate = datum.split(SEPARATOR);
+                if (name.equals(splitDate[NAME_INDEX])) {
+                    LocalDate actualDate = LocalDate.parse(splitDate[DATE_INDEX], dateFormatter);
+                    if (actualDate.isEqual(fromDate) || actualDate.isEqual(toDate)
+                            || (actualDate.isAfter(fromDate) && actualDate.isBefore(toDate))) {
+                        totalSalary += Integer.parseInt(splitDate[HOUR_INDEX])
+                                * Integer.parseInt(splitDate[SALARY_INDEX]);
                     }
                 }
             }
-
-        }
-        StringBuilder result = new StringBuilder();
-        result.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
-        for (int i = 0; i < names.length; i++) {
             result.append(System.lineSeparator());
-            result.append(names[i]).append(" - ").append(totalSalary[i]);
+            result.append(name).append(" - ").append(totalSalary);
         }
         return result.toString();
     }
