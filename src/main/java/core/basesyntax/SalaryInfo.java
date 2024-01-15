f5603dea665e85009146ef5f3ec03ec3bf019e2b
YEvhen Korobka <korobkaevgeny@gmail.com>
2024-01-15 13:21:12 +0200

    Add constants in getSalaryInfo method

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,20 +4,25 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    public static final String FORMAT = "dd.MM.yyyy";
+    public static final int DATE = 0;
+    public static final int NAME = 1;
+    public static final int HOUR = 2;
+    public static final int SALARY = 3;
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern(FORMAT);
         LocalDate fromDate = LocalDate.parse(dateFrom, dateFormatter);
         LocalDate toDate = LocalDate.parse(dateTo, dateFormatter);
         int[] totalSalary = new int[names.length];
         for (int i = 0; i < data.length; i++) {
             String[] splitDate = data[i].split(" ");
-            LocalDate actualDate = LocalDate.parse(splitDate[0], dateFormatter);
+            LocalDate actualDate = LocalDate.parse(splitDate[DATE], dateFormatter);
             if (actualDate.isEqual(fromDate) || actualDate.isEqual(toDate)
                     || (actualDate.isAfter(fromDate) && actualDate.isBefore(toDate))) {
                 for (int j = 0; j < names.length; j++) {
-                    if (splitDate[1].equals(names[j])) {
-                        totalSalary[j] += Integer.parseInt(splitDate[2])
-                                * Integer.parseInt(splitDate[3]);
+                    if (splitDate[NAME].equals(names[j])) {
+                        totalSalary[j] += Integer.parseInt(splitDate[HOUR])
+                                * Integer.parseInt(splitDate[SALARY]);
                         break;
                     }
                 }
