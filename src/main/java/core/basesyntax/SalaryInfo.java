e13debd41d09504d4c065f8d337cb9a6939c8dfc
Marharyta Salii <margarita12122015@gmail.com>
2024-01-16 19:22:03 +0200

    SalaryInfo_done

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,33 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int HOURS_INDEX = 2;
+    private static final int HOUR_SALARY_INDEX = 3;
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDate dateF = LocalDate.parse(dateFrom, formatter);
+        LocalDate dateT = LocalDate.parse(dateTo, formatter);
+        StringBuilder result = new StringBuilder("Report for period " + dateFrom + " - " + dateTo);
+        for (String name : names) {
+            int salary = 0;
+            for (String datum : data) {
+                String[] splitData = datum.split(" ");
+                LocalDate date = LocalDate.parse(splitData[DATE_INDEX], formatter);
+                if (name.equals(splitData[NAME_INDEX])
+                        && ((date.isAfter(dateF) && date.isBefore(dateT))
+                        || (date.isEqual(dateF) || date.isEqual(dateT)))) {
+                    salary += Integer.parseInt(splitData[HOURS_INDEX])
+                            * Integer.parseInt(splitData[HOUR_SALARY_INDEX]);
+                }
+            }
+            result.append("\n" + name + " - " + salary);
+        }
+        return result.toString();
     }
 }
