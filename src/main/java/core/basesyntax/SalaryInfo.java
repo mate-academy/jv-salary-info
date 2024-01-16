0d52de70015589a9cb22c53ad0c44c68845692e7
Marharyta Salii <margarita12122015@gmail.com>
2024-01-16 23:07:36 +0200

    SalaryInfo_fixed

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -8,25 +8,25 @@ public class SalaryInfo {
     private static final int NAME_INDEX = 1;
     private static final int HOURS_INDEX = 2;
     private static final int HOUR_SALARY_INDEX = 3;
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final String SEPARATOR = " ";
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        LocalDate dateF = LocalDate.parse(dateFrom, formatter);
-        LocalDate dateT = LocalDate.parse(dateTo, formatter);
+        LocalDate localDateFrom = LocalDate.parse(dateFrom, formatter);
+        LocalDate localDateTo = LocalDate.parse(dateTo, formatter);
         StringBuilder result = new StringBuilder("Report for period " + dateFrom + " - " + dateTo);
         for (String name : names) {
             int salary = 0;
             for (String datum : data) {
-                String[] splitData = datum.split(" ");
+                String[] splitData = datum.split(SEPARATOR);
                 LocalDate date = LocalDate.parse(splitData[DATE_INDEX], formatter);
                 if (name.equals(splitData[NAME_INDEX])
-                        && ((date.isAfter(dateF) && date.isBefore(dateT))
-                        || (date.isEqual(dateF) || date.isEqual(dateT)))) {
+                        && !date.isAfter(localDateTo) && !date.isBefore(localDateFrom)) {
                     salary += Integer.parseInt(splitData[HOURS_INDEX])
                             * Integer.parseInt(splitData[HOUR_SALARY_INDEX]);
                 }
             }
-            result.append("\n" + name + " - " + salary);
+            result.append(System.lineSeparator() + name + " - " + salary);
         }
         return result.toString();
     }
