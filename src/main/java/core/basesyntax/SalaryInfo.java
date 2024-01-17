2ccfa1d482218de15ad94c1c3ab6e4afd8790e3b
Kateryna Riznychenko <lyqvin.kr@gmail.com>
2024-01-17 19:53:54 +0200

    added _INDEX suffixes for clarity

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,10 +4,10 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private static final int DATE = 0;
-    private static final int NAME = 1;
-    private static final int WORKING_HOURS = 2;
-    private static final int INCOME_PER_HOUR = 3;
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int WORKING_HOURS_INDEX = 2;
+    private static final int INCOME_PER_HOUR_INDEX = 3;
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public static String getSalaryInfo(String[] names, String[] data,
@@ -23,10 +23,10 @@ public class SalaryInfo {
 
             for (int i = 0; i < data.length; i++) {
                 String[] splittedData = data[i].split(" ");
-                if (name.equals(splittedData[NAME]) && isDateInRange(splittedData[DATE],
+                if (name.equals(splittedData[NAME_INDEX]) && isDateInRange(splittedData[DATE_INDEX],
                         dateFrom, dateTo)) {
-                    int hours = Integer.parseInt(splittedData[WORKING_HOURS]);
-                    int salaryPerHour = Integer.parseInt(splittedData[INCOME_PER_HOUR]);
+                    int hours = Integer.parseInt(splittedData[WORKING_HOURS_INDEX]);
+                    int salaryPerHour = Integer.parseInt(splittedData[INCOME_PER_HOUR_INDEX]);
                     int salary = hours * salaryPerHour;
                     totalSalary += salary;
                 }
