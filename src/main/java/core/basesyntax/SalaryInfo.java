7311d42099f66e4627084fe9de9164f005d896e8
dreo <mishkin878@gmail.com>
2024-01-18 17:09:50 +0200

    move index to the constant

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,10 +4,15 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int HOURS_INDEX = 2;
+    private static final int RATE_INDEX = 3;
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        LocalDate fromDate = LocalDate.parse(dateFrom, formatter);
-        LocalDate toDate = LocalDate.parse(dateTo, formatter);
+        LocalDate fromDate = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate toDate = LocalDate.parse(dateTo, FORMATTER);
         StringBuilder result = new StringBuilder();
         result.append("Report for period ").append(dateFrom).append(" - ")
                 .append(dateTo).append(System.lineSeparator());
@@ -17,10 +22,11 @@ public class SalaryInfo {
                 int salary = 0;
                 for (String dataFromData : data) {
                     String[] dataEach = dataFromData.split(" ");
-                    LocalDate dataDate = LocalDate.parse(dataEach[0], formatter);
-                    if (dataEach[1].equals(name) && !dataDate.isBefore(fromDate)
+                    LocalDate dataDate = LocalDate.parse(dataEach[DATE_INDEX], FORMATTER);
+                    if (dataEach[NAME_INDEX].equals(name) && !dataDate.isBefore(fromDate)
                             && !dataDate.isAfter(toDate)) {
-                        salary += Integer.parseInt(dataEach[2]) * Integer.parseInt(dataEach[3]);
+                        salary += Integer.parseInt(dataEach[HOURS_INDEX])
+                                * Integer.parseInt(dataEach[RATE_INDEX]);
                     }
                 }
                 result.append(name).append(" - ").append(salary);
