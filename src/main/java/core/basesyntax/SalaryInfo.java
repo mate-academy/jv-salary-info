4dd953bd8b45ece758cddb49328f1f9d26814e0b
dreo <mishkin878@gmail.com>
2024-01-19 19:36:09 +0200

    fix result separator

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -13,11 +13,13 @@ public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate fromDate = LocalDate.parse(dateFrom, FORMATTER);
         LocalDate toDate = LocalDate.parse(dateTo, FORMATTER);
-        StringBuilder result = new StringBuilder();
-        result.append("Report for period ").append(dateFrom).append(" - ")
-                .append(dateTo).append(System.lineSeparator());
+        StringBuilder result = new StringBuilder()
+                .append("Report for period ")
+                .append(dateFrom)
+                .append(" - ")
+                .append(dateTo);
 
-        if (fromDate != null && toDate != null && names != null && data != null) {
+        if (names != null && data != null) {
             for (String name : names) {
                 int salary = 0;
                 for (String dataFromData : data) {
@@ -29,10 +31,10 @@ public class SalaryInfo {
                                 * Integer.parseInt(dataEach[RATE_INDEX]);
                     }
                 }
-                result.append(name).append(" - ").append(salary);
-                if (!name.equals(names[names.length - 1])) {
-                    result.append(System.lineSeparator());
-                }
+                result
+                        .append(System.lineSeparator())
+                        .append(name).append(" - ")
+                        .append(salary);
             }
         }
         return result.toString();
