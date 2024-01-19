4608ddae34a2a4cea9256b063d39a66c7337911a
moskalenkosergii <moskalenko.s.work@gmail.com>
2024-01-19 10:52:16 +0100

    delete unused methods, refactoring gatSalaryInfo and getSalarySumEmployee methods

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,40 +4,32 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private String[] names;
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
     private String[] data;
     private String dateFrom;
     private String dateTo;
 
-    public String getSalaryInfo(String[] names, String[] data,
-                                String dateFrom, String dateTo) {
-        this.names = names;
+    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         this.data = data;
         this.dateFrom = dateFrom;
         this.dateTo = dateTo;
-
-        return getDrawResultSalaries();
-    }
-
-    private String getDrawResultSalaries() {
         StringBuilder result = new StringBuilder();
 
-        result.append("Report for period ").append(dateFrom).append(" - ")
-                .append(dateTo).append(System.lineSeparator());
-        for (int i = 0; i < names.length; i++) {
-            result.append(names[i]).append(" - ")
-                    .append(getSalarySumEmployee()[i]);
-            if (i != names.length - 1) {
-                result.append(System.lineSeparator());
-            }
+        result.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
+
+        for (String name : names) {
+            result.append(System.lineSeparator())
+                    .append(name)
+                    .append(" - ")
+                    .append(getSalarySumEmployee(name));
         }
         return result.toString();
     }
 
-    private int[] getSalarySumEmployee() {
+    private int getSalarySumEmployee(String name) {
         LocalDate dateFromFormat = getFormatData(dateFrom);
         LocalDate dateToFormat = getFormatData(dateTo);
-        int[] salarySumEmployee = initializeArray(names);
+        int salarySumEmployee = 0;
 
         for (String datas : data) {
             String[] splitData = datas.split(" ");
@@ -49,10 +41,8 @@ public class SalaryInfo {
             if (dateWorkEmployee.isAfter(dateFromFormat) && dateWorkEmployee.isBefore(dateToFormat)
                     || dateWorkEmployee.isEqual(dateFromFormat)
                     || dateWorkEmployee.isEqual(dateToFormat)) {
-                for (int i = 0; i < names.length; i++) {
-                    if (names[i].equals(nameWorkEmployee)) {
-                        salarySumEmployee[i] += salaryPerHours * workHours;
-                    }
+                if (name.equals(nameWorkEmployee)) {
+                    salarySumEmployee += salaryPerHours * workHours;
                 }
             }
         }
@@ -60,21 +50,6 @@ public class SalaryInfo {
     }
 
     private LocalDate getFormatData(String date) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-
-        try {
-            return LocalDate.parse(date, formatter);
-        } catch (Exception e) {
-            System.out.println("Error parsing the date: " + e.getMessage());
-            return null;
-        }
-    }
-
-    private int[] initializeArray(String[] count) {
-        int[] initializeArray = new int[count.length];
-        for (int i = 0; i < count.length; i++) {
-            initializeArray[i] = 0;
-        }
-        return initializeArray;
+        return LocalDate.parse(date, FORMATTER);
     }
 }
