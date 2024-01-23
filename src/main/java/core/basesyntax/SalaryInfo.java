52e5c6e4c224422e26b1a730ac37833f735ac3c2
Lolita <ilolita796@gmail.com>
2024-01-23 23:59:53 +0200

    fixed issues

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -13,43 +13,34 @@ public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder builder = new StringBuilder("Report for period ");
         builder.append(dateFrom).append(" - ").append(dateTo);
-        String date;
         for (String name : names) {
             int salary = 0;
             for (String datum : data) {
                 String[] currentData = datum.split(" ");
+                int hours = Integer.parseInt(currentData[INDEX_HOURS]);
+                int income = Integer.parseInt(currentData[INDEX_INCOME]);
+                String date = currentData[INDEX_DATE];
                 try {
-                    date = getDateByName(currentData, name);
+                    if (currentData[INDEX_NAME].equals(name)) {
+                        if (getMonth(dateFrom) < getMonth(dateTo)
+                                && getMonth(date) == getMonth(dateFrom)
+                                && getDay(date) >= getDay(dateFrom)
+                                || getMonth(date) == getMonth(dateTo)
+                                && getDay(date) <= getDay(dateTo)
+                                || getDay(date) >= getDay(dateFrom)
+                                && getDay(date) <= getDay(dateTo)) {
+                            salary += hours * income;
+                        }
+                    } else {
+                        throw new NoSuchNameException("No such name");
+                    }
                 } catch (NoSuchNameException e) {
                     continue;
                 }
-                salary = getSalary(dateFrom, dateTo, date, salary, currentData);
             }
             builder.append(System.lineSeparator()).append(name).append(" - ").append(salary);
         }
         return builder.toString();
-    }
-
-    public int getSalary(String dateFrom, String dateTo,
-                         String date, int salary, String[] currentData) {
-        int hours = Integer.parseInt(currentData[INDEX_HOURS]);
-        int income = Integer.parseInt(currentData[INDEX_INCOME]);
-        if (getMonth(dateFrom) < getMonth(dateTo)) {
-            if (getMonth(date) == getMonth(dateFrom) && getDay(date) >= getDay(dateFrom)
-                    || getMonth(date) == getMonth(dateTo) && getDay(date) <= getDay(dateTo)) {
-                salary += hours * income;
-            }
-        } else if (getDay(date) >= getDay(dateFrom) && getDay(date) <= getDay(dateTo)) {
-            salary += hours * income;
-        }
-        return salary;
-    }
-
-    public String getDateByName(String[] data, String name) throws NoSuchNameException {
-        if (data[INDEX_NAME].equals(name)) {
-            return data[INDEX_DATE];
-        }
-        throw new NoSuchNameException("No such name");
     }
 
     public int getDay(String date) {
