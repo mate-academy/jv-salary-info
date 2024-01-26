d68d72763fe31c6af687ed9e5230b5f689332056
Vitaliy <vitaliyufc@gmail.com>
2024-01-26 15:05:07 +0300

    Merge remote-tracking branch 'origin/SalaryInfo' into SalaryInfo

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -8,6 +8,7 @@ public class SalaryInfo {
             = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
+
         LocalDate startDate;
         LocalDate endDate;
         startDate = LocalDate.parse(dateFrom, dateTimeFormatter);
@@ -16,8 +17,7 @@ public class SalaryInfo {
         StringBuilder builder = new StringBuilder();
         builder.append("Report for period ")
                 .append(dateFrom).append(" - ")
-                .append(dateTo)
-                .append(System.lineSeparator());
+                .append(dateTo);
 
         for (String name : names) {
             int totalEarned = 0;
@@ -36,27 +36,8 @@ public class SalaryInfo {
                     totalEarned += hoursWorkedOfDay * rateOfDay;
                 }
             }
-            builder.append(name).append(" - ").append(totalEarned).append(System.lineSeparator());
+            builder.append(System.lineSeparator()).append(name).append(" - ").append(totalEarned);
         }
         return builder.toString();
-    }
-
-    public static void main(String[] args) {
-        SalaryInfo salaryInfo = new SalaryInfo();
-        String[] names = {"Іван", "Андрій", "Катя", "Андрій", "Іван"};
-        String[] data = {
-                "26.04.2019 Іван 4 50",
-                "05.04.2019 Андрій 3 200",
-                "10.04.2019 Іван 7 100",
-                "22.04.2019 Катерина 9 100",
-                "25.06.2019 Іван 11 50",
-                "26.04.2019 Андрій 3 150",
-                "13.02.2019 Іван 7 100",
-                "26.04.2019 Катя 9 100"
-        };
-        String dateFrom = "01.04.2019";
-        String dateTom = "30.04.2019";
-        String salary = salaryInfo.getSalaryInfo(names, data, dateFrom, dateTom);
-        System.out.println(salary);
     }
 }
