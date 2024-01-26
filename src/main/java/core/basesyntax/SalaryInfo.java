135cc15a33daa07fd21704c5ee55b0d0c1e7b151
Vitaliy <vitaliyufc@gmail.com>
2024-01-26 15:05:07 +0300

    FixedTaskSalary

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -6,8 +6,13 @@ import java.time.format.DateTimeFormatter;
 public class SalaryInfo {
     private static final DateTimeFormatter dateTimeFormatter
             = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int DATES = 0;
+    private static final int NAMES = 1;
+    private static final int HOURS = 2;
+    private static final int RATE = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
+
         LocalDate startDate;
         LocalDate endDate;
         startDate = LocalDate.parse(dateFrom, dateTimeFormatter);
@@ -16,18 +21,17 @@ public class SalaryInfo {
         StringBuilder builder = new StringBuilder();
         builder.append("Report for period ")
                 .append(dateFrom).append(" - ")
-                .append(dateTo)
-                .append(System.lineSeparator());
+                .append(dateTo);
 
         for (String name : names) {
             int totalEarned = 0;
 
             for (String input : data) {
                 String[] parts = input.split(" ");
-                String inputDate = parts[0];
-                String inputName = parts[1];
-                int hoursWorkedOfDay = Integer.parseInt(parts[2]);
-                int rateOfDay = Integer.parseInt(parts[3]);
+                String inputDate = parts[DATES];
+                String inputName = parts[NAMES];
+                int hoursWorkedOfDay = Integer.parseInt(parts[HOURS]);
+                int rateOfDay = Integer.parseInt(parts[RATE]);
 
                 LocalDate localDate = LocalDate.parse(inputDate, dateTimeFormatter);
 
@@ -36,27 +40,8 @@ public class SalaryInfo {
                     totalEarned += hoursWorkedOfDay * rateOfDay;
                 }
             }
-            builder.append(name).append(" - ").append(totalEarned).append(System.lineSeparator());
+            builder.append(System.lineSeparator()).append(name).append(" - ").append(totalEarned);
         }
         return builder.toString();
-    }
-
-    public static void main(String[] args) {
-        SalaryInfo salaryInfo = new SalaryInfo();
-        String[] names = {"Іван", "Андрій", "Катя", "Андрій", "Іван"};
-        String[] data = {
-                "26.04.2019 Іван 4 50",
-                "05.04.2019 Андрій 3 200",
-                "10.04.2019 Іван 7 100",
-                "22.04.2019 Катерина 9 100",
-                "25.06.2019 Іван 11 50",
-                "26.04.2019 Андрій 3 150",
-                "13.02.2019 Іван 7 100",
-                "26.04.2019 Катя 9 100"
-        };
-        String dateFrom = "01.04.2019";
-        String dateTom = "30.04.2019";
-        String salary = salaryInfo.getSalaryInfo(names, data, dateFrom, dateTom);
-        System.out.println(salary);
     }
 }
