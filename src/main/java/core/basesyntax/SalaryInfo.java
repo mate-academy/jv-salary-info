2a25a6e9aafc0bedc91442355e35f33c17184015
Yaromyr Matviichuk <yaromyrmatviichuk@gmail.com>
2024-01-30 21:42:43 +0200

    made constants, removed empty line, used different methods

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,13 +4,19 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    private static final DateTimeFormatter DATE_FORMATTER
+            = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final String SEPARATOR = " ";
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int HOURS_INDEX = 2;
+    private static final int INCOME_INDEX = 3;
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder result = new StringBuilder();
-        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
-        LocalDate fromDate = LocalDate.parse(dateFrom, dateFormat);
-        LocalDate toDate = LocalDate.parse(dateTo, dateFormat);
-
+        LocalDate fromDate = LocalDate.parse(dateFrom, DATE_FORMATTER);
+        LocalDate toDate = LocalDate.parse(dateTo, DATE_FORMATTER);
         result.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
 
         for (String name : names) {
@@ -26,21 +32,19 @@ public class SalaryInfo {
         int totalSalary = 0;
 
         for (String inception : data) {
-            String[] parts = inception.split(" ");
-            String entryDateOfEmployee = parts[0];
-            String entryName = parts[1];
-            int hours = Integer.parseInt(parts[2]);
-            int income = Integer.parseInt(parts[3]);
+            String[] parts = inception.split(SEPARATOR);
+            String entryDateOfEmployee = parts[DATE_INDEX];
+            String entryName = parts[NAME_INDEX];
+            int hours = Integer.parseInt(parts[HOURS_INDEX]);
+            int income = Integer.parseInt(parts[INCOME_INDEX]);
 
-            LocalDate entryDate = LocalDate.parse(entryDateOfEmployee,
-                    DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+            LocalDate entryDate = LocalDate.parse(entryDateOfEmployee, DATE_FORMATTER);
 
-            if (entryName.equals(employeeName) && entryDate.compareTo(fromDate) >= 0
-                    && entryDate.compareTo(toDate) <= 0) {
+            if (entryName.equals(employeeName) && !entryDate.isBefore(fromDate)
+                    && !entryDate.isAfter(toDate)) {
                 totalSalary += (hours * income);
             }
         }
         return totalSalary;
     }
 }
-
