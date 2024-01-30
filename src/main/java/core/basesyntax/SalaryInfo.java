dcef7a29afd983ec73a2df1dd9043780478ff072
Oleksii Bashkirov <bashkirov.o.u@ukr.net>
2024-01-30 22:53:43 +0200

    made requested changes #2: add separator at the beginning of the line removed cumbersome if created constructor for LocalDate parseDate for encapsulating the parsing logic.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,26 +4,25 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private static final DateTimeFormatter FORMATTER = DateTimeFormatter
-            .ofPattern("dd.MM.yyyy");
     private static final int DATE_INDEX = 0;
     private static final int NAME_INDEX = 1;
     private static final int HOURS_INDEX = 2;
     private static final int PER_HOUR_INDEX = 3;
-    private static final String REPORT_SEPARATOR = " - ";
+    private static final String SEPARATOR = " - ";
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter
+            .ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         if (dateFrom == null || dateTo == null || names.length == 0 || data.length == 0) {
             return "Invalid data. Dates should not be null,"
                     + "also names and data should not be empty";
         }
-        LocalDate fromDate = LocalDate.parse(dateFrom, FORMATTER);
-        LocalDate toDate = LocalDate.parse(dateTo, FORMATTER);
+        LocalDate fromDate = parseDate(dateFrom);
+        LocalDate toDate = parseDate(dateTo);
         StringBuilder result = new StringBuilder("Report for period ")
                     .append(dateFrom)
-                    .append(REPORT_SEPARATOR)
-                    .append(dateTo)
-                    .append(System.lineSeparator());
+                    .append(SEPARATOR)
+                    .append(dateTo);
 
         for (int i = 0; i < names.length; i++) {
             int earningsForName = 0;
@@ -41,13 +40,15 @@ public class SalaryInfo {
                     }
                 }
             }
-            result.append(names[i])
-                        .append(REPORT_SEPARATOR)
-                        .append(earningsForName);
-            if (i < names.length - 1) {
-                result.append(System.lineSeparator());
-            }
+            result.append(System.lineSeparator())
+                    .append(names[i])
+                    .append(SEPARATOR)
+                    .append(earningsForName);
         }
         return result.toString();
+    }
+
+    private LocalDate parseDate(String dateString) {
+        return LocalDate.parse(dateString, FORMATTER);
     }
 }
