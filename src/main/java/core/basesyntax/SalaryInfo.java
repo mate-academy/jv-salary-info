3e2c48e0edb54d6ada2709905a31e202f6109896
Evgeniy Chernonog <chernonog.evgeniy@gmail.com>
2024-01-31 09:55:52 +0200

    added hw-solution

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,40 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        Map<String, Integer> mapOfEmployees = new LinkedHashMap<>();
+        for (String name : names) {
+            mapOfEmployees.put(name, 0);
+        }
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        for (String datum : data) {
+            String[] seperatedLine = datum.split(" ");
+            String checkedDataFromLine = seperatedLine[0];
+            String nameOfEmployee = seperatedLine[1];
+            int totalSalary = Integer.parseInt(seperatedLine[2])
+                    * Integer.parseInt(seperatedLine[3]);
+            if ((LocalDate.parse(checkedDataFromLine, formatter)
+                    .isAfter(LocalDate.parse(dateFrom, formatter))
+                    || LocalDate.parse(checkedDataFromLine, formatter)
+                    .isEqual(LocalDate.parse(dateFrom, formatter)))
+                    && (LocalDate.parse(checkedDataFromLine, formatter)
+                    .isBefore(LocalDate.parse(dateTo, formatter))
+                    || LocalDate.parse(checkedDataFromLine, formatter)
+                    .equals(LocalDate.parse(dateTo, formatter)))) {
+                mapOfEmployees.merge(nameOfEmployee, totalSalary, Integer::sum);
+            }
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Map.Entry<String, Integer> stringIntegerEntry : mapOfEmployees.entrySet()) {
+            stringBuilder.append(stringIntegerEntry.getKey()).append(" - ")
+                    .append(stringIntegerEntry.getValue()).append("\r\n");
+        }
+        return "Report for period " + dateFrom + " - " + dateTo + "\r\n"
+                + stringBuilder.toString().trim();
     }
 }
