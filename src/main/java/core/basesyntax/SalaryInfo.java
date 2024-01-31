04ac350ffcebe8733b4d93367d851b80b7223937
Evgeniy Chernonog <chernonog.evgeniy@gmail.com>
2024-01-31 10:20:10 +0200

    added finally hw-solution

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -6,7 +6,8 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class SalaryInfo {
-   private final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         Map<String, Integer> mapOfEmployees = new LinkedHashMap<>();
         for (String name : names) {
@@ -19,23 +20,23 @@ public class SalaryInfo {
             String nameOfEmployee = seperatedLine[1];
             int totalSalary = Integer.parseInt(seperatedLine[2])
                     * Integer.parseInt(seperatedLine[3]);
-            if ((LocalDate.parse(checkedData, FORMATTER)
-                    .isAfter(LocalDate.parse(dateFrom, FORMATTER))
-                    || LocalDate.parse(checkedData, FORMATTER)
-                    .isEqual(LocalDate.parse(dateFrom, FORMATTER)))
-                    && (LocalDate.parse(checkedData, FORMATTER)
-                    .isBefore(LocalDate.parse(dateTo, FORMATTER))
-                    || LocalDate.parse(checkedData, FORMATTER)
-                    .equals(LocalDate.parse(dateTo, FORMATTER)))) {
+            if ((LocalDate.parse(checkedData, formatter)
+                    .isAfter(LocalDate.parse(dateFrom, formatter))
+                    || LocalDate.parse(checkedData, formatter)
+                    .isEqual(LocalDate.parse(dateFrom, formatter)))
+                    && (LocalDate.parse(checkedData, formatter)
+                    .isBefore(LocalDate.parse(dateTo, formatter))
+                    || LocalDate.parse(checkedData, formatter)
+                    .equals(LocalDate.parse(dateTo, formatter)))) {
                 mapOfEmployees.merge(nameOfEmployee, totalSalary, Integer::sum);
             }
         }
         StringBuilder stringBuilder = new StringBuilder();
         for (Map.Entry<String, Integer> stringIntegerEntry : mapOfEmployees.entrySet()) {
             stringBuilder.append(stringIntegerEntry.getKey()).append(" - ")
-                    .append(stringIntegerEntry.getValue()).append("\n");
+                    .append(stringIntegerEntry.getValue()).append("\r\n");
         }
-        return "Report for period " + dateFrom + " - " + dateTo + "\n"
+        return "Report for period " + dateFrom + " - " + dateTo + "\r\n"
                 + stringBuilder.toString().trim();
     }
 }
