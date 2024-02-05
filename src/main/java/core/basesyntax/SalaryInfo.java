e78168b28ae3a9f9fe6130c2fd46b19cc87c1c0e
rk0v <rz20@ukr.net>
2024-02-05 22:19:55 +0200

    changes after review

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -6,6 +6,10 @@ import java.time.format.DateTimeFormatter;
 public class SalaryInfo {
     private static final DateTimeFormatter DATE_FORMATTER
             = DateTimeFormatter.ofPattern("dd.MM.uuuu");
+    private static final int INDEX_OF_DATE = 0;
+    private static final int INDEX_OF_NAME = 1;
+    private static final int INDEX_OF_HOURS = 2;
+    private static final int INDEX_OF_INCOME_PER_HOUR = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate parseDateFrom = LocalDate.parse(dateFrom, DATE_FORMATTER);
@@ -15,17 +19,20 @@ public class SalaryInfo {
             int moneyEarned = 0;
             for (String stringOfData : data) {
                 String[] splitData = stringOfData.split(" ");
-                LocalDate splitDataDate = LocalDate.parse(splitData[0], DATE_FORMATTER);
+                LocalDate splitDataDate = LocalDate.parse(splitData[INDEX_OF_DATE], DATE_FORMATTER);
                 if ((splitDataDate.isEqual(parseDateFrom)
                         || splitDataDate.isAfter(parseDateFrom))
                         && (splitDataDate.isEqual(parseDateTo)
                         || splitDataDate.isBefore(parseDateTo))
-                        && splitData[1].equals(name)) {
-                    moneyEarned += Integer.parseInt(splitData[2])
-                            * Integer.parseInt(splitData[3]);
+                        && splitData[INDEX_OF_NAME].equals(name)) {
+                    moneyEarned += Integer.parseInt(splitData[INDEX_OF_HOURS])
+                            * Integer.parseInt(splitData[INDEX_OF_INCOME_PER_HOUR]);
                 }
             }
-            builder.append(System.lineSeparator()).append(name).append(" - ").append(moneyEarned);
+            builder.append(System.lineSeparator())
+                    .append(name)
+                    .append(" - ")
+                    .append(moneyEarned);
         }
         return builder.toString();
     }
