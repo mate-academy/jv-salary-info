08c393b5e2cc9ee62cd010f7fb942774b477eab3
Danil Ponomarenko <akame.dante13@gmail.com>
2024-02-06 21:31:34 +0200

    Created constants for literals, optimized getSalaryInfo method.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -9,40 +9,42 @@ public class SalaryInfo {
     private static final int NAME_POSITION = 1;
     private static final int WORK_HOURS_POSITION = 2;
     private static final int INCOME_PER_HOUR_POSITION = 3;
+    private static final String REPORT_TITLE = "Report for period ";
+    private static final String DASH = " - ";
+    private static final String SPACE_REGEX = " ";
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder builder = new StringBuilder();
         LocalDate localDateFrom = LocalDate.parse(dateFrom, formatter);
         LocalDate localDateTo = LocalDate.parse(dateTo, formatter);
 
-        builder.append("Report for period ")
+        builder.append(REPORT_TITLE)
                 .append(dateFrom)
-                .append(" - ")
+                .append(DASH)
                 .append(dateTo)
                 .append(System.lineSeparator());
 
         for (String name : names) {
             int totalIncome = 0;
-            for (String daraRow : data) {
-                LocalDate date =
-                        LocalDate.parse(daraRow.split(" ")[DATE_POSITION], formatter);
-                String nameFromData =
-                        daraRow.split(" ")[NAME_POSITION];
-                int workedHours =
-                        Integer.parseInt(daraRow.split(" ")[WORK_HOURS_POSITION]);
-                int incomePerHour =
-                        Integer.parseInt(daraRow.split(" ")[INCOME_PER_HOUR_POSITION]);
-                int totalIncomePerDay = workedHours * incomePerHour;
-
-                if (date.isBefore(localDateTo) && date.isAfter(localDateFrom)
-                        || date.isEqual(localDateTo) || date.isEqual(localDateFrom)) {
-                    if (name.equals(nameFromData)) {
+            for (String dataRow : data) {
+                String[] splitedRow = dataRow.split(SPACE_REGEX);
+                String nameFromData = splitedRow[NAME_POSITION];
+                if (name.equals(nameFromData)) {
+                    LocalDate date =
+                            LocalDate.parse(splitedRow[DATE_POSITION], formatter);
+                    int workedHours =
+                            Integer.parseInt(splitedRow[WORK_HOURS_POSITION]);
+                    int incomePerHour =
+                            Integer.parseInt(splitedRow[INCOME_PER_HOUR_POSITION]);
+                    int totalIncomePerDay = workedHours * incomePerHour;
+                    if (date.isBefore(localDateTo) && date.isAfter(localDateFrom)
+                            || date.isEqual(localDateTo) || date.isEqual(localDateFrom)) {
                         totalIncome += totalIncomePerDay;
                     }
                 }
             }
             builder.append(name)
-                    .append(" - ")
+                    .append(DASH)
                     .append(totalIncome)
                     .append(System.lineSeparator());
         }
