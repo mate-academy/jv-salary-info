aafe6600a9579e446c6e6e2a58a785eecda53681
HAIOVIY <gaevoywork@gmail.com>
2024-02-08 10:51:25 +0200

    add HashMap for same names

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -2,18 +2,17 @@ package core.basesyntax;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         String[] splitedString = new String[4];
         LocalDate parseSplitedDate = null;
-
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
         LocalDate parseDateFrom = LocalDate.parse(dateFrom, formatter);
         LocalDate parseDateTo = LocalDate.parse(dateTo, formatter);
-
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
+        Map<String, Integer> totalHoursMap = new HashMap<>();
         for (String d : data) {
             splitedString = d.split(" ");
             parseSplitedDate = LocalDate.parse(splitedString[0], formatter);
@@ -25,11 +24,15 @@ public class SalaryInfo {
                     if (name.equals(splitedString[1])) {
                         int perHour = Integer.parseInt(splitedString[2])
                                 * Integer.parseInt(splitedString[3]);
-                        stringBuilder.append("/n").append(name).append(" - ").append(perHour);
+                        totalHoursMap.put(name, totalHoursMap.getOrDefault(name, 0) + perHour);
                     }
                 }
-
             }
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
+        for (Map.Entry<String, Integer> entry : totalHoursMap.entrySet()) {
+            stringBuilder.append("\n").append(entry.getKey()).append(" - ").append(entry.getValue());
         }
         return stringBuilder.toString();
     }
