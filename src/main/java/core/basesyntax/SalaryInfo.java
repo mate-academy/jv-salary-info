ebf07badd21735d3fa9ab1b589fbac01b0dbdbc7
Oleksandr Samoylenko <ntskazhu1985@gmail.com>
2024-02-14 12:04:59 +0200

    Fixed using constants in another method and their names. New constants were added. trimZeros method was extracted

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,14 +4,18 @@ import java.time.LocalDate;
 
 public class SalaryInfo {
 
-    private static final int WORK_DAY = 0;
-    private static final int HOURS = 2;
-    private static final int INCOME_PER_HOUR = 3;
-    private static final int WORKER_NAME = 1;
+    private static final int WORK_DAY_INDEX = 0;
+    private static final int DAY_INDEX = 0;
+    private static final int WORKER_NAME_INDEX = 1;
+    private static final int MONTH_INDEX = 1;
+    private static final int HOURS_IDEX = 2;
+    private static final int INCOME_PER_HOUR_INDEX = 3;
+    private static final int YEAR_INDEX = 3;
+
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        StringBuilder info = new StringBuilder("Report for period ");
-        info.append(dateFrom).append(" - ").append(dateTo);
+        StringBuilder result = new StringBuilder("Report for period ");
+        result.append(dateFrom).append(" - ").append(dateTo);
         SalaryInfo salaryInfo = new SalaryInfo();
         LocalDate fromDate = salaryInfo.toLocalDate(dateFrom);
         LocalDate toDate = salaryInfo.toLocalDate(dateTo);
@@ -19,21 +23,21 @@ public class SalaryInfo {
         LocalDate workDayDate;
         int salary;
         for (String name : names) {
-            salary = WORK_DAY;
+            salary = 0;
             for (String datum : data) {
                 splitedData = datum.split(" ");
-                if (!name.equals(splitedData[WORKER_NAME])) {
+                if (!name.equals(splitedData[WORKER_NAME_INDEX])) {
                     continue;
                 }
-                workDayDate = salaryInfo.toLocalDate(splitedData[WORK_DAY]);
+                workDayDate = salaryInfo.toLocalDate(splitedData[WORK_DAY_INDEX]);
                 if (isWorkDayValid(fromDate, toDate, workDayDate)) {
-                    salary += Integer.parseInt(splitedData[HOURS])
-                            * Integer.parseInt(splitedData[INCOME_PER_HOUR]);
+                    salary += Integer.parseInt(splitedData[HOURS_IDEX])
+                            * Integer.parseInt(splitedData[INCOME_PER_HOUR_INDEX]);
                 }
             }
-            info.append(System.lineSeparator()).append(name).append(" - ").append(salary);
+            result.append(System.lineSeparator()).append(name).append(" - ").append(salary);
         }
-        return info.toString();
+        return result.toString();
     }
 
     private boolean isWorkDayValid(LocalDate fromDate, LocalDate toDate, LocalDate workDayDate) {
@@ -44,15 +48,16 @@ public class SalaryInfo {
 
     private LocalDate toLocalDate(String date) {
         String[] splittedDate = date.split("\\.");
-        if (splittedDate[WORK_DAY].charAt(WORK_DAY) == '0') {
-            splittedDate[WORK_DAY] = String.valueOf(splittedDate[WORK_DAY].charAt(WORKER_NAME));
+        trimZeros(splittedDate, DAY_INDEX);
+        trimZeros(splittedDate, MONTH_INDEX);
+        return LocalDate.of(Integer.parseInt(splittedDate[YEAR_INDEX]),
+                Integer.parseInt(splittedDate[MONTH_INDEX]),
+                Integer.parseInt(splittedDate[DAY_INDEX]));
+    }
+
+    private void trimZeros(String[] splittedDate, int index) {
+        if (splittedDate[index].charAt(0) == '0') {
+            splittedDate[index] = String.valueOf(splittedDate[index].charAt(1));
         }
-        if (splittedDate[WORKER_NAME].charAt(WORK_DAY) == '0') {
-            splittedDate[WORKER_NAME] = String.valueOf(splittedDate[WORKER_NAME]
-                    .charAt(WORKER_NAME));
-        }
-        return LocalDate.of(Integer.parseInt(splittedDate[HOURS]),
-                Integer.parseInt(splittedDate[WORKER_NAME]),
-                Integer.parseInt(splittedDate[WORK_DAY]));
     }
 }
