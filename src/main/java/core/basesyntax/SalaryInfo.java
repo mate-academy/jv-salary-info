6fa0f87d8ced617b408b987165a019e24feccde0
Oleksandr Farion <sashafarion@gmail.com>
2024-02-14 16:39:51 +0200

    added separate methods for generating report and finding name index.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -9,11 +9,7 @@ public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate startDate = LocalDate.parse(dateFrom, formatter);
         LocalDate endDate = LocalDate.parse(dateTo, formatter);
-        StringBuilder builder = new StringBuilder().append("Report for period ")
-                .append(startDate.format(formatter))
-                .append(" - ")
-                .append(endDate.format(formatter))
-                .append(System.lineSeparator());
+        int[] salaries = new int[names.length];
         for (String entry : data) {
             String[] parts = entry.split(" ");
             String name = parts[1];
@@ -21,24 +17,38 @@ public class SalaryInfo {
             int hours = Integer.parseInt(parts[2]);
             int rate = Integer.parseInt(parts[3]);
             if (entryDate.isAfter(startDate.minusDays(1))
-                    && entryDate.isBefore(endDate.plusDays(1))
-                    && checkName(name, names)) {
-                builder.append(name)
-                        .append(" - ");
-                int salary = rate * hours;
-                builder.append(salary)
-                        .append(System.lineSeparator());
+                    && entryDate.isBefore(endDate.plusDays(1))) {
+                int index = indexOfName(name, names);
+                if (index != -1) {
+                    salaries[index] += rate * hours;
+                }
             }
         }
-        return builder.toString();
+        return generateReport(startDate, endDate, names, salaries);
     }
 
-    public boolean checkName(String firstName, String[] names) {
-        for (String name : names) {
-            if (name.equals(firstName)) {
-                return true;
+    private int indexOfName(String name, String[] names) {
+        for (int i = 0; i < names.length; i++) {
+            if (name.equals(names[i])) {
+                return i;
             }
         }
-        return false;
+        return -1;
+    }
+
+    private String generateReport(LocalDate startDate, LocalDate endDate, String[] names, int[] salaries) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("Report for period ")
+                .append(startDate.format(formatter))
+                .append(" - ")
+                .append(endDate.format(formatter))
+                .append(System.lineSeparator());
+        for (int i = 0; i < names.length; i++) {
+            builder.append(names[i])
+                    .append(" - ")
+                    .append(salaries[i])
+                    .append(System.lineSeparator());
+        }
+        return builder.toString();
     }
 }
