36845ed1fca3d9f0546d9018a552843f8842c899
Bohdan Markatov <bogdanmarkatov@gmail.com>
2024-02-14 10:48:52 +0200

    solution with Arrays.фыasList().indexOf() method

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -2,38 +2,44 @@ package core.basesyntax;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
 
 public class SalaryInfo {
     private static final DateTimeFormatter DATE_FORMATTER
             = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-    private static final int DATE_SUBSTRING_START = 0;
-    private static final int DATE_SUBSTRING_END = 10;
-    private static final int HOURS_INDEX = 2;
-    private static final int RATE_INDEX = 3;
+    private static final int DATA_COLUMN_INDEX = 0;
+    private static final int NAME_COLUMN_INDEX = 1;
+    private static final int HOURS_COLUMN_INDEX = 2;
+    private static final int RATE_COLUMN_INDEX = 3;
     private static final String COLUMNS_SEPARATOR = " ";
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        //I split the getSalaryInfo method into 4 separate methods
         int[] salary = salaryCounter(names, data, dateFrom, dateTo);
         return resultExecutor(names, salary, dateFrom, dateTo);
     }
 
     public int[] salaryCounter(String[] names, String[] data, String dateFrom, String dateTo) {
+        //Hi, in this solution I used the Arrays.asList() method,
+        //I know that this is a collection method, and we are not allowed to use collections
+        // in this task, but I don’t know how to solve this problem without using collections
+        //or less than three loops (and I think the method Arrays.asList().indexOf()
+        //actually uses a loop too). If you don't like this solution,
+        //you can look at the previous commit (called split method),
+        //it does not have collection methods,
+        //but it uses 3 loops (2 for counting and 1 for displaying information)
         int[] salary = new int[names.length];
         LocalDate localDateFrom = LocalDate.parse(dateFrom, DATE_FORMATTER);
         LocalDate localDateTo = LocalDate.parse(dateTo, DATE_FORMATTER);
-        for (int i = 0; i < names.length; i++) {
-            for (int j = 0; j < data.length; j++) {
-                if (data[j].contains(names[i])) {
-                    LocalDate localDateCurrent = LocalDate.parse(data[j]
-                                    .substring(DATE_SUBSTRING_START,
-                                            DATE_SUBSTRING_END),
-                            DATE_FORMATTER);
-                    if (dateChecker(localDateFrom, localDateTo, localDateCurrent)) {
-                        String[] currentData = data[j].split(COLUMNS_SEPARATOR);
-                        salary[i] += Integer.parseInt(currentData[HOURS_INDEX])
-                                * Integer.parseInt(currentData[RATE_INDEX]);
-                    }
+        for (int i = 0; i < data.length; i++) {
+            String[] columns = data[i].split(COLUMNS_SEPARATOR);
+            LocalDate localDateCurrent
+                    = LocalDate.parse(columns[DATA_COLUMN_INDEX], DATE_FORMATTER);
+            if (dateChecker(localDateFrom, localDateTo, localDateCurrent)) {
+                int hours = Integer.parseInt(columns[HOURS_COLUMN_INDEX]);
+                int rate = Integer.parseInt(columns[RATE_COLUMN_INDEX]);
+                int index = Arrays.asList(names).indexOf(columns[NAME_COLUMN_INDEX]);
+                if (index != -1) {
+                    salary[index] += hours * rate;
                 }
             }
         }
