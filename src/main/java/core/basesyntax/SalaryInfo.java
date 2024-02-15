ed8d1521186e0f8e28790b71e8febc2ae1c83487
OlleksandrMolchanov <rybkolhoz@gmail.com>
2024-02-15 16:43:16 +0200

    next try

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,30 +1,32 @@
 package core.basesyntax;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private static final SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
+    public static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
+        LocalDate fromDate = LocalDate.parse(dateFrom, formatter);
+        LocalDate toDate = LocalDate.parse(dateTo, formatter);
         StringBuilder stringBuilder = new StringBuilder(
                 "Report for period " + dateFrom + " - " + dateTo);
         String[] temp;
+        LocalDate dataDate;
         int sum = 0;
         for (int i = 0; i < data.length; i++) {
-            for (String oneData: data) {
+            for (String oneData : data) {
                 temp = oneData.split(" ");
-                try {
-                    if (names[i].equals(temp[1])
-                            && ((sdf.parse(temp[0]).compareTo(sdf.parse(dateFrom))) >= 0
-                            && (sdf.parse(temp[0]).compareTo(sdf.parse(dateTo))) <= 0)) {
-                        sum = sum + Integer.parseInt(temp[2]) * Integer.parseInt(temp[3]);
-                    }
-                } catch (ParseException e) {
-                    throw new RuntimeException(e);
+                dataDate = LocalDate.parse(temp[0], formatter);
+                if (names[i].equals(temp[1])
+                        && ((dataDate.isEqual(fromDate)
+                        || dataDate.isEqual(toDate))
+                        || dataDate.isAfter(fromDate)
+                        && dataDate.isBefore(toDate))) {
+                    sum = sum + Integer.parseInt(temp[2]) * Integer.parseInt(temp[3]);
                 }
             }
-            stringBuilder.append("\n").append(names[i]).append(" - ").append(sum);
+            stringBuilder.append(System.lineSeparator()).append(names[i]).append(" - ").append(sum);
             sum = 0;
             if (i == names.length - 1) {
                 break;
