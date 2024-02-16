72c301349eec909e88871831eb3ef005fdf37c95
Eduard Fakhrutdinov <edikf03@gmail.com>
2024-02-16 17:40:58 +0200

    update const var

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -2,55 +2,56 @@ package core.basesyntax;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
 
 public class SalaryInfo {
 
-    static final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final DateTimeFormatter DATE_TIME_FORMATTER =
+            DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int DATE_INDEX = 0;
+    private static final int HOURS_WORKED_INDEX = 2;
+    private static final int HOURLY_RATE_INDEX = 3;
+    private static final String DASH_SEPARATOR = " - ";
+    private static final String NEW_LINE = System.lineSeparator();
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder result = new StringBuilder();
-        result.append("Report for period ").append(dateFrom).append(" - ")
-                .append(dateTo).append(System.lineSeparator());
+        result.append("Report for period ")
+                .append(dateFrom).append(DASH_SEPARATOR)
+                .append(dateTo)
+                .append(NEW_LINE);
 
-        try {
-            for (int i = 0; i < names.length; ++i) {
-                int totalSalary = 0;
-                for (String record : data) {
-                    totalSalary += calculateSalaryForNameAndDate(names[i],
-                            record, dateFrom, dateTo);
-                }
-                result.append(names[i]).append(" - ").append(totalSalary);
-
-                if (i != names.length - 1) {
-                    result.append(System.lineSeparator());
-                }
+        for (String name : names) {
+            int totalSalary = 0;
+            for (String record : data) {
+                totalSalary += calculateSalaryForNameAndDate(name,
+                        record, dateFrom, dateTo);
             }
-        } catch (DateTimeParseException e) {
-            System.out.println("Is not parsable: " + e.getMessage());
-            throw e;
+            result.append(name)
+                    .append(DASH_SEPARATOR)
+                    .append(totalSalary)
+                    .append(NEW_LINE);
         }
 
-        return result.toString();
+        return result.toString().trim();
     }
 
     private int calculateSalaryForNameAndDate(String name, String record,
                                               String dateFrom, String dateTo) {
         String[] recordParts = record.split(" ");
-        String recordDate = recordParts[0];
+        String recordDate = recordParts[DATE_INDEX];
 
         if (isDateInRange(recordDate, dateFrom, dateTo) && record.contains(name)) {
-            int hoursWorked = Integer.parseInt(recordParts[2]);
-            int hourlyRate = Integer.parseInt(recordParts[3]);
+            int hoursWorked = Integer.parseInt(recordParts[HOURS_WORKED_INDEX]);
+            int hourlyRate = Integer.parseInt(recordParts[HOURLY_RATE_INDEX]);
             return hoursWorked * hourlyRate;
         }
         return 0;
     }
 
     private boolean isDateInRange(String recordDate, String dateFrom, String dateTo) {
-        LocalDate recordLocalDate = LocalDate.parse(recordDate, dateFormatter);
-        LocalDate fromDate = LocalDate.parse(dateFrom, dateFormatter);
-        LocalDate toDate = LocalDate.parse(dateTo, dateFormatter);
+        LocalDate recordLocalDate = LocalDate.parse(recordDate, DATE_TIME_FORMATTER);
+        LocalDate fromDate = LocalDate.parse(dateFrom, DATE_TIME_FORMATTER);
+        LocalDate toDate = LocalDate.parse(dateTo, DATE_TIME_FORMATTER);
         return !recordLocalDate.isBefore(fromDate) && !recordLocalDate.isAfter(toDate);
     }
 }
