46b3c5e45113d80a9afb0eca136db066ac30485a
HAIOVIY <gaevoywork@gmail.com>
2024-02-16 17:35:53 +0200

    add new method dateBetweenDates

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -12,11 +12,12 @@ public class SalaryInfo {
     private static final String DATE_FORMAT = "dd.MM.yyyy";
     private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern(
             DATE_FORMAT);
+    private static final String DELIMITER = " - ";
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         int[] salarySum = new int[names.length];
         StringBuilder result = new StringBuilder(
-                "Report for period " + dateFrom + " - " + dateTo);
+                "Report for period " + dateFrom + DELIMITER + dateTo);
 
         LocalDate parseDateFrom = LocalDate.parse(dateFrom, DATE_FORMATTER);
         LocalDate parseDateTo = LocalDate.parse(dateTo, DATE_FORMATTER);
@@ -25,18 +26,19 @@ public class SalaryInfo {
             for (int j = 0; j < data.length; j++) {
                 String[] dataInfo = data[j].split(SPLITERATOR);
                 LocalDate dataDate = LocalDate.parse(dataInfo[DATE_INDEX], DATE_FORMATTER);
-                if ((dataDate.isAfter(parseDateFrom)
-                        || dataDate.isEqual(parseDateFrom))
-                        && (dataDate.isBefore(parseDateTo)
-                        || dataDate.isEqual(parseDateTo))) {
-                    if (dataInfo[NAME_INDEX].equals(names[i])) {
-                        salarySum[i] += (Integer.parseInt(dataInfo[HOURS_INDEX])
-                                * Integer.parseInt(dataInfo[INCOME_INDEX]));
-                    }
+                if (dateBetweenDates(dataDate, parseDateFrom, parseDateTo)
+                        && (dataInfo[NAME_INDEX].equals(names[i]))) {
+                    salarySum[i] += (Integer.parseInt(dataInfo[HOURS_INDEX])
+                            * Integer.parseInt(dataInfo[INCOME_INDEX]));
                 }
             }
-            result.append("\n" + names[i] + " - " + salarySum[i]);
+            result.append("\n" + names[i] + DELIMITER + salarySum[i]);
         }
         return result.toString();
+    }
+
+    private boolean dateBetweenDates(LocalDate date, LocalDate dateFrom, LocalDate dateTo) {
+        return ((date.isAfter(dateFrom) || date.isEqual(dateFrom))
+                && (date.isBefore(dateTo) || date.isEqual(dateTo)));
     }
 }
