33c2210a80474b68c71a8c14b0301d646f4e5bfa
bodya4243 <105318502+bodya4243@users.noreply.github.com>
2024-02-17 21:23:03 +0200

    1)Method Decomposition 2)Made "FORMATTER" constant 3)Add magic numbers

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,35 +4,50 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        LocalDate firstDate = LocalDate.parse(dateFrom, formatter);
-        LocalDate secondDate = LocalDate.parse(dateTo, formatter);
+    static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
-        String[] temp;
-        LocalDate dataDate;
+    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
+        LocalDate firstDate = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate secondDate = LocalDate.parse(dateTo, FORMATTER);
+
+        int[] incomeInfo = calculateIncomeEmployee(names, data, firstDate, secondDate);
+
+        return formattingResult(dateFrom, dateTo, names, incomeInfo);
+    }
+
+    private boolean isDateInRange (LocalDate dataDate, LocalDate firstDate, LocalDate secondDate) {
+        return  ((dataDate.isEqual(firstDate) || dataDate.isEqual(secondDate))
+                || (dataDate.isAfter(firstDate) && dataDate.isBefore(secondDate)));
+    }
+
+    private int[] calculateIncomeEmployee (String[] names, String[] data, LocalDate firstDate, LocalDate secondDate) {
+        final int DATE = 0;
+        final int NAME = 1;
+        final int HOURS = 2;
+        final int SALARY_PER_HOUR = 3;
         int firstIncome;
-        int[] incomeInfo = new int[3];
+        String[] dataInfo;
+        LocalDate dataDate;
+        int[] incomeInfo = new int[names.length];
 
         for (int c = 0; c < names.length; c++) {
             firstIncome = 0;
-
             for (String datum : data) {
-                temp = datum.split(" ");
-                dataDate = LocalDate.parse(temp[0], formatter);
+                dataInfo = datum.split(" ");
+                dataDate = LocalDate.parse(dataInfo[DATE], FORMATTER);
 
-                if (temp[1].equals(names[c])) {
-                    if ((dataDate.isEqual(firstDate) || dataDate.isEqual(secondDate))
-                            || (dataDate.isAfter(firstDate) && dataDate.isBefore(secondDate))) {
-                        firstIncome += Integer.parseInt(temp[2]) * Integer.parseInt(temp[3]);
-                        incomeInfo[c] = firstIncome;
-                    } else {
-                        incomeInfo[c] += 0;
-                    }
+                if (dataInfo[NAME].equals(names[c]) && isDateInRange(dataDate, firstDate, secondDate)) {
+                    firstIncome += Integer.parseInt(dataInfo[HOURS]) * Integer.parseInt(dataInfo[SALARY_PER_HOUR]);
+                    incomeInfo[c] = firstIncome;
+                } else {
+                    incomeInfo[c] += 0;
                 }
             }
         }
+        return incomeInfo;
+    }
 
+    private String formattingResult (String dateFrom, String dateTo, String[] names, int[] incomeInfo) {
         StringBuilder formattedInfo = new StringBuilder();
         formattedInfo.append("Report for period ").append(dateFrom)
                 .append(" - ").append(dateTo).append(System.lineSeparator());
