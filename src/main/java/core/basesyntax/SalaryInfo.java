d3a5282093835816b46f1bfaa4e18824c03e0133
VolandevlodD <vladradko2000@gmail.com>
2024-02-18 18:16:26 +0200

    Added implementation for the getSalaryInfo
    Added implementation for the getSalaryInfo method in the SalaryInfo class. This method calculates employee salaries based on provided data within a specified date range. The generated report includes earnings for each employee. Additionally, ensured inclusivity in date range calculation for accurate results.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,45 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+    public static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter
+            .ofPattern(("dd.MM.yyyy"));
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        LocalDate parsedDateFrom = LocalDate.parse(dateFrom, DATE_TIME_FORMATTER);
+        LocalDate parsedDateTo = LocalDate.parse(dateTo, DATE_TIME_FORMATTER);
+        StringBuilder reportStringBuilder = new StringBuilder("Report for period ")
+                .append(dateFrom)
+                .append(" - ")
+                .append(dateTo);
+        for (String name : names) {
+            int moneyEarned = calculateMoneyEarned(name, data, parsedDateFrom, parsedDateTo);
+            reportStringBuilder
+                    .append(System.lineSeparator())
+                    .append(name)
+                    .append(" - ")
+                    .append(moneyEarned);
+        }
+        return reportStringBuilder.toString();
+    }
+
+    private int calculateMoneyEarned(String name, String[] data,
+                                     LocalDate dateFrom, LocalDate dateTo) {
+        int moneyEarned = 0;
+        for (String personalData : data) {
+            String[] splitedData = personalData.split(" ");
+            LocalDate date = LocalDate.parse(splitedData[0], DATE_TIME_FORMATTER);
+            String personName = splitedData[1];
+            int workingHours = Integer.parseInt(splitedData[2]);
+            int incomePerHour = Integer.parseInt(splitedData[3]);
+            if (name.equals(personName)
+                    && (date.isAfter(dateFrom) || date.isEqual(dateFrom))
+                    && (date.isBefore(dateTo) || date.isEqual(dateTo))) {
+                moneyEarned += incomePerHour * workingHours;
+            }
+        }
+        return moneyEarned;
     }
 }
