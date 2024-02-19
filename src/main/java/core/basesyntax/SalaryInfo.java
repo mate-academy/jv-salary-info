fdf6867be143a4db27d654a05f4fda005cfa0567
MolchanovAlexander <rybkolhoz@gmail.com>
2024-02-19 14:55:53 +0200

    renaming completed

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,25 +4,35 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    public static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int DATE_INDEX = 0;
+    private static final int HOURS_INDEX = 2;
+    private static final int AMOUNT_INDEX = 3;
+    private static final String DIVIDER = " - ";
+    private static final String START_REPORT = "Report for period ";
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        LocalDate fromDate = LocalDate.parse(dateFrom, formatter);
-        LocalDate toDate = LocalDate.parse(dateTo, formatter);
+        LocalDate fromDate = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate toDate = LocalDate.parse(dateTo, FORMATTER);
         StringBuilder stringBuilder = new StringBuilder(
-                "Report for period " + dateFrom + " - " + dateTo);
-        String[] temp;
+                START_REPORT + dateFrom + DIVIDER + dateTo);
+        String[] semiResult;
         LocalDate dataDate;
         int sum = 0;
         for (int i = 0; i < data.length; i++) {
-            for (String oneData : data) {
-                temp = oneData.split(" ");
-                dataDate = LocalDate.parse(temp[0], formatter);
-                if (isFulfillConditions(temp, dataDate, fromDate, toDate, names[i])) {
-                    sum = sum + Integer.parseInt(temp[2]) * Integer.parseInt(temp[3]);
+            for (String line : data) {
+                semiResult = line.split(" ");
+                dataDate = LocalDate.parse(semiResult[DATE_INDEX], FORMATTER);
+                if (isFulfillConditions(semiResult, dataDate, fromDate, toDate, names[i])) {
+                    sum = sum + Integer.parseInt(semiResult[HOURS_INDEX])
+                            * Integer.parseInt(semiResult[AMOUNT_INDEX]);
                 }
             }
-            stringBuilder.append(System.lineSeparator()).append(names[i]).append(" - ").append(sum);
+            stringBuilder
+                    .append(System.lineSeparator())
+                    .append(names[i])
+                    .append(DIVIDER)
+                    .append(sum);
             sum = 0;
             if (i == names.length - 1) {
                 break;
@@ -31,9 +41,9 @@ public class SalaryInfo {
         return stringBuilder.toString();
     }
 
-    private boolean isFulfillConditions(String[] temp, LocalDate dataDate,
+    private boolean isFulfillConditions(String[] semiResult, LocalDate dataDate,
                                         LocalDate fromDate, LocalDate toDate, String names) {
-        return names.equals(temp[1])
+        return names.equals(semiResult[1])
                 && ((dataDate.isEqual(fromDate)
                 || dataDate.isEqual(toDate))
                 || dataDate.isAfter(fromDate)
