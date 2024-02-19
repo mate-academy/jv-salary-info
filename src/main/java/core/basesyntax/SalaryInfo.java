db4eb1159bf098be9a048e838489236ed1eaa857
Taras Fedak <tarasfedak@me.com>
2024-02-19 15:20:53 +0200

    Made improvements. Code is much simpler

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -10,40 +10,34 @@ public class SalaryInfo {
     private static final int INDEX_OF_NAME = 1;
     private static final int INDEX_OF_HOURS = 2;
     private static final int INDEX_OF_SALARY = 3;
-    private static final String NAME_AND_SALARY_SEPARATOR = " - ";
+    private static final String DASH = " - ";
     private static final String SALARY_INFO_HEADER = "Report for period ";
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate localDateFrom = getLocalDate(dateFrom);
         LocalDate localDateTo = getLocalDate(dateTo);
         StringBuilder resultString = new StringBuilder(SALARY_INFO_HEADER)
-                .append(dateFrom).append(NAME_AND_SALARY_SEPARATOR)
+                .append(dateFrom).append(DASH)
                 .append(dateTo);
-        String date;
-        String name;
-        int hours;
-        int salary;
-        String[] row;
-        LocalDate rowDate;
-        EmployeeSalary employeeSalary;
+
         for (String employeeName : names) {
-            employeeSalary = new EmployeeSalary(employeeName);
+            int totalSalary = 0;
             for (String record : data) {
-                row = record.split(" ");
-                date = getDateFromRow(row);
-                rowDate = getLocalDate(date);
-                name = getNameFromRow(row);
-                if (employeeSalary.getName().equals(name)
+                String[] row = record.split(" ");
+                String dateInRow = getDateFromRow(row);
+                String nameInRow = getNameFromRow(row);
+                LocalDate rowDate = getLocalDate(dateInRow);
+                if (employeeName.equals(nameInRow)
                         && rowDate.isAfter(localDateFrom)
                         && !rowDate.isAfter(localDateTo)) {
-                    hours = getHoursFromRow(row);
-                    salary = getSalaryFromRow(row);
-                    employeeSalary.addSalary(salary * hours);
+                    int hours = getHoursFromRow(row);
+                    int salary = getSalaryFromRow(row);
+                    totalSalary += (salary * hours);
                 }
             }
             resultString.append(System.lineSeparator())
-                    .append(employeeSalary.getName()).append(NAME_AND_SALARY_SEPARATOR)
-                    .append(employeeSalary.getSalary());
+                    .append(employeeName).append(DASH)
+                    .append(totalSalary);
         }
         return resultString.toString();
     }
