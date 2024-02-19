f0cb783f7e99fbd4e8445865637bd2be0a5a4de8
Nazar Klimovych <rksvjdbx@gmail.com>
2024-02-19 14:15:31 +0200

    Code refactored due to mentor suggestions. Added new constant DASH_SEPARATOR, changed access modifier for variable DATE_FORMAT. Rename variable datum to row & dataPart to rowElement.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -2,29 +2,37 @@ package core.basesyntax;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
 
 public class SalaryInfo {
-    public static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("dd.MM.yyyy");
     private static final int INDEX_DATE = 0;
     private static final int INDEX_NAME = 1;
     private static final int INDEX_HOURS_WORKED = 2;
     private static final int INDEX_INCOME_PER_HOUR = 3;
+    private static final String DASH_SEPARATOR = " - ";
 
     public static String getSalaryInfo(String[] names, String[] data,
                                        String dateFrom, String dateTo) {
         StringBuilder result = new StringBuilder("Report for period " + dateFrom + " - " + dateTo);
         int[] employeeSalaries = new int[names.length];
 
-        for (String datum : data) {
-            String[] dataParts = datum.split(" ");
-            String date = dataParts[INDEX_DATE];
-            String name = dataParts[INDEX_NAME];
-            int hoursWorked = Integer.parseInt(dataParts[INDEX_HOURS_WORKED]);
-            int incomePerHour = Integer.parseInt(dataParts[INDEX_INCOME_PER_HOUR]);
+        for (String row : data) {
+            String[] rowElement = row.split(" ");
+            String date = rowElement[INDEX_DATE];
+            String name = rowElement[INDEX_NAME];
+            int hoursWorked = Integer.parseInt(rowElement[INDEX_HOURS_WORKED]);
+            int incomePerHour = Integer.parseInt(rowElement[INDEX_INCOME_PER_HOUR]);
 
             if (isDateInRange(date, dateFrom, dateTo)) {
-                int employeeIndex = Arrays.asList(names).indexOf(name);
+                int employeeIndex = -1;
+
+                for (int i = 0; i < names.length; i++) {
+                    if (names[i].equals(name)) {
+                        employeeIndex = i;
+                        break;
+                    }
+                }
+
                 if (employeeIndex != -1) {
                     int totalEarnings = hoursWorked * incomePerHour;
                     employeeSalaries[employeeIndex] += totalEarnings;
@@ -35,7 +43,7 @@ public class SalaryInfo {
         for (int i = 0; i < names.length; i++) {
             int totalEarnings = employeeSalaries[i];
             result.append(System.lineSeparator())
-                    .append(names[i]).append(" - ").append(totalEarnings);
+                    .append(names[i]).append(DASH_SEPARATOR).append(totalEarnings);
         }
 
         return result.toString();
