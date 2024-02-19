5030ca01868a9de7a6b61a49184c58199760f1a3
bodya4243 <105318502+bodya4243@users.noreply.github.com>
2024-02-19 18:16:46 +0200

    1)Change the variables names. 2)Move "isDateInRange", after the method it is used in. 3)Rename "firstIncome" to "currentEmployeeIncome".

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,15 +4,43 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int HOURS_INDEX = 2;
+    private static final int SALARY_PER_HOUR = 3;
     static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate firstDate = LocalDate.parse(dateFrom, FORMATTER);
         LocalDate secondDate = LocalDate.parse(dateTo, FORMATTER);
 
-        int[] incomeInfo = calculateIncomeEmployee(names, data, firstDate, secondDate);
+        int[] incomeInfo = calculateEmployeeIncome(names, data, firstDate, secondDate);
 
-        return formattingResult(dateFrom, dateTo, names, incomeInfo);
+        return generateReport(dateFrom, dateTo, names, incomeInfo);
+    }
+
+    private int[] calculateEmployeeIncome(String[] names, String[] data,
+                                          LocalDate firstDate, LocalDate secondDate) {
+        int currentEmployeeIncome;
+        String[] dataInfo;
+        LocalDate dataDate;
+        int[] incomeInfo = new int[names.length];
+
+        for (int i = 0; i < names.length; i++) {
+            currentEmployeeIncome = 0;
+            for (String datum : data) {
+                dataInfo = datum.split(" ");
+                dataDate = LocalDate.parse(dataInfo[DATE_INDEX], FORMATTER);
+
+                if (dataInfo[NAME_INDEX].equals(names[i]) && isDateInRange(dataDate, firstDate,
+                        secondDate)) {
+                    currentEmployeeIncome += Integer.parseInt(dataInfo[HOURS_INDEX])
+                            * Integer.parseInt(dataInfo[SALARY_PER_HOUR]);
+                    incomeInfo[i] = currentEmployeeIncome;
+                }
+            }
+        }
+        return incomeInfo;
     }
 
     private boolean isDateInRange(LocalDate dataDate, LocalDate firstDate, LocalDate secondDate) {
@@ -20,38 +48,8 @@ public class SalaryInfo {
                 || (dataDate.isAfter(firstDate) && dataDate.isBefore(secondDate)));
     }
 
-    private int[] calculateIncomeEmployee(String[] names, String[] data,
-                                          LocalDate firstDate, LocalDate secondDate) {
-        final int date = 0;
-        final int name = 1;
-        final int hours = 2;
-        final int salaryPerHour = 3;
-        int firstIncome;
-        String[] dataInfo;
-        LocalDate dataDate;
-        int[] incomeInfo = new int[names.length];
-
-        for (int c = 0; c < names.length; c++) {
-            firstIncome = 0;
-            for (String datum : data) {
-                dataInfo = datum.split(" ");
-                dataDate = LocalDate.parse(dataInfo[date], FORMATTER);
-
-                if (dataInfo[name].equals(names[c]) && isDateInRange(dataDate, firstDate,
-                        secondDate)) {
-                    firstIncome += Integer.parseInt(dataInfo[hours])
-                            * Integer.parseInt(dataInfo[salaryPerHour]);
-                    incomeInfo[c] = firstIncome;
-                } else {
-                    incomeInfo[c] += 0;
-                }
-            }
-        }
-        return incomeInfo;
-    }
-
-    private String formattingResult(String dateFrom, String dateTo, String[] names,
-                                    int[] incomeInfo) {
+    private String generateReport(String dateFrom, String dateTo, String[] names,
+                                  int[] incomeInfo) {
         StringBuilder formattedInfo = new StringBuilder();
         formattedInfo.append("Report for period ").append(dateFrom)
                 .append(" - ").append(dateTo).append(System.lineSeparator());
