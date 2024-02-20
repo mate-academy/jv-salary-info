c45b3221aefad0ea5831658f3e48167837a70aab
VolandevlodD <vladradko2000@gmail.com>
2024-02-20 15:47:49 +0200

    Update SalaryInfo.java

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -7,18 +7,20 @@ public class SalaryInfo {
     private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter
             .ofPattern(("dd.MM.yyyy"));
     private static final String HYPHEN = " - ";
+    private static final int DATE = 0;
+    private static final int NAME = 1;
+    private static final int WORKING_HOURS = 2;
+    private static final int INCOME_PER_HOUR = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate parsedDateFrom = LocalDate.parse(dateFrom, DATE_TIME_FORMATTER);
         LocalDate parsedDateTo = LocalDate.parse(dateTo, DATE_TIME_FORMATTER);
         StringBuilder reportStringBuilder = new StringBuilder("Report for period ")
-                .append(dateFrom)
-                .append(HYPHEN)
-                .append(dateTo);
+                .append(dateFrom).append(HYPHEN).append(dateTo);
         for (String name : names) {
-            int moneyEarned = calculateMoneyEarned(name, data, parsedDateFrom, parsedDateTo);
-            reportStringBuilder
-                    .append(System.lineSeparator())
+            int moneyEarned =
+                    calculateMoneyEarned(name, data, parsedDateFrom, parsedDateTo);
+            reportStringBuilder.append(System.lineSeparator())
                     .append(name)
                     .append(HYPHEN)
                     .append(moneyEarned);
@@ -31,12 +33,11 @@ public class SalaryInfo {
         int moneyEarned = 0;
         for (String personalData : data) {
             String[] splitedData = personalData.split(" ");
-            LocalDate date = LocalDate.parse(splitedData[0], DATE_TIME_FORMATTER);
-            String personName = splitedData[1];
-            int workingHours = Integer.parseInt(splitedData[2]);
-            int incomePerHour = Integer.parseInt(splitedData[3]);
-            if (name.equals(personName)
-                    && (date.isAfter(dateFrom) || date.isEqual(dateFrom))
+            LocalDate date = LocalDate.parse(splitedData[DATE], DATE_TIME_FORMATTER);
+            String personName = splitedData[NAME];
+            int workingHours = Integer.parseInt(splitedData[WORKING_HOURS]);
+            int incomePerHour = Integer.parseInt(splitedData[INCOME_PER_HOUR]);
+            if (name.equals(personName) && (date.isAfter(dateFrom) || date.isEqual(dateFrom))
                     && (date.isBefore(dateTo) || date.isEqual(dateTo))) {
                 moneyEarned += incomePerHour * workingHours;
             }
