5a64d7d075f45abea04ccaeb8550b70e87cb7a40
Wojtas1996PL <mat.wojtkowiak@gmail.com>
2024-02-26 15:44:23 +0100

    Third solution

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,45 +4,31 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("d.MM.yyyy");
-    private static int endSum = 0;
-    private StringBuilder result = new StringBuilder();
-    private String endResult = "";
-
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        if (!endResult.equals("")) {
-            endResult = "";
-        }
-        if (!result.isEmpty()) {
-            result.setLength(0);
-        }
+        String endResult = "";
+        StringBuilder result = new StringBuilder();
         String first = "Report for period " + dateFrom + " - " + dateTo
                 + System.lineSeparator();
         result.append(first);
-        LocalDate beginningDate = LocalDate.parse(dateFrom, FORMATTER);
-        LocalDate endingDate = LocalDate.parse(dateTo, FORMATTER);
-        int[] numbers = new int[names.length];
-        for (int y = 0; y < names.length; y++) {
-            for (int i = 0; i < data.length; i++) {
-                LocalDate date = LocalDate.parse(data[i].substring(0,
-                        data[i].indexOf(" ")), FORMATTER);
-                if (date.compareTo(beginningDate) >= 0 && date.compareTo(endingDate) <= 0
-                        && data[i].contains(names[y])) {
-                    int index = data[i].indexOf(names[y])
-                            + names[y].length() + 1;
-                    int hour = Integer.valueOf(data[i].substring(index, data[i].lastIndexOf(" ")));
-                    int income = Integer.valueOf(data[i].substring(data[i]
-                            .lastIndexOf(" ") + 1, data[i].length()));
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d.MM.yyyy");
+        LocalDate beginningDate = LocalDate.parse(dateFrom, formatter);
+        LocalDate endingDate = LocalDate.parse(dateTo, formatter);
+
+        for (String name : names) {
+            int salary = 0;
+            for (String dataString : data) {
+                String[] dataSplitArray = dataString.split(" ");
+                LocalDate date = LocalDate.parse(dataSplitArray[0], formatter);
+                if ((date.isAfter(beginningDate) || date.isEqual(beginningDate))
+                        && (date.isBefore(endingDate) || date.isEqual(endingDate))
+                        && dataSplitArray[1].equals(name)) {
+                    int hour = Integer.valueOf(dataSplitArray[2]);
+                    int income = Integer.valueOf(dataSplitArray[3]);
                     int sum = hour * income;
-                    endSum = endSum + sum;
-                }
-                if (i == data.length - 1) {
-                    numbers[y] = endSum;
-                    endSum = 0;
+                    salary = salary + sum;
                 }
             }
-            result.append(names[y]).append(" - ").append(numbers[y])
-                    .append(System.lineSeparator());
+            result.append(name).append(" - ").append(salary).append(System.lineSeparator());
         }
         String resultToString = result.toString();
         int index = resultToString.lastIndexOf(System.lineSeparator());
