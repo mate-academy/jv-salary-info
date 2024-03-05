c99368db8627acdba6212b93a0c8548f68d10133
Dmytro Makhanok <dmmhnk23@gmail.com>
2024-03-05 23:10:38 +0200

    Corrected remarks

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,17 +1,19 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        String[] dateFromParts = dateFrom.split("\\.");
-        String[] dateToParts = dateTo.split("\\.");
-        int fromDay = Integer.parseInt(dateFromParts[0]);
-        int fromMonth = Integer.parseInt(dateFromParts[1]);
-        int fromYear = Integer.parseInt(dateFromParts[2]);
-        int toDay = Integer.parseInt(dateToParts[0]);
-        int toMonth = Integer.parseInt(dateToParts[1]);
-        int toYear = Integer.parseInt(dateToParts[2]);
-        int startDateValue = fromYear * 10000 + fromMonth * 100 + fromDay;
-        int endDateValue = toYear * 10000 + toMonth * 100 + toDay;
+        final int FIRST_INDEX = 0;
+        final int HOUR_INDEX = 2;
+        final int SALARY_INDEX = 3;
+        final String SEPARATOR = System.lineSeparator();
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDate fromData = LocalDate.parse(dateFrom,dateTimeFormatter);
+        LocalDate toData = LocalDate.parse(dateTo,dateTimeFormatter);
+        int startDateValue = fromData.getYear() * 10000 + fromData.getMonthValue() * 100 + fromData.getDayOfMonth();
+        int endDateValue = toData.getYear() * 10000 + toData.getMonthValue() * 100 + toData.getDayOfMonth();
 
         StringBuilder result = new StringBuilder();
         result.append("Report for period ").append(dateFrom);
@@ -21,20 +23,17 @@ public class SalaryInfo {
             int totalSalary = 0;
             for (String record : data) {
                 String[] parts = record.split(" ");
-                String[] dateParts = parts[0].split("\\.");
-                int day = Integer.parseInt(dateParts[0]);
-                int month = Integer.parseInt(dateParts[1]);
-                int year = Integer.parseInt(dateParts[2]);
-                int dateValue = year * 10000 + month * 100 + day;
+                LocalDate currentData = LocalDate.parse(parts[FIRST_INDEX],dateTimeFormatter);
+                int dateValue = currentData.getYear() * 10000 + currentData.getMonthValue() * 100 + currentData.getDayOfMonth();
 
                 if (parts[1].equals(name) && (dateValue >= startDateValue
                         && dateValue <= endDateValue)) {
-                    int hoursWorked = Integer.parseInt(parts[2]);
-                    int payPerHour = Integer.parseInt(parts[3]);
+                    int hoursWorked = Integer.parseInt(parts[HOUR_INDEX]);
+                    int payPerHour = Integer.parseInt(parts[SALARY_INDEX]);
                     totalSalary += hoursWorked * payPerHour;
                 }
             }
-            result.append(name).append(" - ").append(totalSalary).append(System.lineSeparator());
+            result.append(name).append(" - ").append(totalSalary).append(SEPARATOR);
         }
         return result.toString().trim();
     }
