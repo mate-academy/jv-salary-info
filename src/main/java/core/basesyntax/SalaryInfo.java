86a95e1a4e341d840fec1278ebdbc69bda84bb60
Ruslan Shastkiv <shastkiv.ruslan.dev@gmail.com>
2024-03-14 22:38:02 +0200

    [Java core/OOP/Salary info] solution v1 -implement calculating method without using formatter

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,38 @@
 package core.basesyntax;
 
 public class SalaryInfo {
+    private static final int DATE_OF_WORKING_DAY = 0;
+    private static final int WORKING_HOURS = 2;
+    private static final int SALARY_PER_HOUR = 3;
+    private static final int EMPLOYEE_NAME = 1;
+    private static final int MONTH = 1;
+    private static final int DAY_OF_MONTH = 0;
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        StringBuilder result = new StringBuilder("Report for period ")
+                .append(dateFrom)
+                .append(" - ")
+                .append(dateTo);
+        for (String name : names) {
+            int totalSalary = 0;
+            result.append(System.lineSeparator()).append(name).append(" - ");
+            for (String dataLine : data) {
+                String[] employeeData = dataLine.split(" ");
+                if (employeeData[EMPLOYEE_NAME].equals(name)) {
+                    if (getDate(dateFrom) < getDate(employeeData[DATE_OF_WORKING_DAY])
+                            && getDate(dateTo) >= getDate(employeeData[DATE_OF_WORKING_DAY])) {
+                        totalSalary += Integer.parseInt(employeeData[WORKING_HOURS])
+                                * Integer.parseInt(employeeData[SALARY_PER_HOUR]);
+                    }
+                }
+            }
+            result.append(totalSalary);
+        }
+        return result.toString();
+    }
+
+    public int getDate(String date) {
+        String[] dateArray = date.split("\\.");
+        return Integer.parseInt(dateArray[MONTH] + dateArray[DAY_OF_MONTH]);
     }
 }
