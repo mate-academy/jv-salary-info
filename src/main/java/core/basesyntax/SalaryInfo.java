06adfcad0c1cb21ea62e36c9cf239256fc449964
Bartlomiej Smreczak <b.smreczak@gmail.com>
2024-03-14 21:14:20 +0100

    First solution, without exceptions

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,44 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDate dateFromFormatted = LocalDate.parse(dateFrom, formatter);
+        LocalDate dateToFormatted = LocalDate.parse(dateTo, formatter);
+
+        StringBuilder builder = new StringBuilder();
+        builder.append("Report for period ")
+                .append(dateFrom)
+                .append(" - ")
+                .append(dateTo)
+                .append("\n");
+
+        for (int i = 0; i < names.length; i++) {
+            String searchedName = names[i];
+            int salary = 0;
+
+            for (int j = 0; j < data.length; j++) {
+                String[] dividedData = data[j].split(" ");
+                LocalDate currentDate = LocalDate.parse(dividedData[0], formatter);
+                String currentName = dividedData[1];
+                int currentMultiplier = Integer.valueOf(dividedData[2]);
+                int currentAmount = Integer.valueOf(dividedData[3]);
+
+                if (searchedName.equals(currentName)
+                        && currentDate.compareTo(dateFromFormatted) >= 0
+                        && currentDate.compareTo(dateToFormatted) <= 0) {
+                    int currentSalary = currentMultiplier * currentAmount;
+                    salary = currentSalary + salary;
+                }
+            }
+            builder.append(names[i])
+                    .append(" - ")
+                    .append(salary)
+                    .append("\n");
+        }
+        return builder.toString();
     }
 }
