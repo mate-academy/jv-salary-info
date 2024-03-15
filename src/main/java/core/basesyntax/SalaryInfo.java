a5dc81a896a4a8ec302a54103acff58ae015a8db
Oleh <vojtihivskijo@gmail.com>
2024-03-15 19:20:58 +0200

    reimplemented isDateFit method

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,9 +1,15 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        StringBuilder salaryInfo = new StringBuilder();
-        salaryInfo.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
+        StringBuilder salaryInfo = new StringBuilder("Report for period ")
+                .append(dateFrom)
+                .append(" - ")
+                .append(dateTo);
+
         String[][] report = new String[names.length][2];
 
         for (int i = 0; i < names.length; i++) {
@@ -35,31 +41,15 @@ public class SalaryInfo {
         return salaryInfo.toString();
     }
 
-    public boolean isDateFit(String localDate, String dateFrom, String dateTo) {
-        String[] dateSplited = localDate.split("\\.");
-        String[] dateFromSplited = dateFrom.split("\\.");
-        String[] dateToSplited = dateTo.split("\\.");
+    private boolean isDateFit(String localDate, String dateFrom, String dateTo) {
+        LocalDate parsedDate = LocalDate.parse(localDate,
+                DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        LocalDate parsedDateFrom = LocalDate.parse(dateFrom,
+                DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        LocalDate parsedDateTo = LocalDate.parse(dateTo,
+                DateTimeFormatter.ofPattern("dd.MM.yyyy"));
 
-        int[] dateI = {
-                Integer.parseInt(dateSplited[0]),
-                Integer.parseInt(dateSplited[1])
-        };
-
-        int[] dateFromI = {
-                Integer.parseInt(dateFromSplited[0]),
-                Integer.parseInt(dateFromSplited[1])
-        };
-
-        int[] dateToI = {
-                Integer.parseInt(dateToSplited[0]),
-                Integer.parseInt(dateToSplited[1])
-        };
-
-        if (dateFromI[1] == dateToI[1]) {
-            return dateI[0] >= dateFromI[0] && dateI[0] <= dateToI[0];
-        }
-
-        return (dateI[0] <= dateToI[0] && dateI[1] == dateToI[1])
-                || (dateI[0] >= dateFromI[0] && dateI[1] == dateFromI[1]);
+        return ((parsedDate.isAfter(parsedDateFrom) || parsedDate.isEqual(parsedDateFrom))
+                && (parsedDate.isBefore(parsedDateTo) || parsedDate.isEqual(parsedDateTo)));
     }
 }
