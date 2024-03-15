746fecefe6a9ddefd926d4d7b06e8acbd55acef7
Ruslan Shastkiv <shastkiv.ruslan.dev@gmail.com>
2024-03-15 18:31:23 +0200

    [Java core/OOP/Salary info] solution v2 -implemented new logic for date calculations using LocalDate and DateTimeFormatter class;

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,12 +1,14 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
     private static final int DATE_OF_WORKING_DAY = 0;
     private static final int WORKING_HOURS = 2;
     private static final int SALARY_PER_HOUR = 3;
     private static final int EMPLOYEE_NAME = 1;
-    private static final int MONTH = 1;
-    private static final int DAY_OF_MONTH = 0;
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder result = new StringBuilder("Report for period ")
@@ -18,21 +20,17 @@ public class SalaryInfo {
             result.append(System.lineSeparator()).append(name).append(" - ");
             for (String dataLine : data) {
                 String[] employeeData = dataLine.split(" ");
-                if (employeeData[EMPLOYEE_NAME].equals(name)) {
-                    if (getDate(dateFrom) < getDate(employeeData[DATE_OF_WORKING_DAY])
-                            && getDate(dateTo) >= getDate(employeeData[DATE_OF_WORKING_DAY])) {
-                        totalSalary += Integer.parseInt(employeeData[WORKING_HOURS])
-                                * Integer.parseInt(employeeData[SALARY_PER_HOUR]);
-                    }
+                LocalDate dateOfWorkingDay
+                        = LocalDate.parse(employeeData[DATE_OF_WORKING_DAY], FORMATTER);
+                if (employeeData[EMPLOYEE_NAME].equals(name)
+                        && !dateOfWorkingDay.isAfter(LocalDate.parse(dateTo, FORMATTER))
+                        && !dateOfWorkingDay.isBefore(LocalDate.parse(dateFrom, FORMATTER))) {
+                    totalSalary += Integer.parseInt(employeeData[WORKING_HOURS])
+                            * Integer.parseInt(employeeData[SALARY_PER_HOUR]);
                 }
             }
             result.append(totalSalary);
         }
         return result.toString();
-    }
-
-    public int getDate(String date) {
-        String[] dateArray = date.split("\\.");
-        return Integer.parseInt(dateArray[MONTH] + dateArray[DAY_OF_MONTH]);
     }
 }
