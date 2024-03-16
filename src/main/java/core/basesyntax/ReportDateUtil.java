397961c4edfd61480420bf70d4fc0168271904f8
Olexander Savchuk <savchukolexanderwork@gmail.com>
2024-03-17 01:16:58 +0200

    Created report date util to check is report date in range limits

---
+++ src/main/java/core/basesyntax/ReportDateUtil.java
@@ -0,0 +1,24 @@
+package core.basesyntax;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class ReportDateUtil {
+    private static final String DATE_PATTERN = "dd.MM.yyyy";
+    private final LocalDate date;
+
+    public static LocalDate stringDateToLocalDate(String date) {
+        return LocalDate.parse(date, DateTimeFormatter.ofPattern(DATE_PATTERN));
+    }
+
+    public ReportDateUtil(String dateString) {
+        this.date = stringDateToLocalDate(dateString);
+    }
+
+    public boolean checkIsDateInRange(String rangeStart, String rangeEnd) {
+        LocalDate rangeStartDate = stringDateToLocalDate(rangeStart);
+        LocalDate rangeEndDate = stringDateToLocalDate(rangeEnd);
+        return (date.isAfter(rangeStartDate) || date.isEqual(rangeStartDate))
+                && (date.isBefore(rangeEndDate) || date.isEqual(rangeEndDate));
+    }
+}
