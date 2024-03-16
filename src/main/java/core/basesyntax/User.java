67e8bf26f19702763bf52a1c81b0217d934f8968
Olexander Savchuk <savchukolexanderwork@gmail.com>
2024-03-17 01:14:07 +0200

    Created class User which contains name of user and salary

---
+++ src/main/java/core/basesyntax/User.java
@@ -0,0 +1,19 @@
+package core.basesyntax;
+
+public class User {
+    private final String name;
+    private int salaryAmount = 0;
+
+    public User(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getSalaryAmount() {
+        return salaryAmount;
+    }
+    public void addMoneyToSalary(int amount) {
+        salaryAmount += amount;
+    }
+}
