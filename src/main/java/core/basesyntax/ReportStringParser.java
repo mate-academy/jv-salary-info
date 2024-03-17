69c548daa52da09804575e2c237b905fde85772a
Olexander Savchuk <savchukolexanderwork@gmail.com>
2024-03-17 18:16:33 +0200

    Create string parser which parses reportString and returns Report

---
+++ src/main/java/core/basesyntax/ReportStringParser.java
@@ -0,0 +1,17 @@
+package core.basesyntax;
+
+public class ReportStringParser {
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int HOURS_AMOUNT_INDEX = 2;
+    private static final int SALARY_PER_HOUR_INDEX = 3;
+
+    public Report parseReportString(String stringReport) {
+        String[] splitReportString = stringReport.split(" ");
+        String date = splitReportString[DATE_INDEX];
+        String name = splitReportString[NAME_INDEX];
+        int hoursAmount = Integer.parseInt(splitReportString[HOURS_AMOUNT_INDEX]);
+        int salaryPerHour = Integer.parseInt(splitReportString[SALARY_PER_HOUR_INDEX]);
+        return new Report(date, name, hoursAmount, salaryPerHour);
+    }
+}
