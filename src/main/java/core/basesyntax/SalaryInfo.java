9ba99f0754a2d1589751cd3253ea35637a68f7b9
Banderan <bartoszwojcik2004@gmail.com>
2024-03-19 11:46:07 +0100

    making constant and refactor code

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,36 +4,39 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int DATA_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int HOURS_INDEX = 2;
+    private static final int SALARY_PER_HOUR_INDEX = 3;
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
         StringBuilder stringBuilder = new StringBuilder(
                 "Report for period " + dateFrom + " - " + dateTo);
 
-        LocalDate localDateFrom = LocalDate.parse(dateFrom, formatter);
-        LocalDate localDateTo = LocalDate.parse(dateTo, formatter);
-
-        final int dataIndex = 0;
-        final int nameIndex = 1;
-        final int hoursIndex = 2;
-        final int salaryPerHourIndex = 3;
+        LocalDate localDateFrom = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate localDateTo = LocalDate.parse(dateTo, FORMATTER);
 
         for (String name : names) {
             int salary = 0;
             for (String entry : data) {
                 String[] entrySplit = entry.split(" ");
-                LocalDate entryDate = LocalDate.parse(entrySplit[dataIndex], formatter);
-                String nameFromEntrySplit = entrySplit[nameIndex];
+                LocalDate entryDate = LocalDate.parse(entrySplit[DATA_INDEX], FORMATTER);
+                String entryName = entrySplit[NAME_INDEX];
 
-                if (!(entryDate.isBefore(localDateFrom) || entryDate.isAfter(localDateTo))) {
-                    if (nameFromEntrySplit.equals(name)) {
-                        int hoursInData = Integer.parseInt(entrySplit[hoursIndex]);
-                        int salaryPerHour = Integer.parseInt(entrySplit[salaryPerHourIndex]);
+                if (entryName.equals(name)) {
+                    if (!(entryDate.isBefore(localDateFrom) || entryDate.isAfter(localDateTo))) {
+
+                        int hoursInData = Integer.parseInt(entrySplit[HOURS_INDEX]);
+                        int salaryPerHour = Integer.parseInt(entrySplit[SALARY_PER_HOUR_INDEX]);
 
                         salary += hoursInData * salaryPerHour;
                     }
                 }
             }
-            stringBuilder.append(System.lineSeparator()).append(name).append(" - ").append(salary);
+            stringBuilder.append(System.lineSeparator())
+                    .append(name).append(" - ")
+                    .append(salary);
         }
         return stringBuilder.toString();
     }
