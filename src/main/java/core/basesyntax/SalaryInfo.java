8a3ff0a57e5fd3de47839a102f4c0560409c3216
Andrey Chertykovtsev <chertiav@gmail.com>
2024-03-19 15:53:11 +0100

    implemented getSalaryInfo method logic: - implemented the logic for getting information about salary by an employee per period in getSalaryByName method; - created getDateFromString method and implemented the logic for conversion "String" format to "Date";  - created isDateInRange method and implemented the logic for determining whether a date matches a given range;

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,55 @@
 package core.basesyntax;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class SalaryInfo {
+    private static final String START_TEXT = "Report for period";
+    private static final String DATE_FORMAT = "dd.MM.yyyy";
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        StringBuilder salaryInfo = new StringBuilder(START_TEXT)
+                .append(" ")
+                .append(dateFrom)
+                .append(" - ")
+                .append(dateTo);
+        for (String name : names) {
+            salaryInfo
+                    .append("\n")
+                    .append(name)
+                    .append(" - ")
+                    .append(getSalaryByName(name, data, dateFrom, dateTo));
+        }
+        return salaryInfo.toString();
+    }
+
+    private int getSalaryByName(String name, String[] data, String dateFrom, String dateTo) {
+        int sumSalary = 0;
+
+        for (String itemData : data) {
+            String[] employerData = itemData.split(" ");
+            if (isDateInRange(employerData[0], dateFrom, dateTo) && name.equals(employerData[1])) {
+                sumSalary += Integer.parseInt(employerData[2]) * Integer.parseInt(employerData[3]);
+            }
+        }
+        return sumSalary;
+    }
+
+    private Date getDateFromString(String date) {
+        SimpleDateFormat format = new SimpleDateFormat();
+        format.applyPattern(DATE_FORMAT);
+        try {
+            return format.parse(date);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private boolean isDateInRange(String date, String dateFrom, String dateTo) {
+        return (getDateFromString(date).after(getDateFromString(dateFrom))
+                || getDateFromString(date).equals(getDateFromString(dateFrom)))
+                && (getDateFromString(date).before(getDateFromString(dateTo))
+                || getDateFromString(date).equals(getDateFromString(dateTo)));
     }
 }
