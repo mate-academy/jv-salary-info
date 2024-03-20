b58814c26da926474997f2a5033ae9ed771d9be0
Olexander Savchuk <savchukolexanderwork@gmail.com>
2024-03-20 18:27:42 +0200

    Static ReportStringParserUtil

--- src/main/java/core/basesyntax/ReportStringParserUtil.java
+++ src/main/java/core/basesyntax/ReportStringParserUtil.java
@@ -1,12 +1,12 @@
 package core.basesyntax;
 
-public class ReportStringParser {
+public final class ReportStringParserUtil {
     private static final int DATE_INDEX = 0;
     private static final int NAME_INDEX = 1;
     private static final int HOURS_AMOUNT_INDEX = 2;
     private static final int SALARY_PER_HOUR_INDEX = 3;
 
-    public Report parseReportString(String stringReport) {
+    public static Report parseReportString(String stringReport) {
         String[] splitReportString = stringReport.split(" ");
         String date = splitReportString[DATE_INDEX];
         String name = splitReportString[NAME_INDEX];
