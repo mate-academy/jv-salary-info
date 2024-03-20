e3435a7f59895741004489f12dfec5ec5452a9b9
Andrey Chertykovtsev <chertiav@gmail.com>
2024-03-20 14:09:22 +0100

    improved readability code, and changed the use “Date” to “LocalDate”:: - changed name 'START_TEXT' to specific name for this constant in SalaryInfo; - changed the 'salaryInfo' in getSalaryInfo method, whitespace of the start creating a message moved to the 'HEADER_TO_START_MESSAGE' constant; - changed use Date to LocalDate in getDateFromString method; - improved isDateInRange method, added variables for call some method multiple times.

--- src/main/java/core/basesyntax/EmployeeWorkingTimeData.java
+++ src/main/java/core/basesyntax/EmployeeWorkingTimeData.java
@@ -1,16 +1,21 @@
 package core.basesyntax;
 
 public class EmployeeWorkingTimeData {
+    private static final int WORKING_DAY_ARRAY_POSITION = 0;
+    private static final int EMPLOYEE_NAME_ARRAY_POSITION = 1;
+    private static final int RATE_PER_HOUR_ARRAY_POSITION = 2;
+    private static final int NUMBER_OF_HOURS_ARRAY_POSITION = 3;
     private String workingDay;
     private String name;
     private int ratePerHour;
     private int numberOfHours;
 
     public EmployeeWorkingTimeData(String[] employeeWorkingTimeData) {
-        this.workingDay = employeeWorkingTimeData[0];
-        this.name = employeeWorkingTimeData[1];
-        this.ratePerHour = Integer.parseInt(employeeWorkingTimeData[2]);
-        this.numberOfHours = Integer.parseInt(employeeWorkingTimeData[3]);
+        this.workingDay = employeeWorkingTimeData[WORKING_DAY_ARRAY_POSITION];
+        this.name = employeeWorkingTimeData[EMPLOYEE_NAME_ARRAY_POSITION];
+        this.ratePerHour = Integer.parseInt(employeeWorkingTimeData[RATE_PER_HOUR_ARRAY_POSITION]);
+        this.numberOfHours = Integer.parseInt(
+                employeeWorkingTimeData[NUMBER_OF_HOURS_ARRAY_POSITION]);
     }
 
     public String getWorkingDay() {
--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,18 +1,16 @@
 package core.basesyntax;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private static final String START_TEXT = "Report for period";
+    private static final String HEADER_TO_START_MESSAGE = "Report for period ";
     private static final String DATE_FORMAT = "dd.MM.yyyy";
     private static final String WRONG_DATE_FORMAT_MESSAGE = "Incorrect date format. "
-            + "The date format should be 'dd.MM.yyyy'";
+            + "The date format should be " + DATE_FORMAT;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        StringBuilder salaryInfo = new StringBuilder(START_TEXT)
-                .append(" ")
+        StringBuilder salaryInfo = new StringBuilder(HEADER_TO_START_MESSAGE)
                 .append(dateFrom)
                 .append(" - ")
                 .append(dateTo);
@@ -31,28 +29,28 @@ public class SalaryInfo {
         for (String itemData : data) {
             EmployeeWorkingTimeData employeeData =
                     new EmployeeWorkingTimeData(itemData.split(" "));
-            if (isDateInRange(employeeData.getWorkingDay(), dateFrom, dateTo)
-                    && name.equals(employeeData.getName())) {
+            if (name.equals(employeeData.getName())
+                    && isDateInRange(employeeData.getWorkingDay(), dateFrom, dateTo)) {
                 sumSalary += employeeData.getRatePerHour() * employeeData.getNumberOfHours();
             }
         }
         return sumSalary;
     }
 
-    private Date getDateFromString(String date) {
-        SimpleDateFormat format = new SimpleDateFormat();
-        format.applyPattern(DATE_FORMAT);
+    private LocalDate getDateFromString(String stringDate) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_FORMAT);
         try {
-            return format.parse(date);
-        } catch (ParseException e) {
+            return LocalDate.parse(stringDate, formatter);
+        } catch (Exception e) {
             throw new DateFormatValidException(WRONG_DATE_FORMAT_MESSAGE, e);
         }
     }
 
     private boolean isDateInRange(String date, String dateFrom, String dateTo) {
-        return (getDateFromString(date).after(getDateFromString(dateFrom))
-                || getDateFromString(date).equals(getDateFromString(dateFrom)))
-                && (getDateFromString(date).before(getDateFromString(dateTo))
-                || getDateFromString(date).equals(getDateFromString(dateTo)));
+        LocalDate parseDate = getDateFromString(date);
+        LocalDate parseDateFrom = getDateFromString(dateFrom);
+        LocalDate parseDateTo = getDateFromString(dateTo);
+        return (parseDate.isAfter(parseDateFrom) || parseDate.equals(parseDateFrom))
+                && (parseDate.isBefore(parseDateTo) || parseDate.equals(parseDateTo));
     }
 }
