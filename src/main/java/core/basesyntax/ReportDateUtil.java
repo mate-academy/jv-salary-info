b7180f107fff090125cb45705b9368995516c880
Olexander Savchuk <savchukolexanderwork@gmail.com>
2024-03-20 18:39:21 +0200

    Fixes by the linter

--- src/main/java/core/basesyntax/ReportDateUtil.java
+++ src/main/java/core/basesyntax/ReportDateUtil.java
@@ -5,7 +5,8 @@ import java.time.format.DateTimeFormatter;
 
 public final class ReportDateUtil {
     private static final String REPORT_DATE_PATTERN = "dd.MM.yyyy";
-    private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(REPORT_DATE_PATTERN);
+    private static final DateTimeFormatter dateTimeFormatter =
+            DateTimeFormatter.ofPattern(REPORT_DATE_PATTERN);
 
     public static LocalDate parseReportStringDate(String date) {
         return LocalDate.parse(date, dateTimeFormatter);
--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -6,15 +6,14 @@ public class SalaryInfo {
     private static final String REPORT_TITLE = "Report for period ";
     private static final String HYPHEN_WITH_SPACES = " - ";
 
-
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder reportText = new StringBuilder(REPORT_TITLE)
                 .append(dateFrom)
                 .append(HYPHEN_WITH_SPACES)
                 .append(dateTo);
-       for (String userName : names) {
-           UserWithSalary userWithSalary = new UserWithSalary(userName);
-           for (String dataPerUser : data) {
+        for (String userName : names) {
+            UserWithSalary userWithSalary = new UserWithSalary(userName);
+            for (String dataPerUser : data) {
                Report report = ReportStringParserUtil.parseReportString(dataPerUser);
                if (userWithSalary.getUserName().equals(report.getUserName())
                        && checkIsDateInRange(report.getDate(), dateFrom, dateTo)) {
