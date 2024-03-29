22de6a1a48edc8881b10638458eff8dece558533
Havrylenko Volodymyr <gvs88986@gmail.com>
2024-03-29 20:52:27 +0200

    home_task_8_corrected_codeStyle

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,30 +4,50 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    public String getSalaryInfo(String[] names, String[] data,
-                                String dateFrom, String dateTo) throws SalaryInfoException {
-        if (names.length == 0 || data.length == 0 || dateFrom == null || dateTo == null) {
-            throw new SalaryInfoException("incorrect data entry");
-        }
-        DateTimeFormatter df = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        LocalDate dateFrom1 = LocalDate.parse(dateFrom.trim(), df);
-        LocalDate dateTo1 = LocalDate.parse(dateTo.trim(), df);
+    private final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
+    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo)
+            throws SalaryInfoException {
+        validateInputData(names, data, dateFrom, dateTo);
+        int length = names.length;
         String info;
-        StringBuilder stringBuilder = new StringBuilder();
-        int[]sums = new int[names.length];
-        for (int i = 0; i < names.length; i++) {
+        String report = "Report for period ";
+        StringBuilder getResult = new StringBuilder();
+        LocalDate dateFromDate = LocalDate.parse(dateFrom.trim(), dateTimeFormatter);
+        LocalDate dateToDate = LocalDate.parse(dateTo.trim(), dateTimeFormatter);
+        int[] salary = new int[length];
+        for (int i = 0; i < length; i++) {
             for (String dat : data) {
-                String[] dateLine = dat.split(" ");
-                LocalDate d1 = LocalDate.parse(dateLine[0].trim(), df);
-                if (names[i].equals(dateLine[1]) && (d1.isAfter(dateFrom1.minusDays(1))
-                        && d1.isBefore(dateTo1.plusDays(1)))) {
-                    sums[i] += Integer.parseInt(dateLine[2]) * Integer.parseInt(dateLine[3]);
+                String[] dataSeparater = dat.split(" ");
+                String getDate = dataSeparater[0];
+                String getName = dataSeparater[1];
+                String getQuantity = dataSeparater[2];
+                String getSalary = dataSeparater[3];
+                LocalDate dayToCheck = LocalDate.parse(getDate.trim(), dateTimeFormatter);
+                boolean isTrue = (dayToCheck.isAfter(dateFromDate.minusDays(1))
+                        && dayToCheck.isBefore(dateToDate.plusDays(1)));
+                if (names[i].equals(getName) && isTrue) {
+                    salary[i] += Integer.parseInt(getQuantity) * Integer.parseInt(getSalary);
                 }
             }
-            info = names[i] + " - " + sums[i];
-            stringBuilder.append(System.lineSeparator()).append(info);
+            info = names[i] + " - " + salary[i];
+            getResult.append(System.lineSeparator()).append(info);
         }
-        return "Report for period " + dateFrom + " - " + dateTo + stringBuilder;
+        return report + dateFrom + " - " + dateTo + getResult;
+    }
+
+    public void validateInputData(String[] names, String[] data, String dateFrom, String dateTo) {
+        if (names.length == 0) {
+            throw new SalaryInfoException("incorrect data entry, check: name");
+        }
+        if (data.length == 0) {
+            throw new SalaryInfoException("incorrect data entry, check: data");
+        }
+        if (dateFrom == null) {
+            throw new SalaryInfoException("incorrect data entry, check: dateFrom");
+        }
+        if (dateTo == null) {
+            throw new SalaryInfoException("incorrect data entry, check: dateTo");
+        }
     }
 }
-
