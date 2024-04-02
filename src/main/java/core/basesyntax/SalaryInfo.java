26afd2ab32fd6b6ce18f689ba89900c06a32495a
Igor Glebov <igor.glebov0312@gmail.com>
2024-04-02 18:06:04 +0300

    Implementation for salary info method using Local date

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,33 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        StringBuilder builder = new StringBuilder();
+        int result = 0;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDate startDate = LocalDate.parse(dateFrom, formatter);
+        LocalDate endDate = LocalDate.parse(dateTo, formatter);
+        builder.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
+        for (String name : names) {
+            for (String dataString : data) {
+                String [] infoFromData = dataString.split(" ");
+                LocalDate currentDate = LocalDate.parse(infoFromData[0], formatter);
+                if (name.equals(infoFromData[1])) {
+                    if (ChronoUnit.DAYS.between(currentDate, endDate) >= 0
+                            && ChronoUnit.DAYS.between(startDate, currentDate) >= 0) {
+                        result += Integer.parseInt(infoFromData[2])
+                                * Integer.parseInt(infoFromData[3]);
+
+                    }
+                }
+            }
+            builder.append("\r\n").append(name).append(" - ").append(result);
+            result = 0;
+        }
+        return builder.toString();
     }
 }
