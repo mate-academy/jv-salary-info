cf46b0d9ce8b2aae99be4af5ba30fe923c6dabb9
Vadik Rakitin <vadym.rakitin@gmail.com>
2024-04-10 22:13:12 +0300

    added append method for each string, replaced for loop to for-each loop, fixed magic numbers

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,30 +5,29 @@ import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int INCOME_INDEX = 2;
+    private static final int HOURS_INDEX = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        StringBuilder result = new StringBuilder("Report for period "
-                + dateFrom + " - " + dateTo + System.lineSeparator());
-        int totalNames = names.length;
-        for (int i = 0; i < totalNames; i++) {
-            String name = names[i];
+        StringBuilder result = new StringBuilder();
+        result.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
+        for (String name : names) {
             int totalSalary = 0;
             LocalDate dateStart = LocalDate.parse(dateFrom, FORMATTER);
             LocalDate dateFinal = LocalDate.parse(dateTo, FORMATTER).plusDays(1);
             for (String entry : data) {
                 String[] parts = entry.split(" ");
-                LocalDate date = LocalDate.parse(parts[0], FORMATTER);
+                LocalDate date = LocalDate.parse(parts[DATE_INDEX], FORMATTER);
                 if (!date.isBefore(dateStart) && date.isBefore(dateFinal)
-                        && name.equals(parts[1])) {
-                    int hours = Integer.parseInt(parts[2]);
-                    int incomePerHour = Integer.parseInt(parts[3]);
+                        && name.equals(parts[NAME_INDEX])) {
+                    int hours = Integer.parseInt(parts[INCOME_INDEX]);
+                    int incomePerHour = Integer.parseInt(parts[HOURS_INDEX]);
                     totalSalary += hours * incomePerHour;
                 }
             }
-            result.append(name).append(" - ").append(totalSalary);
-            if (i < totalNames - 1) {
-                result.append(System.lineSeparator());
-            }
+            result.append(System.lineSeparator()).append(name).append(" - ").append(totalSalary);
         }
         return result.toString();
     }
