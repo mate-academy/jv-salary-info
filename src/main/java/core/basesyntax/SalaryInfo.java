2ecbdd8e859ccdcd41311940a01edc2e24d76d73
Volodymyr Kulesha <v.s.kulesha@gmail.com>
2024-04-10 22:19:54 +0300

    Fixed bugs after review

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -6,7 +6,10 @@ import java.time.format.DateTimeFormatter;
 public class SalaryInfo {
     public static final DateTimeFormatter DATE_TIME_FORMATTER
             = DateTimeFormatter.ofPattern("dd.MM.yyy");
-    public static final int DATE_PART_LENGTH = 10;
+    public static final int DATE_POSITION_IN_DATASET = 0;
+    public static final int NAME_POSITION_IN_DATASET = 1;
+    public static final int WORKING_HOURS_POSITION_IN_DATASET = 2;
+    public static final int INCOME_PER_HOUR_POSITION_IN_DATASET = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         if (names == null || data == null || dateFrom == null || dateTo == null) {
@@ -16,17 +19,20 @@ public class SalaryInfo {
         LocalDate fromLocalDate = LocalDate.parse(dateFrom, DATE_TIME_FORMATTER);
         LocalDate toLocalDate = LocalDate.parse(dateTo, DATE_TIME_FORMATTER);
 
-        StringBuilder stringBuilder = new StringBuilder("Report for period "
-                + dateFrom + " - " + dateTo);
+        StringBuilder stringBuilder = new StringBuilder("Report for period ")
+                .append(dateFrom)
+                .append(" - ")
+                .append(dateTo);
 
         for (String name : names) {
             int totalUserSalary = 0;
             for (String dataString : data) {
                 String[] dataSet = dataString.split(" ");
                 if (isDateInTheLimits(fromLocalDate, toLocalDate,
-                        LocalDate.parse(dataSet[0], DATE_TIME_FORMATTER))
-                        && name.equals(dataSet[1])) {
-                    totalUserSalary += Integer.parseInt(dataSet[2]) * Integer.parseInt(dataSet[3]);
+                        LocalDate.parse(dataSet[DATE_POSITION_IN_DATASET], DATE_TIME_FORMATTER))
+                        && name.equals(dataSet[NAME_POSITION_IN_DATASET])) {
+                    totalUserSalary += Integer.parseInt(dataSet[WORKING_HOURS_POSITION_IN_DATASET])
+                            * Integer.parseInt(dataSet[INCOME_PER_HOUR_POSITION_IN_DATASET]);
                 }
             }
             addSalaryRecord(stringBuilder, name, totalUserSalary);
