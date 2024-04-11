a39f2811ff9b1df069b3a3350044727737783e48
Vlad Nadtochiy <v20102002n@gmail.com>
2024-04-11 11:46:47 +0300

    suggestions were implemented

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -12,23 +12,17 @@ public class SalaryInfo {
     public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        LocalDate date;
-        String nameOfEmployee;
-        int hours;
-        int salaryPerHour;
-        String[] personInformation;
         LocalDate localDateFrom = LocalDate.parse(dateFrom, FORMATTER);
         LocalDate localDateTo = LocalDate.parse(dateTo, FORMATTER);
         int[] finalSalary = new int[names.length];
         for (String line : data) {
-            personInformation = line.split(" ");
-            date = LocalDate.parse(personInformation[INDEX_OF_DATE], FORMATTER);
-            nameOfEmployee = personInformation[INDEX_OF_NAME];
-            hours = Integer.parseInt(personInformation[INDEX_OF_DAYS]);
-            salaryPerHour = Integer.parseInt(personInformation[INDEX_OF_PRICE]);
-            if (List.of(names).contains(nameOfEmployee)
-                    && (localDateFrom.isBefore(date) || localDateFrom.isEqual(date))
-                    && (localDateTo.isAfter(date) || localDateTo.isEqual(date))) {
+            String[] personInformation = line.split(" ");
+            LocalDate date = LocalDate.parse(personInformation[INDEX_OF_DATE], FORMATTER);
+            String nameOfEmployee = personInformation[INDEX_OF_NAME];
+            int hours = Integer.parseInt(personInformation[INDEX_OF_DAYS]);
+            int salaryPerHour = Integer.parseInt(personInformation[INDEX_OF_PRICE]);
+            if (List.of(names).contains(nameOfEmployee) && !localDateFrom.isAfter(date)
+                    && !localDateTo.isBefore(date)) {
                 finalSalary[List.of(names).indexOf(nameOfEmployee)] += hours * salaryPerHour;
             }
         }
@@ -37,11 +31,17 @@ public class SalaryInfo {
 
     private String dataToString(String[] names, int[] salary, String dateFrom, String dateTo) {
         StringBuilder resultString = new StringBuilder();
-        resultString.append(String.format("Report for period %s - %s",dateFrom,dateTo));
+        resultString
+                .append("Report for period ")
+                .append(dateFrom)
+                .append(" - ")
+                .append(dateTo);
         for (int i = 0; i < names.length; i++) {
             resultString
                     .append(System.lineSeparator())
-                    .append(names[i]).append(" - ").append(salary[i]);
+                    .append(names[i])
+                    .append(" - ")
+                    .append(salary[i]);
         }
         return resultString.toString();
     }
