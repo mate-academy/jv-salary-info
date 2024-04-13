e099753c5271d1ea66a9107896e1fcab133ae38d
Dmytro Lapikov <it.lapikov@gmail.com>
2024-04-13 10:37:12 +0300

    fix issues, now code works universaly

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -7,38 +7,42 @@ public class SalaryInfo {
     private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
+        final int dataDate = 0;
+        final int dataName = 1;
+        final int dataHours = 2;
+        final int dataIncome = 3;
         LocalDate salaryDateFrom = LocalDate.parse(dateFrom, formatter);
         LocalDate salaryDateTo = LocalDate.parse(dateTo, formatter);
-
-        int johnSalary = 0;
-        int andrewSalary = 0;
-        int kateSalary = 0;
+        StringBuilder builder = new StringBuilder();
+        int[] salaries = new int[names.length];
 
         for (int i = 0; i < data.length; i++) {
             String[] tempData = data[i].split(" ");
-            LocalDate currentDate = LocalDate.parse(tempData[0], formatter);
+            LocalDate currentDate = LocalDate.parse(tempData[dataDate], formatter);
 
             if (currentDate.isAfter(salaryDateFrom.minusDays(1))
                     && currentDate.isBefore(salaryDateTo.plusDays(1))) {
-                String name = tempData[1];
-                int hours = Integer.parseInt(tempData[2]);
-                int incomePerHour = Integer.parseInt(tempData[3]);
+                String name = tempData[dataName];
+                int hours = Integer.parseInt(tempData[dataHours]);
+                int incomePerHour = Integer.parseInt(tempData[dataIncome]);
+                int index = 0;
 
-                switch (name) {
-                    case "John" -> johnSalary += hours * incomePerHour;
-                    case "Andrew" -> andrewSalary += hours * incomePerHour;
-                    case "Kate" -> kateSalary += hours * incomePerHour;
-                    default -> {
-                        continue;
+                for (int j = 0; j < names.length; j++) {
+                    if (name.equals(names[j])) {
+                        index = j;
                     }
                 }
+                salaries[index] += hours * incomePerHour;
             }
         }
-        String report = "Report for period " + dateFrom + " - " + dateTo
-                + "\n" + names[0] + " - " + johnSalary
-                + "\n" + names[1] + " - " + andrewSalary
-                + "\n" + names[2] + " - " + kateSalary;
+        builder.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
+        for (int i = 0; i < names.length; i++) {
+            builder.append("\n")
+                    .append(names[i])
+                    .append(" - ")
+                    .append(salaries[i]);
+        }
 
-        return report;
+        return builder.toString();
     }
 }
