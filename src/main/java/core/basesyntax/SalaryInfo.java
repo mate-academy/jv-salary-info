99379138ee476337aeaf659d60c109b6b790bbc5
BelitskyiAlexandr <vilisapett@gmail.com>
2024-04-15 13:31:07 +0300

    move earnings calculation to a separate method

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -10,27 +10,12 @@ public class SalaryInfo {
 
     public String getSalaryInfo(String[] names, String[] data,
                                 String dateFrom, String dateTo) {
-        int[] earnings = new int[names.length];
         LocalDate fromDate = LocalDate.parse(dateFrom, formatter);
         LocalDate toDate = LocalDate.parse(dateTo, formatter);
 
-        for (String entry : data) {
-            String[] parts = entry.split(" ");
-            LocalDate currentDate = LocalDate.parse(parts[0], formatter);
-            String currentName = parts[1];
-            int hours = Integer.parseInt(parts[2]);
-            int incomePerHour = Integer.parseInt(parts[3]);
+        int[] earnings = this.getEarnings(names, data, fromDate, toDate);
 
-            if (isWithinDateRange(currentDate, fromDate, toDate)) {
-                for (int i = 0; i < names.length; i++) {
-                    if (currentName.equals(names[i])) {
-                        earnings[i] += hours * incomePerHour;
-                        break;
-                    }
-                }
-            }
-        }
-
+        //report creation
         StringBuilder report = new StringBuilder();
         report.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
         for (int i = 0; i < names.length; i++) {
@@ -44,5 +29,29 @@ public class SalaryInfo {
     private boolean isWithinDateRange(LocalDate currentDate, LocalDate fromDate,
                                       LocalDate toDate) {
         return !currentDate.isBefore(fromDate) && !currentDate.isAfter(toDate);
+    }
+
+    private int[] getEarnings(String[] names, String[] data,
+                              LocalDate fromDate, LocalDate toDate) {
+        int[] earnings = new int[names.length];
+        for (String entry : data) {
+            //parsing
+            String[] parts = entry.split(" ");
+            LocalDate currentDate = LocalDate.parse(parts[0], formatter);
+            String currentName = parts[1];
+            int hours = Integer.parseInt(parts[2]);
+            int incomePerHour = Integer.parseInt(parts[3]);
+
+            //calculate earnings
+            if (isWithinDateRange(currentDate, fromDate, toDate)) {
+                for (int i = 0; i < names.length; i++) {
+                    if (currentName.equals(names[i])) {
+                        earnings[i] += hours * incomePerHour;
+                        break;
+                    }
+                }
+            }
+        }
+        return earnings;
     }
 }
