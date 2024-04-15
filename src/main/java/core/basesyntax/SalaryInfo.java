990ab48e9e066f0809752205b6d0e65f2e08103e
Dmytro Lapikov <it.lapikov@gmail.com>
2024-04-15 12:02:32 +0300

    fix problems, fix mistakes

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,44 +5,37 @@ import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private final int dataDate = 0;
+    private final int dataName = 1;
+    private final int dataHours = 2;
+    private final int dataIncome = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        final int dataDate = 0;
-        final int dataName = 1;
-        final int dataHours = 2;
-        final int dataIncome = 3;
+
         LocalDate salaryDateFrom = LocalDate.parse(dateFrom, formatter);
         LocalDate salaryDateTo = LocalDate.parse(dateTo, formatter);
-        StringBuilder builder = new StringBuilder();
-        int[] salaries = new int[names.length];
+        StringBuilder reportBuilder = new StringBuilder();
+        reportBuilder.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
 
-        for (int i = 0; i < data.length; i++) {
-            String[] tempData = data[i].split(" ");
-            LocalDate currentDate = LocalDate.parse(tempData[dataDate], formatter);
+        for (String name : names) {
+            int totalSalary = 0;
+            for (String datum : data) {
+                String[] tempData = datum.split(" ");
+                LocalDate currentDate = LocalDate.parse(tempData[dataDate], formatter);
 
-            if (currentDate.isAfter(salaryDateFrom.minusDays(1))
-                    && currentDate.isBefore(salaryDateTo.plusDays(1))) {
-                String name = tempData[dataName];
-                int hours = Integer.parseInt(tempData[dataHours]);
-                int incomePerHour = Integer.parseInt(tempData[dataIncome]);
-                int index = 0;
-
-                for (int j = 0; j < names.length; j++) {
-                    if (name.equals(names[j])) {
-                        index = j;
+                if (currentDate.isAfter(salaryDateFrom.minusDays(1))
+                        && currentDate.isBefore(salaryDateTo.plusDays(1))) {
+                    if (name.equals(tempData[dataName])) {
+                        totalSalary += Integer.parseInt(tempData[dataHours])
+                                * Integer.parseInt(tempData[dataIncome]);
                     }
                 }
-                salaries[index] += hours * incomePerHour;
             }
-        }
-        builder.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
-        for (int i = 0; i < names.length; i++) {
-            builder.append("\n")
-                    .append(names[i])
+            reportBuilder.append("\n")
+                    .append(name)
                     .append(" - ")
-                    .append(salaries[i]);
+                    .append(totalSalary);
         }
-
-        return builder.toString();
+        return reportBuilder.toString();
     }
 }
