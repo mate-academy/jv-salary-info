5fc18bdde3a4e80f38d4538d4034c58711fceeb1
Anton <antchaycka@gmail.com>
2024-04-20 12:41:52 +0200

    made fixes according to review (added constants, added method "isDateIncludes" and changes in "getSalaryInfo")

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -6,33 +6,37 @@ import java.time.format.DateTimeFormatter;
 public class SalaryInfo {
     private static final DateTimeFormatter DATE_TIME_FORMATTER =
             DateTimeFormatter.ofPattern("dd.MM.yyyy");
-    private String[] str;
-    private int sum = 0;
+    private static final int TARGET_DATE_INDEX = 0;
+    private static final int TARGET_NAME_INDEX = 1;
+    private static final int HOURS_INDEX = 2;
+    private static final int SALARY_INDEX = 3;
     private String separator = System.lineSeparator();
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        StringBuilder result = new StringBuilder("Report for period " + dateFrom + " - "
-                + dateTo + separator);
+        StringBuilder result = new StringBuilder("Report for period ").append(dateFrom)
+                .append(" - ").append(dateTo);
         LocalDate localDateFrom = LocalDate.parse(dateFrom, DATE_TIME_FORMATTER);
         LocalDate localDateTo = LocalDate.parse(dateTo, DATE_TIME_FORMATTER);
 
-        for (int i = 0; i < names.length; i++) {
+        for (String name : names) {
+            int totalSalary = 0;
+
             for (String info : data) {
-                str = info.split(" ");
-                LocalDate date = LocalDate.parse(str[0], DATE_TIME_FORMATTER);
-                if (names[i].equals(str[1])
-                        && (date.isAfter(localDateFrom) || date.equals(localDateFrom))
-                        && (date.isBefore(localDateTo) || date.equals(localDateTo))) {
-                    sum += Integer.parseInt(str[2]) * Integer.parseInt(str[3]);
+                String[] str = info.split(" ");
+                LocalDate date = LocalDate.parse(str[TARGET_DATE_INDEX], DATE_TIME_FORMATTER);
+                if (name.equals(str[TARGET_NAME_INDEX])
+                        && isDateIncludes(date, localDateFrom, localDateTo)) {
+                    totalSalary += Integer.parseInt(str[HOURS_INDEX])
+                            * Integer.parseInt(str[SALARY_INDEX]);
                 }
             }
-            if (i < names.length - 1) {
-                result.append(names[i]).append(" - ").append(sum).append(separator);
-            } else {
-                result.append(names[i]).append(" - ").append(sum);
-            }
-            sum = 0;
+            result.append(separator).append(name).append(" - ").append(totalSalary);
         }
         return result.toString();
+    }
+
+    private boolean isDateIncludes(LocalDate target, LocalDate from, LocalDate to) {
+        return (target.isAfter(from) || target.equals(from))
+                && (target.isBefore(to) || target.equals(to));
     }
 }
