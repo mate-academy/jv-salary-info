b677e948a9647aff22b90e4b832b2a48afe49dc3
Roman Chygryn <chygryn.ra@gmail.com>
2024-04-21 23:23:29 +0200

    Implement salary calculator

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,29 +5,17 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
 public class SalaryInfo {
-    public static DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    public static final int DATE_PART = 0;
+    public static final int NAME_PART = 1;
+    public static final int WORKED_HOURS_PART = 2;
+    public static final int RATE_PART = 3;
+    public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
-    public static void main(String[] args) {
-        String[] sampleNames = {"John", "Andrew", "Kate"};
-        String[] scriptArray = {
-                "25.04.2019 John 60 50",
-                "25.04.2019 Andrew 3 200",
-                "25.04.2019 Kate 10 100",
-
-                "26.04.2019 Andrew 3 200",
-                "26.04.2019 Kate 9 100",
-
-                "27.04.2019 John 7 100",
-                "27.04.2019 Kate 3 80",
-                "27.04.2019 Andrew 8 100"
-        };
-
-        String res = getSalaryInfo(sampleNames, scriptArray, "24.04.2019", "26.04.2019");
-    }
-
-    public static String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
+    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate parsedDateFrom;
         LocalDate parsedDateTo;
+        StringBuilder sb = new StringBuilder("Report for period ");
+        sb.append(dateFrom).append(" - ").append(dateTo);
 
         try {
             parsedDateFrom = LocalDate.parse(dateFrom.trim(), FORMATTER);
@@ -37,33 +25,30 @@ public class SalaryInfo {
         }
 
         for (String name : names) {
-            for (int i = 0; i < data.length; i++) {
-                String[] dataParts = data[i].split(" ");
-                String namePart = dataParts[1];
+            int currentPersonEarnings = 0;
 
-                if (!namePart.equals(name)) {
+            for (int i = 0; i < data.length; i++) {
+                String[] stringParts = data[i].split(" ");
+
+                if (!stringParts[NAME_PART].equals(name)) {
                     continue;
                 }
 
-                String datePart = dataParts[0];
-                String workedHoursPart = dataParts[2];
-                String ratePart = dataParts[3];
-                LocalDate parsedDatePart = LocalDate.parse(datePart, FORMATTER);
+                LocalDate parsedDatePart = LocalDate.parse(stringParts[DATE_PART], FORMATTER);
 
-                if (data[i].contains(name)) {
+                if (!parsedDatePart.isBefore(parsedDateFrom)
+                        && !parsedDatePart.isAfter(parsedDateTo)) {
+                    int earningsInCurrentEntry = Integer.parseInt(stringParts[WORKED_HOURS_PART])
+                            * Integer.parseInt(stringParts[RATE_PART]);
 
+                    currentPersonEarnings += earningsInCurrentEntry;
                 }
             }
+
+            sb.append(System.lineSeparator());
+            sb.append(name).append(" - ").append(currentPersonEarnings);
         }
 
-        return null;
-    }
-
-    private int calculatePersonIncomePerDay() {
-        return 0;
-    }
-
-    private String createHeader(String dateFrom, String dateTo) {
-        return "Report for period" + dateFrom + " - " + dateTo;
+        return sb.toString();
     }
 }
