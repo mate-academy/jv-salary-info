0d3afa244ec0e9f968f0da9fa3942031fae77e30
Ivan Demchenko <experywow@gmail.com>
2024-05-01 20:29:32 +0100

    Removing magic numbers, creating instead private constants.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,24 +4,28 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int WORKING_DATE_INDEX = 0;
+    private static final int WORKING_HOURS_INDEX = 2;
+    private static final int PAY_RATE_INDEX = 3;
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        LocalDate fromDate = LocalDate.parse(dateFrom, dateFormatter);
-        LocalDate toDate = LocalDate.parse(dateTo, dateFormatter);
+        LocalDate fromDate = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate toDate = LocalDate.parse(dateTo, FORMATTER);
 
         String[] reportForPeriod = names.clone();
         StringBuilder reportBuilder = new StringBuilder("Report for period ")
-                .append(dateFormatter.format(fromDate))
+                .append(FORMATTER.format(fromDate))
                 .append(" - ")
-                .append(dateFormatter.format(toDate));
+                .append(FORMATTER.format(toDate));
 
         for (int i = 0; i < names.length; i++) {
             int reportForPeriodSalary = 0;
             for (int j = 0; j < data.length; j++) {
                 String[] temp = data[j].split(" ");
-                LocalDate workingDate = LocalDate.parse(temp[0], dateFormatter);
-                double workingHours = Double.parseDouble(temp[2]);
-                double workingRate = Double.parseDouble(temp[3]);
+                LocalDate workingDate = LocalDate.parse(temp[WORKING_DATE_INDEX], FORMATTER);
+                double workingHours = Double.parseDouble(temp[WORKING_HOURS_INDEX]);
+                double workingRate = Double.parseDouble(temp[PAY_RATE_INDEX]);
                 double salary = workingHours * workingRate;
                 if (data[j].contains(names[i])
                         && (!workingDate.isBefore(fromDate) && !workingDate.isAfter(toDate))) {
