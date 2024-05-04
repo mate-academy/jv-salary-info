a67bd341517eb3923c8d8936f4d605659e32ebd6
Nataliya Plastun <natalya21753@gmail.com>
2024-05-04 21:47:43 +0300

    commit StringBuilder changes

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -2,28 +2,32 @@ package core.basesyntax;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
 public class SalaryInfo {
     static final int DATE_INDEX = 0;
     static final int NAME_INDEX = 1;
     static final int HOUR_INDEX = 2;
     static final int RATE_INDEX = 3;
+    private SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
         dateFormat.setLenient(false);
         try {
             Date fromDate = dateFormat.parse(dateFrom);
             Date toDate = dateFormat.parse(dateTo);
 
-            List<String> reportLines = new ArrayList<>();
-            reportLines.add("Report for period " + dateFrom + " - " + dateTo);
-            for (String name : names) {
+            StringBuilder reportLines = new StringBuilder();
+            reportLines.append("Report for period ").append(dateFrom).append(" - ")
+                    .append(dateTo).append(System.lineSeparator());
+            for (int i = 0; i < names.length; i++) {
+                String name = names[i];
                 int earned = calculateEarnedSalary(name, data, fromDate, toDate, dateFormat);
-                reportLines.add(name + " - " + earned);
+                reportLines.append(name).append(" - ")
+                        .append(earned);
+                if (i < names.length - 1) {
+                    reportLines.append(System.lineSeparator());
+                }
             }
             return String.join(System.lineSeparator(), reportLines);
         } catch (ParseException e) {
