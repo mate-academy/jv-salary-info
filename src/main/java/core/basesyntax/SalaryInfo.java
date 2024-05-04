d71f58e87a8551a090ce0f3b020c00258335a7a2
Yura Yurchuk <yurayurchuk88@gmail.com>
2024-05-04 16:34:44 +0300

    added the changes specified by the mentor

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -11,38 +11,27 @@ public class SalaryInfo {
     private static final int NAME = 1;
     private static final int HOURS = 2;
     private static final int PRICE_PER_HOUR = 3;
-    private static final int JOHN = 0;
-    private static final int ANDREW = 1;
-    private static final int KATE = 2;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        LocalDate localDateFrom = LocalDate.parse(dateFrom,FORMATTER);
-        LocalDate localDateTo = LocalDate.parse(dateTo,FORMATTER);
-        int johnSalary = 0;
-        int andrewSalary = 0;
-        int kateSalary = 0;
-        for (String dateSalary : data) {
-            String[] firstLineArrays = dateSalary.split(" ");
-            LocalDate firstArraysDate = LocalDate.parse(firstLineArrays[CURRENT_DATE],FORMATTER);
-            if (localDateFrom.compareTo(firstArraysDate) <= 0
-                    && localDateTo.compareTo(firstArraysDate) >= 0) {
-                if (names[JOHN].equals(firstLineArrays[NAME])) {
-                    johnSalary += (Integer.parseInt(firstLineArrays[HOURS])
-                            * Integer.parseInt(firstLineArrays[PRICE_PER_HOUR]));
-                } else if (names[ANDREW].equals(firstLineArrays[NAME])) {
-                    andrewSalary += (Integer.parseInt(firstLineArrays[HOURS])
-                            * Integer.parseInt(firstLineArrays[PRICE_PER_HOUR]));
-                } else if (names[KATE].equals(firstLineArrays[NAME])) {
-                    kateSalary += (Integer.parseInt(firstLineArrays[HOURS])
+        LocalDate localDateFrom = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate localDateTo = LocalDate.parse(dateTo, FORMATTER);
+        StringBuilder salaryInfo = new StringBuilder("Report for period "
+                + dateFrom + " - " + dateTo);
+        for (int i = 0; i < names.length; i++) {
+            int salary = 0;
+            for (String dateSalary : data) {
+                String[] firstLineArrays = dateSalary.split(" ");
+                LocalDate firstArraysDate = LocalDate.parse(
+                        firstLineArrays[CURRENT_DATE], FORMATTER);
+                if (localDateFrom.compareTo(firstArraysDate) <= 0
+                        && localDateTo.compareTo(firstArraysDate) >= 0
+                        && names[i].equals(firstLineArrays[NAME])) {
+                    salary += (Integer.parseInt(firstLineArrays[HOURS])
                             * Integer.parseInt(firstLineArrays[PRICE_PER_HOUR]));
                 }
             }
+            salaryInfo.append(System.lineSeparator() + names[i] + " - " + salary);
         }
-        StringBuilder salaryInfo = new StringBuilder("Report for period "
-                + dateFrom + " - " + dateTo);
-        salaryInfo.append(System.lineSeparator() + names[JOHN] + " - " + johnSalary);
-        salaryInfo.append(System.lineSeparator() + names[ANDREW] + " - " + andrewSalary);
-        salaryInfo.append(System.lineSeparator() + names[KATE] + " - " + kateSalary);
         return salaryInfo.toString();
     }
 }
