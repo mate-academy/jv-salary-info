5224f718f9f77a526d7210eda9799a8a549d59e0
Oleksii <sukhenko.oleksii@gmail.com>
2024-05-18 15:09:18 +0300

    1) Refactor if statement for checking date range 2) Use constants for indexes 3) Append directly "Report for period..." 4) Change psf to private

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,28 +4,30 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    public static final DateTimeFormatter DATE_TIME_FORMATTER
+    private static final DateTimeFormatter DATE_TIME_FORMATTER
             = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int CURRENT_DATE_INDEX = 0;
+    private static final int EMPLOYEE_NAME_INDEX = 1;
+    private static final int HOURS_WORKED_INDEX = 2;
+    private static final int HOURS_RATE_INDEX = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder result = new StringBuilder();
         LocalDate fromDate = LocalDate.parse(dateFrom, DATE_TIME_FORMATTER);
         LocalDate toDate = LocalDate.parse(dateTo, DATE_TIME_FORMATTER);
         if (data.length > 0) {
-            String reportPeriod = "Report for period " + dateFrom + " - " + dateTo;
-            result.insert(0, reportPeriod);
             for (String name : names) {
                 StringBuilder nameResult = new StringBuilder();
                 int totalSalary = 0;
                 for (String datum : data) {
                     String[] date = datum.split(" ");
-                    LocalDate currentDate = LocalDate.parse(date[0], DATE_TIME_FORMATTER);
-                    String employeeName = date[1];
-                    int hoursWorked = Integer.parseInt(date[2]);
-                    int hoursRate = Integer.parseInt(date[3]);
+                    LocalDate currentDate = LocalDate.parse(date[CURRENT_DATE_INDEX], DATE_TIME_FORMATTER);
+                    String employeeName = date[EMPLOYEE_NAME_INDEX];
+                    int hoursWorked = Integer.parseInt(date[HOURS_WORKED_INDEX]);
+                    int hoursRate = Integer.parseInt(date[HOURS_RATE_INDEX]);
                     int salary = hoursRate * hoursWorked;
-                    if ((currentDate.isAfter(fromDate) || currentDate.isEqual(fromDate))
-                            && (currentDate.isBefore(toDate) || currentDate.isEqual(toDate))
+                    if ((!currentDate.isAfter(toDate))
+                            && (!currentDate.isBefore(fromDate))
                             && employeeName.equals(name)) {
                         totalSalary += salary;
                     }
@@ -34,6 +36,6 @@ public class SalaryInfo {
                 result.append(System.lineSeparator()).append(nameResult);
             }
         }
-        return result.toString();
+        return "Report for period " + dateFrom + " - " + dateTo + result;
     }
 }
