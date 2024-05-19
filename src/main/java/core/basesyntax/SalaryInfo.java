e33ac54550c5b162e839e822c4f5272739943ab6
Oleksii <sukhenko.oleksii@gmail.com>
2024-05-19 14:23:13 +0300

    Divide logic for 2 helpful private methods

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -13,30 +13,38 @@ public class SalaryInfo {
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder result = new StringBuilder();
-        LocalDate fromDate = LocalDate.parse(dateFrom, DATE_TIME_FORMATTER);
-        LocalDate toDate = LocalDate.parse(dateTo, DATE_TIME_FORMATTER);
+        LocalDate fromDate = parseDate(dateFrom);
+        LocalDate toDate = parseDate(dateTo);
         if (data.length > 0) {
             for (String name : names) {
-                StringBuilder nameResult = new StringBuilder();
-                int totalSalary = 0;
-                for (String datum : data) {
-                    String[] date = datum.split(" ");
-                    LocalDate currentDate
-                            = LocalDate.parse(date[CURRENT_DATE_INDEX], DATE_TIME_FORMATTER);
-                    String employeeName = date[EMPLOYEE_NAME_INDEX];
-                    int hoursWorked = Integer.parseInt(date[HOURS_WORKED_INDEX]);
-                    int hoursRate = Integer.parseInt(date[HOURS_RATE_INDEX]);
-                    int salary = hoursRate * hoursWorked;
-                    if ((!currentDate.isAfter(toDate))
-                            && (!currentDate.isBefore(fromDate))
-                            && employeeName.equals(name)) {
-                        totalSalary += salary;
-                    }
-                }
-                nameResult.append(name).append(" - ").append(totalSalary);
-                result.append(System.lineSeparator()).append(nameResult);
+                int totalSalary = calculateTotalSalaryForEmployee(name, data, fromDate, toDate);
+                result.append(System.lineSeparator())
+                        .append(name).append(" - ").append(totalSalary);
             }
         }
         return "Report for period " + dateFrom + " - " + dateTo + result;
+    }
+
+    private LocalDate parseDate(String date) {
+        return LocalDate.parse(date, DATE_TIME_FORMATTER);
+    }
+
+    private int calculateTotalSalaryForEmployee(String name, String[] data,
+                                                LocalDate fromDate, LocalDate toDate) {
+        int totalSalary = 0;
+        for (String datum : data) {
+            String[] date = datum.split(" ");
+            LocalDate currentDate = LocalDate.parse(date[CURRENT_DATE_INDEX], DATE_TIME_FORMATTER);
+            String employeeName = date[EMPLOYEE_NAME_INDEX];
+            int hoursWorked = Integer.parseInt(date[HOURS_WORKED_INDEX]);
+            int hoursRate = Integer.parseInt(date[HOURS_RATE_INDEX]);
+            int salary = hoursRate * hoursWorked;
+            if (!currentDate.isAfter(toDate)
+                    && !currentDate.isBefore(fromDate)
+                    && employeeName.equals(name)) {
+                totalSalary += salary;
+            }
+        }
+        return totalSalary;
     }
 }
