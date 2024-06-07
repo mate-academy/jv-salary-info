648600dcd8672465a2efa751f3ea438c24447abd
Oleksii <obabych1@stu.vistula.edu.pl>
2024-06-07 19:57:42 +0200

    fixed mistakes with redundant loops

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -16,19 +16,16 @@ public class SalaryInfo {
         int[] salaries = new int[names.length];
         StringBuilder result = new StringBuilder();
 
-        for (String partOfData : data) {
-            String[] parts = partOfData.split(" ");
-            LocalDate date = LocalDate.parse(parts[DATE_INDEX], formatter);
-            String nameOfEmployee = parts[NAME_INDEX];
-            int hours = Integer.parseInt(parts[HOURS_INDEX]);
-            int rate = Integer.parseInt(parts[RATE_INDEX]);
+        for (int i = 0; i < names.length; i++) {
+            for (String partOfData : data) {
+                String[] parts = partOfData.split(" ");
+                LocalDate date = LocalDate.parse(parts[DATE_INDEX], formatter);
+                String nameOfEmployee = parts[NAME_INDEX];
+                int hours = Integer.parseInt(parts[HOURS_INDEX]);
+                int rate = Integer.parseInt(parts[RATE_INDEX]);
 
-            if (date.compareTo(fromDate) >= 0 && date.compareTo(toDate) <= 0) {
-                for (int i = 0; i < names.length; i++) {
-                    if (nameOfEmployee.equals(names[i])) {
-                        salaries[i] += hours * rate;
-                        break;
-                    }
+                if (names[i].equals(nameOfEmployee) && !date.isBefore(fromDate) && !date.isAfter(toDate)) {
+                    salaries[i] += hours * rate;
                 }
             }
         }
