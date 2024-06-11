936de9169e302ff8450eec88fec01047202e2481
Artem Hurievskyi <gurievsky.artem@gmail.com>
2024-06-11 11:52:31 +0300

    create an implementation for the method "getSalaryInfo"

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,38 @@
 package core.basesyntax;
 
-public class SalaryInfo {
-    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+import java.time.format.DateTimeParseException;
+
+public class SalaryInfo extends DataComparison {
+
+    public String getSalaryInfo(String[] names, String[] dates, String dateFrom, String dateTo) {
+        StringBuilder builder = new StringBuilder("Report for period ");
+        int[] employeeSalary = new int[names.length];
+
+        try {
+            for (String date : dates) {
+                String[] record = date.split("\\s+");
+                String recordDate = record[0];
+                String recordName = record[1];
+                int totalHours = Integer.parseInt(record[2]);
+                int salaryPerHour = Integer.parseInt(record[3]);
+
+                for (int i = 0; i < names.length; i++) {
+                    if (names[i].equals(recordName)
+                            && isDateInRange(dateFrom, dateTo, recordDate)) {
+                        employeeSalary[i] += totalHours * salaryPerHour;
+                    }
+                }
+            }
+        } catch (DateTimeParseException | NumberFormatException e) {
+            System.out.println("Invalid date format" + e);
+
+        }
+
+        builder.append(dateFrom).append(" - ").append(dateTo).append(System.lineSeparator());
+        for (int i = 0; i < names.length; i++) {
+            builder.append(names[i]).append(" - ").append(employeeSalary[i])
+                    .append(System.lineSeparator());
+        }
+        return builder.toString().trim();
     }
 }
