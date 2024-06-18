0f66a755b8efff1c10f15585e24b8bf252f4c609
Panchenko Dmytro <panchenko0795@gmail.com>
2024-06-19 00:33:06 +0300

    'if' statement for date checking was simplified; removed 'try catch'; The variable 'income' renamed to 'employeeIncome'; removed the 'lineSeparator' variable and used System.lineSeparator() instead; added a length check to "employeeInfoArray" before accessing its elements to ensure all required data is present

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -10,45 +10,37 @@ public class SalaryInfo {
     private static final int NAME_INDEX = 1;
     private static final int HOUR_INDEX = 2;
     private static final int INCOME_PER_HOUR_INDEX = 3;
+    private static final int EMPLOYEE_INFO_ARRAY_LENGTH = 4;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder result = new StringBuilder();
-        String lineSeparator = System.lineSeparator();
         result.append("Report for period ").append(dateFrom).append(" - ").append(dateTo)
-                .append(lineSeparator);
+                .append(System.lineSeparator());
 
         LocalDate parsedDateFrom = LocalDate.parse(dateFrom, DATE_TIME_FORMATTER);
         LocalDate parsedDateTo = LocalDate.parse(dateTo, DATE_TIME_FORMATTER);
 
         for (int i = 0; i < names.length; i++) {
-            try {
-                int income = 0;
-                for (String employeeInfo : data) {
-                    String[] employeeInfoArray = employeeInfo.split(" ");
-
+            int employeeIncome = 0;
+            for (String employeeInfo : data) {
+                String[] employeeInfoArray = employeeInfo.split(" ");
+                if (employeeInfoArray.length == EMPLOYEE_INFO_ARRAY_LENGTH) {
                     String nameFromEmployeeInfo = employeeInfoArray[NAME_INDEX];
 
                     if (nameFromEmployeeInfo.equals(names[i])) {
                         LocalDate dateFromEmployeeInfo =
                                 LocalDate.parse(employeeInfoArray[DATE_INDEX], DATE_TIME_FORMATTER);
-                        if (dateFromEmployeeInfo.isEqual(parsedDateFrom)
-                                || (dateFromEmployeeInfo.isAfter(parsedDateFrom)
-                                && dateFromEmployeeInfo.isBefore(parsedDateTo))
-                                || dateFromEmployeeInfo.isEqual(parsedDateTo)) {
-                            income += Integer.parseInt(employeeInfoArray[HOUR_INDEX])
+                        if (!dateFromEmployeeInfo.isBefore(parsedDateFrom)
+                                && !dateFromEmployeeInfo.isAfter(parsedDateTo)) {
+                            employeeIncome += Integer.parseInt(employeeInfoArray[HOUR_INDEX])
                                     * Integer.parseInt(employeeInfoArray[INCOME_PER_HOUR_INDEX]);
                         }
                     }
                 }
-                result.append(names[i]).append(" - ").append(income);
-                if (i < names.length - 1) {
-                    result.append(lineSeparator);
-                }
-            } catch (IndexOutOfBoundsException exception) {
-                System.out.println("Invalid input data in employee info for "
-                        + names[i] + ", skip it and continue...");
-            } catch (Exception exception) {
-                throw new RuntimeException("Something went wrong" + exception);
+            }
+            result.append(names[i]).append(" - ").append(employeeIncome);
+            if (i < names.length - 1) {
+                result.append(System.lineSeparator());
             }
         }
         return result.toString();
