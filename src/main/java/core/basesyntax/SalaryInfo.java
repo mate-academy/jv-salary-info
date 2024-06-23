cb2b3c0840caba3cdeac0548d62ce2f0ae0b6dc4
Ihor Sydorenko <sidorenko.igorek@gmail.com>
2024-06-23 13:44:38 +0300

    valid solution

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,41 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+    private static DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int WORK_HOURS_INDEX = 2;
+    private static final int SALARY_PER_HOUR_INDEX = 3;
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        StringBuilder stringBuilder = new StringBuilder("Report for period ")
+                .append(dateFrom)
+                .append(" - ")
+                .append(dateTo);
+        LocalDate localDateFrom = getParseDate(dateFrom);
+        LocalDate localDateTo = getParseDate(dateTo);
+        for (String name : names) {
+            int totalSalary = 0;
+            for (String splitData : data) {
+                String[] split = splitData.split(" ");
+                LocalDate foundDate = getParseDate(split[DATE_INDEX]);
+                if (name.equals(split[NAME_INDEX])
+                        && localDateFrom.isBefore(foundDate)
+                        && (localDateTo.isAfter(foundDate) || localDateTo.isEqual(foundDate))) {
+                    totalSalary += Integer.parseInt(split[WORK_HOURS_INDEX])
+                            * Integer.parseInt(split[SALARY_PER_HOUR_INDEX]);
+                }
+            }
+            stringBuilder.append(System.lineSeparator()).append(name)
+                    .append(" - ").append(totalSalary);
+        }
+        return stringBuilder.toString();
+    }
+
+    private LocalDate getParseDate(String date) {
+        return LocalDate.parse(date, FORMATTER);
     }
 }
