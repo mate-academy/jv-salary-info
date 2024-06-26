371e5b15c5c4898baa722228edeae7e4669e8c4c
Denys Mazurenko <0gur4ik3992@gmail.com>
2024-06-26 22:19:02 +0300

    Implemented getSalaryInfo.

---
+++ src/main/java/core/basesyntax/Main.java
@@ -0,0 +1,26 @@
+package core.basesyntax;
+
+public class Main {
+    public static void main(String[] args) {
+        String [] data = new String[] {
+                "26.04.2019 John 4 50",
+                "05.04.2019 Andrew 3 200",
+                "10.04.2019 John 7 100",
+                "22.04.2019 Kate 9 100",
+                "25.06.2019 John 11 50",
+                "26.04.2019 Andrew 3 150",
+                "13.02.2019 John 7 100",
+                "26.04.2019 Kate 9 100"
+        };
+        String [] name = new String[] {
+                "John",
+                "Andrew",
+                "Kate"
+        };
+        String dateFrom = "01.04.2024";
+        String dateTo = "30.04.2024";
+
+        SalaryInfo salaryInfo = new SalaryInfo();
+        System.out.println(salaryInfo.getSalaryInfo(name, data, dateFrom, dateTo));
+    }
+}
--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,49 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int INDEX_OF_DATE = 0;
+    private static final int INDEX_OF_NAME = 1;
+    private static final int INDEX_OF_HOUR = 2;
+    private static final int INDEX_OF_MONEY_PER_HOUR = 3;
+    private static final String LINE_SEPARATOR = System.lineSeparator();
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder
+                .append("Report for period ")
+                .append(dateFrom)
+                .append(" - ")
+                .append(dateTo);
+        LocalDate startDate = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate endDate = LocalDate.parse(dateTo, FORMATTER);
+        for (String name: names) {
+            int totalSalary = 0;
+            for (String dates: data) {
+                String [] part = dates.split(" ");
+                LocalDate recordDate = LocalDate.parse(part[INDEX_OF_DATE],FORMATTER);
+
+                if (name.equals(part[INDEX_OF_NAME])
+                        && isDatesIncludes(recordDate, startDate, endDate)) {
+                    totalSalary +=
+                            Integer.parseInt(part[INDEX_OF_HOUR])
+                                    * Integer.parseInt(part[INDEX_OF_MONEY_PER_HOUR]);
+                }
+            }
+            stringBuilder
+                    .append(LINE_SEPARATOR)
+                    .append(name)
+                    .append(" - ")
+                    .append(totalSalary);
+        }
+        return stringBuilder.toString();
+    }
+
+    private boolean isDatesIncludes(LocalDate localDate, LocalDate dateFrom, LocalDate dateTo) {
+        return (localDate.isAfter(dateFrom) || localDate.equals(dateFrom))
+                && (localDate.isBefore(dateTo) || localDate.equals(dateTo));
     }
 }
