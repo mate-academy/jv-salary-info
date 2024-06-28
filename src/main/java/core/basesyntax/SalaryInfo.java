bebd07c60d95ff18b10e1414e6551c8ad4f654d7
Kutik <vvladimirkutovoy@gmail.com>
2024-06-27 20:41:24 -0700

    fix conflict

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,6 @@
 package core.basesyntax;
 
 public class SalaryInfo {
-    private final Formatter formatter = new Formatter();
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder builder = new StringBuilder();
@@ -14,12 +13,12 @@ public class SalaryInfo {
         for (int i = 0; i < names.length; i++) {
             User user = new User(names[i]);
             for (int j = 0; j < data.length; j++) {
-                DataSalary dataSalary = formatter.stringToDataSalary(data[j]);
+                DataSalary dataSalary = new Formatter().stringToDataSalary(data[j]);
                 if (dataSalary.getName().equals(user.getName())
                         && dataSalary.getDate()
-                        .isBefore(formatter.stringToLocalDate(dateTo).plusDays(1))
+                        .isBefore(new Formatter().stringToLocalDate(dateTo).plusDays(1))
                         && dataSalary.getDate()
-                        .isAfter(formatter.stringToLocalDate(dateFrom).minusDays(1))) {
+                        .isAfter(new Formatter().stringToLocalDate(dateFrom).minusDays(1))) {
                     user.addAmountSalary(
                             dataSalary.getHoursQuantity() * dataSalary.getSalaryPreHour());
                 }
