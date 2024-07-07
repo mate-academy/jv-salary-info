9e8ef59fe6f290c9b1bf42f1b4828c7d420aa5ee
David Vozniak <david.vozniak.ua@gmail.com>
2024-07-07 17:35:25 -0600

    added some extra logic

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         if (names.length == 0 || data.length == 0 || dateFrom.isEmpty() || dateTo.isEmpty()) {
-            return null;
+            throw new IllegalArgumentException("No information provided");
         }
         StringBuilder results = new StringBuilder("Report for period ");
         int firstIndex = dateFrom.indexOf('=') + 3;
@@ -11,15 +11,24 @@ public class SalaryInfo {
                 .append(" - ")
                 .append(dateTo.substring(firstIndex, dateTo.lastIndexOf(dateTo)))
                 .append(System.lineSeparator());
-        for (int i = 0; i < data.length; i++) {
-            int lastData = Integer.parseInt(dateTo.substring(firstIndex, firstIndex + 2));
-            int userDay = Integer.parseInt(data[0].substring(0,2));
-            int sumOfDay = lastData - userDay + 1;
-            int sum = Integer.parseInt(data[2]) * Integer.parseInt(data[3]) * sumOfDay;
-            results.append(names[i])
-                    .append(" - ")
-                    .append(sum)
-                    .append(System.lineSeparator());
+        for (String dates: data) {
+            for (String name: names) {
+                String[] composite = dates.split(" ");
+                int month = Integer.parseInt(dateTo.substring(firstIndex + 3, firstIndex + 5));
+                int userMonth = Integer.parseInt(composite[0].substring(3, 5));
+                if (month == userMonth) {
+                    if (name.equals(composite[1])) {
+                        int totalSum = 0;
+                        int daySum = Integer.parseInt(composite[2]) * Integer.parseInt(composite[3]);
+                        totalSum += daySum;
+                        results.append(composite[1])
+                                .append(" - ")
+                                .append(totalSum)
+                                .append(System.lineSeparator());
+                    }
+                }
+
+            }
         }
         return results.toString();
     }
