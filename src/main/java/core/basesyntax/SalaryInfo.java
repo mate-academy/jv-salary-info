6a3fcad5019aa2de4d876429447c60ec38022b7d
David Vozniak <david.vozniak.ua@gmail.com>
2024-07-07 20:12:33 -0600

    fixed issues

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -11,24 +11,29 @@ public class SalaryInfo {
                 .append(" - ")
                 .append(dateTo.substring(firstIndex, dateTo.lastIndexOf(dateTo)))
                 .append(System.lineSeparator());
-        for (String dates: data) {
-            for (String name: names) {
-                String[] composite = dates.split(" ");
-                int month = Integer.parseInt(dateTo.substring(firstIndex + 3, firstIndex + 5));
-                int userMonth = Integer.parseInt(composite[0].substring(3, 5));
-                if (month == userMonth) {
-                    if (name.equals(composite[1])) {
-                        int totalSum = 0;
-                        int daySum = Integer.parseInt(composite[2]) * Integer.parseInt(composite[3]);
-                        totalSum += daySum;
-                        results.append(composite[1])
-                                .append(" - ")
-                                .append(totalSum)
-                                .append(System.lineSeparator());
-                    }
-                }
-
+        String[] usedNames = new String[data.length];
+        int[] userSum = new int[data.length];
+        int sum = 0;
+        for (int i = 0; i < data.length; i++) {
+            String[] composite = data[i].split(" ");
+            int month = Integer.parseInt(dateTo.substring(firstIndex + 3, firstIndex + 5));
+            int userMonth = Integer.parseInt(composite[0].substring(3, 5));
+            if (month == userMonth) {
+                usedNames[i] = composite[1];
+                int daySum = Integer.parseInt(composite[2]) * Integer.parseInt(composite[3]);
+                userSum[i] = daySum;
             }
+        }
+        for (int i = 0; i < usedNames.length; i++) {
+            for (String name : names) {
+                if (usedNames[i].equals(name)) {
+                    sum += userSum[i];
+                }
+            }
+            results.append(usedNames[i])
+                    .append(" - ")
+                    .append(sum)
+                    .append(System.lineSeparator());
         }
         return results.toString();
     }
