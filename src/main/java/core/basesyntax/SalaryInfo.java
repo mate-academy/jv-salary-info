10cfbb1dc144829b91e477137750580ef1cb323d
Yehor Nazarets <nazarets1111@gmail.com>
2024-07-17 19:25:11 +0200

    shorten method, because I don`t know how to divide this method, he is not working after divide

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,43 +4,38 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private static final String DATE_FORMAT = "dd.MM.yyyy";
-    private static final String INFO_SEPARATOR = " - ";
-    private static final String DATA_SEPARATOR = " ";
-    private static final int INDEX_OF_DATA = 0;
-    private static final int INDEX_OF_NAME = 1;
-    private static final int INDEX_OF_WORK_HOURS = 2;
-    private static final int INDEX_SALARY_PER_HOUR = 3;
-    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern(DATE_FORMAT);
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int WORK_HOURS_INDEX = 2;
+    private static final int SALARY_PER_HOUR_INDEX = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        LocalDate startDate = parseDateWithFormatter(dateFrom);
-        LocalDate endDate = parseDateWithFormatter(dateTo);
-        StringBuilder employeeInfo = new StringBuilder("Report for period ")
+        StringBuilder stringBuilder = new StringBuilder("Report for period ")
                 .append(dateFrom)
-                .append(INFO_SEPARATOR)
+                .append(" - ")
                 .append(dateTo);
+        LocalDate localDateFrom = getParseDate(dateFrom);
+        LocalDate localDateTo = getParseDate(dateTo);
         for (String name : names) {
             int totalSalary = 0;
-            employeeInfo.append(System.lineSeparator())
-                    .append(name)
-                    .append(INFO_SEPARATOR);
-            for (String currentData : data) {
-                String[] dateSplit = currentData.split(DATA_SEPARATOR);
-                LocalDate date = parseDateWithFormatter(dateSplit[INDEX_OF_DATA]);
-                if ((date.equals(startDate) || date.isAfter(startDate))
-                        && (date.equals(endDate) || date.isBefore(endDate))
-                        && name.equals(dateSplit[INDEX_OF_NAME])) {
-                    totalSalary += Integer.parseInt(dateSplit[INDEX_OF_WORK_HOURS])
-                            * Integer.parseInt(dateSplit[INDEX_SALARY_PER_HOUR]);
+            for (String splitData : data) {
+                String[] splitArray = splitData.split(" ");
+                LocalDate foundDate = getParseDate(splitArray[DATE_INDEX]);
+                if (name.equals(splitArray[NAME_INDEX])
+                        && localDateFrom.isBefore(foundDate)
+                        && (localDateTo.isAfter(foundDate) || localDateTo.isEqual(foundDate))) {
+                    totalSalary += Integer.parseInt(splitArray[WORK_HOURS_INDEX])
+                            * Integer.parseInt(splitArray[SALARY_PER_HOUR_INDEX]);
                 }
             }
-            employeeInfo.append(totalSalary);
+            stringBuilder.append(System.lineSeparator()).append(name)
+                    .append(" - ").append(totalSalary);
         }
-        return employeeInfo.toString();
+        return stringBuilder.toString();
     }
 
-    private LocalDate parseDateWithFormatter(String date) {
+    private LocalDate getParseDate(String date) {
         return LocalDate.parse(date, FORMATTER);
     }
 }
