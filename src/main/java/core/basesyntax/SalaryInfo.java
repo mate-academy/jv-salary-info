d4c992beedf61724a63ed9ce33f63fd4f52f2ae0
Wersebu <cz.damax@gmail.com>
2024-07-27 22:52:21 +0200

    All commits translated. Created new method IsValid to make code more clean. Changed names of constant variables

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -6,56 +6,54 @@ import java.util.Arrays;
 
 public class SalaryInfo {
     private static final DateTimeFormatter DATE_TIME_FORMATTER
-            = DateTimeFormatter.ofPattern("dd.MM.yyyy"); //Definiowanie formatu DAT dla programu
-    private static final int defaultDataToAray = 0;
-    private static final int datePoss = 0;
-    //Ustawienie pozycji Daty w Tablicy
-    private static final int namePoss = 1;
-    //Ustawienie pozycji Imienia Pracownika w Tablicy
-    private static final int hoursPoss = 2;
-    //Ustawienie pozycji Liczby przepracowanych godzin danego dnia
-    private static final int salaryPoss = 3;
-    //Ustawienie pozycji stawki godzinowej
+            = DateTimeFormatter.ofPattern("dd.MM.yyyy"); //Define the date format for the program
+    private static final int DEAFULT_DATA_TO_ARAY = 0;
+    private static final int DATE_POSS = 0;
+    //Setting the Date position in the Table
+    private static final int EMPLOY_NAME_POSS = 1;
+    //Setting the position of the Employee's Name in the Board
+    private static final int HOURS_WORKED_POSS = 2;
+    //Setting the Number of hours worked on a given day
+    private static final int SALARY_ERNED_POSS = 3;
+    //Setting the hourly rate item
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         final String header = "Report for period " + dateFrom + " - " + dateTo;
-        //Definiowanie hedera dla tablicy
+        //Defining a header for an array
         StringBuilder stringBuilder = new StringBuilder(header);
-        //Budowanie StringBuildera
+        //Building StringBuilder
 
         LocalDate localFrom = LocalDate.parse(dateFrom, DATE_TIME_FORMATTER);
-        //Pobieranie dateFrom i jego konwersja na LocalDate
+        //Retrieving dateFrom and converting it to LocalDate
         LocalDate localTo = LocalDate.parse(dateTo,DATE_TIME_FORMATTER);
-        //Pobieranie dateTo i jego konwersja na LocalDate
+        //Retrieving dateTo and converting it to LocalDate
 
         int[] salaries = new int[names.length];
-        //Tworzenie tablicy wypłat o długości liczby imion pracowników
-        Arrays.fill(salaries,defaultDataToAray);
-        //Ustawienie pozycji nr 0 jako pustej pod header
+        //Creating a pay table with the length of the number of employee names
+        Arrays.fill(salaries,DEAFULT_DATA_TO_ARAY);
+        //Setting position no. 0 as empty under the header
 
         for (int i = 0; i < names.length; i++) {
             for (String split : data) {
-                //Wstawienie splita jako " " (SPACJA)
                 String[] splitData = split.split(" ");
-                LocalDate localDateFromData = LocalDate
-                        .parse(splitData[datePoss], DATE_TIME_FORMATTER);
-                //Ustawienie wyżej zdefiniowanego rodzaju czytania dat dla tej petli
-
-                if (names[i].equals(splitData[namePoss])
-                        && !localDateFromData.isBefore(localFrom)
-                        && !localDateFromData.isAfter(localTo)) {
-                    salaries[i] += Integer.parseInt(splitData[hoursPoss])
-                            * Integer.parseInt(splitData[salaryPoss]);
-                    //Warunek sprawdzający czy daty z tablicy
-                    // mieszczą się w widełkach daty od do daty do
+                if (isValid(names[i], splitData, localFrom, localTo)) {
+                    salaries[i] += Integer.parseInt(splitData[HOURS_WORKED_POSS])
+                            * Integer.parseInt(splitData[SALARY_ERNED_POSS]);
                 }
             }
             stringBuilder.append(System.lineSeparator()).append(names[i])
                     .append(" - ").append(salaries[i]);
-            //Wstawienie separatora dla końcowego wyniku w postaci myślnika
         }
 
         return stringBuilder.toString();
-        //Zwrócenie powstałego wyniku w postaci tekstu "String"
+        //Returning the resulting result as "String" text
+    }
+
+    private boolean isValid(String employeName, String[] recordData,
+                            LocalDate dateFrom, LocalDate dateTo) {
+        LocalDate recordDate = LocalDate.parse(recordData[DATE_POSS], DATE_TIME_FORMATTER);
+        return employeName.equals(recordData[EMPLOY_NAME_POSS])
+                && !recordDate.isBefore(dateFrom)
+                && !recordDate.isAfter(dateTo);
     }
 }
