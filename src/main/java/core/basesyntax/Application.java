14eb1e79590013d79ec99baacaf2a5668e540d16
Igor Neminuschii <igorgorslav@gmail.com>
2024-07-29 00:18:51 +0300

    implemented getSalaryInfo method

---
+++ src/main/java/core/basesyntax/Application.java
@@ -0,0 +1,20 @@
+package core.basesyntax;
+
+public class Application {
+    public static void main(String[] args) {
+        SalaryInfo salaryInfo = new SalaryInfo();
+        System.out.println(salaryInfo.getSalaryInfo(new String[] {"John", "Andrew", "Kate"},
+                new String[] {
+                        "13.07.2019 John 60 50",
+                        "15.07.2019 Andrew 3 200",
+                        "15.07.2019 Kate 10 100",
+
+                        "16.07.2019 Andrew 3 200",
+                        "16.07.2019 Kate 9 100",
+
+                        "10.08.2019 John 7 100",
+                        "08.08.2019 Kate 3 80",
+                        "11.08.2019 Andrew 8 100"},
+                "14.07.2019", "10.08.2019"));
+    }
+}
--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,35 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+    private static final int DATE = 0;
+    private static final int NAME = 1;
+    private static final int HOURS = 2;
+    private static final int RATE = 3;
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        StringBuilder report = new StringBuilder("Report for period " + dateFrom + " - " + dateTo);
+        final LocalDate beginning = LocalDate.parse(dateFrom, FORMATTER);
+        final LocalDate end = LocalDate.parse(dateTo, FORMATTER);
+        int income = 0;
+
+        for (String name : names) {
+            for (String datum : data) {
+                String[] dataElements = datum.split(" ");
+                LocalDate currentDate = LocalDate.parse(dataElements[DATE], FORMATTER);
+                if ((currentDate.isAfter(beginning) || currentDate.isEqual(beginning))
+                        && (currentDate.isBefore(end) || currentDate.isEqual(end))
+                        && name.equals(dataElements[NAME])) {
+                    income += Integer.parseInt(dataElements[HOURS])
+                            * Integer.parseInt(dataElements[RATE]);
+                }
+            }
+            report.append(System.lineSeparator()).append(name).append(" - ").append(income);
+            income = 0;
+        }
+        return report.toString();
     }
 }
