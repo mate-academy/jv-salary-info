142969783ce8f18128ec6565c1376459517d9335
skrypkasv <skrypkasv@gmail.com>
2024-07-30 07:43:57 +0300

    create helpful meth isDayInRange

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -11,9 +11,16 @@ public class SalaryInfo {
     private static final int HOURS_INDEX = 2;
     private static final int PAY_FOR_HOUR_INDEX = 3;
 
+    private static Boolean isDayInRange(String day, String startOfRange, String finalOfRange) {
+        LocalDate startDay = LocalDate.parse(startOfRange, FORMATTER);
+        LocalDate finalDay = LocalDate.parse(finalOfRange, FORMATTER);
+        LocalDate checkDay = LocalDate.parse(day, FORMATTER);
+        return (checkDay.equals(startDay) || checkDay.isAfter(startDay))
+                && (checkDay.equals(finalDay)
+                || checkDay.isBefore(finalDay));
+    }
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        LocalDate startDay = LocalDate.parse(dateFrom, FORMATTER);
-        LocalDate finalDay = LocalDate.parse(dateTo, FORMATTER);
         StringBuilder builder = new StringBuilder();
         builder.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
 
@@ -21,14 +28,12 @@ public class SalaryInfo {
             int salary = 0;
             for (String record : data) {
                 String[] workingDayInfo = record.split(" ");
-                LocalDate workingDay = LocalDate.parse(workingDayInfo[DAY_INDEX], FORMATTER);
+                String workingDay = workingDayInfo[DAY_INDEX];
                 String employeeName = workingDayInfo[EMPLOYEE_INDEX];
                 int workingHours = Integer.parseInt(workingDayInfo[HOURS_INDEX]);
                 int payForHour = Integer.parseInt(workingDayInfo[PAY_FOR_HOUR_INDEX]);
                 if (name != null && name.equals(employeeName)) {
-                    if ((workingDay.equals(startDay) || workingDay.isAfter(startDay))
-                            && (workingDay.equals(finalDay)
-                            || workingDay.isBefore(finalDay))) {
+                    if (isDayInRange(workingDay, dateFrom, dateTo)) {
                         int dayIncome = workingHours * payForHour;
                         salary += dayIncome;
                     }
