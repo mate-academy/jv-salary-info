adef4dddca74b0e16894088dd6dcd2fc42555a59
Oleh <olegvyrvykhvost@gmail.com>
2024-08-09 12:03:04 +0300

    reworked method getSalaryInfo

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,105 +1,41 @@
 package core.basesyntax;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd.MM.yyyy");
-    private static final int DATE_PART = 0;
-    private static final int NAME_PART = 1;
-    private static final int HOURS_PART = 2;
-    private static final int INCOMING_PER_PART = 3;
+    private static final DateTimeFormatter DATE_FORMATTER
+            = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        Map<String, Integer> mapOfName = putNameInMap(names);
-        Date startDate = getStartDate(dateFrom);
-        Date endDate = getEndDate(dateTo);
-        putSalaryInMap(mapOfName, data, startDate, endDate);
-        return getFormatedString(mapOfName, names, dateFrom, dateTo);
-    }
+        StringBuilder report = new StringBuilder("Report for period ");
+        report.append(getFormattedData(dateFrom, "-", dateTo));
 
-    private Map<String, Integer> putNameInMap(String[] names) {
-        Map<String, Integer> salaries = new HashMap<>();
         for (String name : names) {
-            salaries.put(name, 0);
-        }
-        return salaries;
-    }
-
-    private Date getStartDate(String dateFrom) {
-        Date startDate;
-        try {
-            startDate = DATE_FORMAT.parse(dateFrom);
-        } catch (ParseException e) {
-            throw new RuntimeException("Invalid date format", e);
-        }
-        return startDate;
-    }
-
-    private Date getEndDate(String dateTo) {
-        Date endDate;
-        try {
-            endDate = DATE_FORMAT.parse(dateTo);
-        } catch (ParseException e) {
-            throw new RuntimeException("Invalid date format", e);
-        }
-        return endDate;
-    }
-
-    private void putSalaryInMap(Map<String,Integer> mapOfName,
-                                String[] data,
-                                Date startDate, Date endDate) {
-
-        Date entryDate;
-        for (String entry : data) {
-            String[] entryParts = entry.split(" ");
-            String dateStr = entryParts[DATE_PART];
-            String employeeName = entryParts[NAME_PART];
-            int hoursWorked = Integer.parseInt(entryParts[HOURS_PART]);
-            int incomePerHour = Integer.parseInt(entryParts[INCOMING_PER_PART]);
-
-            try {
-                entryDate = DATE_FORMAT.parse(dateStr);
-            } catch (ParseException e) {
-                throw new RuntimeException("Invalid entry date format", e);
+            int totalSalary = 0;
+            for (String values : data) {
+                String[] dataInfo = values.split(" ");
+                if (isInRangeOfPeriod(dataInfo[0], dateFrom, dateTo)
+                        && name.equals(dataInfo[1])) {
+                    totalSalary += Integer.parseInt(dataInfo[2]) * Integer.parseInt(dataInfo[3]);
+                }
             }
-            putSalary(mapOfName, entryDate, startDate, endDate,
-                    employeeName, hoursWorked, incomePerHour);
+            report.append(System.lineSeparator())
+                    .append(getFormattedData(name, "-", Integer.toString(totalSalary)));
         }
+        return report.toString();
     }
 
-    private void putSalary(Map<String,Integer> mapOfName,
-                           Date entryDate, Date startDate,
-                           Date endDate, String employeeName,
-                           int hoursWorked, int incomePerHour) {
-
-        if (!entryDate.before(startDate)
-                && !entryDate.after(endDate)
-                && mapOfName.containsKey(employeeName)) {
-            int currentSalary = mapOfName.get(employeeName);
-            currentSalary += hoursWorked * incomePerHour;
-            mapOfName.put(employeeName, currentSalary);
-        }
+    private boolean isInRangeOfPeriod(String data, String dataFrom, String dataTo) {
+        return !encodeData(data).isBefore(encodeData(dataFrom))
+                && !encodeData(data).isAfter(encodeData(dataTo));
     }
 
-    private String getFormatedString(Map<String, Integer> mapOfName,
-                                     String[] names, String dateFrom, String dateTo) {
-        StringBuilder result = new StringBuilder();
-        result.append("Report for period ")
-                .append(dateFrom)
-                .append(" - ")
-                .append(dateTo)
-                .append(System.lineSeparator());
+    private LocalDate encodeData(String data) {
+        return LocalDate.parse(data, DATE_FORMATTER);
+    }
 
-        for (String name : names) {
-            result.append(name)
-                    .append(" - ")
-                    .append(mapOfName.get(name))
-                    .append(System.lineSeparator());
-        }
-        return result.toString().trim();
+    private String getFormattedData(String...data) {
+        return String.join(" ", data);
     }
 }
