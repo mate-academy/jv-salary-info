ae6e9aca8dccca07d6c8636c28001ce625ac5c59
ttikr <ia03.danchuk.volodymyr@gmail.com>
2024-08-29 20:11:02 +0300

    refactored after checklist.md

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,10 +5,16 @@ import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d.MM.yyyy");
+    private final int startPositionForSubstringDate = 0;
+    private final int endPositionForSubstringDate = 10;
+    private final int startPositionForSubstringIncomeData = 12;
+    private final int arrayPositionOfWorkingHours = 0;
+    private final int arrayPositionOfIncomePerHour = 1;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate localDateFrom = LocalDate.parse(dateFrom, formatter);
         LocalDate localDateTo = LocalDate.parse(dateTo, formatter);
+
         StringBuilder salaryInfo = new StringBuilder();
         salaryInfo.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
 
@@ -17,21 +23,22 @@ public class SalaryInfo {
             int salary = 0;
             for (String simpleData : data) {
                 if (simpleData.contains(name)) {
-                    LocalDate rowDate = LocalDate.parse(simpleData.substring(0, 10), formatter);
+                    LocalDate rowDate = LocalDate.parse(simpleData.substring(
+                                    startPositionForSubstringDate, endPositionForSubstringDate),
+                            formatter);
                     if ((rowDate.isAfter(localDateFrom) || rowDate.isEqual(localDateFrom))
                             && rowDate.isBefore(localDateTo) || rowDate.isEqual(localDateTo)) {
 
-                        String[] dayIncomeInformation = simpleData.substring(12
-                                + name.length()).split(" ");
-                        salary += Integer.parseInt(dayIncomeInformation[0])
-                                * Integer.parseInt(dayIncomeInformation[1]);
+                        String[] dayIncomeInfo = simpleData.substring(
+                                startPositionForSubstringIncomeData
+                                        + name.length()).split(" ");
+                        salary += Integer.parseInt(dayIncomeInfo[arrayPositionOfWorkingHours])
+                                * Integer.parseInt(dayIncomeInfo[arrayPositionOfIncomePerHour]);
                     }
                 }
             }
             salaryInfo.append(salary);
         }
-
         return salaryInfo.toString();
     }
-
 }
