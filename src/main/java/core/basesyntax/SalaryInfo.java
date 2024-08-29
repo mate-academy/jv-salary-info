beeb4cf1552afe64cb73de8c352628c7e703d7c0
ttikr <ia03.danchuk.volodymyr@gmail.com>
2024-08-29 19:56:38 +0300

    first version of test-passed solution

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,37 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        LocalDate localDateFrom = LocalDate.parse(dateFrom, formatter);
+        LocalDate localDateTo = LocalDate.parse(dateTo, formatter);
+        StringBuilder salaryInfo = new StringBuilder();
+        salaryInfo.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
+
+        for (String name : names) {
+            salaryInfo.append(System.lineSeparator()).append(name).append(" - ");
+            int salary = 0;
+            for (String simpleData : data) {
+                if (simpleData.contains(name)) {
+                    LocalDate rowDate = LocalDate.parse(simpleData.substring(0, 10), formatter);
+                    if ((rowDate.isAfter(localDateFrom) || rowDate.isEqual(localDateFrom))
+                            && rowDate.isBefore(localDateTo) || rowDate.isEqual(localDateTo)) {
+
+                        String[] dayIncomeInformation = simpleData.substring(12
+                                + name.length()).split(" ");
+                        salary += Integer.parseInt(dayIncomeInformation[0])
+                                * Integer.parseInt(dayIncomeInformation[1]);
+                    }
+                }
+            }
+            salaryInfo.append(salary);
+        }
+
+        return salaryInfo.toString();
     }
+
 }
