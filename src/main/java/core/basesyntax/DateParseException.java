6ae94e0e9f2670cafe9e249c5c109408fb163654
Serhiy <sergie3007@gmail.com>
2024-08-31 21:42:33 +0300

    add DataParseException, rename checkData to splitData, improved SalaryInfo class for better readability

---
+++ src/main/java/core/basesyntax/DateParseException.java
@@ -0,0 +1,7 @@
+package core.basesyntax;
+
+public class DateParseException extends RuntimeException {
+    public DateParseException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,42 +5,23 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class SalaryInfo {
-    private static final int SALARY_SPLIT_INDEX = 2;
+    private static final int FIRST_SALARY_SPLIT_INDEX = 2;
+    private static final int SECOND_SALARY_SPLIT_INDEX = 3;
     private static final int NAME_SPLIT_INDEX = 1;
     private static final int DATE_SPLIT_INDEX = 0;
-    private SimpleDateFormat sdformat = new SimpleDateFormat("dd.MM.yyyy");
-
-    private int salaryCounter(String data) {
-        String[] dataSplit = data.split(" ");
-        return Integer.valueOf(dataSplit[SALARY_SPLIT_INDEX]) * Integer.valueOf(dataSplit[3]);
-    }
-
-    private String nameSplit(String data) {
-        String[] dataSplit = data.split(" ");
-        return dataSplit[NAME_SPLIT_INDEX];
-    }
-
-    private Date checkDate(String data) {
-        String[] dataSplit = data.split(" ");
-        try {
-            Date date = sdformat.parse(dataSplit[DATE_SPLIT_INDEX]);
-            return date;
-        } catch (ParseException e) {
-            System.out.println("Failed to parse date");
-            return null;
-        }
-    }
+    private static final String WHITESPACE = " ";
+    private static final SimpleDateFormat SIMPLE_DATE_FORMAT = new SimpleDateFormat("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder builder = new StringBuilder();
         builder.append("Report for period " + dateFrom + " - " + dateTo);
         int[] salary = new int[names.length];
         try {
-            Date firstDate = sdformat.parse(dateFrom);
-            Date lastDate = sdformat.parse(dateTo);
+            Date firstDate = SIMPLE_DATE_FORMAT.parse(dateFrom);
+            Date lastDate = SIMPLE_DATE_FORMAT.parse(dateTo);
             for (int i = 0; i < names.length; i++) {
                 for (String datum : data) {
-                    Date currentDate = checkDate(datum);
+                    Date currentDate = splitDate(datum);
 
                     if (currentDate.after(lastDate)) {
                         continue;
@@ -48,7 +29,7 @@ public class SalaryInfo {
 
                     if ((currentDate.after(firstDate) || currentDate.equals(firstDate))
                             && (currentDate.before(lastDate) || currentDate.equals(lastDate))
-                            && names[i].equals(nameSplit(datum))) {
+                            && names[i].equals(splitName(datum))) {
                         salary[i] += salaryCounter(datum);
                     }
                 }
@@ -58,5 +39,25 @@ public class SalaryInfo {
             System.out.println("Failed to parse date");
         }
         return builder.toString();
+    }
+
+    private int salaryCounter(String data) {
+        String[] dataSplit = data.split(WHITESPACE);
+        return Integer.valueOf(dataSplit[FIRST_SALARY_SPLIT_INDEX])
+                * Integer.valueOf(dataSplit[SECOND_SALARY_SPLIT_INDEX]);
+    }
+
+    private String splitName(String data) {
+        String[] dataSplit = data.split(WHITESPACE);
+        return dataSplit[NAME_SPLIT_INDEX];
+    }
+
+    private Date splitDate(String data) {
+        String[] dataSplit = data.split(WHITESPACE);
+        try {
+            return SIMPLE_DATE_FORMAT.parse(dataSplit[DATE_SPLIT_INDEX]);
+        } catch (ParseException e) {
+            throw new DateParseException("Failed to parse date from input: " + data, e);
+        }
     }
 }
