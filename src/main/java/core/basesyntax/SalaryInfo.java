ccd02133658514569338db8603f2e1e46e6bbf37
ttikr <ia03.danchuk.volodymyr@gmail.com>
2024-09-02 17:44:07 +0300

    refactored after mentor reject (give convention name for static values, change pattern in formatter)

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,16 +4,16 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d.MM.yyyy");
-    private static final int startPositionForSubstringDate = 0;
-    private static final int endPositionForSubstringDate = 10;
-    private static final int startPositionForSubstringIncomeData = 12;
-    private static final int arrayPositionOfWorkingHours = 0;
-    private static final int arrayPositionOfIncomePerHour = 1;
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int START_POSITION_FOR_SUBSTRING_DATE = 0;
+    private static final int END_POSITION_FOR_SUBSTRING_DATE = 10;
+    private static final int START_POSITION_FOR_SUBSTRING_INCOME_DATA = 12;
+    private static final int ARRAY_POSITION_OF_WORKING_HOURS = 0;
+    private static final int ARRAY_POSITION_OF_INC_PER_HOUR = 1;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        LocalDate localDateFrom = LocalDate.parse(dateFrom, formatter);
-        LocalDate localDateTo = LocalDate.parse(dateTo, formatter);
+        LocalDate localDateFrom = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate localDateTo = LocalDate.parse(dateTo, FORMATTER);
 
         StringBuilder salaryInfo = new StringBuilder();
         salaryInfo.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
@@ -24,14 +24,15 @@ public class SalaryInfo {
             for (String simpleData : data) {
                 if (simpleData.contains(name)) {
                     LocalDate rowDate = LocalDate.parse(simpleData.substring(
-                                    startPositionForSubstringDate, endPositionForSubstringDate),
-                            formatter);
+                                    START_POSITION_FOR_SUBSTRING_DATE,
+                                    END_POSITION_FOR_SUBSTRING_DATE),
+                            FORMATTER);
                     if (!rowDate.isBefore(localDateFrom) && !rowDate.isAfter(localDateTo)) {
                         String[] dayIncomeInfo = simpleData.substring(
-                                startPositionForSubstringIncomeData
+                                START_POSITION_FOR_SUBSTRING_INCOME_DATA
                                         + name.length()).split(" ");
-                        salary += Integer.parseInt(dayIncomeInfo[arrayPositionOfWorkingHours])
-                                * Integer.parseInt(dayIncomeInfo[arrayPositionOfIncomePerHour]);
+                        salary += Integer.parseInt(dayIncomeInfo[ARRAY_POSITION_OF_WORKING_HOURS])
+                                * Integer.parseInt(dayIncomeInfo[ARRAY_POSITION_OF_INC_PER_HOUR]);
                     }
                 }
             }
