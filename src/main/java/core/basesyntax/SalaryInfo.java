113f9f3f0d36ec980dbdac24f2492436d299a1c5
Lilia <liliazuravel6@gmail.com>
2024-09-08 20:45:53 +0300

    corrected according to recommendations

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,33 +1,29 @@
 package core.basesyntax;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 public class SalaryInfo {
     private static final String DATE_PATTERN = "dd.MM.yyyy";
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_PATTERN);
+    private static final int HOURS_WORKED = 2;
+    private static final int HOURLY_FEE = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        SimpleDateFormat sdf = new SimpleDateFormat(DATE_PATTERN);
         StringBuilder staffSalaryInformation = new StringBuilder("Report for period "
                                              + dateFrom + " - " + dateTo);
         for (String name : names) {
             int totalSalary = 0;
-            staffSalaryInformation.append("\r\n");
+            staffSalaryInformation.append(System.lineSeparator());
             for (String dataItem : data) {
                 String[] items = dataItem.split(" ");
                 if (name.equals(items[1])) {
                     try {
-                        Date workDate = sdf.parse(items[0]);
-                        Date dateToBorder = sdf.parse(dateTo);
-                        Date dateFromBorder = sdf.parse(dateFrom);
-
-                        if ((workDate.before(dateToBorder) || workDate.equals(dateToBorder))
-                                && (workDate.after(dateFromBorder)
-                                || workDate.equals(dateFromBorder))) {
+                        if (dateCompare(items[0], dateTo, dateFrom)) {
                             totalSalary += salaryCounter(items);
                         }
-                    } catch (ParseException e) {
+                    } catch (DateTimeParseException e) {
                         System.out.println("Something went wrong with date parsing");
                     }
                 }
@@ -38,7 +34,18 @@ public class SalaryInfo {
         return staffSalaryInformation.toString();
     }
 
+    public boolean dateCompare(String workDate, String dateTo, String dateFrom) {
+        LocalDate localWorkDate = LocalDate.parse(workDate, formatter);
+        LocalDate localDateTo = LocalDate.parse(dateTo, formatter);
+        LocalDate localDateFrom = LocalDate.parse(dateFrom, formatter);
+
+        return (localWorkDate.isBefore(localDateTo) || localWorkDate.equals(localDateTo))
+                && (localWorkDate.isAfter(localDateFrom)
+                || localWorkDate.equals(localDateFrom));
+    }
+
     public int salaryCounter(String[] items) {
-        return Integer.parseInt(items[2]) * Integer.parseInt(items[3]);
+        return Integer.parseInt(items[HOURS_WORKED])
+                * Integer.parseInt(items[HOURLY_FEE]);
     }
 }
