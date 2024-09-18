a07d28f8b069b059c392f1419f92d367cbfbac3e
Tetiana Hrytsenko <shviridtanya107@gmail.com>
2024-09-18 23:27:59 +0200

    added checking null, created formatter field, used append method instead of concatenation +

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -8,13 +8,18 @@ public class SalaryInfo {
     public static final int NAME_POSITION = 1;
     public static final int WORKING_HOURS_POSITION = 2;
     public static final int INCOME_PER_HOUR_POSITION = 3;
+    public static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
     public static final String SPACE = " ";
     public static final String INFO_TEXT = "Report for period ";
     public static final String DASH = " - ";
     public static final String SEPARATOR = System.lineSeparator();
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
+        if (names == null || data == null || names.length == 0 || data.length == 0) {
+            throw new IllegalArgumentException("Invalid input data");
+        }
         StringBuilder result = new StringBuilder();
+        result.append(INFO_TEXT).append(dateFrom).append(DASH).append(dateTo);
         for (String name : names) {
             int salary = 0;
             for (String currentData : data) {
@@ -27,11 +32,10 @@ public class SalaryInfo {
             }
             result.append(SEPARATOR).append(name).append(DASH).append(salary);
         }
-        return INFO_TEXT + dateFrom + DASH + dateTo + result;
+        return result.toString();
     }
 
     private boolean isInRange(String dateFrom, String dateTo, String targetDate) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
         LocalDate dateFromFormated = LocalDate.parse(dateFrom, formatter);
         LocalDate dateToFormated = LocalDate.parse(dateTo, formatter);
         LocalDate current = LocalDate.parse(targetDate, formatter);
