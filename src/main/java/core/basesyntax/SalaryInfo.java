5eb3e527cf7bbfc70caf50e14e882e466c46fb4d
Roman <romanvoynahiy@gmail.com>
2024-09-24 17:26:07 +0300

    Implement getSalaryInfo method for calculating and reporting employee salaries based on working hours. pay rates and specified date range.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,49 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDate startDate = LocalDate.parse(dateFrom, formatter);
+        LocalDate endDate = LocalDate.parse(dateTo, formatter);
+
+        int[] employeesSalaries = new int[names.length];
+
+        for (String record : data) {
+            String[] partsOfData = record.split(" ");
+            String dataDate = partsOfData[0];
+            LocalDate currentDate = LocalDate.parse(dataDate, formatter);
+            String dataName = partsOfData[1];
+            int dataHoursWorked = Integer.parseInt(partsOfData[2]);
+            int dataRatePerHour = Integer.parseInt(partsOfData[3]);
+
+            if (currentDate.isEqual(startDate) || currentDate.isAfter(startDate)) {
+                if (currentDate.isEqual(endDate) || currentDate.isBefore(endDate)) {
+                    for (int i = 0; i < names.length; i++) {
+                        if (names[i].equals(dataName)) {
+                            employeesSalaries[i] += dataHoursWorked * dataRatePerHour;
+                        }
+                    }
+                }
+            }
+        }
+
+        StringBuilder salaryInfoReport = new StringBuilder();
+        salaryInfoReport.append("Report for period ")
+                .append(dateFrom).append(" - ")
+                .append(dateTo)
+                .append(System.lineSeparator());
+
+        for (int i = 0; i < names.length; i++) {
+            salaryInfoReport.append(names[i])
+                    .append(" - ")
+                    .append(employeesSalaries[i]);
+            if (i < names.length - 1) {
+                salaryInfoReport.append(System.lineSeparator());
+            }
+        }
+        return salaryInfoReport.toString();
     }
 }
