9e53943be45b15d31efb980d72d522e8eeda4f65
opdhk <ahhsigjl@gmail.com>
2024-09-26 22:18:52 +0300

    2nd iteration of changes according to the reviewer's fixes

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -16,23 +16,37 @@ public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         final LocalDate fromDate = LocalDate.parse(dateFrom, FORMATTER);
         final LocalDate toDate = LocalDate.parse(dateTo, FORMATTER);
-        StringBuilder report = new StringBuilder(REPORT_HEADER).append(dateFrom)
-                .append(REPORT_DELIMITER)
-                .append(dateTo);
         int[] salaries = new int[names.length];
         for (String info : data) {
             String[] parts = info.split(DATA_DELIMITER);
             LocalDate recordDay = LocalDate.parse(parts[DATE_INDEX], FORMATTER);
-            if ((recordDay.isAfter(fromDate) || recordDay.isEqual(fromDate)) && (
-                    recordDay.isBefore(toDate) || recordDay.isEqual(toDate))) {
-                for (int i = 0; i < names.length; i++) {
-                    if (names[i].equals(parts[NAME_INDEX])) {
-                        salaries[i] += Integer.parseInt(parts[HOURS_INDEX]) * Integer.parseInt(
-                                parts[INCOME_INDEX]);
-                    }
-                }
+            if (isWithinDateRange(recordDay, fromDate, toDate)) {
+                calculateSalaries(names, parts, salaries);
             }
         }
+        return composeReport(names, dateFrom, dateTo, salaries);
+    }
+
+    private static boolean isWithinDateRange(LocalDate currentDate, LocalDate fromDate,
+                                             LocalDate toDate) {
+        return (currentDate.isAfter(fromDate) || currentDate.isEqual(fromDate)) && (
+                currentDate.isBefore(toDate) || currentDate.isEqual(toDate));
+    }
+
+    private static void calculateSalaries(String[] names, String[] parts, int[] salaries) {
+        for (int i = 0; i < names.length; i++) {
+            if (names[i].equals(parts[NAME_INDEX])) {
+                salaries[i] += Integer.parseInt(parts[HOURS_INDEX]) * Integer.parseInt(
+                        parts[INCOME_INDEX]);
+            }
+        }
+    }
+
+    private static String composeReport(String[] names, String dateFrom, String dateTo,
+                                        int[] salaries) {
+        StringBuilder report = new StringBuilder(REPORT_HEADER).append(dateFrom)
+                .append(REPORT_DELIMITER)
+                .append(dateTo);
         for (int i = 0; i < names.length; i++) {
             report.append(System.lineSeparator())
                     .append((names[i]))
