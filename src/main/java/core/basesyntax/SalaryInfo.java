f5251acc04f8b51d6d561c6f9c03f33e7e387eb8
Bohdan <111230201+FireFalcons@users.noreply.github.com>
2024-09-26 18:25:16 +0300

    split the method into several private methods

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,35 +4,46 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private final DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final DateTimeFormatter TIME_FORMATTER =
+                DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int DATE_INDEX = 0;
+    private static final int WORKING_HOURS_INDEX = 2;
+    private static final int MONEY_PER_HOUR_INDEX = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder result = new StringBuilder("Report for period " + dateFrom + " - " + dateTo);
 
-        final int date = 0;
-        final int workingHours = 2;
-        final int moneyPerHour = 3;
-
-        LocalDate localDateFrom = LocalDate.parse(dateFrom, timeFormatter);
-        LocalDate localDateTo = LocalDate.parse(dateTo, timeFormatter);
+        LocalDate localDateFrom = parseDate(dateFrom);
+        LocalDate localDateTo = parseDate(dateTo);
 
         for (String username: names) {
             int money = 0;
             for (String jobData: data) {
                 if (jobData.contains(username)) {
                     String[] parts = jobData.split(" ");
-                    LocalDate workDate = LocalDate.parse(parts[date],
-                                        DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-                    if ((workDate.isAfter(localDateFrom) || workDate.isEqual(localDateFrom))
-                                && (workDate.isBefore(localDateTo)
-                                || workDate.isEqual(localDateTo))) {
-                        money += Integer.parseInt(parts[workingHours])
-                                * Integer.parseInt(parts[moneyPerHour]);
+                    LocalDate workDate = parseDate(parts[DATE_INDEX]);
+                    if (ifDateInStatisticRange(workDate,localDateFrom,localDateTo)) {
+                        money += calculateMoneyPerHour(parts[WORKING_HOURS_INDEX],
+                            parts[MONEY_PER_HOUR_INDEX]);
                     }
                 }
             }
             result.append(System.lineSeparator()).append(username).append(" - ").append(money);
         }
         return result.toString();
+    }
+
+    private boolean ifDateInStatisticRange(LocalDate workDate, LocalDate dateFrom,
+                LocalDate dateTo) {
+        return (workDate.isAfter(dateFrom) || workDate.isEqual(dateFrom))
+            && (workDate.isBefore(dateTo) || workDate.isEqual(dateTo));
+    }
+
+    private int calculateMoneyPerHour(String workingHours, String moneyPerHour) {
+        return Integer.parseInt(workingHours) * Integer.parseInt(moneyPerHour);
+    }
+
+    private LocalDate parseDate(String date) {
+        return LocalDate.parse(date, TIME_FORMATTER);
     }
 }
