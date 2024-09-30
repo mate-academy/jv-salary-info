3c7e1a32a2ddfb41f2e433f5c893f82c46707618
Valeriy M <valeriym250@gmail.com>
2024-09-30 19:27:47 +0300

    fixed Bugs

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,36 +5,27 @@ public class SalaryInfo {
     public static final int FIRST = 1;
     public static final int SECOND = 2;
     public static final int THIRD = 3;
+    public static final int FOURTH = 4;
     public static final int FIFTH = 5;
+    public static final String INPUT_DIVIDER = " ";
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
 
-        String [] peopleNames = new String[3];
-        int [] salaries = new int[3];
-        String [] splitInputString = new String[4];
-
+        String [] peopleNames = new String[THIRD];
+        int [] salaries = new int[THIRD];
+        String [] splitInputString = new String[FOURTH];
         int currentDay = 0;
         int currentMonth = 0;
-        int toDay = Integer.parseInt(dateTo.substring(ZERO, SECOND));
-        int fromDay = Integer.parseInt(dateFrom.substring(ZERO, SECOND));
-        int toMonth = Integer.parseInt(dateTo.substring(THIRD, FIFTH));
-        int fromMonth = Integer.parseInt(dateFrom.substring(THIRD, FIFTH));
-
-        peopleNames[ZERO] = names[0];
+        peopleNames[ZERO] = names[ZERO];
         peopleNames[FIRST] = defineName(names, peopleNames[ZERO], peopleNames[ZERO]);
         peopleNames[SECOND] = defineName(names, peopleNames[ZERO], peopleNames[FIRST]);
 
         for (int i = 0; i < data.length; i++) {
-            splitInputString = data[i].split(" ");
+            splitInputString = data[i].split(INPUT_DIVIDER);
             currentDay = Integer.parseInt(data[i].substring(ZERO, SECOND));
             currentMonth = Integer.parseInt(data[i].substring(THIRD, FIFTH));
 
-            if ((fromDay <= currentDay && currentDay <= toDay
-                    && currentMonth == toMonth && currentMonth == fromMonth)
-                    || (fromDay <= currentDay && fromMonth == currentMonth
-                    && currentMonth < toMonth)
-                    || (fromMonth < currentMonth && currentDay <= toDay
-                    && currentMonth == toMonth)) {
+            if (defineBorders(parseInputString(dateFrom, dateTo), currentDay, currentMonth)) {
 
                 salaries[ZERO] += defineIncome(peopleNames[ZERO], splitInputString[FIRST],
                         splitInputString[SECOND], splitInputString[THIRD]);
@@ -50,12 +41,14 @@ public class SalaryInfo {
     }
 
     private String defineName(String [] names, String firstName, String secondName) {
+        String name = "";
         for (int i = 1; i < names.length; i++) {
             if (!firstName.equals(names[i]) && !secondName.equals(names[i])) {
-                return names[i];
+                name = names[i];
+                break;
             }
         }
-        return "";
+        return name;
     }
 
     private int defineIncome(String personName, String comparisonName,
@@ -67,13 +60,39 @@ public class SalaryInfo {
         return 0;
     }
 
+    private int [] parseInputString(String dateFrom, String dateTo) {
+        int [] ranges = new int[FOURTH];
+        ranges[ZERO] = Integer.parseInt(dateFrom.substring(ZERO, SECOND));
+        ranges[FIRST] = Integer.parseInt(dateFrom.substring(THIRD, FIFTH));
+        ranges[SECOND] = Integer.parseInt(dateTo.substring(ZERO, SECOND));
+        ranges[THIRD] = Integer.parseInt(dateTo.substring(THIRD, FIFTH));
+        return ranges;
+    }
+
+    private boolean defineBorders(int [] ranges, int currentDay, int currentMonth) {
+        return (ranges[ZERO] <= currentDay && currentDay <= ranges[SECOND]
+                && currentMonth == ranges[THIRD] && currentMonth == ranges[FIRST])
+                || (ranges[ZERO] <= currentDay && ranges[FIRST] == currentMonth
+                && currentMonth < ranges[THIRD])
+                || (ranges[FIRST] < currentMonth && currentDay <= ranges[SECOND]
+                && currentMonth == ranges[THIRD]);
+    }
+
     private String createReport(String [] names, int [] salaries, String dateFrom, String dateTo) {
         StringBuilder builder = new StringBuilder("Report for period ");
-        return builder.append(dateFrom).append(" - ").append(dateTo)
-                        .append(System.lineSeparator()).append(names[ZERO])
-                        .append(" - ").append(salaries[ZERO]).append(System.lineSeparator())
-                        .append(names[FIRST]).append(" - ").append(salaries[FIRST])
-                        .append(System.lineSeparator()).append(names[SECOND])
-                        .append(" - ").append(salaries[SECOND]).toString();
+        return builder.append(dateFrom)
+                        .append(" - ")
+                        .append(dateTo)
+                        .append(System.lineSeparator())
+                        .append(names[ZERO])
+                        .append(" - ").append(salaries[ZERO])
+                        .append(System.lineSeparator())
+                        .append(names[FIRST])
+                        .append(" - ")
+                        .append(salaries[FIRST])
+                        .append(System.lineSeparator())
+                        .append(names[SECOND])
+                        .append(" - ")
+                        .append(salaries[SECOND]).toString();
     }
 }
