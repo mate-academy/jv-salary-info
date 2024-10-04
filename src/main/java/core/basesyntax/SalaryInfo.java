e9778c3efb44aa5e470fa06cab4f9060ea8980a1
Valeriy M <valeriym250@gmail.com>
2024-10-04 23:01:12 +0300

    fixed Bugs according to your recommendations

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,40 +1,48 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
-    public static final int ZERO = 0;
-    public static final int FIRST = 1;
-    public static final int SECOND = 2;
+    public static final int NAME_INDEX_ZERO = 0;
+    public static final int NAME_INDEX_FIRST = 1;
+    public static final int NAME_INDEX_SECOND = 2;
     public static final int THIRD = 3;
     public static final int FOURTH = 4;
-    public static final int FIFTH = 5;
     public static final String INPUT_DIVIDER = " ";
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
 
         String [] peopleNames = new String[THIRD];
         int [] salaries = new int[THIRD];
         String [] splitInputString = new String[FOURTH];
-        int currentDay = 0;
-        int currentMonth = 0;
-        peopleNames[ZERO] = names[ZERO];
-        peopleNames[FIRST] = defineName(names, peopleNames[ZERO], peopleNames[ZERO]);
-        peopleNames[SECOND] = defineName(names, peopleNames[ZERO], peopleNames[FIRST]);
+        peopleNames[NAME_INDEX_ZERO] = names[NAME_INDEX_ZERO];
+        peopleNames[NAME_INDEX_FIRST] = defineName(names, peopleNames[NAME_INDEX_ZERO],
+                peopleNames[NAME_INDEX_ZERO]);
+        peopleNames[NAME_INDEX_SECOND] = defineName(names, peopleNames[NAME_INDEX_ZERO],
+                peopleNames[NAME_INDEX_FIRST]);
+        LocalDate parsedDateFrom = LocalDate.parse(dateFrom, formatter);
+        LocalDate parsedDateTo = LocalDate.parse(dateTo, formatter);
+        LocalDate parsedCurrentData;
 
         for (int i = 0; i < data.length; i++) {
             splitInputString = data[i].split(INPUT_DIVIDER);
-            currentDay = Integer.parseInt(data[i].substring(ZERO, SECOND));
-            currentMonth = Integer.parseInt(data[i].substring(THIRD, FIFTH));
+            parsedCurrentData = LocalDate.parse(splitInputString[0], formatter);
 
-            if (defineBorders(parseInputString(dateFrom, dateTo), currentDay, currentMonth)) {
+            if (isWithinDateRange(parsedCurrentData, parsedDateFrom, parsedDateTo)) {
 
-                salaries[ZERO] += defineIncome(peopleNames[ZERO], splitInputString[FIRST],
-                        splitInputString[SECOND], splitInputString[THIRD]);
+                salaries[NAME_INDEX_ZERO] += defineIncome(peopleNames[NAME_INDEX_ZERO],
+                        splitInputString[NAME_INDEX_FIRST], splitInputString[NAME_INDEX_SECOND],
+                        splitInputString[THIRD]);
 
-                salaries[FIRST] += defineIncome(peopleNames[FIRST], splitInputString[FIRST],
-                        splitInputString[SECOND], splitInputString[THIRD]);
+                salaries[NAME_INDEX_FIRST] += defineIncome(peopleNames[NAME_INDEX_FIRST],
+                        splitInputString[NAME_INDEX_FIRST], splitInputString[NAME_INDEX_SECOND],
+                        splitInputString[THIRD]);
 
-                salaries[SECOND] += defineIncome(peopleNames[SECOND], splitInputString[FIRST],
-                        splitInputString[SECOND], splitInputString[THIRD]);
+                salaries[NAME_INDEX_SECOND] += defineIncome(peopleNames[NAME_INDEX_SECOND],
+                        splitInputString[NAME_INDEX_FIRST], splitInputString[NAME_INDEX_SECOND],
+                        splitInputString[THIRD]);
             }
         }
         return createReport(peopleNames, salaries, dateFrom, dateTo);
@@ -60,39 +68,25 @@ public class SalaryInfo {
         return 0;
     }
 
-    private int [] parseInputString(String dateFrom, String dateTo) {
-        int [] ranges = new int[FOURTH];
-        ranges[ZERO] = Integer.parseInt(dateFrom.substring(ZERO, SECOND));
-        ranges[FIRST] = Integer.parseInt(dateFrom.substring(THIRD, FIFTH));
-        ranges[SECOND] = Integer.parseInt(dateTo.substring(ZERO, SECOND));
-        ranges[THIRD] = Integer.parseInt(dateTo.substring(THIRD, FIFTH));
-        return ranges;
-    }
-
-    private boolean defineBorders(int [] ranges, int currentDay, int currentMonth) {
-        return (ranges[ZERO] <= currentDay && currentDay <= ranges[SECOND]
-                && currentMonth == ranges[THIRD] && currentMonth == ranges[FIRST])
-                || (ranges[ZERO] <= currentDay && ranges[FIRST] == currentMonth
-                && currentMonth < ranges[THIRD])
-                || (ranges[FIRST] < currentMonth && currentDay <= ranges[SECOND]
-                && currentMonth == ranges[THIRD]);
+    private boolean isWithinDateRange(LocalDate date, LocalDate dateFrom, LocalDate dateTo) {
+        return !date.isBefore(dateFrom) && !date.isAfter(dateTo);
     }
 
     private String createReport(String [] names, int [] salaries, String dateFrom, String dateTo) {
         StringBuilder builder = new StringBuilder("Report for period ");
         return builder.append(dateFrom)
-                        .append(" - ")
-                        .append(dateTo)
-                        .append(System.lineSeparator())
-                        .append(names[ZERO])
-                        .append(" - ").append(salaries[ZERO])
-                        .append(System.lineSeparator())
-                        .append(names[FIRST])
-                        .append(" - ")
-                        .append(salaries[FIRST])
-                        .append(System.lineSeparator())
-                        .append(names[SECOND])
-                        .append(" - ")
-                        .append(salaries[SECOND]).toString();
+                .append(" - ")
+                .append(dateTo)
+                .append(System.lineSeparator())
+                .append(names[NAME_INDEX_ZERO])
+                .append(" - ").append(salaries[NAME_INDEX_ZERO])
+                .append(System.lineSeparator())
+                .append(names[NAME_INDEX_FIRST])
+                .append(" - ")
+                .append(salaries[NAME_INDEX_FIRST])
+                .append(System.lineSeparator())
+                .append(names[NAME_INDEX_SECOND])
+                .append(" - ")
+                .append(salaries[NAME_INDEX_SECOND]).toString();
     }
 }
