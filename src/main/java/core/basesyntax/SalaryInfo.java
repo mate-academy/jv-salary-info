0c4351c0eb7f801cb5c3999a9211de747907c5a1
dmitry <dmarmul228@gmail.com>
2024-10-09 17:32:36 +0300

    Implemented getSalaryInfo method, don't use Map

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -2,8 +2,6 @@ package core.basesyntax;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.HashMap;
-import java.util.Map;
 
 public class SalaryInfo {
     private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("dd.MM.yyyy");
@@ -11,42 +9,45 @@ public class SalaryInfo {
     private static final int NAME_INDEX = 1;
     private static final int WORK_TIME_INDEX = 2;
     private static final int WAGE_RATE_INDEX = 3;
-    private final Map<String, Integer> workerSalaries = new HashMap<>();
+    private static final String SEPARATOR = " - ";
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate fromDate = LocalDate.parse(dateFrom, DATE_FORMAT);
         LocalDate toDate = LocalDate.parse(dateTo, DATE_FORMAT);
+        String[] workerSalaries = new String[names.length];
 
-        for (String name : names) {
-            workerSalaries.put(name, 0);
+        for (int i = 0; i < workerSalaries.length; i++) {
+            workerSalaries[i] = names[i] + SEPARATOR + 0;
         }
-        calculateSalaries(fromDate, toDate, data);
 
-        return generateReport(fromDate, toDate, names);
-    }
-
-    private void calculateSalaries(LocalDate fromDate, LocalDate toDate, String[] data) {
         for (String date : data) {
             String[] worker = date.split(" ");
             LocalDate workDate = LocalDate.parse(worker[DATE_INDEX], DATE_FORMAT);
 
             if ((workDate.isAfter(fromDate) && workDate.isBefore(toDate))
                     || workDate.equals(fromDate) || workDate.equals(toDate)) {
-                Integer salary = Integer.parseInt(worker[WORK_TIME_INDEX])
+                int salary = Integer.parseInt(worker[WORK_TIME_INDEX])
                         * Integer.parseInt(worker[WAGE_RATE_INDEX]);
-                workerSalaries.put(worker[NAME_INDEX],
-                        workerSalaries.get(worker[NAME_INDEX]) + salary);
+                for (int i = 0; i < workerSalaries.length; i++) {
+                    String name = workerSalaries[i].substring(0, workerSalaries[i].indexOf(" "));
+                    if (name.equals(worker[NAME_INDEX])) {
+                        int oldSalary = Integer.parseInt(workerSalaries[i]
+                                .substring(workerSalaries[i].indexOf(SEPARATOR) + 3));
+                        workerSalaries[i] = name + SEPARATOR + (salary + oldSalary);
+                    }
+                }
             }
         }
+
+        return generateReport(fromDate, toDate, workerSalaries);
     }
 
-    private String generateReport(LocalDate fromDate, LocalDate toDate, String[] names) {
+    private String generateReport(LocalDate fromDate, LocalDate toDate, String[] workerSalaries) {
         StringBuilder report = new StringBuilder();
         report.append("Report for period ").append(fromDate.format(DATE_FORMAT))
-                .append(" - ").append(toDate.format(DATE_FORMAT));
-        for (String name : names) {
-            report.append(System.lineSeparator())
-                    .append(name).append(" - ").append(workerSalaries.get(name));
+                .append(SEPARATOR).append(toDate.format(DATE_FORMAT));
+        for (String workerSalary : workerSalaries) {
+            report.append(System.lineSeparator()).append(workerSalary);
         }
 
         return report.toString();
