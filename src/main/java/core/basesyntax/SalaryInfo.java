73a3aa8764b2a1c7d21f0cd547f42b49d4cfe718
Liza <lizabalashova7@gmail.com>
2024-10-18 13:42:55 +0300

    add calculate salary method

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -6,33 +6,42 @@ import java.time.format.DateTimeFormatter;
 public class SalaryInfo {
     private static final DateTimeFormatter DATE_FORMATTER =
             DateTimeFormatter.ofPattern("dd.MM.yyyy");
-    private static final int DAY_POSITION = 0;
-    private static final int NAME = 1;
-    private static final int HOUR = 2;
-    private static final int SALARY_PER_HOUR = 3;
+    private static final int DAY_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int HOUR_INDEX = 2;
+    private static final int SALARY_PER_HOUR_INDEX = 3;
+    private static final String SPACE = " ";
+    private static final String DASH = " - ";
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate fromDate = LocalDate.parse(dateFrom, DATE_FORMATTER);
         LocalDate toDate = LocalDate.parse(dateTo, DATE_FORMATTER);
         StringBuilder salaryInfo = new StringBuilder("Report for period "
-                + dateFrom + " - " + dateTo);
+                + dateFrom + DASH + dateTo);
 
         for (String name : names) {
-            int salary = 0;
-            for (String info: data) {
-                String[] infoSplit = info.split(" ");
-                LocalDate recordDate = LocalDate.parse(infoSplit[DAY_POSITION], DATE_FORMATTER);
-                String employee = infoSplit[NAME];
-                int hours = Integer.parseInt(infoSplit[HOUR]);
-                int salaryHour = Integer.parseInt(infoSplit[SALARY_PER_HOUR]);
-
-                if (employee.equals(name) && !recordDate.isBefore(fromDate)
-                        && !recordDate.isAfter(toDate)) {
-                    salary += hours * salaryHour;
-                }
-            }
-            salaryInfo.append(System.lineSeparator()).append(name).append(" - ").append(salary);
+            salaryInfo.append(System.lineSeparator()).append(name).append(DASH)
+                    .append(calculateSallaryInfo(name, data, fromDate, toDate));
         }
+
         return salaryInfo.toString();
+    }
+
+    public int calculateSallaryInfo(String name, String[] data, LocalDate fromDate,
+                                    LocalDate toDate) {
+        int salary = 0;
+        for (String info : data) {
+            String[] infoSplit = info.split(SPACE);
+            LocalDate recordDate = LocalDate.parse(infoSplit[DAY_INDEX], DATE_FORMATTER);
+            String employee = infoSplit[NAME_INDEX];
+            int hours = Integer.parseInt(infoSplit[HOUR_INDEX]);
+            int salaryHour = Integer.parseInt(infoSplit[SALARY_PER_HOUR_INDEX]);
+
+            if (employee.equals(name) && !recordDate.isBefore(fromDate)
+                    && !recordDate.isAfter(toDate)) {
+                salary += hours * salaryHour;
+            }
+        }
+        return salary;
     }
 }
