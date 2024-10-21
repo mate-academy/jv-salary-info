61d24330e6f2e289f25722972fd35d8274c8d19d
OksanaEnko <132217385+OksanaEnko@users.noreply.github.com>
2024-10-21 17:53:47 +0200

    UpdateCheckstyle SalaryInfo.java

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -6,39 +6,41 @@ import java.time.format.DateTimeFormatter;
 public class SalaryInfo {
     private static final DateTimeFormatter DATE_FORMATTER = 
             DateTimeFormatter.ofPattern("dd.MM.yyyy");
-
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        StringBuilder report = new StringBuilder();
-        LocalDate localDateFrom = parseDateFromString(dateFrom);
-        LocalDate localDateTo = parseDateFromString(dateTo);
+    StringBuilder report = new StringBuilder();
+    LocalDate localDateFrom = parseDateFromString(dateFrom);
+    LocalDate localDateTo = parseDateFromString(dateTo);
 
-        report.append("Report for period ")
-              .append(dateFrom)
-              .append(" - ")
-              .append(dateTo);
+    report.append("Report for period ")
+            .append(dateFrom)
+            .append(" - ")
+            .append(dateTo);
 
-        for (String currentName : names) {
-            int salaryAmount = 0;
-            for (String record : data) {
-                if (record == null || record.isEmpty()) {
-                    continue;
-                }
-                String[] parseRecord = record.split(" ");
-                LocalDate workDate = parseDateFromString(parseRecord[0]);
-                String name = parseRecord[1];
-                int hoursWorked = parseNumberFromString(parseRecord[2]);
-                int hourlyRate = parseNumberFromString(parseRecord[3]);
+    for (String currentName : names) {
+            
+        int salaryAmount = 0;
+        for (String record : data) {
+            if (record == null || record.isEmpty()) {
+                continue;
+    }
+            
+    String[] parseRecord = record.split(" ");
+    LocalDate workDate = parseDateFromString(parseRecord[0]);
+    String name = parseRecord[1];
+    int hoursWorked = parseNumberFromString(parseRecord[2]);
+    int hourlyRate = parseNumberFromString(parseRecord[3]);
 
-                if ((workDate.isEqual(localDateFrom) || workDate.isAfter(localDateFrom))
-                        && (workDate.isEqual(localDateTo) || workDate.isBefore(localDateTo))
-                        && currentName.equals(name)) {
-                    salaryAmount += hoursWorked * hourlyRate;
-                }
-            }
-            report.append(System.lineSeparator())
-                  .append(currentName)
-                  .append(" - ")
-                  .append(salaryAmount);
+    if ((workDate.isEqual(localDateFrom) || workDate.isAfter(localDateFrom))
+            && (workDate.isEqual(localDateTo) || workDate.isBefore(localDateTo))
+            && currentName.equals(name)) {
+        salaryAmount += hoursWorked * hourlyRate;
+       }
+    }
+    
+    report.append(System.lineSeparator())
+            .append(currentName)
+            .append(" - ")
+            .append(salaryAmount);
         }
 
         return report.toString();
