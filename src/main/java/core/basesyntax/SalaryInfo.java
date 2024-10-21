bcb2163d51df52321f500f07f0e7f1bb52fcdb8a
Ihor <ihor.rome@gmail.com>
2024-10-21 13:44:54 +0300

    fixed code style issues

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,6 +5,10 @@ import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.uuuu");
+    public static final int DATE_INDEX = 0;
+    public static final int NAME_INDEX = 1;
+    public static final int HOURS_INDEX = 2;
+    public static final int SALARY_PER_HOUR_INDEX = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate localDateFrom = LocalDate.parse(dateFrom, FORMATTER);
@@ -18,21 +22,27 @@ public class SalaryInfo {
             resultBuilder.append(System.lineSeparator())
                     .append(name)
                     .append(" - ");
-            int totalSalary = 0;
-            for (String lineData : data) {
-                String[] arrData = lineData.split(" ");
-                String nameData = arrData[1];
-                if (name.equals(nameData)) {
-                    LocalDate dateData = LocalDate.parse(arrData[0], FORMATTER);
-                    if (dateData.compareTo(localDateFrom) >= 0
-                            && dateData.compareTo(localDateTo) <= 0) {
-                        totalSalary += Integer.valueOf(arrData[2]) * Integer.valueOf(arrData[3]);
-                    }
-                }
-            }
+            int totalSalary = calculateSalary(name, data, localDateFrom, localDateTo);
             resultBuilder.append(totalSalary);
         }
-
         return resultBuilder.toString();
+    }
+
+    private int calculateSalary(String name, String[] data,
+                                LocalDate localDateFrom, LocalDate localDateTo) {
+        int totalSalary = 0;
+        for (String lineData : data) {
+            String[] arrData = lineData.split(" ");
+            String nameData = arrData[NAME_INDEX];
+            if (name.equals(nameData)) {
+                LocalDate dateData = LocalDate.parse(arrData[DATE_INDEX], FORMATTER);
+                if (dateData.compareTo(localDateFrom) >= 0
+                        && dateData.compareTo(localDateTo) <= 0) {
+                    totalSalary += Integer.valueOf(arrData[HOURS_INDEX])
+                            * Integer.valueOf(arrData[SALARY_PER_HOUR_INDEX]);
+                }
+            }
+        }
+        return totalSalary;
     }
 }
