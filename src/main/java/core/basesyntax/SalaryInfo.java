086aebf85492d9208c7f5dcb560c2e1aa8754b81
Ihor <ihor.rome@gmail.com>
2024-10-21 12:36:28 +0300

    add solution

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,38 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+    public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.uuuu");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        LocalDate localDateFrom = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate localDateTo = LocalDate.parse(dateTo, FORMATTER);
+        StringBuilder resultBuilder = new StringBuilder("Report for period ")
+                .append(dateFrom)
+                .append(" - ")
+                .append(dateTo);
+
+        for (String name : names) {
+            resultBuilder.append(System.lineSeparator())
+                    .append(name)
+                    .append(" - ");
+            int totalSalary = 0;
+            for (String lineData : data) {
+                String[] arrData = lineData.split(" ");
+                String nameData = arrData[1];
+                if (name.equals(nameData)) {
+                    LocalDate dateData = LocalDate.parse(arrData[0], FORMATTER);
+                    if (dateData.compareTo(localDateFrom) >= 0
+                            && dateData.compareTo(localDateTo) <= 0) {
+                        totalSalary += Integer.valueOf(arrData[2]) * Integer.valueOf(arrData[3]);
+                    }
+                }
+            }
+            resultBuilder.append(totalSalary);
+        }
+
+        return resultBuilder.toString();
     }
 }
