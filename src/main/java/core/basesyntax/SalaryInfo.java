14f7a207c801eaa798144445d724adfec737e02a
Mihail <mishadyakivnich@gmail.com>
2024-11-03 23:32:28 +0200

    Using two cycles for , StringBuilder , LocalDate, if , else if

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,37 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        StringBuilder salaryInfo = new StringBuilder();
+        LocalDate startDate = LocalDate.parse(dateFrom);
+        LocalDate endDate = LocalDate.parse(dateTo);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        salaryInfo.append("Report for period ");
+        salaryInfo.append(dateFrom);
+        salaryInfo.append(" - ");
+        salaryInfo.append(dateTo);
+        for (String name : names) {
+            int salary = 0;
+            for (String datum : data) {
+                String[] lines = datum.split(" ");
+                LocalDate date = LocalDate.parse(lines[0]);
+                String nameUser = lines[1];
+                if (date.isAfter(startDate) && date.isBefore(endDate)
+                        && name.equals(nameUser)) {
+                    salary += Integer.parseInt(lines[2]) * Integer.parseInt(lines[3]);
+                } else if (date.isEqual(startDate) && name.equals(nameUser)
+                        || date.isEqual(endDate) && name.equals(nameUser))  {
+                    salary += Integer.parseInt(lines[2]) * Integer.parseInt(lines[3]);
+                }
+            }
+            salaryInfo.append(System.lineSeparator());
+            salaryInfo.append(name);
+            salaryInfo.append(" - ");
+            salaryInfo.append(salary);
+        }
+      return salaryInfo.toString();
     }
 }
