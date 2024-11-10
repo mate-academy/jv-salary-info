785d547ecb7657b42cb33729bce32be083ad4831
Mihail <mishadyakivnich@gmail.com>
2024-11-10 11:52:27 +0200

    nnn

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,27 +4,31 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder salaryInfo = new StringBuilder();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        LocalDate startDate = LocalDate.parse(dateFrom, formatter);
-        LocalDate endDate = LocalDate.parse(dateTo, formatter);
-        salaryInfo.append("Report for period ");
-        salaryInfo.append(dateFrom);
-        salaryInfo.append(" - ");
-        salaryInfo.append(dateTo);
+        int salary;
+        LocalDate start = LocalDate.parse(dateFrom, formatter);
+        LocalDate end = LocalDate.parse(dateTo, formatter);
+        salaryInfo.append("Report for period ")
+                .append(dateFrom)
+                .append(" - ")
+                .append(dateTo);
         for (String name : names) {
-            int salary = 0;
+            salary = 0;
             for (String datum : data) {
                 String[] lines = datum.split(" ");
-                LocalDate date = LocalDate.parse(lines[0]);
+                LocalDate currentDate = LocalDate.parse(lines[0], formatter);
                 String nameUser = lines[1];
-                if (!date.isBefore(startDate) && !date.isAfter(endDate)
+                String workHours = lines[2];
+                String incomePerHour = lines[3];
+                if (!currentDate.isBefore(start) && !currentDate.isAfter(end)
                         && name.equals(nameUser)) {
-                    salary += Integer.parseInt(lines[2]) * Integer.parseInt(lines[3]);
-                } else if (date.isEqual(startDate) && name.equals(nameUser)
-                        || date.isEqual(endDate) && name.equals(nameUser)) {
-                    salary += Integer.parseInt(lines[2]) * Integer.parseInt(lines[3]);
+                    salary += Integer.parseInt(workHours) * Integer.parseInt(incomePerHour);
+                } else if (currentDate.isEqual(start) && name.equals(nameUser)
+                        || currentDate.isEqual(end) && name.equals(nameUser)) {
+                    salary += Integer.parseInt(workHours) * Integer.parseInt(incomePerHour);
                 }
             }
             salaryInfo.append(System.lineSeparator());
