6959c044ba37dd69ee8a34e81995cfb32d98209a
Rafal <pyszkowski.r@gmail.com>
2024-11-26 21:02:16 +0100

    revised

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,6 +5,7 @@ import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate dateStart = LocalDate.parse(dateFrom, formatter);
         LocalDate dateEnd = LocalDate.parse(dateTo, formatter);
@@ -19,11 +20,13 @@ public class SalaryInfo {
                 String employeeName = partOfRecord[1];
                 int workingHours = Integer.parseInt(partOfRecord[2]);
                 int salaryPerHour = Integer.parseInt(partOfRecord[3]);
-                if (employeeName.equals(name) && !workingDay.isBefore(dateStart) && !workingDay.isAfter(dateEnd)) {
+                if (employeeName.equals(name) && !workingDay.isBefore(dateStart)
+                        && !workingDay.isAfter(dateEnd)) {
                     salaryTotal += workingHours * salaryPerHour;
                 }
             }
-            salaryReport.append(name).append(" - ").append(salaryTotal).append(System.lineSeparator());
+            salaryReport.append(name).append(" - ")
+                    .append(salaryTotal).append(System.lineSeparator());
         }
         return salaryReport.toString();
     }
