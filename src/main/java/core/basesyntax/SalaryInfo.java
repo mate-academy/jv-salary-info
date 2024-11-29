828cbb8ad2aa54d86c989476be6a05f8ebc1be5a
Bohdan Lysachenko <lysachenko111@ukr.net>
2024-11-29 10:59:51 +0200

    Solution

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,38 +4,46 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private static final DateTimeFormatter DATE_FORMATTER = (
-            DateTimeFormatter.ofPattern("dd.MM.yyyy")
-    );
+    private static String HEADER = "Report for period ";
+    private static String SEPARATOR = " - ";
+    private static String LINE_SEPARATOR = "\n";
+    private static final DateTimeFormatter DATE_FORMATTER =
+            DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        StringBuilder result = new StringBuilder(
-                "Report for period "
-        ).append(dateFrom).append(" - ").append(dateTo).append("\n");
+        String[] result = new String[names.length + 1];
+        result[0] = HEADER + dateFrom + SEPARATOR + dateTo;
 
-        LocalDate fromDate = LocalDate.parse(dateFrom, DATE_FORMATTER);
-        LocalDate toDate = LocalDate.parse(dateTo, DATE_FORMATTER);
-
-        for (String name : names) {
+        for (int i = 0; i < names.length; i++) {
+            String name = names[i];
             int salary = 0;
             for (String salaryData : data) {
                 String[] parsedData = salaryData.split(" ");
-                LocalDate date = LocalDate.parse(parsedData[0], DATE_FORMATTER);
-                String workerName = parsedData[1];
                 int hours = Integer.parseInt(parsedData[2]);
                 int salaryPerHour = Integer.parseInt(parsedData[3]);
 
-                if (
-                        workerName.equals(name) && (date.isEqual(fromDate)
-                                || date.isEqual(toDate)
-                                || (date.isAfter(fromDate) && date.isBefore(toDate)))
-                ) {
+                if (isWorkerShouldGetMoney(salaryData, name, dateFrom, dateTo)) {
                     salary += hours * salaryPerHour;
                 }
             }
-            result.append(name).append(" - ").append(salary).append("\n");
+            result[i + 1] = name + SEPARATOR + salary;
         }
+        return String.join(LINE_SEPARATOR, result);
+    }
 
-        return result.deleteCharAt(result.length() - 1).toString();
+    private boolean isWorkerShouldGetMoney(
+            String salaryData, String name, String dateFrom, String dateTo
+    ) {
+        String[] parsedData = salaryData.split(" ");
+        LocalDate date = LocalDate.parse(parsedData[0], DATE_FORMATTER);
+        String workerName = parsedData[1];
+        LocalDate fromDate = LocalDate.parse(dateFrom, DATE_FORMATTER);
+        LocalDate toDate = LocalDate.parse(dateTo, DATE_FORMATTER);
+
+        return workerName.equals(name)
+                && (date.isEqual(fromDate)
+                || date.isEqual(toDate)
+                || (date.isAfter(fromDate) && date.isBefore(toDate)));
+
     }
 }
