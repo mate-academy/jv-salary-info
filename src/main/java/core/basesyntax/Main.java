0f2319ef95967b6b90d478af407dbd720b9142eb
Igor <sokol.igor29@gmail.com>
2024-12-17 20:50:08 +0200

    made the method getSalaryInfo

---
+++ src/main/java/core/basesyntax/Main.java
@@ -0,0 +1,30 @@
+package core.basesyntax;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String[] names = {"John", "Andrew", "Kate"};
+
+        String[] data = {
+                "13.07.2019 John 60 50",
+                "15.07.2019 Andrew 3 200",
+                "15.07.2019 Kate 10 100",
+
+                "16.07.2019 Andrew 3 200",
+                "16.07.2019 Kate 9 100",
+
+                "10.08.2019 John 7 100",
+                "08.08.2019 Kate 3 80",
+                "11.08.2019 Andrew 8 100"
+        };
+
+        String dateFrom = "14.07.2019";
+        String dateTo = "10.08.2019";
+
+        SalaryInfo salaryInfo = new SalaryInfo();
+
+        String report = salaryInfo.getSalaryInfo(names, data, dateFrom, dateTo);
+
+        System.out.println(report);
+    }
+}
--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,61 @@
 package core.basesyntax;
 
 public class SalaryInfo {
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        StringBuilder report = new StringBuilder();
+        String lineSeparator = System.lineSeparator();
+        report.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
+        int[] salary = new int[names.length];
+
+        for (String record : data) {
+            String[] parts = record.split(" ");
+            String dateOfWork = parts[0];
+            String name = parts[1];
+            int hoursOfWork = Integer.parseInt(parts[2]);
+            int salaryPerHour = Integer.parseInt(parts[3]);
+
+            if (isDateInRange(dateOfWork, dateFrom, dateTo)) {
+                for (int i = 0; i < names.length; i++) {
+                    if (names[i].equals(name)) {
+                        salary[i] += hoursOfWork * salaryPerHour;
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < names.length; i++) {
+            report.append(lineSeparator).append(names[i]).append(" - ").append(salary[i]);
+        }
+        return report.toString();
+    }
+
+    private static boolean isDateInRange(String dateOfWork, String dateFrom, String dateTo) {
+        String[] dateParts = dateOfWork.split("\\.");
+        String[] fromParts = dateFrom.split("\\.");
+        String[] toParts = dateTo.split("\\.");
+
+        int day = Integer.parseInt(dateParts[0]);
+        int month = Integer.parseInt(dateParts[1]);
+        int year = Integer.parseInt(dateParts[2]);
+
+        int fromDay = Integer.parseInt(fromParts[0]);
+        int fromMonth = Integer.parseInt(fromParts[1]);
+        int fromYear = Integer.parseInt(fromParts[2]);
+
+        int toDay = Integer.parseInt(toParts[0]);
+        int toMonth = Integer.parseInt(toParts[1]);
+        int toYear = Integer.parseInt(toParts[2]);
+
+        if (year < fromYear || (year == fromYear && (month < fromMonth
+                || (month == fromMonth && day < fromDay)))) {
+            return false;
+        }
+
+        if (year > toYear || (year == toYear && (month > toMonth
+                || (month == toMonth && day > toDay)))) {
+            return false;
+        }
+        return true;
     }
 }
