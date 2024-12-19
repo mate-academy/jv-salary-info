2af2ec8cb10afe2dc7fa2802f99a462594c42205
Geks <gekatonheyr@ukr.net>
2024-12-19 16:14:52 +0200

    implemented getSalaryInfo() method to calculate employee's salary for given period of time accordingly to quantity of working hours and hourly rate

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -2,6 +2,48 @@ package core.basesyntax;
 
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        StringBuilder outputString = new StringBuilder("Report for period ")
+                .append(dateFrom).append(" - ").append(dateTo);
+
+        for (String dataEmployeeName : names) {
+            int totalEmployeeSalary = 0;
+            for (String employeeRecord : data) {
+                String[] splittedEmployeeRecord = employeeRecord.split(" ");
+                String recordEmployeeName = splittedEmployeeRecord[1];
+                if (!dataEmployeeName.equals(recordEmployeeName)) {
+                    continue;
+                }
+
+                String recordDate = splittedEmployeeRecord[0];
+                String employeeHoursOfWork = splittedEmployeeRecord[2];
+                String employeeSalaryPerHour = splittedEmployeeRecord[3];
+
+                String[] splittedDateFrom = dateFrom.split("\\.");
+                String[] splittedDateTo = dateTo.split("\\.");
+                String[] splittedRecordDate = recordDate.split("\\.");
+                if ((Integer.parseInt(splittedRecordDate[2])
+                                <= Integer.parseInt(splittedDateFrom[2])
+                            && (Integer.parseInt(splittedRecordDate[1])
+                                <= Integer.parseInt(splittedDateFrom[1])
+                            && (Integer.parseInt(splittedRecordDate[0])
+                                <= Integer.parseInt(splittedDateFrom[0]))))
+                        || (Integer.parseInt(splittedRecordDate[2])
+                                >= Integer.parseInt(splittedDateTo[2])
+                            && (Integer.parseInt(splittedRecordDate[1])
+                                >= Integer.parseInt(splittedDateTo[1])
+                            && (Integer.parseInt(splittedRecordDate[0])
+                                > Integer.parseInt(splittedDateTo[0]))))) {
+                    continue;
+                }
+                totalEmployeeSalary += Integer.parseInt(employeeHoursOfWork)
+                        * Integer.parseInt(employeeSalaryPerHour);
+            }
+            outputString.append(System.lineSeparator())
+                        .append(dataEmployeeName)
+                        .append(" - ")
+                        .append(totalEmployeeSalary);
+
+        }
+        return outputString.toString();
     }
 }
