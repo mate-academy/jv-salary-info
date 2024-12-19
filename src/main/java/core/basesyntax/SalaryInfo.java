03b30d2ab3444b107417af85b9b1872876a90195
Geks <gekatonheyr@ukr.net>
2024-12-19 21:03:11 +0200

    had little bit done fix syntax and implemented another approach to determine date

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,42 +1,34 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+    public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder outputString = new StringBuilder("Report for period ")
-                .append(dateFrom).append(" - ").append(dateTo);
+                .append(dateFrom)
+                .append(" - ")
+                .append(dateTo);
 
         for (String dataEmployeeName : names) {
             int totalEmployeeSalary = 0;
             for (String employeeRecord : data) {
                 String[] splittedEmployeeRecord = employeeRecord.split(" ");
-                String recordEmployeeName = splittedEmployeeRecord[1];
-                if (!dataEmployeeName.equals(recordEmployeeName)) {
+
+                if (!dataEmployeeName.equals(splittedEmployeeRecord[1])) {
                     continue;
                 }
 
-                String recordDate = splittedEmployeeRecord[0];
-                String employeeHoursOfWork = splittedEmployeeRecord[2];
-                String employeeSalaryPerHour = splittedEmployeeRecord[3];
-
-                String[] splittedDateFrom = dateFrom.split("\\.");
-                String[] splittedDateTo = dateTo.split("\\.");
-                String[] splittedRecordDate = recordDate.split("\\.");
-                if ((Integer.parseInt(splittedRecordDate[2])
-                                <= Integer.parseInt(splittedDateFrom[2])
-                            && (Integer.parseInt(splittedRecordDate[1])
-                                <= Integer.parseInt(splittedDateFrom[1])
-                            && (Integer.parseInt(splittedRecordDate[0])
-                                <= Integer.parseInt(splittedDateFrom[0]))))
-                        || (Integer.parseInt(splittedRecordDate[2])
-                                >= Integer.parseInt(splittedDateTo[2])
-                            && (Integer.parseInt(splittedRecordDate[1])
-                                >= Integer.parseInt(splittedDateTo[1])
-                            && (Integer.parseInt(splittedRecordDate[0])
-                                > Integer.parseInt(splittedDateTo[0]))))) {
-                    continue;
+                LocalDate ldDateFrom = LocalDate.parse(dateFrom, FORMATTER);
+                LocalDate ldDateTo = LocalDate.parse(dateTo, FORMATTER);
+                LocalDate ldRecordDate = LocalDate.parse(splittedEmployeeRecord[0], FORMATTER);
+                if ((ldRecordDate.equals(ldDateFrom) || ldRecordDate.isAfter(ldDateFrom))
+                        && (ldRecordDate.equals(ldDateTo) || ldRecordDate.isBefore(ldDateTo))) {
+                    totalEmployeeSalary += Integer.parseInt(splittedEmployeeRecord[2])
+                            * Integer.parseInt(splittedEmployeeRecord[3]);
                 }
-                totalEmployeeSalary += Integer.parseInt(employeeHoursOfWork)
-                        * Integer.parseInt(employeeSalaryPerHour);
             }
             outputString.append(System.lineSeparator())
                         .append(dataEmployeeName)
