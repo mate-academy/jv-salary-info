84aebf3f07a90292cffd721d2e1f00ce2573dd2e
Piotr Hic <piotrhic@gmail.com>
2024-12-22 11:12:05 +0100

    v1

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -2,6 +2,12 @@ package core.basesyntax;
 
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Report for period").append(dateFrom).append(" - ")
+                .append(dateTo).append("\n");
+        for (int i = 0; i < names.length; i++) {
+            stringBuilder.append(names[0]).append(" - ").append(data[0]).append("\n");
+        }
+        return stringBuilder.toString();
     }
 }
