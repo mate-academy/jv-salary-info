d311efdf0d967cfaef907dfd668ccddc735da061
Your_Name Your_Surname <your@email>
2024-12-25 23:05:48 +0200

    fixed naming, created constants

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,26 +5,30 @@ import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int DATE = 0;
+    private static final int NAME = 1;
+    private static final int WORK_HOURS = 2;
+    private static final int SALARY_PER_HOUR = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        LocalDate dateFromFormed = LocalDate.parse(dateFrom, FORMATTER);
-        LocalDate dateToFormed = LocalDate.parse(dateTo, FORMATTER);
-        StringBuilder builder = new StringBuilder();
+        LocalDate startDate = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate endDate = LocalDate.parse(dateTo, FORMATTER);
+        StringBuilder salaryAmount = new StringBuilder();
 
         for (String name: names) {
             int salary = 0;
             for (String info: data) {
                 String[] information = info.split(" ");
-                LocalDate dateFormed = LocalDate.parse(information[0], FORMATTER);
-                if (!dateFormed.isAfter(dateToFormed) && !dateFormed.isBefore(dateFromFormed)) {
-                    if (name.equals(information[1])) {
-                        salary += Integer.parseInt(information[2])
-                                * Integer.parseInt(information[3]);
+                LocalDate dateFormed = LocalDate.parse(information[DATE], FORMATTER);
+                if (!dateFormed.isAfter(endDate) && !dateFormed.isBefore(startDate)) {
+                    if (name.equals(information[NAME])) {
+                        salary += Integer.parseInt(information[WORK_HOURS])
+                                * Integer.parseInt(information[SALARY_PER_HOUR]);
                     }
                 }
             }
-            builder.append(System.lineSeparator()).append(name).append(" - ").append(salary);
+            salaryAmount.append(System.lineSeparator()).append(name).append(" - ").append(salary);
         }
-        return "Report for period " + dateFrom + " - " + dateTo + builder;
+        return "Report for period " + dateFrom + " - " + dateTo + salaryAmount;
     }
 }
