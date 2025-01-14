66dad08424ceb867f8d6ca32e3a87b5b931aed19
Petro <petroipatiy@gmail.com>
2025-01-14 02:13:33 +0000

    second update

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,48 +1,35 @@
 package core.basesyntax;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder result = new StringBuilder();
-        SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
-
         result.append("Report for period ").append(dateFrom).append(" - ")
                 .append(dateTo);
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDate startDate = LocalDate.parse(dateFrom, dateFormatter);
+        LocalDate endDate = LocalDate.parse(dateTo, dateFormatter);
 
-        Date startDate = null;
-        Date endDate = null;
-
-        try {
-            startDate = dateFormat.parse(dateFrom);
-            endDate = dateFormat.parse(dateTo);
-        } catch (Exception e) {
-            System.err.println("Error parsing dates: " + e.getMessage());
-            return "Invalid date format.";
-        }
         for (String name : names) {
             int totalSalary = 0;
             for (String record : data) {
                 String[] recordParts = record.split(" ");
+
                 String employeeName = recordParts[1];
-                Date workDate = null;
-                try {
-                    workDate = dateFormat.parse(recordParts[0]);
-                } catch (Exception e) {
-                    System.err.println("Error parsing work date for "
-                            + employeeName + ": " + e.getMessage());
-                    continue;
-                }
+                LocalDate workDate = LocalDate.parse(recordParts[0], dateFormatter);
                 int workHours = Integer.parseInt(recordParts[2]);
                 int incomePerHour = Integer.parseInt(recordParts[3]);
-                if (employeeName.equals(name) && !workDate.before(startDate)
-                        && !workDate.after(endDate)) {
+                if (employeeName.equals(name) && (workDate.equals(startDate)
+                        || workDate.equals(endDate) || (workDate.isAfter(startDate)
+                        && workDate.isBefore(endDate)))) {
                     totalSalary += workHours * incomePerHour;
                 }
             }
             result.append(System.lineSeparator()).append(name).append(" - ").append(totalSalary);
         }
+
         return result.toString();
 
     }
