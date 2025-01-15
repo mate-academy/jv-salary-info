f717a1353d83b593320582a15f81360251674f23
Hanna Voloshko <aa.voloshko09@gmail.com>
2025-01-16 00:50:37 +0200

    implemented logic in SalaryInfo class

---
+++ src/main/java/core/basesyntax/Main.java
@@ -0,0 +1,9 @@
+package core.basesyntax;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,34 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDate dateFromFormatted = LocalDate.parse(dateFrom, formatter);
+        LocalDate dateToFormatted = LocalDate.parse(dateTo, formatter);
+        StringBuilder builder = new StringBuilder();
+        builder.append("Report for period ").append(dateFrom).append(" - ").append(dateTo).append("\r\n");
+
+        for (String name : names) {
+            int totalSalary = 0;
+            for (String record : data) {
+                String[] splittedData = record.split(" ");
+                LocalDate workDate = LocalDate.parse(splittedData[0], formatter);
+                String employeeName = splittedData[1];
+                int hoursWorked = Integer.parseInt(splittedData[2]);
+                int hourlyRate = Integer.parseInt(splittedData[3]);
+
+                if (employeeName.equals(name) &&
+                        (workDate.isEqual(dateFromFormatted) || workDate.isAfter(dateFromFormatted)) &&
+                        (workDate.isEqual(dateToFormatted) || workDate.isBefore(dateToFormatted))) {
+                    totalSalary += hoursWorked * hourlyRate;
+                }
+            }
+            builder.append(name).append(" - ").append(totalSalary).append("\r\n");
+        }
+        return builder.toString().trim();
     }
+
 }
