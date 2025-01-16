2a99c7d9cb53c54fe1c40d7d7cced8986a5a0bd3
Kostyantyn Savchenko <kostyantyn.savchenko@gmail.com>
2025-01-16 19:56:58 +0200

    added asked changes v1.0

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -5,6 +5,24 @@ public class Main {
     private static final String DATE_TO = "10.08.2019";
     private static final String[] names = {"John", "Andrew", "Kate"};
     private static final String[] data = {
+            "25.04.2019 John 60 50",
+            "25.04.2019 Andrew 3 200",
+            "25.04.2019 Kate 10 100",
+
+            "26.04.2019 Andrew 3 200",
+            "26.04.2019 Kate 9 100",
+
+            "27.04.2019 John 7 100",
+            "27.04.2019 Kate 3 80",
+            "27.04.2019 Andrew 8 100"
+    };
+    private static final String[] dates = {
+            "24.04.2019",
+            "25.04.2019",
+            "26.04.2019",
+            "27.04.2019"
+    };
+    private static final String[] secondScriptArray = {
             "13.07.2019 John 60 50",
             "15.07.2019 Andrew 3 200",
             "15.07.2019 Kate 10 100",
@@ -16,15 +34,20 @@ public class Main {
             "08.08.2019 Kate 3 80",
             "11.08.2019 Andrew 8 100"
     };
-    private static final String[] dates = {
-            "24.04.2019",
-            "25.04.2019",
-            "26.04.2019",
-            "27.04.2019"
+    private static final String[] secondDates = {
+            "14.07.2019",
+            "10.08.2019",
     };
+
 
     public static void main(String[] args) {
         SalaryInfo salaryInfo = new SalaryInfo();
-        System.out.println(salaryInfo.getSalaryInfo(names, data, DATE_FROM, DATE_TO));
+        System.out.println(salaryInfo.getSalaryInfo(names, data, dates[0], dates[0]));
+        System.out.println(System.lineSeparator() + salaryInfo.getSalaryInfo(names, data, dates[0], dates[1]));
+        System.out.println(System.lineSeparator() + salaryInfo.getSalaryInfo(names, data, dates[0], dates[2]));
+        System.out.println(System.lineSeparator() + salaryInfo.getSalaryInfo(names, data, dates[0], dates[3]));
+        System.out.println(System.lineSeparator() + salaryInfo.getSalaryInfo(names, secondScriptArray, secondDates[0], secondDates[1]));
+
     }
 }
+
--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -10,30 +10,32 @@ public class SalaryInfo {
     private static final int NAME_INDEX = 1;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        int [] result = new int[names.length];
+        int[] result = new int[names.length];
         LocalDate fromDate = LocalDate.parse(dateFrom, PATTERN);
-
         LocalDate toDate = LocalDate.parse(dateTo, PATTERN);
+        int sumOfSalary = 0;
         for (int j = 0; j < names.length; j++) {
-            int sumOfSalary = 0;
             for (int i = 0; i < data.length; i++) {
                 StringBuilder stringBuilderData = new StringBuilder(Arrays
                         .toString(new String[]{data[i]}));
-                String [] splitData = stringBuilderData.toString().split(" ");
+                String[] splitData = stringBuilderData.toString().split(" ");
                 LocalDate date = LocalDate.parse(splitData[DATE_INDEX]
                         .substring(1), PATTERN);
                 String employeeName = splitData[NAME_INDEX];
-                if (names[j].equals(employeeName) && (date.isAfter(fromDate)
-                        && (date.isBefore(toDate)))) {
+                if (names[j].equals(employeeName) && (!date.isAfter(toDate)
+                        && !date.isBefore(fromDate))) {
                     sumOfSalary += Integer.parseInt(splitData[2]) * Integer.parseInt(splitData[3]
                             .replaceAll("]", ""));
                 }
                 result[j] = sumOfSalary;
             }
         }
-        return "Report for period" + " " + dateFrom + "-" + dateTo + "\n"
-                + names[0] + " " + "-" + " " + result[0] + "\n"
-                + names[1] + " " + "-" + " " + result[1] + "\n"
-                + names[2] + " " + "-" + " " + result[2] + "\n";
+        StringBuilder report = new StringBuilder();
+        String reportResult = null;
+        for (int y = 0; y < result.length; y++) {
+            reportResult = String.valueOf(report.append(System.lineSeparator()).append(names[y]).append(" - ")
+                    .append(result[y]));
+        }
+        return "Report for period" + " " + dateFrom + "-" + dateTo + reportResult;
     }
 }
