9d511f1fb79121f8e926d20889d58b720e7693b9
Roman Malyarenko <malyarenko.roman@gmail.com>
2025-01-29 15:56:47 +0200

    changed the conditions for comparing dates

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,31 +4,27 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private final DateTimeFormatter format = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final DateTimeFormatter FORMAT = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        StringBuilder infoBuilder = new StringBuilder("Report for period ")
-                .append(dateFrom)
-                .append(" - ")
-                .append(dateTo);
+        StringBuilder infoBuilder = new StringBuilder("Report for period ");
+        infoBuilder.append(dateFrom).append(" - ").append(dateTo);
+        LocalDate dateFromFormat = LocalDate.parse(dateFrom, FORMAT);
+        LocalDate dateToFormat = LocalDate.parse(dateTo, FORMAT);
         for (String name : names) {
             infoBuilder.append(System.lineSeparator()).append(name);
-            int sumSalary = 0;
+            int sumSalaryTemp = 0;
             for (String dataString : data) {
-                String[] dataSplit = dataString.split(" ");
-                if (dataSplit[1].equals(name)) {
-                    LocalDate localDate = LocalDate.parse(dataSplit[0], format);
-                    LocalDate fromDate = LocalDate.parse(dateFrom, format);
-                    LocalDate toDate = LocalDate.parse(dateTo, format);
-                    if (localDate.isEqual(fromDate)
-                            || localDate.isAfter(fromDate) && localDate.isBefore(toDate)
-                            || localDate.isEqual(toDate)) {
-                        sumSalary += Integer.parseInt(dataSplit[2])
-                                * Integer.parseInt(dataSplit[3]);
+                String[] dataStringSplit = dataString.split(" ");
+                if (dataStringSplit[1].equals(name)) {
+                    LocalDate localDate = LocalDate.parse(dataStringSplit[0], FORMAT);
+                    if (!localDate.isBefore(dateFromFormat) && !localDate.isAfter(dateToFormat)) {
+                        sumSalaryTemp += Integer.parseInt(dataStringSplit[2])
+                                * Integer.parseInt(dataStringSplit[3]);
                     }
                 }
             }
-            infoBuilder.append(" - ").append(sumSalary);
+            infoBuilder.append(" - ").append(sumSalaryTemp);
         }
         return infoBuilder.toString();
     }
