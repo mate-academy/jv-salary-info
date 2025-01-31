0a614a9506b80de8c8584228e0d34499a11ecda7
Liliya <klv.liliya.klv@gmail.com>
2025-01-31 22:02:45 +0200

    fixed checking if dailyDate and method findEmployeesIndex

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -6,13 +6,13 @@ import java.time.format.DateTimeFormatter;
 public class SalaryInfo {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
-    private int findEmployeesIndex(String[] names, String name) {
+    private int findEmployeesIndex(String[] names, String name) throws Exception {
         for (int i = 0; i < names.length; i++) {
             if (name.equals(names[i])) {
                 return i;
             }
         }
-        return -1;
+        throw new Exception("Name not found");
     }
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
@@ -23,13 +23,15 @@ public class SalaryInfo {
         for (String dailyData : data) {
             dailyDataInfo = dailyData.split(" ",4);
             LocalDate dailyDate = LocalDate.parse(dailyDataInfo[0], FORMATTER);
-            if (dailyDate.isAfter(fromDate)
-                    && dailyDate.isBefore(toDate)
-                    || dailyDate.isEqual(fromDate)
-                    || dailyDate.isEqual(toDate)) {
-                salary[findEmployeesIndex(names, dailyDataInfo[1])]
-                        += Integer.parseInt(dailyDataInfo[2])
-                        * Integer.parseInt(dailyDataInfo[3]);
+            if (!dailyDate.isAfter(toDate)
+                    && !dailyDate.isBefore(fromDate)) {
+                try {
+                    salary[findEmployeesIndex(names, dailyDataInfo[1])]
+                            += Integer.parseInt(dailyDataInfo[2])
+                            * Integer.parseInt(dailyDataInfo[3]);
+                } catch (Exception e) {
+                    System.out.println("Name " + dailyDataInfo[1] + " not found");
+                }
             }
         }
         StringBuilder salaryInfo = new StringBuilder("Report for period "
