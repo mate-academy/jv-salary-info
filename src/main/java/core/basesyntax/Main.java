76f5690ff722fa8ddfa1b47cd7624ec468c1cc5d
Weronika Rzekiecka <weronikarzekiecka@interia.pl>
2025-02-12 13:55:13 +0100

    poprawka

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,6 +2,7 @@ package core.basesyntax;
 
 public class Main {
     public static void main(String[] args) {
+
         SalaryService salaryService = new SalaryService();
 
         String[] names = {"John", "Andrew", "Kate"};
