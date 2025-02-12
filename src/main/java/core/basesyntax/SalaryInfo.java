0b254e2428f298f056a307159f7a8308ad0ba826
Weronika Rzekiecka <weronikarzekiecka@interia.pl>
2025-02-12 13:58:49 +0100

    info

---
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -0,0 +1,7 @@
+package core.basesyntax;
+
+public class SalaryInfo {
+    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
+        return null;
+    }
+}
