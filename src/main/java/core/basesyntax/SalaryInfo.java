63ad62f332c1f428e1ec375eefe2ac1e9d245c96
Roman Zaharii <romazagariy2003@gmail.com>
2025-02-27 11:57:02 +0200

    Added solution to method getSalaryInfo in class SalaryInfo.java

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,6 +5,7 @@ import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         String newLine = System.lineSeparator();
         StringBuilder salaryInfo = new StringBuilder(String.format("Report for period %s - %s",
@@ -15,7 +16,7 @@ public class SalaryInfo {
                 String[] employee = employeeData.split(" ");
                 LocalDate employeeDate = LocalDate.parse(employee[0], formatter);
                 LocalDate startDate = LocalDate.parse(dateFrom, formatter);
-                LocalDate endDate  = LocalDate.parse(dateTo, formatter);
+                LocalDate endDate = LocalDate.parse(dateTo, formatter);
                 if (!employeeDate.isBefore(startDate) && !employeeDate.isAfter(endDate)
                         && employee[1].equals(name)) {
                     employeeSalary += Integer.parseInt(employee[2]) * Integer.parseInt(employee[3]);
