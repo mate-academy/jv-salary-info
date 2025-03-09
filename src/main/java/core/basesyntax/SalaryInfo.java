af6e26f1a3d4087422f02a56f105b234bd6258d2
Acid <deadfoxua1337@gmail.com>
2025-03-09 21:10:55 +0100

    StyleFix

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,8 +4,7 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    final static DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy.MM.dd");
-
+    static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy.MM.dd");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate dateFromData = LocalDate.parse(dateFrom, FORMATTER);
@@ -24,13 +23,15 @@ public class SalaryInfo {
             if (!entryDate.isBefore(dateFromData) && !entryDate.isAfter(dateToDate)) {
                 for (int namescounter = 0; namescounter < names.length; namescounter++) {
                     if (names[namescounter].equals(splitData[namesPos])) {
-                        salary[namescounter] += Integer.parseInt(splitData[moneyPos]) * Integer.parseInt(splitData[hourPos]);
+                        salary[namescounter] += Integer.parseInt(splitData[moneyPos])
+                                * Integer.parseInt(splitData[hourPos]);
                     }
                 }
             }
         }
 
-        StringBuilder builder = new StringBuilder("Report for period " + dateFrom + " to " + dateTo + "\n");
+        StringBuilder builder = new StringBuilder("Report for period " + dateFrom
+                + " to " + dateTo + "\n");
         for (int i = 0; i < names.length; i++) {
             builder.append(names[i]).append(" - ").append(salary[i]).append("\n");
         }
