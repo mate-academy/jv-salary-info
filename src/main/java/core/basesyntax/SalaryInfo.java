343a8d620a95d3965724215b23fe44b82797e1a0
Sergii Nosachenko <54940595+sergii-nosachenko@users.noreply.github.com>
2025-03-13 15:06:28 +0200

    Update SalaryInfo.java

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -2,66 +2,35 @@ package core.basesyntax;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
 
 public class SalaryInfo {
-    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
     private static final int DATE_POSITION = 0;
     private static final int NAME_POSITION = 1;
     private static final int HOURS_POSITION = 2;
-    private static final int RATE_POSITION = 3;
+    private static final int INCOME_POSITION = 3;
+    private static final DateTimeFormatter Date = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        LocalDate dateBegin = null;
-        try {
-            dateBegin = LocalDate.parse(dateFrom, FORMATTER);
-        } catch (DateTimeParseException e) {
-            System.out.println(dateFrom + " date is incorrect!");
-        }
-        LocalDate dateEnd = null;
-        try {
-            dateEnd = LocalDate.parse(dateTo, FORMATTER);
-        } catch (DateTimeParseException e) {
-            System.out.println(dateTo + " date is incorrect!");
-        }
-        int[] salaries = new int[names.length];
-        for (String record: data) {
-            String[] employeeInfo = record.split(" ");
-            LocalDate currentDate = LocalDate.parse(employeeInfo[DATE_POSITION], FORMATTER);
-            String name = employeeInfo[NAME_POSITION];
-            int workHours = Integer.parseInt(employeeInfo[HOURS_POSITION]);
-            int rate = Integer.parseInt(employeeInfo[RATE_POSITION]);
-            int salary = workHours * rate;
-            for (int i = 0; i < names.length; i++) {
-                if (!currentDate.isBefore(dateBegin) && !currentDate.isAfter(dateEnd)
-                        && names[i].equals(name)) {
-                    salaries[i] += salary;
-                    break;
+        LocalDate from = LocalDate.parse(dateFrom, Date);
+        LocalDate to = LocalDate.parse(dateTo, Date);
+        StringBuilder builder = new StringBuilder("Report for period " + dateFrom + " - " + dateTo);
+        for (String name : names) {
+            int salary = 0;
+            for (String dataInfo : data) {
+                String[] info = dataInfo.split(" ");
+                if (name.equals(info[NAME_POSITION])) {
+                    LocalDate recordDate = LocalDate.parse(info[DATE_POSITION], Date);
+                    if (!recordDate.isBefore(from) && !recordDate.isAfter(to)) {
+                        salary += Integer.parseInt(info[INCOME_POSITION])
+                                * Integer.parseInt(info[HOURS_POSITION]);
+                    }
                 }
             }
-        }
-        StringBuilder builder = new StringBuilder("Report for period ");
-        builder.append(dateFrom).append(" - ").append(dateTo);
-        for (int i = 0; i < names.length; i++) {
-            builder.append(System.lineSeparator()).append(names[i]).append(" - ")
-                    .append(salaries[i]);
+            builder.append(System.lineSeparator())
+                    .append(name)
+                    .append(" - ")
+                    .append(salary);
         }
         return builder.toString();
-    }
-
-    public static void main(String[] args) {
-        SalaryInfo info = new SalaryInfo();
-        String[] names = {"John", "Andrew", "Kate"};
-        String[] data = {
-                "26.04.2019 John 4 50",
-                "05.04.2019 Andrew 3 200",
-                "10.04.2019 John 7 100",
-                "22.04.2019 Kate 9 100",
-                "26.04.2019 Andrew 3 150",
-                "26.04.2019 Kate 9 100"
-        };
-        String dateFrom = "01.04.2019";
-        String dateTo = "30.04.2019";
-        System.out.println(info.getSalaryInfo(names, data, dateFrom, dateTo));
     }
 }
