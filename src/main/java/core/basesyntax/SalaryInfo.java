699b76c2f11e575792d0c823ac1357cfdabe8423
Alexandros <olek4004@gmail.com>
2025-03-16 11:38:41 +0100

    fix

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,47 +1,50 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
 
-        StringBuilder sb = new StringBuilder("Report for period " + dateFrom.trim() +
-                " - " + dateTo.trim() + "\n");
-
-        int pay = 0;
+        StringBuilder sb = new StringBuilder("Report for period ")
+                .append(dateFrom.trim()).append(" - ")
+                .append(dateTo.trim());
 
         String[] dateFromArr = dateFrom.split("\\.");
         int dayFrom = Integer.parseInt(dateFromArr[0]);
         int monthFrom = Integer.parseInt(dateFromArr[1]);
         int yearFrom = Integer.parseInt(dateFromArr[2]);
+        LocalDate date1 = LocalDate.of(yearFrom, monthFrom, dayFrom);
 
-        String[] dateToArr = dateFrom.split("\\.");
+        String[] dateToArr = dateTo.split("\\.");
         int dayTo = Integer.parseInt(dateToArr[0]);
         int monthTo = Integer.parseInt(dateToArr[1]);
         int yearTo = Integer.parseInt(dateToArr[2]);
+        LocalDate date2 = LocalDate.of(yearTo, monthTo, dayTo);
 
         for (String n : names) {
+            int pay = 0;
 
             for (String d : data) {
+                String[] dataArr = d.split(" ");
+                String[] dayArr = dataArr[0].split("\\.");
 
-                if (d.contains(n)) {
+                int day = Integer.parseInt(dayArr[0]);
+                int month = Integer.parseInt(dayArr[1]);
+                int year = Integer.parseInt(dayArr[2]);
+                String name = dataArr[1];
+                int hours = Integer.parseInt(dataArr[2]);
+                int wage = Integer.parseInt(dataArr[3]);
 
-                    String[] dataArr = d.split(" ");
-                    int day = Integer.parseInt(dataArr[0]);
-                    int month = Integer.parseInt(dataArr[1]);
-                    int year = Integer.parseInt(dataArr[2]);
-                    int hours = Integer.parseInt(dataArr[4]);
-                    int wage = Integer.parseInt(dataArr[5]);
+                if (name.equals(n)) {
+                    LocalDate date = LocalDate.of(year, month, day);
 
-                    if (day >= dayFrom && day <= dayTo &&
-                        month >= monthFrom && month <= monthTo &&
-                        year >= yearFrom && year <= yearTo) {
-
+                    if (!date.isBefore(date1) && !date.isAfter(date2)) {
                         pay += hours * wage;
                     }
                 }
             }
 
-            sb.append(n).append(" - ").append(pay).append("\n");
-            pay = 0;
+            sb.append(System.lineSeparator()).append(n).append(" - ").append(pay);
         }
 
         return sb.toString();
