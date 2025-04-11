3a7eac50b70afed1a861800131129981ffb89832
Yulian Popovych <popovuch01@gmail.com>
2025-04-11 19:37:41 +0200

    checkStile update

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,50 +1,52 @@
 package core.basesyntax;
 
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    private static final DateTimeFormatter DATE_TIME_FORMATTER =
+            DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        Date date = new Date();
+
         StringBuilder result = new StringBuilder();
         result.append("Report for period ")
                 .append(dateFrom)
                 .append(" - ")
                 .append(dateTo);
-        int salaryCalculator = 0;
+
+        int salaryCalculator;
+
         for (String name : names) {
             salaryCalculator = 0;
             result.append(System.lineSeparator()).append(name)
                     .append(" - ");
-            for (String employeeData : data) {
 
-                String[] splitedDate = employeeData.split("\\s+");
+            for (String employeeData : data) {
+                String[] spllitedDate = employeeData.split("\\s+");
+
                 try {
-                    date = stringToDate(splitedDate[0]);
-                    if (name.equals(splitedDate[1])
-                            && date.after(stringToDate(dateFrom))
-                            | stringToDate(dateFrom).equals(date)
-                            && date.before(stringToDate(dateTo))
-                            | stringToDate(dateTo).equals(date)) {
-                        salaryCalculator += Integer.parseInt(splitedDate[2])
-                                * Integer.parseInt(splitedDate[3]);
+                    LocalDate startDate = stringToLocalDate(dateFrom);
+                    LocalDate endDate = stringToLocalDate(dateTo);
+                    LocalDate date = stringToLocalDate(spllitedDate[0]);
+
+                    if (name.equals(spllitedDate[1])
+                            && !date.isAfter(endDate)
+                            && !date.isBefore(startDate)) {
+                        salaryCalculator += Integer.parseInt(spllitedDate[2])
+                                * Integer.parseInt(spllitedDate[3]);
                     }
                 } catch (ParseException e) {
                     System.out.println("Date parsing exception");
                 }
-
             }
             result.append(salaryCalculator);
         }
         return result.toString();
     }
 
-    public Date stringToDate(String string) throws ParseException {
-        SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyyy", Locale.ENGLISH);
-        Date date = formatter.parse(string);
-        return date;
+    public LocalDate stringToLocalDate(String string) throws ParseException {
+        return LocalDate.parse(string, DATE_TIME_FORMATTER);
     }
 }
