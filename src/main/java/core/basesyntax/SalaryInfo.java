e66ca9dd608c6df7ba1350d958659b675513f61d
Denis Basarab <denya2007basarab@gmail.com>
2025-05-10 20:13:34 +0300

    Implement method getSalaryInfo

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,24 @@
 package core.basesyntax;
 
 public class SalaryInfo {
+    private ValidDate validDate = new ValidDate();
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        StringBuilder result = new StringBuilder("Report for period " + dateFrom + " - " + dateTo);
+        for (int j = 0; j < names.length; j++) {
+            result.append("\r\n").append(names[j]);
+            int allSalary = 0;
+            for (int i = 0; i < data.length; i++) {
+                String[] arrayWithDate = data[i].split(" ");
+                if (arrayWithDate[1].equals(names[j])) {
+                    if (validDate.checkValidDate(arrayWithDate[0], dateFrom, dateTo)) {
+                        allSalary += Integer.parseInt(arrayWithDate[2])
+                                * Integer.parseInt(arrayWithDate[3]);
+                    }
+                }
+            }
+            result.append(" - ").append(allSalary);
+        }
+        return result.toString();
     }
 }
