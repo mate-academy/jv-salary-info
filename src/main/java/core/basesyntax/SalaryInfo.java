f0bc06f01b2ac1a894424c0c6df4f190e00a1e98
Denis Basarab <denya2007basarab@gmail.com>
2025-05-10 20:31:06 +0300

    fixed the bug with empty line

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -6,7 +6,7 @@ public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder result = new StringBuilder("Report for period " + dateFrom + " - " + dateTo);
         for (int j = 0; j < names.length; j++) {
-            result.append("\r\n").append(names[j]);
+            result.append(System.lineSeparator()).append(names[j]);
             int allSalary = 0;
             for (int i = 0; i < data.length; i++) {
                 String[] arrayWithDate = data[i].split(" ");
