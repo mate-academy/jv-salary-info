d804a513468f5672a840ab146893a5c4d456f6a3
michal <mihacik228@gmail.com>
2025-05-19 01:27:46 +0200

    done task

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,44 @@
 package core.basesyntax;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
-    }
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
+        LocalDate startDate = LocalDate.parse(dateFrom, formatter); // перетвореня String в формат дати(dateFrom)
+        LocalDate endDate = LocalDate.parse(dateTo, formatter); // перетвореня String в формат дати(dateTo)
+
+        StringBuilder info = new StringBuilder("Report for period ")
+                .append(startDate.format(formatter))
+                .append(" - ")
+                .append(endDate.format(formatter));
+
+        int[] salaries = new int[names.length];
+
+        for (String records : data) {
+            String[] parts = records.split(" ");
+            LocalDate workDate = LocalDate.parse(parts[0], formatter);
+            String name = parts[1];
+            int hours = Integer.parseInt(parts[2]);
+            int salaryPerHour = Integer.parseInt(parts[3]);
+
+                if ((workDate.isEqual(startDate) || workDate.isAfter(startDate)) &&
+                        (workDate.isEqual(endDate) || workDate.isBefore(endDate))) {
+                    for (int i = 0; i < names.length; i++) {
+                        if (names[i].equals(name)) {
+                            salaries[i] += salaryPerHour * hours;
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < names.length; i++) {
+            info.append(System.lineSeparator())
+                    .append(names[i])
+                    .append(" - ")
+                    .append(salaries[i]);
+        }
+            return info.toString();
+        }
 }
+
